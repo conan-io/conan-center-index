de62c5abf2a9c7c8f3415726ff123fe1d9798ed1
Uilian Ries <uilianries@gmail.com>
2019-09-02 17:17:12 -0300

    Add Poco Project!
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ recipes/poco/ALL/test_package/conanfile.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import os
+from conans import ConanFile
+from conans import CMake
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        self.run(os.path.join("bin", "sample"), run_environment=True)
+        self.run(os.path.join("bin", "socket"), run_environment=True)
+        self.run(os.path.join("bin", "https"), run_environment=True)
+        self.run(os.path.join("bin", "sqlite"), run_environment=True)
