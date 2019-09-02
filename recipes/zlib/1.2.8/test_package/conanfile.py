1e606134df8ac570361b177bc08d9edfdacd60cd
Javier G. Sogo <jgsogo@gmail.com>
2019-09-02 11:38:11 +0200

    new recipe: zlib 1.2.8

---
+++ recipes/zlib/1.2.8/test_package/conanfile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from conans import ConanFile, CMake
+
+
+class DefaultNameConan(ConanFile):
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "cmake"
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        assert os.path.exists(os.path.join(self.deps_cpp_info["zlib"].rootpath, "licenses", "LICENSE"))
+        if "x86" in self.settings.arch:
+            self.run(os.path.join("bin", "test_package"), run_environment=True)
