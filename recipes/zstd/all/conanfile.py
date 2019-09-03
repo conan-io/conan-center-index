d3fb6d4d09b4b8782b3b79b69422588004112cd8
Uilian Ries <uilianries@gmail.com>
2019-09-03 11:25:48 -0300

    Add zstd
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ recipes/zstd/all/conanfile.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from conans import ConanFile, CMake, tools
+
+
+class ZstdConan(ConanFile):
+    name = "zstd"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/facebook/zstd"
+    description = "Zstandard - Fast real-time compression algorithm"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    topics = ("conan", "zstd", "compression", "algorithm", "decoder")
+    license = "BSD-3-Clause"
+    exports = ["LICENSE.md"]
+    exports_sources = ['CMakeLists.txt']
+    generators = 'cmake'
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.libstd
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["ZSTD_BUILD_PROGRAMS"] = False
+        cmake.definitions["ZSTD_BUILD_STATIC"] = not self.options.shared
+        cmake.definitions["ZSTD_BUILD_SHARED"] = self.options.shared
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
