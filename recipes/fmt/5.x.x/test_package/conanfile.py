0bd73cb5f1f9bb3827f09ff9c22d69ca14d97b3a
Uilian Ries <uilianries@gmail.com>
2019-09-04 08:56:51 -0300

    Add fmt
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ recipes/fmt/5.x.x/test_package/conanfile.py
@@ -0,0 +1,17 @@
+import os
+from conans import ConanFile, CMake, tools, RunEnvironment
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake_find_package_multi", "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["FMT_HEADER_ONLY"] = self.options["fmt"].header_only
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        self.run(os.path.join("bin","test_package"), run_environment=True)
+        self.run(os.path.join("bin","test_ranges"), run_environment=True)
