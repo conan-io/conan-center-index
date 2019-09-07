14efcfb18d8e9412e31d1c8e842a682aed72690c
SSE4 <tomskside@gmail.com>
2019-09-08 01:01:19 +0700

    - add libiconv
    Signed-off-by: SSE4 <tomskside@gmail.com>

---
+++ recipes/libiconv/all/test_package/conanfile.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        self.run(os.path.join("bin", "test_package"), run_environment=True)
