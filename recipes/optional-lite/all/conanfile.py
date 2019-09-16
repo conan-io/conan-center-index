b1cf899596fbebf05438583c0db7198e30e8599f
Javier G. Sogo <jgsogo@gmail.com>
2019-09-16 17:51:35 +0200

    add optional-lite/3.2.0 recipe

---
+++ recipes/optional-lite/all/conanfile.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from conans import ConanFile, CMake, tools
+
+
+class OptionalLite(ConanFile):
+    name = "optional-lite"
+    
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/martinmoene/optional-lite"
+    description = "A single-file header-only version of a C++17-like optional, a nullable object for C++98, C++11 and later"
+    author = "Martin Moene"
+    topics = ("conan", "cpp98", "cpp17", "optional", "optional-implementations")
+    license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
+    
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+
+    def package_info(self):
+        self.info.header_only()
