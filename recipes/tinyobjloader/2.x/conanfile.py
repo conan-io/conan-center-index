5e24a5e3ad3978793e5009e371ca5debfcbb0128
Florian Stellbrink <flo@stellbr.ink>
2019-09-17 20:41:23 +0200

    Add tinyobjloader recipe
    Recipe adapted from https://github.com/flostellbrink/conan-tinyobjloader

---
+++ recipes/tinyobjloader/2.x/conanfile.py
@@ -0,0 +1,50 @@
+import os
+from conans import ConanFile, CMake, tools
+
+
+class TinyObjLoaderConan(ConanFile):
+    name = "tinyobjloader"
+    description = "Tiny but powerful single file wavefront obj loader - Builds on Windows, Linux and Macos/OSX"
+    settings = "os", "arch", "build_type", "compiler"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {'shared': False, "fPIC": True}
+    license = "MTI"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/syoyo/tinyobjloader"
+    author = "Florian Stellbrink <flo@stellbr.ink>"
+    topics = ("conan", "tinyobjloader", "wavefront", "geometry")
+    exports = "LICENSE"
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def source(self):
+        entry = self.conan_data["sources"][self.version]
+        tools.get(entry["url"], sha256=entry["sha256"])
+        extracted_folder = self.name + '-' + self.version
+        os.rename(extracted_folder, self._source_subfolder)
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        if self.settings.os == "Windows" and self.options.shared:
+            cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
+        cmake.configure(build_dir=self._build_subfolder)
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*.pdb", dst="bin", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
