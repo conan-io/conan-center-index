10948709e1e63239bb00c1d1ba78b8527cec7b27
Flex Ferrum <flexferrum@gmail.com>
2019-09-21 00:19:33 +0300

    Add span-lite/0.5.0

---
+++ recipes/span-lite/all/conanfile.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from conans import ConanFile, tools
+
+
+class SpanLite(ConanFile):
+    name = "span-lite"
+    
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/martinmoene/span-lite"
+    description = "span lite - A C++20-like span for C++98, C++11 and later in a single-file header-only library"
+    author = "Martin Moene"
+    topics = ("conan", "cpp98", "cpp11", "cpp14", "cpp17", "span", "span-implementations")
+    license = "BSL-1.0"
+    no_copy_source = True
+    
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+
+    def package_info(self):
+        self.info.header_only()
