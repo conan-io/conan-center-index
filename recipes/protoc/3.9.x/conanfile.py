25b16b2d22a486df138705bd99b7a985b80e0846
Uilian Ries <uilianries@gmail.com>
2019-09-20 15:33:28 -0300

    Add Protobuf protoc 3.9.1
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ recipes/protoc/3.9.x/conanfile.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import os
+
+from conans import ConanFile, CMake, tools
+
+
+class ProtocConan(ConanFile):
+    name = "protoc"
+    description = "Protocol Buffers - Google's data interchange format"
+    topics = ("conan", "protobuf", "protocol-buffers", "protocol-compiler", "serialization", "rpc", "protocol-compiler")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/protocolbuffers/protobuf"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    license = "BSD-3-Clause"
+    exports_sources = ["CMakeLists.txt", "protoc.patch"]
+    generators = "cmake"
+    short_paths = True
+    settings = "os_build", "arch_build", "compiler", "arch"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_folder = "protobuf-" + self.version
+        os.rename(extracted_folder, self._source_subfolder)
+
+    def requirements(self):
+        self.requires.add("protobuf/3.9.1", private=True)
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["protobuf_BUILD_TESTS"] = False
+        cmake.definitions["protobuf_WITH_ZLIB"] = False
+        if self.settings.compiler == "Visual Studio":
+            cmake.definitions["protobuf_MSVC_STATIC_RUNTIME"] = "MT" in self.settings.compiler.runtime
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+
+    def build(self):
+        tools.patch(base_path=self._source_subfolder, patch_file="protoc.patch")
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_id(self):
+        del self.info.settings.compiler
+        del self.info.settings.arch
+        self.info.include_build_settings()
+
+    def package_info(self):
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(bindir))
+        self.env_info.PATH.append(bindir)
+        self.cpp_info.builddirs = [os.path.join(self.package_folder, "lib", "cmake", "protoc")]
+        protoc = "protoc.exe" if self.settings.os_build == "Windows" else "protoc"
+        self.env_info.PROTOC_BIN = os.path.normpath(os.path.join(self.package_folder, "bin", protoc))
