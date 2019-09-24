3b07f5fb9d5fbab3e3f24e81c03a93b0d7745b24
ericLemanissier <eric.lemanissier@gmail.com>
2019-09-25 01:05:18 +0200

    add libpq/11.5

---
+++ recipes/libpq/all/conanfile.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, AutoToolsBuildEnvironment, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+
+class LibpqConan(ConanFile):
+    name = "libpq"
+    description = "The library used by all the standard PostgreSQL tools."
+    topics = ("conan", "libpq", "postgresql", "database", "db")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://www.postgresql.org/docs/current/static/libpq.html"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    license = "PostgreSQL"
+    exports_sources = ["CMakeLists.txt"]
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_zlib": [True, False],
+        "with_openssl": [True, False],
+        "disable_rpath": [True, False]}
+    default_options = {'shared': False, 'fPIC': True, 'with_zlib': True, 'with_openssl': False, 'disable_rpath': False}
+    generators = "cmake"
+    _autotools = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _is_clang8_x86(self):
+        return self.settings.os == "Linux" and \
+               self.settings.compiler == "clang" and \
+               self.settings.compiler.version == "8" and \
+               self.settings.arch == "x86"
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+        if self.settings.os == 'Windows' and self.options.shared:
+            raise ConanInvalidConfiguration("libpq can not be built as shared library on Windows")
+
+    def requirements(self):
+        if self.options.with_zlib:
+            self.requires.add("zlib/1.2.11")
+        if self.options.with_openssl:
+            self.requires.add("openssl/1.0.2s")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = "postgresql-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_autotools(self):
+        if not self._autotools:
+            self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+            args = ['--without-readline']
+            args.append('--with-zlib' if self.options.with_zlib else '--without-zlib')
+            args.append('--with-openssl' if self.options.with_openssl else '--without-openssl')
+            if self.options.disable_rpath:
+                args.append('--disable-rpath')
+            if self._is_clang8_x86:
+                self._autotools.flags.append("-msse2")
+            with tools.chdir(self._source_subfolder):
+                self._autotools.configure(args=args)
+        return self._autotools
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            cmake = self._configure_cmake()
+            cmake.definitions["USE_OPENSSL"] = self.options.with_openssl
+            cmake.definitions["USE_ZLIB"] = self.options.with_zlib
+            cmake.build()
+        else:
+            autotools = self._configure_autotools()
+            with tools.chdir(os.path.join(self._source_subfolder, "src", "backend")):
+                autotools.make(target="generated-headers")
+            with tools.chdir(os.path.join(self._source_subfolder, "src", "common")):
+                autotools.make()
+            with tools.chdir(os.path.join(self._source_subfolder, "src", "include")):
+                autotools.make()
+            with tools.chdir(os.path.join(self._source_subfolder, "src", "interfaces", "libpq")):
+                autotools.make()
+            with tools.chdir(os.path.join(self._source_subfolder, "src", "bin", "pg_config")):
+                autotools.make()
+
+    def package(self):
+        self.copy(pattern="COPYRIGHT", dst="licenses", src=self._source_subfolder)
+        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            cmake = self._configure_cmake()
+            cmake.install()
+        else:
+            autotools = self._configure_autotools()
+            with tools.chdir(os.path.join(self._source_subfolder, "src", "common")):
+                autotools.install()
+            with tools.chdir(os.path.join(self._source_subfolder, "src", "include")):
+                autotools.install()
+            with tools.chdir(os.path.join(self._source_subfolder, "src", "interfaces", "libpq")):
+                autotools.install()
+            with tools.chdir(os.path.join(self._source_subfolder, "src", "bin", "pg_config")):
+                autotools.install()
+            tools.rmdir(os.path.join(self.package_folder, "include", "postgresql", "server"))
+            self.copy(pattern="*.h", dst=os.path.join("include", "catalog"), src=os.path.join(self._source_subfolder, "src", "include", "catalog"))
+        self.copy(pattern="*.h", dst=os.path.join("include", "catalog"), src=os.path.join(self._source_subfolder, "src", "backend", "catalog"))
+        tools.rmdir(os.path.join(self.package_folder, "share"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        self.env_info.PostgreSQL_ROOT = self.package_folder
+        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
+        elif self.settings.os == "Windows":
+            self.cpp_info.libs.extend(["ws2_32", "secur32", "advapi32", "shell32", "crypt32"])
