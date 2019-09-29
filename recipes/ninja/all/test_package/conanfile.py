3189464ebdc29d2fe23c25af03722c917e8faa28
SSE4 <tomskside@gmail.com>
2019-09-29 16:47:22 +0700

    Update conanfile.py

--- recipes/ninja/all/test_package/conanfile.py
+++ recipes/ninja/all/test_package/conanfile.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from conans import ConanFile
 import os
 
@@ -8,4 +5,4 @@ import os
 class TestPackage(ConanFile):
 
     def test(self):
-        self.run("ninja --version")
+        self.run("ninja --version", run_environment=True)
