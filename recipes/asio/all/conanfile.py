bca7f4c64f2f727d2a2a2f48f9fc0167e183f3a7
Javier G. Sogo <javierg@jfrog.com>
2019-09-30 11:26:54 +0200

    just the header only

--- recipes/asio/all/conanfile.py
+++ recipes/asio/all/conanfile.py
@@ -3,7 +3,6 @@
 
 import os
 from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
 
 
 class Asio(ConanFile):
@@ -15,32 +14,10 @@ class Asio(ConanFile):
     topics = ("conan", "asio", "network", "io", "low-level")
     license = "BSL-1.0"
 
-    options = {
-        "standalone": [True, False],
-        "with_boost_regex": [True, False],
-        "with_openssl": [True, False]
-    }
-    default_options = {
-        "standalone": True,
-        "with_boost_regex": False,
-        "with_openssl": False
-    }
-
-    settings = "os"
+    settings = "os", "arch", "compiler", "build_type"
 
     no_copy_source = True
     _source_subfolder = "source_subfolder"
-
-    def configure(self):
-        if self.options.standalone and self.options.with_boost_regex:
-            raise ConanInvalidConfiguration("'standalone' and 'with_boost_regex' are mutually exclusive! Please disable one of them.")
-
-    def requirements(self):
-        if self.options.with_boost_regex:
-            self.requires.add("boost/1.69.0")
-
-        if self.options.with_openssl:
-            self.requires.add("openssl/1.0.2t")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -56,10 +33,7 @@ class Asio(ConanFile):
         self.copy(pattern="*.ipp", dst="include", src=include_dir)
 
     def package_info(self):
-        if self.options.standalone:
-            self.cpp_info.defines.append('ASIO_STANDALONE')
-        if self.settings.os == 'Linux':
-            self.cpp_info.libs.append('pthread')
+        self.cpp_info.defines.append('ASIO_STANDALONE')
 
     def package_id(self):
         self.info.header_only()
