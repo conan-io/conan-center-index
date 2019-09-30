08760a017a6f5216d38cad1fc812563c08fb5c8f
ericLemanissier <ericLemanissier@users.noreply.github.com>
2019-09-30 12:20:14 +0200

    simplify test recipe

--- recipes/mysql-connector-c/all/test_package/conanfile.py
+++ recipes/mysql-connector-c/all/test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools, RunEnvironment
+from conans import ConanFile, CMake, tools
 import os
 
 
@@ -12,11 +12,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
+        if not tools.cross_building(self.settings):
             bin_path = os.path.join("bin", "test_package")
-            if self.settings.os == "Windows":
-                self.run(bin_path)
-            elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
-            else:
-                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+            self.run(bin_path, run_environment=True)
