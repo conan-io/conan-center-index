f8f44515a062c98b17db0b78b4d97fcaf8c18a4b
theirix <theirix@gmail.com>
2019-10-01 00:29:32 +0300

    Add yaml-cpp 0.6.3

---
+++ recipes/yaml-cpp/all/conanfile.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools
+import os
+
+
+class YamlCppConan(ConanFile):
+    name = "yaml-cpp"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/jbeder/yaml-cpp"
+    topics = ("conan", "yaml")
+    description = "A YAML parser and emitter in C++"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    license = "MIT"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def configure(self):
+        if self.settings.compiler == "Visual Studio" and self.settings.compiler.version == "12":
+            raise Exception("Visual Studio 12 not supported: Library needs C++11 standard")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["YAML_CPP_BUILD_TESTS"] = False
+        cmake.definitions["YAML_CPP_BUILD_CONTRIB"] = True
+        cmake.definitions["YAML_CPP_BUILD_TOOLS"] = False
+        cmake.definitions["YAML_BUILD_SHARED_LIBS"] = self.options.shared
+        if self.settings.compiler == "Visual Studio":
+            cmake.definitions["MSVC_SHARED_RT"] = "MD" in self.settings.compiler.runtime
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        # drop pc and cmake files
+        tools.rmdir(os.path.join(self.package_folder, 'lib', 'cmake'))
+        tools.rmdir(os.path.join(self.package_folder, 'CMake'))
+        tools.rmdir(os.path.join(self.package_folder, 'lib', 'pkgconfig'))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.builddirs = ["lib/cmake"]
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append('m')
+        if self.settings.compiler == 'Visual Studio':
+            self.cpp_info.defines.append('_NOEXCEPT=noexcept')
