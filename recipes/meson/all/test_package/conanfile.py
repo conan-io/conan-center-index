f73080de38e5f01e4db6f69c2a2235fab0247dad
SSE4 <tomskside@gmail.com>
2019-10-02 00:24:44 +0700

    Update conanfile.py

--- recipes/meson/all/test_package/conanfile.py
+++ recipes/meson/all/test_package/conanfile.py
@@ -7,8 +7,6 @@ class TestPackageConan(ConanFile):
     generators = "pkg_config"
 
     def build(self):
-        # FIXME: meson.build:1:0: ERROR:  prefix value 'None' must be an absolute path
-        self.package_folder = os.getcwd()
         meson = Meson(self)
         meson.configure(build_folder="build")
         meson.build()
