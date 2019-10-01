4e52c2cf13b12af530f1379d90f1fe9b6ba852e1
Javier G. Sogo <javierg@jfrog.com>
2019-10-01 23:38:43 +0200

    simplify test_package

--- recipes/libxml2/all/test_package/conanfile.py
+++ recipes/libxml2/all/test_package/conanfile.py
@@ -15,13 +15,7 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            bin_path = os.path.join("bin", "test_package")
-            arg_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "books.xml"))
-            bin_arg_path = "%s %s" % (bin_path, arg_path)
-            if self.settings.os == "Windows":
-                self.run(bin_arg_path)
-            elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_arg_path))
-            else:
-                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_arg_path))
+        bin_path = os.path.join("bin", "test_package")
+        arg_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "books.xml"))
+        bin_arg_path = "%s %s" % (bin_path, arg_path)
+        self.run(bin_arg_path, run_environment=True)
