d72989184e24455c4bd37609035946a36e49b04e
Javier G. Sogo <javierg@jfrog.com>
2019-10-02 01:04:47 +0200

    compile with actual examplee

--- recipes/cpr/all/test_package/test_package.cpp
+++ recipes/cpr/all/test_package/test_package.cpp
@@ -1,8 +1,15 @@
-#include <cstdlib>
+
 #include <iostream>
+#include <cpr/cpr.h>
 
 int main()
 {
-    std::cout << "Bincrafters\n";
+    auto r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+                      cpr::Authentication{"user", "pass"},
+                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+    std::cout << r.status_code << std::endl;                  // 200
+    std::cout << r.header["content-type"] << std::endl;       // application/json; charset=utf-8
+    std::cout << r.text << std::endl;  
+
     return EXIT_SUCCESS;
 }
