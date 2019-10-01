04bb1d0e5e801b5b3ae1ef5937a028fd5ac8c9cf
Javier G. Sogo <javierg@jfrog.com>
2019-10-02 01:04:47 +0200

    compile with actual examplee

--- recipes/cpr/all/conanfile.py
+++ recipes/cpr/all/conanfile.py
@@ -7,51 +7,59 @@ import os
 
 class CprConan(ConanFile):
     name = "cpr"
-    version = "1.3.0"
+
     url = "https://github.com/DEGoodmanWilson/conan-cpr"
     description = "Keep it short"
     license = "https://github.com/whoshuu/cpr/blob/1.3.0/LICENSE"
-    exports_sources = ["CMakeLists.txt", "LICENSE"]
+    exports_sources = ["CMakeLists.txt", "patches/*"]
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False],
-               "use_ssl": [True, False]}
-    default_options = "shared=False", "libcurl:with_ldap=False", "use_ssl=True"
+               "use_ssl": [True, False],
+               "fPIC": [True, False]}
+    default_options = {"shared": False,
+                       "libcurl:with_ldap": False,
+                       "use_ssl": True,
+                       "fPIC": True}
 
-    requires = "libcurl/7.56.1"
     generators = "cmake"
+    _source_subfolder = "source_subfolder"
 
     def requirements(self):
+        self.requires("libcurl/7.66.0")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
         if self.options.use_ssl:
             self.options["libcurl"].with_openssl = True
         else:
             self.options["libcurl"].with_openssl = False
 
-        if tools.os_info.is_macos:
-            if "libcurl" in self.requires:
-                del self.requires["libcurl"]
-
-
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, "sources")
+        os.rename(extracted_dir, self._source_subfolder)
+
+        for it in self.conan_data["patches"][self.version]:
+            tools.patch(patch_file=it, base_path=os.path.join(self.source_folder, self._source_subfolder))
 
     def build(self):
         cmake = CMake(self)
         cmake.definitions["USE_SYSTEM_CURL"] = True # Force CPR to not try to build curl itself from a git submodule
         cmake.definitions["BUILD_CPR_TESTS"] = False
+        cmake.definitions["GENERATE_COVERAGE"] = False
+        cmake.definitions["USE_SYSTEM_GTEST"] = False
         cmake.configure()
         cmake.build()
 
     def package(self):
-        with tools.chdir("sources"):
-            self.copy(pattern="LICENSE")
-            self.copy(pattern="*", dst="include", src="sources/include", keep_path=True)
-            self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="LICENSE", src=self._source_subfolder, dst="licenses")
+        self.copy(pattern="*.h", dst="include", src=os.path.join(self._source_subfolder, 'include'), keep_path=True)
+        self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
