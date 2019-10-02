c9c860a83c3d3d80f58ad10e1cf00d40690a9126
SSE4 <tomskside@gmail.com>
2019-10-02 14:23:40 +0700

    Update test_package.cpp

--- recipes/fmt/all/test_package/test_package.cpp
+++ recipes/fmt/all/test_package/test_package.cpp
@@ -42,7 +42,7 @@ int main() {
 
     fmt::memory_buffer buf;
     fmt::format_to(buf, "{}", 2.7182818);
-    fmt::print("Euler number: {}\n", buf.data());
+    fmt::print("Euler number: {}\n", fmt::to_string(buf.data()));
 
     const std::string date = fmt::format("The date is {}\n", Date(2012, 12, 9));
     fmt::print(date);
