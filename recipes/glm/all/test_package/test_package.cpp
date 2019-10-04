802a7934b137152c4476c1e4ec57f6b3029c84e6
SSE4 <tomskside@gmail.com>
2019-10-04 15:28:20 +0700

    - fix assertion (https://github.com/g-truc/glm/issues/733)
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- recipes/glm/all/test_package/test_package.cpp
+++ recipes/glm/all/test_package/test_package.cpp
@@ -4,7 +4,7 @@
 int main (int argc, char * argv[]) {
     glm::vec4 position = glm::vec4(glm::vec3(0.0), 1.0);
     glm::mat4 model = glm::mat4(1.0);
-    model[4] = glm::vec4(1.0, 1.0, 0.0, 1.0);
+    model[3] = glm::vec4(1.0, 1.0, 0.0, 1.0);
     glm::vec4 transformed = model * position;
 
     return EXIT_SUCCESS;
