74e5a09794c4d3e2e32b249cf6f5ad6184c56061
Morwenn <morwenn29@hotmail.fr>
2019-10-09 00:06:09 +0200

    Add cpp-sort 1.5.1

---
+++ recipes/cpp-sort/all/conanfile.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from conans import CMake, ConanFile, tools
+
+
+class CppSortConan(ConanFile):
+    name = "cpp-sort"
+    description = "Additional sorting algorithms & related tools"
+    topics = "conan", "cpp-sort", "sorting", "algorithms"
+    url = "https://github.com/Morwenn/cpp-sort"
+    homepage = "https://github.com/Morwenn/cpp-sort"
+    author = "Morwenn <morwenn29@hotmail.fr>"
+    license = "MIT"
+    exports = "license.txt"
+    no_copy_source = True
+
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        # Install with CMake
+        cmake = CMake(self)
+        cmake.definitions["BUILD_TESTING"] = "OFF"
+        cmake.configure(_source_subfolder)
+        cmake.install()
+
+        # Copy license file
+        self.copy("license.txt", dst="licenses", src=self._source_subfolder)
+
+        # Remove CMake config files (only files in lib)
+        tools.rmdir(os.path.join(self.package_folder, "lib"))
+
+    def package_id(self):
+        self.info.header_only()
