eda6295c4d5cfa725bf2f00a7dd15f35c306018c
Uilian Ries <uilianries@gmail.com>
2019-10-22 11:04:07 -0300

    Fix zlib name for CMake
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/zlib/1.2.11/conanfile.py
+++ recipes/zlib/1.2.11/conanfile.py
@@ -163,3 +163,5 @@ class ZlibConan(ConanFile):
             if self.options.shared:
                 self.cpp_info.defines.append('MINIZIP_DLL')
         self.cpp_info.libs.append('zlib' if self.settings.os == "Windows" else "z")
+        self.cpp_info.name = "ZLIB"
+
