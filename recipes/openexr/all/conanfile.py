faacd2c9b275647cb28aeb8339b526353d6d6a47
Ivan Kulikov <ivankulikov187@gmail.com>
2019-10-22 12:02:27 +0300

    Try fix remarks.find zlib patch was added.

--- recipes/openexr/all/conanfile.py
+++ recipes/openexr/all/conanfile.py
@@ -1,8 +1,5 @@
 from conans import ConanFile, CMake, tools
 import os
-import shutil
-
-
 class OpenEXRConan(ConanFile):
     name = "openexr"
     url = "https://github.com/conan-io/conan-center-index"
@@ -13,10 +10,8 @@ class OpenEXRConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False], "namespace_versioning": [True, False]}
     default_options = {"shared": False, "namespace_versioning": True, 'fPIC': True}
-    generators = "cmake"
-
-    exports_sources = ["CMakeLists.txt"]
-
+    generators = "cmake", "cmake_find_package"
+    exports_sources = ["CMakeLists.txt", "patches/0001-find-zlib.patch"]
     _source_subfolder = "source_subfolder"
 
     def source(self):
@@ -33,41 +28,41 @@ class OpenEXRConan(ConanFile):
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["ZLIB_LIBRARY"] = self.deps_cpp_info["zlib"].libs[0]
-        cmake.definitions["ZLIB_INCLUDE_DIR"] = self.deps_cpp_info["zlib"].include_paths[0]
         cmake.definitions["OPENEXR_BUILD_PYTHON_LIBS"] = False
         cmake.definitions["BUILD_TESTING"] = False
         cmake.definitions["OPENEXR_VIEWERS_ENABLE"] = False
         cmake.definitions["OPENEXR_BUILD_SHARED"] = self.options.shared
-        cmake.definitions["OPENEXR_BUILD_STATIC"] = not bool(self.options.shared)
+        cmake.definitions["OPENEXR_BUILD_STATIC"] = not self.options.shared
         cmake.definitions["OPENEXR_NAMESPACE_VERSIONING"] = self.options.namespace_versioning
         cmake.definitions["OPENEXR_ENABLE_TESTS"] = False
         cmake.definitions["OPENEXR_FORCE_CXX03"] = True
         cmake.definitions["OPENEXR_BUILD_UTILS"] = False
         cmake.definitions["ENABLE_TESTS"] = False
         cmake.definitions["OPENEXR_BUILD_TESTS"] = False
+
         cmake.configure(source_folder=self._source_subfolder)
         return cmake
 
     def build(self):
-        cmake = CMake(self)
-        cmake = self._configure_cmake()
-        cmake.install()
+        with tools.chdir(self.source_folder):
+            tools.patch(base_path=self._source_subfolder, patch_file="patches/0001-find-zlib.patch")
+            cmake = self._configure_cmake()
+            cmake.build()
 
     def package(self):
-        cmake = CMake(self)
         cmake = self._configure_cmake()
         cmake.install()
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses", keep_path=False)
+        self.copy("LICENSE", src=self._source_subfolder, dst=self.package_folder, keep_path=False)
+        tools.rmdir(os.path.join(self.package_folder, "share"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         parsed_version = self.version.split('.')
         version_suffix = "-%s_%s" % (parsed_version[0], parsed_version[1]) if self.options.namespace_versioning else ""
-        if not self.options.shared:
-            version_suffix += "_s"
+        #if not self.options.shared:
+        #    version_suffix += "_s"
         if self.settings.compiler == 'Visual Studio' and self.settings.build_type == 'Debug':
             version_suffix += "_d"
-
         self.cpp_info.libs = ['IlmImf' + version_suffix,
                               'IlmImfUtil' + version_suffix,
                               'IlmThread' + version_suffix,
@@ -79,4 +74,4 @@ class OpenEXRConan(ConanFile):
             self.cpp_info.defines.append("OPENEXR_DLL")
 
         if self.settings.os != "Windows":
-            self.cpp_info.cppflags = ["-pthread"]
+            self.cpp_info.cppflags = ["pthread"]
