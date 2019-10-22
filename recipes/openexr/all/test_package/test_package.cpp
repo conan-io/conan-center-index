91d0bf9295be9fbe4be32dc93c7f3d9da2daf54c
Ivan Kulikov <ivankulikov187@gmail.com>
2019-10-22 13:37:38 +0300

    openexr 2.4.0: try fix test_package

--- recipes/openexr/all/test_package/test_package.cpp
+++ recipes/openexr/all/test_package/test_package.cpp
@@ -1,9 +1,18 @@
 #include <string>
 #include <iostream>
+#include <ImfRgbaFile.h>
+#include <ImfArray.h>
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	std::cout << "Hello, World!" << std::endl;
+	int const width = 512;
+	int const height = 512;
+
+	Imf::RgbaOutputFile file_exr(
+		"example.exr",
+		width, height,
+		Imf::WRITE_RGBA);
+	file_exr.writePixels(height);
+
 	return 0;
 }
