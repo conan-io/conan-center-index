88c980fc3689ed7fa23638d434c7522efd1607f2
ericLemanissier <eric.lemanissier@gmail.com>
2019-10-23 21:41:56 +0200

    add jasper 2.0.14

---
+++ recipes/jasper/all/test_package/conanfile.py
@@ -0,0 +1,22 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class JasperTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='bin', src='lib')
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            os.chdir("bin")
+            command = ".%sexample" % os.sep
+            self.run(command)
