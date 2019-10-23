056a97f26d650be0de0f975007385cf9f7348cbe
Uilian Ries <uilianries@gmail.com>
2019-10-23 10:43:42 -0300

    Add cmake_find_package to find fmt
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/spdlog/1.4.x/conanfile.py
+++ recipes/spdlog/1.4.x/conanfile.py
@@ -11,7 +11,7 @@ class SpdlogConan(ConanFile):
     topics = ("conan", "spdlog", "logging", "header-only")
     license = "MIT"
     exports_sources = ["CMakeLists.txt", "patches/*"]
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False],
                "fPIC": [True, False],
