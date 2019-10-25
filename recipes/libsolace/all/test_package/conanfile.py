eee5724d0e5f800fb5724fddfd51d5537e2cef0f
abbyssoul <abbyssoul@gmail.com>
2019-10-25 12:22:25 +1100

    Removed imports as per PR comments

--- recipes/libsolace/all/test_package/conanfile.py
+++ recipes/libsolace/all/test_package/conanfile.py
@@ -13,11 +13,6 @@ class SolaceTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-
     def test(self):
         if not tools.cross_building(self.settings):
             with tools.chdir("bin"):
