3a59d4457cbbc8930d487c52fb73c73a255aa516
Ivan Kulikov <ivankulikov187@gmail.com>
2019-10-27 12:01:34 +0300

    openexr: test_package was added

--- recipes/openexr/all/test_package/test_package.cpp
+++ recipes/openexr/all/test_package/test_package.cpp
@@ -1,7 +1,18 @@
-#include <string>
 #include <iostream>
+#include <cstdlib>
 
-int main(int argc, char **argv)
+#include <ImfRgbaFile.h>
+#include <ImfArray.h>
+#include <half.h>
+
+int main (int argc, char *argv[])
 {
-	return 0;
+    if (argc < 2)
+        return EXIT_FAILURE;
+    Imf::RgbaInputFile input_file(argv[1]);
+    const Imf::Header& header = input_file.header();
+    size_t width = header.dataWindow().max.x - header.dataWindow().min.x + 1;
+    size_t height = header.dataWindow().max.y - header.dataWindow().min.y + 1;
+    std::cout << "OpenEXR images size is " << width << "x" << height << std::endl;
+    return EXIT_SUCCESS;
 }
