f4e1fd0cd4d9c23907986e40d703ad34dadf7640
Eric Lemanissier <eric.lemanissier@gmail.com>
2019-11-04 10:05:28 +0100

    add missing macosframework

--- recipes/openal/all/conanfile.py
+++ recipes/openal/all/conanfile.py
@@ -64,7 +64,7 @@ class OpenALConan(ConanFile):
         if self.settings.os == 'Linux':
             self.cpp_info.libs.extend(['dl', 'm'])
         elif self.settings.os == 'Macos':
-            self.cpp_info.frameworks.extend(['AudioToolbox', 'CoreAudio'])
+            self.cpp_info.frameworks.extend(['AudioToolbox', 'CoreAudio', 'CoreFoundation'])
         self.cpp_info.includedirs = ["include", "include/AL"]
         if not self.options.shared:
             self.cpp_info.defines.append('AL_LIBTYPE_STATIC')
