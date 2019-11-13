7527800b94a3d1635c5fd7288981be69e6ada553
SSE4 <tomskside@gmail.com>
2019-11-13 11:42:22 +0700

    Update recipes/fftw/all/conanfile.py
    Co-Authored-By: Uilian Ries <uilianries@gmail.com>

--- recipes/fftw/all/conanfile.py
+++ recipes/fftw/all/conanfile.py
@@ -64,3 +64,5 @@ class FFTWConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["m"]
