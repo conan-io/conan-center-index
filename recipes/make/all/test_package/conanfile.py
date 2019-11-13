3d52134eedcec27cbb7004ed6fc71191d6ec5ce7
SSE4 <tomskside@gmail.com>
2019-11-13 11:41:49 +0700

    - use tools.remove_from_path

--- recipes/make/all/test_package/conanfile.py
+++ recipes/make/all/test_package/conanfile.py
@@ -6,6 +6,6 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def test(self):
-        with tools.chdir(self.source_folder):
+        with tools.chdir(self.source_folder), tools.remove_from_path("make"):
             env_build = AutoToolsBuildEnvironment(self)
             env_build.make(args=["love"])
