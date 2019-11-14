877f093a15f74e4e1fc6b7463f9b61c0ed893b59
Uilian Ries <uilianries@gmail.com>
2019-11-14 10:58:34 -0300

    Fix build
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libsigcpp/3.0.0/conanfile.py
+++ recipes/libsigcpp/3.0.0/conanfile.py
@@ -1,27 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import os
 from conans import ConanFile, CMake, tools
 
 class LibSigCppConan(ConanFile):
     name = "libsigcpp"
     version = "3.0.0"
-    url = "https://github.com/libsigcplusplus/libsigcplusplus"
-    homepage = "https://libsigcplusplus.github.io/libsigcplusplus/"
-    author = "Conan Community"
-    license = "LGPL"
+    homepage = "https://github.com/libsigcplusplus/libsigcplusplus"
+    url = "https://github.com/conan-io/conan-center-index"
+    license = "LGPL-3.0"
     description = "libsigc++ implements a typesafe callback system for standard C++."
     topics = ("conan", "libsigcpp", "callback")
     settings = "os", "compiler", "arch", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False], "build_executable": [True, False], "disable_deprecated": [True, False]}
-    default_options = {"shared": False, "fPIC": True, "build_executable": True, "disable_deprecated": False}
-    exports_sources = ["CMakeLists.txt", "0001-libsigcpp.patch"]
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    exports_sources = ["CMakeLists.txt", "*.patch"]
     generators = "cmake"
-    no_copy_source = True
 
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -30,22 +30,25 @@ class LibSigCppConan(ConanFile):
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["SIGCXX_DISABLE_DEPRECATED"] = False
-        cmake.definitions["SIGCXX_BUILD_TESTS"] = False
-        cmake.definitions["SIGCXX_BUILD_EXAMPLES"] = False
-        cmake.definitions['CMAKE_INSTALL_INCLUDEDIR'] = 'include'
         cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        with tools.chdir(self.source_folder):
-            tools.patch(base_path=self._source_subfolder, patch_file="0001-libsigcpp.patch")
-            cmake = self._configure_cmake()
-            cmake.build()
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
+        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
-    def package_id(self):
-        self.info.header_only()
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("m")
+        self.cpp_info.includedirs.extend([os.path.join('include', "sigc++-3.0"),
+                                          os.path.join('lib', "sigc++-3.0", "include")])
