ffbdf385fe230f88219f959ec379bac14da10c0f
SSE4 <tomskside@gmail.com>
2019-11-14 20:42:14 +0700

    Update conanfile.py

--- recipes/icu/all/test_package/conanfile.py
+++ recipes/icu/all/test_package/conanfile.py
@@ -11,12 +11,6 @@ class ICUTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        lib_dir_src = 'lib64' if self.settings.arch == 'x86_64' and self.settings.os == 'Windows' else 'lib'
-        self.copy("*.dll", dst="bin", src=lib_dir_src)
-        self.copy("*.dylib*", dst="bin", src=lib_dir_src)
-        self.copy('*.so*', dst='bin', src=lib_dir_src)
-
     def test(self):
         if not tools.cross_building(self.settings):
             bin_path = os.path.join("bin", "test_package")
