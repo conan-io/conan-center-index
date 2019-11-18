dc3b8a00f1bd160770218aae6546bd2c82bc88d4
Uilian Ries <uilianries@gmail.com>
2019-11-18 18:33:16 -0300

    Improve conan package
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/docopt.cpp/0.6.2/conanfile.py
+++ recipes/docopt.cpp/0.6.2/conanfile.py
@@ -4,14 +4,15 @@ import os
 
 class DocoptCppConan(ConanFile):
     name = "docopt.cpp"
+    version = "0.6.2"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/docopt/docopt.cpp"
     settings = "os", "compiler", "build_type", "arch"
     description = "C++11 port of docopt"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
-    topics = ("CLI")
+    options = {"shared": [True, False], "fPIC": [True, False], "boost_regex": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "boost_regex": False}
+    topics = ("CLI", "getopt", "options", "argparser")
     generators = 'cmake'
     exports_sources = ['patches/**', 'CMakeLists.txt']
     _source_subfolder = "source_subfolder"
@@ -20,37 +21,34 @@ class DocoptCppConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def requirements(self):
+        if self.options.boost_regex:
+            self.requires("boost/1.71.0")
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["USE_BOOST_REGEX"] = self.options.boost_regex
+        cmake.configure()
+        return cmake
+
     def build(self):
         for patch in self.conan_data["patches"][self.version]:
             tools.patch(**patch)
-        cmake = CMake(self)
-        # docopt's cmakelists actually ignores BUILD_SHARED_LIBS and builds both
-        # libs everytime. This makes conan export two targets with the same name
-        # which is illegal. To work around this, we install in a stage folder
-        # and we take only the lib we want.
-        cmake.definitions["CMAKE_INSTALL_PREFIX"] = "stage"
-        cmake.configure()
+        cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        stage_dir = "stage"
-
-        self.copy("include/*", src=stage_dir, excludes=("*docopt_private.h", "*docopt_util.h"))
-        if self.options.shared:
-            self.copy("*.so*", src=stage_dir, dst="lib", symlinks=True, keep_path=False)
-            self.copy("*.dylib", src=stage_dir, dst="lib", symlinks=True, keep_path=False)
-            self.copy("*.dll", src=stage_dir, dst="bin", keep_path=False)
-            self.copy("*docopt.lib", src=stage_dir, dst="lib", keep_path=False)
-        else:
-            self.copy("*.a", src=stage_dir, dst="lib", keep_path=False)
-            self.copy("*docopt_s.lib", src=stage_dir, dst="lib", keep_path=False)
-        self.copy("*LICENSE*", dst="licenses", keep_path=False)
+        self.copy("LICENSE*", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["m"]
