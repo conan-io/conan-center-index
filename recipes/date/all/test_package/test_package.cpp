0cee545564123f6c21c3fa048431368054a096f9
Uilian Ries <uilianries@gmail.com>
2019-11-18 14:57:20 -0300

    Simplified date tests
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/date/all/test_package/test_package.cpp
+++ recipes/date/all/test_package/test_package.cpp
@@ -1,11 +1,23 @@
 #include <cstdlib>
 #include <iostream>
+#include <chrono>
 
 #include "date/date.h"
 #include "date/tz.h"
 
+
 int main() {
-    auto& db = date::get_tzdb();
-    std::cout << date::weekday{date::jul/4/2001} << '\n';
+    using namespace std::chrono;
+    using namespace date;
+
+    constexpr auto date1 = 2015_y/March/22;
+    std::cout << date1 << '\n';
+    constexpr auto date2 = March/22/2015;
+    std::cout << date2 << '\n';
+    constexpr auto date3 = 22_d/March/2015;
+    std::cout << date3 << '\n';
+
+    auto timezone = make_zoned(current_zone(), system_clock::now());
+    std::cout << timezone << '\n';
     return EXIT_SUCCESS;
 }
