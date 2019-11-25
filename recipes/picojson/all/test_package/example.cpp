914c5937e8678d1d932bd6e209023e7ebee48089
vaerizk <ezerblaze@gmail.com>
2019-11-25 13:35:00 +0300

    Fix gcc compatibility for picojson test package

--- recipes/picojson/all/test_package/example.cpp
+++ recipes/picojson/all/test_package/example.cpp
@@ -1,15 +1,17 @@
 #include <picojson.h>
 
-void main() {
+int main() {
 	std::string json{ "{ \"picojson_test\" : { \"number\" : 13, \"array\" : [1, 2, 3] } }" };
 	picojson::value json_value;
 
 	std::string error{ picojson::parse(json_value, json) };
 	if (error.empty()) {
 		std::cout << json_value << std::endl;
-	} else {
-		std::cerr << error << std::endl;
+		std::cout << "Package test completed successfully";
+		return 0;
 	}
 
-	std::cout << "Package test completed";
+	std::cout << "Package test completed, errors occurred:" << std::endl;
+	std::cerr << error << std::endl;
+	return 1;
 }
