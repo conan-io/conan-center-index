2ad30c5324281a89d657990f2474dd96e442bc8c
Théo DELRIEU <theo.delrieu@tanker.io>
2019-11-27 12:55:19 +0100

    doctest: review fixes

--- recipes/doctest/2.3.5/conanfile.py
+++ recipes/doctest/2.3.5/conanfile.py
@@ -7,11 +7,9 @@ class DoctestConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/onqtam/doctest"
     description = "C++11/14/17/20 single header testing framework"
+    settings = "os", "compiler"
     license = "MIT"
-    settings = ("os", "compiler", "build_type", "arch")
-    generators = "cmake"
     _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
 
     @property
     def _is_mingw(self):
@@ -22,22 +20,9 @@ class DoctestConan(ConanFile):
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["DOCTEST_WITH_TESTS"] = False
-        cmake.configure(build_folder=self._build_subfolder, source_folder=self._source_subfolder)
-        return cmake
-
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
-
     def package(self):
-        cmake = self._configure_cmake()
-        cmake.install()
-
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
         self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*doctest.h", dst="include", src=self._source_subfolder)
 
     def package_info(self):
         if self._is_mingw:
