e4ba1956deb450a4969758383271ad91c3009491
intelligide <intelligide@hotmail.fr>
2019-11-30 14:47:00 -0500

    re2: link pthread

--- recipes/re2/all/conanfile.py
+++ recipes/re2/all/conanfile.py
@@ -54,3 +54,6 @@ class Re2Conan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
