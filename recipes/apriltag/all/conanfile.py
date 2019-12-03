c9a330b9d049f022db5d344bdaec09cbd0aadce4
Riff, Eric <eriff@irobot.com>
2019-12-02 17:00:22 -0800

    Define "m" and "pthread" as system_libs if we're using linux.

--- recipes/apriltag/all/conanfile.py
+++ recipes/apriltag/all/conanfile.py
@@ -50,4 +50,6 @@ class apriltagConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["m", "pthread"]
         self.cpp_info.includedirs = ['include/apriltag']  # Ordered list of include paths
