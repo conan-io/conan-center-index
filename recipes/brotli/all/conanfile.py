f0cd82e503a96fe2924f6b69cecd6823f8c8cec7
Uilian Ries <uilianries@gmail.com>
2019-12-12 08:22:01 -0300

    Add external patch
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/brotli/all/conanfile.py
+++ recipes/brotli/all/conanfile.py
@@ -4,12 +4,11 @@ import os
 
 class BrotliConan(ConanFile):
     name = "brotli"
-    description = "Brotli is a generic-purpose lossless compression algorithm that compresses data using a combination of a modern variant of the LZ77 algorithm, Huffman coding and 2nd order context modeling, with a compression ratio comparable to the best currently available general-purpose compression methods. It is similar in speed with deflate but offers more dense compression."
-    topics = ("conan", "snappy", "google", "compressor", "decompressor")
+    description = "Brotli compression format"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/google/brotli"
     license = "MIT",
-    exports_sources = "CMakeLists.txt",
+    exports_sources = ["CMakeLists.txt", "patches/*"]
     generators = "cmake",
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -37,23 +36,6 @@ class BrotliConan(ConanFile):
         extracted_folder = "brotli-{}".format(self.version)
         os.rename(extracted_folder, self._source_subfolder)
 
-    def _patch_sources(self):
-        # avoid building and installing static libraries if shared build and vice versa
-        cmakefile = os.path.join(self._source_subfolder, "CMakeLists.txt")
-        tools.save(cmakefile,
-                   "set_target_properties(brotli PROPERTIES EXCLUDE_FROM_ALL ON EXCLUDE_FROM_DEFAULT ON)\n", append=True)
-        for lib in self._get_libraries(not self.options.shared):
-            tools.save(cmakefile,
-                       "set_target_properties({} PROPERTIES EXCLUDE_FROM_ALL ON EXCLUDE_FROM_DEFAULT ON)\n".format(lib), append=True)
-        shared_libraries_cmake = "${BROTLI_LIBRARIES_CORE}"
-        static_libraries_cmake = "${BROTLI_LIBRARIES_CORE_STATIC}"
-        tools.replace_in_file(cmakefile,
-                              "TARGETS {}".format(static_libraries_cmake if self.options.shared else shared_libraries_cmake),
-                              "TARGETS {}".format(shared_libraries_cmake if self.options.shared else static_libraries_cmake),)
-        tools.replace_in_file(cmakefile,
-                              "TARGETS brotli",
-                              "TARGETS {}".format(shared_libraries_cmake if self.options.shared else static_libraries_cmake),)
-
     def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["BROTLI_BUNDLED_MODE"] = False
@@ -62,7 +44,8 @@ class BrotliConan(ConanFile):
         return cmake
 
     def build(self):
-        self._patch_sources()
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -70,7 +53,6 @@ class BrotliConan(ConanFile):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def _get_libraries(self, shared):
@@ -82,7 +64,8 @@ class BrotliConan(ConanFile):
     def package_info(self):
         self.cpp_info.name = "Brotli"
         self.cpp_info.libs = self._get_libraries(self.options.shared)
-        self.cpp_info.includedirs = [os.path.join(self.package_folder, "include"),
-                                     os.path.join(self.package_folder, "include", "brotli")]
+        self.cpp_info.includedirs = ["include", os.path.join("include", "brotli")]
         if self.options.shared:
             self.cpp_info.defines.append("BROTLI_SHARED_COMPILATION")
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["m"]
