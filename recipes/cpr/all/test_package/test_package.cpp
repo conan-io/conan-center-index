c335d8839bbd79e13b50e4a7edd549f38a9fbcfd
javierg <javierg@jfrog.com>
2019-12-13 17:08:03 +0100

    use cmake_find_package to consume in the test_package

--- recipes/cpr/all/test_package/test_package.cpp
+++ recipes/cpr/all/test_package/test_package.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    auto r = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/todos"},
+    auto r = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/todos/1"},
         cpr::Parameters{{"Accept", "application/json"}});
     std::cout << "status code: " << r.status_code << '\n';                  // 200
     std::cout << "headers:     " << r.header["content-type"] << '\n';       // application/json; charset=utf-8
