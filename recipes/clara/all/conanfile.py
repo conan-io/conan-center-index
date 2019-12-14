c51ec3a2de2bf6ed4fe401983e2c6ed179027e67
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-12-14 22:44:03 +0100

    Add clara/1.1.5 recipe

---
+++ recipes/clara/all/conanfile.py
@@ -0,0 +1,27 @@
+import os
+from conans import ConanFile, tools
+
+
+class ClaraConan(ConanFile):
+    name = "clara"
+    description = "A simple to use, composable, command line parser for C++ 11 and beyond"
+    homepage = "https://github.com/catchorg/Clara"
+    topics = ("conan", "clara", "cli", "cpp11", "command-parser")
+    url = "https://github.com/conan-io/conan-center-index"
+    license = "BSL-1.0"
+    no_copy_source = True
+
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("clara-{}".format(self.version), self._source_subfolder)
+
+    def package(self):
+        self.copy(pattern="LICENSE.txt", src=self._source_subfolder, dst="licenses")
+        self.copy(pattern="*", src=os.path.join(self._source_subfolder, "include"), dst="include", keep_path=True)
+    def package_id(self):
+        self.info.header_only()
+
+    def package_info(self):
+        self.output.warn("clara is unmaintained. Consider updating to lyra.")
