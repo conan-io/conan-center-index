29ac9e995f35ad23f8227c5063c3def8bbb7714a
Alexandr Timofeev <minimonium@outlook.com>
2019-12-16 23:00:23 +0300

    Fix builddirs on Windows to pass the check

--- recipes/catch2/2.x.x/conanfile.py
+++ recipes/catch2/2.x.x/conanfile.py
@@ -50,4 +50,6 @@ class ConanRecipe(ConanFile):
         self.info.header_only()
 
     def package_info(self):
-        self.cpp_info.builddirs = [os.path.join("lib", "cmake", "Catch2")]
+        self.cpp_info.builddirs = [
+            os.path.join("lib", "cmake", "Catch2").replace("\\", "/")
+        ]
