d79b12ce81b0b43c94861a026decab0bf5987c0c
ericLemanissier <ericLemanissier@users.noreply.github.com>
2019-12-19 16:40:25 +0100

    fix mingw compilation
    msys2 build requirement was missing
    
    #511

--- recipes/openssl/ALL/conanfile.py
+++ recipes/openssl/ALL/conanfile.py
@@ -104,6 +104,9 @@ class OpenSSLConan(ConanFile):
                 self.build_requires("strawberryperl/5.30.0.1")
             if not self.options.no_asm and not tools.which("nasm"):
                 self.build_requires("nasm/2.14")
+        if self._win_bash:
+            if "CONAN_BASH_PATH" not in os.environ:
+                self.build_requires("msys2/20190524")
 
     @property
     def _is_msvc(self):
