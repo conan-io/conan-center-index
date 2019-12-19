7e0613949feaf1d495949f373fa2118c5d471b16
Eric Lemanissier <eric.lemanissier@gmail.com>
2019-12-19 12:03:46 +0100

    add sqlpp11-connector-sqlite/0.29

---
+++ recipes/sqlpp11-connector-sqlite3/all/conanfile.py
@@ -0,0 +1,49 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class sqlpp11Conan(ConanFile):
+    name = "sqlpp11-connector-sqlite3"
+    version = "0.29"
+    description = "A C++ wrapper for sqlite3 meant to be used in combination with sqlpp11."
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/rbock/sqlpp11-connector-sqlite3"
+    license = "BSD 2-Clause"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+    requires = "sqlpp11/0.58", "sqlite3/3.30.1"
+    short_paths = True
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["ENABLE_TESTS"] = False
+        cmake.definitions['HinnantDate_ROOT_DIR'] = self.deps_cpp_info['date'].include_paths[0]
+        cmake.definitions['SQLPP11_INCLUDE_DIR'] = self.deps_cpp_info['sqlpp11'].include_paths[0]
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["sqlpp11-connector-sqlite3"]
