b2b4f32a406347ef2d891e9b7af4f4b6efc0ce92
Riff, Eric <eriff@irobot.com>
2019-12-20 17:00:23 -0800

    Allocate memory on the test_package cpp

--- recipes/tinycbor/all/test_package/example.cpp
+++ recipes/tinycbor/all/test_package/example.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "cbor.h"
 
 int main(int argc, char *argv[])
@@ -6,7 +7,7 @@ int main(int argc, char *argv[])
     CborParser parser;
     CborValue it;
     size_t length;
-    uint8_t *buf;
+    uint8_t *buf = (uint8_t*)malloc(sizeof(int[10]));
     CborError err = cbor_parser_init(buf, length, 0, &parser, &it);
 
     printf("Tinycbor test_package ran successfully \n");
