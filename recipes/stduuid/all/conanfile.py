3ec24d1e6d552d29ca0854b4cd187850c1a70540
intelligide <intelligide@hotmail.fr>
2019-12-23 12:08:37 -0500

    stduuid: Fix

--- recipes/stduuid/all/conanfile.py
+++ recipes/stduuid/all/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
+from conans.tools import Version
 import os
 
 
@@ -12,6 +13,8 @@ class StduuidConan(ConanFile):
     license = "MIT"
 
     requires = "ms-gsl/2.0.0"
+
+    settings =  "compiler"
 
     no_copy_source = True
     _source_subfolder = "source_subfolder"
@@ -27,14 +30,13 @@ class StduuidConan(ConanFile):
         if self.settings.compiler.cppstd and \
            not any([str(self.settings.compiler.cppstd) == std for std in ["17", "20", "gnu17", "gnu20"]]):
             raise ConanInvalidConfiguration("stduuid requires at least c++17")
-        elif compiler == "Visual Studio" and \
-            not any([self.settings.compiler.cppstd == std for std in ["17", "20"]]):
-                raise ConanInvalidConfiguration("stduuid requires at least c++17")
+        elif compiler == "Visual Studio":
+            if version < "16":
+                raise ConanInvalidConfiguration("stduuid requires at least Visual Studio version 16")
         else:
             if ( compiler == "gcc" and version < "7" ) or ( compiler == "clang" and version < "5" ):
                 raise ConanInvalidConfiguration("stduuid requires a compiler that supports at least C++17")
             elif compiler == "apple-clang":
-                self.output.warn("stduuid is not tested with apple-clang")
                 if version < "10":
                     raise ConanInvalidConfiguration("stduuid requires a compiler that supports at least C++17")
 
@@ -43,11 +45,6 @@ class StduuidConan(ConanFile):
         include_dir = os.path.join(root_dir, "include")
         self.copy(pattern="LICENSE", dst="licenses", src=root_dir)
         self.copy(pattern="uuid.h", dst="include", src=include_dir)
-
-    def package_info(self):
-        self.cpp_info.defines.append('ASIO_STANDALONE')
-        if tools.os_info.is_linux:
-            self.cpp_info.system_libs.append('pthread')
 
     def package_id(self):
         self.info.header_only()
