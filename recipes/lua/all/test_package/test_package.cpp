126f52651e8a5a85f0f80ee62b05c628a89d4ae5
Gavin <gavin.n.lobo@gmail.com>
2019-12-26 14:01:25 -0500

    Added Lua recipe

---
+++ recipes/lua/all/test_package/test_package.cpp
@@ -0,0 +1,19 @@
+extern "C"
+{
+      #include "lua.h"
+      #include "lualib.h"
+      #include "lauxlib.h"
+}
+
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    lua_State* L = luaL_newstate();
+    luaL_dostring(L, "x = 47");
+    lua_getglobal(L, "x");
+    lua_Number x = lua_tonumber(L, 1);
+    printf("lua says x = %d\n", (int)x);
+    lua_close(L);
+    return 0;
+}
