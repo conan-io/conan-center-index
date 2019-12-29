8c947cded0ced050e3c16ec8d436b3d0601e2579
David Yip <dwyip@peach-bun.com>
2019-12-29 11:07:31 -0600

    Port tl-expected recipe from https://github.com/yipdw/conan-tl-expected

---
+++ recipes/tl-expected/all/conanfile.py
@@ -0,0 +1,36 @@
+from conans import ConanFile, CMake, tools
+from fnmatch import fnmatch
+import os
+
+
+class TlExpectedConan(ConanFile):
+    name = "tl-expected"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://tl.tartanllama.xyz"
+    description = "C++11/14/17 std::expected with functional-style extensions"
+    topics = ("cpp11", "cpp14", "cpp17", "expected")
+    license = "CC0-1.0"
+    _source_subfolder = "tl-expected"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename(self._archive_dir, self._source_subfolder)
+
+    @property
+    def _archive_dir(self):
+        # the archive expands to a directory named expected-[COMMIT SHA1];
+        # we'd like to put this under a stable name
+        expected_dirs = [
+            de for de in os.scandir(self.source_folder)
+            if de.is_dir() and fnmatch(de.name, "expected-*")
+        ]
+        return expected_dirs[0].name
+
+    def package(self):
+        self.copy("*",
+                  src=os.path.join(self._source_subfolder, "include"),
+                  dst="include")
+        self.copy("COPYING", src=self._source_subfolder, dst="licenses")
+
+    def package_id(self):
+        self.info.header_only()
