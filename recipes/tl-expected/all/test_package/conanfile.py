ca409225103c658f01efa51e1f7cf40ee1a03121
David Yip <dwyip@peach-bun.com>
2019-12-29 11:07:31 -0600

    Port tl-expected recipe from https://github.com/yipdw/conan-tl-expected

---
+++ recipes/tl-expected/all/test_package/conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class ExpectedTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
+        # in "test_package"
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            os.chdir("bin")
+            self.run(".%sexample" % os.sep)
