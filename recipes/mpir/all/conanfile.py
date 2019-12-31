ceed614dce15d8ff219a0b4c695b62a331512a38
czoido <mrgalleta@gmail.com>
2019-12-31 17:45:50 +0100

    patch props

--- recipes/mpir/all/conanfile.py
+++ recipes/mpir/all/conanfile.py
@@ -13,7 +13,9 @@ class MpirConan(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
+    _dll_or_lib = "lib"
     _source_subfolder = "source_subfolder"
+    _platforms={'x86': 'Win32', 'x86_64': 'x64'}
 
     def build_requirements(self):
         self.build_requires("yasm/1.3.0")
@@ -25,6 +27,7 @@ class MpirConan(ConanFile):
     def configure(self):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
+        self._dll_or_lib = "dll" if self.options.shared else "lib"
         # maybe configure architecture
 
     def source(self):
@@ -34,20 +37,29 @@ class MpirConan(ConanFile):
 
     def build(self):
         if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
-            dll_or_lib = "dll" if self.options.shared else "lib"
+            if "MD" in self.settings.compiler.runtime and not self.options.shared:
+                props_path = os.path.join(self._source_subfolder, "build.vc", 
+                "mpir_{}_{}.props".format(str(self.settings.build_type).lower(), self._dll_or_lib))
+                if self.settings.build_type == "Debug":
+                    tools.replace_in_file(props_path, "<RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>",
+                                                      "<RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>")
+                else:
+                    tools.replace_in_file(props_path, "<RuntimeLibrary>MultiThreaded</RuntimeLibrary>",
+                                                      "<RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>")
+
             vcxproj_path = os.path.join(self._source_subfolder,
                                         "build.vc{}".format(self.settings.compiler.version),
-                                        "{}_mpir_gc".format(dll_or_lib),
-                                        "{}_mpir_gc.vcxproj".format(dll_or_lib))
+                                        "{}_mpir_gc".format(self._dll_or_lib),
+                                        "{}_mpir_gc.vcxproj".format(self._dll_or_lib))
             self.output.info(vcxproj_path)
             msbuild = MSBuild(self)
-            msbuild.build(vcxproj_path)
-            #TODO handle runtime
+            msbuild.build(vcxproj_path, platforms=self._platforms, upgrade_project=False)
 
     def package(self):
         self.copy("COPYING", dst="licenses", src=self._source_subfolder)        
-        lib_folder = os.path.join(self._source_subfolder,
-                                  "lib", "x64", str(self.settings.build_type))
+        lib_folder = os.path.join(self._source_subfolder, self._dll_or_lib, 
+                                  self._platforms.get(str(self.settings.arch)), 
+                                  str(self.settings.build_type))
         self.copy("*.h", dst="include", src=lib_folder, keep_path=True)
         self.copy(pattern="*.dll*", dst="bin", src=lib_folder, keep_path=False)
         self.copy(pattern="*.lib", dst="lib", src=lib_folder, keep_path=False)        
