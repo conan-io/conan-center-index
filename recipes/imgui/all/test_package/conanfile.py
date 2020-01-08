57f2394fe745f09521bed3a2560b21b80555b14a
Pau Farré <paurubifg@gmail.com>
2020-01-09 00:04:41 +0100

    RunEnvironment is no longer needed

--- recipes/imgui/all/test_package/conanfile.py
+++ recipes/imgui/all/test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools, RunEnvironment
+from conans import ConanFile, CMake, tools
 import os
 
 class TestPackageConan(ConanFile):
@@ -12,11 +12,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            with tools.environment_append(RunEnvironment(self).vars):
-                bin_path = os.path.join("bin", "test_package")
-                if self.settings.os == "Windows":
-                    self.run(bin_path)
-                elif self.settings.os == "Macos":
-                    self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
-                else:
-                    self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
