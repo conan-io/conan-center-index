e8df13d5385e8ce1d7f2122ef7fbbea7de47550d
Pau Farré <paurubifg@gmail.com>
2020-01-08 23:41:06 +0100

    Import bincrafters/conan-imgui

---
+++ recipes/imgui/all/conanfile.py
@@ -0,0 +1,56 @@
+import os
+from conans import ConanFile, CMake, tools
+
+
+class IMGUIConan(ConanFile):
+    name = "imgui"
+    url = "https://github.com/bincrafters/conan-imgui"
+    homepage = "https://github.com/ocornut/imgui"
+    description = "Bloat-free Immediate Mode Graphical User interface for C++ with minimal dependencies"
+    topics = ("conan", "imgui", "gui", "graphical")
+    license = "MIT"
+    
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+         "fPIC": [True, False]
+    }
+    default_options = {
+        "shared": False, 
+        "fPIC": True
+    }
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+    
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="examples/imgui_impl_*", dst="misc/bindings", src=self._source_subfolder, keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.srcdirs = ["misc"]
+        self.cpp_info.libs = tools.collect_libs(self)
