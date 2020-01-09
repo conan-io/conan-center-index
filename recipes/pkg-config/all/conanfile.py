b0bf15bfa5191bc895d671306da9e26b1ec0174c
Uilian Ries <uilianries@gmail.com>
2020-01-09 09:41:37 -0300

    Separate autotools for pkg-config
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/pkg-config/all/conanfile.py
+++ recipes/pkg-config/all/conanfile.py
@@ -13,16 +13,21 @@ class PkgConfigConan(ConanFile):
     settings = "os_build", "arch_build", "compiler"
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
+    _autotools = None
 
     @property
     def _is_mingw_windows(self):
         return self.settings.os_build == "Windows" and self.settings.compiler == "gcc" and os.name == "nt"
 
     def build_requirements(self):
-        if self._is_mingw_windows and "CONAN_BASH_PATH" not in os.environ and os_info.detect_windows_subsystem() != 'msys2':
+        if self._is_mingw_windows and "CONAN_BASH_PATH" not in os.environ and \
+           tools.os_info.detect_windows_subsystem() != 'msys2':
             self.build_requires("msys2/20190524")
 
     def configure(self):
+        if self.settings.compiler == "Visual Studio":
+            raise ConanInvalidConfiguration("builds with Visual Studio aren't currently supported, "
+                                            "use MinGW instead to build for Windows")
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
@@ -30,20 +35,22 @@ class PkgConfigConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("pkg-config-" + self.version, self._source_subfolder)
 
-    def build(self):
-        if self.settings.compiler == "Visual Studio":
-            raise ConanInvalidConfiguration("builds with Visual Studio aren't currently supported, "
-                                            "use MinGW instead to build for Windows")
-        with tools.chdir(self._source_subfolder):
+    def _configure_autotools(self):
+        if not self._autotools:
             # http://www.linuxfromscratch.org/lfs/view/systemd/chapter06/pkg-config.html
             args = ["--with-internal-glib", "--disable-host-tool"]
-            env_build = AutoToolsBuildEnvironment(self, self._is_mingw_windows)
-            env_build.configure(args=args)
-            env_build.make()
-            env_build.install()
+            self._autotools = AutoToolsBuildEnvironment(self, self._is_mingw_windows)
+            self._autotools.configure(args=args, configure_dir=self._source_subfolder)
+        return self._autotools
+
+    def build(self):
+        autotools = self._configure_autotools()
+        autotools.make()
 
     def package(self):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
+        autotools = self._configure_autotools()
+        autotools.install()
         if self._is_mingw_windows:
             package_folder = tools.unix_path(os.path.join(self.package_folder, "bin"))
             tools.run_in_windows_bash(self, "cp $(which libwinpthread-1.dll) {}".format(package_folder))
@@ -53,5 +60,6 @@ class PkgConfigConan(ConanFile):
         del self.info.settings.compiler
 
     def package_info(self):
-        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
-
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info('Appending PATH environment variable: {}'.format(bindir))
+        self.env_info.PATH.append(bindir)
