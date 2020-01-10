d05f07856c1539eaecdc4fe35f3af4ecc6eb0a59
Piotr Podsiadly <ppodsiadly@mykolab.com>
2020-01-10 11:15:31 +0100

    doctest: include doctest.cmake in the package

--- recipes/doctest/2.x.x/conanfile.py
+++ recipes/doctest/2.x.x/conanfile.py
@@ -24,11 +24,21 @@ class DoctestConan(ConanFile):
         self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
         self.copy(pattern="*doctest.h", dst="include", src=self._source_subfolder)
 
+        cmake_script_dirs = {
+            "src": os.path.join(self._source_subfolder, "scripts/cmake"),
+            "dst": "lib/cmake/doctest"
+        }
+
+        self.copy(pattern="doctest.cmake", **cmake_script_dirs)
+        self.copy(pattern="doctestAddTests.cmake", **cmake_script_dirs)
+
     def package_info(self):
         if self._is_mingw:
             # See https://sourceforge.net/p/mingw-w64/bugs/727/
             # can't use destructors in thread_local with mingw
             self.cpp_info.defines.append("DOCTEST_THREAD_LOCAL=")
+
+        self.cpp_info.builddirs.append("lib/cmake/doctest")
 
     def package_id(self):
         self.info.header_only()
