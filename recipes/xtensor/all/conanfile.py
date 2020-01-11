2cfa95a1c8f84f16e31e0c8e884cda9c45a9036b
Jimmy M. Gong <jimmy.m.gong@gmail.com>
2020-01-11 15:35:46 +0800

    cppstd is not configured by default, so remove it.

--- recipes/xtensor/all/conanfile.py
+++ recipes/xtensor/all/conanfile.py
@@ -40,27 +40,14 @@ class XtensorConan(ConanFile):
         # - On Unix platforms, gcc 4.9 or a recent version of Clang
         version = tools.Version(self.settings.compiler.version)
         compiler = self.settings.compiler
-        if self.settings.compiler.cppstd and not any(
-            [
-                str(self.settings.compiler.cppstd) == std
-                for std in ["14", "17", "20", "gnu14", "gnu17", "gnu20"]
-            ]
+        if compiler == "Visual Studio" and version < "16":
+            raise ConanInvalidConfiguration(
+                "xtensor requires at least Visual Studio version 15.9, please use 16"
+            )
+        if (compiler == "gcc" and version < "5.0") or (
+            compiler == "clang" and version < "4"
         ):
             raise ConanInvalidConfiguration("xtensor requires at least C++14")
-        elif compiler == "Visual Studio":
-            if version < "16":
-                raise ConanInvalidConfiguration(
-                    "xtensor requires at least Visual Studio version 15.9, please use 16"
-                )
-            if not any(
-                [self.settings.compiler.cppstd == std for std in ["14", "17", "20"]]
-            ):
-                raise ConanInvalidConfiguration("xtensor requires at least C++14")
-        else:
-            if (compiler == "gcc" and version < "6.0") or (
-                compiler == "clang" and version < "4"
-            ):
-                raise ConanInvalidConfiguration("xtensor requires at least C++14")
 
     def requirements(self):
         self.requires("xtl/0.6.9")
