6f94008041767fb8bc230225e20e2a12245fc42a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-01-14 15:30:21 +0100

    do not call glad functions in test_package

--- recipes/glad/all/test_package/test_package.c
+++ recipes/glad/all/test_package/test_package.c
@@ -1,13 +1,7 @@
 #include "glad/glad.h"
 
-#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  if(!gladLoadGL()) {
-    printf("Failed to initialize OpenGL context, expected since no context has been initialized\n");
-  } else {
-    printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
-  }
-
-  return 0;
+  return EXIT_SUCCESS;
 }
