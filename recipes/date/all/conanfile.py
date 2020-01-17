9bd088a3df9421b460ec86222a2eee12ea4dbf7f
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-01-18 00:21:35 +0100

    chore: add optional curl dependency

--- recipes/date/all/conanfile.py
+++ recipes/date/all/conanfile.py
@@ -7,10 +7,17 @@ class DateConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/HowardHinnant/date"
     description = "A date and time library based on the C++11/14/17 <chrono> header"
-    topics = ("date", "datetime", "timezone", "calendar", "time", "iana-database")
+    topics = ("date", "datetime", "timezone",
+              "calendar", "time", "iana-database")
     license = "MIT"
     exports_sources = ["patches/*"]
-    no_copy_source = True
+    options = {"shared": [True, False],
+               "fPIC": [True, False],
+               "use_system_tz_db": [True, False]
+               }
+    default_options = {"shared": False,
+                       "fPIC": True,
+                       "use_system_tz_db": False}
 
     @property
     def _source_subfolder(self):
@@ -26,19 +33,26 @@ class DateConan(ConanFile):
     def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["ENABLE_DATE_TESTING"] = False
-        cmake.definitions["USE_SYSTEM_TZ_DB"] = True
+        cmake.definitions["USE_SYSTEM_TZ_DB"] = self.options.use_system_tz_db
         cmake.configure(source_folder=self._source_subfolder)
         return cmake
 
+    def requirements(self):
+        if self.options.use_system_tz_db == False:
+            self.requires("libcurl/7.67.0")
+        
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
-        
+
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="LICENSE.txt", dst="licenses",
+                  src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib"))
 
-    def package_id(self):
-        self.info.header_only()
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        if tools.os_info.is_linux:
+            self.cpp_info.libs.append("pthread")
