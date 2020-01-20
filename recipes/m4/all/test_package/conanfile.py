c531b0408039646279507c1dd7b7d700bd05cd2a
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-01-20 23:05:14 +0100

    m4: convert CRLF (\r\n) of input of test_package to LF (\n)

--- recipes/m4/all/test_package/conanfile.py
+++ recipes/m4/all/test_package/conanfile.py
@@ -4,11 +4,17 @@ import os
 
 class TestPackageConan(ConanFile):
 
-    def test(self):
+    def build(self):
         test_file = os.path.join(self.source_folder, "test_package.m4")
-        self.run("m4 --version")
-        self.run("m4 -P %s" % test_file)
-        self.run("m4 -P %s > out" % test_file)
+        in_file = os.path.join(self.build_folder, "input.m4")
+        with open(in_file, "w") as f_in_file, open(test_file, "r") as f_test_file:
+            f_in_file.write(f_test_file.read().replace("\r\n", "\r"))
+        assert "\r\n" not in open(in_file, "r")
 
-        if "\r\n" in open("out", "rb").read().decode():
+        self.run("{} --version".format(os.environ["M4"]))
+        self.run("{} -P {}".format(os.environ["M4"], in_file))
+        self.run("{} -P {} > out".format(os.environ["M4"], in_file))
+
+    def test(self):
+        if "\r\n" in open("out", "rb"):
             raise Exception("m4 output has DOS line-endings!")
