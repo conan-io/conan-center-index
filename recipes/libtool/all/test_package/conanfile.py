946e47228691c7fc47b99901f0439314c1c5ade1
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-01-20 20:36:02 +0100

    libtool: fixes

--- recipes/libtool/all/test_package/conanfile.py
+++ recipes/libtool/all/test_package/conanfile.py
@@ -12,9 +12,6 @@ class TestPackageConan(ConanFile):
         if tools.os_info.is_windows and "CONAN_BASH_PATH" not in os.environ:
             self.build_requires("msys2/20190524")
 
-    def requirements(self):
-        self.requires("automake/1.16.1")
-
     @contextmanager
     def _build_context(self):
         if self.settings.compiler == "Visual Studio":
@@ -28,11 +25,13 @@ class TestPackageConan(ConanFile):
     def build(self):
         for src in self.exports_sources:
             shutil.copy(os.path.join(self.source_folder, src), self.build_folder)
-        self.run("autoreconf --verbose --install --force", win_bash=tools.os_info.is_windows)
+        self.run("{} --verbose --install --force".format(os.environ["AUTORECONF"]), win_bash=tools.os_info.is_windows)
 
-        autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        autotools.configure()
-        autotools.make()
+        with self._build_context():
+            autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+            autotools.configure()
+            autotools.make()
 
     def test(self):
-        self.run(os.path.join(".", "test_package"), run_environment=True)
+        exeext = ".exe" if self.settings.os == "Windows" else ""
+        self.run(os.path.join(".", "test_package{}".format(exeext)), run_environment=True)
