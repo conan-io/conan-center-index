80be1acad0c7120bcb1980d81a6d14f91a8ec3a5
Michael "Croydon" Keck <git@cr0ydon.com>
2020-01-21 23:59:59 +0100

    premake: Cleanups
    Co-Authored-By: Uilian Ries <uilianries@gmail.com>

--- recipes/premake/5.x/test_package/conanfile.py
+++ recipes/premake/5.x/test_package/conanfile.py
@@ -5,4 +5,4 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            self.run("premake5 --version")
+            self.run("premake5 --version", run_environment=True)
