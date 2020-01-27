ee97c9b54a86a96e51c265d1042883db1d30207b
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-01-27 10:16:28 +0100

    m4: do not mix text and binary mode in test_package

--- recipes/m4/all/test_package/conanfile.py
+++ recipes/m4/all/test_package/conanfile.py
@@ -7,8 +7,8 @@ class TestPackageConan(ConanFile):
     def build(self):
         test_file = os.path.join(self.source_folder, "test_package.m4")
         in_file = os.path.join(self.build_folder, "input.m4")
-        with open(in_file, "w") as f_in_file, open(test_file, "r") as f_test_file:
-            f_in_file.write(f_test_file.read().replace("\r\n", "\r"))
+        with open(in_file, "wb") as f_in_file, open(test_file, "rb") as f_test_file:
+            f_in_file.write(f_test_file.read().replace(b"\r\n", b"\n"))
         assert "\r\n" not in open(in_file, "r")
 
         self.run("{} --version".format(os.environ["M4"]))
@@ -16,5 +16,5 @@ class TestPackageConan(ConanFile):
         self.run("{} -P {} > out".format(os.environ["M4"], in_file))
 
     def test(self):
-        if "\r\n" in open("out", "rb"):
+        if b"\r\n" in open("out", "rb"):
             raise Exception("m4 output has DOS line-endings!")
