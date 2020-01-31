062184ca2cf04611570eab4116a86ba1b4b1b257
Uilian Ries <uilianries@gmail.com>
2020-01-31 08:40:56 -0300

    Check msys2 for xz_utils

--- recipes/xz_utils/all/conanfile.py
+++ recipes/xz_utils/all/conanfile.py
@@ -25,7 +25,7 @@ class XZUtils(ConanFile):
         return self.settings.compiler == 'gcc' and self.settings.os == 'Windows' and os.name == 'nt'
 
     def build_requirements(self):
-        if self._is_mingw_windows and "CONAN_BASH_PATH" not in os.environ:
+        if self._is_mingw_windows and "CONAN_BASH_PATH" not in os.environ and tools.os_info.detect_windows_subsystem() != 'msys2':
             self.build_requires("msys2/20161025")
 
     def _effective_msbuild_type(self):
