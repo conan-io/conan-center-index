bd794b310dc0bd26b8fd537bc7c7000ed368a32d
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-03 17:04:32 +0100

    cc65: simplify recipe
    based on review by SSE4

--- recipes/cc65/all/test_package/conanfile.py
+++ recipes/cc65/all/test_package/conanfile.py
@@ -12,20 +12,18 @@ class TestPackageConan(ConanFile):
     def build(self):
         for src in self.exports_sources:
             shutil.copy(os.path.join(self.source_folder, src), os.path.join(self.build_folder, src))
-        try:
-            os.unlink("c64.lib")
-        except FileNotFoundError:
-            pass
         for target in self._targets:
+            output = "hello_{}".format(target)
             tools.mkdir(target)
             try:
-                os.unlink("hello_{}".format(target))
+                # Try removing the output file to give confidence it is created by cc65
+                os.unlink(output)
             except FileNotFoundError:
                 pass
             self.run("{p} -O -t {t} hello.c -o {t}/hello.s".format(p=os.environ["CC65"], t=target))
             self.run("{p} -t {t} {t}/hello.s -o {t}/hello.o".format(p=os.environ["AS65"], t=target))
             self.run("{p} -t {t} text.s -o {t}/text.o".format(p=os.environ["AS65"], t=target))
-            self.run("{p} -o hello_{t} -t {t} {t}/hello.o {t}/text.o {t}.lib".format(p=os.environ["LD65"], t=target))
+            self.run("{p} -o {o} -t {t} {t}/hello.o {t}/text.o {t}.lib".format(o=output, p=os.environ["LD65"], t=target))
 
     def test(self):
         for target in self._targets:
