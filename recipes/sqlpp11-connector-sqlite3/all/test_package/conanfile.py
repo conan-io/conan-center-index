0db369aa5dedfabb58dc405814cf84ef7f8cfa96
Théo DELRIEU <theo.delrieu@tanker.io>
2020-02-03 16:43:49 +0100

    sqlpp11-connector-sqlite3: add sqlcipher support

--- recipes/sqlpp11-connector-sqlite3/all/test_package/conanfile.py
+++ recipes/sqlpp11-connector-sqlite3/all/test_package/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, CMake
 import os
+import sqlite3
 
 
 class TestPackageConan(ConanFile):
@@ -14,3 +15,14 @@ class TestPackageConan(ConanFile):
     def test(self):
         bin_path = os.path.join("bin", "test_package")
         self.run(bin_path, run_environment=True)
+        # test that the database is encrypted when sqlcipher is used
+        con = sqlite3.connect("test.db")
+        cursor = con.cursor()
+        try:
+            cursor.execute("select * from tab_sample")
+        except sqlite3.DatabaseError:
+            assert self.options["sqlpp11-connector-sqlite3"].with_sqlcipher
+            print("database is encrypted with sqlcipher")
+            return
+        assert not self.options["sqlpp11-connector-sqlite3"].with_sqlcipher
+        print("database is not encrypted")
