6387fd6e2798a047953fbb7932cf1e8b437000e3
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-05 17:20:49 +0100

    gperf: add support for mingw on Linux

--- recipes/gperf/all/test_package/conanfile.py
+++ recipes/gperf/all/test_package/conanfile.py
@@ -3,6 +3,9 @@ from conans import ConanFile, tools
 
 class TestPackageConan(ConanFile):
 
+    settings = "arch_build", "os_build"
+
     def test(self):
+        bin_ext = ".exe" if self.settings.os_build == "Windows" else ""
         if not tools.cross_building(self.settings):
-            self.run("gperf --version")
+            self.run("gperf{} --version".format(bin_ext))
