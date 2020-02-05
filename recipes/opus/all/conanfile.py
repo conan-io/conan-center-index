3f95e8a027aa7991799687b7c41943c827d11495
Daniel <danimanzaneque@gmail.com>
2020-02-05 10:43:15 +0100

    Update recipes/opus/all/conanfile.py
    Co-Authored-By: Javier G. Sogo <jgsogo@gmail.com>

--- recipes/opus/all/conanfile.py
+++ recipes/opus/all/conanfile.py
@@ -64,7 +64,6 @@ class OpusConan(ConanFile):
     def package_info(self):
         self.cpp_info.names['cmake_find_package'] = 'OPUS'
         self.cpp_info.names['cmake_find_package_multi'] = 'OPUS'
-        self.cpp_info.names['pkg_config'] = 'opus'
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == 'Linux' or self.settings.os == "Android":
             self.cpp_info.system_libs.append('m')
