2c0640a3276f406d488966888dda4cec128258b3
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-07 19:39:34 +0100

    Add libtool-ltdl/2.4.6 recipe

---
+++ recipes/libtool-ltdl/all/conanfile.py
@@ -0,0 +1,104 @@
+import os
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from contextlib import contextmanager
+
+
+class LibtoolLtDlConan(ConanFile):
+    name = "libtool-ltdl"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://www.gnu.org/software/libtool/"
+    description = "GNU libtool is a generic library support script."
+    topics = ("conan", "libtool", "configure", "library", "shared", "static")
+    license = ("GPL-2.0-or-later", "GPL-3.0-or-later")
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    _autotools = None
+
+    @property
+    def _source_subfolder(self):
+        return os.path.join(self.source_folder, "source_subfolder")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("libtool-{}".format(self.version), self._source_subfolder)
+
+    def build_requirements(self):
+        self.build_requires("automake/1.16.1")
+        if tools.os_info.is_windows and "CONAN_BASH_PATH" not in os.environ \
+                and tools.os_info.detect_windows_subsystem() != "msys2":
+            self.build_requires("msys2/20190524")
+
+    @contextmanager
+    def _build_context(self):
+        if self.settings.compiler == "Visual Studio":
+            with tools.vcvars(self.settings):
+                with tools.environment_append({"CC": "cl -nologo", "CXX": "cl -nologo",}):
+                    yield
+        else:
+            yield
+
+    def _configure_autotools(self):
+        if self._autotools:
+            return self._autotools
+        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        conf_args = []
+        if self.options.shared:
+            conf_args.extend(["--enable-shared", "--disable-static"])
+        else:
+            conf_args.extend(["--disable-shared", "--enable-static"])
+
+        self._autotools.configure(args=conf_args, configure_dir=self._source_subfolder)
+        return self._autotools
+
+    def build(self):
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.make()
+
+    def package(self):
+        self.copy("COPYING*", src=self._source_subfolder, dst="licenses")
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.install()
+
+        os.unlink(os.path.join(self.package_folder, "lib", "libltdl.la"))
+        tools.rmdir(os.path.join(self.package_folder, "share"))
+        if self.settings.os != "Windows":
+            tools.rmdir(os.path.join(self.package_folder, "bin"))
+        else:
+            binfiles = os.listdir(os.path.join(self.package_folder, "bin"))
+            for binfile in binfiles:
+                binpath = os.path.join(self.package_folder, "bin", binfile)
+                if not binfile.endswith(".dll"):
+                    os.unlink(binpath)
+
+    def package_info(self):
+        lib = "ltdl"
+        if self.settings.os == "Windows" and self.options.shared:
+            lib += ".dll" + ".lib" if self.settings.compiler == "Visual Studio" else ".a"
+        self.cpp_info.libs = [lib]
+
+        if self.options.shared:
+            if self.settings.os == "Windows":
+                self.cpp_info.defines = ["LIBLTDL_DLL_IMPORT"]
+        else:
+            if self.settings.os == "Linux":
+                self.cpp_info.system_libs = ["dl"]
