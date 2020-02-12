7c8218d7da2a9ee8d420da65d3d5f783e823897b
SSE4 <tomskside@gmail.com>
2020-02-12 18:09:01 +0700

    - add simple test for coroutine
    Signed-off-by: SSE4 <tomskside@gmail.com>

---
+++ recipes/boost/all/test_package/coroutine.cpp
@@ -0,0 +1,19 @@
+#include <boost/coroutine/all.hpp>
+#include <iostream>
+
+using namespace boost::coroutines;
+
+void cooperative(coroutine<void>::push_type &sink)
+{
+  std::cout << "Hello";
+  sink();
+  std::cout << "world";
+}
+
+int main()
+{
+  coroutine<void>::pull_type source{cooperative};
+  std::cout << ", ";
+  source();
+  std::cout << "!\n";
+}
