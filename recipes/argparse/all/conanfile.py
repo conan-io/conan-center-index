d79e26b4a1d5c47fbb5705041186118418b60a4d
Uilian Ries <uilianries@gmail.com>
2020-02-12 08:55:42 -0300

    argparse keep recipe simple
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/argparse/all/conanfile.py
+++ recipes/argparse/all/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 import os
 
@@ -10,17 +10,8 @@ class ArgparseConan(ConanFile):
     topics = ("conan", "argparse", "argument", "parsing")
     license = "MIT"
     description = "Argument Parser for Modern C++"
-    generators = "cmake"
-
     settings = "compiler"
-
-    exports_sources = ["CMakeLists.txt"]
-
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    no_copy_source = True
 
     _compiler_required_cpp17 = {
         "gcc": "7",
@@ -29,9 +20,13 @@ class ArgparseConan(ConanFile):
         "apple-clang": "10",
     }
 
+    @property
+    def _source_subfolder(self):
+        return os.path.join(self.source_folder, "source_subfolder")
+
     def configure(self):
-        if self.settings.compiler.cppstd and self.settings.compiler.cppstd in ("98", "gnu98", "11", "gnu11", "14", "gnu14"):
-            raise ConanInvalidConfiguration("This package requires C++17")
+        if self.settings.get_safe("compiler.cppstd"):
+            tools.check_valid_cppstd(self, "17")
         try:
             minimum_required_compiler_version = self._compiler_required_cpp17[str(self.settings.compiler)]
             if tools.Version(self.settings.compiler.version) < minimum_required_compiler_version:
@@ -43,25 +38,10 @@ class ArgparseConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("argparse-{}".format(self.version), self._source_subfolder)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-
-        self._cmake = CMake(self)
-        self._cmake.configure(source_folder=self._source_subfolder)
-        return self._cmake
-
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.install()
-
     def package(self):
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses", keep_path=False)
-        cmake = self._configure_cmake()
-        cmake.install()
-
-        tools.rmdir(os.path.join(self.package_folder, "lib"))
-
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        self.copy("*.hpp", src=os.path.join(self._source_subfolder, "include"), dst=os.path.join("include", "argparse"))
+        
     def package_id(self):
         self.info.header_only()
 
