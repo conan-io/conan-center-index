5c0b876d7d436e0fef1b4250e181ae88edb9e369
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-12 15:50:19 +0100

    cimg: simplify package_id + use tools.check_min_cppstd + add TODO

--- recipes/cimg/all/conanfile.py
+++ recipes/cimg/all/conanfile.py
@@ -14,7 +14,7 @@ class CImgConan(ConanFile):
     no_copy_source = True
     settings = "compiler"
 
-    _cimg_options = [
+    _cimg_options = (
         ("enable_fftw",         "cimg_use_fftw",       "fftw/3.3.8"),
         ("enable_jpeg",         "cimg_use_jpeg",       "libjpeg/9d"),
         ("enable_openexr",      "cimg_use_openexr",    "openexr/2.4.0"),
@@ -25,14 +25,14 @@ class CImgConan(ConanFile):
         # ("enable_magick"        "cimg_use_magick",     "magick/???"),
         # ("enable_xrandr",       "cimg_use_xrandr",     "xrandr/???"),
         # ("enable_xshm",         "cimg_use_xshm",       "xshm/???"),
-    ]
+    )
+    # TODO: Update requirements when available in CCI
 
     options = dict((option, [True, False]) for option, _, _ in _cimg_options)
     default_options = dict((option, True) for option, _, _ in _cimg_options)
 
     def configure(self):
-        if self.settings.compiler.cppstd and self.settings.compiler.cppstd in ("98"):
-            raise ConanInvalidConfiguration("cimg requires at least c++11")
+        tools.check_min_cppstd(self, "11")
 
     @property
     def _source_subfolder(self):
@@ -57,10 +57,7 @@ class CImgConan(ConanFile):
         pass
 
     def package_id(self):
-        del self.settings.compiler
         self.info.header_only()
-        for option, _, _ in self._cimg_options:
-            delattr(self.info.options, option)
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "CImg"
