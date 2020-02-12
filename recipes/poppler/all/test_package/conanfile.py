73254e809232208da59e7a73c8856be8debbaf29
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-12 19:22:51 +0100

    Add poppler/20.09.0 recipe

---
+++ recipes/poppler/all/test_package/conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "cmake_find_package"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            pdf_path = os.path.join(self.source_folder, "sample.pdf")
+            self.run("{} {}".format(bin_path, pdf_path), run_environment=True)
+
