e1de116e0144518df0a192d4aabdeceee042014b
vaerizk <ezerblaze@gmail.com>
2020-02-14 20:11:12 +0300

    wtl: add recipe

---
+++ recipes/wtl/all/conanfile.py
@@ -0,0 +1,24 @@
+from conans import ConanFile, tools
+
+import os
+
+class WTLConan(ConanFile):
+    name = "wtl"
+    license = "Microsoft Public License"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://sourceforge.net/projects/wtl"
+    description = "Windows Template Library (WTL) is a C++ library for developing Windows applications and UI components."
+    topics = ("atl", "template library", "windows", "template", "ui", "gdi")
+
+    settings = {'os': ['Windows']}
+    no_copy_source = True
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+
+    def package(self):
+        self.copy("*", dst="include", src=os.path.join(self.source_folder, "include"))
+        self.copy("MS-PL.TXT", dst="licenses", src=self.source_folder)
+
+    def package_id(self):
+        self.info.header_only()
