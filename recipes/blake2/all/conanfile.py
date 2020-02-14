0238d010a3a2c95a56bc7f31f288789b6f44131c
Riff, Eric <eriff@irobot.com>
2020-02-14 09:26:52 -0800

    Use CMakeList instead of makefile.

--- recipes/blake2/all/conanfile.py
+++ recipes/blake2/all/conanfile.py
@@ -1,7 +1,8 @@
 import os
 import stat
 import shutil
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
+from conans import ConanFile, tools, CMake
+from conans.errors import ConanInvalidConfiguration
 
 class blake2Conan(ConanFile):
     name = "blake2"
@@ -13,37 +14,30 @@ class blake2Conan(ConanFile):
                     SHA-1, SHA-2, and SHA-3, yet is at least as secure as the latest standard SHA-3")
     settings = "os", "arch", "compiler", "build_type"
     topics = ("conan", "blake2", "hash")
-    exports_sources = ["makefile"]
-    options = {"fPIC": [True, False], "use_sse": [True, False]}
-    default_options = {"fPIC": True, "use_sse": True}
+    exports_sources = ["CMakeLists.txt", "CMakeLists_build.txt"] #The first file is the conan wrapper, the second one is the actual CMakeList that builds the project
+    generators = ["cmake"]
+    options = {"fPIC": [True, False], "use_sse": [True, False], "use_neon": [True, False]}
+    default_options = {"fPIC": True, "use_sse": False, "use_neon": False}
     _source_subfolder = "source_subfolder"
-    _env_build = None
-    _env_vars = None
+    _cmake = None
 
-
-    def _configure_envbuilds(self):
-        if not self._env_build:
-            self._env_build = AutoToolsBuildEnvironment(self)
-            self._env_vars = self._env_build.vars
-            if self.settings.arch == "x86_64":
-                if self.options.use_sse:
-                    self._env_vars['USE_SSE'] = "True"
-                else:
-                    self._env_vars['USE_REF'] = "True"
-            elif "arm" in self.settings.arch:
-                self._env_vars['USE_NEON'] = "True"
-            self._env_vars['DESTDIR'] = self.package_folder
-        return self._env_build
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            self._cmake.definitions["USE_SSE"] = self.options.use_sse
+            self._cmake.definitions["USE_NEON"] = self.options.use_neon
+            self._cmake.configure()
+        return self._cmake
 
     def configure(self):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
+        if self.options.use_neon and not "arm" in self.settings.arch:
+            raise ConanInvalidConfiguration("Neon sources only supported on arm-based CPUs")
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        if self.settings.arch != "x86_64":
-            del self.options.use_sse
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -54,16 +48,14 @@ class blake2Conan(ConanFile):
 
     def build(self):
         #Move makefile into _source_subfolder.
-        shutil.move(src=os.path.join(self.source_folder, "makefile"), dst=os.path.join(self.source_folder,self._source_subfolder, "makefile"))
-        with tools.chdir(self._source_subfolder):
-            env_build = self._configure_envbuilds()
-            env_build.make(vars=self._env_vars)
+        shutil.move(src=os.path.join(self.source_folder, "CMakeLists_build.txt"), dst=os.path.join(self.source_folder,self._source_subfolder, "CMakeLists.txt"))
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
         self.copy("COPYING", src=self._source_subfolder, dst="licenses")
-        with tools.chdir(self._source_subfolder):
-            env_build = self._configure_envbuilds()
-            env_build.install(vars=self._env_vars)
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
