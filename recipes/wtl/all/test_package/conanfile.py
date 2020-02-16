f26ee7516fac638e8c9bca4c6e981cc2a78f49b6
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-16 21:51:15 +0100

    wtl: add cmake generator in test_package

--- recipes/wtl/all/test_package/conanfile.py
+++ recipes/wtl/all/test_package/conanfile.py
@@ -4,23 +4,14 @@ import os
 
 class WTLTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package"
-
-    @property
-    def _is_multi_configuration(self):
-        cmake = CMake(self)
-        return cmake.is_multi_configuration
+    generators = "cmake", "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = "example"
-            if self._is_multi_configuration:
-                bin_path = os.path.join(str(self.settings.build_type), bin_path)
+            bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
