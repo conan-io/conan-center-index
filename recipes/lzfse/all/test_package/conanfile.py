edf9a9af864ecd5ba9e2f73b905c392cc7088426
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-02-16 16:24:08 +0100

    add lzfse/1.0

---
+++ recipes/lzfse/all/test_package/conanfile.py
@@ -0,0 +1,18 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            txt_name = os.path.join(self.source_folder, "test.txt")
+            bin_path = os.path.join("bin", "test_package")
+            self.run("{0} -encode -i {1}".format(bin_path, txt_name), run_environment=True)
