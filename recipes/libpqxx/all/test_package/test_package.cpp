ee89d3e644e21199fd3aa25bfc87296f8ce4cc3b
tt4g <mainlinett@gmail.com>
2020-02-17 22:53:09 +0900

    Remove the code that makes the external connection

--- recipes/libpqxx/all/test_package/test_package.cpp
+++ recipes/libpqxx/all/test_package/test_package.cpp
@@ -7,8 +7,5 @@ int main()
 {
     std::cout << "libpqxx version: " << PQXX_VERSION << std::endl;
 
-    pqxx::connection conn("postgres://tpilupsd:7hJFAnmfyuD27orImLVfKQX1mdMUJTq_@stampy.db.elephantsql.com:5432/tpilupsd");
-    std::cout << "is connected: "<< conn.is_open() << std::endl;
-
     return EXIT_SUCCESS;
 }
