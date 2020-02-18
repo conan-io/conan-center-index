685d5ac87b0299ed570bbd4f28fe78bb181b632c
Croydon <git@cr0ydon.com>
2020-02-18 11:10:15 +0100

    libmodplug: Use sha256 parameter of tools.download() for caching

--- recipes/libmodplug/all/test_package/conanfile.py
+++ recipes/libmodplug/all/test_package/conanfile.py
@@ -14,8 +14,10 @@ class TestPackageConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             # Download a public domain xm file; https://modarchive.org/module.php?178293
-            tools.download("https://api.modarchive.org/downloads.php?moduleid=178293#burbs.xm", filename="burbs.xm")
-            tools.check_sha256("burbs.xm", "ced080401a2635cddc6d13b9095efa217f260ce7b3a482a29b454f72317b0c4d")
-
+            tools.download(
+                "https://api.modarchive.org/downloads.php?moduleid=178293#burbs.xm",
+                filename="burbs.xm",
+                sha256="ced080401a2635cddc6d13b9095efa217f260ce7b3a482a29b454f72317b0c4d",
+                )
             bin_path = os.path.join("bin", "test_package")
             self.run("%s %s" % (bin_path, "burbs.xm"), run_environment=True)
