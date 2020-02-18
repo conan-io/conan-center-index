9300ceeac4e27f1e50e0eb75eec43342b8a0378f
Anonymous Maarten <madebr@users.noreply.github.com>
2020-02-18 10:15:59 +0100

    physx: simplify testing of shared libraries in test_package
    Co-Authored-By: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/physx/4.1.1/test_package/conanfile.py
+++ recipes/physx/4.1.1/test_package/conanfile.py
@@ -9,7 +9,7 @@ class TestPackageConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["TEST_SHARED_LIBRARY"] = self.options["physx"].shared or "fPIC" not in self.options["physx"].fields or ("fPIC" in self.options["physx"].fields and self.options["physx"].fPIC)
+        cmake.definitions["TEST_SHARED_LIBRARY"] = True if "fPIC" not in self.options["physx"].fields else self.options["physx"].fPIC
         cmake.configure()
         cmake.build()
 
