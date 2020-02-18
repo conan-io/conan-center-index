8d85d901ff1e7311b3d365a527b7fa23a3269fc8
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-02-18 22:35:34 +0100

    Apply suggestions from code review
    Co-Authored-By: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/bacnet-stack/all/conanfile.py
+++ recipes/bacnet-stack/all/conanfile.py
@@ -67,6 +67,6 @@ class BacnetStackConan(ConanFile):
                                  "lib", "bacnet-stack", "cmake"))
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        if tools.os_info.is_linux:
+        self.cpp_info.libs = ["bacnet-stack"]
+        if self.settings.os == "Linux":
             self.cpp_info.system_libs = ["pthread"]
