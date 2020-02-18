c0e2a18e024aebe6d1bd84671311ea97e3b34d94
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-02-18 18:37:53 +0100

    Update recipes/bacnet-stack/all/test_package/conanfile.py
    Co-Authored-By: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/bacnet-stack/all/test_package/conanfile.py
+++ recipes/bacnet-stack/all/test_package/conanfile.py
@@ -16,4 +16,4 @@ class TestConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             bin_path = os.path.join("bin", "test_package")
-            self.run(".%sexample" % os.sep)
+            bin_path = self.run(bin_path, run_environment=True)
