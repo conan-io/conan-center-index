de15712a6129f14c38f763499bf4f59a5c5321b4
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-02-18 17:50:42 +0100

    Update recipes/bacnet-stack/all/test_package/conanfile.py
    Co-Authored-By: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/bacnet-stack/all/test_package/conanfile.py
+++ recipes/bacnet-stack/all/test_package/conanfile.py
@@ -15,7 +15,6 @@ class TestConan(ConanFile):
         self.copy("*.dll", dst="bin", src="bin")
         self.copy("*.dylib*", dst="bin", src="lib")
         self.copy('*.so*', dst='bin', src='lib')
-
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
