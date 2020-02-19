7ccc32a4c593c140c3b4ee52c08aadfc40102b60
Eric Riff <ericriff@gmail.com>
2020-02-18 18:08:46 -0800

    Fix test_package on windows

--- recipes/blake2/all/test_package/example.cpp
+++ recipes/blake2/all/test_package/example.cpp
@@ -9,13 +9,13 @@ int main() {
     const std::string key = "C++";
     const std::string expected_hash = "aff48ebab1a2b9947c811248b52a157738159af3a2599993188fd3977d20c58e67a98636717e13b62b95183ff779abdb2dcad039599f5e54588d83a7b2fc3827";
     std::array<unsigned char, BLAKE2B_OUTBYTES> output;
-    bool result; // Zero if OK
+    int result; // Zero if OK
 
 	result = blake2(output.data(), output.size(), input.data(), input.size(), key.data(), key.size());
 
-    std::cout << "Hashing string: " << input << std::endl;
-    std::cout << "Using key: " << key << std::endl;
-    std::cout << "Expected hash: " << expected_hash << std::endl;
+    std::cout << "Hashing string: " << input.data() << std::endl;
+    std::cout << "Using key: " << key.data() << std::endl;
+    std::cout << "Expected hash: " << expected_hash.data() << std::endl;
 
     if (result != 0){
         std::cout << "Error during hashing";
