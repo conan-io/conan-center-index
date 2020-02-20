f657db32fb3c59008edcb0d3a7fb94816df177e3
Uilian Ries <uilianries@gmail.com>
2020-02-20 11:53:57 -0300

    Update libtiff recipe
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libtiff/all/conanfile.py
+++ recipes/libtiff/all/conanfile.py
@@ -12,15 +12,23 @@ class LibtiffConan(ConanFile):
     homepage = "http://www.simplesystems.org/libtiff"
     topics = ("tiff", "image", "bigtiff", "tagged-image-file-format")
     exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False], "lzma": [True, False], "jpeg": [True, False]}
-    default_options = {'shared': False, 'fPIC': True, "lzma": True, "jpeg": True}
-    _source_subfolder = "source_subfolder"
+    options = {"shared": [True, False], "fPIC": [True, False], "lzma": [True, False], "jpeg": [True, False], "zlib": [True, False], "zstd": [True, False]}
+    default_options = {'shared': False, 'fPIC': True, "lzma": True, "jpeg": True, "zlib": True, "zstd": True}
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
-            self.options.remove("fPIC")
+            del self.options.remove.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
@@ -33,8 +41,10 @@ class LibtiffConan(ConanFile):
                     os.path.join(self._source_subfolder, "CMakeLists.txt"))
 
     def requirements(self):
-        self.requires("zlib/1.2.11")
-        self.requires("zstd/1.4.3")
+        if self.options.zlib:
+            self.requires("zlib/1.2.11")
+        if self.options.zstd:
+            self.requires("zstd/1.4.3")
         if self.options.lzma:
             self.requires("xz_utils/5.2.4")
         if self.options.jpeg:
@@ -42,44 +52,51 @@ class LibtiffConan(ConanFile):
         if Version(self.version) >= "4.1.0":
             self.requires("libwebp/1.0.3")
 
-    def build(self):
-        cmake = CMake(self)
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            self._cmake.definitions['CMAKE_INSTALL_LIBDIR'] = 'lib'
+            self._cmake.definitions['CMAKE_INSTALL_BINDIR'] = 'bin'
+            self._cmake.definitions['CMAKE_INSTALL_INCLUDEDIR'] = 'include'
+            self._cmake.definitions["lzma"] = self.options.lzma
+            self._cmake.definitions["jpeg"] = self.options.jpeg
+            self._cmake.definitions["jbig"] = False
+            self._cmake.definitions["zlib"] = self.options.zlib
+            self._cmake.definitions["zstd"] = self.options.zstd
 
-        cmake.definitions['CMAKE_INSTALL_LIBDIR'] = 'lib'
-        cmake.definitions['CMAKE_INSTALL_BINDIR'] = 'bin'
-        cmake.definitions['CMAKE_INSTALL_INCLUDEDIR'] = 'include'
+            if self.options.shared and self.settings.compiler == "Visual Studio":
+                # https://github.com/Microsoft/vcpkg/blob/master/ports/tiff/fix-cxx-shared-libs.patch
+                tools.replace_in_file(os.path.join(self._source_subfolder, 'libtiff', 'CMakeLists.txt'),
+                                    r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION})',
+                                    r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION} '
+                                    r'WINDOWS_EXPORT_ALL_SYMBOLS ON)')
 
-        cmake.definitions["lzma"] = self.options.lzma
-        cmake.definitions["jpeg"] = self.options.jpeg
-        cmake.definitions["jbig"] = False
-        if self.options.shared and self.settings.compiler == "Visual Studio":
-            # https://github.com/Microsoft/vcpkg/blob/master/ports/tiff/fix-cxx-shared-libs.patch
-            tools.replace_in_file(os.path.join(self._source_subfolder, 'libtiff', 'CMakeLists.txt'),
-                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION})',
-                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION} '
-                                  r'WINDOWS_EXPORT_ALL_SYMBOLS ON)')
-
-        if self.settings.os == "Windows" and self.settings.compiler != "Visual Studio":
-            tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
-                                      "find_library(M_LIBRARY m)",
-                                      "if (NOT MINGW)\n  find_library(M_LIBRARY m)\nendif()")
-            if self.version == '4.0.8':
-                # only one occurence must be patched. fixed in 4.0.9
+            if self.settings.os == "Windows" and self.settings.compiler != "Visual Studio":
                 tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
-                                      "if (UNIX)",
-                                      "if (UNIX OR MINGW)")
+                                        "find_library(M_LIBRARY m)",
+                                        "if (NOT MINGW)\n  find_library(M_LIBRARY m)\nendif()")
+                if self.version == '4.0.8':
+                    # only one occurence must be patched. fixed in 4.0.9
+                    tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
+                                        "if (UNIX)",
+                                        "if (UNIX OR MINGW)")
 
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
-            "add_subdirectory(tools)\nadd_subdirectory(test)\nadd_subdirectory(contrib)\nadd_subdirectory(build)\n"
-            "add_subdirectory(man)\nadd_subdirectory(html)", "")
+            tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
+                "add_subdirectory(tools)\nadd_subdirectory(test)\nadd_subdirectory(contrib)\nadd_subdirectory(build)\n"
+                "add_subdirectory(man)\nadd_subdirectory(html)", "")
 
-        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        cmake.configure(source_folder=self._source_subfolder)
+            self._cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+            self._cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
         self.copy("COPYRIGHT", src=self._source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
         tools.rmdir(os.path.join(self.package_folder, 'lib', 'pkgconfig'))
 
     def package_info(self):
