e8c60cc56e658a115ba38f63beb704786e99969c
Uilian Ries <uilianries@gmail.com>
2020-02-20 12:29:46 -0300

    Move patch for libtiff
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libtiff/all/conanfile.py
+++ recipes/libtiff/all/conanfile.py
@@ -70,19 +70,19 @@ class LibtiffConan(ConanFile):
         if self.options.shared and self.settings.compiler == "Visual Studio":
             # https://github.com/Microsoft/vcpkg/blob/master/ports/tiff/fix-cxx-shared-libs.patch
             tools.replace_in_file(os.path.join(self._source_subfolder, 'libtiff', 'CMakeLists.txt'),
-                                r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION})',
-                                r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION} '
-                                r'WINDOWS_EXPORT_ALL_SYMBOLS ON)')
+                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION})',
+                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION} '
+                                  r'WINDOWS_EXPORT_ALL_SYMBOLS ON)')
 
             if self.settings.os == "Windows" and self.settings.compiler != "Visual Studio":
                 tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
-                                        "find_library(M_LIBRARY m)",
-                                        "if (NOT MINGW)\n  find_library(M_LIBRARY m)\nendif()")
+                                      "find_library(M_LIBRARY m)",
+                                      "if (NOT MINGW)\n  find_library(M_LIBRARY m)\nendif()")
                 if self.version == '4.0.8':
                     # only one occurence must be patched. fixed in 4.0.9
                     tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
-                                        "if (UNIX)",
-                                        "if (UNIX OR MINGW)")
+                                          "if (UNIX)",
+                                          "if (UNIX OR MINGW)")
 
         tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
             "add_subdirectory(tools)\nadd_subdirectory(test)\nadd_subdirectory(contrib)\nadd_subdirectory(build)\n"
