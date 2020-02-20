7bd0fa5553f8f5c1ef5e3705bd5845eaaccb027e
Carlos Gomes Martinho <carlos.gomes_martinho@siemens.com>
2020-02-20 14:17:52 +0100

    chore: add some code to libcoap test package

--- recipes/libcoap/all/test_package/test_package.cpp
+++ recipes/libcoap/all/test_package/test_package.cpp
@@ -1,5 +1,13 @@
+#include "coap2/coap.h"
 
 int main(int argc, char *argv[]) {
+  coap_startup();
+
+  // create CoAP context and a client session
+  coap_context_t *ctx = coap_new_context(nullptr);
+
+  coap_free_context(ctx);
+  coap_cleanup();
 
   return 0;
 }
