ffe0629c073793ec073be9f7cc41d61678a1840e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-02-23 01:13:05 +0100

    add fast-cpp-csv-parser/20191004

---
+++ recipes/fast-cpp-csv-parser/all/test_package/conanfile.py
@@ -0,0 +1,18 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            csv_name = os.path.join(self.source_folder, "test_package.csv")
+            bin_path = os.path.join("bin", "test_package")
+            self.run("{0} {1}".format(bin_path, csv_name), run_environment=True)
