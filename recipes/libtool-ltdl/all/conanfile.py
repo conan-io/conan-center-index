d7beba1daa97fe434e6513094f8339f1dd807c26
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-24 14:20:40 +0100

    libtool-ltdl: address review comments:
    - Moved deletion of libcxx/cppstd to configure
    - Fixed license
    
    Reviewed by @uilianries

--- recipes/libtool-ltdl/all/conanfile.py
+++ recipes/libtool-ltdl/all/conanfile.py
@@ -9,7 +9,7 @@ class LibtoolLtDlConan(ConanFile):
     homepage = "https://www.gnu.org/software/libtool/"
     description = "GNU libtool is a generic library support script."
     topics = ("conan", "libtool", "configure", "library", "shared", "static")
-    license = ("GPL-2.0-or-later", "GPL-3.0-or-later")
+    license = "GPL-2.0-or-later"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -29,10 +29,10 @@ class LibtoolLtDlConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
 
     def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
         if self.options.shared:
             del self.options.fPIC
 
