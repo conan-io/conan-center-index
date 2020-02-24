81b5152b9ed0e715806383f1a41f725120ccc658
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-02-24 13:17:56 +0100

    add libsgp4/20191207

---
+++ recipes/libsgp4/all/conanfile.py
@@ -0,0 +1,56 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+class Libsgp4Conan(ConanFile):
+    name = "libsgp4"
+    description = "C++11 library for SGP4 Simplified perturbations model."
+    license = "Apache-2.0"
+    topics = ("conan", "libsgp4", "sgp4", "orbit", "satellite", "perturbation")
+    homepage = "https://github.com/dnwrnr/sgp4"
+    url = "https://github.com/conan-io/conan-center-index"
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        url = self.conan_data["sources"][self.version]["url"]
+        extracted_dir = "sgp4-" + os.path.splitext(os.path.basename(url))[0]
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs = ["include", os.path.join("include", "libsgp4")]
