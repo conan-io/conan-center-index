5e3244826b7430bf933a01becb9f03744847cc08
Carlos Gomes Martinho <carlos.gomes_martinho@siemens.com>
2020-02-25 15:21:22 +0100

    feat: add replxx

---
+++ recipes/replxx/all/test_package/test_package.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "replxx.hxx"
+
+int main(int argc, char **argv) {
+  using Replxx = replxx::Replxx;
+
+  Replxx rx;
+  return 0;
+}
