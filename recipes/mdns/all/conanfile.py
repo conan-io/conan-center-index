f288a8271a0e4ec786492384404ace6fa22f1022
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-02-25 21:53:30 +0100

    chore: fix path to license

--- recipes/mdns/all/conanfile.py
+++ recipes/mdns/all/conanfile.py
@@ -60,8 +60,7 @@ class MdnsConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst='licenses',
-                  src=os.path.join(self._source_subfolder, "license"))
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
 
