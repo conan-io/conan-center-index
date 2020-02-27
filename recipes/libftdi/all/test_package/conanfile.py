71bdf1b5015c2c070f285acca57014c53674b170
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-27 20:59:36 +0100

    Add libftdi/0.20 recipe

---
+++ recipes/libftdi/all/test_package/conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            config_path = os.path.join(self.source_folder, "hello.conf")
+            self.run("{} {}".format(bin_path, config_path), run_environment=True)
