b1869aec51b197e5b339702385781260fcf4bd7c
Flex Ferrum <flexferrum@gmail.com>
2020-02-28 15:42:29 +0300

    Add proper conan environment setup for the test package

--- recipes/jinja2cpp/1.1.0/test_package/conanfile.py
+++ recipes/jinja2cpp/1.1.0/test_package/conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class Jinja2CppTestPackage(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake_find_package"
+    generators = "cmake", "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
@@ -17,13 +17,13 @@ class Jinja2CppTestPackage(ConanFile):
 
     def test(self):
         if tools.os_info.is_windows:
-            bin_path = str(self.settings.build_type).lower()
+            bin_path = '.\\bin'
             extension = ".exe"
         elif tools.os_info.is_linux:
-            bin_path = "."
+            bin_path = "./bin"
             extension = ""
         else:
-            bin_path = "."
+            bin_path = "./bin"
             extension = ""
 
         self.run(os.path.join(bin_path, "jinja2cpp-test-package" + extension), run_environment=True)
