b5582208d2eb56ff2a02dcd56fb423396e3d2d7c
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-28 17:39:39 +0100

    ncurses: fix package + simplify test_package

--- recipes/ncurses/all/test_package/test_package.c
+++ recipes/ncurses/all/test_package/test_package.c
@@ -2,39 +2,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-
-#if defined(_WINDOWS)
-#include <windows.h>
-#else
-#include <unistd.h>
-#endif
 
 int main(int argc, char *argv[]) {
-    WINDOW* window = NULL;
-
-    printf("ncurses version: %s\n", curses_version());
-
-    window = initscr();
-
-    if (window != NULL) {
-        if (start_color() != ERR) {
-            init_pair(1, COLOR_BLACK, COLOR_CYAN);
-            init_pair(2, COLOR_BLACK, COLOR_GREEN);
-
-            attron(COLOR_PAIR(1));
-            printw("Conan, the C / C++ Package Manager for Developers\n");
-            attron(COLOR_PAIR(2));
-        }
-        printw("This is the ncurses Conan package!\n");
-
-        wrefresh(stdscr);
-#ifdef _MSC_VER
-        Sleep(1000);
-#else
-        sleep(1);
-#endif
-    }
-    endwin();
+    printf("ncurses version '%s'\n", curses_version());
     return EXIT_SUCCESS;
 }
