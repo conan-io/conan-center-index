f43605d0f6eb7b83c2744c117d0ccdb0919109cc
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-28 17:59:41 +0100

    ncurses: do not run 'reset' in test_package

--- recipes/ncurses/all/test_package/conanfile.py
+++ recipes/ncurses/all/test_package/conanfile.py
@@ -15,5 +15,3 @@ class TestPackageConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             self.run(os.path.join("bin", "test_package"), run_environment=True)
-            if tools.which("reset"):
-                self.run("reset")
