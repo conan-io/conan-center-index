db9d5dda6ed053d0ae4453ddc655a79e2f01aebb
Quentin Chateau <quentin.chateau@gmail.com>
2020-03-03 22:05:51 +0100

    renamed libguarded to cs_libguarded

--- recipes/cs_libguarded/all/test_package/conanfile.py
+++ recipes/cs_libguarded/all/test_package/conanfile.py
@@ -3,7 +3,7 @@ import os
 from conans import ConanFile, CMake, tools
 
 
-class LibguardedTestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
