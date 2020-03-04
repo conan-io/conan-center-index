4043bd1b773b51d025a70394e581a88fd8b8dc8e
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-03-04 09:51:18 +0100

    use upstream cmakelists

--- recipes/flac/all/conanfile.py
+++ recipes/flac/all/conanfile.py
@@ -8,7 +8,7 @@ class FlacConan(ConanFile):
     topics = ("conan", "flac", "codec", "audio", )
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/xiph/flac"
-    license = "BSD"
+    license = ("BSD", "GPL", "LPGL")
     requires = "ogg/1.3.4"
     exports_sources = "CMakeLists.txt",
     generators = "cmake",
@@ -16,12 +16,10 @@ class FlacConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "use_asm": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "use_asm": False,
     }
     _source_subfolder = "source_subfolder"
 
@@ -30,8 +28,7 @@ class FlacConan(ConanFile):
             del self.options.fPIC
 
     def build_requirements(self):
-        if self.options.use_asm:
-            self.build_requires("nasm/2.13.02")
+        self.build_requires("nasm/2.14")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -40,7 +37,9 @@ class FlacConan(ConanFile):
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["USE_ASM"] = self.options.use_asm
+        cmake.definitions["BUILD_EXAMPLES"] = False
+        cmake.definitions["BUILD_DOCS"] = False
+        cmake.definitions["BUILD_TESTING"] = False
         cmake.configure()
         return cmake
 
@@ -49,11 +48,15 @@ class FlacConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy(pattern="COPYING.*", dst="licenses", src=self._source_subfolder, keep_path=False)
         cmake = self._configure_cmake()
         cmake.install()
+        self.copy(pattern="COPYING.*", dst="licenses", src=self._source_subfolder, keep_path=False)
+        self.copy(pattern="*.h", dst=os.path.join("include", "share"), src=os.path.join(self._source_subfolder, "include", "share"), keep_path=False)
+        self.copy(pattern="*.h", dst=os.path.join("include", "share", "grabbag"),
+                  src=os.path.join(self._source_subfolder, "include", "share", "grabbag"), keep_path=False)
+        tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
-        self.cpp_info.libs = ['flac++', 'flac']
+        self.cpp_info.libs = ['FLAC++', 'FLAC']
         if not self.options.shared:
             self.cpp_info.defines = ["FLAC__NO_DLL"]
