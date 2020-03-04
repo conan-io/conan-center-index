92bbecb71bf6936394c1b5b97e6d650e2feba88d
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-03-04 17:23:03 +0100

    jwt-cpp: test_package needs cmake generator

--- recipes/jwt-cpp/all/conanfile.py
+++ recipes/jwt-cpp/all/conanfile.py
@@ -1,6 +1,6 @@
 from conans import ConanFile, tools
-
 import os
+
 
 class JwtCppConan(ConanFile):
     name = "jwt-cpp"
@@ -26,7 +26,7 @@ class JwtCppConan(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def package(self):
-        header_dir = self._source_subfolder + "/include/jwt-cpp/"
+        header_dir = os.path.join(self._source_subfolder, "include", "jwt-cpp/")
         self.copy("jwt.h", dst="include", src=header_dir, keep_path=False)
         self.copy("base.h", dst="include", src=header_dir, keep_path=False)
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
