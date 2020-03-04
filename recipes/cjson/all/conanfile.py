72fae9c3856d84ba2a6c8112ef6051505e5ac2c2
Morwenn <morwenn29@hotmail.fr>
2020-03-04 09:51:06 +0100

    cjson: add include/cjson to inlude directories
    Some libraries such as k4a use #include <cJSON.h> directly.

--- recipes/cjson/all/conanfile.py
+++ recipes/cjson/all/conanfile.py
@@ -89,6 +89,7 @@ class CjsonConan(ConanFile):
                 shutil.move(dll_file, bin_dir)
 
     def package_info(self):
+        self.cpp_info.includedirs.append(os.path.join("include", "cjson"))
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("m")
