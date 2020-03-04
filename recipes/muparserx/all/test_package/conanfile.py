c68f774ac0ac72dbb096296ff96f6dcc26010d8c
vvilpas <vvilpas@gmail.com>
2020-03-04 20:17:31 +0100

    Fixing things

--- recipes/muparserx/all/test_package/conanfile.py
+++ recipes/muparserx/all/test_package/conanfile.py
@@ -13,5 +13,5 @@ class MuparserxTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            os.chdir("bin")
-            self.run(".%stest_package" % os.sep)
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
