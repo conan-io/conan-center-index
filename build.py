f29ac19edf6db452adea3af533d92d4616fb6da6
Vladimir Looze <woldemar@mimas.ru>
2020-03-05 18:57:10 +0300

    workaround for cpt username channel behavior (#12)
    cpt username & channel workaround

--- build.py
+++ build.py
@@ -1,8 +1,19 @@
 from os import environ
+from sys import platform
 from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
+
+    # workaround for platform-specific CPT behaviour
+    # see https://github.com/conan-io/conan-package-tools/issues/479
+    if platform in ["darwin", "win32"]:
+        environ["CONAN_USERNAME"] = "_"
+        environ["CONAN_CHANNEL"] = "_"
+    else:
+        if not environ["CONAN_REFERENCE"].endswith("@/"):
+            environ["CONAN_REFERENCE"] += "@/"
+
     builder = ConanMultiPackager(
         login_username="trassir-ci-bot",
         upload="https://api.bintray.com/conan/trassir/conan-public",
