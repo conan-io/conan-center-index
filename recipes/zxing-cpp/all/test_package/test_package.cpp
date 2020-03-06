27184543b4721ecee967c35a79ecf86f7b173d25
Anonymous Maarten <madebr@users.noreply.github.com>
2020-03-06 23:53:44 +0100

    zxing-cpp: fix include in test_package

--- recipes/zxing-cpp/all/test_package/test_package.cpp
+++ recipes/zxing-cpp/all/test_package/test_package.cpp
@@ -1,8 +1,8 @@
-#include "BarcodeFormat.h"
-#include "MultiFormatWriter.h"
-#include "BitMatrix.h"
-#include "ByteMatrix.h"
-#include "TextUtfEncoding.h"
+#include "ZXing/BarcodeFormat.h"
+#include "ZXing/MultiFormatWriter.h"
+#include "ZXing/BitMatrix.h"
+#include "ZXing/ByteMatrix.h"
+#include "ZXing/TextUtfEncoding.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
