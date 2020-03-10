75c0881df79b3af95ab945f558f452d98d3a939b
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-03-10 16:41:16 +0100

    protoc: do not build and test when cross building

--- recipes/protoc/3.9.x/test_package/conanfile.py
+++ recipes/protoc/3.9.x/test_package/conanfile.py
@@ -10,11 +10,12 @@ class TestPackageConan(ConanFile):
         self.requires("protobuf/{}".format(self.requires["protoc"].ref.version))
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["protobuf_VERBOSE"] = True
-        cmake.definitions["protobuf_MODULE_COMPATIBLE"] = True
-        cmake.configure()
-        cmake.build()
+        if not tools.cross_building(self.settings):
+            cmake = CMake(self)
+            cmake.definitions["protobuf_VERBOSE"] = True
+            cmake.definitions["protobuf_MODULE_COMPATIBLE"] = True
+            cmake.configure()
+            cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
@@ -22,5 +23,5 @@ class TestPackageConan(ConanFile):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
 
-        assert os.path.isfile(os.path.join(self.build_folder, "addressbook.pb.cc"))
-        assert os.path.isfile(os.path.join(self.build_folder, "addressbook.pb.h"))
+            assert os.path.isfile(os.path.join(self.build_folder, "addressbook.pb.cc"))
+            assert os.path.isfile(os.path.join(self.build_folder, "addressbook.pb.h"))
