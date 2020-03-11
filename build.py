90c5557d7e24e95c80753bef5606a471efc446ed
Artalus <artalus-mail@yandex.ru>
2020-03-11 17:31:11 +0300

    Use IS_PURE_C envvar to speed up C libraries build (#24)

--- build.py
+++ build.py
@@ -1,12 +1,14 @@
 from os import environ
 from sys import platform
 from cpt.packager import ConanMultiPackager
+from cpt.tools import get_bool_from_env
 
 
 if __name__ == "__main__":
     environ["CONAN_USERNAME"] = "_"
     environ["CONAN_CHANNEL"] = "ci"
 
+    is_pure_c = get_bool_from_env('IS_PURE_C')
     builder = ConanMultiPackager(
         login_username="trassir-ci-bot",
         upload="https://api.bintray.com/conan/trassir/conan-public",
@@ -15,5 +17,5 @@ if __name__ == "__main__":
         stable_channel="_",
         remotes="https://api.bintray.com/conan/trassir/conan-public"
     )
-    builder.add_common_builds(pure_c=False)
+    builder.add_common_builds(pure_c=is_pure_c)
     builder.run()
