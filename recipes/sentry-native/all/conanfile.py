707ba6bbf12e9403ea3756d0bfa9361120318100
Carlos Gomes Martinho <carlos.gomes_martinho@siemens.com>
2020-03-11 14:00:56 +0100

    feat: add sentry native

---
+++ recipes/sentry-native/all/conanfile.py
@@ -0,0 +1,67 @@
+import glob
+import os
+from conans import ConanFile, CMake, tools
+from conans.model.version import Version
+from conans.errors import ConanInvalidConfiguration
+
+
+class SentryNativeConan(ConanFile):
+    name = "sentry-native"
+    description = "Sentry SDK for C, C++ and native applications."
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/getsentry/sentry-native"
+    license = "MIT"
+    topics = ("conan", "breakpad", "crashpad",
+              "error-reporting", "crash-reporting")
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "backend": ["none", "crashpad", "inproc"],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "backend": "none"
+    }
+    _source_subfolder = "source_subfolder"
+
+    _cmake = None
+
+    def requirements(self):
+        self.requires("libcurl/7.67.0")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        self._cmake.configure()
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs.append("pthread")
+        if self.settings.os == "Windows":
+            self.cpp_info.system_libs.extend(["winhttp"])
+
---
+++ recipes/sentry-native/all/test_package/test_package.cpp
@@ -0,0 +1,15 @@
+#include "sentry.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+  sentry_options_t *options = sentry_options_new();
+
+  sentry_options_set_environment(options, "Production");
+  sentry_options_set_release(options, "test-example-release");
+
+  sentry_init(options);
+
+  sentry_shutdown();
+}
