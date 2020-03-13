b199e72bfc6cecfad9b0e462b6c736fb4df7276a
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-03-13 09:11:31 +0100

    Update recipes/sentry-native/all/conanfile.py
    Co-Authored-By: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/sentry-native/all/conanfile.py
+++ recipes/sentry-native/all/conanfile.py
@@ -68,10 +68,11 @@ class SentryNativeConan(ConanFile):
         if self.settings.os in ("Android", "Windows"):
             self.cpp_info.exelinkflags= ["--build-id=sha1"]
             self.cpp_info.sharedlinkflags = ["--build-id=sha1"]
-        if self.settings.os == "Linux":
-            self.cpp_info.system_libs = ["pthread", "dl"]
-        elif self.settings.os == "Windows":
-            self.cpp_info.system_libs = ["winhttp"]
+        if not self.options.shared:
+            if self.settings.os == "Linux":
+                self.cpp_info.system_libs = ["pthread", "dl"]
+            elif self.settings.os == "Windows":
+                self.cpp_info.system_libs = ["winhttp", "dbghelp", "pathcch"]
 
         if not self.options.shared:
             self.cpp_info.defines = ["SENTRY_BUILD_STATIC"]
