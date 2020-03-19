bd5cbfef1d2ae02fee87bb6cc2057f0a111374ad
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-03-20 00:14:28 +0100

    aws-c-common: patch out fPIC override

--- recipes/aws-c-common/all/conanfile.py
+++ recipes/aws-c-common/all/conanfile.py
@@ -7,9 +7,9 @@ class AwsCCommon(ConanFile):
     description = "Core c99 package for AWS SDK for C. Includes cross-platform primitives, configuration, data structures, and error handling."
     topics = ("conan", "aws", "amazon", "cloud", )
     url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/aws/aws-sdk-cpp"
+    homepage = "https://github.com/awslabs/aws-sdk-cpp"
     license = "Apache-2.0",
-    exports_sources = "CMakeLists.txt"
+    exports_sources = "CMakeLists.txt", "patches/**"
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -50,7 +50,12 @@ class AwsCCommon(ConanFile):
         self._cmake.configure()
         return self._cmake
 
+    def _patch_sources(self):
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
+
     def build(self):
+        self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
 
