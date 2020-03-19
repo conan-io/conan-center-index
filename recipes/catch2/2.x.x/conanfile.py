72e10a3b95029cc5d19fbb5e70c250daceb5e387
Chris Mc <prince.chrismc@gmail.com>
2020-03-19 08:53:01 -0400

    making sure cmake is only configured once

--- recipes/catch2/2.x.x/conanfile.py
+++ recipes/catch2/2.x.x/conanfile.py
@@ -12,8 +12,15 @@ class ConanRecipe(ConanFile):
     license = "BSL-1.0"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    
+    _cmake = None
+    
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -21,15 +28,17 @@ class ConanRecipe(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = "OFF"
-        cmake.definitions["CATCH_INSTALL_DOCS"] = "OFF"
-        cmake.definitions["CATCH_INSTALL_HELPERS"] = "ON"
-        cmake.configure(
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_TESTING"] = "OFF"
+        self._cmake.definitions["CATCH_INSTALL_DOCS"] = "OFF"
+        self._cmake.definitions["CATCH_INSTALL_HELPERS"] = "ON"
+        self._cmake.configure(
             source_folder=self._source_subfolder,
             build_folder=self._build_subfolder
         )
-        return cmake
+        return self._cmake
 
     def build(self):
         cmake = self._configure_cmake()
