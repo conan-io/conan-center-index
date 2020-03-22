0250b3b6a92d053dac4e04e0b06ac32a3c423193
Chris Mc <prince.chrismc@gmail.com>
2020-03-22 09:40:57 -0400

    migrating websocketpp/0.8.1@bincrafters/stable to CCI
    Based on: https://github.com/bincrafters/conan-websocketpp/tree/fea456bee75c7e8459e358207e2477c8243ef735
    
    fixes #1156
    
    Co-authored-by: Croydon <git@cr0ydon.com>

---
+++ recipes/websocketpp/all/test_package/conanfile.py
@@ -0,0 +1,16 @@
+from conans import ConanFile, CMake, RunEnvironment, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        with tools.environment_append(RunEnvironment(self).vars):
+            self.run(os.path.join("bin", "test_package"))
