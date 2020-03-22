dd2245d2aefea5f246bb5aba89bdcea816e33f53
Chris Mc <prince.chrismc@gmail.com>
2020-03-22 10:29:44 -0400

    correcting errors from hooks

--- recipes/websocketpp/all/test_package/conanfile.py
+++ recipes/websocketpp/all/test_package/conanfile.py
@@ -12,5 +12,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            self.run(os.path.join("bin", "test_package"))
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
