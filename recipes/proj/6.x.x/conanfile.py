bb4f67847f830e540202134f378a174e8a13b3d8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-03-26 10:17:27 +0100

    proj: several improvements
    - 6.x.x folder instead of all, since v7 add options and change cmake
    target
    - use exe in sqlite3 package in order to create proj.db during build process,
    instead of a python script
    - add threadsafe option
    - rename conan class
    - bump sqlite3 version in requirements
    - remove dl from system_libs
    - define PROJ_MSVC_DLL_IMPORT if shared and Visual Studio
    - add generated execs folder to env PATH
    - install proj.db in res folder
    - define PROJ4 as CMake exported target

--- recipes/proj/6.x.x/conanfile.py
+++ recipes/proj/6.x.x/conanfile.py
@@ -1,8 +1,9 @@
 from conans import ConanFile, CMake, tools
+import glob
 import os
 
 
-class LibaecConan(ConanFile):
+class ProjConan(ConanFile):
     name = "proj"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
@@ -13,14 +14,16 @@ class LibaecConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "threadsafe": [True, False]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
+        "threadsafe": True
     }
 
     generators = "cmake"
-    exports_sources = ["CMakeLists.txt", "sqlite_init.py", "patches/*"]
+    exports_sources = ["CMakeLists.txt", "patches/*"]
 
     _cmake = None
 
@@ -37,7 +40,7 @@ class LibaecConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("sqlite3/3.31.0")
+        self.requires.add("sqlite3/3.31.1")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -50,15 +53,22 @@ class LibaecConan(ConanFile):
         self._cmake = CMake(self)
         self._cmake.definitions["PROJ_TESTS"] = False
         self._cmake.definitions["BUILD_LIBPROJ_SHARED"] = self.options.shared
+        self._cmake.definitions["USE_THREAD"] = self.options.threadsafe
+        self._cmake.definitions["ENABLE_LTO"] = False
+        self._cmake.definitions["JNI_SUPPORT"] = False
+        self._cmake.definitions["BUILD_CCT"] = True
+        self._cmake.definitions["BUILD_CS2CS"] = True
+        self._cmake.definitions["BUILD_GEOD"] = True
+        self._cmake.definitions["BUILD_GIE"] = True
+        self._cmake.definitions["BUILD_PROJ"] = True
+        self._cmake.definitions["BUILD_PROJINFO"] = True
+        self._cmake.definitions["PROJ_DATA_SUBDIR"] = "res"
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
     def build(self):
         for patch in self.conan_data["patches"][self.version]:
             tools.patch(**patch)
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "lib_proj.cmake"),
-                              "include_directories(${CMAKE_SOURCE_DIR}/include)",
-                              "include_directories(${PROJ4_SOURCE_DIR}/include)")
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -66,14 +76,21 @@ class LibaecConan(ConanFile):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        self.copy("*.db",
-                  src=os.path.join(self.package_folder, "share", "proj"),
-                  dst=os.path.join(self.package_folder, "lib", "proj"))
         tools.rmdir(os.path.join(self.package_folder, "share"))
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        for data_file in glob.glob(os.path.join(self.package_folder, "res", "*")):
+            if not data_file.endswith("proj.db"):
+                os.remove(data_file)
 
     def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "PROJ4"
+        self.cpp_info.names["cmake_find_package_multi"] = "PROJ4"
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
-            self.cpp_info.system_libs = ["pthread", "dl", "m"]
-        self.env_info.PROJ_LIB.append(os.path.join(self.package_folder, "lib", "proj"))
+            self.cpp_info.system_libs.append("m")
+            if self.options.threadsafe:
+                self.cpp_info.system_libs.append("pthread")
+        if self.options.shared and self.settings.compiler == "Visual Studio":
+            self.cpp_info.defines.append("PROJ_MSVC_DLL_IMPORT")
+        self.env_info.PROJ_LIB.append(os.path.join(self.package_folder, "res"))
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
