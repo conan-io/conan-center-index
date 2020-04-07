3d07ecedad618603dec679dcc32b6fc556c7fdbb
ericLemanissier <ericLemanissier@users.noreply.github.com>
2020-04-07 15:34:48 +0200

    Apply suggestions from code review
    Co-Authored-By: Uilian Ries <uilianries@gmail.com>

--- recipes/tl/all/conanfile.py
+++ recipes/tl/all/conanfile.py
@@ -1,5 +1,5 @@
 from conans import ConanFile, CMake, tools
-from fnmatch import fnmatch
+from conans.errors import ConanInvalidConfiguration
 import os
 
 
@@ -12,7 +12,30 @@ class TlConan(ConanFile):
     license = "CC0-1.0"
     no_copy_source = True
     _source_subfolder = "tl"
+    def configure(self):
+        minimal_cpp_standard = "14"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
 
+        minimal_version = {
+            "gcc": "6.4",
+            "clang": "5",
+            "apple-clang": "10",
+            "Visual Studio": "15"
+        }
+
+        compiler = str(self.settings.compiler)
+        if compiler not in minimal_version:
+            self.output.warn(
+                "%s recipe lacks information about the %s compiler standard version support" % (self.name, compiler))
+            self.output.warn(
+                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+            return
+
+        version = tools.Version(self.settings.compiler.version)
+        if version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration(
+                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("tl-%s" % self.version, self._source_subfolder)
