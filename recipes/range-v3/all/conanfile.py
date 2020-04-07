cfb79d1e4802ea583dbca95c0ac927c4eb757366
Chris Mc <prince.chrismc@gmail.com>
2020-04-07 08:28:13 -0400

    Update recipes/range-v3/all/conanfile.py
    Co-Authored-By: Uilian Ries <uilianries@gmail.com>

--- recipes/range-v3/all/conanfile.py
+++ recipes/range-v3/all/conanfile.py
@@ -1,7 +1,7 @@
 import os
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
-from conans.tools import Version, check_min_cppstd, cppstd_flag
+from conans.tools import Version, check_min_cppstd
 
 class Rangev3Conan(ConanFile):
     name = "range-v3"
