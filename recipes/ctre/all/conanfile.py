6c8f8e1e1d0f9106d617e8e42853a5628f3044c0
Martin Miralles-Cordal <martin.miralles-cordal@lhsystems.com>
2020-04-09 21:48:02 -0400

    Added recipe for ctre/2.8.2

---
+++ recipes/ctre/all/conanfile.py
@@ -0,0 +1,28 @@
+import os
+from conans import ConanFile, tools
+
+class CtreConan(ConanFile):
+    name = "ctre"
+    url = "https://github.com/hanickadot/compile-time-regular-expressions"
+    homepage = "https://github.com/hanickadot/compile-time-regular-expressions"
+    author = "Hana Dusíková (ctre@hanicka.net)"
+    description = "Compile Time Regular Expression for C++17/20"
+    topics = ("cpp17", "regex", "compile-time-regular-expressions")
+    license = "Apache 2.0 with LLVM Exception"
+    no_copy_source = True
+
+    _source_name = "compile-time-regular-expressions"
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self._source_name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
+
---
+++ recipes/ctre/all/test_package/example.cpp
@@ -0,0 +1,41 @@
+#include <ctre.hpp>
+#include <string_view>
+
+int main() {}
+
+using namespace std::string_view_literals;
+
+static inline constexpr auto pattern1 = ctll::fixed_string{"^[\\x30-\\x39]+?$"};
+static inline constexpr auto pattern2 = ctll::fixed_string{""};
+
+
+static_assert(ctre::re<pattern1>().match("123456789"sv));
+static_assert(ctre::re<pattern2>().match(""sv));
+
+template <auto & ptn> constexpr bool re() {
+#if (__cpp_nontype_template_parameter_class || (__cpp_nontype_template_args >= 201911L))
+	constexpr auto _ptn = ptn;
+#else
+	constexpr auto & _ptn = ptn;
+#endif
+	return ctll::parser<ctre::pcre, _ptn, ctre::pcre_actions>::template correct_with<ctre::pcre_context<>>;
+}
+
+static_assert(re<pattern2>());
+
+static inline constexpr ctll::fixed_string pat = "hello";
+
+template <auto & ptn> constexpr bool re2() {
+#if (__cpp_nontype_template_parameter_class || (__cpp_nontype_template_args >= 201911L))
+	constexpr auto _ptn = ptn;
+#else
+	constexpr auto & _ptn = ptn;
+#endif
+	return ctll::parser<ctre::pcre, _ptn, ctre::pcre_actions>::template correct_with<ctre::pcre_context<>>;
+}
+
+static_assert(re<pat>());
+
+static_assert(ctre::re<pat>().match("hello"sv));
+
+static_assert(ctre::match<pat>("hello"sv));
