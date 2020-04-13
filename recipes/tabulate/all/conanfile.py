a07230d0c98e5afa32b35077203038aca3d86504
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-13 17:27:36 -0300

    adds recipe for Tabulate project

---
+++ recipes/tabulate/all/conanfile.py
@@ -0,0 +1,27 @@
+import os
+from conans import ConanFile, tools
+
+class Tabulate(ConanFile):
+    name = "tabulate"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/p-ranav/tabulate"
+    description = "Table Maker for Modern C++"
+    topics = ("conan", "cpp17", "any", "any-implementations")
+    license = "MIT License"
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
