50ba6879339f13c8a15ded4bc78b1bf38bc6022c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-04-15 21:22:54 +0200

    nlopt: don't force PIC

--- recipes/nlopt/all/conanfile.py
+++ recipes/nlopt/all/conanfile.py
@@ -50,8 +50,16 @@ class NloptConan(ConanFile):
         os.rename(self.name + "-" + self.version, self._source_subfolder)
 
     def build(self):
+        self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
+
+    def _patch_sources(self):
+        # don't force PIC
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+                                          "set (CMAKE_C_FLAGS \"-fPIC ${CMAKE_C_FLAGS}\")", "")
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+                                          "set (CMAKE_CXX_FLAGS \"-fPIC ${CMAKE_CXX_FLAGS}\")", "")
 
     def _configure_cmake(self):
         if self._cmake:
