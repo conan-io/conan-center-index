90bff2e83bf5ef6d9f410779b4fa1efa64e1f633
Chris Mc <prince.chrismc@gmail.com>
2020-04-16 19:29:34 -0400

    Update OpenSSL version for websocketpp

--- recipes/websocketpp/all/conanfile.py
+++ recipes/websocketpp/all/conanfile.py
@@ -5,27 +5,27 @@ from conans import ConanFile, tools, CMake
 class WebsocketPPConan(ConanFile):
     name = "websocketpp"
     description = "Header only C++ library that implements RFC6455 The WebSocket Protocol"
-    topics = ("conan", "websocketpp", "websocket", "network", "web", "rfc6455")
+    topics = ("conan", "websocketpp", "websocket", "network", "web", "rfc6455", "header-only")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/zaphoyd/websocketpp"
     license = "BSD-3-Clause"
     settings = "os", "arch", "compiler", "build_type"
-    exports_sources = ["CMakeLists.txt", 'patches/*']
+    exports_sources = ["CMakeLists.txt", "patches/*"]
     generators = ["cmake"]
-    options = {'asio': ['boost', 'standalone']}
-    default_options = {'asio': 'boost'}
-
+    options = {"asio": ["boost", "standalone"]}
+    default_options = {"asio": "standalone"}
+    
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
     def requirements(self):
-        self.requires.add('openssl/1.1.1e')
-        self.requires.add('zlib/1.2.11')
-        if self.options.asio == 'standalone':
-            self.requires.add('asio/1.14.0')
+        self.requires.add("openssl/1.1.1f")
+        self.requires.add("zlib/1.2.11")
+        if self.options.asio == "standalone":
+            self.requires.add("asio/1.16.0")
         else:
-            self.requires.add('boost/1.72.0')
+            self.requires.add("boost/1.72.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -41,11 +41,11 @@ class WebsocketPPConan(ConanFile):
         self._patch_sources()
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
         # We have to copy the headers manually, since the current cmake.install() step in the 0.8.1 release doesn't do so.
-        self.copy(pattern="*.hpp", dst="include/websocketpp", src=self._source_subfolder + '/websocketpp')
+        self.copy(pattern="*.hpp", dst="include/websocketpp", src=self._source_subfolder + "/websocketpp")
 
-    def package_info(self):    
-        if self.options.asio == 'standalone':
-            self.cpp_info.defines.extend(['ASIO_STANDALONE', '_WEBSOCKETPP_CPP11_STL_'])
+    def package_info(self):
+        if self.options.asio == "standalone":
+            self.cpp_info.defines.extend(["ASIO_STANDALONE", "_WEBSOCKETPP_CPP11_STL_"])
 
     def package_id(self):
         self.info.header_only()
