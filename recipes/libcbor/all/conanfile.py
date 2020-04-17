8bbb0f994a20e5e91d42d9dc5a7c7e9e45fd02e7
Carlos Gomes Martinho <carlos.gomes_martinho@siemens.com>
2020-04-17 12:10:13 +0200

    chore: fix license path

--- recipes/libcbor/all/conanfile.py
+++ recipes/libcbor/all/conanfile.py
@@ -62,10 +62,11 @@ class LibCborStackConan(ConanFile):
 
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy("LICENSE.md", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
