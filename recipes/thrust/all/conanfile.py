c2bb35db82d3f87588868922c5ffc97ba2ae469e
vvilpas <vvilpas@gmail.com>
2020-04-20 09:58:34 +0200

    Set thrust_device_system to cpp in develop. Copy everything from thrust as we need all subdirs and other files that are not .h. Append device_system define

--- recipes/thrust/all/conanfile.py
+++ recipes/thrust/all/conanfile.py
@@ -10,13 +10,17 @@ class ThrustConan(ConanFile):
     homepage = "https://thrust.github.io/"
     url = "https://github.com/conan-io/conan-center-index"
     no_copy_source = True
-    options = {"thrust_device_system": ["cuda", "cpp", "omp", "tbb"]
+    options = {"thrust_device_system": ["cuda", "cpp", "omp", "tbb"]}
     default_options = {"thrust_device_system": "cuda"}
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
-    
+
+    def configure(self):
+        if self.develop:
+            self.options.thrust_device_system = "cpp"
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
@@ -24,7 +28,12 @@ class ThrustConan(ConanFile):
 
     def package(self):
         self.copy(pattern="LICENSE", src=self._source_subfolder, dst="licenses")
-        self.copy("*.h", src=os.path.join(self._source_subfolder, "thrust"), dst=os.path.join("include", "thrust"))
+        self.copy("*", src=os.path.join(self._source_subfolder, "thrust"),
+                  dst=os.path.join("include", "thrust"))
 
     def package_id(self):
         self.info.header_only()
+
+    def package_info(self):
+        self.cpp_info.defines = ["THRUST_DEVICE_SYSTEM=THRUST_DEVICE_SYSTEM_{}".format(
+            str(self.options.thrust_device_system).upper())]
