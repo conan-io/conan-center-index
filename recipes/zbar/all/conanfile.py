0c3e009c77395503b2114fc113d63e0a8f6ac7e4
Eric Riff <57375845+ericriff@users.noreply.github.com>
2020-04-20 16:18:18 -0300

    Update recipes/zbar/all/conanfile.py
    Co-Authored-By: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/zbar/all/conanfile.py
+++ recipes/zbar/all/conanfile.py
@@ -97,4 +97,5 @@ class zbarConan(ConanFile):
             self.cpp_info.system_libs = ["pthread"]
         if tools.is_apple_os(self.settings.os):
             self.cpp_info.system_libs = ["iconv"]
-            self.cpp_info.system_libs = ["pthread"]
+            if self.options.enable_pthread:
+                self.cpp_info.system_libs.append("pthread")
