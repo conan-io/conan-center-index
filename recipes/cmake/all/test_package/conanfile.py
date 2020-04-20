f7aa085f4b7beb5c730e76cfa525dc0b186bc928
danimtb <danimanzaneque@gmail.com>
2020-04-20 16:55:13 +0200

    fix parsing output and assert

--- recipes/cmake/all/test_package/conanfile.py
+++ recipes/cmake/all/test_package/conanfile.py
@@ -10,10 +10,9 @@ class TestPackageConan(ConanFile):
         if not tools.cross_building(self.settings):
             output = StringIO()
             self.run("cmake --version", output=output, run_environment=True)
-            self.output.info("Installed: %s" % str(output.getvalue()))
-            ver = str(self.deps_cpp_info["cmake"].version)
-            value = str(output.getvalue())
-            cmake_version = value.split('\n')[0]
-            self.output.info("Expected value: {}".format(ver))
-            self.output.info("Detected value: {}".format(cmake_version))
-            assert(ver in cmake_version)
+            output_str = str(output.getvalue())
+            self.output.info("Installed version: {}".format(output_str))
+            require_version = str(self.deps_cpp_info["cmake"].version)
+            self.output.info("Expected version: {}".format(require_version))
+            assert_cmake_version = "cmake version %s" % require_version
+            assert(assert_cmake_version in output_str)
