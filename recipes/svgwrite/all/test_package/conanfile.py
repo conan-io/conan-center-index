ad4e2805f433db5ee7b07121d64ddf85e82b402b
Uilian Ries <uilianries@gmail.com>
2020-04-22 11:41:30 -0300

    Prepare svgwrite for Conan Center Index
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/svgwrite/all/test_package/conanfile.py
+++ recipes/svgwrite/all/test_package/conanfile.py
@@ -9,12 +9,10 @@ class SvgwriteTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
-            os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+            bin_path = os.path.join("bin", "example")
+            self.run(bin_path, run_environment=True)
