9db3bc566f2af8e028053b8af9e6f09420fd52f1
Harald <harald.achitz@gmail.com>
2020-04-23 09:56:28 +0200

    get patches the safe way
    Co-Authored-By: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/miniz/all/conanfile.py
+++ recipes/miniz/all/conanfile.py
@@ -46,7 +46,7 @@ class MinizConan(ConanFile):
         return self._cmake
 
     def build(self):
-        for patch in self.conan_data["patches"][self.version]:
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
