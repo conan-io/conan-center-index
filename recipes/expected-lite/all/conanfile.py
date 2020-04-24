fd5a04e68f79c1a04b525c30f3e55b321f5172bc
Chris Mc <prince.chrismc@gmail.com>
2020-04-24 10:50:12 -0400

    adding patch for an upstream bug
    https://github.com/prince-chrismc/expected-lite/commit/20a6edb34579fec6e588dd76ddf681cdc0858330

--- recipes/expected-lite/all/conanfile.py
+++ recipes/expected-lite/all/conanfile.py
@@ -4,19 +4,24 @@ from conans import ConanFile, tools
 
 class ExpectedLite(ConanFile):
     name = "expected-lite"
+    license = "BSL-1.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/martinmoene/expected-lite"
     description = "expected lite - Expected objects in C++11 and later in a single-file header-only library"
     topics = ("conan", "cpp11", "cpp14", "cpp17", "expected", "expected-implementations")
-    license = "BSL-1.0"
+    exports_sources = ["patches/**"]
     no_copy_source = True
-    
-    _source_subfolder = "source_subfolder"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
 
     def package(self):
         self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
