baef587a2558d902aa2d0cc22472f06bfe27e33d
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-04-24 22:16:42 +0200

    pkgconf: fix MSVC static library + test pkgconf library

--- recipes/pkgconf/all/test_package/conanfile.py
+++ recipes/pkgconf/all/test_package/conanfile.py
@@ -1,13 +1,21 @@
-from conans import ConanFile, tools
+from conans import CMake, ConanFile, tools
 from conans.errors import ConanException
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
+            self.run(os.path.join("bin", "test_package"), run_environment=True)
+
             if not os.environ["PKG_CONFIG"].startswith(self.deps_cpp_info["pkgconf"].rootpath):
                 raise ConanException("PKG_CONFIG variable incorrect")
 
