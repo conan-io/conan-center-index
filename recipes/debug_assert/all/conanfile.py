08947015076ce6ede1126e77994cca058033b50c
Manu343726 <Manu343726@gmail.com>
2020-04-27 01:29:07 +0200

    Add debug_assert/1.3.3

---
+++ recipes/debug_assert/all/conanfile.py
@@ -0,0 +1,37 @@
+from conans import ConanFile, tools
+from conans.tools import Version
+from conans.errors import ConanInvalidConfiguration
+import os
+
+class DebugAssert(ConanFile):
+    name = 'debug_assert'
+    description = 'Simple, flexible and modular assertion macro'
+    url = 'https://github.com/conan-io/conan-center-index'
+    homepage = 'http://foonathan.net/blog/2016/09/16/assertions.html'
+    license = 'zlib'
+    topics = 'conan', 'assert', 'debugging', 'utilities'
+
+    settings = 'compiler'
+
+    no_copy_source = True
+    _source_subfolder = 'source_subfolder'
+
+    @property
+    def repo_folder(self):
+        return os.path.join(self.source_folder, self._source_subfolder)
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name +  "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, '11')
+
+    def package(self):
+        self.copy("*LICENSE", dst="licenses", keep_path=False)
+        self.copy("debug_assert.hpp", src=self.repo_folder, dst='include/')
+
+    def package_id(self):
+        self.info.header_only()
