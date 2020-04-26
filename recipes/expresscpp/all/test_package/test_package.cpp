824920e9bd12f48e7d6d019448568eae7164b2af
Carlos Gomes Martinho <carlos.gomes_martinho@siemens.com>
2020-04-26 18:48:34 +0200

    chore: make sure we exit after listening

--- recipes/expresscpp/all/test_package/test_package.cpp
+++ recipes/expresscpp/all/test_package/test_package.cpp
@@ -1,9 +1,15 @@
 #include "expresscpp/expresscpp.hpp"
 
-int main() {
+int main()
+{
   auto expresscpp = std::make_shared<expresscpp::ExpressCpp>();
+
   expresscpp->Get("/", [](auto /*req*/, auto res) { res->Send("hello world!"); });
+
   constexpr uint16_t port = 3000u;
-  expresscpp->Listen(port,[=](auto /*ec*/) { std::cout << "Listening on port " << port << std::endl; }).Run();
+  expresscpp->Listen(port, [=](auto /*ec*/) {
+    std::cout << "Listening on port " << port << std::endl;
+    std::cout << "And exiting..." << std::endl;
+  });
   return 0;
 }
