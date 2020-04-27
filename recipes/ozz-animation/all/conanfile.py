bd6720cd9dd8a2ba6416e45219fbddc4172352b2
intelligide <intelligide@hotmail.fr>
2020-04-27 17:00:00 -0400

    Add ozz-animation 0.12.1

---
+++ recipes/ozz-animation/all/conanfile.py
@@ -0,0 +1,61 @@
+import os
+from conans import ConanFile, CMake, tools
+
+class OzzAnimationConan(ConanFile):
+    name = "ozz-animation"
+    description = "Open source c++ skeletal animation library and toolset."
+    license = "MIT"
+    topics = ("conan", "ozz", "animation", "skeletal")
+    homepage = "https://github.com/guillaumeblanc/ozz-animation"
+    url = "https://github.com/conan-io/conan-center-index"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    short_paths = True
+
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["ozz_build_fbx"] = False
+        cmake.definitions["ozz_build_data"] = False
+        cmake.definitions["ozz_build_samples"] = False
+        cmake.definitions["ozz_build_howtos"] = False
+        cmake.definitions["ozz_build_tests"] = False
+        cmake.definitions["ozz_build_cpp11"] = True
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+        os.remove(os.path.join(self.package_folder, "CHANGES.md"))
+        os.remove(os.path.join(self.package_folder, "LICENSE.md"))
+        os.remove(os.path.join(self.package_folder, "README.md"))
+        self.copy(pattern="LICENSE.md", dst="licenses", src=self._source_subfolder)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
