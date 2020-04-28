261302598db4a6d4a59c13c38f91432b1c08697b
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-04-28 14:21:39 +0200

    pybind: add 2.5.0 + cache cmake

--- recipes/pybind11/all/test_package/conanfile.py
+++ recipes/pybind11/all/test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 import sys
 
@@ -9,10 +9,17 @@ class TestPackageConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["Python_ADDITIONAL_VERSIONS"] = "{}.{}".format(sys.version_info.major,sys.version_info.minor)
         cmake.configure()
         cmake.build()
 
+    @property
+    def _python_available(self):
+        # FIXME: this can be removed once a python interpreter is available in CCI
+        return getattr(sys, "frozen", False)
+
     def test(self):
-        self.run("{} {} {}".format(sys.executable,
-                                   os.path.join(self.source_folder, "test.py"),
-                                   os.path.join(self.build_folder, "lib")), run_environment=True)
+        if self._python_available and not tools.cross_building(self.settings):
+            self.run("{} {} {}".format(sys.executable,
+                                       os.path.join(self.source_folder, "test.py"),
+                                       os.path.join(self.build_folder, "lib")), run_environment=True)
