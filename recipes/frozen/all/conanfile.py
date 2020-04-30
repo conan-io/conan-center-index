d392e3ab91cc1b6342776870ccaf370b8ec276fb
Yoann POTINET <intelligide@hotmail.fr>
2020-04-30 04:58:21 -0400

    Add frozen 1.0.0 (#1478)
    * Add frozen 1.0.0
    
    * Fix frozen 1.0.0
    
    * Correct frozen 1.0.0
    
    * Fix frozen 1.0.0

---
+++ recipes/frozen/all/conanfile.py
@@ -0,0 +1,47 @@
+import os
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+
+class FrozenConan(ConanFile):
+    name = "frozen"
+    description = "A header-only, constexpr alternative to gperf for C++14 users."
+    homepage = "https://github.com/serge-sans-paille/frozen"
+    url = "https://github.com/conan-io/conan-center-index"
+    license = "Apache-2.0"
+    topics = ("gperf", "constexpr", "header-only")
+    no_copy_source = True
+    settings = "compiler"
+    _source_subfolder = "source_subfolder"
+
+    def configure(self):
+        minimal_cpp_standard = "14"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
+        minimal_version = {
+            "gcc": "5",
+            "clang": "3.4",
+            "apple-clang": "10",
+            "Visual Studio": "14"
+        }
+        compiler = str(self.settings.compiler)
+        if compiler not in minimal_version:
+            self.output.warn(
+                "%s recipe lacks information about the %s compiler standard version support" % (self.name, compiler))
+            self.output.warn(
+                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+            return
+        version = tools.Version(self.settings.compiler.version)
+        if version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy(pattern="*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
