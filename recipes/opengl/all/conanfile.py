ba5b3dda7d2a0df076ccfdd818cfb01eb67f7e45
SSE4 <tomskside@gmail.com>
2020-05-04 21:18:48 +0700

    - virtual package for OpenGL
    Signed-off-by: SSE4 <tomskside@gmail.com>

---
+++ recipes/opengl/all/conanfile.py
@@ -0,0 +1,69 @@
+from conans import ConanFile, tools
+from conans.errors import ConanException
+import os
+
+
+class SysConfigOpenGLConan(ConanFile):
+    name = "opengl"
+    version = "system"
+    description = "cross-platform virtual conan package for the OpenGL support"
+    topics = ("conan", "opengl", "gl")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://www.opengl.org/"
+    license = "MIT"
+    settings = ("os",)
+
+    def package_id(self):
+        self.info.header_only()
+
+    def _fill_cppinfo_from_pkgconfig(self, name):
+        pkg_config = tools.PkgConfig(name)
+        if not pkg_config.provides:
+            raise ConanException("OpenGL development files aren't available, give up")
+        libs = [lib[2:] for lib in pkg_config.libs_only_l]
+        lib_dirs = [lib[2:] for lib in pkg_config.libs_only_L]
+        ldflags = [flag for flag in pkg_config.libs_only_other]
+        include_dirs = [include[2:] for include in pkg_config.cflags_only_I]
+        cflags = [flag for flag in pkg_config.cflags_only_other if not flag.startswith("-D")]
+        defines = [flag[2:] for flag in pkg_config.cflags_only_other if flag.startswith("-D")]
+
+        self.cpp_info.system_libs.extend(libs)
+        self.cpp_info.libdirs.extend(lib_dirs)
+        self.cpp_info.sharedlinkflags.extend(ldflags)
+        self.cpp_info.exelinkflags.extend(ldflags)
+        self.cpp_info.defines.extend(defines)
+        self.cpp_info.includedirs.extend(include_dirs)
+        self.cpp_info.cflags.extend(cflags)
+        self.cpp_info.cxxflags.extend(cflags)
+
+    def system_requirements(self):
+        if tools.os_info.is_linux and self.settings.os == "Linux":
+            package_tool = tools.SystemPackageTool(conanfile=self, default_mode='verify')
+            if tools.os_info.with_yum:
+                if tools.os_info.linux_distro == "fedora" and tools.os_info.os_version >= "32":
+                    packages = ["libglvnd-devel"]
+                else:
+                    packages = ["mesa-libGL-devel"]
+            elif tools.os_info.with_apt:
+                ubuntu_20_or_later = tools.os_info.linux_distro == "ubuntu" and tools.os_info.os_version >= "20"
+                debian_11_or_later = tools.os_info.linux_distro == "debian" and tools.os_info.os_version >= "11"
+                if ubuntu_20_or_later or debian_11_or_later:
+                    packages = ["libgl-dev"]
+                else:
+                    packages = ["libgl1-mesa-dev"]
+            elif tools.os_info.with_pacman:
+                packages = ["libglvnd"]
+            elif tools.os_info.with_zypper:
+                packages = ["Mesa-libGL-devel"]
+            else:
+                self.warn("don't know how to install OpenGL for your distro")
+            package_tool.install(update=True, packages=packages)
+
+    def package_info(self):
+        if self.settings.os == "Macos":
+            self.cpp_info.defines.append("GL_SILENCE_DEPRECATION=1")
+            self.cpp_info.frameworks.append("OpenGL")
+        elif self.settings.os == "Windows":
+            self.cpp_info.system_libs = ["OpenGL32"]
+        elif self.settings.os == "Linux":
+            self._fill_cppinfo_from_pkgconfig('gl')
