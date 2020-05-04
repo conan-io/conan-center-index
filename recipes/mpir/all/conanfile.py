9c243652f1e24807ad3d14e2e3a098b12746fda1
SSE4 <tomskside@gmail.com>
2020-05-04 22:19:48 +0700

    Update recipes/mpir/all/conanfile.py
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>

--- recipes/mpir/all/conanfile.py
+++ recipes/mpir/all/conanfile.py
@@ -37,9 +37,7 @@ class MpirConan(ConanFile):
                                                    "{}_mpir_gc.vcxproj".format(self._dll_or_lib))
 
     def source(self):
-        args = self.conan_data["sources"][self.version]
-        args["keep_permissions"] = True
-        tools.get(**args)
+        tools.get(keep_permissions=True, **self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
