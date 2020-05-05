d070c6eec9abe7240a48756e17ac14102bb340ee
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-05-05 20:37:25 +0200

    google-guetzli: use autotools to run make

--- recipes/google-guetzli/all/test_package/conanfile.py
+++ recipes/google-guetzli/all/test_package/conanfile.py
@@ -5,8 +5,7 @@ from conans import ConanFile, tools
 
 class GoogleguetzliTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-   
-    
+
     def test(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         bees_path  = os.path.join(dir_path, "bees.png")
@@ -16,9 +15,3 @@ class GoogleguetzliTestConan(ConanFile):
             if self.settings.os == "Windows":
                 app += ".exe"
             self.run("{} --quality 84 {} bees.jpg".format(app,bees_path), run_environment=True)
-            
-
-   
-    
-
-    
