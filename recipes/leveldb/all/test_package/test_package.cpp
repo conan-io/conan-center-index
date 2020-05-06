1dea380f5d268ab2148cd181317f7eccfcccccef
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-05-06 14:05:12 +0200

    Update recipes/leveldb/all/test_package/test_package.cpp
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/leveldb/all/test_package/test_package.cpp
+++ recipes/leveldb/all/test_package/test_package.cpp
@@ -6,7 +6,7 @@ int main()
   leveldb::DB *db;
   leveldb::Options options;
   options.create_if_missing = true;
-  leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+  leveldb::Status status = leveldb::DB::Open(options, "testdb", &db);
 
   if (status.ok())
   {
