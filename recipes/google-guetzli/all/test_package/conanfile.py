b01f6c78b114f22e78d8b5ba1431a2259682129b
davidtazy <david.allemant@gmail.com>
2020-05-06 18:10:39 +0200

    cleaner test package recipe

--- recipes/google-guetzli/all/test_package/conanfile.py
+++ recipes/google-guetzli/all/test_package/conanfile.py
@@ -5,20 +5,12 @@ from conans import ConanFile, tools
 
 class GoogleguetzliTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-   
-    
+
     def test(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        bees_path  = os.path.join(dir_path, "bees.png")
-        print("{}".format(dir_path))
+        bees_path = os.path.join(self.source_folder, "bees.png")
         if not tools.cross_building(self.settings):
             app = "guetzli"
             if self.settings.os == "Windows":
                 app += ".exe"
-            self.run("{} --quality 84 {} bees.jpg".format(app,bees_path), run_environment=True)
-            
-
-   
-    
-
-    
+            self.run("{} --quality 84 {} bees.jpg".format(app, bees_path),
+                     run_environment=True)
