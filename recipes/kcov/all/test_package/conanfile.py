783792fefe12eb3536b6fbf1a0712ae974a5ddbf
davidtazy <david.allemant@gmail.com>
2020-05-06 17:54:06 +0200

    style guide alignement

--- recipes/kcov/all/test_package/conanfile.py
+++ recipes/kcov/all/test_package/conanfile.py
@@ -5,16 +5,19 @@ from conans import ConanFile, CMake, tools
 
 class KcovTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = ["cmake","virtualenv"]
+    generators = ["cmake"]
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["CMAKE_BUILD_TYPE"]="Debug"
+        cmake.definitions["CMAKE_BUILD_TYPE"] = "Debug"
         cmake.configure()
         cmake.build()
-    
 
     def test(self):
         if not tools.cross_building(self.settings):
-            #os.chdir("bin")
-            self.run("kcov --exclude-pattern=/usr/ --include-path=${CMAKE_CURRENT_LIST_DIR} cov ./example")
+            bindir = os.path.join("bin", "test_package")
+            args = ["--exclude-pattern=/usr/",
+                    "--include-path={}".format(self.source_folder),
+                    "cov",
+                    bindir]
+            self.run("kcov {}".format(" ".join(args)), run_environment=True)
