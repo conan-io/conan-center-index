074c0cfbfc61c17ef1fdef998fe9fce218bc0585
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-05-08 00:01:34 +0200

    itk: patch sources to use conan modules + include modules at start

--- recipes/itk/all/conanfile.py
+++ recipes/itk/all/conanfile.py
@@ -3,29 +3,35 @@ import os
 from conans import ConanFile, CMake, tools
 
 
-class LibITKConan(ConanFile):
+class ITKConan(ConanFile):
     name = "itk"
-    topics = "Image processing"
+    topics = ("itk", "scientific", "image", "processing")
     homepage = "http://www.itk.org/"
     url = "https://github.com/conan-io/conan-center-index"
     license = "Apache-2.0"
     description = "Insight Segmentation and Registration Toolkit"
-    exports_sources = ["CMakeLists.txt"]
+    exports_sources = "CMakeLists.txt", "patches/**"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "use_jpegturbo": [True, False]}
+    }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "use_jpegturbo": False}
+    }
     generators = "cmake", "cmake_find_package"
     short_paths = True
-    _source_subfolder = "sf"
-    _build_subfolder = "build"
-    _upstream_version = ""
-    _upstream_patch = ""
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     # TODO: Some packages can be added as optional, but they are not in CCI:
     # - mkl
@@ -38,161 +44,169 @@ class LibITKConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
-        version_token = self.version.split(".")
-        self._upstream_version = "{0}.{1}".format(
-                                        version_token[0],
-                                        version_token[1])
-        self._upstream_patch = version_token[2]
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
     def requirements(self):
+        self.requires("libjpeg/9d")
 
-        if(self.options["use_jpegturbo"]):
-            self.requires("libjpeg-turbo/2.0.2")
-        else:
-            self.requires("libjpeg/9d")
-
+        self.requires("dcmtk/3.6.5")
+        self.requires("double-conversion/3.1.5")
+        self.requires("eigen/3.3.7")
         self.requires("expat/2.2.9")
-        self.requires("libtiff/4.0.9")
-        self.requires("libpng/1.6.37")
-        self.requires("zlib/1.2.11")
+        self.requires("fftw/3.3.8")
+        self.requires("hdf5/1.12.0")
         self.requires("icu/64.2")
+        self.requires("libtiff/4.1.0")
+        self.requires("libpng/1.6.37")
+        self.requires("openjpeg/2.3.1")
+        self.requires("zlib/1.2.11")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename("ITK-{0}.{1}".format(
-            self._upstream_version,
-            self._upstream_patch),
-            self._source_subfolder)
+        os.rename("ITK-{}".format(self.version), self._source_subfolder)
+
+    def _patch_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_EXAMPLES"] = "OFF"
-        cmake.definitions["BUILD_TESTING"] = "OFF"
-        cmake.definitions["BUILD_DOCUMENTATION"] = "OFF"
-        cmake.definitions["ITK_USE_REVIEW"] = "ON"
-        cmake.definitions["ITK_SKIP_PATH_LENGTH_CHECKS"] = "ON"
+        if self._cmake:
+            return self._cmake
 
-        cmake.definitions["ITK_USE_SYSTEM_DCMTK"] = "OFF"
-        cmake.definitions["ITK_USE_SYSTEM_GDCM"] = "OFF"
-        cmake.definitions["ITK_USE_SYSTEM_PNG"] = "ON"
-        cmake.definitions["ITK_USE_SYSTEM_TIFF"] = "ON"
-        cmake.definitions["ITK_USE_SYSTEM_ZLIB"] = "ON"
-        cmake.definitions["ITK_USE_SYSTEM_JPEG"] = "ON"
-        cmake.definitions["ITK_USE_SYSTEM_EXPAT"] = "ON"
-        cmake.definitions["ITK_USE_SYSTEM_ICU"] = "ON"
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_EXAMPLES"] = False
+        self._cmake.definitions["BUILD_TESTING"] = False
+        self._cmake.definitions["BUILD_DOCUMENTATION"] = False
+        self._cmake.definitions["ITK_SKIP_PATH_LENGTH_CHECKS"] = True
 
-        cmake.definitions["ITK_BUILD_DEFAULT_MODULES"] = "OFF"
-        cmake.definitions["Module_ITKDeprecated"] = "OFF"
-        cmake.definitions["Module_ITKMINC"] = "OFF"
-        cmake.definitions["Module_ITKIOMINC"] = "OFF"
-        cmake.definitions["Module_ITKV3Compatibility"] = "OFF"
+        self._cmake.definitions["ITK_USE_SYSTEM_LIBRARIES"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_DCMTK"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_DOUBLECONVERSION"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_EIGEN"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_FFTW"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_HDF5"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_ICU"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_JPEG"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_PNG"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_TIFF"] = True
+        self._cmake.definitions["ITK_USE_SYSTEM_ZLIB"] = True
 
-        cmake.definitions["Module_ITKVideoBridgeOpenCV"] = "OFF"
+        self._cmake.definitions["ITK_USE_SYSTEM_KWIML"] = False  # FIXME: missing from CCI
+        self._cmake.definitions["ITK_USE_SYSTEM_VXL"] = False  # FIXME: missing from CCI
+        self._cmake.definitions["ITK_USE_SYSTEM_GDCM"] = False  # FIXME: missing from CCI
+        self._cmake.definitions["GDCM_USE_SYSTEM_OPENJPEG"] = True
 
-        cmake.definitions["Module_ITKDCMTK"] = "ON"
-        cmake.definitions["Module_ITKIODCMTK"] = "ON"
-        cmake.definitions["Module_ITKIOHDF5"] = "ON"
-        cmake.definitions["Module_ITKIOTransformHDF5"] = "OFF"
-        cmake.definitions["Module_ITKAnisotropicSmoothing"] = "ON"
-        cmake.definitions["Module_ITKAntiAlias"] = "ON"
-        cmake.definitions["Module_ITKBiasCorrection"] = "ON"
-        cmake.definitions["Module_ITKBinaryMathematicalMorphology"] = "ON"
-        cmake.definitions["Module_ITKBioCell"] = "ON"
-        cmake.definitions["Module_ITKClassifiers"] = "ON"
-        cmake.definitions["Module_ITKColormap"] = "ON"
-        cmake.definitions["Module_ITKConnectedComponents"] = "ON"
-        cmake.definitions["Module_ITKConvolution"] = "ON"
-        cmake.definitions["Module_ITKCurvatureFlow"] = "ON"
-        cmake.definitions["Module_ITKDeconvolution"] = "ON"
-        cmake.definitions["Module_ITKDeformableMesh"] = "ON"
-        cmake.definitions["Module_ITKDenoising"] = "ON"
-        cmake.definitions["Module_ITKDiffusionTensorImage"] = "ON"
-        cmake.definitions["Module_ITKDisplacementField"] = "ON"
-        cmake.definitions["Module_ITKDistanceMap"] = "ON"
-        cmake.definitions["Module_ITKEigen"] = "ON"
-        cmake.definitions["Module_ITKFEM"] = "ON"
-        cmake.definitions["Module_ITKFEMRegistration"] = "ON"
-        cmake.definitions["Module_ITKFFT"] = "ON"
-        cmake.definitions["Module_ITKFastMarching"] = "ON"
-        cmake.definitions["Module_ITKGIFTI"] = "ON"
-        cmake.definitions["Module_ITKGPUAnisotropicSmoothing"] = "ON"
-        cmake.definitions["Module_ITKGPUImageFilterBase"] = "ON"
-        cmake.definitions["Module_ITKGPUPDEDeformableRegistration"] = "ON"
-        cmake.definitions["Module_ITKGPURegistrationCommon"] = "ON"
-        cmake.definitions["Module_ITKGPUSmoothing"] = "ON"
-        cmake.definitions["Module_ITKGPUThresholding"] = "ON"
-        cmake.definitions["Module_ITKIOCSV"] = "ON"
-        cmake.definitions["Module_ITKIOGE"] = "ON"
-        cmake.definitions["Module_ITKIOIPL"] = "ON"
-        cmake.definitions["Module_ITKIOMesh"] = "ON"
-        cmake.definitions["Module_ITKIOPhilipsREC"] = "ON"
-        cmake.definitions["Module_ITKIORAW"] = "ON"
-        cmake.definitions["Module_ITKIOSiemens"] = "ON"
-        cmake.definitions["Module_ITKIOSpatialObjects"] = "ON"
-        cmake.definitions["Module_ITKIOTransformBase"] = "ON"
-        cmake.definitions["Module_ITKIOTransformInsightLegacy"] = "ON"
-        cmake.definitions["Module_ITKIOTransformMatlab"] = "ON"
-        cmake.definitions["Module_ITKIOXML"] = "ON"
-        cmake.definitions["Module_ITKImageCompare"] = "ON"
-        cmake.definitions["Module_ITKImageCompose"] = "ON"
-        cmake.definitions["Module_ITKImageFeature"] = "ON"
-        cmake.definitions["Module_ITKImageFusion"] = "ON"
-        cmake.definitions["Module_ITKImageGradient"] = "ON"
-        cmake.definitions["Module_ITKImageGrid"] = "ON"
-        cmake.definitions["Module_ITKImageIntensity"] = "ON"
-        cmake.definitions["Module_ITKImageLabel"] = "ON"
-        cmake.definitions["Module_ITKImageSources"] = "ON"
-        cmake.definitions["Module_ITKImageStatistics"] = "ON"
-        cmake.definitions["Module_ITKIntegratedTest"] = "ON"
-        cmake.definitions["Module_ITKKLMRegionGrowing"] = "ON"
-        cmake.definitions["Module_ITKLabelMap"] = "ON"
-        cmake.definitions["Module_ITKLabelVoting"] = "ON"
-        cmake.definitions["Module_ITKLevelSets"] = "ON"
-        cmake.definitions["Module_ITKLevelSetsv4"] = "ON"
-        cmake.definitions["Module_ITKMarkovRandomFieldsClassifiers"] = "ON"
-        cmake.definitions["Module_ITKMathematicalMorphology"] = "ON"
-        cmake.definitions["Module_ITKMetricsv4"] = "ON"
-        cmake.definitions["Module_ITKNarrowBand"] = "ON"
-        cmake.definitions["Module_ITKNeuralNetworks"] = "ON"
-        cmake.definitions["Module_ITKOptimizers"] = "ON"
-        cmake.definitions["Module_ITKOptimizersv4"] = "ON"
-        cmake.definitions["Module_ITKPDEDeformableRegistration"] = "ON"
-        cmake.definitions["Module_ITKPath"] = "ON"
-        cmake.definitions["Module_ITKPolynomials"] = "ON"
-        cmake.definitions["Module_ITKQuadEdgeMeshFiltering"] = "ON"
-        cmake.definitions["Module_ITKRegionGrowing"] = "ON"
-        cmake.definitions["Module_ITKRegistrationCommon"] = "ON"
-        cmake.definitions["Module_ITKRegistrationMethodsv4"] = "ON"
-        cmake.definitions["Module_ITKReview"] = "ON"
-        cmake.definitions["Module_ITKSignedDistanceFunction"] = "ON"
-        cmake.definitions["Module_ITKSmoothing"] = "ON"
-        cmake.definitions["Module_ITKSpatialFunction"] = "ON"
-        cmake.definitions["Module_ITKThresholding"] = "ON"
-        cmake.definitions["Module_ITKVideoCore"] = "ON"
-        cmake.definitions["Module_ITKVideoFiltering"] = "ON"
-        cmake.definitions["Module_ITKVideoIO"] = "OFF"
-        cmake.definitions["Module_ITKVoronoi"] = "ON"
-        cmake.definitions["Module_ITKWatersheds"] = "ON"
-        cmake.definitions["Module_ITKDICOMParser"] = "ON"
+        self._cmake.definitions["ITK_BUILD_DEFAULT_MODULES"] = False
+        self._cmake.definitions["Module_ITKDeprecated"] = False
+        self._cmake.definitions["Module_ITKMINC"] = False
+        self._cmake.definitions["Module_ITKIOMINC"] = False
+        self._cmake.definitions["Module_ITKV3Compatibility"] = False
 
-        cmake.definitions["Module_ITKVTK"] = "OFF"
-        cmake.definitions["Module_ITKVtkGlue"] = "OFF"
+        self._cmake.definitions["Module_ITKVideoBridgeOpenCV"] = False
 
-        cmake.definitions["USE_FFTWF"] = "OFF"
-        cmake.definitions["USE_FFTWD"] = "OFF"
+        self._cmake.definitions["Module_ITKDCMTK"] = True
+        self._cmake.definitions["Module_ITKIODCMTK"] = True
+        self._cmake.definitions["Module_ITKIOHDF5"] = True
+        self._cmake.definitions["Module_ITKIOTransformHDF5"] = False
+        self._cmake.definitions["Module_ITKAnisotropicSmoothing"] = True
+        self._cmake.definitions["Module_ITKAntiAlias"] = True
+        self._cmake.definitions["Module_ITKBiasCorrection"] = True
+        self._cmake.definitions["Module_ITKBinaryMathematicalMorphology"] = True
+        self._cmake.definitions["Module_ITKBioCell"] = True
+        self._cmake.definitions["Module_ITKClassifiers"] = True
+        self._cmake.definitions["Module_ITKColormap"] = True
+        self._cmake.definitions["Module_ITKConnectedComponents"] = True
+        self._cmake.definitions["Module_ITKConvolution"] = True
+        self._cmake.definitions["Module_ITKCurvatureFlow"] = True
+        self._cmake.definitions["Module_ITKDeconvolution"] = True
+        self._cmake.definitions["Module_ITKDeformableMesh"] = True
+        self._cmake.definitions["Module_ITKDenoising"] = True
+        self._cmake.definitions["Module_ITKDiffusionTensorImage"] = True
+        self._cmake.definitions["Module_ITKDisplacementField"] = True
+        self._cmake.definitions["Module_ITKDistanceMap"] = True
+        self._cmake.definitions["Module_ITKEigen"] = True
+        self._cmake.definitions["Module_ITKFEM"] = True
+        self._cmake.definitions["Module_ITKFEMRegistration"] = True
+        self._cmake.definitions["Module_ITKFFT"] = True
+        self._cmake.definitions["Module_ITKFastMarching"] = True
+        self._cmake.definitions["Module_ITKGIFTI"] = True
+        self._cmake.definitions["Module_ITKGPUAnisotropicSmoothing"] = True
+        self._cmake.definitions["Module_ITKGPUImageFilterBase"] = True
+        self._cmake.definitions["Module_ITKGPUPDEDeformableRegistration"] = True
+        self._cmake.definitions["Module_ITKGPURegistrationCommon"] = True
+        self._cmake.definitions["Module_ITKGPUSmoothing"] = True
+        self._cmake.definitions["Module_ITKGPUThresholding"] = True
+        self._cmake.definitions["Module_ITKIOCSV"] = True
+        self._cmake.definitions["Module_ITKIOGE"] = True
+        self._cmake.definitions["Module_ITKIOIPL"] = True
+        self._cmake.definitions["Module_ITKIOMesh"] = True
+        self._cmake.definitions["Module_ITKIOPhilipsREC"] = True
+        self._cmake.definitions["Module_ITKIORAW"] = True
+        self._cmake.definitions["Module_ITKIOSiemens"] = True
+        self._cmake.definitions["Module_ITKIOSpatialObjects"] = True
+        self._cmake.definitions["Module_ITKIOTransformBase"] = True
+        self._cmake.definitions["Module_ITKIOTransformInsightLegacy"] = True
+        self._cmake.definitions["Module_ITKIOTransformMatlab"] = True
+        self._cmake.definitions["Module_ITKIOXML"] = True
+        self._cmake.definitions["Module_ITKImageCompare"] = True
+        self._cmake.definitions["Module_ITKImageCompose"] = True
+        self._cmake.definitions["Module_ITKImageFeature"] = True
+        self._cmake.definitions["Module_ITKImageFusion"] = True
+        self._cmake.definitions["Module_ITKImageGradient"] = True
+        self._cmake.definitions["Module_ITKImageGrid"] = True
+        self._cmake.definitions["Module_ITKImageIntensity"] = True
+        self._cmake.definitions["Module_ITKImageLabel"] = True
+        self._cmake.definitions["Module_ITKImageSources"] = True
+        self._cmake.definitions["Module_ITKImageStatistics"] = True
+        self._cmake.definitions["Module_ITKIntegratedTest"] = True
+        self._cmake.definitions["Module_ITKKLMRegionGrowing"] = True
+        self._cmake.definitions["Module_ITKLabelMap"] = True
+        self._cmake.definitions["Module_ITKLabelVoting"] = True
+        self._cmake.definitions["Module_ITKLevelSets"] = True
+        self._cmake.definitions["Module_ITKLevelSetsv4"] = True
+        self._cmake.definitions["Module_ITKMarkovRandomFieldsClassifiers"] = True
+        self._cmake.definitions["Module_ITKMathematicalMorphology"] = True
+        self._cmake.definitions["Module_ITKMetricsv4"] = True
+        self._cmake.definitions["Module_ITKNarrowBand"] = True
+        self._cmake.definitions["Module_ITKNeuralNetworks"] = True
+        self._cmake.definitions["Module_ITKOptimizers"] = True
+        self._cmake.definitions["Module_ITKOptimizersv4"] = True
+        self._cmake.definitions["Module_ITKPDEDeformableRegistration"] = True
+        self._cmake.definitions["Module_ITKPath"] = True
+        self._cmake.definitions["Module_ITKPolynomials"] = True
+        self._cmake.definitions["Module_ITKQuadEdgeMeshFiltering"] = True
+        self._cmake.definitions["Module_ITKRegionGrowing"] = True
+        self._cmake.definitions["Module_ITKRegistrationCommon"] = True
+        self._cmake.definitions["Module_ITKRegistrationMethodsv4"] = True
+        self._cmake.definitions["Module_ITKReview"] = True
+        self._cmake.definitions["Module_ITKSignedDistanceFunction"] = True
+        self._cmake.definitions["Module_ITKSmoothing"] = True
+        self._cmake.definitions["Module_ITKSpatialFunction"] = True
+        self._cmake.definitions["Module_ITKThresholding"] = True
+        self._cmake.definitions["Module_ITKVideoCore"] = True
+        self._cmake.definitions["Module_ITKVideoFiltering"] = True
+        self._cmake.definitions["Module_ITKVideoIO"] = False
+        self._cmake.definitions["Module_ITKVoronoi"] = True
+        self._cmake.definitions["Module_ITKWatersheds"] = True
+        self._cmake.definitions["Module_ITKDICOMParser"] = True
+
+        self._cmake.definitions["Module_ITKVTK"] = False
+        self._cmake.definitions["Module_ITKVtkGlue"] = False
 
         # Disabled on Linux (link errors)
-        cmake.definitions["Module_ITKLevelSetsv4Visualization"] = "OFF"
+        self._cmake.definitions["Module_ITKLevelSetsv4Visualization"] = False
 
-        # Disabled because Vxl vidl is not build anymore
-        cmake.definitions["Module_ITKVideoBridgeVXL"] = "OFF"
+        # Disabled because Vxl vidl is not built anymore
+        self._cmake.definitions["Module_ITKVideoBridgeVXL"] = False
 
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
 
     def build(self):
+        self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -200,29 +214,18 @@ class LibITKConan(ConanFile):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "gdcmopenjpeg-2.1"))
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "etc"))
         tools.rmdir(os.path.join(self.package_folder, "share"))
-        tools.rmdir(os.path.join(self.package_folder, "cmake"))
 
     def package_info(self):
-
-        # in linux we need to link also with these libs
+        self.cpp_info.libs = tools.collect_libs(self)  # FIXME: correct order?
+        v = tools.Version(self.version)
+        self.cpp_info.includedirs.append(os.path.join("include", "ITK-{}.{}".format(v.major, v.minor)))
         if self.settings.os == "Linux":
-            self.cpp_info.libs.extend(["pthread", "dl", "rt"])
+            self.cpp_info.system_libs.extend(["pthread", "dl", "rt"])
 
-        self.cpp_info.builddirs = ['lib/cmake/ITK-{0}/'.format(
-            self._upstream_version)]
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.includedirs = ['include']
-        # This hack allow to include directly, for example, a vnl
-        # file using `#include <vln/...>`
-        # Without the hack, the vln files cannot themselves include
-        # files using `#include<vnl/..>` since only <ITK-xx/vnl/...> is
-        # available. More, one has to add in CMakeLists.txt
-        # include_directories(${CONAN_INCLUDE_DIRS_ITK_SUB})
-        # to finish the use of this hack.
-        self.cpp_info.sub.includedirs.append('include/ITK-{0}'.format(
-            self._upstream_version))
+        # FIXME: use conan components
+        self.cpp_info.names["cmake_find_package"] = "ITK"
+        self.cpp_info.names["cmake_find_package_multi"] = "ITK"
+
