5777d71af8daa710cb4a95314dccf8654b9bedc4
Mark Nefedov <mark_nefedov@outlook.com>
2020-05-08 12:47:11 +0300

    bump volk version, but conan and vulkan-headers version need to be fixed.

--- recipes/volk/all/conanfile.py
+++ recipes/volk/all/conanfile.py
@@ -41,7 +41,10 @@ class VolkConan(ConanFile):
             del self.settings.compiler.libcxx
 
     def requirements(self):
-        self.requires("vulkan-headers/1.2.135.0")
+        if self.version <= 20200318:
+            self.requires("vulkan-headers/1.2.135.0")
+        else:
+            self.requires("vulkan-headers/1.2.140.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
