fa07792d98cf0f7b68b3dd4377b698385c8b55b3
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-05-08 04:38:37 +0200

    Add tcp-wrappers/7.6 recipe

---
+++ recipes/tcp-wrappers/all/test_package/test_package.c
@@ -0,0 +1,11 @@
+#include "tcpd.h"
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    struct request_info req;
+    request_init(&req, RQ_DAEMON, argv[0], RQ_FILE, 0, 0);
+    printf("host_acces: %d\n", hosts_access(&req));
+    return 0;
+}
