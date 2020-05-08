b5b3b9070e799fed62cae12a6ef25266172f1c00
SSE4 <tomskside@gmail.com>
2020-05-08 21:28:58 +0700

    - add GCC 10.1.0
    Signed-off-by: SSE4 <tomskside@gmail.com>

---
+++ recipes/gcc/all/conanfile.py
@@ -0,0 +1,116 @@
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+from conans.errors import ConanException, ConanInvalidConfiguration
+import os
+
+
+class GccConan(ConanFile):
+    name = "gcc"
+    description = "The GNU Compiler Collection includes front ends for C, " \
+                  "C++, Objective-C, Fortran, Ada, Go, and D, as well as " \
+                  "libraries for these languages (libstdc++,...). "
+    topics = ("gcc", "gnu", "compiler", "c", "c++")
+    homepage = "https://gcc.gnu.org/"
+    url = "https://github.com/conan-io/conan-center-index"
+    license = "	GPL-3.0-only"
+    settings = "os", "compiler", "arch", "build_type"
+    _autotools = None
+
+    def build_requirements(self):
+        self.build_requires("flex/2.6.4")
+
+    def _configure_autotools(self):
+        if self._autotools:
+            return self._autotools
+        self._autotools = AutoToolsBuildEnvironment(self)
+        pkgversion = 'conan GCC %s' % self.version
+        bugurl = self.url + '/issues'
+        args = [
+            "--enable-languages=c,c++",
+            "--disable-nls",
+            "--disable-multilib",
+            "--disable-bootstrap",
+            "--with-system-zlib",
+            "--with-gmp=%s" % self.deps_cpp_info['gmp'].rootpath,
+            '--with-mpc=%s' % self.deps_cpp_info["mpc"].rootpath,
+            "--with-mpfr=%s" % self.deps_cpp_info["mpfr"].rootpath,
+            "--without-isl",
+            '--with-pkgversion=%s' % pkgversion,
+            "--program-suffix=-%s" % self.version,
+            "--with-bugurl=%s" % bugurl
+        ]
+        if self.settings.os == "Macos":
+            xcrun = tools.XCRun(self.settings)
+            args.extend([
+                '--with-native-system-header-dir=/usr/include',
+                "--with-sysroot={}".format(xcrun.sdk_path)
+            ])
+        self._autotools.libs = []  # otherwise causes config.log to fail finding -lmpc
+        if self.settings.compiler in ["clang", "apple-clang"]:
+            # xgcc: error: unrecognized command-line option -stdlib=libc++
+            if self.settings.compiler.libcxx == "libc++":
+                self._autotools.cxx_flags.remove("-stdlib=libc++")
+            elif self.settings.compiler.libcxx in ["libstdc++", "libstdc++11"]:
+                self._autotools.cxx_flags.remove("-stdlib=libstdc++")
+        self._autotools.configure(args=args, configure_dir=self._source_subfolder)
+        return self._autotools
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def requirements(self):
+        self.requires("mpc/1.1.0")
+        self.requires('mpfr/4.0.2')
+        self.requires("gmp/6.1.2")
+        self.requires('zlib/1.2.11')
+
+    def configure(self):
+        if self.settings.os == "Windows":
+            raise ConanInvalidConfiguration("Windows builds aren't supported (yet), sorry")
+        if tools.cross_building(self.settings):
+            raise ConanInvalidConfiguration("no cross-building support (yet), sorry")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = "gcc-releases-gcc-%s" % self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    @property
+    def _make_args(self):
+        if self.settings.os == "Macos":
+            return ["BOOT_LDFLAGS=-Wl,-headerpad_max_install_names"]
+        return []
+
+    def build(self):
+        autotools = self._configure_autotools()
+        autotools.make(args=self._make_args)
+
+    def package_id(self):
+        del self.info.settings.compiler
+
+    def _remove_by_mask(self, pattern):
+        import fnmatch
+        for root, _, filenames in os.walk(self.package_folder):
+            for filename in filenames:
+                if fnmatch.fnmatch(filename, pattern):
+                    os.unlink(os.path.join(self.package_folder, root, filename))
+
+    def package(self):
+        autotools = self._configure_autotools()
+        autotools.install(args=self._make_args)
+        tools.rmdir(os.path.join(self.package_folder, "share"))
+        self._remove_by_mask("*.la")
+        self.copy(pattern="COPYING*", dst="licenses", src=self._source_subfolder)
+
+    def package_info(self):
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH env var with : " + bindir)
+        self.env_info.PATH.append(bindir)
+
+        cc = os.path.join(bindir, "gcc-%s" % self.version)
+        self.output.info("Creating CC env var with : " + cc)
+        self.env_info.CC = cc
+
+        cxx = os.path.join(bindir, "g++-%s" % self.version)
+        self.output.info("Creating CXX env var with : " + cxx)
+        self.env_info.CXX = cxx
---
+++ recipes/gcc/all/test_package/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Bincrafters\n");
+    return 0;
+}
+
