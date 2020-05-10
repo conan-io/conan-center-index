c4331088c4d936932b84dab47e69d7d197f435a6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-05-10 23:53:45 +0200

    draco: add libm in system_libs

--- recipes/draco/all/conanfile.py
+++ recipes/draco/all/conanfile.py
@@ -129,3 +129,5 @@ class DracoConan(ConanFile):
         self.cpp_info.names["cmake_find_package_multi"] = "Draco"
         self.cpp_info.names["pkg_config"] = "Draco"
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs.append("m")
