9727f0ff1288d79f44a39959dc8c178140108c42
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-05-11 19:56:08 +0200

    c-blosc: add pkg_config name

--- recipes/c-blosc/all/conanfile.py
+++ recipes/c-blosc/all/conanfile.py
@@ -103,6 +103,7 @@ class CbloscConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
+        self.cpp_info.names["pkg_config"] = "blosc"
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("pthread")
