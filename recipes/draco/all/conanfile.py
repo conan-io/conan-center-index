954f1060d1e39cb678d509d8866d5349c0856ed3
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-05-12 10:28:02 +0200

    draco: fix pkg_config name

--- recipes/draco/all/conanfile.py
+++ recipes/draco/all/conanfile.py
@@ -127,7 +127,7 @@ class DracoConan(ConanFile):
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "Draco"
         self.cpp_info.names["cmake_find_package_multi"] = "Draco"
-        self.cpp_info.names["pkg_config"] = "Draco"
+        self.cpp_info.names["pkg_config"] = "draco"
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("m")
