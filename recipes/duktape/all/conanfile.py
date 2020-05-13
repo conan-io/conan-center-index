f1c022ccdb1c4ee3a5989c08edfd2c10fc0a5573
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-05-14 00:41:10 +0200

    feat: add duktape

---
+++ recipes/duktape/all/conanfile.py
@@ -0,0 +1,69 @@
+import os
+from conans import CMake, ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+from conans.tools import Version
+
+
+class DuktapeConan(ConanFile):
+    name = "duktape"
+    license = "MIT"
+    description = "Duktape is an embeddable Javascript engine, with a focus on portability and compact footprint."
+    topics = ("javascript", "engine", "embeddable", "compact")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://duktape.org"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake", "cmake_find_package"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["LEVELDB_BUILD_TESTS"] = False
+        self._cmake.definitions["LEVELDB_BUILD_BENCHMARKS"] = False
+        self._cmake.configure()
+        return self._cmake
+
+    def package_info(self):
+        self.cpp_info.includedirs = ['include']
+        self.cpp_info.srcdirs = ['src']
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        if not self.options.shared:
+            if self.settings.os == "Linux":
+                self.cpp_info.system_libs = ["pthread"]
