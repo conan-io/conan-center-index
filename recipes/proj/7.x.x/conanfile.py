cd055793042d44798fdcf303d0c42b596097df5a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-05-14 09:14:04 +0200

    proj 7.x.x: do not build projsync if no curl (#1625)
    * proj 7.x.x: do not build projsync if no curl
    
    build of projsync fails if ENABLE_CURL OFF, BUILD_PROJSYNC has to be
    explicitly disabled in this case
    
    * proj 7.x.x: requires instead of requires.add

--- recipes/proj/7.x.x/conanfile.py
+++ recipes/proj/7.x.x/conanfile.py
@@ -43,11 +43,11 @@ class ProjConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        self.requires.add("sqlite3/3.31.1")
+        self.requires("sqlite3/3.31.1")
         if self.options.with_tiff:
-            self.requires.add("libtiff/4.1.0")
+            self.requires("libtiff/4.1.0")
         if self.options.with_curl:
-            self.requires.add("libcurl/7.69.1")
+            self.requires("libcurl/7.69.1")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -74,7 +74,7 @@ class ProjConan(ConanFile):
         self._cmake.definitions["BUILD_GIE"] = True
         self._cmake.definitions["BUILD_PROJ"] = True
         self._cmake.definitions["BUILD_PROJINFO"] = True
-        self._cmake.definitions["BUILD_PROJSYNC"] = True
+        self._cmake.definitions["BUILD_PROJSYNC"] = self.options.with_curl
         self._cmake.definitions["PROJ_DATA_SUBDIR"] = "res"
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
