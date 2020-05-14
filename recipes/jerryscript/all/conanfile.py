4992223e8cf41128d4d609c111f2a4d91ee9f473
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-05-14 15:05:52 +0200

    feat: add jerryscript

---
+++ recipes/jerryscript/all/conanfile.py
@@ -0,0 +1,67 @@
+import os
+from conans import CMake, ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+
+class JerryScriptStackConan(ConanFile):
+    name = "jerryscript"
+    license = "Apache-2.0"
+    homepage = "https://github.com/jerryscript-project/jerryscript"
+    url = "https://github.com/conan-io/conan-center-index"
+    description = "Ultra-lightweight JavaScript engine for the Internet of Things"
+    topics = ["javascript", "iot", "jerryscript", "javascript-engine"]
+    exports_sources = ['CMakeLists.txt']
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True
+    }
+    generators = "cmake"
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst='licenses', src=os.path.join(self._source_subfolder, "license"))
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["pthread"]
