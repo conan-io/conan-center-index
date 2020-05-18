42be87403439a5f360bda1b2a0614a1c26fc8643
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-05-18 16:10:32 +0200

    flex: simplify build as crossbuilding wouldn't work anyway + rework test_package

--- recipes/flex/all/test_package/conanfile.py
+++ recipes/flex/all/test_package/conanfile.py
@@ -1,19 +1,27 @@
-import os
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanException
+import os
+
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
     def build(self):
-        self.run("flex -+ --outfile basic_nr.cpp %s" % os.path.join(self.source_folder, "basic_nr.l"), run_environment=True)
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        if not tools.cross_building(self.settings, skip_x64_x86=True):
+            self.run("flex --version", run_environment=True)
+
+            flex_bin = tools.which("flex")
+            if not flex_bin.startswith(self.deps_cpp_info["flex"].rootpath):
+                raise ConanException("Wrong flex executable captured")
+            cmake = CMake(self)
+            cmake.verbose = True
+            cmake.definitions["FLEX_ROOT"] = self.deps_cpp_info["flex"].rootpath
+            cmake.configure()
+            cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings, skip_x64_x86=True):
             bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
-            
-            self.run("flex --version", run_environment=True)
+            src = os.path.join(self.source_folder, "basic_nr.txt")
+            self.run("{} {}".format(bin_path, src), run_environment=True)
