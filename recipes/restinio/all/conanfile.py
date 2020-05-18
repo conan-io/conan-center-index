3b45fd917a5ee4ed7f2b82ad726cdcf09e9f1262
Franck W <franck.rj.w@gmail.com>
2020-05-18 23:20:15 +0200

    Changed how the cppstd version is checked.

--- recipes/restinio/all/conanfile.py
+++ recipes/restinio/all/conanfile.py
@@ -26,7 +26,25 @@ class RestinioConan(ConanFile):
         return "build_subfolder"
 
     def configure(self):
-        tools.check_min_cppstd(self, "14")
+        minimal_cpp_standard = "14"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
+        minimal_version = {
+            "gcc": "5",
+            "clang": "3.4",
+            "apple-clang": "10",
+            "Visual Studio": "15"
+        }
+        compiler = str(self.settings.compiler)
+        if compiler not in minimal_version:
+            self.output.warn(
+                "%s recipe lacks information about the %s compiler standard version support" % (self.name, compiler))
+            self.output.warn(
+                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+            return
+        version = tools.Version(self.settings.compiler.version)
+        if version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
 
     def requirements(self):
         self.requires("http_parser/2.9.4")
