3de375dcf2842b759bd164b935943be087161d30
Vladimir Looze <woldemar@mimas.ru>
2020-05-19 14:07:32 +0300

    Conan config install (#202)

--- build.py
+++ build.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
         upload_only_when_stable=1,
         stable_branch_pattern="master",
         stable_channel="_",
+        config_url="https://github.com/trassir/conan-config.git",
         remotes="https://api.bintray.com/conan/trassir/conan-public"
     )
     builder.add_common_builds(pure_c=is_pure_c)
