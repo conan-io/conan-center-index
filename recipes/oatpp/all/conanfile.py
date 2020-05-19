feec03de50c57d269dc5736c428559f346d35820
Uilian Ries <uilianries@gmail.com>
2020-05-19 11:41:49 -0300

    Prepare for CCI
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/oatpp/all/conanfile.py
+++ recipes/oatpp/all/conanfile.py
@@ -4,23 +4,25 @@ import os
 
 class OatppConan(ConanFile):
     name = "oatpp"
-    version = "1.0.0"
     description = "Modern Web Framework for C++"
-    homepage = "https://github.com/zeromq/cppzmq"
-    license = "Apache License 2.0"
+    homepage = "https://github.com/oatpp/oatpp"
+    license = "Apache-2.0"
     topics = ("conan", "oat++", "oatpp", "web-framework")
     url = "https://github.com/conan-io/conan-center-index"
     generators = "cmake", "cmake_find_package"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-
-    _source_subfolder = "source_subfolder"
+    exports_sources = "CMakeLists.txt"
     _cmake = None
 
     @property
-    def _full_source_subfolder(self):
-        return os.path.join(self.source_folder, self._source_subfolder)
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -30,47 +32,28 @@ class OatppConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build(target="oatpp")
-        cmake.build(target="oatpp-test")
-        cmake.install()
-
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
 
         self._cmake = CMake(self)
         self._cmake.definitions["OATPP_BUILD_TESTS"] = False
-        self._cmake.definitions["CMAKE_INSTALL_PREFIX"] = os.path.join(
-            self.build_folder, "__dist"
-        )
-        self._cmake.configure(source_folder=self._source_subfolder)
+        self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
     def package(self):
-        # copy headers
-        self.copy(
-            "*",
-            src="__dist/include/oatpp-{version}/oatpp"
-                .format(version=self.version),
-            dst="include",
-        )
-
-        # copy libraries
-        self.copy(
-            "*",
-            src="__dist/lib/oatpp-{version}".format(version=self.version),
-            dst="lib",
-            keep_path=False,
-        )
-
-        self.copy(
-            "LICENSE",
-            src=self._full_source_subfolder,
-            dst="licenses",
-            keep_path=False
-        )
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
+        self.cpp_info.includedirs = [os.path.join("include", "oatpp-{}".format(self.version), "oatpp")]
+        self.cpp_info.libdirs = [os.path.join("lib",  "oatpp-{}".format(self.version))]
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["pthread"]
