89c4ef0e882b33f83b3a696c632ea795722cde8f
Vladimir Looze <woldemar@mimas.ru>
2020-05-20 14:28:23 +0300

    Remove openssl 1.0.2r (#209)

--- recipes/pjsip/2.8/conanfile.py
+++ recipes/pjsip/2.8/conanfile.py
@@ -58,7 +58,7 @@ class PjsipConan(ConanFile):
         if self.settings.os == "Linux":
             self.requires("libalsa/1.1.0")
         if self.options.SSL:
-            self.requires(_openSSL+"/1.0.2r")
+            self.requires(_openSSL+"/1.1.1d")
 
     def _configure_autotools(self):
         if not self._autotools:
