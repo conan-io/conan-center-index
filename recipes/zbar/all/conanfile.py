d986b348f867cbecc685fca6fb9100a987d7b3b0
Riff, Eric <eriff@irobot.com>
2020-05-21 09:13:33 -0300

    Remove pthread on macOS

--- recipes/zbar/all/conanfile.py
+++ recipes/zbar/all/conanfile.py
@@ -107,5 +107,4 @@ class zbarConan(ConanFile):
             self.cpp_info.system_libs = ["pthread"]
         if tools.is_apple_os(self.settings.os):
             self.cpp_info.system_libs = ["iconv"]
-            if self.options.enable_pthread:
-                self.cpp_info.system_libs.append("pthread")
+
