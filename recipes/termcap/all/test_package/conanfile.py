2af6475088295cc0fa5faa41d96ad85a45039dff
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-05-22 22:45:12 +0200

    termcap: set TERM environment variable in test_package

--- recipes/termcap/all/test_package/conanfile.py
+++ recipes/termcap/all/test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
@@ -12,5 +12,10 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        self.run(bin_path, run_environment=True)
+        if not tools.cross_building(self.settings):
+            env = {}
+            if not tools.get_env("TERM"):
+                env["TERM"] = "dumb"
+            with tools.environment_append(env):
+                bin_path = os.path.join("bin", "test_package")
+                self.run(bin_path, run_environment=True)
