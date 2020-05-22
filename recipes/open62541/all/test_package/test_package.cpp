4b03a44e391e441002cf4e2497aeb6b4fdf90ebb
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-05-22 15:33:57 +0200

    rework integration test to use cpp threads

--- recipes/open62541/all/test_package/test_package.cpp
+++ recipes/open62541/all/test_package/test_package.cpp
@@ -2,30 +2,24 @@
 #include <open62541/server.h>
 #include <open62541/server_config_default.h>
 
-#include <pthread.h>
 #include <stdlib.h>
-
-static volatile UA_Boolean running = true;
-
-void *run(void *arg) {
-  UA_Server *server = (UA_Server *)arg;
-  return (void *)UA_Server_run(server, &running);
-}
+#include <thread>
 
 int main(void) {
 
   UA_Server *server = UA_Server_new();
   UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+  UA_Boolean running = true;
+  UA_StatusCode return_code;
 
-  pthread_t server_thread;
-  pthread_create(&server_thread, NULL, run, (void *)server);
+  std::thread server_thread(
+      [&]() { return_code = UA_Server_run(server, &running); });
 
-  sleep(1);
+  std::this_thread::sleep_for(std::chrono::seconds(1));
   running = false;
-  void *returnCode;
-  pthread_join(server_thread, &returnCode);
-  UA_StatusCode retval = *((unsigned int *)(&returnCode));
 
+  server_thread.join();
   UA_Server_delete(server);
-  return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+
+  return return_code == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
 }
