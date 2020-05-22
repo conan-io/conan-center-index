2744113148ec40aa029120334d87569972bf8db1
tonka3000 <tonka3100@gmail.com>
2020-05-22 10:24:34 +0200

    remove author

--- recipes/doxygen/all/conanfile.py
+++ recipes/doxygen/all/conanfile.py
@@ -11,7 +11,6 @@ class DoxygenInstallerConan(ConanFile):
     topics = ("conan", "doxygen", "installer", "devtool", "documentation")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/doxygen/doxygen"
-    author = "Inexor <info@inexor.org>"
     license = "GPL-2.0-only"
 
     settings = {
