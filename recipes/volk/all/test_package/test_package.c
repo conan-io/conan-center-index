e3ceff19145349c91e184e4cdc73f9d5b359389f
Mark Nefedov <mark_nefedov@outlook.com>
2020-05-25 18:55:10 +0300

    removed old version
    minor test chages
    
     Author:    Mark Nefedov <mark@outlook.com>

--- recipes/volk/all/test_package/test_package.c
+++ recipes/volk/all/test_package/test_package.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 
 int main() {
-    /* Try to initialize volk. This might not work on CI builds,
-     * but should have compiled at least. */
     VkResult r = volkInitialize();
-    uint32_t version = volkGetInstanceVersion();
-    printf("Vulkan version %d.%d.%d initialized.\n",
+    if (r == VK_SUCCESS){
+        uint32_t version = volkGetInstanceVersion();
+        printf("Vulkan version %d.%d.%d initialized.\n",
             VK_VERSION_MAJOR(version),
             VK_VERSION_MINOR(version),
             VK_VERSION_PATCH(version)
             );
+    }
+    return 0;
 }
