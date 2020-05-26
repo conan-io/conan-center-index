dacfaee8ad57de2c1f4f6ee9366a4c92faa1d128
Anonymous Maarten <madebr@users.noreply.github.com>
2020-05-26 03:51:09 +0200

    termcolor: use no_copy_source

--- recipes/termcolor/all/conanfile.py
+++ recipes/termcolor/all/conanfile.py
@@ -9,7 +9,7 @@ class TermcolorConan(ConanFile):
     description = "Termcolor is a header-only C++ library for printing colored messages to the terminal."
     license = "BSD-3-Clause"
     topics = ("conan", "termcolor", "terminal", "color")
-    no_copy_sources = True
+    no_copy_source = True
 
     @property
     def _source_subfolder(self):
