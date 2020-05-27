c29b25bea0bf437f760d1cc2c104f7911d174b5b
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-05-27 22:55:34 +0200

    disable glib

--- recipes/harfbuzz/all/conanfile.py
+++ recipes/harfbuzz/all/conanfile.py
@@ -28,7 +28,7 @@ class HarfbuzzConan(ConanFile):
         "fPIC": True,
         "with_freetype": True,
         "with_icu": False,
-        "with_glib": True,
+        "with_glib": False,
         "with_gdi": True,
         "with_uniscribe": True
     }
