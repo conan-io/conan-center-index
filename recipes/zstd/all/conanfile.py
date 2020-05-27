827d0d8aeb7d8d9a326a999ea566ef30a80c49ed
SSE4 <tomskside@gmail.com>
2020-05-27 21:12:35 +0700

    Update conanfile.py

--- recipes/zstd/all/conanfile.py
+++ recipes/zstd/all/conanfile.py
@@ -49,6 +49,7 @@ class ZstdConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
