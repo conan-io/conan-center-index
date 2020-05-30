6b4d0984c542d65bb779e3b7a56ba8a610ddab62
Geoffrey M Gunter <geoffrey.m.gunter@gmail.com>
2020-05-30 13:53:10 -0700

    Add initial package recipe

---
+++ recipes/outcome/all/conanfile.py
@@ -0,0 +1,31 @@
+import os
+from conans import ConanFile, tools
+
+class OutcomeConan(ConanFile):
+    name = "outcome"
+    homepage = "https://github.com/ned14/outcome"
+    description = "Provides very lightweight outcome<T> and result<T>"
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    topics = ("outcome", "result")
+    settings = "compiler"
+    no_copy_source = True
+
+    _source_subfolder = "source_subfolder"
+
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, "14")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy("outcome.hpp", dst="include",
+                src=os.path.join(self._source_subfolder, "single-header"))
+        self.copy("Licence.txt", dst="licenses", src=self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
