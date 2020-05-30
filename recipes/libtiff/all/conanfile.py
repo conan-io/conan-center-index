0df3f97ea778af9c5d1a7fb6da60c4693ed70f5c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-05-30 14:05:37 +0200

    libtiff: style consistency

--- recipes/libtiff/all/conanfile.py
+++ recipes/libtiff/all/conanfile.py
@@ -65,22 +65,22 @@ class LibtiffConan(ConanFile):
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename('tiff-' + self.version, self._source_subfolder)
+        os.rename("tiff-" + self.version, self._source_subfolder)
 
     def _patch_sources(self):
         cmakefile = os.path.join(self._source_subfolder, "CMakeLists.txt")
         if self.options.shared and self.settings.compiler == "Visual Studio":
             # https://github.com/Microsoft/vcpkg/blob/master/ports/tiff/fix-cxx-shared-libs.patch
-            tools.replace_in_file(os.path.join(self._source_subfolder, 'libtiff', 'CMakeLists.txt'),
-                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION})',
-                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION} '
-                                  r'WINDOWS_EXPORT_ALL_SYMBOLS ON)')
+            tools.replace_in_file(os.path.join(self._source_subfolder, "libtiff", "CMakeLists.txt"),
+                                  r"set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION})",
+                                  r"set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION} "
+                                  r"WINDOWS_EXPORT_ALL_SYMBOLS ON)")
 
             if self.settings.os == "Windows" and self.settings.compiler != "Visual Studio":
                 tools.replace_in_file(cmakefile,
                                       "find_library(M_LIBRARY m)",
                                       "if (NOT MINGW)\n  find_library(M_LIBRARY m)\nendif()")
-                if tools.Version(self.version) == '4.0.8':
+                if tools.Version(self.version) == "4.0.8":
                     # only one occurence must be patched. fixed in 4.0.9
                     tools.replace_in_file(cmakefile, "if (UNIX)", "if (UNIX OR MINGW)")
 
@@ -110,16 +110,16 @@ class LibtiffConan(ConanFile):
         self.copy("COPYRIGHT", src=self._source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, 'lib', 'pkgconfig'))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.libs = ["tiffxx", "tiff"]
-        if self.settings.os == "Windows" and self.settings.build_type == "Debug" and self.settings.compiler == 'Visual Studio':
-            self.cpp_info.libs = [lib+'d' for lib in self.cpp_info.libs]
-        if self.options.shared and self.settings.os == "Windows" and self.settings.compiler != 'Visual Studio':
-            self.cpp_info.libs = [lib+'.dll' for lib in self.cpp_info.libs]
+        if self.settings.os == "Windows" and self.settings.build_type == "Debug" and self.settings.compiler == "Visual Studio":
+            self.cpp_info.libs = [lib + "d" for lib in self.cpp_info.libs]
+        if self.options.shared and self.settings.os == "Windows" and self.settings.compiler != "Visual Studio":
+            self.cpp_info.libs = [lib + ".dll" for lib in self.cpp_info.libs]
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("m")
         self.cpp_info.names["cmake_find_package"] = "TIFF"
         self.cpp_info.names["cmake_find_package_multi"] = "TIFF"
-        self.cpp_info.names['pkg_config'] = 'libtiff-4'
+        self.cpp_info.names["pkg_config"] = "libtiff-4"
