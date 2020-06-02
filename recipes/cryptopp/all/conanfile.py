9171525b59cc9016b9238d6ad89eab5d45db6480
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-06-02 10:47:53 +0200

    Update recipes/cryptopp/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/cryptopp/all/conanfile.py
+++ recipes/cryptopp/all/conanfile.py
@@ -67,3 +67,5 @@ class CryptoPPConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["pthread"]
