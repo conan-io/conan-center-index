0eba93043420277cfe8881fcb0b9aa01ea831c34
xyz1001 <zgzf1001@gmail.com>
2020-06-03 16:57:40 +0800

    Add new package Qr-Code-generator

---
+++ recipes/qr-code-generator/all/test_package/test_package.cpp
@@ -0,0 +1,10 @@
+#include <stdexcept>
+
+#include "qrcodegen/QrCode.hpp"
+
+int main() {
+    qrcodegen::QrCode::encodeText("test", qrcodegen::QrCode::Ecc::MEDIUM)
+            .toSvgString(0);
+
+    return 0;
+}
