a0910725cf99ce3325ef86dffdd76977d5f1cbe3
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-06-04 04:30:50 -0300

    Add QuickFIX 1.15.1

---
+++ recipes/quickfix/conanfile.py
@@ -0,0 +1,40 @@
+from conans import ConanFile, CMake, tools
+
+
+class QuickfixConan(ConanFile):
+    name = "quickfix"
+    version = "1.15.1"
+    license = "The QuickFIX Software License, Version 1.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "http://www.quickfixengine.org"
+    description = "QuickFIX is a free and open source implementation of the FIX protocol"
+    topics = ("conan", "QuickFIX", "FIX", "Financial Information Exchange", "libraries", "cpp")
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/quickfix/quickfix.git")
+        # This small hack might be useful to guarantee proper /MT /MD linkage
+        # in MSVC if the packaged project doesn't have variables to set it
+        # properly
+        tools.replace_in_file("quickfix/CMakeLists.txt",
+                              "project(${quickfix_PROJECT_NAME} VERSION 0.1 LANGUAGES CXX C)",
+                              '''project(${quickfix_PROJECT_NAME} VERSION 0.1 LANGUAGES CXX C)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()''')
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build(target="quickfix")
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["quickfix"]
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="quickfix")
+        return cmake
---
+++ recipes/quickfix/test_package/example.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "quickfix/Initiator.h"
+
+int main()
+{
+  std::cout << "oi\n";
+}
