15b7c202da379412579226dfd76d78c79e4e65d1
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-06-05 05:34:57 -0300

    Add executor example from quickfix project in test_package

--- recipes/quickfix/test_package/conanfile.py
+++ recipes/quickfix/test_package/conanfile.py
@@ -7,6 +7,18 @@ class QuickfixTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    def source(self):
+        self.run("""
+rm -rf source || true
+mkdir source
+cd source
+git init
+git remote add origin https://github.com/quickfix/quickfix.git
+git config core.sparseCheckout true
+echo "examples/" >> .git/info/sparse-checkout
+git pull origin master
+        """)
+
     def build(self):
         cmake = CMake(self)
         # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
@@ -22,4 +34,4 @@ class QuickfixTestConan(ConanFile):
     def test(self):
         if not tools.cross_building(self):
             os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+            self.run(".%sexecutor" % os.sep)
