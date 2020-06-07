b83781e8faea54efbccc4cdc57b305585aa83901
Pavel_Kisliak <real@tut.by>
2020-06-07 18:05:44 +0300

    [pugixml] Add displaying of current package options (wchar_mode, header_only, no_exceptions) in the test package

--- recipes/pugixml/all/test_package/test_package.cpp
+++ recipes/pugixml/all/test_package/test_package.cpp
@@ -13,8 +13,26 @@ int main() {
         version << '.' << maintenanceVersion;
 
     pugi::xml_document doc;
-    doc.append_child(PUGIXML_TEXT("PugiXml"))
-        .append_attribute(PUGIXML_TEXT("Version"))
+    pugi::xml_node node = doc.append_child(PUGIXML_TEXT("PugiXml"));
+    node.append_attribute(PUGIXML_TEXT("Version"))
         .set_value(version.str().c_str());
+
+    node.append_attribute(PUGIXML_TEXT("wchar_mode"))
+        .set_value(sizeof(PUGIXML_CHAR) != 1);
+
+    node.append_attribute(PUGIXML_TEXT("header_only"))
+#ifdef PUGIXML_HEADER_ONLY
+        .set_value(true);
+#else
+        .set_value(false);
+#endif
+
+    node.append_attribute(PUGIXML_TEXT("no_exceptions"))
+#ifdef PUGIXML_NO_EXCEPTIONS
+        .set_value(true);
+#else
+        .set_value(false);
+#endif
+
     doc.print(std::cout);
 }
