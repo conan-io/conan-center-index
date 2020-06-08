e47ffe0b367804d8a630b038f4a07c2bec76e33c
Danielle Candido <dcandido@dyndrite.com>
2020-06-08 12:14:47 -0700

    Simplify license copy, add CXX_STANDARD property.

--- recipes/nanoflann/1.3.1/conanfile.py
+++ recipes/nanoflann/1.3.1/conanfile.py
@@ -19,11 +19,6 @@ class NanoflannConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
-    @property
-    def _license_text(self):
-        license = open(os.path.join(self.source_folder, self._source_subfolder, "COPYING"), "rb").read()
-        return license.lstrip()
-
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = glob.glob(self.name + "-*/")[0]
@@ -31,9 +26,8 @@ class NanoflannConan(ConanFile):
 
     def package(self):
         include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
         self.copy(pattern="*", dst="include", src=include_folder)
-        tools.save(os.path.join(self.package_folder, "licenses", "LICENSE"), self._license_text)
-
 
     def package_id(self):
         self.info.header_only()
