fed89f6d02e66f86aa0d26933132b26970df86df
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-06-09 17:23:49 -0300

    Simplify test_packge

--- recipes/quickfix/all/test_package/executor.cpp
+++ recipes/quickfix/all/test_package/executor.cpp
@@ -1,6 +1,7 @@
 #include "config.h"
 
 #include "quickfix/FileStore.h"
+#include "quickfix/Application.h"
 #include "quickfix/SocketAcceptor.h"
 #ifdef HAVE_SSL
   #include "quickfix/ThreadedSSLSocketAcceptor.h"
@@ -8,67 +9,22 @@
 #endif
 #include "quickfix/Log.h"
 #include "quickfix/SessionSettings.h"
-#include "quickfix/Application.h"
-#include "quickfix/MessageCracker.h"
-#include "quickfix/Values.h"
-#include "quickfix/Utility.h"
-#include "quickfix/Mutex.h"
-#include "quickfix/Session.h"
-
-#include "quickfix/fix40/ExecutionReport.h"
-#include "quickfix/fix41/ExecutionReport.h"
-#include "quickfix/fix42/ExecutionReport.h"
-#include "quickfix/fix43/ExecutionReport.h"
-#include "quickfix/fix44/ExecutionReport.h"
-#include "quickfix/fix50/ExecutionReport.h"
-
-#include "quickfix/fix40/NewOrderSingle.h"
-#include "quickfix/fix41/NewOrderSingle.h"
-#include "quickfix/fix42/NewOrderSingle.h"
-#include "quickfix/fix43/NewOrderSingle.h"
-#include "quickfix/fix44/NewOrderSingle.h"
-#include "quickfix/fix50/NewOrderSingle.h"
-
-
 
 class Application
-    : public FIX::Application, public FIX::MessageCracker
+    : public FIX::Application
 {
 public:
-  Application() : m_orderID(0), m_execID(0) {}
-
   // Application overloads
   void onCreate( const FIX::SessionID& );
   void onLogon( const FIX::SessionID& sessionID );
   void onLogout( const FIX::SessionID& sessionID );
   void toAdmin( FIX::Message&, const FIX::SessionID& );
   void toApp( FIX::Message&, const FIX::SessionID& )
-  throw( FIX::DoNotSend );
+  EXCEPT( FIX::DoNotSend );
   void fromAdmin( const FIX::Message&, const FIX::SessionID& )
-  throw( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon );
+  EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon );
   void fromApp( const FIX::Message& message, const FIX::SessionID& sessionID )
-  throw( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType );
-
-  // MessageCracker overloads
-  void onMessage( const FIX40::NewOrderSingle&, const FIX::SessionID& );
-  void onMessage( const FIX41::NewOrderSingle&, const FIX::SessionID& );
-  void onMessage( const FIX42::NewOrderSingle&, const FIX::SessionID& );
-  void onMessage( const FIX43::NewOrderSingle&, const FIX::SessionID& );
-  void onMessage( const FIX44::NewOrderSingle&, const FIX::SessionID& );
-  void onMessage( const FIX50::NewOrderSingle&, const FIX::SessionID& );
-
-  std::string genOrderID() {
-    std::stringstream stream;
-    stream << ++m_orderID;
-    return stream.str();
-  }
-  std::string genExecID() {
-    std::stringstream stream;
-    stream << ++m_execID;
-    return stream.str();
-  }
-private:
-  int m_orderID, m_execID;
+  EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType );
 };
 
 void Application::onCreate( const FIX::SessionID& sessionID ) {}
@@ -78,350 +34,41 @@ void Application::toAdmin( FIX::Message& message,
                            const FIX::SessionID& sessionID ) {}
 void Application::toApp( FIX::Message& message,
                          const FIX::SessionID& sessionID )
-throw( FIX::DoNotSend ) {}
+EXCEPT( FIX::DoNotSend ) {}
 
 void Application::fromAdmin( const FIX::Message& message,
                              const FIX::SessionID& sessionID )
-throw( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon ) {}
+EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon ) {}
 
 void Application::fromApp( const FIX::Message& message,
                            const FIX::SessionID& sessionID )
-throw( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType )
-{ crack( message, sessionID ); }
-
-void Application::onMessage( const FIX40::NewOrderSingle& message,
-                             const FIX::SessionID& sessionID )
-{
-  FIX::Symbol symbol;
-  FIX::Side side;
-  FIX::OrdType ordType;
-  FIX::OrderQty orderQty;
-  FIX::Price price;
-  FIX::ClOrdID clOrdID;
-  FIX::Account account;
-
-  message.get( ordType );
-
-  if ( ordType != FIX::OrdType_LIMIT )
-    throw FIX::IncorrectTagValue( ordType.getField() );
-
-  message.get( symbol );
-  message.get( side );
-  message.get( orderQty );
-  message.get( price );
-  message.get( clOrdID );
-
-  FIX40::ExecutionReport executionReport = FIX40::ExecutionReport
-      ( FIX::OrderID( genOrderID() ),
-        FIX::ExecID( genExecID() ),
-        FIX::ExecTransType( FIX::ExecTransType_NEW ),
-        FIX::OrdStatus( FIX::OrdStatus_FILLED ),
-        symbol,
-        side,
-        orderQty,
-        FIX::LastShares( orderQty ),
-        FIX::LastPx( price ),
-        FIX::CumQty( orderQty ),
-        FIX::AvgPx( price ) );
-
-  executionReport.set( clOrdID );
-
-  if( message.isSet(account) )
-    executionReport.setField( message.get(account) );
-
-  try
-  {
-    FIX::Session::sendToTarget( executionReport, sessionID );
-  }
-  catch ( FIX::SessionNotFound& ) {}
-}
-
-void Application::onMessage( const FIX41::NewOrderSingle& message,
-                             const FIX::SessionID& sessionID )
-{
-  FIX::Symbol symbol;
-  FIX::Side side;
-  FIX::OrdType ordType;
-  FIX::OrderQty orderQty;
-  FIX::Price price;
-  FIX::ClOrdID clOrdID;
-  FIX::Account account;
-
-  message.get( ordType );
-
-  if ( ordType != FIX::OrdType_LIMIT )
-    throw FIX::IncorrectTagValue( ordType.getField() );
-
-  message.get( symbol );
-  message.get( side );
-  message.get( orderQty );
-  message.get( price );
-  message.get( clOrdID );
-
-  FIX41::ExecutionReport executionReport = FIX41::ExecutionReport
-      ( FIX::OrderID( genOrderID() ),
-        FIX::ExecID( genExecID() ),
-        FIX::ExecTransType( FIX::ExecTransType_NEW ),
-        FIX::ExecType( FIX::ExecType_FILL ),
-        FIX::OrdStatus( FIX::OrdStatus_FILLED ),
-        symbol,
-        side,
-        orderQty,
-        FIX::LastShares( orderQty ),
-        FIX::LastPx( price ),
-        FIX::LeavesQty( 0 ),
-        FIX::CumQty( orderQty ),
-        FIX::AvgPx( price ) );
-
-  executionReport.set( clOrdID );
-
-  if( message.isSet(account) )
-    executionReport.setField( message.get(account) );
-
-  try
-  {
-    FIX::Session::sendToTarget( executionReport, sessionID );
-  }
-  catch ( FIX::SessionNotFound& ) {}
-}
-
-void Application::onMessage( const FIX42::NewOrderSingle& message,
-                             const FIX::SessionID& sessionID )
-{
-  FIX::Symbol symbol;
-  FIX::Side side;
-  FIX::OrdType ordType;
-  FIX::OrderQty orderQty;
-  FIX::Price price;
-  FIX::ClOrdID clOrdID;
-  FIX::Account account;
-
-  message.get( ordType );
-
-  if ( ordType != FIX::OrdType_LIMIT )
-    throw FIX::IncorrectTagValue( ordType.getField() );
-
-  message.get( symbol );
-  message.get( side );
-  message.get( orderQty );
-  message.get( price );
-  message.get( clOrdID );
-
-  FIX42::ExecutionReport executionReport = FIX42::ExecutionReport
-      ( FIX::OrderID( genOrderID() ),
-        FIX::ExecID( genExecID() ),
-        FIX::ExecTransType( FIX::ExecTransType_NEW ),
-        FIX::ExecType( FIX::ExecType_FILL ),
-        FIX::OrdStatus( FIX::OrdStatus_FILLED ),
-        symbol,
-        side,
-        FIX::LeavesQty( 0 ),
-        FIX::CumQty( orderQty ),
-        FIX::AvgPx( price ) );
-
-  executionReport.set( clOrdID );
-  executionReport.set( orderQty );
-  executionReport.set( FIX::LastShares( orderQty ) );
-  executionReport.set( FIX::LastPx( price ) );
-
-  if( message.isSet(account) )
-    executionReport.setField( message.get(account) );
-
-  try
-  {
-    FIX::Session::sendToTarget( executionReport, sessionID );
-  }
-  catch ( FIX::SessionNotFound& ) {}
-}
-
-void Application::onMessage( const FIX43::NewOrderSingle& message,
-                             const FIX::SessionID& sessionID )
-{
-  FIX::Symbol symbol;
-  FIX::Side side;
-  FIX::OrdType ordType;
-  FIX::OrderQty orderQty;
-  FIX::Price price;
-  FIX::ClOrdID clOrdID;
-  FIX::Account account;
-
-  message.get( ordType );
-
-  if ( ordType != FIX::OrdType_LIMIT )
-    throw FIX::IncorrectTagValue( ordType.getField() );
-
-  message.get( symbol );
-  message.get( side );
-  message.get( orderQty );
-  message.get( price );
-  message.get( clOrdID );
-
-  FIX43::ExecutionReport executionReport = FIX43::ExecutionReport
-      ( FIX::OrderID( genOrderID() ),
-        FIX::ExecID( genExecID() ),
-        FIX::ExecType( FIX::ExecType_FILL ),
-        FIX::OrdStatus( FIX::OrdStatus_FILLED ),
-        side,
-        FIX::LeavesQty( 0 ),
-        FIX::CumQty( orderQty ),
-        FIX::AvgPx( price ) );
-
-  executionReport.set( clOrdID );
-  executionReport.set( symbol );
-  executionReport.set( orderQty );
-  executionReport.set( FIX::LastQty( orderQty ) );
-  executionReport.set( FIX::LastPx( price ) );
-
-  if( message.isSet(account) )
-    executionReport.setField( message.get(account) );
-
-  try
-  {
-    FIX::Session::sendToTarget( executionReport, sessionID );
-  }
-  catch ( FIX::SessionNotFound& ) {}
-}
-
-void Application::onMessage( const FIX44::NewOrderSingle& message,
-                             const FIX::SessionID& sessionID )
-{
-  FIX::Symbol symbol;
-  FIX::Side side;
-  FIX::OrdType ordType;
-  FIX::OrderQty orderQty;
-  FIX::Price price;
-  FIX::ClOrdID clOrdID;
-  FIX::Account account;
-
-  message.get( ordType );
-
-  if ( ordType != FIX::OrdType_LIMIT )
-    throw FIX::IncorrectTagValue( ordType.getField() );
-
-  message.get( symbol );
-  message.get( side );
-  message.get( orderQty );
-  message.get( price );
-  message.get( clOrdID );
-
-  FIX44::ExecutionReport executionReport = FIX44::ExecutionReport
-      ( FIX::OrderID( genOrderID() ),
-        FIX::ExecID( genExecID() ),
-        FIX::ExecType( FIX::ExecType_TRADE ),
-        FIX::OrdStatus( FIX::OrdStatus_FILLED ),
-        side,
-        FIX::LeavesQty( 0 ),
-        FIX::CumQty( orderQty ),
-        FIX::AvgPx( price ) );
-
-  executionReport.set( clOrdID );
-  executionReport.set( symbol );
-  executionReport.set( orderQty );
-  executionReport.set( FIX::LastQty( orderQty ) );
-  executionReport.set( FIX::LastPx( price ) );
-
-  if( message.isSet(account) )
-    executionReport.setField( message.get(account) );
-
-  try
-  {
-    FIX::Session::sendToTarget( executionReport, sessionID );
-  }
-  catch ( FIX::SessionNotFound& ) {}
-}
-
-void Application::onMessage( const FIX50::NewOrderSingle& message,
-                             const FIX::SessionID& sessionID )
-{
-  FIX::Symbol symbol;
-  FIX::Side side;
-  FIX::OrdType ordType;
-  FIX::OrderQty orderQty;
-  FIX::Price price;
-  FIX::ClOrdID clOrdID;
-  FIX::Account account;
-
-  message.get( ordType );
-
-  if ( ordType != FIX::OrdType_LIMIT )
-    throw FIX::IncorrectTagValue( ordType.getField() );
-
-  message.get( symbol );
-  message.get( side );
-  message.get( orderQty );
-  message.get( price );
-  message.get( clOrdID );
-
-  FIX50::ExecutionReport executionReport = FIX50::ExecutionReport
-      ( FIX::OrderID( genOrderID() ),
-        FIX::ExecID( genExecID() ),
-        FIX::ExecType( FIX::ExecType_TRADE ),
-        FIX::OrdStatus( FIX::OrdStatus_FILLED ),
-        side,
-        FIX::LeavesQty( 0 ),
-        FIX::CumQty( orderQty ) );
-
-  executionReport.set( clOrdID );
-  executionReport.set( symbol );
-  executionReport.set( orderQty );
-  executionReport.set( FIX::LastQty( orderQty ) );
-  executionReport.set( FIX::LastPx( price ) );
-  executionReport.set( FIX::AvgPx( price ) );
-
-  if( message.isSet(account) )
-    executionReport.setField( message.get(account) );
-
-  try
-  {
-    FIX::Session::sendToTarget( executionReport, sessionID );
-  }
-  catch ( FIX::SessionNotFound& ) {}
-}
+EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType )
+{ }
 
 int main( int argc, char** argv )
 {
-  if ( argc < 2 )
-  {
-    std::cout << "usage: " << argv[ 0 ]
-              << " FILE." << std::endl;
-    return 0;
-  }
-  std::string file = argv[ 1 ];
-#ifdef HAVE_SSL
-  std::string isSSL;
-  if (argc > 2)
-  {
-    isSSL.assign(argv[2]);
-  }
-#endif
-
   FIX::Acceptor * acceptor = 0;
   try
   {
-    FIX::SessionSettings settings( file );
+    FIX::SessionSettings settings( "" );
 
     Application application;
     FIX::FileStoreFactory storeFactory( settings );
     FIX::ScreenLogFactory logFactory( settings );
 
 #ifdef HAVE_SSL
-    if (isSSL.compare("SSL") == 0)
-      acceptor = new FIX::ThreadedSSLSocketAcceptor ( application, storeFactory, settings, logFactory );
-    else if (isSSL.compare("SSL-ST") == 0)
-      acceptor = new FIX::SSLSocketAcceptor ( application, storeFactory, settings, logFactory );
-    else
+    acceptor = new FIX::ThreadedSSLSocketAcceptor ( application, storeFactory, settings, logFactory );
+    acceptor = new FIX::SSLSocketAcceptor ( application, storeFactory, settings, logFactory );
 #endif
     acceptor = new FIX::SocketAcceptor ( application, storeFactory, settings, logFactory );
 
     acceptor->start();
     acceptor->stop();
-    delete acceptor;
-    return 0;
   }
-  catch ( std::exception & e )
+  catch( std::exception & )
   {
-    std::cout << e.what() << std::endl;
     delete acceptor;
-    return 1;
   }
+
+  return 0;
 }
