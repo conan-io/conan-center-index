984a447e9b1fa21af24fbca14a151a9ea4e8c243
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-06-09 09:40:37 -0300

    Use patches instead of programmatically replacing source files

--- recipes/quickfix/all/conanfile.py
+++ recipes/quickfix/all/conanfile.py
@@ -16,188 +16,22 @@ class QuickfixConan(ConanFile):
     default_options = {"ssl": False, "fPIC": True}
     generators = "cmake"
     file_pattern = re.compile(r'quickfix-(.*)')
+    exports_sources = "patches/**"
+
+    @property
+    def _source_subfolder(self):
+        return "quickfix"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-
         files = os.listdir()
-        quickfix_dir = list(filter(self.file_pattern.search, files))
+        match_dirs = list(filter(self.file_pattern.search, files))
+        extracted_dir = match_dirs[0]
+        os.rename(extracted_dir, self._source_subfolder)
 
-        shutil.move(quickfix_dir[0], "quickfix")
-
-        tools.replace_in_file("quickfix/CMakeLists.txt",
-                              "cmake_minimum_required(VERSION 3.0 FATAL_ERROR)",
-                              "cmake_minimum_required(VERSION 3.16 FATAL_ERROR)")
-
-        tools.replace_in_file("quickfix/CMakeLists.txt",
-                              "project(${quickfix_PROJECT_NAME} VERSION 0.1 LANGUAGES CXX C)",
-                              '''project(${quickfix_PROJECT_NAME} VERSION 0.1 LANGUAGES CXX C)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
-
-        tools.replace_in_file("quickfix/examples/executor/C++/CMakeLists.txt",
-                              "add_executable(${executor_NAME} Application.cpp executor.cpp ${applink_SOURCE})",
-                              "add_executable(${executor_NAME} Application.cpp executor.cpp)")
-
-        tools.replace_in_file("quickfix/examples/tradeclient/CMakeLists.txt",
-                              "add_executable(tradeclient Application.cpp tradeclient.cpp ${applink_SOURCE})",
-                              "add_executable(tradeclient Application.cpp tradeclient.cpp)")
-
-        tools.replace_in_file("quickfix/examples/ordermatch/CMakeLists.txt",
-                              "add_executable(ordermatch Application.cpp Market.cpp ordermatch.cpp ${applink_SOURCE})",
-                              "add_executable(ordermatch Application.cpp Market.cpp ordermatch.cpp)")
-
-        tools.replace_in_file("quickfix/CMakeLists.txt",
-                              "include(FindSharedPtr)",
-                              '''message(STATUS "Checking for nullptr.")
-unset(HAVE_NULLPTR CACHE)
-set(NULLPTR_FOUND FALSE)
-check_include_file_cxx(cstddef HAVE_STD_CSTDDEF_HEADER)
-if (HAVE_STD_CSTDDEF_HEADER)
-    include(CheckCXXSourceCompiles)
-    check_cxx_source_compiles("#include <cstddef>
-                               int main() {
-                                 std::nullptr_t null = nullptr;
-                                 return 0;
-                               }"
-            HAVE_NULLPTR)
-    if (HAVE_NULLPTR)
-        message(STATUS "Found support to nullptr.")
-        set(NULLPTR_FOUND TRUE)
-    endif(HAVE_NULLPTR)
-endif(HAVE_STD_CSTDDEF_HEADER)
-
-if (HAVE_NULLPTR)
-    message(STATUS "set HAVE_NULLPTR")
-    add_definitions("-DHAVE_NULLPTR=1")
-    file(APPEND ${CMAKE_SOURCE_DIR}/config.h
-     "#ifndef HAVE_NULLPTR\n"
-     "#define HAVE_NULLPTR\n"
-     "#endif\n" )
-endif (HAVE_NULLPTR)
-
-include(FindSharedPtr)
-''')
-
-        tools.replace_in_file("quickfix/CMakeLists.txt",
-                              "include(FindSharedPtr)",
-                              '''message(STATUS "Checking for use of enumeration in nested specifier.")
-unset(HAVE_ENUM_NESTED CACHE)
-set(ENUM_NESTED_FOUND FALSE)
-check_cxx_source_compiles("namespace NS {
-                           enum E {
-                           Item
-                           };
-                           }
-                           int main() {
-                             NS::E e = NS::E::Item;
-                           }"
-                          HAVE_ENUM_NESTED)
-if (HAVE_ENUM_NESTED)
-    message(STATUS "Found support to use of enumeration in nested specifier.")
-    set(ENUM_NESTED_FOUND TRUE)
-
-    message(STATUS "set HAVE_ENUM_NESTED")
-    add_definitions("-DHAVE_ENUM_NESTED=1")
-
-    file(APPEND ${CMAKE_SOURCE_DIR}/config.h
-     "#ifndef HAVE_ENUM_NESTED\n"
-     "#define HAVE_ENUM_NESTED\n"
-     "#endif\n" )
-endif (HAVE_ENUM_NESTED)
-
-include(FindSharedPtr)
-''')
-
-        tools.replace_in_file("quickfix/CMakeLists.txt",
-                              "include(FindSharedPtr)",
-                              '''message(STATUS "Checking support in-class initialization of non-static data member.")
-unset(HAVE_IN_CLASS_NON_STATIC CACHE)
-set(IN_CLASS_NON_STATIC_FOUND FALSE)
-check_cxx_source_compiles("namespace NS {
-                           class C {
-                           int f = 0;
-                           };
-                           }
-                           int main() {
-                             NS::C c;
-                           }"
-                          HAVE_IN_CLASS_NON_STATIC)
-if (HAVE_IN_CLASS_NON_STATIC)
-    message(STATUS "Found support to in-class initialization of non-static data member.")
-    set(IN_CLASS_NON_STATIC_FOUND TRUE)
-
-    message(STATUS "set HAVE_IN_CLASS_NON_STATIC")
-    add_definitions("-DHAVE_IN_CLASS_NON_STATIC=1")
-
-    file(APPEND ${CMAKE_SOURCE_DIR}/config.h
-     "#ifndef HAVE_IN_CLASS_NON_STATIC\n"
-     "#define HAVE_IN_CLASS_NON_STATIC\n"
-     "#endif\n" )
-endif (HAVE_IN_CLASS_NON_STATIC)
-
-include(FindSharedPtr)
-''')
-
-        tools.replace_in_file("quickfix/CMakeLists.txt",
-                              'add_definitions("-DHAVE_STD_SHARED_PTR=1")',
-                              '''
-add_definitions("-DHAVE_STD_SHARED_PTR=1")
-file(APPEND ${CMAKE_SOURCE_DIR}/config.h
- "#ifndef HAVE_STD_SHARED_PTR\n"
- "#define HAVE_STD_SHARED_PTR\n"
- "#endif\n" )
-''')
-
-        tools.replace_in_file("quickfix/CMakeLists.txt",
-                              'add_definitions("-DHAVE_STD_TR1_SHARED_PTR=1")',
-                              '''
-add_definitions("-DHAVE_STD_TR1_SHARED_PTR=1")
-file(APPEND ${CMAKE_SOURCE_DIR}/config.h
- "#ifndef HAVE_STD_TR1_SHARED_PTR\n"
- "#define HAVE_STD_TR1_SHARED_PTR\n"
- "#endif\n" )
-''')
-
-        tools.replace_in_file("quickfix/CMakeLists.txt",
-                              'add_definitions("-DHAVE_STD_TR1_SHARED_PTR_FROM_TR1_MEMORY_HEADER=1")',
-                              '''
-add_definitions("-DHAVE_STD_TR1_SHARED_PTR_FROM_TR1_MEMORY_HEADER=1")
-file(APPEND ${CMAKE_SOURCE_DIR}/config.h
- "#ifndef HAVE_STD_TR1_SHARED_PTR_FROM_TR1_MEMORY_HEADER\n"
- "#define HAVE_STD_TR1_SHARED_PTR_FROM_TR1_MEMORY_HEADER\n"
- "#endif\n" )
-''')
-
-        tools.replace_in_file("quickfix/CMakeLists.txt",
-                              'add_definitions("-DHAVE_STD_UNIQUE_PTR=1")',
-                              '''
-add_definitions("-DHAVE_STD_UNIQUE_PTR=1")
-file(APPEND ${CMAKE_SOURCE_DIR}/config.h
- "#ifndef HAVE_STD_UNIQUE_PTR\n"
- "#define HAVE_STD_UNIQUE_PTR\n"
- "#endif\n" )
-''')
-
-        tools.replace_in_file("quickfix/src/CMakeLists.txt",
-                              'if( WIN32 OR ${CMAKE_SYSTEM_NAME} STREQUAL "Linux" )',
-                              '''
-if ( FALSE )
-''')
-
-        tools.replace_in_file("quickfix/src/C++/Utility.h",
-                              "#elif defined(HAVE_STD_TR1_SHARED_PTR)",
-                              "#elif defined(HAVE_STD_TR1_SHARED_PTR_FROM_TR1_MEMORY_HEADER)")
-
-        os.makedirs("quickfix/include")
-        shutil.copyfile("quickfix/src/C++/Except.h", "quickfix/include/Except.h")
-
-        if self.options.ssl and not self.options["openssl"].shared:
-            tools.replace_in_file("quickfix/src/C++/CMakeLists.txt",
-                                  "  target_link_libraries(${PROJECT_NAME} ${OPENSSL_LIBRARIES} ${MYSQL_CLIENT_LIBS} "
-                                  "${PostgreSQL_LIBRARIES} ws2_32)",
-                                  "  target_link_libraries(${PROJECT_NAME} ${OPENSSL_LIBRARIES} ${MYSQL_CLIENT_LIBS} "
-                                  "${PostgreSQL_LIBRARIES} ws2_32 crypt32)")
+        os.makedirs(f"{self._source_subfolder}/include")
+        shutil.copyfile(f"{self._source_subfolder}/src/C++/Except.h",
+                        f"{self._source_subfolder}/include/Except.h")
 
     def requirements(self):
         if self.options.ssl:
@@ -208,15 +42,16 @@ if ( FALSE )
             del self.options.fPIC
 
     def build(self):
+        self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build(target="quickfix")
 
     def package(self):
         cmake = self._configure_cmake()
         cmake.install()
-        self.copy("config.h", dst="include", src="quickfix")
-        self.copy("Except.h", dst="include", src="quickfix/src/C++")
-        self.copy("LICENSE", dst="licenses", src="quickfix")
+        self.copy("config.h", dst="include", src=self._source_subfolder)
+        self.copy("Except.h", dst="include", src=f"{self._source_subfolder}/src/C++")
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         shutil.rmtree(f"{self.package_folder}{os.sep}share")
 
     def package_info(self):
@@ -231,5 +66,9 @@ if ( FALSE )
         if self.options.ssl:
             cmake.definitions["HAVE_SSL"] = "ON"
 
-        cmake.configure(source_folder="quickfix")
+        cmake.configure(source_folder=self._source_subfolder)
         return cmake
+
+    def _patch_sources(self):
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
