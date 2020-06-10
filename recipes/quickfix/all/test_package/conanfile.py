f8dd89c3ae0a8b273ee08c521f6e96fa45a0babb
Uilian Ries <uilianries@gmail.com>
2020-06-10 11:32:23 -0300

    Simplify quickfix recipe
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/quickfix/all/test_package/conanfile.py
+++ recipes/quickfix/all/test_package/conanfile.py
@@ -5,27 +5,14 @@ from conans import ConanFile, CMake, tools
 
 class QuickfixTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    options = {"ssl": [True, False], "fPIC": [True, False]}
-    default_options = "ssl=False", "fPIC=True"
     generators = "cmake"
 
-    def configure(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
-        else:
-            self.options["quickfix"].fPIC = self.options.fPIC
-
-        self.options["quickfix"].ssl = self.options.ssl
-
     def build(self):
-        self.source()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self):
-            program = "executor"
-
-            self.run(f".{os.sep}{program}", run_environment=True,
-                     cwd=f"{self.build_folder}{os.sep}bin")
+            bin_path = os.path.join("bin", "executor")
+            self.run(bin_path, run_environment=True)
