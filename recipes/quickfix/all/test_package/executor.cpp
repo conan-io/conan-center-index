a012c7195c6f12882a65b2074e51f8a4e8bc69d6
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-06-10 03:41:21 -0300

    Further simplify test_package

--- recipes/quickfix/all/test_package/executor.cpp
+++ recipes/quickfix/all/test_package/executor.cpp
@@ -3,10 +3,6 @@
 #include "quickfix/FileStore.h"
 #include "quickfix/Application.h"
 #include "quickfix/SocketAcceptor.h"
-#ifdef HAVE_SSL
-  #include "quickfix/ThreadedSSLSocketAcceptor.h"
-  #include "quickfix/SSLSocketAcceptor.h"
-#endif
 #include "quickfix/Log.h"
 #include "quickfix/SessionSettings.h"
 
@@ -15,60 +11,33 @@ class Application
 {
 public:
   // Application overloads
-  void onCreate( const FIX::SessionID& );
-  void onLogon( const FIX::SessionID& sessionID );
-  void onLogout( const FIX::SessionID& sessionID );
-  void toAdmin( FIX::Message&, const FIX::SessionID& );
+  void onCreate( const FIX::SessionID& ) {}
+  void onLogon( const FIX::SessionID& sessionID ) {}
+  void onLogout( const FIX::SessionID& sessionID ) {}
+  void toAdmin( FIX::Message&, const FIX::SessionID& ) {}
   void toApp( FIX::Message&, const FIX::SessionID& )
-  EXCEPT( FIX::DoNotSend );
+  EXCEPT( FIX::DoNotSend ) {};
   void fromAdmin( const FIX::Message&, const FIX::SessionID& )
-  EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon );
+  EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon )
+  {}
   void fromApp( const FIX::Message& message, const FIX::SessionID& sessionID )
-  EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType );
+  EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType )
+  {}
 };
-
-void Application::onCreate( const FIX::SessionID& sessionID ) {}
-void Application::onLogon( const FIX::SessionID& sessionID ) {}
-void Application::onLogout( const FIX::SessionID& sessionID ) {}
-void Application::toAdmin( FIX::Message& message,
-                           const FIX::SessionID& sessionID ) {}
-void Application::toApp( FIX::Message& message,
-                         const FIX::SessionID& sessionID )
-EXCEPT( FIX::DoNotSend ) {}
-
-void Application::fromAdmin( const FIX::Message& message,
-                             const FIX::SessionID& sessionID )
-EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon ) {}
-
-void Application::fromApp( const FIX::Message& message,
-                           const FIX::SessionID& sessionID )
-EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType )
-{ }
 
 int main( int argc, char** argv )
 {
-  FIX::Acceptor * acceptor = 0;
   try
   {
-    FIX::SessionSettings settings( "" );
-
     Application application;
+    FIX::SessionSettings settings( "" );
     FIX::FileStoreFactory storeFactory( settings );
     FIX::ScreenLogFactory logFactory( settings );
 
-#ifdef HAVE_SSL
-    acceptor = new FIX::ThreadedSSLSocketAcceptor ( application, storeFactory, settings, logFactory );
-    acceptor = new FIX::SSLSocketAcceptor ( application, storeFactory, settings, logFactory );
-#endif
-    acceptor = new FIX::SocketAcceptor ( application, storeFactory, settings, logFactory );
-
-    acceptor->start();
-    acceptor->stop();
+    FIX::SocketAcceptor acceptor( application, storeFactory, settings, logFactory );
   }
   catch( std::exception & )
-  {
-    delete acceptor;
-  }
+  { }
 
   return 0;
 }
