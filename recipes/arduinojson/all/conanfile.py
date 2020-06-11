b112665f3b6f7e05300fbd161fa8424021b638df
danimtb <danimanzaneque@gmail.com>
2020-06-11 14:36:31 +0200

    Add arduinojson/6.15.2

---
+++ recipes/arduinojson/all/conanfile.py
@@ -0,0 +1,23 @@
+from conans import ConanFile, tools
+import os
+
+
+class ArduinojsonConan(ConanFile):
+    name = "arduinojson"
+    license = "The MIT License (MIT)"
+    description = "C++ JSON library for IoT. Simple and efficient."
+    homepage = "https://github.com/bblanchon/ArduinoJson"
+    url = "https://github.com/conan-io/conan-center-index"
+    no_copy_source = True
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("ArduinoJson", "sources")
+
+    def package(self):
+        self.copy("*LICENSE*", dst="licenses", src="sources")
+        self.copy("*.h", dst="include", src="sources")
+        self.copy("*.hpp", dst="include", src="sources")
+
+    def package_info(self):
+        self.cpp_info.includedirs.append(os.path.join("include", "src"))
