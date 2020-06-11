944ff8bec94f921254a23982a4f74863960b6c3e
danimtb <danimanzaneque@gmail.com>
2020-06-11 16:39:31 +0200

    Add box2d/2.3.1

---
+++ recipes/box2d/all/test_package/conanfile.py
@@ -0,0 +1,28 @@
+import os
+from conans import ConanFile, CMake, tools, RunEnvironment
+
+
+class Box2DTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch", "cppstd"
+    generators = "cmake"
+    options = {"kk": [True, False]}
+    default_options = "kk=True"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy(pattern="*.dll", src="bin", dst="bin")
+        self.copy(pattern="*.dylib", src="lib", dst="bin")
+
+    def test(self):
+        with tools.environment_append(RunEnvironment(self).vars):
+            bin_path = os.path.join("bin", "test_package")
+            if self.settings.os == "Windows":
+                self.run(bin_path)
+            elif self.settings.os == "Macos":
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+            else:
+                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
