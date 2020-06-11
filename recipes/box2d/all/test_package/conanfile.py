03e7a5c396fea2727b84700a1e92d6ee4bbe8726
danimtb <danimanzaneque@gmail.com>
2020-06-11 16:56:16 +0200

    remove imports() - of course!

--- recipes/box2d/all/test_package/conanfile.py
+++ recipes/box2d/all/test_package/conanfile.py
@@ -11,10 +11,6 @@ class Box2DTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy(pattern="*.dll", src="bin", dst="bin")
-        self.copy(pattern="*.dylib", src="lib", dst="bin")
-
     def test(self):
         bin_path = os.path.join("bin", "test_package")
         self.run(bin_path, run_environment=True)
