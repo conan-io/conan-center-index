e5b8285bb59afd7395755e1b2506cc6ae834e31d
Dmitry Bely <dmitry.bely@gmail.com>
2020-06-12 22:05:45 +0300

    Fix protobuf test_package

--- recipes/protobuf/all/test_package/conanfile.py
+++ recipes/protobuf/all/test_package/conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "build_type", "arch", "os_build", "arch_build"
     generators = "cmake"
 
     def build(self):
@@ -12,6 +12,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if self.settings.os == "Windows" or not tools.cross_building(self.settings):
             bin_path = os.path.abspath(os.path.join("bin", "test_package"))
             self.run(bin_path, run_environment=True)
