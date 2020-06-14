9ed170fce301bb77029f36256c66ead779549acc
Sam Mosleh <sam.mosleh@ut.ac.ir>
2020-06-14 23:26:35 +0430

    Printing version in PackageTest

--- recipes/librdkafka/all/test_package/test.cpp
+++ recipes/librdkafka/all/test_package/test.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char const *argv[]) {
   rd_kafka_conf_t *conf = rd_kafka_conf_new();
   std::cout << std::endl
-            << "----------------->Tests are done for now.<---------------------" << std::endl
-            << "///////////////////////////////////////////////////////////////" << std::endl;
+            << "----------------->Tests are done.<---------------------" << std::endl
+	    << "Using version " << rd_kafka_version_str() << std::endl
+            << "///////////////////////////////////////////////////////" << std::endl;
   return 0;
 }
