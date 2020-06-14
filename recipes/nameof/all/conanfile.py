28735f2ebb0af1b698ecdf4f6489b460d1df7328
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-06-14 22:21:58 +0200

    feat: add nameof

---
+++ recipes/nameof/all/conanfile.py
@@ -0,0 +1,57 @@
+import os
+from conans import CMake, ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+from conans.tools import Version
+
+
+class NameofConan(ConanFile):
+    name = "nameof"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/Neargye/nameof"
+    license = "MIT"
+    description = "Header-only C++17 library provides nameof macros and functions to simply obtain the name of a variable, type, function, macro, and enum."
+    topics = (
+        "conan",
+        "nameof",
+        "cplusplus",
+        "enum-to-string",
+        "serialization",
+        "reflection",
+        "header-only",
+        "compile-time"
+    )
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def supported_compiler(self):
+        compiler = str(self.settings.compiler)
+        version = tools.Version(self.settings.compiler.version)
+        if compiler == "Visual Studio" and version >= "15":
+            return True
+        if compiler == "gcc" and version >= "7":
+            return True
+        if compiler == "clang" and version >= "5":
+            return True
+        if compiler == "apple-clang" and version >= "9":
+            return True
+        return False
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def configure(self):
+        if not self.supported_compiler:
+            raise ConanInvalidConfiguration("nameof: Unsupported compiler (https://github.com/Neargye/nameof#compiler-compatibility).")
+
+    def package(self):
+        self.copy("include/*")
+        self.copy("LICENSE", dst="licenses")
+
+    def package_id(self):
+        self.info.header_only()
