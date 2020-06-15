41aca6ae60f3045edeb41cd00821a444fe7a5166
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-06-15 17:17:05 -0300

    Refactor config_options() along the divide between shared_ptr and unique_ptr

--- recipes/quickfix/all/conanfile.py
+++ recipes/quickfix/all/conanfile.py
@@ -36,6 +36,51 @@ class QuickfixConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    @property
+    def _unique_ptr(self):
+        # auto_ptr is fully removed in C++17.
+        if hasattr(self.settings.compiler, "cppstd"):
+            cppstd = str(self.settings.compiler.cppstd)
+            if cppstd.find("17") != -1 or cppstd.find("20") != -1:
+                return "unique"
+
+        # gcc default standard for version 5.5 is gnu++98. In the 6 series, it
+        # changed to gnu++14.
+        version = Version(str(self.settings.compiler.version))
+        if self.settings.compiler == "gcc" and (
+                str(self.settings.compiler.cppstd).find("98") != -1 or
+                (version <= "5.5" and self.settings.compiler.cppstd == "None")):
+            return "auto"
+
+        if self.settings.compiler == "clang" and (
+                (version <= "5.0" and self.settings.compiler.libcxx == "libstdc++" and
+                 self.settings.compiler.cppstd == "None")):
+            return "auto"
+
+        return self.default_options["unique_ptr"]
+
+    @property
+    def _shared_ptr(self):
+        # libc++ doesn't know of tr1.
+        if str(self.settings.compiler).find("clang") != -1:
+            if self.settings.compiler.libcxx == "libc++":
+                return "std"
+
+        # gcc default standard for version 5.5 is gnu++98. In the 6 series, it
+        # changed to gnu++11.
+        version = Version(str(self.settings.compiler.version))
+        if self.settings.compiler == "gcc" and (
+                str(self.settings.compiler.cppstd).find("98") != -1 or
+                (version <= "5.5" and self.settings.compiler.cppstd == "None")):
+            return "tr1"
+
+        if self.settings.compiler == "clang" and (
+                (version <= "5.0" and self.settings.compiler.libcxx == "libstdc++" and
+                 self.settings.compiler.cppstd == "None")):
+            return "tr1"
+
+        return self.default_options["shared_ptr"]
+
     def _configure_cmake(self):
         if not self._cmake:
             self._cmake = CMake(self)
@@ -62,31 +107,12 @@ class QuickfixConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-        if hasattr(self.settings.compiler, "cppstd"):
-            cppstd = str(self.settings.compiler.cppstd)
-            if cppstd.find("17") != -1 or cppstd.find("20") != -1:
-                self.options.unique_ptr = "unique"
-            elif cppstd.find("98") != -1:
-                self.options.unique_ptr = "auto"
+        self.options.unique_ptr = self._unique_ptr
 
-        version = Version(str(self.settings.compiler.version))
-        if self.settings.compiler == "Visual Studio" and version <= "10":
-            self.options.shared_ptr = "tr1"
-
-        if self.settings.compiler == "gcc" and (
-                str(self.settings.compiler.cppstd).find("98") != -1 or
-                (version < "6.0" and self.settings.compiler.cppstd == "None")):
-            self.options.shared_ptr = "tr1"
-            self.options.unique_ptr = "auto"
-
-        if self.settings.compiler == "clang" and (
-                (version <= "5.0" and self.settings.compiler.cppstd == "None")):
-            self.options.shared_ptr = "tr1"
-            self.options.unique_ptr = "auto"
-
-        if str(self.settings.compiler).find("clang") != -1:
-            if self.settings.compiler.libcxx == "libc++":
-                self.options.shared_ptr = "std"
+        # It's reasonable to suppose that whenever there's support to
+        # std::unique_ptr, there's also to std::shared_ptr. Otherwise
+        # we need to obtain it from the settings.
+        self.options.shared_ptr = "std" if self.options.unique_ptr == "unique" else self._shared_ptr
 
     def configure(self):
         if self.settings.os == "Windows" and self.options.shared:
