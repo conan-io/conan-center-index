7da2fb750a5a91c8e9e243b8b4cb70f327ede374
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-06-15 23:44:32 +0200

    Apply suggestions from code review
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/dbg-macro/all/conanfile.py
+++ recipes/dbg-macro/all/conanfile.py
@@ -1,7 +1,6 @@
 import os
-from conans import CMake, ConanFile, tools
+from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
-from conans.tools import Version
 
 
 class DbgMacroConan(ConanFile):
