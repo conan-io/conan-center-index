5457be668a7220961d7d9dc0ed7976d5c6b41b8c
Chris Mc <prince.chrismc@gmail.com>
2020-06-16 08:42:41 -0400

    chore: hooks

--- recipes/string-view-lite/all/conanfile.py
+++ recipes/string-view-lite/all/conanfile.py
@@ -1,22 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import os
 from conans import ConanFile, tools
 
 
 class StringViewLite(ConanFile):
     name = "string-view-lite"
-    
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/martinmoene/string-view-lite"
     description = "string-view lite - A C++17-like string_view for C++98, C++11 and later in a single-file header-only library"
-    author = "Martin Moene"
     topics = ("conan", "cpp98", "cpp11", "cpp14", "cpp17", "string-view", "string-view-implementations")
     license = "BSL-1.0"
     no_copy_source = True
-    
-    _source_subfolder = "source_subfolder"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
