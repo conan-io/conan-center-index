7fce61451f0d311f83cbe63ff95704a463c17ef0
intelligide <intelligide@hotmail.fr>
2020-06-17 13:00:57 -0400

    mattiasgustavsson-libs: Fix

--- recipes/mattiasgustavsson-libs/all/conanfile.py
+++ recipes/mattiasgustavsson-libs/all/conanfile.py
@@ -9,29 +9,31 @@ class MattiasgustavssonLibsConan(ConanFile):
     description = "Single-file public domain libraries for C/C++"
     homepage = "https://github.com/mattiasgustavsson/libs"
     url = "https://github.com/conan-io/conan-center-index"
-    license = ("Public domain", "MIT")
+    license = ("Unlicense", "MIT")
     topics = ("utilities", "mattiasgustavsson", "libs")
     no_copy_source = True
 
     @property
     def _source_subfolder(self):
-        return "source_subfolder"
+        return os.path.join(self.source_folder, "source_subfolder")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = glob.glob('libs-*/')[0]
         os.rename(extracted_dir, self._source_subfolder)
 
+    def _extract_licenses(self):
+        header = tools.load(os.path.join(self._source_subfolder, "thread.h"))
+        mit_content = header[header.find("ALTERNATIVE A - "):header.find("ALTERNATIVE B -")]
+        tools.save("LICENSE_MIT", mit_content)
+        unlicense_content = header[header.find("ALTERNATIVE B - "):header.rfind("*/", 1)]
+        tools.save("LICENSE_UNLICENSE", unlicense_content)
+
     def package(self):
         self.copy(pattern="*.h", dst="include", src=self._source_subfolder)
-
-        with open(os.path.join(self.source_folder, self._source_subfolder, "thread.h")) as f:
-            content_lines = f.readlines()
-        license_content = []
-        for i in range(1449, 1498):
-            license_content.append(content_lines[i][:-1])
-
-        tools.save(os.path.join(self.package_folder, "licenses", "license.txt"), "\n".join(license_content))
+        self._extract_licenses()
+        self.copy("LICENSE_MIT", dst="licenses")
+        self.copy("LICENSE_UNLICENSE", dst="licenses")
 
     def package_id(self):
         self.info.header_only()
