0c0b0a5ebf13460c738d7a5261ee521098869e3d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-06-17 13:49:29 +0200

    c-blosc: remove fPIC option if shared

--- recipes/c-blosc/all/conanfile.py
+++ recipes/c-blosc/all/conanfile.py
@@ -44,6 +44,8 @@ class CbloscConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.cppstd
         del self.settings.compiler.libcxx
 
