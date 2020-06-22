dd4f234e623d7193769932c47bf83bc0b386b804
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-06-22 20:48:51 +0200

    change windows includes

--- recipes/mdnsd/all/test_package/test_package.cpp
+++ recipes/mdnsd/all/test_package/test_package.cpp
@@ -13,8 +13,9 @@
 
 #ifdef _WIN32
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include "windows.h"
-#include <winsock2.h>
+#include <io.h>
+#include <windows.h>
+#include <winsock.h>
 #include <ws2tcpip.h>
 
 static int my_inet_pton(int af, const char *src, void *dst) {
