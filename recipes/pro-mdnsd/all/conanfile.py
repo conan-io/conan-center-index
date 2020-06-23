f3dd6901b84051d9c3589abc566b67cb71b4eb82
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-06-23 11:15:48 +0200

    change license identifier to use SPDX definition

--- recipes/pro-mdnsd/all/conanfile.py
+++ recipes/pro-mdnsd/all/conanfile.py
@@ -7,7 +7,7 @@ from conans.model.version import Version
 
 class mdnsdConan(ConanFile):
     name = "pro-mdnsd"
-    license = "BSD 3-Clause"
+    license = "BSD-3-Clause"
     exports_sources = [
         "CMakeLists.txt",
         "patches/**"
