1049304cc65478fe261e71dff11356664749610f
Mahe Thomas <thomas.mahe@nxp.com>
2020-06-24 10:14:32 +0200

    libftdi: recipes moved to 1.x sub-folder
    To avoid conflict with libftdi 0.x releases

--- recipes/libftdi/1.x/conanfile.py
+++ recipes/libftdi/1.x/conanfile.py
@@ -3,32 +3,36 @@ from conans import ConanFile, CMake, tools
 
 
 class LibFtdiConan(ConanFile):
-    name = "libftdi1"
+    name = "libftdi"
     description = "A library to talk to FTDI chips"
-    license = ["MIT", "BSD-3-Clause"]
+    license = ["GNU LGPL v2.1"]
     topics = ("conan", "libftdi1")
-    homepage = "http://developer.intra2net.com/git/?p=libftdi;a=summary"
+    homepage = "https://www.intra2net.com/en/developer/libftdi/"
     url = "https://github.com/conan-io/conan-center-index"
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake", "cmake_find_package"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    _source_subfolder = "source_subfolder"
-
     _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
+        extracted_dir = "libftdi1-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
     def requirements(self):
         self.requires("libusb/1.0.23")
         self.requires("libconfuse/3.2.2")
-        self.requires("boost/1.72.0")
+        self.requires("boost/1.73.0")
 
     def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
         self._cmake = CMake(self)
         self._cmake.definitions["BUILD_TESTS"] = False
         self._cmake.configure(source_dir=self._source_subfolder)
