0eeda32930034a839ee50c2c0c0f7a663a4b18b1
Croydon <git@cr0ydon.com>
2020-06-26 03:38:01 +0200

    openssl/3.x.x.: update test_package to never  use find_package()
    Several reasons for this decision:
    
    1) No matter the value of the paramter use_find_package CMake did always use a FindOpenSSL.cmake file
    
    2) On my machine that meant that CMake used a FindOpenSSL.cmake file from my Python installation, which can't yet handle version 3.0.0 due to a regex error
    
    3) Even when I addded the Conan cmake_find_package generator, it did not work (more cpp_info work needed, maybe?)
    
    4) Even when 3) would be fixed, it would still always use the FindOpenSSL.cmake approach for some reason (I guess), so the entire use_find_package switch is pointless

--- recipes/openssl/3.x.x/test_package/conanfile.py
+++ recipes/openssl/3.x.x/test_package/conanfile.py
@@ -1,34 +1,26 @@
 from conans import CMake, tools, ConanFile
-from conans.tools import Version
 import os
 
 
-class DefaultNameConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
 
-    def _build_cmake(self, use_find_package):
+    def build(self):
         cmake = CMake(self)
 
         if self.settings.os == "Android":
             cmake.definitions["CONAN_LIBCXX"] = ""
-        openssl_version = Version(self.deps_cpp_info["openssl"].version)
-        if openssl_version.major == "1" and openssl_version.minor == "1":
-            cmake.definitions["OPENSSL_WITH_ZLIB"] = False
-        else:
-            cmake.definitions["OPENSSL_WITH_ZLIB"] = not self.options["openssl"].no_zlib
-        cmake.definitions["USE_FIND_PACKAGE"] = use_find_package
+
+        cmake.definitions["OPENSSL_WITH_ZLIB"] = self.options["openssl"].zlib
         cmake.definitions["OPENSSL_ROOT_DIR"] = self.deps_cpp_info["openssl"].rootpath
         cmake.definitions["OPENSSL_USE_STATIC_LIBS"] = not self.options["openssl"].shared
         if self.settings.compiler == 'Visual Studio':
-            cmake.definitions["OPENSSL_MSVC_STATIC_RT"] = 'MT' in str(self.settings.compiler.runtime)
+            cmake.definitions["OPENSSL_MSVC_STATIC_RT"] = 'MT' in str(
+                self.settings.compiler.runtime)
 
         cmake.configure()
         cmake.build()
-
-    def build(self):
-        self._build_cmake(use_find_package=True)
-        self._build_cmake(use_find_package=False)
 
     def test(self):
         if not tools.cross_building(self.settings):
