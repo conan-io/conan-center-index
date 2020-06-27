9a7f10ac5b4f3ef2a7291ab554823959772637f6
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-06-27 02:23:47 +0200

    pkgconf: update to 1.7.3 + fix + test autotools integration

--- recipes/pkgconf/all/test_package/conanfile.py
+++ recipes/pkgconf/all/test_package/conanfile.py
@@ -1,13 +1,27 @@
-from conans import CMake, ConanFile, tools
+from conans import AutoToolsBuildEnvironment, CMake, ConanFile, tools
 from conans.errors import ConanException
 import os
+import shutil
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake"
 
+    def build_requirements(self):
+        self.build_requires("automake/1.16.2")
+        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH") \
+                and tools.os_info.detect_windows_subsystem() != "msys2":
+            self.build_requires("msys2/20190524")
+
     def build(self):
+        # Test pkg.m4 integration into automake
+        shutil.copy(os.path.join(self.source_folder, "configure.ac"),
+                    os.path.join(self.build_folder, "configure.ac"))
+        self.run("autoreconf -fiv", run_environment=True, win_bash=tools.os_info.is_windows)
+        autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        autotools.configure()
+
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
