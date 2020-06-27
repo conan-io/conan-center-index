ed60ce7a4a5ee50f36dbd4d84914797509013d8e
d70-t <tobi@die70.de>
2020-06-27 19:17:17 +0200

    netcdf: remove useless fPIC option when building shared
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/netcdf/all/conanfile.py
+++ recipes/netcdf/all/conanfile.py
@@ -43,6 +43,8 @@ class NetcdfConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
