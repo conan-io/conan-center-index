db4eb9a9b169d3f366efb2c5c07e83659289fa44
Andrew Marshall <planetmarshall@users.noreply.github.com>
2020-06-29 22:58:39 +0100

    Update recipes/pcl/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/pcl/all/conanfile.py
+++ recipes/pcl/all/conanfile.py
@@ -50,7 +50,26 @@ class PclConanRecipe(ConanFile):
     def configure(self):
         if tools.msvs_toolset(self) == "v140":
             raise ConanInvalidConfiguration("Unsupported Visual Studio Compiler or Toolset")
-        tools.check_min_cppstd(self, "14")
+        minimal_cpp_standard = "14"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
+        minimal_version = {
+            "gcc": "5",
+            "clang": "3.4",
+            "apple-clang": "10",
+            "Visual Studio": "14"
+        }
+        compiler = str(self.settings.compiler)
+        if compiler not in minimal_version:
+            self.output.warn(
+                "%s recipe lacks information about the %s compiler standard version support" % (self.name, compiler))
+            self.output.warn(
+                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+            return
+        version = tools.Version(self.settings.compiler.version)
+        if version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+
 
     def _configure_cmake(self):
         cmake_definitions = {
