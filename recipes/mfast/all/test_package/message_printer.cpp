8bcce2a0a3e78c7d1eb47d44c40ff4d80a579776
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-07-01 19:45:35 -0300

    Simplify test package further

--- recipes/mfast/all/test_package/message_printer.cpp
+++ recipes/mfast/all/test_package/message_printer.cpp
@@ -1,27 +1,9 @@
-#include "MDRefreshSample.h"
 #include <iostream>
-#include <iomanip>
-#include <map>
-#include <vector>
-#include "message_printer.h"
+#include "Test.h"
 
 int main()
 {
-  MDRefreshSample::MDRefreshSample message;
-  MDRefreshSample::MDRefreshSample_mref ref = message.ref();
-  ref.set_MDEntries().resize(1);
-  MDRefreshSample::MDRefreshSample_mref::MDEntries_element_mref entry(ref.set_MDEntries()[0]);
-  entry.set_MDUpdateAction().as(1);
-  const char* str = "abcd";
-  entry.set_MDEntryType().as(str);
-
-  entry.set_Symbol().as("AAPL");
-  entry.set_SecurityType().as("Stock");
-  entry.set_MDEntryPx().as(1, 2);
-  entry.set_MDEntrySize().as(3,4);
-  entry.set_NumberOfOrders().as(100);
-
-  message_printer printer(std::cout);
-  ref.accept_accessor(printer);
-  return 0;
+  Test::Test message;
+  Test::Test_mref ref = message.ref();
+  std:: cout << ref.get_String().c_str() << "\n";
 }
