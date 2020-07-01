ebf711a3f11596018fb5d35c71029cac8bfc100d
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-07-01 04:55:05 +0200

    boostdep: even lower required boost version

--- recipes/boostdep/all/conanfile.py
+++ recipes/boostdep/all/conanfile.py
@@ -25,7 +25,7 @@ class BoostDepConan(ConanFile):
         return "build_subfolder"
 
     def requirements(self):
-        self.requires("boost/1.72.0")
+        self.requires("boost/1.70.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version][0])
