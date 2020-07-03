5205cd47c32dd137ea3b4cae4b3451f2f06a2315
Winfried Dobbe <winfried.dobbe@dynniq.com>
2020-07-03 12:07:39 +0200

    Issue #2102 : move m4 requirement from automake to autoconf

--- recipes/autoconf/all/conanfile.py
+++ recipes/autoconf/all/conanfile.py
@@ -21,6 +21,9 @@ class AutoconfConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("{}-{}".format(self.name, self.version), self._source_subfolder)
 
+    def requirements(self):
+        self.requires("m4/1.4.18")
+
     def build_requirements(self):
         if tools.os_info.is_windows and "CONAN_BASH_PATH" not in os.environ:
             self.build_requires("msys2/20190524")
--- recipes/automake/all/conanfile.py
+++ recipes/automake/all/conanfile.py
@@ -29,7 +29,6 @@ class AutomakeConan(ConanFile):
 
     def requirements(self):
         self.requires("autoconf/2.69")
-        self.requires("m4/1.4.18")
         # automake requires perl-Thread-Queue package
 
     def build_requirements(self):
