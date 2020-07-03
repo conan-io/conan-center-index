7c21a79f7acbd40c4db39d4aef5d7f89fff751c9
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-07-03 16:35:19 +0200

    pkgconf: AUTOMAKE_CONAN_INCLUDES needs to be a unix path

--- recipes/pkgconf/all/conanfile.py
+++ recipes/pkgconf/all/conanfile.py
@@ -104,7 +104,7 @@ class PkgConfConan(ConanFile):
         self.output.info("Appending PATH env var: {}".format(bindir))
         self.env_info.PATH.append(bindir)
 
-        pkg_config = os.path.join(bindir, "pkgconf").replace("\\", "/")
+        pkg_config = tools.unix_path(os.path.join(bindir, "pkgconf"))
         self.output.info("Setting PKG_CONFIG env var: {}".format(pkg_config))
         self.env_info.PKG_CONFIG = pkg_config
 
