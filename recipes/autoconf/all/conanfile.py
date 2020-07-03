5d9733ae9703d3ca797d918379080ee68a9bed55
Winfried Dobbe <winfried.dobbe@dynniq.com>
2020-07-03 17:52:37 +0200

    Issue #2102 : Do not add m4 as requirement on Windows because the Conan autoconf package currently fails with the Conan m4 package on Windows

--- recipes/autoconf/all/conanfile.py
+++ recipes/autoconf/all/conanfile.py
@@ -22,7 +22,9 @@ class AutoconfConan(ConanFile):
         os.rename("{}-{}".format(self.name, self.version), self._source_subfolder)
 
     def requirements(self):
-        self.requires("m4/1.4.18")
+        if not tools.os_info.is_windows:
+            #the Conan m4 package currently doesn't work correctly with autoconf
+            self.requires("m4/1.4.18")
 
     def build_requirements(self):
         if tools.os_info.is_windows and "CONAN_BASH_PATH" not in os.environ:
