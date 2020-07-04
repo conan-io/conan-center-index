19b460e7299443e1b041212851b1feef4aeb1029
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-07-04 11:45:19 +0200

    meson: install program in test_package

--- recipes/meson/all/test_package/conanfile.py
+++ recipes/meson/all/test_package/conanfile.py
@@ -6,14 +6,21 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "pkg_config"
 
+    @property
+    def _prefix(self):
+        return os.path.join(self.build_folder, "prefix")
+
     def build(self):
+        self.package_folder = self._prefix
         meson = Meson(self)
         meson.configure(build_folder="build")
         meson.build()
+        meson.install()
 
     def test(self):
-        bin_path = os.path.join("build", "test_package")
-        self.run(bin_path, run_environment=True)
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join(self._prefix, "bin", "test_package")
+            self.run(bin_path, run_environment=True)
 
         self.run("meson --version")
 
