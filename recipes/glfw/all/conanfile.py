c887d87847043e7df15565be0a5e396aaeac5424
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-07-09 09:46:38 +0200

    glfw: delete fPIC option if shared

--- recipes/glfw/all/conanfile.py
+++ recipes/glfw/all/conanfile.py
@@ -42,6 +42,8 @@ class GlfwConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
