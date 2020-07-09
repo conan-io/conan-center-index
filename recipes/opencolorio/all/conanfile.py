7deb83e3a5f83472b265196496ff76546ca8be97
Piotr Podsiadly <ppodsiadly@mykolab.com>
2020-07-09 07:18:53 +0200

    opencolorio: set PATH in package_info()

--- recipes/opencolorio/all/conanfile.py
+++ recipes/opencolorio/all/conanfile.py
@@ -92,3 +92,7 @@ class OpenEXRConan(ConanFile):
 
         if not self.options.shared:
             self.cpp_info.defines.append("OpenColorIO_STATIC")
+
+        bin_path = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH env var with: {}".format(bin_path))
+        self.env_info.PATH.append(bin_path)
