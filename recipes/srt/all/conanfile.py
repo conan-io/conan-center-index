69603c9cf6e8c1ec516f9b336349df33d37a5aec
Chris Mc <prince.chrismc@gmail.com>
2020-07-11 16:05:54 -0400

    Revert "Update recipes/srt/all/conanfile.py" enable short paths
    This reverts commit 47ab32cb3aacf253420efe6969decda9e3fce769.

--- recipes/srt/all/conanfile.py
+++ recipes/srt/all/conanfile.py
@@ -14,6 +14,7 @@ class SrtConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
+    short_paths = True
 
     _cmake = None
 
