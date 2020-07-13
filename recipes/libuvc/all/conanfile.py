092478f08bd46e4424058360e1e973af2d060622
Uilian Ries <uilianries@gmail.com>
2020-07-13 16:41:28 -0300

    Improve libuvc
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libuvc/all/conanfile.py
+++ recipes/libuvc/all/conanfile.py
@@ -7,18 +7,28 @@ class LibuvcConan(ConanFile):
     name = "libuvc"
     description = "A cross-platform library for USB video devices"
     topics = ("conan", "libuvc", "libusb", "usb", "video")
-    license = "MIT"
+    license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/libuvc/libuvc"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False], "jpeg_turbo": [True, False]}
     default_options = {"shared": False, "fPIC": True, "jpeg_turbo": False}
-    generators = "cmake_find_package"
+    generators = "cmake", "cmake_find_package"
     requires = "libusb/1.0.23"
-    exports_sources = ["CMakeLists.txt"]
+    exports_sources = ["CMakeLists.txt", "patches/*"]
+    _cmake = None
 
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
     def configure(self):
         del self.settings.compiler.libcxx
@@ -27,13 +37,14 @@ class LibuvcConan(ConanFile):
             # Upstream issues, e.g.:
             # https://github.com/libuvc/libuvc/issues/100
             # https://github.com/libuvc/libuvc/issues/105
-            raise ConanInvalidConfiguration("This library is not compatible with Windows")
+            raise ConanInvalidConfiguration("'{}' is not compatible with Windows.".format(self.name))
+        if self.options.shared:
+            del self.options.fPIC
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
-
 
     def requirements(self):
         if self.options.jpeg_turbo:
@@ -41,27 +52,25 @@ class LibuvcConan(ConanFile):
         else:
             self.requires("libjpeg/9d")
 
-    def build(self):
-        _cmakelists = os.path.join(self._source_subfolder, "CMakeLists.txt")
-        tools.replace_in_file(_cmakelists, "pkg_check_modules(LIBUSB libusb-1.0)", "find_package(libusb REQUIRED)")
-        tools.replace_in_file(_cmakelists, "${LIBUSB_INCLUDE_DIRS}", "${libusb_INCLUDE_DIRS}")
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            self._cmake.definitions["CMAKE_BUILD_TARGET"] = "Shared" if self.options.shared else "Static"
+            self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
 
-        cmake = CMake(self)
-        if self.options.shared:
-            _cmake_defs = {"CMAKE_BUILD_TARGET" : "Shared" }
-        else:
-            _cmake_defs = {"CMAKE_BUILD_TARGET" : "Static" }
-        cmake.configure(defs = _cmake_defs, source_folder=self._source_subfolder, build_folder=self._build_subfolder)
+    def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE*", dst="licenses", src=self._source_subfolder)
-        self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
-        self.copy("*.h", dst="include", src=os.path.join(self._build_subfolder, "include"))
-        if self.options.shared:
-            self.copy("libuvc.so", dst="lib", src=self._build_subfolder, keep_path=False)
-        else:
-            self.copy("libuvc.a", dst="lib", src=self._build_subfolder, keep_path=False)
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
