5e98e9dbb0014df77bb1bad4b6e0c44b40d363b4
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-07-14 15:35:06 +0200

    fix test recipe

--- recipes/xorg/all/test_package/test_package.c
+++ recipes/xorg/all/test_package/test_package.c
@@ -1,8 +1,6 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
-#include <X11/extensions/dmxext.h>
-
 #include <X11/Xauth.h>
 
 #include <stdio.h>
@@ -13,12 +11,6 @@ int main() {
     if (!display) {
         printf("XOpenDisplay failed\n");
         return 0;
-    }
-
-    {
-        int dmx_major = 0, dmx_minor = 0, dmx_patch = 0;
-        if (DMXQueryVersion(display, &dmx_major, &dmx_minor, &dmx_patch))
-            printf("DMXQueryVersion: %d.%d.%d\n", dmx_major, dmx_minor, dmx_patch);
     }
 
     {
