adf520aec67064716f6c2e1819d28cea0a6a1b3e
Gavin <gavin.n.lobo@gmail.com>
2020-07-15 15:40:00 -0400

    Fixed conan-center package requirements

--- recipes/tng/all/conanfile.py
+++ recipes/tng/all/conanfile.py
@@ -82,6 +82,7 @@ class tngConan(ConanFile):
         self.copy("COPYING", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.libs = ["tng_io"]
