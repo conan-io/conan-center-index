7e30e689e878fc0043c91c23b3232eadff66ee8f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-07-17 16:55:00 +0200

    qhull: delete fPIC option if shared

--- recipes/qhull/all/conanfile.py
+++ recipes/qhull/all/conanfile.py
@@ -34,6 +34,8 @@ class QhullConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
