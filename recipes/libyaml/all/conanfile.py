c311de607aebf55b57cb13f81667c47cb4be54af
Julien Marrec <julien.marrec@gmail.com>
2020-07-21 14:02:09 +0200

    Modify for conan-center-index (config.yml, conandata.yml) and add 0.25.0 version

--- recipes/libyaml/all/conanfile.py
+++ recipes/libyaml/all/conanfile.py
@@ -4,10 +4,9 @@ import os
 
 class LibYAMLConan(ConanFile):
     name = "libyaml"
-    version = "0.2.2"
     description = "LibYAML is a YAML parser and emitter library."
     topics = ("conan", "libyaml", "yaml", "parser", "emitter")
-    url = "https://github.com/bincrafters/conan-libyaml"
+    url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/yaml/libyaml"
     license = "MIT"
     exports_sources = ["CMakeLists.txt"]
@@ -33,9 +32,8 @@ class LibYAMLConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        sha256 = "46bca77dc8be954686cff21888d6ce10ca4016b360ae1f56962e6882a17aa1fe"
-        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
-        extracted_dir = self.name + "-" + self.version
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = '{}-{}'.format(self.name, self.version)
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
@@ -57,4 +55,6 @@ class LibYAMLConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.compiler == "Visual Studio":
-            self.cpp_info.defines = ["YAML_DECLARE_EXPORT"] if self.options.shared else ["YAML_DECLARE_STATIC"]
+            self.cpp_info.defines = (["YAML_DECLARE_EXPORT"] if
+                                     self.options.shared else
+                                     ["YAML_DECLARE_STATIC"])
