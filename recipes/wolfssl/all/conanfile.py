06ebb53dd8bc62512fd8be99c474b51ce6f179c8
kirandivekar <kirandivekar@gmail.com>
2020-07-27 17:12:31 +0530

    Update recipes/wolfssl/all/conanfile.py
    Update the change as per suggestion from @madebr.
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/wolfssl/all/conanfile.py
+++ recipes/wolfssl/all/conanfile.py
@@ -79,11 +79,9 @@ class WolfSSLConan(ConanFile):
             "--disable-crypttests",
             "--enable-harden",
             "--enable-debug" if self.settings.build_type == "Debug" else "--disable-debug",
+            "--enable-opensslextra" if self.options.opensslextra else "--disable-opensslextra",
+            "--enable-opensslall" if self.options.opensslall else "--disable-opensslall",
         ]
-        if self.options.opensslextra:
-            conf_args.extend(["--enable-opensslextra"])
-            if self.options.opensslall:
-                conf_args.extend(["--enable-opensslall"])
         if self.options.shared:
             conf_args.extend(["--enable-shared", "--disable-static"])
         else:
