5d62d7ee322775f770791bec6890c332163ce6ff
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-07-29 19:33:01 +0200

    freetype: delete fPIC option if shared

--- recipes/freetype/all/conanfile.py
+++ recipes/freetype/all/conanfile.py
@@ -45,6 +45,8 @@ class FreetypeConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
