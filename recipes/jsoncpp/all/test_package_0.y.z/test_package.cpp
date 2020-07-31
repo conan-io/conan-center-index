9bda10c05248f3b92b31a2a44034aca93fdcfb7c
marcvilletard <villetard.marc@gmail.com>
2020-07-31 10:09:34 +0200

    Add test_package_0.y.z

---
+++ recipes/jsoncpp/all/test_package_0.y.z/test_package.cpp
@@ -0,0 +1,9 @@
+#include <jsoncpp/json/json.h>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	Json::Value value("Hello, World");
+	std::cout << value.asString() << std::endl;
+	return 0;
+}
