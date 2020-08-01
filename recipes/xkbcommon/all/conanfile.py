d4037a7a92c658183bd4bc42a2c255b0ed408dd1
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-08-01 09:57:01 +0200

    xkbcommon: add components

--- recipes/xkbcommon/all/conanfile.py
+++ recipes/xkbcommon/all/conanfile.py
@@ -73,5 +73,12 @@ class XkbcommonConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.components['libxkbcommon'].libs = ['xkbcommon']
+        self.cpp_info.components['libxkbcommon'].name = 'xkbcommon'
+        self.cpp_info.components['libxkbcommon'].requires = ['xorg::xorg']
+        if self.options.with_x11:
+            self.cpp_info.components['libxkbcommon-x11'].libs = ['xkbcommon-x11']
+            self.cpp_info.components['libxkbcommon-x11'].name = 'xkbcommon-x11'
+            self.cpp_info.components['libxkbcommon-x11'].requires = ['libxkbcommon']
+            self.cpp_info.components['libxkbcommon-x11'].requires.append('xorg::xorg')
 
