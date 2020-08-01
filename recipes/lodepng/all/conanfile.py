d4ba5ca9673edd22fc104a1901aa0a17dcdb2526
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-01 15:10:48 -0400

    Fix lodepng

--- recipes/lodepng/all/conanfile.py
+++ recipes/lodepng/all/conanfile.py
@@ -35,6 +35,10 @@ class LodepngConan(ConanFile):
         if self.settings.os == 'Windows':
             del self.options.fPIC
 
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = glob.glob('lodepng-*/')[0]
