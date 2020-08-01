237d826e6d7594cd3fef56fcfc1724d8f4a149b1
ItsBasi <5033630+ItsBasi@users.noreply.github.com>
2020-08-02 00:28:59 +0200

    Minizip package test gets passed preprocessor defines depending on which options are enabled for minizip

--- recipes/minizip/all/test_package/conanfile.py
+++ recipes/minizip/all/test_package/conanfile.py
@@ -5,9 +5,36 @@ import os
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    _cmake = None
+
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            if self.options["minizip"].compress_only:
+                self._cmake.definitions["MZ_ZIP_NO_DECOMPRESSION"] = True
+            if self.options["minizip"].decompress_only:
+                self._cmake.definitions["MZ_ZIP_NO_COMPRESSION"] = True
+            if self.options["minizip"].bzip2:
+                self._cmake.definitions["HAVE_BZIP2"] = True
+            if self.options["minizip"].zlib:
+                self._cmake.definitions["HAVE_ZLIB"] = True
+            if self.options["minizip"].lzma:
+                self._cmake.definitions["HAVE_LZMA"] = True
+            if self.options["minizip"].zstd:
+                self._cmake.definitions["HAVE_ZSTD"] = True
+            if self.options["minizip"].compat:
+                self._cmake.definitions["HAVE_COMPAT"] = True
+            if self.options["minizip"].pkcrypt:
+                self._cmake.definitions["HAVE_PKCRYPT"] = True
+            if self.options["minizip"].wzaes:
+                self._cmake.definitions["HAVE_WZAES"] = True
+            if not self.options["minizip"].wzaes and not self.options["minizip"].pkcrypt:
+                self._cmake.definitions["MZ_ZIP_NO_ENCRYPTION"] = True
+            self._cmake.configure()
+        return self._cmake
 
     def build(self):
-        cmake = CMake(self)
+        cmake = self._configure_cmake()
         cmake.configure()
         cmake.build()
 
