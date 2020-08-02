0c1b6f3f35766baee5a4b1729937601dda98e540
Piotr Podsiadly <ppodsiadly@mykolab.com>
2020-08-02 19:45:13 +0200

    openimageio: update to 2.1.18.1

--- recipes/openimageio/all/test_package/test_package.cpp
+++ recipes/openimageio/all/test_package/test_package.cpp
@@ -1,8 +1,9 @@
 #include <OpenImageIO/imagecache.h>
-#include <algorithm>
 
 int main()
 {
+    std::cout << "OpenImageIO " << OIIO_VERSION_STRING << "\n";
+
     std::string formats = OIIO::get_string_attribute("format_list");
     std::cout << "Supported formats:\n" << formats;
 }
