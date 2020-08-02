127d5bf1ce07dd02f7aebf6719c5776b3b27ab44
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-02 17:33:53 -0400

    Add recastnavigation 20200511

---
+++ recipes/recastnavigation/all/conanfile.py
@@ -0,0 +1,85 @@
+import os
+import glob
+from conans import ConanFile, CMake, tools
+
+
+class RecastNavigationConan(ConanFile):
+    name = "recastnavigation"
+    homepage = "https://github.com/recastnavigation/recastnavigation"
+    description = " Navigation-mesh Toolset for Games"
+    topics = ("conan", "navmesh", "recast", "navigation", "crowd")
+    url = "https://github.com/conan-io/conan-center-index"
+    license = "ZLIB"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = glob.glob('recastnavigation-*/')[0]
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["RECASTNAVIGATION_DEMO"] = False
+        self._cmake.definitions["RECASTNAVIGATION_TESTS"] = False
+        self._cmake.definitions["RECASTNAVIGATION_EXAMPLES"] = False
+        self._cmake.definitions["RECASTNAVIGATION_STATIC"] = not self.options.shared
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.components["Recast"].name = "Recast"
+        self.cpp_info.components["Recast"].libs = ["Recast"]
+
+        self.cpp_info.components["Detour"].name = "Detour"
+        self.cpp_info.components["Detour"].libs = ["Detour"]
+
+        self.cpp_info.components["DetourCrowd"].name = "DetourCrowd"
+        self.cpp_info.components["DetourCrowd"].libs = ["DetourCrowd"]
+        self.cpp_info.components["DetourCrowd"].requires = ["Detour"]
+
+        self.cpp_info.components["DetourTileCache"].name = "DetourTileCache"
+        self.cpp_info.components["DetourTileCache"].libs = ["DetourTileCache"]
+        self.cpp_info.components["DetourTileCache"].requires = ["Detour"]
+
+        self.cpp_info.components["DebugUtils"].name = "DebugUtils"
+        self.cpp_info.components["DebugUtils"].libs = ["DebugUtils"]
+        self.cpp_info.components["DebugUtils"].requires = ["Recast", "Detour", "DetourTileCache"]
---
+++ recipes/recastnavigation/all/test_package/test_package.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Recast.h"
+
+int main() {
+    std::cout << (rcMin(10, 20) == 10);
+    return EXIT_SUCCESS;
+}
