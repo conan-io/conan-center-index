aef9d653dfb0e1c7a62fe02157f8baa309916b38
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-03 16:48:43 +0200

    mpfr: delete fPIC option if shared

--- recipes/mpfr/all/conanfile.py
+++ recipes/mpfr/all/conanfile.py
@@ -27,6 +27,8 @@ class MpfrConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
