3a99fc8fec08e2c2985fc3b24cb558423fa51669
marcvilletard <villetard.marc@gmail.com>
2020-08-03 16:56:07 +0200

    Remove test_package_0.y.z folder

--- recipes/jsoncpp/all/test_package_0.y.z/test_package.cpp
+++
@@ -1,9 +0,0 @@
-#include <jsoncpp/json/json.h>
-#include <iostream>
-
-int main(int argc, char **argv)
-{
-	Json::Value value("Hello, World");
-	std::cout << value.asString() << std::endl;
-	return 0;
-}
