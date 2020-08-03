3b5e09a68cfadc8bc1090938d22e345d67759fdb
Gordon Greene <greeng3@obscure-reference.com>
2020-08-03 09:48:41 -0700

    Put back something lost in one too many undos.

--- recipes/mbedtls/all/conanfile.py
+++ recipes/mbedtls/all/conanfile.py
@@ -70,7 +70,7 @@ class MBedTLSConan(ConanFile):
         return cmake
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
 
         cmake = self._configure_cmake()
