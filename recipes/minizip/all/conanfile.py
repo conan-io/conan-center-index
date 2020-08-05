d1ff532232610ba58dd36426e552f094e47e3e15
ItsBasi <5033630+ItsBasi@users.noreply.github.com>
2020-08-05 20:08:58 +0200

    Correct lzma option prefix

--- recipes/minizip/all/conanfile.py
+++ recipes/minizip/all/conanfile.py
@@ -21,7 +21,7 @@ class MinizipConan(ConanFile):
         "with_zlib": [True, False],
         "with_bzip2": [True, False],
         "with_zstd": [True, False],
-        "lwith_zma": [True, False],
+        "with_lzma": [True, False],
         "pkcrypt": [True, False],
         "wzaes": [True, False],
         "libcomp": [True, False],
