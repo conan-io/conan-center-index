d90bf9f24eb3ebb1b7f53845e15d587e5589e7ef
Nils Gerke <nils.gerke@digitalwerk.net>
2020-08-06 12:28:48 +0200

    only use conan center

--- recipes/graphviz/2.44.1/conanfile.py
+++ recipes/graphviz/2.44.1/conanfile.py
@@ -5,11 +5,11 @@ class Graphviz(ConanFile):
     name = "graphviz"
     description = "Conan installer for Graphviz"
     topics = ("conan", "graphviz", "dot", "doxygen", "installer")
-    url = "https://gitlab.com/graphviz/graphviz/"
+    url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://graphviz.org/"
     license = "Eclipse Public License - v 1.0"
-    generators = "virtualrunenv", "cmake"
-    settings = "os", "arch", "compiler", "build_type"
+    generators = "virtualrunenv", "cmake_find_package"
+    settings = "os", "arch", "compiler"
 
     _cmake = None
     _source_subfolder = "source_subfolder"
@@ -18,13 +18,8 @@ class Graphviz(ConanFile):
     def build_requirements(self):
         if tools.os_info.is_linux and self.settings.os == "Linux":
             self.build_requires("bison/3.5.3")
-            self.build_requires("expat/2.2.9")
             self.build_requires("flex/2.6.4")
             self.build_requires("fontconfig/2.13.91")
-            self.build_requires("freetype/2.10.2")
-            self.build_requires("libpng/1.6.37")
-            self.build_requires("pixman/0.40.0")
-            self.build_requires("zlib/1.2.11")
 
         if tools.os_info.is_windows and self.settings.os == "Windows":
             if not tools.get_env("CONAN_BASH_PATH"):
@@ -32,14 +27,23 @@ class Graphviz(ConanFile):
             self.build_requires("strawberryperl/5.30.0.1")
             self.build_requires("winflexbison/2.5.22")
 
+        self.build_requires("freetype/2.10.2")
+        self.build_requires("expat/2.2.9")
+        self.build_requires("libpng/1.6.37")
+        self.build_requires("libtool/2.4.6")
+        self.build_requires("pixman/0.40.0")
+        self.build_requires("zlib/1.2.11")
+
+    def export_sources(self):
+        if tools.os_info.is_windows and self.settings.os == "Windows":
+            self.copy("rxspencer.tar.gz")
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         folder_name = "%s-%s" % (self.name, self.version)
         os.rename(folder_name, self._source_subfolder)
-        # Get windows dependencies from official repo
         if tools.os_info.is_windows and self.settings.os == "Windows":
-            git = tools.Git(folder=os.path.join(self._source_subfolder, "windows", "dependencies", "libraries"))
-            git.clone("https://github.com/ErwinJanssen/graphviz-windows-dependencies.git", branch="141d3a21be904fa8dc2ae3ed01d36684db07a35d", shallow=True)
+            tools.untargz("rxspencer.tar.gz", destination=os.path.join(self._source_subfolder, "windows", "dependencies", "libraries")) #TODO build rxspencer as conan pkg
 
     def _configure_cmake(self):
         if not self._cmake:
@@ -50,23 +54,6 @@ class Graphviz(ConanFile):
                     "windows", "dependencies", "libraries", "x64", "lib")
         return self._cmake
 
-    def system_requirements(self):
-        if tools.os_info.is_linux and self.settings.os == "Linux":
-            package_tool = tools.SystemPackageTool(conanfile=self, default_mode="verify")
-            if tools.os_info.with_apt:
-                packages = ["libltdl-dev"]
-            elif tools.os_info.with_yum or tools.os_info.with_dnf:
-                packages = ["libtool-ltdl-devel"]
-            elif tools.os_info.with_pacman:
-                packages = ["libtool"]
-            elif tools.os_info.with_zypper:
-                packages = ["libltdl7"]
-            else:
-                self.output.warn("Do not know how to install 'libltdl' for {}.".format(tools.os_info.linux_distro))
-                packages = []
-            for p in packages:
-                package_tool.install(update=True, packages=p)
-
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
@@ -74,10 +61,7 @@ class Graphviz(ConanFile):
     def package(self):
         cmake = self._configure_cmake()
         cmake.install()
-
         tools.mkdir("licences")
-        if tools.os_info.is_windows and self.settings.os == "Windows":
-            self.copy("COPYING-*", dst="licenses", src=os.path.join(self._source_subfolder, "windows", "dependencies", "libraries", "x64"))
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
         tools.rmdir(os.path.join(self.package_folder, "share"))
