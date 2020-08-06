a78eb212f4bed4e78c927dd3d35eddf08e5e337b
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-08-06 16:48:35 -0300

    Add linkage spec for client projects
    For client projects, require linking against r and m on Linux and
    against ws2_32 on Windows.

--- recipes/hdrhistogram-c/all/conanfile.py
+++ recipes/hdrhistogram-c/all/conanfile.py
@@ -75,3 +75,9 @@ class QuickfixConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.includedirs = [os.path.join("include", "hdr")]
+
+        if self.settings.os == "Windows":
+            self.cpp_info.system_libs.extend(["ws2_32"])
+        elif self.settings.os == "Linux":
+            self.cpp_info.system_libs.extend(["rt"])
+            self.cpp_info.system_libs.extend(["m"])
