b7fbecb4e9bf5b1076aa906c95d018c7d73b23b0
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-08-08 05:00:48 -0300

    Make sure both c and c++ can consume this package

--- recipes/hdrhistogram-c/all/test_package/conanfile.py
+++ recipes/hdrhistogram-c/all/test_package/conanfile.py
@@ -13,5 +13,8 @@ class HDRHistogramCTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "hdr_decoder")
+            bin_path = os.path.join("bin", "hdr_decoder_c")
+            self.run(bin_path, run_environment=True)
+
+            bin_path = os.path.join("bin", "hdr_decoder_cxx")
             self.run(bin_path, run_environment=True)
