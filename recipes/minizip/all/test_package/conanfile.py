21ce15d53e8540fdbe4bc62e6fcdf5feb0ae2cfb
ItsBasi <5033630+ItsBasi@users.noreply.github.com>
2020-08-10 01:03:11 +0200

    Package test does not cache the cmake object any more

--- recipes/minizip/all/test_package/conanfile.py
+++ recipes/minizip/all/test_package/conanfile.py
@@ -5,37 +5,30 @@ import os
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    _cmake = None
-
-    def _configure_cmake(self):
-        if not self._cmake:
-            self._cmake = CMake(self)
-            self._cmake.definitions["CONAN_MZ_VERSION"] = self.requires["minizip"].ref.version
-            if self.options["minizip"].compress_only:
-                self._cmake.definitions["MZ_ZIP_NO_DECOMPRESSION"] = "MZ_ZIP_NO_DECOMPRESSION"
-            if self.options["minizip"].decompress_only:
-                self._cmake.definitions["MZ_ZIP_NO_COMPRESSION"] = "MZ_ZIP_NO_COMPRESSION"
-            if self.options["minizip"].with_bzip2:
-                self._cmake.definitions["HAVE_BZIP2"] = "HAVE_BZIP2"
-            if self.options["minizip"].with_zlib:
-                self._cmake.definitions["HAVE_ZLIB"] = "HAVE_ZLIB"
-            if self.options["minizip"].lzma:
-                self._cmake.definitions["HAVE_LZMA"] = "HAVE_LZMA"
-            if self.options["minizip"].with_zstd:
-                self._cmake.definitions["HAVE_ZSTD"] = "HAVE_ZSTD"
-            if self.options["minizip"].compat:
-                self._cmake.definitions["HAVE_COMPAT"] = "HAVE_COMPAT"
-            if self.options["minizip"].pkcrypt:
-                self._cmake.definitions["HAVE_PKCRYPT"] = "HAVE_PKCRYPT"
-            if self.options["minizip"].wzaes:
-                self._cmake.definitions["HAVE_WZAES"] = "HAVE_WZAES"
-            if not self.options["minizip"].wzaes and not self.options["minizip"].pkcrypt:
-                self._cmake.definitions["MZ_ZIP_NO_ENCRYPTION"] = "MZ_ZIP_NO_ENCRYPTION"
-            self._cmake.configure()
-        return self._cmake
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.definitions["CONAN_MZ_VERSION"] = self.requires["minizip"].ref.version
+        if self.options["minizip"].compress_only:
+            cmake.definitions["MZ_ZIP_NO_DECOMPRESSION"] = "MZ_ZIP_NO_DECOMPRESSION"
+        if self.options["minizip"].decompress_only:
+            cmake.definitions["MZ_ZIP_NO_COMPRESSION"] = "MZ_ZIP_NO_COMPRESSION"
+        if self.options["minizip"].with_bzip2:
+            cmake.definitions["HAVE_BZIP2"] = "HAVE_BZIP2"
+        if self.options["minizip"].with_zlib:
+            cmake.definitions["HAVE_ZLIB"] = "HAVE_ZLIB"
+        if self.options["minizip"].lzma:
+            cmake.definitions["HAVE_LZMA"] = "HAVE_LZMA"
+        if self.options["minizip"].with_zstd:
+            cmake.definitions["HAVE_ZSTD"] = "HAVE_ZSTD"
+        if self.options["minizip"].compat:
+            cmake.definitions["HAVE_COMPAT"] = "HAVE_COMPAT"
+        if self.options["minizip"].pkcrypt:
+            cmake.definitions["HAVE_PKCRYPT"] = "HAVE_PKCRYPT"
+        if self.options["minizip"].wzaes:
+            cmake.definitions["HAVE_WZAES"] = "HAVE_WZAES"
+        if not self.options["minizip"].wzaes and not self.options["minizip"].pkcrypt:
+            cmake.definitions["MZ_ZIP_NO_ENCRYPTION"] = "MZ_ZIP_NO_ENCRYPTION"
         cmake.configure()
         cmake.build()
 
