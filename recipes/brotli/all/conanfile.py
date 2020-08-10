ff714a53d390f52aad8fa3bb423e9c2c10b7bbbf
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-10 10:15:03 +0200

    brotli: add components + cache cmake (#2319)
    * brotli: add components
    
    * brotli: fix hooks
    
    * brotli: cache cmake
    
    * brotli: add topics
    
    * brotli: defines only in brotlicommon
    
    brotlidec and brotlienc will inherit from brotlicommon defines
    
    * brolti: restore unofficial cmake name
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>

--- recipes/brotli/all/conanfile.py
+++ recipes/brotli/all/conanfile.py
@@ -5,6 +5,7 @@ import os
 class BrotliConan(ConanFile):
     name = "brotli"
     description = "Brotli compression format"
+    topics = ("conan", "brotli", "compression")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/google/brotli"
     license = "MIT",
@@ -19,7 +20,16 @@ class BrotliConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    _source_subfolder = "source_subfolder"
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -37,11 +47,13 @@ class BrotliConan(ConanFile):
         os.rename(extracted_folder, self._source_subfolder)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BROTLI_BUNDLED_MODE"] = False
-        cmake.definitions["BROTLI_DISABLE_TESTS"] = True
-        cmake.configure()
-        return cmake
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["BROTLI_BUNDLED_MODE"] = False
+        self._cmake.definitions["BROTLI_DISABLE_TESTS"] = True
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
 
     def build(self):
         for patch in self.conan_data["patches"][self.version]:
@@ -55,18 +67,31 @@ class BrotliConan(ConanFile):
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
-    def _get_libraries(self, shared):
-        libs = ["brotlienc", "brotlidec", "brotlicommon"]
-        if not shared:
-            libs = ["{}-static".format(l) for l in libs]
-        return libs
-
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "Brotli"
         self.cpp_info.names["cmake_find_package_multi"] = "Brotli"
-        self.cpp_info.libs = self._get_libraries(self.options.shared)
-        self.cpp_info.includedirs = ["include", os.path.join("include", "brotli")]
-        if self.options.shared:
-            self.cpp_info.defines.append("BROTLI_SHARED_COMPILATION")
+        includedir = os.path.join("include", "brotli")
+        # brotlicommon
+        self.cpp_info.components["brotlicommon"].names["pkg_config"] = "libbrotlicommon"
+        self.cpp_info.components["brotlicommon"].includedirs.append(includedir)
+        self.cpp_info.components["brotlicommon"].libs = [self._get_decorated_lib("brotlicommon")]
+        if self.settings.os == "Windows" and self.options.shared:
+            self.cpp_info.components["brotlicommon"].defines.append("BROTLI_SHARED_COMPILATION")
+        # brotlidec
+        self.cpp_info.components["brotlidec"].names["pkg_config"] = "libbrotlidec"
+        self.cpp_info.components["brotlidec"].includedirs.append(includedir)
+        self.cpp_info.components["brotlidec"].libs = [self._get_decorated_lib("brotlidec")]
+        self.cpp_info.components["brotlidec"].requires = ["brotlicommon"]
+        # brotlienc
+        self.cpp_info.components["brotlienc"].names["pkg_config"] = "libbrotlienc"
+        self.cpp_info.components["brotlienc"].includedirs.append(includedir)
+        self.cpp_info.components["brotlienc"].libs = [self._get_decorated_lib("brotlienc")]
+        self.cpp_info.components["brotlienc"].requires = ["brotlicommon"]
         if self.settings.os == "Linux":
-            self.cpp_info.system_libs = ["m"]
+            self.cpp_info.components["brotlienc"].system_libs = ["m"]
+
+    def _get_decorated_lib(self, name):
+        libname = name
+        if not self.options.shared:
+            libname += "-static"
+        return libname
