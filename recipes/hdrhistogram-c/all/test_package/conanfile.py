24d1f9564feb55ef758265f456cd6ea179740164
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-08-10 20:10:37 -0300

    Leave only one exe in test_package

--- recipes/hdrhistogram-c/all/test_package/conanfile.py
+++ recipes/hdrhistogram-c/all/test_package/conanfile.py
@@ -13,8 +13,5 @@ class HDRHistogramCTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "hdr_decoder_c")
-            self.run(bin_path, run_environment=True)
-
             bin_path = os.path.join("bin", "hdr_decoder_cxx")
             self.run(bin_path, run_environment=True)
