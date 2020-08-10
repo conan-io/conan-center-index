009043c6dfb29d1cc41cc8dfa83317648dd64926
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-10 08:28:23 -0400

    assimp: new test + cmake cache + del fPIC if shared (#2372)
    * assimp: New cpp test
    
    * assimp: del fPIC if shared
    
    * assimp: Bump test cmake version
    
    * assimp: move requirements
    
    * assimp: cmake cache
    
    * assimp: Fix test

--- recipes/assimp/5.0.x/test_package/test_package.cpp
+++ recipes/assimp/5.0.x/test_package/test_package.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-#include "assimp/version.h"
-#include "assimp/Importer.hpp"
 
-int main() {
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Need at least one argument" << std::endl;
+        return 1;
+    }
+
     Assimp::Importer importer;
-    std::cout << "Assimp version " << aiGetVersionMajor() << "." << aiGetVersionMinor() << "." << aiGetVersionRevision() << std::endl;
+    const aiScene* scene = importer.ReadFile(argv[1],
+        aiProcess_CalcTangentSpace       |
+        aiProcess_Triangulate            |
+        aiProcess_JoinIdenticalVertices  |
+        aiProcess_SortByPType);
+
+    if (!scene) {
+        return 1;
+    }
+
+    return 0;
 }
