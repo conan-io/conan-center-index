b966b7d38efbfab11683e2fc44455c63206ad0cc
Croydon <git@cr0ydon.com>
2020-08-11 06:48:48 +0200

    protobuf: refactor
    * fix CMake file names to be lowercase
    * replace huge version dependent patches which a few replace_in_file which are much easier to maintain
    * protoc is now always available; removing all non-protoc ways in the test_package

--- recipes/protobuf/all/conanfile.py
+++ recipes/protobuf/all/conanfile.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 from conans.tools import Version
@@ -55,22 +56,49 @@ class ProtobufConan(ConanFile):
         return os.path.join("lib", "cmake", "protobuf")
 
     def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["CMAKE_INSTALL_CMAKEDIR"] = self._cmake_install_base_path.replace("\\", "/")
-        self._cmake.definitions["protobuf_BUILD_TESTS"] = False
-        self._cmake.definitions["protobuf_WITH_ZLIB"] = self.options.with_zlib
-        self._cmake.definitions["protobuf_BUILD_PROTOC_BINARIES"] = not self.options.lite
-        self._cmake.definitions["protobuf_BUILD_PROTOBUF_LITE"] = self.options.lite
-        if self.settings.compiler == "Visual Studio":
-            self._cmake.definitions["protobuf_MSVC_STATIC_RUNTIME"] = "MT" in self.settings.compiler.runtime
-        self._cmake.configure(build_folder=self._build_subfolder)
+        if not self._cmake:
+            self._cmake = CMake(self)
+            self._cmake.definitions["CMAKE_INSTALL_CMAKEDIR"] = self._cmake_install_base_path.replace("\\", "/")
+            self._cmake.definitions["protobuf_BUILD_TESTS"] = False
+            self._cmake.definitions["protobuf_WITH_ZLIB"] = self.options.with_zlib
+            if self.settings.compiler == "Visual Studio":
+                self._cmake.definitions["protobuf_MSVC_STATIC_RUNTIME"] = "MT" in self.settings.compiler.runtime
+            self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
     def build(self):
-        for patch in self.conan_data["patches"][self.version]:
+        for patch in self.conan_data["patches"].get(self.version, []):
             tools.patch(**patch)
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "cmake", "protobuf-config.cmake.in"),
+            "@_protobuf_FIND_ZLIB@",
+            "# CONAN PATCH @_protobuf_FIND_ZLIB@"
+        )
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "cmake", "protobuf-config.cmake.in"),
+            'include("${CMAKE_CURRENT_LIST_DIR}/protobuf-targets.cmake")',
+            '# CONAN PATCH include("${CMAKE_CURRENT_LIST_DIR}/protobuf-targets.cmake")'
+        )
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "cmake", "protobuf-config.cmake.in"),
+            'COMMAND  protobuf::protoc',
+            'COMMAND "${CMAKE_COMMAND}"',
+        )
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "cmake", "protobuf-config.cmake.in"),
+            'ARGS --${protobuf_generate_LANGUAGE}_out',
+            'ARGS -E env "DYLD_LIBRARY_PATH=${Protobuf_LIB_DIRS}:${CONAN_LIB_DIRS}:${Protobuf_LIB_DIRS_RELEASE}:${Protobuf_LIB_DIRS_DEBUG}:${Protobuf_LIB_DIRS_RELWITHDEBINFO}:${Protobuf_LIB_DIRS_MINSIZEREL}" protoc --${protobuf_generate_LANGUAGE}_out',
+        )
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "cmake", "protobuf-module.cmake.in"),
+            '# Environment',
+            'if(0)',
+        )
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "cmake", "protobuf-module.cmake.in"),
+            '# Version info variable',
+            'endif()',
+        )
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -79,10 +107,33 @@ class ProtobufConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        os.unlink(os.path.join(self.package_folder, "lib", "cmake", "protobuf", "protobuf-config-version.cmake"))
+        os.unlink(os.path.join(self.package_folder, "lib", "cmake", "protobuf", "protobuf-targets.cmake"))
+        os.unlink(os.path.join(self.package_folder, "lib", "cmake", "protobuf", "protobuf-targets-{}.cmake".format(str(self.settings.build_type).lower())))
+        os.rename(os.path.join(self.package_folder, "lib", "cmake", "protobuf", "protobuf-config.cmake"),
+                  os.path.join(self.package_folder, "lib", "cmake", "protobuf", "protobuf-generate.cmake"))
+
+        if self.options.lite:
+            for f in glob.glob(os.path.join(self.package_folder, "lib", "libprotobuf.*")):
+                os.remove(f)
+        else:
+            for f in glob.glob(os.path.join(self.package_folder, "lib", "libprotobuf-lite.*")):
+                os.remove(f)
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "Protobuf"
-        self.cpp_info.names["cmake_find_package_multi"] = "Protobuf"
+        self.cpp_info.filenames["cmake_find_package"] = "Protobuf"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "Protobuf"
+
+        self.cpp_info.names["cmake_find_package"] = "protobuf"
+        self.cpp_info.names["cmake_find_package_multi"] = "protobuf"
+
+        self.cpp_info.components["protoc"].name = "protoc"
+        if self.options.with_zlib:
+            self.cpp_info.components["protoc"].requires.append("zlib::zlib")
+
+        self.cpp_info.components["libprotoc"].name = "libprotoc"
+        self.cpp_info.components["libprotoc"].libs = ["protoc"]
+        self.cpp_info.components["libprotoc"].requires = ["libprotobuf"]
 
         if not self.options.lite:
             self.cpp_info.components["libprotobuf"].name = "libprotobuf"
@@ -94,11 +145,17 @@ class ProtobufConan(ConanFile):
             if self.settings.os == "Windows":
                 if self.options.shared:
                     self.cpp_info.components["libprotobuf"].defines = ["PROTOBUF_USE_DLLS"]
-            self.cpp_info.components["protoc"].requires = ["zlib::zlib"]
 
-            self.cpp_info.components["libprotoc"].name = "libprotoc"
-            self.cpp_info.components["libprotoc"].libs = ["protoc"]
-            self.cpp_info.components["libprotoc"].requires = ["libprotobuf"]
+            self.cpp_info.components["libprotobuf"].builddirs = [
+                self._cmake_install_base_path,
+            ]
+
+            self.cpp_info.components["libprotobuf"].build_modules.extend([
+                os.path.join(self._cmake_install_base_path, "protobuf-generate.cmake"),
+                os.path.join(self._cmake_install_base_path, "protobuf-module.cmake"),
+                os.path.join(self._cmake_install_base_path, "protobuf-options.cmake"),
+            ])
+
         else:
             self.cpp_info.components["libprotobuf-lite"].name = "libprotobuf-lite"
             self.cpp_info.components["libprotobuf-lite"].libs = ["protobuf-lite"]
@@ -110,16 +167,7 @@ class ProtobufConan(ConanFile):
                 if self.options.shared:
                     self.cpp_info.components["libprotobuf-lite"].defines = ["PROTOBUF_USE_DLLS"]
 
-        self.cpp_info.components["libprotobuf"].builddirs = [
-            self._cmake_install_base_path,
-        ]
-        self.cpp_info.components["libprotobuf"].build_modules = [
-            os.path.join(self._cmake_install_base_path, "protobuf-generate.cmake"),
-            os.path.join(self._cmake_install_base_path, "protobuf-module.cmake"),
-            os.path.join(self._cmake_install_base_path, "protobuf-options.cmake"),
-        ]
 
-        if not self.options.lite:
-            bindir = os.path.join(self.package_folder, "bin")
-            self.output.info("Appending PATH environment variable: {}".format(bindir))
-            self.env_info.PATH.append(bindir)
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(bindir))
+        self.env_info.PATH.append(bindir)
