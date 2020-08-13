3c456fba3efea619ac0a071c7f917ad744b0019d
Anonymous Maarten <madebr@users.noreply.github.com>
2020-08-13 18:11:26 +0200

    libbsd: don't remove fPIC option on Windows (and Windows is not supported anyways)

--- recipes/libbsd/all/conanfile.py
+++ recipes/libbsd/all/conanfile.py
@@ -28,10 +28,6 @@ class LibBsdConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
