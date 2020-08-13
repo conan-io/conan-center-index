02a8d73717bbfe725014ca9da403677be86db62f
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-08-13 03:52:37 -0300

    Force c++14, following zxing-cpp's way

--- recipes/hana/all/conanfile.py
+++ recipes/hana/all/conanfile.py
@@ -1,7 +1,9 @@
-import os
 from conans import ConanFile, tools, CMake
+from conans.errors import ConanInvalidConfiguration
+import os
 
-class eigenConan(ConanFile):
+
+class HanaConan(ConanFile):
     name = "hana"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "http://boostorg.github.io/hana/"
@@ -12,6 +14,13 @@ class eigenConan(ConanFile):
     no_copy_source = True
     exports_sources = "CMakeLists.txt"
 
+    _compiler_cpp14_support = {
+        "gcc": "5",
+        "Visual Studio": "14",
+        "clang": "3.4",
+        "apple-clang": "3.4",
+    }
+
     @property
     def _source_subfolder(self):
         return "_source_subfolder"
@@ -20,8 +29,15 @@ class eigenConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("hana-" + self.version, self._source_subfolder)
 
-    def configure(self):
+    def config_options(self):
         tools.check_min_cppstd(self, "14")
+        try:
+            minimum_required_version = self._compiler_cpp14_support[str(self.settings.compiler)]
+            if self.settings.compiler.version < tools.Version(minimum_required_version):
+                raise ConanInvalidConfiguration(
+                    "This compiler is too old. This library needs a compiler with c++14 support")
+        except KeyError:
+            self.output.warn("This recipe might not support the compiler. Consider adding it.")
 
     def package(self):
         cmake = CMake(self)
