66f414c635ca1bda7563bef789965224d9b0b359
Christopher McArthur <prince.chrismc@gmail.com>
2020-08-13 20:51:42 -0400

    cmake passes configuration

--- recipes/fizz/all/conanfile.py
+++ recipes/fizz/all/conanfile.py
@@ -10,8 +10,8 @@ class FizzConan(ConanFile):
     homepage = "https://github.com/facebookincubator/fizz"
     topics = ("conan", "tls", "networking", "client", "server")
     license = "MIT"
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake", "cmake_find_package"
+    exports_sources = ["CMakeLists.txt", "patches/**"]
+    generators = "cmake", "cmake_find_package_multi"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -43,6 +43,7 @@ class FizzConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
+        self.requires("folly/2019.10.21.00")
         self.requires("boost/1.73.0")
         self.requires("libevent/2.1.12")
         self.requires("double-conversion/3.1.5")
@@ -56,10 +57,11 @@ class FizzConan(ConanFile):
         self.requires("jemalloc/5.2.1")
         self.requires("openssl/1.1.1g")
         self.requires("libsodium/1.0.18")
+        self.requires("fmt/7.0.3") # TBA
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-v" + self.version
+        extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
@@ -72,6 +74,8 @@ class FizzConan(ConanFile):
         return self._cmake
 
     def build(self):
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
