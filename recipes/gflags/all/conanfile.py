a6dd65d5fd76de3103babe903473c39ed071ecdf
Uilian Ries <uilianries@gmail.com>
2020-08-14 11:34:46 -0300

    Export gflags defines for glog
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/gflags/all/conanfile.py
+++ recipes/gflags/all/conanfile.py
@@ -66,3 +66,5 @@ class GflagsConan(ConanFile):
             self.cpp_info.libs.extend(['shlwapi'])
         elif self.settings.os == "Linux":
             self.cpp_info.libs.extend(["pthread", "m"])
+        if self.version == "2.2.0" and self.options.shared and self.settings.compiler == "Visual Studio":
+            self.cpp_info.bindirs.append("Lib")
