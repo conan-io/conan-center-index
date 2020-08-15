bb5a1fdd3746c41bd5dd7e19e77fe17a82eba176
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-15 21:57:08 +0200

    optional-lite: fix hooks

--- recipes/optional-lite/all/conanfile.py
+++ recipes/optional-lite/all/conanfile.py
@@ -8,7 +8,6 @@ class OptionalLiteConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/martinmoene/optional-lite"
     description = "A single-file header-only version of a C++17-like optional, a nullable object for C++98, C++11 and later"
-    author = "Martin Moene"
     topics = ("conan", "cpp98", "cpp17", "optional", "optional-implementations")
     license = "BSL-1.0"
     no_copy_source = True
