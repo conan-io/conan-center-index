709ce22fc33e27f859828d264b23430fc8cbdbbb
johanneskares <johannes@kares.pro>
2020-08-16 00:54:22 +0200

    Apply suggestions from code review
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/r8brain-free-src/all/conanfile.py
+++ recipes/r8brain-free-src/all/conanfile.py
@@ -5,38 +5,51 @@ from conans import ConanFile, CMake, tools
 
 class R8brainFreeSrcConan(ConanFile):
     name = "r8brain-free-src"
-    version = "4.6"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/avaneev/r8brain-free-srcn"
-    description = "https://github.com/avaneev/r8brain-free-src"
+    description = "High-quality pro audio sample rate converter / resampler C++ library"
     topics = ("Audio", "Sample Rate", "Conversion")
     exports_sources = ["CMakeLists.txt"]
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    
+    _cmake = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_folder = "r8brain-free-src-version-{}".format(self.version)
         os.rename(extracted_folder, self._source_subfolder)
 
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure()
+        return self._cmake
+
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("*.h", dst="include", src=self._source_subfolder)
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["r8brain"]
