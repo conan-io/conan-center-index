3c1528de0ab103654920bf1b5df4f969775b8fd1
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-16 13:39:12 -0400

    Add butteraugli/20190319

---
+++ recipes/butteraugli/all/conanfile.py
@@ -0,0 +1,64 @@
+import os
+import glob
+from conans import ConanFile, CMake, tools
+
+class ButteraugliConan(ConanFile):
+    name = "butteraugli"
+    description = "A tool for measuring perceived differences between images"
+    license = "Apache-2.0"
+    topics = ("conan", "image", "butteraugli", "diff")
+    homepage = "https://github.com/google/butteraugli"
+    url = "https://github.com/conan-io/conan-center-index"
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = glob.glob('butteraugli-*/')[0]
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
