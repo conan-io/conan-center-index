86b7d3fb263199f12cfc9ce302262941ea37f029
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-17 17:16:13 +0200

    timsort: fix cmake names

--- recipes/timsort/all/conanfile.py
+++ recipes/timsort/all/conanfile.py
@@ -1,7 +1,8 @@
 import os
 
-from conans import CMake, ConanFile, tools
+from conans import ConanFile, tools
 
+required_conan_version = ">=1.28.0"
 
 class TimsortConan(ConanFile):
     name = "timsort"
@@ -10,9 +11,19 @@ class TimsortConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/timsort/cpp-TimSort"
     license = "MIT"
+    settings = "compiler"
     no_copy_source = True
 
-    _source_subfolder = "source_subfolder"
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def configure(self):
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, 11)
+
+    def package_id(self):
+        self.info.header_only()
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -23,5 +34,10 @@ class TimsortConan(ConanFile):
         self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
 
-    def package_id(self):
-        self.info.header_only()
+    def package_info(self):
+        self.cpp_info.filenames["cmake_find_package"] = "gfx-timsort"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "gfx-timsort"
+        self.cpp_info.names["cmake_find_package"] = "gfx"
+        self.cpp_info.names["cmake_find_package_multi"] = "gfx"
+        self.cpp_info.components["gfx-timsort"].names["cmake_find_package"] = "timsort"
+        self.cpp_info.components["gfx-timsort"].names["cmake_find_package_multi"] = "timsort"
