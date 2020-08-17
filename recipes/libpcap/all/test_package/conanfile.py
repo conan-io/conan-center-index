95c8858044b5ecae34278f1b2a805faa63a70cc8
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2020-08-17 22:28:42 +0300

    Update recipes/libpcap/all/test_package/conanfile.py
    Co-authored-by: Michael "Croydon" Keck <git@cr0ydon.com>

--- recipes/libpcap/all/test_package/conanfile.py
+++ recipes/libpcap/all/test_package/conanfile.py
@@ -12,5 +12,5 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "list_network")
+        bin_path = os.path.join("bin", "test_package")
         self.run(bin_path, run_environment=True)
