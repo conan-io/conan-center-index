24654ea949f767090e2907780addae984e444cf2
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-18 01:57:56 -0400

    vulkan-loader: remove pkgconfig folder

--- recipes/vulkan-loader/all/conanfile.py
+++ recipes/vulkan-loader/all/conanfile.py
@@ -87,6 +87,7 @@ class VulkanLoaderConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
