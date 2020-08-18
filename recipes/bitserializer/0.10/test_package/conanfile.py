0c6791d81fff1429075ceef13e4f0e3614ac8f85
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-18 11:59:33 +0200

    bitserializer: add options and components

--- recipes/bitserializer/0.10/test_package/conanfile.py
+++ recipes/bitserializer/0.10/test_package/conanfile.py
@@ -4,10 +4,13 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["WITH_CPPRESTSDK"] = self.options["bitserializer"].with_cpprestsdk
+        cmake.definitions["WITH_RAPIDJSON"] = self.options["bitserializer"].with_rapidjson
+        cmake.definitions["WITH_PUGIXML"] = self.options["bitserializer"].with_pugixml
         cmake.configure()
         cmake.build()
 
