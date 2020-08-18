dd91b9f912733e15e4e51c9a823331b21d63a95d
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-18 13:59:11 -0400

    astc-codec: check C++11

--- recipes/astc-codec/all/conanfile.py
+++ recipes/astc-codec/all/conanfile.py
@@ -34,6 +34,10 @@ class AstcCodecConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def configure(self):
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, "11")
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = glob.glob("astc-codec-*")[0]
