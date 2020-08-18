94be8d8c905b3fa2147169f9791c4f0679260339
Martin Delille <martin@delille.org>
2020-08-18 22:16:18 +0200

    Add requirements

--- recipes/libsndfile/1.0.29/conanfile.py
+++ recipes/libsndfile/1.0.29/conanfile.py
@@ -13,10 +13,16 @@ class LibsndfileConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "with_alsa": [True, False],
+        "with_sqlite": [True, False],
+        "with_external_libs": [True, False]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
+        "with_alsa": False,
+        "with_sqlite": True,
+        "with_external_libs": True,
     }
     generators = "cmake"
 
@@ -24,6 +30,16 @@ class LibsndfileConan(ConanFile):
 
     def _source_subfolder(self):
         return "source_subfolder"
+
+    def requirements(self):
+        if self.options.with_alsa:
+            self.requires("libalsa/1.1.9")
+        if self.options.with_sqlite:
+            self.requires("sqlite3/3.29.0")
+        if self.options.with_external_libs:
+            self.requires("flac/1.3.3")
+            self.requires("ogg/1.3.4")
+            self.requires("vorbis/1.3.6")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -42,6 +58,9 @@ class LibsndfileConan(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        self._cmake.definitions["BUILD_REGTEST"] = self.options.with_sqlite
+        self._cmake.definitions["ENABLE_EXTERNAL_LIBS"] = self.options.with_external_libs
         self._cmake.configure(source_folder=self._source_subfolder())
         return self._cmake
 
