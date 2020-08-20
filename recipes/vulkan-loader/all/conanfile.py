0a502793babd4f88288381051484cdbbe0c2f97c
Uilian Ries <uilianries@gmail.com>
2020-08-20 11:39:49 -0300

    Improve vulkan-loader
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/vulkan-loader/all/conanfile.py
+++ recipes/vulkan-loader/all/conanfile.py
@@ -5,8 +5,8 @@ from conans import ConanFile, tools, CMake
 
 class VulkanLoaderConan(ConanFile):
     name = "vulkan-loader"
-    description = "Vulkan Loader"
-    topics = ("conan", "vulkan", "loader")
+    description = "Khronos official Vulkan ICD desktop loader for Windows, Linux, and MacOS."
+    topics = ("conan", "vulkan", "loader", "desktop", "gpu")
     homepage = "https://github.com/KhronosGroup/Vulkan-Loader"
     url = "https://github.com/conan-io/conan-center-index"
     license = "Apache-2.0"
@@ -14,14 +14,12 @@ class VulkanLoaderConan(ConanFile):
     settings = "os", "arch", "build_type", "compiler"
     generators = "cmake", "cmake_find_package"
     options = {
-        "fPIC": [True, False],
         "with_wsi_xcb": [True, False],
         "with_wsi_xlib": [True, False],
         "with_wsi_wayland": [True, False],
         "with_wsi_directfb": [True, False],
     }
     default_options = {
-        "fPIC": True,
         "with_wsi_xcb": False,
         "with_wsi_xlib": False,
         "with_wsi_wayland": False,
@@ -38,14 +36,9 @@ class VulkanLoaderConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
     def configure(self):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
-
         if self.options.with_wsi_wayland:
             # TODO wayland package
             self.output.warn("Conan package for Wayland is not available, this package will be used from system.")
@@ -60,19 +53,14 @@ class VulkanLoaderConan(ConanFile):
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        url = self.conan_data["sources"][self.version]["url"]
-        version = os.path.basename(url).replace(".tar.gz", "").replace(".zip", "")
-        if version.startswith('v'):
-            version = version[1:]
-        extracted_dir = "Vulkan-Loader-" + version
-        os.rename(extracted_dir, self._source_subfolder)
+        os.rename(glob.glob("Vulkan-Loader-*")[0], self._source_subfolder)
 
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
+
         self._cmake = CMake(self)
         self._cmake.definitions["BUILD_TESTS"] = False
-
         self._cmake.definitions["BUILD_WSI_XCB_SUPPORT"] = self.options.with_wsi_xcb
         self._cmake.definitions["BUILD_WSI_XLIB_SUPPORT"] = self.options.with_wsi_xlib
         self._cmake.definitions["BUILD_WSI_WAYLAND_SUPPORT"] = self.options.with_wsi_wayland
@@ -83,7 +71,7 @@ class VulkanLoaderConan(ConanFile):
 
     def build(self):
         cmake = self._configure_cmake()
-        cmake.install()
+        cmake.build()
 
     def package(self):
         cmake = self._configure_cmake()
@@ -93,13 +81,13 @@ class VulkanLoaderConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "loader"))
 
     def package_info(self):
+        self.cpp_info.names["pkg_config"] = "vulkan"
+        self.cpp_info.filenames["cmake_find_package"] = "VulkanLoader"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "VulkanLoader"
+        self.cpp_info.names["cmake_find_package"] = "Vulkan"
+        self.cpp_info.names["cmake_find_package_multi"] = "Vulkan"
         self.cpp_info.libs = tools.collect_libs(self)
-
-        self.cpp_info.names["pkg_config"] = "Vulkan-Loader"
-        self.cpp_info.names["cmake_find_package"] = "Vulkan-Loader"
-        self.cpp_info.names["cmake_find_package_multi"] = "Vulkan-Loader"
-
         if self.settings.os != "Windows":
-            self.cpp_info.system_libs.extend(["dl", "pthread", "m"])
+            self.cpp_info.system_libs = ["dl", "pthread", "m"]
         if self.settings.os == "Macos":
-            self.cpp_info.frameworks.append("CoreFoundation")
+            self.cpp_info.frameworks = ["CoreFoundation"]
