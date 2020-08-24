564e91468ea79cf001d8fe49a038e4ef5c4d2cab
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-24 15:52:16 +0200

    jinja2cpp: simplify test_package

--- recipes/jinja2cpp/1.1.0/test_package/conanfile.py
+++ recipes/jinja2cpp/1.1.0/test_package/conanfile.py
@@ -1,22 +1,16 @@
 from conans import ConanFile, CMake, tools
 import os
 
-class Jinja2CppTestPackage(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
-        compiler = self.settings.get_safe("compiler")
-        if compiler == 'Visual Studio':
-            runtime = self.settings.get_safe("compiler.runtime")
-            cmake.definitions["MSVC_RUNTIME_TYPE"] = '/' + runtime
-            
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
-            ext = ".exe" if self.settings.os == "Windows" else ""
-            bin_path = os.path.join("bin", "jinja2cpp-test-package" + ext)
+            bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
