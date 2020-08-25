7d4269c706e0e3a08eae9ecb443b6eb2f610055e
Dominik Berner <dominik.berner@gmail.com>
2020-08-25 16:08:49 +0200

    Tidying up si/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/si/all/conanfile.py
+++ recipes/si/all/conanfile.py
@@ -1,4 +1,3 @@
-
 from conans import CMake, ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 from conans.tools import Version
@@ -15,9 +14,10 @@ class SiConan(ConanFile):
          for handling pyhsical values defined in the International System of Units."
     topics = ("physical units", "SI-unit-conversion",
               "cplusplus-library", "cplusplus-17")
-    exports_sources = "include/*", "CMakeLists.txt", "test/*", "doc/CMakeLists.txt", "doc/*.md"
+    exports_sources = "CMakeLists.txt"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
-    generators = "cmake", "cmake_find_package"
+    generators = "cmake"
     settings = {"compiler"}
 
     _cmake = None
@@ -30,12 +30,21 @@ class SiConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, "17")
+        elif not self._supports_cpp17():
+            raise ConanInvalidConfiguration("SI requires C++17 support")
+
     def _supports_cpp17(self):
         supported_compilers = [
             ("gcc", "7"), ("clang", "5"), ("apple-clang", "10"), ("Visual Studio", "15.7")]
         compiler = self.settings.compiler
         version = Version(compiler.version)
         return any(compiler == sc[0] and version >= sc[1] for sc in supported_compilers)
+
+    def package_id(self):
+        self.info.header_only()
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -46,9 +55,9 @@ class SiConan(ConanFile):
         if not self._cmake:
             self._cmake = CMake(self)
             # Add additional settings with cmake.definitions["SOME_DEFINITION"] = True
-            self._cmake.definitions["SI_BUILD_TESTING"] = "OFF"
-            self._cmake.definitions["SI_BUILD_DOC"] = "OFF"
-            self._cmake.definitions["SI_INSTALL_LIBRARY"] = "ON"
+            self._cmake.definitions["SI_BUILD_TESTING"] = False
+            self._cmake.definitions["SI_BUILD_DOC"] = False
+            self._cmake.definitions["SI_INSTALL_LIBRARY"] = True
             self._cmake.configure(
                 source_folder=self._source_subfolder, build_folder=self._build_subfolder)
         return self._cmake
@@ -63,10 +72,6 @@ class SiConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.build()
 
-    def test(self):
-        cmake = self._configure_cmake()
-        cmake.test()
-
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
@@ -74,5 +79,6 @@ class SiConan(ConanFile):
         #  Remove folder containing cmake-specific files such as SIConfig.cmake and SIVersion.cmake
         tools.rmdir(os.path.join(self.package_folder, "share"))
 
-    def package_id(self):
-        self.info.header_only()
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "SI"
+        self.cpp_info.names["cmake_find_package_multi"] = "SI"
