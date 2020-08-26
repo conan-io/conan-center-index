68e30b4680d69e61032fe6e3985ebfe92714b295
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-26 19:24:05 -0400

    Add freeimage 3.18.0

---
+++ recipes/freeimage/all/test_package/conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "cmake_find_package"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            bees = os.path.join(self.source_folder, "test.png")
+            self.run("{} {}".format(bin_path, bees), run_environment=True)
