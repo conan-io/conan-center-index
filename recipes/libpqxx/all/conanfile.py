3bce9122629027cbdff87cad5c66ee9debff88fa
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-26 16:55:52 +0200

    libpqxx: delete fPIC option if shared

--- recipes/libpqxx/all/conanfile.py
+++ recipes/libpqxx/all/conanfile.py
@@ -32,6 +32,9 @@ class LibpqxxRecipe(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
         compiler = str(self.settings.compiler)
         compiler_version = Version(self.settings.compiler.version.value)
 
