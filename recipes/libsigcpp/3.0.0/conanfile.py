370bbb48bdee2bbf934898774c07cfc5d18c06c0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-27 14:07:27 +0200

    libsigcpp: cache cmake

--- recipes/libsigcpp/3.0.0/conanfile.py
+++ recipes/libsigcpp/3.0.0/conanfile.py
@@ -19,6 +19,8 @@ class LibSigCppConan(ConanFile):
     exports_sources = ["CMakeLists.txt", "patches/**"]
     generators = "cmake"
 
+    _cmake = None
+
     @property
     def _source_subfolder(self):
         return "source_subfolder"
@@ -56,9 +58,11 @@ class LibSigCppConan(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
 
     def build(self):
         for patch in self.conan_data["patches"][self.version]:
