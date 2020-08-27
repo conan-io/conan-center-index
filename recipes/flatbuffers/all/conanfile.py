00b82e9b5dc1c9fd1e242f7531bc7c25105a3dab
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-27 11:01:33 +0200

    flatbuffers: cache cmake

--- recipes/flatbuffers/all/conanfile.py
+++ recipes/flatbuffers/all/conanfile.py
@@ -1,5 +1,3 @@
-"""Conan recipe package for Google FlatBuffers
-"""
 import glob
 import os
 import shutil
@@ -19,6 +17,8 @@ class FlatbuffersConan(ConanFile):
     exports_sources = "CMakeLists.txt"
     generators = "cmake"
 
+    _cmake = None
+
     @property
     def _source_subfolder(self):
         return "source_subfolder"
@@ -27,24 +27,30 @@ class FlatbuffersConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def package_id(self):
+        if self.options.header_only:
+            self.info.header_only()
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["FLATBUFFERS_BUILD_TESTS"] = False
-        cmake.definitions["FLATBUFFERS_BUILD_SHAREDLIB"] = self.options.shared
-        cmake.definitions["FLATBUFFERS_BUILD_FLATLIB"] = not self.options.shared
-        cmake.definitions["FLATBUFFERS_BUILD_FLATC"] = False
-        cmake.definitions["FLATBUFFERS_BUILD_FLATHASH"] = False
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["FLATBUFFERS_BUILD_TESTS"] = False
+        self._cmake.definitions["FLATBUFFERS_BUILD_SHAREDLIB"] = self.options.shared
+        self._cmake.definitions["FLATBUFFERS_BUILD_FLATLIB"] = not self.options.shared
+        self._cmake.definitions["FLATBUFFERS_BUILD_FLATC"] = False
+        self._cmake.definitions["FLATBUFFERS_BUILD_FLATHASH"] = False
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
 
     def build(self):
         if not self.options.header_only:
@@ -65,10 +71,6 @@ class FlatbuffersConan(ConanFile):
                 tools.mkdir(os.path.join(self.package_folder, "bin"))
                 for dll_path in glob.glob(os.path.join(self.package_folder, "lib", "*.dll")):
                     shutil.move(dll_path, os.path.join(self.package_folder, "bin", os.path.basename(dll_path)))
-
-    def package_id(self):
-        if self.options.header_only:
-            self.info.header_only()
 
     def package_info(self):
         if not self.options.header_only:
