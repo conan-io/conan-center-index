355dd72b708097a7fa38ac367dc64a72dc2bedb7
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-28 22:30:55 +0200

    libwebp: delete fPIC option if shared

--- recipes/libwebp/all/conanfile.py
+++ recipes/libwebp/all/conanfile.py
@@ -30,6 +30,8 @@ class LibwebpConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
