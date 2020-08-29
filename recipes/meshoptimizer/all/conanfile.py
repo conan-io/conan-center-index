289649adf5a3dca8150abf01819b15f2c7fd4e51
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-29 17:37:41 +0200

    meshoptimizer: don't override warning levels

--- recipes/meshoptimizer/all/conanfile.py
+++ recipes/meshoptimizer/all/conanfile.py
@@ -54,9 +54,15 @@ class MeshOptimizerConan(ConanFile):
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
-    def build(self):
+    def _patch_sources(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
+        # Don't override warning levels for msvc
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+                              "add_compile_options(/W4 /WX)", "")
+
+    def build(self):
+        self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
 
