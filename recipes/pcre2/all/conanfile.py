b23d9ff6caae994872a90dd85a24b4abc90da756
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-09-01 11:26:12 +0200

    pcre2: delete fPIC option if shared

--- recipes/pcre2/all/conanfile.py
+++ recipes/pcre2/all/conanfile.py
@@ -51,6 +51,8 @@ class PCREConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
         if not self.options.build_pcre2grep:
