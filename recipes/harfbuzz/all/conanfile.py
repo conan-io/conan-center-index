f8de9c61e8d5a584e69085b0b822ec419eed8c25
bincrafters-user <bincrafters@gmail.com>
2020-09-01 13:13:52 +0000

    harfbuzz: Update Conan conventions
    Automatically created by bincrafters-conventions 0.24.5

--- recipes/harfbuzz/all/conanfile.py
+++ recipes/harfbuzz/all/conanfile.py
@@ -41,7 +41,7 @@ class HarfbuzzConan(ConanFile):
 
     def requirements(self):
         if self.options.with_freetype:
-            self.requires("freetype/2.10.1")
+            self.requires("freetype/2.10.2")
         if self.options.with_icu:
             self.requires("icu/64.2")
         if self.options.with_glib:
