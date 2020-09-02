327bf947d09b0c398263168dbb68fdaff4175719
René Ferdinand Rivera Morell <grafikrobot@gmail.com>
2020-09-02 03:54:44 -0500

    Remove ligering BSL license statements. (#2724)
    Not ideal as MIT is a more restrictive license than BS. And of
    questionable benefit as it was present only in the test sources.
    But it makes things slightly more consistent.

--- recipes/lyra/all/test_package/test.cpp
+++ recipes/lyra/all/test_package/test.cpp
@@ -1,10 +1,3 @@
-/*
-Copyright Rene Rivera 2019
-Distributed under the Boost Software License, Version 1.0.
-(See accompanying file LICENSE.txt or copy at
-http://www.boost.org/LICENSE_1_0.txt)
-*/
-
 #include <lyra/lyra.hpp>
 
 int main(int argc, const char** argv)
