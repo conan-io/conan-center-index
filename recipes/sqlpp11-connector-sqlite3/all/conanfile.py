8164048f67043196aa35077bffc3832579ac56a9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-09-03 00:20:36 +0200

    sqlpp11-connector-sqlite3: fix cmake

--- recipes/sqlpp11-connector-sqlite3/all/conanfile.py
+++ recipes/sqlpp11-connector-sqlite3/all/conanfile.py
@@ -13,9 +13,17 @@ class sqlpp11Conan(ConanFile):
     generators = "cmake"
     options = {"shared": [True, False], "fPIC": [True, False], "with_sqlcipher": [True, False]}
     default_options = {"shared": False, "fPIC": True, "with_sqlcipher": False}
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
     short_paths = True
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -34,12 +42,14 @@ class sqlpp11Conan(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["ENABLE_TESTS"] = False
-        cmake.definitions["SQLCIPHER"] = self.options.with_sqlcipher
-        cmake.definitions["SQLPP11_INCLUDE_DIR"] = self.deps_cpp_info["sqlpp11"].include_paths[0]
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["ENABLE_TESTS"] = False
+        self._cmake.definitions["SQLCIPHER"] = self.options.with_sqlcipher
+        self._cmake.definitions["SQLPP11_INCLUDE_DIR"] = self.deps_cpp_info["sqlpp11"].include_paths[0]
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
 
     def build(self):
         cmake = self._configure_cmake()
