98a29440a6ba258adafb469172d6eba7641a78b4
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-09-04 10:20:36 +0200

    cspice: delete fPIC option if shared

--- recipes/cspice/all/conanfile.py
+++ recipes/cspice/all/conanfile.py
@@ -28,6 +28,8 @@ class CspiceConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
         self._raise_if_not_supported_triplet()
