7c083403a87ff86b49646ae0e3bd6d0a202fd965
Chris Mc <prince.chrismc@gmail.com>
2020-09-03 20:36:37 -0400

    Update test_package.cpp

--- recipes/skyr-url/all/test_package/test_package.cpp
+++ recipes/skyr-url/all/test_package/test_package.cpp
@@ -1,8 +1,3 @@
-// Copyright 2018-19 Glyn Matthews.
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt of copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-
 #include <iostream>
 #include <skyr/url.hpp>
 
