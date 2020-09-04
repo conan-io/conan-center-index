7b6ca6c7ec4c059b4cbba7856fff2748c9c1e011
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-09-04 15:36:26 +0200

    onedpl: fix CMake imported target (#2726)

--- recipes/onedpl/all/test_package/conanfile.py
+++ recipes/onedpl/all/test_package/conanfile.py
@@ -3,9 +3,9 @@ import os
 from conans import ConanFile, CMake, tools
 
 
-class ParallelSTLTestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = ["cmake", "cmake_find_package"]
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -14,5 +14,5 @@ class ParallelSTLTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "pstl_test_package")
+            bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
