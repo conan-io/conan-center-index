907cfb69dd26c88a2f2a8e25b01939c4dc634471
Tian Xiao <xiaotianrandom@gmail.com>
2020-09-11 15:39:16 +0800

    Add a test for shared=False and with_asio=True

---
+++ recipes/libnghttp2/all/test_package_static_asio/conanfile.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    default_options = {
+        "libnghttp2:shared": False,
+        "libnghttp2:with_asio": True,
+    }
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
