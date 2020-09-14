17b90915758e9a3ae21018836abe11f7f4672083
Chris Mc <prince.chrismc@gmail.com>
2020-09-14 08:36:29 -0400

    add minimum compiler version

--- recipes/cxxopts/all/conanfile.py
+++ recipes/cxxopts/all/conanfile.py
@@ -1,5 +1,6 @@
 import os
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 
 
 class CxxOptsConan(ConanFile):
@@ -18,9 +19,30 @@ class CxxOptsConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    @property
+    def _minimum_cpp_standard(self):
+        return 11
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "14",
+            "gcc": "5",
+            "clang": "3.8",
+            "apple-clang": "8",
+        }
+
     def configure(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "11")
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
 
     def requirements(self):
         if self.options.unicode:
