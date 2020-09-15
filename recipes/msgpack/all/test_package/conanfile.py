4afb905e518f6d666f173b2fd6256a0e388e1d78
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-09-15 20:46:16 +0200

    msgpack: add 3.3.0 + refactor options + fix boost dependency + add components (#2721)

--- recipes/msgpack/all/test_package/conanfile.py
+++ recipes/msgpack/all/test_package/conanfile.py
@@ -4,14 +4,20 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["MSGPACK_C_API"] = self.options["msgpack"].c_api
+        cmake.definitions["MSGPACK_CPP_API"] = self.options["msgpack"].cpp_api
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+            if self.options["msgpack"].c_api:
+                bin_c_path = os.path.join("bin", "test_package_c")
+                self.run(bin_c_path, run_environment=True)
+            if self.options["msgpack"].cpp_api:
+                bin_cpp_path = os.path.join("bin", "test_package_cpp")
+                self.run(bin_cpp_path, run_environment=True)
