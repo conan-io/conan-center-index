9be8efd8730d0af64c4b4c5adb760eee92b9a4e5
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-09-18 18:00:06 +0200

    subunit: add subunit/1.4.0 recipe

---
+++ recipes/subunit/all/conanfile.py
@@ -0,0 +1,117 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from contextlib import contextmanager
+import glob
+import os
+
+
+class SubunitConan(ConanFile):
+    name = "subunit"
+    description = "A streaming protocol for test results"
+    topics = "conan", "subunit", "streaming", "protocol", "test", "results"
+    license = "Apache-2.0", "BSD-3-Clause"
+    homepage = "https://launchpad.net/subunit"
+    url = "https://github.com/conan-io/conan-center-index"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    exports_sources = "patches/**"
+    generators = "pkg_config"
+
+    _autotools = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def requirements(self):
+        self.requires("cppunit/1.15.1")
+
+    def build_requirements(self):
+        self.build_requires("pkgconf/1.7.3")
+        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/20200517")
+        if self.settings.compiler == "Visual Studio":
+            self.build_requires("automake/1.16.2")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("{}-{}".format(self.name, self.version), self._source_subfolder)
+
+    @contextmanager
+    def _build_context(self):
+        env = {}
+        if self.settings.compiler == "Visual Studio":
+            with tools.vcvars(self.settings):
+                env.update({
+                    "AR": "{} lib".format(tools.unix_path(self.deps_user_info["automake"].ar_lib)),
+                    "CC": "{} cl -nologo".format(tools.unix_path(self.deps_user_info["automake"].compile)),
+                    "CXX": "{} cl -nologo".format(tools.unix_path(self.deps_user_info["automake"].compile)),
+                    "NM": "dumpbin -symbols",
+                    "OBJDUMP": ":",
+                    "RANLIB": ":",
+                    "STRIP": ":",
+                })
+                with tools.environment_append(env):
+                    yield
+        else:
+            with tools.environment_append(env):
+                yield
+
+    def _configure_autotools(self):
+        if self._autotools:
+            return self._autotools
+        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        self._autotools.libs = []
+        yes_no = lambda v: "yes" if v else "no"
+        conf_args = [
+            "--enable-shared={}".format(yes_no(self.options.shared)),
+            "--enable-static={}".format(yes_no(not self.options.shared)),
+            "CHECK_CFLAGS=' '",
+            "CHECK_LIBS=' '",
+        ]
+        self._autotools.configure(args=conf_args, configure_dir=self._source_subfolder)
+        return self._autotools
+
+    def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.make(args=["-j1"])
+
+    def package(self):
+        self.copy("COPYING", src=self._source_subfolder, dst="licenses")
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.install()
+
+        os.unlink(os.path.join(self.package_folder, "lib", "libcppunit_subunit.la"))
+        os.unlink(os.path.join(self.package_folder, "lib", "libsubunit.la"))
+        for d in glob.glob(os.path.join(self.package_folder, "lib", "python*")):
+            tools.rmdir(d)
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.components["libsubunit"].libs = ["subunit"]
+        self.cpp_info.components["libsubunit"].names["pkgconfig"] = "libsubunit"
+        self.cpp_info.components["libcppunit_subunit"].libs = ["cppunit_subunit"]
+        self.cpp_info.components["libcppunit_subunit"].requires = ["cppunit::cppunit"]
+        self.cpp_info.components["libcppunit_subunit"].names["pkgconfig"] = "libcppunit_subunit"
+
+        bin_path = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(bin_path))
+        self.env_info.PATH.append(bin_path)
