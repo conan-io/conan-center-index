0a3fc856e4166ba51ede3ccd91f341e13d1a30a2
Anonymous Maarten <madebr@users.noreply.github.com>
2020-09-18 11:31:47 +0200

    (#2912) doxygen: build from sources + add 1.8.20
    * doxygen: build from sources + add 1.8.20
    
    * doxygen: fix doxysearch WIN_EXTRA_LIBS libs + remove bison/flex cmake modules
    
    * doxygen: remove Findxapian.cmake file
    
    * doxygen: link to libuuid too
    
    * doxysearch: use xapiann find_package module

--- recipes/doxygen/all/test_package/conanfile.py
+++ recipes/doxygen/all/test_package/conanfile.py
@@ -1,13 +1,20 @@
 from conans import ConanFile, CMake, tools
+import os
+
 
 class TestPackageConan(ConanFile):
-    generators = "cmake_paths"
+    generators = "cmake"
+
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        if not tools.cross_building(self.settings, skip_x64_x86=True):
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
+            if not os.path.isdir(os.path.join(self.build_folder, "html")):
+                raise ConanException("doxygen did not create html documentation directory")
+
             self.output.info("Version:")
             self.run("doxygen --version", run_environment=True)
