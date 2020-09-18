b84a7f9189c43b0ebad2cda5cc2b2bb7e0c26b43
Riff, Eric <eriff@irobot.com>
2020-09-18 13:20:30 -0300

    Simplify test_package

--- recipes/tinyalsa/all/test_package/example.c
+++ recipes/tinyalsa/all/test_package/example.c
@@ -1,13 +1,11 @@
-
-// This test_package has been taken from examples/pcm-readi.c
-
 #include <stdio.h>
-#include <stdlib.h>
 
 #include <tinyalsa/pcm.h>
 
-static size_t read_frames(void **frames)
+int main(void)
 {
+    printf("TinyALSA test_package\n");
+
     unsigned int card = 0;
     unsigned int device = 0;
     int flags = PCM_IN;
@@ -24,63 +22,7 @@ static size_t read_frames(void **frames)
     };
 
     struct pcm *pcm = pcm_open(card, device, flags, &config);
-    if (pcm == NULL) {
-        fprintf(stderr, "failed to allocate memory for PCM\n");
-        return 0;
-    } else if (!pcm_is_ready(pcm)){
-        pcm_close(pcm);
-        fprintf(stderr, "failed to open PCM\n");
-        return 0;
-    }
-
-    unsigned int frame_size = pcm_frames_to_bytes(pcm, 1);
-    unsigned int frames_per_sec = pcm_get_rate(pcm);
-
-    *frames = malloc(frame_size * frames_per_sec);
-    if (*frames == NULL) {
-        fprintf(stderr, "failed to allocate frames\n");
-        pcm_close(pcm);
-        return 0;
-    }
-
-    int read_count = pcm_readi(pcm, *frames, frames_per_sec);
-
-    size_t byte_count = pcm_frames_to_bytes(pcm, read_count);
-
     pcm_close(pcm);
 
-    return byte_count;
-}
-
-static int write_file(const void *frames, size_t size)
-{
-    FILE *output_file = fopen("audio.raw", "wb");
-    if (output_file == NULL) {
-        perror("failed to open 'audio.raw' for writing");
-        return EXIT_FAILURE;
-    }
-    fwrite(frames, 1, size, output_file);
-    fclose(output_file);
     return 0;
-}
-
-int main(void)
-{
-    fprintf(stdout, "TinyALSA test_package\n");
-    void *frames = NULL;
-    size_t size = 0;
-
-    size = read_frames(&frames);
-    if (size == 0) {
-        return EXIT_FAILURE;
-    }
-
-    if (write_file(frames, size) < 0) {
-        free(frames);
-        return EXIT_FAILURE;
-    }
-
-    free(frames);
-
-    return EXIT_SUCCESS;
 }
