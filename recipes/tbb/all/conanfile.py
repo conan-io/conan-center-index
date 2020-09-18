7d67c7b44caaeacc3e80652fc8286bdcb52f982c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-09-18 11:01:45 +0200

    (#2934) tbb: add components
    * tbb: reorder methods and few cosmetic changes
    
    * tbb: add components

--- recipes/tbb/all/conanfile.py
+++ recipes/tbb/all/conanfile.py
@@ -1,6 +1,5 @@
 import os
 from conans import ConanFile, tools
-from conans.model.version import Version
 from conans.errors import ConanInvalidConfiguration
 
 
@@ -14,23 +13,31 @@ programs that take full advantage of multicore performance, that are portable an
 that have future-proof scalability"""
     topics = ("conan", "tbb", "threading", "parallelism", "tbbmalloc")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False], "tbbmalloc": [True, False], "tbbproxy": [True, False]}
-    default_options = {"shared": True, "fPIC": True, "tbbmalloc": False, "tbbproxy": False}
-    _source_subfolder = "source_subfolder"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "tbbmalloc": [True, False],
+        "tbbproxy": [True, False]
+    }
+    default_options = {
+        "shared": True,
+        "fPIC": True,
+        "tbbmalloc": False,
+        "tbbproxy": False
+    }
 
-    def build_requirements(self):
-        if tools.os_info.is_windows:
-            if "CONAN_MAKE_PROGRAM" not in os.environ and not tools.which("make"):
-                self.build_requires("make/4.2.1")
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
 
     def config_options(self):
-        if self.settings.os == 'Windows':
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
         if self.settings.os == "Macos" and \
            self.settings.compiler == "apple-clang" and \
-           Version(self.settings.compiler.version.value) < "8.0":
+           tools.Version(self.settings.compiler.version) < "8.0":
             raise ConanInvalidConfiguration("%s %s couldn't be built by apple-clang < 8.0" % (self.name, self.version))
         if not self.options.shared:
             self.output.warn("Intel-TBB strongly discourages usage of static linkage")
@@ -39,17 +46,26 @@ that have future-proof scalability"""
             not self.options.tbbmalloc):
             raise ConanInvalidConfiguration("tbbproxy needs tbbmaloc and shared options")
 
+    def package_id(self):
+        del self.info.options.tbbmalloc
+        del self.info.options.tbbmalloc_proxy
+
+    def build_requirements(self):
+        if tools.os_info.is_windows:
+            if "CONAN_MAKE_PROGRAM" not in os.environ and not tools.which("make"):
+                self.build_requires("make/4.2.1")
+
     @property
     def _is_msvc(self):
-        return self.settings.compiler == 'Visual Studio'
+        return self.settings.compiler == "Visual Studio"
 
     @property
     def _is_mingw(self):
-        return self.settings.os == 'Windows' and self.settings.compiler == 'gcc'
+        return self.settings.os == "Windows" and self.settings.compiler == "gcc"
 
     @property
     def _is_clanglc(self):
-        return self.settings.os == 'Windows' and self.settings.compiler == 'clang'
+        return self.settings.os == "Windows" and self.settings.compiler == "clang"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -60,18 +76,10 @@ that have future-proof scalability"""
         tools.replace_in_file(linux_include, "shell gcc", "shell $(CC)")
         tools.replace_in_file(linux_include, "= gcc", "= $(CC)")
 
-    def _get_targets(self):
-        targets = ["tbb"]
-        if self.options.tbbmalloc:
-            targets.append("tbbmalloc")
-        if self.options.tbbproxy:
-            targets.append("tbbproxy")
-        return targets
-
     def build(self):
         def add_flag(name, value):
             if name in os.environ:
-                os.environ[name] += ' ' + value
+                os.environ[name] += " " + value
             else:
                 os.environ[name] = value
 
@@ -111,23 +119,23 @@ MALLOCPROXY.DEF =
                 "x86_64": "intel64",
                 "armv7":  "armv7",
                 "armv8": "aarch64"}.get(str(self.settings.arch))
-        if self.settings.compiler in ['gcc', 'clang', 'apple-clang']:
-            if str(self.settings.compiler.libcxx) in ['libstdc++', 'libstdc++11']:
+        if self.settings.compiler in ["gcc", "clang", "apple-clang"]:
+            if str(self.settings.compiler.libcxx) in ["libstdc++", "libstdc++11"]:
                 extra += " stdlib=libstdc++"
-            elif str(self.settings.compiler.libcxx) == 'libc++':
+            elif str(self.settings.compiler.libcxx) == "libc++":
                 extra += " stdlib=libc++"
-            extra += " compiler=gcc" if self.settings.compiler == 'gcc' else " compiler=clang"
+            extra += " compiler=gcc" if self.settings.compiler == "gcc" else " compiler=clang"
 
             extra += " gcc_version={}".format(str(self.settings.compiler.version))
-        make = tools.get_env("CONAN_MAKE_PROGRAM", tools.which("make") or tools.which('mingw32-make'))
+        make = tools.get_env("CONAN_MAKE_PROGRAM", tools.which("make") or tools.which("mingw32-make"))
         if not make:
             raise ConanInvalidConfiguration("This package needs 'make' in the path to build")
 
         with tools.chdir(self._source_subfolder):
             # intentionally not using AutoToolsBuildEnvironment for now - it's broken for clang-cl
             if self._is_clanglc:
-                add_flag('CFLAGS', '-mrtm')
-                add_flag('CXXFLAGS', '-mrtm')
+                add_flag("CFLAGS", "-mrtm")
+                add_flag("CXXFLAGS", "-mrtm")
 
             targets = ["tbb", "tbbmalloc", "tbbproxy"]
             if self._is_msvc:
@@ -175,16 +183,30 @@ MALLOCPROXY.DEF =
                     self.run("ln -s \"%s\" \"%s\"" %
                              (fpath, fpath[0:fpath.rfind("." + extension) + len(extension) + 1]))
 
-    def package_id(self):
-        del self.info.options.tbbmalloc
-        del self.info.options.tbbmalloc_proxy
-
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "TBB"
         self.cpp_info.names["cmake_find_package_multi"] = "TBB"
-        suffix = "_debug" if self.settings.build_type == "Debug" else ""
-        libs = {"tbb": "tbb", "tbbproxy": "tbbmalloc_proxy", "tbbmalloc": "tbbmalloc"}
-        targets = self._get_targets()
-        self.cpp_info.libs = ["{}{}".format(libs[target], suffix) for target in targets]
+        # tbb
+        self.cpp_info.components["libtbb"].names["cmake_find_package"] = "tbb"
+        self.cpp_info.components["libtbb"].names["cmake_find_package_multi"] = "tbb"
+        self.cpp_info.components["libtbb"].libs = [self._lib_name("tbb")]
         if self.settings.os == "Linux":
-            self.cpp_info.system_libs.extend(["dl", "rt", "m", "pthread"])
+            self.cpp_info.components["libtbb"].system_libs = ["dl", "rt", "pthread"]
+        # tbbmalloc
+        if self.options.tbbmalloc:
+            self.cpp_info.components["tbbmalloc"].names["cmake_find_package"] = "tbbmalloc"
+            self.cpp_info.components["tbbmalloc"].names["cmake_find_package_multi"] = "tbbmalloc"
+            self.cpp_info.components["tbbmalloc"].libs = [self._lib_name("tbbmalloc")]
+            if self.settings.os == "Linux":
+                self.cpp_info.components["tbbmalloc"].system_libs = ["dl", "pthread"]
+            # tbbmalloc_proxy
+            if self.options.tbbproxy:
+                self.cpp_info.components["tbbmalloc_proxy"].names["cmake_find_package"] = "tbbmalloc_proxy"
+                self.cpp_info.components["tbbmalloc_proxy"].names["cmake_find_package_multi"] = "tbbmalloc_proxy"
+                self.cpp_info.components["tbbmalloc_proxy"].libs = [self._lib_name("tbbmalloc_proxy")]
+                self.cpp_info.components["tbbmalloc_proxy"].requires = ["tbbmalloc"]
+
+    def _lib_name(self, name):
+        if self.settings.build_type == "Debug":
+            return name + "_debug"
+        return name
