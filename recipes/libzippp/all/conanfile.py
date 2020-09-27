bde8a38addc996d8ac87a066d57e9eb78f8614f5
bruno <bruno.manga95@gmail.com>
2020-09-27 15:16:57 +0200

    Add libzippp recipe
    Update recipes/libzippp/all/test_package/CMakeLists.txt
    
    Co-authored-by: Yoann Potinet <intelligide@hotmail.fr>
    
    Update libzippp's license name

---
+++ recipes/libzippp/all/conanfile.py
@@ -0,0 +1,76 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class LibZipppConan(ConanFile):
+    name = "libzippp"
+    description = "A simple basic C++ wrapper around the libzip library"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/ctabin/libzippp"
+    license = "BSD-3-Clause"
+    topics = ("conan", "zip", "libzippp", "zip-archives", "zip-editing")
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake", "cmake_find_package"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def requirements(self):
+        self.requires("zlib/1.2.11")
+        self.requires("libzip/1.7.3")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        # eg. 'libzippp-libzippp-v4.0-1.7.3'
+        extracted_dir = self.name + "-" + self.name + "-v" + self.version + \
+            "-" + self.deps_cpp_info["libzip"].version
+        os.rename(extracted_dir, self._source_subfolder)
+        tools.replace_in_file('source_subfolder/CMakeLists.txt',
+                              'find_package(LIBZIP MODULE REQUIRED)',
+                              'find_package(libzip REQUIRED)')
+        tools.replace_in_file('source_subfolder/CMakeLists.txt',
+                              'libzip::zip', 'libzip::libzip')
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["LIBZIPPP_INSTALL"] = True
+        self._cmake.definitions["LIBZIPPP_INSTALL_HEADERS"] = True
+        self._cmake.configure()
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+        self.copy(pattern="LICENCE", dst="licenses", src=self._source_subfolder)
+        tools.rmdir(os.path.join(self.package_folder, "share"))
+        tools.rmdir(os.path.join(self.package_folder, "cmake"))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.names["cmake_find_package"] = "libzippp"
+        self.cpp_info.names["cmake_find_package_multi"] = "libzippp"
