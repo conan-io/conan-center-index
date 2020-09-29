a6c3136887e4e1a3ab6019953587e3a6f7e25ab1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-09-29 10:44:11 +0200

    rabbitmq-c: fix component requires

--- recipes/rabbitmq-c/all/conanfile.py
+++ recipes/rabbitmq-c/all/conanfile.py
@@ -93,3 +93,5 @@ class RabbitmqcConan(ConanFile):
                 self.cpp_info.components["rabbitmq"].system_libs.append("pthread")
         if not self.options.shared:
             self.cpp_info.components["rabbitmq"].defines.append("AMQP_STATIC")
+        if self.options.ssl:
+            self.cpp_info.components["rabbitmq"].requires.append("openssl::openssl")
