06975c7d4169591b9ad0dc5c87737545f012a5e1
Yoann Potinet <yoann.potinet@gmail.com>
2020-09-28 20:09:14 -0400

    Apply suggestions from code review
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/butteraugli/all/conanfile.py
+++ recipes/butteraugli/all/conanfile.py
@@ -71,3 +71,5 @@ class ButteraugliConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["m"]
