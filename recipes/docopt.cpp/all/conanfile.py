663c2daa21048af4c0fc48cfefc5bdea6f490491
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-09-29 10:39:56 +0200

    docopt.cpp: bump boost

--- recipes/docopt.cpp/all/conanfile.py
+++ recipes/docopt.cpp/all/conanfile.py
@@ -37,7 +37,7 @@ class DocoptCppConan(ConanFile):
 
     def requirements(self):
         if self.options.boost_regex:
-            self.requires("boost/1.73.0")
+            self.requires("boost/1.74.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
