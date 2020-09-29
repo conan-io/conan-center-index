ae525048c136f91a6d7740d812baa006134cd226
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-09-29 22:31:30 +0200

    (#2928) abseil: add components + cache cmake
    * abseil: cache cmake and patch in build method
    
    * abseil: just check min cppstd
    
    * abseil: add components
    
    * abseil: C++11 is required in test_package
    
    * abseil: use cmake_find_package in test_package
    
    cmake_find_package_multi doesn't take into account frameworks
    in conan 1.28.1.

--- recipes/abseil/all/conanfile.py
+++ recipes/abseil/all/conanfile.py
@@ -1,5 +1,6 @@
-import os
 import glob
+import json
+import os
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration, ConanException
 
@@ -15,71 +16,48 @@ class ConanRecipe(ConanFile):
 
     license = "Apache-2.0"
 
-    settings = "os", "arch", "compiler", "build_type"
+    exports = "components/**"
+    generators = "cmake"
+    short_paths = True
 
+    settings = "os", "arch", "compiler", "build_type"
     options = {"fPIC": [True, False]}
     default_options = {"fPIC": True}
 
-    generators = "cmake"
-    short_paths = True
+    _cmake = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = glob.glob('abseil-cpp-*/')[0]
-        os.rename(extracted_dir, self._source_subfolder)
-        tools.replace_in_file(
-            os.path.join(self._source_subfolder, "CMakeLists.txt"),
-            "project(absl CXX)", """project(absl CXX)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()""")
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
-        minimal_cpp_standard = "11"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, 11)
 
-        try:
-            tools.check_min_cppstd(self, minimal_cpp_standard)
-        except ConanInvalidConfiguration:
-            raise
-        except ConanException:
-            # FIXME: We need to handle the case when Conan doesn't know
-            # about a user defined compiler's default standard version
-            self.output.warn(
-                "Unnable to determine the default standard version of the compiler")
-
-        minimal_version = {
-            "Visual Studio": "14",
-        }
-
-        compiler = str(self.settings.compiler)
-        if compiler not in minimal_version:
-            self.output.warn(
-                "%s recipe lacks information about the %s compiler support" % (self.name, compiler))
-            self.output.warn(
-                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
-            return
-
-        version = tools.Version(self.settings.compiler.version)
-        if version < minimal_version[compiler]:
-            raise ConanInvalidConfiguration(
-                "%s requires at least %s %s" % (self.name, compiler, minimal_version[compiler]))
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = glob.glob('abseil-cpp-*/')[0]
+        os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = False
-        cmake.configure(
-            source_folder=self._source_subfolder
-        )
-        return cmake
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["ABSL_ENABLE_INSTALL"] = True
+        self._cmake.definitions["BUILD_TESTING"] = False
+        self._cmake.configure(source_folder=self._source_subfolder)
+        return self._cmake
 
     def build(self):
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "CMakeLists.txt"),
+            "project(absl CXX)", """project(absl CXX)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()""")
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -90,64 +68,24 @@ conan_basic_setup()""")
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.libs = [
-            "absl_flags_parse",
-            "absl_flags_usage",
-            "absl_flags_usage_internal",
-            "absl_flags",
-            "absl_flags_internal",
-            "absl_flags_registry",
-            "absl_flags_config",
-            "absl_flags_program_name",
-            "absl_flags_marshalling",
-            "absl_raw_hash_set",
-            "absl_random_seed_sequences",
-            "absl_hashtablez_sampler",
-            "absl_synchronization",
-            "absl_time",
-            "absl_civil_time",
-            "absl_time_zone",
-            "absl_failure_signal_handler",
-            "absl_random_internal_distribution_test_util",
-            "absl_examine_stack",
-            "absl_symbolize",
-            "absl_str_format_internal",
-            "absl_graphcycles_internal",
-            "absl_stacktrace",
-            "absl_malloc_internal",
-            "absl_demangle_internal",
-            "absl_debugging_internal",
-            "absl_periodic_sampler",
-            "absl_exponential_biased",
-            "absl_random_internal_pool_urbg",
-            "absl_random_distributions",
-            "absl_random_internal_seed_material",
-            "absl_random_seed_gen_exception",
-            "absl_hash",
-            "absl_strings",
-            "absl_strings_internal",
-            "absl_bad_variant_access",
-            "absl_throw_delegate",
-            "absl_city",
-            "absl_base",
-            "absl_dynamic_annotations",
-            "absl_bad_any_cast_impl",
-            "absl_scoped_set_env",
-            "absl_bad_optional_access",
-            "absl_raw_logging_internal",
-            "absl_log_severity",
-            "absl_spinlock_wait",
-            "absl_random_internal_randen",
-            "absl_random_internal_randen_hwaes",
-            "absl_random_internal_randen_slow",
-            "absl_random_internal_randen_hwaes_impl",
-            "absl_leak_check",
-            "absl_leak_check_disable",
-            "absl_int128"
-        ]
-        if self.settings.os == "Linux":
-            self.cpp_info.system_libs.append("pthread")
-        if self.settings.os == "Macos":
-            self.cpp_info.frameworks.append("CoreFoundation")
         self.cpp_info.names["cmake_find_package"] = "absl"
         self.cpp_info.names["cmake_find_package_multi"] = "absl"
+        self._register_components()
+
+    def _register_components(self):
+        components_json_filepath = os.path.join(self.recipe_folder, "components", "components_{}.json".format(self.version))
+        with open(components_json_filepath) as components_json_file:
+            abseil_components = json.load(components_json_file)
+            for component in abseil_components:
+                self._register_component(component)
+
+    def _register_component(self, component):
+        conan_name = component["conan_name"]
+        cmake_target = component["cmake_target"]
+        self.cpp_info.components[conan_name].names["cmake_find_package"] = cmake_target
+        self.cpp_info.components[conan_name].names["cmake_find_package_multi"] = cmake_target
+        self.cpp_info.components[conan_name].libs = component.get("libs", [])
+        self.cpp_info.components[conan_name].defines = component.get("defines", [])
+        self.cpp_info.components[conan_name].system_libs = component.get("system_libs", {}).get(str(self.settings.os), [])
+        self.cpp_info.components[conan_name].frameworks = component.get("frameworks", [])
+        self.cpp_info.components[conan_name].requires = component.get("requires", [])
