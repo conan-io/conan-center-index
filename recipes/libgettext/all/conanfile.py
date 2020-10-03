5649ef2356ea4d450cc1d731a50209d3d4f3ee8a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-10-04 00:24:11 +0200

    bump msys2

--- recipes/libgettext/all/conanfile.py
+++ recipes/libgettext/all/conanfile.py
@@ -51,7 +51,7 @@ class GetTextConan(ConanFile):
         if tools.os_info.is_windows:
             if "CONAN_BASH_PATH" not in os.environ and \
                tools.os_info.detect_windows_subsystem() != "msys2":
-                self.build_requires("msys2/20190524")
+                self.build_requires("msys2/20200517")
         if self._is_msvc:
             self.build_requires("automake/1.16.2")
 
