bc8be4e478a2c3bb49da314b076e90d2e9b1ad9f
an-tao <antao2002@gmail.com>
2020-10-06 11:11:17 +0800

    Add trantor/1.0.0

---
+++ recipes/trantor/all/conanfile.py
@@ -0,0 +1,54 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class TrantorConan(ConanFile):
+    name = "trantor"
+    license = "https://raw.githubusercontent.com/an-tao/trantor/master/License"
+    exports_sources = ["CMakeLists.txt"]
+    homepage = "https://github.com/an-tao/trantor"
+    url = "https://github.com/conan-io/conan-center-index"
+    description = "Non-blocking I/O TCP network library based on +14/17"
+    topics = ("Network library", "C++14/17", "Non-blocking I/O")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+    _cmake = None
+
+    def requirements(self):
+        self.requires("openssl/1.1.1h")
+        self.requires("c-ares/1.16.1")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+    
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = "trantor-{}".format(self.version)
+        os.rename(extracted_dir, self._source_subfolder())
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure()
+        return self._cmake
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+    def package(self):
+        self.copy("License", src=self._source_subfolder(), dst="licenses")
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "Trantor"
+        self.cpp_info.names["cmake_find_package_multi"] = "Trantor"
+        self.cpp_info.libs = tools.collect_libs(self)
---
+++ recipes/trantor/all/test_package/test_package.cpp
@@ -0,0 +1,8 @@
+#include <trantor/net/EventLoop.h>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    trantor::EventLoop loop;
+    return 0;
+}
