57933fb65c53208f7ffbc524bff91c81b3e9f2eb
theirix <theirix@gmail.com>
2020-10-08 20:10:54 +0300

    easyexif: cache cmake
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/easyexif/all/conanfile.py
+++ recipes/easyexif/all/conanfile.py
@@ -16,6 +16,8 @@ class EasyExifConan(ConanFile):
     }
     default_options = {"shared": False, "fPIC": True}
     generators = "cmake"
+    
+    _cmake = None
 
     @property
     def _source_subfolder(self):
@@ -25,15 +27,23 @@ class EasyExifConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, 11)
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure()
-        return cmake
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure()
+        return self._cmake
 
     def build(self):
         cmake = self._configure_cmake()
