b512b2ee650d672f17fa43bd4d8a154dd0baa067
Joel Falcou <joel.falcou@lri.fr>
2020-10-09 10:42:20 +0200

    Add recipes for TTS

---
+++ recipes/tts/all/test_package/test_package.cpp
@@ -0,0 +1,15 @@
+#include <tts/tts.hpp>
+
+TTS_CASE( "Check basic tests" )
+{
+  TTS_EXPECT(true);
+  TTS_EXPECT_NOT(false);
+
+  int x = 0;
+  TTS_EQUAL( x, x );
+  TTS_NOT_EQUAL( 1, x );
+  TTS_LESS(x,0);
+  TTS_GREATER(1,x);
+  TTS_LESS_EQUAL(x,1);
+  TTS_GREATER_EQUAL(x,0);
+}
