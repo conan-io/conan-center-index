45f376948bf0d9509477c011b2ab111992bec171
Wes Huie <huiew@justin.tv>
2020-10-13 15:00:16 -0700

    test ConnectionFactory

--- recipes/twitch-native-ipc/all/test_package/conanfile.py
+++ recipes/twitch-native-ipc/all/test_package/conanfile.py
@@ -14,5 +14,6 @@ class TwitchNativeIpcTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "example")
-            self.run(bin_path, run_environment=True)
+            #bin_path = os.path.join("bin", "example")
+            cmd = "{} && {}".format(os.path.join("bin", "example"), os.path.join("bin", "example2"))
+            self.run(cmd, run_environment=True)
