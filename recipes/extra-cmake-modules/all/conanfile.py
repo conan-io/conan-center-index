e6bb5d3397bb34d409d2a37c3927aa6fd8a81e0f
Walid Boussaffa <boussaffa.walid@outlook.com>
2020-10-15 20:12:39 +0100

    added recipe for extra-cmake-modules 5.75.0

---
+++ recipes/extra-cmake-modules/all/conanfile.py
@@ -0,0 +1,56 @@
+import os
+from conans import ConanFile, CMake, tools
+
+class ExtracmakemodulesConan(ConanFile):
+    name = "extra-cmake-modules"
+    license = ("MIT", "BSD-2-Clause", "BSD-3-Clause")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://api.kde.org/ecm/"
+    topics = ("conan", "cmake", "toolchain", "build-settings")
+    description = "KDE's CMake modules"
+    generators = "cmake"
+    no_copy_source = False
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("extra-cmake-modules-{}".format(self.version), self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+
+        # KB-H016: do not install Find*.cmake
+        tools.replace_path_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+            "install(FILES ${installFindModuleFiles} DESTINATION ${FIND_MODULES_INSTALL_DIR})", "")
+
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_HTML_DOCS"] = False
+        self._cmake.definitions["BUILD_QTHELP_DOCS"] = False
+        self._cmake.definitions["BUILD_MAN_DOCS"] = False
+        self._cmake.definitions["SHARE_INSTALL_DIR"] = os.path.join(self.package_folder, "res")
+        self._cmake.configure(source_folder=os.path.join(self.source_folder, self._source_subfolder))
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+        self.copy("testhelper.h", src=os.path.join(self.source_folder, self._source_subfolder, "tests/ECMAddTests"), dst="res/tests")
+        self.copy("*", src=os.path.join(self.source_folder, self._source_subfolder, "LICENSES"), dst="licenses")
+
+    def package_info(self):
+        self.cpp_info.resdirs = ["res"]
+        self.cpp_info.builddirs = ["res/ECM/cmake", "res/ECM/kde-modules", "res/ECM/modules", "res/ECM/test-modules", "res/ECM/toolchain"]
+
+    def package_id(self):
+        self.info.header_only()
+
---
+++ recipes/extra-cmake-modules/all/test_package/example.cpp
@@ -0,0 +1,6 @@
+#include <Foo> // This header should be created by ECM
+
+int main() 
+{
+    return success();
+}
