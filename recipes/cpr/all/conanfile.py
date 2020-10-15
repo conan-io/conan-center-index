9aef944dee975d2ed709585396f6c86860f71a0e
Uilian Ries <uilianries@gmail.com>
2020-10-15 13:29:03 -0300

    Revert "Do not force CURL SSL option"
    This reverts commit f20cf6ac63c05c275a95f9609c31a37c1902d6b0.

--- recipes/cpr/all/conanfile.py
+++ recipes/cpr/all/conanfile.py
@@ -34,6 +34,9 @@ class CprConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+        if self.options.with_openssl:
+            # If using OpenSSL, we need it to be active in libcurl too
+            self.options["libcurl"].with_ssl = "openssl"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -64,8 +67,6 @@ class CprConan(ConanFile):
         return cmake
 
     def build(self):
-        if self.options["libcurl"].with_ssl != "openssl":
-            raise ConanInvalidConfiguration("CPR requires libcurl built with the option with_ssl=\"openssl\".")
         self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
