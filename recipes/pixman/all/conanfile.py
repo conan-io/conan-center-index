1f61df10b452651c40aa61a4fce5eec786ede5cf
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-10-16 19:02:22 +0200

    remove double quote

--- recipes/pixman/all/conanfile.py
+++ recipes/pixman/all/conanfile.py
@@ -33,7 +33,7 @@ class PixmanConan(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
         if self.settings.os == "Windows" and self.options.shared:
-            raise ConanInvalidConfiguration("pixman can only be built as a static library on Windows")"
+            raise ConanInvalidConfiguration("pixman can only be built as a static library on Windows")
 
     def build_requirements(self):
         if tools.os_info.is_windows:
