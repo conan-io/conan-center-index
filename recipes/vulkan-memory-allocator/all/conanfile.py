66b1bdc4093c65192ab583fd5ae67a57677e5a2a
Vladimir Chistyakov <30690670+b1ackviking@users.noreply.github.com>
2020-10-16 23:06:04 +0700

    Apply suggestions from code review
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/vulkan-memory-allocator/all/conanfile.py
+++ recipes/vulkan-memory-allocator/all/conanfile.py
@@ -10,34 +10,20 @@ class VulkanMemoryAllocatorConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     description = "Easy to integrate Vulkan memory allocation library."
     topics = ("vulkan", "memory-allocator", "graphics")
-    requires = ("vulkan-headers/[>=1.0]")
+    requires = ("vulkan-headers/1.2.154.0")
     no_copy_source = True
-    # No settings/options are necessary, this is header only
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
+    def package_id(self):
+        self.info.header_only()
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        url = self.conan_data["sources"][self.version]["url"]
-        version = os.path.basename(url).replace(".zip", "")
-        if version.startswith('v'):
-            version = version[1:]
-        extracted_dir = "VulkanMemoryAllocator-" + version
-        os.rename(extracted_dir, self._source_subfolder)
-
-    def build(self):
-        # This is a header only
-        pass
+        os.rename("VulkanMemoryAllocator-{}".format(self.version), self._source_subfolder)
 
     def package(self):
-        self.copy("LICENSE.txt",
-                  src=os.path.join(self.source_folder,
-                                   self._source_subfolder),
-                  dst="licenses")
-        self.copy("vk_mem_alloc.h",
-                  src=os.path.join(self.source_folder,
-                                   self._source_subfolder,
-                                   "src"),
-                  dst="include")
+        self.copy("LICENSE.txt", src=self._source_subfolder, dst="licenses")
+        self.copy("vk_mem_alloc.h", src=os.path.join(self._source_subfolder, "src"), dst="include")
