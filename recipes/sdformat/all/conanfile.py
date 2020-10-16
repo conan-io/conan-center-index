3cd93e9179792fc7f85178626262bbe53b6d1414
Juan Oxoby <me@jmoxo.by>
2020-10-15 20:07:05 -0700

    Address review comments (same as ignition-math)

--- recipes/sdformat/all/conanfile.py
+++ recipes/sdformat/all/conanfile.py
@@ -1,6 +1,7 @@
 import os
 
 from conans import CMake, ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 
 
 class SDFormat(ConanFile):
@@ -14,33 +15,64 @@ class SDFormat(ConanFile):
     generators = "cmake", "cmake_find_package_multi"
 
     @property
+    def _minimum_cpp_standard(self):
+        return 17
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "16",
+            "gcc": "7",
+            "clang": "5",
+            "apple-clang": "10",
+        }
+
+    @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    def configure(self):
+        if self.settings.get_safe("compiler.cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn(
+                "{} recipe lacks information about the {} compiler support.".format(
+                    self.name, self.settings.compiler
+                )
+            )
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration(
+                    "{} requires c++17 support. The current compiler {} {} does not support it.".format(
+                        self.name,
+                        self.settings.compiler,
+                        self.settings.compiler.version,
+                    )
+                )
 
     def source(self):
         version_major = self.version.split('.')[0]
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename(f"sdformat-sdformat{version_major}_{self.version}", self._source_subfolder)
+        os.rename("sdformat-sdformat{}_{}".format(version_major, self.version), self._source_subfolder)
 
     def requirements(self):
         self.requires("ignition-math/6.4.0")
         self.requires("tinyxml2/8.0.0")
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = False
-        cmake.definitions["USE_INTERNAL_URDF"] = True
-        cmake.configure(source_folder=self._source_subfolder)
-        return cmake
+        self.cmake = CMake(self)
+        self.cmake.definitions["BUILD_TESTING"] = False
+        self.cmake.definitions["USE_INTERNAL_URDF"] = True
+        self.cmake.configure(source_folder=self._source_subfolder)
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+        self._configure_cmake()
+        self.cmake.build()
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
+        self.cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "share"))
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
@@ -49,6 +81,6 @@ class SDFormat(ConanFile):
         version_major = self.version.split('.')[0]
         version_till_minor = ".".join(self.version.split(".")[0:2])
         self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.names["cmake_find_package"] = f"sdformat{version_major}"
-        self.cpp_info.names["cmake_find_package_multi"] = f"sdformat{version_major}"
-        self.cpp_info.includedirs = [f"include/sdformat-{version_till_minor}"]
+        self.cpp_info.names["cmake_find_package"] = "sdformat{}".format(version_major)
+        self.cpp_info.names["cmake_find_package_multi"] = "sdformat{}".format(version_major)
+        self.cpp_info.includedirs = ["include/sdformat-{}".format(version_till_minor)]
