ad4877845e86e5a10cb95d84689a8e026c7fe3a7
Juan Oxoby <49600381+joxoby@users.noreply.github.com>
2020-10-19 15:36:33 -0700

    Update recipes/ignition-cmake/all/conanfile.py
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/ignition-cmake/all/conanfile.py
+++ recipes/ignition-cmake/all/conanfile.py
@@ -34,32 +34,16 @@ class IgnitionCmakeConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    def package_id(self):
+        self.info.header_only()
+
     def _configure_cmake(self):
         if self._cmake is not None:
             return self._cmake
         self._cmake = CMake(self)
+        self._cmake.definitions["CMAKE_INSTALL_DATAROOTDIR"] = "lib"
         self._cmake.configure(source_folder=self._source_subfolder)
         return self._cmake
-
-    def configure(self):
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
-        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
-        if not min_version:
-            self.output.warn(
-                "{} recipe lacks information about the {} compiler support.".format(
-                    self.name, self.settings.compiler
-                )
-            )
-        else:
-            if tools.Version(self.settings.compiler.version) < min_version:
-                raise ConanInvalidConfiguration(
-                    "{} requires c++17 support. The current compiler {} {} does not support it.".format(
-                        self.name,
-                        self.settings.compiler,
-                        self.settings.compiler.version,
-                    )
-                )
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
