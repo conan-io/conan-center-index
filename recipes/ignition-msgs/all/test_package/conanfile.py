6a60b491eb868b205f8e41f8fc7a120e603a680f
Juan Oxoby <me@jmoxo.by>
2020-10-19 20:20:55 -0700

    Add ignition-msgs

---
+++ recipes/ignition-msgs/all/test_package/conanfile.py
@@ -0,0 +1,19 @@
+import os
+
+from conans import CMake, ConanFile, tools
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "cmake_find_package_multi"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["IGN_MSGS_MAJOR_VER"] = tools.Version(self.deps_cpp_info["ignition-msgs"].version).major
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
