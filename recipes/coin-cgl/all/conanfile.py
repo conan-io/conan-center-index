134f61d2cdf91268d466d8bda19001b67b30b284
Anonymous Maarten <madebr@users.noreply.github.com>
2020-10-20 15:18:30 +0200

    coin-cgl: fix homepage
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/coin-cgl/all/conanfile.py
+++ recipes/coin-cgl/all/conanfile.py
@@ -9,7 +9,7 @@ class CoinCglConan(ConanFile):
     description = "COIN-OR Cut Generator Library"
     topics = ("conan", "clp", "simplex", "solver", "linear", "programming")
     url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/coin-or/Clp"
+    homepage = "https://github.com/coin-or/Cgl"
     license = ("EPL-2.0",)
     settings = "os", "arch", "build_type", "compiler"
     options = {
