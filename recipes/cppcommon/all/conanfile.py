f040921094c73d9973fd3594b18456656aaa9ab9
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-10-20 13:22:05 -0300

    Add cppcommon recipe

---
+++ recipes/cppcommon/all/conanfile.py
@@ -0,0 +1,98 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+
+class CppCommon(ConanFile):
+    name = "cppcommon"
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/chronoxor/CppCommon"
+    description = "C++ Common Library contains reusable components and patterns" \
+        " for error and exceptions handling, filesystem manipulations, math," \
+        " string format and encoding, shared memory, threading, time management" \
+        " and others."
+    topics = ("conan", "utils", "library")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"fPIC": [True, False],
+               "shared": [True, False]}
+    default_options = {"fPIC": True,
+                       "shared": False}
+    requires = ["fmt/7.0.3", "stduuid/1.0"]
+    generators = "cmake"
+    exports_sources = ["patches/**", "CMakeLists.txt"]
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "apple-clang": 10,
+            "clang": 6,
+            "gcc": 7,
+            "Visual Studio": 16,
+        }
+
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            self._cmake.definitions["CPPCOMMON_MODULE"] = "ON"
+            self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("CppCommon-" + self.version,
+            self._source_subfolder)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.settings.compiler == "Visual Studio" and self.settings.arch == "x86":
+            raise ConanInvalidConfiguration("Visual Studio x86 builds are not supported.")
+
+        if self.options.shared:
+            del self.options.fPIC
+
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, "17")
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version:
+            if tools.Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration("cppcommon requires C++17, which your compiler does not support.")
+        else:
+            self.output.warn("cppcommon requires C++17. Your compiler is unknown. Assuming it supports C++17.")
+
+    def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+        include = os.path.join(self._source_subfolder, "include")
+        self.copy("*.h", src=include, dst="include", keep_path=True)
+        self.copy("*.inl", src=include, dst="include", keep_path=True)
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["pthread", "rt", "dl"]
+
+        if not self.options.shared:
+            if self.settings.os == "Windows":
+                self.cpp_info.system_libs = ["userenv"]
---
+++ recipes/cppcommon/all/test_package/test_package.cpp
@@ -0,0 +1,11 @@
+#include "threads/thread.h"
+#include "system/uuid.h"
+#include "system/environment.h"
+#include "time/timespan.h"
+
+int main()
+{
+    CppCommon::Thread::Yield();
+    CppCommon::UUID id;
+    CppCommon::Timespan span;
+}
