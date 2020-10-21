ab8b827925919760ea8b67fb3551256d9239f1fb
Hossein Moein <31854960+hosseinmoein@users.noreply.github.com>
2020-10-21 08:12:16 -0400

    Fixed the build method for loop per @Croydon

--- recipes/dataframe/all/conanfile.py
+++ recipes/dataframe/all/conanfile.py
@@ -100,7 +100,7 @@ class DataFrameConan(ConanFile):
         return self._cmake
 
     def build(self):
-        for patch in self.conan_data["patches"][self.version]:
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
