c2e02719baad13606fd5322045e2b5316f73db2e
Wes Huie <huiew@justin.tv>
2020-10-21 08:16:02 -0700

    run each test separately

--- recipes/twitch-native-ipc/all/test_package/conanfile.py
+++ recipes/twitch-native-ipc/all/test_package/conanfile.py
@@ -14,6 +14,5 @@ class TwitchNativeIpcTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self):
-            #bin_path = os.path.join("bin", "example")
-            cmd = "{} && {}".format(os.path.join("bin", "example"), os.path.join("bin", "example2"))
-            self.run(cmd, run_environment=True)
+            self.run(os.path.join("bin", "example"), run_environment=True)
+            self.run(os.path.join("bin", "example2"), run_environment=True)
