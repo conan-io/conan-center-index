819caf9dda103c01bae196ed358baabb4c546b2a
Anonymous Maarten <madebr@users.noreply.github.com>
2020-10-21 15:52:43 +0200

    poppler-data: raise AssertionError when datadir does not exist

--- recipes/poppler-data/all/test_package/conanfile.py
+++ recipes/poppler-data/all/test_package/conanfile.py
@@ -5,4 +5,5 @@ import os
 class TestPackageConan(ConanFile):
 
     def test(self):
-        os.path.isdir(self.deps_user_info["poppler-data"].datadir)
+        if not os.path.isdir(self.deps_user_info["poppler-data"].datadir):
+            raise AssertionError("datadir is not a directory")
