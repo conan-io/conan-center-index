618ace0a07cc6776f8f9d11f090e7bc284400edc
Martin Miralles-Cordal <martin.miralles-cordal@lhsystems.com>
2020-10-23 21:10:34 -0400

    fire-hpp: added initial recipe, prerelease snapshot

---
+++ recipes/fire-hpp/all/conanfile.py
@@ -0,0 +1,46 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class FireHppConan(ConanFile):
+    name = "fire-hpp"
+    homepage = "https://github.com/kongaskristjan/fire-hpp"
+    url = "https://github.com/conan-io/conan-center-index"
+    description = "Fire for C++: Create fully functional CLIs using function signatures"
+    topics = ("command-line", "argument", "parser")
+    license = "BSL-1.0"
+    no_copy_source = True
+    settings = "os", "arch", "compiler", "build_type"
+    exports_sources = ["LICENSE"]
+
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+    _cmake = None
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        url = self.conan_data["sources"][self.version]["url"]
+        tar_name = url[url.rfind("/")+1:url.find(".tar.gz")]
+        extracted_dir = "{}-{}".format(self.name, tar_name)
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["FIRE_EXAMPLES"] = False
+        self._cmake.definitions["FIRE_UNIT_TESTS"] = False
+        self._cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses")
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib"))
+
+    def package_id(self):
+        self.info.header_only()
