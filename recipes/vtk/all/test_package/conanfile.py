e0f30b90732835caeda327f8b079b8584a282041
atrelinski <atrelinski@no-mail.com>
2020-10-26 03:02:38 +0100

    Remove test for "cmake_find_package_multi" generator

--- recipes/vtk/all/test_package/conanfile.py
+++ recipes/vtk/all/test_package/conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools, RunEnvironment
 
 class TestVTKConan(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
-    generators = "cmake" # Replace "cmake" by "cmake_find_package_multi" to test multi
+    generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
@@ -12,6 +12,5 @@ class TestVTKConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            # Replace "bin" by "Debug" on Win or by "." (dot) on Mac when testing "cmake_find_package_multi"
             bin_path = os.path.join("bin", "test_vtk_package")
             self.run(bin_path, run_environment=True)
