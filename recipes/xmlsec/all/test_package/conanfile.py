e250b0aa49412a8466d9ffd046f5a354762872b2
Thomas Steiner <47025493+steinerthomas@users.noreply.github.com>
2020-10-26 19:21:23 +0100

    Only run tests if not cross-compiling
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/xmlsec/all/test_package/conanfile.py
+++ recipes/xmlsec/all/test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
@@ -12,8 +12,9 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        arg_path1 = os.path.abspath(os.path.join(os.path.dirname(__file__), "sign1-tmpl.xml"))
-        arg_path2 = os.path.abspath(os.path.join(os.path.dirname(__file__), "rsakey.pem"))
-        bin_arg_path = "%s %s %s" % (bin_path, arg_path1, arg_path2)
-        self.run(bin_arg_path, run_environment=True)
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            arg_path1 = os.path.abspath(os.path.join(os.path.dirname(__file__), "sign1-tmpl.xml"))
+            arg_path2 = os.path.abspath(os.path.join(os.path.dirname(__file__), "rsakey.pem"))
+            bin_arg_path = "%s %s %s" % (bin_path, arg_path1, arg_path2)
+            self.run(bin_arg_path, run_environment=True)
