79a338cfc43d4dda043951aed1d845506b10c98c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-10-26 18:07:16 +0100

    explicit cpp_info.libs
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/ezc3d/all/conanfile.py
+++ recipes/ezc3d/all/conanfile.py
@@ -81,6 +81,7 @@ class Ezc3dConan(ConanFile):
     def package_info(self):
         # FIXME: remove namespace for imported target
         self.cpp_info.includedirs = [os.path.join("include", "ezc3d")]
-        self.cpp_info.libs = tools.collect_libs(self)
+        lib_suffix = {"Debug": "_debug"}.get(str(self.settings.build_type), "")
+        self.cpp_info.libs = ["ezc3d" + lib_suffix]
         if self.settings.os == "Linux":
             self.cpp_info.system_libs = ["m"]
