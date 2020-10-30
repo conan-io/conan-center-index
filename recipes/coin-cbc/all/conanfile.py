bbf0f2b3c48b04c5b69f77b7ef553d34e72f6bf0
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-10-30 21:31:28 +0100

    coin-cbc: installation expects include/coin to already exist

--- recipes/coin-cbc/all/conanfile.py
+++ recipes/coin-cbc/all/conanfile.py
@@ -102,6 +102,8 @@ class CoinCbcConan(ConanFile):
 
     def package(self):
         self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        # Installation script expects include/coin to already exist
+        tools.mkdir(os.path.join(self.package_folder, "include", "coin"))
         with self._build_context():
             autotools = self._configure_autotools()
             autotools.install()
