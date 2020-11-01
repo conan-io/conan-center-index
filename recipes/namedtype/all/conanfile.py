87be0afe47396539ca6a5e43f707cb026f2d62bb
theirix <theirix@gmail.com>
2020-11-01 14:38:11 +0300

    Restrict namedtype to C++11

--- recipes/namedtype/all/conanfile.py
+++ recipes/namedtype/all/conanfile.py
@@ -1,5 +1,6 @@
 import os
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 
 
 class NamedTypeConan(ConanFile):
@@ -9,6 +10,7 @@ class NamedTypeConan(ConanFile):
     homepage = "https://github.com/joboccara/NamedType"
     description = "Implementation of strong types in C++"
     topics = ("strong types",)
+    settings = "compiler"
     no_copy_source = True
 
     @property
@@ -24,6 +26,31 @@ class NamedTypeConan(ConanFile):
         else:
             extracted_dir = "NamedType-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
+
+    @property
+    def _minimum_cpp_standard(self):
+        return 11
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "14",
+            "gcc": "5",
+            "clang": "3.9",
+            "apple-clang": "8",
+        }
+
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
 
     def package(self):
         self.copy("*.hpp", dst="include", src=self._source_subfolder, keep_path=False)
