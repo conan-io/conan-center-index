3dc7524891c2d07020dbb8ac7ea90f9b1b8ff16a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-11-01 19:45:44 +0100

    add xkbcommon/1.0.1

--- recipes/xkbcommon/all/conanfile.py
+++ recipes/xkbcommon/all/conanfile.py
@@ -16,6 +16,7 @@ class XkbcommonConan(ConanFile):
         "fPIC": [True, False],
         "with_x11": [True, False],
         "with_wayland": [True, False],
+        "xkbregistry": [True, False],
         "docs": [True, False]
     }
     default_options = {
@@ -23,6 +24,7 @@ class XkbcommonConan(ConanFile):
         "fPIC": True,
         "with_x11": True,
         "with_wayland": False,
+        "xkbregistry": False, # FIXME: should be True, but requires pkg_config generator which will improperly model xorg pkg-config files
         "docs": False
     }
 
@@ -36,14 +38,26 @@ class XkbcommonConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    @property
+    def _has_xkbregistry_option(self):
+        return tools.Version(self.version) >= "1.0.0"
+
+    def config_options(self):
+        if not self._has_xkbregistry_option:
+            del self.options.xkbregistry
+
     def configure(self):
         if self.settings.os != "Linux":
             raise ConanInvalidConfiguration("This library is only compatible with Linux")
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
+        if self.options.get_safe("xkbregistry"):
+            raise ConanInvalidConfiguration("xkbregistry can't be enabled yet.")
 
     def requirements(self):
         self.requires("xorg/system")
+        if self.options.get_safe("xkbregistry"):
+            self.requires("libxml2/2.9.10")
 
     def build_requirements(self):
         self.build_requires("meson/0.56.0")
@@ -63,6 +77,8 @@ class XkbcommonConan(ConanFile):
             "enable-x11": self.options.with_x11,
             "libdir": os.path.join(self.package_folder, "lib"),
             "default_library": ("shared" if self.options.shared else "static")}
+        if self._has_xkbregistry_option:
+            defs["enable-xkbregistry"] = self.options.xkbregistry
 
         self._meson = Meson(self)
         self._meson.configure(
@@ -73,8 +89,9 @@ class XkbcommonConan(ConanFile):
         return self._meson
 
     def build(self):
-        meson = self._configure_meson()
-        meson.build()
+        with tools.run_environment(self):
+            meson = self._configure_meson()
+            meson.build()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
@@ -83,11 +100,19 @@ class XkbcommonConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
-        self.cpp_info.components['libxkbcommon'].libs = ['xkbcommon']
-        self.cpp_info.components['libxkbcommon'].name = 'xkbcommon'
-        self.cpp_info.components['libxkbcommon'].requires = ['xorg::xkeyboard-config']
+        self.cpp_info.components["libxkbcommon"].libs = ["xkbcommon"]
+        self.cpp_info.components["libxkbcommon"].name = "xkbcommon"
+        self.cpp_info.components["libxkbcommon"].requires = ["xorg::xkeyboard-config"]
         if self.options.with_x11:
-            self.cpp_info.components['libxkbcommon-x11'].libs = ['xkbcommon-x11']
-            self.cpp_info.components['libxkbcommon-x11'].name = 'xkbcommon-x11'
-            self.cpp_info.components['libxkbcommon-x11'].requires = ['libxkbcommon']
-            self.cpp_info.components['libxkbcommon-x11'].requires.extend(['xorg::xcb', 'xorg::xcb-xkb'])
+            self.cpp_info.components["libxkbcommon-x11"].libs = ["xkbcommon-x11"]
+            self.cpp_info.components["libxkbcommon-x11"].name = "xkbcommon-x11"
+            self.cpp_info.components["libxkbcommon-x11"].requires = ["libxkbcommon", "xorg::xcb", "xorg::xcb-xkb"]
+        if self.options.get_safe("xkbregistry"):
+            self.cpp_info.components["libxkbregistry"].libs = ["xkbregistry"]
+            self.cpp_info.components["libxkbregistry"].name = "xkbregistry"
+            self.cpp_info.components["libxkbregistry"].requires = ["libxml2::libxml2"]
+
+        if tools.Version(self.version) >= "1.0.0":
+            bin_path = os.path.join(self.package_folder, "bin")
+            self.output.info("Appending PATH environment variable: {}".format(bin_path))
+            self.env_info.PATH.append(bin_path)
