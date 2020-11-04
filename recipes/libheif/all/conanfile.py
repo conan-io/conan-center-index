e2d5e92ba1c775dec32a754f3934d4e6b2b8a767
theirix <theirix@gmail.com>
2020-11-04 21:16:13 +0300

    Remove unneeded BUILD_SHARED_LIBS
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/libheif/all/conanfile.py
+++ recipes/libheif/all/conanfile.py
@@ -53,7 +53,6 @@ class Libheif(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
-        self._cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
         self._cmake.definitions["WITH_EXAMPLES"] = False
         self._cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.get_safe("fPIC", True)
         self._cmake.configure()
