646af80f6a8c6465694b9ffc66f4fa09bb087afa
theirix <theirix@gmail.com>
2020-11-04 17:37:21 +0300

    Restrict compilers by C++14

--- recipes/namedtype/all/conanfile.py
+++ recipes/namedtype/all/conanfile.py
@@ -1,6 +1,7 @@
 import glob
 import os
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 
 
 class NamedTypeConan(ConanFile):
@@ -22,9 +23,30 @@ class NamedTypeConan(ConanFile):
         extracted_dir = glob.glob("NamedType-*")[0]
         os.rename(extracted_dir, self._source_subfolder)
 
+    @property
+    def _minimum_cpp_standard(self):
+        return 14
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "15",
+            "gcc": "5",
+            "clang": "3.4",
+            "apple-clang": "5.1",
+        }
+
     def configure(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 14)
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
 
     def package(self):
         if self.version == "20190324":
