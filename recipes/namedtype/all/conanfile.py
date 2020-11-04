0f0713b7ad4263209876c81035769a10fbf0ab43
theirix <theirix@gmail.com>
2020-11-04 17:24:08 +0300

    Add header_only, restrict by C++14

--- recipes/namedtype/all/conanfile.py
+++ recipes/namedtype/all/conanfile.py
@@ -1,7 +1,6 @@
 import glob
 import os
 from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
 
 
 class NamedTypeConan(ConanFile):
@@ -23,30 +22,9 @@ class NamedTypeConan(ConanFile):
         extracted_dir = glob.glob("NamedType-*")[0]
         os.rename(extracted_dir, self._source_subfolder)
 
-    @property
-    def _minimum_cpp_standard(self):
-        return 11
-
-    @property
-    def _minimum_compilers_version(self):
-        return {
-            "Visual Studio": "15",
-            "gcc": "5",
-            "clang": "3.9",
-            "apple-clang": "8",
-        }
-
     def configure(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
-        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
-        if not min_version:
-            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
-                self.name, self.settings.compiler))
-        else:
-            if tools.Version(self.settings.compiler.version) < min_version:
-                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
-                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
+            tools.check_min_cppstd(self, 14)
 
     def package(self):
         if self.version == "20190324":
@@ -54,3 +32,6 @@ class NamedTypeConan(ConanFile):
         else:
             self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, 'include'))
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
