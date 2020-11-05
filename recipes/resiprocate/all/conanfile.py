6ceff2d2c773a0c926c7243f8bc2b3f785f718a9
Alexeii Kislinskii <kingslinskij@yandex.ua>
2020-11-06 00:07:05 +0200

    fix build errors with hooks

--- recipes/resiprocate/all/conanfile.py
+++ recipes/resiprocate/all/conanfile.py
@@ -12,7 +12,6 @@ class ResiprocateConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     exports = "LICENSE"
-    release_name = "%s-%s" % (name, version)
     install_dir = tempfile.mkdtemp(suffix=name)
 
     def source(self):
@@ -24,15 +23,15 @@ class ResiprocateConan(ConanFile):
         env_build.cxx_flags.append("-w")
         with tools.environment_append(env_build.vars):
             configure_args = ['--prefix=%s' % self.install_dir]
-            with tools.chdir(self.release_name):
+            with tools.chdir("%s-%s" % (self.name, self.version)):
                 env_build.configure(args=configure_args)
                 env_build.make(args=["clean"])
                 env_build.make(args=["install"])
 
     def package(self):
-        self.copy(pattern="*", dst="include/repro", src=os.path.join(self.release_name, "repro"))
-        self.copy(pattern="*", dst="include/resip", src=os.path.join(self.release_name, "resip"))
-        self.copy(pattern="*", dst="include/rutil", src=os.path.join(self.release_name, "rutil"))
+        self.copy(pattern="*", dst="include/repro", src=os.path.join(self.name, "repro"))
+        self.copy(pattern="*", dst="include/resip", src=os.path.join(self.name, "resip"))
+        self.copy(pattern="*", dst="include/rutil", src=os.path.join(self.name, "rutil"))
         self.copy(pattern="*.a", dst="lib", src=os.path.join(self.install_dir, "lib"))
         self.copy(pattern="*.la", dst="lib", src=os.path.join(self.install_dir, "lib"))
         self.copy(pattern="*.so", dst="lib", src=os.path.join(self.install_dir, "lib"))
