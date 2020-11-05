c41f8b8814f9110ed95ca11ec8c95712c86edddb
Mark Jan van Kampen <mjvk@allseas.com>
2020-11-05 12:56:06 +0100

    Adds patch to enable all features (z3 & pcre)

--- recipes/cppcheck/all/conanfile.py
+++ recipes/cppcheck/all/conanfile.py
@@ -13,6 +13,9 @@ class CppcheckConan(ConanFile):
     license = "GPL-3.0-or-later"
     generators = "cmake_find_package"
     settings = "os", "arch", "compiler", "build_type"
+    options = {"with_z3": [True, False], "have_rules": [True, False]}
+    default_options = {"with_z3": True, "have_rules": True}
+    exports_sources = ["patches/**"]
     
     @property
     def _source_subfolder(self):
@@ -21,6 +24,16 @@ class CppcheckConan(ConanFile):
     @property
     def _build_subfolder(self):
         return "build_subfolder"
+        
+    def _patch_sources(self):
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
+
+    def requirements(self):
+        if self.options.with_z3:
+            self.requires("z3/4.8.8")
+        if self.options.have_rules:
+            self.requires("pcre/8.44")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -29,11 +42,16 @@ class CppcheckConan(ConanFile):
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["USE_Z3"] = "OFF"
+        cmake.definitions["USE_Z3"] = "ON" if self.options.with_z3 else "OFF"
+        cmake.definitions["HAVE_RULES"] = "ON" if self.options.have_rules else "OFF"
+        cmake.definitions["Z3_LIBRARIES"] = "z3::z3"
+        cmake.definitions["Z3_CXX_INCLUDE_DIRS"] = "${z3_INCLUDE_DIRS}"
+        cmake.definitions["USE_MATCHCOMPILER"] = "ON" if self.settings.build_type == "Release" else "OFF"
         cmake.configure(build_folder=self._build_subfolder, source_folder=self._source_subfolder)
         return cmake
 
     def build(self):
+        self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
 
