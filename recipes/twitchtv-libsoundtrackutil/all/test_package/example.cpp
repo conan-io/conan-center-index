af532b5d9778fc6168b59ae58a93345d9ffb9714
wwhuie <wwhuie@gmail.com>
2020-11-05 19:14:30 -0800

    Update recipes/twitchtv-libsoundtrackutil/all/test_package/example.cpp
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/twitchtv-libsoundtrackutil/all/test_package/example.cpp
+++ recipes/twitchtv-libsoundtrackutil/all/test_package/example.cpp
@@ -1,6 +1,3 @@
-// Copyright Twitch Interactive, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT
-
 #include <libsoundtrackutil/SoundtrackIPC.h>
 
 using namespace Twitch::Audio;
