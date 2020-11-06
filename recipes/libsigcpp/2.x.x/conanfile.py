b3363589d5c1028b0eb42dd0d08d643b3ce6257e
Bart Verhagen <barrie.verhagen@gmail.com>
2020-11-06 12:40:51 +0100

    Addressed review comment regarding renaming generated cmake config files

--- recipes/libsigcpp/2.x.x/conanfile.py
+++ recipes/libsigcpp/2.x.x/conanfile.py
@@ -91,5 +91,3 @@ class LibSigCppConan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
 
         self.cpp_info.names["pkg_config"] = "sigc++-2.0"
-        self.cpp_info.names["cmake_find_package"] = "sigc-2.0"
-        self.cpp_info.names["cmake_find_package_multi"] = "sigc++-2"
