3b36b7ac310db74aa0550c382553b4de745f0bcd
Harald <harald.achitz@gmail.com>
2020-11-06 13:31:57 +0100

    use check_min_cppstd to check compiler
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/elfio/all/conanfile.py
+++ recipes/elfio/all/conanfile.py
@@ -16,6 +16,10 @@ class ElfioConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    def configure(self):
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, 11)
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("elfio-{}".format(self.version), self._source_subfolder)
