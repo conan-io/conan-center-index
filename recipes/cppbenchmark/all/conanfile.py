24c1b43a67c9a49ccd3a22dd18e0a4126a4a0e79
Tarcísio Genaro Rodrigues <tarcisio.genaro@gmail.com>
2020-11-10 11:57:12 -0300

    Update recipes/cppbenchmark/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/cppbenchmark/all/conanfile.py
+++ recipes/cppbenchmark/all/conanfile.py
@@ -85,3 +85,5 @@ class CppBenchmark(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["pthread"]
