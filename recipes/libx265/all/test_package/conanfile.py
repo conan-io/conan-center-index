8410a56d53c5c4d51e5f6b3c85ef815e831c7df2
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-11-11 15:45:20 +0100

    libx265: build as C + test shared library

--- recipes/libx265/all/test_package/conanfile.py
+++ recipes/libx265/all/test_package/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanException
 import os
 
 
@@ -6,8 +7,16 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    @property
+    def _test_shared_library(self):
+        try:
+            return self.options["libx265"].fPIC
+        except ConanException:
+            return True
+
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["TEST_LIBRARY"] = self._test_shared_library
         cmake.configure()
         cmake.build()
 
--- recipes/libx265/all/test_package/test_library.c
+++ recipes/libx265/all/test_package/test_library.c
@@ -1,9 +1,18 @@
 #include "x265.h"
 
-#include <cstdlib>
+#include <stdlib.h>
 
-int main()
-{
+#ifdef _WIN32
+# define TEST_EXPORT __declspec(dllexport)
+#else
+# define TEST_EXPORT __attribute__ ((visibility("default")))
+#endif
+
+TEST_EXPORT const char *test_library_version(void) {
+    return x265_version_str;
+}
+
+TEST_EXPORT void test_library_something(void) {
     x265_param * param = x265_param_alloc();
     x265_param_default(param);
     param->sourceWidth = 640;
@@ -13,5 +22,4 @@ int main()
     x265_encoder * encoder = x265_encoder_open(param);
     x265_encoder_close(encoder);
     x265_param_free(param);
-    return EXIT_SUCCESS;
 }
