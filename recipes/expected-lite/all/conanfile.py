bf45b2bef483deb04e47e226754b9c7f723d1956
Martin Moene <martin.moene@gmail.com>
2020-11-13 14:43:36 +0100

    Update recipes/expected-lite/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/expected-lite/all/conanfile.py
+++ recipes/expected-lite/all/conanfile.py
@@ -18,7 +18,7 @@ class ExpectedLiteConan(ConanFile):
         return "source_subfolder"
 
     def configure(self):
-        if tools.valid_min_cppstd(self, 11):
+        if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, 11)
 
     def package_id(self):
