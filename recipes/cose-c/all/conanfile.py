4119331d039080aafcb82fbfe1be699d823fb311
Chris Mc <prince.chrismc@gmail.com>
2020-11-16 13:11:55 -0500

    Update recipes/cose-c/all/conanfile.py
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/cose-c/all/conanfile.py
+++ recipes/cose-c/all/conanfile.py
@@ -39,6 +39,8 @@ class CoseCConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
