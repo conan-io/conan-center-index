2bac4c42de05807d8d276729b35147810b2bbb77
nicraMarcin <marcin@nicram.net>
2020-11-17 14:01:36 +0100

    (#3579) [oatpp-postgresql/1.2.0] Oatpp should be found as INSTALLED. Few suggested fixes
    * Oatpp should be found as INSTALLED. Few suggested fixes
    
    * Update recipes/oatpp-postgresql/all/test_package/CMakeLists.txt
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update recipes/oatpp-postgresql/config.yml
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/oatpp-postgresql/all/test_package/conanfile.py
+++ recipes/oatpp-postgresql/all/test_package/conanfile.py
@@ -9,13 +9,11 @@ class OatppPostgresqlTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self):
-           self.run(os.path.join("bin", "oatpp-postgresql-test"), run_environment=True)
+           self.run(os.path.join("bin", "test_package"), run_environment=True)
 
 
