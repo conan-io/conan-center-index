7cf5a84a2e16527cf1902ecd753eeec6b6f64cdb
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-11-18 20:39:19 +0100

    remove pdb files

--- recipes/pango/all/conanfile.py
+++ recipes/pango/all/conanfile.py
@@ -80,6 +80,7 @@ class PangoConan(ConanFile):
                     self.output.info("rename %s into %s" % (filename_old, filename_new))
                     shutil.move(filename_old, filename_new)
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.remove_files_by_mask(self.package_folder, "*.pdb")
 
     def package_info(self):
         self.cpp_info.components['pango_'].libs = ['pango-1.0']
