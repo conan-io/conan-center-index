b2d3666f6c29e2995d8210408c75cb4520579e35
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-11-20 12:57:20 +0100

    gtk 3.24.22
    this isa port of https://github.com/bincrafters/conan-gtk

---
+++ recipes/gtk/all/conanfile.py
@@ -0,0 +1,128 @@
+from conans import ConanFile, Meson, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+
+class LibnameConan(ConanFile):
+    name = "gtk"
+    description = "libraries used for creating graphical user interfaces for applications."
+    topics = ("conan", "gtk", "widgets")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://www.gtk.org"
+    license = "LGPL-2.1-or-later"
+    generators = "pkg_config"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_wayland": [True, False],
+        "with_x11": [True, False],
+        "with_pango": [True, False]
+        }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_wayland": False,
+        "with_x11": True,
+        "with_pango": True}
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+        if self.settings.os != "Linux":
+            del self.options.with_wayland
+            del self.options.with_x11
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+        if self.settings.os == "Linux":
+            if self.options.with_wayland or self.options.with_x11:
+                if not self.options.with_pango:
+                    raise ConanInvalidConfiguration("with_pango option is mandatory when with_wayland or with_x11 is used")
+        if self.settings.os == "Windows":
+            raise ConanInvalidConfiguration("GTK recipe is not yet compatible with Windows. Contributions are welcome.")
+    
+    def build_requirements(self):
+        self.build_requires("meson/0.54.2")
+        if not tools.which("pkg-config"):
+            self.build_requires("pkgconf/1.7.3")
+    
+    def requirements(self):
+        self.requires("gdk-pixbuf/2.42.0")
+        self.requires("glib/2.67.0")
+        if self.settings.compiler != "Visual Studio":
+            self.requires("cairo/1.17.2")
+        if self.settings.os == "Linux":
+            self.requires("at-spi2-atk/2.38.0")
+            if self.options.with_wayland:
+                self.requires("xkbcommon/0.10.0")
+                self.requires("wayland") # FIXME: Create an actual Wayland package(s)
+            if self.options.with_x11:
+                self.requires("xorg/system")
+        self.requires("atk/2.36.0")
+        self.requires("libepoxy/1.5.4")
+        if self.options.with_pango:
+            self.requires("pango/1.48.0")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir.replace("gtk", "gtk+"), self._source_subfolder)
+
+    def _configure_meson(self):
+        meson = Meson(self)
+        defs = {}
+        if self.settings.os == "Linux":
+            defs["wayland_backend"] = "true" if self.options.with_wayland else "false"
+            defs["x11_backend"] = "true" if self.options.with_x11 else "false"
+        defs["introspection"] = "false"
+        defs["documentation"] = "false"
+        defs["man-pages"] = "false"
+        defs["tests"] = "false"
+        defs["examples"] = "false"
+        defs["demos"] = "false"
+        args=[]
+        args.append("--wrap-mode=nofallback")
+        meson.configure(defs=defs, build_folder=self._build_subfolder, source_folder=self._source_subfolder, pkg_config_paths=[self.install_folder], args=args)
+        return meson
+
+    def build(self):
+        tools.replace_in_file(os.path.join(self._source_subfolder, "meson.build"), "\ntest(\n", "\nfalse and test(\n")
+        with tools.environment_append(tools.RunEnvironment(self).vars):
+            meson = self._configure_meson()
+            meson.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        meson = self._configure_meson()
+        with tools.environment_append({"PKG_CONFIG_PATH": self.install_folder}):
+            meson.install()
+        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
+        # If so, you can just remove the lines below
+        include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy(pattern="*", dst="include", src=include_folder)
+        self.copy(pattern="*.dll", dst="bin", keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["gailutil-3", "gtk-3", "gdk-3"]
+        self.cpp_info.includedirs.append(os.path.join("include", "gtk-3.0"))
+        self.cpp_info.includedirs.append(os.path.join("include", "gail-3.0"))
+        self.cpp_info.names["pkg_config"] = "gtk+-3.0"
+        if self.settings.os == "Macos":
+            self.cpp_info.frameworks = ["AppKit", "Carbon"]
