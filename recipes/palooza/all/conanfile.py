1aea134590cf24c19d258ae57738c0dd4a722448
SSE4 <tomskside@gmail.com>
2020-11-21 14:37:54 +0700

    Update conanfile.py

--- recipes/palooza/all/conanfile.py
+++ recipes/palooza/all/conanfile.py
@@ -61,7 +61,8 @@ class PaloozaConan(ConanFile):
         raise Exception(inspect.currentframe().f_code.co_name)
         
     def init(self):
-        raise Exception(inspect.currentframe().f_code.co_name)
+        pass # 1
+        #raise Exception(inspect.currentframe().f_code.co_name)
 
     def export(self):
         raise Exception(inspect.currentframe().f_code.co_name)
