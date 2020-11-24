df8025f0f4aa1c3982beeacc4a548bbf2ba310f6
Walid Boussaffa <boussaffa.walid@outlook.com>
2020-11-24 13:34:04 +0100

    added recipe for seqan

---
+++ recipes/seqan/2.x.x/conanfile.py
@@ -0,0 +1,39 @@
+import os
+from conans import ConanFile, tools
+
+class SeqanConan(ConanFile):
+    name = "seqan"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/seqan/seqan"
+    description = """
+SeqAn is an open source C++ library of efficient algorithms and data structures for the analysis of sequences 
+with the focus on biological data. Our library applies a unique generic design that guarantees high performance, 
+generality, extensibility, and integration with other libraries. 
+SeqAn is easy to use and simplifies the development of new software tools with a minimal loss of performance.
+"""
+    topics = ("conan", "cpp98", "cpp11", "cpp14", "cpp17",
+              "algorithms", "data structures", "biological sequences")
+    license = "BSD-3-Clause"
+    settings = "compiler"
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def configure(self):
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, 14)
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = "seqan-seqan-v" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy("*", dst="include",
+                  src=os.path.join(self._source_subfolder, "include"), keep_path=True)
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
---
+++ recipes/seqan/2.x.x/test_package/test_package.cpp
@@ -0,0 +1,25 @@
+// Demo program for clipping with Gaps objects.
+
+#include <iostream>
+#include <seqan/sequence.h>
+#include <seqan/align.h>
+
+using namespace seqan;
+
+int main()
+{
+    // Create sequence variable and gaps basd on sequence.
+    CharString seq("ABCDEFGHIJ");
+    Gaps<CharString> gaps(seq);
+
+    // Insert gaps, the positions are in (clipped) view space.
+    insertGaps(gaps, 2, 2);
+    insertGap(gaps, 6);
+    insertGap(gaps, 10);
+
+    // Print to stdout.
+    std::cout << "gaps\t" << gaps << "\n"
+              << "seq \t" << seq << "\n\n";
+
+    return 0;
+}
