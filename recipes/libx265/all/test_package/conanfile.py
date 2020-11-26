601933dbced85f6500cdf6283e3deaedf06afd78
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-11-26 21:53:30 +0100

    libx265: set LD_LIBRARY_PATH/DYLD_LIBRARY_PATH to test linked library

--- recipes/libx265/all/test_package/conanfile.py
+++ recipes/libx265/all/test_package/conanfile.py
@@ -17,10 +17,12 @@ class TestPackageConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         cmake.definitions["TEST_LIBRARY"] = self._test_shared_library
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.build_folder.replace("\\", "/")
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join(".", "test_package")
-            self.run(bin_path, run_environment=True)
+            with tools.environment_append({"LD_LIBRARY_PATH": "bin", "DYLD_LIBRARY_PATH": "bin"}):
+                bin_path = os.path.join("bin", "test_package")
+                self.run(bin_path, run_environment=True)
