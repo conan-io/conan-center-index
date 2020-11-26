fbe0cf5a5829fd993e902a7aae20ed8801b10b08
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-11-26 02:34:11 +0100

    ceres-solver: add 2.0.0 release

--- recipes/ceres-solver/all/conanfile.py
+++ recipes/ceres-solver/all/conanfile.py
@@ -33,39 +33,19 @@ class ceressolverConan(ConanFile):
                        "use_CXX11": False,
                        "use_schur_specializations": True}
     exports_sources = ["CMakeLists.txt","patches/*"]
-    _source_subfolder = "source_subfolder"
-    _cmake = None
 
-    def _configure_cmake(self):
-        if not self._cmake:
-            self._cmake = CMake(self)       #You can check what these flags do in http://ceres-solver.org/installation.html
-            self._cmake.definitions["LIB_SUFFIX"] = ""
-            self._cmake.definitions["GFLAGS"] = self.options.use_gflags
-            self._cmake.definitions["BUILD_EXAMPLES"] = False           #Requires gflags
-            self._cmake.definitions["BUILD_TESTING"] = False            #Requires gflags
-            self._cmake.definitions["BUILD_DOCUMENTATION"] = False      #Requires python modules Sphinx and sphinx-rtd-theme
-            self._cmake.definitions["CUSTOM_BLAS"] = self.options.use_custom_blas
-            self._cmake.definitions["EIGEN_PREFER_EXPORTED_EIGEN_CMAKE_CONFIGURATION"] = False    #Set to false to Force CMake to use the conan-generated dependencies
-            self._cmake.definitions["GLOG_PREFER_EXPORTED_GLOG_CMAKE_CONFIGURATION"] = False      #Set to false to Force CMake to use the conan-generated dependencies
-            self._cmake.definitions["GFLAGS_PREFER_EXPORTED_GFLAGS_CMAKE_CONFIGURATION"] = False  #Set to false to Force CMake to use the conan-generated dependencies
-            self._cmake.definitions["EIGENSPARSE"] = self.options.use_eigen_sparse
-            self._cmake.definitions["SUITESPARSE"] = False  #Optional. Not sufpported right now because SuiteSparse is not part of conan-index
-            self._cmake.definitions["LAPACK"] = False       #Optional. Not supported right now because LAPACK is not part of conan-index
-            self._cmake.definitions["OPENMP"] = False
-            self._cmake.definitions["CXSPARSE"] = False     #Optional. Not supported right now because CXSSPARSE is not part of conan-index
-            self._cmake.definitions["MINIGLOG"] = not self.options.use_glog
-            self._cmake.definitions["TBB"] = self.options.use_TBB
-            self._cmake.definitions["CXX11_THREADS"] = self.options.use_CXX11_threads
-            self._cmake.definitions["CXX11"] = self.options.use_CXX11
-            self._cmake.definitions["SCHUR_SPECIALIZATIONS"] = self.options.use_schur_specializations
-            if self.settings.compiler == "Visual Studio":
-                self._cmake.definitions["MSVC_USE_STATIC_CRT"] = str(self.settings.compiler.runtime) == "MT" or str(self.settings.compiler.runtime) == "MTd"
-            self._cmake.configure()
-        return self._cmake
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    _cmake = None
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+        if tools.Version(self.version) >= "2.0":
+            del self.options.use_CXX11_threads
+            del self.options.use_CXX11
 
     def configure(self):
         if self.options.shared:
@@ -74,6 +54,9 @@ class ceressolverConan(ConanFile):
             raise ConanInvalidConfiguration("Ceres-solver only links against the release version of glog")
         if self.options.use_glog and not self.options.use_gflags: #At this stage we can't check the value of self.options["glog"].with_gflags so we asume it is true because is the default value
             raise ConanInvalidConfiguration("To depend on glog built with gflags (Default behavior) set use_gflags=True, otherwise Ceres may fail to link due to missing gflags symbols.")
+        if tools.Version(self.version) >= "2.0":
+            # 1.x uses ceres-solver specific FindXXX.cmake modules
+            self.generators.append("cmake_find_package")
 
     def requirements(self):
         self.requires("eigen/3.3.8")
@@ -90,6 +73,37 @@ class ceressolverConan(ConanFile):
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)       #You can check what these flags do in http://ceres-solver.org/installation.html
+        self._cmake.definitions["LIB_SUFFIX"] = ""
+        self._cmake.definitions["GFLAGS"] = self.options.use_gflags
+        self._cmake.definitions["BUILD_EXAMPLES"] = False           #Requires gflags
+        self._cmake.definitions["BUILD_TESTING"] = False            #Requires gflags
+        self._cmake.definitions["BUILD_DOCUMENTATION"] = False      #Requires python modules Sphinx and sphinx-rtd-theme
+        self._cmake.definitions["CUSTOM_BLAS"] = self.options.use_custom_blas
+        self._cmake.definitions["GLOG_PREFER_EXPORTED_GLOG_CMAKE_CONFIGURATION"] = False      #Set to false to Force CMake to use the conan-generated dependencies
+        self._cmake.definitions["EIGENSPARSE"] = self.options.use_eigen_sparse
+        self._cmake.definitions["SUITESPARSE"] = False  #Optional. Not sufpported right now because SuiteSparse is not part of conan-index
+        self._cmake.definitions["LAPACK"] = False       #Optional. Not supported right now because LAPACK is not part of conan-index
+        self._cmake.definitions["CXSPARSE"] = False     #Optional. Not supported right now because CXSSPARSE is not part of conan-index
+        self._cmake.definitions["MINIGLOG"] = not self.options.use_glog
+        if not self.options.use_TBB:
+            self._cmake.definitions["CMAKE_DISABLE_FIND_PACKAGE_TBB"] = True
+        if tools.Version(self.version) < "2.0":
+            self._cmake.definitions["TBB"] = self.options.use_TBB
+            self._cmake.definitions["OPENMP"] = False
+            self._cmake.definitions["EIGEN_PREFER_EXPORTED_EIGEN_CMAKE_CONFIGURATION"] = False    #Set to false to Force CMake to use the conan-generated dependencies
+            self._cmake.definitions["GFLAGS_PREFER_EXPORTED_GFLAGS_CMAKE_CONFIGURATION"] = False  #Set to false to Force CMake to use the conan-generated dependencies
+            self._cmake.definitions["CXX11_THREADS"] = self.options.use_CXX11_threads
+            self._cmake.definitions["CXX11"] = self.options.use_CXX11
+        self._cmake.definitions["SCHUR_SPECIALIZATIONS"] = self.options.use_schur_specializations
+        if self.settings.compiler == "Visual Studio":
+            self._cmake.definitions["MSVC_USE_STATIC_CRT"] = str(self.settings.compiler.runtime) == "MT" or str(self.settings.compiler.runtime) == "MTd"
+        self._cmake.configure()
+        return self._cmake
+
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
@@ -97,9 +111,9 @@ class ceressolverConan(ConanFile):
         cmake.build()
 
     def package(self):
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
         cmake = self._configure_cmake()
         cmake.install()
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
         tools.rmdir(os.path.join(self.package_folder, "CMake"))
 
@@ -110,6 +124,9 @@ class ceressolverConan(ConanFile):
         self.cpp_info.components["ceres"].includedirs = ["include", os.path.join("include","ceres")]
         if not self.options.use_glog:
             self.cpp_info.components["ceres"].includedirs.append(os.path.join("include","ceres", "internal", "miniglog"))
+        if self.settings.os == "Linux":
+            if self.options.get_safe("use_CXX11_threads", True):
+                self.cpp_info.components["ceres"].system_libs.append("pthread")
         self.cpp_info.components["ceres"].requires = ["eigen::eigen"]
         if self.options.use_glog:
             self.cpp_info.components["ceres"].requires.append("glog::glog")
