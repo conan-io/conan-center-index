6a449d57d6e18bbf305d63e6895385720943dd17
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-11-28 16:09:56 +0100

    prefer to be explicit about glfw install

--- recipes/glfw/all/conanfile.py
+++ recipes/glfw/all/conanfile.py
@@ -58,6 +58,7 @@ class GlfwConan(ConanFile):
             self._cmake.definitions["GLFW_BUILD_EXAMPLES"] = False
             self._cmake.definitions["GLFW_BUILD_TESTS"] = False
             self._cmake.definitions["GLFW_BUILD_DOCS"] = False
+            self._cmake.definitions["GLFW_INSTALL"] = True
             if self.settings.compiler == "Visual Studio":
                 self._cmake.definitions["USE_MSVC_RUNTIME_LIBRARY_DLL"] = "MD" in self.settings.compiler.runtime
             self._cmake.configure()
