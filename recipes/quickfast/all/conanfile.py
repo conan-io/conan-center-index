489156288a75418f60b033576bb003afb62d9aaa
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-12-03 01:33:10 -0300

    Revert back to cmake build

--- recipes/quickfast/all/conanfile.py
+++ recipes/quickfast/all/conanfile.py
@@ -1,7 +1,6 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild
-from conans.errors import ConanInvalidConfiguration
-from conans.model.version import Version
+from conans import ConanFile, CMake, tools
 import os
+import shutil
 
 
 class QuickfastConan(ConanFile):
@@ -18,12 +17,9 @@ class QuickfastConan(ConanFile):
     default_options = {"fPIC": True,
                        "shared": False}
     requires = ["boost/1.73.0", "xerces-c/3.2.3"]
-    build_requires = "makefile-project-workspace-creator/4.1.46"
-    exports_sources = "patches/**"
-    generators = "visual_studio", "make"
-    _msbuild = None
-    _env_build = None
-    _args = None
+    generators = "cmake"
+    exports_sources = "CMakeLists.txt", "patches/**"
+    _cmake = None
 
     @property
     def _source_subfolder(self):
@@ -33,103 +29,12 @@ class QuickfastConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
-    @property
-    def _visual_studio_platform(self):
-        if self.settings.arch == "x86":
-            return "Win32"
-        elif self.settings.arch == "x86_64":
-            return "x64"
-        else:
-            raise ConanInvalidConfiguration("QuickFAST cannot be built on Visual Studio for the {} architecture"
-                                            .format(self.settings.arch))
-
-    @property
-    def _wss_platform(self):
-        subsystem = tools.os_info.detect_windows_subsystem()
-        if subsystem:
-            raise ConanInvalidConfiguration("QuickFAST cannot be built for Windows subsytems ({})"
-                                            .format(subsystem))
-        return "mingw_msys"
-
-    @property
-    def _platform(self):
-        if self.settings.compiler == "Visual Studio":
-            return self._visual_studio_platform
-        elif self.settings.os == 'Macos':
-            return 'macosx'
-        elif self.settings.os == 'Linux':
-            return 'linux'
-        elif self.settings.os == 'Windows':
-            return self._wss_platform
-        else:
-            raise ConanInvalidConfiguration("QuickFAST cannot be built for the {} operating system"
-                                            .format(self.settings.os))
-
-    @property
-    def _visual_studio_build_type(self):
-        compiler = self.settings.compiler
-        version = Version(compiler.version.value)
-
-        if "8" <= version <= "14":
-            return "v" + version
-        elif version == "14":
-            return "vs2017"
-        elif version == "16":
-            return "vs2019"
-        else:
-            raise ConanInvalidConfiguration("QuickFAST cannot be built with Visual Studio version {}"
-                                            .format(version))
-
-    @property
-    def _build_type(self):
-        if self.settings.compiler == "Visual Studio":
-            return self._visual_studio_build_type
-        else:
-            return 'make'
-
-    @property
-    def _core_value_template_linkflags(self):
-        if self.settings.os == "Macos":
-            return ' -value_template "linkflags+=-framework CoreServices -framework CoreFoundation"'
-        return ''
-
-    @property
-    def _mwc_command_line(self):
-        link_flag = ' -static' if not self.options.shared else ' '
-        fpic_flag = '-fPIC' if self.options.shared or self.options.get_safe("fPIC", default=False) else ''
-        platform = self._platform
-        build_type = self._build_type
-        core_value_template_linkflags = self._core_value_template_linkflags
-
-        return 'perl -S mwc.pl -type {} -exclude src/Examples'.format(build_type) + \
-               link_flag + \
-               ' -value_template "pic={}"'.format(fpic_flag) + \
-               ' -value_template platforms={}'.format(platform) + \
-               core_value_template_linkflags + \
-               ' -value_template extracppflags+=-DBOOST_BIND_GLOBAL_PLACEHOLDERS' + \
-               ' QuickFAST.mwc'
-
-    def _configure_msbuild(self):
-        if self._msbuild:
-            return self._msbuild
-
-        self.run(self._mwc_command_line)
-        self._msbuild = MSBuild(self)
-        return self._msbuild
-
-    def _configure_autotools(self):
-        if self._env_build:
-            return self._env_build, self._args
-
-        self._env_build = AutoToolsBuildEnvironment(self, win_bash=self.settings.os is "Windows")
-        self._args = ['CONAN_MAKE_FILE=' + os.path.join(self.build_folder, "conanbuildinfo.mak")]
-        self.run(self._mwc_command_line, win_bash=self.settings.os is "Windows")
-        return self._env_build, self._args
-
-    def _patch_sources(self):
-        patches = self.conan_data["patches"][self.version]
-        for patch in patches:
-            tools.patch(**patch)
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            self._cmake.definitions["BOOST_BIND_NO_PLACEHOLDERS"] = True
+            self._cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
+        return self._cmake
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -139,62 +44,25 @@ class QuickfastConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def configure(self):
-        if self.settings.os == "Windows":
-            raise ConanInvalidConfiguration("Windows builds still not working...")
-
-        tools.check_min_cppstd(self, "11")
-
-        if self.options.shared:
-            del self.options.fPIC
-
-    def build_requirements(self):
-        if self.settings.compiler != "Visual Studio" and \
-                tools.os_info.is_windows and \
-                "CONAN_BASH_PATH" not in os.environ and \
-                tools.os_info.detect_windows_subsystem() != "msys2":
-            self.build_requires("msys2/20190524")
-
     def build(self):
-        self._patch_sources()
+        # Patch taken from:
+        # https://raw.githubusercontent.com/microsoft/vcpkg/master/ports/quickfast/00001-fix-boost-asio.patch
+        patches = self.conan_data["patches"][self.version]
+        for patch in patches:
+            tools.patch(**patch)
 
-        with tools.chdir(self._source_subfolder), \
-             tools.environment_append(
-                 {"QUICKFAST_ROOT": os.path.join(self.build_folder, self._source_subfolder),
-                  "XERCESCROOT": self.deps_cpp_info["xerces-c"].rootpath,
-                  }):
+        shutil.copy("CMakeLists.txt",
+                    os.path.join(self._source_subfolder, "CMakeLists.txt"))
 
-            if self.settings.compiler == "Visual Studio":
-                msbuild = self._configure_msbuild()
-                msbuild.build("QuickFAST.sln")
-            else:
-                env_build, args = self._configure_autotools()
-                env_build.make(args=args, target="QuickFAST")
+        cmake = self._configure_cmake()
+        cmake.build(target="quickfast")
 
     def package(self):
-        with tools.chdir(self._source_subfolder), \
-             tools.environment_append(
-                 {"QUICKFAST_ROOT": os.path.join(self.build_folder, self._source_subfolder),
-                  "XERCESCROOT": self.deps_cpp_info["xerces-c"].rootpath,
-                  }):
-
-            if self.settings.compiler == "Visual Studio":
-                msbuild = self._configure_msbuild()
-                msbuild.build("QuickFAST")
-            else:
-                env_build, args = self._configure_autotools()
-                env_build.make(args=args, target="QuickFAST")
-
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.h", dst="include/quickfast", src=os.path.join(self._source_subfolder, "src"))
+        cmake = self._configure_cmake()
+        cmake.install()
         self.copy("license.txt", dst="licenses", src=self._source_subfolder)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.includedirs.append(os.path.join("include", "quickfast"))
         self.cpp_info.defines.append("BOOST_BIND_NO_PLACEHOLDERS")
-        self.cpp_info.defines.append("XML_USE_PTHREADS")
-        if self.options.shared:
-            self.cpp_info.defines.append("QUICKFAST_BUILD_DLL")
