c7106e92e8dc18fb2ff6f9d5508dff167538dedf
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-12-03 08:13:03 -0300

    again...

--- recipes/quickfast/all/conanfile.py
+++ recipes/quickfast/all/conanfile.py
@@ -70,5 +70,6 @@ class QuickfastConan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.includedirs.append(os.path.join("include", "quickfast"))
         self.cpp_info.defines.append("BOOST_BIND_NO_PLACEHOLDERS")
+
         if not self.options.shared:
             self.cpp_info.defines.append("QUICKFAST_HAS_DLL=0")
