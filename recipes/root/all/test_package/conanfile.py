66718b6410b80dbf785475a1a21be040733b5d5e
David Hadley <davehadley@users.noreply.github.com>
2020-12-04 22:58:28 +0000

    Set CMAKE_CXX_STANDARD in root/all/test_package.

--- recipes/root/all/test_package/conanfile.py
+++ recipes/root/all/test_package/conanfile.py
@@ -7,12 +7,32 @@ class RootTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
 
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+
+    @property
+    def _minimum_cpp_standard(self):
+        return 11
+
     def build(self):
         env_build = RunEnvironment(self)
         with tools.environment_append(env_build.vars):
             cmake = CMake(self)
-            cmake.configure()
+            cmake.configure(
+                {
+                    "CMAKE_CXX_STANDARD": self._cmake_cxx_standard,
+                }
+            )
             cmake.build()
+
+    @property
+    def _cmake_cxx_standard(self):
+        compileropt = self.settings.compiler.get_safe("cppstd")
+        if compileropt:
+            return str(compileropt)
+        else:
+            return "11"
 
     def test(self):
         if not tools.cross_building(self):
