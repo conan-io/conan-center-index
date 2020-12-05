65a0f69faea5ec22cc5b25948f00d8e2836535ef
czoido <mrgalleta@gmail.com>
2020-12-05 07:32:01 +0100

    add librealsense 2.40.0

---
+++ recipes/librealsense/all/conanfile.py
@@ -0,0 +1,72 @@
+from conans import ConanFile, tools, CMake
+import os
+import glob
+
+
+class LibRealSenseConan(ConanFile):
+    name = "librealsense"
+    description = "Cross-platform library for Intel RealSense depth cameras (D400 series and the SR300) " \
+                  "and the T265 tracking camera."
+    homepage = "https://github.com/IntelRealSense/librealsense"
+    topics = "conan", "intel", "3d", "computer vision", "robotics"
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    exports_sources = "CMakeLists.txt"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False], "openmp": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "openmp": True}
+    generators = "cmake"
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def requirements(self):
+        if self.settings.os == "Linux":
+            self.requires("libusb/1.0.23")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename(glob.glob("librealsense*")[0], self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_EXAMPLES"] = False
+        self._cmake.definitions["BUILD_GLSL_EXTENSIONS"] = False
+        self._cmake.definitions["BUILD_WITH_OPENMP"] = self.options.openmp
+        self._cmake.definitions["BUILD_UNIT_TESTS"] = False
+        self._cmake.definitions["BUILD_WITH_TM2"] = False
+        self._cmake.definitions["BUILD_NETWORK_DEVICE"] = False
+        self._cmake.definitions["IMPORT_DEPTH_CAM_FW"] = False
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
---
+++ recipes/librealsense/all/test_package/test_package.cpp
@@ -0,0 +1,7 @@
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+
+int main(int argc, char * argv[])
+{
+    rs2::log_to_console(RS2_LOG_SEVERITY_ERROR);
+    return EXIT_SUCCESS;
+}
