e081854649a84750b9cd14a20349e4058510c82d
mjvankampen <mjvk@allseas.com>
2020-12-08 14:32:12 +0100

    (#3345) Flatbuffers flatc integration
    * Adds flatc
    
    * Fixes test package
    
    * Better handling of options
    
    * Adds flatc patch and static
    
    * Fixes missing export
    
    * Adds back fpic
    
    * Cleans up
    
    * Renames autodetect option to be more clear
    
    * Fixes some pep8 issues
    
    * Fixes order of deleting options

--- recipes/flatbuffers/all/test_package/conanfile.py
+++ recipes/flatbuffers/all/test_package/conanfile.py
@@ -7,13 +7,17 @@ class TestPackageConan(ConanFile):
     generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["FLATBUFFERS_HEADER_ONLY"] = self.options["flatbuffers"].header_only
-        cmake.definitions["FLATBUFFERS_SHARED"] = not self.options["flatbuffers"].header_only and self.options["flatbuffers"].shared
-        cmake.configure()
-        cmake.build()
+        if self.options["flatbuffers"].flatbuffers:
+            cmake = CMake(self)
+            cmake.definitions["FLATBUFFERS_HEADER_ONLY"] = self.options["flatbuffers"].header_only
+            cmake.definitions["FLATBUFFERS_SHARED"] = not self.options["flatbuffers"].header_only and self.options["flatbuffers"].shared
+            cmake.configure()
+            cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+            if self.options["flatbuffers"].flatbuffers:
+                bin_path = os.path.join("bin", "test_package")
+                self.run(bin_path, run_environment=True)
+            if self.options["flatbuffers"].flatc:
+                self.run("flatc --version", run_environment=True)
