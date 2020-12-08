8b2a79cda42515b5de6cfb335db1cee55180ee42
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-12-08 09:58:18 +0100

    re-enable clang 9 support for 1.1.3

--- recipes/open62541/all/conanfile.py
+++ recipes/open62541/all/conanfile.py
@@ -135,13 +135,14 @@ class Open62541Conan(ConanFile):
                 raise ConanInvalidConfiguration(
                     "Lower Open62541 versions than 1.1.0 are not cpp compatible due to -fpermisive flags")
 
+            if self.settings.compiler == "clang" and tools.Version(self.settings.compiler.version) >= "9":
+                raise ConanInvalidConfiguration(
+                    "Open62541 does not support Clang 9.x.x and later compiler versions")
+
         if self.settings.compiler == "clang":
             if tools.Version(self.settings.compiler.version) <= "4":
                 raise ConanInvalidConfiguration(
                     "Older clang compiler version than 4.0 are not supported")
-            if tools.Version(self.settings.compiler.version) >= "9":
-                raise ConanInvalidConfiguration(
-                    "Open62541 does not support Clang 9.x.x and later compiler versions")
 
         if self.options.pub_sub != False and self.settings.os != "Linux":
             raise ConanInvalidConfiguration(
