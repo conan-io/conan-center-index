062e9d517d9644683d5ba7aed9c49738bd6ed3bc
Anonymous Maarten <madebr@users.noreply.github.com>
2020-12-09 22:32:06 +0100

    (#3639) tinyspline: bump to 0.3.0
    * tinyspline: bump to 0.3
    
    * tinyspline: fix type in test_package macro/cmake option
    
    * tinyspline: fix typo
    
    * tinyspline: 0.2.0 has a lib prefix
    
    * tinyspline: Debug 0.2.0 tinyspline libraries on MSVC has d suffx
    
    * tinyspline: 0.2.0 has only lib prefix when building static MSVC

--- recipes/tinyspline/all/test_package/test_package.c
+++ recipes/tinyspline/all/test_package/test_package.c
@@ -2,7 +2,12 @@
 
 int main() {
   tsBSpline spline;
+#ifdef TINYSPLINE_API_0_3
+  tsStatus status;
+  ts_bspline_new(6, 3, 3, TS_OPENED, &spline, &status);
+#else
   ts_bspline_new(6, 3, 3, TS_OPENED, &spline);
+#endif
   ts_bspline_free(&spline);
 
   return 0;
