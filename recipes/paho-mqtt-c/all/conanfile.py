bdd34b2bec31a639557832ec29898a6b2876d301
a4z <harald.achitz@gmail.com>
2020-12-09 11:54:58 +0100

    update paho-mqtt-c to 1.3.7

--- recipes/paho-mqtt-c/all/conanfile.py
+++ recipes/paho-mqtt-c/all/conanfile.py
@@ -44,7 +44,7 @@ class PahoMqttcConan(ConanFile):
 
     def requirements(self):
         if self.options.ssl:
-            self.requires("openssl/1.1.1g")
+            self.requires("openssl/1.1.1h")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
