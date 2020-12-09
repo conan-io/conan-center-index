d8dcc3c120ab593c6062240aa8e26ecad3b67d10
mjvankampen <markjanvankampen@gmail.com>
2020-12-09 03:38:06 +0100

    Update recipes/baical-p7/all/conanfile.py
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/baical-p7/all/conanfile.py
+++ recipes/baical-p7/all/conanfile.py
@@ -2,7 +2,7 @@ import os
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.29.0"
+required_conan_version = ">=1.29.1"
 
 class FlatbuffersConan(ConanFile):
     name = "baical-p7"
