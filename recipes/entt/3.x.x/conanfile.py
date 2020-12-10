7ee396e2ad5534984349e02d82cce182003f08ea
jgsogo <jgsogo@gmail.com>
2020-12-10 11:55:24 +0100

    assume minor version satisfies

--- recipes/entt/3.x.x/conanfile.py
+++ recipes/entt/3.x.x/conanfile.py
@@ -21,14 +21,14 @@ class EnttConan(ConanFile):
     def configure(self):
         # FIXME: Here we are implementing a workaround because ConanCenter is not generating any configuration with
         #   C++17 support (either supported by default by a compiler or using 'compiler.cppstd=17' in the Conan profile)
-        #   and ConanCenter requires that at least one package is generated. Once, ConanCenter iterates a profile using
+        #   and ConanCenter requires that at least one package is generated. Once ConanCenter uses a profile
         #   with C++17 support, only a raw call to `tools.check_mis_cppstd(self, "17")` will be required.
         minimal_cpp_standard = "17"
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, minimal_cpp_standard)
 
         minimal_version = {
-            "Visual Studio": "15",
+            "Visual Studio": "15.9",
             "gcc": "7",
             "clang": "5",
             "apple-clang": "10"
@@ -42,8 +42,15 @@ class EnttConan(ConanFile):
                 "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
             return
 
-        version = tools.Version(self.settings.compiler.version)
-        if version < minimal_version[compiler]:
+        # Compare versions asuming minor satisfies if not explicitly set
+        def gte_compiler_version(version1, version2):
+            v1, *v1_res = version1.split(".", 1)
+            v2, *v2_res = version2.split(".", 1)
+            if v1 == v2 and v1_res and v2_res:
+                return gte_compiler_version(v1_res, v2_res)
+            return v1 >= v2
+
+        if not gte_compiler_version(str(self.settings.compiler.version), minimal_version[compiler]):
             raise ConanInvalidConfiguration(
                 "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
 
