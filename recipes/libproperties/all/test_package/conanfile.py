41c028676ca3bdb240098486e51747dd331b7110
libbylg <libbylg@126.com>
2020-12-14 07:36:31 +0800

    Update recipes/libproperties/all/test_package/conanfile.py
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/libproperties/all/test_package/conanfile.py
+++ recipes/libproperties/all/test_package/conanfile.py
@@ -12,11 +12,6 @@ class LibpropertiesTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="lib")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-
     def test(self):
         if not tools.cross_building(self):
             os.chdir("bin")
