0654b94fec18ec699163fb0beb9d7c045fab03f4
Juan Jose Castellanos <juancho.312@hotmail.com>
2020-12-16 18:00:57 -0500

    add soci 4.0.1 package
    - Squash commits to fix bad user

---
+++ recipes/soci/all/test_package/conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake, tools
+
+class SociTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    requires = ["catch2/2.13.3", "fmt/6.2.0"]
+
+    def requirements(self):
+        self.requires("catch2/2.13.3")
+        self.requires("fmt/6.2.0")
+
+    def configure(self):
+        self.options["soci"].static     = True
+        self.options["soci"].cxx11      = True
+        self.options["soci"].empty      = True
+        self.options["soci"].fPIC       = True
+        self.options["soci"].shared     = True
+        self.options["soci"].sqlite3    = True
+        self.options["soci"].db2        = False
+        self.options["soci"].odbc       = False
+        self.options["soci"].oracle     = False
+        self.options["soci"].firebird   = False
+        self.options["soci"].mysql      = False
+        self.options["soci"].postgresql = False
+        self.options["soci"].tests      = False
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self):
+            self.run("ctest . -Q")
