d0790da636f3c09b2e236c7ea4463cc35fd95461
Juan Jose Castellanos <juancho.312@hotmail.com>
2020-12-16 18:30:05 -0500

    add conditional fPIC for Windows in test_package

--- recipes/soci/all/test_package/conanfile.py
+++ recipes/soci/all/test_package/conanfile.py
@@ -9,6 +9,10 @@ class SociTestConan(ConanFile):
         self.requires("catch2/2.13.3")
         self.requires("fmt/6.2.0")
 
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
     def configure(self):
         self.options["soci"].static     = True
         self.options["soci"].cxx11      = True
