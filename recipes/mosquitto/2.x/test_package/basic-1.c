0d111ccca621184be3cd02e63a239a9b02bde2c6
a4z <harald.achitz@gmail.com>
2020-12-20 21:43:31 +0100

    remove unneded test code
    we want to build and link and do just some call
    that's enough

--- recipes/mosquitto/2.x/test_package/basic-1.c
+++ recipes/mosquitto/2.x/test_package/basic-1.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+
 
 
 /* Callback called when the client receives a CONNACK message from the broker. */
@@ -40,39 +40,6 @@ void on_publish(struct mosquitto *mosq, void *obj, int mid)
 }
 
 
-int get_temperature(void)
-{
-	sleep(1); /* Prevent a storm of messages - this pretend sensor works at 1Hz */
-	return random()%100;
-}
-
-/* This function pretends to read some data from a sensor and publish it.*/
-void publish_sensor_data(struct mosquitto *mosq)
-{
-	char payload[20];
-	int temp;
-	int rc;
-
-	/* Get our pretend data */
-	temp = get_temperature();
-	/* Print it to a string for easy human reading - payload format is highly
-	 * application dependent. */
-	snprintf(payload, sizeof(payload), "%d", temp);
-
-	/* Publish the message
-	 * mosq - our client instance
-	 * *mid = NULL - we don't want to know what the message id for this message is
-	 * topic = "example/temperature" - the topic on which this message will be published
-	 * payloadlen = strlen(payload) - the length of our payload in bytes
-	 * payload - the actual payload
-	 * qos = 2 - publish with QoS 2 for this example
-	 * retain = false - do not use the retained message feature for this message
-	 */
-	rc = mosquitto_publish(mosq, NULL, "example/temperature", strlen(payload), payload, 2, false);
-	if(rc != MOSQ_ERR_SUCCESS){
-		fprintf(stderr, "Error publishing: %s\n", mosquitto_strerror(rc));
-	}
-}
 
 
 int main(int argc, char *argv[])
@@ -98,35 +65,7 @@ int main(int argc, char *argv[])
 	mosquitto_connect_callback_set(mosq, on_connect);
 	mosquitto_publish_callback_set(mosq, on_publish);
 
-	// /* Connect to test.mosquitto.org on port 1883, with a keepalive of 60 seconds.
-	//  * This call makes the socket connection only, it does not complete the MQTT
-	//  * CONNECT/CONNACK flow, you should use mosquitto_loop_start() or
-	//  * mosquitto_loop_forever() for processing net traffic. */
-	// rc = mosquitto_connect(mosq, "test.mosquitto.org", 1883, 60);
-	// if(rc != MOSQ_ERR_SUCCESS){
-	// 	mosquitto_destroy(mosq);
-	// 	fprintf(stderr, "Error: %s\n", mosquitto_strerror(rc));
-	// 	return 1;
-	// }
 
-	// /* Run the network loop in a background thread, this call returns quickly. */
-	// rc = mosquitto_loop_start(mosq);
-	// if(rc != MOSQ_ERR_SUCCESS){
-	// 	mosquitto_destroy(mosq);
-	// 	fprintf(stderr, "Error: %s\n", mosquitto_strerror(rc));
-	// 	return 1;
-	// }
-
-	// /* At this point the client is connected to the network socket, but may not
-	//  * have completed CONNECT/CONNACK.
-	//  * It is fairly safe to start queuing messages at this point, but if you
-	//  * want to be really sure you should wait until after a successful call to
-	//  * the connect callback.
-	//  * In this case we know it is 1 second before we start publishing.
-	//  */
-	// while(1){
-	// 	publish_sensor_data(mosq);
-	// }
 
 	mosquitto_lib_cleanup();
 	return 0;
