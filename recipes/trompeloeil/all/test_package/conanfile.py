55d931f4b12256222ce281d89f497d26cce16352
Florian Berchtold <florian.berchtold@luminartech.com>
2020-12-22 00:02:48 +0100

    test_package uses cmake_find_package generator

--- recipes/trompeloeil/all/test_package/conanfile.py
+++ recipes/trompeloeil/all/test_package/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
@@ -13,5 +13,4 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+            self.run("test_package", run_environment=True)
