3341e8e1af7c67a0cd3c2d238c9a13f98b653732
Harald <harald.achitz@gmail.com>
2020-12-22 18:20:56 +0100

    remove unneeded cmake generator
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/mosquitto/2.x/test_package/conanfile.py
+++ recipes/mosquitto/2.x/test_package/conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
@@ -14,4 +14,3 @@ class TestPackageConan(ConanFile):
         if not tools.cross_building(self.settings):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
-
