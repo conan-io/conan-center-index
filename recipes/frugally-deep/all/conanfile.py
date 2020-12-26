0d33cdc5ebba61f4b9fce459178262e8f3b11b14
Dobiasd <editgym@gmail.com>
2020-12-26 08:29:45 +0100

    add frugally-deep/v0.15.1-p0, closes #3956

---
+++ recipes/frugally-deep/all/conanfile.py
@@ -0,0 +1,67 @@
+from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+
+class FrugallyDeepConan(ConanFile):
+    name = "frugally-deep"
+    description = "Use Keras models in C++ with ease."
+    license = "The MIT License (MIT)"
+    topics = ("keras", "tensorflow")
+    homepage = "https://github.com/Dobiasd/frugally-deep"
+    url = "https://github.com/conan-io/conan-center-index"
+    settings = "os", "compiler"
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "4.9",
+            "Visual Studio": "14",
+            "clang": "3.7",
+            "apple-clang": "9",
+        }
+
+    def configure(self):
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, 14)
+
+        def lazy_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if not minimum_version:
+            self.output.warn("frugally-deep requires C++14. Your compiler is unknown. Assuming it supports C++14.")
+        elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration("frugally-deep requires C++14, which your compiler does not support.")
+
+    def requirements(self):
+        self.requires("eigen/3.3.9")
+        self.requires("functionalplus/v0.2.13-p0")
+        self.requires("jsonformoderncpp/3.1.0@conan/stable")
+
+    def package_id(self):
+        self.info.header_only()
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("frugally-deep-" + self.version, self._source_subfolder)
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy("*", dst="include", src=os.path.join(self._source_subfolder, "include"))
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "frugally-deep"
+        self.cpp_info.names["cmake_find_package_multi"] = "frugally-deep"
+        self.cpp_info.components["fdeep"].names["cmake_find_package"] = "fdeep"
+        self.cpp_info.components["fdeep"].names["cmake_find_package_multi"] = "fdeep"
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.components["fdeep"].system_libs = ["pthread"]
