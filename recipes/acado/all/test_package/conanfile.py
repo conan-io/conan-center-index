192a9dab7ef8ea7fe8fd0347caf073f31465e6fc
Florian Berchtold <florian.berchtold@luminartech.com>
2020-12-26 20:39:52 +0100

    windows stuff?

--- recipes/acado/all/test_package/conanfile.py
+++ recipes/acado/all/test_package/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package"
+    generators = "cmake", "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
