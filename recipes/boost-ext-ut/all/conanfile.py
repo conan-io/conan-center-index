b8301796479e9291e01045c3e0499ca7c44aa588
Jordan Williams <jordan@jwillikers.com>
2020-12-27 08:28:28 -0600

    Configure CMake for using find_package
    The behavior now models [Boost-ext].UT's current find_package behavior.
    A CMake config file is provided with the name ut.
    The library can be found with find_package(ut) as expected.
    The CMake target, boost::ut, is also provided as expected.
    I'm not sure how to remove the global target boost::boost.
    I'd hate for this to conflict with the Boost library.

--- recipes/boost-ext-ut/all/conanfile.py
+++ recipes/boost-ext-ut/all/conanfile.py
@@ -28,10 +28,15 @@ class UTConan(ConanFile):
             "apple-clang": "11",
         }
 
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
     def configure(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
-        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        min_version = self._minimum_compilers_version.get(
+            str(self.settings.compiler))
         if not min_version:
             self.output.warn("{} recipe lacks information about the {} compiler support.".format(
                 self.name, self.settings.compiler))
@@ -39,10 +44,6 @@ class UTConan(ConanFile):
             if tools.Version(self.settings.compiler.version) < min_version:
                 raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
                     self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -60,5 +61,8 @@ class UTConan(ConanFile):
         self.info.header_only()
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "UT"
-        self.cpp_info.names["cmake_find_package_multi"] = "UT"
+        self.cpp_info.names["cmake_find_package"] = "boost"
+        self.cpp_info.names["cmake_find_package_multi"] = "boost"
+        self.cpp_info.filenames["cmake_find_package"] = "ut"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "ut"
+        self.cpp_info.components["ut"].includedirs = []
