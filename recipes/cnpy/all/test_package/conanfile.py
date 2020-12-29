222547b77c658cafcec46cc751af0bcd111363b4
yuxi.hong <yuxi.hong@kaust.edu.sa>
2020-12-29 17:27:38 -0500

    change python indention

--- recipes/cnpy/all/test_package/conanfile.py
+++ recipes/cnpy/all/test_package/conanfile.py
@@ -3,17 +3,17 @@ import os
 from conans import ConanFile, CMake, tools
 
 class TestPackageConan(ConanFile):
-  settings = "os", "compiler", "build_type", "arch"
-  generators = "cmake"
-  requires = [
-    "zlib/1.2.11"
-  ]
-  def build(self):
-    cmake = CMake(self)
-    cmake.configure()
-    cmake.build()
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    requires = [
+        "zlib/1.2.11"
+    ]
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
-  def test(self):
-    if not tools.cross_building(self.settings):
-      bin_path = os.path.join("bin", "test_package")
-      self.run(bin_path, run_environment=True)
+    def test(self):
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
