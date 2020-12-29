c0625786cbc450f889d4b4e09304269f1314a180
yuxi.hong <yuxi.hong@kaust.edu.sa>
2020-12-29 17:07:08 -0500

    create cnpy package

---
+++ recipes/cnpy/all/conanfile.py
@@ -0,0 +1,66 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+class CnpyConan(ConanFile):
+  name = "cnpy"
+  description = "cnpy conan package"
+  license = "MIT License"
+  topics = ("conan", "cnpy")
+  homepage = "https://github.com/hongyx11/cnpy"
+  url = "https://github.com/conan-io/conan-center-index"
+  exports_sources = ["CMakeLists.txt"]
+  generators = "cmake"
+  settings = "os", "arch", "compiler", "build_type"
+  _cmake = None
+
+
+  options = {
+    "shared": [True, False],
+    "fPIC": [True, False]
+  }
+  default_options = {
+    "shared": False,
+    "fPIC": True
+  }
+
+  @property
+  def _source_subfolder(self):
+    return "source_subfolder"
+
+  @property
+  def _build_subfolder(self):
+    return "build_subfolder"
+
+  def config_options(self):
+    if self.settings.os == "Windows":
+      del self.options.fPIC
+
+  def configure(self):
+    del self.settings.compiler.libcxx
+    del self.settings.compiler.cppstd
+
+  def source(self):
+    tools.get(**self.conan_data["sources"][self.version])
+    os.rename(self.name + "-" + self.version, self._source_subfolder)
+
+  def build(self):
+    cmake = self._configure_cmake()
+    cmake.build()
+
+  def _configure_cmake(self):
+    if self._cmake:
+      return self._cmake
+    self._cmake = CMake(self)
+    self._cmake.configure(build_folder=self._build_subfolder)
+    return self._cmake
+
+  def package(self):
+    self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+    cmake = self._configure_cmake()
+    cmake.install()
+
+  def package_info(self):
+    self.cpp_info.names["cmake_find_package"] = "cnpy"
+    self.cpp_info.names["cmake_find_package_multi"] = "cnpy"
+    self.cpp_info.libs = tools.collect_libs(self)
