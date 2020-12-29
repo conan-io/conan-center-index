24cfb21657bc9286396df7732f60a177ef2e1ef1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-12-29 23:16:49 +0100

    explicit cpp_info.libs

--- recipes/openjpeg/all/conanfile.py
+++ recipes/openjpeg/all/conanfile.py
@@ -89,7 +89,7 @@ class OpenjpegConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.includedirs.append(os.path.join("include", self._openjpeg_subdir))
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["openjp2"]
         if not self.options.shared:
             self.cpp_info.defines.append('OPJ_STATIC')
         if self.settings.os == "Linux":
