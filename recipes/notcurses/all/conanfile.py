2a2b533792450be4509691cc9897d5a67e683aa5
nick black <dankamongmen@gmail.com>
2020-12-30 03:00:36 -0500

    notcurses: address feedback from Spacelm
    - Remove gitlab-ci.yml, build.py, toplevel conanfile.py
    - Merge toplevel conanfile.py into all/conanfile.py
    - Add conandata.yml for 2.1.2
    - Replace git source() stanza with one fetching tarball
    - Add requirements() stanza to all/conanfile.py
    - Remove with_doctest option (no testing in conan)
    - Remove version and author attributes
    - Drop conandata.yml "requirements"
    - notcurses: use cmake.install for package()
    - need pkg_config generator

--- recipes/notcurses/all/conanfile.py
+++ recipes/notcurses/all/conanfile.py
@@ -1,6 +1,5 @@
 from conans import ConanFile, CMake, tools
 
-
 class NotcursesConan(ConanFile):
     name = "notcurses"
     version = "2.1.2"
@@ -9,36 +8,24 @@ class NotcursesConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://nick-black.com/dankwiki/index.php/Notcurses"
     license = "Apache-2.0"
-    author = "nick black dank@qemfd.net"
     exports_sources = ["CMakeLists.txt"]
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False],
-               "with_doctest": [True, False]}
-    default_options = {"shared": True,
-                       "with_doctest": True}
-    generators = "cmake"
+    requires = "openimageio/2.2.7.0", "libunistring/0.9.10"
+    options = {"shared": [True, False]}
+    default_options = {"shared": True}
+    generators = "cmake", "pkg_config"
 
     def source(self):
-        self.run("git clone https://github.com/dankamongmen/notcurses.git")
+        tools.get(**self.conan_data["sources"][self.version])
 
     def build(self):
         cmake = CMake(self)
         cmake.configure(source_folder="notcurses")
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/notcurses %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="notcurses")
-        self.copy("*notcurses.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["notcurses"]
-
+        self.cpp_info.libs = ["notcurses", "notcurses++"]
