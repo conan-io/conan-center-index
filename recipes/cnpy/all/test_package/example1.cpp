45041425b05d93e26c29ba4e524a5a1ea4c98923
yuxi.hong <yuxi.hong@kaust.edu.sa>
2020-12-31 16:14:03 -0500

    windows

--- recipes/cnpy/all/test_package/example1.cpp
+++ recipes/cnpy/all/test_package/example1.cpp
@@ -1,20 +1,15 @@
 #include <cstdlib>
 #include<vector>
-#include<complex>
-
 #include"cnpy.h"
 
 int main() {
-    const int Nx = 128;
-    const int Ny = 64;
-    const int Nz = 32;
+    const size_t Nx = 16;
+    const size_t Ny = 16;
+    const size_t Nz = 16;
 
-    std::vector<std::complex<double>> data(Nx*Ny*Nz);
-    for(int i = 0;i < Nx*Ny*Nz;i++) data[i] = std::complex<double>(rand(),rand());
-
+    std::vector<double> data(Nx*Ny*Nz);
+    for(int i = 0;i < Nx*Ny*Nz;i++) data[i] = rand();
     cnpy::npy_save("arr1.npy",&data[0],{Nz,Ny,Nx},"w");
     cnpy::NpyArray arr = cnpy::npy_load("arr1.npy");
-    std::complex<double>* loaded_data = arr.data<std::complex<double>>();
-
     return EXIT_SUCCESS;
 }
