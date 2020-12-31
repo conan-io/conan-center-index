cf2d5a08763bc8f1ff2f53db02dfb07e7ee97695
Florian Berchtold <florian.berchtold@luminartech.com>
2020-12-31 13:02:08 +0100

    catch2/2.13.4 fix for main target

--- recipes/catch2/2.x.x/conanfile.py
+++ recipes/catch2/2.x.x/conanfile.py
@@ -12,13 +12,12 @@ class ConanRecipe(ConanFile):
     license = "BSL-1.0"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    no_copy_source=True
-    
     _cmake = None
-    
+
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
     @property
     def _build_subfolder(self):
         return "build_subfolder"
@@ -37,9 +36,13 @@ class ConanRecipe(ConanFile):
         self._cmake.definitions["CATCH_INSTALL_HELPERS"] = "ON"
         self._cmake.configure(
             source_folder=self._source_subfolder,
-            build_folder=self._build_subfolder
+            build_folder=self._build_subfolder,
         )
         return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
         self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
@@ -48,14 +51,17 @@ class ConanRecipe(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
         tools.rmdir(os.path.join(self.package_folder, "share"))
         for cmake_file in ["ParseAndAddCatchTests.cmake", "Catch.cmake", "CatchAddTests.cmake"]:
-            self.copy(cmake_file,
-                      src=os.path.join(self._source_subfolder, "contrib"),
-                      dst=os.path.join("lib", "cmake", "Catch2"))
-
-    def package_id(self):
-        self.info.header_only()
+            self.copy(
+                cmake_file,
+                src=os.path.join(self._source_subfolder, "contrib"),
+                dst=os.path.join("lib", "cmake", "Catch2"),
+            )
 
     def package_info(self):
-        self.cpp_info.builddirs = [os.path.join("lib", "cmake", "Catch2")]
         self.cpp_info.names["cmake_find_package"] = "Catch2"
         self.cpp_info.names["cmake_find_package_multi"] = "Catch2"
+
+        self.cpp_info.components["Catch2WithMain"].builddirs = [os.path.join("lib", "cmake", "Catch2")]
+        self.cpp_info.components["Catch2WithMain"].libs = ["Catch2WithMain"]
+        self.cpp_info.components["Catch2WithMain"].names["cmake_find_package"] = "Catch2WithMain"
+        self.cpp_info.components["Catch2WithMain"].names["cmake_find_package_multi"] = "Catch2WithMain"
--- recipes/catch2/2.x.x/test_package/conanfile.py
+++ recipes/catch2/2.x.x/test_package/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
@@ -13,5 +13,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+            self.run(os.path.join("bin", "test_package"), run_environment=True)
+            self.run(os.path.join("bin", "standalone"), run_environment=True)
