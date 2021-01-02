a46094d765a0435416d3dd4f83f8600b8134011f
resttime <resttime@users.noreply.github.com>
2021-01-02 00:57:06 -0600

    Fix builds on windows and osx
    My friend helped test this

--- recipes/raylib/all/conanfile.py
+++ recipes/raylib/all/conanfile.py
@@ -64,6 +64,6 @@ class RaylibConan(ConanFile):
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.extend(["m", "pthread", "dl", "rt"])
         elif self.settings.os == "Windows":
-            self.cpp_info.system_libs.append("gdi32")
+            self.cpp_info.system_libs.extend(["gdi32", "winmm"])
         elif self.settings.os == "Macos":
-            self.cpp_info.frameworks.extend(["Cocoa", "IOKit", "CoreFoundation"])
+            self.cpp_info.frameworks.extend(["Cocoa", "IOKit", "CoreVideo", "GLUT", "OpenGL"])
