3b3acc507806b1773562f3e092ea4765c137096a
Croydon <git@cr0ydon.com>
2021-01-03 03:56:18 +0100

    update openssl to 1.1.1i

--- recipes/paho-mqtt-c/all/conanfile.py
+++ recipes/paho-mqtt-c/all/conanfile.py
@@ -44,7 +44,7 @@ class PahoMqttcConan(ConanFile):
 
     def requirements(self):
         if self.options.ssl:
-            self.requires("openssl/1.1.1g")
+            self.requires("openssl/1.1.1i")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
