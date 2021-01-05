b02ddc97db87e1faa5309a6e25276b448e25181f
libbylg <libbylg@126.com>
2021-01-05 22:31:38 +0800

    (#3877) add libproperties for conan
    * add libproperties for conan
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/libproperties/all/conanfile.py
    
    we do not need version, because the parent folder is just the version
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/test_package/CMakeLists.txt
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/test_package/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/test_package/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/test_package/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Update recipes/libproperties/all/test_package/CMakeLists.txt
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Use the release package but not the branch
    
    * add all/test_package/conanfile.py
    
    * use the cmake 1.15 as the basic version
    
    * repair build faild for conan
    
    * make the assert never be optimized
    
    * remove source which noused
    
    * remove source which noused
    
    * missing `import os`
    
    * missing `import os`
    
    * missing `import os`
    
    * recover folder name
    
    * recover folder name
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * use the wrong version
    
    * download package from wrong path
    
    * libproperties: make it work on Linux
    
    * libproperties: don't build test
    
    * the output dir is warong
    
    * libproperties: handle fPIC + LIBPROPERTIES_STATIC + set C_STANDARD
    
    * Update recipes/libproperties/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * missing import os
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    Co-authored-by: Anonymous Maarten <anonymous.maarten@gmail.com>

---
+++ recipes/libproperties/all/test_package/test_package.c
@@ -0,0 +1,31 @@
+#include "properties.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LIBPROPERTIES_TEST_ASSERT(X) do {if(!(X))    {fprintf(stderr, "FAIL: %s\n", #X); abort();}} while(0)
+
+
+int check_handler(void* context, char* key, int key_len, char* val, int val_len)
+{
+    LIBPROPERTIES_TEST_ASSERT(key_len == 3);
+    LIBPROPERTIES_TEST_ASSERT(val_len == 3);
+    LIBPROPERTIES_TEST_ASSERT(strncmp("aaa", key, 3) == 0);
+    LIBPROPERTIES_TEST_ASSERT(strncmp("bbb", val, 3) == 0);
+    return 0;
+}
+
+
+int main(int argc, char* argv[]) 
+{
+    char str[] = "aaa=bbb";
+    struct properties_source_string_t source =
+    {
+        str,
+        str + strlen(str)
+    };
+    properties_parse(&source, properties_source_string_read, NULL, check_handler);
+
+    return 0;        
+}
