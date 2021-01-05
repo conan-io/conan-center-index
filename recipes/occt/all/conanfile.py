07c93b5f553bbba3b4ef90b40f4eddc91cb015b9
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2021-01-05 21:34:32 +0800

    Fix Conan-Center Hook Errors

--- recipes/occt/all/conanfile.py
+++ recipes/occt/all/conanfile.py
@@ -1,14 +1,16 @@
 from conans import ConanFile, CMake, tools
 import os
 
+
 class OcctConan(ConanFile):
     name = "occt"
     version = "7.5.0"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Occt here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    description = """a software development platform providing services for 3D
+        surface and solid modeling, CAD data exchange, and visualization."""
+    homepage = "https://github.com/Open-Cascade-SAS/OCCT"
+    url = "https://github.com/conan-io/conan-center-index"
+    license = "LGPL-2.1-or-later"
+    topics = ("3D", "modeling")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
@@ -25,18 +27,23 @@ class OcctConan(ConanFile):
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name.upper() + "-" + self.version.replace(".", "_")
-        os.rename(extracted_dir, self._source_subfolder)
+        extracted_dir = self.name.upper() + "-" + \
+            self.version.replace(".", "_")
+        tools.rename(extracted_dir, self._source_subfolder)
 
     def _patch_sources(self):
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "project (OCCT)",
-        '''project (OCCT)
-           include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-           conan_basic_setup()''')
-        tools.replace_in_file(os.path.join(self._source_subfolder, "adm/cmake/tk.cmake"),
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "CMakeLists.txt"),
+            "project (OCCT)",
+            '''project (OCCT)
+                include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+                conan_basic_setup()''')
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "adm/cmake/tk.cmake"),
             "${CSF_TclTkLibs}",
             self.deps_cpp_info["tk"].libs[0])
-        tools.replace_in_file(os.path.join(self._source_subfolder, "adm/cmake/tcl.cmake"),
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "adm/cmake/tcl.cmake"),
             "${CSF_TclLibs}",
             self.deps_cpp_info["tcl"].libs[0])
 
@@ -44,19 +51,21 @@ class OcctConan(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
-        # self._cmake.definitions["3RDPARTY_TCL_DIR"] = self.deps_cpp_info["tcl"].rootpath
-        self._cmake.definitions["3RDPARTY_TCL_LIBRARY_DIR"] = self.deps_cpp_info["tcl"].lib_paths[0]
-        self._cmake.definitions["3RDPARTY_TCL_INCLUDE_DIR"] = self.deps_cpp_info["tcl"].include_paths[0]
-        # self._cmake.definitions["3RDPARTY_TK_DIR"] = self.deps_cpp_info["tk"].rootpath
-        self._cmake.definitions["3RDPARTY_TK_LIBRARY_DIR"] = self.deps_cpp_info["tk"].lib_paths[0]
-        self._cmake.definitions["3RDPARTY_TK_INCLUDE_DIR"] = self.deps_cpp_info["tk"].include_paths[0]
+        self._cmake.definitions["3RDPARTY_TCL_LIBRARY_DIR"] = \
+            self.deps_cpp_info["tcl"].lib_paths[0]
+        self._cmake.definitions["3RDPARTY_TCL_INCLUDE_DIR"] = \
+            self.deps_cpp_info["tcl"].include_paths[0]
+        self._cmake.definitions["3RDPARTY_TK_LIBRARY_DIR"] = \
+            self.deps_cpp_info["tk"].lib_paths[0]
+        self._cmake.definitions["3RDPARTY_TK_INCLUDE_DIR"] = \
+            self.deps_cpp_info["tk"].include_paths[0]
         if not self.options.shared:
             self._cmake.definitions["BUILD_LIBRARY_TYPE"] = "Static"
 
         self._cmake.definitions["INSTALL_DIR_BIN"] = "bin"
         self._cmake.definitions["INSTALL_DIR_INCLUDE"] = "include"
         self._cmake.definitions["INSTALL_DIR_LIB"] = "lib"
-        self._cmake.definitions["INSTALL_DIR_RESOURCES"] = "res"
+        self._cmake.definitions["INSTALL_DIR_RESOURCE"] = "res"
 
         self._cmake.configure(source_folder=self._source_subfolder)
         return self._cmake
@@ -69,6 +78,19 @@ class OcctConan(ConanFile):
     def package(self):
         cmake = self._configure_cmake()
         cmake.install()
+        with tools.chdir(self.package_folder):
+            print()
+            tools.mkdir("licenses")
+            tools.rename(
+                "LICENSE_LGPL_21.txt",
+                os.path.join("licenses", "LICENSE_LGPL_21.txt"))
+            tools.rename(
+                "OCCT_LGPL_EXCEPTION.txt",
+                os.path.join("licenses", "OCCT_LGPL_EXCEPTION.txt"))
+            tools.rmdir("cmake")
+            for file in os.listdir():
+                if ".bat" in file:
+                    os.remove(file)
 
     def package_info(self):
         libs: List[str] = []
--- recipes/occt/all/test_package/conanfile.py
+++ recipes/occt/all/test_package/conanfile.py
@@ -14,12 +14,7 @@ class OcctTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-
     def test(self):
         if not tools.cross_building(self):
             os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+            self.run(".%sexample" % os.sep, run_environment=True)
