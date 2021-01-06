7f5eacb41b3e89f59b39261ffb4e61f5200a830d
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2021-01-06 15:35:26 +0800

    Support Windows Shared Build

--- recipes/occt/all/conanfile.py
+++ recipes/occt/all/conanfile.py
@@ -16,13 +16,13 @@ class OcctConan(ConanFile):
     generators = "cmake"
     requires = [("tcl/8.6.10"),
                 ("tk/8.6.10"),
-                ("freetype/2.10.2")]
+                ("freetype/2.10.4")]
     _cmake = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
- 
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -40,25 +40,30 @@ class OcctConan(ConanFile):
             '''project (OCCT)
                 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
                 conan_basic_setup()''')
-        tools.replace_in_file(
-            os.path.join(self._source_subfolder, "adm/cmake/tk.cmake"),
-            "${CSF_TclTkLibs}",
-            self.deps_cpp_info["tk"].libs[0])
+        tcl_libs = self.deps_cpp_info["tcl"].libs
+        tcl_lib = next(filter(lambda lib: "tcl8" in lib, tcl_libs))
         tools.replace_in_file(
             os.path.join(self._source_subfolder, "adm/cmake/tcl.cmake"),
             "${CSF_TclLibs}",
-            self.deps_cpp_info["tcl"].libs[0])
+            tcl_lib)
+
+        tk_libs = self.deps_cpp_info["tk"].libs
+        tk_lib = next(filter(lambda lib: "tk8" in lib, tk_libs))
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "adm/cmake/tk.cmake"),
+            "${CSF_TclTkLibs}",
+            tk_lib)
 
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
         self._cmake.definitions["3RDPARTY_TCL_LIBRARY_DIR"] = \
-            self.deps_cpp_info["tcl"].lib_paths[0]
+            os.path.join(self.deps_cpp_info["tcl"].rootpath, "lib")
         self._cmake.definitions["3RDPARTY_TCL_INCLUDE_DIR"] = \
             self.deps_cpp_info["tcl"].include_paths[0]
         self._cmake.definitions["3RDPARTY_TK_LIBRARY_DIR"] = \
-            self.deps_cpp_info["tk"].lib_paths[0]
+            os.path.join(self.deps_cpp_info["tk"].rootpath, "lib")
         self._cmake.definitions["3RDPARTY_TK_INCLUDE_DIR"] = \
             self.deps_cpp_info["tk"].include_paths[0]
         if not self.options.shared:
@@ -68,11 +73,16 @@ class OcctConan(ConanFile):
         self._cmake.definitions["INSTALL_DIR_INCLUDE"] = "include"
         self._cmake.definitions["INSTALL_DIR_LIB"] = "lib"
         self._cmake.definitions["INSTALL_DIR_RESOURCE"] = "res"
-
+        
         self._cmake.configure(source_folder=self._source_subfolder)
         return self._cmake
 
     def build(self):
+        if self.options.shared:
+            if not self.options["tcl"].shared or \
+                    not self.options["tk"].shared or \
+                    not self.options["freetype"].shared:
+                raise ConanInvalidConfiguration("tcl, tk and freetype must be shared when occt is shared.")
         self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
