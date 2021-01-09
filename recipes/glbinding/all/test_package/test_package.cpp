76b21d380cd11245a95556b2b4f1f767064ecd6d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-01-09 20:01:12 +0100

    simplify test_package

--- recipes/glbinding/all/test_package/test_package.cpp
+++ recipes/glbinding/all/test_package/test_package.cpp
@@ -3,28 +3,9 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-#include <stdio.h>
-
 int main() {
-#if defined(__APPLE__) && defined(__MACH__)
-    glfwInitHint(GLFW_COCOA_MENUBAR, GLFW_FALSE);
-#endif
-    if (!glfwInit()) {
-        printf("glfwInit() failed. Ignoring because this is not testing glfw.\n");
-        return 0;
-    }
-
-    glfwDefaultWindowHints();
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-
-    GLFWwindow *window = glfwCreateWindow(320, 240, "", nullptr, nullptr);
-    if (!window) {
-        glfwTerminate();
-        printf("glfwCreateWindow failed. Ignoring because this is not testing glfw.\n");
-        return 0;
-    }
-
-    glfwMakeContextCurrent(window);
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Window name", monitor, NULL);
 
     glbinding::initialize(glfwGetProcAddress, false);
 
