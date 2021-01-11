6218f139b2a1b987c5a34749c0ee6d67f333f179
Ayaz Salikhov <asalikhov@aimtech.com>
2021-01-11 08:05:13 +0300

    Add more complex example for netssl

--- recipes/poco/all/test_package/test_netssl.cpp
+++ recipes/poco/all/test_package/test_netssl.cpp
@@ -1,23 +1,46 @@
-#include "Poco/Net/Context.h"
-#include "Poco/Net/HTTPSClientSession.h"
-#include "Poco/Net/SSLManager.h"
+#include "Poco/StreamCopier.h"
 #include "Poco/URI.h"
+#include "Poco/Exception.h"
+#include "Poco/SharedPtr.h"
+#include "Poco/Net/SSLManager.h"
+#include "Poco/Net/KeyConsoleHandler.h"
+#include "Poco/Net/AcceptCertificateHandler.h"
+#include "Poco/Net/HTTPSClientSession.h"
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Net/HTTPResponse.h"
+#include <memory>
+#include <iostream>
 
+using namespace Poco;
 using namespace Poco::Net;
-using Poco::Net::Context;
-using Poco::Net::HTTPSClientSession;
-using Poco::URI;
 
 class SSLInitializer {
 public:
-   SSLInitializer() { Poco::Net::initializeSSL(); }
+    SSLInitializer() { Poco::Net::initializeSSL(); }
 
-   ~SSLInitializer() { Poco::Net::uninitializeSSL(); }
+    ~SSLInitializer() { Poco::Net::uninitializeSSL(); }
 };
 
-int main()
-{
-   SSLInitializer sslInitializer;
-   URI uri("https://pocoproject.org/");
-   return 0;
+int main(int argc, char** argv) {
+    SSLInitializer sslInitializer;
+
+    SharedPtr<InvalidCertificateHandler> ptrCert = new AcceptCertificateHandler(false);
+    Context::Ptr ptrContext = new Context(Context::CLIENT_USE, "", "", "", Context::VERIFY_NONE, 9, false, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+    SSLManager::instance().initializeClient(0, ptrCert, ptrContext);
+
+    try {
+        URI uri("https://httpbin.org/user-agent");
+        HTTPSClientSession s(uri.getHost(), uri.getPort());
+        HTTPRequest request(HTTPRequest::HTTP_GET, uri.getPath());
+        request.set("user-agent", "Poco HTTPSClientSession");
+        s.sendRequest(request);
+        HTTPResponse response;
+        std::istream& rs = s.receiveResponse(response);
+        StreamCopier::copyStream(rs, std::cout);
+    } catch (const Exception& ex) {
+        std::cout << ex.displayText() << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
