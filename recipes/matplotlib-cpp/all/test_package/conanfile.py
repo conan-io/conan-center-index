664e104a4b5219453c0a6c16a5cb2e61a461e33a
Florian Berchtold <florian.berchtold@luminartech.com>
2021-01-11 11:43:52 +0100

    possible CCI illegal way to get numpy \.O_O./

--- recipes/matplotlib-cpp/all/test_package/conanfile.py
+++ recipes/matplotlib-cpp/all/test_package/conanfile.py
@@ -11,7 +11,21 @@ class TestPackageConan(ConanFile):
         cmake.configure()
         cmake.build()
 
+    def import_numpy():
+        try:
+            import numpy
+        except ImportError:
+            import pip
+
+            if hasattr(pip, "main"):
+                pip.main(["install", "numpy"])
+            else:
+                pip._internal.main(["install", "numpy"])
+
+            import numpy
+
     def test(self):
+
         if not tools.cross_building(self.settings):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
