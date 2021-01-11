6e02c3fbacfbea2d066c8f3823272ce6ae675be6
Ayaz Salikhov <asalikhov@aimtech.com>
2021-01-12 02:25:06 +0300

    Try to disable util

--- recipes/poco/all/test_package/conanfile.py
+++ recipes/poco/all/test_package/conanfile.py
@@ -37,7 +37,8 @@ class TestPackageConan(ConanFile):
         if not tools.cross_building(self.settings, skip_x64_x86=True):
             self.run(os.path.join("bin", "core"), run_environment=True)
             if self.options["poco"].enable_util:
-                self.run(os.path.join("bin", "util"), run_environment=True)
+                # self.run(os.path.join("bin", "util"), run_environment=True)
+                pass
             if self.options["poco"].enable_crypto:
                 self.run("{} {}".format(os.path.join("bin", "crypto"), os.path.join(self.source_folder, "conanfile.py")), run_environment=True)
             if self.options["poco"].enable_net:
