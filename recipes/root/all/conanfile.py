conan-io/conan-center-index

commit 9181355d75fc92bbf998e10ebaf3244243fa8648

Include conanbuildinfo.cmake in ROOT CMake as this is needed for MSVC.
Following recommendation in comments for #3732 by @madebr:
"This cmake script sets the current C runtime on MSVC (MT vs MD vd MTd vs MDd).
It also sets libstdc++11 vs libstdc++."

recipes/root/all/conanfile.py
@@ -44,7 +44,7 @@ class RootConan(ConanFile):
         # default python=off as there is currently no libpython in Conan center
         "python": PythonOption.OFF,
     }
-    generators = "cmake_find_package"
+    generators = ("cmake", "cmake_find_package")
     requires = (
         "opengl/system",
         "libxml2/2.9.10",
@@ -124,6 +124,11 @@ class RootConan(ConanFile):
             os.sep.join((self._rootsrcdir, "CMakeLists.txt")),
             "project(ROOT)",
             """project(ROOT)
+
+            # cmake script sets the current C runtime on MSVC (MT vs MD vd MTd vs MDd)
+            include(conanbuildinfo.cmake)
+            conan_basic_setup()
+
             find_package(OpenSSL REQUIRED)
             set(OPENSSL_VERSION ${OpenSSL_VERSION})
             find_package(LibXml2 REQUIRED)

commit 8d2a8a2153276e90443b967b3b36e42b0587de3a

Fix failing build causing by changes for MSVC (introduced in 9181355d)

recipes/root/all/conanfile.py
@@ -135,9 +135,9 @@ class RootConan(ConanFile):
             "project(ROOT)",
             """project(ROOT)
 
-            # cmake script sets the current C runtime on MSVC (MT vs MD vd MTd vs MDd)
+            # sets the current C runtime on MSVC (MT vs MD vd MTd vs MDd)
             include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-            conan_basic_setup()
+            conan_set_vs_runtime()
 
             find_package(OpenSSL REQUIRED)
             set(OPENSSL_VERSION ${OpenSSL_VERSION})