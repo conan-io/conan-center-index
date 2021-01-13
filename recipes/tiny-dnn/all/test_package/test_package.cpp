0fbf8e2ab29e7f16a184383c02d7b770894e0786
Uilian Ries <uilianries@gmail.com>
2021-01-13 15:31:29 -0300

    (#4079) Tiny DNN 1.0.0a3
    * Add Tiny DNN 1.0.0a3
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Provide latest version
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add optional requirements
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ recipes/tiny-dnn/all/test_package/test_package.cpp
@@ -0,0 +1,34 @@
+/*
+    Copyright (c) 2013, Taiga Nomi and the respective contributors
+    All rights reserved.
+    Use of this source code is governed by a BSD-style license that can be found
+    in the LICENSE file.
+*/
+
+#include <iostream>
+
+#include "tiny_dnn/tiny_dnn.h"
+
+int main() {
+  tiny_dnn::models::alexnet nn;
+
+  // change all layers at once
+  nn.weight_init(tiny_dnn::weight_init::constant(2.0));
+  nn.bias_init(tiny_dnn::weight_init::constant(2.0));
+  nn.init_weight();
+
+  tiny_dnn::vec_t in(224 * 224 * 3);
+
+  // generate random variables
+  tiny_dnn::uniform_rand(in.begin(), in.end(), 0, 1);
+
+  tiny_dnn::timer t;  // start the timer
+
+  // predict
+  auto res = nn.predict(in);
+
+  double elapsed_s = t.elapsed();
+  t.stop();
+
+  std::cout << "Elapsed time(s): " << elapsed_s << std::endl;
+}
