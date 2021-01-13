7af582af02db152f57d3491c63bab0f42e4e55d5
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-01-13 15:43:10 -0300

    add recipe for Apple libdispatch (aka Grand Central Dispatch)

---
+++ recipes/libdispatch/all/conanfile.py
@@ -0,0 +1,62 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+class LibDispatchConan(ConanFile):
+    name = "libdispatch"
+    homepage = "https://github.com/apple/swift-corelibs-libdispatch"
+    description = "A safe and fast alternative to printf and IOStreams."
+    topics = ("conan", "libdispatch", "apple", "GCD", "concurrency")
+    url = "https://github.com/conan-io/conan-center-index"
+    license = "MIT"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    _cmake = None
+
+    def validate(self):
+        if self.settings.compiler != "clang":
+            raise ConanInvalidConfiguration("Clang compiler required")
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+            if self.settings.compiler == "Visual Studio" and "MT" in self.settings.compiler.runtime:
+                raise ConanInvalidConfiguration("Visual Studio build for shared library with MT runtime is not supported")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = f'swift-corelibs-{self.name}-swift-{self.version}-RELEASE'
+
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
