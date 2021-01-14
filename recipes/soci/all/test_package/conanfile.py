7edf14abd25d9577f4ca61b7b69473b81a23641b
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-01-14 18:07:22 -0500

    remove extra options

--- recipes/soci/all/test_package/conanfile.py
+++ recipes/soci/all/test_package/conanfile.py
@@ -5,15 +5,9 @@ class SociTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
-    def configure(self):
-        self.options["soci"].shared     = True
-        self.options["soci"].empty      = True
-
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["SOCI_SHARED"] = self.options["soci"].shared
         cmake.configure()
-        cmake.verbose = True
         cmake.build()
 
     def test(self):
