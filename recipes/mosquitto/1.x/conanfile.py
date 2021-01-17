3f1dfa2419b23b0c2c2639c5278ed50fb4b3c435
Chris McArthur <prince.chrismc@gmail.com>
2021-01-17 15:57:37 -0500

    fix fPIC + more tls + websocket on

--- recipes/mosquitto/1.x/conanfile.py
+++ recipes/mosquitto/1.x/conanfile.py
@@ -8,7 +8,7 @@ class MosquittoConan(ConanFile):
     name = "mosquitto"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/eclipse/mosquitto"
-    topics = ("mqtt", "broker", "libwebsockets", "mosquitto", "eclipse-iot")
+    topics = ("mqtt", "broker", "libwebsockets", "mosquitto", "eclipse", "iot")
     license = "EPL-1.0", "BSD-3-Clause" # https://lists.spdx.org/g/Spdx-legal/message/2701
     description = "Eclipse Mosquitto - An open source MQTT broker"
     exports_sources = "CMakeLists.txt"
@@ -21,7 +21,7 @@ class MosquittoConan(ConanFile):
     default_options = {"shared": False,
                        "fPIC": True,
                        "with_tls": True,
-                       "with_websockets": False}
+                       "with_websockets": True}
     _cmake = None
 
     @property
@@ -54,18 +54,17 @@ class MosquittoConan(ConanFile):
     def _configure_cmake(self):
         if not self._cmake:
             self._cmake = CMake(self)
+            self._cmake.definitions["WITH_PIC"] = self.options.get_safe("fPIC", True)
             self._cmake.definitions["WITH_STATIC_LIBRARIES"] = not self.options.shared
             self._cmake.definitions["WITH_TLS"] = self.options.with_tls
+            self._cmake.definitions["WITH_TLS_PSK"] = self.options.with_tls
+            self._cmake.definitions["WITH_EC"] = self.options.with_tls
             self._cmake.definitions["DOCUMENTATION"] = False
             self._cmake.definitions["WITH_THREADING"] = self.settings.compiler != "Visual Studio"
             self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
-    def _patch(self):
-        if not self.options.shared and not self.options.get_safe("fPIC"):
-            tools.replace_in_file(os.path.join(self._source_subfolder, "lib", "CMakeLists.txt"),
-                                  "POSITION_INDEPENDENT_CODE 1",
-                                  "POSITION_INDEPENDENT_CODE 0")
+    def _patch_sources(self):
         if self.settings.os == "Windows":
             if self.options.with_tls:
                 tools.replace_in_file(os.path.join(self._source_subfolder, "lib", "CMakeLists.txt"),
@@ -85,7 +84,7 @@ class MosquittoConan(ConanFile):
                                 "install(TARGETS mosquittopp RUNTIME DESTINATION \"${CMAKE_INSTALL_BINDIR}\" LIBRARY DESTINATION \"${CMAKE_INSTALL_LIBDIR}\" ARCHIVE DESTINATION \"${CMAKE_INSTALL_LIBDIR}\")")
 
     def build(self):
-        self._patch()
+        self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -110,7 +109,7 @@ class MosquittoConan(ConanFile):
         self.cpp_info.components["libmosquitto"].names["pkgconfig"] = "libmosquitto"
         if self.options.with_tls:
             self.cpp_info.components["libmosquitto"].requires.append("openssl::openssl")
-            self.cpp_info.components["libmosquitto"].defines.append("WITH_TLS")
+            self.cpp_info.components["libmosquitto"].defines.extend(["WITH_TLS", "WITH_TLS_PSK", "WITH_EC"])
         if self.options.with_websockets:
             self.cpp_info.components["libmosquitto"].requires.append("libwebsockets::libwebsockets")
             self.cpp_info.components["libmosquitto"].defines.append("WITH_WEBSOCKETS")
