27245e1c54eb0dbb4138dc767b788b57e689a0e2
bobrofon <bobrofon@gmail.com>
2021-01-17 11:49:29 +0300

    Move compiller check to validate phase
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/sdbus-cpp/all/conanfile.py
+++ recipes/sdbus-cpp/all/conanfile.py
@@ -52,6 +52,7 @@ class SdbusCppConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
+    def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
@@ -62,8 +63,6 @@ class SdbusCppConan(ConanFile):
             if tools.Version(self.settings.compiler.version) < min_version:
                 raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
                     self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
-
-    def validate(self):
         if self.settings.os != "Linux":
             raise ConanInvalidConfiguration("Only Linux supported")
 
