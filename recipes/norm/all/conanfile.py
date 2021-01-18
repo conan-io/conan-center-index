3064cf7ae5370e19abd4a0db21b0b2a3e1dc788d
Mark Jan van Kampen <mjvk@allseas.com>
2021-01-18 21:25:04 +0100

    Fixes for latest norm version

--- recipes/norm/all/conanfile.py
+++ recipes/norm/all/conanfile.py
@@ -9,42 +9,50 @@ class NormConan(ConanFile):
     topics = ("conan", "norm", "multicast", "transport protocol")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.nrl.navy.mil/itd/ncs/products/norm"
-    exports_sources = ["LICENSE"]
-    generators = "cmake", "cmake_find_package"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
     _cmake = None
-    license = "MIT"
+    license = "NRL"
     no_copy_source = True
+    settings = "os", "compiler", "build_type", "arch"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-        
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         url = self.conan_data["sources"][self.version]["url"]
-        extracted_dir = self.name + "-" + os.path.splitext(os.path.basename(url))[0]
+        extracted_dir = self.name + "-" + \
+            os.path.splitext(os.path.basename(url))[0]
         os.rename(extracted_dir, self._source_subfolder)
-        
+
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
-        self._cmake.configure(build_folder=self._build_subfolder, source_folder=self._source_subfolder)
+        self._cmake.definitions["NORM_CUSTOM_PROTOLIB_VERSION"] = self.conan_data["protolib"][self.version]["githash"]
+        self._cmake.configure()
         return self._cmake
- 
+
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
-        
+
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "RapidJSON"
-        self.cpp_info.names["cmake_find_package_multi"] = "RapidJSON"
+        self.cpp_info.names["cmake_find_package"] = "norm"
+        self.cpp_info.names["cmake_find_package_multi"] = "norm"
+        self.cpp_info.libs = tools.collect_libs(self)
+        if(self.settings.os == "Windows"):
+            self.cpp_info.system_libs = [
+                "ws2_32", "iphlpapi", "user32", "gdi32", "Advapi32", "ntdll"]
+        else:
+            self.cpp_info.system_libs = ["pthread"]
+        if(self.settings.os == "Linux"):
+            self.cpp_info.system_libs.append(["dl rt"])
--- recipes/norm/all/test_package/test_package.cpp
+++ recipes/norm/all/test_package/test_package.cpp
@@ -21,5 +21,13 @@
 // Test to validate Conan package generated
 
 int main(int /*argc*/, const char * /*argv*/ []) {
-  return EXIT_SUCCESS;
+	NormInstanceHandle handle = NormCreateInstance();
+	if(handle == NORM_INSTANCE_INVALID)
+	{
+		std::cout << "Failed to create handle" << std::endl;
+		return EXIT_FAILURE;
+	}
+	std::cout << "Succesfully created handle" << std::endl;
+	NormDestroyInstance(handle);
+	return EXIT_SUCCESS;
 }
