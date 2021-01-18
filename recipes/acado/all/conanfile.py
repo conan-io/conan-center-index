15d8c59f28ee87eebb0152ad0cf15dc55decd6d2
Florian Berchtold <florian.berchtold@luminartech.com>
2021-01-18 16:34:06 +0100

    acado/1.2.2-beta relocatable

--- recipes/acado/all/conanfile.py
+++ recipes/acado/all/conanfile.py
@@ -93,6 +93,8 @@ class AcadoConan(ConanFile):
         tools.remove_files_by_mask(self.package_folder, "*.pdb")
 
     def package_info(self):
+        self.env_info.ACADO_TEMPLATE_PATHS = os.path.join(self.package_folder, "include", "acado", "code_generation", "templates")
+
         if self.options.shared:
             self.cpp_info.libs = ["acado_toolkit_s", "acado_casadi"]
         else:
