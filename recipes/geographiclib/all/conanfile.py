ac6a6835c1930c4682d4f942ec9ad2377ad2a053
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-01-18 17:49:34 +0100

    add precision option
    partial support (float and double only)
    extended, quadruple and variable values require more love

--- recipes/geographiclib/all/conanfile.py
+++ recipes/geographiclib/all/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.29.1"
@@ -16,11 +17,13 @@ class GeographiclibConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "precision": ["float", "double", "extended", "quadruple", "variable"],
         "tools": [True, False]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
+        "precision": "double",
         "tools": True
     }
 
@@ -43,6 +46,10 @@ class GeographiclibConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+        if self.options.precision not in ["float", "double"]:
+            # FIXME: add support for extended, quadruple and variable precisions
+            # (may require external libs: boost multiprecision for quadruple, mpfr for variable)
+            raise ConanInvalidConfiguration("extended, quadruple and variable precisions not yet supported in this recipe")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -65,8 +72,19 @@ class GeographiclibConan(ConanFile):
         if not self._cmake:
             self._cmake = CMake(self)
             self._cmake.definitions["GEOGRAPHICLIB_LIB_TYPE"] = "SHARED" if self.options.shared else "STATIC"
+            self._cmake.definitions["GEOGRAPHICLIB_PRECISION"] = self._cmake_option_precision
             self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
+
+    @property
+    def _cmake_option_precision(self):
+        return {
+            "float": 1,
+            "double": 2,
+            "extended": 3,
+            "quadruple": 4,
+            "variable": 5,
+        }.get(str(self.options.precision))
 
     def build(self):
         self._patch_sources()
