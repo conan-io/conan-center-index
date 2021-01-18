b48875d0b3048abe269ca1b6d473297062979340
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-01-18 09:58:57 -0500

    comment soci::session

--- recipes/soci/all/test_package/test_package.cpp
+++ recipes/soci/all/test_package/test_package.cpp
@@ -9,8 +9,8 @@ int main()
   const auto& connectString{"../database0.empty.db"};
   const auto& table{"table1"};
 #ifdef SOCI_EMPTY
-  const soci::backend_factory& backEnd = *soci::factory_empty();
-  soci::session sql(backEnd, connectString);
+//  const soci::backend_factory& backEnd = *soci::factory_empty();
+//  soci::session sql(backEnd, connectString);
   std::cout << "soci empty backend\n";
 #endif
   std::cout << "soci database connected successfully\n";
