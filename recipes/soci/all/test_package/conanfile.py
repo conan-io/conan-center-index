9efc542edc3f5a1bb4f8a6879f8b20881ad75840
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-01-19 11:18:01 -0500

    rename class to default name to run test under bin

--- recipes/soci/all/test_package/conanfile.py
+++ recipes/soci/all/test_package/conanfile.py
@@ -1,7 +1,8 @@
 import os
 from conans import ConanFile, CMake, tools
 
-class SociTestConan(ConanFile):
+
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
@@ -11,5 +12,6 @@ class SociTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            self.run(".%stest_package" % os.sep, run_environment=True)
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
