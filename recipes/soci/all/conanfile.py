365e4208468f4c8b13ce0c8cab2c633632bf344f
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-01-18 20:01:56 -0500

    add requires boost, and use @prince-chrismc compiler validations

--- recipes/soci/all/conanfile.py
+++ recipes/soci/all/conanfile.py
@@ -49,16 +49,29 @@ class SociConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
-    def _validate_compiler_settings(self):
-        tools.check_min_cppstd(self, "11")
-        if self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "5.0":
-            raise ConanInvalidConfiguration("gcc minimum required version is 5.0")
-        elif self.settings.compiler == "clang" and tools.Version(self.settings.compiler.version) < "3.8":
-            raise ConanInvalidConfiguration("clang minimum required version is 3.8")
-        elif self.settings.compiler == "apple-clang" and tools.Version(self.settings.compiler.version) < "10.0":
-            raise ConanInvalidConfiguration("apple-clang minimum required version is 10.0")
-        elif self.settings.compiler == "Visual Studio" and tools.Version(self.settings.compiler.version) < "19":
-            raise ConanInvalidConfiguration("Visual Studio minimum required version is 19")
+    @property
+    def _minimum_cpp_standard(self):
+        return 11
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "19",
+            "gcc": "5.0",
+            "clang": "3.8",
+            "apple-clang": "10.0",
+        }
+
+    # def _validate_compiler_settings(self):
+    #     tools.check_min_cppstd(self, "11")
+    #     if self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "5.0":
+    #         raise ConanInvalidConfiguration("gcc minimum required version is 5.0")
+    #     elif self.settings.compiler == "clang" and tools.Version(self.settings.compiler.version) < "3.8":
+    #         raise ConanInvalidConfiguration("clang minimum required version is 3.8")
+    #     elif self.settings.compiler == "apple-clang" and tools.Version(self.settings.compiler.version) < "10.0":
+    #         raise ConanInvalidConfiguration("apple-clang minimum required version is 10.0")
+    #     elif self.settings.compiler == "Visual Studio" and tools.Version(self.settings.compiler.version) < "19":
+    #         raise ConanInvalidConfiguration("Visual Studio minimum required version is 19")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -68,7 +81,17 @@ class SociConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
-        self._validate_compiler_settings()
+        # self._validate_compiler_settings()
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+            min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+            if not min_version:
+                self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                    self.name, self.settings.compiler))
+            else:
+                if tools.Version(self.settings.compiler.version) < min_version:
+                    raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                        self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
 
     def requirements(self):
         prefix  = "Dependencies for "
@@ -91,6 +114,8 @@ class SociConan(ConanFile):
             self.requires("libmysqlclient/8.0.17")
         if self.options.with_postgresql:
             self.requires("libpq/11.5")
+        if self.options.with_boost:
+            self.requires("boost/1.73.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -108,15 +133,15 @@ class SociConan(ConanFile):
 
         if self.options.shared:
             self._cmake.definitions["SOCI_STATIC"] = False
-        else:
-            self.options.with_sqlite3       = False
-            self.options.with_db2           = False
-            self.options.with_odbc          = False
-            self.options.with_oracle        = False
-            self.options.with_firebird      = False
-            self.options.with_mysql         = False
-            self.options.with_postgresql    = False
-            self.options.with_boost         = False
+        # else:
+        #     self.options.with_sqlite3       = False
+        #     self.options.with_db2           = False
+        #     self.options.with_odbc          = False
+        #     self.options.with_oracle        = False
+        #     self.options.with_firebird      = False
+        #     self.options.with_mysql         = False
+        #     self.options.with_postgresql    = False
+        #     self.options.with_boost         = False
 
         self._cmake.definitions["SOCI_EMPTY"]       = self.options.empty
         self._cmake.definitions["WITH_SQLITE3"]     = self.options.with_sqlite3
