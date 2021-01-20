672fb29a4a2af2f5794452b9329ab84c0071a3ea
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-01-20 15:59:37 +0100

    test on Linux x86_64

--- recipes/jwasm/all/test_package/conanfile.py
+++ recipes/jwasm/all/test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
 import os
 
 
@@ -8,3 +8,10 @@ class TestPackageConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             self.run("jwasm -h", run_environment=True, ignore_errors=True)
+            if self.settings.os == "Linux" and self.settings.arch == "x86_64":
+                asm_file = os.path.join(self.source_folder, "Lin64_1.asm") # content from https://www.japheth.de/JWasm/Lin64_1.html
+                obj_file = os.path.join(self.build_folder, "Lin64_1.o")
+                bin_file = os.path.join(self.build_folder, "Lin64_1")
+                self.run("jwasm -elf64 -Fo={obj} {asm}".format(asm=asm_file, obj=obj_file), run_environment=True)
+                self.run("ld {obj} -o {bin}".format(obj=obj_file, bin=bin_file))
+                self.run(bin_file)
