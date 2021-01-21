8d275d288a58b825c3a18d559353af50eedee5f2
Uilian Ries <uilianries@gmail.com>
2021-01-21 12:35:08 -0300

    Mingw is a generic installer
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/mingw/all/conanfile.py
+++ recipes/mingw/all/conanfile.py
@@ -10,24 +10,19 @@ class MingwConan(ConanFile):
     homepage = "https://www.mingw.org"
     license = "ZPL-2.1", "MIT", "GPL-2.0-or-later"
     topics = ("gcc", "gnu", "unix", "mingw32", "binutils")
-    settings = "os", "arch", "compiler"
+    settings = "os", "arch"
+    options = {"threads": ["posix", "win32"], "exception": ["seh", "sjlj"]}
+    default_options = {"threads": "posix", "exception": "seh"}
     build_requires = "7zip/19.00"
+    no_copy_source = True
 
     def validate(self):
         if self.settings.os != "Windows":
             raise ConanInvalidConfiguration("MinGW is only supported by Windows.")
-        if self.settings.compiler != "gcc":
-            raise ConanInvalidConfiguration("MinGW is only supports gcc.")
 
-    def _get_best_installer(self):
-        host_arch = str(self.settings.arch)
-        compiler_version = str(self.settings.compiler.version)
-        compiler_exception = str(self.settings.compiler.exception)
-        compiler_threads = str(self.settings.compiler.threads)
-        return self.conan_data["sources"][self.version]["url"][compiler_version][host_arch][compiler_threads][compiler_exception]
-
-    def build(self):
-        url = self._get_best_installer()
+    def source(self):
+        url = self.conan_data["sources"][self.version]["url"][str(self.settings.arch)] \
+                             [str(self.options.threads)][str(self.options.exception)]
         self.output.info("Downloading: %s" % url["url"])
         tools.download(url["url"], "file.7z", sha256=url["sha256"])
         self.run("7z x file.7z")
@@ -37,6 +32,7 @@ class MingwConan(ConanFile):
         target = "mingw64" if self.settings.arch == "x86_64" else "mingw32"
         self.copy("*", dst="", src=target)
         tools.rmdir(target)
+        tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         bin_path = os.path.join(self.package_folder, "bin")
