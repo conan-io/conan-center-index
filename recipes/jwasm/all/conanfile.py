cc77eb80ed34fd12d2d70d943d6f17981dc01fdf
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-01-24 05:30:45 +0100

    (#4321) add jwasm/2.13
    * add jwasm/2.13
    
    * fix for case sensitive os
    
    * cleanup import
    
    * ignore return code
    
    * test on Linux x86_64
    
    * ignore error code in test bin file
    
    * launch jwasm on all os/arch
    
    * jwasm: patch fatal errors on MSVC
    
    * ignore code return again
    
    * remove ignore code return
    
    * link to PR in patch
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    Co-authored-by: Anonymous Maarten <anonymous.maarten@gmail.com>
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

---
+++ recipes/jwasm/all/conanfile.py
@@ -0,0 +1,54 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class JwasmConan(ConanFile):
+    name = "jwasm"
+    description = "JWasm is intended to be a free Masm-compatible assembler."
+    license = "Watcom-1.0"
+    topics = ("conan", "jwasm", "masm", "assembler")
+    homepage = "https://github.com/JWasm/JWasm"
+    url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "os", "arch", "compiler", "build_type"
+
+    exports_sources = "CMakeLists.txt", "patches/*"
+    generators = "cmake"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
+    def package_id(self):
+        del self.info.settings.compiler
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("JWasm-" + self.version, self._source_subfolder)
+
+    def _patch_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+                              'string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")',
+                              "")
+
+    def build(self):
+        self._patch_sources()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy("License.txt", dst="licenses", src=self._source_subfolder)
+        suffix = ".exe" if self.settings.os == "Windows" else ""
+        self.copy("jwasm" + suffix, dst="bin", src="bin")
+
+    def package_info(self):
+        bin_path = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(bin_path))
+        self.env_info.PATH.append(bin_path)
