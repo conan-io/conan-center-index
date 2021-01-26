657c9bec0741b13232fd05379063123290714f6f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-01-26 16:20:06 +0000

    Fix PC-FILES (KB-H020) by deleting lib/pkgconfig directory

--- recipes/cpputest/all/conanfile.py
+++ recipes/cpputest/all/conanfile.py
@@ -66,6 +66,7 @@ class CppUTestConan(ConanFile):
         self.copy("COPYING", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "CppUTest"
