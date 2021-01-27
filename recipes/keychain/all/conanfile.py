cdc4bf9ea3ab7dad43528515b8fcd9e5b4ce4669
Hannes Rantzsch <hannes.rantzsch@nexenio.com>
2021-01-27 09:28:07 +0100

    force the test_package to link to keychain

--- recipes/keychain/all/conanfile.py
+++ recipes/keychain/all/conanfile.py
@@ -44,7 +44,7 @@ class KeychainConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs        = tools.collect_libs(self)
+        self.cpp_info.libs = tools.collect_libs(self)
 
         if self.settings.os == 'Macos':
             self.cpp_info.frameworks = ['Security', 'CoreFoundation']
