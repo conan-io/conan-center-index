4660fef7d998267c99a8c6dc731bbd6a4ff80950
Martin Delille <martin@delille.org>
2021-01-27 20:50:12 +0100

    wip

---
+++ recipes/aws-sdk-cpp/all/conanfile.py
@@ -0,0 +1,59 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+class AwsSdkCppConan(ConanFile):
+    name = "aws-sdk-cpp"
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/aws/aws-sdk-cpp"
+    description = "AWS SDK for C++"
+    topics = ("aws", "cpp", "crossplateform", "amazon", "cloud")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+            "shared": [True, False],
+            "fPIC": [True, False]
+    }
+    default_options = {
+            "shared": False,
+            "fPIC": True
+    }
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("{}-{}".format(self.name, self.version), self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_TESTING"] = False
+        self._cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["aws-cpp-sdk-core"]
+
