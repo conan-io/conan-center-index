2b56c424e87614c1bcf7306a7d00f1ebbc6195c4
Martin Delille <martin@delille.org>
2021-01-30 01:36:57 +0100

    Fix local recipe on MacOS 10.15

--- recipes/aws-sdk-cpp/all/conanfile.py
+++ recipes/aws-sdk-cpp/all/conanfile.py
@@ -228,6 +228,9 @@ class AwsSdkCppConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
 
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+
     def package_info(self):
         libs = list([])
 
