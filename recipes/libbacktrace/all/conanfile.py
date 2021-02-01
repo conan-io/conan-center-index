aba534021dd64d7a46b3d569f08b419d52a3b92e
Alexey Klimkin <klimkin@gmail.com>
2021-02-01 15:50:33 -0800

    Better message for ConanInvalidConfiguration

--- recipes/libbacktrace/all/conanfile.py
+++ recipes/libbacktrace/all/conanfile.py
@@ -27,8 +27,8 @@ class LibbacktraceConan(ConanFile):
     def configure(self):
         if self.settings.compiler == "Visual Studio":
             raise ConanInvalidConfiguration(
-                "libbacktrace doesn't support compiler: {} on OS: {}.".format(
-                    self.settings.compiler, self.settings.os))
+                "libsafec doesn't support {}/{}".format(
+                    self.settings.compiler, self.settings.compiler.version))
         if self.options.shared:
             del self.options.fPIC
         del self.settings.compiler.libcxx
