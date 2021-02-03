e3cadf1897e4652df7be36def758633cc1720b31
Chris McArthur <prince.chrismc@gmail.com>
2021-02-02 21:32:17 -0500

    fix windows build

--- recipes/libavrocpp/all/conanfile.py
+++ recipes/libavrocpp/all/conanfile.py
@@ -2,7 +2,6 @@ import os
 
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
-from conans.tools import Version
 
 
 class LibavrocppConan(ConanFile):
@@ -12,15 +11,13 @@ class LibavrocppConan(ConanFile):
     description = "Avro is a data serialization system."
     homepage = "https://avro.apache.org/"
     topics = ("serialization", "deserialization")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
-        "with_snappy": [True, False],
-    }
     exports_sources = ["patches/*.patch"]
-    default_options = {"shared": False, "fPIC": True, "with_snappy": True}
     generators = "cmake", "cmake_find_package"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    _cmake = None
 
     @property
     def _source_subfolder(self):
@@ -30,22 +27,9 @@ class LibavrocppConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
-    _cmake = None
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-
-    def _configure_cmake(self):
-
-        if self._cmake:
-            return self._cmake
-
-        self._cmake = CMake(self)
-        if not self.options.shared:
-            self._cmake.definitions["AVRO_DYN_LINK"] = ""
-        self._cmake.configure(source_folder=self._source_subfolder)
-        return self._cmake
 
     def configure(self):
         if self.options.shared:
@@ -55,8 +39,7 @@ class LibavrocppConan(ConanFile):
 
     def requirements(self):
         self.requires("boost/1.75.0")
-        if self.options.with_snappy:
-            self.requires("snappy/1.1.8")
+        self.requires("snappy/1.1.8")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -70,14 +53,20 @@ class LibavrocppConan(ConanFile):
             "project (Avro-cpp)",
             """project (Avro-cpp)
                include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-               conan_basic_setup()""",
+               conan_basic_setup()"""
         )
-        if self.options.with_snappy:
-            tools.replace_in_file(
-                os.path.join(self._source_subfolder, "CMakeLists.txt"),
-                "find_package(Snappy)",
-                "",
-            )
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, "CMakeLists.txt"),
+            "${SNAPPY_LIBRARIES}", "${Snappy_LIBRARIES}"
+        )
+
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            # self._cmake.verbose = True
+            self._cmake.definitions["SNAPPY_ROOT_DIR"] = self.deps_cpp_info["snappy"].rootpath.replace("\\", "/")
+            self._cmake.configure(source_folder=self._source_subfolder)
+        return self._cmake
 
     def build(self):
         self._patch_sources()
@@ -94,6 +83,7 @@ class LibavrocppConan(ConanFile):
         # FIXME: avro does not install under a CMake namespace https://github.com/apache/avro/blob/351f589913b9691322966fb77fe72269a0a2ec82/lang/c%2B%2B/CMakeLists.txt#L193
         target = "avrocpp" if self.options.shared else "avrocpp_s"
         self.cpp_info.components[target].libs = [target]
-        self.cpp_info.components[target].requires = ["boost::boost"]
-        if self.options.with_snappy:
-            self.cpp_info.components[target].requires.append("snappy::snappy")
+        self.cpp_info.components[target].requires = ["boost::boost", "snappy::snappy"]
+        # self.cpp_info.components[target].defines.append("AVRO_SOURCE")
+        if self.options.shared:
+            self.cpp_info.components[target].defines.append("AVRO_DYN_LINK")
--- recipes/libavrocpp/all/test_package/test_package.cpp
+++ recipes/libavrocpp/all/test_package/test_package.cpp
@@ -2,24 +2,43 @@
 #include "avro/Encoder.hh"
 #include "avro/Specific.hh"
 
+namespace c {
+struct cpx {
+    double re;
+    double im;
+};
+
+}
+namespace avro {
+template<> struct codec_traits<c::cpx> {
+    static void encode(Encoder& e, const c::cpx& v) {
+        avro::encode(e, v.re);
+        avro::encode(e, v.im);
+    }
+    static void decode(Decoder& d, c::cpx& v) {
+        avro::decode(d, v.re);
+        avro::decode(d, v.im);
+    }
+};
+
+}
+
 int main()
 {
-    int64_t dummy = 1234;
-    std::unique_ptr<avro::OutputStream> out = avro::memoryOutputStream();
+    std::unique_ptr<avro::OutputStream> out = avro::memoryOutputStream(128);
     avro::EncoderPtr e = avro::binaryEncoder();
     e->init(*out);
-    avro::encode(*e, dummy);
+    c::cpx c1;
+    c1.re = 1.0;
+    c1.im = 2.13;
+    avro::encode(*e, c1);
 
     std::unique_ptr<avro::InputStream> in = avro::memoryInputStream(*out);
     avro::DecoderPtr d = avro::binaryDecoder();
     d->init(*in);
 
-    int64_t decoded;
-    avro::decode(*d, decoded);
-
-    if (dummy != decoded)
-    {
-        return 1;
-    }
+    c::cpx c2{};
+    avro::decode(*d, c2);
+    std::cout << '(' << c2.re << ", " << c2.im << ')' << std::endl;
     return 0;
 }
