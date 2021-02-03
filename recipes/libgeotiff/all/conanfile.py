2c1a1d3193b65a40f5702fc9d053a6e9941869e4
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-03 19:11:49 +0100

    delete fPIC option if shared

--- recipes/libgeotiff/all/conanfile.py
+++ recipes/libgeotiff/all/conanfile.py
@@ -31,6 +31,8 @@ class LibgeotiffConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.cppstd
         del self.settings.compiler.libcxx
 
