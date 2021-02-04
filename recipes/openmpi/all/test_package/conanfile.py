a2a16441311367feda514af96e372243366a2e6c
SSE4 <tomskside@gmail.com>
2021-02-04 09:04:38 -0800

    Update recipes/openmpi/all/test_package/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/openmpi/all/test_package/conanfile.py
+++ recipes/openmpi/all/test_package/conanfile.py
@@ -12,6 +12,7 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        mpiexec = os.path.join(os.environ['MPI_BIN'], 'mpiexec')
-        command = '%s -mca plm_rsh_agent yes -np 2 %s' % (mpiexec, os.path.join("bin", "test_package"))
-        self.run(command, run_environment=True)
+        if not tools.cross_building(self.settings):
+            mpiexec = os.path.join(os.environ['MPI_BIN'], 'mpiexec')
+            command = '%s -mca plm_rsh_agent yes -np 2 %s' % (mpiexec, os.path.join("bin", "test_package"))
+            self.run(command, run_environment=True)
