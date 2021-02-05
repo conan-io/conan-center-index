6f8648d0e79c6e6ae6abc3a74103ed1ff7d50039
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-05 17:42:54 +0100

    decrease build path length

--- recipes/opencv/4.x/conanfile.py
+++ recipes/opencv/4.x/conanfile.py
@@ -48,10 +48,6 @@ class OpenCVConan(ConanFile):
         return "source_subfolder"
 
     @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
-    @property
     def _contrib_folder(self):
         return "contrib"
 
@@ -258,7 +254,7 @@ class OpenCVConan(ConanFile):
             if "ANDROID_NDK_HOME" in os.environ:
                 self._cmake.definitions["ANDROID_NDK"] = os.environ.get("ANDROID_NDK_HOME")
 
-        self._cmake.configure(build_folder=self._build_subfolder)
+        self._cmake.configure()
         return self._cmake
 
     def build(self):
