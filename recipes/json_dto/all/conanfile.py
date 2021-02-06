940108731b04871ecc0825565a7654a8c294e842
ngrodzitski <utromvecherom@gmail.com>
2021-02-06 15:14:26 +0300

    Add initial json_dto package recipe
    Add a json_dto library of v.0.2.11 to conan-index (https://github.com/Stiffstream/json_dto).

---
+++ recipes/json_dto/all/conanfile.py
@@ -0,0 +1,61 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+class JsondtoConan(ConanFile):
+    name = "json_dto"
+    license = "BSD-3-Clause"
+    homepage = "https://github.com/Stiffstream/json_dto"
+    url = "https://github.com/conan-io/conan-center-index"
+    description = "A small header-only helper for converting data between json representation and c++ structs"
+    topics = ("JSON", "DTO", "Serialization")
+    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+    no_copy_source = True
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def requirements(self):
+        self.requires("rapidjson/1.1.0")
+
+    def configure(self):
+        minimal_cpp_standard = "14"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+
+        self._cmake = CMake(self)
+        self._cmake.definitions["JSON_DTO_INSTALL"] = True
+        self._cmake.definitions["JSON_DTO_FIND_DEPS"] = False
+        self._cmake.configure(source_folder=self._source_subfolder+"/dev/json_dto")
+        return self._cmake
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-v." + self.version
+        os.rename(extracted_dir, self._source_subfolder )
+
+    def package(self):
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        cmake = self._configure_cmake()
+        cmake.install()
+
+        tools.rmdir(os.path.join(self.package_folder, "lib"))
+
+    def package_id(self):
+        self.info.settings.clear()
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "json_dto"
+        self.cpp_info.names["cmake_find_package_multi"] = "json_dto"
