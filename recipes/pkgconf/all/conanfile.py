50f52b0b8b6aa7ce3db5a77fb7b6ca08fbc37e0a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-10 22:31:37 +0100

    (#4519) pkgconf: download archive from official url + bump meson
    * update sha256 of 1.7.3
    
    * download source from recommended url
    
    * bump meson

--- recipes/pkgconf/all/conanfile.py
+++ recipes/pkgconf/all/conanfile.py
@@ -43,10 +43,10 @@ class PkgConfConan(ConanFile):
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename("pkgconf-pkgconf-{}".format(self.version), self._source_subfolder)
+        os.rename("pkgconf-{}".format(self.version), self._source_subfolder)
 
     def build_requirements(self):
-        self.build_requires("meson/0.55.1")
+        self.build_requires("meson/0.56.2")
 
     @property
     def _sharedstatedir(self):
