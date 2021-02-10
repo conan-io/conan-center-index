4256a340e2a5b9582080ce86ea6a93d6e8e2bedd
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-10 09:53:34 +0100

    download source from recommended url

--- recipes/pkgconf/all/conanfile.py
+++ recipes/pkgconf/all/conanfile.py
@@ -43,7 +43,7 @@ class PkgConfConan(ConanFile):
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename("pkgconf-pkgconf-{}".format(self.version), self._source_subfolder)
+        os.rename("pkgconf-{}".format(self.version), self._source_subfolder)
 
     def build_requirements(self):
         self.build_requires("meson/0.55.1")
