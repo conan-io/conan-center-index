f63fd411d4d3aa99a1e8a910bd604a9f1abb99a3
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-11 17:30:50 +0100

    add share folder in bin

--- recipes/gettext/all/conanfile.py
+++ recipes/gettext/all/conanfile.py
@@ -110,17 +110,18 @@ class GetTextConan(ConanFile):
         with self._build_context():
             env_build = self._configure_autotools()
             env_build.install()
-        os.rename(os.path.join(self.package_folder, "share", "aclocal"),
-                  os.path.join(self.package_folder, "bin", "aclocal"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        tools.rmdir(os.path.join(self.package_folder, "lib"))
         tools.rmdir(os.path.join(self.package_folder, "include"))
+        tools.rmdir(os.path.join(self.package_folder, "lib"))
+        tools.mkdir(os.path.join(self.package_folder, "bin", "share"))
+        os.rename(os.path.join(self.package_folder, "share", "aclocal"),
+                  os.path.join(self.package_folder, "bin", "share", "aclocal"))
+        tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         bindir = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH environment variable: {}".format(bindir))
         self.env_info.PATH.append(bindir)
 
-        gettext_aclocal = tools.unix_path(os.path.join(self.package_folder, "bin", "aclocal"))
+        gettext_aclocal = tools.unix_path(os.path.join(self.package_folder, "bin", "share", "aclocal"))
         self.output.info("Appending ACLOCAL_PATH environment variable: {}".format(gettext_aclocal))
         self.env_info.ACLOCAL_PATH.append(gettext_aclocal)
