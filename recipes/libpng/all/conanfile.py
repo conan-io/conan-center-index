aa84e99a717556cdc8bd4ee78269f50aa0045c18
sunnycase <sunnycase@live.cn>
2021-02-15 02:31:29 +0800

    (#4535) Fix libpng on Apple M1
    * Skip test_arm on Apple M1
    
    * Use NEON on Apple M1
    
    * Use settings.os instead of tools.os_info

--- recipes/libpng/all/conanfile.py
+++ recipes/libpng/all/conanfile.py
@@ -65,6 +65,9 @@ class LibpngConan(ConanFile):
             cmake.definitions["M_LIBRARY"] = ""
             cmake.definitions["ZLIB_LIBRARY"] = self.deps_cpp_info["zlib"].libs[0]
             cmake.definitions["ZLIB_INCLUDE_DIR"] = self.deps_cpp_info["zlib"].include_paths[0]
+        if self.settings.os == "Macos":
+            if 'arm' in self.settings.arch:
+                cmake.definitions["PNG_ARM_NEON"] = "on"
         if self.options.api_prefix:
             cmake.definitions["PNG_PREFIX"] = self.options.api_prefix
         cmake.configure()
