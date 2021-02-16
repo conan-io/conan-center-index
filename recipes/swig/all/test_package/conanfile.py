cfb141f69d50c67dfd4a683a8a8b875ca3ae83ff
Alexey Klimkin <klimkin@gmail.com>
2021-02-15 18:31:23 -0800

    Refactor test_package/conanfile.py

--- recipes/swig/all/test_package/conanfile.py
+++ recipes/swig/all/test_package/conanfile.py
@@ -5,13 +5,28 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package"
 
+    __cmake = None
+
+    @property
+    def _cmake(self):
+        if not self.__cmake:
+            self.__cmake = CMake(self)
+        return self.__cmake
+
+    @property
+    def _can_build(self):
+        # FIXEM: Fix SWIG Python build with Visual Studio
+        # Have a problem linking final module with Visual Studio. CMake finds
+        # correct Python library, but linker tries to use _d variant and fails.
+        return self.settings.compiler != "Visual Studio"
+
     def build(self):
-        if self.settings.compiler != "Visual Studio":
-            cmake = CMake(self)
-            cmake.configure()
-            cmake.build()
-            cmake.test(output_on_failure=True)
+        if self._can_build:
+            self._cmake.configure()
+            self._cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
+            if self._can_build:
+                self._cmake.test(output_on_failure=True)
             self.run("swig -version")
