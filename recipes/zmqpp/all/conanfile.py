4c47bd4e9733b251de792ec53dd015c2999fa3ff
Evgeniy Chugunniy <chgena@mail.ru>
2021-02-17 07:05:36 -0800

    Linux & macOS fixes (static/shared)

--- recipes/zmqpp/all/conanfile.py
+++ recipes/zmqpp/all/conanfile.py
@@ -2,6 +2,8 @@ import os
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 
+# for f in \*_False \*_True zmqpp_False zmqpp_True ; do t=`echo $f|cut -f1 -d_`; sh=`echo $f|cut -f2 -d_`; echo "\!\!\!DOING: $t $sh \!\!\!"; rm -rf /Users/genya/.conan/data/zmqpp/4.2.0 ./test_package/build && conan create . 4.2.0@  -o "${t}:shared=${sh}" ; otool -L test_package/build/*/bin/test_package ; done
+
 class ZmqppConan(ConanFile):
     name = "zmqpp"
     homepage = "https://github.com/zeromq/zmqpp"
@@ -13,7 +15,7 @@ class ZmqppConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
 
     @property
     def _source_subfolder(self):
@@ -34,10 +36,26 @@ class ZmqppConan(ConanFile):
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("zmqpp-%s" % (self.version), self._source_subfolder)
+        cmakeFile = os.path.join(self._source_subfolder, "CMakeLists.txt")
         if self.options.shared == "False": # zmqpp uses different name for static library
-            cmakeFile = os.path.join(self._source_subfolder, "CMakeLists.txt")
             tools.replace_in_file(cmakeFile, "generate_export_header(zmqpp)", "generate_export_header(zmqpp-static)")
             tools.replace_in_file(cmakeFile, "${CMAKE_CURRENT_BINARY_DIR}/zmqpp_export.h", "${CMAKE_CURRENT_BINARY_DIR}/zmqpp-static_export.h")
+        else:
+            tools.replace_in_file(cmakeFile, "set( LIB_TO_LINK_TO_EXAMPLES zmqpp )", "set( LIB_TO_LINK_TO_EXAMPLES zmqpp )\n"
+                                                          "target_link_libraries(zmqpp sodium)")
+            tools.replace_in_file(cmakeFile, "enable_testing()", "enable_testing()\n"
+                                                           "find_package(libsodium REQUIRED)")
+        #
+#         os.rename("Findlibsodium.cmake", "FindSodium.cmake")
+#         tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+#                                        "SODIUM_FOUND",
+#                                        "libsodium_FOUND")
+#         tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+#                                        "SODIUM_INCLUDE_DIRS",
+#                                        "libsodium_INCLUDE_DIRS")
+#         tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+#                                        "SODIUM_LIBRARIES",
+#                                        "libsodium_LIBRARIES")
 
     def validate(self):
         compiler = self.settings.compiler
@@ -54,6 +72,7 @@ class ZmqppConan(ConanFile):
 
     def build(self):
         self.cmake = CMake(self)
+        self.cmake.verbose = True
         self.cmake.definitions["ZMQPP_BUILD_SHARED"] = self.options.shared
         self.cmake.definitions["ZMQPP_BUILD_STATIC"] = not self.options.shared
 #       self.cmake.definitions["BUILD_SHARED"] = self.options.shared
@@ -61,10 +80,21 @@ class ZmqppConan(ConanFile):
 #        if not self.options.shared:
 #            self.cmake.definitions["ZMQ_STATIC"] = False
         self.cmake.configure(build_folder=self._build_subfolder)
+        #self.cmake.configure()
         self.cmake.build()
 
     def package(self):
+        self.cmake.configure()
         self.cmake.install() #TODO: check packages contents!
 
     def package_info(self):
-        self.cpp_info.libs = ["zmqpp"]
+        #self.cpp_info.libs = ["zmqpp"]
+
+        self.cpp_info.names["cmake_find_package"] = "zmqpp"
+        self.cpp_info.names["cmake_find_package_multi"] = "zmqpp"
+        self.cpp_info.names["pkg_config"] = "libzmqpp"
+        libzmq_target = "libzmqpp" if self.options.shared else "libzmqpp"
+        self.cpp_info.components[libzmq_target].names["cmake_find_package"] = libzmq_target
+        self.cpp_info.components[libzmq_target].names["cmake_find_package_multi"] = libzmq_target
+        self.cpp_info.components[libzmq_target].libs = tools.collect_libs(self)
+        self.cpp_info.components[libzmq_target].requires = ["libsodium::libsodium", "zeromq::zeromq"]
