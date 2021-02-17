e10f00b8ba038f7b29781d17b1bc7f9b8137630a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-17 11:44:03 +0100

    provide official variables in cmake_find_package

--- recipes/openssl/1.x.x/conanfile.py
+++ recipes/openssl/1.x.x/conanfile.py
@@ -4,6 +4,8 @@ from functools import total_ordering
 from conans.errors import ConanInvalidConfiguration
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
 
+required_conan_version = ">=1.33.0"
+
 
 @total_ordering
 class OpenSSLVersion(object):
@@ -758,9 +760,39 @@ class OpenSSLConan(ConanFile):
 
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
+        self._create_cmake_module_variables(
+            os.path.join(self.package_folder, self._module_subfolder, self._module_file)
+        )
+
+    @staticmethod
+    def _create_cmake_module_variables(module_file):
+        content = (
+            "set(OPENSSL_FOUND ${OpenSSL_FOUND})\n"
+            "set(OPENSSL_INCLUDE_DIR ${OpenSSL_INCLUDE_DIR})\n"
+            "set(OPENSSL_CRYPTO_LIBRARY ${OpenSSL_Crypto_LIBS})\n"
+            "set(OPENSSL_CRYPTO_LIBRARIES ${OpenSSL_Crypto_LIBS} ${OpenSSL_Crypto_DEPENDENCIES} ${OpenSSL_Crypto_FRAMEWORKS} ${OpenSSL_Crypto_SYSTEM_LIBS})\n"
+            "set(OPENSSL_SSL_LIBRARY ${OpenSSL_SSL_LIBS})\n"
+            "set(OPENSSL_SSL_LIBRARIES ${OpenSSL_SSL_LIBS} ${OpenSSL_SSL_DEPENDENCIES} ${OpenSSL_SSL_FRAMEWORKS} ${OpenSSL_SSL_SYSTEM_LIBS})\n"
+            "set(OPENSSL_LIBRARIES ${OpenSSL_LIBRARIES})\n"
+            "set(OPENSSL_VERSION ${OpenSSL_VERSION})\n"
+        )
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file(self):
+        return "conan-official-{}-variables.cmake".format(self.name)
+
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "OpenSSL"
         self.cpp_info.names["cmake_find_package_multi"] = "OpenSSL"
+        self.cpp_info.components["ssl"].builddirs = [self._module_subfolder]
+        self.cpp_info.components["ssl"].build_modules["cmake_find_package"] = [os.path.join(self._module_subfolder, self._module_file)]
+        self.cpp_info.components["crypto"].builddirs = [self._module_subfolder]
+        self.cpp_info.components["crypto"].build_modules["cmake_find_package"] = [os.path.join(self._module_subfolder, self._module_file)]
         if self._use_nmake:
             libsuffix = "d" if self.settings.build_type == "Debug" else ""
             if self._full_version < "1.1.0":
