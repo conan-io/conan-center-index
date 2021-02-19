161ceea29c1e4670b86d1c5aba4dac7d37bd8e4e
Riff, Eric <eriff@irobot.com>
2021-02-19 17:48:58 -0300

    Simplify test_package so it doesn't use external png image

--- recipes/opencv/2.x/test_package/conanfile.py
+++ recipes/opencv/2.x/test_package/conanfile.py
@@ -14,6 +14,4 @@ class OpenCVTestConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             bin_path = os.path.join("bin", "test_package")
-            peppers = os.path.join(self.source_folder, "peppers.jpg")
-            res_path = self.deps_cpp_info["opencv"].res_paths[0]
-            self.run("{} {} {}".format(bin_path, res_path, peppers), run_environment=True)
+            self.run(bin_path, run_environment=True)
