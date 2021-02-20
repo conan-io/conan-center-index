77ed4c980222f401c241fdbc0b33bf06ed0d40bf
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-20 12:56:18 +0100

    cache cmake

--- recipes/bzip2/1.0.8/conanfile.py
+++ recipes/bzip2/1.0.8/conanfile.py
@@ -10,11 +10,22 @@ class Bzip2Conan(ConanFile):
     license = "bzip2-1.0.8"
     description = "bzip2 is a free and open-source file compression program that uses the Burrows Wheeler algorithm."
     topics = ("conan", "bzip2", "data-compressor", "file-compression")
+
     settings = "os", "compiler", "arch", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False], "build_executable": [True, False]}
-    default_options = {"shared": False, "fPIC": True, "build_executable": True}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "build_executable": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "build_executable": True
+    }
+
     exports_sources = "CMakeLists.txt"
     generators = "cmake"
+    _cmake = None
 
     @property
     def _source_subfolder(self):
@@ -36,13 +47,15 @@ class Bzip2Conan(ConanFile):
         os.rename(folder_name, self._source_subfolder)
 
     def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
         major = self.version.split(".")[0]
-        cmake = CMake(self)
-        cmake.definitions["BZ2_VERSION_STRING"] = self.version
-        cmake.definitions["BZ2_VERSION_MAJOR"] = major
-        cmake.definitions["BZ2_BUILD_EXE"] = "ON" if self.options.build_executable else "OFF"
-        cmake.configure()
-        return cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["BZ2_VERSION_STRING"] = self.version
+        self._cmake.definitions["BZ2_VERSION_MAJOR"] = major
+        self._cmake.definitions["BZ2_BUILD_EXE"] = "ON" if self.options.build_executable else "OFF"
+        self._cmake.configure()
+        return self._cmake
 
     def build(self):
         cmake = self._configure_cmake()
