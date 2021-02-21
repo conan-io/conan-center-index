761aae3182d004c52879b0faf4f56e06e988d5ff
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-21 10:14:52 +0100

    check if defined variables in CMake module file

--- recipes/openssl/1.x.x/conanfile.py
+++ recipes/openssl/1.x.x/conanfile.py
@@ -766,30 +766,30 @@ class OpenSSLConan(ConanFile):
     @staticmethod
     def _create_cmake_module_variables(module_file):
         content = """\
-            if(OpenSSL_FOUND)
+            if(DEFINED OpenSSL_FOUND)
                 set(OPENSSL_FOUND ${OpenSSL_FOUND})
             endif()
-            if(OpenSSL_INCLUDE_DIR)
+            if(DEFINED OpenSSL_INCLUDE_DIR)
                 set(OPENSSL_INCLUDE_DIR ${OpenSSL_INCLUDE_DIR})
             endif()
-            if(OpenSSL_Crypto_LIBS)
+            if(DEFINED OpenSSL_Crypto_LIBS)
                 set(OPENSSL_CRYPTO_LIBRARY ${OpenSSL_Crypto_LIBS})
                 set(OPENSSL_CRYPTO_LIBRARIES ${OpenSSL_Crypto_LIBS}
                                              ${OpenSSL_Crypto_DEPENDENCIES}
                                              ${OpenSSL_Crypto_FRAMEWORKS}
                                              ${OpenSSL_Crypto_SYSTEM_LIBS})
             endif()
-            if(OpenSSL_SSL_LIBS)
+            if(DEFINED OpenSSL_SSL_LIBS)
                 set(OPENSSL_SSL_LIBRARY ${OpenSSL_SSL_LIBS})
                 set(OPENSSL_SSL_LIBRARIES ${OpenSSL_SSL_LIBS}
                                           ${OpenSSL_SSL_DEPENDENCIES}
                                           ${OpenSSL_SSL_FRAMEWORKS}
                                           ${OpenSSL_SSL_SYSTEM_LIBS})
             endif()
-            if(OpenSSL_LIBRARIES)
+            if(DEFINED OpenSSL_LIBRARIES)
                 set(OPENSSL_LIBRARIES ${OpenSSL_LIBRARIES})
             endif()
-            if(OpenSSL_VERSION)
+            if(DEFINED OpenSSL_VERSION)
                 set(OPENSSL_VERSION ${OpenSSL_VERSION})
             endif()
         """
