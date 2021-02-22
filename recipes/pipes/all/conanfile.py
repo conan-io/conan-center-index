cad83d9f30f0bcfa2a47f99ebdf4025192b257be
hnOsmium0001 <hn.osmium0001@gmail.com>
2021-02-21 20:54:00 -0800

    Revert to .format() from f-string

--- recipes/pipes/all/conanfile.py
+++ recipes/pipes/all/conanfile.py
@@ -34,10 +34,12 @@ class PipesConan(ConanFile):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
         if not min_version:
-            self.output.warn(f"{self.name} recipe lacks information about the {self.settings.compiler} compiler support.")
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                self.name, self.settings.compiler))
         else:
             if tools.Version(self.settings.compiler.version) < min_version:
-                raise ConanInvalidConfiguration(f"{self.name} requires C++{self._minimum_cpp_standard} support. The current compiler {self.settings.compiler} {self.settings.compiler.version} does not support it.")
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
 
     def package_id(self):
         self.info.header_only()
