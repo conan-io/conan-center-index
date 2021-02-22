5ef68ba8992cd6576eb8637e92c3d5a7e58de4ed
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-22 15:12:05 +0100

    improve genericity of build across versions

--- recipes/moltenvk/all/conanfile.py
+++ recipes/moltenvk/all/conanfile.py
@@ -29,7 +29,7 @@ class MoltenVKConan(ConanFile):
         "tools": True
     }
 
-    exports_sources = "CMakeLists.txt"
+    exports_sources = ["CMakeLists.txt", "patches/**"]
     generators = "cmake"
     _cmake = None
 
@@ -48,47 +48,90 @@ class MoltenVKConan(ConanFile):
     def requirements(self):
         self.requires("cereal/1.3.0")
         self.requires("glslang/8.13.3559")
-        self.requires("spirv-cross/20210115")
-        self.requires("vulkan-headers/1.2.162.0")
+        self.requires("spirv-cross/{}".format(self._spirv_cross_version))
+        self.requires("vulkan-headers/{}".format(self._vulkan_headers_version))
         if self.options.with_spirv_tools:
             self.requires("spirv-tools/v2020.5")
+        if tools.Version(self.version) < "1.1.0":
+            raise ConanInvalidConfiguration("MoltenVK < 1.1.0 requires vulkan-portability")
+
+    @property
+    def _spirv_cross_version(self):
+        return {
+            "1.1.1": "20210115", # can't compile with spirv-cross < 20210115
+            "1.1.0": "20200917", # works with spirv-cross 20200917 only
+        }.get(self.version)
+
+    @property
+    def _vulkan_headers_version(self):
+        return {
+            "1.1.1": "1.2.162.0",
+            "1.1.0": "1.2.154.0",
+            "1.0.44": "1.2.148.0",
+            "1.0.43": "1.2.141.0",
+            "1.0.42": "1.2.141.0",
+            "1.0.41": "1.2.135.0",
+            "1.0.40": "1.2.131.1",
+            "1.0.39": "1.1.130.0",
+            "1.0.38": "1.1.126.0",
+            "1.0.37": "1.1.121.0",
+            "1.0.36": "1.1.114.0",
+            "1.0.35": "1.1.108.0",
+            "1.0.34": "1.1.106.0",
+            "1.0.33": "1.1.101.0",
+            "1.0.32": "1.1.97.0",
+            "1.0.31": "1.1.97.0",
+            "1.0.30": "1.1.92.0",
+            "1.0.29": "1.1.92.0",
+            "1.0.28": "1.1.92.0",
+            "1.0.27": "1.1.92.0",
+            "1.0.26": "1.1.85.0",
+            "1.0.25": "1.1.85.0",
+            "1.0.24": "1.1.85.0",
+            "1.0.23": "1.1.85.0",
+            "1.0.22": "1.1.82.0",
+            "1.0.21": "1.1.82.0",
+            "1.0.20": "1.1.82.0",
+            "1.0.19": "1.1.82.0",
+            "1.0.18": "1.1.82.0",
+            "1.0.17": "1.1.82.0",
+        }.get(self.version)
+
+    def package_id(self):
+        del self.info.settings.compiler.version
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("MoltenVK-" + self.version, self._source_subfolder)
 
-    def package_id(self):
-        del self.info.settings.compiler.version
-
     def _patch_sources(self):
-        # Note: All these fixes might be very specific to 1.1.1
-        # Properly include external spirv-cross headers
-        files_to_patch = [
-            os.path.join(self._source_subfolder, "MoltenVK", "MoltenVK", "GPUObjects", "MVKPixelFormats.h"),
-            os.path.join(self._source_subfolder, "MoltenVKShaderConverter", "Common", "SPIRVSupport.cpp"),
-            os.path.join(self._source_subfolder, "MoltenVKShaderConverter", "MoltenVKShaderConverter", "SPIRVReflection.h"),
-            os.path.join(self._source_subfolder, "MoltenVKShaderConverter", "MoltenVKShaderConverter", "SPIRVToMSLConverter.h")
-        ]
-        for file_to_patch in files_to_patch:
-            tools.replace_in_file(file_to_patch, "SPIRV-Cross", "spirv_cross")
-        # Provide a random spirv-cross hash
-        tools.replace_in_file(os.path.join(self._source_subfolder, "MoltenVK", "MoltenVK", "GPUObjects", "MVKDevice.mm"),
-                              "#include \"mvkGitRevDerived.h\"",
-                              "static const char* mvkRevString = \"fc0750d67cfe825b887dd2cf25a42e9d9a013eb2\";")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        if tools.Version(self.version) >= "1.1.0":
+            tools.replace_in_file(os.path.join(self._source_subfolder, "MoltenVK", "MoltenVK", "GPUObjects", "MVKDevice.mm"),
+                                  "#include \"mvkGitRevDerived.h\"",
+                                  "static const char* mvkRevString = \"{}\";".format(self._mvk_commit_hash))
+
+    @property
+    def _mvk_commit_hash(self):
+        return {
+            "1.1.1": "49de6604b0395057e7d3b7ce7001ed29b25708f7",
+            "1.1.0": "b9b78def172074872bfbb1015ccf75eeec554ae2",
+        }.get(self.version)
 
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
+        self._cmake.definitions["MVK_VERSION"] = self.version
         self._cmake.definitions["MVK_WITH_SPIRV_TOOLS"] = self.options.with_spirv_tools
         self._cmake.definitions["MVK_BUILD_SHADERCONVERTER_TOOL"] = self.options.tools
         self._cmake.configure()
         return self._cmake
 
     def build(self):
-        # Might depend on MoltenVK version
-        if tools.Version(self.settings.compiler.version) < 12:
-            raise ConanInvalidConfiguration("MoltenVK {} requires XCode 12+ at build time".format(self.version))
+        if tools.Version(self.version) >= "1.0.42" and tools.Version(self.settings.compiler.version) < "12":
+            raise ConanInvalidConfiguration("MoltenVK {} requires XCode 12 or higher at build time".format(self.version))
         self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
