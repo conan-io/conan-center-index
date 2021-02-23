ddd49bf8d2a5d109dc4fc78ded2e373ae2609d5a
hnOsmium0001 <hn.osmium0001@gmail.com>
2021-02-22 18:31:29 -0800

    (#4631) Add pipes/1.0.0
    * Add pipes/1.0.0
    
    * Add line breaks at end of files
    
    * Minimum c++ standard version
    
    * Fix min std version
    
    * Apply suggestions
    
    * Fix not importing errors
    
    * Revert to .format() from f-string

---
+++ recipes/pipes/all/test_package/test_package.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <pipes/pipes.hpp>
+
+int main() {
+    auto source = std::vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    auto destination = std::vector<int>{};
+
+    source >>= pipes::filter([](int i){ return i % 2 == 0; })
+        >>= pipes::transform([](int i){ return i * 2; })
+        >>= pipes::push_back(destination);
+
+    auto expected = std::vector<int>{0, 4, 8, 12, 16};
+    bool success = destination == expected;
+
+    return success ? 0 : 1;
+}
