6fe148e82977c328addde8be61ada1b8c861ed2f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-25 15:23:08 +0100

    remove vcruntime

--- recipes/powershell/all/conanfile.py
+++ recipes/powershell/all/conanfile.py
@@ -47,6 +47,8 @@ class PowershellConan(ConanFile):
         self.copy(pattern="*", dst="bin", src=self._source_subfolder, keep_path=True, symlinks=True)
         tools.remove_files_by_mask(os.path.join(self.package_folder, "bin"), "*.pdb")
         self._fix_permissions()
+        if self.settings.os_build == "Windows":
+            tools.remove_files_by_mask(os.path.join(self.package_folder, "bin"), "vcruntime*.dll")
         if self.settings.os_build == "Linux":
             os.unlink(os.path.join(self.package_folder, "bin", "libcrypto.so.1.0.0"))
             os.unlink(os.path.join(self.package_folder, "bin", "libssl.so.1.0.0"))
@@ -63,3 +65,8 @@ class PowershellConan(ConanFile):
         bin_dir = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH environment variable: {}".format(bin_dir))
         self.env_info.PATH.append(bin_dir)
+
+        self.cpp_info.includedurs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
+        self.cpp_info.frameworkdirs = []
