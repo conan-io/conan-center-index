dff548f74a3200bf497af6fbedde08ed33b9e6cb
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-25 15:28:47 +0100

    add test_package

--- recipes/powershell/all/test_package/conanfile.py
+++ recipes/powershell/all/test_package/conanfile.py
@@ -1,7 +1,14 @@
-from conans import ConanFile
+from conans import ConanFile, tools
+from conans.errors import ConanException
 
 
 class TestPackageConan(ConanFile):
 
     def test(self):
-        pass
+        pwsh = tools.which("pwsh")
+        if pwsh:
+            self.output.info("using pwsh from: " + pwsh)
+        else:
+            raise ConanException("No instance of pwsh could be found in PATH")
+
+        self.run("pwsh --help")
