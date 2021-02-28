83453ba9e14623af6cc6883298d716501d3604ca
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-28 10:55:56 +0100

    improve module file defining official imported targets

--- recipes/opencv/4.x/conanfile.py
+++ recipes/opencv/4.x/conanfile.py
@@ -1,8 +1,9 @@
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 import os
+import textwrap
 
-required_conan_version = ">=1.29.1"
+required_conan_version = ">=1.33.0"
 
 
 class OpenCVConan(ConanFile):
@@ -292,12 +293,12 @@ class OpenCVConan(ConanFile):
     def _create_cmake_module_alias_targets(module_file, targets):
         content = ""
         for alias, aliased in targets.items():
-            content += (
-                "if(TARGET {aliased} AND NOT TARGET {alias})\n"
-                "    add_library({alias} INTERFACE IMPORTED)\n"
-                "    target_link_libraries({alias} INTERFACE {aliased})\n"
-                "endif()\n"
-            ).format(alias=alias, aliased=aliased)
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
         tools.save(module_file, content)
 
     @property
@@ -420,8 +421,10 @@ class OpenCVConan(ConanFile):
                 requires = component["requires"]
                 self.cpp_info.components[conan_component].names["cmake_find_package"] = cmake_target
                 self.cpp_info.components[conan_component].names["cmake_find_package_multi"] = cmake_target
-                self.cpp_info.components[conan_component].builddirs = [self._module_subfolder]
-                self.cpp_info.components[conan_component].build_modules = [os.path.join(self._module_subfolder, self._module_file)]
+                self.cpp_info.components[conan_component].builddirs.append(self._module_subfolder)
+                module_rel_path = os.path.join(self._module_subfolder, self._module_file)
+                self.cpp_info.components[conan_component].build_modules["cmake_find_package"] = [module_rel_path]
+                self.cpp_info.components[conan_component].build_modules["cmake_find_package_multi"] = [module_rel_path]
                 self.cpp_info.components[conan_component].libs = [lib_name]
                 self.cpp_info.components[conan_component].includedirs.append(os.path.join("include", "opencv4"))
                 self.cpp_info.components[conan_component].requires = requires
