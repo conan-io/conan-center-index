21baf50ca5ccd5d6b1d3e08c7c366c4259e52871
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-28 01:45:14 +0100

    fix compiler versions verifications

--- recipes/simdjson/all/conanfile.py
+++ recipes/simdjson/all/conanfile.py
@@ -48,12 +48,17 @@ class SimdjsonConan(ConanFile):
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, "17")
 
+        def lazy_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
+
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version:
-            if tools.Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not fully support.".format(self.name))
-        else:
+        if not minimum_version:
             self.output.warn("{} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name))
+        elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not fully support.".format(self.name))
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
