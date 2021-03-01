0ffb718ca191799b3a9268482eaa1802caad26fe
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-01 18:02:10 +0100

    (#4739) cgns: don't suggest unofficial CMake names + simplify and speed-up build
    * don't suggest unofficial CMake names
    
    * explicit cpp_info.libs
    
    * really build static or shared
    
    We have to disable build of tools for that (they were removed from package anyway).
    Also explicitly set several upstream options.
    
    * more aggressive patch to avoid static & shared
    
    * fix cpp_info.defines if windows and shared
    
    cgns has several global variables, therefore CGNSDLL must be properly defined

--- recipes/cgns/all/test_package/test_package.cpp
+++ recipes/cgns/all/test_package/test_package.cpp
@@ -1,5 +1,7 @@
 #include <cgnslib.h>
 
+#include <cstdio>
+
 int main() {
     int indexFile;
     int indexBase;
@@ -7,6 +9,13 @@ int main() {
     cg_open("test.cgns", CG_MODE_WRITE, &indexFile);
 
     cg_base_write(indexFile, "Base2D", 2, 3, &indexBase);
+
+    // test a global variable
+    printf("MassUnits: ");
+    for (unsigned i = 0; i < NofValidMassUnits; ++i) {
+        printf("%s ", MassUnitsName[i]);
+    }
+    printf("\n");
 
     return 0;
 }
