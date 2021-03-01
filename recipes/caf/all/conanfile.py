7bde9b81e8fccfa81af5279ace6f5ccd1e2b096f
Eric Pederson <ericacm+github@gmail.com>
2021-03-01 14:23:05 -0500

    Use with_openssl
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/caf/all/conanfile.py
+++ recipes/caf/all/conanfile.py
@@ -20,7 +20,7 @@ class CAFConan(ConanFile):
         "log_level": ["ERROR", "WARNING", "INFO", "DEBUG", "TRACE", "QUIET"],
         "with_openssl": [True, False]
     }
-    default_options = {"shared": False, "fPIC": True, "log_level": "QUIET", "openssl": True}
+    default_options = {"shared": False, "fPIC": True, "log_level": "QUIET", "with_openssl": True}
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
     _cmake = None
