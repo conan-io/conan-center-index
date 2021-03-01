eeb1440c516420277e4b1e7f23fe4b370030d81a
Eric Pederson <ericacm+github@gmail.com>
2021-03-01 14:18:05 -0500

    Fix license attribute
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/caf/all/conanfile.py
+++ recipes/caf/all/conanfile.py
@@ -10,7 +10,7 @@ class CAFConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/actor-framework/actor-framework"
     topics = "conan", "caf", "actor-framework", "actor-model", "pattern-matching", "actors"
-    license = "BSD-3-Clause, BSL-1.0"
+    license = "BSD-3-Clause", "BSL-1.0"
     exports_sources = ["CMakeLists.txt", "patches/**"]
     generators = "cmake", "cmake_find_package"
     settings = "os", "compiler", "build_type", "arch"
