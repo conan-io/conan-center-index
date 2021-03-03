eeab9464e91aee6461f32e532b7b5ca6b70e08d2
bincrafters-user <bincrafters@gmail.com>
2021-03-03 07:13:51 +0000

    simple-websocket-server: Update Conan conventions
    Automatically created by bincrafters-conventions 0.30.2

--- recipes/simple-websocket-server/all/conanfile.py
+++ recipes/simple-websocket-server/all/conanfile.py
@@ -25,7 +25,7 @@ class SimpleWebSocketServerConan(ConanFile):
         return "source_subfolder"
 
     def requirements(self):
-        self.requires("openssl/1.1.1h")
+        self.requires("openssl/1.1.1i")
         # only version 2.0.2 upwards is able to build against asio 1.18.0 or higher
         if tools.Version(self.version) <= "2.0.1":
             if self.options.use_asio_standalone:
