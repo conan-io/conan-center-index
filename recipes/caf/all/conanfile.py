2d3e2cce935cb3a17b3f145c0e3c8b4f0acd2538
Eric Pederson <ericacm+github@gmail.com>
2021-03-07 14:11:01 -0500

    Delete fPIC if shared

--- recipes/caf/all/conanfile.py
+++ recipes/caf/all/conanfile.py
@@ -29,6 +29,10 @@ class CAFConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("actor-framework-" + self.version, self._source_subfolder)
