7b25237e819420aab3e06fd6e8b47ab0103cf465
Riff, Eric <eriff@irobot.com>
2021-03-08 13:58:14 -0300

    Clean up recipe

--- recipes/qarchive/all/conanfile.py
+++ recipes/qarchive/all/conanfile.py
@@ -25,8 +25,6 @@ class qarchiveConan(ConanFile):
     def _configure_cmake(self):
         if not self._cmake:
             self._cmake = CMake(self)
-            if self.settings.os == "Windows" and self.options.shared:
-                self._cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
             self._cmake.configure()
         return self._cmake
 
