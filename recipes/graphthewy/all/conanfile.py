ab67b17ced5b331ba7d91941d7340863dc4f6022
alex-87 <alexis.legoadec@laposte.net>
2021-03-10 21:09:23 +0100

    Implementing review on compiler version checking and CMake

--- recipes/graphthewy/all/conanfile.py
+++ recipes/graphthewy/all/conanfile.py
@@ -11,7 +11,7 @@ class GraphthewyConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     description = "Simple header-only C++ Library for graph modelling (directed or not) and graph cycle detection. "
     topics = ("graph", "algorithm", "modelling", "header-only")
-    settings = "os", "compiler"
+    settings = "compiler"
     no_copy_source = True
 
     def source(self):
@@ -33,18 +33,23 @@ class GraphthewyConan(ConanFile):
 
     def configure(self):
         if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, 17)
+            tools.check_min_cppstd(self, "17")
+
+        def lazy_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
 
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version:
-            if tools.Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration("graphthewy requires C++17, which your compiler does not support.")
-        else:
+        if not minimum_version:
             self.output.warn("graphthewy requires C++17. Your compiler is unknown. Assuming it supports C++17.")
+        elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration("graphthewy requires C++17, which your compiler does not support.")
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("*.hpp", "include/graphthewy", keep_path=False)
+        self.copy("*.hpp", dst=os.path.join("include", "graphthewy"))
 
     def package_id(self):
         self.info.header_only()
