998d3ff2270644d9f337bf261a21e6d628090238
alex-87 <alexis.legoadec@laposte.net>
2021-03-10 23:27:59 +0100

    Fixing path src to _source_subfolder

--- recipes/graphthewy/all/conanfile.py
+++ recipes/graphthewy/all/conanfile.py
@@ -49,7 +49,7 @@ class GraphthewyConan(ConanFile):
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("*.hpp", dst=os.path.join("include", "graphthewy"))
+        self.copy("*.hpp", dst=os.path.join("include", "graphthewy"), src=self._source_subfolder)
 
     def package_id(self):
         self.info.header_only()
