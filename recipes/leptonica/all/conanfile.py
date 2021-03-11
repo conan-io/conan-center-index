cce44a96c660f3b0284ef45050bedd3299edcd57
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-11 23:18:05 +0100

    fix CMake imported target

--- recipes/leptonica/all/conanfile.py
+++ recipes/leptonica/all/conanfile.py
@@ -1,5 +1,8 @@
 from conans import ConanFile, CMake, tools
 import os
+import textwrap
+
+required_conan_version = ">=1.33.0"
 
 
 class LeptonicaConan(ConanFile):
@@ -149,6 +152,31 @@ class LeptonicaConan(ConanFile):
         self.copy(pattern="leptonica-license.txt", dst="licenses", src=self._source_subfolder)
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
         tools.rmdir(os.path.join(self.package_folder, "cmake"))
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"leptonica": "Leptonica::Leptonica"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
@@ -156,5 +184,8 @@ class LeptonicaConan(ConanFile):
             self.cpp_info.system_libs = ["m"]
         self.cpp_info.names["cmake_find_package"] = "Leptonica"
         self.cpp_info.names["cmake_find_package_multi"] = "Leptonica"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
         self.cpp_info.names["pkg_config"] = "lept"
         self.cpp_info.includedirs.append(os.path.join("include", "leptonica"))
