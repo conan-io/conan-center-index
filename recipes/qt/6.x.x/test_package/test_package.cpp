3b6460d721150001ba63c3961ceacae5eca35754
jgsogo <jgsogo@gmail.com>
2021-03-12 12:45:50 +0100

    qt from master

---
+++ recipes/qt/6.x.x/test_package/test_package.cpp
@@ -0,0 +1,22 @@
+#include <QCoreApplication>
+#include <QObject>
+#include <QString>
+#include <QTimer>
+#include "greeter.h"
+
+int main(int argc, char *argv[]){
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("Application Example");
+    QCoreApplication::setApplicationVersion("1.0.0");
+
+    QString name = argc > 0 ? argv[1] : "";
+    if (name.isEmpty()) {
+        name = "World";
+    }
+
+    Greeter* greeter = new Greeter(name, &app);
+    QObject::connect(greeter, SIGNAL(finished()), &app, SLOT(quit()));
+    QTimer::singleShot(0, greeter, SLOT(run()));
+
+    return app.exec();
+}
