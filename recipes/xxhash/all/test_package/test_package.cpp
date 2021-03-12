ef65e1657b1cd0ef29720c6312493b05c8e6bcd2
danimtb <danimanzaneque@gmail.com>
2021-03-12 11:46:52 +0100

    fix test package

--- recipes/xxhash/all/test_package/test_package.cpp
+++ recipes/xxhash/all/test_package/test_package.cpp
@@ -1,12 +1,14 @@
 #include "xxhash.h"
+
 #include <iostream>
+#include <stdlib.h>
 
 
 int main()
 {
     size_t const bufferSize = 10;
     void* const buffer = malloc(bufferSize);
-    XXH64_hash_t hash = XXH64(buffer, 8, 1);
+    XXH64_hash_t hash = XXH64(buffer, bufferSize, 0);
     std::cout << hash << std::endl;
     free(buffer);
     return 0;
