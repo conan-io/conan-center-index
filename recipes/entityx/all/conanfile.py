64b74dec1768af277c770194e3396befd9bb7348
James <james@conan.io>
2021-03-12 17:03:35 +0100

    Update recipes/entityx/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/entityx/all/conanfile.py
+++ recipes/entityx/all/conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.32.0"
 
 
 class EntityXConan(ConanFile):
