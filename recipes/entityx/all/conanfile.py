c176ea2b0d170534098823dcf18cd6e2608197e0
memsharded <james@conan.io>
2021-03-12 10:48:26 +0100

    adding new library entityx/1.1.2

---
+++ recipes/entityx/all/conanfile.py
@@ -0,0 +1,79 @@
+import os
+
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+
+
+required_conan_version = ">=1.33.0"
+
+
+class EntityXConan(ConanFile):
+    name = "entityx"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/alecthomas/entityx/"
+    topics = ("entity", "conan")
+    license = "MIT"
+    description = "EntityX is an EC system that uses C++11 features to provide type-safe component management, event delivery, etc."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    generators = "cmake"
+
+    exports_sources = "CMakeLists.txt"
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("entityx-" + self.version, self._source_subfolder)
+
+    def validate(self):
+        if self.settings.os == "Windows" and self.options.shared:
+            raise ConanInvalidConfiguration("Windows shared library do not export all symbols")
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["ENTITYX_BUILD_SHARED"] = "1" if self.options.shared else "0"
+        self._cmake.definitions["ENTITYX_BUILD_TESTING"] = "0"
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+        self.copy("COPYING", src=self._source_subfolder, dst="licenses", keep_path=False)
+        # Missing dll in windows, will submit a patch upstream to CMake install
+        self.copy("*entity*.dll", src=self._build_subfolder, dst="bin", keep_path=False)
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        debug = "-d" if self.settings.build_type == "Debug" else ""
+        self.cpp_info.libs = ["entityx{}".format(debug)]
