c0d79ba035931fdd3a094287cca7311669e710b9
SSE4 <tomskside@gmail.com>
2021-03-12 17:04:22 +0700

    - add protable-file-dialogs/0.1.0
    Signed-off-by: SSE4 <tomskside@gmail.com>

---
+++ recipes/portable-file-dialogs/all/conanfile.py
@@ -0,0 +1,29 @@
+import os
+from conans import ConanFile, CMake, tools
+
+
+class PortableFileDialogsConan(ConanFile):
+    license = "WTFPL"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/samhocevar/portable-file-dialogs"
+    description = "Portable GUI dialogs library, C++11, single-header"
+    topics = ("conan", "gui", "dialogs")
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy("portable-file-dialogs.h", dst="include", src=self._source_subfolder)
+        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
+
+    def package_info(self):
+        self.cpp_info.libdirs = []
