f0257303af4e6d4a7df83441c016c7521879a697
SSE4 <tomskside@gmail.com>
2021-03-12 18:49:03 +0700

    - try to skip GCC 4.9...
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- recipes/luple/all/conanfile.py
+++ recipes/luple/all/conanfile.py
@@ -9,6 +9,28 @@ class LupleConan(ConanFile):
     homepage = "https://github.com/alexpolt/luple"
     description = "Home to luple, nuple, C++ String Interning, Struct Reader and C++ Type Loophole"
     topics = ("conan", "loophole", "luple", "nuple", "struct", "intern")
+    settings = "compiler"
+
+    def configure(self):
+        minimal_cpp_standard = "14"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
+        minimal_version = {
+            "gcc": "5",
+            "clang": "3.4",
+            "apple-clang": "10",
+            "Visual Studio": "14"
+        }
+        compiler = str(self.settings.compiler)
+        if compiler not in minimal_version:
+            self.output.warn(
+                "%s recipe lacks information about the %s compiler standard version support" % (self.name, compiler))
+            self.output.warn(
+                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+            return
+        version = tools.Version(self.settings.compiler.version)
+        if version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
 
     @property
     def _source_subfolder(self):
