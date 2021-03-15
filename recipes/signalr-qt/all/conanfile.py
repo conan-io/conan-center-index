c5e754be2346f0fe1f040b5f7e3701ce4da068f6
s.sadovskiy <ss@dssl.ru>
2021-03-15 11:32:25 +0300

    signalr-qt

---
+++ recipes/signalr-qt/all/conanfile.py
@@ -0,0 +1,76 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class SignalrQtConan(ConanFile):
+    name = "signalr-qt"
+    url = "https://github.com/trassir/conan-center-index"
+    homepage = "https://github.com/p3root/signalr-qt"
+    topics = ("signalr-qt", "conan")
+    license = "signalr-qt"
+    description = "C++ implementation of Microsofts ASP.Net SignalR"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False]
+    }
+    default_options = {
+        "shared": True
+    }
+    generators = "qmake"
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("AMQP-CPP-" + self.version, self._source_subfolder)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+            del self.options.linux_tcp_module
+
+    def requirements(self):
+        self.requires.add("qt/5.14.1")
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["AMQP-CPP_BUILD_SHARED"] = self.options.shared
+        self._cmake.definitions["AMQP-CPP_BUILD_EXAMPLES"] = False
+        self._cmake.definitions["AMQP-CPP_LINUX_TCP"] = self.options.get_safe("linux_tcp_module") or False
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def _patch_sources(self):
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
+
+    def build(self):
+        self._patch_sources()
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses", keep_path=False)
+        tools.rmdir(os.path.join(self.package_folder, "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.names["pkg_config"] = "amqpcpp"
+        self.cpp_info.names["cmake_find_package"] = "amqpcpp"
+        self.cpp_info.names["cmake_find_package_multi"] = "amqpcpp"
+        self.cpp_info.libs = ["amqpcpp"]
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["dl", "pthread"]
