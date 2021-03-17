e27580380f787ded1773e7c89f99407468f6d657
Tobias Ribizel <ribizel@kit.edu>
2021-03-17 17:54:40 +0100

    [ginkgo] Avoid shadowing compiler settings

--- recipes/ginkgo/all/conanfile.py
+++ recipes/ginkgo/all/conanfile.py
@@ -10,13 +10,24 @@ class GinkgoConan(ConanFile):
     description = "High-performance linear algebra library for manycore systems, with a focus on sparse solution of linear systems."
     topics = ("hpc", "linear-algebra")
     settings = {"os": None,
-                "compiler": {"Visual Studio": {"version": ["16"]},
-                             "gcc": {"version": ["5.4", "5.5", "6", "6.1", "6.2", "6.3", "6.4", "6.5",
-                                                 "7", "7.1", "7.2", "7.3", "7.4", "7.5", "8", "8.1", "8.2", "8.3", "8.4",
-                                                 "9", "9.1", "9.2", "9.3", "10", "10.1"]},
-                             "clang": {"version": ["3.9", "4.0", "5.0", "6.0", "7.0", "7.1", "8", "9", "10", "11"]},
-                             "apple-clang": {"version": ["10.0", "11.0", "12.0"]},
-                             "intel": {"version": ["18", "19"]}}, "build_type": None, "arch": None}
+                "compiler": {"Visual Studio": {"runtime": None, "toolset": None,
+                                               "version": ["16"]},
+                             "gcc": {"exception": None, "libcxx": None,
+                                     "threads": None,
+                                     "version": [
+                                         "5.4", "5.5", "6", "6.1", "6.2", "6.3",
+                                         "6.4", "6.5", "7", "7.1", "7.2", "7.3",
+                                         "7.4", "7.5", "8", "8.1", "8.2", "8.3",
+                                         "8.4", "9", "9.1", "9.2", "9.3", "10",
+                                         "10.1"]},
+                             "clang": {"runtime": None, "libcxx": None,
+                                       "version": [
+                                           "3.9", "4.0", "5.0", "6.0", "7.0",
+                                           "7.1", "8", "9", "10", "11"]},
+                             "apple-clang": {"libcxx": None, "version": [
+                                                 "10.0", "11.0", "12.0"]},
+                             "intel": {"version": ["18", "19"]}},
+                "build_type": None, "arch": None}
     options = {"shared": [True, False], "fPIC": [True, False], "openmp": [
         True, False], "cuda": [True, False]}
     default_options = {"shared": False, "fPIC": False,
@@ -50,8 +61,6 @@ class GinkgoConan(ConanFile):
         self._cmake.definitions["GINKGO_BUILD_EXAMPLES"] = False
         self._cmake.definitions["GINKGO_BUILD_BENCHMARKS"] = False
         self._cmake.definitions["GINKGO_DEVEL_TOOLS"] = False
-        self._cmake.definitions["GINKGO_SKIP_DEPENDENCY_UPDATE"] = True
-        self._cmake.definitions["CMAKE_DISABLE_FIND_PACKAGE_Git"] = True
         self._cmake.definitions["GINKGO_BUILD_REFERENCE"] = True
         self._cmake.definitions["GINKGO_BUILD_OMP"] = self.options.openmp
         self._cmake.definitions["GINKGO_BUILD_CUDA"] = self.options.cuda
