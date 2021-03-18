3d07eff493e168cfb66cfdf802dd4afaa406ebd9
SSE4 <tomskside@gmail.com>
2021-03-19 00:06:27 +0700

    Update conanfile.py

--- recipes/theora/all/conanfile.py
+++ recipes/theora/all/conanfile.py
@@ -110,6 +110,8 @@ class TheoraConan(ConanFile):
             with tools.chdir(self._source_subfolder):
                 autotools = self._configure_autotools()
                 autotools.install()
+            tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
+            tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
