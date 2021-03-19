9d676447d2dcf96ec2847464397bd6ec4c948974
Rui Oliveira <ruimail24@gmail.com>
2021-03-19 13:01:34 +0000

    (#4939) Add pffft package
    * Add pffft package
    
    * Remove cppstd and libcxx because it's a C library
    
    * [pffft] Add missing package_info
    
    Co-authored-by: SSE4 <tomskside@gmail.com>
    
    * [pffft] Make libm a public dependency
    
    * [pffft] Add libm as a dependency
    
    Co-authored-by: SSE4 <tomskside@gmail.com>
    
    * [pffft] Add printf to the testcase
    
    * [pffft] add fPIC option
    
    * [pffft] Fix printf of ints
    
    I swear I know how to program... C++...
    
    Co-authored-by: SSE4 <tomskside@gmail.com>

---
+++ recipes/pffft/all/conanfile.py
@@ -0,0 +1,65 @@
+import os
+from conans import ConanFile, CMake, tools
+
+required_conan_version = ">=1.28.0"
+
+
+class ConanPffft(ConanFile):
+    name = "pffft"
+    description = "PFFFT, a pretty fast Fourier Transform."
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://bitbucket.org/jpommier/pffft/src/master/"
+    topics = ("fft", "pffft")
+    license = "BSD-like (FFTPACK license)"
+    generators = "cmake"
+    settings = "os", "compiler", "arch", "build_type"
+    exports_sources = ["CMakeLists.txt"]
+    options = {"disable_simd": [True, False], "fPIC": [True, False]}
+    default_options = {"disable_simd": False, "fPIC": True}
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        url = self.conan_data["sources"][self.version]["url"]
+        archive_name = os.path.basename(url)
+        archive_name = "jpommier-pffft-" + os.path.splitext(archive_name)[0]
+        os.rename(archive_name, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["DISABLE_SIMD"] = self.options.disable_simd
+        self._cmake.configure()
+        return self._cmake
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        del self.settings.compiler.cppstd
+        del self.settings.compiler.libcxx
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package_info(self):
+        self.cpp_info.libs.extend(["pffft"])
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs.append("m")
+
+    def package(self):
+        header = tools.load(os.path.join(self._source_subfolder, "pffft.h"))
+        license_content = header[: header.find("*/", 1)]
+        tools.save(
+            os.path.join(self.package_folder, "licenses", "LICENSE"), license_content
+        )
+        cmake = self._configure_cmake()
+        cmake.install()
