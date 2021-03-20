60ed774c3c353e788e317f98a9eb181047f421a4
Tobias Ribizel <ribizel@kit.edu>
2021-03-20 17:20:04 +0100

    [ginkgo] remove duplicate function from conanfile

--- recipes/ginkgo/all/conanfile.py
+++ recipes/ginkgo/all/conanfile.py
@@ -42,6 +42,8 @@ class GinkgoConan(ConanFile):
         }
 
     def configure(self):
+        if (self.settings.os == "Windows") or self.options.shared:
+            del self.options.fPIC
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(
@@ -53,14 +55,6 @@ class GinkgoConan(ConanFile):
             if tools.Version(self.settings.compiler.version) < min_version:
                 raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
                     self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            del self.options.fPIC
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
