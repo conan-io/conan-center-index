9038eeda40a3b75af844e4305524230c034248c8
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2021-03-21 16:01:28 +0300

    (#4958) [open62541] update dependencies, remove openssl fix
    * [open62541] update dependencies, remove openssl fix
    
    * Try to fix new clang

--- recipes/open62541/all/conanfile.py
+++ recipes/open62541/all/conanfile.py
@@ -90,10 +90,10 @@ class Open62541Conan(ConanFile):
             elif self.options.encryption == "mbedtls-gpl":
                 self.requires("mbedtls/2.16.3-gpl")
             elif self.options.encryption == "openssl":
-                self.requires("openssl/1.1.1g")
+                self.requires("openssl/1.1.1j")
 
             if self.options.web_socket:
-                self.requires("libwebsockets/4.0.15")
+                self.requires("libwebsockets/4.1.6")
         else:
             if self.options.encryption == "mbedtls-apache":
                 self.requires("mbedtls/2.16.3-apache")
@@ -135,9 +135,11 @@ class Open62541Conan(ConanFile):
                 raise ConanInvalidConfiguration(
                     "Lower Open62541 versions than 1.1.0 are not cpp compatible due to -fpermisive flags")
 
-            if self.settings.compiler == "clang" and tools.Version(self.settings.compiler.version) >= "9":
-                raise ConanInvalidConfiguration(
-                    "Open62541 does not support Clang 9.x.x and later compiler versions")
+        # FIXME: correct clang versions condition
+        max_clang_version = "8" if tools.Version(self.version) < "1.1.0" else "9"
+        if self.settings.compiler == "clang" and tools.Version(self.settings.compiler.version) > max_clang_version:
+            raise ConanInvalidConfiguration(
+                "Open62541 supports Clang up to {} compiler version".format(max_clang_version))
 
         if self.settings.compiler == "clang":
             if tools.Version(self.settings.compiler.version) < "5":
