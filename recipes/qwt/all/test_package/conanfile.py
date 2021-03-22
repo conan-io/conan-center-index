fb230498f740f88dd152fde9f90f121e7cca0dae
Walid Boussaffa <boussaffa.walid@outlook.com>
2021-03-22 06:35:42 +0100

    del self.options.fPIC for test package

--- recipes/qwt/all/test_package/conanfile.py
+++ recipes/qwt/all/test_package/conanfile.py
@@ -1,9 +1,14 @@
 from conans import ConanFile, CMake, tools
 import os
 
+
 class QwtTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
 
     def build(self):
         cmake = CMake(self)
