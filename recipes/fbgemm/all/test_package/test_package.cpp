64f8fda2ef13fb73bf2e86a3bbc30c7131f1c676
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-23 22:18:24 +0100

    add test_package

--- recipes/fbgemm/all/test_package/test_package.cpp
+++ recipes/fbgemm/all/test_package/test_package.cpp
@@ -1,3 +1,15 @@
+#include <fbgemm/Fbgemm.h>
+
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
 int main() {
-    return 0;
+  std::vector<std::int8_t> bint8{8, 2, 92, -45, 2, -4};
+  fbgemm::PackBMatrix<std::int8_t> packedBN(fbgemm::matrix_op_t::NoTranspose, 2,
+                                            3, bint8.data(), 3);
+  std::cout << "Offset of the element which was at (1, 2): "
+            << packedBN.addr(1, 2) << std::endl;
+
+  return 0;
 }
