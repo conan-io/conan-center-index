e0473392095b5c1ac2488a0110321a7664ff297a
rilerez <32276427+rilerez@users.noreply.github.com>
2021-03-23 22:32:15 -0400

    Update recipes/hedley/all/test_package/conanfile.py
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/hedley/all/test_package/conanfile.py
+++ recipes/hedley/all/test_package/conanfile.py
@@ -1,20 +1,17 @@
 import os
-
 from conans import ConanFile, CMake, tools
 
 
-class HedleyTestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "example")
+            self.run(bin_path, run_environment=True)
