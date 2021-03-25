446aae71de697b687eca902dd26f18d8b67bc579
Lukasz Laszko <lukaszlaszko@gmail.com>
2021-03-25 12:10:32 +0800

    Add Aeron 1.31.2

---
+++ recipes/aeron/all/conanfile.py
@@ -0,0 +1,138 @@
+import os
+import shutil
+import re
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+
+
+class AeronConan(ConanFile):
+    name = "aeron"
+    description = "Efficient reliable UDP unicast, UDP multicast, and IPC message transport"
+    topics = ("conan", "aeron", "udp", "messaging", "low-latency")
+    url = "https://github.com/real-logic/aeron"
+    homepage = "https://github.com/real-logic/aeron/wiki"
+    license = "Apache License 2.0"
+    exports_sources = "CMakeLists.txt",
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "build_aeron_driver": [True, False],
+        "build_tests": [True, False],
+        "build_samples": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "build_aeron_driver": True,
+        "build_tests": False,
+        "build_samples": False
+    }
+    generators = "cmake",
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+        if self.settings.os == "Windows":
+            self.requires.add("pthreads4w/3.0.0")
+        if self.settings.compiler == "Visual Studio" and self.settings.arch != "x86_64":
+            # https://github.com/real-logic/aeron#c-build
+            raise ConanInvalidConfiguration("{} currently only supports 64-bit builds on Windows".format(self.name))
+
+    def build_requirements(self):
+        self.build_requires("zulu-openjdk/11.0.8")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+
+        cmake = CMake(self)
+        cmake.definitions["AERON_INSTALL_TARGETS"] = True
+        cmake.definitions["BUILD_AERON_DRIVER"] = self.options.build_aeron_driver
+        cmake.definitions["AERON_TESTS"] = self.options.build_tests
+        cmake.definitions["AERON_BUILD_SAMPLES"] = self.options.build_samples
+        cmake.definitions["BUILD_AERON_ARCHIVE_API"] = True
+        cmake.definitions["AERON_ENABLE_NONSTANDARD_OPTIMIZATIONS"] = True
+
+        if self.settings.os != 'Windows':
+            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+        if self.options.build_tests:
+            self.output.info("Running {} tests".format(self.name))
+            source_path = os.path.join(self._build_subfolder, self._source_subfolder)
+            with tools.chdir(source_path):
+                self.run("ctest")
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+        # Files install straight to './include', but we want './include/aeron'
+        old_include_folder = os.path.join(self.package_folder, "include")
+        new_include_folder = os.path.join(self.package_folder, "include", self.name)
+        tools.rmdir(new_include_folder)
+        tools.mkdir(new_include_folder)
+
+        files = os.listdir(old_include_folder)
+        with tools.chdir(self.package_folder):
+            for f in files:
+                shutil.move(os.path.join(old_include_folder, f), new_include_folder)
+
+        # additional resources
+        archive_resources_dir = os.path.join(self._source_subfolder, "aeron-archive", "src", "main", "resources")
+        self.copy("*", dst="resources", src=archive_resources_dir)
+
+        # archive client headers
+        archive_include_dir = os.path.join(self._source_subfolder, "aeron-archive", "src", "main", "cpp", "client")
+        self.copy("*.h", dst="include/aeron-archive", src=archive_include_dir)
+
+        libs_folder = os.path.join(self.package_folder, "lib")
+        for root, dirs, files in os.walk(libs_folder, topdown=True):
+            for file in files:
+                _, ext = os.path.splitext(file)
+                if ext == '.so' and not self.options.shared:
+                    os.remove(os.path.join(root, file))
+                elif ext == '.a' and self.options.shared:
+                    os.remove(os.path.join(root, file))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs.append("include/{}".format(self.name))
+
+        self.output.info("{} requires C++11. Enforcing for downstream targets...".format(self.name))
+        self.cpp_info.cppflags.append("-std=c++11")
+
+        # See: https://github.com/real-logic/aeron/blob/23f9ef8c6bd25955c3a64454f4e5d9c4a86c8d5a/CMakeLists.txt#L213
+        self.cpp_info.defines.append("_ENABLE_EXTENDED_ALIGNED_STORAGE")
+
+        resources_folder = os.path.join(self.package_folder, "resources")
+        self.user_info.RESOURCES_DIR = resources_folder
+        self.user_info.VERSION = self.version
---
+++ recipes/aeron/all/test_package/test_package.cpp
@@ -0,0 +1,11 @@
+#include <aeron/Aeron.h>
+
+#include <cstdlib>
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Aeron: " << ::aeron::Aeron::version() << std::endl;
+    return EXIT_SUCCESS;
+}
