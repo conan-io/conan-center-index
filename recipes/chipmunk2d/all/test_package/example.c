231edb16d66babb6c80555e2dd124a1c522688d5
czoido <mrgalleta@gmail.com>
2021-03-26 16:56:56 +0100

    fix indent

--- recipes/chipmunk2d/all/test_package/example.c
+++ recipes/chipmunk2d/all/test_package/example.c
@@ -1,9 +1,9 @@
 #include "chipmunk/chipmunk.h"
 
-int main() 
+int main()
 {
-	cpSpace *space = cpSpaceNew();
-	cpSpaceSetGravity(space, cpv(0, -600));
+    cpSpace *space = cpSpaceNew();
+    cpSpaceSetGravity(space, cpv(0, -600));
     cpSpaceFree(space);
     return 0;
 }
