40eb661242f6222b12670f7a8727d7da61b33f6a
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-03-27 04:00:09 +0100

    mpdecimal: print Makefile for debug purposes

--- recipes/mpdecimal/2.5.x/conanfile.py
+++ recipes/mpdecimal/2.5.x/conanfile.py
@@ -143,6 +143,7 @@ class MpdecimalConan(ConanFile):
             with tools.chdir(self._source_subfolder):
                 self.run("autoreconf -fiv", win_bash=tools.os_info.is_windows)
                 autotools = self._configure_autotools()
+                self.output.info(tools.load(os.path.join("libmpdec", "Makefile")))
                 libmpdec, libmpdecpp = self._target_names
                 with tools.chdir("libmpdec"):
                     autotools.make(target=libmpdec)
