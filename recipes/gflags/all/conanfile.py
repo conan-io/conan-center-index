3162adc7f702d431f36e7417b8421cd10fc38323
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-30 15:33:45 +0200

    cache cmake

--- recipes/gflags/all/conanfile.py
+++ recipes/gflags/all/conanfile.py
@@ -9,8 +9,7 @@ class GflagsConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/gflags/gflags"
     license = "BSD-3-Clause"
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
@@ -24,6 +23,10 @@ class GflagsConan(ConanFile):
         "nothreads": True,
         "namespace": "gflags",
     }
+
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    _cmake = None
 
     @property
     def _source_subfolder(self):
@@ -47,21 +50,23 @@ class GflagsConan(ConanFile):
         os.rename("%s-%s" % (self.name, self.version), self._source_subfolder)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        cmake.definitions["BUILD_STATIC_LIBS"] = not self.options.shared
-        cmake.definitions["BUILD_gflags_LIB"] = not self.options.nothreads
-        cmake.definitions["BUILD_gflags_nothreads_LIB"] = self.options.nothreads
-        cmake.definitions["BUILD_PACKAGING"] = False
-        cmake.definitions["BUILD_TESTING"] = False
-        cmake.definitions["INSTALL_HEADERS"] = True
-        cmake.definitions["INSTALL_SHARED_LIBS"] = self.options.shared
-        cmake.definitions["INSTALL_STATIC_LIBS"] = not self.options.shared
-        cmake.definitions["REGISTER_BUILD_DIR"] = False
-        cmake.definitions["REGISTER_INSTALL_PREFIX"] = False
-        cmake.definitions["GFLAGS_NAMESPACE"] = self.options.namespace
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        self._cmake.definitions["BUILD_STATIC_LIBS"] = not self.options.shared
+        self._cmake.definitions["BUILD_gflags_LIB"] = not self.options.nothreads
+        self._cmake.definitions["BUILD_gflags_nothreads_LIB"] = self.options.nothreads
+        self._cmake.definitions["BUILD_PACKAGING"] = False
+        self._cmake.definitions["BUILD_TESTING"] = False
+        self._cmake.definitions["INSTALL_HEADERS"] = True
+        self._cmake.definitions["INSTALL_SHARED_LIBS"] = self.options.shared
+        self._cmake.definitions["INSTALL_STATIC_LIBS"] = not self.options.shared
+        self._cmake.definitions["REGISTER_BUILD_DIR"] = False
+        self._cmake.definitions["REGISTER_INSTALL_PREFIX"] = False
+        self._cmake.definitions["GFLAGS_NAMESPACE"] = self.options.namespace
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
 
     def build(self):
         cmake = self._configure_cmake()
