7569c628a21e0271803d169ada524f3f8c60624f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-30 15:40:06 +0200

    also provide non namespace imported target
    official config files provides both gflags and gflags::gflags

--- recipes/gflags/all/conanfile.py
+++ recipes/gflags/all/conanfile.py
@@ -1,5 +1,8 @@
 from conans import ConanFile, CMake, tools
 import os
+import textwrap
+
+required_conan_version = ">=1.33.0"
 
 
 class GflagsConan(ConanFile):
@@ -78,8 +81,39 @@ class GflagsConan(ConanFile):
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"gflags": "gflags::gflags"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "gflags"
+        self.cpp_info.names["cmake_find_package_multi"] = "gflags"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
+        self.cpp_info.names["pkg_config"] = "gflags"
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Windows":
             self.cpp_info.system_libs.extend(["shlwapi"])
