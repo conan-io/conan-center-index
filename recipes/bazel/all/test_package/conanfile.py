1353dd81d3a88485edc780bf6993d7869e5c4c72
Uilian Ries <uilianries@gmail.com>
2021-03-30 20:27:34 -0300

    Windows support for Bazel
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/bazel/all/test_package/conanfile.py
+++ recipes/bazel/all/test_package/conanfile.py
@@ -8,11 +8,11 @@ class TestPackage(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
     def build(self):
-        slashes = "///" if self.settings.os == "Windows" else "//"
-        self.run("bazel build {}main:hello-world".format(slashes,), run_environment=True)
+        self.run("bazel build //main:hello-world", run_environment=True)
         for directory in glob.glob(os.path.join(self.source_folder, "bazel-*")):
             shutil.move(directory, self.build_folder)
 
     def test(self):
         self.run("bazel --version", run_environment=True)
-        self.run("bazel-bin/main/hello-world", run_environment=True)
+        bin_path = os.path.join("bazel-bin", "main", "hello-world")
+        self.run(bin_path, run_environment=True)
