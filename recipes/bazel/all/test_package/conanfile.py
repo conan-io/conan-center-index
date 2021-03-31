66597c9ffe8f1b5f8288932508069650aa8af474
Uilian Ries <uilianries@gmail.com>
2021-03-31 17:06:38 -0300

    Fix Windows build for bazel
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/bazel/all/test_package/conanfile.py
+++ recipes/bazel/all/test_package/conanfile.py
@@ -1,15 +1,23 @@
 from conans import ConanFile, tools
 import os
+import shutil
+import glob
 
 
 class TestPackage(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
     def build(self):
-        with tools.chdir(self.build_folder):
-            open("WORKSPACE", "w")
-            self.run("bazel --output_user_root {} build -s --package_path {} //main:hello-world"
-                     .format(self.build_folder, self.source_folder), run_environment=True)
+        if self.settings.os == "Windows":
+            open(os.path.join(self.source_folder, "WORKSPACE"), "w")
+            self.run("bazel build //main:hello-world", run_environment=True)
+            for directory in glob.glob(os.path.join(self.source_folder, "bazel-*")):
+                shutil.move(directory, self.build_folder)
+        else:
+            with tools.chdir(self.build_folder):
+                open("WORKSPACE", "w")
+                self.run("bazel --output_user_root {} build --package_path {} //main:hello-world"
+                         .format(self.build_folder, self.source_folder), run_environment=True)
 
     def test(self):
         self.run("bazel --version", run_environment=True)
