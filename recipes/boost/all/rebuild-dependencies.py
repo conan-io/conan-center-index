89c0a21483c5f2d016c37509ce88956c4b39b7ef
Alexande B <abobrikovich@gmail.com>
2021-04-05 14:11:40 +0300

    Revert "[boost] Add 'algorithm' lib #3961"
    This reverts commit 5f1419d12a2ff530682826b334c123cddbd7c67c.

--- recipes/boost/all/rebuild-dependencies.py
+++ recipes/boost/all/rebuild-dependencies.py
@@ -25,7 +25,6 @@ BOOST_GIT_URL = "https://github.com/boostorg/boost.git"
 # When adding (or removing) an option, also add this option to the list in
 # `conanfile.py` and re-run this script.
 CONFIGURE_OPTIONS = (
-    "algorithm",
     "atomic",
     "chrono",
     "container",
