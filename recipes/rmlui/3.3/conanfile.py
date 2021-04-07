35413853a875a885d141aefc98961958ce0f2e1d
ZombieRaccoon <ZombieRaccoon@users.noreply.github.com>
2021-04-07 18:45:27 +0200

    Check compiler-eligibility the right way

--- recipes/rmlui/3.3/conanfile.py
+++ recipes/rmlui/3.3/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 import os
 
 
@@ -31,6 +32,18 @@ class RmluiConan(ConanFile):
     generators = ["cmake", "cmake_find_package"]
 
     @property
+    def _minimum_compilers_version(self):
+        # Reference: https://en.cppreference.com/w/cpp/compiler_support/14
+        return {
+            "apple-clang": "5.1",
+            "clang": "3.4",
+            "gcc": "5",
+            "intel": "17",
+            "sun-cc": "5.15",
+            "Visual Studio": "15"
+        }
+
+    @property
     def _minimum_cpp_standard(self):
         return 14
 
@@ -39,10 +52,20 @@ class RmluiConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
-        tools.check_min_cppstd(self, self._minimum_cpp_standard)
-
         if self.options.shared:
             del self.options.fPIC
+
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(
+            str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
 
     def requirements(self):
         if self.options.font_interface == "freetype":
