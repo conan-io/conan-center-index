ab3e8ab18fd7fc26fee2608fba2b2b30e785334c
SSE4 <tomskside@gmail.com>
2021-04-12 21:09:02 +0700

    Update recipes/kuba-zip/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/kuba-zip/all/conanfile.py
+++ recipes/kuba-zip/all/conanfile.py
@@ -55,4 +55,6 @@ class ZipConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "zip"
+        self.cpp_info.names["cmake_find_package_multi"] = "zip"
         self.cpp_info.libs = ["zip"]
