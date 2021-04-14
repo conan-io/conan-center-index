ddc27812d47f703f35eb079bce44a430fdedefd2
Canmor Lam <canmor.lam@gmail.com>
2021-04-14 10:56:34 +0800

    [hidapi] fix `pc` file hook check

--- recipes/hidapi/all/conanfile.py
+++ recipes/hidapi/all/conanfile.py
@@ -90,7 +90,7 @@ class HidapiConan(ConanFile):
         else:
             autotools = self._configure_autotools()
             autotools.install()
-            # tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+            tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
             tools.rmdir(os.path.join(self.package_folder, "share"))
             tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
 
