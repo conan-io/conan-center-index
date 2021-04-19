a56de536cca9eb66cf8c181ce2ed54c89bb524fc
tkrupa <tomas@krupa.hu>
2021-04-19 21:42:07 +0200

    fixed PR issues
    - libname renamed to _libname
    - removed cmake
    
    Signed-off-by: tkrupa <tomas@krupa.hu>

--- recipes/hippomocks/all/conanfile.py
+++ recipes/hippomocks/all/conanfile.py
@@ -1,16 +1,15 @@
-from conans import CMake, ConanFile, ConanFile, tools
+from conans import ConanFile, ConanFile, tools
 import os, glob
 
 class HippomocksConan(ConanFile):
     name = 'hippomocks'
-    libname = 'HippoMocks'
+    _libname = 'HippoMocks'
     description = 'Single-header mocking framework.'
     topics = ("conan", "hippomocks", "mock", "framework")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = 'https://github.com/dascandy/hippomocks.git'
     license = 'LGPL-2.1'
     no_copy_source = True
-    _cmake = None
 
     @property
     def _source_subfolder(self):
@@ -21,20 +20,9 @@ class HippomocksConan(ConanFile):
         extracted_dir = glob.glob("%s-*" % (self.name))[0]
         os.rename(extracted_dir, self._source_subfolder)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.configure(source_folder=self._source_subfolder)
-        return self._cmake
-
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
-
     def package(self):
         self.copy('LICENSE', dst='licenses', src=self._source_subfolder)
-        self.copy('*.h', dst=os.path.join('include', self.name), src=os.path.join(self._source_subfolder, self.libname))
+        self.copy('*.h', dst=os.path.join('include', self.name), src=os.path.join(self._source_subfolder, self._libname))
 
     def package_id(self):
         self.info.header_only()
