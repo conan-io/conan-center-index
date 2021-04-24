6b986d9760b13f90587bb53e1d0065da503ab132
Tomáš Krupa <69798455+tomas-krupa@users.noreply.github.com>
2021-04-24 11:11:26 +0200

    Update recipes/cajun-jsonapi/all/conanfile.py
    Co-authored-by: theirix <theirix@gmail.com>

--- recipes/cajun-jsonapi/all/conanfile.py
+++ recipes/cajun-jsonapi/all/conanfile.py
@@ -4,7 +4,7 @@ import os, glob
 class CajunJsonApiConan(ConanFile):
     name = 'cajun-jsonapi'
     description = 'CAJUN* is a C++ API for the JSON object interchange format.'
-    topics = ("conan", "CAJUN", "JSON")
+    topics = ("conan", "cajun", "json")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/cajun-jsonapi/cajun-jsonapi"
     license = "BSD-3-Clause"
