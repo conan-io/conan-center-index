8d6caa2126b584eeaa84cd439cb6e11a55e14045
theirix <theirix@gmail.com>
2021-04-24 20:11:28 +0300

    Enable shared builds for test_package

--- recipes/libjpeg-turbo/all/test_package/conanfile.py
+++ recipes/libjpeg-turbo/all/test_package/conanfile.py
@@ -8,6 +8,7 @@ class TestPackageConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["USE_SHARED"] = self.options["libjpeg-turbo"].shared
         cmake.configure()
         cmake.build()
 
