e2824979ed506c6d125ff2c182c4902c6005412a
Eric Lemanissier <eric.lemanissier@gmail.com>
2021-04-26 12:42:26 +0200

    bump msys

--- recipes/pkgconf/all/test_package/conanfile.py
+++ recipes/pkgconf/all/test_package/conanfile.py
@@ -12,7 +12,7 @@ class TestPackageConan(ConanFile):
         self.build_requires("automake/1.16.3")
         if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH") \
                 and tools.os_info.detect_windows_subsystem() != "msys2":
-            self.build_requires("msys2/20190524")
+            self.build_requires("msys2/20210105")
 
     def build(self):
         # Test pkg.m4 integration into automake
