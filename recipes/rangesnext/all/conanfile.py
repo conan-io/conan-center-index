1ebc119899e31b7f9f4f07887d481d8ef118cc6c
César García Tapia <cesar.garcia.tapia@gmail.com>
2021-04-27 09:56:47 +0200

    Add rangesnext/cci.20210426

---
+++ recipes/rangesnext/all/conanfile.py
@@ -0,0 +1,53 @@
+from conans import CMake, ConanFile, tools
+import os
+import glob
+
+
+class RangesnextConan(ConanFile):
+    name = "rangesnext"
+    description = "ranges features for C++23 ported to C++20"
+    topics = ("conan", "rangesnext", "ranges", "backport", "backport-cpp")
+    url = "https://github.com/cor3ntin/rangesnext"
+    homepage = "https://github.com/cor3ntin/rangesnext"
+    license = "BSL-1.0"
+    exports_sources = "patches/**"
+    settings = "compiler"
+
+    _compilers_minimum_version = {
+        "gcc": "10",
+        "Visual Studio": "19",
+        "clang": "13"
+    }
+    _source_subfolder = "source_subfolder"
+
+    def configure(self):
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, "20")
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version:
+            if tools.Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration("rangesnext requires C++20, which your compiler does not fully support.")
+        else:
+            self.output.warn("rangesnext requires C++20. Your compiler is unknown. Assuming it supports C++20.")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = glob.glob(self.name + "-*/")[0]
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def build(self):
+        self._patch_sources()
+
+        cmake = CMake(self)
+        cmake.configure(source_folder=self._source_subfolder)
+        cmake.build()
+
+    def package(self):
+        include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy(pattern="LICENSE.md", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*", dst="include", src=include_folder)
+
+    def _patch_sources(self):
+        for patchdata in self.conan_data["patches"][self.version]:
+            tools.patch(**patchdata)
