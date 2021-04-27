b053a3c51d04d5bfc587cbaf82f6c5f61166d841
ZombieRaccoon <14248312+ZombieRaccoon@users.noreply.github.com>
2021-04-27 18:32:23 +0200

    Update recipes/earcut/all/test_package/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/earcut/all/test_package/conanfile.py
+++ recipes/earcut/all/test_package/conanfile.py
@@ -7,15 +7,12 @@ class EarcutTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
-    def _configure_cmake(self):
-        if not hasattr(self, "_cmake"):
-            self._cmake = CMake(self)
-            self._cmake.configure()
-        
-        return self._cmake
-
     def build(self):
-        self._configure_cmake().build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
-        self._configure_cmake().test()
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "example")
+            self.run(bin_path, run_environment=True)
