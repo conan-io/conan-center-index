734e1d9ee5e9716962027b3663bf4175db287235
Claudio Bantaloukas <rockdreamer@gmail.com>
2021-04-27 15:01:43 +0100

    (#5274) fontconfig/2.13.93: fix build on macos with dynamically linked dependencies
    * Unconditionally set RUN_FC_CACHE_TEST=false
    
    disables running fc-cache test during make install that fails due to libraries not being found.
    
    * Move replace_in_file into _patch_files method

--- recipes/fontconfig/all/conanfile.py
+++ recipes/fontconfig/all/conanfile.py
@@ -72,6 +72,12 @@ class FontconfigConan(ConanFile):
     def _patch_files(self):
         #  - fontconfig requires libtool version number, change it for the corresponding freetype one
         tools.replace_in_file(os.path.join(self._source_subfolder, 'configure'), '21.0.15', '2.8.1')
+        # disable fc-cache test to enable cross compilation but also builds with shared libraries on MacOS
+        tools.replace_in_file(
+            os.path.join(self._source_subfolder, 'Makefile.in'),
+            '@CROSS_COMPILING_TRUE@RUN_FC_CACHE_TEST = false',
+            'RUN_FC_CACHE_TEST=false'
+        )
 
     def build(self):
         # Patch files from dependencies
