34e72383816780072f9b9b7360a6d4ddf6ebd24b
Riley Levy <rileylevy95@gmail.com>
2021-04-28 14:30:29 -0400

    Add boilerplate for minimum versions

--- recipes/refl-cpp/all/conanfile.py
+++ recipes/refl-cpp/all/conanfile.py
@@ -11,6 +11,11 @@ class ReflCppConan(ConanFile):
     description = "A modern compile-time reflection library for C++ with support for overloads, templates, attributes and proxies "
     topics = ("header", "header-only", "reflection", "modern", "metaprogramming")
     no_copy_source = True
+    settings = "os", "compiler", "build_type", "arch"
+
+    @property
+    def _minimum_cpp_standard(self):
+        return 17
 
     @property
     def _source_subfolder(self):
@@ -28,3 +33,24 @@ class ReflCppConan(ConanFile):
 
     def package_id(self):
         self.info.header_only()
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "15",
+            "gcc": "5",
+            "clang": "3.4",
+            "apple-clang": "5.1",
+        }
+
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
