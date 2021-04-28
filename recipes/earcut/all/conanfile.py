22bd5554a47975541250bafb30c50c435bd47f35
ZombieRaccoon <14248312+ZombieRaccoon@users.noreply.github.com>
2021-04-28 18:22:42 +0200

    Update recipes/earcut/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/earcut/all/conanfile.py
+++ recipes/earcut/all/conanfile.py
@@ -49,7 +49,7 @@ class EarcutConan(ConanFile):
             self.output.warn("{} recipe lacks information about the {} compiler support.".format(
                 self.name, self.settings.compiler))
         else:
-            if tools.Version(self.settings.compiler.version) < min_version:
+            if lazy_lt_semver(str(self.settings.compiler.version), min_version):
                 raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
                     self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
 
