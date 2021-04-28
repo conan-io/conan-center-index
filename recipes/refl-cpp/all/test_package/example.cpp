25c8c922aef3f37f31d024b0bed3a68eb5e2d77d
Riley Levy <rileylevy95@gmail.com>
2021-04-28 14:54:01 -0400

    Add reflcpp readme's example to test's example.cpp

--- recipes/refl-cpp/all/test_package/example.cpp
+++ recipes/refl-cpp/all/test_package/example.cpp
@@ -1,5 +1,67 @@
 #include "refl.hpp"
 
+// The example from the refl-cpp github readme:
+
+struct NonNegative : refl::attr::usage::member {};
+
+struct Point {
+    double x, y;
+};
+
+REFL_AUTO(
+    type(Point),
+    field(x, NonNegative()),
+    field(y, NonNegative())
+)
+
+struct Circle {
+    Point origin;
+    double radius;
+};
+
+REFL_AUTO(
+    type(Circle),
+    field(origin),
+    field(radius, NonNegative())
+)
+
+template <typename T>
+constexpr bool checkNonNegative(const T& obj) {
+    // Get the type descriptor for T
+    constexpr auto type = refl::reflect<T>();
+    // Get the compile-time refl::type_list<...> of member descriptors
+    constexpr auto members = get_members(type);
+    // Filter out the non-readable members (not field or getters marked with the property() attribute)
+    constexpr auto readableMembers = filter(members, [](auto member) { return is_readable(member); });
+
+    auto invalidMemberCount = count_if(readableMembers, [&](auto member) {
+        // Function-call syntax is a uniform way to get the value of a member (whether a field or a getter)
+        auto&& value = member(obj);
+        // Check if the NonNegative attribute is present
+        if constexpr (refl::descriptor::has_attribute<NonNegative>(member)) {
+            // And if so, make the necessary checks
+            return value < 0;
+        }
+        // Recursively check the value of the member
+        else if (!checkNonNegative(value)) {
+            return true;
+        }
+        return false;
+    });
+
+    return invalidMemberCount == 0;
+}
+
+// These all run at compile-time
+constexpr Circle c1{ {0., 5.}, 100. };
+static_assert(checkNonNegative(c1));
+
+constexpr Circle c2{ {0., 5.}, -100. };
+static_assert(!checkNonNegative(c2));
+
+constexpr Circle c3{ {0., -5.}, 100. };
+static_assert(!checkNonNegative(c3));
+
 int main() {
     return 0;
 }
