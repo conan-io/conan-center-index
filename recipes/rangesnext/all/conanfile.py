c293a9cfcff3a4ee970f0bc1a3019c5d2e5f3baf
César García Tapia <cesargt@jfrog.com>
2021-04-28 10:06:24 +0200

    rearrangement of imports
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/rangesnext/all/conanfile.py
+++ recipes/rangesnext/all/conanfile.py
@@ -1,7 +1,6 @@
-from conans import CMake, ConanFile, tools
+from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 import os
-import glob
 
 
 class RangesnextConan(ConanFile):
