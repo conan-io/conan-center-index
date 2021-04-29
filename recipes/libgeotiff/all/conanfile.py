9ee824e332ff21ef832345243f58cc0c34245fc0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-04-29 17:54:05 +0200

    use destination & strip_root in source()

--- recipes/libgeotiff/all/conanfile.py
+++ recipes/libgeotiff/all/conanfile.py
@@ -44,8 +44,8 @@ class LibgeotiffConan(ConanFile):
         self.requires("proj/8.0.0")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename(self.name + "-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def build(self):
         for patch in self.conan_data["patches"][self.version]:
