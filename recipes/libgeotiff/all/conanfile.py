66239dc56bcf536a88e6d70a43752bc7b9f332b3
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-04-29 17:53:02 +0200

    bump proj

--- recipes/libgeotiff/all/conanfile.py
+++ recipes/libgeotiff/all/conanfile.py
@@ -41,7 +41,7 @@ class LibgeotiffConan(ConanFile):
 
     def requirements(self):
         self.requires("libtiff/4.2.0")
-        self.requires("proj/7.2.1")
+        self.requires("proj/8.0.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
