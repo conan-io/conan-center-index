aa2127c58fa166dd4bc5a8185f716506cd26848f
Mateusz Pusz <mateusz.pusz@gmail.com>
2021-04-30 20:22:05 +0200

    Minimum C++ version checking added

--- recipes/mdspan/all/conanfile.py
+++ recipes/mdspan/all/conanfile.py
@@ -1,4 +1,6 @@
 from conans import ConanFile, tools
+from conans.tools import check_min_cppstd
+from conans.errors import ConanInvalidConfiguration
 import os
 
 
@@ -9,11 +11,43 @@ class MDSpanConan(ConanFile):
     topics = ("multi-dimensional", "array", "span")
     license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
+    settings = "compiler"
     no_copy_source = True
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    @property
+    def _minimum_cpp_standard(self):
+        return 11
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "14",
+            "gcc": "4.8",
+            "clang": "3.8",
+            "apple-clang": "8.0"
+        }
+
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(
+            str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} "
+                             "compiler support.".format(
+                                 self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration(
+                    "{} requires C++{} support. "
+                    "The current compiler {} {} does not support it.".format(
+                        self.name, self._minimum_cpp_standard,
+                        self.settings.compiler,
+                        self.settings.compiler.version))
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
