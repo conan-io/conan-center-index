059b800f947df2faf6a8bb35351068433c5d3178
César García Tapia <cesargt@jfrog.com>
2021-05-01 15:01:54 +0200

    (#5339) Add rangesnext/cci.20210426
    * Add rangesnext/cci.20210426
    
    * Code formatting
    
    * enforce CCI rules
    
    * Add missing import
    
    * Change in configure(): if we're not sure a compiler supports C++20, assume it doesn't
    
    * Minor refactor
    
    * Change tarball url on conandata.yml from .zip to .tgz
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Rename outdated method configure() to validate()
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Small code simplification
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * rearrangement of imports
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove the build() method. Not needed with a header-only library
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * This is a header-only library, so building is not necessary
    
    * Optimize the test_package/CMakeLists.txt
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

---
+++ recipes/rangesnext/all/test_package/test_package.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cor3ntin/rangesnext/enumerate.hpp>
+
+namespace rangesnext = cor3ntin::rangesnext;
+
+template <class RangeT>
+bool test_enumerate_with(RangeT &&range) {
+    auto enumerated_range = rangesnext::enumerate(range);
+
+    std::size_t idx_ref = 0;
+    auto it_ref = std::ranges::begin(range);
+
+    bool success = true;
+    for (auto &&[i, v] : enumerated_range) {
+    	std::cout << i << " - " << v << "\n";
+
+        success = (i == idx_ref++) && (v == *it_ref++);
+        if (success == false) {
+        	return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int const test_array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    test_enumerate_with(test_array);
+}
