044fa1417b46658de2c32145efff941b1fa143a7
Anonymous Maarten <madebr@users.noreply.github.com>
2021-05-01 18:02:00 +0200

    (#5388) mpdecimal: autoreconf needs automake (due to aclocal)

--- recipes/mpdecimal/2.5.x/conanfile.py
+++ recipes/mpdecimal/2.5.x/conanfile.py
@@ -48,7 +48,7 @@ class MpdecimalConan(ConanFile):
 
     def build_requirements(self):
         if self.settings.compiler != "Visual Studio":
-            self.build_requires("autoconf/2.69")
+            self.build_requires("automake/1.16.3")
             if tools.os_info.is_windows:
                 self.build_requires("msys2/20200517")
 
