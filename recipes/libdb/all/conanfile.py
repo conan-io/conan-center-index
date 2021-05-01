075abdea9cc004ff1bdc1ad7052d8b7b8ac660d1
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-05-01 22:21:00 +0200

    libdb: no os.rename + remove unused import

--- recipes/libdb/all/conanfile.py
+++ recipes/libdb/all/conanfile.py
@@ -1,7 +1,6 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, MSBuild, tools
 from conans.errors import ConanInvalidConfiguration
 import os
-import shutil
 
 
 class LibdbConan(ConanFile):
@@ -66,8 +65,7 @@ class LibdbConan(ConanFile):
                 self.build_requires("msys2/20190524")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("db-{}".format(self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def _patch_sources(self):
         for patch_data in self.conan_data["patches"][self.version]:
