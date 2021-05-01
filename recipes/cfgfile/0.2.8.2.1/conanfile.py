e3a8630a7076f0aed653d889427bc71b09c7b59a
Igor Mironchik <igor.mironchik@gmail.com>
2021-05-01 20:10:35 +0300

    Make addition changes to recipe as this library is header-only + tool.

--- recipes/cfgfile/0.2.8.2.1/conanfile.py
+++ recipes/cfgfile/0.2.8.2.1/conanfile.py
@@ -9,14 +9,19 @@ class CfgfileConan(ConanFile):
     homepage = "https://github.com/igormironchik/cfgfile.git"
     license = "MIT"
     description = "Header-only library for reading/saving configuration files with schema defined in sources."
-    no_copy_source = True
     generators = "cmake"
     topics = ("conan", "cfgfile", "configuration")
-    settings = "compiler"
+    settings = "os", "arch", "compiler", "build_type"
+
+    _cmake = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     @property
     def _compilers_minimum_version(self):
@@ -26,6 +31,13 @@ class CfgfileConan(ConanFile):
             "clang": "3.5",
             "apple-clang": "10"
         }
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(source_folder=self._source_subfolder + "/generator", build_folder=self._build_subfolder)
+        return self._cmake
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
@@ -40,13 +52,15 @@ class CfgfileConan(ConanFile):
         if version < self._compilers_minimum_version[compiler]:
             raise ConanInvalidConfiguration("cfgfile requires a compiler that supports at least C++14")
 
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
     def package(self):
         self.copy("COPYING", src=self._source_subfolder, dst="licenses")
         self.copy("*.hpp", src=os.path.join(self._source_subfolder, "cfgfile"), dst=os.path.join("include", "cfgfile"))
-        cmake = CMake(self)
-        cmake.configure(source_folder="source_subfolder/generator")
-        cmake.build()
+        cmake = self._configure_cmake()
         cmake.install()
