03477142d4f0572188d33fb40eb36fe29ef4fc4b
Eric Riff <ericriff@gmail.com>
2021-05-03 17:31:11 -0300

    Run test_package only if not cross building

--- recipes/crashpad/all/test_package/conanfile.py
+++ recipes/crashpad/all/test_package/conanfile.py
@@ -12,9 +12,10 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        test_env_dir = "test_env"
-        tools.mkdir(test_env_dir)
-        bin_path = os.path.join("bin", "test_package")
-        handler_exe = "crashpad_handler.exe" if self.settings.os == "Windows" else "crashpad_handler"
-        handler_bin_path = os.path.join(self.deps_cpp_info['crashpad'].rootpath, "bin", handler_exe)
-        self.run("%s %s/db %s" % (bin_path, test_env_dir, handler_bin_path), run_environment=True)
+        if not tools.cross_building(self.settings):
+            test_env_dir = "test_env"
+            tools.mkdir(test_env_dir)
+            bin_path = os.path.join("bin", "test_package")
+            handler_exe = "crashpad_handler.exe" if self.settings.os == "Windows" else "crashpad_handler"
+            handler_bin_path = os.path.join(self.deps_cpp_info['crashpad'].rootpath, "bin", handler_exe)
+            self.run("%s %s/db %s" % (bin_path, test_env_dir, handler_bin_path), run_environment=True)
