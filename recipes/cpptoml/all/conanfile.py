88a381600b17206c5c42b5ee4840fa53dac65120
dotchris90 <christian.kahr@gmx.de>
2021-05-04 21:27:09 +0200

    Validated via pep8

--- recipes/cpptoml/all/conanfile.py
+++ recipes/cpptoml/all/conanfile.py
@@ -6,7 +6,7 @@ import glob
 class CppTomlConan(ConanFile):
     name = "cpptoml"
     description = "cpptoml is a header-only library for parsing TOML "
-    topics = ("conan","toml","cpptoml")
+    topics = ("conan", "toml", "cpptoml")
     license = " MIT License "
     homepage = "https://github.com/skystrife/cpptoml"
     url = "https://github.com/conan-io/conan-center-index"
@@ -23,29 +23,53 @@ class CppTomlConan(ConanFile):
     @property
     def _source_subfolder(self):
         return "source_subfolder"
-    
+
     @property
     def _build_subfolder(self):
         return "build_subfolder"
-        
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = glob.glob(self.name + "-*/")[0]
-        tools.replace_in_file("{0}/CMakeLists.txt".format(extracted_dir),
-            "list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/deps/meta-cmake)",
-            "")
-        os.rename(extracted_dir,self._source_subfolder)
-        
+        cmake_file = "{0}/CMakeLists.txt"\
+                     .format(extracted_dir)
+        search_pattern = "list(APPEND CMAKE_MODULE_PATH "\
+                         "${CMAKE_CURRENT_SOURCE_DIR}"\
+                         "/deps/meta-cmake)"
+        tools.replace_in_file(
+            cmake_file,
+            search_pattern,
+            ""
+        )
+        os.rename(extracted_dir, self._source_subfolder)
+
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["CPPTOML_BUILD_EXAMPLES"] = self.options.build_examples
-        cmake.configure(build_folder=self._build_subfolder,source_folder=self._source_subfolder)
+        cmake.definitions["CPPTOML_BUILD_EXAMPLES"] = \
+            self.options.build_examples
+
+        cmake.configure(
+            build_folder=self._build_subfolder,
+            source_folder=self._source_subfolder
+        )
+
         cmake.build()
 
     def package(self):
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="*.h", dst="include", src=include_folder)
+        include_folder = os.path.join(
+            self._source_subfolder,
+            "include"
+        )
+        self.copy(
+            pattern="LICENSE",
+            dst="licenses",
+            src=self._source_subfolder
+        )
+        self.copy(
+            pattern="*.h",
+            dst="include",
+            src=include_folder
+        )
 
     def package_id(self):
         self.info.header_only()
--- recipes/cpptoml/all/test_package/conanfile.py
+++ recipes/cpptoml/all/test_package/conanfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
 
@@ -15,10 +15,11 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        if self.settings.os == "Windows":
-            self.run(bin_path)
-        elif self.settings.os == "Macos":
-            self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
-        else:
-            self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+        with tools.environment_append(RunEnvironment(self).vars):
+            bin_path = os.path.join("bin", "test_package")
+            if self.settings.os == "Windows":
+                self.run(bin_path)
+            elif self.settings.os == "Macos":
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+            else:
+                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
