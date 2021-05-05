9ee3f57408174cf23dbad57b1c94267866645e8a
Uilian Ries <uilianries@gmail.com>
2021-05-05 19:02:54 -0300

    Improve cpptoml
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/cpptoml/all/conanfile.py
+++ recipes/cpptoml/all/conanfile.py
@@ -1,79 +1,28 @@
-from conans import ConanFile, tools, CMake
+from conans import ConanFile, tools
 import os
-import glob
 
 
 class CppTomlConan(ConanFile):
     name = "cpptoml"
     description = "cpptoml is a header-only library for parsing TOML "
-    topics = ("conan", "toml", "cpptoml")
-    license = " MIT License "
+    topics = ("toml", "header-only", "configuration")
+    license = "MIT"
     homepage = "https://github.com/skystrife/cpptoml"
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os", "arch", "compiler", "build_type"
-    options = {
-        "build_examples": [True, False]
-    }
-    default_options = {
-        "build_examples": False
-    }
-    generators = "cmake"
     no_copy_source = True
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = glob.glob(self.name + "-*/")[0]
-        cmake_file = "{0}/CMakeLists.txt"\
-                     .format(extracted_dir)
-        search_pattern = "list(APPEND CMAKE_MODULE_PATH "\
-                         "${CMAKE_CURRENT_SOURCE_DIR}"\
-                         "/deps/meta-cmake)"
-        tools.replace_in_file(
-            cmake_file,
-            search_pattern,
-            ""
-        )
-        os.rename(extracted_dir, self._source_subfolder)
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.definitions["CPPTOML_BUILD_EXAMPLES"] = \
-            self.options.build_examples
-
-        cmake.configure(
-            build_folder=self._build_subfolder,
-            source_folder=self._source_subfolder
-        )
-
-        cmake.build()
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True,
+                  destination=self._source_subfolder)
 
     def package(self):
-        include_folder = os.path.join(
-            self._source_subfolder,
-            "include"
-        )
-        self.copy(
-            pattern="LICENSE",
-            dst="licenses",
-            src=self._source_subfolder
-        )
-        self.copy(
-            pattern="*.h",
-            dst="include",
-            src=include_folder
-        )
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
 
     def package_id(self):
         self.info.header_only()
-
-    def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "cpptoml"
-        self.cpp_info.names["cmake_find_package_multi"] = "cpptoml"
--- recipes/cpptoml/all/test_package/conanfile.py
+++ recipes/cpptoml/all/test_package/conanfile.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from conans import ConanFile, CMake, tools
 import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -14,18 +11,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        is_at_least_cpp11 = None
-        try: 
-            tools.check_min_cppstd(self, "11")
-            is_at_least_cpp11 = True
-        except Exception as error :
-            is_at_least_cpp11 = False
-        bin_path = os.path.join("bin", "test_package")
-        if self.settings.os == "Windows" and is_at_least_cpp11:
-            self.run(bin_path)
-        elif self.settings.os == "Macos" and is_at_least_cpp11:
-            self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
-        elif is_at_least_cpp11:
-            self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
-        else:
-            pass 
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
