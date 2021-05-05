2f6a4d1149c78ba0b01075056a62acdbfb0e639f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-05 12:15:51 +0200

    robust loop in patches

--- recipes/fcl/all/conanfile.py
+++ recipes/fcl/all/conanfile.py
@@ -62,7 +62,7 @@ class FclConan(ConanFile):
                   destination=self._source_subfolder, strip_root=True)
 
     def build(self):
-        for patch in self.conan_data["patches"][self.version]:
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
