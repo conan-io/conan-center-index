6ed927ccc4de64871796bb4cb1bab2315e44e2f3
dotchris90 <christian.kahr@gmx.de>
2021-05-05 21:59:27 +0200

    just functional with cpp11 and higher

--- recipes/cpptoml/all/test_package/conanfile.py
+++ recipes/cpptoml/all/test_package/conanfile.py
@@ -14,10 +14,18 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
+        is_at_least_cpp11 = None
+        try: 
+            tools.check_min_cppstd(self, "11")
+            is_at_least_cpp11 = True
+        except Exception as error :
+            is_at_least_cpp11 = False
         bin_path = os.path.join("bin", "test_package")
-        if self.settings.os == "Windows":
+        if self.settings.os == "Windows" and is_at_least_cpp11:
             self.run(bin_path)
-        elif self.settings.os == "Macos":
+        elif self.settings.os == "Macos" and is_at_least_cpp11:
             self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
-        else:
+        elif is_at_least_cpp11:
             self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+        else:
+            pass 
