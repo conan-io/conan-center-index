c0ad60f6edf2e0148be0adfb1287cc13a20540e0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-05 13:23:43 +0200

    delete fPIC option if shared

--- recipes/libccd/all/conanfile.py
+++ recipes/libccd/all/conanfile.py
@@ -41,6 +41,8 @@ class LibccdConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
