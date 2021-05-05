f8e196847faa4fdc30c486c56586207ed1aa1033
Javier G. Sogo <jgsogo@gmail.com>
2021-05-05 15:06:46 +0200

    Update recipes/crc32c/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/crc32c/all/conanfile.py
+++ recipes/crc32c/all/conanfile.py
@@ -43,6 +43,7 @@ class crc32cConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
+    def validate(self):
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, 11)
 
