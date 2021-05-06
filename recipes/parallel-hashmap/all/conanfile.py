0ddf2eb4d77875fdd5be23746593699a6661db15
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-06 22:15:22 +0200

    fix CMake imported target

--- recipes/parallel-hashmap/all/conanfile.py
+++ recipes/parallel-hashmap/all/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, tools
 import os
+import textwrap
 
 required_conan_version = ">=1.33.0"
 
@@ -30,7 +31,35 @@ class ParallelHashmapConan(ConanFile):
                   dst=os.path.join("include", "parallel_hashmap"),
                   src=os.path.join(self._source_subfolder, "parallel_hashmap"))
         self.copy("phmap.natvis", dst="res", src=self._source_subfolder)
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"phmap": "phmap::phmap"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "phmap"
         self.cpp_info.names["cmake_find_package_multi"] = "phmap"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
