efa6eff9b869240fc673bd65152788769750ee27
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-05-08 08:23:52 +0200

    Fix test

--- recipes/pkgconf/all/test_package/conanfile.py
+++ recipes/pkgconf/all/test_package/conanfile.py
@@ -12,7 +12,7 @@ class TestPackageConan(ConanFile):
         self.build_requires("automake/1.16.3")
         if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH") \
                 and tools.os_info.detect_windows_subsystem() != "msys2":
-            self.build_requires("msys2/20210105")
+            self.build_requires("msys2/cci.latest")
 
     def build(self):
         # Test pkg.m4 integration into automake
