c50e3336d3ba89c2a20d91cbcfdc5026f8cdbd9b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-09 21:18:26 +0200

    don't run test_package if cross build

--- recipes/xtensor/all/test_package/conanfile.py
+++ recipes/xtensor/all/test_package/conanfile.py
@@ -13,4 +13,5 @@ class XtensorTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        self.run(os.path.join("bin", "test_package"), run_environment=True)
+        if not tools.cross_building(self.settings):
+            self.run(os.path.join("bin", "test_package"), run_environment=True)
