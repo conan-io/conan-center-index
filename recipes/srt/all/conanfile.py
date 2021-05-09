618bc71807254e771bfe08b3622ee341e94e4e54
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-09 23:42:09 +0200

    no os.rename

--- recipes/srt/all/conanfile.py
+++ recipes/srt/all/conanfile.py
@@ -1,6 +1,8 @@
 from conans import ConanFile, CMake, tools
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class SrtConan(ConanFile):
     name = "srt"
@@ -49,9 +51,8 @@ class SrtConan(ConanFile):
             self.requires("pthreads4w/3.0.0")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
