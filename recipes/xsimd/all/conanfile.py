81395ff0e29a2a21e2b2c959de36afa6524a0ef6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-09 22:06:27 +0200

    xsimd is header only

--- recipes/xsimd/all/conanfile.py
+++ recipes/xsimd/all/conanfile.py
@@ -19,6 +19,9 @@ class XsimdConan(ConanFile):
     def _source_subfolder(self):
         return os.path.join(self.source_folder, "source_subfolder")
 
+    def package_id(self):
+        self.info.header_only()
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
