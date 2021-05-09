f44a838c4d9143d10f8465e2f6737e8e077ff05d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-09 21:29:37 +0200

    fix CMake imported target
    official CMake imported target is not namespaced

--- recipes/xtensor/all/conanfile.py
+++ recipes/xtensor/all/conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 import os
+import textwrap
 
 required_conan_version = ">=1.33.0"
 
@@ -69,8 +70,39 @@ class XtensorConan(ConanFile):
         self.copy(
             "*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include")
         )
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"xtensor": "xtensor::xtensor"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "xtensor"
+        self.cpp_info.names["cmake_find_package_multi"] = "xtensor"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
+        self.cpp_info.names["pkg_config"] = "xtensor"
         if self.options.xsimd:
             self.cpp_info.defines.append("XTENSOR_USE_XSIMD")
         if self.options.tbb:
