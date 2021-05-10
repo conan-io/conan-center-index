e98284d513ca54d7ef11eb1d3f1c8f5460eee4e1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-10 16:32:23 +0200

    (#5491) xtensor: add 0.23.9 + bump dependencies + fix CMake imported target
    * no os.rename
    
    * reorder methods
    
    * cleanup
    
    * more cleanup
    
    * bump dependencies
    
    * don't run test_package if cross build
    
    * fix CMake imported target
    
    official CMake imported target is not namespaced
    
    * add xtensor/0.23.9

--- recipes/xtensor/all/conanfile.py
+++ recipes/xtensor/all/conanfile.py
@@ -1,7 +1,9 @@
-import os
-
-from conans import CMake, ConanFile, tools
+from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
+import os
+import textwrap
+
+required_conan_version = ">=1.33.0"
 
 
 class XtensorConan(ConanFile):
@@ -17,17 +19,16 @@ class XtensorConan(ConanFile):
         "tbb": [True, False],
         "openmp": [True, False],
     }
-    default_options = {"xsimd": True, "tbb": False, "openmp": False}
+    default_options = {
+        "xsimd": True,
+        "tbb": False,
+        "openmp": False,
+    }
     no_copy_source = True
 
     @property
     def _source_subfolder(self):
-        return os.path.join(self.source_folder, "source_subfolder")
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_folder = self.name + "-" + self.version
-        os.rename(extracted_folder, self._source_subfolder)
+        return "source_subfolder"
 
     def configure(self):
         if self.options.tbb and self.options.openmp:
@@ -50,23 +51,58 @@ class XtensorConan(ConanFile):
             raise ConanInvalidConfiguration("xtensor requires at least C++14")
 
     def requirements(self):
-        self.requires("xtl/0.6.12")
-        self.requires("nlohmann_json/3.7.3")
+        self.requires("xtl/0.7.2")
+        self.requires("nlohmann_json/3.9.1")
         if self.options.xsimd:
-            self.requires("xsimd/7.4.6")
+            self.requires("xsimd/7.4.10")
         if self.options.tbb:
-            self.requires("tbb/2020.0")
+            self.requires("tbb/2020.3")
+
+    def package_id(self):
+        self.info.header_only()
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy(
             "*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include")
         )
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"xtensor": "xtensor::xtensor"}
+        )
 
-    def package_id(self):
-        self.info.header_only()
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "xtensor"
+        self.cpp_info.names["cmake_find_package_multi"] = "xtensor"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
+        self.cpp_info.names["pkg_config"] = "xtensor"
         if self.options.xsimd:
             self.cpp_info.defines.append("XTENSOR_USE_XSIMD")
         if self.options.tbb:
