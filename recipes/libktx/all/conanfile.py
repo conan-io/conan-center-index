b505aa7f1cdeb7725433bd278193deb66fc381d0
Gavin <GavinNL@users.noreply.github.com>
2021-05-11 10:58:08 -0400

    Apply suggestions from code review
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/libktx/all/conanfile.py
+++ recipes/libktx/all/conanfile.py
@@ -1,7 +1,8 @@
 from conans import ConanFile, tools, CMake
 from conans.errors import ConanInvalidConfiguration
 import os
-import glob
+
+required_conan_version = ">=1.33.0"
 
 
 class LibKtxConan(ConanFile):
@@ -42,42 +43,12 @@ class LibKtxConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
-        # Copied this section from the entt recipe
-        minimal_cpp_standard = "17"
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, minimal_cpp_standard)
-
-        minimal_version = {
-            "Visual Studio": "15.9",
-            "gcc": "7",
-            "clang": "5",
-            "apple-clang": "10"
-        }
-
-        compiler = str(self.settings.compiler)
-        if compiler not in minimal_version:
-            self.output.warn(
-                "%s recipe lacks information about the %s compiler standard version support" % (self.name, compiler))
-            self.output.warn(
-                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
-            return
-
-        # Compare versions asuming minor satisfies if not explicitly set
-        def lazy_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
-
-        if lazy_lt_semver(str(self.settings.compiler.version), minimal_version[compiler]):
-            raise ConanInvalidConfiguration(
-                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 11)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        url = self.conan_data["sources"][self.version]["url"]
-        extracted_dir = "KTX-Software-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def build(self):
         cmake = self._configure_cmake()
@@ -98,12 +69,11 @@ class LibKtxConan(ConanFile):
         return self._cmake
 
     def package(self):
-        self.copy("LICENSES/*", dst="", src=self._source_subfolder)
+        self.copy("LICENSE.md", dst="licenses", src=self._source_subfolder)
+        self.copy("*", dst="licenses", src=os.path.join(self._source_subfolder, "LICENSES"))
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        os.rename(os.path.join(self.package_folder, "LICENSES"),
-                  os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
         self.cpp_info.libs = ["ktx"]
