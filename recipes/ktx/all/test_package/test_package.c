db669fb5696413b3d1504079475c6308a1086931
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-11 20:34:47 +0200

    improvements:
    - properly set C++ standard at compile time
    - add see option
    - add tools option (True dy default, except if iOS, Android or Emscripten)
    - add bin folder to PATH when tools are built
    - propagate libcxx, since ktx is a C++ library with C API
    - test consuming ktx in C project

--- recipes/ktx/all/test_package/test_package.c
+++ recipes/ktx/all/test_package/test_package.c
@@ -1,19 +1,19 @@
 #include <ktx.h>
 
-#include <iostream>
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
     if (argc < 2) {
-        std::cerr << "Need at least one argument\n";
+        fprintf(stderr, "Need at least one argument\n");
         return 1;
     }
+
     ktxTexture* texture;
     KTX_error_code result;
     result = ktxTexture_CreateFromNamedFile(argv[1],
                                             KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT,
                                             &texture);
-
 
     return 0;
 }
