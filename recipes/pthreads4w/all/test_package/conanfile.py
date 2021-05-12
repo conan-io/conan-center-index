1312235cd3a2720d9101722bf74b462cf0708061
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-12 14:32:12 +0200

    (#5497) pthreads4w: honor settings from profile + bump msys2
    * honor settings from profile
    
    don't force MT runtime and propagate settings from profile
    
    * remove useless msvc checks in autotools build
    
    * bump msys2
    
    * no os.rename
    
    * consistent formatting
    
    * don't run test_package if cross build
    
    * more patch to honor VC runtime
    
    * use standard name for source subfolder
    
    * less cumbersome definitions
    
    * formatting
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * simplify args for nmake build
    
    * rely on autoconf recipe to call autoheader & autoconf
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/pthreads4w/all/test_package/conanfile.py
+++ recipes/pthreads4w/all/test_package/conanfile.py
@@ -12,5 +12,5 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        self.run(os.path.join("bin", "test_package"), run_environment=True)
-
+        if not tools.cross_building(self.settings):
+            self.run(os.path.join("bin", "test_package"), run_environment=True)
