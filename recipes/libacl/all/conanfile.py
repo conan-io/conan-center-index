8a3f7bed1df09d1a84d3dd3fc6da68bd6982a2fd
dotchris90 <christian.kahr@gmx.de>
2021-05-18 04:57:49 -0700

    first try

---
+++ recipes/libacl/all/conanfile.py
@@ -0,0 +1,90 @@
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+import os
+from conans.errors import ConanInvalidConfiguration
+
+
+class LibACLConan(ConanFile):
+    name = "libacl"
+    description = "Commands for Manipulating POSIX Access Control Lists"
+    topics = ("conan", "acl", "POSIX", "access controll")
+    license = "MIT"
+    homepage = "https://savannah.nongnu.org/projects/acl"
+    url = "https://github.com/conan-io/conan-center-index"
+    settings = "os", "arch", "compiler", "build_type"
+    version = "2.2.53"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True
+    }
+    _autotools = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_folder(self):
+        return "build"
+
+    def config_options(self):
+        if self.settings.os != "Linux":
+            raise ConanInvalidConfiguration(
+                "libacl is just supported for Linux")
+
+    def configure(self):
+        del self.settings.compiler.cppstd
+        del self.settings.compiler.libcxx
+        if self.options.shared:
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True,
+                  destination=self._source_subfolder)
+
+    def _configure_autotools(self):
+        if self._autotools:
+            return self._autotools
+        self._autotools = AutoToolsBuildEnvironment(
+            self, win_bash=tools.os_info.is_windows)
+        conf_args = [
+            "--prefix={}".format(tools.unix_path(self.package_folder)),
+            "--bindir={}".format(tools.unix_path(
+                os.path.join(self.package_folder, "bin"))),
+            "--libdir={}".format(tools.unix_path(
+                os.path.join(self.package_folder, "lib")))
+        ]
+        if self.options.shared:
+            conf_args.extend(["--enable-shared", "--disable-static"])
+        else:
+            conf_args.extend(["--disable-shared", "--enable-static"])
+
+        self._autotools.configure(
+            args=conf_args, use_default_install_dirs=False)
+        return self._autotools
+
+    def build(self):
+        with tools.chdir(self._source_subfolder):
+            autotools = self._configure_autotools()
+            autotools.make()
+
+    def package(self):
+        with tools.chdir(self._source_subfolder):
+            autotools = self._configure_autotools()
+            autotools.install()
+            #autotools.make(target="install-lib-headers")
+            #with tools.chdir("librhash"):
+            #    if self.options.shared:
+            #        autotools.make(target="install-so-link")
+        tools.rmdir(os.path.join(self.package_folder, "bin"))
+        tools.rmdir(os.path.join(self.package_folder, "etc"))
+        tools.rmdir(os.path.join(self.package_folder, "share"))
+
+    def package_id(self):
+        pass 
+        #self.cpp_info.names["cmake_find_package"] = "LibACL"
+        #self.cpp_info.names["cmake_find_package_multi"] = "LibACL"
+        #self.cpp_info.libs = tools.collect_libs(self)
