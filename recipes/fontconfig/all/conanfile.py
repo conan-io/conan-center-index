92061802929146820186e067d1c27c23012cdf91
Andrei Malashkin <malashkin.andrey@gmail.com>
2021-05-19 11:31:31 +0200

    (#5554) use latest dependencies for fontconfig/2.13.93
    * use latest dependencies
    
    * Update recipes/fontconfig/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/fontconfig/all/conanfile.py
+++ recipes/fontconfig/all/conanfile.py
@@ -37,15 +37,15 @@ class FontconfigConan(ConanFile):
 
     def requirements(self):
         self.requires("freetype/2.10.4")
-        self.requires("expat/2.2.10")
+        self.requires("expat/2.3.0")
         if self.settings.os == "Linux":
             self.requires("libuuid/1.0.3")
 
     def build_requirements(self):
         self.build_requires("gperf/3.1")
-        self.build_requires("pkgconf/1.7.3")
+        self.build_requires("pkgconf/1.7.4")
         if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/20200517")
+            self.build_requires("msys2/cci.latest")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
