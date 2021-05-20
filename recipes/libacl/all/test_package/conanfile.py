72b850390094512d7221a601d7a2b37e960f000f
dotchris90 <christian.kahr@gmx.de>
2021-05-20 11:15:11 -0700

    adding test

---
+++ recipes/libacl/all/test_package/conanfile.py
@@ -0,0 +1,14 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "cmake_find_package_multi"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        pass 
