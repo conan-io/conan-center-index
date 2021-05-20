508cb9007a2a053947a9dad583832925d705aff7
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-05-20 23:35:48 +0200

    apriltag: fix fPIC option handling

--- recipes/apriltag/all/conanfile.py
+++ recipes/apriltag/all/conanfile.py
@@ -30,14 +30,13 @@ class ApriltagConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.configure()
-        return self._cmake
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
@@ -49,6 +48,13 @@ class ApriltagConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure()
+        return self._cmake
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
