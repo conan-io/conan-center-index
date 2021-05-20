89e0c09386bacd77aad1625310a1b286bca2ca96
danimtb <danimanzaneque@gmail.com>
2021-05-20 10:31:41 +0200

    use strip_root

--- recipes/nmslib/all/conanfile.py
+++ recipes/nmslib/all/conanfile.py
@@ -38,9 +38,8 @@ class Nmslib(ConanFile):
         return "source_subfolder"
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True,
+                  destination=self._source_subfolder)
 
     def _configure_cmake(self):
         if self._cmake is None:
