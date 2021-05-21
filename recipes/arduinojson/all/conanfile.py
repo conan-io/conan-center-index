974f69fd6737fa8c87c91ec92c4947c268bf6958
Anonymous Maarten <madebr@users.noreply.github.com>
2021-05-21 17:31:34 +0200

    (#5580) arduinojson: bump to 6.18.0 + add ArduinoJson target

--- recipes/arduinojson/all/conanfile.py
+++ recipes/arduinojson/all/conanfile.py
@@ -1,14 +1,17 @@
 from conans import ConanFile, tools
 import os
+import textwrap
+
+required_conan_version = ">=1.33.0"
 
 
 class ArduinojsonConan(ConanFile):
     name = "arduinojson"
-    license = "The MIT License (MIT)"
     description = "C++ JSON library for IoT. Simple and efficient."
     homepage = "https://github.com/bblanchon/ArduinoJson"
-    url = "https://github.com/conan-io/conan-center-index"
     topics = ("json", "arduino", "iot", "embedded", "esp8266")
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
     no_copy_source = True
 
     @property
@@ -19,15 +22,43 @@ class ArduinojsonConan(ConanFile):
         self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("ArduinoJson", self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
 
     def package(self):
         self.copy("*LICENSE*", dst="licenses", src=self._source_subfolder)
         self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "src"))
         self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "src"))
 
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"ArduinoJson": "ArduinoJson::ArduinoJson"}
+        )
+
     def package_info(self):
-        # TODO: CMake imported target shouldn't be namespaced
         self.cpp_info.names["cmake_find_package"] = "ArduinoJson"
         self.cpp_info.names["cmake_find_package_multi"] = "ArduinoJson"
+
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
