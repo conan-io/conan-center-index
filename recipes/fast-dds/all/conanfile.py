025a4abe4329396664cb42eb87715a070d045398
dotchris90 <christian.kahr@gmx.de>
2021-05-23 01:43:08 -0700

    compile success, link fails

--- recipes/fast-dds/all/conanfile.py
+++ recipes/fast-dds/all/conanfile.py
@@ -13,13 +13,15 @@ class FastDDSConan(ConanFile):
     homepage = "https://fast-dds.docs.eprosima.com/"
     url = "https://github.com/conan-io/conan-center-index"
     description = "The most complete DDS - Proven: Plenty of success cases."
-    topics = ("Memory","Allocator")
+    topics = ("DDS","Middleware","IPC")
     settings = "os", "compiler", "build_type", "arch"
     options = {
-        "shared":          [True, False]
+        "shared":           [True, False],
+        "with_ssl":         [True, False]
     }
     default_options = {
-        "shared":            False
+        "shared":           True,
+        "with_ssl":         False
     }
     generators = "cmake", "cmake_find_package"
     _cmake = None
@@ -54,6 +56,8 @@ class FastDDSConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+        if self.options.shared:
+            self.options["fast-cdr"].shared = True
 
     def _get_configured_cmake(self):
         if self._cmake:
@@ -61,6 +65,7 @@ class FastDDSConan(ConanFile):
         else:
             self._cmake = CMake(self)
         self._cmake.definitions["BUILD_MEMORY_TOOLS"] = False
+        self._cmake.definitions["NO_TLS"] = True
         self._cmake.configure(
             source_dir = self._source_subfolder
         )
@@ -72,11 +77,19 @@ class FastDDSConan(ConanFile):
         self.requires("fast-cdr/1.0.21")
         self.requires("foo-mem-ven/1.1.0")
 
+    def _patch_sources(self):
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True,
                   destination=self._source_subfolder)
 
+    def imports(self):
+        self.copy("*")
+
     def build(self):
+        self._patch_sources()
 
         cmake = self._get_configured_cmake()
         cmake.build()
@@ -85,17 +98,12 @@ class FastDDSConan(ConanFile):
         cmake = self._get_configured_cmake()
         cmake.install()
         self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
-        lib = tools.relative_dirs(self._pkg_lib)[0]
-        lib_new = lib.replace("-0.6.2-dbg","") \
-                     .replace("-0.6.2","")
-        tools.rename(
-            os.path.join(self._pkg_lib,lib),
-            os.path.join(self._pkg_lib,lib_new)
-        )
-        tools.rmdir(self._pkg_cmake)
-        tools.rmdir(self._pkg_share)
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "foonathan_memory"
-        self.cpp_info.names["cmake_find_multi_package"] = "foonathan_memory"
-        self.cpp_info.libs = ["foonathan_memory"]
+        self.cpp_info.names["cmake_find_package"] = "fast-dds"
+        self.cpp_info.names["cmake_find_multi_package"] = "fast-dds"
+        self.cpp_info.libs = ["fastrtps"]
+        self.cpp_info.requires = [
+            "fastcdr::fastcdr",
+            "tinyxml2::tinyxml2"
+        ]
