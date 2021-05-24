8d6e659629d061634fb4d060d96b19865388d47b
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-05-24 22:03:49 +0200

    Update recipes/qt/6.x.x/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -775,7 +775,7 @@ class QtConan(ConanFile):
             _create_module("WaylandClient", ["Gui", "wayland::wayland-client"])
             _create_module("WaylandCompositor", ["Gui", "wayland::wayland-server"])
 
-        if self.settings.compiler != "Visual Studio":
+        if self.settings.os != "Windows":
             self.cpp_info.components["qtCore"].cxxflags.append("-fPIC")
 
         if not self.options.shared:
