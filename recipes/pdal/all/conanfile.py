584f565a8336991684d77473539a2fe1c6d0ce86
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-24 12:57:15 +0200

    no os.rename

--- recipes/pdal/all/conanfile.py
+++ recipes/pdal/all/conanfile.py
@@ -1,6 +1,9 @@
-import os
 from conans import ConanFile, tools, CMake
 from conans.errors import ConanInvalidConfiguration
+import os
+
+required_conan_version = ">=1.33.0"
+
 
 class PdalConan(ConanFile):
     name = "pdal"
@@ -66,8 +69,8 @@ class PdalConan(ConanFile):
             raise ConanInvalidConfiguration ("This compiler version is unsupported")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("PDAL-%s-src" % self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
