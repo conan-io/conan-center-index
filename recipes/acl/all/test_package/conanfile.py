b89385455ed1f6db77886e24aed012394bccc587
dotchris90 <christian.kahr@gmx.de>
2021-05-29 12:35:58 -0700

    back to old name

--- recipes/acl/all/test_package/conanfile.py
+++ recipes/acl/all/test_package/conanfile.py
@@ -12,5 +12,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "example_c")
+            bin_path = os.path.join("bin", "example")
             self.run(bin_path, run_environment=True)
