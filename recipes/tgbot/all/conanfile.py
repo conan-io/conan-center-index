85a9852046c6ba59a50f3759d9fd026ec0771a42
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-05-31 11:01:30 +0200

    (#5683) tgbot: bump dependencies + check mandatory boost components
    * no os.rename
    
    * bump dependencies
    
    * consistent quotes
    
    * don't run test_package if cross build
    
    * check that boost system is available
    
    * standard order of shared & fPIC options
    
    * bump libcurl version

--- recipes/tgbot/all/conanfile.py
+++ recipes/tgbot/all/conanfile.py
@@ -1,5 +1,8 @@
-import os
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+required_conan_version = ">=1.33.0"
 
 
 class TgbotConan(ConanFile):
@@ -13,16 +16,16 @@ class TgbotConan(ConanFile):
 
     settings = "os", "arch", "compiler", "build_type"
     options = {
+        "shared": [True, False],
         "fPIC": [True, False],
-        "shared": [True, False]
     }
     default_options = {
+        "shared": False,
         "fPIC": True,
-        "shared": False
     }
 
     generators = "cmake", "cmake_find_package"
-    exports_sources = ['CMakeLists.txt']
+    exports_sources = ["CMakeLists.txt"]
 
     _cmake = None
 
@@ -41,14 +44,22 @@ class TgbotConan(ConanFile):
             tools.check_min_cppstd(self, 11)
 
     def requirements(self):
-        self.requires("boost/1.75.0")
-        self.requires("libcurl/7.75.0")
-        self.requires("openssl/1.1.1j")
+        self.requires("boost/1.76.0")
+        self.requires("libcurl/7.77.0")
+        self.requires("openssl/1.1.1k")
+
+    @property
+    def _required_boost_components(self):
+        return ["system"]
+
+    def validate(self):
+        miss_boost_required_comp = any(getattr(self.options["boost"], "without_{}".format(boost_comp), True) for boost_comp in self._required_boost_components)
+        if self.options["boost"].header_only or miss_boost_required_comp:
+            raise ConanInvalidConfiguration("{0} requires non header-only boost with these components: {1}".format(self.name, ", ".join(self._required_boost_components)))
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-cpp-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _patch_sources(self):
         # Don't force PIC
@@ -77,4 +88,5 @@ class TgbotConan(ConanFile):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
 
     def package_info(self):
-        self.cpp_info.libs = ['TgBot']
+        self.cpp_info.libs = ["TgBot"]
+        self.cpp_info.requires = ["boost::headers", "boost::system", "libcurl::libcurl", "openssl::openssl"]
