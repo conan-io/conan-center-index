3cf7379c37aa7322b8c0446e18e4b326f4b22412
Anonymous Maarten <madebr@users.noreply.github.com>
2021-06-01 19:32:44 +0200

    (#5630) crashpad: 2 common libraries are built -> rename them
    * crashpad: 2 common libraries are built -> rename them
    
    * crashpad: rename crashpad_handler_com.com -> crashpad_handler.com
    
    * crashpad: python == spelling nazi :D

--- recipes/crashpad/all/test_package/test_package.cpp
+++ recipes/crashpad/all/test_package/test_package.cpp
@@ -9,6 +9,8 @@
 #include "client/crashpad_client.h"
 #include "client/settings.h"
 
+#include "client/crash_report_database.h"
+
 bool startCrashpad(const base::FilePath &db,
                    const base::FilePath &handler) {
     std::string              url("http://localhost");
@@ -43,6 +45,10 @@ int main(int argc, char* argv[]) {
     base::FilePath db(argv[1]);
     base::FilePath handler(argv[2]);
 #endif
+
+    // Test availability of this function.
+    auto database = crashpad::CrashReportDatabase::Initialize(db);
+    (void) database; /* Avoid warning about unused variable */
 
     return startCrashpad(db, handler) ? 0 : 1;
 }
