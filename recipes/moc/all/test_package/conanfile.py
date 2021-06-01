77479da6c03fcd59de99626464af16c8a0ed3909
s@m.c <s@m.c>
2021-06-01 08:06:56 -0700

    add moc package to repo

---
+++ recipes/moc/all/test_package/conanfile.py
@@ -0,0 +1,38 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanException
+import os
+import sys
+
+
+class MocTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def requirements(self):
+        self.requires('moc/0.9.2')
+
+    def build(self):
+        if not tools.cross_building(self.settings, skip_x64_x86=True):
+            print("Testing version of moc");
+            try:
+                self.run("moc -v", run_environment=True)
+            except ConanException as err:
+                print("MOC failed to run with err %s" % err );
+                raise
+            except:
+                print("MOC failed to run", sys.exc_info()[0]);
+                raise
+
+            moc_bin = tools.which("moc")
+            print("Moc was located at '%s'" % moc_bin);
+            if not moc_bin.startswith(self.deps_cpp_info["moc"].rootpath):
+                raise ConanException("Wrong moc executable captured")
+            cmake = CMake(self)
+            cmake.definitions["MOC_ROOT"] = self.deps_cpp_info["moc"].rootpath
+            cmake.configure()
+            cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings, skip_x64_x86=True):
+            os.chdir("bin")
+            self.run(".%stest_package" % os.sep)
