6090d861d9b7a8c2f62265f56f9e04b6cda0fb6d
Eric Lemanissier <eric.lemanissier@gmail.com>
2021-06-02 18:30:45 +0200

    fix qmake test

--- recipes/qt/5.x.x/test_package/conanfile.py
+++ recipes/qt/5.x.x/test_package/conanfile.py
@@ -7,7 +7,7 @@ from conans.errors import ConanException
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "qt", "cmake", "cmake_find_package_multi"
+    generators = "qt", "cmake", "cmake_find_package_multi", "qmake"
 
     def build_requirements(self):
         if tools.os_info.is_windows and self.settings.compiler == "Visual Studio":
