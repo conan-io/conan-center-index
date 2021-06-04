d697e3dc59fe3bb886210e440f7e6259518bcd90
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-04 17:40:25 +0200

    add gainput/1.0.0

---
+++ recipes/gainput/all/test_package/test_package.cpp
@@ -0,0 +1,23 @@
+#include <gainput/gainput.h>
+
+int main() {
+    enum Button
+    {
+        ButtonConfirm
+    };
+
+    gainput::InputManager manager;
+    manager.SetDisplaySize(800, 600);
+    const gainput::DeviceId keyboardId = manager.CreateDevice<gainput::InputDeviceKeyboard>();
+    const gainput::DeviceId mouseId = manager.CreateDevice<gainput::InputDeviceMouse>();
+    const gainput::DeviceId padId = manager.CreateDevice<gainput::InputDevicePad>();
+    const gainput::DeviceId touchId = manager.CreateDevice<gainput::InputDeviceTouch>();
+
+    gainput::InputMap map(manager);
+    map.MapBool(ButtonConfirm, keyboardId, gainput::KeyReturn);
+    map.MapBool(ButtonConfirm, mouseId, gainput::MouseButtonLeft);
+    map.MapBool(ButtonConfirm, padId, gainput::PadButtonA);
+    map.MapBool(ButtonConfirm, touchId, gainput::Touch0Down);
+
+    return 0;
+}
