fda316af80ebcc45748eef934ebdaa00d79d7a0c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-07 00:30:18 +0200

    add xbyak/5.993

---
+++ recipes/xbyak/all/conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, tools
+import os
+
+required_conan_version = ">=1.33.0"
+
+
+class XbyakConan(ConanFile):
+    name = "xbyak"
+    description = "Xbyak is a C++ header library that enables dynamically to " \
+                  "assemble x86(IA32), x64(AMD64, x86-64) mnemonic."
+    license = "BSD-3-Clause"
+    topics = ("conan", "xbyak", "jit", "assembler")
+    homepage = "https://github.com/herumi/xbyak"
+    url = "https://github.com/conan-io/conan-center-index"
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def package_id(self):
+        self.info.header_only()
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    def package(self):
+        self.copy("COPYRIGHT", dst="licenses", src=self._source_subfolder)
+        self.copy("*.h", dst=os.path.join("include", "xbyak"), src=os.path.join(self._source_subfolder, "xbyak"))
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "xbyak"
+        self.cpp_info.names["cmake_find_package_multi"] = "xbyak"
