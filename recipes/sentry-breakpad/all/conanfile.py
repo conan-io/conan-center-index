48418da3dd94e2790a6def722c82837ab377048b
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-06-06 16:59:20 +0200

    sentry-breakpad: implement feedback + MSVC support

--- recipes/sentry-breakpad/all/conanfile.py
+++ recipes/sentry-breakpad/all/conanfile.py
@@ -6,7 +6,7 @@ required_conan_version = ">=1.28.0"
 
 class SentryBreakpadConan(ConanFile):
     name = "sentry-breakpad"
-    description = "A set of client and server components which implement a crash-reporting system."
+    description = "Client component that implements a crash-reporting system."
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/getsentry/breakpad"
     license = "Apache-2.0"
@@ -28,12 +28,17 @@ class SentryBreakpadConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("linux-syscall-support/cci.20200813")
+        if self.settings.os in ("FreeBSD", "Linux"):
+            self.requires("linux-syscall-support/cci.20200813")
 
     def validate(self):
         if self.settings.compiler.cppstd:
@@ -46,7 +51,7 @@ class SentryBreakpadConan(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
-        self._cmake.configure()
+        self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
     def _patch_sources(self):
--- recipes/sentry-breakpad/all/test_package/test_package.cpp
+++ recipes/sentry-breakpad/all/test_package/test_package.cpp
@@ -1,4 +1,8 @@
-#include "client/linux/handler/exception_handler.h"
+#ifdef _WIN32
+# include "client/windows/handler/exception_handler.h"
+#else
+# include "client/linux/handler/exception_handler.h"
+#endif
 
 #include <iostream>
 
@@ -6,6 +10,24 @@ using namespace google_breakpad;
 
 namespace {
 
+#ifdef _WIN32
+
+bool filter_callback(void *context, EXCEPTION_POINTERS *exinfo,
+                     MDRawAssertionInfo* assertion) {
+    // If a FilterCallback returns true, Breakpad will continue processing
+    return true;
+}
+
+bool callback(const wchar_t* dump_path,
+              const wchar_t* minidump_id,
+              void* context,
+              EXCEPTION_POINTERS* exinfo,
+              MDRawAssertionInfo* assertion,
+              bool succeeded) {
+    // succeeded indicates whether a minidump file was successfully written.
+    return succeeded;
+}
+#else
 bool callback(const MinidumpDescriptor &descriptor,
               void *context,
               bool succeeded) {
@@ -14,12 +36,22 @@ bool callback(const MinidumpDescriptor &descriptor,
     // the exception handler's constructor.
     return succeeded;
 }
+#endif
 
 }
 
 int main(int argc, char *argv[]) {
     std::cout << "Breakpad test_package\n";
 
+#ifdef _WIN32
+    ExceptionHandler eh(
+      /* dump_path */ L".",
+      filter_callback,
+      callback,
+      /* context */ nullptr,
+      ExceptionHandler::HANDLER_ALL
+    );
+#else
     MinidumpDescriptor descriptor("path/to/cache");
     ExceptionHandler eh(
       descriptor,
@@ -29,6 +61,7 @@ int main(int argc, char *argv[]) {
       /* install handler */ true,
       /* server FD */ -1
     );
+#endif
 
     // run your program here
     return 0;
