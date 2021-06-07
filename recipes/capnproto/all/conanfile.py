8ac87a33c4709f541463d8a0fa58704702097871
Florian Berchtold <florian.berchtold@luminartech.com>
2021-06-07 22:01:36 +0200

    (#5591) capnproto/0.7.0
    * capnproto 0.7.0
    
    * add patch, try with no symlink for now
    
    * fix patch
    
    * add patches
    
    * disable zlib
    
    * disable tls
    
    * Apply suggestions from code review
    
    try spacelm version
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * throw on ssl windows 0.7.0 combo
    
    * typo
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/capnproto/all/conanfile.py
+++ recipes/capnproto/all/conanfile.py
@@ -52,6 +52,8 @@ class CapnprotoConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+        if tools.Version(self.version) < "0.8.0":
+            del self.options.with_zlib
 
     def configure(self):
         if self.options.shared:
@@ -65,11 +67,13 @@ class CapnprotoConan(ConanFile):
             raise ConanInvalidConfiguration("Cap'n Proto requires C++14, which your compiler does not support.")
         if self.settings.compiler == "Visual Studio" and self.options.shared:
             raise ConanInvalidConfiguration("Cap'n Proto doesn't support shared libraries for Visual Studio")
+        if self.settings.os == "Windows" and tools.Version(self.version) < "0.8.0" and self.options.with_openssl:
+            raise ConanInvalidConfiguration("Cap'n Proto doesn't support OpenSSL on Windows pre 0.8.0")
 
     def requirements(self):
         if self.options.with_openssl:
             self.requires("openssl/1.1.1k")
-        if self.options.with_zlib:
+        if self.options.get_safe("with_zlib"):
             self.requires("zlib/1.2.11")
 
     def build_requirements(self):
@@ -98,9 +102,10 @@ class CapnprotoConan(ConanFile):
             "--enable-shared" if self.options.shared else "--disable-shared",
             "--disable-static" if self.options.shared else "--enable-static",
             "--with-openssl" if self.options.with_openssl else "--without-openssl",
-            "--with-zlib" if self.options.with_zlib else "--without-zlib",
             "--enable-reflection"
         ]
+        if tools.Version(self.version) >= "0.8.0":
+            args.append("--with-zlib" if self.options.with_zlib else "--without-zlib")
         self._autotools = AutoToolsBuildEnvironment(self)
         self._autotools.configure(args=args, configure_dir=os.path.join(self._source_subfolder, "c++"))
         return self._autotools
@@ -157,7 +162,7 @@ function(CAPNP_GENERATE_CPP SOURCES HEADERS)""")
             {"name": "kj-http", "requires": ["kj", "kj-async"]},
             {"name": "kj-test", "requires": ["kj"]},
         ]
-        if self.options.with_zlib:
+        if self.options.get_safe("with_zlib"):
             components.append({"name": "kj-gzip", "requires": ["kj", "kj-async", "zlib::zlib"]})
         if self.options.with_openssl:
             components.append({"name": "kj-tls", "requires": ["kj", "kj-async", "openssl::openssl"]})
