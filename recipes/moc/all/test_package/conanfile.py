c7bd94c741c08a40492f8ae96b53119078c90d65
zuut <zuut.git@gmail.com>
2021-06-09 14:05:56 -0700

    Update recipes/moc/all/test_package/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/moc/all/test_package/conanfile.py
+++ recipes/moc/all/test_package/conanfile.py
@@ -7,7 +7,7 @@ from six import StringIO
 
 class MocTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         if not tools.cross_building(self.settings, skip_x64_x86=True):
