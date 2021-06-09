f6034a0e03679fe1022e2e8823dffde219794582
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-09 21:28:54 +0200

    do not raise for Visual Studio 2017

--- recipes/taocpp-pegtl/3.x.x/conanfile.py
+++ recipes/taocpp-pegtl/3.x.x/conanfile.py
@@ -28,17 +28,21 @@ class TaoCPPPEGTLConan(ConanFile):
             "apple-clang": "10",
         }
 
-    def configure(self):
+    def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, "17")
+
+        def lazy_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
+
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if not minimum_version:
-            self.output.warn("TaoCPP PEGTL requires C++17. Your compiler is unknown. Assuming it supports C++17.")
-        elif tools.Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration("TaoCPP PEGTL requires C++17 or higher support standard."
-                                            " {} {} is not supported."
-                                            .format(self.settings.compiler,
-                                                    self.settings.compiler.version))
+            self.output.warn("{} {} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name, self.version))
+        elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration("{} {} requires C++17, which your compiler does not support.".format(self.name, self.version))
 
     def package_id(self):
         self.info.header_only()
