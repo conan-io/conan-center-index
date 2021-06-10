c16aa3027249752de743cffe3444d0e35ae94ecf
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-10 22:01:32 +0200

    no os.rename

--- recipes/glog/all/conanfile.py
+++ recipes/glog/all/conanfile.py
@@ -1,6 +1,8 @@
 from conans import ConanFile, CMake, tools
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class GlogConan(ConanFile):
     name = "glog"
@@ -47,9 +49,8 @@ class GlogConan(ConanFile):
             self.requires("gflags/2.2.2")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
