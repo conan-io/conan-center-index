d459590331ba607d61ad3c17d484eb045a8d2663
andrei.malashkin <andrei.malashkin@reactivereality.com>
2021-06-10 10:24:23 +0200

    correct check for cpp14

--- recipes/libigl/2.x.x/conanfile.py
+++ recipes/libigl/2.x.x/conanfile.py
@@ -30,8 +30,9 @@ class LibiglConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def configure(self):
-        tools.check_min_cppstd(self, "14")
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 14)
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
