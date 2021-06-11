c24a1f5cdbc2118f288db8530e36efe34a64b63e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-11 13:29:27 +0200

    move compiler verification to validate()

--- recipes/hlslpp/all/conanfile.py
+++ recipes/hlslpp/all/conanfile.py
@@ -18,8 +18,8 @@ class HlslppConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def configure(self):
-        if self.settings.compiler.cppstd:
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, 11)
 
     def package_id(self):
