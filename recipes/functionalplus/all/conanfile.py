eef95fd4f59c40638e0a7b1c3085056a3fe82be9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-11 13:49:14 +0200

    no os.rename

--- recipes/functionalplus/all/conanfile.py
+++ recipes/functionalplus/all/conanfile.py
@@ -2,6 +2,8 @@ from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class FunctionalPlusConan(ConanFile):
     name = "functionalplus"
@@ -46,8 +48,8 @@ class FunctionalPlusConan(ConanFile):
         self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("FunctionalPlus-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
