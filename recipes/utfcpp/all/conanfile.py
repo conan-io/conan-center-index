0d5902e51f310028e0c7872177233970754ca876
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-11 13:00:20 +0200

    fix CMake imported target
    official CMake imported target is not namespaced: utf8cpp

--- recipes/utfcpp/all/conanfile.py
+++ recipes/utfcpp/all/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, tools
 import os
+import textwrap
 
 required_conan_version = ">=1.33.0"
 
@@ -31,8 +32,36 @@ class UtfCppConan(ConanFile):
                   src=os.path.join(self._source_subfolder, "source"))
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
 
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"utf8cpp": "utf8cpp::utf8cpp"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
+
     def package_info(self):
-        # TODO: imported CMake target shouldn't be namespaced
         self.cpp_info.names["cmake_find_package"] = "utf8cpp"
         self.cpp_info.names["cmake_find_package_multi"] = "utf8cpp"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
         self.cpp_info.includedirs.append(os.path.join("include", "utf8cpp"))
