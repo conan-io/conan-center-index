3daa5adfe0d32ff3bcbb1b472888a2c007c4ff00
marc wang <m384wang@uwaterloo.ca>
2021-06-13 22:29:32 -0400

    Update example.cpp
    use vtk hello world example for test

--- recipes/vtk/all/test_package/example.cpp
+++ recipes/vtk/all/test_package/example.cpp
@@ -1,10 +1,43 @@
-#include "vtkPolyData.h"
-#include "vtkImageData.h"
-#include "vtkFillHolesFilter.h"
-#include "vtkNew.h"
+#include <vtkActor.h>
+#include <vtkCylinderSource.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
 
-int main() {
-    vtkNew<vtkPolyData> PolyInst;
-    vtkNew<vtkImageData> ImgInst;
-    vtkNew<vtkFillHolesFilter> FilterInst;
+#include <array>
+
+int main()
+{
+    // Taken from vtk hello world example 
+    // https://kitware.github.io/vtk-examples/site/Cxx/GeometricObjects/CylinderExample/
+    
+    vtkNew<vtkNamedColors> colors;
+
+    // Set the background color.
+    std::array<unsigned char, 4> bkg{{26, 51, 102, 255}};
+    colors->SetColor("BkgColor", bkg.data());
+
+    // This creates a polygonal cylinder model with eight circumferential facets
+    // (i.e, in practice an octagonal prism).
+    vtkNew<vtkCylinderSource> cylinder;
+    cylinder->SetResolution(8);
+
+    // The mapper is responsible for pushing the geometry into the graphics
+    // library. It may also do color mapping, if scalars or other attributes are
+    // defined.
+    vtkNew<vtkPolyDataMapper> cylinderMapper;
+    cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
+
+    // The actor is a grouping mechanism: besides the geometry (mapper), it
+    // also has a property, transformation matrix, and/or texture map.
+    // Here we set its color and rotate it around the X and Y axes.
+    vtkNew<vtkActor> cylinderActor;
+    cylinderActor->SetMapper(cylinderMapper);
+    cylinderActor->GetProperty()->SetColor(
+        colors->GetColor4d("Tomato").GetData());
+    cylinderActor->RotateX(30.0);
+    cylinderActor->RotateY(-45.0);
+    cout << "Success" << endl;
+    return 0;
 }
