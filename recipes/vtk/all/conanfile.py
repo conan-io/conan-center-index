31fb8cc04a8da076953d60c1420a829f1e90bed3
marc wang <m384wang@uwaterloo.ca>
2021-06-13 21:26:02 -0400

    Update conanfile.py
    added more options

--- recipes/vtk/all/conanfile.py
+++ recipes/vtk/all/conanfile.py
@@ -16,6 +16,12 @@ class VtkConan(ConanFile):
                 "vtk_use_cuda" : [True, False],
                 "vtk_use_mpi": [True, False],
                 "vtk_group_enable_qt" : ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
+                "vtk_group_enable_rendering": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
+                "vtk_group_enable_mpi": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
+                "vtk_group_enable_standalone": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
+                "vtk_group_enable_views" : ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
+                "vtk_group_enable_web" : ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
+                "vtk_group_enable_imaging" : ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
                 "fPIC": [True, False]}
     default_options = {"shared": False, 
                         "build_testing": "OFF", 
@@ -23,9 +29,15 @@ class VtkConan(ConanFile):
                         "vtk_wrap_python" : False,
                         "vtk_use_cuda": False,
                         "vtk_use_mpi" : False,
+                        "vtk_group_enable_imaging" : "DEFAULT",
                         "vtk_group_enable_qt" : "DEFAULT",
+                        "vtk_group_enable_rendering": "WANT",
+                        "vtk_group_enable_mpi" : "DONT_WANT",
+                        "vtk_group_enable_standalone": "WANT",
+                        "vtk_group_enable_views" : "DEFAULT",
+                        "vtk_group_enable_web" : "DEFAULT",
                         "fPIC": True}
-    generators = "cmake"
+    generators = "cmake_find_package"
 
     _cmake = None
 
@@ -52,6 +64,13 @@ class VtkConan(ConanFile):
             self._cmake.definitions["VTK_SMP_IMPLEMENTATION_TYPE"] = self.options.vtk_smp_implementation
             self._cmake.definitions["VTK_WRAP_PYTHON"] = self.options.vtk_wrap_python
             self._cmake.definitions["VTK_USE_CUDA"] = self.options.vtk_use_cuda
+            self._cmake.definitions["VTK_GROUP_ENABLE_Imaging"] = self.options.vtk_group_enable_imaging
+            self._cmake.definitions["VTK_GROUP_ENABLE_Qt"] = self.options.vtk_group_enable_qt
+            self._cmake.definitions["VTK_GROUP_ENABLE_Rendering"] = self.options.vtk_group_enable_rendering
+            self._cmake.definitions["VTK_GROUP_ENABLE_MPI"] = self.options.vtk_group_enable_mpi
+            self._cmake.definitions["VTK_GROUP_ENABLE_StandAlone"] = self.options.vtk_group_enable_standalone
+            self._cmake.definitions["VTK_GROUP_ENABLE_Views"] = self.options.vtk_group_enable_views
+            self._cmake.definitions["VTK_GROUP_ENABLE_Web"] = self.options.vtk_group_enable_web
             self._cmake.configure(source_folder=self._source_subfolder)
         
         return self._cmake
@@ -61,8 +80,11 @@ class VtkConan(ConanFile):
         cmake.build()
     
     def requirements(self):
+        if self.options.vtk_group_enable_rendering in ["YES", "WANT", "DEFAULT"]:
+            self.requires("opengl/system")
+
         if self.options.vtk_group_enable_qt in ["YES", "WANT", "DEFAULT"]:
-            self.requires("qt/[>5.0]")
+            self.requires("qt/5.15.2")
 
         if self.options.vtk_smp_implementation == "TBB":
             self.requires("TBB/[>2020]")
@@ -77,3 +99,4 @@ class VtkConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "VTK"
+        self.cpp_info.names["cmake_find_package_multi"] = "VTK"
