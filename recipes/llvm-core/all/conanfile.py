9b57ca500d6e48c13ab7017ebe996f67af8b8d25
Henning Becker <henning.becker@gmail.com>
2021-06-14 07:43:02 +0200

    Fix handling of LLVMTableGenGlobalISel on Windows
    This also adjusts the test package such that it needs to link
    against LLVMTableGenGlobalISel.

--- recipes/llvm-core/all/conanfile.py
+++ recipes/llvm-core/all/conanfile.py
@@ -216,7 +216,7 @@ class LLVMCoreConan(ConanFile):
         if not self.options.shared:
             for ext in ['.a', '.lib']:
                 lib = '*LLVMTableGenGlobalISel{}'.format(ext)
-                self.copy(lib, dst='lib', src='lib')
+                self.copy(lib, dst='lib', keep_path=False)
 
             self.run('cmake --graphviz=graph/llvm.dot .')
             with tools.chdir('graph'):
