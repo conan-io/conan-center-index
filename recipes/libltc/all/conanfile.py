41407d35e534541f07631fba8f71c9ab560f2c02
Martin Delille <martin@delille.org>
2021-06-15 12:06:03 +0200

    Remove share from package

--- recipes/libltc/all/conanfile.py
+++ recipes/libltc/all/conanfile.py
@@ -60,6 +60,7 @@ class LibltcConan(ConanFile):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
         autotools = self._configure_autotools()
         autotools.install()
+        tools.rmdir(os.path.join(self.package_folder, "share"))
         os.unlink(os.path.join(self.package_folder, "lib", "libltc.la"))
 
     def package_info(self):
