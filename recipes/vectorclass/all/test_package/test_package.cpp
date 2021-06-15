401e83ba2a0a84a3078dac77820a9e40262c0da2
Marian Klymov <nekto1989@gmail.com>
2021-06-15 21:31:28 +0300

    (#5829) vectorclass: allow building as static library
    * vectorclass: allow building as static library
    
    * Use GLOB instead of hardcoded list of headers
    
    * Remove collect_libs
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Remove obsolete FATAL_ERROR
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Use more suitable project name
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * change default value for header_only to False
    
    * Enforce C++17 through CMake
    
    * Hide CMAKE_CXX_STANDARD_REQUIRED under if
    
    It is not available on CMake 3.0
    
    * VERSION_GREATER_EQUAL is only available since CMake 3.7
    
    * Increase minimum required CMake version to 3.8
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Use target_compile_features
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Remove unnecessary check
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Provide VECTORCLASS_HEADER_ONLY through package_info
    
    * Remove header-only option
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/vectorclass/all/test_package/test_package.cpp
+++ recipes/vectorclass/all/test_package/test_package.cpp
@@ -1,8 +1,54 @@
 #include <vectorclass.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <string>
+
+void print_instruction_set() {
+   // 0          = 80386 instruction set
+   // 1 or above = SSE (XMM) supported by CPU (not testing for OS support)
+   // 2 or above = SSE2
+   // 3 or above = SSE3
+   // 4 or above = Supplementary SSE3 (SSSE3)
+   // 5 or above = SSE4.1
+   // 6 or above = SSE4.2
+   // 7 or above = AVX supported by CPU and operating system
+   // 8 or above = AVX2
+   // 9 or above = AVX512F
+   //10 or above = AVX512VL, AVX512BW, AVX512DQ
+    const auto instruction_set = instrset_detect();
+    switch (instruction_set) {
+        case 10:
+            std::cout << "AVX512VL, AVX512BW, AVX512DQ\n";
+        case 9:
+            std::cout << "AVX512F\n";
+        case 8:
+            std::cout << "AVX2\n";
+        case 7:
+            std::cout << "AVX supported by CPU and operating system\n";
+        case 6:
+            std::cout << "SSE4.2\n";
+        case 5:
+            std::cout << "SSE4.1\n";
+        case 4:
+            std::cout << "Supplementary SSE3 (SSSE3)\n";
+        case 3:
+            std::cout << "SSE3\n";
+        case 2:
+            std::cout << "SSE2\n";
+        case 1:
+            std::cout << "SSE (XMM) supported by CPU (not testing for OS support)\n";
+        case 0:
+            std::cout << "80386 instruction set\n";
+            break;
+        default:
+            throw std::runtime_error("Unexpected instruction set " + std::to_string(instruction_set));
+   }
+}
 
 int main() {
+    print_instruction_set();
+
     Vec4i a(10, 11, 12, 13);
     Vec4i b(20, 21, 22, 23);
 
