518a6faf49a5faa4e370b2e013c0d2a124d94063
jgsogo <jgsogo@gmail.com>
2021-06-15 12:14:29 +0200

    [ninja/1.9.x][m1] Cross-building not supported (bootstrap)

--- recipes/ninja/1.9.x/test_package/conanfile.py
+++ recipes/ninja/1.9.x/test_package/conanfile.py
@@ -1,8 +1,9 @@
-from conans import ConanFile
+from conans import ConanFile, tools
 import os
 
 
 class TestPackage(ConanFile):
 
     def test(self):
-        self.run("ninja --version", run_environment=True)
+        if not tools.cross_building(self, skip_x64_x86=True):
+            self.run("ninja --version", run_environment=True)
