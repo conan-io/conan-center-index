bd378234b4031d18787431be4e5c71bd1b234f56
Martin Delille <martin@delille.org>
2021-06-16 13:41:38 +0200

    Add automake to build requirements for Visual Studio

--- recipes/libltc/all/conanfile.py
+++ recipes/libltc/all/conanfile.py
@@ -39,6 +39,8 @@ class LibltcConan(ConanFile):
     def build_requirements(self):
         if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/20200517")
+        if self.settings.compiler == "Visual Studio":
+            self.build_requires("automake/1.16.2")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
