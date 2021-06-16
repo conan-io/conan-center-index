6958be1f3d870f4cd229a74386d5457ff7d86a69
Martin Delille <martin@delille.org>
2021-06-16 10:23:52 +0200

    Fix indentation

--- recipes/libltc/all/conanfile.py
+++ recipes/libltc/all/conanfile.py
@@ -36,8 +36,8 @@ class LibltcConan(ConanFile):
             del self.options.fPIC
 
     def source(self):
-    tools.get(**self.conan_data["sources"][self.version],
-              destination=self._source_subfolder, strip_root=True)
+        tools.get(**self.conan_data["sources"][self.version],
+            destination=self._source_subfolder, strip_root=True)
 
     def _configure_autotools(self):
         if self._autotools:
