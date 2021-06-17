e4e8ae0588e8bf59705ec016177b595a79867ea6
Chris Mc <prince.chrismc@gmail.com>
2021-06-17 09:44:03 -0400

    Update conanfile.py

--- recipes/recastnavigation/all/conanfile.py
+++ recipes/recastnavigation/all/conanfile.py
@@ -38,6 +38,10 @@ class RecastNavigationConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
