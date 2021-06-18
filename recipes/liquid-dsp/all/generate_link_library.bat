202e0ab42ba4ab14cb7534920a9a2ed2bf7bb34a
Rui Oliveira <ruimail24@gmail.com>
2021-06-18 16:38:44 +0100

    Use link instead of dumpbin for compatibily

--- recipes/liquid-dsp/all/generate_link_library.bat
+++ recipes/liquid-dsp/all/generate_link_library.bat
@@ -1,2 +1,2 @@
 echo EXPORTS >> source_subfolder\libliquid.def
-for /f "skip=19 tokens=4" %%A in ('dumpbin /exports source_subfolder\libliquid.dll') do echo %%A >> source_subfolder\libliquid.def
+for /f "skip=19 tokens=4" %%A in ('link /dump /EXPORTS source_subfolder\libliquid.dll') do echo %%A >> source_subfolder\libliquid.def
