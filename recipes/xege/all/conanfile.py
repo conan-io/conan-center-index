5f778d043bbe3e2549bb315197710b02bbdbb45a
Guyutongxue <guyutongxue@163.com>
2021-06-19 09:59:14 +0800

    Update recipes/xege/all/conanfile.py
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/xege/all/conanfile.py
+++ recipes/xege/all/conanfile.py
@@ -2,6 +2,8 @@ from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class XegeConan(ConanFile):
     name = "xege"
