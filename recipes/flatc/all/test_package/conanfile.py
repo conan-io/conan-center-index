29db638939f4fcc83b55891cd4c6ecbfb7381020
jgsogo <jgsogo@gmail.com>
2021-06-21 10:55:27 +0200

    [flatc][xbuild] Executable not tested if cross-building

--- recipes/flatc/all/test_package/conanfile.py
+++ recipes/flatc/all/test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
@@ -7,10 +7,12 @@ class TestPackageConan(ConanFile):
     generators = "cmake", "cmake_find_package"
     
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build(target="flatbuffers")
+        if not tools.cross_building(self, skip_x64_x86=True):
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build(target="flatbuffers")
         
     def test(self):
-        self.run("flatc --version", run_environment=True)
-        self.run("flathash fnv1_16 conan", run_environment=True)
+        if not tools.cross_building(self, skip_x64_x86=True):
+            self.run("flatc --version", run_environment=True)
+            self.run("flathash fnv1_16 conan", run_environment=True)
