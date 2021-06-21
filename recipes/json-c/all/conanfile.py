24bda123ac37307589e1ebca90c9ad2d8a8be8a4
jgsogo <jgsogo@gmail.com>
2021-06-21 19:00:39 +0200

    handle fPIC

--- recipes/json-c/all/conanfile.py
+++ recipes/json-c/all/conanfile.py
@@ -36,6 +36,8 @@ class JSONCConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
