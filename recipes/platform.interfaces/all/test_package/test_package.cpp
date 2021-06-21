695b8ce87e965d25096228e6de9c5a0ea6121381
uselessgoddess <68294279+uselessgoddess@users.noreply.github.com>
2021-06-21 20:55:30 +0300

    fixed errors in the test_package.cpp
    [for update pull request]

--- recipes/platform.interfaces/all/test_package/test_package.cpp
+++ recipes/platform.interfaces/all/test_package/test_package.cpp
@@ -30,22 +30,22 @@ requires
     IList<Collection> ||
     ISet<Collection> ||
     IDictionary<Collection>
-void add(Collection&& collection, Item&& item)
+void add(Collection& collection, const std::same_as<Item> auto& item)
 {
     if constexpr (IList<Collection>)
     {
-        collection.push_back(std::forward<Item>(item));
+        collection.push_back(item);
     }
     else
     {
-        collection.insert(std::forward<Item>(item));
+        collection.insert(item);
     }
 }
-std::int32_t
+
 int main()
 {
-    std::vector v{ 1, 2, 3 };
-    std::set s{ 1, 2, 3 };
+    std::vector v { 1, 2, 3 };
+    std::set s { 1, 2, 3 };
 
     add(v, 2); // 1 2 3 2
     add(v, 1); // 1 2 3 2 1
@@ -58,4 +58,3 @@ int main()
     print(v); // print: [1, 2, 3, 2, 1]
     print(s); // print: [0, 1, 2, 3]
 }
-
