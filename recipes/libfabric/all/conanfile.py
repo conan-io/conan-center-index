9344d5a8478b895b7bdcd35456531f99bd65433b
dvirtz <dvirtz@gmail.com>
2021-06-23 14:47:58 +0300

    use strip_root

--- recipes/libfabric/all/conanfile.py
+++ recipes/libfabric/all/conanfile.py
@@ -60,9 +60,8 @@ class LibfabricConan(ConanFile):
             raise ConanInvalidConfiguration("Value of with_libnl must be an existing directory")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_autotools(self):
         if self._autotools:
