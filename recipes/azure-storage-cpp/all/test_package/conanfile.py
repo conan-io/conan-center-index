cd64d3edcd5ca82e0afbdfc178b2cb36eef93936
dvirtz <dvirtz@gmail.com>
2021-06-23 10:00:21 +0000

    move c++ standard

--- recipes/azure-storage-cpp/all/test_package/conanfile.py
+++ recipes/azure-storage-cpp/all/test_package/conanfile.py
@@ -9,8 +9,6 @@ class AwsSdkCppTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        if not self.settings.compiler.cppstd:
-            cmake.definitions["CMAKE_CXX_STANDARD"] = 11
         cmake.configure()
         cmake.build()
 
