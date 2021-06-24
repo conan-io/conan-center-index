65b5e9b371aa9dbb9a699ba31ceccd7e8dd1fe71
jgsogo <jgsogo@gmail.com>
2021-06-24 12:05:17 +0200

    for versions that do not have patches

--- recipes/opencl-icd-loader/all/conanfile.py
+++ recipes/opencl-icd-loader/all/conanfile.py
@@ -52,7 +52,7 @@ class OpenclIcdLoaderConan(ConanFile):
         return self._cmake
 
     def _patch_sources(self):
-        for patch in self.conan_data["patches"].get(self.version, []):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
 
     def build(self):
