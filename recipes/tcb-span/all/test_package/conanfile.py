d717eb0c99fc75a37c1daeec54610464ab574b58
Alejandro Colomar (EXFO) <alx.manpages@gmail.com>
2021-06-25 13:30:54 +0200

    tcb-span: Add test_package
    Signed-off-by: Alejandro Colomar (EXFO) <alx.manpages@gmail.com>

---
+++ recipes/tcb-span/all/test_package/conanfile.py
@@ -0,0 +1,16 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake_find_package"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            self.run("test_package", run_environment=True)
