a7608be147e3cd1604f6efd98284fd6f290143e5
Alejandro Colomar (EXFO) <alx.manpages@gmail.com>
2021-06-25 09:37:04 +0200

    tcb-span: New package
    I based this package on trompeloeil and hedley,
    which I found to be 2 header-only packages, like this one.
    
    I removed the test_package folder from those, though.
    That's something to be added.
    
    Signed-off-by: Alejandro Colomar (EXFO) <alx.manpages@gmail.com>

---
+++ recipes/tcb-span/all/conanfile.py
@@ -0,0 +1,32 @@
+from conans import ConanFile, tools
+import os
+import glob
+
+
+class TcbSpanConan(ConanFile):
+    name = "tcb-span"
+    description = "Implementation of C++20's std::span for older compilers"
+    topics = ("conan", "span", "header-only", "tcb-span")
+    homepage = "https://github.com/tcbrindle/span"
+    url = "https://github.com/conan-io/conan-center-index"
+    license = "BSL-1.0"
+    settings = "os", "compiler", "build_type", "arch"
+
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = glob.glob(self.name + "-*/")[0]
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy(pattern="LICENSE*.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*.hpp", dst="include", src=include_folder)
+
+    def package_id(self):
+        self.info.header_only()
