7c563866a86d845946a9243cd7d23a0f0dd8c74b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-29 15:25:10 +0200

    add miniz/2.2.0

--- recipes/miniz/all/conanfile.py
+++ recipes/miniz/all/conanfile.py
@@ -47,6 +47,12 @@ class MinizConan(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
+        if tools.Version(self.version) >= "2.2.0":
+            self._cmake.definitions["BUILD_EXAMPLES"] = False
+            self._cmake.definitions["BUILD_FUZZERS"] = False
+            self._cmake.definitions["AMALGAMATE_SOURCES"] = False
+            self._cmake.definitions["BUILD_HEADER_ONLY"] = False
+            self._cmake.definitions["INSTALL_PROJECT"] = True
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
@@ -60,7 +66,12 @@ class MinizConan(ConanFile):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "miniz"
+        self.cpp_info.names["cmake_find_package_multi"] = "miniz"
+        self.cpp_info.names["pkg_config"] = "miniz"
         self.cpp_info.libs = ["miniz"]
         self.cpp_info.includedirs = ["include", os.path.join("include", "miniz")]
