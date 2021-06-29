ca3629519c0eb1352364ebdd7a8ae1774a4f7540
Ashley Roll <ash@digitalnemesis.com>
2021-06-29 22:10:51 +1000

    Initial package for crc_cpp, Version 1.0.0

---
+++ recipes/crc_cpp/all/conanfile.py
@@ -0,0 +1,29 @@
+from conans import ConanFile, tools
+import os
+
+required_conan_version = ">=1.33.0"
+
+
+class Crc_CppConan(ConanFile):
+    name = "crc_cpp"
+    description = "A header only, constexpr / compile time small-table based CRC library for C++17 or newer"
+    topics = ("conan", "crc_cpp", "crc", "constexpr" "C++17", "C++20")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/AshleyRoll/crc_cpp"
+    license = "MIT"
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+       tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*", dst="include", src=os.path.join(self._source_subfolder, "include"))
+
+    def package_id(self):
+        self.info.header_only()
+
