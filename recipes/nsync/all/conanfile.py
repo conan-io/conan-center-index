d5d378690db55e68b1070ed53e8065201ebca8da
Alexande B <abobrikovich@gmail.com>
2021-06-29 14:06:42 +0300

    #4806 nsync add LICENSE file package

--- recipes/nsync/all/conanfile.py
+++ recipes/nsync/all/conanfile.py
@@ -43,6 +43,7 @@ class NsyncConan(ConanFile):
         cmake.build()
 
     def package(self):
+        self.copy("LICENSE", dst='licenses', src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
 
