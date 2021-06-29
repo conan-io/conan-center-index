ca8644bfe1ef86e1c416b6d8301d50ecc14181f0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-29 15:23:15 +0200

    explicit cpp_info.libs

--- recipes/miniz/all/conanfile.py
+++ recipes/miniz/all/conanfile.py
@@ -62,5 +62,5 @@ class MinizConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["miniz"]
         self.cpp_info.includedirs = ["include", os.path.join("include", "miniz")]
