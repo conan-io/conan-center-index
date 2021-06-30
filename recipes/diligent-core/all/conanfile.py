52d9f1de302da235bbcfd211d47eac710236907f
andrei.malashkin <andrei.malashkin@reactivereality.com>
2021-06-30 09:52:55 +0200

    initial add diligent recipe

---
+++ recipes/diligent-core/all/conanfile.py
@@ -0,0 +1,81 @@
+import os
+from conans import ConanFile, tools, CMake
+
+
+class DiligentCoreConan(ConanFile):
+    name = "diligent-core"
+    url = "https://github.com/DiligentGraphics/DiligentCore/"
+    homepage = "https://github.com/DiligentGraphics/DiligentCore/tree/v2.5"
+    description = "Diligent Core is a modern cross-platfrom low-level graphics API."
+    license = ("Apache 2.0")
+    topics = ("graphics")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake_find_package", "cmake"
+    exports_sources = ["CMakeLists.txt", "patches/*"]
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def _patch_sources(self):
+        pass
+        #for patch in self.conan_data["patches"][self.version]:
+        #    tools.patch(**patch)
+
+    def requirements(self):
+        self.requires("libjpeg/9d")
+        self.requires("libtiff/4.2.0")
+        self.requires("zlib/1.2.11")
+        self.requires("libpng/1.6.37")
+
+        #self.requires("spirv-headers/cci.20210526")
+
+        #self.requires("spirv-tools/2019.2")
+        #self.requires("spirv-cross/20200403")
+        #self.requires("glslang/8.13.3559")
+        
+        self.requires("vulkan-memory-allocator/2.3.0")
+        self.requires("vulkan-loader/1.2.172")
+        self.requires("vulkan-headers/1.2.172")
+        
+        self.requires("glew/2.2.0")
+        self.requires("stb/20200203")
+        self.requires("volk/1.2.170")
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["DILIGENT_BUILD_SAMPLES"] = False
+        self._cmake.definitions["DILIGENT_BUILD_TESTS"] = False
+        self._cmake.definitions["DILIGENT_NO_FORMAT_VALIDATION"] = True
+        return self._cmake
+
+    def build(self):
+        self._patch_sources()
+        cmake = self._configure_cmake()
+        cmake.configure(build_folder=self._build_subfolder)
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+        self.copy("License.txt", dst="licenses", src=self._source_subfolder)
---
+++ recipes/diligent-core/all/test_package/test_package.cpp
@@ -0,0 +1,8 @@
+#include "Common/interface/RefCntAutoPtr.hpp"
+#include "Graphics/GraphicsEngine/interface/Buffer.h"
+
+int main()
+{
+    Diligent::RefCntAutoPtr<Diligent::IBuffer>  buffer;
+    return 0;
+}
