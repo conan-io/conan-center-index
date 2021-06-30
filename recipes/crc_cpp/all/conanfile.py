fd62888af2a49e5cd3ed1c14ad75636b54631546
Ashley Roll <ash@digitalnemesis.com>
2021-06-30 20:26:59 +1000

    Added additional compiler version checks

--- recipes/crc_cpp/all/conanfile.py
+++ recipes/crc_cpp/all/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.33.0"
@@ -18,6 +19,28 @@ class Crc_CppConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    @property
+    def _supported_compiler(self):
+        compiler = str(self.settings.compiler)
+        version = tools.Version(self.settings.compiler.version)
+        if compiler == "Visual Studio" and version >= "15":
+            return True
+        elif compiler == "gcc" and version >= "9":
+            return True
+        elif compiler == "clang" and version >= "5":
+            return True
+        elif compiler == "apple-clang" and version >= "10":
+            return True
+        else:
+            self.output.warn("{} recipe lacks information about the {} compiler standard version support".format(self.name, compiler))
+        return False
+
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, "17")
+        if not self._supported_compiler:
+            raise ConanInvalidConfiguration("crc_cpp: Unsupported compiler: {}-{} "
+                                            "Minimum C++17 constexpr features required.".format(self.settings.compiler, self.settings.compiler.version))
     def source(self):
        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
@@ -27,8 +50,4 @@ class Crc_CppConan(ConanFile):
 
     def package_id(self):
         self.info.header_only()
-
-    def configure(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "17")
 
