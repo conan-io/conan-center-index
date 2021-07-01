21abfbdd1b0dcbbce990c685e75f1bd22a6eccc0
jgsogo <jgsogo@gmail.com>
2021-07-01 13:13:05 +0200

    win use ms2sy

--- recipes/coin-utils/all/conanfile.py
+++ recipes/coin-utils/all/conanfile.py
@@ -54,8 +54,9 @@ class CoinUtilsConan(ConanFile):
     def build_requirements(self):
         if self.settings.compiler != "Visual Studio":
             self.build_requires("gnu-config/cci.20201022")
-            if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
-                self.build_requires("msys2/cci.latest")
+
+        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/cci.latest")
 
         if self.settings.compiler == "Visual Studio":
             self.build_requires("automake/1.16.2")
