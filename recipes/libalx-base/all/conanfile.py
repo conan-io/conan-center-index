74c3c70440371afc1f6752c341200ec7777245f6
Alejandro Colomar <alx.manpages@gmail.com>
2021-07-02 22:27:12 +0200

    conanfile: Define dir variables once
    Signed-off-by: Alejandro Colomar <alx.manpages@gmail.com>

--- recipes/libalx-base/all/conanfile.py
+++ recipes/libalx-base/all/conanfile.py
@@ -19,19 +19,25 @@ class libalx_base_conan(ConanFile):
     def configure(self):
         if self.settings.os != "Linux":
             raise ConanInvalidConfiguration("libalx-base is only available for GNU operating systems (e.g. Linux)")
+        self._srcdir   = self.source_folder
+        self._builddir = self.build_subfolder
+        self._DESTDIR  = self.package_folder
+        self._prefix   = ""
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     def build(self):
-        self.run("make -C {} build-base builddir={}".format(self.source_folder, self.build_folder))
+        srcdir   = self._srcdir
+        builddir = self._builddir
+        self.run("make -C {} build-base builddir={}".format(srcdir, builddir))
 
     def package(self):
-        srcdir   = self.source_folder
-        builddir = self.build_subfolder
-        DESTDIR  = self.package_folder
-        prefix   = ""
+        srcdir   = self._srcdir
+        builddir = self._builddir
+        DESTDIR  = self._DESTDIR
+        prefix   = self._prefix
         self.run("make -C {} install-base builddir={} DESTDIR={} prefix={}".format(
                           srcdir, builddir, DESTDIR, prefix))
 
