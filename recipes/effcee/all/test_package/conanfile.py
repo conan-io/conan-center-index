4bddbd56d84281f58c80ec875f34a5090a1bde84
andrei.malashkin <andrei.malashkin@reactivereality.com>
2021-07-02 13:30:47 +0400

    add effcee recipe

---
+++ recipes/effcee/all/test_package/conanfile.py
@@ -0,0 +1,16 @@
+import os
+from conans import ConanFile, CMake, tools
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "cmake_find_package"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        pass
+
