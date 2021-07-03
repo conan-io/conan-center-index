697abe2dec13f76b8677fc9eeb57a253a9520a91
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-03 23:16:08 +0200

    no os.rename

--- recipes/sofa/all/conanfile.py
+++ recipes/sofa/all/conanfile.py
@@ -1,6 +1,8 @@
+from conans import ConanFile, CMake, tools
 import os
 
-from conans import ConanFile, CMake, tools
+required_conan_version = ">=1.33.0"
+
 
 class SofaConan(ConanFile):
     name = "sofa"
@@ -36,8 +38,8 @@ class SofaConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename(self.name, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def build(self):
         cmake = self._configure_cmake()
