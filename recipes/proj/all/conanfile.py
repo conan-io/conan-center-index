3b64e596d66138e502d9937c8b5ecbc68e211e88
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-04 14:06:35 +0200

    bump dependencies

--- recipes/proj/all/conanfile.py
+++ recipes/proj/all/conanfile.py
@@ -49,11 +49,11 @@ class ProjConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("sqlite3/3.35.5")
+        self.requires("sqlite3/3.36.0")
         if self.options.get_safe("with_tiff"):
             self.requires("libtiff/4.2.0")
         if self.options.get_safe("with_curl"):
-            self.requires("libcurl/7.75.0")
+            self.requires("libcurl/7.77.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
