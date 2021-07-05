0183af9302fca2de0695d9d0bb33c2d6aa797b95
Victor Campmany Canes <vcampmany@posteo.net>
2021-07-05 21:45:35 +0200

    Fix validate conditions order

--- recipes/jsonnet/all/conanfile.py
+++ recipes/jsonnet/all/conanfile.py
@@ -36,11 +36,11 @@ class JsonnetConan(ConanFile):
         self.requires("nlohmann_json/3.9.1")
 
     def validate(self):
-        if self.settings.compiler.libcxx != "libstdc++11":
-            raise ConanInvalidConfiguration("jsonnet requires libstdc++11")
         if self.settings.compiler not in ["gcc", "clang", "apple-clang"]:
             raise ConanInvalidConfiguration("{} compiler not supported"
                                             .format(self.settings.compiler))
+        if self.settings.compiler.libcxx != "libstdc++11":
+            raise ConanInvalidConfiguration("jsonnet requires libstdc++11")
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, "11")
 
