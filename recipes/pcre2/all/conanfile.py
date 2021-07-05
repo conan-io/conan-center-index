f01095958645364a671bcdc1e002408cac9a978f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-05 17:52:07 +0200

    no os.rename

--- recipes/pcre2/all/conanfile.py
+++ recipes/pcre2/all/conanfile.py
@@ -2,6 +2,8 @@ from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class PCRE2Conan(ConanFile):
     name = "pcre2"
@@ -71,9 +73,8 @@ class PCRE2Conan(ConanFile):
             self.requires("bzip2/1.0.8")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _patch_sources(self):
         cmakelists = os.path.join(self._source_subfolder, "CMakeLists.txt")
