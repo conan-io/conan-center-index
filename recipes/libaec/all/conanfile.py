670968a1763b10fa6625e55a2fb3051b7327764f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-05 16:09:15 +0200

    no os.rename

--- recipes/libaec/all/conanfile.py
+++ recipes/libaec/all/conanfile.py
@@ -1,6 +1,8 @@
 from conans import ConanFile, CMake, tools
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class LibaecConan(ConanFile):
     name = "libaec"
@@ -39,9 +41,8 @@ class LibaecConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         cmake = CMake(self)
