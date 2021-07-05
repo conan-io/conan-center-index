e00100cc0601d9fe57362d0b4d43193ff3f254ad
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-05 15:45:02 +0200

    explicit cpp_info.libs

--- recipes/lzfse/all/conanfile.py
+++ recipes/lzfse/all/conanfile.py
@@ -58,7 +58,7 @@ class LzfseConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["lzfse"]
         bin_path = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH environment variable: {}".format(bin_path))
         self.env_info.PATH.append(bin_path)
