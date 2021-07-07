6c3373bf69beee38dd8acc5435d4366fdf216543
dvirtz <dvirtz@gmail.com>
2021-07-07 11:34:41 +0000

    apply review suggestions

--- recipes/aws-libfabric/all/conanfile.py
+++ recipes/aws-libfabric/all/conanfile.py
@@ -14,28 +14,28 @@ class LibfabricConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     _providers = ['gni', 'psm', 'psm2', 'sockets', 'rxm', 'tcp', 'udp', 'usnic', 'verbs', 'bgq', 'shm', 'efa', 'rxd', 'mrail', 'rstream', 'perf', 'hook_debug']
     options = {
-        **{ p: "ANY" for p in _providers },
+        **{ p: [True, False, "shared"] for p in _providers },
         **{
             "shared": [True, False],
             "fPIC": [True, False],
             "with_libnl": [True, False],
-            "with_bgq_progress": [None, "auto", "manual"],
-            "with_bgq_mr": [None, "basic", "scalable"]
+            "with_bgq_progress": ["auto", "manual"],
+            "with_bgq_mr": ["basic", "scalable"]
         }
     }
     default_options = {
-        **{ p: "auto" for p in _providers if p not in ('efa', 'verbs', 'rxd') },
+        **{ p: False for p in _providers },
         **{
             "shared": False,
             "fPIC": True,
-            "efa": "yes",
-            "verbs": "no",
-            "rxd": "no",
+            "tcp": True,
+            "efa": tools.os_info.is_linux,
             "with_libnl": False,
-            "with_bgq_progress": None,
-            "with_bgq_mr": None
+            "with_bgq_progress": "manual",
+            "with_bgq_mr": "basic"
         }
     }
+    build_requires = "autoconf/2.71", "automake/1.16.3", "libtool/2.4.6"
 
     @property
     def _source_subfolder(self):
@@ -44,7 +44,7 @@ class LibfabricConan(ConanFile):
     _autotools = None
 
     def config_options(self):
-        if self.settings.os == 'Windows':
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
@@ -54,15 +54,12 @@ class LibfabricConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def requirements(self):
-        if self.options.get_safe('with_libnl'):
-            self.requires('libnl/3.2.25')
+        if self.options.with_libnl:
+            self.requires("libnl/3.2.25")
 
     def validate(self):
         if self.settings.compiler == "Visual Studio":
             raise ConanInvalidConfiguration("The libfabric package cannot be built on Visual Studio.")
-        for p in self._providers:
-            if self.options.get_safe(p) not in ["auto", "yes", "no", "dl"] and not os.path.isdir(str(self.options.get_safe(p))):
-                raise ConanInvalidConfiguration("Option {} can only be one of 'auto', 'yes', 'no', 'dl' or a directory path")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
@@ -74,15 +71,19 @@ class LibfabricConan(ConanFile):
         self._autotools = AutoToolsBuildEnvironment(self)
         with tools.chdir(self._source_subfolder):
             self.run("./autogen.sh", win_bash=tools.os_info.is_windows)
-        args = []
+        yes_no_dl = lambda v: {"True": "yes", "False": "no", "shared": "dl"}[str(v)]
+        args = [
+            "--with-bgq-progress={}".format(self.options.with_bgq_progress),
+            "--with-bgq-mr={}".format(self.options.with_bgq_mr),
+        ]
         for p in self._providers:
-            args.append('--enable-{}={}'.format(p, self.options.get_safe(p)))
+            args.append("--enable-{}={}".format(p, yes_no_dl(getattr(self.options, p))))
         if self.options.with_libnl:
-            args.append('--with-libnl={}'.format(self.deps_cpp_info["libnl"].rootpath))
-        if self.options.with_bgq_progress:
-            args.append('--with-bgq-progress={}'.format(self.options.with_bgq_progress))
-        if self.options.with_bgq_mr:
-            args.append('--with-bgq-mr={}'.format(self.options.with_bgq_mr))
+            args.append("--with-libnl={}".format(tools.unix_path(self.deps_cpp_info["libnl"].rootpath))),
+        else:
+            args.append("--with-libnl=no")
+        if self.settings.build_type == "Debug":
+            args.append("--enable-debug")
         self._autotools.configure(args=args, configure_dir=self._source_subfolder)
         return self._autotools
 
@@ -99,10 +100,10 @@ class LibfabricConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
         tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
         if self.options.shared:
-            tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.a")
+            tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "libfabric.a")
         else:
-            tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.so*")
-            tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.dylib")
+            tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "libfabric.so*")
+            tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "libfabric.dylib")
 
     def package_info(self):
         self.cpp_info.names["pkg_config"] = "libfabric"
