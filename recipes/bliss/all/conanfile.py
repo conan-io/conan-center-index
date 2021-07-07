8396ff136fb20915c8e208e457b24c4eb2ae6758
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-07 16:26:11 +0200

    drop Visual Studio < 2017
    bliss uses and/or/not keywords. msvc can't handle these keywords without /permissive- option, introduced in Visual Studio 2017

--- recipes/bliss/all/conanfile.py
+++ recipes/bliss/all/conanfile.py
@@ -1,5 +1,6 @@
-import os
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
 
 required_conan_version = ">=1.33.0"
 
@@ -52,6 +53,8 @@ class BlissConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, 11)
+        if self.settings.compiler == "Visual Studio" and tools.Version(self.settings.compiler.version) < "15":
+            raise ConanInvalidConfiguration("bliss doesn't support Visual Studio < 2017")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
