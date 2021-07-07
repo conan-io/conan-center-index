4ee32f0a9d8a18685733d45088eaa920067ae2a5
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-07 11:16:43 +0200

    fix indentation

--- recipes/bdwgc/all/conanfile.py
+++ recipes/bdwgc/all/conanfile.py
@@ -88,7 +88,7 @@ class BdwGcConan(ConanFile):
             return self._cmake
         self._cmake = CMake(self)
         for option, _ in self._autotools_options_defaults:
-                self._cmake.definitions["enable_{}".format(option)] = self.options.get_safe(option)
+            self._cmake.definitions["enable_{}".format(option)] = self.options.get_safe(option)
         self._cmake.definitions["disable_gc_debug"] = not self.options.gc_debug
         self._cmake.definitions["disable_handle_fork"] = not self.options.handle_fork
         self._cmake.definitions["install_headers"] = True
