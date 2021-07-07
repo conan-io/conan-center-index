25ede94b4e8d28b018c0b5c093db86ab727984d3
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-07 17:24:00 +0200

    explicit cpp_info.libs

--- recipes/cctz/all/conanfile.py
+++ recipes/cctz/all/conanfile.py
@@ -67,7 +67,7 @@ class CCTZConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["cctz"]
         if tools.is_apple_os(self.settings.os):
             self.cpp_info.frameworks.append("CoreFoundation")
 
