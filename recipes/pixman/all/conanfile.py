68cc63a30562241e05e3c9257ec3dae741aeafae
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-07 15:45:45 +0200

    bump msys2

--- recipes/pixman/all/conanfile.py
+++ recipes/pixman/all/conanfile.py
@@ -40,9 +40,8 @@ class PixmanConan(ConanFile):
             raise ConanInvalidConfiguration("pixman can only be built as a static library on Windows")
 
     def build_requirements(self):
-        if tools.os_info.is_windows:
-            if "CONAN_BASH_PATH" not in os.environ and tools.os_info.detect_windows_subsystem() != 'msys2':
-                self.build_requires("msys2/20200517")
+        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/cci.latest")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
