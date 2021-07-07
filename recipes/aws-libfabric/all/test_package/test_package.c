7a2936388f0b48f9ecd0c97c0cb6c7be04550870
dvirtz <dvirtz@gmail.com>
2021-07-07 15:13:53 +0300

    return EXIT_SUCCESS

--- recipes/aws-libfabric/all/test_package/test_package.c
+++ recipes/aws-libfabric/all/test_package/test_package.c
@@ -2,8 +2,10 @@
 
 #include <rdma/fabric.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main () {
     const uint32_t version = fi_version();
     printf("hello libfabric version %d.%d\n", FI_MAJOR(version), FI_MINOR(version));
+    return EXIT_SUCCESS;
 }
