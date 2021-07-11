4b013be48dafbe1e3a9a41968fca502b99e73289
werto87 <huhulasmichinruhe@web.de>
2021-07-11 11:15:21 +0200

    run build and test if os is Emscripten

--- recipes/emsdk_installer/all/test_package/conanfile.py
+++ recipes/emsdk_installer/all/test_package/conanfile.py
@@ -10,13 +10,16 @@ class TestPackageConan(ConanFile):
     test_type = "build_requires"
 
     def build(self):
-        cmake = CMake(self, generator='MinGW Makefiles' if os.name ==
-                      'nt' else 'Unix Makefiles', parallel=False)
-        cmake.definitions["CONAN_DISABLE_CHECK_COMPILER"] = True
-        cmake.configure()
-        cmake.build()
+        if self.settings.os == "Emscripten":
+            cmake = CMake(self, generator='MinGW Makefiles' if os.name ==
+                          'nt' else 'Unix Makefiles', parallel=False)
+            cmake.definitions["CONAN_DISABLE_CHECK_COMPILER"] = True
+            cmake.configure()
+            cmake.build()
         #self.run("cmake --build %s --config Release" % self.build_folder)
 
     def test(self):
-        test_file = os.path.join(self.build_folder, "bin", "test_package.js")
-        self.run('node %s' % test_file, run_environment=True)
+        if self.settings.os == "Emscripten":
+            test_file = os.path.join(
+                self.build_folder, "bin", "test_package.js")
+            self.run('node %s' % test_file, run_environment=True)
