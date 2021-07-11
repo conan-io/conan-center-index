234fd9a281d1c6fd2a5ca709323c0eefe7344b05
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-11 11:10:09 +0200

    no os.rename

--- recipes/editline/all/conanfile.py
+++ recipes/editline/all/conanfile.py
@@ -1,7 +1,8 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
 from conans.errors import ConanInvalidConfiguration
-import glob
 import os
+
+required_conan_version = ">=1.33.0"
 
 
 class EditlineConan(ConanFile):
@@ -51,9 +52,8 @@ class EditlineConan(ConanFile):
             raise ConanInvalidConfiguration("Windows is not supported by libedit (missing termios.h)")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        archive_name = glob.glob("{}-*-{}".format("libedit", self.version))[0]
-        os.rename(archive_name, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_autotools(self):
         if self._autotools:
