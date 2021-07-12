05d242bf457f934a5f4d8d6a268aa335aae99f76
hnOsmium0001 <hn.osmium0001@gmail.com>
2021-07-11 21:33:19 -0700

    Fix recipe

--- recipes/zippy/all/conanfile.py
+++ recipes/zippy/all/conanfile.py
@@ -8,20 +8,22 @@ class NoWideConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/troldal/Zippy"
     license = "MIT"
-    no_copy_source = True
+    exports_sources = "patches/*"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
     def requirements(self):
-        self.requires("miniz/2.1.0")
+        self.requires("miniz/2.2.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     def package(self):
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy(pattern="*.hpp", dst="include", src=os.path.join(self._source_subfolder, "library"))
 
