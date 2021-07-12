59aaa8a2a1083a329dd3bae116c2fa12afb56094
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-12 11:40:08 +0200

    avoid unmanaged conan calls
    Upstream CMakeLists has an intrusive integration of conan, calling conan install with a custom conanfile using version range. It can't be allowed in CCI.

--- recipes/imagl/all/conanfile.py
+++ recipes/imagl/all/conanfile.py
@@ -27,7 +27,7 @@ class ImaglConan(ConanFile):
         "allow_clang_11": None
     }
     generators = "cmake"
-    exports_sources = "CMakeLists.txt"
+    exports_sources = ["CMakeLists.txt", "patches/**"]
     _cmake = None
 
     @property
@@ -120,6 +120,8 @@ class ImaglConan(ConanFile):
         return self._cmake
 
     def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
