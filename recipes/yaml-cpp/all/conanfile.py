1a6b68956b80c5979df012b21b4e8a3afad24d6d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-12 20:10:03 +0200

    remove useless definition and C++ standard injection

--- recipes/yaml-cpp/all/conanfile.py
+++ recipes/yaml-cpp/all/conanfile.py
@@ -64,5 +64,3 @@ class YamlCppConan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append('m')
-        if self.settings.compiler == 'Visual Studio':
-            self.cpp_info.defines.append('_NOEXCEPT=noexcept')
