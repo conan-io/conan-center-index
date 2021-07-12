54f0974690e192b8c4287c9d9e3fc32386588c86
jgsogo <jgsogo@gmail.com>
2021-07-12 18:47:48 +0200

    [corrade] Add components + build_module + modernize

--- recipes/corrade/all/test_package/conanfile.py
+++ recipes/corrade/all/test_package/conanfile.py
@@ -4,18 +4,14 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
-        if self.settings.compiler == "Visual Studio":
-            cmake.definitions["CORRADE_MSVC2015_COMPATIBILITY"] = "ON" if self.settings.compiler.version == "14" else "OFF"
-            cmake.definitions["CORRADE_MSVC2017_COMPATIBILITY"] = "ON" if self.settings.compiler.version == "15" else "OFF"
-            cmake.definitions["CORRADE_MSVC2019_COMPATIBILITY"] = "ON" if self.settings.compiler.version == "16" else "OFF"
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
