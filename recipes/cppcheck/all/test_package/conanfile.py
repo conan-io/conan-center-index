3e3d7f9fb1ab201d89cfc9c288cbae3f83220da9
Jan Sommer <github@soja.uber.space>
2021-07-13 16:18:31 +0200

    Fix missing newline error

--- recipes/cppcheck/all/test_package/conanfile.py
+++ recipes/cppcheck/all/test_package/conanfile.py
@@ -9,4 +9,3 @@ class TestPackageConan(ConanFile):
             self.run("cppcheck --version", run_environment=True)
             self.run("cppcheck --enable=warning,style,performance --std=c++11 ./main.cpp",
                      cwd=self.source_folder, run_environment=True)
-        
