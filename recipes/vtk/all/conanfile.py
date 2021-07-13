ee8eed9a873bb672db94b3709122322c88b29ee4
marc wang <m384wang@uwaterloo.ca>
2021-07-13 18:01:27 -0400

    Update conanfile.py
    simplified options and added os specific requires

--- recipes/vtk/all/conanfile.py
+++ recipes/vtk/all/conanfile.py
@@ -12,30 +12,30 @@ class VtkConan(ConanFile):
     topics = ("conan", "vtk", "geometry", "algorithms")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
-               "vtk_smp_implementation": ["Sequential", "OpenMP", "TBB", "STDThread"],
+               "vtk_smp_implementation": ["sequential", "openmp", "tbb", "stl"],
                "vtk_wrap_python": [True, False],
                "vtk_use_cuda": [True, False],
                "vtk_use_mpi": [True, False],
-               "vtk_group_enable_qt": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
-               "vtk_group_enable_rendering": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
-               "vtk_group_enable_mpi": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
-               "vtk_group_enable_standalone": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
-               "vtk_group_enable_views": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
-               "vtk_group_enable_web": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
-               "vtk_group_enable_imaging": ["YES", "WANT", "DONT_WANT", "NO", "DEFAULT"],
+               "vtk_group_enable_qt": [True, False, "default"],
+               "vtk_group_enable_rendering": [True, False, "default"],
+               "vtk_group_enable_mpi": [True, False, "default"],
+               "vtk_group_enable_standalone": [True, False, "default"],
+               "vtk_group_enable_views": [True, False, "default"],
+               "vtk_group_enable_web": [True, False, "default"],
+               "vtk_group_enable_imaging": [True, False, "default"],
                "fPIC": [True, False]}
     default_options = {"shared": False,
-                       "vtk_smp_implementation": "Sequential",
+                       "vtk_smp_implementation": "sequential",
                        "vtk_wrap_python": False,
                        "vtk_use_cuda": False,
                        "vtk_use_mpi": False,
-                       "vtk_group_enable_imaging": "DEFAULT",
-                       "vtk_group_enable_qt": "DEFAULT",
-                       "vtk_group_enable_rendering": "WANT",
-                       "vtk_group_enable_mpi": "DONT_WANT",
-                       "vtk_group_enable_standalone": "WANT",
-                       "vtk_group_enable_views": "DEFAULT",
-                       "vtk_group_enable_web": "DEFAULT",
+                       "vtk_group_enable_imaging": True,
+                       "vtk_group_enable_qt": True,
+                       "vtk_group_enable_rendering": True,
+                       "vtk_group_enable_mpi": False,
+                       "vtk_group_enable_standalone": True,
+                       "vtk_group_enable_views": "default",
+                       "vtk_group_enable_web": False,
                        "fPIC": True}
     generators = "cmake"
     short_paths = True
@@ -57,22 +57,43 @@ class VtkConan(ConanFile):
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   strip_root=True, destination=self._source_subfolder)
+    
+    def _convert_smp_option(self, v):
+        if v == "sequential":
+            return "Sequential"
+        elif v == "openmp":
+            return "OpenMP"
+        elif v == "tbb":
+            return "TBB"
+        elif v == "stl":
+            return "STDThread"
+        else:
+            raise "Invalid SMP Option"
+    
+    def _convert_vtk_choice(self, v):
+        if v == "default":
+            return "DEFAULT"
+        elif v:
+            return "WANT"
+        else:
+            return "DONT_WANT"
 
     def _configure_cmake(self):
+
         if not self._cmake:
             self._cmake = CMake(self)
             self._cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
             self._cmake.definitions["BUILD_TESTING"] = "OFF"
-            self._cmake.definitions["VTK_SMP_IMPLEMENTATION_TYPE"] = self.options.vtk_smp_implementation
+            self._cmake.definitions["VTK_SMP_IMPLEMENTATION_TYPE"] = self._convert_smp_option(self.options.vtk_smp_implementation)
             self._cmake.definitions["VTK_WRAP_PYTHON"] = self.options.vtk_wrap_python
             self._cmake.definitions["VTK_USE_CUDA"] = self.options.vtk_use_cuda
-            self._cmake.definitions["VTK_GROUP_ENABLE_Imaging"] = self.options.vtk_group_enable_imaging
-            self._cmake.definitions["VTK_GROUP_ENABLE_Qt"] = self.options.vtk_group_enable_qt
-            self._cmake.definitions["VTK_GROUP_ENABLE_Rendering"] = self.options.vtk_group_enable_rendering
-            self._cmake.definitions["VTK_GROUP_ENABLE_MPI"] = self.options.vtk_group_enable_mpi
-            self._cmake.definitions["VTK_GROUP_ENABLE_StandAlone"] = self.options.vtk_group_enable_standalone
-            self._cmake.definitions["VTK_GROUP_ENABLE_Views"] = self.options.vtk_group_enable_views
-            self._cmake.definitions["VTK_GROUP_ENABLE_Web"] = self.options.vtk_group_enable_web
+            self._cmake.definitions["VTK_GROUP_ENABLE_Imaging"] = self._convert_vtk_choice(self.options.vtk_group_enable_imaging)
+            self._cmake.definitions["VTK_GROUP_ENABLE_Qt"] = self._convert_vtk_choice(self.options.vtk_group_enable_qt)
+            self._cmake.definitions["VTK_GROUP_ENABLE_Rendering"] = self._convert_vtk_choice(self.options.vtk_group_enable_rendering)
+            self._cmake.definitions["VTK_GROUP_ENABLE_MPI"] = self._convert_vtk_choice(self.options.vtk_group_enable_mpi)
+            self._cmake.definitions["VTK_GROUP_ENABLE_StandAlone"] = self._convert_vtk_choice(self.options.vtk_group_enable_standalone)
+            self._cmake.definitions["VTK_GROUP_ENABLE_Views"] = self._convert_vtk_choice(self.options.vtk_group_enable_views)
+            self._cmake.definitions["VTK_GROUP_ENABLE_Web"] = self._convert_vtk_choice(self.options.vtk_group_enable_web)
             self._cmake.configure(source_folder=self._source_subfolder)
 
         return self._cmake
@@ -83,10 +104,13 @@ class VtkConan(ConanFile):
 
     def requirements(self):
 
-        if self.options.vtk_group_enable_qt in ["YES", "WANT"]:
+        if self.options.vtk_group_enable_rendering in [True, "default"]:
+            self.requires("opengl/system")
+
+        if self.options.vtk_group_enable_qt in [True, "default"]:
             self.requires("qt/5.15.2")
 
-        if self.options.vtk_smp_implementation == "TBB":
+        if self.options.vtk_smp_implementation == "tbb":
             self.requires("TBB")
 
         if self.options.vtk_use_mpi:
@@ -102,11 +126,14 @@ class VtkConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "vtk"))
         self.copy("modules.json", dst="bin", src=self.build_folder)
     
-    def filter_dep(self, dependencies, enabled_pkg):
+    def filter_dep(self, name, dependencies, enabled_pkg):
         ls_dep = []
         for dep in dependencies:
-            if dep in enabled_pkg.keys():
+            if dep in enabled_pkg.keys() and dep != name:
                 ls_dep.append(enabled_pkg[dep]['alias'])
+            elif dep == "VTK::RenderingCore" and not "VTK::RenderingOpenGL2" in dependencies:
+                ls_dep.append("RenderingOpenGL2")
+
         return ls_dep
 
     def package_info(self):
@@ -135,10 +162,33 @@ class VtkConan(ConanFile):
             self.cpp_info.components[alias].names["cmake_find_package"]  = alias
             self.cpp_info.components[alias].names["cmake_find_package_multi"] = alias
             self.cpp_info.components[alias].libs = ['%s-%d.%d' % ( lib_name, pkg_ver[0], pkg_ver[1])]
-            dep = self.filter_dep(content['depends'], enabled_pkg)
-            opt_dep = self.filter_dep(content['private_depends'], enabled_pkg)
+            dep = self.filter_dep(name, content['depends'], enabled_pkg)
+            opt_dep = self.filter_dep(name, content['private_depends'], enabled_pkg)
             self.cpp_info.components[alias].requires.extend(dep)
             self.cpp_info.components[alias].requires.extend(opt_dep)
+            
+            if name == "VTK::GUISupportQt" :
+                self.cpp_info.components[alias].requires.append("qt")
+            
+            if name == "VTK::RenderingOpenGL2":
+                self.cpp_info.components[alias].requires.append("opengl/system")
+            
+            # Adding system libs without 'lib' prefix and '.so' or '.so.X' suffix.
+            if self.settings.os == 'Linux':
+                self.cpp_info.components[alias].requires.append('pthread')
+                if name == "VTK::vtksys":
+                    self.cpp_info.components[alias].requires.append('dl') # 'libvtksys-7.1.a' require 'dlclose', 'dlopen', 'dlsym' and 'dlerror' which on CentOS are in 'dl' library
+
+            if not self.options.shared and name == "VTK::GUISupportQt":
+                if self.settings.os == 'Windows':
+                    self.cpp_info.components[alias].requires.append('Ws2_32')    # 'vtksys-9.0d.lib' require 'gethostbyname', 'gethostname', 'WSAStartup' and 'WSACleanup' which are in 'Ws2_32.lib' library
+                    self.cpp_info.components[alias].requires.append('Psapi')     # 'vtksys-9.0d.lib' require 'GetProcessMemoryInfo' which is in 'Psapi.lib' library
+                    self.cpp_info.components[alias].requires.append('dbghelp')   # 'vtksys-9.0d.lib' require '__imp_SymGetLineFromAddr64', '__imp_SymInitialize' and '__imp_SymFromAddr' which are in 'dbghelp.lib' library
+
+                if self.settings.os == 'Macos':
+                    self.cpp_info.components[alias].frameworks.extend(["CoreFoundation"]) # 'libvtkRenderingOpenGL2-9.0.a' require '_CFRelease', '_CFRetain', '_objc_msgSend' and much more which are in 'CoreFoundation' library
+                    self.cpp_info.components[alias].frameworks.extend(["Cocoa"])          # 'libvtkRenderingOpenGL2-9.0.a' require '_CGWarpMouseCursorPosition' and more, 'libvtkRenderingUI-9.0.a' require '_OBJC_CLASS_$_NSApplication' and more, which are in 'Cocoa' library
+
 
             incl_dir = {base_include_dir}
             for incl_f in content['headers']:
