01b1ff986952175dc7460e5fa7176d237fca55f3
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-14 00:07:29 +0200

    fix CMake imported target

--- recipes/kangaru/all/conanfile.py
+++ recipes/kangaru/all/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, tools, CMake
 import os
+import textwrap
 
 required_conan_version = ">=1.33.0"
 
@@ -61,3 +62,35 @@ class KangaruConan(ConanFile):
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib"))
         self.copy(os.path.join(self._source_subfolder, "LICENSE"), "licenses")
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"kangaru": "kangaru::kangaru"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "kangaru"
+        self.cpp_info.names["cmake_find_package_multi"] = "kangaru"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
