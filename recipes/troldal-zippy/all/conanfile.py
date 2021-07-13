f078f7e4419074c9a52b75eb5a300ed6a4a5fc0c
hnOsmium0001 <hn.osmium0001@gmail.com>
2021-07-13 16:26:09 -0700

    Check minimum compiler version for c++17

--- recipes/troldal-zippy/all/conanfile.py
+++ recipes/troldal-zippy/all/conanfile.py
@@ -1,5 +1,8 @@
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 import os
+
+required_conan_version = ">=1.33.0"
 
 class TroldalZippyConan(ConanFile):
     name = "troldal-zippy"
@@ -18,9 +21,30 @@ class TroldalZippyConan(ConanFile):
     def requirements(self):
         self.requires("miniz/2.2.0")
 
+    @property
+    def _minimum_cpp_standard(self):
+        return 17
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "17",
+            "gcc": "7",
+            "clang": "6",
+            "apple-clang": "10",
+        }
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "17")
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
