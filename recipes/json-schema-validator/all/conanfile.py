6fa068dbc3cdc3eb1a205c7fb69a77cc1407859a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-13 17:03:03 +0200

    no os.rename

--- recipes/json-schema-validator/all/conanfile.py
+++ recipes/json-schema-validator/all/conanfile.py
@@ -2,6 +2,8 @@ from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class JsonSchemaValidatorConan(ConanFile):
     name = "json-schema-validator"
@@ -57,9 +59,8 @@ class JsonSchemaValidatorConan(ConanFile):
         self.requires("nlohmann_json/3.9.1")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
