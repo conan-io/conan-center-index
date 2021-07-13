fe5651d2d8b958a1d6e49434ce323cc89130161c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-14 00:58:56 +0200

    bump msys2

--- recipes/libatomic_ops/all/conanfile.py
+++ recipes/libatomic_ops/all/conanfile.py
@@ -40,9 +40,8 @@ class Libatomic_opsConan(ConanFile):
 
     def build_requirements(self):
         self.build_requires("gnu-config/cci.20201022")
-        if tools.os_info.is_windows and "CONAN_BASH_PATH" not in os.environ \
-                and tools.os_info.detect_windows_subsystem() != "msys2":
-            self.build_requires("msys2/20190524")
+        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/cci.latest")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
