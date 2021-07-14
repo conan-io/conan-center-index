54baccccdbfd51cb18e5ad3397f3d37ebb69078e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-14 19:32:38 +0200

    bump msys2

--- recipes/libdb/all/conanfile.py
+++ recipes/libdb/all/conanfile.py
@@ -62,9 +62,8 @@ class LibdbConan(ConanFile):
     def build_requirements(self):
         if self.settings.compiler != "Visual Studio":
             self.build_requires("gnu-config/cci.20201022")
-            if tools.os_info.is_windows and not "CONAN_BASH_PATH" in os.environ and \
-                    tools.os_info.detect_windows_subsystem() != "msys2":
-                self.build_requires("msys2/20190524")
+            if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+                self.build_requires("msys2/cci.latest")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
