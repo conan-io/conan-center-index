512ccaf91c7a871935324bf4cdeefa3e9166e52a
Alexey Klimkin <klimkin@gmail.com>
2021-07-14 16:39:10 -0700

    Update recipes/abseil/all/test_package/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/abseil/all/test_package/conanfile.py
+++ recipes/abseil/all/test_package/conanfile.py
@@ -7,8 +7,6 @@ class TestPackageConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        if not self.settings.compiler.cppstd:
-            cmake.definitions["CMAKE_CXX_STANDARD"] = 11
         cmake.configure()
         cmake.build()
 
