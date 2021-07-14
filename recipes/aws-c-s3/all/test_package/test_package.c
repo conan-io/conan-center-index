04b6c523098489094c43028cf487e7acb14a6212
Martin Delille <martin@delille.org>
2021-07-14 22:47:10 +0200

    aws-c-s3/0.1.19

---
+++ recipes/aws-c-s3/all/test_package/test_package.c
@@ -0,0 +1,9 @@
+#include <aws/s3/s3.h>
+
+int main() {
+    struct aws_allocator *allocator = aws_default_allocator();
+    aws_s3_library_init(allocator);
+    aws_s3_library_clean_up();
+
+    return EXIT_SUCCESS;
+}
