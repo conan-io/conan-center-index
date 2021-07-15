210a1e9670a6c3c189d48c4e278b179dc58ee9b2
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-15 18:15:24 +0200

    bump msys2

--- recipes/libfdk_aac/all/conanfile.py
+++ recipes/libfdk_aac/all/conanfile.py
@@ -24,9 +24,9 @@ class FDKAACConan(ConanFile):
             del self.options.fPIC
 
     def build_requirements(self):
-        if self._use_winbash and self.settings.compiler != 'Visual Studio':
-            if "CONAN_BASH_PATH" not in os.environ and tools.os_info.detect_windows_subsystem() != 'msys2':
-                self.build_requires("msys2/20190524")
+        if self.settings.compiler != "Visual Studio":
+            if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+                self.build_requires("msys2/cci.latest")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
