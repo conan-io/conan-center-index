55eb832ebd147289d78b5becdec297944b010623
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-15 17:12:59 +0200

    move checks to validate()

--- recipes/libev/all/conanfile.py
+++ recipes/libev/all/conanfile.py
@@ -36,6 +36,8 @@ class LibevConan(ConanFile):
     def configure(self):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
+
+    def validate(self):
         if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
             raise ConanInvalidConfiguration("libev is not supported by Visual Studio")
         if self.settings.os == "Windows" and self.options.shared:
