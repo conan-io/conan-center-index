854fd9567989aacdc0d614bc9e227cd84457632c
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2021-07-15 12:50:30 +0200

    move checks to validate method

--- recipes/effcee/all/conanfile.py
+++ recipes/effcee/all/conanfile.py
@@ -25,13 +25,15 @@ class EffceeConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
-    def configure(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "14")
+    def validate(self):
         if self.options.shared:
             del self.options.fPIC
             if self.settings.compiler == "Visual Studio" and "MT" in self.settings.compiler.runtime:
                 raise ConanInvalidConfiguration("Visual Studio build for shared library with MT runtime is not supported")
+
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, "14")
 
     def config_options(self):
         if self.settings.os == "Windows":
