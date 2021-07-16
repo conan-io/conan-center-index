0891a54c91c757034e77c134dea7454a9a4e25d9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-16 14:21:39 +0200

    no os.rename

--- recipes/libmodplug/all/conanfile.py
+++ recipes/libmodplug/all/conanfile.py
@@ -1,6 +1,8 @@
 from conans import ConanFile, CMake, tools
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class LibmodplugConan(ConanFile):
     name = "libmodplug"
@@ -32,10 +34,8 @@ class LibmodplugConan(ConanFile):
             del self.options.fPIC
 
     def source(self):
-        commit = os.path.splitext(os.path.basename(self.conan_data["sources"][self.version]["url"]))[0]
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + commit
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
