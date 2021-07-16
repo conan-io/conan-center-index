d6d300a900b3b72c3a7ca07b36547a79a6f691be
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-16 13:57:10 +0200

    cleanup

--- recipes/libmodbus/all/conanfile.py
+++ recipes/libmodbus/all/conanfile.py
@@ -27,10 +27,6 @@ class LibmodbusConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
