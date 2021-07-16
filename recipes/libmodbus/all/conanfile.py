0a2943b98dc5dd87292cf2fba74bd9d71d53542b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-16 13:58:46 +0200

    bump msys2 & automake

--- recipes/libmodbus/all/conanfile.py
+++ recipes/libmodbus/all/conanfile.py
@@ -39,16 +39,15 @@ class LibmodbusConan(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
+    def build_requirements(self):
+        if self.settings.compiler == "Visual Studio":
+            self.build_requires("automake/1.16.3")
+        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/cci.latest")
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
-
-    def build_requirements(self):
-        if self.settings.compiler == "Visual Studio":
-            self.build_requires("automake/1.16.1")
-        if tools.os_info.is_windows and "CONAN_BASH_PATH" not in os.environ \
-                and tools.os_info.detect_windows_subsystem() != "msys2":
-            self.build_requires("msys2/20190524")
 
     def _configure_autotools(self):
         if self._autotools:
