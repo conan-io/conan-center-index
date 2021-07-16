73b7d12f2c0a5e6e86d4ea258da1a4eb41bf37b2
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-07-16 19:22:52 +0200

    Update conanfile.py

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -904,8 +904,8 @@ class QtConan(ConanFile):
 
         for m in os.listdir(os.path.join("lib", "cmake")):
             module = os.path.join("lib", "cmake", m, "%sMacros.cmake" % m)
+            component_name = m.replace("Qt6", "qt")
             if os.path.isfile(module):
-                component_name = m.replace("Qt6", "qt")
                 self.cpp_info.components[component_name].build_modules["cmake_find_package"].append(module)
                 self.cpp_info.components[component_name].build_modules["cmake_find_package_multi"].append(module)
             self.cpp_info.components[component_name].builddirs.append(os.path.join("lib", "cmake", m))
