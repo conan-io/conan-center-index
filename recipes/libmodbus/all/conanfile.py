cbb92eb35991d68237f8c8759db3e36aaa851aef
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-16 18:39:24 +0200

    rely on settings_build

--- recipes/libmodbus/all/conanfile.py
+++ recipes/libmodbus/all/conanfile.py
@@ -29,6 +29,10 @@ class LibmodbusConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    @property
+    def _settings_build(self):
+        return self.settings_build if hasattr(self, "settings_build") else self.settings
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -42,7 +46,7 @@ class LibmodbusConan(ConanFile):
     def build_requirements(self):
         if self.settings.compiler == "Visual Studio":
             self.build_requires("automake/1.16.3")
-        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/cci.latest")
 
     def source(self):
