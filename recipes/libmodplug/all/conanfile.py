064dc9d4abbd963b04b710e5455841546d862509
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-16 14:20:55 +0200

    cache cmake

--- recipes/libmodplug/all/conanfile.py
+++ recipes/libmodplug/all/conanfile.py
@@ -10,15 +10,22 @@ class LibmodplugConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "http://modplug-xmms.sourceforge.net"
     license = "Unlicense"  # public domain
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
 
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -31,9 +38,11 @@ class LibmodplugConan(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
 
     def build(self):
         cmake = self._configure_cmake()
