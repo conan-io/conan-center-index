cb45d25b6caedf40d146fbf8962dc3d8bc3c5c76
boussaffawalid <boussaffa.walid@outlook.com>
2021-07-16 08:50:27 +0100

    Update recipes/openjdk/all/test_package/conanfile.py
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/openjdk/all/test_package/conanfile.py
+++ recipes/openjdk/all/test_package/conanfile.py
@@ -1,18 +1,22 @@
 from conans import ConanFile
-from six import StringIO
+from conans.errors import ConanException
+from io import StringIO
+
+required_conan_version = ">=1.36.0"
 
 
 class TestPackage(ConanFile):
+    test_type = "build_requires"
 
     def build(self):
         pass # nothing to build, but tests should not warn
 
     def test(self):
-        test_cmd = ['java', '--version']
         output = StringIO()
-        self.run(test_cmd, output=output)
+        self.run("java --version", output=output, run_environment=True)
+        print(output.getvalue)
         version_info = output.getvalue()
         if "openjdk" in version_info:
             pass
         else:
-            raise Exception("java call seems not use the openjdk bin")
+            raise ConanException("java call seems not use the openjdk bin")
