777d9a11330772cb7b37b746176bda0c62598cf9
Andrei Malashkin <malashkin.andrey@gmail.com>
2021-07-17 18:34:56 +0200

    Update recipes/effcee/all/conanfile.py
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/effcee/all/conanfile.py
+++ recipes/effcee/all/conanfile.py
@@ -31,10 +31,6 @@ class EffceeConan(ConanFile):
             if self.settings.compiler == "Visual Studio" and "MT" in self.settings.compiler.runtime:
                 raise ConanInvalidConfiguration("Visual Studio build for shared library with MT runtime is not supported")
 
-    def configure(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "14")
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -42,6 +38,10 @@ class EffceeConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, "14")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
