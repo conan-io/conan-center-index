43dea322503839c9f3724e78320dc89bd7c32ba6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-17 23:54:45 +0200

    explicit cpp_info.libs

--- recipes/libsvm/all/conanfile.py
+++ recipes/libsvm/all/conanfile.py
@@ -63,6 +63,6 @@ class libsvmConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["svm"]
         self.cpp_info.names["cmake_find_package"] = "LibSVM"
         self.cpp_info.names["cmake_find_package_multi"] = "LibSVM"
