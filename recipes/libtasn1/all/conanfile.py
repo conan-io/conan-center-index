6e1ab29457a1f6287decde4af9cc99e8366c7aed
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-18 00:25:45 +0200

    move checks to validate()

--- recipes/libtasn1/all/conanfile.py
+++ recipes/libtasn1/all/conanfile.py
@@ -34,10 +34,12 @@ class LibTasn1Conan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
-        if self.settings.compiler == "Visual Studio":
-            raise ConanInvalidConfiguration("Visual Studio is unsupported by libtasn1")
         del self.settings.compiler.cppstd
         del self.settings.compiler.libcxx
+
+    def validate(self):
+        if self.settings.compiler == "Visual Studio":
+            raise ConanInvalidConfiguration("Visual Studio is unsupported by libtasn1")
 
     def build_requirements(self):
         self.build_requires("bison/3.5.3")
