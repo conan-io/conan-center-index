fc14299995f38cfaa44cf9d872c0c3f7d206dc6f
Javier G. Sogo <jgsogo@gmail.com>
2021-07-17 17:58:15 +0200

    add test linking the lib

--- recipes/nas/all/test_package/test_package.c
+++ recipes/nas/all/test_package/test_package.c
@@ -1,9 +1,13 @@
 
 #include <stdio.h>
 
-#include <audio/audio.h>
+#include <audio/audiolib.h>
 
 int main(void)
 {
     printf("NAS test_package\n");
+
+
+    AuServer* aud = AuOpenServer(NULL, 0, NULL, 0, NULL, NULL);
+	return 0;
 }
