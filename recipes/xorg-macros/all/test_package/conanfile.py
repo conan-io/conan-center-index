3c8d2bed0d6077e5e9ae893df8e5f0136a76389f
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-07-17 23:50:40 +0200

    xorg-macros: fix test_package

--- recipes/xorg-macros/all/test_package/conanfile.py
+++ recipes/xorg-macros/all/test_package/conanfile.py
@@ -8,21 +8,21 @@ class TestPackageConan(ConanFile):
     exports_sources = "configure.ac", "Makefile.am",
     generators = "pkg_config"
 
+    @property
     def _settings_build(self):
         return self.settings_build if hasattr(self, "settings_build") else self.settings
 
     def build_requirements(self):
         self.build_requires("automake/1.16.3")
-        if self._settings_build == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/cci.latest")
 
     def build(self):
         for src in self.exports_sources:
             shutil.copy(os.path.join(self.source_folder, src), self.build_folder)
         print(tools.get_env("AUTOMAKE_CONAN_INCLUDES"))
-        self.run("{} --verbose".format(tools.get_env("ACLOCAL")), win_bash=self._settings_build == "Windows", run_environment=True)
-        self.run("{} -fiv".format(tools.get_env("AUTORECONF")), win_bash=self._settings_build == "Windows", run_environment=True)
-        autotools = AutoToolsBuildEnvironment(self, win_bash=self._settings_build == "Windows")
+        self.run("{} -fiv".format(tools.get_env("AUTORECONF")), win_bash=self._settings_build.os == "Windows", run_environment=True)
+        autotools = AutoToolsBuildEnvironment(self, win_bash=self._settings_build.os == "Windows")
         autotools.configure()
 
     def test(self):
