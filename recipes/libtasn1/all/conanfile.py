a87b7e5975387c6aa8b3d8924bdcf58957dfd17d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-18 00:26:39 +0200

    no os.rename

--- recipes/libtasn1/all/conanfile.py
+++ recipes/libtasn1/all/conanfile.py
@@ -2,6 +2,8 @@ from conans import ConanFile, tools, AutoToolsBuildEnvironment
 from conans.errors import ConanInvalidConfiguration
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class LibTasn1Conan(ConanFile):
     name = "libtasn1"
@@ -45,9 +47,8 @@ class LibTasn1Conan(ConanFile):
         self.build_requires("bison/3.5.3")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = os.path.join("libtasn1-" + self.version)
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_autotools(self):
         if self._autotools:
