018e0c4c094ae94a4cb1ba99bb453f051eb2d413
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-18 13:41:20 +0200

    no os.rename

--- recipes/libvpx/all/conanfile.py
+++ recipes/libvpx/all/conanfile.py
@@ -3,6 +3,8 @@ from conans.errors import ConanInvalidConfiguration
 import os
 import shutil
 
+required_conan_version = ">=1.33.0"
+
 
 class LibVPXConan(ConanFile):
     name = "libvpx"
@@ -43,9 +45,8 @@ class LibVPXConan(ConanFile):
             self.build_requires('msys2/20200517')
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_autotools(self):
         win_bash = tools.os_info.is_windows
