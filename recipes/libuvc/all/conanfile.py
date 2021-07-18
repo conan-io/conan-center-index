0c169bfab8e0c6203af41039c0b1ebe4c199a46e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-18 13:20:13 +0200

    no os.rename

--- recipes/libuvc/all/conanfile.py
+++ recipes/libuvc/all/conanfile.py
@@ -2,7 +2,8 @@ import os
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.28.0"
+required_conan_version = ">=1.33.0"
+
 
 class LibuvcConan(ConanFile):
     name = "libuvc"
@@ -49,9 +50,8 @@ class LibuvcConan(ConanFile):
             self.requires("libjpeg/9d")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if not self._cmake:
