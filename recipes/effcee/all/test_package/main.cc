e28e98f7fdeca22368b3d23e71fd192f56f28089
andrei.malashkin <andrei.malashkin@reactivereality.com>
2021-07-19 17:02:22 +0200

    correct review changes

--- recipes/effcee/all/test_package/main.cc
+++ recipes/effcee/all/test_package/main.cc
@@ -1,7 +1,7 @@
 #include "effcee/effcee.h"
 
 int main(void) {
-  const std::string = effcee::Match("foo bar qux", "foo",
+  auto match = effcee::Match("foo bar qux", "foo",
                               effcee::Options().SetChecksName("checks"));
   return 0;
 }
