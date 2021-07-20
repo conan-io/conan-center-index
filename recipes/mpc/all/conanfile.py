e5bebf829af4233a3cc38480fd508a3689d11c86
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-20 21:35:42 +0200

    fix build with MinGW@Windows

--- recipes/mpc/all/conanfile.py
+++ recipes/mpc/all/conanfile.py
@@ -35,9 +35,17 @@ class MpcConan(ConanFile):
     def validate(self):
         if self.settings.compiler == "Visual Studio":
             raise ConanInvalidConfiguration("The mpc package cannot be built on Visual Studio.")
-            
+
     def requirements(self):
         self.requires("mpfr/4.1.0")
+
+    @property
+    def _settings_build(self):
+        return self.settings_build if hasattr(self, "settings_build") else self.settings
+
+    def build_requirements(self):
+        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/cci.latest")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
@@ -45,7 +53,7 @@ class MpcConan(ConanFile):
     def _configure_autotools(self):
         if self._autotools:
             return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self)
+        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
         args = []
         if self.options.shared:
             args.extend(["--disable-static", "--enable-shared"])
