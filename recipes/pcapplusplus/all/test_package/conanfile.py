14b3d590c3d38ad476d02fb6e090e8632ec5e568
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-07-20 13:27:55 +0200

    pcapplusplus: pass CPPFLAGS+CXXFLAGS to 3rd party sources

--- recipes/pcapplusplus/all/test_package/conanfile.py
+++ recipes/pcapplusplus/all/test_package/conanfile.py
@@ -1,21 +1,19 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
-import logging
 
 
 class PcapplusplusTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
- 
-    def configure(self):
-        del self.settings.compiler.libcxx
 
     def build(self):
         cmake = CMake(self)
+        cmake.verbose = True
         cmake.configure()
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        pcap_file_path = os.path.join(self.source_folder, "1_packet.pcap")
-        self.run("{0} {1}".format(bin_path, pcap_file_path), run_environment=True)
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            pcap_file_path = os.path.join(self.source_folder, "1_packet.pcap")
+            self.run("{0} {1}".format(bin_path, pcap_file_path), run_environment=True)
