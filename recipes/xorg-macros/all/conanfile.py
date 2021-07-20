95c3df1df4ab14c2004ee67dca4aa46120b32132
Anonymous Maarten <madebr@users.noreply.github.com>
2021-07-20 22:55:31 +0200

    xorg-macros: is header-only + use automake/1.16.3

--- recipes/xorg-macros/all/conanfile.py
+++ recipes/xorg-macros/all/conanfile.py
@@ -32,7 +32,10 @@ class XorgMacrosConan(ConanFile):
     def build_requirements(self):
         if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/cci.latest")
-        self.build_requires("automake/1.16.2")
+        self.build_requires("automake/1.16.3")
+
+    def package_id(self):
+        self.info.header_only()
 
     @property
     def _datarootdir(self):
