cbca25549b1c4850e61630b2c6e8ab1f472d32af
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-21 00:22:45 +0200

    no os.rename

--- recipes/netcdf/all/conanfile.py
+++ recipes/netcdf/all/conanfile.py
@@ -2,6 +2,8 @@ from conans import CMake, ConanFile, tools
 import glob
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class NetcdfConan(ConanFile):
     name = "netcdf"
@@ -61,8 +63,8 @@ class NetcdfConan(ConanFile):
             self.requires("libcurl/7.77.0")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("netcdf-c-{}".format(self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
