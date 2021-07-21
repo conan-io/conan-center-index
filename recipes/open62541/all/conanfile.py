581c697718aaecddd6b9f215f870f3d3fe64827c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-22 01:18:17 +0200

    multilines for the very long description

--- recipes/open62541/all/conanfile.py
+++ recipes/open62541/all/conanfile.py
@@ -12,7 +12,14 @@ class Open62541Conan(ConanFile):
     license = "MPLv2"
     homepage = "https://open62541.org/"
     url = "https://github.com/conan-io/conan-center-index"
-    description = "open62541 is an open source and free implementation of OPC UA (OPC Unified Architecture) written in the common subset of the C99 and C++98 languages. The library is usable with all major compilers and provides the necessary tools to implement dedicated OPC UA clients and servers, or to integrate OPC UA-based communication into existing applications. open62541 library is platform independent. All platform-specific functionality is implemented via exchangeable plugins. Plugin implementations are provided for the major operating systems."
+    description = "open62541 is an open source and free implementation of OPC UA " \
+                  "(OPC Unified Architecture) written in the common subset of the " \
+                  "C99 and C++98 languages. The library is usable with all major " \
+                  "compilers and provides the necessary tools to implement dedicated " \
+                  "OPC UA clients and servers, or to integrate OPC UA-based communication " \
+                  "into existing applications. open62541 library is platform independent. " \
+                  "All platform-specific functionality is implemented via exchangeable " \
+                  "plugins. Plugin implementations are provided for the major operating systems."
     topics = (
         "OPC UA", "open62541", "sdk", "server/client", "c", "iec-62541",
         "industrial automation", "tsn", "time sensetive networks", "publish-subscirbe", "pubsub"
