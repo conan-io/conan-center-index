e9b5d82aeb52a6f4b5b110a8a336dc3136d8f35e
César García Tapia <cesar.garcia.tapia@gmail.com>
2021-07-23 14:42:22 +0200

    Migrate the fmt recipe to use the new CMake toolchain

--- recipes/fmt/all/conanfile.py
+++ recipes/fmt/all/conanfile.py
@@ -1,6 +1,9 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
 import os
+
+from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.layout import cmake_layout
 
 required_conan_version = ">=1.33.0"
 
@@ -9,11 +12,10 @@ class FmtConan(ConanFile):
     name = "fmt"
     homepage = "https://github.com/fmtlib/fmt"
     description = "A safe and fast alternative to printf and IOStreams."
-    topics = ("conan", "fmt", "format", "iostream", "printf")
+    topics = ("fmt", "format", "iostream", "printf")
     url = "https://github.com/conan-io/conan-center-index"
     license = "MIT"
-    exports_sources = ["CMakeLists.txt", "patches/**"]
-    generators = "cmake"
+    exports_sources = ["patches/**"]
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "header_only": [True, False],
@@ -29,16 +31,6 @@ class FmtConan(ConanFile):
         "with_fmt_alias": False,
         "with_os_api": True,
     }
-
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
 
     @property
     def _has_with_os_api_option(self):
@@ -71,44 +63,48 @@ class FmtConan(ConanFile):
             del self.info.options.with_fmt_alias
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        tools.get(**self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["FMT_DOC"] = False
-        self._cmake.definitions["FMT_TEST"] = False
-        self._cmake.definitions["FMT_INSTALL"] = True
-        self._cmake.definitions["FMT_LIB_DIR"] = "lib"
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        cmake_toolchain = CMakeToolchain(self)
+
+        cmake_toolchain.variables["FMT_DOC"] = False
+        cmake_toolchain.variables["FMT_TEST"] = False
+        cmake_toolchain.variables["FMT_INSTALL"] = True
+        cmake_toolchain.variables["FMT_LIB_DIR"] = "lib"
+
         if self._has_with_os_api_option:
-            self._cmake.definitions["FMT_OS"] = self.options.with_os_api
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+            cmake_toolchain.variables["FMT_OS"] = self.options.with_os_api
+
+        cmake_toolchain.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+        for patch in self.conan_data["patches"].get(self.version, []):
             tools.patch(**patch)
+
         if not self.options.header_only:
-            cmake = self._configure_cmake()
+            cmake = CMake(self)
+            cmake.configure()
             cmake.build()
 
     def package(self):
-        self.copy("LICENSE.rst", dst="licenses", src=self._source_subfolder)
+        self.copy("LICENSE.rst", dst="licenses")
         if self.options.header_only:
-            self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
+            self.copy("*.h", dst="include")
         else:
-            cmake = self._configure_cmake()
+            cmake = CMake(self)
             cmake.install()
             tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
             tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
             tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "fmt"
-        self.cpp_info.names["cmake_find_package_multi"] = "fmt"
-        self.cpp_info.names["pkg_config"] = "fmt"
+        self.cpp_info.set_property("cmake_find_package", "fmt")
+        self.cpp_info.set_property("cmake_find_package_multi", "fmt")
+        self.cpp_info.set_property("pkg_config", "fmt")
         if self.options.header_only:
             self.cpp_info.components["fmt-header-only"].defines.append("FMT_HEADER_ONLY=1")
             if self.options.with_fmt_alias:
--- recipes/fmt/all/test_package/conanfile.py
+++ recipes/fmt/all/test_package/conanfile.py
@@ -1,18 +1,27 @@
 import os
-from conans import ConanFile, CMake, tools
+
+from conans import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.layout import cmake_layout
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package", "cmake"
+    generators = "CMakeDeps", "CMakeToolchain", "PkgConfigDeps", "VirtualRunEnv"
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["FMT_HEADER_ONLY"] = self.options["fmt"].header_only
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
-            self.run(os.path.join("bin","test_package"), run_environment=True)
-            self.run(os.path.join("bin","test_ranges"), run_environment=True)
+            # FIXME: Very ugly interface to get the current test executable path
+            cmd = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(cmd, env=["conanrunenv"])
+
+            cmd = os.path.join(self.cpp.build.bindirs[0], "test_ranges")
+            self.run(cmd, env=["conanrunenv"])
