67a54e707715f37d0babf331e8f4e2b66d0c7272
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-24 19:08:00 +0200

    no os.rename

--- recipes/pro-mdnsd/all/conanfile.py
+++ recipes/pro-mdnsd/all/conanfile.py
@@ -1,6 +1,7 @@
-import os
 from conans import ConanFile, CMake, tools
-from conans.tools import download, unzip
+import os
+
+required_conan_version = ">=1.33.0"
 
 
 class mdnsdConan(ConanFile):
@@ -43,9 +44,8 @@ class mdnsdConan(ConanFile):
             del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        folder_name = "mdnsd-" + self.version
-        os.rename(folder_name, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
