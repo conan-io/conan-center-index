f97701142c5350187b26bba77f489f40b93ae5d9
dab0bby <9976654+dab0bby@users.noreply.github.com>
2021-07-24 18:42:35 +0200

    bumps sqlite3 dependency

--- recipes/poco/all/conanfile.py
+++ recipes/poco/all/conanfile.py
@@ -149,7 +149,7 @@ class PocoConan(ConanFile):
         if self.options.enable_xml:
             self.requires("expat/2.4.1")
         if self.options.enable_data_sqlite:
-            self.requires("sqlite3/3.35.5")
+            self.requires("sqlite3/3.36.0")
         if self.options.enable_apacheconnector:
             self.requires("apr/1.7.0")
             self.requires("apr-util/1.6.1")
