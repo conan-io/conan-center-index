5734459c5987908473fa1da6860191c3360ada72
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-07-25 02:32:32 +0200

    xorg-macros: increase Windows support

--- recipes/xorg-proto/all/conanfile.py
+++ recipes/xorg-proto/all/conanfile.py
@@ -1,4 +1,5 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
+import contextlib
 import glob
 import os
 import re
@@ -29,29 +30,57 @@ class XorgProtoConan(ConanFile):
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
+    @property
+    def _user_info_build(self):
+        return getattr(self, "user_info_build", self.deps_user_info)
+
     def build_requirements(self):
+        self.build_requires("automake/1.16.3")
         self.build_requires("xorg-macros/1.19.3")
         self.build_requires("pkgconf/1.7.4")
+        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/cci.latest")
+
+    def requirements(self):
+        if hasattr(self, "settings_build"):
+            self.requires("xorg-macros/1.19.3")
 
     def package_id(self):
-        self.info.header_only()
+        # self.info.header_only() would be fine too, but keep the os to add c3i test coverage for Windows.
+        del self.info.settings.arch
+        del self.info.settings.build_type
+        del self.info.settings.compiler
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
+    @contextlib.contextmanager
+    def _build_context(self):
+        if self.settings.compiler == "Visual Studio":
+            with tools.vcvars(self.settings):
+                env = {
+                    "CC": "{} cl -nologo".format(self._user_info_build["automake"].compile).replace("\\", "/"),
+                }
+                with tools.environment_append(env):
+                    yield
+        else:
+            yield
+
     def _configure_autotools(self):
         if self._autotools:
             return self._autotools
         self._autotools = AutoToolsBuildEnvironment(self, win_bash=self._settings_build.os == "Windows")
+        self._autotools.libs = []
         self._autotools.configure(configure_dir=self._source_subfolder)
         return self._autotools
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
-        autotools = self._configure_autotools()
-        autotools.make()
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.make()
 
     @property
     def _pc_data_path(self):
@@ -59,8 +88,9 @@ class XorgProtoConan(ConanFile):
 
     def package(self):
         self.copy("COPYING-*", src=self._source_subfolder, dst="licenses")
-        autotools = self._configure_autotools()
-        autotools.install()
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.install()
 
         pc_data = {}
         for fn in glob.glob(os.path.join(self.package_folder, "share", "pkgconfig", "*.pc")):
