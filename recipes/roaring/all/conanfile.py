2743d84e64ee2837b602b7056d0cf2d2e41a9b5c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-25 13:16:34 +0200

    move checks to validate()

--- recipes/roaring/all/conanfile.py
+++ recipes/roaring/all/conanfile.py
@@ -38,8 +38,10 @@ class ConanRecipe(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+
+    def validate(self):
         if self.settings.compiler.cppstd:
-            check_min_cppstd(self, "11")
+            tools.check_min_cppstd(self, "11")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
