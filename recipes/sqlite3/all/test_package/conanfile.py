f904c6b1d678d87f0a7762992e86f48cc6e4ec27
reymondzzzz <starkov.kirill123@gmail.com>
2021-07-26 19:28:19 +0300

    T16399: add components in sqlite3 recipe

--- recipes/sqlite3/all/test_package/conanfile.py
+++ recipes/sqlite3/all/test_package/conanfile.py
@@ -4,10 +4,11 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["USE_EMPTY_VFS"] = not self.options["sqlite3"].enable_default_vfs
         cmake.configure()
         cmake.build()
 
