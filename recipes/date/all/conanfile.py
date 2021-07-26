e2620eaebe6a55e4cbe537f668739e8d5604e022
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-26 14:34:20 +0200

    conventions

--- recipes/date/all/conanfile.py
+++ recipes/date/all/conanfile.py
@@ -12,20 +12,25 @@ class DateConan(ConanFile):
     topics = ("date", "datetime", "timezone",
               "calendar", "time", "iana-database")
     license = "MIT"
-    exports_sources = ["patches/*", "CMakeLists.txt"]
-    settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package"
-    options = {"shared": [True, False],
-               "fPIC": [True, False],
-               "header_only": [True, False],
-               "use_system_tz_db": [True, False],
-               "use_tz_db_in_dot": [True, False]}
-    default_options = {"shared": False,
-                       "fPIC": True,
-                       "header_only": False,
-                       "use_system_tz_db": False,
-                       "use_tz_db_in_dot": False}
 
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "header_only": [True, False],
+        "use_system_tz_db": [True, False],
+        "use_tz_db_in_dot": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "header_only": False,
+        "use_system_tz_db": False,
+        "use_tz_db_in_dot": False,
+    }
+
+    exports_sources = ["patches/*", "CMakeLists.txt"]
+    generators = "cmake", "cmake_find_package"
     _cmake = None
 
     @property
@@ -35,23 +40,6 @@ class DateConan(ConanFile):
     @property
     def _build_subfolder(self):
         return "build_subfolder"
-
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        cmake = CMake(self)
-        cmake.definitions["ENABLE_DATE_TESTING"] = False
-        cmake.definitions["USE_SYSTEM_TZ_DB"] = self.options.use_system_tz_db
-        cmake.definitions["USE_TZ_DB_IN_DOT"] = self.options.use_tz_db_in_dot
-        cmake.definitions["BUILD_TZ_LIB"] = not self.options.header_only
-        # workaround for clang 5 not having string_view
-        if tools.Version(self.version) >= "3.0.0" and self.settings.compiler == "clang" \
-                and tools.Version(self.settings.compiler.version) <= "5.0":
-            cmake.definitions["DISABLE_STRING_VIEW"] = True
-        cmake.configure()
-
-        self._cmake = cmake
-        return self._cmake
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -69,9 +57,30 @@ class DateConan(ConanFile):
         if not self.options.use_system_tz_db:
             self.requires("libcurl/7.69.1")
 
+    def package_id(self):
+        if self.options.header_only:
+            self.info.header_only()
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        cmake = CMake(self)
+        cmake.definitions["ENABLE_DATE_TESTING"] = False
+        cmake.definitions["USE_SYSTEM_TZ_DB"] = self.options.use_system_tz_db
+        cmake.definitions["USE_TZ_DB_IN_DOT"] = self.options.use_tz_db_in_dot
+        cmake.definitions["BUILD_TZ_LIB"] = not self.options.header_only
+        # workaround for clang 5 not having string_view
+        if tools.Version(self.version) >= "3.0.0" and self.settings.compiler == "clang" \
+                and tools.Version(self.settings.compiler.version) <= "5.0":
+            cmake.definitions["DISABLE_STRING_VIEW"] = True
+        cmake.configure()
+
+        self._cmake = cmake
+        return self._cmake
 
     def build(self):
         for patch in self.conan_data["patches"][self.version]:
@@ -119,7 +128,3 @@ class DateConan(ConanFile):
             defines.append("DATE_USE_DLL=1")
 
         self.cpp_info.defines.extend(defines)
-
-    def package_id(self):
-        if self.options.header_only:
-            self.info.header_only()
