2a877d39073993a9ae0c3686197f7063134cb8da
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-26 20:18:30 +0200

    no os.rename

--- recipes/sundials/all/conanfile.py
+++ recipes/sundials/all/conanfile.py
@@ -3,6 +3,8 @@ import glob
 import os
 import shutil
 
+required_conan_version = ">=1.33.0"
+
 
 class SundialsConan(ConanFile):
     name = "sundials"
@@ -51,9 +53,8 @@ class SundialsConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
