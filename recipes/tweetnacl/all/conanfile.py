788d37f7b28a3461f8b15f0179cd8b05666fbf52
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-27 18:54:13 +0200

    explicit cpp_info.libs

--- recipes/tweetnacl/all/conanfile.py
+++ recipes/tweetnacl/all/conanfile.py
@@ -63,4 +63,4 @@ class TweetnaclConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["tweetnacl"]
