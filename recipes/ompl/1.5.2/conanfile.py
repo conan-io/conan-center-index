2764f17695093f1327c0fce65b1b4521d4bd184f
Mohamed Ghita <mohamed.ghita@radalytica.com>
2021-07-27 10:11:46 +0200

    ompl: fix conan attributes url, author

--- recipes/ompl/1.5.2/conanfile.py
+++ recipes/ompl/1.5.2/conanfile.py
@@ -2,10 +2,12 @@ from conans import ConanFile, tools, CMake
 import os
 import glob
 
-BOOST_MODULES = ["system","filesystem","program_options","serialization","test"]
-BOOST_UNUSED_MODULES = ["atomic","chrono","container","context","contract","coroutine","date_time","exception",
-                        "fiber","graph","graph_parallel","iostreams","locale","log","math","mpi","python","random",
-                        "regex","stacktrace","thread","timer","type_erasure","wave"]
+BOOST_MODULES = ["system", "filesystem",
+                 "program_options", "serialization", "test"]
+BOOST_UNUSED_MODULES = ["atomic", "chrono", "container", "context", "contract", "coroutine", "date_time", "exception",
+                        "fiber", "graph", "graph_parallel", "iostreams", "locale", "log", "math", "mpi", "python", "random",
+                        "regex", "stacktrace", "thread", "timer", "type_erasure", "wave"]
+
 
 class ConanOmpl(ConanFile):
     name = "ompl"
@@ -13,14 +15,15 @@ class ConanOmpl(ConanFile):
     license = "BSD"
     homepage = "https://ompl.kavrakilab.org/"
     description = "OMPL, the Open Motion Planning Library, consists of many state-of-the-art sampling-based motion planning algorithms."
-    url = "https://bitbucket.org/radalytica/conan-packages"
-    author = "Mohamed Ghita (https://github.com/mohamedghita)"
+    url = "https://github.com/conan-io/conan-center-index"
+    topics = ("ompl", "motion-planning", "robotics", "collision")
     generators = "cmake_find_package", "cmake_paths", "cmake"
     BOOST_VERSION = "1.72.0"
     requires = "boost/{}".format(BOOST_VERSION), "eigen/3.3.9"
 
     settings = 'os', 'compiler', 'build_type', 'arch'
-    options = {"shared": [True, False], "run_tests": [True, False], "fPIC": [True, False],}
+    options = {"shared": [True, False], "run_tests": [
+        True, False], "fPIC": [True, False]}
     default_options = {"shared": True, "run_tests": False, "fPIC": True}
 
     def configure(self):
@@ -38,27 +41,29 @@ class ConanOmpl(ConanFile):
         return "ompl-{}".format(self.version)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
-        tools.replace_in_file("{}/CMakeLists.txt".format(self._source_subfolder), 
-                        "project(ompl VERSION {} LANGUAGES CXX)".format(self.version),
-                        'project(ompl VERSION {} LANGUAGES CXX)\n'.format(self.version) +
-                        'include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\n' +
-                        'conan_basic_setup(KEEP_RPATHS)\n' + 
-                        'include(${CMAKE_BINARY_DIR}/conan_paths.cmake)\n')
+        tools.replace_in_file("{}/CMakeLists.txt".format(self._source_subfolder),
+                              "project(ompl VERSION {} LANGUAGES CXX)".format(
+                                  self.version),
+                              'project(ompl VERSION {} LANGUAGES CXX)\n'.format(self.version) +
+                              'include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\n' +
+                              'conan_basic_setup(KEEP_RPATHS)\n' +
+                              'include(${CMAKE_BINARY_DIR}/conan_paths.cmake)\n')
 
-        tools.replace_in_file("{}/CMakeLists.txt".format(self._source_subfolder), 
-                        "find_package(Boost 1.58 QUIET REQUIRED COMPONENTS serialization filesystem system program_options)",
-                        'find_package(Boost {} REQUIRED)\n'.format(self.BOOST_VERSION) +     
-                        'set(Boost_SERIALIZATION_LIBRARY "${Boost_LIBRARIES_TARGETS}")\n' + 
-                        'set(Boost_FILESYSTEM_LIBRARY "${Boost_LIBRARIES_TARGETS}")\n' + 
-                        'set(Boost_SYSTEM_LIBRARY "${Boost_LIBRARIES_TARGETS}")\n' +
-                        'set(Boost_LIBRARY_DIRS " ")')
+        tools.replace_in_file("{}/CMakeLists.txt".format(self._source_subfolder),
+                              "find_package(Boost 1.58 QUIET REQUIRED COMPONENTS serialization filesystem system program_options)",
+                              'find_package(Boost {} REQUIRED)\n'.format(self.BOOST_VERSION) +
+                              'set(Boost_SERIALIZATION_LIBRARY "${Boost_LIBRARIES_TARGETS}")\n' +
+                              'set(Boost_FILESYSTEM_LIBRARY "${Boost_LIBRARIES_TARGETS}")\n' +
+                              'set(Boost_SYSTEM_LIBRARY "${Boost_LIBRARIES_TARGETS}")\n' +
+                              'set(Boost_LIBRARY_DIRS " ")')
 
-        tools.replace_in_file("{}/CMakeLists.txt".format(self._source_subfolder), 
-                "find_package(Eigen3 REQUIRED)", 
-                'find_package(Eigen3 REQUIRED)\n' +
-                'set(EIGEN3_INCLUDE_DIR ${Eigen3_INCLUDE_DIR})')
+        tools.replace_in_file("{}/CMakeLists.txt".format(self._source_subfolder),
+                              "find_package(Eigen3 REQUIRED)",
+                              'find_package(Eigen3 REQUIRED)\n' +
+                              'set(EIGEN3_INCLUDE_DIR ${Eigen3_INCLUDE_DIR})')
 
     def _configure_cmake(self):
         cmake = CMake(self)
@@ -66,15 +71,19 @@ class ConanOmpl(ConanFile):
         cmake.definitions["OMPL_BUILD_TESTS"] = self.options.run_tests
         cmake.definitions["OMPL_BUILD_DEMOS"] = self.options.run_tests
 
-        ignored_packages = ["pypy","flann","ODE", "spot","MORSE","Triangle"]
+        ignored_packages = ["pypy", "flann",
+                            "ODE", "spot", "MORSE", "Triangle"]
         for pkg in ignored_packages:
-            cmake.definitions["CMAKE_DISABLE_FIND_PACKAGE_{}".format(pkg)] = "ON" 
+            cmake.definitions["CMAKE_DISABLE_FIND_PACKAGE_{}".format(
+                pkg)] = "ON"
 
-        disabled_options = ["BUILD_PYBINDINGS","BUILD_PYTESTS","REGISTRATION","VERSIONED_INSTALL"]
+        disabled_options = ["BUILD_PYBINDINGS",
+                            "BUILD_PYTESTS", "REGISTRATION", "VERSIONED_INSTALL"]
         for opt in disabled_options:
             cmake.definitions["OMPL_{}".format(opt)] = "OFF"
 
-        cmake.configure(source_folder=os.path.join(self.build_folder, self._source_subfolder))
+        cmake.configure(source_folder=os.path.join(
+            self.build_folder, self._source_subfolder))
         return cmake
 
     def build(self):
