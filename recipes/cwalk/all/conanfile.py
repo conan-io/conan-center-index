efafa1c22f7e9f9c9cacf364a61753b2a3e8ed0a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-28 15:12:17 +0200

    fix CMake names
    since 1.2.5, a CMake config file is installed with an imported target (not namespaced)

--- recipes/cwalk/all/conanfile.py
+++ recipes/cwalk/all/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, CMake, tools
 import os
+import textwrap
 
 required_conan_version = ">=1.33.0"
 
@@ -64,8 +65,40 @@ class CwalkConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"cwalk": "cwalk::cwalk"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
+        self.cpp_info.filenames["cmake_find_package"] = "Cwalk"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "Cwalk"
+        self.cpp_info.names["cmake_find_package"] = "cwalk"
+        self.cpp_info.names["cmake_find_package_multi"] = "cwalk"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
         self.cpp_info.libs = ["cwalk"]
         if self.options.shared and tools.Version(self.version) >= "1.2.5":
             self.cpp_info.defines.append("CWK_SHARED")
