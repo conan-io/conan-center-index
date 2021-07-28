f0f4844839a06a77f4b6696ed8b19bfbad0c62f6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-28 14:31:28 +0200

    no os.rename

--- recipes/cwalk/all/conanfile.py
+++ recipes/cwalk/all/conanfile.py
@@ -1,5 +1,7 @@
-import os
 from conans import ConanFile, CMake, tools
+import os
+
+required_conan_version = ">=1.33.0"
 
 
 class CwalkConan(ConanFile):
@@ -39,8 +41,8 @@ class CwalkConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename(self.name + "-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def build(self):
         cmake = CMake(self)
