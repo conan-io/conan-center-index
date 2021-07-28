fd8d1dcaaba545adef6b0168fcad5715c49fccad
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-28 14:30:35 +0200

    delete fPIC option if shared

--- recipes/cwalk/all/conanfile.py
+++ recipes/cwalk/all/conanfile.py
@@ -33,6 +33,8 @@ class CwalkConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
