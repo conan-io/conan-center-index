5493710543968dd32ef0adcbb67006bd5d591fb8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-28 02:16:20 +0200

    cleanup & conventions

--- recipes/zopfli/all/conanfile.py
+++ recipes/zopfli/all/conanfile.py
@@ -11,25 +11,28 @@ class ZopfliConan(ConanFile):
     homepage = "https://github.com/google/zopfli/"
     description = "Zopfli Compression Algorithm is a compression library programmed in C to perform very good, but slow, deflate or zlib compression."
     topics = ("conan", )
+
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False],
-               "fPIC": [True, False]}
-    default_options = {"shared": False,
-                       "fPIC": True}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
     exports_sources = ["CMakeLists.txt"]
-    generators = "cmake",
+    generators = "cmake"
     _cmake = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
     def configure(self):
         if self.options.shared:
@@ -37,15 +40,14 @@ class ZopfliConan(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
         cmake = CMake(self)
-        cmake.definitions['ZOPFLI_BUILD_INSTALL'] = True
+        cmake.definitions["ZOPFLI_BUILD_INSTALL"] = True
         cmake.definitions["CMAKE_MACOSX_BUNDLE"] = False
         cmake.configure()
         self._cmake = cmake
@@ -61,7 +63,6 @@ class ZopfliConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "ZopFli"
