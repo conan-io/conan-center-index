7695557f15796b9a45828007a6deb6ca797aac3b
theirix <theirix@gmail.com>
2021-07-28 19:03:09 +0300

    (#6496) celero: add 2.8.2, export without namespace, modernize
    * Add 2.8.2, export without namespace, modernize
    
    * Fix runtime on MSVC
    
    * Drop msvc runtime adjustments

--- recipes/celero/all/conanfile.py
+++ recipes/celero/all/conanfile.py
@@ -1,9 +1,9 @@
 import os
-
+import textwrap
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.28.0"
+required_conan_version = ">=1.33.0"
 
 class CeleroConan(ConanFile):
     name = "celero"
@@ -49,8 +49,7 @@ class CeleroConan(ConanFile):
             raise ConanInvalidConfiguration("celero requires C++14, which your compiler does not support.")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("Celero-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -76,17 +75,44 @@ class CeleroConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "share"))
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"celero": "celero::celero"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
-        # FIXME: official CMake target is exported without namespace
         self.cpp_info.filenames["cmake_find_package"] = "Celero"
         self.cpp_info.filenames["cmake_find_package_multi"] = "Celero"
         self.cpp_info.names["cmake_find_package"] = "celero"
         self.cpp_info.names["cmake_find_package_multi"] = "celero"
         self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
         if not self.options.shared:
             self.cpp_info.defines = ["CELERO_STATIC"]
-        if self.settings.os == "Linux":
+        if self.settings.os in ("FreeBSD", "Linux"):
             self.cpp_info.system_libs = ["pthread"]
         elif self.settings.os == "Windows":
             self.cpp_info.system_libs = ["powrprof", "psapi"]
