c0a1d8b8ecadebe1879cbaeee6a2340e0c59407c
Martin Delille <martin@delille.org>
2021-07-29 13:13:05 +0200

    Manual collect

--- recipes/aaf/all/conanfile.py
+++ recipes/aaf/all/conanfile.py
@@ -76,6 +76,6 @@ class AafConan(ConanFile):
         self.copy("LEGAL/AAFSDKPSL.TXT", dst="licenses", src=self._source_subfolder, keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["aaflib", "aafiid", "com-api"]
         if self.settings.os in ("FreeBSD", "Linux"):
             self.cpp_info.system_libs = ["dl"]
