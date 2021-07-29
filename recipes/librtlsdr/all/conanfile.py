6a6708f156fbcccab729a9510bb57b40e5992324
Rui Oliveira <ruimail24@gmail.com>
2021-07-29 16:09:04 +0100

    Remove a wrong check

--- recipes/librtlsdr/all/conanfile.py
+++ recipes/librtlsdr/all/conanfile.py
@@ -122,8 +122,7 @@ class LibRtlSdrConan(ConanFile):
             self.cpp_info.defines.extend(["rtlsdr_STATIC=1"])
         # Necessary to libusb
         if self.settings.os in ["Linux", "Android"]:
-            if self.options.enable_udev:
-                self.cpp_info.system_libs.append("udev")
+            self.cpp_info.system_libs.append("udev")
         elif self.settings.os == "Macos":
             self.cpp_info.system_libs = ["objc"]
             self.cpp_info.frameworks = ["IOKit", "CoreFoundation"]
