6953e89aad4abd96dea80a36d3325c132746d4b6
maspri <16707185+maspri@users.noreply.github.com>
2021-07-29 12:49:00 +0200

    add csv-parser/2.1.3

---
+++ recipes/csv-parser/all/conanfile.py
@@ -0,0 +1,36 @@
+from conans import ConanFile, tools
+import os
+
+required_conan_version = ">=1.33.0"
+
+
+class CsvParserConan(ConanFile):
+    name = "csv-parser"
+    description = "Vince's CSV Parser with simple and intuitive syntax"
+    topics = ("conan", "csv-parser", "csv", "rfc 4180", "parser", "generator")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/vincentlaucsb/csv-parser"
+    license = "MIT"
+    settings = "os"
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*", dst="include", src=os.path.join(self._source_subfolder, "single_include"))
+
+    def package_id(self):
+        self.info.header_only()
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "CSVParser"
+        self.cpp_info.names["cmake_find_package_multi"] = "CSVParser"
+
+        if self.settings.os == "Linux" or self.settings.os == "FreeBSD":
+            self.cpp_info.system_libs.append("pthread")
