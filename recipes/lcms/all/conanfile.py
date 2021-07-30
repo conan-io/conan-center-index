72c268c20f6de08adb464721f91417ccdac1e644
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-07-30 13:34:41 +0200

    lcms: only do LTO in Visual Studio when CFLAGS contains /GL

--- recipes/lcms/all/conanfile.py
+++ recipes/lcms/all/conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild
 from conans.tools import Version
 import os
+import re
 import shutil
 
 required_conan_version = ">=1.33.0"
@@ -14,14 +15,28 @@ class LcmsConan(ConanFile):
     homepage = "https://github.com/mm2/Little-CMS"
     topics = ("conan", "lcms", "cmm", "icc", "cmm-engine")
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
 
     _autotools = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
+    @property
+    def _user_info_build(self):
+        return getattr(self, "user_info_build", self.deps_user_info)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -36,7 +51,7 @@ class LcmsConan(ConanFile):
     def build_requirements(self):
         if self.settings.compiler != "Visual Studio":
             self.build_requires("gnu-config/cci.20201022")
-            if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
                 self.build_requires("msys2/cci.latest")
 
     def source(self):
@@ -48,7 +63,7 @@ class LcmsConan(ConanFile):
             # since VS2015 vsnprintf is built-in
             path = os.path.join(self._source_subfolder, "src", "lcms2_internal.h")
             tools.replace_in_file(path, "#       define vsnprintf  _vsnprintf", "")
-        if self.settings.os == "Android" and tools.os_info.is_windows:
+        if self.settings.os == "Android" and self._settings_build.os == "Windows":
             # remove escape for quotation marks, to make ndk on windows happy
             tools.replace_in_file(os.path.join(self._source_subfolder, "configure"),
                                   "s/[	 `~#$^&*(){}\\\\|;'\\\''\"<>?]/\\\\&/g",
@@ -58,31 +73,27 @@ class LcmsConan(ConanFile):
         with tools.chdir(os.path.join(self._source_subfolder, "Projects", "VC2013")):
             target = "lcms2_DLL" if self.options.shared else "lcms2_static"
             upgrade_project = Version(self.settings.compiler.version) > "12"
+            properties = {
+                # Enable LTO when CFLAGS contains -GL
+                "WholeProgramOptimization": "true" if any(re.finditer("(^| )[/-]GL($| )", tools.get_env("CFLAGS", ""))) else "false",
+            }
             # run build
             msbuild = MSBuild(self)
-            msbuild.build("lcms2.sln", targets=[target], platforms={"x86": "Win32"}, upgrade_project=upgrade_project)
+            msbuild.build("lcms2.sln", targets=[target], platforms={"x86": "Win32"}, upgrade_project=upgrade_project, properties=properties)
 
     def _configure_autotools(self):
         if self._autotools:
             return self._autotools
         self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        args = []
-        if self.options.shared:
-            args.extend(["--disable-static", "--enable-shared"])
-        else:
-            args.extend(["--disable-shared", "--enable-static"])
-        args.append("--without-tiff")
-        args.append("--without-jpeg")
+        yes_no = lambda v: "yes" if v else "no"
+        args = [
+            "--enable-shared={}".format(yes_no(self.options.shared)),
+            "--enable-static={}".format(yes_no(not self.options.shared)),
+            "--without-tiff",
+            "--without-jpeg",
+        ]
         self._autotools.configure(args=args, configure_dir=self._source_subfolder)
         return self._autotools
-
-    @property
-    def _user_info_build(self):
-        # If using the experimental feature with different context for host and
-        # build, the 'user_info' attributes of the 'build_requires' packages
-        # will be located into the 'user_info_build' object. In other cases they
-        # will be located into the 'deps_user_info' object.
-        return getattr(self, "user_info_build", None) or self.deps_user_info
 
     def build(self):
         self._patch_sources()
@@ -133,5 +144,5 @@ class LcmsConan(ConanFile):
         else:
             self.cpp_info.libs = ["lcms2"]
         self.cpp_info.names["pkg_config"] = "lcms2"
-        if self.settings.os == "Linux":
+        if self.settings.os in ("FreeBSD", "Linux"):
             self.cpp_info.system_libs.append("m")
