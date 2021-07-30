aab06b1a7ae681254faaedc3bdc61e35597bfb67
Samuel Dowling <samuel.dowling@protonmail.com>
2021-07-31 01:35:14 +0930

    Remove fPIC management from config_options function
    * Remove superfluous fPIC management from config_options function
    * Remove superfluous C library requirements to remove cpp flags

--- recipes/geotrans/all/conanfile.py
+++ recipes/geotrans/all/conanfile.py
@@ -39,14 +39,11 @@ class GeotransConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        if self.options.shared:
-            del self.options.fPIC
+
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
-         # And if this is a C library:
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+
     def source(self):
         tools.get(
             **self.conan_data["sources"][self.version],
