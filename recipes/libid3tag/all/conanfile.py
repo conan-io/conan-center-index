b4c1bf90475c293f7aec5c27228c919a0b80bd6c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-30 14:02:55 +0200

    simplify _user_info_build

--- recipes/libid3tag/all/conanfile.py
+++ recipes/libid3tag/all/conanfile.py
@@ -89,7 +89,7 @@ class LibId3TagConan(ConanFile):
 
     @property
     def _user_info_build(self):
-        return getattr(self, "user_info_build", None) or self.deps_user_info
+        return getattr(self, "user_info_build", self.deps_user_info)
 
     def _build_autotools(self):
         shutil.copy(self._user_info_build["gnu-config"].CONFIG_SUB,
