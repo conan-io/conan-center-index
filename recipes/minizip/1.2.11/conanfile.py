d14b32dd4ad09334846bc439b9dc9b16edc54f84
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-01 22:36:51 +0200

    delete fPIC option if shared

--- recipes/minizip/1.2.11/conanfile.py
+++ recipes/minizip/1.2.11/conanfile.py
@@ -38,6 +38,8 @@ class MinizipConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
