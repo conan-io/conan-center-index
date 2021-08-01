d138e35ce501109cc163c4a07ec0b046806b7d98
Christoph Bachhuber <cbachhuber89@gmail.com>
2021-08-01 19:54:49 +0200

    Use self, not self.settings
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/patchelf/all/test_package/conanfile.py
+++ recipes/patchelf/all/test_package/conanfile.py
@@ -5,5 +5,5 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def test(self):
-        if not tools.cross_building(self.settings, skip_x64_x86=True):
+        if not tools.cross_building(self, skip_x64_x86=True):
             self.run("patchelf --version", run_environment=True)
