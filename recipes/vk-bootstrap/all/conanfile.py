f4247163dbbfc3b7e68505f46feec26d7b1d71b5
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-03 01:59:39 +0200

    bump vulkan-headers

--- recipes/vk-bootstrap/all/conanfile.py
+++ recipes/vk-bootstrap/all/conanfile.py
@@ -34,7 +34,7 @@ class VkBootstrapConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("vulkan-headers/1.2.170.0")
+        self.requires("vulkan-headers/1.2.184")
 
     @property
     def _compilers_minimum_version(self):
