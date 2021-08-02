2e49fa7e5a9aec5afda2b0a25f2a6402ef758fdf
redradist <redradist@gmail.com>
2021-08-02 23:12:20 +0300

    Used copytree instead of copy individual files

--- recipes/wasmtime/all/conanfile.py
+++ recipes/wasmtime/all/conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration, ConanException
 import os
+import shutil
 
 required_conan_version = ">=1.33.0"
 
@@ -21,10 +22,6 @@ class WasmtimeConan(ConanFile):
         'fPIC': True,
     }
     no_copy_source = True
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
     def _minimum_cpp_standard(self):
@@ -71,13 +68,11 @@ class WasmtimeConan(ConanFile):
             raise ConanInvalidConfiguration("Binaries for this combination of architecture/version/os not available")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version][str(self.settings.os)][str(self.settings.arch)], destination=self._source_subfolder, strip_root=True)
+        tools.get(**self.conan_data["sources"][self.version][str(self.settings.os)][str(self.settings.arch)], destination=self.source_folder, strip_root=True)
 
     def package(self):
-        include_path = os.path.join(self._source_subfolder, 'include')
-        self.copy('*.h', dst='include', src=include_path)
-        self.copy('*.hh', dst='include', src=include_path)
-        self.copy('*.hpp', dst='include', src=include_path)
+        shutil.copytree(os.path.join(self.source_folder, "include"),
+                        os.path.join(self.package_folder, "include"))
 
         self.copy('*.lib', dst='lib', keep_path=False)
         self.copy('*.dll', dst='bin', keep_path=False)
@@ -85,7 +80,7 @@ class WasmtimeConan(ConanFile):
         self.copy('*.dylib', dst='lib', keep_path=False)
         self.copy('*.a', dst='lib', keep_path=False)
 
-        self.copy('LICENSE', dst='licenses', src=self._source_subfolder)
+        self.copy('LICENSE', dst='licenses', src=self.source_folder)
 
     def package_info(self):
         if self.options.shared:
--- recipes/wasmtime/all/test_package/conanfile.py
+++ recipes/wasmtime/all/test_package/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class WasmtimeTestConan(ConanFile):
     settings = 'os', 'compiler', 'build_type', 'arch'
-    generators = 'cmake', 'cmake_find_package'
+    generators = 'cmake'
 
     def build(self):
         cmake = CMake(self)
