ac110fc3b0f822c9257e207b08bd65ecadd09bf5
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-04 14:31:58 +0200

    properly test CMake target

--- recipes/hdf4/all/test_package/conanfile.py
+++ recipes/hdf4/all/test_package/conanfile.py
@@ -1,6 +1,6 @@
+from conans import ConanFile, CMake, tools
 import os
 
-from conans import ConanFile, CMake, tools
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -8,11 +8,10 @@ class TestPackageConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["HDF4_SHARED"] = self.options["hdf4"].shared
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
