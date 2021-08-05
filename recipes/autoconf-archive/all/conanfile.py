3096c7d2eb298cf505d8992034a1a7d5cae1e382
sparik <sparikhayrapetyan@gmail.com>
2021-08-06 03:17:06 +0400

    add autoconf-archive recipe

---
+++ recipes/autoconf-archive/all/conanfile.py
@@ -0,0 +1,48 @@
+import os
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+
+
+class AutoconfArchiveConan(ConanFile):
+    name = "autoconf-archive"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://www.gnu.org/software/autoconf-archive/"
+    license = "GPL-2.0-or-later"
+    description = "The GNU Autoconf Archive is a collection of more than 500 macros for GNU Autoconf"
+    topics = ("conan", "GNU", "autoconf", "autoconf-archive", "macro")
+    settings = "os"
+    _source_subfolder = "source_subfolder"
+    _autotools = None
+
+    def configure(self):
+        if self.settings.os != "Linux":
+            raise ConanInvalidConfiguration("only Linux is supported")
+
+    def build_requirements(self):
+        self.build_requires("libtool/2.4.6")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_name = "autoconf-archive-" + self.version
+        tools.rename(extracted_name, self._source_subfolder)
+
+    def _configure_autotools(self):
+        if not self._autotools:
+            self._autotools = AutoToolsBuildEnvironment(self)
+            self._autotools.configure()
+        return self._autotools
+
+    def build(self):
+        with tools.chdir(os.path.join(self._source_subfolder)):
+            self._autotools = self._configure_autotools()
+            self._autotools.make()
+
+    def package(self):
+        with tools.chdir(os.path.join(self._source_subfolder)):
+            self.copy("COPYING", dst="licenses", src=self._source_subfolder)
+            self._autotools = self._configure_autotools()
+            self._autotools.install()
+
+    def package_info(self):
+        aclocal_path = os.path.join(self.package_folder, "share", "aclocal")
+        self.output.info("Appending ACLOCAL_PATH environment var: {}".format(aclocal_path))
+        self.env_info.ACLOCAL_PATH.append(aclocal_path)
---
+++ recipes/autoconf-archive/all/test_package/conanfile.py
@@ -0,0 +1,24 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+import os
+import shutil
+
+required_conan_version = ">=1.36.0"
+
+class TestPackageConan(ConanFile):
+    settings = "os"
+    exports_sources = "configure.ac", "Makefile.am", "hello.c"
+    test_type = "build_requires"
+
+    def build_requirements(self):
+        self.build_requires("libtool/2.4.6")
+
+    def build(self):
+        for src in self.exports_sources:
+            shutil.copy(os.path.join(self.source_folder, src), self.build_folder)
+        self.run("{} -fiv".format(tools.get_env("AUTORECONF")), run_environment=True)
+        autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        autotools.configure()
+        autotools.make()
+
+    def test(self):
+        self.run(os.path.join(".", "hello"))
