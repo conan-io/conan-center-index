c23429d9100d13ce36000c298e650caaed24889d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-08 20:40:53 +0200

    reproducible test package

--- recipes/libnova/all/test_package/test_package.c
+++ recipes/libnova/all/test_package/test_package.c
@@ -3,7 +3,14 @@
 #include <stdio.h>
 
 int main(void) {
-    double julian_day = ln_get_julian_from_sys();
+    struct ln_date date;
+    date.years = 1987;
+    date.months = 4;
+    date.days = 10;
+    date.hours = 19;
+    date.minutes = 21;
+    date.seconds = 0.0;
+    double julian_day = ln_get_julian_day(&date);
     printf("JD %f\n", julian_day);
 
     struct ln_helio_posn pos;
