f9f1a7999e61d6edb9939da376d9cdcd30d7517e
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-08-09 12:32:12 +0200

    libx264: get it building using mingw on Windows

--- recipes/libx264/all/test_package/conanfile.py
+++ recipes/libx264/all/test_package/conanfile.py
@@ -12,7 +12,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if tools.cross_building(self.settings, skip_x64_x86=True):
-            return
-        bin_path = os.path.join("bin", "test_package")
-        self.run(bin_path, run_environment=True)
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
