b156608fa08cb368b7111c5558ce6fa4d8339ddb
Anonymous Maarten <madebr@users.noreply.github.com>
2021-08-09 16:31:59 +0200

    (#6719) make: bump make/4.3
    * make: bump make/4.3
    
    * make: make path of executable available as user variable
    
    * make: use user variable to set CONAN_MAKE_PROGRAM when using 2 profile build

--- recipes/make/all/conanfile.py
+++ recipes/make/all/conanfile.py
@@ -1,32 +1,37 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class MakeConan(ConanFile):
     name = "make"
     description = "GNU Make is a tool which controls the generation of executables and other non-source files of a program from the program's source files"
     topics = ("conan", "make", "build", "makefile")
-    url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.gnu.org/software/make/"
+    url = "https://github.com/conan-io/conan-center-index"
     license = "GPL-3.0-or-later"
     settings = "os", "arch", "compiler", "build_type"
-    exports_sources = ["patches/*"]
-    _source_subfolder = "source_subfolder"
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "make-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+    exports_sources = "patches/*"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
 
     def configure(self):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
     def package_id(self):
         del self.info.settings.compiler
 
     def build(self):
-        for patch in self.conan_data["patches"][self.version]:
+        for patch in self.conan_data.get("patches").get(self.version, []):
             tools.patch(**patch)
 
         with tools.chdir(self._source_subfolder):
@@ -44,12 +49,16 @@ class MakeConan(ConanFile):
                 self.run(command)
 
     def package(self):
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="make", dst="bin", src=self._source_subfolder, keep_path=False)
-        self.copy(pattern="*gnumake.exe", dst="bin", src=self._source_subfolder, keep_path=False)
+        self.copy(pattern="COPYING", src=self._source_subfolder, dst="licenses")
+        self.copy(pattern="make", src=self._source_subfolder, dst="bin", keep_path=False)
+        self.copy(pattern="*gnumake.exe", src=self._source_subfolder, dst="bin", keep_path=False)
 
     def package_info(self):
-        make = "gnumake.exe" if self.settings.os == "Windows" else "make"
-        make = os.path.join(self.package_folder, "bin", make)
+        self.cpp_info.libdirs = []
+
+        make = os.path.join(self.package_folder, "bin", "gnumake.exe" if self.settings.os == "Windows" else "make")
+
+        self.user_info.make = make
+
         self.output.info('Creating CONAN_MAKE_PROGRAM environment variable: %s' % make)
         self.env_info.CONAN_MAKE_PROGRAM = make
