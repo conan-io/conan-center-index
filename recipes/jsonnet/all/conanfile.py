0d68d9f6bab397e1966393b5c1d7d8b1fc7436ca
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-08-09 18:20:25 +0200

    jsonnet: get it building on mingw@Windows

--- recipes/jsonnet/all/conanfile.py
+++ recipes/jsonnet/all/conanfile.py
@@ -1,6 +1,8 @@
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 
+required_conan_version = ">=1.33.0"
+
 
 class JsonnetConan(ConanFile):
     name = "jsonnet"
@@ -24,9 +26,9 @@ class JsonnetConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
     def configure(self):
         if self.options.shared:
@@ -40,8 +42,8 @@ class JsonnetConan(ConanFile):
             raise ConanInvalidConfiguration("{} compiler not supported"
                                             .format(self.settings.compiler))
 
-        if self.settings.compiler == "gcc" and self.settings.compiler.libcxx != "libstdc++11":
-            raise ConanInvalidConfiguration("jsonnet gcc package requires libstdc++11")
+        if tools.cross_building(self, skip_x64_x86=True):
+            raise ConanInvalidConfiguration("jsonnet does not support cross building")
 
         if self.settings.compiler in ["clang", "apple-clang"] and self.settings.compiler.libcxx != "libstdc++":
             raise ConanInvalidConfiguration("jsonnet {} package requires libstdc++".
@@ -53,9 +55,9 @@ class JsonnetConan(ConanFile):
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, "11")
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -66,8 +68,7 @@ class JsonnetConan(ConanFile):
         self._cmake.definitions["BUILD_JSONNET"] = False
         self._cmake.definitions["BUILD_JSONNETFMT"] = False
         self._cmake.definitions["USE_SYSTEM_JSON"] = True
-        self._cmake.configure(build_folder=self._build_subfolder,
-                              source_folder=self._source_subfolder)
+        self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
     def build(self):
@@ -82,7 +83,11 @@ class JsonnetConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["jsonnet++", "jsonnet"]
-        self.cpp_info.names["cmake_find_package"] = "jsonnet"
-        self.cpp_info.names["cmake_find_package_multi"] = "jsonnet"
-        self.cpp_info.names["pkg_config"] = "jsonnet"
+        self.cpp_info.components["libjsonnet"].libs = ["jsonnet"]
+        self.cpp_info.components["libjsonnet"].requires = ["nlohmann_json::nlohmann_json"]
+        if tools.stdcpp_library(self):
+            self.cpp_info.components["libjsonnet"].system_libs.append(tools.stdcpp_library(self))
+
+        self.cpp_info.components["libjsonnetpp"].libs = ["jsonnet++"]
+        self.cpp_info.components["libjsonnetpp"].requires = ["libjsonnet"]
+
