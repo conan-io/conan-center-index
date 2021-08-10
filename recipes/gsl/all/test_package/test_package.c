6d628c1dae454827d1e3d8cd22e3b4e520126998
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-08-10 03:10:26 +0200

    gsl: fix MSVC + mingw@Windows

--- recipes/gsl/all/test_package/test_package.c
+++ recipes/gsl/all/test_package/test_package.c
@@ -1,17 +1,14 @@
-#include <cstdio>
 #include <gsl/gsl_sf_bessel.h>
-
+#include <stdio.h>
 
 /**
  * The example program from the GSL documentation
  * http://www.gnu.org/software/gsl/doc/html/usage.html#an-example-program
  */
 
-
-int main (void) {
+int main() {
     double x = 5.0;
     double y = gsl_sf_bessel_J0 (x);
     printf ("J0(%g) = %.18e\n", x, y);
     return 0;
 }
-
