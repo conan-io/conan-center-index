c637eb9f414b9f50ae8a18dd517e80aa3fe198e5
Martin Delille <martin@delille.org>
2021-08-13 17:41:39 +0200

    Add c++ min check and conan_basic_setup(TARGET)

--- recipes/aws-crt-cpp/all/conanfile.py
+++ recipes/aws-crt-cpp/all/conanfile.py
@@ -36,6 +36,10 @@ class AwsCrtCpp(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, "11")
+
     def requirements(self):
         self.requires("aws-c-event-stream/0.2.7")
         self.requires("aws-c-common/0.6.7")
