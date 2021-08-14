da299742d38508831da77e0efbd549b661b2e922
werto87 <huhulasmichinruhe@web.de>
2021-08-14 21:28:12 +0200

    only coppy allowed files

--- recipes/emsdk/all/conanfile.py
+++ recipes/emsdk/all/conanfile.py
@@ -57,7 +57,7 @@ class EmSDKConan(ConanFile):
         self.copy(pattern='*', dst='bin',
                   src=self._source_subfolder, excludes=('*.pc', '*Find*.cmake', '*Box2DConfig.cmake', '*CTestConfig.cmake',
                                                         '*concrt140.dll', '*msvcr100.dll', '*concrt140d.dll', '*msvcp140.dll', '*msvcp140d.dll', '*vcruntime140.dll',
-                                                        '*vcruntime140_1.dll', '*vcruntime140_1d.dll', '*vcruntime140d.dl'))
+                                                        '*vcruntime140_1.dll', '*vcruntime140_1d.dll', '*vcruntime140d.dl', '*vcruntime140d.dll'))
         emsdk = self.package_folder
         emscripten = os.path.join(emsdk, 'bin', 'upstream', 'emscripten')
         toolchain = os.path.join(
