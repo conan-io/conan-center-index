7956b030e0d7d7402aac97fe2471ee970b8458c9
Anonymous Maarten <madebr@users.noreply.github.com>
2021-08-14 04:31:47 +0200

    (#6694) m4: modernize recipe + don't add `-rtlib=compiler-rt`
    * m4: modernize recipe
    
    * m4: don't add a build requirement on m4 in its test_package
    
    * m4: really remove m4 build requirement
    
    * m4: revert "Link against compiler-rt for clang"
    
    7c5762b976f80459026fe79e29c16cf853154412
    https://github.com/conan-io/conan-center-index/commit/7c5762b976f80459026fe79e29c16cf853154412
    This belongs in a profile.

--- recipes/m4/all/test_package/conanfile.py
+++ recipes/m4/all/test_package/conanfile.py
@@ -2,14 +2,7 @@ from conans import ConanFile, tools
 from conans.errors import ConanException
 from io import StringIO
 import os
-
-
-M4_CONTENTS = """\
-m4_define(NAME1, `Harry, Jr.')
-m4_define(NAME2, `Sally')
-m4_define(MET, `$1 met $2')
-MET(`NAME1', `NAME2')
-"""
+import textwrap
 
 
 class TestPackageConan(ConanFile):
@@ -19,19 +12,24 @@ class TestPackageConan(ConanFile):
     def _m4_input_path(self):
         return os.path.join(self.build_folder, "input.m4")
 
-    def build_requirements(self):
-        if tools.os_info.is_windows:
-            self.build_requires("msys2/20200517")
-
     def build(self):
-        tools.save(self._m4_input_path, M4_CONTENTS)
+        tools.save(self._m4_input_path, textwrap.dedent("""\
+            m4_define(NAME1, `Harry, Jr.')
+            m4_define(NAME2, `Sally')
+            m4_define(MET, `$1 met $2')
+            MET(`NAME1', `NAME2')
+        """))
 
     def test(self):
-        m4_bin = tools.get_env("M4")
-        if m4_bin is None or not m4_bin.startswith(self.deps_cpp_info["m4"].rootpath):
-            raise ConanException("M4 environment variable not set")
+        if hasattr(self, "settings_build"):
+            exe_suffix = ".exe" if self.settings.os == "Windows" else ""
+            m4_bin = os.path.join(self.deps_cpp_info["m4"].rootpath, "bin", "m4" + exe_suffix)
+        else:
+            m4_bin = tools.get_env("M4")
+            if m4_bin is None or not m4_bin.startswith(self.deps_cpp_info["m4"].rootpath):
+                raise ConanException("M4 environment variable not set")
 
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self, skip_x64_x86=True):
             self.run("{} --version".format(m4_bin), run_environment=True)
             self.run("{} -P {}".format(m4_bin, self._m4_input_path))
 
