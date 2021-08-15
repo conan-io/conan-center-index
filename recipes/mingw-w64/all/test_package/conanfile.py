4003013f24ea5122e94feeabb9d05db1662967b8
Stefan Profanter <stefan.profanter@agile-robots.com>
2021-08-15 15:32:11 +0200

    [mingw-w64] add missing version

--- recipes/mingw-w64/all/test_package/conanfile.py
+++ recipes/mingw-w64/all/test_package/conanfile.py
@@ -15,7 +15,7 @@ class MinGWTestConan(ConanFile):
             self.run("x86_64-w64-mingw32-g++ {} @conanbuildinfo.gcc -lstdc++ -o main".format(source_file), run_environment=True)
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             if self.settings.os == "Windows":
                 self.run("gcc.exe --version", run_environment=True)
                 self.run("main")
