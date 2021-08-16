b7d0ae517a1001587ecd918a398dd41c8a8f6f50
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-16 13:28:17 +0200

    bump msys2

--- recipes/libtool/all/conanfile.py
+++ recipes/libtool/all/conanfile.py
@@ -46,10 +46,14 @@ class LibtoolConan(ConanFile):
     def requirements(self):
         self.requires("automake/1.16.3")
 
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
     def build_requirements(self):
-        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/20200517")
         self.build_requires("gnu-config/cci.20201022")
+        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/cci.latest")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
--- recipes/libtool/all/test_package/conanfile.py
+++ recipes/libtool/all/test_package/conanfile.py
@@ -9,9 +9,13 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
     def build_requirements(self):
-        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/20200517")
+        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/cci.latest")
 
     @contextmanager
     def _build_context(self):
