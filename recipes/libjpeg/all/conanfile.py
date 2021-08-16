ddbcb56917bedf3556eabc6325d26498279a5e3a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-16 13:47:52 +0200

    bump msys2

--- recipes/libjpeg/all/conanfile.py
+++ recipes/libjpeg/all/conanfile.py
@@ -34,10 +34,14 @@ class LibjpegConan(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
     def build_requirements(self):
-        if tools.os_info.is_windows and self.settings.compiler != "Visual Studio" and \
+        if self._settings_build.os == "Windows" and self.settings.compiler != "Visual Studio" and \
            not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/20200517")
+            self.build_requires("msys2/cci.latest")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
