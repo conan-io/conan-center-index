d2ddbf5105e2556e8bdae96485d01bb3451dc22d
KerstinKeller <KerstinKeller@users.noreply.github.com>
2021-08-16 10:41:18 +0200

    Extra linkage only required for shared==False

--- recipes/qt/5.x.x/conanfile.py
+++ recipes/qt/5.x.x/conanfile.py
@@ -1077,7 +1077,6 @@ Examples = bin/datadir/examples""")
 
         if self.options.qtwinextras:
             _create_module("WinExtras")
-            self.cpp_info.components["qtWinExtras"].system_libs.append("dwmapi")
 
         if self.options.qtxmlpatterns:
              _create_module("XmlPatterns", ["Network"])
@@ -1090,7 +1089,10 @@ Examples = bin/datadir/examples""")
                 self.cpp_info.components["qtCore"].system_libs.append("userenv")  # qtcore requires "__imp_GetUserProfileDirectoryW " which is in "UserEnv.Lib" library
                 self.cpp_info.components["qtCore"].system_libs.append("ws2_32")  # qtcore requires "WSAStartup " which is in "Ws2_32.Lib" library
                 self.cpp_info.components["qtNetwork"].system_libs.append("dnsapi")  # qtnetwork from qtbase requires "DnsFree" which is in "Dnsapi.lib" library
-                self.cpp_info.components["qtNetwork"].system_libs.append("iphlpapi")
+                self.cpp_info.components["qtNetwork"].system_libs.append("iphlpapi")            
+                if self.options.qtwinextras:
+                    self.cpp_info.components["qtWinExtras"].system_libs.append("dwmapi")  # qtwinextras requires "DwmGetColorizationColor" which is in "dwmapi.lib" library
+
 
             if self.settings.os == "Macos":
                 self.cpp_info.components["qtCore"].frameworks.append("IOKit")     # qtcore requires "_IORegistryEntryCreateCFProperty", "_IOServiceGetMatchingService" and much more which are in "IOKit" framework
