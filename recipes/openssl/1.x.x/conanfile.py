058240fe5e2a2670e71437265b315edc03f09c28
Volodymyr Boichentsov <v.boichentsov@elsevier.com>
2021-08-17 09:41:27 +0100

    remove unnecessary disabled things

--- recipes/openssl/1.x.x/conanfile.py
+++ recipes/openssl/1.x.x/conanfile.py
@@ -176,8 +176,6 @@ class OpenSSLConan(ConanFile):
 
         if self.settings.os == "Emscripten":
             self.options.no_asm = True
-            self.options.no_deprecated = True
-            self.options.no_dso = True
             self.options.no_threads = True
             self.options.no_stdio = True
             self.options.no_tests = True
@@ -860,8 +858,8 @@ class OpenSSLConan(ConanFile):
 
     def validate(self):
         if self.settings.os == "Emscripten":
-            if not all((self.options.no_asm, self.options.no_deprecated, self.options.no_dso, self.options.no_threads, self.options.no_stdio, self.options.no_tests)):
-                raise ConanInvalidConfiguration("os=Emscripten requires openssl:{no_asm,no_deprecated,no_dso,no_threads,no_stdio,no_tests}=True")
+            if not all((self.options.no_asm, self.options.no_threads, self.options.no_stdio, self.options.no_tests)):
+                raise ConanInvalidConfiguration("os=Emscripten requires openssl:{no_asm,,no_threads,no_stdio,no_tests}=True")
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "OpenSSL"
