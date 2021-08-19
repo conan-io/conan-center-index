8a7107c442a7caf634583d5496d00bcbf2010177
Alexande B <abobrikovich@gmail.com>
2021-08-19 15:45:54 +0200

    #4806 nsync: simplify tests

--- recipes/nsync/all/test_package/example_c.c
+++ recipes/nsync/all/test_package/example_c.c
@@ -1,15 +1,8 @@
 #include <nsync.h>
 
-static int int_is_zero (const void *v) {
-    return (*(const int *)v == 0);
-}
-
 int main() {
-    nsync_mu testing_mu;  /* protects fields below */
-    int child_count;      /* count of testing_s structs whose base is this testing_base_s */
-
+    nsync_mu testing_mu;
     nsync_mu_lock (&testing_mu);
-    nsync_mu_wait (&testing_mu, &int_is_zero, &child_count, NULL);
     nsync_mu_unlock (&testing_mu);
     return 0;
 }
--- recipes/nsync/all/test_package/example_cpp.cpp
+++ recipes/nsync/all/test_package/example_cpp.cpp
@@ -1,15 +1,8 @@
 #include <nsync.h>
 
-static int int_is_zero (const void *v) {
-    return (*(const int *)v == 0);
-}
-
 int main() {
-    nsync::nsync_mu testing_mu;  /* protects fields below */
-    int child_count;      /* count of testing_s structs whose base is this testing_base_s */
-
+    nsync::nsync_mu testing_mu;
     nsync::nsync_mu_lock (&testing_mu);
-    nsync::nsync_mu_wait (&testing_mu, &int_is_zero, &child_count, NULL);
     nsync::nsync_mu_unlock (&testing_mu);
     return 0;
 }
