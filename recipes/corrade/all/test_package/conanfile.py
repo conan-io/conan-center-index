6a8d64f6e4a1bafb63accf356ba19c4d00a722d2
Javier G. Sogo <jgsogo@gmail.com>
2021-08-20 22:24:58 +0200

    discard changes for corrade

--- recipes/corrade/all/test_package/conanfile.py
+++ recipes/corrade/all/test_package/conanfile.py
@@ -4,14 +4,18 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package"
+    generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
+        if self.settings.compiler == "Visual Studio":
+            cmake.definitions["CORRADE_MSVC2015_COMPATIBILITY"] = "ON" if self.settings.compiler.version == "14" else "OFF"
+            cmake.definitions["CORRADE_MSVC2017_COMPATIBILITY"] = "ON" if self.settings.compiler.version == "15" else "OFF"
+            cmake.definitions["CORRADE_MSVC2019_COMPATIBILITY"] = "ON" if self.settings.compiler.version == "16" else "OFF"
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not tools.cross_building(self.settings):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
