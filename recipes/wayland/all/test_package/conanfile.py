4bafb4313e66eda70d6de46dd804f622ff08ffa8
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-08-21 09:32:38 +0200

    (#6934) wayland: declare variables in pc files
    * wayland: declare variables in pc files
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * bump reqs

--- recipes/wayland/all/test_package/conanfile.py
+++ recipes/wayland/all/test_package/conanfile.py
@@ -4,12 +4,16 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "pkg_config"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+        if not tools.cross_building(self, skip_x64_x86=True):
+            with tools.environment_append({'PKG_CONFIG_PATH': "."}):
+                pkg_config = tools.PkgConfig("wayland-scanner")
+                self.run('%s --version' % pkg_config.variables["wayland_scanner"], run_environment=True)
 
     def test(self):
         if not tools.cross_building(self.settings):
