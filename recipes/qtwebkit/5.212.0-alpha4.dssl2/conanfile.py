9955c8ecbec1521799513d0902d4f44989f88d5d
reymondzzzz <starkov.kirill123@gmail.com>
2021-08-22 21:06:01 +0300

    rework qtwebkit for components

--- recipes/qtwebkit/5.212.0-alpha4.dssl2/conanfile.py
+++ recipes/qtwebkit/5.212.0-alpha4.dssl2/conanfile.py
@@ -5,14 +5,14 @@ import platform
 
 class QtWebKitConan(ConanFile):
     name = "qtwebkit"
-    version = "5.212.0-alpha4.dssl1"
+    version = "5.212.0-alpha4.dssl2"
     original_version = "5.212.0-alpha4"
     license = "LGPL-2.0-or-later, LGPL-2.1-or-later, BSD-2-Clause"
     homepage = "https://github.com/qtwebkit/qtwebkit"
     description = "Qt port of WebKit"
     topics = ("qt", "browser-engine", "webkit", "qt5", "qml", "qtwebkit")
     settings = "os", "compiler", "build_type", "arch"
-    generators = 'cmake'
+    generators = "cmake_find_package"
     exports_sources = [
         "patches/*.patch"
     ]
@@ -42,7 +42,7 @@ class QtWebKitConan(ConanFile):
 
     requires = [
         "qt/5.14.1.dssl1",
-        "libjpeg-turbo/2.0.4",
+        "libjpeg/9d",
         "libpng/1.6.37",
         "libwebp/1.1.0",
         "sqlite3/3.31.0",
@@ -90,14 +90,16 @@ class QtWebKitConan(ConanFile):
 
         # check recipe conistency
         tools.check_with_algorithm_sum("sha1", "patches/clang-11-jsc.patch", "03358658f12a895d00f5a7544618dc7019fb2882")
-        tools.check_with_algorithm_sum("sha1", "patches/OptionsQt.patch", "d8587c6ce3d498f71888f90890780df8387b0f48")
-        tools.check_with_algorithm_sum("sha1", "patches/cmake_version.patch", "030c7f2d1d6daceee54497eac6c734af457f10bf")
+        tools.check_with_algorithm_sum("sha1", "patches/OptionsQt.patch", "b51dbd74b78bbf54ea12145583c980c64bb17074")
+        tools.check_with_algorithm_sum("sha1", "patches/cmake_version.patch", "06907610b577145b11c6412decdf0fec8406b45a")
         # apply patches
         tools.patch(base_path=self._source_subfolder, patch_file="patches/clang-11-jsc.patch", strip=1)
-
-        if platform.system() == "Linux":
-            tools.patch(base_path=self._source_subfolder, patch_file="patches/OptionsQt.patch", strip=1)
+        tools.patch(base_path=self._source_subfolder, patch_file="patches/OptionsQt.patch", strip=1)
+        if tools.os_info.is_linux:
             tools.patch(base_path=self._source_subfolder, patch_file="patches/cmake_version.patch", strip=1)
+
+        os.remove(os.path.join(self._source_subfolder, 'Source', 'cmake', 'FindICU.cmake'))
+        os.remove(os.path.join(self._source_subfolder, 'Source', 'cmake', 'FindSqlite.cmake'))
 
     def _configure_cmake(self):
         cmake = CMake(self, generator="Ninja", build_type=self.settings.build_type)
@@ -108,6 +110,7 @@ class QtWebKitConan(ConanFile):
 
         if tools.os_info.is_linux:
             cmake.definitions["USE_OPENGL"] = 1
+            cmake.definitions["ENABLE_OPENGL"] = 1
 
         # TODO on linux we should check kernel version. On kernels < 3.4 bmalloc cannot be compiled
         if not self.options["with_bmalloc"]:
@@ -127,10 +130,12 @@ class QtWebKitConan(ConanFile):
         if not self.options["with_woff2"]:
             cmake.definitions["USE_WOFF2"] = "OFF"
 
-        cmake.definitions["QT_CONAN_DIR"] = os.getcwd()
-
-        qt_dir = self.deps_cpp_info["qt"]
-        cmake.definitions["Qt5_DIR"] = os.path.join(qt_dir.libdirs[0], "cmake", "Qt5")
+        if hasattr(self.deps_user_info['qt'], '_extra_cmake_variables_'):
+            for key, value in self.deps_user_info['qt']._extra_cmake_variables_.items():
+                cmake.definitions[key] = value
+        if hasattr(self.deps_user_info['qt'], '_extra_cmake_paths_'):
+            for key, pathes in self.deps_user_info['qt']._extra_cmake_paths_.items():
+                cmake.definitions[key] = ";".join([os.path.join(self.deps_cpp_info['qt'].rootpath, p) for p in pathes])
 
         cmake.configure(build_folder=self._build_subfolder, source_folder=self._source_subfolder)
 
@@ -152,16 +157,47 @@ class QtWebKitConan(ConanFile):
         pass
 
     def package_info(self):
-        if tools.is_apple_os(self.settings.os):
-           libs = ["QtWebKit", "QtWebKitWidgets"]
-        else:
-           libs = ["Qt5WebKit", "Qt5WebKitWidgets"]
+        self.cpp_info.names["cmake_find_package"] = "QtWebKit"
+        self.cpp_info.names["cmake_find_package_multi"] = "QtWebKit"
+
+        self.cpp_info.components["WebKit"].names["cmake_find_package"] = "WebKit"
+        self.cpp_info.components["WebKit"].names["cmake_find_package_multi"] = "WebKit"
+        self.cpp_info.components["WebKit"].includedirs = ["include", os.path.join("include", "QtWebKit")]
+        reqs = [
+            "qt::qt", 
+            "libxml2::libxml2", 
+            "libjpeg::libjpeg", 
+            "icu::icu",
+            "libwebp::libwebp",
+            "libpng::libpng",
+            "sqlite3::sqlite3",
+            "libxslt::libxslt",
+            "zlib::zlib"
+        ]
+        
+        if platform.system() == "Linux":
+            reqs.append('libxcomposite::libxcomposite')
+
+        if self.options["with_webcrypto"]:
+            reqs.append("libgcrypt::libgcrypt")
+
+        if self.options["with_gstreamer"]:
+            reqs.append("gstreamer::gstreamer")
+
+        self.cpp_info.components["WebKit"].requires = reqs
+
+        self.cpp_info.components["WebKitWidgets"].names["cmake_find_package"] = "WebKitWidgets"
+        self.cpp_info.components["WebKitWidgets"].names["cmake_find_package_multi"] = "WebKitWidgets"
+        self.cpp_info.components["WebKitWidgets"].includedirs = ["include", os.path.join("include", "WebKitWidgets")]
+        self.cpp_info.components["WebKitWidgets"].requires = [
+            "WebKit"
+        ]
 
         if tools.is_apple_os(self.settings.os):
-            self.cpp_info.frameworkdirs = ['lib']
-            self.cpp_info.frameworks = libs[:]
+            self.cpp_info.components["WebKit"].frameworks = ["QtWebKit"]
+            self.cpp_info.components["WebKitWidgets"].frameworks = ["QtWebKitWidgets"]
         else:
-            self.cpp_info.libdirs.append('lib')
-            self.cpp_info.libs = libs[:]
+            self.cpp_info.components["WebKit"].libs = ["Qt5WebKit"]
+            self.cpp_info.components["WebKitWidgets"].libs = ["Qt5WebKitWidgets"]
 
         self.env_info.CMAKE_PREFIX_PATH.append(self.package_folder)
