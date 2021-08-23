9da6df8378655bc4bae5d13f0360712ced4beff3
uselessgoddess <dodickgod@gmail.com>
2021-08-23 10:54:55 +0300

    Checking flags

--- recipes/platform.hashing/all/conanfile.py
+++ recipes/platform.hashing/all/conanfile.py
@@ -58,6 +58,25 @@ class PlatformInterfacesConan(ConanFile):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
 
+        if self.settings.compiler != "Visual Studio":
+            def check_mfpu_flag(flag, safe=False):
+                tabulation = ' ' * 24
+                cxxflags = tools.get_env("CXXFLAGS", "")
+                if not safe and flag not in cxxflags:
+                    self.output.warn("`{}` not detected in cxxflags.\n "
+                                     "{tab}Consider adding it in your profile for more performance.\n "
+                                     "{tab}Missing a flag can cause undefined behavior. "
+                                     .format(flag, tab=tabulation))
+                return flag in cxxflags
+
+            if "armv7" in self.settings.arch:
+                check_mfpu_flag("-mfpu=neon")
+            elif "armv8" in self.settings.arch:
+                if not check_mfpu_flag("-march=armv8-a+fp+simd+crypto+crc", safe=True):
+                    self.output.warn("Remove `crypto` and/or `crc` if your architecture does not support cryptographic "
+                                     "and/or CRC32 extensions")
+                    check_mfpu_flag("-march=armv8-a+fp+simd")
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
@@ -67,12 +86,3 @@ class PlatformInterfacesConan(ConanFile):
 
     def package_id(self):
         self.info.header_only()
-
-    def package_info(self):
-        if self.settings.compiler != "Visual Studio":
-            if "armv7" in self.settings.arch:
-                self.cpp_info.cxxflags = ["-mfpu=neon"]
-            elif "armv8" in self.settings.arch:
-                self.cpp_info.cxxflags = ["-march=armv8-a+fp+simd+crypto+crc"]
-            else:
-                self.cpp_info.cxxflags = ["-march=native"]
