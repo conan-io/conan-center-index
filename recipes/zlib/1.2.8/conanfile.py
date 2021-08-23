039c45e3dd1cebea930f6111b5fd928cd656def6
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-08-23 17:18:16 +0200

    zlib: modernize zlib/1.2.8

--- recipes/zlib/1.2.8/conanfile.py
+++ recipes/zlib/1.2.8/conanfile.py
@@ -1,21 +1,29 @@
+from conans import ConanFile, tools, CMake, AutoToolsBuildEnvironment
 import os
 import stat
-import shutil
-from conans import ConanFile, tools, CMake, AutoToolsBuildEnvironment
+
+required_conan_version = ">=1.33.0"
 
 
 class ZlibConan(ConanFile):
     name = "zlib"
     description = ("A Massively Spiffy Yet Delicately Unobtrusive Compression Library "
                   "(Also Free, Not to Mention Unencumbered by Patents)")
-    topics = ("conan", "zlib", "compression")
+    topics = ("zlib", "compression")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://zlib.net"
     license = "Zlib"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    exports_sources = ["CMakeLists.txt"]
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    exports_sources = "CMakeLists.txt"
     generators = "cmake"
 
     @property
@@ -37,44 +45,41 @@ class ZlibConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("{}-{}".format(self.name, self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    def build(self):
         tools.rmdir(os.path.join(self._source_subfolder, "contrib"))
         if not tools.os_info.is_windows:
             configure_file = os.path.join(self._source_subfolder, "configure")
             st = os.stat(configure_file)
             os.chmod(configure_file, st.st_mode | stat.S_IEXEC)
-
-    def build(self):
-        if self.settings.os != "Windows":
+        if self.settings.os == "Windows":
+            cmake = CMake(self)
+            cmake.configure(build_dir=self._build_subfolder)
+            cmake.build()
+        else:
             with tools.chdir(self._source_subfolder):
-                env_build = AutoToolsBuildEnvironment(self)
+                autotools = AutoToolsBuildEnvironment(self)
                 if self.settings.arch == "x86" or self.settings.arch == "x86_64":
-                    env_build.flags.append("-mstackrealign")
+                    autotools.flags.append("-mstackrealign")
 
                 if self.settings.os == "Macos":
                     old_str = "-install_name $libdir/$SHAREDLIBM"
                     new_str = "-install_name $SHAREDLIBM"
-                    tools.replace_in_file("./configure", old_str, new_str)
+                    tools.replace_in_file("configure", old_str, new_str)
 
                 # Zlib configure doesnt allow this parameters (in 1.2.8)
-                env_build.configure("./", build=False, host=False, target=False)
-                env_build.make()
+                autotools.configure(build=False, host=False, target=False)
+                autotools.make()
 
-        else:
-            cmake = CMake(self)
-            cmake.configure(build_dir=self._build_subfolder)
-            cmake.build()
+    @property
+    def _license_text(self):
+        text = tools.load(os.path.join(self._source_subfolder, "zlib.h"))
+        return text[2:text.find("*/", 1)]
 
     def package(self):
-        # Extract the License/s from the header to a file
-        with tools.chdir(self._source_subfolder):
-            tmp = tools.load("zlib.h")
-            license_contents = tmp[2:tmp.find("*/", 1)]
-            tools.save("LICENSE", license_contents)
-
-        # Copy the license files
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        tools.save(os.path.join(self.package_folder, "licenses", "LICENSE"), self._license_text)
 
         # Copying zlib.h, zutil.h, zconf.h
         self.copy("*.h", "include", "%s" % self._source_subfolder, keep_path=False)
@@ -84,36 +89,37 @@ class ZlibConan(ConanFile):
             suffix = "d" if self.settings.build_type == "Debug" else ""
             self.copy(pattern="*.h", dst="include", src=self._build_subfolder, keep_path=False)
             if self.options.shared:
-                self.copy(pattern="*.dll", dst="bin", src=self._build_subfolder, keep_path=False)
-                self.copy(pattern="libzlib.dll.a", dst="lib", src=os.path.join(self._build_subfolder, "lib"))
-                self.copy(pattern="zlib%s.lib" % suffix, dst="lib", src=os.path.join(self._build_subfolder, "lib"))
+                self.copy(pattern="*.dll", src=self._build_subfolder, dst="bin", keep_path=False)
+                self.copy(pattern="libzlib.dll.a", src=os.path.join(self._build_subfolder, "lib"), dst="lib")
+                self.copy(pattern="zlib{}.lib".format(suffix), src=os.path.join(self._build_subfolder, "lib"), dst="lib")
             else:
-                self.copy(pattern="zlibstatic%s.lib" % suffix, dst="lib", src=os.path.join(self._build_subfolder, "lib"))
-                self.copy(pattern="libzlibstatic.a", dst="lib", src=os.path.join(self._build_subfolder, "lib"))
+                self.copy(pattern="zlibstatic{}.lib".format(suffix), src=os.path.join(self._build_subfolder, "lib"), dst="lib")
+                self.copy(pattern="libzlibstatic.a", src=os.path.join(self._build_subfolder, "lib"), dst="lib")
 
                 lib_path = os.path.join(self.package_folder, "lib")
                 if self.settings.compiler == "Visual Studio":
-                    current_lib = os.path.join(lib_path, "zlibstatic%s.lib" % suffix)
-                    shutil.move(current_lib, os.path.join(lib_path, "zlib%s.lib" % suffix))
+                    tools.rename(os.path.join(lib_path, "zlibstatic{}.lib".format(suffix)),
+                                 os.path.join(lib_path, "zlib{}.lib".format(suffix)))
                 elif self.settings.compiler == "gcc":
-                    current_lib = os.path.join(lib_path, "libzlibstatic.a")
-                    shutil.move(current_lib, os.path.join(lib_path, "libzlib.a"))
+                    tools.rename(os.path.join(lib_path, "libzlibstatic.a"),
+                                 os.path.join(lib_path, "libzlib.a"))
         else:
             if self.options.shared:
                 if self.settings.os == "Macos":
-                    self.copy(pattern="*.dylib", dst="lib", src=self._source_subfolder, keep_path=False)
+                    self.copy(pattern="*.dylib", src=self._source_subfolder, dst="lib", keep_path=False)
                 else:
-                    self.copy(pattern="*.so*", dst="lib", src=self._source_subfolder, keep_path=False)
+                    self.copy(pattern="*.so*", src=self._source_subfolder, dst="lib", keep_path=False)
             else:
-                self.copy(pattern="*.a", dst="lib", src=self._source_subfolder, keep_path=False)
+                self.copy(pattern="*.a", src=self._source_subfolder, dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "ZLIB"
         self.cpp_info.names["cmake_find_package_multi"] = "ZLIB"
         self.cpp_info.names["pkg_config"] = "zlib"
         if self.settings.os == "Windows":
-            self.cpp_info.libs = ["zlib"]
+            suffix = ""
             if self.settings.build_type == "Debug" and self.settings.compiler == "Visual Studio":
-                self.cpp_info.libs[0] += "d"
+                suffix = "d"
+            self.cpp_info.libs = ["zlib{}".format(suffix)]
         else:
             self.cpp_info.libs = ["z"]
