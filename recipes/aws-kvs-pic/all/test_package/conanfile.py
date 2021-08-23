f4d7204c1546428baf1dc120a3b02e6081110703
Eric Riff <57375845+ericriff@users.noreply.github.com>
2021-08-23 04:32:03 -0300

    (#6716) Add aws-kvs-pic
    * Add aws-kvs-pic
    
    * Fix test package
    
    * Make the test package a C projext
    
    * Try commenting out deletion of self.settings.compiler.cppstd and self.settings.compiler.libcxx
    
    * fix system libs
    
    * Disable shared builds on MacOS since there are linking errors.
    
    * Bump version
    
    * Allow shared builds only on Linux
    
    * Restore deletion of self.settings.compiler.cppstd and self.settings.compiler.libcx since this is a C project
    
    * Pass 'self' instead of 'self.settings' to tools.corss_building()
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Improve topics
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Use tar.gz for sources instead of zip
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

---
+++ recipes/aws-kvs-pic/all/test_package/conanfile.py
@@ -0,0 +1,19 @@
+import os
+from conans import ConanFile, CMake, tools
+
+class TestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "pkg_config"
+
+    def build_requirements(self):
+        self.build_requires("pkgconf/1.7.4")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
