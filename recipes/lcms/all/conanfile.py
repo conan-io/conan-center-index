61b2887eb6c6991fce0c965d747bbdaf24d72677
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-08-24 03:25:39 +0200

    lcms: modernize + add 2.12

--- recipes/lcms/all/conanfile.py
+++ recipes/lcms/all/conanfile.py
@@ -12,16 +12,26 @@ class LcmsConan(ConanFile):
     description = "A free, open source, CMM engine."
     license = "MIT"
     homepage = "https://github.com/mm2/Little-CMS"
-    topics = ("conan", "lcms", "cmm", "icc", "cmm-engine")
+    topics = ("lcms", "cmm", "icc", "cmm-engine")
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
 
     _autotools = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -36,7 +46,7 @@ class LcmsConan(ConanFile):
     def build_requirements(self):
         if self.settings.compiler != "Visual Studio":
             self.build_requires("gnu-config/cci.20201022")
-            if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
                 self.build_requires("msys2/cci.latest")
 
     def source(self):
@@ -66,13 +76,13 @@ class LcmsConan(ConanFile):
         if self._autotools:
             return self._autotools
         self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        args = []
-        if self.options.shared:
-            args.extend(["--disable-static", "--enable-shared"])
-        else:
-            args.extend(["--disable-shared", "--enable-static"])
-        args.append("--without-tiff")
-        args.append("--without-jpeg")
+        yes_no = lambda v: "yes" if v else "no"
+        args = [
+            "--enable-shared={}".format(yes_no(self.options.shared)),
+            "--enable-static={}".format(yes_no(not self.options.shared)),
+            "--without-tiff",
+            "--without-jpeg",
+        ]
         self._autotools.configure(args=args, configure_dir=self._source_subfolder)
         return self._autotools
 
@@ -133,5 +143,5 @@ class LcmsConan(ConanFile):
         else:
             self.cpp_info.libs = ["lcms2"]
         self.cpp_info.names["pkg_config"] = "lcms2"
-        if self.settings.os == "Linux":
+        if self.settings.os in ("FreeBSD", "Linux"):
             self.cpp_info.system_libs.append("m")
