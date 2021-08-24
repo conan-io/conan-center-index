b6e8c6fe19ba94a7f04802faea78eb977bf5ce48
Anonymous Maarten <madebr@users.noreply.github.com>
2021-08-24 21:33:39 +0200

    (#6978) nasm: modernize

--- recipes/nasm/all/test_package/conanfile.py
+++ recipes/nasm/all/test_package/conanfile.py
@@ -2,16 +2,17 @@ import os
 from conans import ConanFile, tools
 
 
-class DefaultNameConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "arch",
 
     def test(self):
-        if not tools.cross_building(self.settings, skip_x64_x86=True):
+        if not tools.cross_building(self, skip_x64_x86=True):
             self.run("nasm --version", run_environment=True)
+            asm_file = os.path.join(self.source_folder, "hello_linux.asm")
+            out_file = os.path.join(self.build_folder, "hello_linux.o")
+            bin_file = os.path.join(self.build_folder, "hello_linux")
+            self.run("nasm -felf64 {} -o {}".format(asm_file, out_file), run_environment=True)
             if self.settings.os == "Linux" and self.settings.arch == "x86_64":
-                asm_file = os.path.join(self.source_folder, "hello_linux.asm")
-                out_file = os.path.join(self.build_folder, "hello_linux.o")
-                bin_file = os.path.join(self.build_folder, "hello_linux")
-                self.run("nasm -felf64 {} -o {}".format(asm_file, out_file), run_environment=True)
-                self.run("ld hello_linux.o -o {}".format(bin_file), run_environment=True)
+                ld = tools.get_env("LD", "ld")
+                self.run("{} hello_linux.o -o {}".format(ld, bin_file), run_environment=True)
                 self.run(bin_file)
