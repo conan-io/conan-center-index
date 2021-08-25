252c51858f4755af061fb630e28542b80f409d7e
Javier G. Sogo <jgsogo@gmail.com>
2021-08-25 23:32:26 +0200

    (#6290) [corrade] Add components + build_module + modernize
    * [corrade] Add components + build_module + modernize
    
    * explicit components name for pkg_config
    
    * pkg_config - avoid conflicts in filesystem
    
    * typo
    
    * run corrade-rc
    
    * Update recipes/corrade/all/conanfile.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * review
    
    * handle corrade:with_utility option
    
    * Update recipes/corrade/all/conanfile.py
    
    * test_package when 'utility' is deactivated. Version 2019.10 packages nothing
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/corrade/all/test_package/test_package.cpp
+++ recipes/corrade/all/test_package/test_package.cpp
@@ -1,5 +1,23 @@
-#include <Corrade/Utility/Debug.h>
+#include <iostream>
+#include <Corrade/Corrade.h>  // Here it is 'nothing'
+
+#ifndef VERSION_2019_10
+  #include <Corrade/version.h>
+#endif
+
+#ifdef WITH_UTILITY
+  #include <Corrade/Utility/Debug.h>
+#endif
+
 
 int main() {
-  Corrade::Utility::Debug{} << "Success";
+  std::cout << "Test package for Corrade\n";
+  
+  #ifndef VERSION_2019_10
+    std::cout << "Corrade " << CORRADE_VERSION_YEAR << "." << CORRADE_VERSION_MONTH << std::endl;
+  #endif
+
+  #ifdef WITH_UTILITY
+    Corrade::Utility::Debug{} << "Success";
+  #endif
 }
