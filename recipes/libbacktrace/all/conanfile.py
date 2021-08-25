247bf10dd8f274e98105725882a8e9b0413283df
Anonymous Maarten <madebr@users.noreply.github.com>
2021-08-25 06:03:48 +0200

    (#6999) libbacktrace: modernize
    * libbacktrace: modernize
    
    * libbacktrace: add `-FS` for MSVC>=12
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/libbacktrace/all/conanfile.py
+++ recipes/libbacktrace/all/conanfile.py
@@ -1,6 +1,6 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
-from contextlib import contextmanager
+import contextlib
 import os
 
 required_conan_version = ">=1.33.0"
@@ -12,18 +12,28 @@ class LibbacktraceConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/ianlancetaylor/libbacktrace"
     license = "BSD-3-Clause"
-    topics = ("conan", "backtrace", "stack-trace")
-
+    topics = ("backtrace", "stack-trace")
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
 
-    exports_sources = "patches/**"
+    exports_sources = "patches/*"
+
     _autotools = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -35,24 +45,24 @@ class LibbacktraceConan(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
-    def validate(self):
-        if self.settings.compiler == "Visual Studio" and self.options.shared:
-            raise ConanInvalidConfiguration("libbacktrace shared is not supported with Visual Studio")
-
     def build_requirements(self):
-        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/cci.latest")
         if self.settings.compiler == "Visual Studio":
             self.build_requires("automake/1.16.3")
+
+    def validate(self):
+        if self.settings.compiler == "Visual Studio" and self.options.shared:
+            raise ConanInvalidConfiguration("libbacktrace shared is not supported with Visual Studio")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
-    @contextmanager
+    @contextlib.contextmanager
     def _build_context(self):
         if self.settings.compiler == "Visual Studio":
-            with tools.vcvars(self.settings):
+            with tools.vcvars(self):
                 env = {
                     "CC": "{} cl -nologo".format(tools.unix_path(self.deps_user_info["automake"].compile)),
                     "CXX": "{} cl -nologo".format(tools.unix_path(self.deps_user_info["automake"].compile)),
@@ -69,13 +79,14 @@ class LibbacktraceConan(ConanFile):
             return self._autotools
         self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
         self._autotools.libs = []
-        if self.settings.compiler == "Visual Studio":
+        if self.settings.compiler == "Visual Studio" and tools.Version(self.settings.compiler.version) >= "12":
             self._autotools.flags.append("-FS")
-        if self.options.shared:
-            args = ["--enable-shared", "--disable-static"]
-        else:
-            args = ["--disable-shared", "--enable-static"]
-        self._autotools.configure(configure_dir=self._source_subfolder, args=args)
+        yes_no = lambda v: "yes" if v else "no"
+        args = [
+            "--enable-shared={}".format(yes_no(self.options.shared)),
+            "--enable-static={}".format(yes_no(not self.options.shared)),
+        ]
+        self._autotools.configure(args=args, configure_dir=self._source_subfolder)
         return self._autotools
 
     def build(self):
