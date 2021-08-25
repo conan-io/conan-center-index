71ed2408e70e33edde51ea962a49788a07ae1519
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-25 13:35:50 +0200

    modernize

--- recipes/giflib/5.1.x/test_package/conanfile.py
+++ recipes/giflib/5.1.x/test_package/conanfile.py
@@ -3,8 +3,8 @@ import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
@@ -12,7 +12,8 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        img_name = os.path.join(self.source_folder, "testimg.gif")
-        bin_path = os.path.join("bin", "test_package")
-        command = "%s %s" % (bin_path, img_name)
-        self.run(command, run_environment=True)
+        if not tools.cross_building(self):
+            img_name = os.path.join(self.source_folder, "testimg.gif")
+            bin_path = os.path.join("bin", "test_package")
+            command = "{} {}".format(bin_path, img_name)
+            self.run(command, run_environment=True)
