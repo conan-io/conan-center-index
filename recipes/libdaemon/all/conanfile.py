bc4f1f0f8d8d6c007a080cb247f4080620c979d5
Gareth Sylvester-Bradley <31761158+garethsb@users.noreply.github.com>
2021-08-25 10:31:44 +0100

    Apply @SpaceIm suggestions to fix cross-build (and disable examples)
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/libdaemon/all/conanfile.py
+++ recipes/libdaemon/all/conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
-import os
 from conans.errors import ConanInvalidConfiguration
+import os
+import shutil
 
 required_conan_version = ">=1.33.0"
 
@@ -12,7 +13,6 @@ class LibDaemonConan(ConanFile):
     homepage = "http://0pointer.de/lennart/projects/libdaemon/"
     license = "LGPL-2.1-or-later"
     settings = "os", "arch", "compiler", "build_type"
-    generators = "pkg_config"
 
     options = {
         "shared": [True, False],
@@ -29,32 +29,47 @@ class LibDaemonConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    @property
+    def _user_info_build(self):
+        return getattr(self, "user_info_build", self.deps_user_info)
+
     def configure(self):
         del self.settings.compiler.cppstd
         del self.settings.compiler.libcxx
         if self.options.shared:
             del self.options.fPIC
 
+    def validate(self):
+        if self.settings.os == "Windows":
+            raise ConanInvalidConfiguration("Windows not supported")
+
+    def build_requirements(self):
+        self.build_requires("gnu-config/cci.20201022")
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
-
-    @property
-    def _configure_args(self):
-        yes_no = lambda v: "yes" if v else "no"
-        return [
-            "--enable-shared={}".format(yes_no(self.options.shared)),
-            "--enable-static={}".format(yes_no(not self.options.shared)),
-        ]
 
     def _configure_autotools(self):
         if self._autotools:
             return self._autotools
         self._autotools = AutoToolsBuildEnvironment(self)
-        self._autotools.configure(configure_dir=self._source_subfolder, args=self._configure_args)
+        yes_no = lambda v: "yes" if v else "no"
+        args = [
+            "--enable-shared={}".format(yes_no(self.options.shared)),
+            "--enable-static={}".format(yes_no(not self.options.shared)),
+            "--disable-examples",
+        ]
+        if tools.cross_building(self):
+            args.append("ac_cv_func_setpgrp_void=yes")
+        self._autotools.configure(configure_dir=self._source_subfolder, args=args)
         return self._autotools
 
     def build(self):
+        shutil.copy(self._user_info_build["gnu-config"].CONFIG_SUB,
+                    os.path.join(self._source_subfolder, "config.sub"))
+        shutil.copy(self._user_info_build["gnu-config"].CONFIG_GUESS,
+                    os.path.join(self._source_subfolder, "config.guess"))
         autotools = self._configure_autotools()
         autotools.make()
 
