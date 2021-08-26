e2e34752dde289875c66cb92859e7830bd6bf000
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2021-08-26 12:41:40 +0200

    add new version to config.yml

--- recipes/moltenvk/all/conanfile.py
+++ recipes/moltenvk/all/conanfile.py
@@ -13,7 +13,7 @@ class MoltenVKConan(ConanFile):
                   "graphics framework, enabling Vulkan applications to run " \
                   "on iOS and macOS."
     license = "Apache-2.0"
-    topics = ("conan", "moltenvk", "khronos", "vulkan", "metal")
+    topics = ("moltenvk", "khronos", "vulkan", "metal")
     homepage = "https://github.com/KhronosGroup/MoltenVK"
     url = "https://github.com/conan-io/conan-center-index"
 
