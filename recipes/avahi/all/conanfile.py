4aebd4d85cbb2c74e2bdafa1e52451a14526bc74
Gareth Sylvester-Bradley <garethsb@gmail.com>
2021-08-26 09:59:49 +0100

    Remove .la files, pkgconfig, and more dubiously, etc and share directories

--- recipes/avahi/all/conanfile.py
+++ recipes/avahi/all/conanfile.py
@@ -77,7 +77,11 @@ class AvahiConan(ConanFile):
         autotools = self._configure_autotools()
         autotools.install()
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        
+        tools.rmdir(os.path.join(self.package_folder, "etc"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
+        tools.rmdir(os.path.join(self.package_folder, "share"))
+
     def package_info(self):
         self.cpp_info.libs = [
             "avahi-client",
