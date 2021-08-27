d5b7bb4319c9f54c9a1b02f1eeb9644664ef2bdd
Gareth Sylvester-Bradley <31761158+garethsb@users.noreply.github.com>
2021-08-27 16:20:29 +0100

    Apply suggestions from code review
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/mdnsresponder/all/conanfile.py
+++ recipes/mdnsresponder/all/conanfile.py
@@ -2,6 +2,8 @@ from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild
 from conans.errors import ConanInvalidConfiguration
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class MdnsResponderConan(ConanFile):
     name = "mdnsresponder"
