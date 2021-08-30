c7c6246e3952778ad06948681c614f7157f3529c
TheClonerx <theclonerx@outlook.com>
2021-08-29 20:05:49 -0400

    Make test less modern
    It's basically C at this point!

--- recipes/game-networking-sockets/all/test_package/test_package.cpp
+++ recipes/game-networking-sockets/all/test_package/test_package.cpp
@@ -1,32 +1,33 @@
-#include <cassert>
-#include <cstdio>
-#include <cstring>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
 #include <steam/isteamnetworkingutils.h>
 #include <steam/steamnetworkingsockets.h>
 
-static std::FILE* g_fpLog = nullptr;
+static FILE* g_fpLog = NULL;
 static SteamNetworkingMicroseconds g_logTimeZero;
 
 static void DebugOutput(ESteamNetworkingSocketsDebugOutputType eType, char const* pszMsg)
 {
     SteamNetworkingMicroseconds time = SteamNetworkingUtils()->GetLocalTimestamp() - g_logTimeZero;
     if (g_fpLog) {
-        std::fprintf(g_fpLog, "%10.6f %s\n", time * 1e-6, pszMsg);
+        fprintf(g_fpLog, "%10.6f %s\n", time * 1e-6, pszMsg);
     }
 
     if (eType <= k_ESteamNetworkingSocketsDebugOutputType_Msg) {
-        std::printf("%10.6f %s\n", time * 1e-6, pszMsg);
-        std::fflush(stdout);
+        printf("%10.6f %s\n", time * 1e-6, pszMsg);
+        fflush(stdout);
     }
 
     if (eType == k_ESteamNetworkingSocketsDebugOutputType_Bug) {
-        std::fflush(stdout);
-        std::fflush(stderr);
+        fflush(stdout);
+        fflush(stderr);
         if (g_fpLog) {
-            std::fflush(g_fpLog);
+            fflush(g_fpLog);
         }
 
-        if (std::strstr(pszMsg, "SteamNetworkingGlobalLock held for")) {
+        if (strstr(pszMsg, "SteamNetworkingGlobalLock held for")) {
             return;
         }
 
@@ -44,8 +45,8 @@ int main()
     SteamNetworkingUtils()->SetDebugOutputFunction(k_ESteamNetworkingSocketsDebugOutputType_Msg, DebugOutput);
 
     SteamDatagramErrMsg errMsg;
-    if (!GameNetworkingSockets_Init(nullptr, errMsg)) {
-        std::fprintf(stderr, "GameNetworkingSockets_Init failed. %s", errMsg);
+    if (!GameNetworkingSockets_Init(NULL, errMsg)) {
+        fprintf(stderr, "GameNetworkingSockets_Init failed. %s", errMsg);
         return 1;
     }
 
