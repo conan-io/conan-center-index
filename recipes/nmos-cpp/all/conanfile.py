0bb76481e959dda488e55748b9b79ef72e469042
Gareth Sylvester-Bradley <31761158+garethsb@users.noreply.github.com>
2021-09-01 18:11:35 +0100

    Apply suggestions from code review
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/nmos-cpp/all/conanfile.py
+++ recipes/nmos-cpp/all/conanfile.py
@@ -35,8 +35,13 @@ class NmosCppConan(ConanFile):
     _cmake = None
 
     # for out-of-source build, cf. wrapper CMakeLists.txt
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
