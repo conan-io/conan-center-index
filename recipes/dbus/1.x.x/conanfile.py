3c88d0e6b07a3fbf2643a021feafce548ed8ddf1
Eric Lemanissier <eric.lemanissier@gmail.com>
2021-09-01 20:19:31 +0200

    build windows with cmake

--- recipes/dbus/1.x.x/conanfile.py
+++ recipes/dbus/1.x.x/conanfile.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from conans import AutoToolsBuildEnvironment, ConanFile, tools, CMake
 from conans.errors import ConanInvalidConfiguration
 
 
@@ -21,13 +21,18 @@ class DbusConan(ConanFile):
         "with_x11": False,
         "with_glib": False}
 
-    generators = "pkg_config"
+    generators = "pkg_config", "cmake", "cmake_find_package"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
     _autotools = None
+    _cmake = None
 
     def configure(self):
         if self.settings.os not in ("Linux", "FreeBSD"):
@@ -62,22 +67,52 @@ class DbusConan(ConanFile):
 
             args.append("--with-systemdsystemunitdir=%s" % os.path.join(self.package_folder, "lib", "systemd", "system"))
             args.append("--with-systemduserunitdir=%s" % os.path.join(self.package_folder, "lib", "systemd", "user"))
-            
+
             args.append("--disable-launchd")
             args.append("--disable-systemd")
 
             self._autotools.configure(args=args, configure_dir=self._source_subfolder)
         return self._autotools
 
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+
+            self._cmake.definitions["DBUS_BUILD_TESTS"] = False
+            self._cmake.definitions["DBUS_ENABLE_DOXYGEN_DOCS"] = False
+            self._cmake.definitions["DBUS_ENABLE_XML_DOCS"] = False
+
+            self._cmake.definitions["DBUS_BUILD_X11"] = self.options.get_safe("with_x11", False)
+            self._cmake.definitions["DBUS_WITH_GLIB"] = self.options.with_glib
+            self._cmake.definitions["DBUS_DISABLE_ASSERT"] = False
+            self._cmake.definitions["DBUS_DISABLE_CHECKS"] = False
+
+            path_to_cmake_lists = os.path.join(self._source_subfolder, "cmake")
+
+            self._cmake.configure(source_folder=path_to_cmake_lists,
+                                  build_folder=self._build_subfolder)
+        return self._cmake
+
     def build(self):
-        autotools = self._configure_autotools()
-        autotools.make()
+        tools.replace_in_file(os.path.join(self._source_subfolder, "cmake", "CMakeLists.txt"),
+                              "project(dbus)",
+                              "project(dbus)\ninclude(../../conanbuildinfo.cmake)\nconan_basic_setup()")
+        if self.settings.os == "Windows":
+            cmake = self._configure_cmake()
+            cmake.build()
+        else:
+            autotools = self._configure_autotools()
+            autotools.make()
 
     def package(self):
         self.copy(pattern="COPYING", dst="licenses",
                   src=self._source_subfolder)
-        autotools = self._configure_autotools()
-        autotools.install()
+        if self.settings.os == "Windows":
+            cmake = self._configure_cmake()
+            cmake.install()
+        else:
+            autotools = self._configure_autotools()
+            autotools.install()
 
         tools.rmdir(os.path.join(self.package_folder, "share", "doc"))
         for i in ["var", "share", "etc"]:
