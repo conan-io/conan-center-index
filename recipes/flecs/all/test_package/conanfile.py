45e919f12687b07aaccd5cd3f667339e3b767d34
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-09-03 18:18:37 +0200

    modernize

--- recipes/flecs/all/test_package/conanfile.py
+++ recipes/flecs/all/test_package/conanfile.py
@@ -8,11 +8,10 @@ class TestPackageConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["FLECS_SHARED"] = self.options["flecs"].shared
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
