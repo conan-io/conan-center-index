d5a690a0537a8d24cf95fcff3f12ed5b18722bb7
jgsogo <jgsogo@gmail.com>
2021-09-04 16:32:54 +0200

    test executables (Macos)

--- recipes/magnum/all/test_package/conanfile.py
+++ recipes/magnum/all/test_package/conanfile.py
@@ -6,14 +6,22 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package"
 
+    @property
+    def _executables(self):
+        all_execs = ("gl-info", "al-info", "distancefieldconverter", "fontconverter", "imageconverter", "sceneconverter")
+        return [it for it in all_execs if getattr(self.options["magnum"], "with_{}".format(it.replace("-", "_")))]
+
     def build(self):
         cmake = CMake(self)
+        for exec in self._executables:
+            cmake.definitions["EXEC_{}".format(exec.replace("-", "_")).upper()] = True
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self):
-            #self.run("magnum-gl-info", run_environment=True)
+            for exec in self._executables:
+                self.run("magnum-{} --help".format(exec), run_environment=True)
 
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
