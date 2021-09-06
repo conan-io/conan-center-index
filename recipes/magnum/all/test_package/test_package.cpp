70d6ecb28b2060542a965246d6ada23418373b10
Javier G. Sogo <jgsogo@gmail.com>
2021-09-06 22:18:01 +0200

    use windowless application to test

--- recipes/magnum/all/test_package/test_package.cpp
+++ recipes/magnum/all/test_package/test_package.cpp
@@ -1,18 +1,50 @@
+/*
+Example taken from 'magnum-bootstrap' repository 
+at https://github.com/mosra/magnum-bootstrap/tree/windowless.
+*/
+
 #include <iostream>
-#include "Magnum/Math/Vector.h"
-#include "Magnum/Math/StrictWeakOrdering.h"
+#include <Magnum/Magnum.h>
 
+#ifdef MAGNUM_TARGET_HEADLESS
+#include <Magnum/Platform/WindowlessEglApplication.h>
+#elif defined(CORRADE_TARGET_IOS)
+#include <Magnum/Platform/WindowlessIosApplication.h>
+#elif defined(CORRADE_TARGET_APPLE)
+#include <Magnum/Platform/WindowlessCglApplication.h>
+#elif defined(CORRADE_TARGET_UNIX)
+#if !defined(MAGNUM_TARGET_GLES) || defined(MAGNUM_TARGET_DESKTOP_GLES)
+#include <Magnum/Platform/WindowlessGlxApplication.h>
+#else
+#include <Magnum/Platform/WindowlessEglApplication.h>
+#endif
+#elif defined(CORRADE_TARGET_WINDOWS)
+#if !defined(MAGNUM_TARGET_GLES) || defined(MAGNUM_TARGET_DESKTOP_GLES)
+#include <Magnum/Platform/WindowlessWglApplication.h>
+#else
+#include <Magnum/Platform/WindowlessWindowsEglApplication.h>
+#endif
+#else
+#error no windowless application available on this platform
+#endif
 
-// TODO: We should test here using Windowless applications
+using namespace Magnum;
 
-int main() {
-    const Magnum::Math::Vector<2, Magnum::Float> v2a{1.0f, 2.0f};
-    const Magnum::Math::Vector<2, Magnum::Float> v2b{2.0f, 3.0f};
-    const Magnum::Math::Vector<2, Magnum::Float> v2c{1.0f, 3.0f};
+class MyApplication: public Platform::WindowlessApplication {
+    public:
+        explicit MyApplication(const Arguments& arguments);
 
-    Magnum::Math::StrictWeakOrdering o;
-    if (o(v2a, v2b)) {
-        std::cout << "Magnum working\n";
-    }
+        int exec() override;
+};
+
+MyApplication::MyApplication(const Arguments& arguments): Platform::WindowlessApplication{arguments} {
+    /* TODO: Add your initialization code here */
+}
+
+int MyApplication::exec() {
+    /* TODO: Add your processing code here */
+    std::cout << "Magnum working\n";
     return 0;
 }
+
+MAGNUM_WINDOWLESSAPPLICATION_MAIN(MyApplication)
