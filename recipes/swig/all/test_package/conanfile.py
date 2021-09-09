ceecc38285741d4a3fe56ad55b1f7575fb92f521
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-09-09 03:24:11 +0200

    swig: optionally build swig python module + add test_type=requires

--- recipes/swig/all/test_package/conanfile.py
+++ recipes/swig/all/test_package/conanfile.py
@@ -4,15 +4,7 @@ from conans import CMake, ConanFile, tools
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package"
-    test_type = "build_requires"
-
-    __cmake = None
-
-    @property
-    def _cmake(self):
-        if not self.__cmake:
-            self.__cmake = CMake(self)
-        return self.__cmake
+    test_type = "build_requires", "requires"  # add "requires", while not necessary, to ensure building swig when using 2 profiles
 
     @property
     def _can_build(self):
@@ -23,11 +15,13 @@ class TestPackageConan(ConanFile):
     def build(self):
         self.run("swig -swiglib", run_environment=True)
         if self._can_build:
-            self._cmake.configure()
-            self._cmake.build()
+            cmake = CMake(self)
+            cmake.verbose = True
+            cmake.configure()
+            cmake.build()
 
     def test(self):
         if not tools.cross_building(self):
-            if self._can_build:
-                self._cmake.test(output_on_failure=True)
+            cmake = CMake(self)
+            cmake.test(output_on_failure=True)
             self.run("swig -version", run_environment=True)
