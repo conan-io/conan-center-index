2dfbc709bf2340a5a98eceed4c4e82f9eb0444bc
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2021-09-09 09:47:20 +0200

    add volk/1.2.182

--- recipes/volk/all/conanfile.py
+++ recipes/volk/all/conanfile.py
@@ -44,8 +44,8 @@ class VolkConan(ConanFile):
         self.requires("vulkan-headers/" + self.version)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("volk-{}".format(self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
