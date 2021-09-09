2a0ac7f5242a7805954d1551f9277ead33da691e
Jairard <gaetan.champarnaud@gmail.com>
2021-09-09 06:32:50 +0000

    (#7184) Add recipe for msdf-atlas-gen
    * Add recipe for msdf-atlas-gen
    
    * Apply suggestions from code review
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * Revert adding TARGETS as conan_basic_setup argument
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

---
+++ recipes/msdf-atlas-gen/all/test_package/conanfile.py
@@ -0,0 +1,28 @@
+from conans import ConanFile, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+
+    @property
+    def _atlas_texture_file(self):
+        return os.path.join(self.build_folder, "atlas_texture.png")
+
+    @property
+    def _atlas_desc_file(self):
+        return os.path.join(self.build_folder, "atlas_desc.json")
+
+    def test(self):
+        if not tools.cross_building(self):
+            ttf_path = os.path.join(
+                self.source_folder, "Sacramento-Regular.ttf")
+            charset_path = os.path.join(
+                self.source_folder, "uppercase_charset")
+
+            ret_code = self.run(
+                "msdf-atlas-gen -font {} -charset {} -imageout {} -json {}".format(ttf_path, charset_path, self._atlas_texture_file, self._atlas_desc_file), run_environment=True)
+
+            assert ret_code == 0
+            assert os.path.isfile(self._atlas_texture_file)
+            assert os.path.isfile(self._atlas_desc_file)
