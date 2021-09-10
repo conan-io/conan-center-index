771cd2f90303c6df2145cbb332677442ee037271
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-09-10 04:00:32 +0200

    openssl: fix MSVC install + remove options unsupported by openssl 3 + openssl exe is only available when building with no_stio=False

--- recipes/openssl/3.x.x/test_package/conanfile.py
+++ recipes/openssl/3.x.x/test_package/conanfile.py
@@ -13,8 +13,6 @@ class TestPackageConan(ConanFile):
             cmake.definitions["CONAN_LIBCXX"] = ""
 
         cmake.definitions["OPENSSL_WITH_ZLIB"] = not self.options["openssl"].no_zlib
-        if self.settings.compiler == "Visual Studio":
-            cmake.definitions["OPENSSL_MSVC_STATIC_RT"] = "MT" in self.settings.compiler.runtime
 
         cmake.configure()
         cmake.build()
@@ -27,5 +25,6 @@ class TestPackageConan(ConanFile):
             bin_path = os.path.join("bin", "digest")
             self.run(bin_path, run_environment=True)
 
-            self.run("openssl version", run_environment=True)
+            if not self.options["openssl"].no_stdio:
+                self.run("openssl version", run_environment=True)
         assert os.path.exists(os.path.join(self.deps_cpp_info["openssl"].rootpath, "licenses", "LICENSE.txt"))
