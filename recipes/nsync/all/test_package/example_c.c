8410cac9701a03040e442c4d576aad0a79245982
Alexande B <abobrikovich@gmail.com>
2021-09-10 10:44:17 +0200

    #4806 nsync: fix missing nsync_mu_init in tests

--- recipes/nsync/all/test_package/example_c.c
+++ recipes/nsync/all/test_package/example_c.c
@@ -2,6 +2,7 @@
 
 int main() {
     nsync_mu testing_mu;
+    nsync_mu_init (&testing_mu);
     nsync_mu_lock (&testing_mu);
     nsync_mu_unlock (&testing_mu);
     return 0;
--- recipes/nsync/all/test_package/example_cpp.cpp
+++ recipes/nsync/all/test_package/example_cpp.cpp
@@ -2,6 +2,7 @@
 
 int main() {
     nsync::nsync_mu testing_mu;
+    nsync::nsync_mu_init (&testing_mu);
     nsync::nsync_mu_lock (&testing_mu);
     nsync::nsync_mu_unlock (&testing_mu);
     return 0;
