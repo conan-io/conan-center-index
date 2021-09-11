2e1119ae6b671bfeadf2adb3866c2d83fc07e518
Alec Edgington <alec.edgington@cambridgequantum.com>
2021-09-11 15:55:42 +0100

    Use `find_package` to find `symengine` in test package.

--- recipes/symengine/all/conanfile.py
+++ recipes/symengine/all/conanfile.py
@@ -79,4 +79,5 @@ class SymengineConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.names["cmake_find_package"] = "symengine"
+        # FIXME: symengine exports a non-namespaced `symengine` target.
         self.cpp_info.names["cmake_find_package_multi"] = "symengine"
