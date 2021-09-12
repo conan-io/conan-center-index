260a8ebd536af91a1c51671952c041e95717940a
TheSalvator <kazakovilya97@gmail.com>
2021-09-12 17:16:36 +0300

    Revert "(#7208) liburing: remove call to `platform` in validate"
    This reverts commit 4250b69b1867c89cff05feac0f291fe16667365a.

--- recipes/liburing/all/test_package/conanfile.py
+++ recipes/liburing/all/test_package/conanfile.py
@@ -1,25 +1,15 @@
 from conans import ConanFile, CMake, tools
 import os
-import platform
-import re
-
 
 class TestPackageConan(ConanFile):
-    settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
-    @property
-    def _sufficient_linux_kernel_version(self):
-        # FIXME: use kernel version of build/host machine. kernel version should be encoded in profile
-        linux_kernel_version = re.match("([0-9.]+)", platform.release()).group(1)
-        return tools.Version(linux_kernel_version) >= "5.1"
-
     def test(self):
-        if not tools.cross_building(self) and self._sufficient_linux_kernel_version:
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        bin_path = os.path.join("bin", "test_package")
+        self.run(bin_path, run_environment=True)
