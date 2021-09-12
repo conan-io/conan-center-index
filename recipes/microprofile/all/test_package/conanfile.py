d3cf62a8463fc295d1cd78df3721832722a48dc5
Vladimir Bieleny <bielenyv@gmail.com>
2021-09-12 08:06:17 +0200

    Use find_package in CMakeLists.txt for test_package

--- recipes/microprofile/all/test_package/conanfile.py
+++ recipes/microprofile/all/test_package/conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 
 class MicroprofileTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -16,4 +16,3 @@ class MicroprofileTestConan(ConanFile):
         if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
-
