3ba594dca6d1e2353d447edae1ebb3a5380eaa68
jgsogo <jgsogo@gmail.com>
2021-09-13 09:07:14 +0200

    recipe for magnum-extras

---
+++ recipes/magnum-extras/all/conanfile.py
@@ -0,0 +1,109 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+required_conan_version = ">=1.33.0"
+
+
+class MagnumExtrasConan(ConanFile):
+    name = "magnum-extras"
+    description = "Extras for the Magnum C++11/C++14 graphics engine"
+    license = "MIT"
+    topics = ("conan", "magnum", "graphics", "rendering", "3d", "2d", "opengl")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://magnum.graphics"
+
+    settings = "os", "compiler", "build_type", "arch"
+
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_player": [True, False],
+        "with_ui": [True, False],
+        "with_ui_gallery": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_player": True,
+        "with_ui": True,
+        "with_ui_gallery": True,
+    }
+    generators = "cmake", "cmake_find_package"
+    exports_sources = ["CMakeLists.txt", "patches/*"]
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+                              'set(CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/modules/" ${CMAKE_MODULE_PATH})',
+                              "")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+    
+    def requirements(self):
+        self.requires("magnum/{}".format(self.version))
+        self.requires("corrade/{}".format(self.version))
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_STATIC"] = not self.options.shared
+        self._cmake.definitions["BUILD_STATIC_PIC"] = self.options.get_safe("fPIC", False)
+        self._cmake.definitions["BUILD_TESTS"] = False
+        self._cmake.definitions["BUILD_GL_TESTS"] = False
+
+        self._cmake.definitions["WITH_PLAYER"] = self.options.with_player
+        self._cmake.definitions["WITH_UI"] = self.options.with_ui
+        self._cmake.definitions["WITH_UI_GALLERY"] = self.options.with_ui_gallery
+        
+        self._cmake.configure()
+        return self._cmake
+
+    def _patch_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+
+    def build(self):
+        self._patch_sources()
+
+        cm = self._configure_cmake()
+        cm.build()
+
+    def package(self):
+        cm = self._configure_cmake()
+        cm.install()
+
+        #tools.rmdir(os.path.join(self.package_folder, "cmake"))
+        #tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        #tools.rmdir(os.path.join(self.package_folder, "share"))
+
+        self.copy("COPYING", src=self._source_subfolder, dst="licenses")
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "MagnumExtras"
+        self.cpp_info.names["cmake_find_package_multi"] = "MagnumExtras"
+
+        if self.options.with_ui:
+            self.cpp_info.components["ui"].names["cmake_find_package"] = "Ui"
+            self.cpp_info.components["ui"].names["cmake_find_package_multi"] = "Ui"
+            self.cpp_info.components["ui"].libs = ["MagnumUi"]
+            self.cpp_info.components["ui"].requires = ["corrade::interconnect", "magnum::magnum_main", "magnum::gl", "magnum::text"]
+
+        if self.options.with_player or self.options.with_ui_gallery:
+            bin_path = os.path.join(self.package_folder, "bin")
+            self.output.info('Appending PATH environment variable: %s' % bin_path)
+            self.env_info.path.append(bin_path)
