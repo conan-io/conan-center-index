71779d1ce6131ee277e323943837905b24771abb
m.tayseer <mostafa.tayseer@agile-robots.com>
2021-09-16 18:42:49 +0200

    fix: use tarballs instead of git

--- recipes/ua-nodeset/all/conanfile.py
+++ recipes/ua-nodeset/all/conanfile.py
@@ -4,29 +4,26 @@ import os
 
 class UaNodeSetConan(ConanFile):
     name = "UA-Nodeset"
-    license = ""
-    author = 'OPCFoundation'
+    license = "UNKNOWN"  # https://github.com/OPCFoundation/UA-Nodeset/issues/79
     description = "This repository contains UANodeSets and other normative files which are released with a specification"
-    url = 'https://github.com/OPCFoundation/UA-Nodeset'
-    default_user = "ar"
-    default_channel = "thirdparty"
-    generators = "cmake", "virtualenv"
-    settings = 'os'
+    homepage = "https://github.com/OPCFoundation/UA-Nodeset"
+    url = "https://www.github.com/conan-io/conan-center-index"
+    
+    no_copy_source = True
 
 
     def source(self):
-        git = tools.Git()
-        git.clone(
-            url="https://github.com/OPCFoundation/UA-Nodeset.git",
-            branch="v{}".format(self.version))
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("{}-{}".format(self.name, self.version), self._source_subfolder)
 
     def build(self):
         pass
  
 
     def package(self):
-        self.copy("*", dst="ua-nodeset", src="")
+        self.copy("*", dst="res")
 
 
     def package_info(self):
-        self.env_info.open62541_NODESET_DIR = os.path.join(self.package_folder, "ua-nodeset")
+        self.cpp_info.libdirs = []
+        self.user_info.nodeset_path = os.path.join(self.package_folder, "res")
