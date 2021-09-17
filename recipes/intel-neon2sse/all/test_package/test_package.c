5af23007b2af5d481e00efbc71934f205fd422c8
Eric Riff <ericriff@gmail.com>
2021-09-16 23:24:44 -0300

    Fix test_package and disable warning for cleaner logs

--- recipes/intel-neon2sse/all/test_package/test_package.c
+++ recipes/intel-neon2sse/all/test_package/test_package.c
@@ -1,18 +1,25 @@
 #include <NEON_2_SSE.h>
 #include <stdio.h>
 
+#ifdef _MSC_VER
+#pragma warning(disable: 4996)
+#endif
+
 int main()
 {
     uint64_t a = 0x0102030405060708;
     uint64_t b = 0x0102030405060708;
-    uint64_t c;
+    union {
+        char c[8];
+        uint64_t i;
+    } c;
     uint8x8_t va = vcreate_u8(a);
     uint8x8_t vb = vcreate_u8(b);
     uint8x8_t vc = vmul_u8(va, vb);
 
-    vst1_u64(&c, vc);
+    vst1_u8(c.c, vc);
 
-    if (c != 0x0104091019243140) {
+    if (c.i != 0x0104091019243140) {
         fprintf(stderr, "Wrong result\n");
         return 1;
     } else {
