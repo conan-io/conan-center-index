fbaf260e98c485900675da6cd0dea247b1f668aa
Nolan Crane <11447537+NolanC33@users.noreply.github.com>
2021-09-20 16:29:58 -0400

    Update recipes/gsl/all/test_package/conanfile.py
    Co-authored-by: SSE4 <tomskside@gmail.com>

--- recipes/gsl/all/test_package/conanfile.py
+++ recipes/gsl/all/test_package/conanfile.py
@@ -6,6 +6,12 @@ class GslTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package_multi"
 
+    def build_requirements(self):
+        if self.settings.os == "Macos" and self.settings.arch == "armv8":
+            # Attempting to use @rpath without CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG being
+            # set. This could be because you are using a Mac OS X version less than 10.5
+            # or because CMake's platform configuration is corrupt.
+            self.build_requires("cmake/3.20.1")
     def build(self):
         cmake = CMake(self)
         cmake.configure()
