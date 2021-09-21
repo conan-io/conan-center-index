0b9e0c76799bffa54169ddede8b7dc6ae16ed2ed
ItsBasi <5033630+ItsBasi@users.noreply.github.com>
2021-09-21 16:27:17 +0200

    opencl-icd-loader: Use strip_root instead of a manual rename

--- recipes/opencl-icd-loader/all/conanfile.py
+++ recipes/opencl-icd-loader/all/conanfile.py
@@ -36,8 +36,7 @@ class OpenclIcdLoaderConan(ConanFile):
         self.requires("opencl-headers/{}".format(self.version))
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        tools.rename("OpenCL-ICD-Loader-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
