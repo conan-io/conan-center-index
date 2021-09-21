3fc43ceee08f7b5fb16693de4d38231a0c379f24
Siim Meerits <sh0@yutani.ee>
2021-09-21 15:03:27 +0300

    openimageio/all: Modernize test package.

--- recipes/openimageio/all/test_package/conanfile.py
+++ recipes/openimageio/all/test_package/conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         version = tools.Version(self.deps_cpp_info["openimageio"].version)
