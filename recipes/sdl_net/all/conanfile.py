a3715d4b0c6d172a9402b792738d7ab59b1791a7
Connor Clark <c.clark000@hotmail.com>
2021-09-22 07:33:56 -0500

    sdl_net: moving shared version check back to validate

--- recipes/sdl_net/all/conanfile.py
+++ recipes/sdl_net/all/conanfile.py
@@ -48,6 +48,10 @@ class SdlnetConan(ConanFile):
     def requirements(self):
         self.requires("sdl/2.0.16")
 
+    def validate(self):
+        if self.settings.compiler == "Visual Studio" and self.options.shared:
+            raise ConanInvalidConfiguration("sdl_net is not supported with Visual Studio")
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
@@ -62,8 +66,6 @@ class SdlnetConan(ConanFile):
         return self._cmake
 
     def build(self):
-        if self.settings.compiler == "Visual Studio" and self.options.shared:
-            raise ConanInvalidConfiguration("sdl_net is not supported with Visual Studio")
         # FIXME: check that major version of sdl_net is the same than sdl (not possible yet in validate())
         if tools.Version(self.deps_cpp_info["sdl"]).major != tools.Version(self.version):
             raise ConanInvalidConfiguration(f"The major versions of {self.name} and sdl must be the same")
