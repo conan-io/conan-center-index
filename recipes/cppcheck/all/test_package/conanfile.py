578d4d1a390d2b6b8e4afbc080a43df8dc237eb6
Stefan Profanter <stefan.profanter@agile-robots.com>
2021-09-22 12:28:04 +0200

    fix: correction of if

--- recipes/cppcheck/all/test_package/conanfile.py
+++ recipes/cppcheck/all/test_package/conanfile.py
@@ -8,7 +8,8 @@ class TestPackageConan(ConanFile):
         if not tools.cross_building(self.settings):
             self.run("cppcheck --version", run_environment=True)
             # On windows we need to explicitly use python to run the python script
-            if self.settings.os == 'Windows' and tools.which("python3"):
-                self.run("python3 $CPPCHECK_HTMLREPORT -h", run_environment=True)
+            if self.settings.os == 'Windows':
+                if tools.which("python3"):
+                    self.run("python3 $CPPCHECK_HTMLREPORT -h", run_environment=True)
             else:
                 self.run("cppcheck-htmlreport -h", run_environment=True)
