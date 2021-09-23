f629782afac515eaad85a41fce369ad5fdea9f2d
toge <toge.mail@gmail.com>
2021-09-23 22:09:03 +0900

    bump numcpp to 2.5.1 add package options

--- recipes/numcpp/all/conanfile.py
+++ recipes/numcpp/all/conanfile.py
@@ -11,6 +11,14 @@ class NumCppConan(ConanFile):
     topics = ("python", "numpy", "numeric")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/dpilger26/NumCpp"
+    options = {
+        "use_boost" : [True, False],
+        "use_thread" : [True, False],
+    }
+    default_options = {
+        "use_boost" : True,
+        "use_thread" : False,
+    }
     license = "MIT"
     no_copy_source = True
     settings = "compiler"
@@ -41,7 +49,13 @@ class NumCppConan(ConanFile):
             raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
 
     def requirements(self):
-        self.requires("boost/1.75.0")
+        if tools.Version(self.version) < "2.5.0" or self.options.use_boost:
+            self.requires("boost/1.75.0")
+
+    def config_options(self):
+        if tools.Version(self.version) < "2.5.0":
+            del self.options.use_boost
+            self.default_options["use_thread"] = True
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -57,5 +71,13 @@ class NumCppConan(ConanFile):
         self.info.header_only()
         
     def package_info(self):
+        if tools.Version(self.version) >= "2.5.0" and self.options.use_boost == False:
+            self.cpp_info.cxxflags.append("-DNUMCPP_NO_USE_BOOST")
+
+        if tools.Version(self.version) < "2.5.0" and self.options.use_thread == False:
+            self.cpp_info.cxxflags.append("-DNO_MULTITHREAD")
+        if tools.Version(self.version) >= "2.5.0" and self.options.use_thread == True:
+            self.cpp_info.cxxflags.append("-DNUMCPP_USE_MULTITHREAD")
+
         self.cpp_info.names["cmake_find_package"] = "NumCpp"
         self.cpp_info.names["cmake_find_package_multi"] = "NumCpp"
