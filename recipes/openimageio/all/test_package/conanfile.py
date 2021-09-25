9e7104c59a339ee23faca55c160bf56cf09f254a
Siim Meerits <sh0@yutani.ee>
2021-09-25 16:31:43 +0300

    (#7255) openimageio/all: Add new versions and many new dependencies.
    * openimageio/all: Add new versions and many new dependencies.
    
    Adds many new dependency libraries which are now available in Conan.
    The 'with_dicom' and 'with_raw' flag names and default values were left
    as-is to preserve compatibility.
    
    * openimageio/all: Remove dependency overrides.
    
    * openimageio/all: Modernize test package.
    
    * openimageio/all: Bump dependency versions updated recently.

--- recipes/openimageio/all/test_package/conanfile.py
+++ recipes/openimageio/all/test_package/conanfile.py
@@ -3,14 +3,16 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
+        version = tools.Version(self.deps_cpp_info["openimageio"].version)
         cmake = CMake(self)
+        cmake.definitions["CMAKE_CXX_STANDARD"] = 14 if version >= "2.3.0.0" else 11
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
