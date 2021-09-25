b3e5e8b055f44a69d7904aad95ff4a296769c1ef
jgsogo <jgsogo@gmail.com>
2021-09-25 13:57:17 +0200

    use NodeJS from conan package

--- recipes/emsdk/all/conanfile.py
+++ recipes/emsdk/all/conanfile.py
@@ -1,17 +1,17 @@
 from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 import os
-from conans.errors import ConanInvalidConfiguration
+import json
 
 required_conan_version = ">=1.33.0"
 
 
 class EmSDKConan(ConanFile):
     name = "emsdk"
-    description = "Emscripten is an Open Source LLVM to JavaScript compiler"
+    description = "Emscripten SDK. Emscripten is an Open Source LLVM to JavaScript compiler"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/kripken/emscripten"
-    topics = ("conan", "emsdk", "emscripten", "sdk")
+    topics = ("emsdk", "emscripten", "sdk")
     license = "MIT"
     settings = "os", "arch"
 
@@ -20,6 +20,11 @@ class EmSDKConan(ConanFile):
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    def requirements(self):
+        self.requires("nodejs/16.3.0")
+        # self.requires("python")  # FIXME: Not available as Conan package
+        # self.requires("wasm")  # FIXME: Not available as Conan package
 
     def validate(self):
         if hasattr(self, "settings_target") and self.settings_target:
@@ -38,28 +43,39 @@ class EmSDKConan(ConanFile):
         if os.name == "posix":
             os.chmod(filename, os.stat(filename).st_mode | 0o111)
 
-    def package(self):
+    def _tools_for_version(self):
+        ret = {}
+        # Select release-upstream from version (wasm-binaries)
+        with open(os.path.join(self.build_folder, self._source_subfolder, "emscripten-releases-tags.json"), 'r') as f:
+            data = json.load(f)
+            ret['wasm'] = "releases-upstream-{}-64bit".format(data["releases"][self.version])
+        # Select python and node versions
+        with open(os.path.join(self.build_folder, self._source_subfolder, "emsdk_manifest.json"), 'r') as f:
+            data = json.load(f)
+            tools = data["tools"]
+            python = next((it for it in tools if (it["id"] == "python" and not it.get("is_old", False))), None)
+            ret["python"] = "python-{}-64bit".format(python["version"])
+            node = next((it for it in tools if (it["id"] == "node" and not it.get("is_old", False))), None)
+            ret["nodejs"] = "node-{}-64bit".format(node["version"])
+        return ret
+
+    def build(self):
         with tools.chdir(self._source_subfolder):
             emsdk = "emsdk.bat" if tools.os_info.is_windows else "./emsdk"
-            if os.path.isfile("python_selector"):
-                self._chmod_plus_x("python_selector")
             self._chmod_plus_x("emsdk")
-            self.run("%s update" % emsdk)
-            if os.path.isfile("python_selector"):
-                self._chmod_plus_x("python_selector")
-            self._chmod_plus_x("emsdk")
-
-            self.run("%s install %s" % (emsdk, self.version))
-            self.run("%s activate %s --embedded" % (emsdk, self.version))
+            
+            # Install required tools
+            required_tools = self._tools_for_version()
+            for key, value in required_tools.items():
+                if key != 'nodejs':
+                    self.run("%s install %s" % (emsdk, value))
+                    self.run("%s activate %s" % (emsdk, value))
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses",
-                  src=self._source_subfolder)
-        self.copy(pattern="*", dst="bin",
-                  src=self._source_subfolder)
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*", dst="bin", src=self._source_subfolder)
         emscripten = os.path.join(self.package_folder, "bin", "upstream", "emscripten")
-        toolchain = os.path.join(
-            emscripten, "cmake", "Modules", "Platform", "Emscripten.cmake")
+        toolchain = os.path.join(emscripten, "cmake", "Modules", "Platform", "Emscripten.cmake")
         # FIXME: conan should add the root of conan package requirements to CMAKE_PREFIX_PATH (LIBRARY/INCLUDE -> ONLY; PROGRAM -> NEVER)
         # allow to find conan libraries
         tools.replace_in_file(toolchain,
@@ -85,33 +101,27 @@ class EmSDKConan(ConanFile):
         em_config = os.path.join(emsdk, "bin", ".emscripten")
         emscripten = os.path.join(emsdk, "bin", "upstream", "emscripten")
         em_cache = os.path.join(emsdk,  "bin", ".emscripten_cache")
-        toolchain = os.path.join(
-            emscripten, "cmake", "Modules", "Platform", "Emscripten.cmake")
+        toolchain = os.path.join(emscripten, "cmake", "Modules", "Platform", "Emscripten.cmake")
 
         self.output.info("Appending PATH environment variable: %s" % emsdk)
         self.env_info.PATH.append(emsdk)
 
-        self.output.info(
-            "Appending PATH environment variable: %s" % emscripten)
+        self.output.info("Appending PATH environment variable: %s" % emscripten)
         self.env_info.PATH.append(emscripten)
 
         self.output.info("Creating EMSDK environment variable: %s" % emsdk)
         self.env_info.EMSDK = emsdk
 
-        self.output.info(
-            "Creating EMSCRIPTEN environment variable: %s" % emscripten)
+        self.output.info("Creating EMSCRIPTEN environment variable: %s" % emscripten)
         self.env_info.EMSCRIPTEN = emscripten
 
-        self.output.info(
-            "Creating EM_CONFIG environment variable: %s" % em_config)
+        self.output.info("Creating EM_CONFIG environment variable: %s" % em_config)
         self.env_info.EM_CONFIG = em_config
 
-        self.output.info(
-            "Creating EM_CACHE environment variable: %s" % em_cache)
+        self.output.info("Creating EM_CACHE environment variable: %s" % em_cache)
         self.env_info.EM_CACHE = em_cache
 
-        self.output.info(
-            "Creating CONAN_CMAKE_TOOLCHAIN_FILE environment variable: %s" % toolchain)
+        self.output.info("Creating CONAN_CMAKE_TOOLCHAIN_FILE environment variable: %s" % toolchain)
         self.env_info.CONAN_CMAKE_TOOLCHAIN_FILE = toolchain
 
         self.env_info.CC = self._define_tool_var("CC", "emcc")
