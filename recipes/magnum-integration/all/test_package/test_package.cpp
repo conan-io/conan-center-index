aad68b46e6e14d1bb37348e27f1cf1ad4d5fcceb
jgsogo <jgsogo@gmail.com>
2021-09-26 11:20:03 +0200

    add tests

--- recipes/magnum-integration/all/test_package/test_package.cpp
+++ recipes/magnum-integration/all/test_package/test_package.cpp
@@ -1,24 +1,71 @@
 
 #include <iostream>
 
+#ifdef WITH_BULLET
+    #include "Magnum/BulletIntegration/Integration.h"
+
+    void with_bullet() {
+        std::cout << "With Bullet\n";
+        Magnum::Math::Vector3<btScalar> a{btScalar(1.0), btScalar(2.0), btScalar(3.0)};
+        btVector3 b{btScalar(1.0), btScalar(2.0), btScalar(3.0)};
+        assert(Magnum::Math::Vector3<btScalar>{b} == a);
+    }
+#endif
+
+#ifdef WITH_EIGEN
+    #include <Eigen/Geometry>
+    #include "Magnum/EigenIntegration/Integration.h"
+
+    void with_eigen() {
+        std::cout << "With Eigen\n";
+        Magnum::Math::Quaternion<float> q{};
+        Eigen::Quaternion<float> eq{q.scalar(), q.vector().x(), q.vector().y(), q.vector().z()};
+    }
+#endif
+
+#ifdef WITH_GLM
+    #include "Magnum/Magnum.h"
+    #include "Magnum/Math/Matrix3.h"
+    #include "Magnum/Math/Matrix4.h"
+    #include "Magnum/GlmIntegration/Integration.h"
+
+    void with_glm() {
+        std::cout << "With GLM\n";
+        Magnum::Math::BoolVector<2> a{0x6};
+        glm::bvec2 b{false, true};
+        assert(glm::bvec2(a) == b);
+    }
+#endif
+
+#ifdef WITH_IMGUI
+    #include <Magnum/Magnum.h>
+    #include <Magnum/Math/Color.h>
+    #include "Magnum/ImGuiIntegration/Integration.h"
+
+    void with_imgui() {
+        std::cout << "With ImGui\n";
+        ImVec2 imVec2{1.1f, 1.2f};
+        Magnum::Vector2 vec2(1.1f, 1.2f);
+        assert(Magnum::Vector2{imVec2} == vec2);
+    }
+#endif
+
 int main() {
     #ifdef WITH_BULLET
-        std::cout << "With Bullet\n";
+        with_bullet();
     #endif
-    #ifdef WITH_DART
-        std::cout << "With Dart\n";
-    #endif
+
     #ifdef WITH_EIGEN
-        std::cout << "With Eigen\n";
+        with_eigen();
     #endif
+
     #ifdef WITH_GLM
-        std::cout << "With Glm\n";
+        with_glm();
     #endif
+
     #ifdef WITH_IMGUI
-        std::cout << "With ImGui\n";
+        with_imgui();
     #endif
-    #ifdef WITH_OVR
-        std::cout << "With Ovr\n";
-    #endif
+
     return 0;
 }
