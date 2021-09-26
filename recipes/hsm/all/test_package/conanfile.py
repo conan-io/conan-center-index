d9960fb374ba323bc7be73db018f773f8330d125
SSE4 <tomskside@gmail.com>
2021-09-26 03:28:30 -0700

    Update recipes/hsm/all/test_package/conanfile.py

--- recipes/hsm/all/test_package/conanfile.py
+++ recipes/hsm/all/test_package/conanfile.py
@@ -16,4 +16,4 @@ class HsmTestConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+            self.run(".%sexample" % os.sep, run_environment=True)
