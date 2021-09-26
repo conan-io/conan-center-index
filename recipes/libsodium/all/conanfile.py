ef3caab0b0f215cabfe1aaa1aeb85e194933f835
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-09-26 21:22:52 +0200

    robust loop in patches

--- recipes/libsodium/all/conanfile.py
+++ recipes/libsodium/all/conanfile.py
@@ -138,7 +138,7 @@ class LibsodiumConan(ConanFile):
         return self._autotools
 
     def build(self):
-        for patch in self.conan_data["patches"][self.version]:
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         if self.settings.os == "Macos":
             tools.replace_in_file(os.path.join(self._source_subfolder, "configure"), r"-install_name \$rpath/", "-install_name ")
