c8c83938dc9e7d3f50901ffda48cd127b26704b9
Samuel Dowling <samuel.dowling@protonmail.com>
2021-09-27 22:20:47 +0930

    Apply suggestions from code review
    * Configure test_package to use targets
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>

--- recipes/armadillo/all/test_package/conanfile.py
+++ recipes/armadillo/all/test_package/conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 
 class ArmadilloTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
