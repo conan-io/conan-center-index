42212b566b07ede50262d50d1a70d20128effaf8
theirix <theirix@gmail.com>
2021-10-03 01:31:19 +0300

    Switch to config cmake

--- recipes/opencolorio/all/test_package/conanfile.py
+++ recipes/opencolorio/all/test_package/conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class DefaultNameConan(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
