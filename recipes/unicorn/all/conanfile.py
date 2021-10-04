f24fe95507827f0203c77bb7d958cb9dc2a10424
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-10-04 16:45:15 +0200

    unicorn: pass CMAKE_SYSTEM_{NAME/PLATFORM} if cross building

--- recipes/unicorn/all/conanfile.py
+++ recipes/unicorn/all/conanfile.py
@@ -94,6 +94,10 @@ class UnicornConan(ConanFile):
         cmake.definitions["UNICORN_INSTALL"] = True
         cmake.definitions["UNICORN_BUILD_SAMPLES"] = False
         cmake.definitions["UNICORN_ARCH"] = " ".join(self._supported_archs)
+        if tools.cross_building(self, skip_x64_x86=True):
+            # FIXME: this needs conan support (https://github.com/conan-io/conan/pull/8026)
+            cmake.definitions["CMAKE_SYSTEM_NAME"] = str(self.settings.os)
+            cmake.definitions["CMAKE_SYSTEM_PROCESSOR"] = str(self.settings.arch)
         if self._needs_jwasm:
             cmake.definitions["CMAKE_ASM_MASM_COMPILER"] = self._jwasm_wrapper
             if self.settings.arch == "x86_64":
