2b2ec997470e2e0631e59ac83e38a60707efa124
SSE4 <tomskside@gmail.com>
2021-10-03 23:26:05 -0700

    Update recipes/hsm/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/hsm/all/conanfile.py
+++ recipes/hsm/all/conanfile.py
@@ -27,8 +27,9 @@ class HsmConan(ConanFile):
             raise ConanInvalidConfiguration("clang 8+ is required")
         if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < 8:
             raise ConanInvalidConfiguration("GCC 8+ is required")
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def build(self):
         cmake = CMake(self)
