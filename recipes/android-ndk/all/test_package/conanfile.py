c0e843aa0274d207a0cf7d8418b8460de131da0d
jgsogo <jgsogo@gmail.com>
2021-10-04 18:49:03 +0200

    builds a library in test_package

--- recipes/android-ndk/all/test_package/conanfile.py
+++ recipes/android-ndk/all/test_package/conanfile.py
@@ -1,11 +1,18 @@
-from conans import ConanFile, tools
+import os
+from conans import ConanFile, tools, CMake
 
 
 class TestPackgeConan(ConanFile):
     settings = "os", "arch"
+    test_type = "build_requires"
+    generators = "cmake"
 
     def build(self):
-        pass
+        # It only makes sense to build a library, if the target os is Android
+        if self.settings.os == "Android":
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
 
     def test(self):
         if not tools.cross_building(self):
@@ -13,3 +20,9 @@ class TestPackgeConan(ConanFile):
                 self.run("ndk-build.cmd --version", run_environment=True)
             else:
                 self.run("ndk-build --version", run_environment=True)
+
+        # Run the project that was built using emsdk
+        if self.settings.os == "Android":
+            test_file = os.path.join("bin", "test_package")
+            assert os.path.exists(test_file)
+            # self.run("android-emulator {}".format(test_file), run_environment=True)
