04e5551fd41cb513891fad88edea420238784e5e
Eric Riff <ericriff@gmail.com>
2021-10-05 19:42:34 -0300

    Disable shared builds on gcc since they error out on CCI

--- recipes/opengv/all/conanfile.py
+++ recipes/opengv/all/conanfile.py
@@ -58,8 +58,8 @@ class opengvConan(ConanFile):
         if self.settings.os == "Windows":
             raise ConanInvalidConfiguration("Windows builds are not supported by this recipe.")
         #FIXME disable this one CCI has more RAM available
-        if (self.settings.compiler == "gcc"and tools.Version(self.settings.compiler.version) < "6.0" and self.options.shared):
-                raise ConanInvalidConfiguration("Shared builds not supported with gcc5 since CCI errors out due to excessive memory usage.")
+        if (self.settings.compiler == "gcc" and self.options.shared):
+                raise ConanInvalidConfiguration("Shared builds not supported with gcc since CCI errors out due to excessive memory usage.")
 
     def config_options(self):
         if self.settings.os == "Windows":
