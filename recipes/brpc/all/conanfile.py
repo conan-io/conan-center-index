f33d4375430bd25945b0877c617efc1f27fb2287
ehds <ehds@qq.com>
2021-10-05 18:49:33 +0800

    fix url

--- recipes/brpc/all/conanfile.py
+++ recipes/brpc/all/conanfile.py
@@ -7,7 +7,7 @@ from conans.tools import Version
 class BRPCCppConan(ConanFile):
     name = "brpc"
     description = "Industrial-grade RPC framework used throughout Baidu, with 1,000,000+ instances and thousands kinds of services. `brpc` means `better RPC`."
-    url = "https://github.com/apache/incubator-brpc"
+    url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://brpc.apache.org/"
     topics = ("conan", "bprc", "apache", "baidu", "rpc")
     license = ("Apache License 2.0",)
