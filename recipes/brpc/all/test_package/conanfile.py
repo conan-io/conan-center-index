c99bbe6e471b2cb47da90cee34c0032010a68a51
ehds <ehds@qq.com>
2021-10-06 17:13:01 +0800

    update build test on macos

--- recipes/brpc/all/test_package/conanfile.py
+++ recipes/brpc/all/test_package/conanfile.py
@@ -6,6 +6,9 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    def requirements(self):
+        self.requires("openssl/1.1.0l")
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
