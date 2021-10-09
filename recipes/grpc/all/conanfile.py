ed532f2acd786a0be783a4774034179cfb1149c8
TheClonerx <theclonerx@outlook.com>
2021-10-08 22:47:30 -0300

    Try using a patch to set the cxx_std_11 property

--- recipes/grpc/all/conanfile.py
+++ recipes/grpc/all/conanfile.py
@@ -11,7 +11,7 @@ class grpcConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/grpc/grpc"
     license = "Apache-2.0"
-    exports_sources = ["CMakeLists.txt", "cmake/*"]
+    exports_sources = ["CMakeLists.txt", "cmake/*", "patches/**"]
     generators = "cmake", "cmake_find_package", "cmake_find_package_multi"
     short_paths = True
 
@@ -139,7 +139,12 @@ class grpcConan(ConanFile):
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
+    def _patch_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+
     def build(self):
+        self._patch_sources()
         cmake = self._configure_cmake()
         cmake.build()
 
