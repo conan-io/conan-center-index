7c053b9a2e1e4ec1e96ff0d09dd84bdb55bc3518
woidpointer <woidpointer@gmail.com>
2021-10-09 18:45:46 +0200

    Update recipes/lightpcapng/all/test_package/conanfile.py
    Co-authored-by: Sparik Hayrapetyan <sparikhayrapetyan@gmail.com>

--- recipes/lightpcapng/all/test_package/conanfile.py
+++ recipes/lightpcapng/all/test_package/conanfile.py
@@ -12,7 +12,7 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             arg_path = bin_path
             if self.settings.os == "Windows":
