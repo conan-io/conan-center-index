a0d7a5949e85a71d9eddd47adb1ca62189f72bf7
José <33236893+TheClonerx@users.noreply.github.com>
2021-10-11 06:32:40 -0300

    (#7626) autoconf/all: Bump required m4

--- recipes/autoconf/all/conanfile.py
+++ recipes/autoconf/all/conanfile.py
@@ -27,7 +27,7 @@ class AutoconfConan(ConanFile):
         return getattr(self, "settings_build", self.settings)
 
     def requirements(self):
-        self.requires("m4/1.4.18")
+        self.requires("m4/1.4.19")
 
     def build_requirements(self):
         if hasattr(self, "settings_build"):
