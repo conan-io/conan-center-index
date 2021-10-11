3f2589477d9957fd48b71e3260f3356383b78ac6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-10-11 14:06:53 -0400

    convention

--- recipes/embree3/all/conanfile.py
+++ recipes/embree3/all/conanfile.py
@@ -7,7 +7,7 @@ import textwrap
 required_conan_version = ">=1.33.0"
 
 
-class Embree(ConanFile):
+class EmbreeConan(ConanFile):
     name = "embree3"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
