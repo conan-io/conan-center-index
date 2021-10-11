33382f75053765d1666fc289b090ada068b14a74
woidpointer <woidpointer@gmail.com>
2021-10-11 21:11:11 +0200

    Update recipes/lightpcapng/all/test_package/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/lightpcapng/all/test_package/conanfile.py
+++ recipes/lightpcapng/all/test_package/conanfile.py
@@ -14,5 +14,4 @@ class TestPackageConan(ConanFile):
     def test(self):
         if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
-            arg_path = bin_path
-            self.run("{0} {1}".format(bin_path, arg_path), run_environment=True)
+            self.run(bin_path, run_environment=True)
