e36fa4126cd4868b0669393da73716abef12fa71
Maksim Petukhou <Petuhov_M@credo-dialogue.com>
2021-10-11 19:48:22 +0300

    qt5: add mkspecs include directory

--- recipes/qt/5.x.x/conanfile.py
+++ recipes/qt/5.x.x/conanfile.py
@@ -1189,6 +1189,13 @@ Examples = bin/datadir/examples""")
                 self.cpp_info.components[component_name].build_modules["cmake_find_package_multi"].append(module)
             self.cpp_info.components[component_name].builddirs.append(os.path.join("lib", "cmake", m))
 
+        qt5core_config_extras_mkspec_dir_cmake = tools.load(
+            os.path.join("lib", "cmake", "Qt5Core", "Qt5CoreConfigExtrasMkspecDir.cmake"))
+        mkspecs_dir_begin = qt5core_config_extras_mkspec_dir_cmake.find("mkspecs/")
+        mkspecs_dir_end = qt5core_config_extras_mkspec_dir_cmake.find("\"", mkspecs_dir_begin)
+        mkspecs_dir = qt5core_config_extras_mkspec_dir_cmake[mkspecs_dir_begin:mkspecs_dir_end].split('/')
+        self.cpp_info.components["qtCore"].includedirs.append(os.path.join("bin", "archdatadir", *mkspecs_dir))
+
         objects_dirs = glob.glob(os.path.join(self.package_folder, "lib", "objects-*/"))
         for object_dir in objects_dirs:
             for m in os.listdir(object_dir):
