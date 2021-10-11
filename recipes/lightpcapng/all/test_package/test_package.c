27780ef906a26a30b643a5d3196ba40f7c6bdff4
woidpointer <woidpointer@gmail.com>
2021-10-11 21:11:26 +0200

    Update recipes/lightpcapng/all/test_package/test_package.c
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/lightpcapng/all/test_package/test_package.c
+++ recipes/lightpcapng/all/test_package/test_package.c
@@ -1,12 +1,10 @@
-
+#include <stdlib.h>
 #include "light_pcapng_ext.h"
 
 int main(void) {
   const char* outfile = "conan_test.pcapng";
   light_pcapng writer = light_pcapng_open(outfile, "wb");
-
-  light_packet_interface pkt_interface = {0};
-  light_packet_header pkt_header = {0};
-  uint8_t* pkt_data = NULL;
+  light_pcapng_close(writer);
+  return EXIT_SUCCESS;
 }
 
