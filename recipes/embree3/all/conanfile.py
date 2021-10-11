fab0244b1ef1ba95c48be95505742241892b50b2
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-10-10 22:05:55 -0400

    fix cmake names

--- recipes/embree3/all/conanfile.py
+++ recipes/embree3/all/conanfile.py
@@ -2,6 +2,7 @@ from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 import glob
 import os
+import textwrap
 
 required_conan_version = ">=1.33.0"
 
@@ -133,7 +134,39 @@ class Embree(ConanFile):
         else:
             tools.rmdir(os.path.join(self.package_folder, "bin"))
 
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"embree": "embree::embree"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
+
     def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "embree"
+        self.cpp_info.names["cmake_find_package_multi"] = "embree"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
+
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.system_libs = ["dl", "m", "pthread"]
