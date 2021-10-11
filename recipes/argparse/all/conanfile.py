320d55f7e3e451546289d30d3178d88f6fd97cbe
Chris Mc <prince.chrismc@gmail.com>
2021-10-11 18:31:37 -0400

    Update conanfile.py

--- recipes/argparse/all/conanfile.py
+++ recipes/argparse/all/conanfile.py
@@ -19,7 +19,7 @@ class ArgparseConan(ConanFile):
     def _compiler_required_cpp17(self):
         return {
             "gcc": "7" if tools.Version(self.version) <= "2.1" else "8",
-            "clang": "5" if tools.Version(self.version) <= "2.1" else "11",
+            "clang": "5" if tools.Version(self.version) <= "2.1" else "7",
             "Visual Studio": "15",
             "apple-clang": "10",
         }
@@ -28,7 +28,7 @@ class ArgparseConan(ConanFile):
     def _source_subfolder(self):
         return os.path.join(self.source_folder, "source_subfolder")
 
-    def configure(self):
+    def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
             tools.check_min_cppstd(self, "17")
         try:
@@ -37,6 +37,9 @@ class ArgparseConan(ConanFile):
                 raise ConanInvalidConfiguration("This package requires c++17 support. The current compiler does not support it.")
         except KeyError:
             self.output.warn("This recipe has no support for the current compiler. Please consider adding it.")
+
+        if tools.Version(self.version) > "2.1" and self.settings.compiler == "clang":
+            raise ConanInvalidConfiguration("This recipe does not permit >2.1 with clang and stdlibc++. There may be an infrastructure issue in CCI.")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
