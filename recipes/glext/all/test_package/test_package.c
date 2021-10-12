8540370b076c9fb271e8c189bb5060fb23ecd0b2
Siim Meerits <sh0@yutani.ee>
2021-10-12 16:34:15 +0300

    glext: New package to provide 'glext.h' header.

---
+++ recipes/glext/all/test_package/test_package.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/gl.h>
+#include <GL/glext.h>
+
+int main()
+{
+    printf("GL_GLEXT_VERSION: %d\n", GL_GLEXT_VERSION);
+    return EXIT_SUCCESS;
+}
