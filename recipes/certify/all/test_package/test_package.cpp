5f7b2e9d412cf253c4090aabce3765723fa654a3
jhabermas <jhrc2@protonmail.com>
2021-10-14 17:01:34 +0100

    Update test_package.cpp
    Fix stream initialisation

--- recipes/certify/all/test_package/test_package.cpp
+++ recipes/certify/all/test_package/test_package.cpp
@@ -7,9 +7,8 @@
 
 int main() {
       boost::asio::io_context ioc{1};
-      boost::asio::ssl::context context{
-      boost::asio::ssl::context_base::method::tls_client};
-      boost::asio::ssl::stream<boost::asio::ip::tcp::socket> stream{ioc, context};
+      boost::asio::ssl::context context{boost::asio::ssl::context_base::method::tls_client};
+      boost::asio::ssl::stream<boost::asio::ip::tcp::socket> stream(ioc, context);
       constexpr auto hostname = "example.com";
 
       BOOST_TEST(boost::certify::sni_hostname(stream).empty());
