2beb47296ac25a7b42de2f0908d8009155a6e42a
Uilian Ries <uilianries@gmail.com>
2021-10-14 10:42:46 -0300

    Generate and package source files
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/opentelemetry-proto/all/conanfile.py
+++ recipes/opentelemetry-proto/all/conanfile.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from conans import ConanFile, tools
 
 
@@ -16,7 +17,10 @@ class OpenTelemetryProtoConan(ConanFile):
 
     @property
     def _source_subfolder(self):
-        return "source_subfolder"
+        return os.path.join(self.source_folder, "source_subfolder")
+
+    def build_requirements(self):
+        self.build_requires("protobuf/3.17.1")
 
     def package_id(self):
         self.info.header_only()
@@ -28,3 +32,8 @@ class OpenTelemetryProtoConan(ConanFile):
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy("*.proto", dst="res", src=self._source_subfolder)
+        include_dir = os.path.join(self.package_folder, "include")
+        tools.mkdir(include_dir)
+        for proto in Path(self._source_subfolder).rglob('*.proto'):
+            print(f"FILE: {proto}")
+            self.run(f"protoc -I={self._source_subfolder} --cpp_out={include_dir} {proto}", run_environment=True)
--- recipes/opentelemetry-proto/all/test_package/conanfile.py
+++ recipes/opentelemetry-proto/all/test_package/conanfile.py
@@ -6,12 +6,10 @@ from conans import ConanFile, CMake, tools
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    requires = "protobuf/3.17.1"
 
     def test(self):
-        if not tools.cross_building(self):
-            res_folder = self.deps_cpp_info["opentelemetry-proto"].res_paths[0]
-            protofile = os.path.join(res_folder, "opentelemetry", "proto", "common", "v1", "common.proto")
-            self.run(f"protoc -I={res_folder} --cpp_out=. {protofile}", run_environment=True)
-            assert os.path.isfile(os.path.join("opentelemetry", "proto", "common", "v1", "common.pb.cc"))
-            assert os.path.isfile(os.path.join("opentelemetry", "proto", "common", "v1", "common.pb.h"))
+        res_folder = self.deps_cpp_info["opentelemetry-proto"].res_paths[0]
+        include_folder = self.deps_cpp_info["opentelemetry-proto"].include_paths[0]
+        assert os.path.isfile(os.path.join(include_folder, "opentelemetry", "proto", "common", "v1", "common.pb.cc"))
+        assert os.path.isfile(os.path.join(include_folder, "opentelemetry", "proto", "common", "v1", "common.pb.h"))
+        assert os.path.isfile(os.path.join(res_folder, "opentelemetry", "proto", "common", "v1", "common.proto"))
