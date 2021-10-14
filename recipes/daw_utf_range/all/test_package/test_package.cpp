8afbc350ee8f7dbda8049348ebaa6da8fc062dae
toge <toge.mail@gmail.com>
2021-10-14 15:02:53 +0900

    add daw_utf_range recipe

---
+++ recipes/daw_utf_range/all/test_package/test_package.cpp
@@ -0,0 +1,9 @@
+#include "daw/utf_range/daw_utf_range.h"
+
+int main() {
+    auto constexpr const rng = daw::range::create_char_range(u8"あいうえお");
+
+    std::cout << rng.size() << std::endl;
+
+    return 0;
+}
