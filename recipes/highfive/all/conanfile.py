17f5f96f6f24dd10e6856b4ff21de5331937e353
Sebastian Bergt <74961167+sebastian-bergt@users.noreply.github.com>
2021-10-15 13:34:33 +0200

    Update recipes/highfive/all/conanfile.py
    Co-authored-by: Florian Berchtold <florian.berchtold@luminartech.com>

--- recipes/highfive/all/conanfile.py
+++ recipes/highfive/all/conanfile.py
@@ -37,7 +37,7 @@ class HighFiveConan(ConanFile):
     def requirements(self):
         self.requires("hdf5/1.10.6")
         if self.options.with_boost:
-            self.requires("boost/1.75.0")
+            self.requires("boost/1.77.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
