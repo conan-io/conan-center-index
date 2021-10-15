8a9b93586f2dd2c5464ee2814ee12a74b3b0d536
Bighorn <bighorn@pursuitofchallenge.com>
2021-10-14 21:59:46 -0700

    Delint Python files
    Make the files, conanfile.py and test_package/conanfile.py,
    conform to PEP8 guidelines.

--- recipes/cmocka/all/conanfile.py
+++ recipes/cmocka/all/conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, CMake, tools
 
 required_conan_version = ">=1.33.0"
 
+
 class CmockaConan(ConanFile):
     name = "cmocka"
     license = "Apache License 2.0"
@@ -15,13 +16,13 @@ class CmockaConan(ConanFile):
     default_options = {"shared": False, "fPIC": True}
     generators = "cmake"
     exports_sources = ["CMakeLists.txt", "patches/**"]
-    
+
     _cmake = None
-    
+
     @property
     def _source_subfolder(self):
         return "source_subfolder"
-        
+
     @property
     def _build_subfolder(self):
         return "build_subfolder"
@@ -29,7 +30,7 @@ class CmockaConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-            
+
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
@@ -37,32 +38,32 @@ class CmockaConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
-        
+        tools.get(**self.conan_data["sources"][self.version],
+                  strip_root=True, destination=self._source_subfolder)
+
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
-        
+
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
-            
+
         self._cmake = CMake(self)
         self._cmake.definitions["WITH_STATIC_LIB"] = "OFF" if self.options.shared else "ON"
         self._cmake.definitions["WITH_EXAMPLES"] = "OFF"
         self._cmake.configure(build_folder=self._build_subfolder)
-        
+
         return self._cmake
 
     def package(self):
         self.copy("COPYING", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        
+
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.names["cmake_find_package"] = "CMocka"
         self.cpp_info.names["cmake_find_package_multi"] = "CMocka"
-
--- recipes/cmocka/all/test_package/conanfile.py
+++ recipes/cmocka/all/test_package/conanfile.py
@@ -2,6 +2,7 @@ import os
 
 from conans import ConanFile, CMake, tools
 
+
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
