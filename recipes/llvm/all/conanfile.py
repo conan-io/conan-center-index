e823f0da1355eb837ae3fe204548ea9b65edb207
Romain Deterre <romain.deterre@gmail.com>
2021-10-15 08:34:48 -0400

    Limit GCC to 10+

--- recipes/llvm/all/conanfile.py
+++ recipes/llvm/all/conanfile.py
@@ -111,7 +111,7 @@ class Llvm(ConanFile):
                     shutil.rmtree(ignore_path)
 
     def validate(self):
-        if self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "8":
+        if self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "10":
             raise ConanInvalidConfiguration("Compiler version too low for this package.")
 
         if self.settings.compiler == "Visual Studio" and Version(self.settings.compiler.version) < "16.4":
