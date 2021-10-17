2e6644e6449d0c1c4606a448672cdf64eb476e85
toge <toge.mail@gmail.com>
2021-10-17 13:18:04 +0900

    bump aws-c-common to 0.6.14

--- recipes/aws-c-common/all/conanfile.py
+++ recipes/aws-c-common/all/conanfile.py
@@ -85,7 +85,7 @@ class AwsCCommon(ConanFile):
         if self.settings.os == "Linux":
             self.cpp_info.components["aws-c-common-lib"].system_libs = ["dl", "m", "pthread", "rt"]
         elif self.settings.os == "Windows":
-            self.cpp_info.components["aws-c-common-lib"].system_libs = ["bcrypt", "ws2_32"]
+            self.cpp_info.components["aws-c-common-lib"].system_libs = ["bcrypt", "ws2_32", "shlwapi"]
         if not self.options.shared:
             if tools.is_apple_os(self.settings.os):
                 self.cpp_info.components["aws-c-common-lib"].frameworks = ["CoreFoundation"]
