f9d31a5effd15765f8bfee82ad8a8173263e8a84
toge <toge.mail@gmail.com>
2021-10-18 17:37:21 +0900

    (#7730) bump aws_c_common to 0.6.14
    * bump aws-c-common to 0.6.14
    
    * add version check for shlwapi

--- recipes/aws-c-common/all/conanfile.py
+++ recipes/aws-c-common/all/conanfile.py
@@ -86,6 +86,8 @@ class AwsCCommon(ConanFile):
             self.cpp_info.components["aws-c-common-lib"].system_libs = ["dl", "m", "pthread", "rt"]
         elif self.settings.os == "Windows":
             self.cpp_info.components["aws-c-common-lib"].system_libs = ["bcrypt", "ws2_32"]
+            if tools.Version(self.version) >= "0.6.13":
+                self.cpp_info.components["aws-c-common-lib"].system_libs.append("shlwapi")
         if not self.options.shared:
             if tools.is_apple_os(self.settings.os):
                 self.cpp_info.components["aws-c-common-lib"].frameworks = ["CoreFoundation"]
