e77eb3ba8e1c88cfcb784f671e3207d6780b2548
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-10-18 18:40:10 +0200

    Update conanfile.py

--- recipes/darknet/all/test_package/conanfile.py
+++ recipes/darknet/all/test_package/conanfile.py
@@ -5,6 +5,7 @@ from conans import ConanFile, CMake, tools
 class DarknetTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package_multi"
+    
 
     def build(self):
         cmake = CMake(self)
