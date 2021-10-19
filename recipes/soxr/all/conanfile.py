49f716791cb4339c78f07986e3fe11a490f09a80
chausner <15180557+chausner@users.noreply.github.com>
2021-10-19 19:35:38 +0200

    Use cmake_find_package_multi generator
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/soxr/all/conanfile.py
+++ recipes/soxr/all/conanfile.py
@@ -61,7 +61,6 @@ class SoxrConan(ConanFile):
         self._cmake.definitions["BUILD_TESTS"] = False   
         self._cmake.definitions["WITH_OPENMP"] = self.options.with_openmp
         self._cmake.definitions["WITH_LSR_BINDINGS"] = self.options.with_lsr_bindings
-        self._cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.get_safe("fPIC", True)
         self._cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
         return self._cmake
 
