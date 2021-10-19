1c0ae20126eb3f2a83dee88322235a9375822ca7
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-10-19 14:45:02 +0200

    Update conanfile.py

--- recipes/jfalcou-eve/all/conanfile.py
+++ recipes/jfalcou-eve/all/conanfile.py
@@ -38,6 +38,8 @@ class JfalcouEveConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._min_cppstd)
+        if self.settings.compiler == "Visual Studio":
+            raise ConanInvalidConfiguration("EVE does not support MSVC yet (https://github.com/jfalcou/eve/issues/1022).")
 
         def lazy_lt_semver(v1, v2):
             lv1 = [int(v) for v in v1.split(".")]
