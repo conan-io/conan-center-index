3a5392e5843166f36fadf532ff2493051cfba103
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-10-20 19:07:12 +0200

    zint: working with MSVC

--- recipes/zint/all/conanfile.py
+++ recipes/zint/all/conanfile.py
@@ -1,7 +1,10 @@
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import functools
 import os
 
 required_conan_version = ">=1.33.0"
+
 
 class ZintConan(ConanFile):
     name = "zint"
@@ -9,25 +12,23 @@ class ZintConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://sourceforge.net/p/zint/code"
     license = "GPL-3.0"
-    topics = ("conan", "barcode", "qt")
+    topics = ("barcode", "qt")
     exports_sources = ["CMakeLists.txt", "patches/*"]
     generators = "cmake", "cmake_find_package_multi", "cmake_find_package"
     settings = "os", "arch", "compiler", "build_type"
     options = {
-        "shared": [True, False], 
+        "shared": [True, False],
         "fPIC": [True, False],
-        "with_qt" : [True, False],
-        "with_libpng" : [True, False]
+        "with_libpng": [True, False],
+        "with_qt": [True, False],
     }
     default_options = {
-        "shared": False, 
+        "shared": False,
         "fPIC": True,
+        "with_libpng": True,
         "with_qt": False,
-        "with_libpng": True
     }
 
-    _cmake = None
-   
     @property
     def _source_subfolder(self):
         return "source_subfolder"
@@ -37,19 +38,16 @@ class ZintConan(ConanFile):
         return "build_subfolder"
 
     def requirements(self):
-
         self.requires("zlib/1.2.11")
         if self.options.with_libpng:
             self.requires("libpng/1.6.37")
         if self.options.with_qt:
             self.requires("qt/5.15.2")
-    
-    def validate(self):
-    
-        if self.options.with_qt:
-            if not all((self.options["qt"].widgets, self.options["qt"].gui, self.options["qt"].qttools)):
-                raise ConanInvalidConfiguration(f"{self.name} needs qt:{{widgets,gui,qttools}}=True")
 
+    def validate(self):
+        if self.options.with_qt:
+            if not self.options["qt"].gui:
+                raise ConanInvalidConfiguration(f"{self.name} needs qt:gui=True")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -62,23 +60,22 @@ class ZintConan(ConanFile):
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
             destination=self._source_subfolder, strip_root=True)
-        
 
-
+    @functools.lru_cache(1)
     def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake   
-        self._cmake = CMake(self)
-        self._cmake.definitions["ZINT_USE_QT"] = self.options.with_qt
-        self._cmake.definitions["ZINT_USE_PNG"] = self.options.with_libpng
-        self._cmake.configure()
-        return self._cmake
+        cmake = CMake(self)
+        cmake.definitions["DATA_INSTALL_DIR"] = os.path.join(self.package_folder, "lib")
+        cmake.definitions["ZINT_USE_QT"] = self.options.with_qt
+        if self.options.with_qt:
+            cmake.definitions["QT_VERSION_MAJOR"] = tools.Version(self.deps_cpp_info["qt"].version).major
+        cmake.definitions["ZINT_USE_PNG"] = self.options.with_libpng
+        cmake.configure()
+        return cmake
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         cmake = self._configure_cmake()
- 
         cmake.build()
 
     def package(self):
@@ -86,30 +83,28 @@ class ZintConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
 
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "Zint"
         self.cpp_info.names["cmake_find_package_multi"] = "Zint"
-        self.cpp_info.components["libzint"].libs = ["zint"]
-        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
-                self.cpp_info.components["libzint"].libs.append("zint_bundled_getopt")
+        self.cpp_info.components["libzint"].libs = ["zint" if self.options.shared else "zint-static"]
         self.cpp_info.components["libzint"].names["cmake_find_package"] = "Zint"
         self.cpp_info.components["libzint"].names["cmake_find_package_multi"] = "Zint"
         self.cpp_info.components["libzint"].requires = ["zlib::zlib"]
-        
+        if self.settings.os == "Windows" and self.options.shared:
+            self.cpp_info.components["libzint"].defines = ["ZINT_DLL"]
+
         if self.options.with_libpng:
             self.cpp_info.components["libzint"].requires.append("libpng::libpng")
-            
+
         if self.options.with_qt:
             self.cpp_info.components["libqzint"].libs = ["QZint"]
             self.cpp_info.components["libqzint"].names["cmake_find_package"] = "QZint"
             self.cpp_info.components["libqzint"].names["cmake_find_package_multi"] = "QZint"
             self.cpp_info.components["libqzint"].requires.extend([
                 "libzint",
-                "qt::gui", 
-                "qt::widgets",
-                "qt::qttools"
+                "qt::qtGui",
             ])
-        if self.options.with_qt:
-            if self.settings.os == "Windows":
-                self.cpp_info.system_libs = ["dwmapi", "uxtheme"]
-
+            if self.settings.os == "Windows" and self.options.shared:
+                self.cpp_info.components["libqzint"].defines = ["QZINT_DLL"]
