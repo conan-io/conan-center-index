54dc28ec4d59131eec1542c1385f3c0b0ffacb08
toge <toge.mail@gmail.com>
2021-10-21 22:12:00 +0900

    fix KeyError in test_package

--- recipes/xsimd/all/test_package/conanfile.py
+++ recipes/xsimd/all/test_package/conanfile.py
@@ -9,13 +9,18 @@ class XsimdTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+
+        cflags = cmake.definitions.get("CONAN_C_FLAGS", "")
+
         arch = str(self.settings.arch)
         if arch.startswith("x86"):
-            cmake.definitions["CONAN_C_FLAGS"] += " -DXSIMD_FORCE_X86_INSTR_SET"
+            cflags += " -DXSIMD_FORCE_X86_INSTR_SET"
         elif arch.startswith("ppc"):
-            cmake.definitions["CONAN_C_FLAGS"] += " -DXSIMD_FORCE_PPC_INSTR_SET"
+            cflags += " -DXSIMD_FORCE_PPC_INSTR_SET"
         elif arch.startswith("arm"):
-            cmake.definitions["CONAN_C_FLAGS"] += " -DXSIMD_FORCE_ARM_INSTR_SET"
+            cflags += " -DXSIMD_FORCE_ARM_INSTR_SET"
+
+        cmake.definitions["CONAN_C_FLAGS"] = cflags
 
         cmake.configure()
         cmake.build()
