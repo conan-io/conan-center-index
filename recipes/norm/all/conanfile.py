916e040f79831889ebc122e28453e8b523364c41
Mark Jan van Kampen <mjvk@allseas.com>
2021-10-21 13:50:21 +0200

    Fixes norm linking pthread on all unix platforms
    Only required on some platforms, find_package(Threads) used instead

--- recipes/norm/all/conanfile.py
+++ recipes/norm/all/conanfile.py
@@ -8,7 +8,7 @@ class NormConan(ConanFile):
     topics = ("conan", "norm", "multicast", "transport protocol")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.nrl.navy.mil/itd/ncs/products/norm"
-    exports_sources = ["CMakeLists.txt"]
+    exports_sources = "CMakeLists.txt", "patches/**"
     generators = "cmake"
     license = "NRL"
     settings = "os", "compiler", "build_type", "arch"
@@ -43,6 +43,8 @@ class NormConan(ConanFile):
         return self._cmake
 
     def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
