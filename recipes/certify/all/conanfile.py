db4476223b6419601ca8eb0806b976f8266dd01c
jhabermas <86484040+jhabermas@users.noreply.github.com>
2021-10-22 01:41:09 +0100

    Update recipes/certify/all/conanfile.py
    Validate minimum compiler version
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/certify/all/conanfile.py
+++ recipes/certify/all/conanfile.py
@@ -30,6 +30,12 @@ class CertifyConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version:
+            if tools.Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not support.".format(self.name))
+        else:
+            self.output.warn("{} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name))
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
