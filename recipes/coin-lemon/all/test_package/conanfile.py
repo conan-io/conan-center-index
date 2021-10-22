3dde276646ad44402de1a3292524335566a232d9
toge <toge.mail@gmail.com>
2021-10-22 18:44:06 +0900

    fix bin_path

--- recipes/coin-lemon/all/test_package/conanfile.py
+++ recipes/coin-lemon/all/test_package/conanfile.py
@@ -1,5 +1,4 @@
 import os
-
 from conans import ConanFile, CMake, tools
 
 
@@ -9,17 +8,10 @@ class CoinLemonTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-
     def test(self):
         if not tools.cross_building(self):
-            os.chdir("bin")
-            self.run(".%stest_package" % os.sep)
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
