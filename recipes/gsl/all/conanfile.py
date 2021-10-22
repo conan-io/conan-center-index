c9cbccf48e122436da064c34fa7a8adeca67e8e7
Nolan Crane <11447537+NolanC33@users.noreply.github.com>
2021-10-22 14:04:43 -0400

    (#7770) gsl bug fix: removing definition of HAVE_GNUX86_IEEE_INTERFACE when os is linux be���
    * removing definition of HAVE_GNUX86_IEEE_INTERFACE when os is linux because you wont always be on x86 when linux...you could be on arm for example
    
    * gsl defining HAVE_GNUX86_IEEE_INTERFACE when linxu and x86

--- recipes/gsl/all/conanfile.py
+++ recipes/gsl/all/conanfile.py
@@ -94,7 +94,8 @@ class GslConan(ConanFile):
             self._autotools.defines.extend(["HAVE_WIN_IEEE_INTERFACE", "WIN32"])
             if self.options.shared:
                 self._autotools.defines.append("GSL_DLL")
-        elif self.settings.os == "Linux":
+
+        if self.settings.os == "Linux" and "x86" in self.settings.arch:
             self._autotools.defines.append("HAVE_GNUX86_IEEE_INTERFACE")
 
         if self.settings.compiler == "Visual Studio":
