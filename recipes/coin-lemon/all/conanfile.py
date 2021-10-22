a76b46a6601e35f74a01174d7c79c8114220ebbe
toge <toge.mail@gmail.com>
2021-10-22 19:26:13 +0900

    FIX "FPIC MANAGEMENT"

--- recipes/coin-lemon/all/conanfile.py
+++ recipes/coin-lemon/all/conanfile.py
@@ -27,6 +27,10 @@ class CoinLemonConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
