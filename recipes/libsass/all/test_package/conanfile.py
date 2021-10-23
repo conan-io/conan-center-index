952a15e1ec00df2bbd7e4abf6133ab6cf04c5c22
Siim Meerits <sh0@yutani.ee>
2021-10-23 19:21:34 +0300

    libsass: Add MSVC support, modernize.

--- recipes/libsass/all/test_package/conanfile.py
+++ recipes/libsass/all/test_package/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class LibsassTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
