d29016310720828a0b6b8a131c9ee928dea28545
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2021-10-23 07:42:40 +0200

    add patch for volk

--- recipes/diligent-core/all/conanfile.py
+++ recipes/diligent-core/all/conanfile.py
@@ -66,6 +66,7 @@ class DiligentCoreConan(ConanFile):
         self.requires("spirv-tools/diligent-2.5.1@andrei/test")
         self.requires("glslang/diligent-2.5.1@andrei/testing")
         self.requires("vulkan-headers/diligent-2.5.1@andrei/testing")
+        self.requires("volk/1.2.190")
 
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.requires("xorg/system")
