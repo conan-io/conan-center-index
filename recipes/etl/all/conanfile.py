a09ef69d1fda0ebbafe412127c1d6af330f940ef
Joris Putcuyps <joris.putcuyps@gmail.com>
2021-10-25 20:06:08 +0200

    Due to renaming to etl, probably no longer needed

--- recipes/etl/all/conanfile.py
+++ recipes/etl/all/conanfile.py
@@ -21,7 +21,3 @@ class EmbeddedTemplateLibraryConan(ConanFile):
 
     def package_id(self):
         self.info.header_only()
-
-    def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "etl"
-        self.cpp_info.names["cmake_find_package_multi"] = "etl"
