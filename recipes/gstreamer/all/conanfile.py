238930ba321b43d70b514f325c00ea14e339daf9
SSE4 <tomskside@gmail.com>
2021-10-26 08:09:46 -0700

    Update recipes/gstreamer/all/conanfile.py

--- recipes/gstreamer/all/conanfile.py
+++ recipes/gstreamer/all/conanfile.py
@@ -53,7 +53,7 @@ class GStreamerConan(ConanFile):
             del self.options.fPIC
 
     def validate(self):
-        if self.options.shared != self.options["glib"].shared:
+        if self.options["glib"].static and self.options.shared:
             # https://gitlab.freedesktop.org/gstreamer/gst-build/-/issues/133
             raise ConanInvalidConfiguration("GLib and GStreamer must be either all shared, or all static")
 
