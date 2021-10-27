cbabe3c8b7be3e9399999658c42f62920bcea148
Uilian Ries <uilianries@gmail.com>
2021-10-27 16:02:05 -0300

    make getrandom optional for libevent
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libevent/all/conanfile.py
+++ recipes/libevent/all/conanfile.py
@@ -84,7 +84,6 @@ class LibeventConan(ConanFile):
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
-        # tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "getrandom", "")
         cmake = self._configure_cmake()
         cmake.build()
 
