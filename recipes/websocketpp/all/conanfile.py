aeb0f8e6f89f79de504c7f470905e2eb62c28a04
Lars Hagström <lars@foldspace.nu>
2021-11-02 23:04:56 +0100

    Update recipes/websocketpp/all/conanfile.py
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/websocketpp/all/conanfile.py
+++ recipes/websocketpp/all/conanfile.py
@@ -29,10 +29,10 @@ class WebsocketPPConan(ConanFile):
         return "source_subfolder"
 
     def requirements(self):
-        if self.options.openssl:
+        if self.options.with_openssl:
             self.requires("openssl/1.1.1l")
 
-        if self.options.zlib:
+        if self.options.with_zlib:
             self.requires("zlib/1.2.11")
 
         if self.options.asio == "standalone":
