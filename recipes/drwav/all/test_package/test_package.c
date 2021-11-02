dfae35a3594098e79e849f8da221d566c5ebe800
chausner <chausner@users.noreply.github.com>
2021-11-02 23:19:18 +0100

    Convert tabs to spaces

--- recipes/drwav/all/test_package/test_package.c
+++ recipes/drwav/all/test_package/test_package.c
@@ -2,8 +2,8 @@
 #include "dr_wav.h"
 
 int main(void) {
-	const char *version = drwav_version_string();
-	printf("dr_wav version: %s\n", version);
+    const char *version = drwav_version_string();
+    printf("dr_wav version: %s\n", version);
 
-	return 0;
+    return 0;
 }
