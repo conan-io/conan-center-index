aa7c0a031f58ac8ef75cac696c062714cd2c9466
Sebastian Bergt <sebastian.bergt@luminartech.com>
2021-11-03 08:58:49 +0100

    created recipe for maddy

---
+++ recipes/maddy/all/conanfile.py
@@ -0,0 +1,30 @@
+import os
+from conans import ConanFile, tools
+
+
+class maddyConan(ConanFile):
+    name = "maddy"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/progsource/maddy"
+    description = "open-source, maddy is a C++ Markdown to HTML header-only parser library."
+    topics = ("maddy", "markdown", "header-only")
+    license = "MIT"
+
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("maddy-{}".format(self.version), self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
+
+    def package(self):
+        self.copy("LICENSE", src=os.path.join(self.source_folder, self._source_subfolder), dst="licenses")
+        self.copy(
+            pattern="maddy/*.h", src=os.path.join(self.source_folder, self._source_subfolder, "include"), dst="include"
+        )
---
+++ recipes/maddy/all/test_package/example.cpp
@@ -0,0 +1,24 @@
+/*
+ *  This example was copied from https://github.com/progsource/maddy/blob/master/README.md
+ */
+
+#include <memory>
+#include <string>
+#include <cstdlib>
+
+#include <maddy/parser.h>
+
+int main()
+{
+   std::stringstream markdownInput("");
+
+   // config is optional
+   std::shared_ptr<maddy::ParserConfig> config = std::make_shared<maddy::ParserConfig>();
+   config->isEmphasizedParserEnabled = true; // default
+   config->isHTMLWrappedInParagraph = true;  // default
+
+   std::shared_ptr<maddy::Parser> parser = std::make_shared<maddy::Parser>(config);
+   std::string htmlOutput = parser->Parse(markdownInput);
+
+   return EXIT_SUCCESS;
+}
