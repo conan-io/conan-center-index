2e3a4bd99ee2c666e00dd6b3655fd218d71d7650
Uilian Ries <uilianries@gmail.com>
2021-11-03 17:49:33 -0300

    Drop clang 12
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/folly/all/conanfile.py
+++ recipes/folly/all/conanfile.py
@@ -112,6 +112,10 @@ class FollyConan(ConanFile):
         if miss_boost_required_comp:
             raise ConanInvalidConfiguration("Folly requires these boost components: {}".format(", ".join(self._required_boost_components)))
 
+        # FIXME: https://github.com/facebook/folly/issues/1655
+        if tools.Version(self.version) >= "2021.05.31.00" and self.settings.compiler == "apple-clang" and tools.Version(self.settings.compiler) >= "12.0":
+            raise ConanInvalidConfiguration("Apple Clang 12 can not deal with ___cxa_increment_exception_refcount")
+
     def _configure_cmake(self):
         if not self._cmake:
             self._cmake = CMake(self)
