bab2bbb5d17d597f0bcdcd0d2f169bde1a6e6f04
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-11-06 00:39:29 +0100

    modernize

--- recipes/asyncplusplus/all/conanfile.py
+++ recipes/asyncplusplus/all/conanfile.py
@@ -1,20 +1,29 @@
+from conans import ConanFile, CMake, tools
 import os
 
-from conans import ConanFile, CMake, tools
+required_conan_version = ">=1.36.0"
+
 
 class AsyncplusplusConan(ConanFile):
     name = "asyncplusplus"
     description = "Async++ concurrency framework for C++11"
     license = "MIT"
-    topics = ("conan", "asyncplusplus", "async", "parallel", "task", "scheduler")
+    topics = ("async", "parallel", "task", "scheduler")
     homepage = "https://github.com/Amanieu/asyncplusplus"
     url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
     exports_sources = "CMakeLists.txt"
     generators = "cmake"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-
     _cmake = None
 
     @property
@@ -28,12 +37,14 @@ class AsyncplusplusConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+
+    def validate(self):
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, 11)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename(self.name + "-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -54,10 +65,10 @@ class AsyncplusplusConan(ConanFile):
 
     def package_info(self):
         # FIXME: official CMake target is exported without namespace
-        self.cpp_info.names["cmake_find_package"] = "Async++"
-        self.cpp_info.names["cmake_find_package_multi"] = "Async++"
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.set_property("cmake_file_name", "Async++")
+        self.cpp_info.set_property("cmake_target_name", "Async++")
+        self.cpp_info.libs = ["async++"]
         if not self.options.shared:
             self.cpp_info.defines = ["LIBASYNC_STATIC"]
-        if self.settings.os == "Linux":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs = ["pthread"]
