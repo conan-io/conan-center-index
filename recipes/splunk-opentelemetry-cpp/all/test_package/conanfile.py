8055b3efc0af867446af346cbc1b419b7e28ad5d
Siim Kallas <siimkallas@gmail.com>
2021-11-05 09:56:11 +0200

    formatting

--- recipes/splunk-opentelemetry-cpp/all/test_package/conanfile.py
+++ recipes/splunk-opentelemetry-cpp/all/test_package/conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, CMake, tools
 import os
 
+
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package_multi"
