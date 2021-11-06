88b51a96fa698f0e8535cb57f43320f1fc11f41b
Mitchell Mosure <hctimit@gmail.com>
2021-11-05 22:09:51 -0500

    feedback: update topics, compiler version check, remove unneeded package info, CMakeLists.txt consistency for test_package

--- recipes/inversify-cpp/all/conanfile.py
+++ recipes/inversify-cpp/all/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.33.0"
@@ -7,15 +8,30 @@ required_conan_version = ">=1.33.0"
 class InversifyCppConan(ConanFile):
     name = "inversify-cpp"
     description = "C++17 inversion of control and dependency injection container library"
-    topics = ("conan", "inversify-cpp", "ioc", "container", "dependency", "injection")
+    topics = ("ioc", "container", "dependency", "injection")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/mosure/inversify-cpp"
     license = "MIT"
     settings = "compiler"
     no_copy_source = True
-    
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "9",
+            "Visual Studio": "15.7",
+            "clang": "7",
+            "apple-clang": "11",
+        }
     def validate(self):
-        tools.check_min_cppstd(self, "17")
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, 17)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version:
+            if tools.Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not support.".format(self.name))
+        else:
+            self.output.warn("{} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name))
 
     @property
     def _source_subfolder(self):
@@ -30,6 +46,3 @@ class InversifyCppConan(ConanFile):
 
     def package_id(self):
         self.info.header_only()
-
-    def package_info(self):
-        self.cpp_info.includedirs = ['include']
--- recipes/inversify-cpp/all/test_package/conanfile.py
+++ recipes/inversify-cpp/all/test_package/conanfile.py
@@ -4,10 +4,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    
-    def validate(self):
-        tools.check_min_cppstd(self, "17")
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -15,6 +12,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
