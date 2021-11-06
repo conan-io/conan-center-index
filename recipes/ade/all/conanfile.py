2ff29d7af9312590bfabd08ca20b9cae9c8a8c05
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-11-06 01:34:15 +0100

    fix CMake imported target

--- recipes/ade/all/conanfile.py
+++ recipes/ade/all/conanfile.py
@@ -1,7 +1,8 @@
 from conans import ConanFile, CMake, tools
 import os
+import textwrap
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.36.0"
 
 
 class AdeConan(ConanFile):
@@ -60,9 +61,35 @@ class AdeConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "share"))
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"ade": "ade::ade"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
-        # FIXME opencv expects this target to be "ade" but conan will bind it as ade::ade
-        self.cpp_info.names["cmake_find_package"] = "ade"
-        self.cpp_info.names["cmake_find_package_multi"] = "ade"
+        self.cpp_info.set_property("cmake_file_name", "ade")
+        self.cpp_info.set_property("cmake_target_name", "ade")
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.set_property("cmake_build_modules", [self._module_file_rel_path])
         self.cpp_info.libs = ["ade"]
