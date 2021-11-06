74bb8deffb1001ee978a3184cd22ec831a09a8fe
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-11-06 01:31:21 +0100

    convention

--- recipes/ade/all/conanfile.py
+++ recipes/ade/all/conanfile.py
@@ -1,5 +1,5 @@
-import os
 from conans import ConanFile, CMake, tools
+import os
 
 required_conan_version = ">=1.33.0"
 
@@ -11,21 +11,18 @@ class AdeConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     description = "Graph construction, manipulation, and processing framework"
     topics = ("graphs", "opencv")
-    exports_sources = "CMakeLists.txt",
-    settings = "os", "compiler", "build_type", "arch"
+
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "fPIC": [True, False],
     }
     default_options = {
         "fPIC": True,
     }
+
+    exports_sources = "CMakeLists.txt"
     generators = "cmake"
-
     _cmake = None
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
 
     @property
     def _source_subfolder(self):
@@ -35,32 +32,34 @@ class AdeConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, 11)
 
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
     def _configure_cmake(self):
-        if self._cmake is not None:
+        if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version][0],
-                  destination=self._source_subfolder, strip_root=True)
 
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "share"))
-        self.copy(pattern="LICENSE",
-                  dst="licenses",
-                  src=self._source_subfolder)
 
     def package_info(self):
         # FIXME opencv expects this target to be "ade" but conan will bind it as ade::ade
