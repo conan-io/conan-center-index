ebbd4ea0862e67a063cdc0592d7c71c0b3c9bbce
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-11-08 20:05:01 +0100

    (#7973) asyncplusplus: fix CMake imported target + modernize
    * modernize
    
    * fix CMake imported target

--- recipes/asyncplusplus/all/conanfile.py
+++ recipes/asyncplusplus/all/conanfile.py
@@ -1,20 +1,30 @@
-import os
-
 from conans import ConanFile, CMake, tools
+import os
+import textwrap
+
+required_conan_version = ">=1.36.0"
+
 
 class AsyncplusplusConan(ConanFile):
     name = "asyncplusplus"
     description = "Async++ concurrency framework for C++11"
     license = "MIT"
-    topics = ("conan", "asyncplusplus", "async", "parallel", "task", "scheduler")
+    topics = ("async", "parallel", "task", "scheduler")
     homepage = "https://github.com/Amanieu/asyncplusplus"
     url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
     exports_sources = "CMakeLists.txt"
     generators = "cmake"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-
     _cmake = None
 
     @property
@@ -28,12 +38,14 @@ class AsyncplusplusConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+
+    def validate(self):
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, 11)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename(self.name + "-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -51,13 +63,39 @@ class AsyncplusplusConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "cmake"))
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"Async++": "Async++::Async++"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
-        # FIXME: official CMake target is exported without namespace
-        self.cpp_info.names["cmake_find_package"] = "Async++"
-        self.cpp_info.names["cmake_find_package_multi"] = "Async++"
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.set_property("cmake_file_name", "Async++")
+        self.cpp_info.set_property("cmake_target_name", "Async++")
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.set_property("cmake_build_modules", [self._module_file_rel_path])
+        self.cpp_info.libs = ["async++"]
         if not self.options.shared:
             self.cpp_info.defines = ["LIBASYNC_STATIC"]
-        if self.settings.os == "Linux":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs = ["pthread"]
