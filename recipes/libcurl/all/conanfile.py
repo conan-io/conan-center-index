ec7582e0fc27f730adf24b928547ff99f19074b4
Uilian Ries <uilianries@gmail.com>
2021-11-10 14:54:31 -0300

    Use set_property for libcurl
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libcurl/all/conanfile.py
+++ recipes/libcurl/all/conanfile.py
@@ -535,12 +535,10 @@ class LibcurlConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "CURL"
-        self.cpp_info.names["cmake_find_package_multi"] = "CURL"
-        self.cpp_info.names["pkg_config"] = "libcurl"
-        self.cpp_info.components["curl"].names["cmake_find_package"] = "libcurl"
-        self.cpp_info.components["curl"].names["cmake_find_package_multi"] = "libcurl"
-        self.cpp_info.components["curl"].names["pkg_config"] = "libcurl"
+        self.cpp_info.set_property("cmake_file_name", "CURL")
+        self.cpp_info.set_property("cmake_target_name", "CURL")
+        self.cpp_info.set_property("pkg_config_name", "libcurl")
+        self.cpp_info.components["curl"].set_property("cmake_target_name", "libcurl")
 
         if self.settings.compiler == "Visual Studio":
             self.cpp_info.components["curl"].libs = ["libcurl_imp"] if self.options.shared else ["libcurl"]
