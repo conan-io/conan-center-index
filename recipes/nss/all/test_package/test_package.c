9b6b48cc4b591128c93d96904cd475bb5a890ad7
Eric Lemanissier <eric.lemanissier@gmail.com>
2021-11-12 19:31:34 +0100

    test other libs

--- recipes/nss/all/test_package/test_package.c
+++ recipes/nss/all/test_package/test_package.c
@@ -1,9 +1,15 @@
 #include "nss.h"
+#include "nssutil.h"
+#include "smime.h"
+#include "ssl.h"
 #include <stdio.h>
 
 int main() {
     SECStatus rv;
     printf("NSS version: %s\n", NSS_GetVersion());
+    printf("NSSSMIME version: %s\n", NSSSMIME_GetVersion());
+    printf("NSSSSL version: %s\n", NSSSSL_GetVersion());
+    printf("NSSUTIL version: %s\n", NSSUTIL_GetVersion());
 
     rv = NSS_NoDB_Init("./tmp");
     if (rv != SECSuccess){
