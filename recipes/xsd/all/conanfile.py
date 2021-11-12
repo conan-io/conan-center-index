ca469efb67f5aef4f0c9ea2f0827678e81ab79da
bowb <lacktrum@gmail.com>
2021-11-11 20:03:57 -0700

    Remove configure. not needed.

--- recipes/xsd/all/conanfile.py
+++ recipes/xsd/all/conanfile.py
@@ -57,10 +57,6 @@ class ConanXqilla(ConanFile):
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, 11)
 
-    def configure(self):
-        if self.options.shared:
-            del self.options.fPIC
-
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True,
                   destination=self._source_subfolder)
