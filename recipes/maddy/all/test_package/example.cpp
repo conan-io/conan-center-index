bd55a30f01aad8495f0d64339df5e3072f33b747
Sebastian Bergt <Sebastian@Bergt.org>
2021-11-14 19:10:12 +0100

    Update recipes/maddy/all/test_package/example.cpp
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/maddy/all/test_package/example.cpp
+++ recipes/maddy/all/test_package/example.cpp
@@ -11,7 +11,7 @@
 
 int main()
 {
-   std::stringstream markdownInput("");
+   std::stringstream markdownInput("#Hello world from maddy!\nVisit [conan-center-index](https://github.com/conan-io/conan-center-index)\n");
 
    // config is optional
    std::shared_ptr<maddy::ParserConfig> config = std::make_shared<maddy::ParserConfig>();
