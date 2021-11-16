abf1a79c664341042c18c6d81ca5048cb642558c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-11-16 22:43:16 +0100

    don't raise for Visual Studio 2017

--- recipes/certify/all/conanfile.py
+++ recipes/certify/all/conanfile.py
@@ -39,12 +39,18 @@ class CertifyConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._min_cppstd)
+
+        def lazy_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
+
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version:
-            if tools.Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not support.".format(self.name))
-        else:
+        if not minimum_version:
             self.output.warn("{} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name))
+        elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not support.".format(self.name))
 
     def package_id(self):
         self.info.header_only()
