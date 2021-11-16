cd5fa032997dc51b6176f6dd6ca58bbd77358dd1
toge <toge.mail@gmail.com>
2021-11-16 13:34:28 +0900

    (#7933) aws-c-io: add version 0.10.13

--- recipes/aws-c-io/all/conanfile.py
+++ recipes/aws-c-io/all/conanfile.py
@@ -6,7 +6,7 @@ required_conan_version = ">=1.33.0"
 class AwsCIO(ConanFile):
     name = "aws-c-io"
     description = "IO and TLS for application protocols"
-    topics = ("conan", "aws", "amazon", "cloud", )
+    topics = ("aws", "amazon", "cloud", "io", "tls",)
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/awslabs/aws-c-io"
     license = "Apache-2.0",
@@ -39,7 +39,15 @@ class AwsCIO(ConanFile):
         del self.settings.compiler.libcxx
 
     def requirements(self):
-        self.requires("aws-c-cal/0.5.12")
+        # the versions of aws-c-common and aws-c-io are tied since aws-c-common/0.6.12 and aws-c-io/0.10.10
+        # Please refer https://github.com/conan-io/conan-center-index/issues/7763
+        if tools.Version(self.version) <= "0.10.9":
+            self.requires("aws-c-common/0.6.11")
+            self.requires("aws-c-cal/0.5.11")
+        else:
+            self.requires("aws-c-common/0.6.15")
+            self.requires("aws-c-cal/0.5.12")
+
         if self.settings.os in ["Linux", "FreeBSD", "Android"]:
             self.requires("s2n/1.1.0")
 
@@ -73,7 +81,7 @@ class AwsCIO(ConanFile):
         self.cpp_info.components["aws-c-io-lib"].names["cmake_find_package"] = "aws-c-io"
         self.cpp_info.components["aws-c-io-lib"].names["cmake_find_package_multi"] = "aws-c-io"
         self.cpp_info.components["aws-c-io-lib"].libs = ["aws-c-io"]
-        self.cpp_info.components["aws-c-io-lib"].requires = ["aws-c-cal::aws-c-cal-lib"]
+        self.cpp_info.components["aws-c-io-lib"].requires = ["aws-c-cal::aws-c-cal-lib", "aws-c-common::aws-c-common-lib"]
 
         if self.settings.os == "Macos":
             self.cpp_info.components["aws-c-io-lib"].frameworks.append("Security")
