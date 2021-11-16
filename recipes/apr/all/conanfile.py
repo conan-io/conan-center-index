314ccd96f851c8e25c5c782c07ef83c17978db50
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-11-16 14:20:44 +0100

    fix apple-clang >= 13

--- recipes/apr/all/conanfile.py
+++ recipes/apr/all/conanfile.py
@@ -38,6 +38,12 @@ class AprConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    @property
+    def _should_call_autoreconf(self):
+        return self.settings.compiler == "apple-clang" and \
+               tools.Version(self.settings.compiler.version) >= "12" and \
+               self.version == "1.7.0"
+
     def export_sources(self):
         self.copy("CMakeLists.txt")
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
@@ -58,7 +64,7 @@ class AprConan(ConanFile):
             raise ConanInvalidConfiguration("apr cannot be cross compiled due to runtime checks")
 
     def build_requirements(self):
-        if self.settings.os == "Macos":
+        if self._should_call_autoreconf:
             self.build_requires("libtool/2.4.6")
 
     def source(self):
@@ -77,14 +83,6 @@ class AprConan(ConanFile):
     def _configure_autotools(self):
         if self._autotools:
             return self._autotools
-        if (self.settings.compiler == "apple-clang" and
-            tools.Version(self.settings.compiler.version) == "12" and
-            self.version == "1.7.0"):
-
-            with tools.chdir( self._source_subfolder ):
-                os.remove( "configure" )
-                self.run( "./buildconf" )
-
         self._autotools = AutoToolsBuildEnvironment(self)
         self._autotools.libs = []
         yes_no = lambda v: "yes" if v else "no"
@@ -112,6 +110,9 @@ class AprConan(ConanFile):
             cmake = self._configure_cmake()
             cmake.build(target="libapr-1" if self.options.shared else "apr-1")
         else:
+            if self._should_call_autoreconf:
+                with tools.chdir(self._source_subfolder):
+                    self.run("{} -fiv".format(tools.get_env("AUTORECONF")), win_bash=tools.os_info.is_windows)
             autotools = self._configure_autotools()
             autotools.make()
 
