e7361576e662bf29aee764ef8ba1326fb355c8dc
toge <toge.mail@gmail.com>
2021-11-19 01:09:08 +0900

    add dl since 2.0.0

--- recipes/tinyalsa/all/conanfile.py
+++ recipes/tinyalsa/all/conanfile.py
@@ -61,6 +61,8 @@ class TinyAlsaConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["tinyalsa"]
+        if tools.Version(self.version) >= "2.0.0":
+            self.cpp_info.system_libs.append("dl")
         if self.options.with_utils:
             bin_path = os.path.join(self.package_folder, "bin")
             self.output.info('Appending PATH environment variable: %s' % bin_path)
