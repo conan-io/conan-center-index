fadd378626b878053b45572e1bd5ac382b8ac307
Nikita Besperstov <n.bes@pm.me>
2021-11-18 21:47:34 +0300

    init WilzegersAutotestConan

---
+++ recipes/wilzegers_autotest/all/conanfile.py
@@ -0,0 +1,31 @@
+from conans import ConanFile, tools
+import os
+
+
+class WilzegersAutotestConan(ConanFile):
+    name = "wilzegers_autotest"
+    version = "0.1"
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    description = "Autotest facilitates the testing of class interfaces"
+    topics = ("autotest", "testing")
+    settings = "compiler"
+
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    def package_id(self):
+        self.info.header_only()
+
+    def package(self):
+        self.copy("*.hpp", src=os.path.join(self._source_subfolder, "autotest/include"), dst="include")
+
+    def package_info(self):
+        self.cpp_info.libs = ["autotest"]
