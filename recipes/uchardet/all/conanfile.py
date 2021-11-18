4e6ea20de24d4300e3f316a2ccf360932a561e21
andiwand <stefl.andreas@gmail.com>
2021-11-18 13:01:57 +0100

    fix build

--- recipes/uchardet/all/conanfile.py
+++ recipes/uchardet/all/conanfile.py
@@ -72,8 +72,8 @@ class UchardetConan(ConanFile):
         self.copy("COPYING", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "uchardet"
