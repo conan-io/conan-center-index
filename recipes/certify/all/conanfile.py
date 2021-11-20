782d97e1dc7d0c7594ddb22e7bb7b5e26fcabecf
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-11-20 08:35:59 +0100

    (#8075) certify: fix CMake imported target
    * cleanup
    
    * fix CMake imported target
    
    * don't raise for Visual Studio 2017

--- recipes/certify/all/conanfile.py
+++ recipes/certify/all/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.36.0"
 
 
 class CertifyConan(ConanFile):
@@ -12,8 +12,7 @@ class CertifyConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/djarek/certify"
     license = "BSL-1.0"
-    settings = "compiler"
-    generators = "cmake", "cmake_find_package"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
@@ -40,12 +39,21 @@ class CertifyConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._min_cppstd)
+
+        def lazy_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
+
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version:
-            if tools.Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not support.".format(self.name))
-        else:
+        if not minimum_version:
             self.output.warn("{} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name))
+        elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not support.".format(self.name))
+
+    def package_id(self):
+        self.info.header_only()
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
@@ -57,10 +65,8 @@ class CertifyConan(ConanFile):
         self.copy(pattern="*", dst="include",
                   src=os.path.join(self._source_subfolder, "include"))
 
-    def package_id(self):
-        self.info.header_only()
-
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "certify"
-        self.cpp_info.names["cmake_find_package_multi"] = "certify"
-        self.cpp_info.names["pkg_config"] = "certify"
+        self.cpp_info.set_property("cmake_file_name", "certify")
+        self.cpp_info.set_property("cmake_target_name", "certify")
+        self.cpp_info.components["_certify"].set_property("cmake_target_name", "core")
+        self.cpp_info.components["_certify"].requires = ["boost::boost", "openssl::openssl"]
