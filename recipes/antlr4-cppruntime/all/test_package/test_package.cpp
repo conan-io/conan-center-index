378218f90d1fa5444d7589f600133d119b3c94e3
Michal Widera <michal@widera.com.pl>
2021-11-20 02:38:24 +0100

    Issue #8117 patch 3 - test code functional.

--- recipes/antlr4-cppruntime/all/test_package/test_package.cpp
+++ recipes/antlr4-cppruntime/all/test_package/test_package.cpp
@@ -2,15 +2,14 @@
 
 #include <iostream>
 
-//using namespace antlrcpp;
-//using namespace antlr4;
+using namespace antlrcpp;
+using namespace antlr4;
 using namespace std;
 
 int main(int argc, const char *args[])
 {
-    //ifstream ins;
-    // Create the input stream.
-    //ins.open(args[1]);
-    //ANTLRInputStream input(ins);
-    //return 0;
+    ifstream ins;
+    ins.open(args[1]);
+    ANTLRInputStream input(ins);
+    return 0;
 }
