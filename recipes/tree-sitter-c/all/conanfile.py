59da51d9e634728bff65c1b4ae7ec03948f1ad8a
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-11-20 02:05:34 +0100

    tree-sitter-c: add tree-sitter-c/0.20.1 recipe

---
+++ recipes/tree-sitter-c/all/conanfile.py
@@ -0,0 +1,62 @@
+from conans import CMake, ConanFile, tools
+import functools
+
+required_conan_version = ">=1.33.0"
+
+
+class TreeSitterCConan(ConanFile):
+    name = "tree-sitter-c"
+    description = "C grammar for tree-sitter."
+    topics = ("parser", "grammar", "tree", "c", "ide")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/tree-sitter/tree-sitter-c"
+    license = "MIT"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "fPIC": [True, False],
+        "shared": [True, False],
+    }
+    default_options = {
+        "fPIC": True,
+        "shared": False,
+    }
+
+    generators = "cmake", "cmake_find_package_multi"
+    exports_sources = "CMakeLists.txt"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def requirements(self):
+        self.requires("tree-sitter/0.20.0")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    @functools.lru_cache(1)
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["tree-sitter-c"]
