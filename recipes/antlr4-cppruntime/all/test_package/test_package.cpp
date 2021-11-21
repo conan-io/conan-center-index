15dccb409c8456f23801ef8dc1d4a3c423c0c2f2
Michal Widera <michal@widera.com.pl>
2021-11-21 18:53:14 +0100

    Issue #8117 patch 7 - fix docker ld issue

--- recipes/antlr4-cppruntime/all/test_package/test_package.cpp
+++ recipes/antlr4-cppruntime/all/test_package/test_package.cpp
@@ -1,13 +1,7 @@
 #include "antlr4-runtime/antlr4-runtime.h"
 
-#include <iostream>
-
-using namespace antlrcpp;
-using namespace antlr4;
-using namespace std;
-
 int main(int argc, const char *args[])
 {
-    ANTLRInputStream input("test string");
+    antlr4::ANTLRInputStream input();
     return 0;
 }
