a97abe77e1bf6b7246a75256dc7226dc1e14c967
Michal Widera <michal@widera.com.pl>
2021-11-22 13:21:22 +0100

    Update recipes/antlr4-cppruntime/all/conanfile.py
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/antlr4-cppruntime/all/conanfile.py
+++ recipes/antlr4-cppruntime/all/conanfile.py
@@ -94,4 +94,4 @@ class Antlr4CppRuntimeConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.includedirs = ["include", os.path.join("include", "antlr4-runtime")]
+        self.cpp_info.includedirs.append(os.path.join("include", "antlr4-runtime"))
