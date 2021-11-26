2a9109e9abe646fe63367d4214a372e346a671ac
SSE4 <tomskside@gmail.com>
2021-11-26 15:22:50 +0700

    - use new VirtualRunEnv for 2 profiles
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- recipes/protobuf/all/test_package/conanfile.py
+++ recipes/protobuf/all/test_package/conanfile.py
@@ -1,20 +1,32 @@
 from conans import ConanFile, CMake, tools
 import os
-
+from contextlib import contextmanager
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "cmake", "cmake_find_package_multi", "VirtualRunEnv", "VirtualBuildEnv"
 
     def build_requirements(self):
-        if tools.cross_building(self.settings):
+        if hasattr(self, "settings_build"):
             self.build_requires(str(self.requires['protobuf']))
 
+    @contextmanager
+    def _env(self):
+        if hasattr(self, "settings_build"):
+            # 2 profiles - nothing is needed, VirtualRunEnv/VirtualBuildEnv manages the things
+            yield
+        else:
+            from conans import RunEnvironment
+            env_build = RunEnvironment(self)
+            with tools.environment_append(env_build.vars):
+                yield
+
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["protobuf_LITE"] = self.options["protobuf"].lite
-        cmake.configure()
-        cmake.build()
+        with self._env():
+            cmake = CMake(self)
+            cmake.definitions["protobuf_LITE"] = self.options["protobuf"].lite
+            cmake.configure()
+            cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
