3a47e6eb363d5c3687dbcfb0e1c80b6e2dd3edec
toge <toge.mail@gmail.com>
2021-11-29 12:00:05 +0900

    revert filenames, names in cpp_info

--- recipes/sqlite3/all/conanfile.py
+++ recipes/sqlite3/all/conanfile.py
@@ -169,8 +169,14 @@ class ConanSqlite3(ConanFile):
                             "conan-official-{}-variables.cmake".format(self.name))
 
     def package_info(self):
+        self.cpp_info.filenames["cmake_find_package"] = "SQLite3"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "SQLite3"
         self.cpp_info.set_property("cmake_file_name", "SQLite3")
+        self.cpp_info.names["cmake_find_package"] = "SQLite"
+        self.cpp_info.names["cmake_find_package_multi"] = "SQLite"
         self.cpp_info.set_property("cmake_target_name", "SQLite")
+        self.cpp_info.components["sqlite"].names["cmake_find_package"] = "SQLite3"
+        self.cpp_info.components["sqlite"].names["cmake_find_package_multi"] = "SQLite3"
         self.cpp_info.components["sqlite"].set_property("cmake_target_name", "SQLite3")
         self.cpp_info.components["sqlite"].builddirs.append(self._module_subfolder)
         self.cpp_info.components["sqlite"].build_modules["cmake_find_package"] = [self._module_file_rel_path]
