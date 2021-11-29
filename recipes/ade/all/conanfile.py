b9945156314a32d5b59a5d777442df0a2442f220
Luis <lasote@gmail.com>
2021-11-29 12:37:27 +0100

    ade: Add cpp_info.names

--- recipes/ade/all/conanfile.py
+++ recipes/ade/all/conanfile.py
@@ -90,6 +90,12 @@ class AdeConan(ConanFile):
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "ade")
         self.cpp_info.set_property("cmake_target_name", "ade")
+
+        self.cpp_info.names["cmake_find_package"] = "ade"
+        self.cpp_info.names["cmake_find_package_multi"] = "ade"
+        self.cpp_info.filenames["cmake_find_package"] = "ade"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "ade"
+
         self.cpp_info.builddirs.append(self._module_subfolder)
         self.cpp_info.set_property("cmake_build_modules", [self._module_file_rel_path])
         self.cpp_info.libs = ["ade"]
