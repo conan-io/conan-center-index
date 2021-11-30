70dc0ac9db5540140c946819a58f07b95ffd4d5d
Luis Martinez <lasote@gmail.com>
2021-11-30 16:05:55 +0100

    (#8224) ms-gsl: Add cpp_info.names

--- recipes/ms-gsl/all/conanfile.py
+++ recipes/ms-gsl/all/conanfile.py
@@ -71,7 +71,17 @@ class MicrosoftGslConan(ConanFile):
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "Microsoft.GSL")
         self.cpp_info.set_property("cmake_target_name", "Microsoft.GSL")
+
+        self.cpp_info.filenames["cmake_find_package"] = "Microsoft.GSL"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "Microsoft.GSL"
+        self.cpp_info.names["cmake_find_package"] = "Microsoft.GSL"
+        self.cpp_info.names["cmake_find_package_multi"] = "Microsoft.GSL"
+
         self.cpp_info.components["_ms-gsl"].set_property("cmake_target_name", "GSL")
+
+        self.cpp_info.components["_ms-gsl"].names["cmake_find_package"] = "GSL"
+        self.cpp_info.components["_ms-gsl"].names["cmake_find_package_multi"] = "GSL"
+
         if tools.Version(self.version) < "3.0.0":
             self.cpp_info.components["_ms-gsl"].defines = [
                 self._contract_map[str(self.options.on_contract_violation)]
