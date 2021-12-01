defcabce0a5e09fc3ac1d2318a08e5efbd46df98
Eric Lemanissier <eric.lemanissier@gmail.com>
2021-12-01 15:08:28 +0100

    qt 6.2.2

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -48,7 +48,7 @@ class QtConan(ConanFile):
                    "qt3d", "qtimageformats", "qtnetworkauth", "qtcoap", "qtmqtt", "qtopcua",
                    "qtmultimedia", "qtlocation", "qtsensors", "qtconnectivity", "qtserialbus",
                    "qtserialport", "qtwebsockets", "qtwebchannel", "qtwebengine", "qtwebview",
-                   "qtremoteobjects"]
+                   "qtremoteobjects", "qtpositioning"]
 
     generators = "pkg_config", "cmake_find_package", "cmake"
     name = "qt"
