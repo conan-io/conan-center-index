7269966d1fb1ca3613582f223e542e2917caa608
czoido <mrgalleta@gmail.com>
2021-12-01 13:33:56 +0100

    update conanfile.py

--- recipes/proj/all/conanfile.py
+++ recipes/proj/all/conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, CMake, tools
 import os
 
-required_conan_version = ">=1.36.0"
+required_conan_version = ">=1.43.0"
 
 
 class ProjConan(ConanFile):
@@ -131,9 +131,9 @@ class ProjConan(ConanFile):
         cmake_config_filename = "proj" if proj_version >= "7.0.0" else "proj4"
         cmake_namespace = "PROJ" if proj_version >= "7.0.0" else "PROJ4"
         self.cpp_info.set_property("cmake_file_name", cmake_config_filename)
-        self.cpp_info.set_property("cmake_target_name", cmake_namespace)
+        self.cpp_info.set_property("cmake_target_name", "{0}::{0}".format(cmake_namespace))
         self.cpp_info.set_property("pkg_config_name", "proj")
-        self.cpp_info.components["projlib"].set_property("cmake_target_name", "proj")
+        self.cpp_info.components["projlib"].set_property("cmake_target_name", "{}::proj".format(cmake_namespace))
         self.cpp_info.components["projlib"].set_property("pkg_config_name", "proj")
         self.cpp_info.components["projlib"].libs = tools.collect_libs(self)
 
