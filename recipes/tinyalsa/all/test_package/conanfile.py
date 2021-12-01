4021093179c91b7de9497f289ead1ae6b60d7d42
toge <toge.mail@gmail.com>
2021-12-02 00:05:19 +0900

    (#8095) tinyalsa: add version 2.0.0
    * tinyalsa: add version 2.0.0
    
    * add dl since 2.0.0
    
    * add validate()
    
    * rename patch filename
    
    * use 'export_sources' property
    
    * fix strncpy termination bug
    
    * remove extra paren
    
    * don't create PR. this is internal.
    
    * remove other recipe files
    
    * remove other recipe files
    
    * Revert "remove other recipe files"
    
    This reverts commit 58935fd1068152fb6c89fe95ceac6120809d1e0b.
    
    * Revert "remove other recipe files"
    
    This reverts commit 9337e33a86d70bfbf7fa2ba54df88f50e7858eb6.
    
    * Revert "Merge branch 'tinyalsa-2.0.0' of https://github.com/toge/conan-center-index into tinyalsa-2.0.0"
    
    This reverts commit 2e22229847385954dc875e090c0ed1ca4bff9af0, reversing
    changes made to c5784676afc43abb85b57d80aa28ccbd0a12700b.
    
    * Revert "don't create PR. this is internal."
    
    This reverts commit c5784676afc43abb85b57d80aa28ccbd0a12700b.
    
    * remove extra paren

--- recipes/tinyalsa/all/test_package/conanfile.py
+++ recipes/tinyalsa/all/test_package/conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class LibalsaTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -11,6 +11,6 @@ class LibalsaTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "example")
             self.run(bin_path, run_environment=True)
