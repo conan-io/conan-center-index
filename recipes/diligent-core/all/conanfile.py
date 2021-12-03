b7fee044c978c5e7e25b87a39df47d3132b8495d
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2021-12-03 09:35:32 +0100

    remove support of old compilers

--- recipes/diligent-core/all/conanfile.py
+++ recipes/diligent-core/all/conanfile.py
@@ -33,6 +33,35 @@ class DiligentCoreConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "16",
+            "gcc": "6",
+            "clang": "3.4",
+            "apple-clang": "5.1",
+        }
+
+    @property
+    def _minimum_cpp_standard(self):
+        return 14
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
+        if self.settings.compiler == "Visual Studio" and "MT" in self.settings.compiler.runtime and not self.options.header_only:
+            raise ConanInvalidConfiguration("Visual Studio build with MT runtime is not supported")
+        if "arm" in self.settings.arch or "x86" is self.settings.arch:
+            raise ConanInvalidConfiguration("Not available for arm. Requested arch: {}".format(self.settings.arch))
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
