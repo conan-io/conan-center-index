f9a2dc9ad67936596d472b5392d2bcbdaa07f4c7
Jordan Williams <jordan@jwillikers.com>
2021-12-04 19:11:35 -0600

    Make CMake target name absoulet for Conan 1.43

--- recipes/boost-ext-ut/all/conanfile.py
+++ recipes/boost-ext-ut/all/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.36.0"
+required_conan_version = ">=1.43.0"
 
 
 class UTConan(ConanFile):
@@ -69,8 +69,7 @@ class UTConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "ut")
-        self.cpp_info.set_property("cmake_target_name", "boost")
-        self.cpp_info.components["ut"].set_property("cmake_target_name", "ut")
+        self.cpp_info.components["ut"].set_property("cmake_target_name", "boost::ut")
 
         self.cpp_info.names["cmake_find_package"] = "boost"
         self.cpp_info.names["cmake_find_package_multi"] = "boost"
