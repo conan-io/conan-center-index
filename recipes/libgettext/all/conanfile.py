73ce480cada816aee06cd3866e172442b4a54e58
bincrafters-user <bincrafters@gmail.com>
2021-12-06 11:13:03 +0000

    libgettext: Update Conan conventions
    Automatically created by bincrafters-conventions 0.30.5

--- recipes/libgettext/all/conanfile.py
+++ recipes/libgettext/all/conanfile.py
@@ -53,7 +53,7 @@ class GetTextConan(ConanFile):
         if self.settings.os == 'Windows' and not tools.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/cci.latest")
         if self._is_msvc:
-            self.build_requires("automake/1.16.2")
+            self.build_requires("automake/1.16.3")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
