d1c4821f6e8caa44f0ac9db2d5b1e57fbf27692c
Riff, Eric <ericriff@gmail.com>
2021-12-07 19:04:21 -0300

    Allow gperf to build with pr:b==pr:h

--- recipes/gperf/all/test_package/conanfile.py
+++ recipes/gperf/all/test_package/conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, tools
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch"
+    test_type = "build_requires"
 
     def test(self):
         if not tools.cross_building(self):
