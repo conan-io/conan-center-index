893f3f1a0fa1d447296b27a5fc2c882cfdb6e0db
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-12-07 07:05:55 +0100

    (#8341) double-conversion 3.1.6
    * double-conversion 3.1.6
    
    * use alidate and new tools.get arguments
    
    * make test recipe compatible with new version
    
    following SIC google/double-conversion#101

--- recipes/double-conversion/all/test_package/test_package.cpp
+++ recipes/double-conversion/all/test_package/test_package.cpp
@@ -3,6 +3,10 @@
 #include "double-conversion/utils.h"
 #include "double-conversion/ieee.h"
 
+#ifndef UINT64_2PART_C
+#define UINT64_2PART_C(a, b) DOUBLE_CONVERSION_UINT64_2PART_C(a, b)
+#endif
+
 int main() {
   uint64_t ordered = UINT64_2PART_C(0x01234567, 89ABCDEF);
   std::cout << "A value: " << double_conversion::Double(ordered).value() << std::endl;
