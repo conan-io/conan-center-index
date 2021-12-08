3b516081d1f97d4a873ebddc404b42aaddb95727
Kr4is <sr.brunocabado@gmail.com>
2021-12-08 23:29:00 +0100

    follow pep8

--- recipes/ofeli/all/conanfile.py
+++ recipes/ofeli/all/conanfile.py
@@ -4,10 +4,12 @@ from conans.errors import ConanInvalidConfiguration
 
 required_conan_version = ">=1.40.0"
 
+
 class OfeliConan(ConanFile):
     name = "ofeli"
     description = "An Object Finite Element Library"
-    topics = ("conan", "ofeli", "finite element", "finite element library", "finite element analysis", "finite element solver")
+    topics = ("conan", "ofeli", "finite element", "finite element library",
+              "finite element analysis", "finite element solver")
     license = "GNU Lesser General Public License (LGPL)."
     homepage = "http://ofeli.org/index.html"
     url = "https://github.com/conan-io/conan-center-index"
@@ -27,10 +29,12 @@ class OfeliConan(ConanFile):
 
     def validate(self):
         if self.settings.os != "Linux":
-            raise ConanInvalidConfiguration("Ofeli is just supported for Linux")
+            raise ConanInvalidConfiguration(
+                "Ofeli is just supported for Linux")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  strip_root=True, destination=self._source_subfolder)
 
     def _configure_autotools(self):
         if self._autotools:
@@ -38,7 +42,7 @@ class OfeliConan(ConanFile):
         self._autotools = AutoToolsBuildEnvironment(self)
         self._autotools.configure(args=["--enable-release"])
         return self._autotools
-    
+
     def configure(self):
         self.settings.compiler.libcxx = "libstdc++11"
 
@@ -48,14 +52,17 @@ class OfeliConan(ConanFile):
             autotools.make()
 
     def package(self):
-        self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
-        self.copy("*libofeli.a", dst="lib", src=os.path.join(self._source_subfolder, "src"))
-        self.copy("*.md", dst="res", src=os.path.join(self._source_subfolder, "material"))
+        self.copy("*.h", dst="include",
+                  src=os.path.join(self._source_subfolder, "include"))
+        self.copy("*libofeli.a", dst="lib",
+                  src=os.path.join(self._source_subfolder, "src"))
+        self.copy("*.md", dst="res",
+                  src=os.path.join(self._source_subfolder, "material"))
         self.copy("COPYING", dst="licenses", src=self._doc_folder)
-        
+
     def package_info(self):
         self.cpp_info.name = "Ofeli"
         self.cpp_info.names["pkg_config"] = "Ofeli"
         self.cpp_info.libs = ["ofeli"]
-        self.env_info.OFELI_PATH_MATERIAL.append(os.path.join(self.package_folder, "res"))
-
+        self.env_info.OFELI_PATH_MATERIAL.append(
+            os.path.join(self.package_folder, "res"))
