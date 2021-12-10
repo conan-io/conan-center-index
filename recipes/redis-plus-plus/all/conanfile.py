2c70424bdcf6fe846fc31cf7ff63d6cf9857ff6b
Francisco Ramírez <franchuti688@gmail.com>
2021-12-10 15:05:34 +0100

    Update recipes/redis-plus-plus/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/redis-plus-plus/all/conanfile.py
+++ recipes/redis-plus-plus/all/conanfile.py
@@ -117,7 +117,6 @@ class RedisPlusPlusConan(ConanFile):
         self.cpp_info.set_property("cmake_file_name", "redis++")
         self.cpp_info.set_property("cmake_target_name", "redis++::redis++" + "_static" if not self.options.shared else "")
         self.cpp_info.set_property("pkg_config_name", "redis++" + "_static" if not self.options.shared else "")
-        self.cpp_info.components["redis++lib"].set_property("cmake_target_name", "redis++::redis++" + "_static" if not self.options.shared else "")
         self.cpp_info.components["redis++lib"].set_property("pkg_config_name", "redis++" + "_static" if not self.options.shared else "")
 
         self.cpp_info.names["cmake_find_package"] = "redis++"
