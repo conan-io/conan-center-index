0ebc046cc12570421a170d3ea2beb3fe5a69e84d
Marian Klymov <nekto1989@gmail.com>
2021-12-12 21:55:15 +0200

    remove unnecessary check

--- recipes/gklib/all/test_package/conanfile.py
+++ recipes/gklib/all/test_package/conanfile.py
@@ -19,7 +19,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        assert os.path.isfile(os.path.join(self.deps_cpp_info["gklib"].rootpath, "licenses", "LICENSE.txt"))
         if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
