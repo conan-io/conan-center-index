9ac268ac9df03899cd5fdaf38119debc2afba895
jgsogo <jgsogo@gmail.com>
2021-12-21 11:38:53 +0100

    fix import (1.43.1)

--- recipes/fmt/all/test_cmakedeps/conanfile.py
+++ recipes/fmt/all/test_cmakedeps/conanfile.py
@@ -1,7 +1,7 @@
 import os
 from conans import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
-from conan import tools
+from conan.tools.cross_building import cross_building as tools_cross_building
 
 
 class TestPackageConan(ConanFile):
@@ -19,6 +19,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building.cross_building(self):
+        if not tools_cross_building(self):
             self.run("test_package", run_environment=True)
             self.run("test_ranges", run_environment=True)
