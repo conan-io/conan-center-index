1ba700dfd913c14c66f0c9eb3dba84b296ccddd4
toge <toge.mail@gmail.com>
2021-12-24 03:57:35 +0900

    oatpp-websocket: add version 1.3.0

--- recipes/oatpp-websocket/all/conanfile.py
+++ recipes/oatpp-websocket/all/conanfile.py
@@ -2,17 +2,17 @@ from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.28.0"
+required_conan_version = ">=1.43.0"
 
 class OatppWebSocketConan(ConanFile):
     name = "oatpp-websocket"
     description = "WebSocket functionality for oatpp applications"
     homepage = "https://github.com/oatpp/oatpp-websocket"
     license = "Apache-2.0"
-    topics = ("conan", "oat++", "oatpp", "websocket")
+    topics = ("oat++", "oatpp", "websocket")
     url = "https://github.com/conan-io/conan-center-index"
     generators = "cmake", "cmake_find_package"
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     exports_sources = "CMakeLists.txt"
@@ -34,6 +34,8 @@ class OatppWebSocketConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+
+    def validate(self):
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, 11)
 
@@ -48,8 +50,7 @@ class OatppWebSocketConan(ConanFile):
         self.requires("oatpp/" + self.version)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("oatpp-websocket-{0}".format(self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -74,15 +75,18 @@ class OatppWebSocketConan(ConanFile):
     def package_info(self):
         self.cpp_info.filenames["cmake_find_package"] = "oatpp-websocket"
         self.cpp_info.filenames["cmake_find_package_multi"] = "oatpp-websocket"
+        self.cpp_info.set_property("cmake_file_name", "oatpp-websocket")
         self.cpp_info.names["cmake_find_package"] = "oatpp"
         self.cpp_info.names["cmake_find_package_multi"] = "oatpp"
+        self.cpp_info.set_property("cmake_target_name", "oatpp::oatpp-websocket")
         self.cpp_info.components["_oatpp-websocket"].names["cmake_find_package"] = "oatpp-websocket"
         self.cpp_info.components["_oatpp-websocket"].names["cmake_find_package_multi"] = "oatpp-websocket"
+        self.cpp_info.components["_oatpp-websocket"].set_property("cmake_target_name", "oatpp::oatpp-websocket")
         self.cpp_info.components["_oatpp-websocket"].includedirs = [
             os.path.join("include", "oatpp-{}".format(self.version), "oatpp-websocket")
         ]
         self.cpp_info.components["_oatpp-websocket"].libdirs = [os.path.join("lib", "oatpp-{}".format(self.version))]
         self.cpp_info.components["_oatpp-websocket"].libs = ["oatpp-websocket"]
-        if self.settings.os == "Linux":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["_oatpp-websocket"].system_libs = ["pthread"]
         self.cpp_info.components["_oatpp-websocket"].requires = ["oatpp::oatpp"]
