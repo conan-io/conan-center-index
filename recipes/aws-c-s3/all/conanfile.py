3e4e0e19f5911b05d87f9d953ce9a89aa27d72f9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-12-23 11:35:09 +0100

    bump dependencies

--- recipes/aws-c-s3/all/conanfile.py
+++ recipes/aws-c-s3/all/conanfile.py
@@ -40,7 +40,7 @@ class AwsCS3(ConanFile):
         del self.settings.compiler.libcxx
 
     def requirements(self):
-        self.requires("aws-c-common/0.6.15")
+        self.requires("aws-c-common/0.6.17")
         self.requires("aws-c-io/0.10.13")
         self.requires("aws-c-http/0.6.10")
         self.requires("aws-c-auth/0.6.8")
