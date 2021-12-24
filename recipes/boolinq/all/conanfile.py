a77db2b599c86d652ab22c227375f5352d31bba3
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-12-24 01:35:33 +0100

    modernize

--- recipes/boolinq/all/conanfile.py
+++ recipes/boolinq/all/conanfile.py
@@ -1,22 +1,24 @@
 from conans import ConanFile, tools
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class BoolinqConan(ConanFile):
     name = "boolinq"
     description = "Super tiny C++11 single-file header-only LINQ template library"
-    topics = ("conan", "boolinq", "linq", "header-only")
+    topics = ("boolinq", "linq", "header-only")
     license = "MIT"
     homepage = "https://github.com/k06a/boolinq"
     url = "https://github.com/conan-io/conan-center-index"
-    settings = "compiler"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def configure(self):
+    def validate(self):
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, 11)
 
@@ -24,8 +26,8 @@ class BoolinqConan(ConanFile):
         self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename(self.name + "-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
