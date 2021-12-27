5b21a6de6fe054af34dcc05dd45830b5c87cd74d
Werner Palfinger <werner.palfinger@profactor.at>
2021-12-27 11:44:23 +0100

    header-only only

--- recipes/cvplot/all/conanfile.py
+++ recipes/cvplot/all/conanfile.py
@@ -1,5 +1,5 @@
+from conans import ConanFile, tools
 import os
-from conans import ConanFile, CMake, tools
 
 required_conan_version = ">=1.33.0"
 
@@ -12,55 +12,22 @@ class CvPlotConan(ConanFile):
     homepage = "https://github.com/Profactor/cv-plot"
     url = "https://github.com/conan-io/conan-center-index"
     generators = "cmake"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "header_only": [True, False]}
-    default_options = {"shared": False, "header_only": False}
     requires = "opencv/4.5.3"
-
-    _cmake = None
+    no_copy_source = True
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def configure(self):
-        if self.options.header_only:
-            del self.options.shared
-
-    def package_id(self):
-        if self.options.header_only:
-            self.info.header_only()
-            
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
-    def build(self):
-        if not self.options.header_only:
-            cmake = self._configure_cmake()
-            cmake.build()
-
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["CVPLOT_HEADER_ONLY"] = self.options.header_only
-        self._cmake.definitions["CVPLOT_WITH_TESTS"] = False
-        self._cmake.definitions["CVPLOT_WITH_EXAMPLES"] = False
-        self._cmake.configure(source_folder=self._source_subfolder)
-        return self._cmake
-
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        if self.options.header_only:
-            self.copy(pattern="*", dst="include", src=os.path.join(self._source_subfolder, "CvPlot", "inc"))
-        else:
-            cmake = self._configure_cmake()
-            cmake.install()
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*", dst="include", src=os.path.join(self._source_subfolder, "CvPlot", "inc"))
         
+    def package_id(self):
+        self.info.header_only()
+            
     def package_info(self):
-        if self.options.header_only:
-            self.cpp_info.defines.append("CVPLOT_HEADER_ONLY")
-        else:
-            self.cpp_info.libs = ["CvPlot"]
-            if self.options.shared:
-                self.cpp_info.defines.append("CVPLOT_SHARED")
+        self.cpp_info.defines.append("CVPLOT_HEADER_ONLY")
