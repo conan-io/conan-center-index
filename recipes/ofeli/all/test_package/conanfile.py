4f0dcc883207dcf3db8751a3da11dbc288aad3bc
Kr4is <sr.brunocabado@gmail.com>
2021-12-27 15:37:51 +0100

    test with config_options

--- recipes/ofeli/all/test_package/conanfile.py
+++ recipes/ofeli/all/test_package/conanfile.py
@@ -6,6 +6,9 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    def config_options(self):
+        self.settings.compiler.libcxx = "libstdc++11"
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
