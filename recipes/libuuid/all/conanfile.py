93861a4130178fac3abcb3234ecd2d082bd966f2
牛逼的李冰 <88433283+NiuBlibing@users.noreply.github.com>
2021-12-28 21:36:09 +0800

    reserve .pc file for libuuid
    other build system like meson and cmake need the pc file

--- recipes/libuuid/all/conanfile.py
+++ recipes/libuuid/all/conanfile.py
@@ -71,7 +71,6 @@ class LibuuidConan(ConanFile):
         autotools = self._configure_autotools()
         autotools.install()
         tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.names["pkg_config"] = "uuid"
