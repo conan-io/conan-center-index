d593af59ace01b043098e2edc41d0ad3eca15867
toge <toge.mail@gmail.com>
2022-01-01 00:34:26 +0900

    (#8580) tgbot-cpp: add version 1.3

--- recipes/tgbot/all/test_package/conanfile.py
+++ recipes/tgbot/all/test_package/conanfile.py
@@ -12,5 +12,5 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             self.run(os.path.join("bin", "test_package"), run_environment=True)
