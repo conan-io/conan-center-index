bfae9198095222936da06be9c28be9c178e043c1
jgsogo <jgsogo@gmail.com>
2021-12-31 18:23:08 +0100

    compiler needs to provide support for C++17

--- recipes/veque/all/conanfile.py
+++ recipes/veque/all/conanfile.py
@@ -9,6 +9,8 @@ class VequeConan(ConanFile):
     description = "Fast C++ container combining the best features of std::vector and std::deque"
     topics = ("cpp17", "vector", "deque")
     license = "BSL-1.0"
+
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
@@ -22,9 +24,17 @@ class VequeConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
+    def _supports_cpp17(self):
+        supported_compilers = [("gcc", "7"), ("clang", "5"), ("apple-clang", "10"), ("Visual Studio", "15.7")]
+        compiler = self.settings.compiler
+        version = tools.Version(compiler.version)
+        return any(compiler == sc[0] and version >= sc[1] for sc in supported_compilers)
+
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
             tools.check_min_cppstd(self, "17")
+        elif not self._supports_cpp17():
+            raise ConanInvalidConfiguration("C++17 support is required")
 
     def package(self):
         self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
