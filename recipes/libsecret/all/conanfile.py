79fe4cbcc9c86857d199fa9e6912c3c452554793
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-01 17:10:02 +0100

    fix libgcrypt requires in package_info()

--- recipes/libsecret/all/conanfile.py
+++ recipes/libsecret/all/conanfile.py
@@ -105,6 +105,6 @@ class LibsecretConan(ConanFile):
         self.cpp_info.names["pkg_config"] = "libsecret-1"
         self.cpp_info.requires = ["glib::glib-2.0", "glib::gobject-2.0"]
         if self._use_gcrypt:
-            self.cpp_info.requires.append("libgcrypt::gcrypt")
+            self.cpp_info.requires.append("libgcrypt::libgcrypt")
         self.cpp_info.includedirs = [os.path.join("include", "libsecret-1")]
         self.cpp_info.libs = ["secret-1"]
