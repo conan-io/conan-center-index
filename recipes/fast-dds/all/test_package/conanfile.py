cb98c696ef5f7c839da1c515664a72a383e4aa05
dotChris90 <Christian.Kahr@gmx.de>
2022-01-03 06:35:36 -0500

    simplify test according to PR commits

--- recipes/fast-dds/all/test_package/conanfile.py
+++ recipes/fast-dds/all/test_package/conanfile.py
@@ -1,8 +1,5 @@
-import subprocess
 from conans import ConanFile, CMake, tools
 import os
-from subprocess import Popen
-import platform
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -14,22 +11,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            bin_sub_cmd = os.path.join("bin", "test_sub")
-            bin_pub_cmd = os.path.join("bin", "test_pub")
-            if platform.system() == "Darwin":
-                # on Macos check just if able to execute
-                command = "{0} &".format(bin_pub_cmd)
-                self.run(command,run_environment=True)
-            else:
-                # Popen must be used to detect blocking process
-                sub_process = Popen([bin_sub_cmd],stdout=subprocess.PIPE) 
-                pub_process = Popen([bin_pub_cmd],stdout=subprocess.PIPE)
-                sub_process.wait(30)
-                pub_process.wait(30)
-                msg_send = 'Message: HelloWorld with index: 5 SENT' in str(pub_process.stdout.read())
-                msg_received = 'HelloWorld 5 RECEIVED' in str(sub_process.stdout.read())
-                # FIXME: works just proper on Linux, on windows sometimes fail (maybe stdout format problem?)
-                if platform.system() == "Linux":
-                    assert msg_send
-                    assert msg_received
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
