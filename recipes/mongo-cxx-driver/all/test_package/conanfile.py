e2298a58a1297ba6943238b3eb0ef379facb44c7
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-07 00:13:59 +0100

    modernize

--- recipes/mongo-cxx-driver/all/test_package/conanfile.py
+++ recipes/mongo-cxx-driver/all/test_package/conanfile.py
@@ -3,16 +3,15 @@ import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["MONGO-CXX-DRIVER_SHARED"] = self.options["mongo-cxx-driver"].shared
         cmake.definitions["MONGO-CXX-DRIVER_POLYFILL"] = self.options["mongo-cxx-driver"].polyfill
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             self.run(os.path.join("bin", "test_package"), run_environment=True)
