0f2e266a9e147ab8d68046dcb5e34f73df3823c5
ngrodzitski <utromvecherom@gmail.com>
2022-01-07 16:07:09 +0300

    Add so5extra-1.5.0

--- recipes/so5extra/all/conanfile.py
+++ recipes/so5extra/all/conanfile.py
@@ -17,7 +17,10 @@ class So5extraConan(ConanFile):
         return "source_subfolder"
 
     def requirements(self):
-        self.requires("sobjectizer/5.7.2.3")
+        if tools.Version(self.version) >= "1.5.0":
+            self.requires("sobjectizer/5.7.3")
+        else:
+            self.requires("sobjectizer/5.7.2.3")
 
     def configure(self):
         minimal_cpp_standard = "17"
