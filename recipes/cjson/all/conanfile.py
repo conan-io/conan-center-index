6b890e616fde0524ab5bc36688cd11d5c1f7f4bb
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-09 11:49:04 +0100

    modernize

--- recipes/cjson/all/conanfile.py
+++ recipes/cjson/all/conanfile.py
@@ -3,32 +3,32 @@ from conans.errors import ConanInvalidConfiguration
 import os
 import textwrap
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.43.0"
 
 
 class CjsonConan(ConanFile):
     name = "cjson"
     description = "Ultralightweight JSON parser in ANSI C."
     license = "MIT"
-    topics = ("conan", "cjson", "json", "parser")
+    topics = ("cjson", "json", "parser")
     homepage = "https://github.com/DaveGamble/cJSON"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = ["CMakeLists.txt", "patches/**"]
-    generators = "cmake"
+
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
         "utils": [True, False],
-        "use_locales": [True, False]
+        "use_locales": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
         "utils": False,
-        "use_locales": True
+        "use_locales": True,
     }
 
+    generators = "cmake"
     _cmake = None
 
     @property
@@ -39,6 +39,11 @@ class CjsonConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -48,12 +53,16 @@ class CjsonConan(ConanFile):
             del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
-        if self.settings.compiler == "Visual Studio" and self.options.shared and self.settings.compiler.runtime == "MTd":
+
+    def validate(self):
+        if self.options.shared and \
+           ((self.settings.compiler == "Visual Studio" and self.settings.compiler.runtime == "MTd") or \
+            (str(self.settings.compiler) == "msvc" and self.settings.compiler.runtime == "MT" and self.settings.compiler.runtime_type == "Debug")):
             raise ConanInvalidConfiguration("shared cjson is not supported with MTd runtime")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("cJSON-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
@@ -117,27 +126,32 @@ class CjsonConan(ConanFile):
         return "conan-official-{}-targets.cmake".format(self.name)
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "cJSON"
-        self.cpp_info.names["cmake_find_package_multi"] = "cJSON"
+        self.cpp_info.set_property("cmake_file_name", "cJSON")
 
-        module_target_rel_path = os.path.join(self._module_subfolder, self._module_file)
-
-        self.cpp_info.components["_cjson"].names["cmake_find_package"] = "cjson"
-        self.cpp_info.components["_cjson"].names["cmake_find_package_multi"] = "cjson"
-        self.cpp_info.components["_cjson"].names["pkg_config"] = "libcjson"
+        self.cpp_info.components["_cjson"].set_property("cmake_target_name", "cjson")
+        self.cpp_info.components["_cjson"].set_property("pkg_config_name", "libcjson")
         self.cpp_info.components["_cjson"].libs = ["cjson"]
-        self.cpp_info.components["_cjson"].builddirs.append(self._module_subfolder)
-        self.cpp_info.components["_cjson"].build_modules["cmake_find_package"] = [module_target_rel_path]
-        self.cpp_info.components["_cjson"].build_modules["cmake_find_package_multi"] = [module_target_rel_path]
-        if self.settings.os == "Linux":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["_cjson"].system_libs = ["m"]
 
         if self.options.utils:
-            self.cpp_info.components["cjson_utils"].names["cmake_find_package"] = "cjson_utils"
-            self.cpp_info.components["cjson_utils"].names["cmake_find_package_multi"] = "cjson_utils"
-            self.cpp_info.components["cjson_utils"].names["pkg_config"] = "libcjson_utils"
+            self.cpp_info.components["cjson_utils"].set_property("cmake_target_name", "cjson_utils")
+            self.cpp_info.components["cjson_utils"].set_property("pkg_config_name", "libcjson_utils")
             self.cpp_info.components["cjson_utils"].libs = ["cjson_utils"]
             self.cpp_info.components["cjson_utils"].requires = ["_cjson"]
+
+        # TODO: remove in conan v2
+        self.cpp_info.names["cmake_find_package"] = "cJSON"
+        self.cpp_info.names["cmake_find_package_multi"] = "cJSON"
+        module_target_rel_path = os.path.join(self._module_subfolder, self._module_file)
+        self.cpp_info.components["_cjson"].names["cmake_find_package"] = "cjson"
+        self.cpp_info.components["_cjson"].names["cmake_find_package_multi"] = "cjson"
+        self.cpp_info.components["_cjson"].builddirs.append(self._module_subfolder)
+        self.cpp_info.components["_cjson"].build_modules["cmake_find_package"] = [module_target_rel_path]
+        self.cpp_info.components["_cjson"].build_modules["cmake_find_package_multi"] = [module_target_rel_path]
+        if self.options.utils:
+            self.cpp_info.components["cjson_utils"].names["cmake_find_package"] = "cjson_utils"
+            self.cpp_info.components["cjson_utils"].names["cmake_find_package_multi"] = "cjson_utils"
             self.cpp_info.components["cjson_utils"].builddirs.append(self._module_subfolder)
             self.cpp_info.components["cjson_utils"].build_modules["cmake_find_package"] = [module_target_rel_path]
             self.cpp_info.components["cjson_utils"].build_modules["cmake_find_package_multi"] = [module_target_rel_path]
