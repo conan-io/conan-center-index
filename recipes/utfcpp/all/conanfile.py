afb67d7bd495077f922b0bfa08960d8d231f6c4c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-10 23:42:13 +0100

    (#8747) utfcpp: modernize

--- recipes/utfcpp/all/conanfile.py
+++ recipes/utfcpp/all/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, tools
 import os
 import textwrap
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.43.0"
 
 
 class UtfCppConan(ConanFile):
@@ -10,9 +10,9 @@ class UtfCppConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/nemtrif/utfcpp"
     description = "UTF-8 with C++ in a Portable Way"
-    settings = "os", "compiler", "arch", "build_type"
     topics = ("utf", "utf8", "unicode", "text")
     license = "BSL-1.0"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
@@ -31,7 +31,7 @@ class UtfCppConan(ConanFile):
                   dst=os.path.join("include", "utf8cpp"),
                   src=os.path.join(self._source_subfolder, "source"))
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self._create_cmake_module_alias_targets(
             os.path.join(self.package_folder, self._module_file_rel_path),
             {"utf8cpp": "utf8cpp::utf8cpp"}
@@ -59,9 +59,13 @@ class UtfCppConan(ConanFile):
                             "conan-official-{}-targets.cmake".format(self.name))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "utf8cpp")
+        self.cpp_info.set_property("cmake_target_name", "utf8cpp")
+        self.cpp_info.includedirs.append(os.path.join("include", "utf8cpp"))
+
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.names["cmake_find_package"] = "utf8cpp"
         self.cpp_info.names["cmake_find_package_multi"] = "utf8cpp"
         self.cpp_info.builddirs.append(self._module_subfolder)
         self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
         self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
-        self.cpp_info.includedirs.append(os.path.join("include", "utf8cpp"))
