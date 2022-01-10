2472cc2ee2bc422aa71f65fd8e7937dfd9f0080c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-11 00:02:46 +0100

    modernize

--- recipes/cimg/all/conanfile.py
+++ recipes/cimg/all/conanfile.py
@@ -2,16 +2,18 @@ from conans import ConanFile, tools
 import os
 import shutil
 
+required_conan_version = ">=1.36.0"
+
 
 class CImgConan(ConanFile):
     name = "cimg"
     description = "The CImg Library is a small and open-source C++ toolkit for image processing"
     homepage = "http://cimg.eu"
-    topics = "conan", "cimg", "physics", "simulation", "robotics", "kinematics", "engine",
+    topics = ("cimg", "physics", "simulation", "robotics", "kinematics", "engine")
     license = "CeCILL V2"
     url = "https://github.com/conan-io/conan-center-index"
     no_copy_source = True
-    settings = "compiler"
+    settings = "os", "arch", "compiler", "build_type"
 
     _cimg_options = (
         ("enable_fftw",         "cimg_use_fftw",       "fftw/3.3.8"),
@@ -30,9 +32,6 @@ class CImgConan(ConanFile):
     options = dict((option, [True, False]) for option, _, _ in _cimg_options)
     default_options = dict((option, True) for option, _, _ in _cimg_options)
 
-    def configure(self):
-        tools.check_min_cppstd(self, "11")
-
     @property
     def _source_subfolder(self):
         return "source_subfolder"
@@ -42,9 +41,16 @@ class CImgConan(ConanFile):
             if getattr(self.options, option):
                 self.requires(req)
 
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, "11")
+
+    def package_id(self):
+        self.info.header_only()
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("CImg-{}".format(self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def package(self):
         self.copy("Licence*", src=self._source_subfolder, dst="licenses")
@@ -52,13 +58,13 @@ class CImgConan(ConanFile):
         shutil.copytree(os.path.join(self.source_folder, self._source_subfolder, "plugins"),
                         os.path.join(self.package_folder, "include", "plugins"))
 
-    def package_id(self):
-        self.info.header_only()
-
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "CImg"
-        self.cpp_info.names["cmake_find_package_multi"] = "CImg"
-
+        self.cpp_info.set_property("pkg_config_file", "CImg")
         for option, define, _ in self._cimg_options:
             if getattr(self.options, option):
                 self.cpp_info.defines.append(define)
+
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
+        #       do not use this name in CMakeDeps, it was a mistake, there is no offial config file
+        self.cpp_info.names["cmake_find_package"] = "CImg"
+        self.cpp_info.names["cmake_find_package_multi"] = "CImg"
