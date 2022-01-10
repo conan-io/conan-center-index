ef12e2dfaf0c247d6fa4f20b89b2e233145a9602
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2022-01-10 23:22:03 +0800

    Correctly detect MSVC
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>

--- recipes/sassc/all/conanfile.py
+++ recipes/sassc/all/conanfile.py
@@ -23,7 +23,7 @@ class SasscConan(ConanFile):
 
     @property
     def _is_msvc(self):
-        return self.settings.os == "Windows" and self.settings.compiler == "Visual Studio"
+        return str(self.settings.compiler) in ["Visual Studio", "msvc"]
 
     def config_options(self):
         del self.settings.compiler.libcxx
