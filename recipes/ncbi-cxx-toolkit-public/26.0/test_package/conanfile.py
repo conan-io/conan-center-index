354129b29d0124125904741dd102de994206fc19
Gourianov, Andrei <gouriano@ncbi.nlm.nih.gov>
2022-01-11 11:52:27 -0500

    Corrections

--- recipes/ncbi-cxx-toolkit-public/26.0/test_package/conanfile.py
+++ recipes/ncbi-cxx-toolkit-public/26.0/test_package/conanfile.py
@@ -5,16 +5,13 @@ from conans import ConanFile, CMake, tools
 
 class NcbiCxxToolkitTest(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self):
-            os.chdir("bin")
-            self.run(".%sbasic_sample" % os.sep, run_environment=True)
+            self.run(os.path.join("bin", "basic_sample"), run_environment=True)
