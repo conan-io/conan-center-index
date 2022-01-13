dad0d94745b3871796ea4c79724da7b5db4edf17
SSE4 <tomskside@gmail.com>
2022-01-13 17:44:25 +0700

    - fix shared builds, ensure transitive ffmpeg deps are linked
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- recipes/opencv/4.x/conanfile.py
+++ recipes/opencv/4.x/conanfile.py
@@ -322,14 +322,14 @@ class OpenCVConan(ConanFile):
         self._cmake.definitions["WITH_NVCUVID"] = False
 
         self._cmake.definitions["WITH_FFMPEG"] = self.options.get_safe("with_ffmpeg")
-        self._cmake.definitions["HAVE_FFMPEG"] = self.options.get_safe("with_ffmpeg")
         if self.options.get_safe("with_ffmpeg"):
             self._cmake.definitions["OPENCV_FFMPEG_SKIP_BUILD_CHECK"] = True
             self._cmake.definitions["OPENCV_FFMPEG_SKIP_DOWNLOAD"] = True
             # opencv will not search for ffmpeg package, but for
             # libavcodec;libavformat;libavutil;libswscale modules
-            self._cmake.definitions["OPENCV_FFMPEG_USE_FIND_PACKAGE"] = False
+            self._cmake.definitions["OPENCV_FFMPEG_USE_FIND_PACKAGE"] = "ffmpeg"
             self._cmake.definitions["OPENCV_INSTALL_FFMPEG_DOWNLOAD_SCRIPT"] = False
+            self._cmake.definitions["FFMPEG_LIBRARIES"] = "ffmpeg::avcodec;ffmpeg::avformat;ffmpeg::avutil;ffmpeg::swscale"
             for component in ["avcodec", "avformat", "avutil", "swscale", "avresample"]:
                 self._cmake.definitions["FFMPEG_lib%s_VERSION" % component] = self.deps_cpp_info["ffmpeg"].components[component].version
 
