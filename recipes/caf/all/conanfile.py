70c863267464426371448730e1566ebe93fdec79
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-13 21:25:13 +0100

    bump openssl

--- recipes/caf/all/conanfile.py
+++ recipes/caf/all/conanfile.py
@@ -53,7 +53,7 @@ class CAFConan(ConanFile):
 
     def requirements(self):
         if self.options.with_openssl:
-            self.requires("openssl/1.1.1k")
+            self.requires("openssl/1.1.1m")
 
     def _minimum_compilers_version(self, cppstd):
         standards = {
