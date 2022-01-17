ca88e1ad100c77192bbe7e9cad0df419531149f4
ngrodzitski <utromvecherom@gmail.com>
2022-01-17 20:52:17 +0300

    Add llhttp library

---
+++ recipes/llhttp/all/conanfile.py
@@ -0,0 +1,63 @@
+from conans import ConanFile, tools, CMake
+import os
+
+
+class HttpParserConan(ConanFile):
+    name = "http_parser"
+    description = "http request/response parser for c "
+    topics = ("conan", "http", "parser")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/nodejs/llhttp"
+    license = ("MIT",)
+    # exports_sources = "CMakeLists.txt",
+    generators = "cmake",
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+        del self.settings.compiler.cppstd
+        del self.settings.compiler.libcxx
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+
+        self._cmake = CMake(self)
+        self._cmake.configure(source_folder=self._source_subfolder)
+        return self._cmake
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = "llhttp-release-v{}".format(self.version)
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE-MIT", src=os.path.join(self.source_folder, self._source_subfolder), dst="licenses")
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["llhttp"]
