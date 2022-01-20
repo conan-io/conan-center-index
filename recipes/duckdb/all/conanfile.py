48c8aae9d259873bb0ce3e20a51bebe5bbe7479f
toge <toge.mail@gmail.com>
2022-01-21 03:39:28 +0900

    support amalgamation

--- recipes/duckdb/all/conanfile.py
+++ recipes/duckdb/all/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, CMake, tools
 import os
+import shutil
 
 required_conan_version = ">=1.33.0"
 
@@ -10,16 +11,17 @@ class DuckdbConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index/"
     description = "DuckDB is an embeddable SQL OLAP Database Management System"
     topics = ("sql", "database", "olap", "embedded-database")
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     generators = "cmake"
-    exports = "CMakeLists.txt"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "with_icu": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
+        "with_icu": False,
     }
 
     _cmake = None
@@ -28,19 +30,31 @@ class DuckdbConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        tools.get(**(self.conan_data["sources"][self.version][0]),
                   destination=self._source_subfolder, strip_root=True)
+        tools.get(**(self.conan_data["sources"][self.version][1]),
+                  destination=os.path.join(self._source_subfolder, "src", "amalgamation"))
 
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
+        self._cmake.definitions["AMALGAMATION_BUILD"] = True
         self._cmake.definitions["BUILD_UNITTESTS"] = False
+        self._cmake.definitions["BUILD_STATIC_LIBS"] = not self.options.shared
+        self._cmake.definitions["BUILD_ICU_EXTENSION"] = self.options.with_icu
         self._cmake.configure()
         return self._cmake
 
     def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -51,7 +65,11 @@ class DuckdbConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.libs = ["duckdb"]
+        if self.options.shared:
+            self.cpp_info.libs = ["duckdb"]  
+        else:
+            self.cpp_info.libs = ["duckdb_static"]
+            if self.options.with_icu:
+                self.cpp_info.libs.append("icu_extension")
         if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs.append("pthread")
-            self.cpp_info.system_libs.append("dl")
+            self.cpp_info.system_libs = ["pthread", "dl"]
