73c395e20670861b5b2ebaced90107f346cc0e27
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-22 01:55:38 +0100

    modernize

--- recipes/mbedtls/all/conanfile.py
+++ recipes/mbedtls/all/conanfile.py
@@ -2,21 +2,30 @@ from conans import CMake, ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.43.0"
+
 
 class MBedTLSConan(ConanFile):
     name = "mbedtls"
     description = "mbed TLS makes it trivially easy for developers to include cryptographic and SSL/TLS capabilities in their (embedded) products"
-    topics = ("conan", "mbedtls", "polarssl", "tls", "security")
+    topics = ("mbedtls", "polarssl", "tls", "security")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://tls.mbed.org"
     license = ("GPL-2.0", "Apache-2.0",)
-    exports_sources = "CMakeLists.txt", "patches/**"
-    generators = "cmake"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False], "with_zlib": [True, False]}
-    default_options = {"shared": False, "fPIC": True, "with_zlib": True}
 
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_zlib": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_zlib": True,
+    }
+
+    generators = "cmake"
     _cmake = None
 
     @property
@@ -27,19 +36,10 @@ class MBedTLSConan(ConanFile):
     def _license(self):
         return self.version.rsplit("-", 1)[1]
 
-    def validate(self):
-        if tools.Version(self.version) >= "2.23.0" \
-            and self.settings.os == "Windows" and self.options.shared:
-            raise ConanInvalidConfiguration(
-                f"{self.name}/{self.version} does not support shared build on Windows"
-                )
-
-        if tools.Version(self.version) >= "2.23.0" \
-            and self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "5":
-            # The command line flags set are not supported on older versions of gcc
-            raise ConanInvalidConfiguration(
-                f"{self.settings.compiler}-{self.settings.compiler.version} is not supported by this recipe"
-                )
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -59,6 +59,20 @@ class MBedTLSConan(ConanFile):
     def requirements(self):
         if self.options.get_safe("with_zlib"):
             self.requires("zlib/1.2.11")
+
+    def validate(self):
+        if tools.Version(self.version) >= "2.23.0" \
+            and self.settings.os == "Windows" and self.options.shared:
+            raise ConanInvalidConfiguration(
+                f"{self.name}/{self.version} does not support shared build on Windows"
+                )
+
+        if tools.Version(self.version) >= "2.23.0" \
+            and self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "5":
+            # The command line flags set are not supported on older versions of gcc
+            raise ConanInvalidConfiguration(
+                f"{self.settings.compiler}-{self.settings.compiler.version} is not supported by this recipe"
+                )
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
@@ -91,26 +105,29 @@ class MBedTLSConan(ConanFile):
                 self.copy("apache-2.0.txt", src=os.path.join(self.source_folder, self._source_subfolder), dst="licenses")
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
         tools.rmdir(os.path.join(self.package_folder, "cmake"))
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "MbedTLS"
-        self.cpp_info.names["cmake_find_package_multi"] = "MbedTLS"
-        self.cpp_info.components["mbedcrypto"].names["cmake_find_package"] = "mbedcrypto"
-        self.cpp_info.components["mbedcrypto"].names["cmake_find_package_multi"] = "mbedcrypto"
+        self.cpp_info.set_property("cmake_file_name", "MbedTLS")
+        self.cpp_info.set_property("cmake_target_name", "MbedTLS::mbedtls")
+
+        self.cpp_info.components["mbedcrypto"].set_property("cmake_target_name", "MbedTLS::mbedcrypto")
         self.cpp_info.components["mbedcrypto"].libs = ["mbedcrypto"]
 
-        self.cpp_info.components["mbedx509"].names["cmake_find_package"] = "mbedx509"
-        self.cpp_info.components["mbedx509"].names["cmake_find_package_multi"] = "mbedx509"
+        self.cpp_info.components["mbedx509"].set_property("cmake_target_name", "MbedTLS::mbedx509")
         self.cpp_info.components["mbedx509"].libs = ["mbedx509"]
         self.cpp_info.components["mbedx509"].requires = ["mbedcrypto"]
 
-        self.cpp_info.components["libembedtls"].names["cmake_find_package"] = "mbedtls"
-        self.cpp_info.components["libembedtls"].names["cmake_find_package_multi"] = "mbedtls"
+        self.cpp_info.components["libembedtls"].set_property("cmake_target_name", "MbedTLS::mbedtls")
         self.cpp_info.components["libembedtls"].libs = ["mbedtls"]
         self.cpp_info.components["libembedtls"].requires = ["mbedx509"]
 
         if self.options.get_safe("with_zlib"):
             for component in self.cpp_info.components:
                 self.cpp_info.components[component].requires.append("zlib::zlib")
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.names["cmake_find_package"] = "MbedTLS"
+        self.cpp_info.names["cmake_find_package_multi"] = "MbedTLS"
+        self.cpp_info.components["libembedtls"].names["cmake_find_package"] = "mbedtls"
+        self.cpp_info.components["libembedtls"].names["cmake_find_package_multi"] = "mbedtls"
