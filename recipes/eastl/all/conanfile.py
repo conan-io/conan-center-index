bb5e0efb3664fea9c8d48c1c4ffa8d7c7347afaf
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-22 02:35:48 +0100

    (#8962) eastl: add 3.18.00 + modernize
    * modernize
    
    * bump eabase
    
    * add eastl/3.18.00

--- recipes/eastl/all/conanfile.py
+++ recipes/eastl/all/conanfile.py
@@ -1,6 +1,8 @@
-import os
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
+import os
+
+required_conan_version = ">=1.33.0"
 
 
 class EastlConan(ConanFile):
@@ -8,16 +10,22 @@ class EastlConan(ConanFile):
     description = "EASTL stands for Electronic Arts Standard Template Library. " \
                   "It is an extensive and robust implementation that has an " \
                   "emphasis on high performance."
-    topics = ("conan", "eastl", "stl", "high-performance")
+    topics = ("eastl", "stl", "high-performance")
     license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/electronicarts/EASTL"
-    exports_sources = ["CMakeLists.txt", "patches/**"]
-    generators = "cmake"
-    settings = "os", "compiler", "arch", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
 
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    generators = "cmake"
     _cmake = None
 
     @property
@@ -41,25 +49,33 @@ class EastlConan(ConanFile):
             "apple-clang": "4.3",
         }
 
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
-        if self.settings.compiler.cppstd:
+        if self.options.shared:
+            del self.options.fPIC
+
+    def requirements(self):
+        self.requires("eabase/2.09.06")
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
 
         mininum_compiler_version = self._minimum_compilers_version.get(str(self.settings.compiler))
         if mininum_compiler_version and tools.Version(self.settings.compiler.version) < mininum_compiler_version:
             raise ConanInvalidConfiguration("Compiler is too old for c++ {}".format(self._minimum_cpp_standard))
 
-    def requirements(self):
-        self.requires("eabase/2.09.05")
-
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        folder_name = "EASTL-{}".format(self.version)
-        os.rename(folder_name, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -71,7 +87,7 @@ class EastlConan(ConanFile):
         return self._cmake
 
     def _patch_sources(self):
-        for patch in self.conan_data["patches"][self.version]:
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         tools.replace_path_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
                                    "include(CommonCppFlags)",
@@ -89,10 +105,12 @@ class EastlConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "EASTL"
-        self.cpp_info.names["cmake_find_package_multi"] = "EASTL"
         self.cpp_info.libs = ["EASTL"]
-        if self.settings.os == "Linux":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.append("pthread")
         if self.options.shared:
             self.cpp_info.defines.append("EA_DLL")
+
+        # Do not use these names in set_property, it was a mistake, eastl doesn't export its target
+        self.cpp_info.names["cmake_find_package"] = "EASTL"
+        self.cpp_info.names["cmake_find_package_multi"] = "EASTL"
