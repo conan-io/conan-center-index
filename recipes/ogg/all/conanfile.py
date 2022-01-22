886a93aff8679f1dc97a75c1a8c2840894143e2b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-22 01:09:50 +0100

    modernize

--- recipes/ogg/all/conanfile.py
+++ recipes/ogg/all/conanfile.py
@@ -1,20 +1,28 @@
 from conans import ConanFile, CMake, tools
 import os
 
+required_conan_version = ">=1.43.0"
+
 
 class OggConan(ConanFile):
     name = "ogg"
     description = "The OGG library"
-    topics = ("conan", "ogg", "codec", "audio", "lossless")
+    topics = ("ogg", "codec", "audio", "lossless")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/xiph/ogg"
     license = "BSD-2-Clause"
-    exports_sources = ["CMakeLists.txt", "patches/*"]
-    generators = "cmake"
-    settings = "os", "arch", "build_type", "compiler"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
 
+    settings = "os", "arch", "build_type", "compiler"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    generators = "cmake"
     _cmake = None
 
     @property
@@ -24,6 +32,11 @@ class OggConan(ConanFile):
     @property
     def _build_subfolder(self):
         return "build_subfolder"
+
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -36,9 +49,8 @@ class OggConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -62,8 +74,16 @@ class OggConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "Ogg")
+        self.cpp_info.set_property("cmake_target_name", "Ogg::ogg")
+        self.cpp_info.set_property("pkg_config_name", "ogg")
+        # TODO: back to global scope in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.components["ogglib"].libs = ["ogg"]
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.names["cmake_find_package"] = "Ogg"
         self.cpp_info.names["cmake_find_package_multi"] = "Ogg"
         self.cpp_info.components["ogglib"].names["cmake_find_package"] = "ogg"
         self.cpp_info.components["ogglib"].names["cmake_find_package_multi"] = "ogg"
-        self.cpp_info.components["ogglib"].libs = ["ogg"]
+        self.cpp_info.components["ogglib"].set_property("cmake_target_name", "Ogg::ogg")
+        self.cpp_info.components["ogglib"].set_property("pkg_config_name", "ogg")
