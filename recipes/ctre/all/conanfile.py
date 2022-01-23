6adff277ae7bd8011998444cd316202b16d335d9
toge <toge.mail@gmail.com>
2022-01-24 02:45:38 +0900

    downgrade rquired_conan_version

--- recipes/ctre/all/conanfile.py
+++ recipes/ctre/all/conanfile.py
@@ -2,7 +2,7 @@ import os
 from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.33.0"
 
 class CtreConan(ConanFile):
     name = "ctre"
