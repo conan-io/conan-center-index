8877094439681bf02185a4b74e2529fda01de69c
toge <toge.mail@gmail.com>
2022-01-24 02:43:21 +0900

    ctre: add version 3.5

--- recipes/ctre/all/conanfile.py
+++ recipes/ctre/all/conanfile.py
@@ -2,6 +2,8 @@ import os
 from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 
+required_conan_version = ">=1.43.0"
+
 class CtreConan(ConanFile):
     name = "ctre"
     homepage = "https://github.com/hanickadot/compile-time-regular-expressions"
@@ -12,18 +14,18 @@ class CtreConan(ConanFile):
     no_copy_source = True
     settings = "compiler"
 
-    _source_name = "compile-time-regular-expressions"
-    _source_subfolder = "source_subfolder"
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
 
-    def _validate_compiler_settings(self):
-        # See: https://github.com/hanickadot/compile-time-regular-expressions#supported-compilers
+    def validate(self):
         compiler = self.settings.compiler
         version = tools.Version(self.settings.compiler.version)
 
-        min_gcc = "7.4" if self.version < "3" else "8"
+        min_gcc = "7.4" if tools.Version(self.version) < "3" else "8"
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, "17")
-        if compiler == "Visual Studio" and version < "15":
+        if (compiler == "Visual Studio" or compiler == "msvc") and version < "15":
             raise ConanInvalidConfiguration("ctre doesn't support MSVC < 15")
         elif compiler == "gcc" and version < min_gcc:
             raise ConanInvalidConfiguration("ctre doesn't support gcc < {}".format(min_gcc))
@@ -32,18 +34,13 @@ class CtreConan(ConanFile):
         elif compiler == "apple-clang" and version < "10.0":
             raise ConanInvalidConfiguration("ctre doesn't support Apple clang < 10.0")
 
-    def configure(self):
-         self._validate_compiler_settings()
+    def package_id(self):
+        self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self._source_name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def package(self):
         self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-
-    def package_id(self):
-        self.info.header_only()
 
