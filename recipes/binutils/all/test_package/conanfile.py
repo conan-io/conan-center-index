a8d15100d44137fc19c4e2cdef9ddea9b16dab2b
dvirtz <dvirtz@gmail.com>
2022-01-24 14:54:01 +0200

    remove commented lines

--- recipes/binutils/all/test_package/conanfile.py
+++ recipes/binutils/all/test_package/conanfile.py
@@ -6,11 +6,6 @@ class TestPackageConan(ConanFile):
 
     settings = "os"
 
-    # test_type = "explicit"
-
-    # def build_requirements(self):
-    #     self.tool_requires(self.tested_reference_str)
-
     def test(self):
         if not tools.cross_building(self):
             self.run("as --version")
