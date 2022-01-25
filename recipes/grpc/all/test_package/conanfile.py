35ee36f27b99aa6a3cb5614a45e28c52d7d7db23
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-25 22:48:21 +0100

    handle cross-build in test package

--- recipes/grpc/all/test_package/conanfile.py
+++ recipes/grpc/all/test_package/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+import contextlib
 import os
 
 
@@ -10,28 +11,30 @@ class TestPackageConan(ConanFile):
         if hasattr(self, "settings_build"):
             self.build_requires(str(self.requires["grpc"]))
 
-    @property
-    def _skip_test(self):
-        # TODO: remove in conan v2
+    @contextlib.contextmanager
+    def _buildenv(self):
+        # TODO: conan v2: replace by VirtualBuildEnv and always add grpc to build requirements
+        if tools.cross_building(self):
+            yield
+        else:
+            with tools.run_environment(self):
+                yield
+
+    def build(self):
+        # TODO: always build in conan v2
         # this is a limitation of conan v1:
         # at build time we want to inject PATH/LD_LIBRARY/DYLD_LIBRARY_PATH
         # of build requirements so that gprc_cpp_plugin can find its
         # shared dependencies (in build context as well)
         # should be fixed by using: CMakeToolchain + VirtualBuildEnv
-        return tools.cross_building(self) and self.options["grpc"].shared
-
-    def build(self):
-        if self._skip_test:
+        if tools.cross_building(self) and self.options["grpc"].shared:
             return
-        # TODO: conan v2: replace by VirtualBuildEnv if cross-build or VirtualRunEnv for native build
-        with tools.run_environment(self):
+        with self._buildenv:
             cmake = CMake(self)
             cmake.configure()
             cmake.build()
 
     def test(self):
-        if self._skip_test:
-            return
         if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
