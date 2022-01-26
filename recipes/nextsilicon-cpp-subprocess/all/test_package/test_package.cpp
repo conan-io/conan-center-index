a1e0473bd782692fd7755143367460ae97c81749
Elazar Leibovich <elazarl@gmail.com>
2022-01-26 10:44:12 +0200

    timeout on error

--- recipes/nextsilicon-cpp-subprocess/all/test_package/test_package.cpp
+++ recipes/nextsilicon-cpp-subprocess/all/test_package/test_package.cpp
@@ -1,7 +1,19 @@
+#include <chrono>
 #include <cpp-subprocess/subprocess.hpp>
+#include <iostream>
+#include <thread>
 
 int main() {
   using namespace subprocess;
+  std::mutex m;
+  std::thread t([&m]() {
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    if (m.try_lock()) {
+      std::cerr << "timed out!" << std::endl;
+      exit(2);
+    }
+  });
 #ifdef _WIN32
   auto cmd = Popen({"tasklist", "/?"}, shell{false});
 #else
@@ -9,6 +21,8 @@ int main() {
 #endif
 
   cmd.wait();
+  m.lock();
+  t.join();
 
   return 0;
 }
