9bc691482339864f490d9fc5dda0fcf2b85f8340
toge <toge.mail@gmail.com>
2022-01-26 22:26:14 +0900

    remove patch

--- recipes/wasmedge/all/conanfile.py
+++ recipes/wasmedge/all/conanfile.py
@@ -9,9 +9,11 @@ class WasmedgeConan(ConanFile):
     homepage = "https://github.com/WasmEdge/WasmEdge/"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
-    description = "WasmEdge is a lightweight, high-performance, and extensible WebAssembly runtime for cloud native, edge, and decentralized applications. It powers serverless apps, embedded functions, microservices, smart contracts, and IoT devices."
+    description = ("WasmEdge is a lightweight, high-performance, and extensible WebAssembly runtime"
+                "for cloud native, edge, and decentralized applications."
+                "It powers serverless apps, embedded functions, microservices, smart contracts, and IoT devices.")
     topics = ("webassembly", "wasm", "wasi", "emscripten")
-    settings = "os", "arch", "compiler"
+    settings = "os", "arch", "compiler",
 
     @property
     def _source_subfolder(self):
@@ -21,11 +23,8 @@ class WasmedgeConan(ConanFile):
     def _compiler_alias(self):
         return {
             "Visual Studio": "Visual Studio",
+            "msvc": "Visual Studio",
         }.get(str(self.settings.compiler), "gcc")
-
-    def export_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
 
     def configure(self):
         del self.settings.compiler.libcxx
@@ -42,12 +41,10 @@ class WasmedgeConan(ConanFile):
         self.info.settings.compiler = self._compiler_alias
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version][str(self.settings.os)][str(self.settings.arch)][self._compiler_alias],
+        tools.get(**self.conan_data["sources"][self.version][str(self.settings.os)][str(self.settings.arch)][self._compiler_alias][0],
                   destination=self._source_subfolder, strip_root=True)
-
-    def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        tools.download(filename="LICENSE",
+                       **self.conan_data["sources"][self.version][str(self.settings.os)][str(self.settings.arch)][self._compiler_alias][1])
 
     def package(self):
         self.copy("*.h", src=os.path.join(self._source_subfolder, "include"), dst="include", keep_path=True)
