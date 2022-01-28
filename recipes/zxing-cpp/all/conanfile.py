341a1f67834d8bdd94da5d9c1345d9ec8ea0c1d8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-28 01:03:18 +0100

    modernize

--- recipes/zxing-cpp/all/conanfile.py
+++ recipes/zxing-cpp/all/conanfile.py
@@ -2,15 +2,17 @@ from conans import ConanFile, tools, CMake
 from conans.errors import ConanInvalidConfiguration
 import os
 
+required_conan_version = ">=1.43.0"
+
 
 class ZXingCppConan(ConanFile):
     name = "zxing-cpp"
     homepage = "https://github.com/nu-book/zxing-cpp"
     description = "c++14 port of ZXing, a barcode scanning library"
-    topics = ("conan", "zxing", "barcode", "scanner", "generator")
+    topics = ("zxing", "barcode", "scanner", "generator")
     url = "https://github.com/conan-io/conan-center-index"
     license = "Apache-2.0"
-    exports_sources = ["CMakeLists.txt", "patches/**"]
+
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -24,39 +26,52 @@ class ZXingCppConan(ConanFile):
         "enable_encoders": True,
         "enable_decoders": True,
     }
+
     generators = "cmake"
-
     _cmake = None
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
 
-    _compiler_cpp14_support = {
-        "gcc": "5",
-        "Visual Studio": "14",
-        "clang": "3.4",
-        "apple-clang": "3.4",
-    }
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    @property
+    def _compiler_cpp14_support(self):
+        return {
+            "gcc": "5",
+            "Visual Studio": "14",
+            "clang": "3.4",
+            "apple-clang": "3.4",
+        }
+
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, 14)
-        try:
-            minimum_required_version = self._compiler_cpp14_support[str(self.settings.compiler)]
-            if self.settings.compiler.version < tools.Version(minimum_required_version):
-                raise ConanInvalidConfiguration("This compiler is too old. This library needs a compiler with c++14 support")
-        except KeyError:
-            self.output.warn("This recipe might not support the compiler. Consider adding it.")
-
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
 
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 14)
+        min_version = self._compiler_cpp14_support.get(str(self.settings.compiler))
+        if min_version and tools.Version(self.settings.compiler.version) < min_version:
+            raise ConanInvalidConfiguration(
+                "This compiler is too old. This library needs a compiler with c++14 support"
+            )
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("zxing-cpp-{}".format(self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -67,12 +82,9 @@ class ZXingCppConan(ConanFile):
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
-    def _patch_sources(self):
-        for patch in self.conan_data["patches"][self.version]:
-            tools.patch(**patch)
-
     def build(self):
-        self._patch_sources()
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -85,9 +97,14 @@ class ZXingCppConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "ZXing")
+        self.cpp_info.set_property("cmake_target_name", "ZXing::ZXing")
+        self.cpp_info.set_property("pkg_config_name", "zxing")
+        self.cpp_info.libs = ["ZXingCore"]
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs = ["pthread", "m"]
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.names["cmake_find_package"] = "ZXing"
         self.cpp_info.names["cmake_find_package_multi"] = "ZXing"
         self.cpp_info.names["pkg_config"] = "zxing"
-        self.cpp_info.libs = ["ZXingCore"]
-        if self.settings.os == "Linux":
-            self.cpp_info.system_libs = ["pthread", "m"]
