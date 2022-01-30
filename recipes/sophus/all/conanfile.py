cadc1c321e40b345780ffb42f060ece033a3c8e3
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-30 22:53:23 +0100

    modernize

--- recipes/sophus/all/conanfile.py
+++ recipes/sophus/all/conanfile.py
@@ -1,34 +1,43 @@
 from conans import ConanFile, tools
 import os
 
+required_conan_version = ">=1.43.0"
+
 
 class SophusConan(ConanFile):
     name = "sophus"
     description = "C++ implementation of Lie Groups using Eigen."
-    topics = ("conan", "eigen", "numerical", "math")
+    topics = ("eigen", "numerical", "math")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://strasdat.github.io/Sophus/"
     license = "MIT"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
-
-    requires = "eigen/3.3.7"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name.capitalize() + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
-
-    def package(self):
-        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="*.hpp", src=os.path.join(self._source_subfolder, "sophus"), dst=os.path.join("include", "sophus"), keep_path=False)
+    def requirements(self):
+        self.requires("eigen/3.3.7")
 
     def package_id(self):
         self.info.header_only()
 
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    def package(self):
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*.hpp",
+                  src=os.path.join(self._source_subfolder, "sophus"),
+                  dst=os.path.join("include", "sophus"))
+
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "Sophus")
+        self.cpp_info.set_property("cmake_target_name", "Sophus::Sophus")
+
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.names["cmake_find_package"] = "Sophus"
         self.cpp_info.names["cmake_find_package_multi"] = "Sophus"
