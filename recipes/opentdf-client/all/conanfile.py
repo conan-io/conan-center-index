8e398cfcf7eb4686c15e357a7f9d1375974dc49a
Pat Mancuso <pmancuso@virtru.com>
2022-01-31 11:00:58 -0500

    Fix GSL version in conanfile

--- recipes/opentdf-client/all/conanfile.py
+++ recipes/opentdf-client/all/conanfile.py
@@ -32,7 +32,7 @@ class OpenTDFConan(ConanFile):
         self.requires("boost/1.76.0@")
         self.requires("libiconv/1.16@")
         self.requires("zlib/1.2.11@")
-        self.requires("gsl_microsoft/20180102@bincrafters/stable")
+        self.requires("ms-gsl/2.1.0@")
         self.requires("libxml2/2.9.10@")
         self.requires("libarchive/3.5.1@")
         self.requires("nlohmann_json/3.10.4@")
