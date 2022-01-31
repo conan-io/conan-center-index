aa3be2878d68b12274a5cc9bb37b0bd06c4461a3
Pat Mancuso <pmancuso@virtru.com>
2022-01-31 13:01:16 -0500

    Specify destination directory in source fetch

--- recipes/opentdf-client/all/conanfile.py
+++ recipes/opentdf-client/all/conanfile.py
@@ -44,7 +44,7 @@ class OpenTDFConan(ConanFile):
 
     def source(self):
         #self.run("git clone git@github.com:opentdf/client-cpp.git --depth 1 --branch " + self.version + " " + self._source_subfolder)
-        tools.get(**self.conan_data["sources"][self.version])
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
