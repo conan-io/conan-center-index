6b14f040dea059a7b4499397f9cf9d81f303f2e8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-31 23:11:29 +0100

    cleanup test package

--- recipes/openssl/1.x.x/test_package/conanfile.py
+++ recipes/openssl/1.x.x/test_package/conanfile.py
@@ -2,13 +2,12 @@ from conans import CMake, tools, ConanFile
 import os
 
 
-class DefaultNameConan(ConanFile):
-    settings = "os", "compiler", "arch", "build_type"
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package"
 
-    def _build_cmake(self, use_find_package):
+    def build(self):
         cmake = CMake(self)
-
         if self.settings.os == "Android":
             cmake.definitions["CONAN_LIBCXX"] = ""
         openssl_version = tools.Version(self.deps_cpp_info["openssl"].version)
@@ -16,18 +15,8 @@ class DefaultNameConan(ConanFile):
             cmake.definitions["OPENSSL_WITH_ZLIB"] = False
         else:
             cmake.definitions["OPENSSL_WITH_ZLIB"] = not self.options["openssl"].no_zlib
-        cmake.definitions["USE_FIND_PACKAGE"] = use_find_package
-        cmake.definitions["OPENSSL_ROOT_DIR"] = self.deps_cpp_info["openssl"].rootpath
-        cmake.definitions["OPENSSL_USE_STATIC_LIBS"] = not self.options["openssl"].shared
-        if self.settings.compiler == 'Visual Studio':
-            cmake.definitions["OPENSSL_MSVC_STATIC_RT"] = 'MT' in str(self.settings.compiler.runtime)
-
         cmake.configure()
         cmake.build()
-
-    def build(self):
-        self._build_cmake(use_find_package=True)
-        self._build_cmake(use_find_package=False)
 
     def test(self):
         if not tools.cross_building(self):
