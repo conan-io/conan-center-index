94f8645994a1e2f930715bf5cb794a8ed93e082e
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-02-01 11:15:27 +0100

    linter: fixup packages with single cci.* version

--- .github/runlint.py
+++ .github/runlint.py
@@ -31,22 +31,23 @@ def main(pr):
         if line.startswith("+++ b/recipes/") or line.startswith("--- a/recipes/"):
             packages.add(line.split("/")[2])
     for package in packages:
-        version = packaging.version.Version("0.0.0")
+        version = None
         folder = ""
         with open(os.path.join("recipes", package, "config.yml"), "r") as file:
             config = yaml.safe_load(file)
             for v in config["versions"]:
                 try:
                     tmpVer = packaging.version.Version(v)
-                    if tmpVer > version:
+                    if not version or tmpVer > version:
                         version = tmpVer
                         folder = config["versions"][v]["folder"]
                 except packaging.version.InvalidVersion:
                     print("Error parsing version %s for package %s in pr %s" % (v, package, pr))
 
-        shell = bool(platform.system() != "Windows")
-        command = "conan export %s %s/%s@" % (os.path.join("recipes", package, folder), package, version)
-        p = subprocess.run(command, shell=shell, check=True)
+        if version:
+            shell = bool(platform.system() != "Windows")
+            command = "conan export %s %s/%s@" % (os.path.join("recipes", package, folder), package, version)
+            p = subprocess.run(command, shell=shell, check=True)
 
 if __name__ == "__main__":
     # execute only if run as a script
