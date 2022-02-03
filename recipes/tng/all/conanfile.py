88cba3f79c6e30e778aeb772beaaecce1b3c98fa
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-02-03 15:06:54 +0100

    (#9153) tng: modernize
    * modernize
    
    * relocatable shared lib on macOS

--- recipes/tng/all/conanfile.py
+++ recipes/tng/all/conanfile.py
@@ -1,31 +1,28 @@
 from conans import ConanFile, tools, CMake
 import os
-import glob
+
+required_conan_version = ">=1.43.0"
 
 
 class tngConan(ConanFile):
     name = "tng"
     description = "External GROMACS library for loading tng files."
     license = "BSD-3-Clause"
-    topics = ("conan", "tng", "gromacs")
+    topics = ("tng", "gromacs")
     homepage = "https://gitlab.com/gromacs/tng/"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = ["CMakeLists.txt", "patches/*"]
-    generators = "cmake"
+
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
-        "fPIC": [True, False]
+        "fPIC": [True, False],
     }
     default_options = {
         "shared": False,
-        "fPIC": True
+        "fPIC": True,
     }
 
-    requires = (
-        "zlib/1.2.11"
-    )
-
+    generators = "cmake"
     _cmake = None
 
     @property
@@ -35,6 +32,11 @@ class tngConan(ConanFile):
     @property
     def _build_subfolder(self):
         return "build_subfolder"
+
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -46,14 +48,15 @@ class tngConan(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
+    def requirements(self):
+        self.requires("zlib/1.2.11")
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        url = self.conan_data["sources"][self.version]["url"]
-        extracted_dir = self.name + "-v" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def build(self):
-        for patch in self.conan_data["patches"][self.version]:
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
@@ -75,8 +78,12 @@ class tngConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "tng_io")
+        self.cpp_info.set_property("cmake_target_name", "tng_io::tng_io")
         self.cpp_info.libs = ["tng_io"]
         if self.settings.os in ("Linux", "FreeBSD"):
             self.cpp_info.system_libs = ["m"]
+
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.names["cmake_find_package"] = "tng_io"
         self.cpp_info.names["cmake_find_package_multi"] = "tng_io"
