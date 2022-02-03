3d249b32fa92f2065e0a96df2006f5a82fccdbf8
Viktor Arvidsson <Cyriuz@users.noreply.github.com>
2022-02-03 23:57:00 +0100

    Qt6: Added option to disable arbitrary qt features.
    Like the "config" option did for Qt5.

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -91,6 +91,7 @@ class QtConan(ConanFile):
         "cross_compile": "ANY",
         "sysroot": "ANY",
         "multiconfiguration": [True, False],
+        "disabled_features": "ANY",
     }
     options.update({module: [True, False] for module in _submodules})
 
@@ -130,6 +131,7 @@ class QtConan(ConanFile):
         "cross_compile": None,
         "sysroot": None,
         "multiconfiguration": False,
+        "disabled_features": "",
     }
     default_options.update({module: False for module in _submodules})
 
@@ -591,6 +593,9 @@ class QtConan(ConanFile):
             else:
                 self._cmake.definitions["FEATURE_%s" % conf_arg] = "OFF"
                 self._cmake.definitions["FEATURE_system_%s" % feature] = "OFF"
+
+        for feature in str(self.options.disabled_features).split(" "):
+            self._cmake.definitions["FEATURE_%s" % feature] = "OFF"
 
         if self.settings.os == "Macos":
             self._cmake.definitions["FEATURE_framework"] = "OFF"
