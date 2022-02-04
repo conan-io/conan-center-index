dea5360f22bfc025d6bf1616028dc9bc312a299a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-02-05 00:44:56 +0100

    relocatable shared libs on macOS

--- recipes/libiconv/all/conanfile.py
+++ recipes/libiconv/all/conanfile.py
@@ -128,6 +128,10 @@ class LibiconvConan(ConanFile):
     def _patch_sources(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
+        # relocatable shared libs on macOS
+        for configure in ["configure", os.path.join("libcharset", "configure")]:
+            tools.replace_in_file(os.path.join(self._source_subfolder, configure),
+                                  "-install_name \\$rpath/", "-install_name @rpath/")
 
     def build(self):
         self._patch_sources()
