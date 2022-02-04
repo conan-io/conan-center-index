aec26a1f943bdcb05118f743200465bdc9d92bfc
Chris Janaqi <chris.janaqi@xlnaudio.com>
2022-02-04 12:41:40 +0100

    Add support for the ICU extension for the sqlite3 recipe

--- recipes/sqlite3/all/conanfile.py
+++ recipes/sqlite3/all/conanfile.py
@@ -26,6 +26,7 @@ class Sqlite3Conan(ConanFile):
         "enable_fts3_parenthesis": [True, False],
         "enable_fts4": [True, False],
         "enable_fts5": [True, False],
+        "enable_icu": [True, False],
         "enable_json1": [True, False],
         "enable_soundex": [True, False],
         "enable_preupdate_hook": [True, False],
@@ -51,6 +52,7 @@ class Sqlite3Conan(ConanFile):
         "enable_fts3_parenthesis": False,
         "enable_fts4": False,
         "enable_fts5": False,
+        "enable_icu": False,
         "enable_json1": False,
         "enable_soundex": False,
         "enable_preupdate_hook": False,
@@ -112,6 +114,7 @@ class Sqlite3Conan(ConanFile):
         self._cmake.definitions["ENABLE_FTS3_PARENTHESIS"] = self.options.enable_fts3_parenthesis
         self._cmake.definitions["ENABLE_FTS4"] = self.options.enable_fts4
         self._cmake.definitions["ENABLE_FTS5"] = self.options.enable_fts5
+        self._cmake.definitions["ENABLE_ICU"] = self.options.enable_icu
         self._cmake.definitions["ENABLE_JSON1"] = self.options.enable_json1
         self._cmake.definitions["ENABLE_PREUPDATE_HOOK"] = self.options.enable_preupdate_hook
         self._cmake.definitions["ENABLE_SOUNDEX"] = self.options.enable_soundex
@@ -167,6 +170,10 @@ class Sqlite3Conan(ConanFile):
     def _module_file_rel_path(self):
         return os.path.join("lib", "cmake", "conan-official-{}-variables.cmake".format(self.name))
 
+    def requirements(self):
+        if self.options.enable_icu:
+            self.requires("icu/70.1")
+
     def package_info(self):
         self.cpp_info.set_property("cmake_find_mode", "both")
         self.cpp_info.set_property("cmake_file_name", "SQLite3")
@@ -175,6 +182,8 @@ class Sqlite3Conan(ConanFile):
 
         # TODO: back to global scope in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.components["sqlite"].libs = ["sqlite3"]
+        if self.options.enable_icu:
+            self.cpp_info.components["sqlite"].requires = ["icu::icu"]
         if self.options.omit_load_extension:
             self.cpp_info.components["sqlite"].defines.append("SQLITE_OMIT_LOAD_EXTENSION")
         if self.settings.os in ["Linux", "FreeBSD"]:
