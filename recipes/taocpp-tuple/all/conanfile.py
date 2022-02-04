6fa69d23cfefa5681163dca20c68fe5923489597
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-02-04 17:27:03 +0100

    (#9173) taocpp-tuple: modernize

--- recipes/taocpp-tuple/all/conanfile.py
+++ recipes/taocpp-tuple/all/conanfile.py
@@ -1,7 +1,8 @@
-import os
 from conans import ConanFile, tools
+import os
 
-required_conan_version = ">=1.28.0"
+required_conan_version = ">=1.43.0"
+
 
 class TaoCPPTupleConan(ConanFile):
     name = "taocpp-tuple"
@@ -11,32 +12,36 @@ class TaoCPPTupleConan(ConanFile):
     description = "Compile-time-efficient proof-of-concept implementation for std::tuple"
     topics = ("template", "cpp11", "tuple")
     no_copy_source = True
-    settings = "compiler"
+    settings = "os", "arch", "compiler", "build_type"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def configure(self):
-        if self.settings.compiler.cppstd:
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, 11)
 
     def package_id(self):
         self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "tuple-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy("*", dst="include", src=os.path.join(self._source_subfolder, "include"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "taocpp-tuple")
+        self.cpp_info.set_property("cmake_target_name", "taocpp::tuple")
+
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "taocpp-tuple"
         self.cpp_info.filenames["cmake_find_package_multi"] = "taocpp-tuple"
         self.cpp_info.names["cmake_find_package"] = "taocpp"
         self.cpp_info.names["cmake_find_package_multi"] = "taocpp"
         self.cpp_info.components["_taocpp-tuple"].names["cmake_find_package"] = "tuple"
         self.cpp_info.components["_taocpp-tuple"].names["cmake_find_package_multi"] = "tuple"
+        self.cpp_info.components["_taocpp-tuple"].set_property("cmake_target_name", "taocpp::tuple")
