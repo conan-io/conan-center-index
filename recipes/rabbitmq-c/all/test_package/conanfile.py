68b4a23ffad3f794b2b5c6194dfa199efb6f0f40
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-02-06 13:06:48 +0100

    (#9262) rabbitmq-c modernize
    * modernize
    
    * bump openssl
    
    * relocatable shared lib on macOS

--- recipes/rabbitmq-c/all/test_package/conanfile.py
+++ recipes/rabbitmq-c/all/test_package/conanfile.py
@@ -1,10 +1,9 @@
-import os
-
 from conans import CMake, ConanFile, tools
+import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
@@ -15,6 +14,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
