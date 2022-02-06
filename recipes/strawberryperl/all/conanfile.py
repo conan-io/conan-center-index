d3d4f2bbfc92cde6990369cf79c8d30776e507d4
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-02-06 15:33:55 +0100

    strawberryperl: do not publish includedirs

--- recipes/strawberryperl/all/conanfile.py
+++ recipes/strawberryperl/all/conanfile.py
@@ -35,6 +35,7 @@ class StrawberryperlConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libdirs = []
+        self.cpp_info.includedirs = []
 
         bin_path = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH environment variable: %s" % bin_path)
