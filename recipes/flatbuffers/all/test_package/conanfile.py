c7f36d187c1d1da5e615131d8289eea71f70d263
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-02-06 22:34:13 +0100

    refactoring

--- recipes/flatbuffers/all/test_package/conanfile.py
+++ recipes/flatbuffers/all/test_package/conanfile.py
@@ -3,21 +3,18 @@ import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
-        if self.options["flatbuffers"].flatbuffers:
-            cmake = CMake(self)
-            cmake.definitions["FLATBUFFERS_HEADER_ONLY"] = self.options["flatbuffers"].header_only
-            cmake.definitions["FLATBUFFERS_SHARED"] = not self.options["flatbuffers"].header_only and self.options["flatbuffers"].shared
-            cmake.configure()
-            cmake.build()
+        cmake = CMake(self)
+        cmake.definitions["FLATBUFFERS_HEADER_ONLY"] = self.options["flatbuffers"].header_only
+        cmake.configure()
+        cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            if self.options["flatbuffers"].flatbuffers:
-                bin_path = os.path.join("bin", "test_package")
-                self.run(bin_path, run_environment=True)
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
             if self.options["flatbuffers"].flatc:
                 self.run("flatc --version", run_environment=True)
