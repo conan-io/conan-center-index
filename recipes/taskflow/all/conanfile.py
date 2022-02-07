42e354d05b0c777f80a257bb7257ddcfb50c86b1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-02-07 09:25:49 +0100

    (#9171) taskflow: fix CMake config name for CMakeDeps + modernize
    * fix cmake config name CMakeDeps
    
    * modernize

--- recipes/taskflow/all/conanfile.py
+++ recipes/taskflow/all/conanfile.py
@@ -1,35 +1,40 @@
-import os
-
 from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
+import os
 
 required_conan_version = ">=1.43.0"
 
+
 class TaskflowConan(ConanFile):
     name = "taskflow"
-    description = "A fast C++ header-only library to help you quickly write parallel programs with complex task dependencies."
+    description = (
+        "A fast C++ header-only library to help you quickly write parallel "
+        "programs with complex task dependencies."
+    )
     topics = ("taskflow", "tasking", "parallelism")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/taskflow/taskflow"
     license = "MIT"
 
-    settings = "os", "compiler"
+    settings = "os", "arch", "compiler", "build_type"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def export_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+    @property
+    def _is_msvc(self):
+        return str(self.settings.compiler) in ["Visual Studio", "msvc"]
 
-    def validate(self):
-        minimal_cpp_standard = "17" if tools.Version(self.version) <= "2.2.0" or tools.Version(self.version) >= "3.0.0"  else "14"
+    @property
+    def _min_cppstd(self):
+        if tools.Version(self.version) <= "2.2.0" or tools.Version(self.version) >= "3.0.0":
+            return "17"
+        return "14"
 
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, minimal_cpp_standard)
-
-        minimal_version = {
+    @property
+    def _minimum_compiler_version(self):
+        return {
             "17": {
                 "Visual Studio": "16",
                 "gcc": "7.3",
@@ -41,19 +46,27 @@ class TaskflowConan(ConanFile):
                 "gcc": "5",
                 "clang": "4.0",
                 "apple-clang": "8.0"
-            }
-        }[minimal_cpp_standard]
+            },
+        }[self._min_cppstd]
 
-        compiler = str(self.settings.compiler)
-        if compiler not in minimal_version:
-            self.output.warn(
-                "%s recipe lacks information about the %s compiler standard version support" % (self.name, compiler))
-            self.output.warn(
-                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
-            return
-        version = tools.Version(self.settings.compiler.version)
-        if version < minimal_version[compiler]:
-            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+    def export_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._min_cppstd)
+
+        min_version = self._minimum_compiler_version.get(str(self.settings.compiler))
+        if min_version and tools.Version(self.settings.compiler.version) < min_version:
+            raise ConanInvalidConfiguration(
+                "{} requires a compiler that supports at least C++{}".format(
+                    self.name, self._min_cppstd,
+                )
+            )
+
+    def package_id(self):
+        self.info.header_only()
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
@@ -61,21 +74,22 @@ class TaskflowConan(ConanFile):
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            print(patch)
             tools.patch(**patch)
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="*", dst=os.path.join("include", "taskflow"), src=os.path.join(self._source_subfolder, "taskflow"))
-
-    def package_id(self):
-        self.info.header_only()
+        self.copy(pattern="*",
+                  dst=os.path.join("include", "taskflow"),
+                  src=os.path.join(self._source_subfolder, "taskflow"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "Taskflow")
+        self.cpp_info.set_property("cmake_target_name", "Taskflow::Taskflow")
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.append("pthread")
-        if self.settings.compiler == "Visual Studio":
+        if self._is_msvc:
             self.cpp_info.defines.append("_ENABLE_EXTENDED_ALIGNED_STORAGE")
+
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.names["cmake_find_package"] = "Taskflow"
         self.cpp_info.names["cmake_find_package_multi"] = "Taskflow"
-        self.cpp_info.set_property("cmake_target_name", "Taskflow::Taskflow")
