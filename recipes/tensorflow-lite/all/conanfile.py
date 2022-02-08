96dcf00f38c5cf234c04c59b7ac237369d39affd
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2022-02-08 11:48:19 -0800

    [tensorflow-lite] Removed restriction of `ruy` to shared when building `tensorflow-lite` as shared library

--- recipes/tensorflow-lite/all/conanfile.py
+++ recipes/tensorflow-lite/all/conanfile.py
@@ -31,7 +31,7 @@ class TensorflowLiteConan(ConanFile):
         "with_ruy": False,
         "with_nnapi": False,
         "with_mmap": True,
-        "with_xnnpack": True,
+        "with_xnnpack": True
     }
 
 
@@ -71,8 +71,6 @@ class TensorflowLiteConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
-            if self.settings.os == "Linux":
-                self.options["ruy"].shared = True
 
     def requirements(self):
         self.requires("abseil/20211102.0")
@@ -103,10 +101,6 @@ class TensorflowLiteConan(ConanFile):
             self.output.warn(f"{self.name} requires C++14. Your compiler is unknown. Assuming it supports C++14.")
         elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
             raise ConanInvalidConfiguration(f"{self.name} requires C++14, which your compiler does not support.")
-        if self.options.shared:
-            if self.settings.os == "Linux" and not self.options["ruy"].shared:
-                raise ConanInvalidConfiguration(
-                        f"The project {self.name}/{self.version} with shared=True on Linux requires ruy:shared=True")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
