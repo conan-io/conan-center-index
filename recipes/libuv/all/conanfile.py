5d1e3bdade644f1a051d448552b15614196991f7
toge <toge.mail@gmail.com>
2022-02-10 09:16:46 +0900

    add license file

--- recipes/libuv/all/conanfile.py
+++ recipes/libuv/all/conanfile.py
@@ -70,6 +70,8 @@ class libuvConan(ConanFile):
         cmake.build()
 
     def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy("LICENSE-docs", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
