b8573f4760acd5730604091e1d45b52873f46beb
Luis <lasote@gmail.com>
2022-02-11 09:35:53 +0100

    pybind11 tested with cmakedeps

--- recipes/pybind11/all/conanfile.py
+++ recipes/pybind11/all/conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, tools, CMake
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.43.0"
 
 
 class PyBind11Conan(ConanFile):
@@ -66,6 +66,7 @@ class PyBind11Conan(ConanFile):
             self.cpp_info.components["main"].builddirs = [cmake_base_path]
             for generator in ["cmake_find_package", "cmake_find_package_multi"]:
                 self.cpp_info.components["main"].build_modules[generator].append(os.path.join(cmake_base_path, "pybind11Common.cmake"))
+            self.cpp_info.set_property("cmake_build_modules", [os.path.join("lib", "cmake", "pybind11", "pybind11Common.cmake")])
             self.cpp_info.components["headers"].includedirs = [os.path.join("include", "pybind11")]
             self.cpp_info.components["headers"].requires = ["main"]
             self.cpp_info.components["embed"].requires = ["main"]
---
+++ recipes/pybind11/all/test_package_cmakedeps/conanfile.py
@@ -0,0 +1,35 @@
+from conans import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.env import Environment
+from conan.tools.build.cross_building import cross_building
+import os
+import sys
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def generate(self):
+        toolchain = CMakeToolchain(self)
+        toolchain.variables["PYTHON_EXECUTABLE"] = self._python_interpreter
+        toolchain.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    @property
+    def _python_interpreter(self):
+        if getattr(sys, "frozen", False):
+            return "python"
+        return sys.executable
+
+    def test(self):
+        if not cross_building(self):
+            env = Environment()
+            env.define("PYTHONPATH", os.getcwd())
+            env.vars(self).save_script("launcher")
+            test_path = os.path.join(self.source_folder, "test.py")
+            self.run("{} {}".format(self._python_interpreter, test_path), env="launcher")
