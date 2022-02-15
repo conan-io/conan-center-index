0aa099f8bad82ba8c52712bf18ea3acf6bbb6057
Pat Mancuso <46453397+patmantru@users.noreply.github.com>
2022-02-15 06:45:24 -0500

    (#9119) opentdf-client: Publish opentdf client cpp recipe
    * Add openTDF/client-cpp
    
    * Make package naming consistent
    
    * Update current release to 0.2.12 to fix conan-center build fail
    
    * Add 0.2.12 placeholder to conandata.yml
    
    * Improve placeholder
    
    * Fix GSL version in conanfile
    
    * Revert to zipfile-based source fetch
    
    * Add options
    
    * Specify destination directory in source fetch
    
    * Fix package reference to GSL
    
    * Add compiler min version checks
    
    * Import exception to avoid error
    
    * Update recipes/opentdf-client/all/CMakeLists.txt
    
    Code review feedback
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/opentdf-client/all/conanfile.py
    
    Code review feedback
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/opentdf-client/all/conanfile.py
    
    Code review feedback
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/opentdf-client/all/conanfile.py
    
    Code review feedback
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/opentdf-client/all/conanfile.py
    
    Code review feedback
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/opentdf-client/all/test_package/conanfile.py
    
    Code review feedback
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/opentdf-client/all/test_package/conanfile.py
    
    Code review feedback
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/opentdf-client/all/test_package/test_package.cpp
    
    Code review feedback
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/opentdf-client/all/test_package/CMakeLists.txt
    
    Code review feedback
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Fixes after incorporating code review changes
    
    * Fixes after incorporating code review changes
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

---
+++ recipes/opentdf-client/all/conanfile.py
@@ -0,0 +1,99 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+required_conan_version = ">=1.40.0"
+
+
+class OpenTDFConan(ConanFile):
+    name = "opentdf-client"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://www.virtru.com"
+    topics = ("opentdf", "opentdf-client", "tdf", "virtru")
+    description = "openTDF core c++ client library for creating and accessing TDF protected data"
+    license = "MIT"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"build_python": [True, False], "fPIC": [True, False]}
+    default_options = {"build_python": False, "fPIC": True}
+    exports_sources = ["CMakeLists.txt"]
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    @property
+    def _minimum_cpp_standard(self):
+        return 14
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "17",
+            "gcc": "9",
+            "clang": "12",
+            "apple-clang": "12.0.0",
+        }
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
+                self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
+
+    def requirements(self):
+        self.requires("openssl/1.1.1l@")
+        self.requires("boost/1.76.0@")
+        self.requires("libiconv/1.16@")
+        self.requires("zlib/1.2.11@")
+        self.requires("ms-gsl/2.1.0@")
+        self.requires("libxml2/2.9.10@")
+        self.requires("libarchive/3.5.1@")
+        self.requires("nlohmann_json/3.10.4@")
+        self.requires("jwt-cpp/0.4.0@")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+        self.copy("*", dst="lib", src=os.path.join(self._source_subfolder,"tdf-lib-cpp/lib"))
+        self.copy("*", dst="include", src=os.path.join(self._source_subfolder,"tdf-lib-cpp/include"))
+        self.copy("LICENSE", dst="licenses", src=os.path.join(self._source_subfolder,"tdf-lib-cpp"))
+
+    # TODO - this only advertises the static lib, add dynamic lib also
+    def package_info(self):
+        self.cpp_info.components["libopentdf"].libs = ["opentdf_static"]
+        self.cpp_info.components["libopentdf"].names["cmake_find_package"] = "opentdf-client"
+        self.cpp_info.components["libopentdf"].names["cmake_find_package_multi"] = "opentdf-client"
+        self.cpp_info.components["libopentdf"].names["pkg_config"] = "opentdf-client"
+        self.cpp_info.components["libopentdf"].requires = ["openssl::openssl", "boost::boost", "libiconv::libiconv", "zlib::zlib", "ms-gsl::ms-gsl", "libxml2::libxml2", "libarchive::libarchive", "jwt-cpp::jwt-cpp", "nlohmann_json::nlohmann_json"]
+
