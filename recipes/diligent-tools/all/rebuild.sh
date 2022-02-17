f5c1a830c43192613c6aca6095eb3ea0dd282882
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2022-02-17 12:24:19 +0100

    remove unneeded file

--- recipes/diligent-tools/all/rebuild.sh
+++
@@ -1,4 +0,0 @@
-conan source  . --source-folder=sources && \
-conan install . --install-folder=build --build missing && \
-conan build   . --build-folder=build && \
-conan package . --build-folder=build --package-folder=bin
