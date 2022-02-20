dd88009ea14177b62f7fbd5cf1b445b3abadb800
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-02-20 19:17:28 +0100

    add bigint/2010.04.30

---
+++ recipes/bigint/all/conanfile.py
@@ -0,0 +1,67 @@
+from conans import ConanFile, CMake, tools
+import functools
+
+required_conan_version = ">=1.33.0"
+
+
+class BigintConan(ConanFile):
+    name = "bigint"
+    description = "C++ Big Integer Library"
+    license = "Unlicense"
+    topics = ("bigint", "integer")
+    homepage = "https://mattmccutchen.net/bigint"
+    url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    generators = "cmake"
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    @functools.lru_cache(1)
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("README", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["bigint"]
