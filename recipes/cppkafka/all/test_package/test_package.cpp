15ca4bb8b2fb5c6cfe2856fdd12de228f58f4aed
toge <toge.mail@gmail.com>
2022-02-22 02:10:01 +0900

    uncomment test_package

--- recipes/cppkafka/all/test_package/test_package.cpp
+++ recipes/cppkafka/all/test_package/test_package.cpp
@@ -2,34 +2,27 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
 using namespace cppkafka;
 
 int main() {
     // Create the config
-
-	/*
     try {
-	    Configuration kafkaConfig;
-	    kafkaConfig = {
-            {"metadata.broker.list", "68.169.103.40:9092"},
-            {"group.id",             "xxx"},
-            //{"auto.offset.reset", "earliest"},
-            // Disable auto commit
-            {"enable.auto.commit",   false}
-    };
-	        // Create the producer
-    Producer producer(kafkaConfig);
+        Configuration kafkaConfig = {
+            {"metadata.broker.list", "127.0.0.1:9092"},
+            {"group.id", "xxx"},
+            {"auto.offset.reset", "earliest"},
+            {"enable.auto.commit", false}
+        };
+        // Create the producer
+        Producer producer(kafkaConfig);
 
-    // Produce a message!
-    string message = "hey there!";
-    producer.produce(MessageBuilder("my_topic").partition(0).payload(message));
-    producer.flush();
-
-    }
-    catch (std::exception &e) {
-	cout << e.what() << endl;
+        // Produce a message!
+        std::string message = "hey there!";
+        producer.produce(MessageBuilder("my_topic").partition(0).payload(message));
+        producer.flush();
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
     }
 
-    */
+    return 0;
 }
