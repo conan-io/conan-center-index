7fc4c35aeeb86bfeb279fe2534880ade5706dbac
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2022-02-21 09:56:52 +0100

    update test

--- recipes/diligent-tools/all/test_package/test_package.cpp
+++ recipes/diligent-tools/all/test_package/test_package.cpp
@@ -1,6 +1,8 @@
-#include "Common/interface/RefCntAutoPtr.hpp"
+#include <Common/interface/RefCntAutoPtr.hpp>
+#include <DiligentTools/AssetLoader/interface/GLTFLoader.hpp>
 
 int main()
 {
+  Diligent::GLTF::Material material;
   return 0;
 }
