c0d94a8b358dbb5413c62347161d7894a172bbea
toge <toge.mail@gmail.com>
2022-02-22 02:23:52 +0900

    try to fix compilation error on windows

--- recipes/blend2d/all/conanfile.py
+++ recipes/blend2d/all/conanfile.py
@@ -43,8 +43,7 @@ class Blend2dConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
-        if self.settings.os == "Linux":
-            self.options["asmjit"].shared = self.options.shared
+        self.options["asmjit"].shared = self.options.shared
 
     def requirements(self):
         self.requires("asmjit/cci.20220210")
