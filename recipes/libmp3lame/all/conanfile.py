a0792c0677f6f981db323b9c9182250418196a5e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-02-21 17:24:23 +0100

    relocatable shared lib on macOS

--- recipes/libmp3lame/all/conanfile.py
+++ recipes/libmp3lame/all/conanfile.py
@@ -111,6 +111,9 @@ class LibMP3LameConan(ConanFile):
                     os.path.join(self._source_subfolder, "config.sub"))
         shutil.copy(self._user_info_build["gnu-config"].CONFIG_GUESS,
                     os.path.join(self._source_subfolder, "config.guess"))
+        tools.replace_in_file(os.path.join(self._source_subfolder, "configure"),
+                              "-install_name \\$rpath/",
+                              "-install_name @rpath/")
         autotools = self._configure_autotools()
         autotools.make()
 
