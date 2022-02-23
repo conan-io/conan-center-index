be35734f0578d25c1191326f4cfc7e7c90582275
Zvicii <zouyiqi@corp.netease.com>
2022-02-23 12:37:45 +0800

    fix: test build error under macos clang

--- recipes/xpack/all/test_package/test_package.cpp
+++ recipes/xpack/all/test_package/test_package.cpp
@@ -4,10 +4,10 @@
 using namespace rapidjson;
 
 int main() {
-    int number{1};
+    int number = 1;
     std::string xpack_json = xpack::json::encode(number);
     std::cout << xpack_json << std::endl;
-    int number2{0};
+    int number2 = 0;
     xpack::json::decode(xpack_json, number2);
     std::cout << number2 << std::endl;
     return 0;
