6c1b4fe6462c03918db42f35cb756433270f0eae
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-02-24 22:02:05 +0100

    use lru_cache

--- recipes/leveldb/all/conanfile.py
+++ recipes/leveldb/all/conanfile.py
@@ -1,4 +1,5 @@
 from conans import CMake, ConanFile, tools
+import functools
 import os
 
 required_conan_version = ">=1.43.0"
@@ -30,7 +31,6 @@ class LevelDBCppConan(ConanFile):
     }
 
     generators = "cmake", "cmake_find_package_multi"
-    _cmake = None
 
     @property
     def _source_subfolder(self):
@@ -66,16 +66,15 @@ class LevelDBCppConan(ConanFile):
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
+    @functools.lru_cache(1)
     def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["LEVELDB_BUILD_TESTS"] = False
-        self._cmake.definitions["LEVELDB_BUILD_BENCHMARKS"] = False
-        self._cmake.definitions["LEVELDB_WITH_SNAPPY"] = self.options.with_snappy
-        self._cmake.definitions["LEVELDB_WITH_CRC32C"] = self.options.with_crc32c
-        self._cmake.configure()
-        return self._cmake
+        cmake = CMake(self)
+        cmake.definitions["LEVELDB_BUILD_TESTS"] = False
+        cmake.definitions["LEVELDB_BUILD_BENCHMARKS"] = False
+        cmake.definitions["LEVELDB_WITH_SNAPPY"] = self.options.with_snappy
+        cmake.definitions["LEVELDB_WITH_CRC32C"] = self.options.with_crc32c
+        cmake.configure()
+        return cmake
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
