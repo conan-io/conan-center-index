64c3f237e30d6fef8fb2ef9f7dd85c5b7af3d638
Enhex <enhex0@gmail.com>
2022-02-28 07:28:34 +0200

    try tp fix conan's CI using old GCC version

--- recipes/strong_type/all/conanfile.py
+++ recipes/strong_type/all/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 import os
 
 class StrongtypeConan(ConanFile):
@@ -9,10 +10,33 @@ class StrongtypeConan(ConanFile):
     homepage = "https://github.com/Enhex/strong_type"
     url = "https://github.com/conan-io/conan-center-index"
     no_copy_source = True
+    settings = ("compiler")
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 17)
+
+        minimal_version = {
+            "Visual Studio": "15",
+            "gcc": "7",
+            "clang": "5.0",
+            "apple-clang": "9.1"
+        }
+        compiler = str(self.settings.compiler)
+        compiler_version = tools.Version(self.settings.compiler.version)
+
+        if compiler not in minimal_version:
+            self.output.info("{} requires a compiler that supports at least C++17".format(self.name, min))
+            return
+
+        # Exclude compilers not supported
+        if compiler_version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration("{} requires a compiler that supports at least C++17. {} {} is not".format(
+                self.name, compiler, tools.Version(self.settings.compiler.version.value)))
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
