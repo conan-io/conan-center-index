0510fb54b70bfe37f8e96152067419c40ea222bf
Enhex <enhex0@gmail.com>
2022-03-01 20:10:29 +0200

    fix review code

--- recipes/strong_type/all/test_package/conanfile.py
+++ recipes/strong_type/all/test_package/conanfile.py
@@ -16,5 +16,5 @@ class StrongTypeTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
+            bin_path = os.path.join("bin", "example")
             self.run(bin_path, run_environment=True)
