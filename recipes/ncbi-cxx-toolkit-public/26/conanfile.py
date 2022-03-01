841182542a926a7d2e6d609f6666eff1b0ee32c3
Gourianov, Andrei <gouriano@ncbi.nlm.nih.gov>
2022-03-01 15:19:34 -0500

    Disabled cross compilation

--- recipes/ncbi-cxx-toolkit-public/26/conanfile.py
+++ recipes/ncbi-cxx-toolkit-public/26/conanfile.py
@@ -62,8 +62,6 @@ class NcbiCxxToolkit(ConanFile):
                 return None
             if key == "NGHTTP2" and self.settings.os == "Windows":
                 return None
-            if key == "MySQL" and self.settings.os == "Macos" and "arm" in self.settings.arch:
-                return None
             return self.NCBI_to_Conan_requires[key]
         return None
 
@@ -94,6 +92,8 @@ class NcbiCxxToolkit(ConanFile):
             raise ConanInvalidConfiguration("This configuration is not supported")
         if self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "7":
             raise ConanInvalidConfiguration("This version of GCC is not supported")
+        if hasattr(self, "settings_build") and tools.cross_building(self, skip_x64_x86=True):
+            raise ConanInvalidConfiguration("Cross compilation is not supported")
 
     def config_options(self):
         if self.settings.os == "Windows":
