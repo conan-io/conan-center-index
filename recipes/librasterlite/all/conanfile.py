faff0cc39e5d7b2e39b2962f0145d636da548a22
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-02 21:30:11 +0100

    modernize

--- recipes/librasterlite/all/conanfile.py
+++ recipes/librasterlite/all/conanfile.py
@@ -1,7 +1,8 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, VisualStudioBuildEnvironment, tools
+import functools
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.36.0"
 
 
 class LibrasterliteConan(ConanFile):
@@ -25,13 +26,23 @@ class LibrasterliteConan(ConanFile):
         "fPIC": True,
     }
 
-    exports_sources = "patches/**"
     generators = "pkg_config"
-    _autotools = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    @property
+    def _is_msvc(self):
+        return str(self.settings.compiler) in ["Visual Studio", "msvc"]
+
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
+    def export_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -51,12 +62,8 @@ class LibrasterliteConan(ConanFile):
         self.requires("libtiff/4.3.0")
         self.requires("sqlite3/3.38.0")
 
-    @property
-    def _settings_build(self):
-        return getattr(self, "settings_build", self.settings)
-
     def build_requirements(self):
-        if self.settings.compiler != "Visual Studio":
+        if not self._is_msvc:
             self.build_requires("libtool/2.4.6")
             self.build_requires("pkgconf/1.7.4")
             if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
@@ -87,30 +94,29 @@ class LibrasterliteConan(ConanFile):
                 autotools = self._configure_autotools()
                 autotools.make()
 
+    @functools.lru_cache(1)
     def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
         yes_no = lambda v: "yes" if v else "no"
         args = [
             "--enable-static={}".format(yes_no(not self.options.shared)),
             "--enable-shared={}".format(yes_no(self.options.shared)),
             "--disable-gcov",
         ]
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        self._autotools.configure(args=args)
-        return self._autotools
+        autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        autotools.configure(args=args)
+        return autotools
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
-        if self.settings.compiler == "Visual Studio":
+        if self._is_msvc:
             self._build_msvc()
         else:
             self._build_autotools()
 
     def package(self):
         self.copy("COPYING", dst="licenses", src=self._source_subfolder)
-        if self.settings.compiler == "Visual Studio":
+        if self._is_msvc:
             self.copy("rasterlite.h", dst="include", src=os.path.join(self._source_subfolder, "headers"))
             self.copy("*.lib", dst="lib", src=self._source_subfolder)
             self.copy("*.dll", dst="bin", src=self._source_subfolder)
@@ -123,6 +129,9 @@ class LibrasterliteConan(ConanFile):
             tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
-        self.cpp_info.names["pkg_config"] = "rasterlite"
-        suffix = "_i" if self.settings.compiler == "Visual Studio" and self.options.shared else ""
+        self.cpp_info.set_property("pkg_config_name", "rasterlite")
+        suffix = "_i" if self._is_msvc and self.options.shared else ""
         self.cpp_info.libs = ["rasterlite{}".format(suffix)]
+
+        # TODO: to remove in conan v2 once pkg_config generator removed
+        self.cpp_info.names["pkg_config"] = "rasterlite"
