994d4ef84f85517d193a042da2cca7d7c9c2fb25
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-02 11:14:33 +0100

    relocatable shared libs on macOS

--- recipes/libspatialite/all/conanfile.py
+++ recipes/libspatialite/all/conanfile.py
@@ -166,6 +166,8 @@ class LibspatialiteConan(ConanFile):
 
         with tools.chdir(self._source_subfolder):
             self.run("{} -fiv".format(tools.get_env("AUTORECONF")), win_bash=tools.os_info.is_windows)
+            # relocatable shared libs on macOS
+            tools.replace_in_file("configure", "-install_name \\$rpath/", "-install_name @rpath/")
             # avoid SIP issues on macOS when dependencies are shared
             if tools.is_apple_os(self.settings.os):
                 libpaths = ":".join(self.deps_cpp_info.lib_paths)
