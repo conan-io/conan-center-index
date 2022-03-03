002d774909859d354180d53a0c467fb1942862b5
jgsogo <jgsogo@gmail.com>
2022-03-03 09:26:22 +0100

    set default value for license

--- recipes/eigen/all/conanfile.py
+++ recipes/eigen/all/conanfile.py
@@ -19,6 +19,7 @@ class EigenConan(ConanFile):
     default_options = {
         "MPL2_only": False,
     }
+    license = ("MPL-2.0", "LGPL-3.0-or-later")
 
     @property
     def _source_subfolder(self):
