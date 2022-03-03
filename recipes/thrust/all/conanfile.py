84941372eec5f10c4be7958cb3fb4411bc46e2c3
Uilian Ries <uilianries@gmail.com>
2022-03-03 13:06:41 +0100

    Update recipes/thrust/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/thrust/all/conanfile.py
+++ recipes/thrust/all/conanfile.py
@@ -33,9 +33,7 @@ class ThrustConan(ConanFile):
 
     def package(self):
         self.copy(pattern="LICENSE", src=self._source_subfolder, dst="licenses")
-        self.copy("*.h", src=os.path.join(self._source_subfolder, "thrust"),
-                      dst=os.path.join("include", "thrust"))
-        self.copy("*.inl", src=os.path.join(self._source_subfolder, "thrust"),
+        self.copy("*[.h|.inl]", src=os.path.join(self._source_subfolder, "thrust"),
                       dst=os.path.join("include", "thrust"))
 
     def package_id(self):
