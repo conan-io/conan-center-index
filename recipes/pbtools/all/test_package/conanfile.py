12e2cdcb10d75e964a2ff6d60534b4bff5f4f52c
Erik Moqvist <erik.moqvist@gmail.com>
2022-03-03 23:22:40 +0100

    pbtools: Try to make it work

--- recipes/pbtools/all/test_package/conanfile.py
+++ recipes/pbtools/all/test_package/conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 
 class PbtoolsTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -13,6 +13,6 @@ class PbtoolsTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
