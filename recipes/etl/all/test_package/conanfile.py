8749241d4203cd5ba7db73008a1ebe41e4699097
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-03 16:06:49 +0100

    (#9510) etl: modernize & fix CMake imported target

--- recipes/etl/all/test_package/conanfile.py
+++ recipes/etl/all/test_package/conanfile.py
@@ -1,11 +1,10 @@
+from conans import ConanFile, CMake, tools
 import os
 
-from conans import ConanFile, CMake, tools
 
-
-class EmbeddedTemplateLibraryTestConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake","cmake_find_package_multi"
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -14,5 +13,5 @@ class EmbeddedTemplateLibraryTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self):
-            bin_path = os.path.abspath(os.path.join("bin", "example"))
+            bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
