5665e1374367277cae905f44f67dc63f3cf9d766
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-03 15:47:45 +0100

    (#9508) leveldb: relocatable shared lib on macOS + ensure to honor options
    * ensure to honor options
    
    * relocatable shared lib on macOS
    
    * use lru_cache
    
    * better injection of dependencies

--- recipes/leveldb/all/conanfile.py
+++ recipes/leveldb/all/conanfile.py
@@ -1,4 +1,5 @@
 from conans import CMake, ConanFile, tools
+import functools
 import os
 
 required_conan_version = ">=1.43.0"
@@ -29,8 +30,7 @@ class LevelDBCppConan(ConanFile):
         "with_crc32c": True,
     }
 
-    generators = "cmake", "cmake_find_package"
-    _cmake = None
+    generators = "cmake", "cmake_find_package_multi"
 
     @property
     def _source_subfolder(self):
@@ -66,14 +66,15 @@ class LevelDBCppConan(ConanFile):
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
+    @functools.lru_cache(1)
     def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["LEVELDB_BUILD_TESTS"] = False
-        self._cmake.definitions["LEVELDB_BUILD_BENCHMARKS"] = False
-        self._cmake.configure()
-        return self._cmake
+        cmake = CMake(self)
+        cmake.definitions["LEVELDB_BUILD_TESTS"] = False
+        cmake.definitions["LEVELDB_BUILD_BENCHMARKS"] = False
+        cmake.definitions["HAVE_SNAPPY"] = self.options.with_snappy
+        cmake.definitions["HAVE_CRC32C"] = self.options.with_crc32c
+        cmake.configure()
+        return cmake
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
