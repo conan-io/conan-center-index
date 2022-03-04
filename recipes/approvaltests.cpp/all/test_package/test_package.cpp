fea731d05cbc6f51f8ae4fbb126c16182955f5e1
Uilian Ries <uilianries@gmail.com>
2022-03-04 10:28:10 +0100

    Update recipes/approvaltests.cpp/all/test_package/test_package.cpp
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>

--- recipes/approvaltests.cpp/all/test_package/test_package.cpp
+++ recipes/approvaltests.cpp/all/test_package/test_package.cpp
@@ -5,6 +5,6 @@
 
 
 int main(void) {
-    std::cout << ApprovalTests::StringMaker::toString(42) << " is the answer."<< std::endl;
+    std::cout << ApprovalTests::StringMaker::toString(42) << ApprovalTests::Grid::print(1, 1, " is the answer.")<< std::endl;
     return EXIT_SUCCESS;
 }
