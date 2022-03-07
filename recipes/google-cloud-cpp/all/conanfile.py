4a1cc840e229dd0c053c261909b3bc5f70190c44
SSE4 <tomskside@gmail.com>
2022-03-07 18:52:33 +0700

    Update conanfile.py

--- recipes/google-cloud-cpp/all/conanfile.py
+++ recipes/google-cloud-cpp/all/conanfile.py
@@ -65,7 +65,7 @@ class GoogleCloudCppConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def requirements(self):
-        self.requires('protobuf/3.17.1')
+        self.requires('protobuf/3.19.2')
         self.requires('grpc/1.39.1')
         self.requires('nlohmann_json/3.10.2')
         self.requires('crc32c/1.1.1')
