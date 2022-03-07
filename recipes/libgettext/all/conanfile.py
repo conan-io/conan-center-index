4bc6161c9ab53886230eeec1249683496338159b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-07 11:14:40 +0100

    no -FS flag for Visual Studio < 2013

--- recipes/libgettext/all/conanfile.py
+++ recipes/libgettext/all/conanfile.py
@@ -124,7 +124,9 @@ class GetTextConan(ConanFile):
                 with tools.chdir(os.path.join(self._source_subfolder, self._gettext_folder)):
                     env_build = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
                     if self._is_msvc:
-                        env_build.flags.append("-FS")
+                        if not (self.settings.compiler == "Visual Studio" and
+                                tools.Version(self.settings.compiler.version) < "12"):
+                            env_build.flags.append("-FS")
                     env_build.configure(args=args, build=build, host=host)
                     env_build.make(self._make_args)
 
