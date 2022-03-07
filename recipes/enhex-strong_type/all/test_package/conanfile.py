293b8eeb48fb4efeba86ff030b4ac0614ff23a63
Enhex <enhex0@gmail.com>
2022-03-08 00:16:35 +0200

    Update recipes/enhex-strong_type/all/test_package/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/enhex-strong_type/all/test_package/conanfile.py
+++ recipes/enhex-strong_type/all/test_package/conanfile.py
@@ -5,12 +5,10 @@ from conans import ConanFile, CMake, tools
 
 class EnhexStrongTypeTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
