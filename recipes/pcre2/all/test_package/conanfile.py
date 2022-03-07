01c63a1ac6962e88378bdcdffee21833a456c1b5
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-07 23:07:07 +0100

    modernize

--- recipes/pcre2/all/test_package/conanfile.py
+++ recipes/pcre2/all/test_package/conanfile.py
@@ -3,8 +3,8 @@ import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -12,7 +12,7 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
-            arguments = "%sw+ Bincrafters" % ("\\" if self.settings.os == "Windows" else "\\\\")
+            arguments = "%sw+ conan-center" % ("\\" if self.settings.os == "Windows" else "\\\\")
             self.run("%s %s" % (bin_path, arguments), run_environment=True)
