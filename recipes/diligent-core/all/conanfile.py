c23140c6a97d7ea63e2150cea2bca9a5188cb4a7
Andrei Malashkin <malashkin.andrey@gmail.com>
2022-03-07 00:08:19 -0800

    (#9556) [diligent-core] package file2string tool
    * package file2string tool
    
    * add test for file2string
    
    * export FILE2STRING_PATH variable
    
    * add .exe file extension on windows
    
    * Apply suggestions from code review
    
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>
    
    * Update recipes/diligent-core/all/conanfile.py
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Update recipes/diligent-core/all/conanfile.py
    
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>
    
    * Update recipes/diligent-core/all/conanfile.py
    
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>
    
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/diligent-core/all/conanfile.py
+++ recipes/diligent-core/all/conanfile.py
@@ -153,6 +153,10 @@ class DiligentCoreConan(ConanFile):
         self.copy(pattern="*.dylib", dst="lib", keep_path=False)
         self.copy(pattern="*.so", dst="lib", keep_path=False)
         self.copy(pattern="*.dll", dst="bin", keep_path=False)
+        self.copy(pattern="*.fxh", dst="res", keep_path=False)
+
+        self.copy("File2String*", src=os.path.join(self._build_subfolder, "bin"), dst="bin", keep_path=False)
+        tools.remove_files_by_mask(self.package_folder, "*.pdb")
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
