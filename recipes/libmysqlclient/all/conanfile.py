a9bc1b11ee6bd4a67aee414ca44d951885c77dbc
bincrafters-user <bincrafters@gmail.com>
2022-03-08 09:07:39 +0000

    libmysqlclient: Update Conan conventions
    Automatically created by bincrafters-conventions 0.31.0

--- recipes/libmysqlclient/all/conanfile.py
+++ recipes/libmysqlclient/all/conanfile.py
@@ -33,7 +33,7 @@ class LibMysqlClientCConan(ConanFile):
 
     def requirements(self):
         if self.options.with_ssl:
-            self.requires("openssl/1.1.1k")
+            self.requires("openssl/1.1.1m")
 
         if self.options.with_zlib:
             self.requires("zlib/1.2.11")
