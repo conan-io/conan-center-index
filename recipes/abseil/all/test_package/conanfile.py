70c220f458e39b98fde6f266993f0c915f5f4803
SSE4 <tomskside@gmail.com>
2022-03-11 17:57:21 +0700

    Update recipes/abseil/all/test_package/conanfile.py

--- recipes/abseil/all/test_package/conanfile.py
+++ recipes/abseil/all/test_package/conanfile.py
@@ -8,7 +8,7 @@ class TestPackageConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["CXX20_SUPPORTED"] = tools.Version(self.deps_cpp_info["abseil"].version) > "20200923.3"
+        cmake.definitions["CXX20_SUPPORTED"] = tools.Version(self.deps_cpp_info["abseil"].version) > "20210324.2"
         cmake.configure()
         cmake.build()
 
