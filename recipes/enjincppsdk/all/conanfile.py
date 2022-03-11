2b4f0929fcd470388c2977ed04b64eb79aca8018
BishopJohnson <BishopJohnson@users.noreply.github.com>
2022-03-10 23:44:32 -0800

    Fixed typo in compiler validation

--- recipes/enjincppsdk/all/conanfile.py
+++ recipes/enjincppsdk/all/conanfile.py
@@ -90,7 +90,7 @@ class EnjinCppSdk(ConanFile):
         elif tools.Version(compiler.version) < minimum_version:
             raise ConanInvalidConfiguration("C++17 support is required, which your compiler does not support.")
 
-        if compiler == "clang" and compiler.libxx in ["libstdc++", "libstdc++11"] and compiler.version == "11":
+        if compiler == "clang" and compiler.libcxx in ["libstdc++", "libstdc++11"] and compiler.version == "11":
             raise ConanInvalidConfiguration("clang 11 with libstdc++ is not supported due to old libstdc++ missing "
                                             "C++17 support")
 
