62fb4e8ce17c7bb745b433d7aaa13e3c2ffca886
Erik Moqvist <erik.moqvist@gmail.com>
2022-03-12 11:29:43 +0100

    c-dbg-macro: A set of dbg(…) macros for C
    Add version 0.12.1.
    
    A few macros that prints and returns the value of a given expression
    for quick and dirty debugging, inspired by Rusts dbg!(…) macro and its
    C++ variant.
    
    * dbg(expr) for primitive data types (int, float, etc.), strings and pointers.
    
    * dbgb(expr) to force boolean true/false output.
    
    * dbga(expr, length) for array of primitive data types.
    
    * dbgh(expr, size) for hexdump output.
    
    * dbge(expr) for negative error codes.
    
    * dbgbt() for a backtrace.
    
    Just include dbg.h in your project to use it.

---
+++ recipes/c-dbg-macro/all/conanfile.py
@@ -0,0 +1,34 @@
+import os
+from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+
+
+class DbgMacroConan(ConanFile):
+    name = "c-dbg-macro"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/eerimoq/dbg-macro"
+    license = "MIT"
+    description = "A dbg(...) macro for C"
+    topics = ("conan", "debugging", "macro", "pretty-printing", "header-only")
+    settings = ("compiler",  "build_type", "os", "arch")
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def validate(self):
+        if self.settings.os == "Windows":
+            raise ConanInvalidConfiguration("This library is not compatible with Windows")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = "dbg-macro-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy("include/dbg.h", dst=".", src=self._source_subfolder)
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
