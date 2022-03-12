4a717223baa0db3f642a575ab3989ed1adec9008
toge <toge.mail@gmail.com>
2022-03-12 20:38:06 +0900

    use required in with_thrift

--- recipes/arrow/all/conanfile.py
+++ recipes/arrow/all/conanfile.py
@@ -213,7 +213,7 @@ class ArrowConan(ConanFile):
 
     def _with_thrift(self, required=False):
         # No self.options.with_thift exists
-        return bool(self.options.with_hiveserver2 or self.options.parquet)
+        return bool(required or self.options.with_hiveserver2 or self.options.parquet)
 
     def _with_utf8proc(self, required=False):
         if required or self.options.with_utf8proc == "auto":
