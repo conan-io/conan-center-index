32efc9ccf5fecae5023ad6c399ae085b8d543411
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-12 13:12:44 +0100

    bump vulkan-loader

--- recipes/glfw/all/conanfile.py
+++ recipes/glfw/all/conanfile.py
@@ -57,7 +57,7 @@ class GlfwConan(ConanFile):
     def requirements(self):
         self.requires("opengl/system")
         if self.options.vulkan_static:
-            self.requires("vulkan-loader/1.2.198.0")
+            self.requires("vulkan-loader/1.3.204.1")
         if self.settings.os == "Linux":
             self.requires("xorg/system")
 
