f1f9d6389a3bf5eb23eff58ca6642f3934fa1e9f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-15 04:25:36 +0100

    (#8862) openexr 2.x: modernize
    * modernize
    
    * cross-build not supported before 2.5.0
    
    * simplify rpath handling in cmake wrapper
    
    * move CMAKE_SKIP_INSTALL_RPATH to conanfile

--- recipes/openexr/2.x/test_package/conanfile.py
+++ recipes/openexr/2.x/test_package/conanfile.py
@@ -2,8 +2,8 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class DefaultNameConan(ConanFile):
-    settings = "os", "compiler", "arch", "build_type"
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
@@ -12,7 +12,7 @@ class DefaultNameConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             imgfile = os.path.join(self.source_folder, "comp_short_decode_piz.exr")
             self.run("{} {}".format(bin_path, imgfile), run_environment=True)
