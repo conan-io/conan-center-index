2ed16a72aea4780dcbc68520b01a7c8d61ed0e41
HerrNamenlos123 <herrnamenlos123@gmail.com>
2022-03-19 17:12:37 -0700

    Fixed allegro5 dependencies on ubuntu

--- recipes/allegro5/all/conanfile.py
+++ recipes/allegro5/all/conanfile.py
@@ -43,6 +43,7 @@ class Allegro5Conan(ConanFile):
             self.requires("libalsa/1.2.5.1")
             self.requires("pulseaudio/14.2")
             self.requires("gtk/system")
+            self.requires("openssl/1.1.1m")
 
     @property
     def _source_subfolder(self):
@@ -64,17 +65,24 @@ class Allegro5Conan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
             
-    #def set_version(self):
-    #    self.version = "5.2.7"
+    def set_version(self):
+        self.version = "5.2.7"
 
     def _patch_addon(self, addon, find, replace):
-        tools.replace_in_file(os.path.join("../source", self._source_subfolder, "addons", addon, "CMakeLists.txt"), find, replace, strict=False)
+        path = None
+        if (addon == None):
+            path = os.path.join("../source", self._source_subfolder, "CMakeLists.txt")
+        elif (addon == "addons"):
+            path = os.path.join("../source", self._source_subfolder, "addons", "CMakeLists.txt")
+        else:
+            path = os.path.join("../source", self._source_subfolder, "addons", addon, "CMakeLists.txt")
+        tools.replace_in_file(path, find, replace, strict=False)
         
     def _patch_sources(self):
         self._patch_addon("image", "${FREEIMAGE_INCLUDE_PATH}", "${FreeImage_INCLUDE_DIR}")
         self._patch_addon("image", "${PNG_INCLUDE_DIR}", "${PNG_INCLUDE_DIR} ${ZLIB_INCLUDE_DIR}")
         self._patch_addon("image", "${WEBP_INCLUDE_DIRS}", "${WebP_INCLUDE_DIRS}")
-        self._patch_addon("acodec", "find_package(FLAC)", "find_package(FLAC)\nset(FLAC_STATIC 1)\n")
+        self._patch_addon("acodec", "find_package(FLAC)", "find_package(flac REQUIRED)\nset(FLAC_STATIC 1)\n")
 
         self._patch_addon("acodec", "${VORBIS_INCLUDE_DIR}", "${Vorbis_INCLUDE_DIR}")
         self._patch_addon("video", "${VORBIS_INCLUDE_DIR}", "${Vorbis_INCLUDE_DIR}")
@@ -84,16 +92,33 @@ class Allegro5Conan(ConanFile):
         self._patch_addon("acodec", "${OGG_INCLUDE_DIR}", "${Ogg_INCLUDE_DIR}")
         self._patch_addon("video", "${OGG_INCLUDE_DIR}", "${Ogg_INCLUDE_DIR}")
 
-        self._patch_addon("acodec", "find_package(Opus)", "find_package(Opus)\nfind_package(opusfile)")
+        self._patch_addon("acodec", "find_package(Opus)", "find_package(Opus REQUIRED)\nfind_package(opusfile)")
         self._patch_addon("acodec", "${OPUS_INCLUDE_DIR}", "${Opus_INCLUDE_DIR} ${opusfile_INCLUDE_DIR}")
         self._patch_addon("acodec", "${OPUS_LIBRARIES}", "${Opus_LIBRARIES} ${opusfile_LIBRARIES}")
 
+        self._patch_addon("acodec", "find_package(MiniMP3)", "find_package(minimp3 REQUIRED)")
         self._patch_addon("acodec", "${MINIMP3_INCLUDE_DIRS}", "${minimp3_INCLUDE_DIRS}")
         self._patch_addon("acodec", "list(APPEND ACODEC_SOURCES mp3.c)", 
             "list(APPEND ACODEC_SOURCES mp3.c)\nlist(APPEND ACODEC_INCLUDE_DIRECTORIES ${minimp3_INCLUDE_DIRS})")
 
+        self._patch_addon("video", "find_package(Theora)", "find_package(theora REQUIRED)")
         self._patch_addon("video", "${THEORA_INCLUDE_DIR}", "${theora_INCLUDE_DIR}")
         self._patch_addon("video", "${THEORA_LIBRARIES}", "${theora_LIBRARIES}")
+
+        self._patch_addon("addons", "find_package(PhysFS)", "find_package(PhysFS REQUIRED)")
+        self._patch_addon("addons", "if(PHYSFS_FOUND)", "if(PhysFS_FOUND)")
+        self._patch_addon("addons", "${PHYSFS_INCLUDE_DIR}", "${physfs_INCLUDE_DIR}")
+        self._patch_addon("addons", "${PHYSFS_LIBRARY}", "${physfs_LIBRARY}")
+        self._patch_addon("addons", "find_package_handle_standard_args(PHYSFS DEFAULT_MSG\n        PHYSFS_LIBRARY PHYSFS_INCLUDE_DIR)", "")
+
+        self._patch_addon(None, "find_package(X11)", "find_package(X11 REQUIRED)\nset(X11_LIBRARIES ${X11_LIBRARIES} xcb dl)")
+        self._patch_addon("addons", "run_c_compile_test(\"${FREETYPE_TEST_SOURCE}\" TTF_COMPILES)", "")
+        self._patch_addon("addons", "run_c_compile_test(\"${FREETYPE_TEST_SOURCE}\" TTF_COMPILES_WITH_EXTRA_DEPS)", 
+            '''find_package(Brotli REQUIRED)
+               list(APPEND FREETYPE_STATIC_LIBRARIES "${Brotli_LIBRARIES}")
+               run_c_compile_test("${FREETYPE_TEST_SOURCE}" TTF_COMPILES_WITH_EXTRA_DEPS)''')
+        
+            
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
@@ -114,9 +139,14 @@ class Allegro5Conan(ConanFile):
         self._cmake.definitions["WANT_RELEASE_LOGGING"] = False
         self._cmake.definitions["WANT_VORBIS"] = True
         self._cmake.definitions["WANT_MP3"] = True
+        self._cmake.definitions["WANT_OGG_VIDEO"] = True
+
+        self._cmake.definitions["FREETYPE_ZLIB"] = True
+        self._cmake.definitions["FREETYPE_BZIP2"] = True
+        self._cmake.definitions["FREETYPE_PNG"] = True
+        self._cmake.definitions["FREETYPE_HARFBUZZ"] = True
             
-        # Disable warnings: possible loss of data, signed/unsigned comparison, unused variables, preprocessor
-        self._cmake.definitions["CMAKE_CXX_FLAGS"] = "/wd4267 /wd4018" if self._is_msvc else "-Wno-unused-variable -Wp,-w"
+        self._cmake.definitions["CMAKE_CXX_FLAGS"] = "/wd4267 /wd4018" if self._is_msvc else "-Wno-unused-variable -w"
         
         self._cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
         return self._cmake
