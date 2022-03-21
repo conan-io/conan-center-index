233e97c07286cfd6a72245e0588e247803daa609
Florian Zachs <44909261+HerrNamenlos123@users.noreply.github.com>
2022-03-21 15:43:22 +0100

    Update recipes/allegro5/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/allegro5/all/conanfile.py
+++ recipes/allegro5/all/conanfile.py
@@ -122,7 +122,7 @@ class Allegro5Conan(ConanFile):
 
         self._cmake = CMake(self)
         self._cmake.definitions["SHARED"] = False
-        self._cmake.definitions["WANT_STATIC_RUNTIME"] = "MT" in self.settings.compiler.runtime if self._is_msvc else False
+        self._cmake.definitions["WANT_STATIC_RUNTIME"] = "MT" in msvc_runtime_flag(self) if self._is_msvc else False
         self._cmake.definitions["CMAKE_BUILD_TYPE"] = self.settings.build_type
         self._cmake.definitions["PREFER_STATIC_DEPS"] = True
         self._cmake.definitions["WANT_DOCS"] = False
