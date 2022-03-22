f2f9c63d22b7fbd8c1fa3cb97c4c938646ddd7cb
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2022-03-22 08:44:45 +0100

    (#9631) [diligent-core] add version 250014
    * add version 250014
    
    * add find_package xxHash
    
    * fix cmake build type
    
    * bump dependencies
    
    * bump vulkan-headers
    
    * use conan.tools.files.rename
    
    * add version 250014 to config
    
    * fix rename
    
    * patch graphics archiver
    
    * make MSVC debug build invalid
    
    * add cmake requirement
    
    * remove unneded patch
    
    * Update recipes/diligent-core/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * add api prefix
    
    * don't use  CONAN_PKG
    
    * correct patch
    
    * correct xxhash patch
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/diligent-core/all/conanfile.py
+++ recipes/diligent-core/all/conanfile.py
@@ -83,6 +83,9 @@ class DiligentCoreConan(ConanFile):
         for patch in self.conan_data["patches"][self.version]:
             tools.patch(**patch)
 
+    def build_requirements(self):
+        self.build_requires("cmake/3.22.0")
+
     def requirements(self):
         self.requires("opengl/system")
 
@@ -92,11 +95,11 @@ class DiligentCoreConan(ConanFile):
         self.requires("libpng/1.6.37")
 
         self.requires("spirv-cross/cci.20210930")
-        self.requires("spirv-headers/1.2.198.0")
-        self.requires("spirv-tools/2021.4")
-        self.requires("vulkan-headers/1.2.198")
-        self.requires("volk/1.2.198")
-        self.requires("glslang/11.7.0")
+        self.requires("spirv-tools/1.3.204.0")
+        self.requires("vulkan-headers/1.3.204")
+        self.requires("volk/1.3.204")
+        self.requires("glslang/1.3.204.0")
+        self.requires("xxhash/0.8.0")
 
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.requires("xorg/system")
@@ -146,7 +149,7 @@ class DiligentCoreConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         tools.rename(src=os.path.join(self.package_folder, "include", "source_subfolder"),
-                     dst=os.path.join(self.package_folder, "include", "DiligentCore"))
+        dst=os.path.join(self.package_folder, "include", "DiligentCore"))
 
         tools.rmdir(os.path.join(self.package_folder, "Licenses"))
         tools.rmdir(os.path.join(self.package_folder, "lib"))
