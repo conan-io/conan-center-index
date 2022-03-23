a46fdfbd6f0f163a053d2230ae2665ecf8922062
toge <toge.mail@gmail.com>
2022-03-23 23:50:53 +0900

    aws-c-io: add version 0.10.20

--- recipes/aws-c-io/all/conanfile.py
+++ recipes/aws-c-io/all/conanfile.py
@@ -47,11 +47,11 @@ class AwsCIO(ConanFile):
             self.requires("aws-c-common/0.6.11")
             self.requires("aws-c-cal/0.5.11")
         else:
-            self.requires("aws-c-common/0.6.15")
+            self.requires("aws-c-common/0.6.19")
             self.requires("aws-c-cal/0.5.12")
 
         if self.settings.os in ["Linux", "FreeBSD", "Android"]:
-            self.requires("s2n/1.1.0")
+            self.requires("s2n/1.2.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
