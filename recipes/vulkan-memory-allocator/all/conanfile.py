d4c968779d485f6a01eded233582592ce1b93702
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-27 13:21:05 +0200

    add vulkan-memory-allocator/3.0.0

--- recipes/vulkan-memory-allocator/all/conanfile.py
+++ recipes/vulkan-memory-allocator/all/conanfile.py
@@ -1,6 +1,7 @@
+from conans import ConanFile, tools
 import os
 
-from conans import ConanFile, tools
+required_conan_version = ">=1.33.0"
 
 
 class VulkanMemoryAllocatorConan(ConanFile):
@@ -10,20 +11,35 @@ class VulkanMemoryAllocatorConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     description = "Easy to integrate Vulkan memory allocation library."
     topics = ("vulkan", "memory-allocator", "graphics")
-    requires = ("vulkan-headers/1.2.182")
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
+    @property
+    def _min_cppstd(self):
+        return "11" if tools.Version(self.version) < "3.0.0" else "14"
+
+    def requirements(self):
+        self.requires("vulkan-headers/1.3.204.1")
+
     def package_id(self):
         self.info.header_only()
 
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._min_cppstd)
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("VulkanMemoryAllocator-{}".format(self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def package(self):
         self.copy("LICENSE.txt", src=self._source_subfolder, dst="licenses")
-        self.copy("vk_mem_alloc.h", src=os.path.join(self._source_subfolder, "src"), dst="include")
+        if tools.Version(self.version) < "3.0.0":
+            include_dir = os.path.join(self._source_subfolder, "src")
+        else:
+            include_dir = os.path.join(self._source_subfolder, "include")
+        self.copy("vk_mem_alloc.h", src=include_dir, dst="include")
