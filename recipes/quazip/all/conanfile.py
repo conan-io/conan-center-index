5b0f1e7ca551ae51c9618a0398ae5c8b10645b6b
Martin Delille <martin@delille.org>
2022-03-28 15:58:32 +0200

    quazip/1.2.0: Add qt6 support

--- recipes/quazip/all/conanfile.py
+++ recipes/quazip/all/conanfile.py
@@ -26,9 +26,13 @@ class QuaZIPConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    @property
+    def _qt_major_version(self):
+        return tools.Version(self.deps_cpp_info["qt"].version).major
+
     def requirements(self):
         self.requires("zlib/1.2.12")
-        self.requires("qt/5.15.3")
+        self.requires("qt/6.2.3")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -46,6 +50,7 @@ class QuaZIPConan(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
+        self._cmake.definitions["QUAZIP_QT_MAJOR_VERSION"] = self._qt_major_version
         self._cmake.configure()
         return self._cmake
 
@@ -62,11 +67,11 @@ class QuaZIPConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.includedirs=[os.path.join('include', 'QuaZip-Qt5-{}'.format(self.version))]
+        self.cpp_info.includedirs=[os.path.join('include', 'QuaZip-Qt{}-{}'.format(self._qt_major_version, self.version))]
         if not self.options.shared:
             self.cpp_info.defines.append("QUAZIP_STATIC")
 
-        self.cpp_info.filenames["cmake_find_package"] = "QuaZip-Qt5"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "QuaZip-Qt5"
+        self.cpp_info.filenames["cmake_find_package"] = 'QuaZip-Qt{}'.format(self._qt_major_version, self.version)
+        self.cpp_info.filenames["cmake_find_package_multi"] = 'QuaZip-Qt{}'.format(self._qt_major_version, self.version)
         self.cpp_info.names["cmake_find_package"] = "QuaZip"
         self.cpp_info.names["cmake_find_package_multi"] = "QuaZip"
