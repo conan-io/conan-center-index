3c173d0398a7a114f170db71dccc13da363ad7a7
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-29 13:23:56 +0200

    (#9911) imath: modernize
    * modernize
    
    * define IMATH_DLL if windows & shared

--- recipes/imath/all/conanfile.py
+++ recipes/imath/all/conanfile.py
@@ -1,14 +1,20 @@
 from conans import ConanFile, CMake, tools
 import os, functools
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.43.0"
+
 
 class ImathConan(ConanFile):
     name = "imath"
-    description = "Imath is a C++ and python library of 2D and 3D vector, matrix, and math operations for computer graphics."
+    description = (
+        "Imath is a C++ and python library of 2D and 3D vector, matrix, and "
+        "math operations for computer graphics."
+    )
     license = "BSD-3-Clause"
+    topics = ("computer-graphics", "matrix", "openexr", "3d-vector")
     homepage = "https://github.com/AcademySoftwareFoundation/Imath"
     url = "https://github.com/conan-io/conan-center-index"
+
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -18,9 +24,9 @@ class ImathConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    generators = "cmake", "cmake_find_package"
+
+    generators = "cmake"
     exports_sources = ["CMakeLists.txt"]
-    topics = ("computer-graphics", "matrix", "openexr", "3d-vector")
 
     @property
     def _source_subfolder(self):
@@ -33,14 +39,14 @@ class ImathConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-    
-    def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 11)
 
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 11)
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
@@ -67,21 +73,30 @@ class ImathConan(ConanFile):
         self.copy("LICENSE.md", src=self._source_subfolder, dst="licenses")
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "Imath"
-        self.cpp_info.names["cmake_find_package_multi"] = "Imath"
-        self.cpp_info.names["pkg_config"] = "Imath"
+        self.cpp_info.set_property("cmake_file_name", "Imath")
+        self.cpp_info.set_property("cmake_target_name", "Imath::Imath")
+        self.cpp_info.set_property("pkg_config_name", "Imath")
 
         # Imath::ImathConfig - header only library
         imath_config = self.cpp_info.components["imath_config"]
-        imath_config.names["cmake_find_package"] = "ImathConfig"
-        imath_config.names["cmake_find_package_multi"] = "ImathConfig"
-        imath_config.names["pkg_config"] = "ImathConfig"
+        imath_config.set_property("cmake_target_name", "Imath::ImathConfig")
         imath_config.includedirs.append(os.path.join("include", "Imath"))
 
         # Imath::Imath - linkable library
         imath_lib = self.cpp_info.components["imath_lib"]
+        imath_lib.set_property("cmake_target_name", "Imath::Imath")
+        imath_lib.set_property("pkg_config_name", "Imath")
+        imath_lib.libs = tools.collect_libs(self)
+        imath_lib.requires = ["imath_config"]
+        if self.settings.os == "Windows" and self.options.shared:
+            imath_lib.defines.append("IMATH_DLL")
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.names["cmake_find_package"] = "Imath"
+        self.cpp_info.names["cmake_find_package_multi"] = "Imath"
+        self.cpp_info.names["pkg_config"] = "Imath"
+        imath_config.names["cmake_find_package"] = "ImathConfig"
+        imath_config.names["cmake_find_package_multi"] = "ImathConfig"
         imath_lib.names["cmake_find_package"] = "Imath"
         imath_lib.names["cmake_find_package_multi"] = "Imath"
         imath_lib.names["pkg_config"] = "Imath"
-        imath_lib.libs = tools.collect_libs(self)
-        imath_lib.requires = ["imath_config"]
