832745867d3e1ba04ef528d74d74669382031b1a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-29 17:13:27 +0200

    modernize

--- recipes/miniz/all/conanfile.py
+++ recipes/miniz/all/conanfile.py
@@ -1,7 +1,8 @@
 from conans import ConanFile, CMake, tools
+import functools
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.43.0"
 
 
 class MinizConan(ConanFile):
@@ -10,16 +11,21 @@ class MinizConan(ConanFile):
                   "implements the zlib (RFC 1950) and Deflate (RFC 1951) " \
                   "compressed data format specification standards"
     license = "MIT"
-    topics = ("conan", "miniz", "compression", "lossless")
+    topics = ("miniz", "compression", "lossless")
     homepage = "https://github.com/richgel999/miniz"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = "CMakeLists.txt", "patches/*"
-    generators = "cmake"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
 
-    _cmake = None
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    generators = "cmake"
 
     @property
     def _source_subfolder(self):
@@ -28,6 +34,11 @@ class MinizConan(ConanFile):
     @property
     def _build_subfolder(self):
         return "build_subfolder"
+
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -43,18 +54,17 @@ class MinizConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
+    @functools.lru_cache(1)
     def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
+        cmake = CMake(self)
         if tools.Version(self.version) >= "2.2.0":
-            self._cmake.definitions["BUILD_EXAMPLES"] = False
-            self._cmake.definitions["BUILD_FUZZERS"] = False
-            self._cmake.definitions["AMALGAMATE_SOURCES"] = False
-            self._cmake.definitions["BUILD_HEADER_ONLY"] = False
-            self._cmake.definitions["INSTALL_PROJECT"] = True
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+            cmake.definitions["BUILD_EXAMPLES"] = False
+            cmake.definitions["BUILD_FUZZERS"] = False
+            cmake.definitions["AMALGAMATE_SOURCES"] = False
+            cmake.definitions["BUILD_HEADER_ONLY"] = False
+            cmake.definitions["INSTALL_PROJECT"] = True
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
@@ -70,8 +80,8 @@ class MinizConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "miniz"
-        self.cpp_info.names["cmake_find_package_multi"] = "miniz"
-        self.cpp_info.names["pkg_config"] = "miniz"
+        self.cpp_info.set_property("cmake_file_name", "miniz")
+        self.cpp_info.set_property("cmake_target_name", "miniz::miniz")
+        self.cpp_info.set_property("pkg_config_name", "miniz")
         self.cpp_info.libs = ["miniz"]
         self.cpp_info.includedirs = ["include", os.path.join("include", "miniz")]
