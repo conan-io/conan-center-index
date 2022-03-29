fd8ea5dec7fa430d4c959d68afeabd81aaa40786
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-03-29 13:29:01 +0200

    relocatable shared lib on macOS

--- recipes/clipper/all/conanfile.py
+++ recipes/clipper/all/conanfile.py
@@ -81,6 +81,8 @@ class ClipperConan(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
+        # To install relocatable shared libs on Macos
+        self._cmake.definitions["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
