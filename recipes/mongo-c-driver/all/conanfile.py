449427abdbe3d2958a64f187dc4467a8dd571496
toge <toge.mail@gmail.com>
2022-04-01 00:16:01 +0900

    update zlib/1.2.12

--- recipes/mongo-c-driver/all/conanfile.py
+++ recipes/mongo-c-driver/all/conanfile.py
@@ -78,7 +78,7 @@ class MongoCDriverConan(ConanFile):
         if self.options.with_snappy:
             self.requires("snappy/1.1.9")
         if self.options.with_zlib:
-            self.requires("zlib/1.2.11")
+            self.requires("zlib/1.2.12")
         if self.options.with_zstd:
             self.requires("zstd/1.5.2")
         if self.options.with_icu:
