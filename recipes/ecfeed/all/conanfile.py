99ac500f27901be35c663f4e74fbf51f9da57727
krzsko <k.skorupski@testify.no>
2022-04-02 21:06:25 +0200

    review - uilianries

--- recipes/ecfeed/all/conanfile.py
+++ recipes/ecfeed/all/conanfile.py
@@ -1,10 +1,11 @@
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+import os
 
 required_conan_version = ">=1.33.0"
 
 class EcFeedConan(ConanFile):
     name = "ecfeed"
-    version = "1.1.0"
     license = "EPL-1.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://ecfeed.com/"
@@ -13,22 +14,48 @@ class EcFeedConan(ConanFile):
         and generates a stream of test cases using a selected algorithm \
         (e.g. Cartesian, N-Wise)."
     topics = ("test", "ecfeed", "nwise")
-    build_policy = 'missing'
-    requires = "libcurl/7.72.0", "openssl/1.1.1c"
-    no_copy_source = True
+    requires = "libcurl/7.80.0", "openssl/1.1.1n"
+    settings = "os", "build_type", "arch", "compiler"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+    
+    @property
+    def _compilers_minimum_version(self):
+            minimum_versions = {
+                "gcc": "8",
+                "Visual Studio": "17",
+                "msvc": "19.22",
+                "clang": "6",
+                "apple-clang": "10"
+            }
+            return minimum_versions
+        
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, 20)
+        minimum_version = self._compilers_minimum_version.get(
+            str(self.settings.compiler), False)
+        if minimum_version:
+            if tools.Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration(f"{self.name} requires C++20, " 
+                    "which your compiler ({} {}) does not support.".format(
+                        self.settings.compiler, self.settings.compiler.version))
+        else:
+            self.output.warn(
+                f"{self.name} requires C++20. Your compiler is unknown. " 
+                    "Assuming it supports C++20.")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True, 
+                  destination=self._source_subfolder)
 
     def package(self):
-        self.copy("ecfeed.cpp-1.1.0/src/ecfeed.hpp", 
-                  dst="include", keep_path=False)
-        self.copy("ecfeed.cpp-1.1.0/license.txt", 
-                  dst="licenses", keep_path=False)
+        self.copy("ecfeed.hpp", 
+                  dst="include", src=os.path.join(self._source_subfolder, "src"))
+        self.copy("license.txt", 
+                  dst="licenses", src=os.path.join(self._source_subfolder))
 
     def package_id(self):
         self.info.header_only()
--- recipes/ecfeed/all/test_package/conanfile.py
+++ recipes/ecfeed/all/test_package/conanfile.py
@@ -3,14 +3,14 @@ from conans import ConanFile, CMake, tools
 
 class HelloTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
-        cmake = CMake(self)       
+        cmake = CMake(self)
         cmake.configure()
-        cmake.build()          
+        cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "example")
+            self.run(bin_path, run_environment=True)
