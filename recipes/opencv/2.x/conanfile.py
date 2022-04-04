07f7c159fe9c8efde5f890b3a3dd90dc5e9eb026
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-04-04 12:21:38 +0200

    decrease paths length

--- recipes/opencv/2.x/conanfile.py
+++ recipes/opencv/2.x/conanfile.py
@@ -48,11 +48,11 @@ class OpenCVConan(ConanFile):
 
     @property
     def _source_subfolder(self):
-        return "source_subfolder"
+        return "src"
 
     @property
     def _build_subfolder(self):
-        return "build_subfolder"
+        return "build"
 
     @property
     def _is_msvc(self):
