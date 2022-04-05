bafc15cd167f6a34de93a6e9588de7d7ce06fc71
toge <toge.mail@gmail.com>
2022-04-05 19:45:10 +0900

    (#10108) quill: update fmt to 8.1.1 on quill/1.6.3

--- recipes/quill/all/conanfile.py
+++ recipes/quill/all/conanfile.py
@@ -62,7 +62,7 @@ class QuillConan(ConanFile):
 
     def requirements(self):
         if tools.Version(self.version) >= "1.6.3":
-            self.requires("fmt/8.0.1")
+            self.requires("fmt/8.1.1")
         elif tools.Version(self.version) >= "1.3.3":
             self.requires("fmt/7.1.2")
         else:
