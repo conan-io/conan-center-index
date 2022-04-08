659d3f6bdc47ef347f2c9bc15982dbe5cd7f578c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-04-08 16:47:15 +0200

    modernize

--- recipes/glib/all/test_package/conanfile.py
+++ recipes/glib/all/test_package/conanfile.py
@@ -3,15 +3,10 @@ import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package", "pkg_config"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
-        if self.settings.os != "Windows":
-            with tools.environment_append({'PKG_CONFIG_PATH': "."}):
-                pkg_config = tools.PkgConfig("gio-2.0")
-                self.run("%s -h" % pkg_config.variables["gdbus_codegen"], run_environment=True)
-
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
