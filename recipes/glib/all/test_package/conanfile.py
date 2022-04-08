121c0c28da26c0e571eba594173a8e425c03809f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-04-08 21:54:27 +0200

    restore pkg_config test

--- recipes/glib/all/test_package/conanfile.py
+++ recipes/glib/all/test_package/conanfile.py
@@ -4,9 +4,14 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "cmake", "cmake_find_package_multi", "pkg_config"
 
     def build(self):
+        if self.settings.os != "Windows":
+            with tools.environment_append({'PKG_CONFIG_PATH': "."}):
+                pkg_config = tools.PkgConfig("gio-2.0")
+                self.run("%s -h" % pkg_config.variables["gdbus_codegen"], run_environment=True)
+
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
