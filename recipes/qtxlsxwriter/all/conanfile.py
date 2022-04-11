5f6bb773819cd17bb49bea96f9ce670822553b68
tbeu <tbeu@users.noreply.github.com>
2022-04-11 22:09:32 +0200

    Bump dependencies

--- recipes/qtxlsxwriter/all/conanfile.py
+++ recipes/qtxlsxwriter/all/conanfile.py
@@ -46,7 +46,7 @@ class QtXlsxWriterConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("qt/5.15.2")
+        self.requires("qt/5.15.3")
 
     def source(self):
         for source in self.conan_data["sources"][self.version]:
