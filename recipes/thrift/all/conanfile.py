84fb92479f515781d68c438033ed6077511fd960
Benoit Vander Stappen <44053930+vdsbenoit@users.noreply.github.com>
2022-04-12 17:56:40 +0200

    thrift: bump zlib version
    thrift packages are currently broken due to the new zlib 1.2.12 version. For instance:
    ````bash
    WARN: boost/1.78.0: requirement zlib/1.2.12 overridden by thrift/0.15.0 to zlib/1.2.11
    ERROR: Conflict in c-blosc/1.20.1:
        'c-blosc/1.20.1' requires 'zlib/1.2.12' while 'thrift/0.15.0' requires 'zlib/1.2.11'.
        To fix this conflict you need to override the package 'zlib' in your root package.
    ````

--- recipes/thrift/all/conanfile.py
+++ recipes/thrift/all/conanfile.py
@@ -85,7 +85,7 @@ class ThriftConan(ConanFile):
         if self.options.with_openssl:
             self.requires("openssl/1.1.1m")
         if self.options.with_zlib:
-            self.requires("zlib/1.2.11")
+            self.requires("zlib/1.2.12")
         if self.options.with_libevent:
             self.requires("libevent/2.1.12")
 
