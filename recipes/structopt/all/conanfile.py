0ce71c18a9ab9e0e8455fe6ffd567eada271f749
toge <toge.mail@gmail.com>
2022-04-13 02:14:54 +0900

    structopt: add version 0.1.3

--- recipes/structopt/all/conanfile.py
+++ recipes/structopt/all/conanfile.py
@@ -2,18 +2,20 @@ import os
 from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 
+required_conan_version = ">=1.33.0"
+
 class StructoptConan(ConanFile):
     name = "structopt"
-    homepage = "https://github.com/p-ranav/structopt"
-    url = "https://github.com/conan-io/conan-center-index"
     description = "Parse command line arguments by defining a struct+"
-    license = "MIT"
-    settings = "compiler", "os"
-    topics = ("conan", "structopt", "argument-parser", "cpp17", "header-only",
+    topics = ("structopt", "argument-parser", "cpp17", "header-only",
         "single-header-lib", "header-library", "command-line", "arguments",
         "mit-license", "modern-cpp", "structopt", "lightweight", "reflection",
         "cross-platform", "library", "type-safety", "type-safe", "argparse",
         "clap", "visit-struct-library", "magic-enum")
+    homepage = "https://github.com/p-ranav/structopt"
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    settings = "compiler", "os"
     no_copy_source = True
 
     @property
@@ -36,21 +38,22 @@ class StructoptConan(ConanFile):
             self.output.warn("{} recipe lacks information about the {} compiler standard version support".format(self.name, compiler))
         return False    
 
-    def configure(self):
+    def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, "17")
         if not self._supported_compiler:
             raise ConanInvalidConfiguration("structopt: Unsupported compiler: {}-{} "
-                                            "(https://github.com/p-ranav/structopt#compiler-compatibility).".format(self.settings.compiler, self.settings.compiler.version))
+                                            "(https://github.com/p-ranav/structopt#compiler-compatibility)."
+                                            .format(self.settings.compiler, self.settings.compiler.version))
+
+    def package_id(self):
+        self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("{}-{}".format(self.name, self.version), self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
     def package(self):
         self.copy(pattern="LICENSE", src=self._source_subfolder, dst="licenses")
         self.copy(pattern="*.h", src=os.path.join(self._source_subfolder, "include"), dst="include")
         self.copy(pattern="*.hpp", src=os.path.join(self._source_subfolder, "include"), dst="include")
 
-    def package_id(self):
-        self.info.header_only()
