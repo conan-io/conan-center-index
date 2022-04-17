fbe47be72ff8f9112435c0aa985b61dc32e148a8
Alexander Voigt <ahgvoigt@gmail.com>
2022-04-17 17:08:04 +0200

    fix warning from conan hooks

--- recipes/gm2calc/all/test_package/conanfile.py
+++ recipes/gm2calc/all/test_package/conanfile.py
@@ -13,5 +13,5 @@ class Gm2calcTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             self.run(os.path.join("bin", "test_package"), run_environment=True)
