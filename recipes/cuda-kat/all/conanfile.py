f595b9d33a9ff5fb27322a70b2df6a99b0e855de
Andrey Borzunov <Andrey.Borzunov@gmail.com>
2022-04-19 18:10:26 +0300

    (#10259) Recipe for cuda-kat

---
+++ recipes/cuda-kat/all/conanfile.py
@@ -0,0 +1,39 @@
+from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+required_conan_version = ">=1.33.0"
+
+class CudaKatConan(ConanFile):
+    name = "cuda-kat"
+    homepage = "https://github.com/eyalroz/cuda-kat"
+    description = "CUDA kernel author's tools"
+    topics = ("GPGPU", "CUDA", "cuda-kat", "header-only")
+    url = "https://github.com/conan-io/conan-center-index"
+    no_copy_source = True
+    license = "BSD 3-Clause License"
+
+    settings = "os"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def package_id(self):
+        self.info.header_only()
+
+    def validate(self):
+        if self.settings.os == "Windows":
+            raise ConanInvalidConfiguration("CUDA-kat library are not compatible on Windows")
+
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+
+    def package(self):
+        self.copy(pattern="LICENSE*", src=self._source_subfolder, dst="licenses")
+        self.copy("*", src=os.path.join(self._source_subfolder, "src"), dst="include")
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "cuda-kat"
+        self.cpp_info.names["cmake_find_package_multi"] = "cuda-kat"
