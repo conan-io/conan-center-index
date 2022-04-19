2644d1508fa983dcd7501c2608456641cfcdea93
Romain Deterre <romain@alazartech.com>
2022-04-19 10:22:21 -0400

    Use options from llvm-core

--- recipes/llvm/all/conanfile.py
+++ recipes/llvm/all/conanfile.py
@@ -55,9 +55,33 @@ class Llvm(ConanFile):
         **{"with_runtime_" + runtime: [True, False]
            for runtime in runtimes},
         **{
+            'shared': [True, False],
             'fPIC': [True, False],
+            'components': 'ANY',
+            'targets': 'ANY',
+            'exceptions': [True, False],
             'rtti': [True, False],
-            'enable_debug': [True, False]
+            'threads': [True, False],
+            'lto': ['On', 'Off', 'Full', 'Thin'],
+            'static_stdlib': [True, False],
+            'unwind_tables': [True, False],
+            'expensive_checks': [True, False],
+            'use_perf': [True, False],
+            'use_sanitizer': [
+                'Address',
+                'Memory',
+                'MemoryWithOrigins',
+                'Undefined',
+                'Thread',
+                'DataFlow',
+                'Address;Undefined',
+                'None'
+            ],
+            'with_ffi': [True, False],
+            'with_zlib': [True, False],
+            'with_xml2': [True, False],
+            'use_llvm_cmake_files': [True, False],
+            'enable_debug': [True, False],
         },
     }
     default_options = {
@@ -70,9 +94,24 @@ class Llvm(ConanFile):
             for runtime in runtimes
         },
         **{
+            'shared': False,
             'fPIC': True,
+            'components': 'all',
+            'targets': 'all',
+            'exceptions': True,
             'rtti': True,
+            'threads': True,
+            'lto': 'Off',
+            'static_stdlib': False,
+            'unwind_tables': True,
+            'expensive_checks': False,
+            'use_perf': False,
+            'use_sanitizer': 'None',
+            'with_ffi': False,
+            'with_zlib': True,
+            'with_xml2': True,
             'enable_debug': False,
+            'use_llvm_cmake_files': False,
         }
     }
     generators = 'cmake_find_package'
@@ -112,6 +151,51 @@ class Llvm(ConanFile):
 
         cmake = CMake(self, parallel=False)
         cmake.configure(defs={
+            'BUILD_SHARED_LIBS': False,
+            'CMAKE_SKIP_RPATH': True,
+            'CMAKE_POSITION_INDEPENDENT_CODE': \
+                self.options.get_safe('fPIC', default=False) or self.options.shared
+            'LLVM_TARGET_ARCH': 'host',
+            'LLVM_TARGETS_TO_BUILD': self.options.targets,
+            'LLVM_BUILD_LLVM_DYLIB': self.options.shared,
+            'LLVM_DYLIB_COMPONENTS': self.options.components,
+            'LLVM_ENABLE_PIC': self.options.get_safe('fPIC', default=False),
+            'LLVM_ABI_BREAKING_CHECKS': 'WITH_ASSERTS',
+            'LLVM_ENABLE_WARNINGS': True,
+            'LLVM_ENABLE_PEDANTIC': True,
+            'LLVM_ENABLE_WERROR': False,
+            'LLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN': True,
+            'LLVM_USE_RELATIVE_PATHS_IN_DEBUG_INFO': False,
+            'LLVM_BUILD_INSTRUMENTED_COVERAGE': False,
+            'LLVM_OPTIMIZED_TABLEGEN': True,
+            'LLVM_REVERSE_ITERATION': False,
+            'LLVM_ENABLE_BINDINGS': False,
+            'LLVM_CCACHE_BUILD': False,
+            'LLVM_INCLUDE_TOOLS': self.options.shared,
+            'LLVM_INCLUDE_EXAMPLES': False,
+            'LLVM_INCLUDE_TESTS': False,
+            'LLVM_INCLUDE_BENCHMARKS': False,
+            'LLVM_APPEND_VC_REV': False,
+            'LLVM_BUILD_DOCS': False,
+            'LLVM_ENABLE_IDE': False,
+            'LLVM_ENABLE_TERMINFO': False,
+            'LLVM_ENABLE_EH': self.options.exceptions,
+            'LLVM_ENABLE_RTTI': self.options.rtti,
+            'LLVM_ENABLE_THREADS': self.options.threads,
+            'LLVM_ENABLE_LTO': self.options.lto,
+            'LLVM_STATIC_LINK_CXX_STDLIB': self.options.static_stdlib,
+            'LLVM_ENABLE_UNWIND_TABLES': self.options.unwind_tables,
+            'LLVM_ENABLE_EXPENSIVE_CHECKS': self.options.expensive_checks,
+            'LLVM_ENABLE_ASSERTIONS': self.settings.build_type == 'Debug',
+            'LLVM_USE_NEWPM': False,
+            'LLVM_USE_OPROFILE': False,
+            'LLVM_USE_PERF': self.options.use_perf,
+            'LLVM_ENABLE_Z3_SOLVER': False,
+            'LLVM_ENABLE_LIBPFM': False,
+            'LLVM_ENABLE_LIBEDIT': False,
+            'LLVM_ENABLE_FFI': self.options.with_ffi,
+            'LLVM_ENABLE_ZLIB': self.options.get_safe('with_zlib', False),
+            'LLVM_ENABLE_LIBXML2': self.options.get_safe('with_xml2', False),
             'LLVM_ENABLE_PROJECTS': ';'.join(enabled_projects),
             'LLVM_ENABLE_RUNTIMES': ';'.join(enabled_runtimes),
             'LLVM_ENABLE_BINDINGS': False,
@@ -119,6 +203,16 @@ class Llvm(ConanFile):
         },
                         source_folder=os.path.join(self._source_subfolder,
                                                    'llvm'))
+        if not self.options.shared:
+            cmake.definitions['DISABLE_LLVM_LINK_LLVM_DYLIB'] = True
+        if self.settings.compiler == 'Visual Studio':
+            build_type = str(self.settings.build_type).upper()
+            cmake.definitions['LLVM_USE_CRT_{}'.format(build_type)] = \
+                self.settings.compiler.runtime
+        if self.options.use_sanitizer == 'None':
+            cmake.definitions['LLVM_USE_SANITIZER'] = ''
+        else:
+            cmake.definitions['LLVM_USE_SANITIZER'] = self.options.use_sanitizer
         return cmake
 
     def build(self):
@@ -149,6 +243,10 @@ class Llvm(ConanFile):
                 else:
                     shutil.rmtree(ignore_path)
 
+    def package_id(self):
+        del self.info.options.enable_debug
+        del self.info.options.use_llvm_cmake_files
+
     def validate(self):
         if self.settings.compiler == "gcc" and tools.Version(
                 self.settings.compiler.version) < "10":
@@ -167,5 +265,63 @@ class Llvm(ConanFile):
                 "Set the 'enable_debug' option to allow debug builds")
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.builddirs = ['lib/cmake']
+        self.cpp_info.set_property("cmake_file_name", "LLVM")
+
+        if self.options.shared:
+            self.cpp_info.libs = tools.collect_libs(self)
+            if self.settings.os == 'Linux':
+                self.cpp_info.system_libs = ['pthread', 'rt', 'dl', 'm']
+            elif self.settings.os == 'Macos':
+                self.cpp_info.system_libs = ['m']
+            return
+
+        components_path = \
+            os.path.join(self.package_folder, 'lib', 'components.json')
+        with open(components_path, 'r') as components_file:
+            components = json.load(components_file)
+
+        dependencies = ['ffi', 'z', 'iconv', 'xml2']
+        targets = {
+            'ffi': 'libffi::libffi',
+            'z': 'zlib::zlib',
+            'xml2': 'libxml2::libxml2'
+        }
+
+        for component, deps in components.items():
+            self.cpp_info.components[component].libs = [component]
+            self.cpp_info.components[component].requires.extend(dep for dep in deps if dep.startswith('LLVM'))
+
+            for lib, target in targets.items():
+                if lib in deps:
+                    self.cpp_info.components[component].requires.append(target)
+
+            self.cpp_info.components[component].system_libs = [
+                dep for dep in deps
+                if not dep.startswith('LLVM') and dep not in dependencies
+            ]
+
+            self.cpp_info.components[component].set_property("cmake_target_name", component)
+            self.cpp_info.components[component].builddirs.append(self._module_subfolder)
+
+            self.cpp_info.components[component].names["cmake_find_package"] = component
+            self.cpp_info.components[component].names["cmake_find_package_multi"] = component
+            self.cpp_info.components[component].build_modules["cmake_find_package"].extend([
+                self._alias_module_file_rel_path,
+                self._old_alias_module_file_rel_path,
+            ])
+            self.cpp_info.components[component].build_modules["cmake_find_package_multi"].extend([
+                self._alias_module_file_rel_path,
+                self._old_alias_module_file_rel_path,
+            ])
+
+            if self.options.use_llvm_cmake_files:
+                self.cpp_info.components[component].build_modules["cmake_find_package"].append(
+                    os.path.join(self._module_subfolder, "LLVMConfigInternal.cmake")
+                )
+                self.cpp_info.components[component].build_modules["cmake_find_package_multi"].append(
+                    os.path.join(self._module_subfolder, "LLVMConfigInternal.cmake")
+                )
+
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
+        self.cpp_info.names["cmake_find_package"] = "LLVM"
+        self.cpp_info.names["cmake_find_package_multi"] = "LLVM"
