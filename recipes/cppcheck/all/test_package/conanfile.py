cc3e30619754dfc6826f5b26112ba8fbe7a3846e
Martin Delille <martin@delille.org>
2022-04-20 20:33:42 +0200

    Check again dummy cpp file

--- recipes/cppcheck/all/test_package/conanfile.py
+++ recipes/cppcheck/all/test_package/conanfile.py
@@ -1,13 +1,17 @@
 from conans import ConanFile, tools
+import os
+import shutil
 import sys
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch"
+    exports_sources = "file_to_check.cpp"
 
     def test(self):
+        shutil.copy(os.path.join(self.source_folder, "file_to_check.cpp"), self.build_folder)
         if not tools.cross_building(self.settings):
-            self.run("cppcheck --version", run_environment=True)
+            self.run("cppcheck .", run_environment=True)
             # On windows we need to explicitly use python to run the python script
             if self.settings.os == 'Windows':
                 self.run("{} {} -h".format(sys.executable, tools.get_env("CPPCHECK_HTMLREPORT")))
