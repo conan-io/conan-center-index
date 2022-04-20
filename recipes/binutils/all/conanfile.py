43dc2fa5fa457f8391cfdebf8a70e6774d5adbf4
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-04-21 01:36:55 +0200

    binutils: bump to 2.38 + test more

--- recipes/binutils/all/conanfile.py
+++ recipes/binutils/all/conanfile.py
@@ -1,70 +1,154 @@
-import os
-import re
-from glob import glob
-
-from conan import ConanFile
-from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
-from conans import tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
 from conans.errors import ConanInvalidConfiguration
+import functools
+import os
 
-required_conan_version = ">=1.43.0"
+
+required_conan_version = ">=1.40.0"
 
 
 class BinutilsConan(ConanFile):
     name = "binutils"
-    description = "a collection of binary tools"
-    topics = ("gnu", "compilation", "tools")
-    url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://www.gnu.org/software/binutils"
+    description = "The GNU Binutils are a collection of binary tools."
     license = "GPL-2.0-or-later"
-
+    url = "https://github.com/conan-io/conan-center-index/"
+    homepage = "https://www.gnu.org/software/binutils"
+    topics = ("binutils", "ld", "linker", "as", "assembler", "objcopy", "objdump")
     settings = "os", "arch", "compiler", "build_type"
 
-    _source_subfolder = "source_subfolder"
+    options = {
+        "multilib": [True, False],
+        "with_libquadmath": [True, False],
+        "target_arch": "ANY",
+        "target_os": "ANY"
+    }
+
+    default_options = {
+        "multilib": True,
+        "with_libquadmath": True,
+        "target_arch": None,  # Initialized in config_options
+        "target_os": None,  # Initialized in config_options
+    }
+
+    def export_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
+    def config_options(self):
+        del self.settings.compiler.cppstd
+        del self.settings.compiler.libcxx
+        self.options.target_arch = str(getattr(self, "settings_target", self.settings).arch)
+        self.options.target_os = str(getattr(self, "settings_target", self.settings).os)
 
     def validate(self):
-        if self.settings.os not in ["Linux", "FreeBSD"]:
+        if self.settings.os not in ("Linux", "FreeBSD"):
             raise ConanInvalidConfiguration("This recipes supports only Linux and FreeBSD")
+
+        # Check whether the actual target_arch and target_os option are valid conan options
+        # FIXME: does there exist a stable Conan API to accomplish this?
+        if self.options.target_arch not in self.settings.arch.values_range:
+            raise ConanInvalidConfiguration(f"target_arch={self.options.target_arch} is invalid (possibilities={self.settings.arch.values_range})")
+        if self.options.target_os not in self.settings.os.values_range:
+            raise ConanInvalidConfiguration(f"target_os={self.options.target_os} is invalid (possibilities={self.settings.os.values_range})")
+        if self._triplet_target is None:
+            self._raise_unsupported_configuration("everython", "not ok")
 
     def package_id(self):
         del self.info.settings.compiler
 
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
+    def _raise_unsupported_configuration(self, key, value):
+        raise ConanInvalidConfiguration(f"This configuration is unsupported by this conan recip. Please consider adding support. ({key}={value})")
+
+    _triplet_arch_lut = {
+        "x86": "i686",
+        "x86_64": "x86_64",
+        "armv8": "aarch64",
+    }
+
+    @property
+    def _triplet_arch_target(self):
+        return self._triplet_arch_lut[str(self.options.target_arch)]
+
+    _triplet_os_lut = {
+        "FreeBSD": "freebsd",
+        "Linux": "linux",
+        "MacOS": "darwin",
+        "Windows": "mingw32",
+    }
+
+    @property
+    def _triplet_os_part(self):
+        return self._triplet_os_lut[str(self.options.target_os)]
+
+    _vendor_default = {
+        "Windows": "w64",
+    }
+
+    @property
+    def _triplet_vendor_part(self):
+        return self._vendor_default.get(str(self.options.target_os), "cci")
+
+    @property
+    def _triplet_target(self):
+        return "{}-{}-{}".format(self._triplet_arch_target, self._triplet_vendor_part, self._triplet_os_part)
+
+    def build_requirements(self):
+        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/cci.latest")
+
     def requirements(self):
-        self.requires("zlib/1.2.11")
-        self.requires("readline/8.0")
-
-    def generate(self):
-        ad = AutotoolsDeps(self)
-        # this can be removed if conan >= 1.44.0 due to https://github.com/conan-io/conan/pull/10192
-        for m in re.finditer("-Wl,-rpath,\"[^\"]+\"", ad.vars()["LDFLAGS"]):
-            ad.environment.remove("LDFLAGS", m[0])
-        ad.generate()
-
-        ac = AutotoolsToolchain(self)
-        ac.default_configure_install_args = True
-        ac.configure_args.extend(["--with-system-zlib", "--with-system-readline"])
-        ac.generate()
+        self.requires("zlib/1.2.12")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  strip_root=True, destination=self._source_subfolder)
+
+    @property
+    def _exec_prefix(self):
+        return os.path.join(self.package_folder, "bin", "exec_prefix")
+
+    @functools.lru_cache(1)
+    def _configure_autotools(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        yes_no = lambda tf : "yes" if tf else "no"
+        conf_args = [
+            "--target={}".format(self._triplet_target),
+            "--enable-multilib={}".format(yes_no(self.options.multilib)),
+            "--with-system-zlib",
+            "--disable-nls",
+            "exec_prefix={}".format(tools.unix_path(self._exec_prefix)),
+        ]
+        autotools.configure(args=conf_args, configure_dir=self._source_subfolder)
+        return autotools
 
     def build(self):
-        at = Autotools(self)
-        at.configure(build_script_folder=self._source_subfolder)
-        at.make()
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        autotools = self._configure_autotools()
+        autotools.make()
 
     def package(self):
-        self.copy(pattern="COPYING*", dst="licenses", src=self._source_subfolder)
+        self.copy("COPYING*", src=self._source_subfolder, dst="licenses")
+        autotools = self._configure_autotools()
+        autotools.install()
 
-        at = Autotools(self)
-        at.install()
-
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
         tools.rmdir(os.path.join(self.package_folder, "share"))
-        for dir in glob(os.path.join(self.package_folder, "{}*".format(self.settings.arch))):
-            tools.rmdir(dir)
 
     def package_info(self):
         bindir = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH environment variable: {}".format(bindir))
         self.env_info.PATH.append(bindir)
+
+        target_bindir = os.path.join(self._exec_prefix, self._triplet_target, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(target_bindir))
+        self.env_info.PATH.append(target_bindir)
+
+        self.user_info.gnu_triplet = self._triplet_target
--- recipes/binutils/all/test_package/conanfile.py
+++ recipes/binutils/all/test_package/conanfile.py
@@ -1,11 +1,88 @@
-from conan import ConanFile
-from conans import tools
+from conans import ConanFile, tools
+from conans.errors import ConanException
+from io import StringIO
+import os
+import shlex
 
 
 class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
 
-    settings = "os"
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
+    @property
+    def _target_arch(self):
+        return str(self.options["binutils"].target_arch)
+
+    @property
+    def _target_os(self):
+        return str(self.options["binutils"].target_os)
+
+    @property
+    def _test_linker_args(self):
+        args = []
+        if self._target_os == "Windows":
+            args.extend(["--subsystem", "console", f"{self.build_folder}/lib/libkernel32.a"])
+        return args
+
+    @property
+    def _test_package_assembly_source(self):
+        return os.path.join(self.source_folder, f"{self._target_os}-{self._target_arch}.s")
+
+    def build(self):
+        if not tools.cross_building(self):
+
+            if not os.path.isfile(self._test_package_assembly_source):
+                self.output.warn(f"Test is missing a test for this target ({self._test_package_assembly_source}. Please consider adding one. (It's a great learning experience)")
+            else:
+                tools.mkdir(os.path.join(self.build_folder, "bin"))
+                tools.mkdir(os.path.join(self.build_folder, "lib"))
+
+                gas = f"{self.deps_user_info['binutils'].gnu_triplet}-as"
+                ld = f"{self.deps_user_info['binutils'].gnu_triplet}-ld"
+                extension = ""
+                if self._target_os == "Windows":
+                    extension = ".exe"
+
+                    # Create minimum import library for kernel32.dll
+                    dlltool = f"{self.deps_user_info['binutils'].gnu_triplet}-dlltool"
+
+                    dlltool_args = [dlltool, "--input-def", f"{self.source_folder}/Windows-kernel32.def", "--output-lib", f"{self.build_folder}/lib/libkernel32.a"]
+                    self.run(shlex.join(dlltool_args))
+
+
+                assembler_args = [gas, self._test_package_assembly_source, "-o", f"{self.build_folder}/object.o"]
+                linker_args = [ld, f"{self.build_folder}/object.o", "-o", f"{self.build_folder}/bin/test_package{extension}"] + self._test_linker_args
+
+                self.run(shlex.join(assembler_args))
+                self.run(shlex.join(linker_args))
+
+    def _can_run_target(self):
+        if self._settings_build.os != self._target_os:
+            return False
+        if self._settings_build.arch == "x86_64":
+            return self._target_arch in ("x86", "x86_64")
+        return self._settings_build.arch == self._target_arch
 
     def test(self):
         if not tools.cross_building(self):
-            self.run("as --version")
+            if self._can_run_target() and os.path.isfile(self._test_package_assembly_source):
+                output = StringIO()
+                self.run(os.path.join("bin", "test_package"), output=output)
+                text = output.getvalue()
+                print(text)
+                assert "Hello, world!" in text
+
+            bins = ["ar", "as", "ld", "nm", "objcopy", "objdump", "ranlib", "readelf", "strip"]
+            for bin in bins:
+                bin_path = os.path.realpath(tools.which(bin))
+                print(f"Found {bin} at {bin_path}")
+                assert bin_path.startswith(self.deps_cpp_info["binutils"].rootpath)
+
+                output = StringIO()
+                self.run("{} --version".format(bin_path), run_environment=True, output=output)
+                text = output.getvalue()
+                print(text)
+                assert str(self.requires["binutils"].ref.version) in text
