719622a2f299bece81ab4a8223073980d1203031
Dmitry Baryshev <dima8w@gmail.com>
2022-04-20 04:11:16 +0300

    Update recipes/sail/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/sail/all/conanfile.py
+++ recipes/sail/all/conanfile.py
@@ -122,6 +122,8 @@ class SAILConan(ConanFile):
         self.cpp_info.components["sail-manip"].libs = ["sail-manip"]
         self.cpp_info.components["sail-manip"].requires = ["sail-common"]
 
+        self.cpp_info.components["sail-c++"].set_property("cmake_target_name", "SAIL::SailC++")
+        self.cpp_info.components["sail-c++"].set_property("pkg_config_name", "libsail-c++")
         self.cpp_info.components["sail-c++"].names["cmake_find_package"]       = "SailC++"
         self.cpp_info.components["sail-c++"].names["cmake_find_package_multi"] = "SailC++"
         self.cpp_info.components["sail-c++"].libs = ["sail-c++"]
