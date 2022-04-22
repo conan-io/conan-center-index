3413eee26b9ef1460827a80ae69a5a9a10abe8c6
SSE4 <tomskside@gmail.com>
2022-04-22 07:28:20 -0700

    Update recipes/sdl_mixer/all/conanfile.py
    Co-authored-by: Carlos Zoido <mrgalleta@gmail.com>

--- recipes/sdl_mixer/all/conanfile.py
+++ recipes/sdl_mixer/all/conanfile.py
@@ -133,6 +133,8 @@ class SDLMixerConan(ConanFile):
         cmake.install()
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "SDL2_mixer")
+        self.cpp_info.set_property("cmake_target_name", "SDL2_mixer::SDL2_mixer")
         self.cpp_info.set_property("pkg_config_name", "SDL2_mixer")
         self.cpp_info.libs = ["SDL2_mixer"]
         self.cpp_info.includedirs = [os.path.join("include", "SDL2")]
