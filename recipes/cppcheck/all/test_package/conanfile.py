af939a7d349f9d490faeefa01846f6a75c7b2565
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-04-22 12:20:41 +0200

    cppcheck/*: Test against dummy file

--- recipes/cppcheck/all/test_package/conanfile.py
+++ recipes/cppcheck/all/test_package/conanfile.py
@@ -7,7 +7,8 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            self.run("cppcheck --version", run_environment=True)
+            self.run("cppcheck --enable=warning,style,performance --std=c++11 ./file_to_check.cpp",
+                    cwd=self.source_folder, run_environment=True)
             # On windows we need to explicitly use python to run the python script
             if self.settings.os == 'Windows':
                 self.run("{} {} -h".format(sys.executable, tools.get_env("CPPCHECK_HTMLREPORT")))
