abde1c622101064576b51abc41c21b0a912a4d19
bincrafters-user <bincrafters@gmail.com>
2022-04-22 10:21:17 +0000

    cppcheck: Update Conan conventions
    Automatically created by bincrafters-conventions 0.31.0

--- recipes/cppcheck/all/test_package/conanfile.py
+++ recipes/cppcheck/all/test_package/conanfile.py
@@ -6,7 +6,7 @@ class TestPackageConan(ConanFile):
     settings = "os", "arch"
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             self.run("cppcheck --enable=warning,style,performance --std=c++11 ./file_to_check.cpp",
                     cwd=self.source_folder, run_environment=True)
             # On windows we need to explicitly use python to run the python script
