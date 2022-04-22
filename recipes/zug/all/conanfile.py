83f597d0ddac158d9e19ea4e24488eb34ce76b83
Uilian Ries <uilianries@gmail.com>
2022-04-22 14:02:46 +0200

    Improve zug
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/zug/all/conanfile.py
+++ recipes/zug/all/conanfile.py
@@ -1,19 +1,17 @@
 import os
-from pprint import pprint
-
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 
 
 class ZugConan(ConanFile):
     name = "zug"
-    version = "0.0.1"
-    license = "Boost Software License 1.0"
+    license = "BSL-1.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://sinusoid.es/zug/"
     description = "Transducers for C++ — Clojure style higher order push/pull \
         sequence transformations"
-    topics = ("zug",)
-    settings = ("compiler",)
+    topics = ("transducer", "algorithm", "signals")
+    settings = ("compiler", "arch", "os", "build_type")
     no_copy_source = True
 
     def source(self):
@@ -23,15 +21,31 @@ class ZugConan(ConanFile):
             destination=self.source_folder
         )
 
-    def build(self):
-        pass  # header only
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "Visual Studio": "15",
+            "gcc": "5",
+            "clang": "3.5",
+            "apple-clang": "10"
+        }
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, "14")
+
+        compiler = str(self.settings.compiler)
+        if compiler not in self._compilers_minimum_version:
+            self.output.warn("Unknown compiler, assuming it supports at least C++14")
+            return
+
+        version = tools.Version(self.settings.compiler.version)
+        if version < self._compilers_minimum_version[compiler]:
+            raise ConanInvalidConfiguration(f"{self.name} requires a compiler that supports at least C++14")
+
+    def package_id(self):
+        self.info.header_only()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self.source_folder)
-        self.copy(
-            pattern="*", dst="include/zug", src=os.path.join(self.source_folder, "zug")
-        )
-
-    def package_info(self):
-        if self.settings.compiler in ["Visual Studio", "msvc"]:
-            self.cpp_info.cxxflags = ["/Zc:externConstexpr"]
+        self.copy(pattern="*.hpp", dst=os.path.join("include", "zug"), src=os.path.join(self.source_folder, "zug"))
