52def319de88d359c6568b3a4ff31f01338e979a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-04-23 23:44:51 +0200

    zib is not a direct dependency of aruco

--- recipes/aruco/3.x.x/conanfile.py
+++ recipes/aruco/3.x.x/conanfile.py
@@ -45,7 +45,6 @@ class ArucoConan(ConanFile):
     def requirements(self):
         self.requires("opencv/4.5.5")
         self.requires("eigen/3.4.0")
-        self.requires("zlib/1.2.12")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
