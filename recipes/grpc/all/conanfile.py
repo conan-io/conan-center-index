4532bda0504b8dc5d23e98798f81f99a3908cb47
jgsogo <jgsogo@gmail.com>
2022-04-25 22:54:23 +0200

    cxx std inherited from abseil targets

--- recipes/grpc/all/conanfile.py
+++ recipes/grpc/all/conanfile.py
@@ -153,12 +153,6 @@ class grpcConan(ConanFile):
         self._cmake.definitions["gRPC_BUILD_GRPC_PYTHON_PLUGIN"] = self.options.python_plugin
         self._cmake.definitions["gRPC_BUILD_GRPC_RUBY_PLUGIN"] = self.options.ruby_plugin
 
-        # Some compilers will start defaulting to C++17, so abseil will be built using C++17
-        # gRPC will force C++11 if CMAKE_CXX_STANDARD is not defined
-        # So, if settings.compiler.cppstd is not defined there will be a mismatch
-        if not tools.valid_min_cppstd(self, 11):
-            self._cmake.definitions["CMAKE_CXX_STANDARD"] = 11
-
         if tools.cross_building(self):
             # otherwise find_package() can't find config files since
             # conan doesn't populate CMAKE_FIND_ROOT_PATH
