e08beac46b5de2083899b65d2db3c49f6e874085
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-04-26 13:59:07 +0200

    gtk 4.6.2

--- recipes/gtk/all/conanfile.py
+++ recipes/gtk/all/conanfile.py
@@ -171,7 +171,7 @@ class GtkConan(ConanFile):
             tools.patch(**patch)
         if self._gtk3:
             tools.replace_in_file(os.path.join(self._source_subfolder, "meson.build"), "\ntest(\n", "\nfalse and test(\n")
-        if tools.Version(self.version) >= "4.2.0":
+        if "4.2.0" <= tools.Version(self.version) < "4.6.1":
             tools.replace_in_file(os.path.join(self._source_subfolder, "meson.build"),
                                   "gtk_update_icon_cache: true",
                                   "gtk_update_icon_cache: false")
