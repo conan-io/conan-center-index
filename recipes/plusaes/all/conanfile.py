a65053c10496c0a20ceb0c13e5358a930e6ad524
toge <toge.mail@gmail.com>
2022-04-29 04:44:10 +0900

    (#10516) plusaes: add recipe
    * plusaes: add recipe
    
    * add C++11 flag

---
+++ recipes/plusaes/all/conanfile.py
@@ -0,0 +1,35 @@
+from conans import ConanFile, tools
+import os
+
+required_conan_version = ">=1.33.0"
+
+class PlusaesConan(ConanFile):
+    name = "plusaes"
+    description = "Header only C++ AES cipher library"
+    topics = ("encryption", "header-only")
+    license = "BSL-1.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/kkAyataka/plusaes"
+    settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def package_id(self):
+        self.info.header_only()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 11)
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    def package(self):
+        root_dir = self._source_subfolder
+        include_dir = os.path.join(root_dir, "include")
+        self.copy(pattern="LICENSE_1_0.txt", dst="licenses", src=root_dir)
+        self.copy(pattern="*plusaes.hpp", dst="include", src=include_dir)
