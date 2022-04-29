3963ee689684388db1eb80dd560c3e73cc5d6443
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-04-29 11:29:16 +0200

    bump libxml2

--- recipes/libxslt/all/conanfile.py
+++ recipes/libxslt/all/conanfile.py
@@ -64,7 +64,7 @@ class LibxsltConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def requirements(self):
-        self.requires("libxml2/2.9.12")
+        self.requires("libxml2/2.9.13")
 
     def validate(self):
         if self.options.plugins and not self.options.shared:
