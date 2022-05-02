7f2b22de6a902ad12fadaed4a87e2a7ada8565c6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-05-02 14:45:50 +0200

    modernize

--- recipes/glbinding/all/test_package/conanfile.py
+++ recipes/glbinding/all/test_package/conanfile.py
@@ -7,7 +7,7 @@ class TestPackageConan(ConanFile):
     generators = "cmake", "cmake_find_package_multi"
 
     def requirements(self):
-        self.requires("glfw/3.3.2")
+        self.requires("glfw/3.3.7")
 
     def build(self):
         cmake = CMake(self)
@@ -15,6 +15,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
