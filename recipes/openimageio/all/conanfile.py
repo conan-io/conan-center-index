5882471bf4f255c4f48069eb44235cdd3a7522d9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-05-03 17:27:08 +0200

    propagate onetbb in package_info() instead of tbb

--- recipes/openimageio/all/conanfile.py
+++ recipes/openimageio/all/conanfile.py
@@ -252,7 +252,7 @@ class OpenImageIOConan(ConanFile):
         if self.options.with_opencv:
             self.cpp_info.components["main"].requires.append("opencv::opencv")
         if self.options.with_tbb:
-            self.cpp_info.components["main"].requires.append("tbb::tbb")
+            self.cpp_info.components["main"].requires.append("onetbb::onetbb")
         if self.options.with_dicom:
             self.cpp_info.components["main"].requires.append("dcmtk::dcmtk")
         if self.options.with_ffmpeg:
