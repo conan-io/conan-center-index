f6199264c74b7693d64b2cc68ac4072d308a1b32
toge <toge.mail@gmail.com>
2022-05-05 02:20:29 +0900

    fix indent

--- recipes/uwebsockets/all/test_package/test_package.cpp
+++ recipes/uwebsockets/all/test_package/test_package.cpp
@@ -6,13 +6,13 @@ int main()
 {
   	uWS::App app;
 
-	app
-	.get("/", [](uWS::HttpResponse<false> *res, uWS::HttpRequest* req) {
-		res->end("Hello world!");
-	})
-	.listen(3000, [](auto* token) {
-		if (token) {
-			std::cout << "Serving over HTTP a 3000." << std::endl; 
-		}
-	});
+  	app
+  	.get("/", [](uWS::HttpResponse<false> *res, uWS::HttpRequest* req) {
+  	  	res->end("Hello world!");
+  	})
+  	.listen(3000, [](auto* token) {
+  	  	if (token) {
+  	  	  	std::cout << "Serving over HTTP a 3000." << std::endl; 
+  	  	}
+  	});
 }
