d18fbd7b87eecfd20d3abf36527a6eaceeeacbc4
Eric Lemanissier <eric.lemanissier@gmail.com>
2022-05-05 12:49:24 +0200

    fix test

--- recipes/gfortran/all/test_package/conanfile.py
+++ recipes/gfortran/all/test_package/conanfile.py
@@ -1,13 +1,22 @@
 from conans import ConanFile, tools
+import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
+    @property
+    def _source(self):
+        return os.path.join(self.source_folder, "hello.f90")
+
+    @property
+    def _exe(self):
+        return "hello%s" % (".exe" if self.settings.os == "Windows" else "")
+
     def build(self):
         if not tools.cross_building(self):
             self.run("gfortran --version", run_environment=True)
-            self.run("gfortran hello.f90 -o hello%s" % (".exe" if self.settings.os == "Windows" else ""), run_environment=True)
+            self.run("gfortran %s -o %s" % (self._source, self._exe), run_environment=True)
 
     def test(self):
         if not tools.cross_building(self):
-            self.run("hello%s" % (".exe" if self.settings.os == "Windows" else ""), run_environment=True)
+            self.run(self._exe, run_environment=True)
