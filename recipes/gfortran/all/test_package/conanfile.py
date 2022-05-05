21dce83252f041e3a979f3b31c46acf1b7b502fe
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-05-05 12:14:28 +0200

    test: actually compile and run a program

--- recipes/gfortran/all/test_package/conanfile.py
+++ recipes/gfortran/all/test_package/conanfile.py
@@ -3,6 +3,11 @@ from conans import ConanFile, tools
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
-    def test(self):
+    def build(self):
         if not tools.cross_building(self):
             self.run("gfortran --version", run_environment=True)
+            self.run("gfortran hello.f90 -o hello%s" % (".exe" if self.settings.os == "Windows" else ""), run_environment=True)
+
+    def test(self):
+        if not tools.cross_building(self):
+            self.run("hello%s" % (".exe" if self.settings.os == "Windows" else ""), run_environment=True)
---
+++ recipes/gfortran/all/test_package/hello.f90
@@ -0,0 +1,4 @@
+program hello
+  ! This is a comment line; it is ignored by the compiler
+  print *, 'Hello, World!'
+end program hello
