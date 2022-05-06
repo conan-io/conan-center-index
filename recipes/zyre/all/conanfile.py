7bd15c9421f0fd2aa028bbc7c6434df90d0443d0
toge <toge.mail@gmail.com>
2022-05-06 10:16:02 +0900

    remove share folder

--- recipes/zyre/all/conanfile.py
+++ recipes/zyre/all/conanfile.py
@@ -72,8 +72,8 @@ class ZyreConan(ConanFile):
                   dst="licenses")
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "share", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig",))
+        tools.rmdir(os.path.join(self.package_folder, "share",))
 
     def package_info(self):
         self.cpp_info.names["pkg_config"] = "libzyre"
