3072b55d1d1589b76ba3fae7b7c3832fd19cef6f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-05-06 14:59:48 +0200

    modernize

--- recipes/zstr/all/conanfile.py
+++ recipes/zstr/all/conanfile.py
@@ -11,22 +11,22 @@ class ZstrConan(ConanFile):
     topics = ("zstr", "zlib", "compression")
     homepage = "https://github.com/mateidavid/zstr"
     url = "https://github.com/conan-io/conan-center-index"
-    settings = "compiler"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 11)
-
     def requirements(self):
         self.requires("zlib/1.2.12")
 
     def package_id(self):
         self.info.header_only()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 11)
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
