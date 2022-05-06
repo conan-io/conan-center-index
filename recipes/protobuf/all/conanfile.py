5aaaa4b217d25f0ed4c42a225149bece8b1234e9
Sil3ntStorm <Sil3ntStorm@users.noreply.github.com>
2022-05-06 17:57:32 +0200

    Add an option to provide sane protobuf library names without suffix in debug builds

--- recipes/protobuf/all/conanfile.py
+++ recipes/protobuf/all/conanfile.py
@@ -24,6 +24,7 @@ class ProtobufConan(ConanFile):
         "with_zlib": [True, False],
         "with_rtti": [True, False],
         "lite": [True, False],
+        "debug_suffix": [True, False],
     }
     default_options = {
         "shared": False,
@@ -31,6 +32,7 @@ class ProtobufConan(ConanFile):
         "with_zlib": True,
         "with_rtti": True,
         "lite": False,
+        "debug_suffix": True,
     }
 
     short_paths = True
@@ -112,6 +114,8 @@ class ProtobufConan(ConanFile):
         cmake.definitions["protobuf_WITH_ZLIB"] = self.options.with_zlib
         cmake.definitions["protobuf_BUILD_TESTS"] = False
         cmake.definitions["protobuf_BUILD_PROTOC_BINARIES"] = True
+        if not self.options.debug_suffix:
+            cmake.definitions["protobuf_DEBUG_POSTFIX"] = ""
         if tools.Version(self.version) >= "3.14.0":
             cmake.definitions["protobuf_BUILD_LIBPROTOC"] = True
         if self._can_disable_rtti:
@@ -231,7 +235,7 @@ class ProtobufConan(ConanFile):
         self.cpp_info.set_property("cmake_build_modules", build_modules)
 
         lib_prefix = "lib" if (self._is_msvc or self._is_clang_cl) else ""
-        lib_suffix = "d" if self.settings.build_type == "Debug" else ""
+        lib_suffix = "d" if self.settings.build_type == "Debug" and self.options.debug_suffix else ""
 
         # libprotobuf
         self.cpp_info.components["libprotobuf"].set_property("cmake_target_name", "protobuf::libprotobuf")
