cd1a44e2807d5f16279db7fee48f091a952821ee
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-05-06 16:30:07 +0200

    bump boost

--- recipes/quantlib/all/conanfile.py
+++ recipes/quantlib/all/conanfile.py
@@ -44,7 +44,7 @@ class QuantlibConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("boost/1.76.0")
+        self.requires("boost/1.79.0")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
