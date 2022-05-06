81e2f8fb9387419526aa4f3d7a63660460a0da24
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-05-06 15:59:13 +0200

    modernize

--- recipes/vcglib/all/test_package/conanfile.py
+++ recipes/vcglib/all/test_package/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -12,7 +12,7 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             obj_path = os.path.join(self.source_folder, "cube.obj")
             self.run("{} {}".format(bin_path, obj_path), run_environment=True)
