fc5b8cbac5ef7160649aef2c676965a876c06186
Woazim <54191598+Woazim@users.noreply.github.com>
2022-05-09 02:42:12 +0200

    Update recipes/glshaderpp/all/test_package/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/glshaderpp/all/test_package/conanfile.py
+++ recipes/glshaderpp/all/test_package/conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def requirements(self):
         self.requires("glew/2.1.0")
