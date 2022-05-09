a4ad07d2aa1fbb204a1f3bd88e70925d368c6b98
Woazim <54191598+Woazim@users.noreply.github.com>
2022-05-09 02:42:53 +0200

    Update recipes/glshaderpp/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/glshaderpp/all/conanfile.py
+++ recipes/glshaderpp/all/conanfile.py
@@ -20,9 +20,28 @@ class GLShaderPPConan(ConanFile):
     def package_id(self):
         self.info.header_only()
 
+    @property
+    def _minimum_cpp_standard(self):
+        return 17
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "17",
+            "gcc": "7",
+            "clang": "6",
+            "apple-clang": "10",
+        }
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 17)
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn(f"{self.name} recipe lacks information about the {self.compiler} compiler support.")
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration(f"{self.name} requires C++{self._minimum_cpp_standard} support. The current compiler {self.settings.compiler} {self.settings.compiler.version} does not support it.".)
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
