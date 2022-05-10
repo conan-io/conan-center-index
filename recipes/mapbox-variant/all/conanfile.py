a40f26f906862428dd01f9bc4b90002cb6337c2e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-05-10 15:24:28 +0200

    (#10646) add mapbox-variant/1.2.0

---
+++ recipes/mapbox-variant/all/conanfile.py
@@ -0,0 +1,32 @@
+from conans import ConanFile, tools
+import os
+
+
+class MapboxVariantConan(ConanFile):
+    name = "mapbox-variant"
+    description = "An header-only alternative to boost::variant for C++11 and C++14"
+    topics = ("variant")
+    license = "BSD-3-Clause"
+    homepage = "https://github.com/mapbox/variant"
+    url = "https://github.com/conan-io/conan-center-index"
+    settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def package_id(self):
+        self.info.header_only()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 11)
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy("*", dst="include", src=os.path.join(self._source_subfolder, "include"))
