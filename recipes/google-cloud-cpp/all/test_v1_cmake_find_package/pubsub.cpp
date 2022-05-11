ed2436b0f8f21204a4c93d0d83ab8fa424c412d0
jgsogo <jgsogo@gmail.com>
2022-05-11 08:01:02 +0200

    same test sources@

--- recipes/google-cloud-cpp/all/test_v1_cmake_find_package/pubsub.cpp
+++ recipes/google-cloud-cpp/all/test_v1_cmake_find_package/pubsub.cpp
@@ -4,7 +4,7 @@
 
 namespace pubsub = ::google::cloud::pubsub;
 
-int main(int argc, char *argv[]) try
+int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
@@ -15,20 +15,15 @@ int main(int argc, char *argv[]) try
     std::string const project_id = argv[1];
     std::string const topic_id = argv[2];
 
-    // Create a namespace alias to make the code easier to read.
     auto publisher = pubsub::Publisher(pubsub::MakePublisherConnection(pubsub::Topic(project_id, topic_id), {}));
-    auto id = publisher.Publish(pubsub::MessageBuilder{}.SetData("Hello World!").Build()).get();
-
-    if (!id)
-    {
-        throw std::runtime_error(id.status().message());
-    }
-    std::cout << "Hello World published with id=" << *id << "\n";
-
-    return 0;
-}
-catch (std::exception const &ex)
-{
-    std::cerr << "Standard exception raised: " << ex.what() << "\n";
+    // FIXME: It needs working credentials
+    // auto id = publisher.Publish(pubsub::MessageBuilder{}.SetData("Hello World!").Build()).get();
+    // if (!id)
+    // {
+    //     std::cerr << "Failed to send message\n";
+    //     return 0;
+    // }
+    //std::cout << "Hello World published with id=" << *id << "\n";
+    std::cout << "pubsub test done\n";
     return 0;
 }
