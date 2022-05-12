a599245b932e8d18a7e534188f89be9b5b7fbb39
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-05-12 14:11:24 +0200

    modernize

--- recipes/dlib/all/conanfile.py
+++ recipes/dlib/all/conanfile.py
@@ -1,7 +1,10 @@
-import os
-
+from conan.tools.microsoft import is_msvc
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
+import functools
+import os
+
+required_conan_version = ">=1.45.0"
 
 
 class DlibConan(ConanFile):
@@ -11,8 +14,6 @@ class DlibConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "http://dlib.net"
     license = "BSL-1.0"
-    exports_sources = "CMakeLists.txt"
-    generators = "cmake"
 
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -36,10 +37,10 @@ class DlibConan(ConanFile):
         "with_sse4": "auto",
         "with_avx": "auto",
         "with_openblas": True,
-
     }
 
-    _cmake = None
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
 
     @property
     def _source_subfolder(self):
@@ -60,12 +61,6 @@ class DlibConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
-        if self.settings.compiler == "Visual Studio" and self.options.shared:
-            raise ConanInvalidConfiguration("dlib can not be built as a shared library with Visual Studio")
-
-    def validate(self):
-        if self.settings.os == "Macos" and self.settings.arch == "armv8":
-            raise ConanInvalidConfiguration("dlib doesn't support macOS M1")
 
     def requirements(self):
         if self.options.with_gif:
@@ -77,36 +72,40 @@ class DlibConan(ConanFile):
         if self.options.with_openblas:
             self.requires("openblas/0.3.17")
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+    def validate(self):
+        if is_msvc(self) and self.options.shared:
+            raise ConanInvalidConfiguration("dlib can not be built as a shared library with Visual Studio")
+        if self.settings.os == "Macos" and self.settings.arch == "armv8":
+            raise ConanInvalidConfiguration("dlib doesn't support macOS M1")
 
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    @functools.lru_cache(1)
     def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
+        cmake = CMake(self)
 
         # With in-project builds dlib is always built as a static library,
         # we want to be able to build it as a shared library too
-        self._cmake.definitions["DLIB_IN_PROJECT_BUILD"] = "OFF"
+        cmake.definitions["DLIB_IN_PROJECT_BUILD"] = False
 
         # Configure external dependencies
-        self._cmake.definitions["DLIB_GIF_SUPPORT"] = self.options.with_gif
-        self._cmake.definitions["DLIB_JPEG_SUPPORT"] = self.options.with_jpeg
-        self._cmake.definitions["DLIB_PNG_SUPPORT"] = self.options.with_png
+        cmake.definitions["DLIB_GIF_SUPPORT"] = self.options.with_gif
+        cmake.definitions["DLIB_JPEG_SUPPORT"] = self.options.with_jpeg
+        cmake.definitions["DLIB_PNG_SUPPORT"] = self.options.with_png
 
         # Configure SIMD options if possible
         if self.settings.arch in ["x86", "x86_64"]:
             if self.options.with_sse2 != "auto":
-                self._cmake.definitions["USE_SSE2_INSTRUCTIONS"] = self.options.with_sse2
+                cmake.definitions["USE_SSE2_INSTRUCTIONS"] = self.options.with_sse2
             if self.options.with_sse4 != "auto":
-                self._cmake.definitions["USE_SSE4_INSTRUCTIONS"] = self.options.with_sse4
+                cmake.definitions["USE_SSE4_INSTRUCTIONS"] = self.options.with_sse4
             if self.options.with_avx != "auto":
-                self._cmake.definitions["USE_AVX_INSTRUCTIONS"] = self.options.with_avx
+                cmake.definitions["USE_AVX_INSTRUCTIONS"] = self.options.with_avx
 
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
 
     def build(self):
         cmake = self._configure_cmake()
@@ -128,9 +127,13 @@ class DlibConan(ConanFile):
             tools.rmdir(os.path.join(self.package_folder, dir_to_remove))
 
     def package_info(self):
-        self.cpp_info.names["pkg_config"] = "dlib-1"
+        self.cpp_info.set_property("cmake_file_name", "dlib")
+        self.cpp_info.set_property("cmake_target_name", "dlib::dlib")
+        self.cpp_info.set_property("pkg_config_name", "dlib-1")
         self.cpp_info.libs = tools.collect_libs(self)
-        if self.settings.os == "Linux":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs = ["pthread"]
         elif self.settings.os == "Windows":
             self.cpp_info.system_libs = ["ws2_32", "winmm", "comctl32", "gdi32", "imm32"]
+
+        self.cpp_info.names["pkg_config"] = "dlib-1"
