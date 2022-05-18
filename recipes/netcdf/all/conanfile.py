1cbe806a1cbab77c78da1031a7ae31ae7b64e53e
Paul Harris <paulharris@computer.org>
2022-05-19 00:17:28 +0800

    Upgrade to 4.8.1.  Fix up patches for 4.7.4.

--- recipes/netcdf/all/conanfile.py
+++ recipes/netcdf/all/conanfile.py
@@ -24,6 +24,7 @@ class NetcdfConan(ConanFile):
         "with_hdf5": [True, False],
         "cdf5": [True, False],
         "dap": [True, False],
+        "byterange": [True, False], # added to recipe when upgraded to 4.8.1
     }
     default_options = {
         "shared": False,
@@ -32,6 +33,7 @@ class NetcdfConan(ConanFile):
         "with_hdf5": True,
         "cdf5": True,
         "dap": True,
+        "byterange": True,
     }
 
     generators = "cmake_find_package", "cmake_find_package_multi", "cmake"
@@ -66,9 +68,18 @@ class NetcdfConan(ConanFile):
 
     def requirements(self):
         if self._with_hdf5:
-            self.requires("hdf5/1.12.0")
-        if self.options.dap:
-            self.requires("libcurl/7.80.0")
+            if self.version == "4.7.4":
+                # 4.7.4 was built and tested with hdf5/1.12.0
+                # It would be nice to upgrade to 1.12.1,
+                # but when the byterange feature is enabled,
+                # it triggers a compile error that was later patched in 4.8.x
+                # So we will require the older hdf5 to keep the older behaviour.
+                self.requires("hdf5/1.12.0")
+            else:
+                self.requires("hdf5/1.12.1")
+
+        if self.options.dap or self.options.byterange:
+            self.requires("libcurl/7.83.1")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
@@ -81,10 +92,12 @@ class NetcdfConan(ConanFile):
         self._cmake.definitions["BUILD_TESTING"] = False
         self._cmake.definitions["BUILD_UTILITIES"] = False
         self._cmake.definitions["ENABLE_TESTS"] = False
+        self._cmake.definitions["ENABLE_FILTER_TESTING"] = False
 
         self._cmake.definitions["ENABLE_NETCDF_4"] = self.options.netcdf4
         self._cmake.definitions["ENABLE_CDF5"] = self.options.cdf5
         self._cmake.definitions["ENABLE_DAP"] = self.options.dap
+        self._cmake.definitions["ENABLE_BYTERANGE"] = self.options.byterange
         self._cmake.definitions["USE_HDF5"] = self.options.with_hdf5
         self._cmake.definitions["NC_FIND_SHARED_LIBS"] = self.options.with_hdf5 and self.options["hdf5"].shared
         self._cmake.configure(build_folder=self._build_subfolder)
@@ -122,7 +135,7 @@ class NetcdfConan(ConanFile):
         self.cpp_info.components["libnetcdf"].libs = ["netcdf"]
         if self._with_hdf5:
             self.cpp_info.components["libnetcdf"].requires.append("hdf5::hdf5")
-        if self.options.dap:
+        if self.options.dap or self.options.byterange:
             self.cpp_info.components["libnetcdf"].requires.append("libcurl::libcurl")
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["libnetcdf"].system_libs = ["dl", "m"]
