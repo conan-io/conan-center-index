40260ae1f1da027d87f0bd58b066eba66a186393
ggulgulia <gajendra.gulgulia@gmail.com>
2022-05-18 10:24:22 +0200

    feat: official upstream fixes + expose utilities component
    gazebo community migrated the github repositories of ignition-* packages
    causing all the sha256 to brake and the urls are redirected. This commit
    fixes the urls, sha256. Ref: https://community.gazebosim.org/t/a-new-era-for-gazebo/1356
    
    Additionally exposing the utilities components in ignition-cmake which
    is also used in downstream ignition dependencies

--- recipes/ignition-cmake/all/conanfile.py
+++ recipes/ignition-cmake/all/conanfile.py
@@ -2,13 +2,15 @@ import glob
 import os
 
 from conans import CMake, ConanFile, tools
+import conan.tools.files
+import textwrap
 
 
 class IgnitionCmakeConan(ConanFile):
     name = "ignition-cmake"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/ignitionrobotics/ign-cmake"
+    homepage = "https://github.com/gazebosim/gz-cmake"
     description = "A set of CMake modules that are used by the C++-based Ignition projects."
     topics = ("ignition", "robotics", "cmake")
     settings = "os", "compiler", "build_type", "arch"
@@ -30,15 +32,23 @@ class IgnitionCmakeConan(ConanFile):
         self._cmake = CMake(self)
         self._cmake.definitions["CMAKE_INSTALL_DATAROOTDIR"] = "lib"
         self._cmake.definitions["SKIP_component_name"] = False
-
         self._cmake.configure(source_folder=self._source_subfolder)
         return self._cmake
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename(glob.glob("ign-cmake*")[0], self._source_subfolder)
+        version_major = self.version.split(".")[0]
+        conan.tools.files.rename(
+             self, f"gz-cmake-ignition-cmake{version_major}_{self.version}",
+             self._source_subfolder
+            )
+
+
 
     def build(self):
+        version_major = tools.Version(self.version).major
+        ignition_cmake_dir = os.path.join(self.package_folder, "lib", "cmake", f"ignition-cmake{version_major}", "cmake2")
+        os. environ['IGNITION_CMAKE_DIR'] = f"{ignition_cmake_dir}"
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         cmake = self._configure_cmake()
@@ -55,15 +65,47 @@ class IgnitionCmakeConan(ConanFile):
         for file in files:
             if file.endswith(".cmake"):
                 os.remove(os.path.join(cmake_config_files_dir, file))
+        self._create_cmake_module_variables(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            tools.Version(self.version)
+        )
+
+    @staticmethod
+    def _create_cmake_module_variables(module_file, version):
+        # the version info is needed by downstream ignition-dependencies
+        content = textwrap.dedent("""\
+            set(ignition-cmake{major}_VERSION_MAJOR {major})
+            set(ignition-cmake{major}_VERSION_MINOR {minor})
+            set(ignition-cmake{major}_VERSION_PATCH {patch})
+            set(ignition-cmake{major}_VERSION_STRING "{major}.{minor}.{patch}")
+        """.format(major=version.major, minor=version.minor, patch=version.patch))
+        tools.save(module_file, content)
 
     def package_info(self):
         version_major = tools.Version(self.version).major
-        self.cpp_info.names["cmake_find_package_multi"] = "ignition-cmake{}".format(version_major)
-        self.cpp_info.names["cmake_find_package"] = "ignition-cmake{}".format(version_major)
-        self.cpp_info.builddirs = [
-            os.path.join("lib", "cmake", "ignition-cmake{}".format(version_major)),
-            os.path.join("lib", "cmake", "ignition-cmake{}".format(version_major), "cmake{}".format(version_major)),
-        ]
-        self.cpp_info.libdirs.append(os.path.join("lib", "cmake", "ignition-cmake{}".format(version_major), "cmake{}".format(version_major)))
-        self.cpp_info.includedirs.append("include/ignition/cmake{}".format(version_major))
+        ign_cmake_component = f"ignition-cmake{version_major}"
+        base_module_path = os.path.join(self.package_folder, "lib", "cmake", ign_cmake_component)
 
+        self.cpp_info.names["cmake_find_package"] = ign_cmake_component
+        self.cpp_info.names["cmake_find_package_multi"] = ign_cmake_component
+        self.cpp_info.names["cmake_paths"] = ign_cmake_component
+
+        self.cpp_info.components[ign_cmake_component].names["cmake_find_package"] = ign_cmake_component
+        self.cpp_info.components[ign_cmake_component].names["cmake_find_package_multi"] = ign_cmake_component
+        self.cpp_info.components[ign_cmake_component].names["cmake_paths"] = ign_cmake_component
+        self.cpp_info.components[ign_cmake_component].libs = []
+        self.cpp_info.components[ign_cmake_component].builddirs.append(os.path.join(base_module_path, f"cmake{version_major}"))
+
+        self.cpp_info.components["utilities"].names["cmake_find_package"] = "utilities"
+        self.cpp_info.components["utilities"].names["cmake_find_package_multi"] = "utilities"
+        self.cpp_info.components["utilities"].names["cmake_paths"] = "utilities"
+        self.cpp_info.components["utilities"].includedirs.append(f"include/ignition/cmake{version_major}")
+        
+        self.cpp_info.set_property("cmake_build_modules", [self._module_file_rel_path])
+        self.cpp_info.components["utilities"].build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.components["utilities"].build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
+        self.cpp_info.components["utilities"].build_modules["cmake_paths"] = [self._module_file_rel_path]
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join("lib", "cmake", "conan-official-{}-variables.cmake".format(self.name))
