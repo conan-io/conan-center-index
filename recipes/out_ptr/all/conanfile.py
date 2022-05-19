3d506267ab8af13536b0b597ce797fe9578eb208
dvirtz <dvirtz@gmail.com>
2022-05-19 12:11:20 +0000

    add out_ptr

---
+++ recipes/out_ptr/all/conanfile.py
@@ -0,0 +1,32 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conans import tools
+
+
+class OutPtrConan(ConanFile):
+    name = "out_ptr"
+    version = "ea379b2"
+
+    # Optional metadata
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/soasis/out_ptr"
+    description = "a C++11 implementation of std::out_ptr (p1132), as a standalone library"
+    topics = ("utility", "backport")
+    generators = "CMakeToolchain"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def package_id(self):
+        self.info.header_only()
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
---
+++ recipes/out_ptr/all/test_package/conanfile.py
@@ -0,0 +1,17 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake
+from conans import tools
+import os
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self):
+            self.run("test_package", run_environment=True)
