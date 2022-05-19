605b7d915032ed3191bce8f7fc05801e4cf43ae6
Hesham <hesham.essam.mail@gmail.com>
2022-05-19 13:19:08 +0000

    Remove pkgconfig from build requirements of test_package

--- recipes/cairomm/all/test_package/conanfile.py
+++ recipes/cairomm/all/test_package/conanfile.py
@@ -6,9 +6,6 @@ class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
-    def build_requirements(self):
-        self.build_requires("pkgconf/1.7.4")
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
