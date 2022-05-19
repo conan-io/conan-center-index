ee23b5b48ad1f9e870f34955a4b6869af059d9e6
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-05-19 07:28:44 +0200

    Update conanfile.py

--- recipes/qt/5.x.x/conanfile.py
+++ recipes/qt/5.x.x/conanfile.py
@@ -834,7 +834,7 @@ Examples = bin/datadir/examples""")
         filecontents += textwrap.dedent("""\
             if(NOT DEFINED QT_DEFAULT_MAJOR_VERSION)
                 set(QT_DEFAULT_MAJOR_VERSION %s)
-            endif()""" % ver.major)
+            endif()""" % v.major)
         filecontents += 'set(CMAKE_AUTOMOC_MACRO_NAMES "Q_OBJECT" "Q_GADGET" "Q_GADGET_EXPORT" "Q_NAMESPACE" "Q_NAMESPACE_EXPORT")'
         tools.save(os.path.join(self.package_folder, self._cmake_core_extras_file), filecontents)
 
