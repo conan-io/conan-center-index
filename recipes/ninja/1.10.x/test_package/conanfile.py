a693445729e2c66614cec60c6eda92c7d810d738
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-05-19 17:44:59 +0200

    (#10850) ninja 1.11.0
    * ninja: add version 1.11.0
    
    Generated and committed by [Conan Center Bot](https://github.com/qchateau/conan-center-bot)
    Find more updatable recipes in the [GitHub Pages](https://qchateau.github.io/conan-center-bot/)
    
    * fix test
    
    * remove old versions
    
    Co-authored-by: Quentin Chateau via Conan Center Bot <quentin.chateau@gmail.com>

--- recipes/ninja/1.10.x/test_package/conanfile.py
+++ recipes/ninja/1.10.x/test_package/conanfile.py
@@ -3,6 +3,12 @@ import os
 
 
 class TestPackage(ConanFile):
+    settings = "os", "arch"
 
+    test_type = "explicit"
+    
+    def build_requirements(self):
+        self.build_requires(self.tested_reference_str)
+        
     def test(self):
         self.run("ninja --version", run_environment=True)
