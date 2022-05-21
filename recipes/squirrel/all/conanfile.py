b0c578c75c29c8e8b9377ff645c0e43b11e25ca6
toge <toge.mail@gmail.com>
2022-05-22 03:34:10 +0900

    squirrel: add version 3.2

--- recipes/squirrel/all/conanfile.py
+++ recipes/squirrel/all/conanfile.py
@@ -1,4 +1,5 @@
 import os
+import functools
 from conans import ConanFile, tools, CMake
 from conans.errors import ConanInvalidConfiguration
 
@@ -9,13 +10,11 @@ class SquirrelConan(ConanFile):
                   "language, designed to be a light-weight scripting language that " \
                   "fits in the size, memory bandwidth, and real-time requirements " \
                   "of applications like video games."
+    topics = ("object-oriented", "scripting", "light-weight")
+    license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "http://www.squirrel-lang.org/"
-    license = "MIT"
-    topics = ("conan", "fruit", "injection")
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    exports_sources = ["CMakeLists.txt", "patches/*"]
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
@@ -24,39 +23,42 @@ class SquirrelConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-
-    _cmake = None
+    generators = "cmake"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
+
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def validate(self):
         if tools.Version(self.version) <= "3.1":
             if self.settings.os == "Macos":
                 raise ConanInvalidConfiguration("squirrel 3.1 and earlier does not support Macos")
             if self.settings.compiler == "clang" and tools.Version(self.settings.compiler.version) < "9":
                 raise ConanInvalidConfiguration("squirrel 3.1 and earlier does not support Clang 8 and earlier")
-        if self.options.shared:
-            del self.options.fPIC
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
+    @functools.lru_cache(1)
     def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["DISABLE_DYNAMIC"] = not self.options.shared
-        self._cmake.definitions["DISABLE_STATIC"] = self.options.shared
-        self._cmake.configure()
-        return self._cmake
+        cmake = CMake(self)
+        cmake.definitions["DISABLE_DYNAMIC"] = not self.options.shared
+        cmake.definitions["DISABLE_STATIC"] = self.options.shared
+        cmake.configure()
+        return cmake
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
@@ -68,6 +70,7 @@ class SquirrelConan(ConanFile):
         self.copy("COPYRIGHT", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         # squirrel
