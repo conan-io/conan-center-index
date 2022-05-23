b5809e3dc8e8eb9bf35bde50cb019c3736fa7a9e
Florian de Gaulejac <florian.degaulejac@stilla.fr>
2022-05-23 12:12:44 +0200

    Do no use raw string literal for old compiler support

--- recipes/cryptopp-pem/all/test_package/test_package.cpp
+++ recipes/cryptopp-pem/all/test_package/test_package.cpp
@@ -8,12 +8,12 @@ int main()
     try
     {
         CryptoPP::RSA::PublicKey publicKey;
-        std::string key = R"(-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDWEDeqk749sYZAt2adT2m1Vo8N
-h86GzAs/ZWvlQDUyiedDuAvOhrVwllp4GuAXP7tZwGoeJ+Un6Nkl45IIQ2U9NMWQ
-aru76CD9kVUdThnKVOaeeqZysM1Bz6iJF5piSUzVKZdEk6rIUZjrwpNaYdBBq4a/
-cNETdvolooljIRB8ywIDAQAB
------END PUBLIC KEY-----)";
+        std::string key = "-----BEGIN PUBLIC KEY-----\n"
+            "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDWEDeqk749sYZAt2adT2m1Vo8N\n"
+            "h86GzAs/ZWvlQDUyiedDuAvOhrVwllp4GuAXP7tZwGoeJ+Un6Nkl45IIQ2U9NMWQ\n"
+            "aru76CD9kVUdThnKVOaeeqZysM1Bz6iJF5piSUzVKZdEk6rIUZjrwpNaYdBBq4a/\n"
+            "cNETdvolooljIRB8ywIDAQAB\n"
+            "-----END PUBLIC KEY-----";
         CryptoPP::StringSource source_str{key, true};
         CryptoPP::PEM_Load(source_str, publicKey);
 
