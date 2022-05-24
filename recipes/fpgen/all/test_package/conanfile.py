18381fe7ed499f669e28099da4ddeec9c65ce34d
jay-tux <sys.jonas@yahoo.com>
2022-05-24 14:54:32 +0200

    fpgen: force GCC >= 10

--- recipes/fpgen/all/test_package/conanfile.py
+++ recipes/fpgen/all/test_package/conanfile.py
@@ -1,11 +1,19 @@
 import os
 
 from conans import ConanFile, CMake, tools
+from conans.model.version import Version
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+
+    def configure(self):
+        v = Version(str(self.settings.compiler.version))
+        if self.settings.compiler == "gcc" and v < "10":
+            raise ConanInvalidConfiguration("Requires GCC > 10")
+        elif not self.settings.compiler == "gcc":
+            raise ConanInvalidConfiguration("Currently only GCC supported")
 
     def build(self):
         cmake = CMake(self)
