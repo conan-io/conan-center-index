8be3b7bd3a16a8d4ad8238cd0f87848b188c78b0
David Fleury <dfleury2@free.fr>
2022-05-26 16:51:52 +0200

    Remove unused import CMakeDeps and CMakeToolchain

--- recipes/beauty/all/conanfile.py
+++ recipes/beauty/all/conanfile.py
@@ -1,6 +1,6 @@
 import os
 from conans import ConanFile, tools
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
+from conan.tools.cmake import CMake
 from conans.errors import ConanInvalidConfiguration
 
 
