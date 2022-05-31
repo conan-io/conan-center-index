bbbbe052c0803020bd8d66f6cb464bb0b54055a0
Paul Harris <paulharris@computer.org>
2022-05-31 17:11:41 +0800

    cgns - add 4.3.0 and 'parallel' option
    Note that HDF5 still can't compile with 'parallel' option enabled,
    hopefully someone else will fix that.

--- recipes/cgns/all/conanfile.py
+++ recipes/cgns/all/conanfile.py
@@ -2,29 +2,30 @@ import os
 from conans import ConanFile, CMake, tools
 
 
-required_conan_version = ">=1.29.1"
+required_conan_version = ">=1.33.0"
 
 
 class CgnsConan(ConanFile):
     name = "cgns"
     description = "Standard for data associated with the numerical solution " \
                   "of fluid dynamics equations."
-    topics = ("conan", "cgns", "data", "cfd", "fluids")
+    topics = ("cgns", "data", "cfd", "fluids")
     homepage = "http://cgns.org/"
     license = "Zlib"
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os", "compiler", "build_type", "arch"
-    exports_sources = ["CMakeLists.txt", "patches/**"]
     generators = "cmake"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "with_hdf5": [True, False]
+        "with_hdf5": [True, False],
+        "parallel": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "with_hdf5": True
+        "with_hdf5": True,
+        "parallel": False,
     }
 
     _cmake = None
@@ -37,9 +38,18 @@ class CgnsConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+
+    def validate(self):
+        if self.options.parallel and not self.options.with_hdf5:
+            raise ConanInvalidConfiguration("Parallel requires HDF5 with parallel enabled")
 
     def configure(self):
         if self.options.shared:
@@ -47,13 +57,18 @@ class CgnsConan(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
+        if self.options.parallel:
+            self.options["hdf5"].parallel = True
+            self.options["hdf5"].enable_cxx = False # can't enable this with parallel
+
     def requirements(self):
         if self.options.with_hdf5:
-            self.requires("hdf5/1.12.0")
+            self.requires("hdf5/1.12.1")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("CGNS-" + self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                strip_root=True,
+                destination=self._source_subfolder)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -66,12 +81,21 @@ class CgnsConan(ConanFile):
         self._cmake.definitions["CGNS_ENABLE_HDF5"] = self.options.with_hdf5
         self._cmake.definitions["CGNS_BUILD_SHARED"] = self.options.shared
         self._cmake.definitions["CGNS_USE_SHARED"] = self.options.shared
+        self._cmake.definitions["CGNS_ENABLE_PARALLEL"] = self.options.parallel
         self._cmake.definitions["CGNS_BUILD_CGNSTOOLS"] = False
         self._cmake.configure()
+
+        # Other flags, seen in appveyor.yml in source code, not currently managed.
+        # CGNS_ENABLE_LFS:BOOL=OFF       --- note in code: needed on 32 bit systems
+        # CGNS_ENABLE_SCOPING:BOOL=OFF   --- disabled in VTK's bundle
+        # HDF5_NEED_ZLIB:BOOL=ON -- should be dealt with by cmake auto dependency management or something?
 
         return self._cmake
 
     def build(self):
+        # conan complains about CRLF in this file, so fix it up
+        tools.dos2unix(os.path.join(self.source_folder,self._source_subfolder,"src","cgnstools","common", "winhtml.c"))
+
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
 
@@ -85,6 +109,7 @@ class CgnsConan(ConanFile):
         cmake.install()
 
         os.remove(os.path.join(self.package_folder, "include", "cgnsBuild.defs"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.libs = ["cgnsdll" if self.settings.os == "Windows" and self.options.shared else "cgns"]
