bd1ac3d7ac5e90eda9c06950b5d0b5c810d9f711
toge <toge.mail@gmail.com>
2022-06-05 03:24:42 +0900

    add configure()

--- recipes/highway/all/conanfile.py
+++ recipes/highway/all/conanfile.py
@@ -49,6 +49,10 @@ class HighwayConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
