9c6227e1a4508015c12bf64bbbdaed5e3549a48b
Uilian Ries <uilianries@gmail.com>
2022-06-06 13:58:23 +0200

    support pkg_config
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/glib/all/test_package/conanfile.py
+++ recipes/glib/all/test_package/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi", "PkgConfigDeps"
+    generators = "cmake", "cmake_find_package_multi", "pkg_config"
 
     def build(self):
         if self.settings.os != "Windows":
