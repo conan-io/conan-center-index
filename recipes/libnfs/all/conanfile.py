3bd107e827168b4895e96dce9e6f09a6393957ec
toge <toge.mail@gmail.com>
2022-06-07 01:08:18 +0900

    fix license name
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libnfs/all/conanfile.py
+++ recipes/libnfs/all/conanfile.py
@@ -9,7 +9,7 @@ class LibnfsConan(ConanFile):
     name = "libnfs"
     description = "LIBNFS is a client library for accessing NFS shares over a network."
     topics = ("async", "nfsv4", "nfs")
-    license = "LGPL 2.1"
+    license = "LGPL-2.1"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/sahlberg/libnfs"
     settings = "os", "arch", "compiler", "build_type"
