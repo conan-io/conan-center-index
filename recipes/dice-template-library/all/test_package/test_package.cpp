28a16229cdb1df0bf419ed35f65f58663e2d725c
Lukas <lukas.kerkemeier@web.de>
2022-06-09 11:58:16 +0200

    works now on the new bugfix version

--- recipes/dice-template-library/all/test_package/test_package.cpp
+++ recipes/dice-template-library/all/test_package/test_package.cpp
@@ -1,11 +1,11 @@
 #include <concepts>
-#include <Dice/template-library/integral_templated_tuple.hpp>
+#include <Dice/template-library/integral_template_tuple.hpp>
 #include <ios>
 #include <iostream>
 
 template <int N> struct Wrapper { static constexpr int i = N; };
 
 int main() {
-  Dice::template_library::IntegralTemplatedTuple<Wrapper, 0, 5> tup;
+  Dice::template_library::integral_template_tuple<Wrapper, 0, 5> tup;
   std::cout << std::boolalpha << "tup.get<3>().i == 3: " << (tup.get<3>().i == 3) << std::endl;
 }
