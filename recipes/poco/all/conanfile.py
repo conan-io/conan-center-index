75fe3f4cf6bf574fd66aadc5ab2b4930104bccdf
tuduongquyet <78657396+tuduongquyet@users.noreply.github.com>
2022-06-10 13:34:18 +0700

    Update conanfile.py

--- recipes/poco/all/conanfile.py
+++ recipes/poco/all/conanfile.py
@@ -122,7 +122,7 @@ class PocoConan(ConanFile):
         if self.options.enable_xml:
             self.requires("expat/2.4.8")
         if self.options.enable_data_sqlite:
-            self.requires("sqlite3/3.38.1")
+            self.requires("sqlite3/3.38.5")
         if self.options.enable_apacheconnector:
             self.requires("apr/1.7.0")
             self.requires("apr-util/1.6.1")
