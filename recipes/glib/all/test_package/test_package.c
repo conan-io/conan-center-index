4f1ec750ca9c824452d4dd79978cfd5836909f77
Uilian Ries <uilianries@gmail.com>
2022-06-13 10:59:38 +0200

    No gpio/networking test
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/glib/all/test_package/test_package.c
+++ recipes/glib/all/test_package/test_package.c
@@ -3,15 +3,15 @@
 
 #include "glib.h"
 #include "gmodule.h"
+#include "gio/gio.h"
 
 
-int main(int argc, char **argv) {
+int main() {
     printf("glib %d.%d.%d\n", glib_major_version, glib_minor_version, glib_micro_version);
     printf("glib interface age: %d\n", glib_interface_age);
     printf("glib binary age: %d\n", glib_binary_age);
 
     GQueue *queue = g_queue_new();
-    if (!g_queue_is_empty(queue)) return;
     g_queue_free(queue);
 
     g_module_supported();
