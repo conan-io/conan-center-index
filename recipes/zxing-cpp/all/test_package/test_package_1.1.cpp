29f5adee3d9cb99c644c7b978c08f26def21a62a
toge <toge.mail@gmail.com>
2022-06-15 18:39:12 +0900

    fix indent

--- recipes/zxing-cpp/all/test_package/test_package_1.1.cpp
+++ recipes/zxing-cpp/all/test_package/test_package_1.1.cpp
@@ -22,25 +22,25 @@ int main(int argc, char* argv[])
     int width = 100, height = 100;
     int margin = 10;
     int eccLevel = 8;
-	auto encoding = CharacterSet::Unknown;
-	auto format = BarcodeFormat::QRCode;
+    auto encoding = CharacterSet::Unknown;
+    auto format = BarcodeFormat::QRCode;
     std::string text = "Hello conan world!";
     std::string outPath = "output.png";
 
-	try {
-		auto writer = MultiFormatWriter(format).setMargin(margin).setEncoding(encoding).setEccLevel(eccLevel);
-		auto matrix = writer.encode(TextUtfEncoding::FromUtf8(text), width, height);
-		auto bitmap = ToMatrix<uint8_t>(matrix);
+    try {
+        auto writer = MultiFormatWriter(format).setMargin(margin).setEncoding(encoding).setEccLevel(eccLevel);
+        auto matrix = writer.encode(TextUtfEncoding::FromUtf8(text), width, height);
+        auto bitmap = ToMatrix<uint8_t>(matrix);
 
-		int success = stbi_write_png(outPath.c_str(), bitmap.width(), bitmap.height(), 1, bitmap.data(), 0);
-		if (!success) {
-			std::cerr << "Failed to write image: " << outPath << std::endl;
-			return -1;
-		}
-	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		return -1;
-	}
+        int success = stbi_write_png(outPath.c_str(), bitmap.width(), bitmap.height(), 1, bitmap.data(), 0);
+        if (!success) {
+            std::cerr << "Failed to write image: " << outPath << std::endl;
+            return -1;
+        }
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
