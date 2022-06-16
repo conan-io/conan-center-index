827246a47048504363b178f9e80ea49700202631
strangeQuark1041 <83468982+strangeQuark1041@users.noreply.github.com>
2022-06-16 13:31:59 +0530

    conanfile.py: change cpp_info
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/samarium/all/conanfile.py
+++ recipes/samarium/all/conanfile.py
@@ -101,3 +101,6 @@ class SamariumConan(ConanFile):
         self.cpp_info.filenames["cmake_find_package_multi"] = "samarium"
         self.cpp_info.names["cmake_find_package"] = "SAMARIUM"
         self.cpp_info.names["cmake_find_package_multi"] = "samarium"
+
+        self.cpp_info.set_property("cmake_file_name", "SAMARIUM")
+        self.cpp_info.set_property("cmake_target_name", "samarium::samarium") 
