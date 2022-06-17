33dc5e13c2bb34f0101ac8f346844ae461033ff3
Uilian Ries <uilianries@gmail.com>
2022-06-17 11:11:17 +0200

    Improve systemc-cci
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/systemc-cci/all/conanfile.py
+++ recipes/systemc-cci/all/conanfile.py
@@ -1,10 +1,14 @@
-from conans import ConanFile, tools, CMake
-from conans.errors import ConanInvalidConfiguration
-import os
+from conans import tools, CMake
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+import functools
+
+
+required_conan_version = ">=1.43.0"
+
 
 class SystemccciConan(ConanFile):
     name = "systemc-cci"
-    version = "1.0.0"
     description = """SystemC Configuration, Control and Inspection library"""
     homepage = "https://www.accellera.org/"
     url = "https://github.com/conan-io/conan-center-index"
@@ -19,59 +23,60 @@ class SystemccciConan(ConanFile):
         "shared": False,
         "fPIC": True
     }
-    requires = "systemc/2.3.3"
-    generators = "cmake"
-    exports_sources = "patches/**"
+    generators = "cmake", "cmake_find_package_multi"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def export(self):
-        self.copy("CMakeLists.txt", dst=self._source_subfolder)
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
-        tools.check_min_cppstd(self, "11")
         if self.options.shared:
             del self.options.fPIC
 
+    def requirements(self):
+        self.requires("systemc/2.3.3")
+        self.requires("rapidjson/1.1.0")
+
     def validate(self):
         if self.settings.os == "Macos":
-            raise ConanInvalidConfiguration("Macos build not supported")
-
-        if self.settings.os == "Windows" and self.options.shared:
-            raise ConanInvalidConfiguration("Building SystemC-CCI as a shared library on Windows is currently not supported")
+            raise ConanInvalidConfiguration(f"{self.name} is not suppported on {self.settings.os}.")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+                  destination=self._source_subfolder, strip_root=True, verify=False)
+
+    @functools.lru_cache(1)
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
 
     def build(self):
         for patch in self.conan_data["patches"][self.version]:
             tools.patch(**patch)
-
-        cmake = CMake(self, parallel=True)
-        cmake.configure(source_folder=self._source_subfolder)
+        cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy("NOTICE", dst="licenses", src=self._source_subfolder)
-        src_dir = os.path.join(self._source_subfolder, "src")
-        self.copy("*.h", dst="include", src=src_dir)
-        self.copy("cci_configuration", dst="include", src=src_dir)
-        lib_dir = os.path.join(self._source_subfolder, "lib")
-        if self.options.shared:
-            self.copy("*.so", dst="lib", src=lib_dir)
-        else:
-            self.copy("*.a", dst="lib", src=lib_dir)
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["cciapi"]
-        if self.settings.os != "Windows":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs = ["m"]
