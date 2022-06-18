97385655b43cc8466b1b15500f24a316e7311e35
theirix <theirix@gmail.com>
2022-06-18 15:24:00 +0300

    Fix include paths

--- recipes/open62541/all/conanfile.py
+++ recipes/open62541/all/conanfile.py
@@ -379,7 +379,7 @@ class Open62541Conan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.includedirs = [
             "include",
-            os.path.join("include", "plugin")
+            os.path.join("include", "open62541", "plugin")
         ]
 
         # required for creating custom servers from ua-nodeset
@@ -392,6 +392,6 @@ class Open62541Conan(ConanFile):
             self.cpp_info.system_libs.append("ws2_32")
             self.cpp_info.includedirs.append(os.path.join("include", "win32"))
         else:
-            self.cpp_info.includedirs.append(os.path.join("include", "posix"))
+            self.cpp_info.includedirs.append(os.path.join("include", "open62541", "posix"))
         self.cpp_info.builddirs.append(self._module_subfolder)
         self.cpp_info.build_modules = [self._module_file_rel_path]
