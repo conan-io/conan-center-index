6255bbe6323561d96314a3042de43ed0789d3862
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-06-20 19:46:06 +0200

    Fix typo

--- recipes/bs-thread-pool/all/conanfile.py
+++ recipes/bs-thread-pool/all/conanfile.py
@@ -4,7 +4,7 @@ from conans.errors import ConanInvalidConfiguration
 required_conan_version = ">=1.33.0"
 
 
-class ThreadPoolConan(ConanFile):
+class BSThreadPoolConan(ConanFile):
     name = "bs-thread-pool"
     description = "Fast, lightweight, and easy-to-use C++17 thread pool library"
     topics = ("thread-pool", "concurrency", "header-only")
