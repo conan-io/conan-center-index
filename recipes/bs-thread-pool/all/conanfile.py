77f4ab83aad83c8ef2a95871e1d341d4538df66c
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-06-20 19:15:57 +0200

    Add check for compiler version

--- recipes/bs-thread-pool/all/conanfile.py
+++ recipes/bs-thread-pool/all/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 
 required_conan_version = ">=1.33.0"
 
@@ -20,9 +21,33 @@ class ThreadPoolConan(ConanFile):
     def _minimum_cpp_standard(self):
         return 17
 
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "apple-clang": "9.3",
+            "clang": "5.0",
+            "gcc": "8.0",
+            "Visual Studio": "19.12"
+        }
+
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
+
+        compiler = self.settings.compiler
+        try:
+            min_version = self._minimum_compilers_version[str(compiler)]
+            if tools.Version(compiler.version) < min_version:
+                msg = (
+                    "{} requires C++{} features which are not supported by compiler {} {}."
+                ).format(self.name, self._minimum_cpp_standard, compiler, compiler.version)
+                raise ConanInvalidConfiguration(msg)
+        except KeyError:
+            msg = (
+                "{} recipe lacks information about the {} compiler, "
+                "support for the required C++{} features is assumed"
+            ).format(self.name, compiler, self._minimum_cpp_standard)
+            self.output.warn(msg)
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
