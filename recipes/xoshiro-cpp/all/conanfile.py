21101b8147dee2835713c4546e107b5c539675b7
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-06-23 19:00:41 +0200

    Update recipe

--- recipes/xoshiro-cpp/all/conanfile.py
+++ recipes/xoshiro-cpp/all/conanfile.py
@@ -6,10 +6,12 @@ required_conan_version = ">=1.33.0"
 class XoshiroCppConan(ConanFile):
     name = "xoshiro-cpp"
     description = "Header-only Xoshiro/Xoroshiro PRNG wrapper library for modern C++ (C++17/C++20)"
-    topics = ("prng", "xoshiro", "header-only")
-    url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/Reputeless/Xoshiro-cpp"
     license = "MIT"
+    homepage = "https://github.com/Reputeless/Xoshiro-cpp"
+    url = "https://github.com/conan-io/conan-center-index"
+    topics = ("prng", "xoshiro", "header-only")
+    settings = "arch", "build_type", "compiler", "os"
+    generators = "cmake", "cmake_find_package_multi"
     no_copy_source = True
 
     @property
@@ -28,10 +30,14 @@ class XoshiroCppConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
-    def package_id(self):
-        self.info.header_only()
-
     def package(self):
         self.copy("*.hpp", dst="include/xoshiro-cpp",
                   src=self._source_subfolder)
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "xoshiro-cpp"
+        self.cpp_info.names["cmake_find_package_multi"] = "xoshiro-cpp"
