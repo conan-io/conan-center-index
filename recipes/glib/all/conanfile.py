5b6bf729954bf8e340c3875541b6df7aa9d2715d
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-06-23 08:41:36 +0000

    fix gvdb resolution

--- recipes/glib/all/conanfile.py
+++ recipes/glib/all/conanfile.py
@@ -46,6 +46,11 @@ class GLibConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -130,6 +135,8 @@ class GLibConan(ConanFile):
         return meson
 
     def _patch_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
         if tools.Version(self.version) < "2.67.2":
             tools.replace_in_file(
                 os.path.join(self._source_subfolder, "meson.build"),
