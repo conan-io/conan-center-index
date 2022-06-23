0ff4675ea027f0796a1eb36173c07ac41ee1cdf6
Andras Kiss <anerbota@gmail.com>
2022-06-23 11:38:32 +0300

    Added archicad-apidevkit

---
+++ recipes/archicad-apidevkit/all/conanfile.py
@@ -0,0 +1,37 @@
+import os
+from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+
+required_conan_version = ">=1.33.0"
+
+
+class ArchicadApidevkitConan (ConanFile):
+    name = "archicad-apidevkit"
+    description = "The General API Development Kit enables software developers to extend the functionality of Archicad"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://archicadapi.graphisoft.com/"
+    license = "https://archicadapi.graphisoft.com/documentation/license-agreement"
+    settings = "os", "arch"
+    no_copy_source = True
+    topics = "API, Archicad, development"
+
+    @property
+    def _source_subfolder(self):
+        return self.source_folder
+
+    @property
+    def _acdevkit_arch(self):
+         return str(self.settings.arch)
+
+    def validate(self):
+        if (not (self.version in self.conan_data["sources"]) or
+                not (str(self.settings.os) in self.conan_data["sources"][self.version])):
+            raise ConanInvalidConfiguration(
+                "Binaries for this combination of architecture/version/os not available")
+
+    def build(self):
+        tools.get(**self.conan_data["sources"][self.version][str(self.settings.os)]
+                  [self._acdevkit_arch], destination=self._source_subfolder, strip_root=True)
+
+    def package(self):
+        self.copy("*", symlinks=True)
