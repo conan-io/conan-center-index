413e71011a2011ce53f4b0056a55d5afbcf66ddd
Benjamin Sergeant <benjamin.sergeant@applovin.com>
2022-06-23 18:03:44 -0700

    bump deps

--- recipes/ixwebsocket/all/conanfile.py
+++ recipes/ixwebsocket/all/conanfile.py
@@ -48,11 +48,11 @@ class IXWebSocketConan(ConanFile):
 
     def requirements(self):
         if self.options.get_safe("with_zlib", True):
-            self.requires("zlib/1.2.11")
+            self.requires("zlib/1.2.12")
         if self.options.tls == "openssl":
-            self.requires("openssl/1.1.1k")
+            self.requires("openssl/1.1.1o")
         elif self.options.tls == "mbedtls":
-            self.requires("mbedtls/2.25.0")
+            self.requires("mbedtls/3.1.0")
 
     @property
     def _can_use_openssl(self):
