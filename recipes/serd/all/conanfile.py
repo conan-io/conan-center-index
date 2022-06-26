20753f390559edfc1452c46dd18a2131f0f3dc80
Alexander Bigerl <alexander@bigerl.eu>
2022-06-26 13:46:48 +0200

    serd: fixed build errors for some configurations

--- recipes/serd/all/conanfile.py
+++ recipes/serd/all/conanfile.py
@@ -37,10 +37,16 @@ class Recipe(ConanFile):
                   destination=Path(self.folders.base_source).joinpath("waflib"),
                   strip_root=True)
 
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
     def configure(self):
         # its a C library. So C++ properties compiler.{libcxx,cppstd} are not required.
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
+        if self.options.shared:
+            del self.options.fPIC
 
     def validate(self):
         if self.settings.compiler == "Visual Studio":
