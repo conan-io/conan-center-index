3a12fda4c4071506bad37a9b0493536d285c58fd
Stanislaw Kaushanski <stas@minres.com>
2022-06-28 16:42:06 +0200

    minor changes

--- recipes/verilator/all/conanfile.py
+++ recipes/verilator/all/conanfile.py
@@ -150,11 +150,10 @@ class VerilatorConan(ConanFile):
 
         os.unlink(os.path.join(self.package_folder, "bin", "share", "verilator", "verilator-config-version.cmake"))
         shutil.move(os.path.join(self.package_folder, "bin", "share", "verilator", "verilator-config.cmake"), 
-        os.path.join(self.package_folder, "verilator-tools.cmake"))
+                    os.path.join(self.package_folder, "verilator-tools.cmake"))
 
         if self.settings.build_type == "Debug":
-            tools.replace_in_file(os.path.join(self.package_folder, "verilator-tools.cmake"),
-             "verilator_bin", "verilator_bin_dbg")
+            tools.replace_in_file(os.path.join(self.package_folder, "verilator-tools.cmake"), "verilator_bin", "verilator_bin_dbg")
 
         shutil.move(os.path.join(self.package_folder, "bin", "share", "verilator", "include"), os.path.join(self.package_folder))
 
