e0b2125fac1dd1b3fee0d54145d6ac3ca8484ba7
toge <toge.mail@gmail.com>
2022-06-29 19:10:19 +0900

    remove print
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/objectbox-generator/all/test_package/conanfile.py
+++ recipes/objectbox-generator/all/test_package/conanfile.py
@@ -8,5 +8,4 @@ class TestPackageConan(ConanFile):
     def test(self):
         if not tools.cross_building(self):
             bin_path = os.path.join(self.deps_cpp_info["objectbox-generator"].rootpath, "bin", "objectbox-generator")
-            print(bin_path)
             self.run(bin_path + " -help", run_environment=True)
