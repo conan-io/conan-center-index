a6256c8fa864338f475ff0aef3d3340d9f95e77a
SSE4 <tomskside@gmail.com>
2022-07-01 13:36:58 -0700

    Update recipes/karchive/all/test_package/conanfile.py

--- recipes/karchive/all/test_package/conanfile.py
+++ recipes/karchive/all/test_package/conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.43.0"
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
+    generators = "CMakeDeps", "cmake"
 
     def generate(self):
         tc = CMakeToolchain(self)
