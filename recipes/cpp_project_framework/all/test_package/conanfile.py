d3f531604c855668afe999230cacad83fb7f9511
Curtis Lo <sheepgrass@gmail.com>
2022-07-02 15:05:54 +0800

    rename example to test_package in test_package of cpp_project_framework/1.0.0

--- recipes/cpp_project_framework/all/test_package/conanfile.py
+++ recipes/cpp_project_framework/all/test_package/conanfile.py
@@ -1,6 +1,5 @@
-import os
-
 from conans import ConanFile, CMake, tools
+import os
 
 
 class CppProjectFrameworkTestConan(ConanFile):
@@ -10,12 +9,10 @@ class CppProjectFrameworkTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self):
-            os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
