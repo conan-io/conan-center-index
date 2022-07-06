59c6f1db4237acf5a0994079c3b25bad7ef1958e
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-07-06 08:11:04 +0200

    re-add old recipe

--- recipes/at-spi2-core/new/conanfile.py
+++ recipes/at-spi2-core/new/conanfile.py
@@ -6,11 +6,13 @@ import os
 class AtSpi2CoreConan(ConanFile):
     name = "at-spi2-core"
     description = "It provides a Service Provider Interface for the Assistive Technologies available on the GNOME platform and a library against which applications can be linked"
-    topics = "atk", "accessibility"
+    topics = ("atk", "accessibility")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://gitlab.gnome.org/GNOME/at-spi2-core/"
     license = "LGPL-2.1-or-later"
     generators = "pkg_config"
+
+    provides = "at-spi2-atk", "atk"
 
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -86,10 +88,15 @@ class AtSpi2CoreConan(ConanFile):
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
-        if tools.Version(self.version) >= "2.42.0":
-            tools.replace_in_file(os.path.join(self._source_subfolder, "bus", "meson.build"),
-                                  "if x11_dep.found()",
-                                  "if x11_option == 'yes'")
+        tools.replace_in_file(os.path.join(self._source_subfolder, "bus", "meson.build"),
+                                "if x11_dep.found()",
+                                "if x11_option == 'yes'")
+        tools.replace_in_file(os.path.join(self._source_subfolder, 'meson.build'),
+            "subdir('tests')",
+            "#subdir('tests')")
+        tools.replace_in_file(os.path.join(self._source_subfolder, 'meson.build'),
+            "libxml_dep = dependency('libxml-2.0', version: libxml_req_version)",
+            "#libxml_dep = dependency('libxml-2.0', version: libxml_req_version)")
         meson = self._configure_meson()
         meson.build()
 
@@ -102,9 +109,20 @@ class AtSpi2CoreConan(ConanFile):
 
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.includedirs = ["include/at-spi-2.0"]
-        self.cpp_info.names["pkg_config"] = "atspi-2"
+        self.cpp_info.components["atspi"].libs = ['atspi']
+        self.cpp_info.components["atspi"].includedirs = ["include/at-spi-2.0"]
+        self.cpp_info.components["atspi"].requires = ["dbus::dbus", "glib::glib"]
+        self.cpp_info.components["atspi"].names["pkg_config"] = "atspi-2"
+        
+        self.cpp_info.components["atk"].libs = ["atk-1.0"]
+        self.cpp_info.components["atk"].includedirs = ['include/atk-1.0']
+        self.cpp_info.components["atk"].requires = ["glib::glib"]
+        self.cpp_info.components["atk"].names['pkg_config'] = 'atk'
+
+        self.cpp_info.components["atk-bridge"].libs = ['atk-bridge-2.0']
+        self.cpp_info.components["atk-bridge"].includedirs = [os.path.join('include', 'at-spi2-atk', '2.0')]
+        self.cpp_info.components["atk-bridge"].requires = ["dbus::dbus", "atk", "glib::glib", "atspi"]
+        self.cpp_info.components["atk-bridge"].names['pkg_config'] = 'atk-bridge-2.0'
 
     def package_id(self):
         self.info.requires["glib"].full_package_mode()
