1e4e77f0930f50176d8d2a241cd6322f7b790b78
Martin Delille <martin@delille.org>
2022-07-06 09:35:00 +0200

    create-dmg/x.x.x: change support location

--- recipes/create-dmg/all/conanfile.py
+++ recipes/create-dmg/all/conanfile.py
@@ -13,24 +13,32 @@ class CreateDmgConan(ConanFile):
     homepage = "https://github.com/create-dmg/create-dmg"
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os", "arch", "compiler", "build_type"
-    no_copy_source = True
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
+    def export_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
     def validate(self):
         if self.settings.os != "Macos":
             raise ConanInvalidConfiguration(f"{self.name} works only on MacOS")
 
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+
     def build(self):
         tools.get(**self.conan_data["sources"][self.version],
                   strip_root=True, destination=self._source_subfolder)
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy("create-dmg", dst="bin", src=self._source_subfolder)
-        self.copy("*", dst=os.path.join("bin","support"), src=os.path.join(self._source_subfolder,"support"))
+        self.copy("*", dst=os.path.join("res", "create-dmg", "support"), src=os.path.join(self._source_subfolder,"support"))
 
         tools.rmdir(os.path.join(self.package_folder, "share"))
 
