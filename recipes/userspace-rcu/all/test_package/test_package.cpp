fa7064a51c4cb945ef671ff86862c55bd1a78e97
Brian Szmyd <bszmyd@ebay.com>
2022-07-11 09:27:26 -0700

    Improve support for GCC < 9

--- recipes/userspace-rcu/all/test_package/test_package.cpp
+++ recipes/userspace-rcu/all/test_package/test_package.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
 #include <benchmark/benchmark.h>
-#include <shared_mutex>
+
 #include <atomic>
+#include <iostream>
+#include <mutex>
 #include <thread>
 
 #include "urcu_helper.hpp"
@@ -12,120 +13,120 @@ RCU_REGISTER_INIT
 #define THREADS 8
 
 struct MyStatus {
-    bool is_shutdown = false;
-    bool is_paused = false;
+  bool is_shutdown{false};
+  bool is_paused{false};
 
-    MyStatus() = default;
-    MyStatus(const MyStatus& other) = default;
+  MyStatus() = default;
+  MyStatus(const MyStatus& other) = default;
 };
 
 struct AtomicStatus {
-    std::atomic< bool > is_shutdown = false;
-    std::atomic< bool > is_paused = false;
+  std::atomic<bool> is_shutdown{false};
+  std::atomic<bool> is_paused{false};
 };
 
 #define NENTRIES_PER_THREAD 50
 
-std::unique_ptr< AtomicStatus > glob_atomic_status;
-test_package::urcu_scoped_ptr< MyStatus > glob_urcu_status;
-std::unique_ptr< MyStatus > glob_raw_status;
-std::shared_mutex glob_mutex;
+std::unique_ptr<AtomicStatus> glob_atomic_status;
+test_package::urcu_scoped_ptr<MyStatus> glob_urcu_status;
+std::unique_ptr<MyStatus> glob_raw_status;
+std::mutex glob_mutex;
 
 void setup() {
-    glob_atomic_status = std::make_unique< AtomicStatus >();
-    glob_raw_status = std::make_unique< MyStatus >();
+  glob_atomic_status = std::make_unique<AtomicStatus>();
+  glob_raw_status = std::make_unique<MyStatus>();
 }
 
 void parallel_update() {
-    auto t = std::thread([]() {
-        for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
-            glob_atomic_status->is_shutdown.store(true);
-            glob_atomic_status->is_paused.store(true);
+  auto t = std::thread([]() {
+    for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
+      glob_atomic_status->is_shutdown.store(true);
+      glob_atomic_status->is_paused.store(true);
 
-            glob_raw_status->is_shutdown = true;
-            glob_raw_status->is_paused = true;
+      glob_raw_status->is_shutdown = true;
+      glob_raw_status->is_paused = true;
 
-            glob_urcu_status.update([](MyStatus* s) {
-                s->is_shutdown = true;
-                s->is_paused = true;
-            });
+      glob_urcu_status.update([](MyStatus* s) {
+        s->is_shutdown = true;
+        s->is_paused = true;
+      });
 
-            {
-                std::unique_lock l(glob_mutex);
-                glob_raw_status->is_shutdown = true;
-                glob_raw_status->is_paused = true;
-            }
-        }
-        std::cout << "Updated all status" << std::endl;
-    });
-    t.detach();
+      {
+        std::lock_guard<std::mutex> l(glob_mutex);
+        glob_raw_status->is_shutdown = true;
+        glob_raw_status->is_paused = true;
+      }
+    }
+    std::cout << "Updated all status" << std::endl;
+  });
+  t.detach();
 }
 
 void test_atomic_gets(benchmark::State& state) {
-    auto t = std::thread([]() {
-        for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
-            glob_atomic_status->is_shutdown.store(true);
-            glob_atomic_status->is_paused.store(true);
-        }
-        std::cout << "Updated all status" << std::endl;
-    });
-
-    for (auto _ : state) { // Loops upto iteration count
-        bool ret;
-        for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
-            benchmark::DoNotOptimize(ret = glob_atomic_status->is_shutdown.load());
-            benchmark::DoNotOptimize(ret = glob_atomic_status->is_paused.load());
-        }
+  auto t = std::thread([]() {
+    for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
+      glob_atomic_status->is_shutdown.store(true);
+      glob_atomic_status->is_paused.store(true);
     }
-    t.join();
+    std::cout << "Updated all status" << std::endl;
+  });
+
+  for (auto _ : state) {  // Loops upto iteration count
+    bool ret;
+    for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
+      benchmark::DoNotOptimize(ret = glob_atomic_status->is_shutdown.load());
+      benchmark::DoNotOptimize(ret = glob_atomic_status->is_paused.load());
+    }
+  }
+  t.join();
 }
 
 void test_urcu_gets(benchmark::State& state) {
+  rcu_register_thread();
+  auto t = std::thread([]() {
     rcu_register_thread();
-    auto t = std::thread([]() {
-        rcu_register_thread();
-        for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
-            glob_urcu_status.update([](MyStatus* s) {
-                s->is_shutdown = true;
-                s->is_paused = true;
-            });
-        }
-        std::cout << "Updated all status" << std::endl;
-        rcu_unregister_thread();
-    });
-
-    for (auto _ : state) {
-        bool ret;
-        for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
-            benchmark::DoNotOptimize(ret = glob_urcu_status.access()->is_shutdown);
-            benchmark::DoNotOptimize(ret = glob_urcu_status.access()->is_paused);
-        }
+    for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
+      glob_urcu_status.update([](MyStatus* s) {
+        s->is_shutdown = true;
+        s->is_paused = true;
+      });
     }
-    t.join();
+    std::cout << "Updated all status" << std::endl;
     rcu_unregister_thread();
+  });
+
+  for (auto _ : state) {
+    bool ret;
+    for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
+      benchmark::DoNotOptimize(ret = glob_urcu_status.access()->is_shutdown);
+      benchmark::DoNotOptimize(ret = glob_urcu_status.access()->is_paused);
+    }
+  }
+  t.join();
+  rcu_unregister_thread();
 }
 
 void test_raw_gets(benchmark::State& state) {
-    parallel_update();
-    for (auto _ : state) {
-        bool ret;
-        for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
-            benchmark::DoNotOptimize(ret = glob_raw_status->is_shutdown);
-            benchmark::DoNotOptimize(ret = glob_raw_status->is_paused);
-        }
+  parallel_update();
+  for (auto _ : state) {
+    bool ret;
+    for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
+      benchmark::DoNotOptimize(ret = glob_raw_status->is_shutdown);
+      benchmark::DoNotOptimize(ret = glob_raw_status->is_paused);
     }
+  }
 }
 
 void test_mutex_gets(benchmark::State& state) {
-    parallel_update();
-    for (auto _ : state) {
-        bool ret;
-        for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
-            std::shared_lock< std::shared_mutex > l(glob_mutex);
-            benchmark::DoNotOptimize(ret = glob_raw_status->is_shutdown);
-            benchmark::DoNotOptimize(ret = glob_raw_status->is_paused);
-        }
+  parallel_update();
+  for (auto _ : state) {
+    bool ret;
+    for (auto i = 0U; i < NENTRIES_PER_THREAD; i++) {
+      std::lock_guard<std::mutex> l(glob_mutex);
+      benchmark::DoNotOptimize(ret = glob_raw_status->is_shutdown);
+      benchmark::DoNotOptimize(ret = glob_raw_status->is_paused);
     }
+  }
 }
 
 BENCHMARK(test_atomic_gets)->Iterations(ITERATIONS)->Threads(THREADS);
@@ -134,7 +135,7 @@ BENCHMARK(test_raw_gets)->Iterations(ITERATIONS)->Threads(THREADS);
 BENCHMARK(test_mutex_gets)->Iterations(ITERATIONS)->Threads(THREADS);
 
 int main(int argc, char** argv) {
-    setup();
-    ::benchmark::Initialize(&argc, argv);
-    ::benchmark::RunSpecifiedBenchmarks();
+  setup();
+  ::benchmark::Initialize(&argc, argv);
+  ::benchmark::RunSpecifiedBenchmarks();
 }
--- recipes/userspace-rcu/all/test_package/urcu_helper.hpp
+++ recipes/userspace-rcu/all/test_package/urcu_helper.hpp
@@ -8,10 +8,11 @@
  * You may obtain a copy of the License at
  *    https://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *License for the specific language governing permissions and limitations under
+ *the License.
  *
  *********************************************************************************/
 #pragma once
@@ -28,117 +29,119 @@
 #else
 #include <urcu.h>
 #endif
-#include <urcu/static/urcu-qsbr.h>
 #include <urcu-call-rcu.h>
+#include <urcu/static/urcu-qsbr.h>
 
+#include <functional>
 #include <memory>
-#include <set>
 #include <mutex>
-#include <vector>
 #include <tuple>
 
 namespace test_package {
 
 class urcu_ctl {
-public:
-    static thread_local bool _rcu_registered_already;
+ public:
+  static thread_local bool _rcu_registered_already;
 
-    static void register_rcu() { rcu_register_thread(); }
+  static void register_rcu() { rcu_register_thread(); }
 
-    static void declare_quiscent_state() { rcu_quiescent_state(); }
+  static void declare_quiscent_state() { rcu_quiescent_state(); }
 
-    static void unregister_rcu() { rcu_unregister_thread(); }
+  static void unregister_rcu() { rcu_unregister_thread(); }
 
-    static void sync_rcu() { synchronize_rcu(); }
+  static void sync_rcu() { synchronize_rcu(); }
 };
 
-/* TODO : Try an ensure the lifetime of this object does not exceed the scoped_ptr that created it */
-template < typename T >
+/* TODO : Try an ensure the lifetime of this object does not exceed the
+ * scoped_ptr that created it */
+template <typename T>
 class _urcu_access_ptr {
+ public:
+  T* m_p;
 
-public:
-    T* m_p;
+  _urcu_access_ptr(T* p) : m_p(p) {}
+  ~_urcu_access_ptr() { rcu_read_unlock(); }
+  _urcu_access_ptr(const _urcu_access_ptr& other) = delete;
+  _urcu_access_ptr& operator=(const _urcu_access_ptr& other) = delete;
+  _urcu_access_ptr(_urcu_access_ptr&& other) noexcept {
+    std::swap(m_p, other.m_p);
+  }
 
-    _urcu_access_ptr(T* p) : m_p(p) {}
-    ~_urcu_access_ptr() { rcu_read_unlock(); }
-    _urcu_access_ptr(const _urcu_access_ptr& other) = delete;
-    _urcu_access_ptr& operator=(const _urcu_access_ptr& other) = delete;
-    _urcu_access_ptr(_urcu_access_ptr&& other) noexcept { std::swap(m_p, other.m_p); }
-
-    const T* operator->() const { return m_p; }
-    T* operator->() { return m_p; }
-    T* get() const { return m_p; }
+  const T* operator->() const { return m_p; }
+  T* operator->() { return m_p; }
+  T* get() const { return m_p; }
 };
 
 /* Simplified urcu pointer access */
-template < typename T, typename... Args >
+template <typename T, typename... Args>
 class urcu_scoped_ptr {
-public:
-    template < class... Args1 >
-    urcu_scoped_ptr(Args1&&... args) : m_args(std::forward< Args1 >(args)...) {
-        m_cur_obj = new T(std::forward< Args1 >(args)...);
+ public:
+  template <class... Args1>
+  urcu_scoped_ptr(Args1&&... args) : m_args(std::forward<Args1>(args)...) {
+    m_cur_obj = new T(std::forward<Args1>(args)...);
+  }
+
+  urcu_scoped_ptr(urcu_scoped_ptr const&) = delete;
+  urcu_scoped_ptr(urcu_scoped_ptr&&) = delete;
+  urcu_scoped_ptr& operator=(urcu_scoped_ptr const&) = delete;
+  urcu_scoped_ptr& operator=(urcu_scoped_ptr&&) = delete;
+
+  ~urcu_scoped_ptr() {
+    rcu_read_lock();  // Take read-fence prior to accessing m_cur_obj
+    if (m_cur_obj) delete rcu_dereference(m_cur_obj);
+    rcu_read_unlock();
+  }
+
+  _urcu_access_ptr<T> access() const {
+    rcu_read_lock();  // Take read-fence prior to accessing m_cur_obj
+    return _urcu_access_ptr<T>(rcu_dereference(
+        m_cur_obj));  // This object will read_unlock when it's destroyed
+  }
+
+  void update(const std::function<void(T*)>& edit_cb) {
+    T* old_obj{nullptr};
+    {
+      std::lock_guard<std::mutex> l(m_updater_mutex);
+      auto new_obj =
+          new T(*rcu_dereference(m_cur_obj));  // Create new obj from old obj
+      edit_cb(new_obj);
+
+      old_obj = rcu_xchg_pointer(&m_cur_obj, new_obj);
     }
+    synchronize_rcu();
+    if (old_obj) delete old_obj;
+  }
 
-    urcu_scoped_ptr(urcu_scoped_ptr const&) = delete;
-    urcu_scoped_ptr(urcu_scoped_ptr&&) = delete;
-    urcu_scoped_ptr& operator=(urcu_scoped_ptr const&) = delete;
-    urcu_scoped_ptr& operator=(urcu_scoped_ptr&&) = delete;
+  T* make_and_exchange(const bool sync_rcu_now = true) {
+    return _make_and_exchange(sync_rcu_now, m_args,
+                              std::index_sequence_for<Args...>());
+  }
 
-    ~urcu_scoped_ptr() {
-        rcu_read_lock(); // Take read-fence prior to accessing m_cur_obj
-        if (m_cur_obj) delete rcu_dereference(m_cur_obj);
-        rcu_read_unlock();
+ private:
+  template <std::size_t... Is>
+  T* _make_and_exchange(const bool sync_rcu_now,
+                        const std::tuple<Args...>& tuple,
+                        std::index_sequence<Is...>) {
+    auto new_obj = new T(std::get<Is>(
+        tuple)...);  // Make new object with saved constructor params
+    auto old_obj = rcu_xchg_pointer(&m_cur_obj, new_obj);
+    if (sync_rcu_now) {
+      synchronize_rcu();
     }
+    return old_obj;
+  }
 
-    void read(const auto& cb) const {
-        rcu_read_lock();
-        auto s = rcu_dereference(m_cur_obj);
-        cb((const T*)s);
-        rcu_read_unlock();
-    }
+ private:
+  // Args to hold onto for new buf
+  std::tuple<Args...> m_args;
 
-    _urcu_access_ptr< T > access() const {
-        rcu_read_lock();                                          // Take read-fence prior to accessing m_cur_obj
-        return _urcu_access_ptr< T >(rcu_dereference(m_cur_obj)); // This object will read_unlock when it's destroyed
-    }
+  // RCU protected pointer
+  T* m_cur_obj = nullptr;
 
-    void update(const auto& edit_cb) {
-        T* old_obj{nullptr};
-        {
-            auto l = std::scoped_lock(m_updater_mutex);        // TODO: Should we have it here or leave it to caller???
-            auto new_obj = new T(*rcu_dereference(m_cur_obj)); // Create new obj from old obj
-            edit_cb(new_obj);
-
-            old_obj = rcu_xchg_pointer(&m_cur_obj, new_obj);
-        }
-        synchronize_rcu();
-        if (old_obj) [[likely]]
-            delete old_obj;
-    }
-
-    T* make_and_exchange(const bool sync_rcu_now = true) {
-        return _make_and_exchange(sync_rcu_now, m_args, std::index_sequence_for< Args... >());
-    }
-
-private:
-    template < std::size_t... Is >
-    T* _make_and_exchange(const bool sync_rcu_now, const std::tuple< Args... >& tuple, std::index_sequence< Is... >) {
-        auto new_obj = new T(std::get< Is >(tuple)...); // Make new object with saved constructor params
-        auto old_obj = rcu_xchg_pointer(&m_cur_obj, new_obj);
-        if (sync_rcu_now) { synchronize_rcu(); }
-        return old_obj;
-    }
-
-private:
-    // Args to hold onto for new buf
-    std::tuple< Args... > m_args;
-
-    // RCU protected pointer
-    T* m_cur_obj = nullptr;
-
-    // Mutex to protect multiple copiers to run the copy step in parallel.
-    std::mutex m_updater_mutex;
+  // Mutex to protect multiple copiers to run the copy step in parallel.
+  std::mutex m_updater_mutex;
 };
 
-#define RCU_REGISTER_INIT thread_local bool test_package::urcu_ctl::_rcu_registered_already = false;
-} // namespace test_package
+#define RCU_REGISTER_INIT \
+  thread_local bool test_package::urcu_ctl::_rcu_registered_already = false;
+}  // namespace test_package
