b3c3ec971802836aa01a9b87ba0eecdaac70b100
toge <toge.mail@gmail.com>
2022-07-12 03:02:10 +0900

    add const

--- recipes/bzip3/all/test_package/test_package.c
+++ recipes/bzip3/all/test_package/test_package.c
@@ -4,7 +4,7 @@
 #include "libbz3.h"
 
 int main() {
-  unsigned int block_size = 8 * 1024 * 1024;
+  const unsigned int block_size = 8 * 1024 * 1024;
   struct bz3_state * state = bz3_new(block_size);
 
   bz3_free(state);
