35124411c3c9d96909a81eb9c7e550596e1cbd1c
Curtis Lo <sheepgrass@gmail.com>
2022-07-13 21:59:30 +0800

    validate compiler version for cpp_project_framework/1.0.0

--- recipes/cpp_project_framework/all/conanfile.py
+++ recipes/cpp_project_framework/all/conanfile.py
@@ -27,11 +27,30 @@ class CppProjectFrameworkConan(ConanFile):
     build_requires = "gtest/1.10.0", "doxygen/1.8.20", "benchmark/1.5.1"
     exports_resources = ".gitignore", "LICENSE", "conanfile.txt", "CMakeLists.txt", "make.bat", "Makefile", "cpp_project_framework_callables.cmake", "cpp_project_framework.cmake"
 
+    @property
+    def _minimum_cpp_standard(self):
+        return 17
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "17",
+            "gcc": "7",
+            "clang": "6",
+            "apple-clang": "10",
+        }
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 14)
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn(f"{self.name} recipe lacks information about the {self.settings.compiler} compiler support.")
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration(f"{self.name} requires C++{self._minimum_cpp_standard} support. The current compiler {self.settings.compiler} {self.settings.compiler.version} does not support it.")
         if self.settings.os != "Linux" and self.settings.os != "Windows":
-            raise ConanInvalidConfiguration("%s is just supported for Linux and Windows" % self.name)
+            raise ConanInvalidConfiguration(f"{self.name} is just supported for Linux and Windows")
 
     @property
     def _source_subfolder(self):
