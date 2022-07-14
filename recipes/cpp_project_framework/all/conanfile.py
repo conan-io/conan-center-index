e4c5a03985b23329155b250962ee4a8be10bb2c2
Curtis Lo <sheepgrass@gmail.com>
2022-07-14 21:16:21 +0800

    fix migrate recipe to conan v2 issue for cpp_project_framework/1.0.0

--- recipes/cpp_project_framework/all/conanfile.py
+++ recipes/cpp_project_framework/all/conanfile.py
@@ -1,9 +1,6 @@
 from conan import ConanFile
-from conan.tools import files
-from conan.tools.build import cppstd
-from conan.tools.cmake import CMake
+from conans import CMake, tools
 from conans.errors import ConanInvalidConfiguration
-from conans.tools import Version
 import os
 
 required_conan_version = ">=1.33.0"
@@ -38,7 +35,7 @@ class CppProjectFrameworkConan(ConanFile):
     @property
     def _minimum_compilers_version(self):
         return {
-            "Visual Studio": "17",
+            "Visual Studio": "16",
             "gcc": "7",
             "clang": "6",
             "apple-clang": "10",
@@ -51,7 +48,7 @@ class CppProjectFrameworkConan(ConanFile):
         compiler = self.settings.compiler
 
         if compiler.get_safe("cppstd"):
-            cppstd.check_min_cppstd(self, self._minimum_cpp_standard)
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
 
         if compiler == "gcc" or compiler == "clang":
             if compiler.get_safe("libcxx") == "libstdc++11":
@@ -61,7 +58,7 @@ class CppProjectFrameworkConan(ConanFile):
         if not min_version:
             self.output.warn(f"{self.name} recipe lacks information about the {compiler} compiler support.")
         else:
-            if Version(compiler.version) < min_version:
+            if tools.Version(compiler.version) < min_version:
                 raise ConanInvalidConfiguration(f"{self.name} requires C++{self._minimum_cpp_standard} support. The current compiler {compiler} {compiler.version} does not support it.")
 
     @property
@@ -81,7 +78,7 @@ class CppProjectFrameworkConan(ConanFile):
             del self.options.fPIC
 
     def source(self):
-        files.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def build(self):
         cmake = CMake(self)
