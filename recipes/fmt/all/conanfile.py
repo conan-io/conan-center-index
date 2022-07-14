f2ae8d0d2e6c3bf561304744cc2e1470825a5caf
SSE4 <tomskside@gmail.com>
2022-07-14 18:07:46 +0700

    Update recipes/fmt/all/conanfile.py
    Co-authored-by: Luis Martinez <lasote@gmail.com>

--- recipes/fmt/all/conanfile.py
+++ recipes/fmt/all/conanfile.py
@@ -64,7 +64,7 @@ class FmtConan(ConanFile):
             del self.options.fPIC
 
     def package_id(self):
-        if self.info.options.header_only:  # might be changed to self.info.header_only() in 1.50
+        if self.info.options.header_only:  # might be changed to self.info.clear() in 1.50
             self.info.header_only()
         else:
             del self.info.options.with_fmt_alias
