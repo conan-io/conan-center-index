644e5f3ac10b000fcd70a437bf3bbca95c0595d2
Curtis Lo <sheepgrass@gmail.com>
2022-07-14 20:11:04 +0800

    migrate recipe to conan v2 for cpp_project_framework/1.0.0

--- recipes/cpp_project_framework/all/conanfile.py
+++ recipes/cpp_project_framework/all/conanfile.py
@@ -1,5 +1,6 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile, tools
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake
 import os
 
 required_conan_version = ">=1.33.0"
