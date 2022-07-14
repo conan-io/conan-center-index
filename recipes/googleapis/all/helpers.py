f91dac25ff1e67925db14c60cc1751a32c8338ae
jgsogo <jgsogo@gmail.com>
2022-07-14 19:09:07 +0200

    f-string no backslash

--- recipes/googleapis/all/helpers.py
+++ recipes/googleapis/all/helpers.py
@@ -88,6 +88,7 @@ def parse_proto_libraries(filename, source_folder, error):
     proto_libraries = []
 
     basedir = os.path.dirname(filename)
+    current_folder_str = os.path.relpath(basedir, source_folder).replace('\\', '/')  # We need forward slashes because of Windows
     proto_library = None
     
     def parsing_sources(line):
@@ -101,7 +102,7 @@ def parse_proto_libraries(filename, source_folder, error):
         elif line.startswith("@com_google_googleapis//"):
             proto_library.deps.add(line[len("@com_google_googleapis"):])
         elif line.startswith(":"):
-            proto_library.deps.add(f"//{os.path.relpath(basedir, source_folder).replace('\\', '/')}{line}")
+            proto_library.deps.add(f"//{current_folder_str}{line}")
         elif line.startswith("//google/"):
             proto_library.deps.add(line)
         elif line.startswith("//grafeas/"):
@@ -137,7 +138,7 @@ def parse_proto_libraries(filename, source_folder, error):
             elif proto_library != None:
                 if line.startswith("name ="):
                     proto_library.name = re_name.search(line).group(1)
-                    proto_library.qname = f"//{os.path.relpath(basedir, source_folder).replace('\\', '/')}"
+                    proto_library.qname = f"//{current_folder_str}"
                 elif line.startswith("srcs = "):
                     m = re_srcs_oneline.search(line)
                     if m:
