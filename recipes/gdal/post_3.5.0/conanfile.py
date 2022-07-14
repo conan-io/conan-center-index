4797d57c0913f7f580450a41c70e34fecdca6e2f
Julien Pilet <julien.pilet@invision.ai>
2022-07-14 10:50:09 +0200

    Format strings with double quotes

--- recipes/gdal/post_3.5.0/conanfile.py
+++ recipes/gdal/post_3.5.0/conanfile.py
@@ -11,8 +11,8 @@ def _strip_version(package):
 
 
 def _option_name(dep):
-    if 'option' in dep:
-        return dep['option']
+    if "option" in dep:
+        return dep["option"]
     return "with_" + _strip_version(dep["require"])
 
 
@@ -56,7 +56,7 @@ class GdalConan(ConanFile):
 
     settings = "os", "arch", "compiler", "build_type"
 
-    generators = "cmake", 'cmake_find_package'
+    generators = "cmake", "cmake_find_package"
 
     # List taken from cmake/helpers/CheckDependentLibraries.cmake within gdal sources
     # with the command:
@@ -67,279 +67,279 @@ class GdalConan(ConanFile):
     #   'PROJ' has been added manually.
 
     gdal_deps = [{
-        'conan_dep': 'proj::projlib',
-        'require': "proj/9.0.0",
-        'dep': 'PROJ',
-        'default': True
+        "conan_dep": "proj::projlib",
+        "require": "proj/9.0.0",
+        "dep": "PROJ",
+        "default": True
     }, {
-        'dep': 'Armadillo',
-        'require': 'armadillo/10.7.3',
-        'descr': 'C++ library for linear algebra (used for TPS transformation)'
+        "dep": "Armadillo",
+        "require": "armadillo/10.7.3",
+        "descr": "C++ library for linear algebra (used for TPS transformation)"
     }, {
-        'dep': 'Arrow',
-        'conan_dep': 'arrow::libarrow',
-        'require': 'arrow/8.0.0',
-        'descr': 'Apache Arrow C++ library'
+        "dep": "Arrow",
+        "conan_dep": "arrow::libarrow",
+        "require": "arrow/8.0.0",
+        "descr": "Apache Arrow C++ library"
     }, {
-        'dep': 'BRUNSLI',
-        'descr': 'Enable BRUNSLI for JPEG packing in MRF'
+        "dep": "BRUNSLI",
+        "descr": "Enable BRUNSLI for JPEG packing in MRF"
     }, {
-        'require': "c-blosc/1.21.1",
-        'option': 'blosc',
-        'dep': 'Blosc',
-        'descr': 'Blosc compression'
+        "require": "c-blosc/1.21.1",
+        "option": "blosc",
+        "dep": "Blosc",
+        "descr": "Blosc compression"
     }, {
-        'require': "cfitsio/4.1.0",
-        'dep': 'CFITSIO',
-        'descr': 'C FITS I/O library'
+        "require": "cfitsio/4.1.0",
+        "dep": "CFITSIO",
+        "descr": "C FITS I/O library"
     }, {
         "conan_dep": "libcurl::curl",
-        'require': "libcurl/7.83.1",
-        'dep': 'CURL',
-        'descr': 'Enable drivers to use web API'
+        "require": "libcurl/7.83.1",
+        "dep": "CURL",
+        "descr": "Enable drivers to use web API"
     }, {
-        'dep': 'Crnlib',
-        'descr': 'enable gdal_DDS driver'
+        "dep": "Crnlib",
+        "descr": "enable gdal_DDS driver"
     }, {
-        'dep': 'CryptoPP',
-        'descr': 'Use crypto++ library for CPL.'
+        "dep": "CryptoPP",
+        "descr": "Use crypto++ library for CPL."
     }, {
-        'require': 'libdeflate/1.12',
-        'default': True,
-        'dep': 'Deflate',
-        'descr': 'Enable libdeflate compression library (complement to ZLib)'
+        "require": "libdeflate/1.12",
+        "default": True,
+        "dep": "Deflate",
+        "descr": "Enable libdeflate compression library (complement to ZLib)"
     }, {
-        'dep': 'ECW',
-        'descr': 'Enable ECW driver'
+        "dep": "ECW",
+        "descr": "Enable ECW driver"
     }, {
         "require": "expat/2.4.8",
-        'dep': 'EXPAT',
-        'descr': 'Read and write XML formats'
+        "dep": "EXPAT",
+        "descr": "Read and write XML formats"
     }, {
-        'dep': 'FYBA',
-        'descr': 'enable ogr_SOSI driver'
+        "dep": "FYBA",
+        "descr": "enable ogr_SOSI driver"
     }, {
-        'dep': 'FileGDB',
-        'descr': 'Enable FileGDB (based on closed-source SDK) driver'
+        "dep": "FileGDB",
+        "descr": "Enable FileGDB (based on closed-source SDK) driver"
     }, {
-        'dep': 'FreeXL',
-        'descr': 'Enable XLS driver'
+        "dep": "FreeXL",
+        "descr": "Enable XLS driver"
     }, {
-        'conan_dep': "geos::geos_c",
-        'require': "geos/3.11.0",
-        'default': True,
-        'dep': 'GEOS',
-        'descr': 'Geometry Engine - Open Source (GDAL core dependency)'
+        "conan_dep": "geos::geos_c",
+        "require": "geos/3.11.0",
+        "default": True,
+        "dep": "GEOS",
+        "descr": "Geometry Engine - Open Source (GDAL core dependency)"
     }, {
-        'require': "giflib/5.2.1",
-        'option': 'gif',
-        'default': True,
-        'dep': 'GIF',
-        'descr': 'GIF compression library (external)'
+        "require": "giflib/5.2.1",
+        "option": "gif",
+        "default": True,
+        "dep": "GIF",
+        "descr": "GIF compression library (external)"
     }, {
-        'dep': 'GTA',
-        'descr': 'Enable GTA driver'
+        "dep": "GTA",
+        "descr": "Enable GTA driver"
     }, {
-        'require': "libgeotiff/1.7.1",
-        'default': True,
-        'dep': 'GeoTIFF',
-        'descr': 'libgeotiff library (external)'
+        "require": "libgeotiff/1.7.1",
+        "default": True,
+        "dep": "GeoTIFF",
+        "descr": "libgeotiff library (external)"
     }, {
-        'dep': 'HDF4',
-        'descr': 'Enable HDF4 driver'
+        "dep": "HDF4",
+        "descr": "Enable HDF4 driver"
     }, {
-        'dep': 'HDF5',
-        'descr': 'Enable HDF5" COMPONENTS "C" "CXX'
+        "dep": "HDF5",
+        "descr": 'Enable HDF5" COMPONENTS "C" "CXX'
     }, {
-        'dep': 'HDFS',
-        'descr': 'Enable Hadoop File System through native library'
+        "dep": "HDFS",
+        "descr": "Enable Hadoop File System through native library"
     }, {
-        'dep': 'HEIF',
-        'descr': 'HEIF >= 1.1'
+        "dep": "HEIF",
+        "descr": "HEIF >= 1.1"
     }, {
-        'dep': 'IDB',
-        'descr': 'enable ogr_IDB driver'
+        "dep": "IDB",
+        "descr": "enable ogr_IDB driver"
     }, {
-        'require': "libiconv/1.17",
-        'dep': 'Iconv',
-        'descr': 'Character set recoding (used in GDAL portability library)'
+        "require": "libiconv/1.17",
+        "dep": "Iconv",
+        "descr": "Character set recoding (used in GDAL portability library)"
     }, {
-        'dep': 'JPEG',
-        'descr': 'JPEG compression library (external)'
+        "dep": "JPEG",
+        "descr": "JPEG compression library (external)"
     }, {
-        'require': "json-c/0.15",
-        'default': True,
-        'dep': 'JSONC',
-        'descr': 'json-c library (external)'
+        "require": "json-c/0.15",
+        "default": True,
+        "dep": "JSONC",
+        "descr": "json-c library (external)"
     }, {
-        'dep': 'JXL',
-        'descr': 'JPEG-XL compression (when used with internal libtiff)'
+        "dep": "JXL",
+        "descr": "JPEG-XL compression (when used with internal libtiff)"
     }, {
-        'dep': 'KDU',
-        'descr': 'Enable KAKADU'
+        "dep": "KDU",
+        "descr": "Enable KAKADU"
     }, {
-        'dep': 'KEA',
-        'descr': 'Enable KEA driver'
+        "dep": "KEA",
+        "descr": "Enable KEA driver"
     }, {
-        'dep': 'LERC',
-        'descr': 'Enable LERC (external)'
+        "dep": "LERC",
+        "descr": "Enable LERC (external)"
     }, {
-        'dep': 'LURATECH',
-        'descr': 'Enable JP2Lura driver'
+        "dep": "LURATECH",
+        "descr": "Enable JP2Lura driver"
     }, {
-        'require': "lz4/1.9.3",
-        'dep': 'LZ4',
-        'descr': 'LZ4 compression'
+        "require": "lz4/1.9.3",
+        "dep": "LZ4",
+        "descr": "LZ4 compression"
     }, {
-        'dep': 'LibLZMA',
-        'descr': 'LZMA compression'
+        "dep": "LibLZMA",
+        "descr": "LZMA compression"
     }, {
-        'require': "libxml2/2.9.14",
-        'dep': 'LibXml2',
-        'descr': 'Read and write XML formats'
+        "require": "libxml2/2.9.14",
+        "dep": "LibXml2",
+        "descr": "Read and write XML formats"
     }, {
-        'dep': 'MONGOCXX',
-        'descr': 'Enable MongoDBV3 driver'
+        "dep": "MONGOCXX",
+        "descr": "Enable MongoDBV3 driver"
     }, {
-        'dep': 'MRSID',
-        'descr': 'MrSID raster SDK'
+        "dep": "MRSID",
+        "descr": "MrSID raster SDK"
     }, {
-        'dep': 'MSSQL_NCLI',
-        'descr': 'MSSQL Native Client to enable bulk copy'
+        "dep": "MSSQL_NCLI",
+        "descr": "MSSQL Native Client to enable bulk copy"
     }, {
-        'dep': 'MSSQL_ODBC',
-        'descr': 'MSSQL ODBC driver to enable bulk copy'
+        "dep": "MSSQL_ODBC",
+        "descr": "MSSQL ODBC driver to enable bulk copy"
     }, {
-        'dep': 'MySQL',
-        'descr': 'MySQL'
+        "dep": "MySQL",
+        "descr": "MySQL"
     }, {
-        'dep': 'NetCDF',
-        'descr': 'Enable netCDF driver'
+        "dep": "NetCDF",
+        "descr": "Enable netCDF driver"
     }, {
-        'dep': 'ODBC',
-        'descr': 'Enable DB support through ODBC'
+        "dep": "ODBC",
+        "descr": "Enable DB support through ODBC"
     }, {
-        'dep': 'ODBCCPP',
-        'descr': 'odbc-cpp library (external)'
+        "dep": "ODBCCPP",
+        "descr": "odbc-cpp library (external)"
     }, {
-        'dep': 'OGDI',
-        'descr': 'Enable ogr_OGDI driver'
+        "dep": "OGDI",
+        "descr": "Enable ogr_OGDI driver"
     }, {
-        'dep': 'OpenCAD',
-        'descr': 'libopencad (external, used by OpenCAD driver)'
+        "dep": "OpenCAD",
+        "descr": "libopencad (external, used by OpenCAD driver)"
     }, {
-        'dep': 'OpenCL',
-        'descr': 'Enable OpenCL (may be used for warping)'
+        "dep": "OpenCL",
+        "descr": "Enable OpenCL (may be used for warping)"
     }, {
-        'dep': 'OpenEXR',
-        'descr': 'OpenEXR >=2.2'
+        "dep": "OpenEXR",
+        "descr": "OpenEXR >=2.2"
     }, {
         "require": "openssl/1.1.1o",
         "conan_dep": "openssl::ssl",
-        'dep': 'OpenSSL',
-        'descr': 'Use OpenSSL library'
+        "dep": "OpenSSL",
+        "descr": "Use OpenSSL library"
     }, {
-        'dep': 'Oracle',
-        'descr': 'Enable Oracle OCI driver'
+        "dep": "Oracle",
+        "descr": "Enable Oracle OCI driver"
     }, {
-        'dep': 'PCRE',
-        'descr': 'Enable PCRE support for sqlite3'
+        "dep": "PCRE",
+        "descr": "Enable PCRE support for sqlite3"
     }, {
-        'dep': 'PCRE2',
-        'descr': 'Enable PCRE2 support for sqlite3'
+        "dep": "PCRE2",
+        "descr": "Enable PCRE2 support for sqlite3"
     }, {
-        'dep': 'PDFIUM',
-        'descr': 'Enable PDF driver with Pdfium (read side)'
+        "dep": "PDFIUM",
+        "descr": "Enable PDF driver with Pdfium (read side)"
     }, {
-        'require': "libpng/1.6.37",
-        'default': True,
-        'dep': 'PNG',
-        'descr': 'PNG compression library (external)'
+        "require": "libpng/1.6.37",
+        "default": True,
+        "dep": "PNG",
+        "descr": "PNG compression library (external)"
     }, {
-        'dep': 'Parquet',
-        'descr': 'Apache Parquet C++ library'
+        "dep": "Parquet",
+        "descr": "Apache Parquet C++ library"
     }, {
-        'dep': 'Podofo',
-        'descr': 'Enable PDF driver with Podofo (read side)'
+        "dep": "Podofo",
+        "descr": "Enable PDF driver with Podofo (read side)"
     }, {
         "require": "poppler/21.07.0",
         "conan_dep": "poppler::libpoppler",
-        'dep': 'Poppler',
-        'descr': 'Enable PDF driver with Poppler (read side)'
+        "dep": "Poppler",
+        "descr": "Enable PDF driver with Poppler (read side)"
     }, {
         "conan_dep": "libpq::pq",
-        'require': "libpq/14.2",
-        'option': 'with_pg',
-        'dep': 'PostgreSQL',
-        'descr': ''
+        "require": "libpq/14.2",
+        "option": "with_pg",
+        "dep": "PostgreSQL",
+        "descr": ""
     }, {
-        'conan_dep': 'qhull::libqhull',
-        'require': "qhull/8.0.1",
-        'default': True,
-        'dep': 'QHULL',
-        'descr': 'Enable QHULL (external)'
+        "conan_dep": "qhull::libqhull",
+        "require": "qhull/8.0.1",
+        "default": True,
+        "dep": "QHULL",
+        "descr": "Enable QHULL (external)"
     }, {
-        'dep': 'RASDAMAN',
-        'descr': 'enable rasdaman driver'
+        "dep": "RASDAMAN",
+        "descr": "enable rasdaman driver"
     }, {
-        'dep': 'RASTERLITE2',
-        'descr': 'Enable RasterLite2 support for sqlite3'
+        "dep": "RASTERLITE2",
+        "descr": "Enable RasterLite2 support for sqlite3"
     }, {
-        'dep': 'SFCGAL',
-        'descr': 'gdal core supports ISO 19107:2013 and OGC Simple Features' \
-                + ' Access 1.2 for 3D operations'
+        "dep": "SFCGAL",
+        "descr": "gdal core supports ISO 19107:2013 and OGC Simple Features" \
+                + " Access 1.2 for 3D operations"
     }, {
-        'dep': 'SPATIALITE',
-        'descr': 'Enable spatialite support for sqlite3'
+        "dep": "SPATIALITE",
+        "descr": "Enable spatialite support for sqlite3"
     }, {
-        'conan_dep': 'sqlite3::sqlite',
-        'require': "sqlite3/3.38.5",
-        'default': True,
-        'dep': 'SQLite3',
-        'descr': 'Enable SQLite3 support (used by SQLite/Spatialite, GPKG, ' \
-                + 'Rasterlite, MBTiles, etc.)'
+        "conan_dep": "sqlite3::sqlite",
+        "require": "sqlite3/3.38.5",
+        "default": True,
+        "dep": "SQLite3",
+        "descr": "Enable SQLite3 support (used by SQLite/Spatialite, GPKG, " \
+                + "Rasterlite, MBTiles, etc.)"
     }, {
-        'dep': 'SWIG',
-        'descr': 'Enable language bindings'
+        "dep": "SWIG",
+        "descr": "Enable language bindings"
     }, {
-        'dep': 'Shapelib',
-        'descr': 'Enable Shapelib support (not recommended, internal Shapelib' \
-                + ' is preferred).'
+        "dep": "Shapelib",
+        "descr": "Enable Shapelib support (not recommended, internal Shapelib" \
+                + " is preferred)."
     }, {
-        'dep': 'TEIGHA',
-        'descr': 'Enable DWG and DGNv8 drivers'
+        "dep": "TEIGHA",
+        "descr": "Enable DWG and DGNv8 drivers"
     }, {
-        'require': "libtiff/4.3.0",
-        'dep': 'TIFF',
-        'default': True,
-        'descr': 'Support for the Tag Image File Format (TIFF).'
+        "require": "libtiff/4.3.0",
+        "dep": "TIFF",
+        "default": True,
+        "descr": "Support for the Tag Image File Format (TIFF)."
     }, {
-        'dep': 'TileDB',
-        'descr': 'enable TileDB driver'
+        "dep": "TileDB",
+        "descr": "enable TileDB driver"
     }, {
-        'dep': 'WebP',
-        'descr': 'WebP compression'
+        "dep": "WebP",
+        "descr": "WebP compression"
     }, {
-        'dep': 'XercesC',
-        'descr': 'Read and write XML formats (needed for GMLAS and ILI drivers)'
+        "dep": "XercesC",
+        "descr": "Read and write XML formats (needed for GMLAS and ILI drivers)"
     }, {
-        'require': "zlib/1.2.12",
-        'default': True,
-        'dep': 'ZLIB',
-        'descr': 'zlib (external)'
+        "require": "zlib/1.2.12",
+        "default": True,
+        "dep": "ZLIB",
+        "descr": "zlib (external)"
     }, {
-        "conan_dep": 'zstd::zstdlib',
-        'require': 'zstd/1.5.2',
-        'dep': 'ZSTD',
-        'descr': 'ZSTD compression library'
+        "conan_dep": "zstd::zstdlib",
+        "require": "zstd/1.5.2",
+        "dep": "ZSTD",
+        "descr": "ZSTD compression library"
     }, {
-        'dep': 'rdb',
-        'descr': 'enable RIEGL RDB library'
+        "dep": "rdb",
+        "descr": "enable RIEGL RDB library"
     },
     {
-        'conan_dep': [ 'libkml::kmldom', 'libkml::kmlengine' ],
-        'dep': 'LibKML',
-        'require': 'libkml/1.3.0'
+        "conan_dep": [ "libkml::kmldom", "libkml::kmlengine" ],
+        "dep": "LibKML",
+        "require": "libkml/1.3.0"
     }]
 
     options = _make_options(gdal_deps)
@@ -366,7 +366,7 @@ class GdalConan(ConanFile):
 
         for dep in self.gdal_deps:
             if "require" in dep and getattr(self.options, _option_name(dep)):
-                self.requires(dep['require'])
+                self.requires(dep["require"])
 
     def layout(self):
         cmake_layout(self)
@@ -414,7 +414,7 @@ class GdalConan(ConanFile):
             "sqlite3"].enable_rtree
 
         for dep in self.gdal_deps:
-            upper = dep['dep'].upper()
+            upper = dep["dep"].upper()
             enabled = "require" in dep and getattr(self.options,
                                                    _option_name(dep))
             cmake.definitions["GDAL_USE_" + upper] = enabled
@@ -422,7 +422,7 @@ class GdalConan(ConanFile):
                 cmake.definitions["GDAL_CONAN_PACKAGE_FOR_" + upper] = \
                        _strip_version(dep["require"])
             else:
-                cmake.definitions[dep['dep'] + "_FOUND"] = False
+                cmake.definitions[dep["dep"] + "_FOUND"] = False
 
         for k, v in cmake.definitions.items():
             print(k, " = ", v)
