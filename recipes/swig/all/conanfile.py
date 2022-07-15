54a32d712d502a341352b2c589a06739387649d1
Sergey Vidyuk <svidyuk@yandex-team.ru>
2022-07-15 23:41:20 +0700

    swig/4.0.2: Fix invalid use of cpp_info.build_modules
    cpp_info.build_modules must be dict with per generator file lists not just
    file list. This commit fixes swig package build with conan-center hooks
    enabled.

--- recipes/swig/all/conanfile.py
+++ recipes/swig/all/conanfile.py
@@ -149,7 +149,10 @@ class SwigConan(ConanFile):
         self.cpp_info.names["cmake_find_package"] = "SWIG"
         self.cpp_info.names["cmake_find_package_multi"] = "SWIG"
         self.cpp_info.builddirs = [self._module_subfolder]
-        self.cpp_info.build_modules = [os.path.join(self._module_subfolder, self._module_file)]
+        self.cpp_info.build_modules["cmake_find_package"] = \
+            [os.path.join(self._module_subfolder, self._module_file)]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = \
+            [os.path.join(self._module_subfolder, self._module_file)]
 
         bindir = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH environment variable: {}".format(bindir))
