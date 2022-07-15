95f270afd2581de5d1dc258bc9c1cc05000c43b9
danimtb <danimanzaneque@gmail.com>
2022-07-15 12:48:01 +0200

    add run env

--- recipes/expat/all/test_package/conanfile.py
+++ recipes/expat/all/test_package/conanfile.py
@@ -6,7 +6,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
 
     def layout(self):
         cmake_layout(self)
