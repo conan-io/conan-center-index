0a13dcdc05c756dceb12c6b589fc81dc3ee2e94d
toge <toge.mail@gmail.com>
2022-07-16 14:04:49 +0900

    downgrade openssl/1.1.1p

--- recipes/xmlsec/all/conanfile.py
+++ recipes/xmlsec/all/conanfile.py
@@ -59,7 +59,7 @@ class XmlSecConan(ConanFile):
     def requirements(self):
         self.requires("libxml2/2.9.14")
         if self.options.with_openssl:
-            self.requires("openssl/1.1.1q")
+            self.requires("openssl/1.1.1p")
         if self.options.with_xslt:
             self.requires("libxslt/1.1.34")
 
