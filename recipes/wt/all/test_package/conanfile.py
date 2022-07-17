de84b970c0a374bb4970abe8cb762a6458bebcc2
toge <toge.mail@gmail.com>
2022-07-18 01:24:26 +0900

    use `conan.tools.build.cross_building`

--- recipes/wt/all/test_package/conanfile.py
+++ recipes/wt/all/test_package/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+from conan.tools.build import cross_building
 import os
 
 
@@ -12,7 +13,7 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             args = " --docroot . --http-listen http://127.0.0.1:8080"
             self.run(bin_path + args, run_environment=True)
