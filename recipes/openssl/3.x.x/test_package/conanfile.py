5631efc6f682589930ec16e9b59e175b181e8c1e
Jan Niklas Grieb <jan.grieb@rohde-schwarz.com>
2022-07-18 15:55:58 +0200

    [openssl] conan v2 linter compatibility
    import cross_building from conan.tools.build instead of conan.tools

--- recipes/openssl/3.x.x/test_package/conanfile.py
+++ recipes/openssl/3.x.x/test_package/conanfile.py
@@ -1,4 +1,5 @@
 from conans import CMake, tools, ConanFile
+from conan.tools.build import cross_building
 import os
 
 
@@ -23,7 +24,7 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not cross_building(self):
             bin_path = os.path.join("bin", "digest")
             self.run(bin_path, run_environment=True)
 
