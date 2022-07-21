c0df1a37acc394835a0d4d9d4da23f766731e27e
Jordan Williams <jordan@jwillikers.com>
2022-07-21 17:01:22 -0500

    Revert "Use ConanFile from conans in attempt to fix Visual Studio builds"
    This reverts commit 549ddbc5ceb7ab1232829983acea2ddbe64a6608.

--- recipes/dbus/1.x.x/test_package/conanfile.py
+++ recipes/dbus/1.x.x/test_package/conanfile.py
@@ -1,13 +1,13 @@
 import os
 
+from conan import ConanFile
 from conan.tools.build import cross_building
-
-from conans import ConanFile, CMake
+from conans import CMake
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "cmake", "cmake_find_package_multi", "VirtualBuildEnv", "VirtualRunEnv"
 
     def build(self):
         cmake = CMake(self)
