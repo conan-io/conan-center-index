2b45f7f2b932c18024bf35c4b9745f22682b4659
Jai Bellare <jaibellare@gmail.com>
2022-07-24 01:17:42 +0530

    conan v2 fix

--- recipes/samarium/v1.0.1/conanfile.py
+++ recipes/samarium/v1.0.1/conanfile.py
@@ -1,92 +1,62 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
-import functools
+from os import path
 
-required_conan_version = ">=1.43.0"
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.files import get, copy
+
+required_conan_version = ">=1.47.0"
 
 
 class SamariumConan(ConanFile):
     name = "samarium"
+    version = "1.0.1"
     description = "2-D physics simulation library"
     homepage = "https://strangequark1041.github.io/samarium/"
     url = "https://github.com/conan-io/conan-center-index/"
     license = "MIT"
     topics = ("cpp20", "physics", "2d", "simulation")
+    generators = "CMakeDeps", "CMakeToolchain"
 
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {"shared": [True, False], "fPIC": [
+        True, False], "build_tests": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "build_tests": False}
 
-    generators = "cmake", "cmake_find_package_multi"
-    requires = "fmt/9.0.0", "sfml/2.5.1", "range-v3/0.12.0", "stb/cci.20210910"
+    def source(self):
+        get(self, **self.conan_data["sources"]
+            [str(self.version)], strip_root=True)
 
-    @property
-    def _min_cppstd(self):
-        return "20"
+    def requirements(self):
+        self.requires("fmt/9.0.0")
+        self.requires("sfml/2.5.1")
+        self.requires("range-v3/0.12.0")
+        self.requires("stb/cci.20210910")
 
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "gcc": "11.0",
-            "Visual Studio": "16.9",
-            "clang": "13",
-            "apple-clang": "13",
-        }
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build"
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+        if self.options.build_tests:
+            self.requires('catch2/3.0.1')
+            self.requires('benchmark/1.6.1')
 
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
 
-    def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._min_cppstd)
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
-        compiler = str(self.settings.compiler)
-        if compiler not in self._compilers_minimum_version:
-            self.output.warn("Unknown compiler, assuming it supports at least C++20")
-            return
-
-        version = tools.Version(self.settings.compiler.version)
-        if version < self._compilers_minimum_version[compiler]:
-            raise ConanInvalidConfiguration(f"{self.name} requires a compiler that supports at least C++20")
-
-    def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  strip_root=True, destination=self._source_subfolder)
-
-    @functools.lru_cache(1)
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure(build_script_folder="src")
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE.md", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE.md", src=self.folders.source_folder,
+             dst=path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs.append("samarium")
+        self.cpp_info.libs = ["samarium"]
--- recipes/samarium/v1.0.1/test_package/conanfile.py
+++ recipes/samarium/v1.0.1/test_package/conanfile.py
@@ -1,10 +1,12 @@
-from conans import ConanFile, CMake, tools
 import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def build(self):
         cmake = CMake(self)
@@ -12,6 +14,5 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        bin_path = os.path.abspath("test_package")
+        self.run(bin_path)
