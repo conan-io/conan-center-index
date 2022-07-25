dfef21457ff4800538e96f0e288dfac5d1c99fb3
andrei malashkin <malashkin.andrey@gmail.com>
2022-07-25 10:00:01 +0200

    drop gcc 10 support

--- recipes/mold/all/conanfile.py
+++ recipes/mold/all/conanfile.py
@@ -19,8 +19,8 @@ class MoldConan(ConanFile):
     def validate(self):
         if self.settings.os == "Windows":
             raise ConanInvalidConfiguration('{} can\'t be built on {}.'.format(self.name, self.settings.os))
-        if self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) <= "9":
-            raise ConanInvalidConfiguration('Use Gcc compiler version higher than 9')
+        if self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) <= "10":
+            raise ConanInvalidConfiguration('Use Gcc compiler version higher than 10')
         if (self.settings.compiler == "clang" or self.settings.compiler == "apple-clang") and tools.Version(self.settings.compiler.version) <= "11":
             raise ConanInvalidConfiguration('Use Clang compiler version higher than 11')
         if self.settings.compiler == "apple-clang" and "arm" in self.settings.arch :
