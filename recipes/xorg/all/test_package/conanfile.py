bc5dfb39756fe51d7b596b259fd731ed1e36c890
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-07-25 11:22:43 +0200

    (#11872) xorg: check .pc files
    * xorg: check .pc files
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py

--- recipes/xorg/all/test_package/conanfile.py
+++ recipes/xorg/all/test_package/conanfile.py
@@ -1,17 +1,20 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conans import CMake
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "pkg_config"
 
     def build(self):
+        self.run("pkg-config --validate ./uuid.pc")
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
