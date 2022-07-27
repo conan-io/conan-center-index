0fbbfa3c5715853e5cebd452f1fada152960dd84
Brian Szmyd <bszmyd@ebay.com>
2022-07-27 07:32:26 -0700

    Support < GCC-5

--- recipes/forestdb/all/test_package/test_package.cpp
+++ recipes/forestdb/all/test_package/test_package.cpp
@@ -3,8 +3,8 @@
 #include <libforestdb/forestdb.h>
 
 int main(void) {
-    auto default_config = fdb_get_default_config();
-    auto const status = fdb_init(&default_config);
+    fdb_config default_config = fdb_get_default_config();
+    fdb_status const status = fdb_init(&default_config);
     if (FDB_RESULT_SUCCESS != status) return -1;
     return 0;
 }
