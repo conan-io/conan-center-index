04195378b99189fec786815bc48d8560dae34f97
Brian Szmyd <bszmyd@ebay.com>
2022-07-27 08:14:36 -0600

    Fix variable names, fdb types not scoped.

--- recipes/forestdb/all/test_package/test_package.cpp
+++ recipes/forestdb/all/test_package/test_package.cpp
@@ -1,9 +1,10 @@
+#include <cstdlib>
 #include <iostream>
 #include <libforestdb/forestdb.h>
 
 int main(void) {
-    auto fdb_config = fdb_get_default_config();
-    auto const fdb_status = fdb_init(&fdb_config);
-    if (FDB_RESULT_SUCCESS != fdb_status) exit(-1);
+    auto default_config = fdb_get_default_config();
+    auto const status = fdb_init(&default_config);
+    if (FDB_RESULT_SUCCESS != status) return -1;
     return 0;
 }
