f0b7712a1cece5b0507438b85abb325bdfaec781
Uilian Ries <uilianries@gmail.com>
2022-07-30 16:24:59 +0200

    spdlog prepared for Conan 2.0
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/spdlog/all/conanfile.py
+++ recipes/spdlog/all/conanfile.py
@@ -1,8 +1,12 @@
-from conans import CMake, ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import get, copy, rmdir, replace_in_file
+from conan.tools.scm import Version
+from conan.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.43.0"
+
+required_conan_version = ">=1.47.0"
 
 
 class SpdlogConan(ConanFile):
@@ -10,9 +14,8 @@ class SpdlogConan(ConanFile):
     description = "Fast C++ logging library"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/gabime/spdlog"
-    topics = ("spdlog", "logging", "header-only")
+    topics = ("logging", "log-filtering", "header-only")
     license = "MIT"
-
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -30,14 +33,7 @@ class SpdlogConan(ConanFile):
         "wchar_filenames": False,
         "no_exceptions": False,
     }
-
-    exports_sources = "CMakeLists.txt"
-    generators = "cmake", "cmake_find_package", "cmake_find_package_multi"
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    generators = "CMakeDeps"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -51,13 +47,13 @@ class SpdlogConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        if tools.Version(self.version) >= "1.10.0":
+        if Version(self.version) >= "1.10.0":
             self.requires("fmt/8.1.1")
-        elif tools.Version(self.version) >= "1.9.0":
+        elif Version(self.version) >= "1.9.0":
             self.requires("fmt/8.0.1")
-        elif tools.Version(self.version) >= "1.7.0":
+        elif Version(self.version) >= "1.7.0":
             self.requires("fmt/7.1.3")
-        elif tools.Version(self.version) >= "1.5.0":
+        elif Version(self.version) >= "1.5.0":
             self.requires("fmt/6.2.1")
         else:
             self.requires("fmt/6.0.0")
@@ -66,67 +62,62 @@ class SpdlogConan(ConanFile):
         if self.settings.os != "Windows" and (self.options.wchar_support or self.options.wchar_filenames):
             raise ConanInvalidConfiguration("wchar is only supported under windows")
         if self.options.get_safe("shared", False):
-            if self.settings.os == "Windows" and tools.Version(self.version) < "1.6.0":
+            if self.settings.os == "Windows" and Version(self.version) < "1.6.0":
                 raise ConanInvalidConfiguration("spdlog shared lib is not yet supported under windows")
             if self.settings.compiler == "Visual Studio" and "MT" in self.settings.compiler.runtime:
                 raise ConanInvalidConfiguration("Visual Studio build for shared library with MT runtime is not supported")
+        if Version(self.version) < "1.7" and Version(self.deps_cpp_info["fmt"].version) >= "7":
+            raise ConanInvalidConfiguration("The project {}/{} requires fmt < 7.x".format(self.name, self.version))
 
     def package_id(self):
         if self.options.header_only:
             self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["SPDLOG_BUILD_EXAMPLE"] = False
-        self._cmake.definitions["SPDLOG_BUILD_EXAMPLE_HO"] = False
-        self._cmake.definitions["SPDLOG_BUILD_TESTS"] = False
-        self._cmake.definitions["SPDLOG_BUILD_TESTS_HO"] = False
-        self._cmake.definitions["SPDLOG_BUILD_BENCH"] = False
-        self._cmake.definitions["SPDLOG_FMT_EXTERNAL"] = True
-        self._cmake.definitions["SPDLOG_FMT_EXTERNAL_HO"] = self.options["fmt"].header_only
-        self._cmake.definitions["SPDLOG_BUILD_SHARED"] = not self.options.header_only and self.options.shared
-        self._cmake.definitions["SPDLOG_WCHAR_SUPPORT"] = self.options.wchar_support
-        self._cmake.definitions["SPDLOG_WCHAR_FILENAMES"] = self.options.wchar_filenames
-        self._cmake.definitions["SPDLOG_INSTALL"] = True
-        self._cmake.definitions["SPDLOG_NO_EXCEPTIONS"] = self.options.no_exceptions
-        if self.settings.os in ("iOS", "tvOS", "watchOS"):
-            self._cmake.definitions["SPDLOG_NO_TLS"] = True
-        if tools.cross_building(self):
-            # Workaround to find CMake config files in some cross-build scenario
-            # TODO: to remove if something like https://github.com/conan-io/conan/issues/9427#issuecomment-993685376
-            # is implemented
-            self._cmake.definitions["CMAKE_FIND_ROOT_PATH_MODE_PACKAGE"] = "BOTH"
-        self._cmake.configure()
-        return self._cmake
+    def generate(self):
+        if not self.options.header_only:
+            tc = CMakeToolchain(self)
+            tc.variables["SPDLOG_BUILD_EXAMPLE"] = False
+            tc.variables["SPDLOG_BUILD_EXAMPLE_HO"] = False
+            tc.variables["SPDLOG_BUILD_TESTS"] = False
+            tc.variables["SPDLOG_BUILD_TESTS_HO"] = False
+            tc.variables["SPDLOG_BUILD_BENCH"] = False
+            tc.variables["SPDLOG_FMT_EXTERNAL"] = True
+            tc.variables["SPDLOG_FMT_EXTERNAL_HO"] = self.options["fmt"].header_only
+            tc.variables["SPDLOG_BUILD_SHARED"] = not self.options.header_only and self.options.shared
+            tc.variables["SPDLOG_WCHAR_SUPPORT"] = self.options.wchar_support
+            tc.variables["SPDLOG_WCHAR_FILENAMES"] = self.options.wchar_filenames
+            tc.variables["SPDLOG_INSTALL"] = True
+            tc.variables["SPDLOG_NO_EXCEPTIONS"] = self.options.no_exceptions
+            if self.settings.os in ("iOS", "tvOS", "watchOS"):
+                self._cmake.definitions["SPDLOG_NO_TLS"] = True
+            tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
 
     def _disable_werror(self):
-        tools.replace_in_file(os.path.join(self._source_subfolder, "cmake", "utils.cmake"), "/WX", "")
+        replace_in_file(self, os.path.join(self.source_folder, "cmake", "utils.cmake"), "/WX", "")
 
     def build(self):
-        if tools.Version(self.version) < "1.7" and tools.Version(self.deps_cpp_info["fmt"].version) >= "7":
-            raise ConanInvalidConfiguration("The project {}/{} requires fmt < 7.x".format(self.name, self.version))
-
         self._disable_werror()
         if not self.options.header_only:
-            cmake = self._configure_cmake()
+            cmake = CMake(self)
+            cmake.configure()
             cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        copy(self, "LICENSE", dst="licenses", src=self.source_folder)
         if self.options.header_only:
-            self.copy(pattern="*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
+            copy(self, pattern="*.h", dst="include", src=os.path.join(self.source_folder, "include"))
         else:
-            cmake = self._configure_cmake()
+            cmake = CMake(self)
             cmake.install()
-            tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-            tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-            tools.rmdir(os.path.join(self.package_folder, "lib", "spdlog", "cmake"))
+            rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+            rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+            rmdir(self, os.path.join(self.package_folder, "lib", "spdlog", "cmake"))
 
     def package_info(self):
         target = "spdlog_header_only" if self.options.header_only else "spdlog"
@@ -134,6 +125,7 @@ class SpdlogConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "spdlog::{}".format(target))
         self.cpp_info.set_property("pkg_config_name", "spdlog")
 
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.names["cmake_find_package"] = "spdlog"
         self.cpp_info.names["cmake_find_package_multi"] = "spdlog"
         self.cpp_info.components["libspdlog"].names["cmake_find_package"] = target
@@ -142,7 +134,7 @@ class SpdlogConan(ConanFile):
         self.cpp_info.components["libspdlog"].defines.append("SPDLOG_FMT_EXTERNAL")
         self.cpp_info.components["libspdlog"].requires = ["fmt::fmt"]
         if not self.options.header_only:
-            self.cpp_info.components["libspdlog"].libs = tools.collect_libs(self)
+            self.cpp_info.components["libspdlog"].libs = ["spdlog"]
             self.cpp_info.components["libspdlog"].defines.append("SPDLOG_COMPILED_LIB")
         if self.options.wchar_support:
             self.cpp_info.components["libspdlog"].defines.append("SPDLOG_WCHAR_TO_UTF8_SUPPORT")
