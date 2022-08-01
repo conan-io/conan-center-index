899a9967787ee954d06eb1c9e9859c159d944caf
Pat Mancuso <pmancuso@virtru.com>
2022-08-01 15:15:13 -0400

    Updates for linter

--- recipes/opentdf-client/all/conanfile.py
+++ recipes/opentdf-client/all/conanfile.py
@@ -1,5 +1,5 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile, CMake, tools
+from conan.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.40.0"
@@ -67,10 +67,10 @@ class OpenTDFConan(ConanFile):
         self.requires("libxml2/2.9.10@")
         self.requires("nlohmann_json/3.10.4@")
         self.requires("jwt-cpp/0.4.0@")
-        # We do not require zlib but conan-center only allows 'stock' references, and boost+libxml2 
+        # We do not require zlib but conan-center only allows 'stock' references, and boost+libxml2
         # specify differerent versions, which causes a build fail due to the dependency conflict.
         # Overriding the version here allows a clean build with the stock build settings.
-        if not self.options.without_zlib: 
+        if not self.options.without_zlib:
             self.requires("zlib/1.2.12@")
 
     def config_options(self):
@@ -112,4 +112,3 @@ class OpenTDFConan(ConanFile):
             self.cpp_info.components["libopentdf"].requires = ["openssl::openssl", "boost::boost", "ms-gsl::ms-gsl", "libxml2::libxml2", "jwt-cpp::jwt-cpp", "nlohmann_json::nlohmann_json"]
         else:
             self.cpp_info.components["libopentdf"].requires = ["openssl::openssl", "boost::boost", "ms-gsl::ms-gsl", "libxml2::libxml2", "jwt-cpp::jwt-cpp", "nlohmann_json::nlohmann_json", "zlib::zlib"]
-
