260eb39225508d47cc3ea7b1234bafcc0723a820
Pat Mancuso <pmancuso@virtru.com>
2022-08-02 10:07:08 -0400

    Start over...

--- recipes/opentdf-client/all/conanfile.py
+++ recipes/opentdf-client/all/conanfile.py
@@ -1,5 +1,5 @@
-from conan import ConanFile, tools
-from conan.tools.cmake import CMake
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.40.0"
@@ -15,7 +15,6 @@ class OpenTDFConan(ConanFile):
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"build_python": [True, False], "fPIC": [True, False], "without_libiconv": [True, False], "without_zlib": [True, False], "branch_version": [True, False]}
-    # Although the default below says otherwise, RECOMMEND that both without_libiconv and without_zlib be set to True as they are not needed
     default_options = {"build_python": False, "fPIC": True, "without_libiconv": False, "without_zlib": False, "branch_version": False}
     exports_sources = ["CMakeLists.txt"]
 
@@ -42,6 +41,18 @@ class OpenTDFConan(ConanFile):
             "apple-clang": "12.0.0"
         }
 
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe could not find compiler minimum version for {}.".format(
+                self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} recipe needs minimum version {} but found tools.Version({}) = {}".format(
+                    self.name, min_version, self.settings.compiler.version, tools.Version(self.settings.compiler.version) ))
+
     def configure(self):
         if self.options.without_zlib:
             self.options["libxml2"].zlib = False
@@ -50,7 +61,7 @@ class OpenTDFConan(ConanFile):
             self.options["boost"].without_log = True
 
     def requirements(self):
-        self.requires("openssl/1.1.1q@")
+        self.requires("openssl/1.1.1o@")
         self.requires("boost/1.76.0@")
         self.requires("ms-gsl/2.1.0@")
         self.requires("libxml2/2.9.10@")
@@ -70,18 +81,17 @@ class OpenTDFConan(ConanFile):
             del self.options.fPIC
 
     def source(self):
-        # If branch_version is TRUE then the version info will be used as a branch to clone, otherwise the conandata.yml info will be used
         if self.options.branch_version:
             self.output.warn("Building branch_version = {}".format(self.version))
             self.run("git clone git@github.com:opentdf/client-cpp.git --depth 1 --branch " + self.version + " " + self._source_subfolder)
         else:
-            tools.files.get(self, **self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+            tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
-        self._cmake.configure(build_script_folder=self._build_subfolder)
+        self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
     def build(self):
@@ -105,3 +115,4 @@ class OpenTDFConan(ConanFile):
             self.cpp_info.components["libopentdf"].requires = ["openssl::openssl", "boost::boost", "ms-gsl::ms-gsl", "libxml2::libxml2", "jwt-cpp::jwt-cpp", "nlohmann_json::nlohmann_json"]
         else:
             self.cpp_info.components["libopentdf"].requires = ["openssl::openssl", "boost::boost", "ms-gsl::ms-gsl", "libxml2::libxml2", "jwt-cpp::jwt-cpp", "nlohmann_json::nlohmann_json", "zlib::zlib"]
+
