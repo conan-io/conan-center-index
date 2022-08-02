16e8f8055fb37598972c370b2e70fecc957a023a
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2022-08-02 13:51:45 +0100

    Adapt zstd recipe to be cross-compatible with Conan v1 and v2

--- recipes/zstd/all/conanfile.py
+++ recipes/zstd/all/conanfile.py
@@ -1,7 +1,11 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools import files
+from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.scm import Version
+
 import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.50.0"
 
 
 class ZstdConan(ConanFile):
@@ -24,21 +28,13 @@ class ZstdConan(ConanFile):
         "threading": True,
     }
 
-    generators = "cmake"
-    _cmake = None
-
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
     def export_sources(self):
-        self.copy("CMakeLists.txt")
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+            files.copy(self, patch["patch_file"], src=self.recipe_folder, dst=self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -51,41 +47,39 @@ class ZstdConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        files.get(self, **self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["ZSTD_BUILD_PROGRAMS"] = False
-        self._cmake.definitions["ZSTD_BUILD_STATIC"] = not self.options.shared
-        self._cmake.definitions["ZSTD_BUILD_SHARED"] = self.options.shared
-        self._cmake.definitions["ZSTD_MULTITHREAD_SUPPORT"] = self.options.threading
-        if tools.Version(self.version) < "1.4.3":
-            # Generate a relocatable shared lib on Macos
-            self._cmake.definitions["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["ZSTD_BUILD_PROGRAMS"] = False
+        tc.variables["ZSTD_BUILD_STATIC"] = not self.options.shared
+        tc.variables["ZSTD_BUILD_SHARED"] = self.options.shared
+        tc.variables["ZSTD_MULTITHREAD_SUPPORT"] = self.options.threading
+        tc.generate()
 
     def _patch_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        files.apply_conandata_patches(self)
+
         # Don't force PIC
-        if tools.Version(self.version) >= "1.4.5":
-            tools.replace_in_file(os.path.join(self._source_subfolder, "build", "cmake", "lib", "CMakeLists.txt"),
+        if Version(self.version) >= "1.4.5":
+            files.replace_in_file(self, os.path.join(self._source_subfolder, "build", "cmake", "lib", "CMakeLists.txt"),
                                   "POSITION_INDEPENDENT_CODE On", "")
 
     def build(self):
         self._patch_sources()
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        build_script_folder = os.path.join(self._source_subfolder, "build", "cmake")
+        cmake.configure(build_script_folder=build_script_folder)
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        
+        license_folder = os.path.join(self.package_folder, "licenses")
+        files.copy(self, pattern="LICENSE", src=self._source_subfolder, dst=license_folder)
+        files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        files.rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         zstd_cmake = "libzstd_shared" if self.options.shared else "libzstd_static"
@@ -96,6 +90,6 @@ class ZstdConan(ConanFile):
         self.cpp_info.components["zstdlib"].names["cmake_find_package"] = zstd_cmake
         self.cpp_info.components["zstdlib"].names["cmake_find_package_multi"] = zstd_cmake
         self.cpp_info.components["zstdlib"].set_property("cmake_target_name", "zstd::{}".format(zstd_cmake))
-        self.cpp_info.components["zstdlib"].libs = tools.collect_libs(self)
+        self.cpp_info.components["zstdlib"].libs = files.collect_libs(self)
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["zstdlib"].system_libs.append("pthread")
