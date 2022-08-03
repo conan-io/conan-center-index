c793717fe3833c945b1d3c7bebdb987573bd5f0e
Tu Duong Quyet <tuduongquyet@gmail.com>
2022-08-03 22:16:13 +0700

    add le

--- recipes/marisa/all/conanfile.py
+++ recipes/marisa/all/conanfile.py
@@ -49,7 +49,7 @@ class MarisaConan(ConanFile):
 
     def source(self):
         tools.files.get(**self.conan_data["sources"][self.version],
-                        destination=self._source_subfolder, strip_root=True)
+                        conanfile=self, destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
         if self._cmake:
