bd1bab77398147604a5086b85e42bd13061ecd13
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-05 00:07:30 +0200

    conan v2 support

---
+++ recipes/assimp/5.x/test_v1_package/conanfile.py
@@ -0,0 +1,23 @@
+# pylint: skip-file
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self):
+            obj_path = os.path.join(self.source_folder, os.pardir, "test_package", "box.obj")
+
+            bin_path = os.path.join("bin", "test_package")
+            self.run("{0} {1}".format(bin_path, obj_path), run_environment=True)
+
+            bin_c_path = os.path.join("bin", "test_package_c")
+            self.run("{0} {1}".format(bin_c_path, obj_path), run_environment=True)
