bf79ee8acde623217a439c062cc5cd53b1afc273
Pat Mancuso <pmancuso@virtru.com>
2022-08-04 09:49:53 -0400

    Fixed packaging, copy(self, xxx) behaves differently than self.copy(xxx)

--- recipes/opentdf-client/all/conanfile.py
+++ recipes/opentdf-client/all/conanfile.py
@@ -112,9 +112,9 @@ class OpenTDFConan(ConanFile):
     def package(self):
         cmake = self._configure_cmake()
         cmake.install()
-        copy(self, "*", dst="lib", src=os.path.join(self._source_subfolder,"tdf-lib-cpp", "lib"), keep_path=False)
-        copy(self, "*", dst="include", src=os.path.join(self._source_subfolder,"tdf-lib-cpp", "include"), keep_path=False)
-        copy(self, "LICENSE", dst="licenses", src=os.path.join(self._source_subfolder,"tdf-lib-cpp"), ignore_case=True, keep_path=False)
+        self.copy("*", dst="lib", src=os.path.join(os.path.join(self._source_subfolder,"tdf-lib-cpp"), "lib"), keep_path=False)
+        self.copy("*", dst="include", src=os.path.join(os.path.join(self._source_subfolder,"tdf-lib-cpp"), "include"), keep_path=False)
+        self.copy("LICENSE", dst="licenses", src=os.path.join(self._source_subfolder,"tdf-lib-cpp"), ignore_case=True, keep_path=False)
 
     # TODO - this only advertises the static lib, add dynamic lib also
     def package_info(self):
