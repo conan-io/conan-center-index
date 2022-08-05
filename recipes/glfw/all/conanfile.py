607b3af5a8f16a3cfadf1680c6851ff3c8c058c4
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-05 19:17:26 +0200

    protect deletion of settings for conan v2 mode

--- recipes/glfw/all/conanfile.py
+++ recipes/glfw/all/conanfile.py
@@ -41,8 +41,14 @@ class GlfwConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+        try:
+           del self.settings.compiler.libcxx
+        except Exception:
+           pass
+        try:
+           del self.settings.compiler.cppstd
+        except Exception:
+           pass
 
     def requirements(self):
         self.requires("opengl/system")
