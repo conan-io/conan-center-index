586a9abe5b3659ae0f06d1147ee0f4cb9ae11f8a
toge <toge.mail@gmail.com>
2022-08-10 00:21:08 +0900

    blend2d: migration to Conan v2 and fix compilation error in gcc12 with c++20

--- recipes/blend2d/all/conanfile.py
+++ recipes/blend2d/all/conanfile.py
@@ -1,15 +1,23 @@
-from conans import ConanFile, CMake, tools
 import os
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan import tools
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools.scm import Version
+from conan.tools.files import apply_conandata_patches
+from conan.tools.microsoft import is_msvc
+from conan.errors import ConanInvalidConfiguration
+
+required_conan_version = ">=1.50.0"
 
 class Blend2dConan(ConanFile):
     name = "blend2d"
     description = "2D Vector Graphics Engine Powered by a JIT Compiler"
-    topics = ("2d-graphics", "rasterization", "asmjit", "jit")
     license = "Zlib"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://blend2d.com/"
+    topics = ("2d-graphics", "rasterization", "asmjit", "jit")
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -19,22 +27,11 @@ class Blend2dConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    generators = "cmake", "cmake_find_package_multi"
-
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _is_msvc(self):
-        return str(self.settings.compiler) in ["Visual Studio", "msvc"]
 
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        tools.files.copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.files.copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -44,47 +41,50 @@ class Blend2dConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
+    def layout(self):
+        cmake_layout(self, src_folder='src')
+
     def requirements(self):
         self.requires("asmjit/cci.20220210")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 11)
+        if self.info.settings.compiler.cppstd:
+            check_min_cppstd(self, 11)
 
         # In Visual Studio < 16, there are compilation error. patch is already provided.
         # https://github.com/blend2d/blend2d/commit/63db360c7eb2c1c3ca9cd92a867dbb23dc95ca7d
-        if self._is_msvc and tools.Version(self.settings.compiler.version) < "16":
-            raise tools.ConanInvalidConfiguration("This recipe does not support this compiler version")
+        if is_msvc(self) and tools.Version(self.settings.compiler.version) < "16":
+            raise ConanInvalidConfiguration("This recipe does not support this compiler version")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-            destination=self._source_subfolder, strip_root=True)
+        tools.files.get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["BLEND2D_TEST"] = False
-        self._cmake.definitions["BLEND2D_EMBED"] = False
-        self._cmake.definitions["BLEND2D_STATIC"] = not self.options.shared
-        self._cmake.definitions["BLEND2D_NO_STDCXX"] = False
+    def generate(self):
+        toolchain = CMakeToolchain(self)
+        toolchain.variables["BUILD_TESTING"] = False
+        toolchain.variables["BLEND2D_TEST"] = False
+        toolchain.variables["BLEND2D_EMBED"] = False
+        toolchain.variables["BLEND2D_STATIC"] = not self.options.shared
+        toolchain.variables["BLEND2D_NO_STDCXX"] = False
         if not self.options.shared:
-            self._cmake.definitions["CMAKE_C_FLAGS"] = "-DBL_STATIC"
-            self._cmake.definitions["CMAKE_CXX_FLAGS"] = "-DBL_STATIC"
-        self._cmake.configure()
-        return self._cmake
+            toolchain.variables["CMAKE_C_FLAGS"] = "-DBL_STATIC"
+            toolchain.variables["CMAKE_CXX_FLAGS"] = "-DBL_STATIC"
+        toolchain.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE.md", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        tools.files.copy(self, "LICENSE.md", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.libs = ["blend2d"]
