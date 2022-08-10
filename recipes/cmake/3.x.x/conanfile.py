43c0caa56d44d550ec464107db80da20b873f530
Thomas Weißschuh <thomas@t-8ch.de>
2022-08-10 12:14:05 +0200

    cmake: clean up whitespace

--- recipes/cmake/3.x.x/conanfile.py
+++ recipes/cmake/3.x.x/conanfile.py
@@ -118,5 +118,5 @@ class CMakeConan(ConanFile):
         self.env_info.CMAKE_MODULE_PATH = mod_path
         if not os.path.exists(mod_path):
             raise ConanException("Module path not found: %s" % mod_path)
-            
+
         self.cpp_info.includedirs = []
