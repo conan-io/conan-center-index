b39a80c94b8de60aae64f3c8dffe4bf11be20ec5
Pavel Geler <53797155+pgeler@users.noreply.github.com>
2022-08-11 05:42:59 -0400

    (#12079) add protobuf package version 3.21.4
    * add protobuf package version 3.21.4
    
    * fix
    
    * fix
    
    * Use scm.Version
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Adopt new tools
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * remove old versions
    
    * fix
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/protobuf/all/conanfile.py
+++ recipes/protobuf/all/conanfile.py
@@ -1,7 +1,12 @@
-from conan.tools.files import rename
+from conan.tools.files import rename, get, apply_conandata_patches, replace_in_file, rmdir, rm
 from conan.tools.microsoft import msvc_runtime_flag
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan.tools.scm import Version
+from conan.tools.build import cross_building
+from conan.tools.apple.apple import is_apple_os
+from conan.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conans import CMake
+
 import functools
 import os
 import textwrap
@@ -60,7 +65,7 @@ class ProtobufConan(ConanFile):
 
     @property
     def _can_disable_rtti(self):
-        return tools.Version(self.version) >= "3.15.4"
+        return Version(self.version) >= "3.15.4"
 
     def export_sources(self):
         self.copy("CMakeLists.txt")
@@ -86,21 +91,21 @@ class ProtobufConan(ConanFile):
             raise ConanInvalidConfiguration("Protobuf can't be built with shared + MT(d) runtimes")
 
         if self.settings.compiler == "Visual Studio":
-            if tools.Version(self.settings.compiler.version) < "14":
+            if Version(self.settings.compiler.version) < "14":
                 raise ConanInvalidConfiguration("On Windows Protobuf can only be built with "
                                                 "Visual Studio 2015 or higher.")
 
         if self.settings.compiler == "clang":
-            if tools.Version(self.version) >= "3.15.4" and tools.Version(self.settings.compiler.version) < "4":
+            if Version(self.version) >= "3.15.4" and Version(self.settings.compiler.version) < "4":
                 raise ConanInvalidConfiguration("protobuf {} doesn't support clang < 4".format(self.version))
 
-        if hasattr(self, "settings_build") and tools.cross_building(self) and \
+        if hasattr(self, "settings_build") and cross_building(self) and \
            self.settings.os == "Macos" and self.options.shared:
             # FIXME: should be allowed, actually build succeeds but it fails at build time of test package due to SIP
             raise ConanInvalidConfiguration("protobuf shared not supported yet in CCI while cross-building on Macos")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        get(self, **self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     @property
@@ -116,7 +121,7 @@ class ProtobufConan(ConanFile):
         cmake.definitions["protobuf_BUILD_PROTOC_BINARIES"] = True
         if not self.options.debug_suffix:
             cmake.definitions["protobuf_DEBUG_POSTFIX"] = ""
-        if tools.Version(self.version) >= "3.14.0":
+        if Version(self.version) >= "3.14.0":
             cmake.definitions["protobuf_BUILD_LIBPROTOC"] = True
         if self._can_disable_rtti:
             cmake.definitions["protobuf_DISABLE_RTTI"] = not self.options.with_rtti
@@ -125,21 +130,20 @@ class ProtobufConan(ConanFile):
             if not runtime:
                 runtime = self.settings.get_safe("compiler.runtime")
             cmake.definitions["protobuf_MSVC_STATIC_RUNTIME"] = "MT" in runtime
-        if tools.Version(self.version) < "3.18.0" and self._is_clang_cl:
+        if Version(self.version) < "3.18.0" and self._is_clang_cl:
             cmake.definitions["CMAKE_RC_COMPILER"] = os.environ.get("RC", "llvm-rc")
         cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def _patch_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        apply_conandata_patches(self)
 
         # Provide relocatable protobuf::protoc target and Protobuf_PROTOC_EXECUTABLE cache variable
         # TODO: some of the following logic might be disabled when conan will
         #       allow to create executable imported targets in package_info()
         protobuf_config_cmake = os.path.join(self._source_subfolder, "cmake", "protobuf-config.cmake.in")
 
-        tools.replace_in_file(
+        replace_in_file(self,
             protobuf_config_cmake,
             "@_protobuf_FIND_ZLIB@",
             "# BEGIN CONAN PATCH\n#_protobuf_FIND_ZLIB@\n# END CONAN PATCH"
@@ -163,7 +167,7 @@ class ProtobufConan(ConanFile):
                 set_property(TARGET protobuf::protoc PROPERTY IMPORTED_LOCATION ${{Protobuf_PROTOC_EXECUTABLE}})
             endif()
         """.format(protoc_rel_path=protoc_rel_path))
-        tools.replace_in_file(
+        replace_in_file(self,
             protobuf_config_cmake,
             "include(\"${CMAKE_CURRENT_LIST_DIR}/protobuf-targets.cmake\")",
             protoc_target
@@ -172,16 +176,16 @@ class ProtobufConan(ConanFile):
         # Set DYLD_LIBRARY_PATH in command line to avoid issues with shared protobuf
         # (even with virtualrunenv, this fix might be required due to SIP)
         # Only works with cmake, cmake_find_package or cmake_find_package_multi generators
-        if tools.is_apple_os(self.settings.os):
-            tools.replace_in_file(
+        if is_apple_os(self.settings.os):
+            replace_in_file(self,
                 protobuf_config_cmake,
                 "add_custom_command(",
                 ("set(CUSTOM_DYLD_LIBRARY_PATH ${CONAN_LIB_DIRS} ${Protobuf_LIB_DIRS} ${Protobuf_LIB_DIRS_RELEASE} ${Protobuf_LIB_DIRS_DEBUG} ${Protobuf_LIB_DIRS_RELWITHDEBINFO} ${Protobuf_LIB_DIRS_MINSIZEREL})\n"
                  "string(REPLACE \";\" \":\" CUSTOM_DYLD_LIBRARY_PATH \"${CUSTOM_DYLD_LIBRARY_PATH}\")\n"
                  "add_custom_command(")
             )
-            cmd_str = "COMMAND  protobuf::protoc" if tools.Version(self.version) < "3.20.0" else "COMMAND protobuf::protoc"
-            tools.replace_in_file(
+            cmd_str = "COMMAND  protobuf::protoc" if Version(self.version) < "3.20.0" else "COMMAND protobuf::protoc"
+            replace_in_file(self,
                 protobuf_config_cmake,
                 cmd_str,
                 "COMMAND ${CMAKE_COMMAND} -E env \"DYLD_LIBRARY_PATH=${CUSTOM_DYLD_LIBRARY_PATH}\" $<TARGET_FILE:protobuf::protoc>"
@@ -190,12 +194,12 @@ class ProtobufConan(ConanFile):
         # Disable a potential warning in protobuf-module.cmake.in
         # TODO: remove this patch? Is it really useful?
         protobuf_module_cmake = os.path.join(self._source_subfolder, "cmake", "protobuf-module.cmake.in")
-        tools.replace_in_file(
+        replace_in_file(self,
             protobuf_module_cmake,
             "if(DEFINED Protobuf_SRC_ROOT_FOLDER)",
             "if(0)\nif(DEFINED Protobuf_SRC_ROOT_FOLDER)",
         )
-        tools.replace_in_file(
+        replace_in_file(self,
             protobuf_module_cmake,
             "# Define upper case versions of output variables",
             "endif()",
@@ -203,8 +207,8 @@ class ProtobufConan(ConanFile):
 
         # https://github.com/protocolbuffers/protobuf/issues/9916
         # it will be solved in protobuf 3.21.0
-        if tools.Version(self.version) == "3.20.0":
-            tools.replace_in_file(os.path.join(self._source_subfolder, "src", "google", "protobuf", "port_def.inc"),
+        if Version(self.version) == "3.20.0":
+            replace_in_file(self, os.path.join(self._source_subfolder, "src", "google", "protobuf", "port_def.inc"),
                 "#elif PROTOBUF_GNUC_MIN(12, 0)",
                 "#elif PROTOBUF_GNUC_MIN(12, 2)")
 
@@ -217,7 +221,7 @@ class ProtobufConan(ConanFile):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         os.unlink(os.path.join(self.package_folder, self._cmake_install_base_path, "protobuf-config-version.cmake"))
         os.unlink(os.path.join(self.package_folder, self._cmake_install_base_path, "protobuf-targets.cmake"))
         os.unlink(os.path.join(self.package_folder, self._cmake_install_base_path, "protobuf-targets-{}.cmake".format(str(self.settings.build_type).lower())))
@@ -225,8 +229,8 @@ class ProtobufConan(ConanFile):
                      os.path.join(self.package_folder, self._cmake_install_base_path, "protobuf-generate.cmake"))
 
         if not self.options.lite:
-            tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "libprotobuf-lite.*")
-            tools.remove_files_by_mask(os.path.join(self.package_folder, "bin"), "libprotobuf-lite.*")
+            rm(self, "libprotobuf-lite.*", os.path.join(self.package_folder, "lib"))
+            rm(self, "libprotobuf-lite.*", os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_find_mode", "both")
@@ -290,6 +294,8 @@ class ProtobufConan(ConanFile):
         self.cpp_info.filenames["cmake_find_package"] = "Protobuf"
         self.cpp_info.filenames["cmake_find_package_multi"] = "protobuf"
         self.cpp_info.names["pkg_config"] ="protobuf_full_package"
-        self.cpp_info.components["libprotobuf"].build_modules = build_modules
+        for generator in ["cmake_find_package", "cmake_find_package_multi"]:
+            self.cpp_info.components["libprotobuf"].build_modules[generator] = build_modules
         if self.options.lite:
-            self.cpp_info.components["libprotobuf-lite"].build_modules = build_modules
+            for generator in ["cmake_find_package", "cmake_find_package_multi"]:
+                self.cpp_info.components["libprotobuf-lite"].build_modules[generator] = build_modules
