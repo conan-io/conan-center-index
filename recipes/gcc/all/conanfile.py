46f41c76aa13edc420a2934cbd0a090310fa3999
robin <robinintheuk@mac.com>
2022-08-12 19:27:55 +1000

    add gcc11 and gcc12 recipes add isl requirement add system libs https://github.com/conan-io/conan-center-index/commit/3b9a23d8b4e189592b6af1da4f25f8e96fa58a0d update to conan v2 format

--- recipes/gcc/all/conanfile.py
+++ recipes/gcc/all/conanfile.py
@@ -1,126 +1,112 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
-from conans.errors import ConanException, ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.29.1"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import cross_building
+from conan.tools.files import get, replace_in_file, rmdir, rm, copy
+from conan.tools.gnu import AutotoolsToolchain, Autotools
+from conan.tools.layout import basic_layout
+
+required_conan_version = '>=1.49.0'
 
 
 class GccConan(ConanFile):
-    name = "gcc"
-    description = "The GNU Compiler Collection includes front ends for C, " \
-                  "C++, Objective-C, Fortran, Ada, Go, and D, as well as " \
-                  "libraries for these languages (libstdc++,...). "
-    topics = ("gcc", "gnu", "compiler", "c", "c++")
-    homepage = "https://gcc.gnu.org"
-    url = "https://github.com/conan-io/conan-center-index"
-    license = "GPL-3.0-only"
-    settings = "os", "compiler", "arch", "build_type"
-    _autotools = None
+    name = 'gcc'
+    description = 'The GNU Compiler Collection includes front ends for C, ' \
+                  'C++, Objective-C, Fortran, Ada, Go, and D, as well as ' \
+                  'libraries for these languages (libstdc++,...). '
+    topics = 'gcc', 'gnu', 'compiler', 'c', 'c++'
+    homepage = 'https://gcc.gnu.org'
+    url = 'https://github.com/conan-io/conan-center-index'
+    license = 'GPL-3.0-only'
+    settings = 'os', 'compiler', 'arch', 'build_type'
+    requires = 'mpc/1.2.0', 'mpfr/4.1.0', 'gmp/6.2.1', 'zlib/1.2.12', 'isl/0.24'
+    tool_requires = 'flex/2.6.4'
+    short_paths = True
 
-    def build_requirements(self):
-        self.build_requires("flex/2.6.4")
+    _auto_tools = None
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self)
-        pkgversion = 'conan GCC %s' % self.version
-        bugurl = self.url + '/issues'
-        libdir = "%s/lib/gcc/%s" % (self.package_folder, self.version)
-        args = [
-            "--enable-languages=c,c++",
-            "--disable-nls",
-            "--disable-multilib",
-            "--disable-bootstrap",
-            "--with-system-zlib",
-            "--with-gmp=%s" % self.deps_cpp_info['gmp'].rootpath,
-            '--with-mpc=%s' % self.deps_cpp_info["mpc"].rootpath,
-            "--with-mpfr=%s" % self.deps_cpp_info["mpfr"].rootpath,
-            "--without-isl",
-            "--libdir=%s" % libdir,
-            '--with-pkgversion=%s' % pkgversion,
-            "--program-suffix=-%s" % self.version,
-            "--with-bugurl=%s" % bugurl
-        ]
-        if self.settings.os == "Macos":
-            xcrun = tools.XCRun(self.settings)
-            args.extend([
-                '--with-native-system-header-dir=/usr/include',
-                "--with-sysroot={}".format(xcrun.sdk_path)
-            ])
-        self._autotools.libs = []  # otherwise causes config.log to fail finding -lmpc
-        if self.settings.compiler in ["clang", "apple-clang"]:
-            # xgcc: error: unrecognized command-line option -stdlib=libc++
-            if self.settings.compiler.libcxx == "libc++":
-                self._autotools.cxx_flags.remove("-stdlib=libc++")
-            elif self.settings.compiler.libcxx in ["libstdc++", "libstdc++11"]:
-                self._autotools.cxx_flags.remove("-stdlib=libstdc++")
-        self._autotools.configure(args=args, configure_dir=self._source_subfolder)
-        return self._autotools
+    def layout(self):
+        basic_layout(self, src_folder=f'gcc-{self.version}')
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def generate(self):
+        tc = AutotoolsToolchain(self)
+        tc.configure_args.extend([
+            '--enable-languages=c,c++',
+            '--disable-nls',
+            '--disable-multilib',
+            '--disable-bootstrap',
+            f'--prefix={self.package_folder}',
+            '--libexecdir=$prefix/bin/libexec'
+            f'--with-zlib={self.deps_cpp_info["zlib"].rootpath}',
+            f'--with-gmp={self.deps_cpp_info["gmp"].rootpath}',
+            f'--with-mpc={self.deps_cpp_info["mpc"].rootpath}',
+            f'--with-mpfr={self.deps_cpp_info["mpfr"].rootpath}',
+            f'--with-isl={self.deps_cpp_info["isl"].rootpath}',
+            f'--with-pkgversion=\'conan GCC {self.version}\'',
+            f'--program-suffix=-{self.version}',
+            f'--with-bugurl={self.url}/issues'])
+        if self.settings.os == 'Macos':
+            tc.make_args.extend(['BOOT_LDFLAGS=-Wl,-headerpad_max_install_names'])
+            tc.configure_args.append('--with-native-system-header-dir=/usr/include')
+        tc.generate()
 
-    def requirements(self):
-        self.requires("mpc/1.2.0")
-        self.requires("mpfr/4.1.0")
-        self.requires("gmp/6.2.0")
-        self.requires("zlib/1.2.11")
-
-    def configure(self):
-        if self.settings.os == "Windows":
-            raise ConanInvalidConfiguration("Windows builds aren't supported (yet), sorry")
-        if tools.cross_building(self.settings):
-            raise ConanInvalidConfiguration("no cross-building support (yet), sorry")
+    def validate(self):
+        if self.settings.os == 'Windows':
+            raise ConanInvalidConfiguration('Windows is not yet supported. Contributions are welcome')
+        if self.settings.os == "Macos" and self.settings.arch == "armv8":
+            raise ConanInvalidConfiguration("Apple silicon is not yet supported. Contributions are welcome")
+        if cross_building(self):
+            raise ConanInvalidConfiguration('no cross-building support (yet), sorry')
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "gcc-%s" % self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        get(self, **self.conan_data['sources'][self.version], strip_root=True)
 
     @property
-    def _make_args(self):
-        if self.settings.os == "Macos":
-            return ["BOOT_LDFLAGS=-Wl,-headerpad_max_install_names"]
-        return []
+    def auto_tools(self):
+        if self._auto_tools is not None:
+            return self._auto_tools
+        self._auto_tools = Autotools(self)
+        return self._auto_tools
 
     def build(self):
         # If building on x86_64, change the default directory name for 64-bit libraries to "lib":
-        libdir = "%s/lib/gcc/%s" % (self.package_folder, self.version)
-        tools.replace_in_file(os.path.join(self.source_folder,
-                                           self._source_subfolder, "gcc", "config", "i386", "t-linux64"),
-                              "m64=../lib64", "m64=../lib", strict=False)
+        replace_in_file(self, os.path.join(self.source_folder, "gcc", "config", "i386", "t-linux64"),
+                        "m64=../lib64", "m64=../lib", strict=False)
         # Ensure correct install names when linking against libgcc_s;
         # see discussion in https://github.com/Homebrew/legacy-homebrew/pull/34303
-        tools.replace_in_file(os.path.join(self.source_folder,
-                                           self._source_subfolder, "libgcc", "config", "t-slibgcc-darwin"),
-                              "@shlib_slibdir@", libdir, strict=False)
-        autotools = self._configure_autotools()
-        autotools.make(args=self._make_args)
+        replace_in_file(self,
+                        os.path.join(self.source_folder, 'libgcc', 'config', 't-slibgcc-darwin'),
+                        '@shlib_slibdir@',
+                        os.path.join(self.package_folder, 'lib'),
+                        strict=False)
+        self.auto_tools.configure()
+        self.auto_tools.make()
 
     def package_id(self):
         del self.info.settings.compiler
 
     def package(self):
-        autotools = self._configure_autotools()
-        if self.settings.build_type == "Debug":
-            autotools.install(args=self._make_args)
+        if self.settings.build_type == 'Debug':
+            self.auto_tools.install()
         else:
-            autotools.make(args=["install-strip"] + self._make_args)
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        tools.remove_files_by_mask(self.package_folder, "*.la")
-        self.copy(pattern="COPYING*", dst="licenses", src=self._source_subfolder)
+            self.auto_tools.make('install-strip')
+        rmdir(self, os.path.join(self.package_folder, 'share'))
+        rm(self, '*.la', self.package_folder, recursive=True)
+        copy(self, pattern='COPYING*', dst=os.path.join(self.package_folder, "licenses"), src=self.folders.source)
 
     def package_info(self):
-        bindir = os.path.join(self.package_folder, "bin")
-        self.output.info("Appending PATH env var with : " + bindir)
-        self.env_info.PATH.append(bindir)
+        bindir = os.path.join(self.package_folder, 'bin')
+        self.output.info('Appending PATH env var with : ' + bindir)
+        self.env_info.PATH = [bindir] + self.env_info.PATH
 
-        cc = os.path.join(bindir, "gcc-%s" % self.version)
-        self.output.info("Creating CC env var with : " + cc)
+        cc = os.path.join(bindir, f'gcc-{self.version}')
+        self.output.info('Creating CC env var with : ' + cc)
         self.env_info.CC = cc
 
-        cxx = os.path.join(bindir, "g++-%s" % self.version)
-        self.output.info("Creating CXX env var with : " + cxx)
+        cxx = os.path.join(bindir, f'g++-{self.version}')
+        self.output.info('Creating CXX env var with : ' + cxx)
         self.env_info.CXX = cxx
+
+        if self.settings.os in ['Linux', 'FreeBSD']:
+            self.cpp_info.system_libs = ['pthread', 'rt', 'dl', 'm']
--- recipes/gcc/all/test_package/conanfile.py
+++ recipes/gcc/all/test_package/conanfile.py
@@ -1,5 +1,8 @@
-from conans import ConanFile, tools
 import os
+
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conans import tools
 
 
 class TestPackageConan(ConanFile):
@@ -21,7 +24,7 @@ class TestPackageConan(ConanFile):
         self.run("%s -dumpversion" % cxx, run_environment=True)
         self.run("%s %s -o hello_c" % (cc, hello_c), run_environment=True)
         self.run("%s %s -o hello_cpp" % (cxx, hello_cpp), run_environment=True)
-        if not tools.cross_building(self.settings):
+        if not cross_building(self):
             chmod_plus_x("hello_c")
             chmod_plus_x("hello_cpp")
             self.run("./hello_c", run_environment=True)
