e83bcac162d70adec1f5e66150c53b92d8cee5d2
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-12 11:00:45 +0200

    honor BUILD_SHARED_LIBS

--- recipes/lz4/all/conanfile.py
+++ recipes/lz4/all/conanfile.py
@@ -61,6 +61,8 @@ class LZ4Conan(ConanFile):
         tc.variables["LZ4_POSITION_INDEPENDENT_LIB"] = self.options.get_safe("fPIC", True)
         # Generate a relocatable shared lib on Macos
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
+        # Honor BUILD_SHARED_LIBS (see https://github.com/conan-io/conan/issues/11840)
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
         tc.generate()
 
     @property
