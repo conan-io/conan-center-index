63fd954a0db99f6b9584e9aa21787901a5a41f7e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-12 18:34:05 +0200

    Apply patch

--- recipes/beauty/all/conanfile.py
+++ recipes/beauty/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rmdir
+from conan.tools.files import apply_conandata_patches, copy, get, rmdir
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 import os
@@ -38,6 +38,10 @@ class BeautyConan(ConanFile):
             "Visual Studio": "16",
             "msvc": "192",
         }
+
+    def export_sources(self):
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -81,6 +85,7 @@ class BeautyConan(ConanFile):
         tc.generate()
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build(target="beauty")
