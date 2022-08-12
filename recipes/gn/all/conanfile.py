702f162c85f61af6f5ed5f61ae07c2b33d0622fa
petamas <petamas@gmail.com>
2022-08-13 01:31:57 +0200

    Fix #KB-H071 hook error

--- recipes/gn/all/conanfile.py
+++ recipes/gn/all/conanfile.py
@@ -124,3 +124,4 @@ class GnConan(ConanFile):
         bin_path = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH environment variable: {}".format(bin_path))
         self.env_info.PATH.append(bin_path)
+        self.cpp_info.includedirs = []
