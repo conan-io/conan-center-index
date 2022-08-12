5d7b3aee621d10e413d119e029819676d2c3136c
jgsogo <jgsogo@gmail.com>
2022-08-12 13:36:02 +0200

    confu_json - v2 linter + yml linter

--- recipes/confu_json/all/conanfile.py
+++ recipes/confu_json/all/conanfile.py
@@ -1,8 +1,6 @@
-from conans import ConanFile, tools
-from conans.tools import check_min_cppstd
-from conans.errors import ConanInvalidConfiguration, ConanException
-import os
-from conans.tools import Version
+from conan import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.33.0"
 
@@ -38,7 +36,7 @@ class ConfuJson(ConanFile):
             raise ConanInvalidConfiguration(
                 "apple-clang is not supported because of missing concept support")
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+            tools.build.check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(
             str(self.settings.compiler))
         if not min_version:
@@ -46,7 +44,7 @@ class ConfuJson(ConanFile):
                              "compiler support.".format(
                                  self.name, self.settings.compiler))
         else:
-            if tools.Version(self.settings.compiler.version) < min_version:
+            if Version(self.settings.compiler.version) < min_version:
                 raise ConanInvalidConfiguration(
                     "{} requires C++{} support. "
                     "The current compiler {} {} does not support it.".format(
@@ -59,7 +57,7 @@ class ConfuJson(ConanFile):
         self.requires("magic_enum/0.7.2")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        tools.files.get(self, **self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     def package(self):
