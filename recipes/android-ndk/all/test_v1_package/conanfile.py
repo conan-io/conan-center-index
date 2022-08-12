16cea14348ec4057b2aa52b8d06c79ede846d09f
Uilian Ries <uilianries@gmail.com>
2022-08-12 11:03:01 +0200

    (#12076) [android-ndk] Export NDK path for CMakeToolchain
    * Export android NDK
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Adapt for Conan 2.0
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * absolute path
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * unzip is member
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * do not pass self
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/android-ndk/all/conanfile.py
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Update test package
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * add test v1
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/android-ndk/all/test_v1_package/conanfile.py
+++ recipes/android-ndk/all/test_v1_package/conanfile.py
@@ -1,21 +1,19 @@
 import os
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conans import CMake
 from conan.tools.build import cross_building
 
 
 class TestPackgeConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+    generators = "cmake"
 
     def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
-
-    def layout(self):
-        cmake_layout(self)
+        self.build_requires(self.tested_reference_str)
 
     def build(self):
-        # INFO: It only makes sense to build a library, if the target OS is Android
+        # It only makes sense to build a library, if the target os is Android
         if self.settings.os == "Android":
             cmake = CMake(self)
             cmake.configure()
@@ -24,11 +22,11 @@ class TestPackgeConan(ConanFile):
     def test(self):
         if not cross_building(self):
             if self.settings.os == "Windows":
-                self.run("ndk-build.cmd --version", env="conanrun")
+                self.run("ndk-build.cmd --version", run_environment=True)
             else:
-                self.run("ndk-build --version", env="conanrun")
+                self.run("ndk-build --version", run_environment=True)
 
-        # INFO: Run the project that was built using Android NDK
+        # Run the project that was built using Android NDK
         if self.settings.os == "Android":
             test_file = os.path.join("bin", "test_package")
             assert os.path.exists(test_file)
