5ae14d5e28596d91605b0f26b83bb3a4edf2607d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-12 11:37:05 +0200

    conan v2 support

--- recipes/uriparser/all/conanfile.py
+++ recipes/uriparser/all/conanfile.py
@@ -1,13 +1,16 @@
-from conans import CMake, ConanFile, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, collect_libs, copy, get, rmdir
+from conan.tools.microsoft import is_msvc, msvc_runtime_flag
 import os
 
-required_conan_version = ">=1.36.0"
+required_conan_version = ">=1.50.0"
 
 
 class UriparserConan(ConanFile):
     name = "uriparser"
     description = "Strictly RFC 3986 compliant URI parsing and handling library written in C89"
-    topics = ("conan", "uriparser", "URI", "parser")
+    topics = ("uriparser", "URI", "parser")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://uriparser.github.io/"
 
@@ -26,21 +29,9 @@ class UriparserConan(ConanFile):
         "with_wchar": True,
     }
 
-    generators = "cmake"
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -49,50 +40,51 @@ class UriparserConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
-        del self.settings.compiler.cppstd
-        del self.settings.compiler.libcxx
+        try:
+           del self.settings.compiler.libcxx
+        except Exception:
+           pass
+        try:
+           del self.settings.compiler.cppstd
+        except Exception:
+           pass
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["URIPARSER_BUILD_DOCS"] = False
-        self._cmake.definitions["URIPARSER_BUILD_TESTS"] = False
-        self._cmake.definitions["URIPARSER_BUILD_TOOLS"] = False
-        self._cmake.definitions["URIPARSER_BUILD_CHAR"] = self.options.with_char
-        self._cmake.definitions["URIPARSER_BUILD_WCHAR"] = self.options.with_wchar
-        if self.settings.compiler in ["Visual Studio", "msvc"]:
-            if self.settings.compiler == "Visual Studio":
-                runtime = self.settings.compiler.runtime
-            else:
-                runtime = "{}{}".format(
-                    "MT" if self.settings.compiler.runtime == "static" else "MD",
-                    "d" if self.settings.compiler.runtime_type == "Debug" else "",
-                )
-            self._cmake.definitions["URIPARSER_MSVC_RUNTIME"] = "/{}".format(runtime)
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["URIPARSER_BUILD_DOCS"] = False
+        tc.variables["URIPARSER_BUILD_TESTS"] = False
+        tc.variables["URIPARSER_BUILD_TOOLS"] = False
+        tc.variables["URIPARSER_BUILD_CHAR"] = self.options.with_char
+        tc.variables["URIPARSER_BUILD_WCHAR"] = self.options.with_wchar
+        if is_msvc(self):
+            tc.variables["URIPARSER_MSVC_RUNTIME"] = f"/{msvc_runtime_flag(self)}"
+        # Honor BUILD_SHARED_LIBS (see https://github.com/conan-io/conan/issues/11840)
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("COPYING", src=self._source_subfolder, dst="licenses")
-        cmake = self._configure_cmake()
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "liburiparser")
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
         if not self.options.shared:
             self.cpp_info.defines.append("URI_STATIC_BUILD")
         if not self.options.with_char:
