59ac61fe292e1cb02819bf0b9d2395e407efe749
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-08-13 18:18:00 +0200

    Bugfix in test_package

--- recipes/libbigwig/all/test_package/conanfile.py
+++ recipes/libbigwig/all/test_package/conanfile.py
@@ -37,5 +37,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join("bin", "test_package")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
             self.run(bin_path, run_environment=True)
