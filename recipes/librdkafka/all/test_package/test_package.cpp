268653adab911fcae5355d828c740cf2ab97f1bd
Eric Pederson <ericacm+github@gmail.com>
2022-08-14 16:38:06 -0400

    Fixes #12213.  Adds code to test package to test rdkafka++ library.

--- recipes/librdkafka/all/test_package/test_package.cpp
+++ recipes/librdkafka/all/test_package/test_package.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <librdkafka/rdkafka.h>
+#include <librdkafka/rdkafkacpp.h>
 
 int main(int argc, char const *argv[]) {
   rd_kafka_conf_t *conf = rd_kafka_conf_new();
+  auto confpp = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
+
   std::cout << std::endl
             << "----------------->Tests are done.<---------------------" << std::endl
-	    << "Using version " << rd_kafka_version_str() << std::endl
+            << "Using version (from C lib) " << rd_kafka_version_str() << std::endl
+            << "Using version (from C++ lib) " << RdKafka::version() << std::endl
             << "///////////////////////////////////////////////////////" << std::endl;
   return 0;
 }
