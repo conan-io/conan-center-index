0aa41c3a96af3e2d4e4c1dc713297f3dcccd388f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-14 22:36:26 +0200

    conan v2 support

--- recipes/inih/all/test_v1_package/conanfile.py
+++ recipes/inih/all/test_v1_package/conanfile.py
@@ -1,19 +1,12 @@
-from conan import ConanFile
-from conan.tools.build import cross_building
-from conan.tools.cmake import CMake, cmake_layout
+# pylint: skip-file
+from conans import ConanFile, CMake, tools
 import os
 import textwrap
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def layout(self):
-        cmake_layout(self)
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -21,7 +14,7 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not cross_building(self):
+        if not tools.cross_building(self):
             with open("test.ini", "w") as fn:
                 fn.write(textwrap.dedent("""\
                     [protocol]
@@ -30,5 +23,5 @@ class TestPackageConan(ConanFile):
                     name = conan-center-index
                     email = info@conan.io
                     """))
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, env="conanrun")
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
