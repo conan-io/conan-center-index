e01ccdc9dd35b9e8fb74b6384ec9129318da3ce8
Sergey Bobrenok <bobrofon@gmail.com>
2022-08-14 18:22:51 +0300

    libcap/all: cross-compatibility conan v1/v2

--- recipes/libcap/all/test_package/conanfile.py
+++ recipes/libcap/all/test_package/conanfile.py
@@ -1,11 +1,33 @@
 import os
 
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake
+from conan.tools.env import Environment
+from conan.tools.cmake import cmake_layout
+
+required_conan_version = ">=1.38.0"
 
 
 class LibcapTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "pkg_config"
+    generators = "CMakeToolchain", "PkgConfigDeps", "VirtualBuildEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def build_requirements(self):
+        self.tool_requires("pkgconf/1.7.4")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        env = Environment()
+        env.prepend_path("PKG_CONFIG_PATH", self.generators_folder)
+        envvars = env.vars(self)
+        envvars.save_script("pkg_config")
 
     def build(self):
         cmake = CMake(self)
@@ -13,6 +35,6 @@ class LibcapTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "example")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "example")
+            self.run(bin_path, env="conanrun")
