f5ea15ba6e1d9e4717eefe97efd6bc7fc1a27df5
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-15 10:38:36 +0200

    improve fix_msvc_libname()

--- recipes/inih/all/conanfile.py
+++ recipes/inih/all/conanfile.py
@@ -94,7 +94,7 @@ class InihConan(ConanFile):
         self.cpp_info.components["inireader"].libs = ["INIReader"]
         self.cpp_info.components["inireader"].requires = ["libinih"]
 
-def fix_msvc_libname(conanfile):
+def fix_msvc_libname(conanfile, remove_lib_prefix=True):
     """remove lib prefix & change extension to .lib"""
     from conan.tools.files import rename
     import glob
@@ -105,7 +105,7 @@ def fix_msvc_libname(conanfile):
         for ext in [".dll.a", ".dll.lib", ".a"]:
             full_folder = os.path.join(conanfile.package_folder, libdir)
             for filepath in glob.glob(os.path.join(full_folder, f"*{ext}")):
-                libname = os.path.splitext(os.path.basename(filepath))[0]
-                if libname[0:3] == "lib":
+                libname = os.path.basename(filepath)[0:-len(ext)]
+                if remove_lib_prefix and libname[0:3] == "lib":
                     libname = libname[3:]
                 rename(conanfile, filepath, os.path.join(os.path.dirname(filepath), f"{libname}.lib"))
