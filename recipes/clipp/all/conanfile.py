ceac800ed7d6a3e7582c0601ed30cf6bd162d774
Chris Mc <christopherm@jfrog.com>
2022-08-16 14:59:07 -0700

    add missing conanfile arg

--- recipes/clipp/all/conanfile.py
+++ recipes/clipp/all/conanfile.py
@@ -18,7 +18,7 @@ class ClippConan(ConanFile):
         return "source_subfolder"
 
     def source(self):
-        tools.files.get(**self.conan_data["sources"][self.version],
+        tools.files.get(self, **self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     def package(self):
