48cec59d118efc7972040af6f3509cda344f53a4
Paulo Coutinho <paulocoutinhox@gmail.com>
2022-08-15 23:33:37 -0300

    Update recipes/sdl/all/conanfile.py
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- recipes/sdl/all/conanfile.py
+++ recipes/sdl/all/conanfile.py
@@ -197,7 +197,9 @@ class SDLConan(ConanFile):
     @functools.lru_cache(1)
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.verbose = True # REMOVER AFTER TEST
+        if tools.is_apple_os(self) and self.settings.arch == "armv8":
+            cmake.definitions["CMAKE_OBJC_FLAGS"] = "-arch arm64"
+        cmake.verbose = True # REMOVE AFTER TEST
         cmake_required_includes = []  # List of directories used by CheckIncludeFile (https://cmake.org/cmake/help/latest/module/CheckIncludeFile.html)
         cmake_extra_ldflags = []
         # FIXME: self.install_folder not defined? Neccessary?
