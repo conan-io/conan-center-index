81bcc1c05207b6c15afc24af0ed41793fa3032ea
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-16 11:14:39 +0200

    bump required_conan_version to 1.50.0
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/erikzenker-hsm/all/conanfile.py
+++ recipes/erikzenker-hsm/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.files import copy, get, rmdir
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.47.0"
+required_conan_version = ">=1.50.0"
 
 
 class HsmConan(ConanFile):
