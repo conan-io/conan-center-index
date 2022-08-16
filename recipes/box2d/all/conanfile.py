f5cd6eda4b67518f3fdcc554b2790f8eb37a04ce
danimtb <danimanzaneque@gmail.com>
2022-08-16 16:41:49 +0200

    use layout and improve package()

--- recipes/box2d/all/conanfile.py
+++ recipes/box2d/all/conanfile.py
@@ -1,6 +1,6 @@
 import os
 from conan import ConanFile, tools
-from conan.tools.cmake import CMakeToolchain, CMake
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 required_conan_version = ">=1.46.0"
 
@@ -28,7 +28,6 @@ class Box2dConan(ConanFile):
     def source(self):
         tools.files.get(self,
                         **self.conan_data["sources"][self.version],
-                        destination=self.source_folder,
                         strip_root=True)
 
     def generate(self):
@@ -47,12 +46,17 @@ class Box2dConan(ConanFile):
 
     def package(self):
         tools.files.copy(self, "License.txt", src=os.path.join(self.source_folder, "Box2D"), dst=os.path.join(self.package_folder,"licenses"))
-        tools.files.copy(self, "*.h", src=os.path.join(self.source_folder, "Box2D", "Box2D"), dst=os.path.join(self.package_folder, "include", "Box2D"))
-        tools.files.copy(self, "*.lib", src=os.path.join(self.source_folder, "Box2D"), dst=os.path.join(self.package_folder, "lib"), keep_path=False)
-        tools.files.copy(self, "*.dll", src=os.path.join(self.source_folder, "Box2D"), dst=os.path.join(self.package_folder, "bin"), keep_path=False)
-        tools.files.copy(self, "*.so*", src=os.path.join(self.source_folder, "Box2D"), dst=os.path.join(self.package_folder, "lib"), keep_path=False)
-        tools.files.copy(self, "*.dylib", src=os.path.join(self.source_folder, "Box2D"), dst=os.path.join(self.package_folder, "lib"), keep_path=False)
-        tools.files.copy(self, "*.a", src=os.path.join(self.source_folder, "Box2D"), dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        tools.files.copy(self, os.path.join("Box2D", "*.h"), src=os.path.join(self.source_folder, "Box2D"), dst=os.path.join(self.package_folder, "include"))
+        tools.files.copy(self, "*.lib", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        tools.files.copy(self, "*.dll", src=self.build_folder, dst=os.path.join(self.package_folder, "bin"), keep_path=False)
+        tools.files.copy(self, "*.so*", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        tools.files.copy(self, "*.dylib", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        tools.files.copy(self, "*.a", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
 
     def package_info(self):
+        print(self.cpp_info.includedirs)
         self.cpp_info.libs = ["Box2D"]
+
+
+    def layout(self):
+        cmake_layout(self, src_folder="Box2D")
