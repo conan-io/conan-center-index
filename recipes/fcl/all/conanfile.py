9420370e9c6314917e3b5eda82b453f13c9bf044
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-16 14:19:39 +0200

    conan v2 support

--- recipes/fcl/all/conanfile.py
+++ recipes/fcl/all/conanfile.py
@@ -1,9 +1,12 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, get, rmdir, save
 import os
 import textwrap
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.50.0"
 
 
 class FclConan(ConanFile):
@@ -27,21 +30,9 @@ class FclConan(ConanFile):
         "with_octomap": True,
     }
 
-    generators = "cmake", "cmake_find_package_multi"
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -58,56 +49,59 @@ class FclConan(ConanFile):
             self.requires("octomap/1.9.7")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 11)
-        if self.settings.os == "Windows" and self.options.shared:
+        if self.info.settings.compiler.cppstd:
+            check_min_cppstd(self, 11)
+        if self.info.settings.os == "Windows" and self.info.options.shared:
             raise ConanInvalidConfiguration(
                 "fcl {} doesn't properly support shared lib on Windows".format(self.version)
             )
 
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["FCL_ENABLE_PROFILING"] = False
+        tc.variables["FCL_TREAT_WARNINGS_AS_ERRORS"] = False
+        tc.variables["FCL_HIDE_ALL_SYMBOLS"] = False
+        tc.variables["FCL_STATIC_LIBRARY"] = not self.options.shared
+        tc.variables["FCL_USE_X64_SSE"] = False # Let consumer decide to add relevant compile options, fcl doesn't have simd intrinsics
+        tc.variables["FCL_USE_HOST_NATIVE_ARCH"] = False
+        tc.variables["FCL_USE_SSE"] = False
+        tc.variables["FCL_COVERALLS"] = False
+        tc.variables["FCL_COVERALLS_UPLOAD"] = False
+        tc.variables["FCL_WITH_OCTOMAP"] = self.options.with_octomap
+        if self.options.with_octomap:
+            tc.variables["OCTOMAP_VERSION"] = self.deps_cpp_info["octomap"].version
+            octomap_major, octomap_minor, octomap_patch = self.deps_cpp_info["octomap"].version.split(".")
+            tc.variables["OCTOMAP_MAJOR_VERSION"] = octomap_major
+            tc.variables["OCTOMAP_MINOR_VERSION"] = octomap_minor
+            tc.variables["OCTOMAP_PATCH_VERSION"] = octomap_patch
+        tc.variables["BUILD_TESTING"] = False
+        tc.variables["FCL_NO_DEFAULT_RPATH"] = False
+        tc.generate()
+
+        cd = CMakeDeps(self)
+        cd.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["FCL_ENABLE_PROFILING"] = False
-        self._cmake.definitions["FCL_TREAT_WARNINGS_AS_ERRORS"] = False
-        self._cmake.definitions["FCL_HIDE_ALL_SYMBOLS"] = False
-        self._cmake.definitions["FCL_STATIC_LIBRARY"] = not self.options.shared
-        self._cmake.definitions["FCL_USE_X64_SSE"] = False # Let consumer decide to add relevant compile options, fcl doesn't have simd intrinsics
-        self._cmake.definitions["FCL_USE_HOST_NATIVE_ARCH"] = False
-        self._cmake.definitions["FCL_USE_SSE"] = False
-        self._cmake.definitions["FCL_COVERALLS"] = False
-        self._cmake.definitions["FCL_COVERALLS_UPLOAD"] = False
-        self._cmake.definitions["FCL_WITH_OCTOMAP"] = self.options.with_octomap
-        if self.options.with_octomap:
-            self._cmake.definitions["OCTOMAP_VERSION"] = self.deps_cpp_info["octomap"].version
-            octomap_major, octomap_minor, octomap_patch = self.deps_cpp_info["octomap"].version.split(".")
-            self._cmake.definitions["OCTOMAP_MAJOR_VERSION"] = octomap_major
-            self._cmake.definitions["OCTOMAP_MINOR_VERSION"] = octomap_minor
-            self._cmake.definitions["OCTOMAP_PATCH_VERSION"] = octomap_patch
-        self._cmake.definitions["BUILD_TESTING"] = False
-        self._cmake.definitions["FCL_NO_DEFAULT_RPATH"] = False
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
-
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "CMake"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "CMake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
         # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self._create_cmake_module_alias_targets(
@@ -115,8 +109,7 @@ class FclConan(ConanFile):
             {"fcl": "fcl::fcl"}
         )
 
-    @staticmethod
-    def _create_cmake_module_alias_targets(module_file, targets):
+    def _create_cmake_module_alias_targets(self, module_file, targets):
         content = ""
         for alias, aliased in targets.items():
             content += textwrap.dedent("""\
@@ -125,7 +118,7 @@ class FclConan(ConanFile):
                     set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
                 endif()
             """.format(alias=alias, aliased=aliased))
-        tools.save(module_file, content)
+        save(self, module_file, content)
 
     @property
     def _module_file_rel_path(self):
@@ -136,6 +129,11 @@ class FclConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "fcl")
         self.cpp_info.set_property("pkg_config_name", "fcl")
         self.cpp_info.libs = ["fcl"]
+
+        # TODO: to remove if required_conan_version updated to 1.51.1 (see https://github.com/conan-io/conan/pull/11790)
+        self.cpp_info.requires = ["eigen::eigen", "libccd::libccd"]
+        if self.options.with_octomap:
+            self.cpp_info.requires.append("octomap::octomap")
 
         # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
