0c2d1a954e465175a217473ff75f27e1a64b0db4
Ofir Iluz <ofir.iluz@cyberark.com>
2022-08-16 12:04:51 +0300

    Removed tests

--- recipes/octo-wildcardmatching-cpp/all/conanfile.py
+++ recipes/octo-wildcardmatching-cpp/all/conanfile.py
@@ -23,14 +23,10 @@ class OctoWildcardMatchingCPPConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][str(self.version)], strip_root=True, destination=self._source_subfolder)
 
-    def requirements(self):
-        self.requires("gtest/cci.20210126")
-
     def build(self):
         cmake = CMake(self)
         cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
         cmake.build(build_dir=self._build_subfolder)
-        cmake.test(build_dir=self._build_subfolder)
 
     def package(self):
         self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
@@ -43,9 +39,7 @@ class OctoWildcardMatchingCPPConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "octo::octo-wildcardmatching-cpp")
         self.cpp_info.set_property("pkg_config_name", "octo-wildcardmatching-cpp")
         self.cpp_info.components["octo-wildcardmatching-cpp"].libs = ["octo-wildcardmatching-cpp"]
-        self.cpp_info.components["octo-wildcardmatching-cpp"].requires = [
-            "gtest::gtest"
-        ]
+        self.cpp_info.components["octo-wildcardmatching-cpp"].requires = []
         self.cpp_info.filenames["cmake_find_package"] = "octo-wildcardmatching-cpp"
         self.cpp_info.filenames["cmake_find_package_multi"] = "octo-wildcardmatching-cpp"
         self.cpp_info.names["cmake_find_package"] = "octo-wildcardmatching-cpp"
