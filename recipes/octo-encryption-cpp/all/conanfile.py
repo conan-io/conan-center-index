f381af6ebde1742254203e271b8a6c281ca5c1de
Ofir Iluz <ofir.iluz@cyberark.com>
2022-08-17 12:51:48 +0300

    Fixes + version

--- recipes/octo-encryption-cpp/all/conanfile.py
+++ recipes/octo-encryption-cpp/all/conanfile.py
@@ -1,15 +1,18 @@
 from conans import ConanFile, CMake
 from conan.tools.files import get
+from conan.tools.build import check_min_cppstd
+from conans.errors import ConanInvalidConfiguration
+from conan.tools.scm import Version
 
 
 class OctoEncryptionCPPConan(ConanFile):
     name = "octo-encryption-cpp"
     license = "MIT"
-    url = "https://github.com/ofiriluz/octo-encryption-cpp"
+    url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/ofiriluz/octo-encryption-cpp"
     description = "Octo encryption library"
     topics = ("cryptography", "cpp")
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
     settings = "os", "compiler", "build_type", "arch"
 
     @property
@@ -20,15 +23,46 @@ class OctoEncryptionCPPConan(ConanFile):
     def _build_subfolder(self):
         return "build"
 
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "8",
+            "clang": "9",
+            "apple-clang": "11",
+            "Visual Studio": "16",
+        }
+
+    def validate(self):
+        if self.info.settings.compiler.cppstd:
+            check_min_cppstd(self, "17")
+
+        minimum_version = self._compilers_minimum_version.get(str(self.info.settings.compiler), False)
+        if minimum_version and Version(self.info.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.name} requires C++17, which your compiler does not support."
+            )
+        else:
+            self.output.warn(f"{self.name} requires C++17. Your compiler is unknown. Assuming it supports C++17.")
+        if self.settings.compiler == "clang" and self.settings.compiler.get_safe("libcxx") == "libc++":
+            raise ConanInvalidConfiguration(f"{self.name} does not support clang with libc++. Use libstdc++ instead.")
+        if self.settings.compiler == "Visual Studio" and self.settings.compiler.runtime in ["MTd", "MT"]:
+            raise ConanInvalidConfiguration(f"{self.name} does not support MSVC MT/MTd configurations, only MD/MDd is supported")
+
     def source(self):
         get(self, **self.conan_data["sources"][str(self.version)], strip_root=True, destination=self._source_subfolder)
 
     def requirements(self):
         self.requires("openssl/3.0.5")
 
+    def build_requirements(self):
+        self.build_requires("cmake/3.16.9")
+
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
+        cmake.configure(source_folder=self._source_subfolder, 
+                        build_folder=self._build_subfolder,
+                        defs={"DISABLE_TESTS": "ON",
+                              "DISABLE_EXAMPLES": "ON"})
         cmake.build(build_dir=self._build_subfolder)
 
     def package(self):
@@ -41,8 +75,8 @@ class OctoEncryptionCPPConan(ConanFile):
         self.cpp_info.set_property("cmake_file_name", "octo-encryption-cpp")
         self.cpp_info.set_property("cmake_target_name", "octo::octo-encryption-cpp")
         self.cpp_info.set_property("pkg_config_name", "octo-encryption-cpp")
-        self.cpp_info.components["octo-encryption-cpp"].libs = ["octo-encryption-cpp"]
-        self.cpp_info.components["octo-encryption-cpp"].requires = [
+        self.cpp_info.components["libocto-encryption-cpp"].libs = ["octo-encryption-cpp"]
+        self.cpp_info.components["libocto-encryption-cpp"].requires = [
             "openssl::openssl"
         ]
         self.cpp_info.filenames["cmake_find_package"] = "octo-encryption-cpp"
@@ -50,7 +84,7 @@ class OctoEncryptionCPPConan(ConanFile):
         self.cpp_info.names["cmake_find_package"] = "octo-encryption-cpp"
         self.cpp_info.names["cmake_find_package_multi"] = "octo-encryption-cpp"
         self.cpp_info.names["pkg_config"] = "octo-encryption-cpp"
-        self.cpp_info.components["octo-encryption-cpp"].names["cmake_find_package"] = "octo-encryption-cpp"
-        self.cpp_info.components["octo-encryption-cpp"].names["cmake_find_package_multi"] = "octo-encryption-cpp"
-        self.cpp_info.components["octo-encryption-cpp"].set_property("cmake_target_name", "octo::octo-encryption-cpp")
-        self.cpp_info.components["octo-encryption-cpp"].set_property("pkg_config_name", "octo-encryption-cpp")
+        self.cpp_info.components["libocto-encryption-cpp"].names["cmake_find_package"] = "octo-encryption-cpp"
+        self.cpp_info.components["libocto-encryption-cpp"].names["cmake_find_package_multi"] = "octo-encryption-cpp"
+        self.cpp_info.components["libocto-encryption-cpp"].set_property("cmake_target_name", "octo::octo-encryption-cpp")
+        self.cpp_info.components["libocto-encryption-cpp"].set_property("pkg_config_name", "octo-encryption-cpp")
