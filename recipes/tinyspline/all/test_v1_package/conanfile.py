ea5071bcb0bd27c378c8faccf02ce04cd344127f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-18 00:18:02 +0200

    conan v2 support

---
+++ recipes/tinyspline/all/test_v1_package/conanfile.py
@@ -0,0 +1,22 @@
+# pylint: skip-file
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["TINYSPLINE_CXX"] = self.options["tinyspline"].cxx
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self):
+            bin_c_path = os.path.join("bin", "test_package_c")
+            self.run(bin_c_path, run_environment=True)
+            if self.options["tinyspline"].cxx:
+                bin_cpp_path = os.path.join("bin", "test_package_cpp")
+                self.run(bin_cpp_path, run_environment=True)
