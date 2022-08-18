cee933672c264e8b4721a15b49f06f3e645f2929
jgsogo <jgsogo@gmail.com>
2022-08-18 11:26:40 +0200

    import both errors

--- linter/conanv2_test_transition.py
+++ linter/conanv2_test_transition.py
@@ -7,8 +7,11 @@ Pylint plugin/rules for test_package folder in Conan Center Index
 from pylint.lint import PyLinter
 from linter.check_import_conanfile import ImportConanFile
 from linter.check_no_test_package_name import NoPackageName
+from linter.check_import_errors import ImportErrorsConanException, ImportErrorsConanInvalidConfiguration
 
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(NoPackageName(linter))
     linter.register_checker(ImportConanFile(linter))
+    linter.register_checker(ImportErrorsConanException(linter))
+    linter.register_checker(ImportErrorsConanInvalidConfiguration(linter))
--- linter/conanv2_transition.py
+++ linter/conanv2_transition.py
@@ -7,10 +7,11 @@ Pylint plugin/rules for conanfiles in Conan Center Index
 from pylint.lint import PyLinter
 from linter.check_package_name import PackageName
 from linter.check_import_conanfile import ImportConanFile
-from linter.check_import_errors import ImportErrors
+from linter.check_import_errors import ImportErrorsConanException, ImportErrorsConanInvalidConfiguration
 
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(PackageName(linter))
     linter.register_checker(ImportConanFile(linter))
-    linter.register_checker(ImportErrors(linter))
+    linter.register_checker(ImportErrorsConanException(linter))
+    linter.register_checker(ImportErrorsConanInvalidConfiguration(linter))
