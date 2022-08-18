37c6201d901e5a5d85c3aef79b1973ebb03236d7
Jordan Williams <jordan@jwillikers.com>
2022-08-18 10:05:28 -0500

    Work around BUILD_SHARED_LIBS issue

--- recipes/gtest/all/conanfile.py
+++ recipes/gtest/all/conanfile.py
@@ -132,6 +132,10 @@ class GTestConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
+
+        # Honor BUILD_SHARED_LIBS from conan_toolchain (see https://github.com/conan-io/conan/issues/11840)
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+        
         if self.settings.build_type == "Debug":
             tc.cache_variables["CUSTOM_DEBUG_POSTFIX"] = str(self.options.debug_postfix)
         if is_msvc(self) or self._is_clang_cl:
