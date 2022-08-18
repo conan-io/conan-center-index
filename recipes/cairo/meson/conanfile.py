d328ae8877715aa9dfd65bfd14ad75b67f04b023
Andrew Marshall <planetmarshalluk@gmail.com>
2022-08-18 21:58:16 +0100

    [cairo] bump requirements versions

--- recipes/cairo/meson/conanfile.py
+++ recipes/cairo/meson/conanfile.py
@@ -107,7 +107,7 @@ class CairoConan(ConanFile):
         if self.options.with_png:
             self.requires("libpng/1.6.37")
         if self.options.with_glib:
-            self.requires("glib/2.73.0")
+            self.requires("glib/2.73.3")
         if self.settings.os == "Linux":
             if self.options.with_xlib or self.options.with_xlib_xrender or self.options.with_xcb:
                 self.requires("xorg/system")
@@ -121,7 +121,7 @@ class CairoConan(ConanFile):
             self.requires("egl/system")
 
     def build_requirements(self):
-        self.build_requires("meson/0.62.1")
+        self.build_requires("meson/0.63.1")
         self.build_requires("pkgconf/1.7.4")
 
     def validate(self):
