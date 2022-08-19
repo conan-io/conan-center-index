65c1683949c55dbbf680ff49d823626162f3c00c
Jordan Williams <jordan@jwillikers.com>
2022-08-19 11:53:14 -0500

    Apply patches as part of the build method

--- recipes/boost-ext-ut/all/conanfile.py
+++ recipes/boost-ext-ut/all/conanfile.py
@@ -76,7 +76,6 @@ class UTConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -90,6 +89,7 @@ class UTConan(ConanFile):
         tc.generate()
     
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
