d9dec6188e2232c43a068cdfb561b60e97257a67
SSE4 <tomskside@gmail.com>
2022-08-19 12:48:12 +0700

    Update recipes/glib/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/glib/all/conanfile.py
+++ recipes/glib/all/conanfile.py
@@ -234,7 +234,7 @@ class GLibConan(ConanFile):
 
     def package(self):
         if Version(self.version) < "2.73.0":
-            self.copy(pattern="COPYING", dst="licenses", src=self.source_folder)
+            copy(self, pattern="COPYING", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         else:
             copy(self, pattern="LGPL-2.1-or-later.txt", dst=os.path.join(self.package_folder, "licenses"), src=os.path.join(self.source_folder, "LICENSES"))
         meson = Meson(self)
