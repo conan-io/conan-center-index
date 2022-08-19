d99e9169de96aafe16403fa6746b1c327ebe58d1
Brian Szmyd <bszmyd@ebay.com>
2022-08-19 16:19:25 -0700

    Disable Windows builds entirely.

--- recipes/nuraft/all/conanfile.py
+++ recipes/nuraft/all/conanfile.py
@@ -42,13 +42,15 @@ class NuRaftConan(ConanFile):
         self.requires("openssl/1.1.1q")
 
     def validate(self):
+        if self.info.settings.os in ["Windows"]
+            raise ConanInvalidConfiguration("{} Builds are unsupported".format(self.info.settings.os))
         if self.info.settings.os in ["Macos"] and self.options.shared:
-            raise ConanInvalidConfiguration("Building Shared Object for {} unsupported".format(self.settings.os))
+            raise ConanInvalidConfiguration("Building Shared Object for {} unsupported".format(self.info.settings.os))
         if self.info.settings.compiler.cppstd:
             check_min_cppstd(self, 11)
 
     def configure(self):
-        if self.options.shared or self.settings.os == "Windows":
+        if self.options.shared:
             del self.options.fPIC
 
     def source(self):
