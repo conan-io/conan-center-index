375cba9fe076e3efa1b071df7575c2f8e319686b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-20 17:17:30 +0200

    fix required_conan_version

--- recipes/openddl-parser/all/conanfile.py
+++ recipes/openddl-parser/all/conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, get, rmdir
 import os
 
-required_conan_version = ">= 1.50.0"
+required_conan_version = ">=1.50.0"
 
 
 class OpenDDLParserConan(ConanFile):
