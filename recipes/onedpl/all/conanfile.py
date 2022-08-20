4f9d6472a30395f6753b53702cebdf3bdd90093e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-20 11:11:07 +0200

    conan v2 support

--- recipes/onedpl/all/conanfile.py
+++ recipes/onedpl/all/conanfile.py
@@ -1,8 +1,11 @@
-from conan import ConanFile, tools
-from conan.tools import files, scm
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.50.2 <1.51.0 || >=1.51.2"
 
 
 class OneDplConan(ConanFile):
@@ -24,39 +27,42 @@ class OneDplConan(ConanFile):
     }
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
     def requirements(self):
         if self.options.backend == "tbb":
             self.requires("onetbb/2020.3")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            if tools.scm.Version(self.version) >= "2021.7.0":
-                tools.build.check_min_cppstd(self, 17)
+            if Version(self.version) >= "2021.7.0":
+                check_min_cppstd(self, 17)
             else:
-                tools.build.check_min_cppstd(self, 11)
+                check_min_cppstd(self, 11)
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def source(self):
-        files.get(self, **self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True, destination=self.source_folder)
 
     def package(self):
-        version_major = int(tools.scm.Version(self.version).major()[0:4])
-        self.copy("*", src=os.path.join(self._source_subfolder, "include"), dst="include")
+        version_major = int(Version(self.version).major[0:4])
+        copy(self, "*", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
         if version_major < 2021:
-            self.copy("*", src=os.path.join(self._source_subfolder, "stdlib"), dst="include")
-            self.copy("LICENSE.txt", src=self._source_subfolder, dst="licenses")
+            copy(self, "*", src=os.path.join(self.source_folder, "stdlib"), dst=os.path.join(self.package_folder, "include"))
+            copy(self, "LICENSE.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         else:
-            self.copy("LICENSE.txt", src=os.path.join(self._source_subfolder, "licensing"), dst="licenses")
+            copy(self, "LICENSE.txt", src=os.path.join(self.source_folder, "licensing"), dst=os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "ParallelSTL")
         self.cpp_info.set_property("cmake_target_name", "pstl::ParallelSTL")
+        self.cpp_info.bindirs = []
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
 
         # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "ParallelSTL"
--- recipes/onedpl/all/test_package/conanfile.py
+++ recipes/onedpl/all/test_package/conanfile.py
@@ -1,19 +1,31 @@
-from conans import ConanFile, CMake
-from conan import tools
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.scm import Version
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps", "VirtualRunEnv"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain
+        tc.variables["ONEDPL_VERSION_MAJOR"] = Version(self.dependencies["onedpl"].ref.version).major[0:4]
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["ONEDPL_VERSION_MAJOR"] = tools.scm.Version(self.deps_cpp_info["onedpl"].version).major()[0:4]
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.build.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if not cross_building(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
