e69963a30291e9f6019cf5c92530d12809530226
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-20 21:50:41 +0200

    cleanup

--- recipes/winflexbison/all/conanfile.py
+++ recipes/winflexbison/all/conanfile.py
@@ -2,7 +2,6 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, get, rename, save
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.47.0"
