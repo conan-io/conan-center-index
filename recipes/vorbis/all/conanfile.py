0f5ed48d491650d673a713ac419b91f6062dc1d0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-22 00:27:53 +0200

    fix indentation

--- recipes/vorbis/all/conanfile.py
+++ recipes/vorbis/all/conanfile.py
@@ -36,13 +36,13 @@ class VorbisConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
         try:
-           del self.settings.compiler.libcxx
+            del self.settings.compiler.libcxx
         except Exception:
-           pass
+            pass
         try:
-           del self.settings.compiler.cppstd
+            del self.settings.compiler.cppstd
         except Exception:
-           pass
+            pass
 
     def requirements(self):
         self.requires("ogg/1.3.5")
