137514d9ddad30be41d6eda20df8f90d97056748
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-22 00:47:52 +0200

    fix indentation

--- recipes/sofa/all/conanfile.py
+++ recipes/sofa/all/conanfile.py
@@ -38,13 +38,13 @@ class SofaConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
         try:
-           del self.settings.compiler.libcxx
+            del self.settings.compiler.libcxx
         except Exception:
-           pass
+            pass
         try:
-           del self.settings.compiler.cppstd
+            del self.settings.compiler.cppstd
         except Exception:
-           pass
+            pass
 
     def layout(self):
         cmake_layout(self, src_folder="src")
