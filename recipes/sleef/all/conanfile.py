08e208350ced738913a6e41cfa9bab9ab6ee1172
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-22 00:41:21 +0200

    fix indentation

--- recipes/sleef/all/conanfile.py
+++ recipes/sleef/all/conanfile.py
@@ -40,13 +40,13 @@ class SleefConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
         try:
-           del self.settings.compiler.libcxx
+            del self.settings.compiler.libcxx
         except Exception:
-           pass
+            pass
         try:
-           del self.settings.compiler.cppstd
+            del self.settings.compiler.cppstd
         except Exception:
-           pass
+            pass
 
     def validate(self):
         if self.settings.os == "Windows" and self.options.shared:
