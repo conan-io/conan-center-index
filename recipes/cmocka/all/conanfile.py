2ca1002c29bb2ed2b80f3170aa3575b5ddcd5977
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-22 00:22:52 +0200

    fix indentation

--- recipes/cmocka/all/conanfile.py
+++ recipes/cmocka/all/conanfile.py
@@ -37,13 +37,13 @@ class CmockaConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
         try:
-           del self.settings.compiler.libcxx
+            del self.settings.compiler.libcxx
         except Exception:
-           pass
+            pass
         try:
-           del self.settings.compiler.cppstd
+            del self.settings.compiler.cppstd
         except Exception:
-           pass
+            pass
 
     def layout(self):
         cmake_layout(self, src_folder="src")
