aa5d89175a56e90d7603e93c072ef0f42a7f002d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-21 23:13:12 +0200

    check compiler from build profile

--- recipes/gsoap/all/conanfile.py
+++ recipes/gsoap/all/conanfile.py
@@ -3,7 +3,6 @@ from conan.tools.build import cross_building
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get
-from conan.tools.microsoft import is_msvc
 import os
 
 required_conan_version = ">=1.46.0"
@@ -36,6 +35,10 @@ class GsoapConan(ConanFile):
     exports_sources = "CMakeLists.txt", "cmake/*.cmake"
     short_paths = True
 
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -49,7 +52,8 @@ class GsoapConan(ConanFile):
         if cross_building(self, skip_x64_x86=True) and hasattr(self, "settings_build"):
             self.tool_requires("gsoap/{}".format(self.version))
 
-        if is_msvc(self):
+        # TODO: use is_msvc with build profile when possible (see https://github.com/conan-io/conan/issues/11926)
+        if str(self._settings_build.compiler) in ["Visual Studio", "msvc"]:
             self.tool_requires("winflexbison/2.5.24")
         else:
             self.tool_requires("bison/3.7.6")
