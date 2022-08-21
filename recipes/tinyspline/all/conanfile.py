669d904e27923ef2b433a63e69d25aa105c97031
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-22 00:40:22 +0200

    fix indentation

--- recipes/tinyspline/all/conanfile.py
+++ recipes/tinyspline/all/conanfile.py
@@ -46,13 +46,13 @@ class TinysplineConan(ConanFile):
             del self.options.fPIC
         if not self.options.cxx:
             try:
-               del self.settings.compiler.libcxx
+                del self.settings.compiler.libcxx
             except Exception:
-               pass
+                pass
             try:
-               del self.settings.compiler.cppstd
+                del self.settings.compiler.cppstd
             except Exception:
-               pass
+                pass
 
     def validate(self):
         if Version(self.version) >= "0.4.0" and self.options.cxx:
