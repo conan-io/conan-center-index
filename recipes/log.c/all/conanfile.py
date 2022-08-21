cdca6411560c3a805e64deb13e1ccc1ec0688d67
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-22 00:35:47 +0200

    fix indentation

--- recipes/log.c/all/conanfile.py
+++ recipes/log.c/all/conanfile.py
@@ -36,13 +36,13 @@ class LogcConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
         try:
-           del self.settings.compiler.libcxx
+            del self.settings.compiler.libcxx
         except Exception:
-           pass
+            pass
         try:
-           del self.settings.compiler.cppstd
+            del self.settings.compiler.cppstd
         except Exception:
-           pass
+            pass
 
     def layout(self):
         cmake_layout(self, src_folder="src")
