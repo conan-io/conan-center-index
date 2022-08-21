574bd01b93c36e24357f0c3e7b716ecf506440eb
Tom Conder <tom@itstom.co>
2022-08-21 02:00:12 -0500

    require conan 1.50.0

--- recipes/sdl/all/conanfile.py
+++ recipes/sdl/all/conanfile.py
@@ -4,7 +4,7 @@ from conans.errors import ConanInvalidConfiguration
 import functools
 import os
 
-required_conan_version = ">=1.47.0"
+required_conan_version = ">=1.50.0"
 
 
 class SDLConan(ConanFile):
