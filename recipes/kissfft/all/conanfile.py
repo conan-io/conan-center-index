adbdd701ca5706576c6cc669be5bc47421d20221
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-22 15:39:52 +0200

    Update recipes/kissfft/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/kissfft/all/conanfile.py
+++ recipes/kissfft/all/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir
 import os
 
-required_conan_version = ">=1.46.0"
+required_conan_version = ">=1.47.0"
 
 
 class KissfftConan(ConanFile):
