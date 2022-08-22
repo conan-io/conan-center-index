da9e9577c2027c5bb82de44a3939da2fd34985d7
Julien Marrec <julien.marrec@gmail.com>
2022-08-22 23:56:26 +0200

    Extend testing to require one of the native extensions

--- recipes/ruby/all/test_package/conanfile.py
+++ recipes/ruby/all/test_package/conanfile.py
@@ -8,8 +8,18 @@ class TestPackageConan(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain"
 
     def build(self):
+
         cmake = CMake(self)
-        cmake.configure()
+        # when --static-linked-ext is used, ruby defines EXTSTATIC as 1
+        # But when ruby itself is static there's nothing, so:
+        # We define RUBY_STATIC_RUBY when ruby itself is static
+        # We define RUBY_STATIC_LINKED_EXT when the ruby extensions are static (same as EXTSTATIC but clearer)
+        defs = {}
+        if not self.options['ruby'].shared:
+            defs['RUBY_STATIC_RUBY'] = 1
+        # if self.options['ruby'].with_static_linked_ext:
+        #     defs['RUBY_STATIC_LINKED_EXT'] = 1
+        cmake.configure(variables=defs)
         cmake.build()
 
     def test(self):
---
+++ recipes/ruby/all/test_package/test_package.cpp
@@ -0,0 +1,51 @@
+#include <ruby.h>
+
+// when --static-linked-ext is used, ruby defines EXTSTATIC as 1
+#if defined(EXTSTATIC) && EXTSTATIC
+#  define RUBY_STATIC_LINKED_EXT
+#else
+#  undef RUBY_STATIC_LINKED_EXT
+#endif
+
+int main(int argc, char* argv[]) {
+  ruby_sysinit(&argc, &argv);
+  RUBY_INIT_STACK;
+  ruby_init();
+
+  rb_eval_string("puts 'Hello, ruby!'");
+
+#ifdef RUBY_STATIC_RUBY
+  rb_eval_string("puts 'Ruby itself is statically linked'");
+#else
+  rb_eval_string("puts 'Ruby itself is dynamically linked'");
+#endif
+
+#ifdef RUBY_STATIC_LINKED_EXT
+  rb_eval_string("puts 'Ruby has statically linked extensions'");
+#else
+  rb_eval_string("puts 'Ruby has dynamically linked extensions'");
+#endif
+
+#ifdef RUBY_STATIC_RUBY
+  rb_provide("bigdecimal");
+  rb_provide("bigdecimal.so");
+#else
+  ruby_init_loadpath();
+#endif
+
+  rb_eval_string(R"(
+       begin
+         (require 'bigdecimal')
+         puts "I can correctly load one of the extension gems - bigdecimal"
+       rescue Exception => e
+         puts
+         puts "Error: #{e.message}"
+         puts "Backtrace:\n\t" + e.backtrace.join("\n\t")
+         raise
+       end
+     )");
+
+  ruby_finalize();
+
+  return EXIT_SUCCESS;
+}
