a38008ac30dfe57cf60aeff1907d90cd55b0d3fe
Jordan Williams <jordan@jwillikers.com>
2022-08-22 12:41:50 -0500

    Use can_run in test package

--- recipes/ms-gsl/all/test_package/conanfile.py
+++ recipes/ms-gsl/all/test_package/conanfile.py
@@ -1,7 +1,7 @@
 import os
 
 from conan import ConanFile
-from conan.tools.build import cross_building
+from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
 
 
@@ -9,11 +9,11 @@ class TestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
 
-    def layout(self):
-        cmake_layout(self)
-
     def requirements(self):
         self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
@@ -21,6 +21,5 @@ class TestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not cross_building(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, env="conanrun")
+        if can_run(self):
+            self.run(os.path.join(self.cpp.build.bindirs[0], "test_package"), env="conanrun")
