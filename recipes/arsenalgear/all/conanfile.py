c4c9df9519ceec8a1d1edf29a21812223585ea10
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 15:56:19 +0200

    migrate arsenalgear to conan v2

--- recipes/arsenalgear/all/conanfile.py
+++ recipes/arsenalgear/all/conanfile.py
@@ -1,5 +1,6 @@
-from conans import CMake, ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile, tools
+from conans import CMake
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.microsoft import is_msvc
 import functools
 
@@ -58,17 +59,17 @@ class ArsenalgearConan(ConanFile):
             raise ConanInvalidConfiguration("{} doesn't support Visual Studio(yet)".format(self.name))
 
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 17)
+            tools.build.check_min_cppstd(self, 17)
 
         minimum_version = self._compiler_required_cpp17.get(str(self.settings.compiler), False)
         if minimum_version:
-            if tools.Version(self.settings.compiler.version) < minimum_version:
+            if tools.scm.Version(self.settings.compiler.version) < minimum_version:
                 raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not support.".format(self.name))
         else:
             self.output.warn("{0} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name))
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        tools.files.get(self, **self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     @functools.lru_cache(1)
