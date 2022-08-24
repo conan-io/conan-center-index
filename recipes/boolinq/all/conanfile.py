90ed59177308ddea8d1f093da6db13bc04d24032
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 12:37:51 +0200

    migrate boolinq to conan v2

--- recipes/boolinq/all/conanfile.py
+++ recipes/boolinq/all/conanfile.py
@@ -20,13 +20,13 @@ class BoolinqConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, 11)
+            tools.build.check_min_cppstd(self, 11)
 
     def package_id(self):
         self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        tools.files.get(self, **self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     def package(self):
