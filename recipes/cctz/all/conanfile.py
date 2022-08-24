4aecc58086899718440f648aacb606c992281e43
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-24 23:06:29 +0200

    (#12418) cctz: conan v2 support

--- recipes/cctz/all/conanfile.py
+++ recipes/cctz/all/conanfile.py
@@ -1,8 +1,12 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, get, rmdir
+from conan.tools.scm import Version
+from conans import tools as tools_legacy
 import os
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.50.0"
 
 
 class CCTZConan(ConanFile):
@@ -10,32 +14,24 @@ class CCTZConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/google/cctz"
     description = "C++ library for translating between absolute and civil times"
-    topics = ("conan", "cctz", "time", "timezones")
+    topics = ("cctz", "time", "timezones")
     license = "Apache-2.0"
 
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "fPIC": [True, False],
         "shared": [True, False],
-        "build_tools" : [True, False]
+        "build_tools" : [True, False],
     }
     default_options = {
         "fPIC": True,
         "shared": False,
-        "build_tools": True
+        "build_tools": True,
     }
 
-    exports_sources = ["CMakeLists.txt", "patches/**"]
-    generators = "cmake"
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
+    def export_sources(self):
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -47,40 +43,44 @@ class CCTZConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler == "Visual Studio" and \
-           tools.Version(self.settings.compiler.version) < 14:
+           Version(self.settings.compiler.version) < 14:
             raise ConanInvalidConfiguration("CCTZ requires MSVC >= 14")
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["BUILD_TOOLS"] = self.options.build_tools
-        self._cmake.definitions["BUILD_EXAMPLES"] = False
-        self._cmake.definitions["BUILD_TESTING"] = False
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_TOOLS"] = self.options.build_tools
+        tc.variables["BUILD_EXAMPLES"] = False
+        tc.variables["BUILD_TESTING"] = False
+        # For shared msvc
+        tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
+        # Relocatable shared lib on Macos
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "cctz"
-        self.cpp_info.names["cmake_find_package_multi"] = "cctz"
+        self.cpp_info.set_property("cmake_file_name", "cctz")
+        self.cpp_info.set_property("cmake_target_name", "cctz::cctz")
         self.cpp_info.libs = ["cctz"]
-        if tools.is_apple_os(self.settings.os):
+        if tools_legacy.is_apple_os(self.settings.os):
             self.cpp_info.frameworks.append("CoreFoundation")
 
         if self.options.build_tools:
