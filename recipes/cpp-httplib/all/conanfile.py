e4f2e29b7b3903f416bb73d83d8ff1f4fb16236f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-25 00:34:14 +0200

    avoid conan warning about not build step

--- recipes/cpp-httplib/all/conanfile.py
+++ recipes/cpp-httplib/all/conanfile.py
@@ -56,6 +56,9 @@ class CpphttplibConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version],
             destination=self.source_folder, strip_root=True)
 
+    def build(self):
+        pass
+
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         copy(self, "httplib.h", src=self.source_folder, dst=os.path.join(self.package_folder, "include", "httplib"))
