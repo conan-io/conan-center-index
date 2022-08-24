9a7e63d7258ae8c3fd9746073ec6943d6ba0524f
danimtb <danimanzaneque@gmail.com>
2022-08-24 13:24:55 +0200

    [conan 2] Migrate box2d 2.4.x recipe

--- recipes/box2d/2.4.x/conanfile.py
+++ recipes/box2d/2.4.x/conanfile.py
@@ -1,5 +1,7 @@
 import os
-from conans import ConanFile, CMake, tools
+from conan import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.29.1"
 
@@ -15,56 +17,47 @@ class Box2dConan(ConanFile):
     options = {"shared": [True, False],
                "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True,}
-
-    exports_sources = ["CMakeLists.txt", "patches/**"]
-    generators = "cmake"
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
+    exports_sources = ["patches/**"]
 
     def config_options(self):
         if self.settings.os == "Windows":
-            del self.options.fPIC
+            try:
+                del self.options.fPIC
+            except Exception:
+                pass
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            try:
+                del self.options.fPIC
+            except Exception:
+                pass
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("box2d-%s" % self.version, self._source_subfolder)
+        tools.files.get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["BOX2D_BUILD_TESTBED"] = False
-        self._cmake.definitions["BOX2D_BUILD_UNIT_TESTS"] = False
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BOX2D_BUILD_TESTBED"] = False
+        tc.variables["BOX2D_BUILD_UNIT_TESTS"] = False
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        tools.files.apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        self.copy("LICENSE", dst="licenses")
+        cmake = CMake(self)
+        cmake.configure()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.pdb")
+        tools.files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "box2d"
         self.cpp_info.names["cmake_find_package_multi"] = "box2d"
         self.cpp_info.libs = ["box2d"]
-        if tools.Version(self.version) >= "2.4.1" and self.options.shared:
+        if Version(self.version) >= "2.4.1" and self.options.shared:
             self.cpp_info.defines.append("B2_SHARED")
--- recipes/box2d/2.4.x/test_package/conanfile.py
+++ recipes/box2d/2.4.x/test_package/conanfile.py
@@ -1,10 +1,17 @@
 import os
-from conans import ConanFile, CMake, tools
+from conan import ConanFile, tools
+from conan.tools.cmake import CMake, cmake_layout
 
 
 class Box2DTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -12,6 +19,6 @@ class Box2DTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if not tools.build.cross_building(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
