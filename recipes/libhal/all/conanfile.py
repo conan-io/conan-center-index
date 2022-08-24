41feafb2fb35d729a997aae1e99c0f3a0c3d93cb
Uilian Ries <uilianries@gmail.com>
2022-08-24 12:53:21 +0200

    Prepare for Conan 2.0
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libhal/all/conanfile.py
+++ recipes/libhal/all/conanfile.py
@@ -1,28 +1,68 @@
 from conan import ConanFile
-from conan.tools import files
+from conan.tools.files import get, copy
+from conan.tools.layout import basic_layout
+from conan.tools.build import check_min_cppstd
+from conan.errors import ConanInvalidConfiguration
+import os
 
-required_conan_version = ">=1.45.0"
+
+required_conan_version = ">=1.50.0"
 
 
-class libhal_conan(ConanFile):
+class LibHALConan(ConanFile):
     name = "libhal"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://libhal.github.io/libhal"
     description = ("A collection of interfaces and abstractions for embedded "
                    "peripherals and devices using modern C++")
-    topics = ("peripherals", "hardware", "abstraction", "devices")
+    topics = ("peripherals", "hardware", "abstraction", "devices", "hal")
     settings = "os", "compiler", "arch", "build_type"
     no_copy_source = True
 
-    def package(self):
-        self.copy("*LICENSE*", dst="licenses", keep_path=False)
-        self.copy("*.h", src="include/", dst="include/")
-        self.copy("*.hpp", src="include/", dst="include/")
-
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def source(self):
-        files.get(self,
-                  **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+
+    @property
+    def _min_cppstd(self):
+        return "20"
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "11",
+            "Visual Studio": "17",
+            "msvc": "19.22",
+            "clang": "13",
+            "apple-clang": "13"
+        }
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+        def lazy_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
+        compiler = str(self.settings.compiler)
+        version = str(self.settings.compiler.version)
+        minimum_version = self._compilers_minimum_version.get(compiler, False)
+        if not minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.name} requires C++20. Your compiler configuration ({compiler}-{version}) wasn't validated. \
+                please report an issue if it does actually supports c++20.")
+        elif lazy_lt_semver(version, minimum_version):
+            raise ConanInvalidConfiguration(
+                f"{self.name} {self.version} requires C++20, which your compiler ({compiler}-{version}) does not support")
+
+    def layout(self):
+        basic_layout(self)
+
+    def package(self):
+        copy(self, "LICENSE", dst=os.path.join(self.package_folder, "licenses"),  src=self.source_folder)
+        copy(self, "*.h", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "include"))
+        copy(self, "*.hpp", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "include"))
--- recipes/libhal/all/test_package/libhal.tweaks.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace hal::config {
-inline int callback_call_count = 0;
-constexpr bool on_error_callback_enabled = true;
-constexpr auto on_error_callback = []() { callback_call_count++; };
-using float_type = double;
-}  // namespace hal::config
