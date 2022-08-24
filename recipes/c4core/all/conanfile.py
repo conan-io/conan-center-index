9b14b3de8d5b942f24f40204cd35ca707a41858e
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 12:38:35 +0200

    migrate c4core to conan v2

--- recipes/c4core/all/conanfile.py
+++ recipes/c4core/all/conanfile.py
@@ -1,5 +1,6 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile, tools
+from conans import CMake
+from conan.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.43.0"
@@ -47,17 +48,17 @@ class C4CoreConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "11")
+            tools.build.check_min_cppstd(self, "11")
 
         ## clang with libc++ is not supported. It is already fixed at 2022-01-03.
-        if tools.Version(self.version) <= "0.1.8":
+        if tools.scm.Version(self.version) <= "0.1.8":
             if (self.settings.compiler == "clang" and self.settings.compiler.get_safe("libcxx") == "libc++"):
                 raise ConanInvalidConfiguration(
                     "{}/{} doesn't support clang with libc++".format(self.name, self.version),
                 )
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        tools.files.get(self, **self.conan_data["sources"][self.version],
             destination=self._source_subfolder, strip_root=True)
 
     def _configure_cmake(self):
@@ -68,8 +69,8 @@ class C4CoreConan(ConanFile):
         return self._cmake
 
     def build(self):
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "c4/ext/fast_float_all.h", "")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "c4", "ext", "fast_float.hpp"),
+        tools.files.replace_in_file(self, os.path.join(self._source_subfolder, "CMakeLists.txt"), "c4/ext/fast_float_all.h", "")
+        tools.files.replace_in_file(self, os.path.join(self._source_subfolder, "src", "c4", "ext", "fast_float.hpp"),
             '#include "c4/ext/fast_float_all.h"',
             '#include "fast_float/fast_float.h"')
 
@@ -80,9 +81,9 @@ class C4CoreConan(ConanFile):
         self.copy(pattern="LICENSE*", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "include"), "*.natvis")
+        tools.files.rmdir(self, os.path.join(self.package_folder, "cmake"))
+        tools.files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        tools.files.rm(self, os.path.join(self.package_folder, "include"), "*.natvis")
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "c4core")
