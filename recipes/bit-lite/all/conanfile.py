f87f2a57d43eeb6db517fd687f9ba970ca1354ac
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 12:37:34 +0200

    migrate bit-lite to conan v2

--- recipes/bit-lite/all/conanfile.py
+++ recipes/bit-lite/all/conanfile.py
@@ -23,7 +23,7 @@ class BitLiteConan(ConanFile):
         self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        tools.files.get(self, **self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     def package(self):
