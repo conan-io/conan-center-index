bbf27e1ebdee640b408c7512196df0f14df8f92b
ggulgulia <gajendra.gulgulia@gmail.com>
2022-08-25 14:14:14 +0200

    fix: conanfile linter error in testpackage, conandata.yml

--- recipes/simbody/all/test_package/conanfile.py
+++ recipes/simbody/all/test_package/conanfile.py
@@ -1,6 +1,7 @@
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, cmake_layout
 import os
-
-from conans import CMake, ConanFile, tools
 
 
 class TestPackageConan(ConanFile):
