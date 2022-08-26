b6f8235e6d6a0a293c3f3d041387a43f7c1d1127
Martin Siggel <martin.siggel@dlr.de>
2022-08-26 15:15:57 +0200

    Pep8 style fixes

--- recipes/tixi3/all/conanfile.py
+++ recipes/tixi3/all/conanfile.py
@@ -11,11 +11,10 @@ required_conan_version = ">=1.45.0"
 class Tixi3Conan(ConanFile):
     name = "tixi3"
     url = "https://github.com/conan-io/conan-center-index"
-    description = "libxslt is a software library implementing XSLT processor, based on libxml2"
+    description = "A simple xml interface based on libxml2 and libxslt"
     topics = ("tixi3", "xml")
-    homepage = "https://xmlsoft.org"
-    license = "MIT"
-    # version = "3.3.0"
+    homepage = "https://github.com/DLR-SC/tixi"
+    license = "Apache-2.0"
 
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -26,14 +25,14 @@ class Tixi3Conan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    
-    source_subfolder="_src_"
-    generators=["CMakeDeps", "CMakeToolchain"]
 
-    @property
+    source_subfolder = "_src_"
+    generators = ["CMakeDeps", "CMakeToolchain"]
 
-    def _build_subfolder(self):
-        return "build_subfolder"
+    def requirements(self):
+        self.requires("libxml2/2.9.14")
+        self.requires("libxslt/1.1.34")
+        self.requires("libcurl/7.84.0")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -42,26 +41,23 @@ class Tixi3Conan(ConanFile):
     def configure(self):
         # tixi is a c library
         del self.settings.compiler.libcxx
-        
+
         if self.options.shared:
             del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
-    def requirements(self):
-        self.requires("libxml2/2.9.14")
-        self.requires("libxslt/1.1.34")
-        self.requires("libcurl/7.84.0")
-
     def export_sources(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             self.copy(patch["patch_file"])
 
-
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self.source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  strip_root=True, destination=self.source_subfolder)
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(patch_file=patch["patch_file"], base_path=self.source_subfolder)
+            tools.patch(
+                patch_file=patch["patch_file"],
+                base_path=self.source_subfolder)
 
     def build(self):
         cmake = CMake(self)
@@ -71,21 +67,20 @@ class Tixi3Conan(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.install()
-        
+
         # remove package generated cmake config files
         tools.rmdir(os.path.join(self.package_folder, "lib", "tixi3"))
-        
+
         # copy the LICENSE file
         self.copy("LICENSE", dst="licenses", src=self.source_subfolder)
-        
-        # remove share directory, which only contains documentation, expamples...
+
+        # remove share directory, which only contains documentation,
+        # expamples...
         tools.rmdir(os.path.join(self.package_folder, "share"))
 
-
-
     def package_info(self):
-        self.cpp_info.includedirs = ['include/tixi3'] 
-        self.cpp_info.libs = ['tixi3']  # The libs to link against
+        self.cpp_info.includedirs = ['include/tixi3']
+        self.cpp_info.libs = ['tixi3']
         self.cpp_info.set_property("cmake_target_aliases", ["tixi3"])
-        self.cpp_info.libdirs = ['lib']  # Directories where libraries can be found
-        self.cpp_info.bindirs = ['bin']  
+        self.cpp_info.libdirs = ['lib']
+        self.cpp_info.bindirs = ['bin']
