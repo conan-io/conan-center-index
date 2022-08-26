8b393cacbce9aa7bf0acead88aba14be1d2ae745
Uilian Ries <uilianries@gmail.com>
2022-08-26 10:53:36 +0200

    Update recipes/semver200/all/test_package/conanfile.py

--- recipes/semver200/all/test_package/conanfile.py
+++ recipes/semver200/all/test_package/conanfile.py
@@ -20,6 +20,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not can_run(self):
+        if can_run(self):
             bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
             self.run(bin_path, env="conanrun")
