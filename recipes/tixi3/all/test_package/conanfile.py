3e5912c43119c2ae66017ef630223fad7db029c6
Martin Siggel <martin.siggel@dlr.de>
2022-08-26 21:59:43 +0200

    Fixed running test on windows

--- recipes/tixi3/all/test_package/conanfile.py
+++ recipes/tixi3/all/test_package/conanfile.py
@@ -1,7 +1,7 @@
 
 from conan import ConanFile
-from conan.tools.cmake import CMake
-from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
 import os
 
 
@@ -9,12 +9,14 @@ class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain"
 
+    def layout(self):
+        cmake_layout(self)
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not cross_building(self):
-            bin_path = os.path.join(".", "tixi3_conan_test")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            self.run(os.path.join(self.cpp.build.bindirs[0], "tixi3_conan_test"), env="conanrun")
