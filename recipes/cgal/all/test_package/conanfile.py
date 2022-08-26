a9123fe369a6008e7328eb6bc4dfef5eeee767f0
Sebastien Loriot <sebastien.loriot@cgal.org>
2022-08-26 18:05:58 +0200

    (#12249) cgal: add versions after 5.3 up to 5.5
    * add versions after 5.3 up to 5.5
    
    * since CGAL 5.3, CGAL is only header-only
    
    * Fix hooks errors
    
    * since CGAL 5.3, CGAL is only header-only
    
    * use get_safe to access the option "header_only"
    
    * Fix linter error
    
    * No 'name' in test_package
    
    I had used the wrong `linterc` file.
    
    * Only add CGAL-5.5
    
    * Only keep latest version of each major release
    
    * Use header-only for all version, remove all options
    
    Anyway, any version after 5.3 (and included 5.3) is header-only and no
    longer supports anything but header-only. That simplifies
    the conan file.
    
    * Follow suggestions from @uilianries review
    
    * Requires Conan >=1 .50 for info.clear()
    
    Co-authored-by: Laurent Rineau <laurent.rineau@cgal.org>

--- recipes/cgal/all/test_package/conanfile.py
+++ recipes/cgal/all/test_package/conanfile.py
@@ -1,6 +1,8 @@
 import os
 
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools import build
+from conans import CMake
 
 
 class TestPackageConan(ConanFile):
@@ -13,6 +15,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not build.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
