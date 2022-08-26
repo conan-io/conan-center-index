7550da82dea258df3c98000eea4f19c4b5295e51
Martin Delille <martin@delille.org>
2022-08-26 12:00:34 +0200

    Use appropriate qt version

--- recipes/qxlsx/all/conanfile.py
+++ recipes/qxlsx/all/conanfile.py
@@ -1,3 +1,4 @@
+from conan.tools.files import apply_conandata_patches
 from conans import CMake, ConanFile, tools
 import functools
 import os
@@ -43,7 +44,7 @@ class QXlsxConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("qt/5.15.3")
+        self.requires("qt/5.15.5")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version],
@@ -52,12 +53,12 @@ class QXlsxConan(ConanFile):
     @functools.lru_cache(1)
     def _configure_cmake(self):
         cmake = CMake(self)
+        cmake.definitions["QT_VERSION_MAJOR"] = tools.Version(self.deps_cpp_info["qt"].version).major
         cmake.configure()
         return cmake
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        apply_conandata_patches(self)
         cmake = self._configure_cmake()
         cmake.build()
 
