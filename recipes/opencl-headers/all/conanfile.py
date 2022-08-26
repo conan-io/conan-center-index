46bcb0daf02adc35557a4f56e58dc9ff7ea3961d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-26 14:23:42 +0200

    conan v2 support

--- recipes/opencl-headers/all/conanfile.py
+++ recipes/opencl-headers/all/conanfile.py
@@ -1,7 +1,9 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
 import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.50.0"
 
 
 class OpenclHeadersConan(ConanFile):
@@ -14,25 +16,31 @@ class OpenclHeadersConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def build(self):
+        pass
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("*", dst=os.path.join("include", "CL"), src=os.path.join(self._source_subfolder, "CL"))
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*", src=os.path.join(self.source_folder, "CL"), dst=os.path.join(self.package_folder, "include", "CL"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "OpenCLHeaders")
         self.cpp_info.set_property("cmake_target_name", "OpenCL::Headers")
         self.cpp_info.set_property("pkg_config_name", "OpenCL")
+        self.cpp_info.bindirs = []
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
 
         # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "OpenCLHeaders"
@@ -44,3 +52,7 @@ class OpenclHeadersConan(ConanFile):
         self.cpp_info.components["_opencl-headers"].names["cmake_find_package_multi"] = "Headers"
         self.cpp_info.components["_opencl-headers"].set_property("cmake_target_name", "OpenCL::Headers")
         self.cpp_info.components["_opencl-headers"].set_property("pkg_config_name", "OpenCL")
+        self.cpp_info.components["_opencl-headers"].bindirs = []
+        self.cpp_info.components["_opencl-headers"].frameworkdirs = []
+        self.cpp_info.components["_opencl-headers"].libdirs = []
+        self.cpp_info.components["_opencl-headers"].resdirs = []
