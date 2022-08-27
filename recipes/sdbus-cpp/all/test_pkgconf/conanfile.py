7a09e9f17fe9b91663a8cdcb21f72cb4cdbe77b7
Sergey Bobrenok <bobrofon@gmail.com>
2022-08-27 17:16:03 +0300

    sdbus-cpp/all: Add pkg-config test
    sdbus-cpp officially supports pkg-config, so we need to test it.

---
+++ recipes/sdbus-cpp/all/test_pkgconf/conanfile.py
@@ -0,0 +1,21 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+
+class SdbusCppTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = ("cmake", "pkg_config")
+
+    def build_requirements(self):
+        self.build_requires("pkgconf/1.7.4")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "example")
+            self.run(bin_path, run_environment=True)
