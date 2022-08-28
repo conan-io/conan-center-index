3e5226cb0b11cdf91c37038b7f5c53e123fa3ff9
Andrey Filipenkov <decapitator@ukr.net>
2022-08-28 16:06:25 +0300

    fix required_conan_version
    due to usage of major/minor in conan.tools.scm.Version
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/libpng/all/conanfile.py
+++ recipes/libpng/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.scm import Version
 from conan.tools.files import apply_conandata_patches, get, rm, rmdir, replace_in_file
 from conan.tools.build.cross_building import cross_building
 
-required_conan_version = ">=1.50.2"
+required_conan_version = ">=1.50.2 <1.51.0 || >=1.51.2"
 
 
 class LibpngConan(ConanFile):
