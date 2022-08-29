7a546686a506eb6033bdc28f885ae380871ee98f
Christopher McArthur <christopherm@jfrog.com>
2022-08-29 11:52:37 -0700

    tensorflow-lite: unvedor as many deps as possible

---
+++ recipes/tensorflow-lite/all/test_v1_package/conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self):
+            model_path = os.path.join(self.source_folder, "..", "test_packacge", "model.tflite")
+            command = os.path.join("bin", "test_package")
+            self.run(" ".join([command, model_path]), run_environment=True)
