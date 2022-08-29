094099c63a35d657a8300d46fd63e7c7e18c144d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-30 00:47:28 +0200

    fix test_package

--- recipes/spirv-tools/all/test_package/conanfile.py
+++ recipes/spirv-tools/all/test_package/conanfile.py
@@ -23,6 +23,6 @@ class TestPackageConan(ConanFile):
         if can_run(self):
             bin_path_c = os.path.join(self.cpp.build.bindirs[0], "test_package_c")
             self.run(bin_path_c, env="conanrun")
-            if not self.dependencies["spirv-tools"].options.shared:
-                bin_path_cpp = os.path.join(self.cpp.build.bindirs[0], "test_package_cpp")
+            bin_path_cpp = os.path.join(self.cpp.build.bindirs[0], "test_package_cpp")
+            if os.path.exists(bin_path_cpp):
                 self.run(bin_path_cpp, env="conanrun")
