8b615bf0edc296465d78e9bfe6158cc94e9435e1
ggulgulia <gajendra.gulgulia@gmail.com>
2022-08-30 20:22:24 +0200

    fix linter error

--- recipes/simbody/all/conanfile.py
+++ recipes/simbody/all/conanfile.py
@@ -2,8 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.scm import Version
-from conan.tools.files import copy, get, rmdir, save
-from conan.tools.patches import apply_conandata_patches
+from conan.tools.files import copy, get, rmdir, save, apply_conandata_patches
 import os
 import textwrap
 
