7ee28f24b5cdd02a013262563a274bc05069fef9
Andrei Malashkin <malashkin.andrey@gmail.com>
2022-08-30 18:44:17 +0300

    (#12528) migrate csvmonkey to conan v2
    * migrate csvmonkey to conan v2
    
    * Update recipes/csvmonkey/all/test_package/conanfile.py
    
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>
    
    * Update recipes/csvmonkey/all/conanfile.py
    
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>
    
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>

--- recipes/csvmonkey/all/test_package/conanfile.py
+++ recipes/csvmonkey/all/test_package/conanfile.py
@@ -1,5 +1,6 @@
 import os
-from conans import ConanFile, CMake, tools
+from conan import ConanFile, tools
+from conans import CMake
 
 class CSVMonkeyTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -11,6 +12,6 @@ class CSVMonkeyTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not tools.build.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
