d376290b62e7d55e3b5888ad14ca51268e8cfdcc
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-08-30 13:46:46 +0200

    conan v2 support

--- recipes/vulkan-memory-allocator/all/conanfile.py
+++ recipes/vulkan-memory-allocator/all/conanfile.py
@@ -1,7 +1,11 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import apply_conandata_patches, copy, get
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.50.0"
 
 
 class VulkanMemoryAllocatorConan(ConanFile):
@@ -14,39 +18,43 @@ class VulkanMemoryAllocatorConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
     def _min_cppstd(self):
-        return "11" if tools.Version(self.version) < "3.0.0" else "14"
+        return "11" if Version(self.version) < "3.0.0" else "14"
 
     def export_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def requirements(self):
         self.requires("vulkan-headers/1.3.216.0")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._min_cppstd)
+            check_min_cppstd(self, self._min_cppstd)
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        apply_conandata_patches(self)
 
     def package(self):
-        self.copy("LICENSE.txt", src=self._source_subfolder, dst="licenses")
-        if tools.Version(self.version) < "3.0.0":
-            include_dir = os.path.join(self._source_subfolder, "src")
+        copy(self, "LICENSE.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        if Version(self.version) < "3.0.0":
+            include_dir = os.path.join(self.source_folder, "src")
         else:
-            include_dir = os.path.join(self._source_subfolder, "include")
-        self.copy("vk_mem_alloc.h", src=include_dir, dst="include")
+            include_dir = os.path.join(self.source_folder, "include")
+        copy(self, "vk_mem_alloc.h", src=include_dir, dst=os.path.join(self.package_folder, "include"))
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
