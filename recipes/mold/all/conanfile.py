899bddab323b7a4ad030a2004459443261a427b4
Riff, Eric <ericriff@gmail.com>
2022-08-30 20:39:34 -0300

    [mold] Add latest version 1.4.1

--- recipes/mold/all/conanfile.py
+++ recipes/mold/all/conanfile.py
@@ -2,8 +2,11 @@ from conan import ConanFile
 from conan.tools.scm import Version
 from conan.tools import files
 from conan.errors import ConanInvalidConfiguration
-from conans import AutoToolsBuildEnvironment
+from conans import AutoToolsBuildEnvironment, CMake
+from conan.tools.files import get
+
 import os
+import functools
 
 required_conan_version = ">=1.47.0"
 
@@ -17,7 +20,7 @@ class MoldConan(ConanFile):
 
     settings = "os", "arch", "compiler", "build_type"
 
-    generators = "make"
+    generators = "make", "cmake", "cmake_find_package"
 
     def validate(self):
         if self.settings.build_type == "Debug":
@@ -41,11 +44,15 @@ class MoldConan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    def export_sources(self):
+        if Version(self.version) > "1.4.0":
+            self.copy("CMakeLists.txt")
+
     def _get_include_path(self, dependency):
         include_path = self.deps_cpp_info[dependency].rootpath
         include_path = os.path.join(include_path, "include")
         return include_path
-        
+
     def _patch_sources(self):
         if self.settings.compiler == "apple-clang" or (self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "11"):
             files.replace_in_file(self, "source_subfolder/Makefile", "-std=c++20", "-std=c++2a")
@@ -64,26 +71,53 @@ class MoldConan(ConanFile):
         files.replace_in_file(self, "source_subfolder/Makefile", "MOLD_LDFLAGS += -lmimalloc", "MOLD_LDFLAGS += -L{} -lmimalloc".format(
             self.deps_cpp_info["mimalloc"].lib_paths[0]))
 
+    @functools.lru_cache(1)
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["MOLD_USE_SYSTEM_MIMALLOC"] = True
+        cmake.definitions["MOLD_USE_SYSTEM_TBB"] = True
+        cmake.definitions["MOLD_USE_MOLD"] = False
+        cmake.configure()
+        return cmake
+
+    def build_requirements(self):
+        self.tool_requires("cmake/3.23.0")
+
     def requirements(self):
         self.requires("zlib/1.2.12")
         self.requires("openssl/1.1.1q")
-        self.requires("xxhash/0.8.1")
         self.requires("onetbb/2021.3.0")
         self.requires("mimalloc/2.0.6")
+        if Version(self.version) < "1.4.0":
+            self.requires("xxhash/0.8.1")
 
     def source(self):
         files.get(self, **self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     def build(self):
-        self._patch_sources()
-        with files.chdir(self, self._source_subfolder):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.make(target="mold", args=['SYSTEM_TBB=1', 'SYSTEM_MIMALLOC=1'])
+        if Version(self.version) < "1.4":
+            self._patch_sources()
+            with files.chdir(self, self._source_subfolder):
+                autotools = AutoToolsBuildEnvironment(self)
+                autotools.make(target="mold", args=['SYSTEM_TBB=1', 'SYSTEM_MIMALLOC=1'])
+        else:
+            # Error out if ZLIB is not found, we want to be predictable
+            tools.replace_in_file("source_subfolder/CMakeLists.txt", "find_package(ZLIB QUIET)", "find_package(ZLIB REQUIRED)")
+            # Since we introduce a conan wrapper, CMAKE_SOURCE_DIR points a dir above. mold_SOURD_DIR is an equivalent to the vanilla behavior
+            tools.replace_in_file("source_subfolder/CMakeLists.txt", "${CMAKE_SOURCE_DIR}/update-git-hash.py", "${mold_SOURCE_DIR}/update-git-hash.py")
+            # This is a bug upstream. You can't assing definitions to a target you don't build.
+            tools.replace_in_file("source_subfolder/CMakeLists.txt", "target_compile_definitions(mimalloc INTERFACE USE_SYSTEM_MIMALLOC)", "target_compile_definitions(mold PRIVATE USE_SYSTEM_MIMALLOC)")
+            cmake = self._configure_cmake()
+            cmake.build()
 
     def package(self):
         self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
-        self.copy("mold", src=self._source_subfolder, dst="bin", keep_path=False)
+        if Version(self.version) < "1.4":
+            self.copy("mold", src=self._source_subfolder, dst="bin", keep_path=False)
+        else:
+            cmake = self._configure_cmake()
+            cmake.install()
 
     def package_id(self):
         del self.info.settings.compiler
