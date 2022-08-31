838f647b585ac040055b6e45d452b655afc0c26c
Khalil Estell <kammcecorp@gmail.com>
2022-08-31 08:42:10 -0700

    Add Boost.LEAF to Conan Center
    Resolves boostorg/leaf#47

---
+++ recipes/boost-leaf/all/test_package/main.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <boost/leaf.hpp>
+
+using namespace boost;
+
+enum error_t
+{
+  err1,
+  err2,
+  err3,
+};
+
+leaf::result<int> f1()
+{
+  return 5;
+}
+
+leaf::result<int> f2()
+{
+  return 15;
+}
+
+leaf::result<int> g(int a, int b)
+{
+  int sum = a + b;
+  if (sum == 20)
+  {
+    return leaf::new_error(error_t::err2);
+  }
+  return sum;
+}
+
+int main()
+{
+  leaf::result<int> r = leaf::try_handle_some(
+      []() -> leaf::result<int>
+      {
+        BOOST_LEAF_AUTO(v1, f1());
+        BOOST_LEAF_AUTO(v2, f2());
+
+        return g(v1, v2);
+      },
+      [](leaf::match<error_t, error_t::err1, error_t::err3>) -> leaf::result<int>
+      {
+        exit(1);
+        return -1;
+      },
+      [](error_t e) -> leaf::result<int>
+      {
+        printf("Error value [%d] handled\n", static_cast<int>(e));
+        return 17;
+      });
+
+  if (r)
+  {
+    printf("value of r = %d\n", r.value());
+  }
+  else
+  {
+    printf("r contains an error!\n");
+  }
+
+  return 0;
+}
