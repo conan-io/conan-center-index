a0eca8d6b0855821a80f4d5de449ca7d82972bf2
ggulgulia <gajendra.gulgulia@gmail.com>
2022-08-31 16:00:18 +0200

    remove unused imports: textwrap, save

--- recipes/simbody/all/conanfile.py
+++ recipes/simbody/all/conanfile.py
@@ -2,9 +2,8 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.scm import Version
-from conan.tools.files import copy, get, rmdir, save, apply_conandata_patches
+from conan.tools.files import copy, get, rmdir, apply_conandata_patches
 import os
-import textwrap
 
 required_conan_version = ">=1.50.0"
 
