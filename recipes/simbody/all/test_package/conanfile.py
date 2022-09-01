91f0de169b9342f262dd528a87b89020cc867cd5
Gajendra <9218964+ggulgulia@users.noreply.github.com>
2022-09-01 09:49:56 +0200

    use new generators in test package
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/simbody/all/test_package/conanfile.py
+++ recipes/simbody/all/test_package/conanfile.py
@@ -6,7 +6,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
