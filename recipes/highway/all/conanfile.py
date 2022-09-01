08ee6e28ff6f95cd3835fcaacc2d84fa84313703
andrei malashkin <malashkin.andrey@gmail.com>
2022-09-01 09:19:32 +0200

    migrate highway to conan v2

--- recipes/highway/all/conanfile.py
+++ recipes/highway/all/conanfile.py
@@ -1,5 +1,6 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile, tools
+from conans import CMake
+from conan.errors import ConanInvalidConfiguration
 import os
 import functools
 
@@ -50,38 +51,38 @@ class HighwayConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
-        if tools.Version(self.version) < "0.16.0":
+        if tools.scm.Version(self.version) < "0.16.0":
             del self.options.shared
         elif self.options.shared:
             del self.options.fPIC
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+            tools.build.check_min_cppstd(self, self._minimum_cpp_standard)
         minimum_version = self._minimum_compilers_version.get(
                                                    str(self.settings.compiler))
         if not minimum_version:
             self.output.warn(
                 "{} recipe lacks information about the {} compiler support."
                 .format(self.name, self.settings.compiler))
-        elif tools.Version(self.settings.compiler.version) < minimum_version:
+        elif tools.scm.Version(self.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(
                 "{} requires a {} version >= {}"
                 .format(self.name, self.settings.compiler,
                         self.settings.compiler.version))
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        tools.files.get(self, **self.conan_data["sources"][self.version],
             destination=self._source_subfolder, strip_root=True)
 
     def _patch_sources(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+            tools.files.patch(self, **patch)
         # Honor fPIC option
         cmakelists = os.path.join(self._source_subfolder, "CMakeLists.txt")
-        tools.replace_in_file(cmakelists,
+        tools.files.replace_in_file(self, cmakelists,
                               "set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)", "")
-        tools.replace_in_file(cmakelists,
+        tools.files.replace_in_file(self, cmakelists,
                               "set_property(TARGET hwy PROPERTY "
                               "POSITION_INDEPENDENT_CODE ON)", "")
 
@@ -102,15 +103,15 @@ class HighwayConan(ConanFile):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.files.rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.names["pkg_config"] = "libhwy"
 
         self.cpp_info.libs = ["hwy"]
-        if tools.Version(self.version) >= "0.12.1":
+        if tools.scm.Version(self.version) >= "0.12.1":
             self.cpp_info.libs.append("hwy_contrib")
-        if tools.Version(self.version) >= "0.15.0":
+        if tools.scm.Version(self.version) >= "0.15.0":
             self.cpp_info.libs.append("hwy_test")
-        if tools.Version(self.version) >= "0.16.0":
+        if tools.scm.Version(self.version) >= "0.16.0":
             self.cpp_info.defines.append("HWY_SHARED_DEFINE" if self.options.shared else "HWY_STATIC_DEFINE")
