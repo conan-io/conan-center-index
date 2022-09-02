ea10b5a7cc4a14eb493bf7ef4e4a9aabc271e8fd
jelle Spijker <j.spijker@ultimaker.com>
2022-09-02 08:43:33 +0200

    applied review comments
    KB-H013 states that everything needs to be installed under bin for tools.
    This also means that the `PATH` for the actual bin location `bin/bin` needs
    to be added to the `build_env`

--- recipes/autoconf/all/conanfile.py
+++ recipes/autoconf/all/conanfile.py
@@ -1,8 +1,8 @@
-from os import environ
-from pathlib import Path
+from functools import lru_cache
+from os import environ, path
 
 from conan import ConanFile
-from conan.tools.files import get, AutoPackager, apply_conandata_patches, rmdir
+from conan.tools.files import get, apply_conandata_patches, rmdir
 from conan.tools.gnu import Autotools
 from conan.tools.microsoft import unix_path
 from conan.tools.layout import basic_layout
@@ -18,7 +18,7 @@ class AutoconfConan(ConanFile):
     topics = ("autoconf", "configure", "build")
     license = ("GPL-2.0-or-later", "GPL-3.0-or-later")
     settings = "os", "arch", "compiler", "build_type"
-    generators = "AutotoolsDeps", "AutotoolsToolchain"
+    generators = "AutotoolsDeps", "AutotoolsToolchain", "VirtualBuildEnv"
     win_bash = True
 
     exports_sources = "patches/*"
@@ -27,17 +27,25 @@ class AutoconfConan(ConanFile):
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
+    @property
+    def _datarootdir(self):
+        return path.join(self.package_folder, "bin", "share")
+
+    @property
+    def _autoconf_datarootdir(self):
+        return path.join(self._datarootdir, "autoconf")
+
     def package_id(self):
         self.info.clear()
 
-    def build_requirements(self):
-        if hasattr(self, "settings_build"):
-            self.build_requires("m4/1.4.19")
-        if self._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
-            self.tool_requires("msys2/cci.latest")
-
     def requirements(self):
         self.requires("m4/1.4.19")
+
+    def build_requirements(self):
+        if hasattr(self, "settings_build"):
+            self.tool_requires("m4/1.4.19")
+        if self._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
+            self.tool_requires("msys2/cci.latest")
 
     def layout(self):
         basic_layout(self, src_folder="source")
@@ -45,59 +53,40 @@ class AutoconfConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def build(self):
-        apply_conandata_patches(self)
+    @lru_cache(1)
+    def _autotools(self):
         autotool = Autotools(self)
         autotool.configure()
         autotool.make()
-        autotool.install(args=[f"DESTDIR={unix_path(self, self.package_folder)}"])
+        return autotool
+
+    def build(self):
+        apply_conandata_patches(self)
+        self._autotools()
 
     def package(self):
-        packager = AutoPackager(self)
-        packager.run()
-        rmdir(self, Path(self.package_folder, "share", "info"))
-        rmdir(self, Path(self.package_folder, "share", "man"))
+        autotools = self._autotools()
+        # KB-H013 we're packaging an application, place everything under bin
+        autotools.install(args=[f"DESTDIR={unix_path(self, path.join(self.package_folder, 'bin'))}"])
+        rmdir(self, path.join(self._datarootdir, "info"))
+        rmdir(self, path.join(self._datarootdir, "man"))
 
-    @property
-    def _datarootdir(self):
-        return Path(self.package_folder, "share")
-
-    @property
-    def _autoconf_datarootdir(self):
-        return Path(self._datarootdir, "autoconf")
+    def _set_env(self, var_name, var_path):
+        self.output.info(f"Setting {var_name} to {var_path}")
+        self.buildenv_info.define_path(var_name, var_path)
+        setattr(self.env_info, var_name, unix_path(self, var_path))
 
     def package_info(self):
-        bin_path = Path(self.package_folder, "bin")
-        self.output.info(f"Appending PATH env var with : {bin_path}")
-        self.buildenv_info.prepend_path("PATH", str(bin_path))
-        self.env_info.PATH.append(str(bin_path))
+        # KB-H013 we're packaging an application, hence the nested bin
+        bin_dir = path.join(self.package_folder, "bin", "bin")
+        self.output.info(f"Appending PATH env var with : {bin_dir}")
+        self.buildenv_info.prepend_path("PATH", bin_dir)
+        self.env_info.PATH.append(unix_path(self, bin_dir))
 
-        ac_macrodir = self._autoconf_datarootdir
-        self.output.info(f"Setting AC_MACRODIR to {ac_macrodir}")
-        self.buildenv_info.define_path("AC_MACRODIR", str(ac_macrodir))
-        self.env_info.AC_MACRODIR = unix_path(self, str(ac_macrodir))
-
-        autoconf = Path(self.package_folder, "bin", "autoconf")
-        self.output.info(f"Setting AUTOCONF to {autoconf}")
-        self.buildenv_info.define_path("AUTOCONF", str(autoconf))
-        self.env_info.AUTOCONF = unix_path(self, str(autoconf))
-
-        autoreconf = Path(self.package_folder, "bin", "autoreconf")
-        self.output.info(f"Setting AUTORECONF to {autoreconf}")
-        self.buildenv_info.define_path("AUTORECONF", str(autoreconf))
-        self.env_info.AUTORECONF = unix_path(self, str(autoreconf))
-
-        autoheader = Path(self.package_folder, "bin", "autoheader")
-        self.output.info(f"Setting AUTOHEADER to {autoheader}")
-        self.buildenv_info.define_path("AUTOHEADER", str(autoheader))
-        self.env_info.AUTOHEADER = unix_path(self, str(autoheader))
-
-        autom4te = Path(self.package_folder, "bin", "autom4te")
-        self.output.info(f"Setting AUTOM4TE to {autom4te}")
-        self.buildenv_info.define_path("AUTOM4TE", str(autom4te))
-        self.env_info.AUTOM4TE = unix_path(self, str(autom4te))
-
-        autom4te_perllibdir = self._autoconf_datarootdir
-        self.output.info(f"Setting AUTOM4TE_PERLLIBDIR to {autom4te_perllibdir}")
-        self.buildenv_info.define_path("AUTOM4TE_PERLLIBDIR", str(autom4te_perllibdir))
-        self.env_info.AUTOM4TE_PERLLIBDIR = unix_path(self, str(autom4te_perllibdir))
+        for var in [("AC_MACRODIR", self._autoconf_datarootdir),
+                    ("AUTOCONF", path.join(bin_dir, "autoconf")),
+                    ("AUTORECONF", path.join(bin_dir, "autoreconf")),
+                    ("AUTOHEADER", path.join(bin_dir, "autoheader")),
+                    ("AUTOM4TE", path.join(bin_dir, "autom4te")),
+                    ("AUTOM4TE_PERLLIBDIR", self._autoconf_datarootdir)]:
+            self._set_env(*var)
