22cb8f332601d85b306123c9624a6374e908f327
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-02 13:23:26 +0200

    conan v2 support

--- recipes/argh/all/conanfile.py
+++ recipes/argh/all/conanfile.py
@@ -1,35 +1,45 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
 import os
+
+required_conan_version = ">=1.50.0"
 
 
 class ArgparseConan(ConanFile):
     name = "argh"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/adishavit/argh"
-    topics = ("conan", "argh", "argument", "parsing")
+    topics = ("argh", "argument", "parsing")
     license = "BSD-3"
     description = "Frustration-free command line processing"
-    settings = "compiler"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return os.path.join(self.source_folder, "source_subfolder")
+    def package_id(self):
+        self.info.clear()
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, 11)
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 11)
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("argh-{}".format(self.version), self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def build(self):
+        pass
 
     def package(self):
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
-        self.copy("argh.h", src=self._source_subfolder, dst=os.path.join("include", "argh"))
-
-    def package_id(self):
-        self.info.header_only()
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "argh.h", src=self.source_folder, dst=os.path.join(self.package_folder, "include"))
 
     def package_info(self):
-        self.cpp_info.includedirs.append(os.path.join("include", "argh"))
+        self.cpp_info.bindirs = []
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
