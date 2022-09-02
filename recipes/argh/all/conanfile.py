9ee892bf83dd8d4506635443945e3b8e3c0f1960
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-02 13:26:11 +0200

    fix CMake imported target
    upstream exports argh target without namespace

--- recipes/argh/all/conanfile.py
+++ recipes/argh/all/conanfile.py
@@ -1,8 +1,9 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import copy, get
+from conan.tools.files import copy, get, save
 from conan.tools.layout import basic_layout
 import os
+import textwrap
 
 required_conan_version = ">=1.50.0"
 
@@ -38,8 +39,35 @@ class ArgparseConan(ConanFile):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         copy(self, "argh.h", src=self.source_folder, dst=os.path.join(self.package_folder, "include"))
 
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"argh": "argh::argh"},
+        )
+
+    def _create_cmake_module_alias_targets(self, module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent(f"""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """)
+        save(self, module_file, content)
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join("lib", "cmake", f"conan-official-{self.name}-targets.cmake")
+
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "argh")
+        self.cpp_info.set_property("cmake_target_name", "argh")
         self.cpp_info.bindirs = []
         self.cpp_info.frameworkdirs = []
         self.cpp_info.libdirs = []
         self.cpp_info.resdirs = []
+
+        # TODO: to remove in conan v2 once legacy generators removed
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
