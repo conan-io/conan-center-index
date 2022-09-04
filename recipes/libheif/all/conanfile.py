cc150607f06bdc7a15b2409360062ad6801d31d8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-04 12:17:02 +0200

    conan v2 support

--- recipes/libheif/all/conanfile.py
+++ recipes/libheif/all/conanfile.py
@@ -1,10 +1,15 @@
-from conans import ConanFile, tools, CMake
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, get, rmdir
+from conan.tools.scm import Version
+from conans import tools as tools_legacy
 import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.50.0"
 
 
-class Libheif(ConanFile):
+class LibheifConan(ConanFile):
     name = "libheif"
     description = "libheif is an HEIF and AVIF file format decoder and encoder."
     topics = ("libheif", "heif", "codec", "video")
@@ -28,22 +33,14 @@ class Libheif(ConanFile):
         "with_libaomav1": False,
     }
 
-    generators = "cmake"
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        if tools.Version(self.version) < "1.11.0":
+        if Version(self.version) < "1.11.0":
             # dav1d supported since 1.10.0 but usable option `WITH_DAV1D`
             # for controlling support exists only since 1.11.0
             del self.options.with_dav1d
@@ -62,53 +59,45 @@ class Libheif(ConanFile):
             self.requires("dav1d/0.9.1")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 11)
+        if self.info.settings.compiler.cppstd:
+            check_min_cppstd(self, 11)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
 
-    def _patch_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["WITH_EXAMPLES"] = False
-        self._cmake.definitions["WITH_RAV1E"] = False
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["WITH_EXAMPLES"] = False
+        tc.variables["WITH_RAV1E"] = False
         # x265
-        self._cmake.definitions["CMAKE_DISABLE_FIND_PACKAGE_X265"] = not self.options.with_x265
-        self._cmake.definitions["WITH_X265"] = self.options.with_x265
+        tc.variables["CMAKE_DISABLE_FIND_PACKAGE_X265"] = not self.options.with_x265
+        tc.variables["WITH_X265"] = self.options.with_x265
         # aom
-        self._cmake.definitions["CMAKE_DISABLE_FIND_PACKAGE_LibAOM"] = not self.options.with_libaomav1
-        self._cmake.definitions["WITH_AOM"] = self.options.with_libaomav1
+        tc.variables["CMAKE_DISABLE_FIND_PACKAGE_LibAOM"] = not self.options.with_libaomav1
+        tc.variables["WITH_AOM"] = self.options.with_libaomav1
         # dav1d
-        self._cmake.definitions["WITH_DAV1D"] = self.options.get_safe("with_dav1d", False)
-
-        # Workaround for cross-build to at least iOS/tvOS/watchOS,
-        # when dependencies are found with find_path() and find_library()
-        # TODO: won't be necessary with CMakeToolchain (https://github.com/conan-io/conan/pull/10186)
-        if tools.cross_building(self):
-            self._cmake.definitions["CMAKE_FIND_ROOT_PATH_MODE_INCLUDE"] = "BOTH"
-            self._cmake.definitions["CMAKE_FIND_ROOT_PATH_MODE_LIBRARY"] = "BOTH"
-
-        self._cmake.configure()
-        return self._cmake
+        tc.variables["WITH_DAV1D"] = self.options.get_safe("with_dav1d", False)
+        if Version(self.version) < "1.11.0":
+            # Honor BUILD_SHARED_LIBS from conan_toolchain (see https://github.com/conan-io/conan/issues/11840)
+            tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+        tc.generate()
 
     def build(self):
-        self._patch_sources()
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "libheif")
@@ -120,8 +109,10 @@ class Libheif(ConanFile):
             self.cpp_info.components["heif"].defines = ["LIBHEIF_STATIC_BUILD"]
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["heif"].system_libs.extend(["m", "pthread"])
-        if not self.options.shared and tools.stdcpp_library(self):
-            self.cpp_info.components["heif"].system_libs.append(tools.stdcpp_library(self))
+        if not self.options.shared:
+            libcxx = tools_legacy.stdcpp_library(self)
+            if libcxx:
+                self.cpp_info.components["heif"].system_libs.append(libcxx)
 
         # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.components["heif"].set_property("cmake_target_name", "libheif::heif")
