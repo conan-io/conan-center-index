c6872e05adffefea3435e0bb71fb9904cc1e6042
Sergey Bobrenok <bobrofon@gmail.com>
2022-09-05 11:24:52 +0300

    (#12700) sdbus-cpp: conan v2 support
    * sdbus-cpp/all: Update requirements/build_requirements
    
    * sdbus-cpp/all: Fix TOOLS CROSS BUILDING (KB-H062) warning
    
    WARN: [TOOLS CROSS BUILDING (KB-H062)] The
    'tools.cross_building(self.settings)' syntax may not work correctly in
    some scenarios. Consider using tools.cross_building(self).
    
    * sdbus-cpp/all: Fix INVALID TOPICS (KB-H064) warning
    
    WARN: [INVALID TOPICS (KB-H064)] The topic 'conan' is invalid and
    should be removed from topics attribute.
    
    * sdbus-cpp/all: Add pkg-config test
    
    sdbus-cpp officially supports pkg-config, so we need to test it.
    
    * sdbus-cpp/all: Add conan v2 support
    
    * sdbus-cpp/all: Make a little cleanup and apply CCI recommendations
    
    * sdbus-cpp/all: Add missing system libs
    
    The math library was probably just missing. And the pthread library
    was always implicitly inherited from libsystemd dependency.
    
    * sdbus-cpp/all: Add information about LGPL exceptions for header files
    
    Since version 0.9.0, sdbus-c++ has an additional license with an
    exception for the template code in the header files. Basically it is a
    fork of Qt LGPL exception 1.1.
    
    * sdbus-cpp/all: Apply suggestions from code review
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * sdbus-cpp/all: Apply test packages review suggestions
    
    * sdbus-cpp/all: Use conan.tools in test packages
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/sdbus-cpp/all/test_pkgconf/conanfile.py
+++ recipes/sdbus-cpp/all/test_pkgconf/conanfile.py
@@ -3,18 +3,27 @@ import os
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.files import copy
 
 
 class SdbusCppTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    generators = "CMakeToolchain", "PkgConfigDeps", "VirtualRunEnv"
     test_type = "explicit"
+
+    def build_requirements(self):
+        self.tool_requires("pkgconf/1.7.4")
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        # workaround for https://gitlab.kitware.com/cmake/cmake/-/issues/18150
+        copy(self, "*.pc", self.generators_folder,
+             os.path.join(self.generators_folder, "lib", "pkgconfig"))
 
     def build(self):
         cmake = CMake(self)
