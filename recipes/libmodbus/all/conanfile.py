0fa4e48cb668a1f3c22aed3ba6be5a7fb2874c68
Manuel Rota <manuel.rota@protonmail.ch>
2022-09-05 16:58:17 +0200

    modernize libmodbus for conanv2

--- recipes/libmodbus/all/conanfile.py
+++ recipes/libmodbus/all/conanfile.py
@@ -1,15 +1,22 @@
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
-from contextlib import contextmanager
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import apply_conandata_patches, copy, get, patch, replace_in_file, rename, rm, rmdir
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps, PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import unix_path, VCVars
+from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.33.0"
+
+required_conan_version = ">=1.51.3"
 
 
 class LibmodbusConan(ConanFile):
     name = "libmodbus"
     description = "libmodbus is a free software library to send/receive data according to the Modbus protocol"
     homepage = "https://libmodbus.org/"
-    topics = ("conan", "libmodbus", "modbus", "protocol", "industry", "automation")
+    topics = ("libmodbus", "modbus", "protocol", "industry", "automation")
     license = "LGPL-2.1"
     url = "https://github.com/conan-io/conan-center-index"
     exports_sources = "patches/**"
@@ -23,101 +30,115 @@ class LibmodbusConan(ConanFile):
         "fPIC": True,
     }
 
-    _autotools = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _settings_build(self):
-        return self.settings_build if hasattr(self, "settings_build") else self.settings
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            try:
+                del self.options.fPIC # once removed by config_options, need try..except for a second del
+            except Exception:
+                pass
+        try:
+            del self.settings.compiler.libcxx # for plain C projects only
+        except Exception:
+            pass
+        try:
+            del self.settings.compiler.cppstd # for plain C projects only
+        except Exception:
+            pass
 
     def build_requirements(self):
         if self.settings.compiler == "Visual Studio":
-            self.build_requires("automake/1.16.3")
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/cci.latest")
+            self.tool_requires("automake/1.16.3")
+        # see https://github.com/conan-io/conan/issues/11969
+        bash_path = os.getenv("CONAN_BASH_PATH") or self.conf.get("tools.microsoft.bash:path")
+        if self.settings.os == "Windows" and not conan_bash_path:
+            self.tool_requires("msys2/cci.latest")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+                destination=self.source_folder, strip_root=True)
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        conf_args = [
-            "--disable-tests",
-            "--without-documentation",
-            "--prefix={}".format(tools.unix_path(self.package_folder)),
-        ]
-        if self.options.shared:
-            conf_args.extend(["--enable-shared", "--disable-static"])
-        else:
-            conf_args.extend(["--enable-static", "--disable-shared"])
-        if self.settings.compiler == "Visual Studio" and tools.Version(self.settings.compiler.version) >= "12":
+    def validate(self):
+        # validate the minimum cpp standard supported
+        if self.info.settings.compiler.cppstd:
+            check_min_cppstd(self, 11)
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def generate(self):
+        # --fpic is automatically managed when 'fPIC' option is declared
+        # --enable/disable-shared is automatically managed when 'shared' option is declared
+        tc = AutotoolsToolchain(self)
+        tc.configure_args.append("--without-documentation")
+        tc.configure_args.append("--disable-tests")
+        # TODO: how to port this?
+        """
+        if self.settings.compiler == "Visual Studio" and Version(self.settings.compiler.version) >= "12":
             if self.settings.build_type in ("Debug", "RelWithDebInfo"):
-                self._autotools.flags.append("-FS")
-        self._autotools.configure(args=conf_args, configure_dir=self._source_subfolder)
-        return self._autotools
+                autotools.flags.append("-FS")
+        """
+        tc.generate()
+        # generate dependencies for pkg-config
+        tc = PkgConfigDeps(self)
+        tc.generate()
+        # generate dependencies for autotools
+        tc = AutotoolsDeps(self)
+        tc.generate()
 
-    @contextmanager
-    def _build_context(self):
+        # inject tools_require env vars in build context
+        ms = VirtualBuildEnv(self)
+        ms.generate()
+
         if self.settings.compiler == "Visual Studio":
-            with tools.vcvars(self.settings):
-                env = {
-                    "CC": "cl -nologo",
-                    "CXX": "cl -nologo",
-                    "LD": "link -nologo",
-                    "AR": "{} \"lib -nologo -verbose\"".format(tools.unix_path(self.deps_user_info["automake"].ar_lib)),
-                    "RANLIB": ":",
-                    "STRING": ":",
-                    "NM": "dumpbin -symbols"}
-                with tools.environment_append(env):
-                    yield
-        else:
-            yield
+            ms = VCVars(self)
+            ms.define("CC", "cl -nologo")
+            ms.define("CXX", "cl -nologo")
+            ms.define("LD", "link -nologo")
+            ms.define("AR", "{} \"lib -nologo -verbose\"".format(unix_path(self.deps_user_info["automake"].ar_lib)))
+            ms.define("RANLIB", ":")
+            ms.define("STRING", ":")
+            ms.define("NM", "dumpbin -symbols")
+            ms.generate()
 
     def _patch_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        apply_conandata_patches(self)
         if not self.options.shared:
             for decl in ("__declspec(dllexport)", "__declspec(dllimport)"):
-                tools.replace_in_file(os.path.join(self._source_subfolder, "src", "modbus.h"), decl, "")
+                replace_in_file(self, os.path.join(self.source_folder, "src", "modbus.h"), decl, "")
 
     def build(self):
         self._patch_sources()
-        with self._build_context():
-            autotools = self._configure_autotools()
-            autotools.make()
+        autotools = Autotools(self)
+         # run autoreconf to generate configure file
+        autotools.autoreconf()
+        # ./configure + toolchain file
+        autotools.configure()
+        autotools.make()
 
     def package(self):
-        self.copy("COPYING*", src=self._source_subfolder, dst="licenses")
-        with self._build_context():
-            autotools = self._configure_autotools()
-            autotools.install()
+        copy(self, pattern="COPYING*", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        autotools = Autotools(self)
+        autotools.install()
 
-        os.unlink(os.path.join(self.package_folder, "lib", "libmodbus.la"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
         if self.settings.compiler == "Visual Studio" and self.options.shared:
-            tools.rename(os.path.join(self.package_folder, "lib", "modbus.dll.lib"),
-                         os.path.join(self.package_folder, "lib", "modbus.lib"))
+            rename(self,
+                    os.path.join(self.package_folder, "lib", "modbus.dll.lib"),
+                    os.path.join(self.package_folder, "lib", "modbus.lib"))
 
     def package_info(self):
-        self.cpp_info.names["pkg_config"] = "libmodbus"
-        self.cpp_info.includedirs.append(os.path.join("include", "modbus"))
         self.cpp_info.libs = ["modbus"]
+
+        # should this be kept for v1?
+        # self.cpp_info.names["pkg_config"] = "libmodbus"
+        self.cpp_info.set_property("pkg_config_name", "libmodbus")
+
+        # self.cpp_info.includedirs.append(os.path.join("include", "modbus"))
         if self.settings.os == "Windows" and not self.options.shared:
             self.cpp_info.system_libs = ["wsock32"]
