b7785719c05dd1f904d26b4e27e89a2c5cc215f5
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-05 22:40:27 +0200

    conan v2 support

--- recipes/mariadb-connector-c/all/test_package_pkgconfigdeps/conanfile.py
+++ recipes/mariadb-connector-c/all/test_package_pkgconfigdeps/conanfile.py
@@ -6,14 +6,24 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    generators = "CMakeToolchain", "PkgConfigDeps", "VirtualBuildEnv", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
+    def build_requirements(self):
+        self.tool_requires("pkgconf/1.7.4")
+
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        # workaround for https://github.com/conan-io/conan/issues/11962
+        from conan.tools.env import Environment
+        env = Environment()
+        env.prepend_path("PKG_CONFIG_PATH", self.generators_folder)
+        env.vars(self).save_script("conanbuildenv_pkg_config_path")
 
     def build(self):
         cmake = CMake(self)
