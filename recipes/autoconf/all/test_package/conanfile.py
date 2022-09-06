357c0785fbb574a883e990f0d6b3b5cff9de8698
j.spijker@ultimaker.com <jelle spijker>
2022-09-06 18:27:58 +0200

    use modern tools in test_package

--- recipes/autoconf/all/test_package/conanfile.py
+++ recipes/autoconf/all/test_package/conanfile.py
@@ -1,15 +1,18 @@
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
-from conan.tools.microsoft import is_msvc
-import contextlib
-import os
 import shutil
+from os import environ, path
 
-required_conan_version = ">=1.45.0"
+from conan import ConanFile
+from conan.tools.gnu import Autotools
+from conan.tools.build import cross_building
+
+required_conan_version = ">=1.50.0"
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     exports_sources = "configure.ac", "config.h.in", "Makefile.in", "test_package_c.c", "test_package_cpp.cpp",
+    generators = "AutotoolsDeps", "AutotoolsToolchain", "VirtualBuildEnv"
+    win_bash = True
     test_type = "explicit"
 
     @property
@@ -17,31 +20,21 @@ class TestPackageConan(ConanFile):
         return getattr(self, "settings_build", self.settings)
 
     def build_requirements(self):
-        self.build_requires(self.tested_reference_str)
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/cci.latest")
-
-    @contextlib.contextmanager
-    def _build_context(self):
-        if is_msvc(self):
-            with tools.vcvars(self):
-                with tools.environment_append({"CC": "cl -nologo", "CXX": "cl -nologo",}):
-                    yield
-        else:
-            yield
+        self.tool_requires(self.tested_reference_str)
+        if self._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
+            self.tool_requires("msys2/cci.latest")
 
     def build(self):
         for src in self.exports_sources:
-            shutil.copy(os.path.join(self.source_folder, src), self.build_folder)
-        self.run("{} --verbose".format(os.environ["AUTOCONF"]),
-                 win_bash=tools.os_info.is_windows, run_environment=True)
-        self.run("{} --help".format(os.path.join(self.build_folder, "configure").replace("\\", "/")),
-                 win_bash=tools.os_info.is_windows, run_environment=True)
-        autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        with self._build_context():
-            autotools.configure()
-            autotools.make()
+            shutil.copy(path.join(self.source_folder, src), self.build_folder)
+
+        self.run("autoconf --verbose", run_environment=True, win_bash=self.settings.os == "Windows")
+        self.run("{} --help".format(path.join(self.build_folder, "configure").replace("\\", "/")), run_environment=True, win_bash=self.settings.os == "Windows")
+
+        autotools = Autotools(self)
+        autotools.configure()
+        autotools.make()
 
     def test(self):
-        if not tools.cross_building(self):
-            self.run(os.path.join(".", "test_package"), run_environment=True)
+        if not cross_building(self):
+            self.run(path.join(".", "test_package"), run_environment=True, win_bash=self.settings.os == "Windows")
