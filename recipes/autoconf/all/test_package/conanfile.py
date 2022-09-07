75e7579e81b08762fb062a76ecdf25379f87a891
Jelle Spijker <j.spijker@ultimaker.com>
2022-09-07 10:09:33 +0200

    Ducktype Autotools `configure` and `make`
    This allows us to use the msys2 bash without using the conf_info
    in the test_package. Which is currently problematic due to:
    conan-io/conan#11975
    
    Contributes to CURA-9574

--- recipes/autoconf/all/test_package/conanfile.py
+++ recipes/autoconf/all/test_package/conanfile.py
@@ -1,9 +1,52 @@
 import shutil
+
 from os import environ, path
 
 from conan import ConanFile
 from conan.tools.gnu import Autotools
 from conan.tools.build import cross_building
+
+
+def win_bash_configure(self, build_script_folder=None, args=None):
+    # Workaround for conan-io/conan#11975
+    # Duck typing configure and make methods, to skip calling the `subsystem` conf
+    from conans.tools import args_to_string
+    from conans.client.subsystems import subsystem_path
+    script_folder = path.join(self._conanfile.source_folder, build_script_folder) \
+        if build_script_folder else self._conanfile.source_folder
+
+    configure_args = []
+    configure_args.extend(args or [])
+
+    self._configure_args = "{} {}".format(self._configure_args, args_to_string(configure_args))
+
+    configure_cmd = "{}/configure".format(script_folder)
+    if self._conanfile._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
+        subsystem = "msys"
+    else:
+        subsystem = None
+    configure_cmd = subsystem_path(subsystem, configure_cmd)
+    cmd = '"{}" {}'.format(configure_cmd, self._configure_args)
+    self._conanfile.output.info("Calling:\n > %s" % cmd)
+    self._conanfile.run('"{}" {}'.format(subsystem_path(subsystem, path.join(self._conanfile.build_folder, "configure")), self._configure_args), run_environment=True, win_bash=self._conanfile._settings_build.os == "Windows")
+
+
+def win_bash_make(self, target=None, args=None):
+    # Workaround for conan-io/conan#11975
+    # Duck typing configure and make methods, to skip calling the `subsystem` conf
+    from conan.tools.build import build_jobs
+    make_program = self._conanfile.conf.get("tools.gnu:make_program",
+                                            default="mingw32-make" if self._use_win_mingw() else "make")
+    str_args = self._make_args
+    str_extra_args = " ".join(args) if args is not None else ""
+    jobs = ""
+    if "-j" not in str_args and "nmake" not in make_program.lower():
+        njobs = build_jobs(self._conanfile)
+        if njobs:
+            jobs = "-j{}".format(njobs)
+    command = " ".join(filter(None, [make_program, target, str_args, str_extra_args, jobs]))
+    self._conanfile.run(command, run_environment=True, win_bash=self._conanfile._settings_build.os == "Windows")
+
 
 required_conan_version = ">=1.50.0"
 
@@ -23,18 +66,23 @@ class TestPackageConan(ConanFile):
         self.tool_requires(self.tested_reference_str)
         if self._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
             self.tool_requires("msys2/cci.latest")
+            # Workaround for conan-io/conan#11975
+            # Duck typing configure and make methods, to skip calling the `subsystem` conf
+            Autotools.configure = win_bash_configure
+            Autotools.make = win_bash_make
 
     def build(self):
         for src in self.exports_sources:
             shutil.copy(path.join(self.source_folder, src), self.build_folder)
 
-        self.run("autoconf --verbose", run_environment=True, win_bash=self.settings.os == "Windows")
-        self.run("{} --help".format(path.join(self.build_folder, "configure").replace("\\", "/")), run_environment=True, win_bash=self.settings.os == "Windows")
+        self.run("autoconf --verbose", run_environment=True, win_bash=self._settings_build.os == "Windows")
 
+        # Workaround for conan-io/conan#11975
         autotools = Autotools(self)
         autotools.configure()
         autotools.make()
 
     def test(self):
         if not cross_building(self):
-            self.run(path.join(".", "test_package"), run_environment=True, win_bash=self.settings.os == "Windows")
+            ext = ".exe" if self.settings.os == "Windows" else ""
+            self.run(path.join(self.build_folder, f"test_package{ext}"),  run_environment=True, win_bash=self.settings.os == "Windows")
