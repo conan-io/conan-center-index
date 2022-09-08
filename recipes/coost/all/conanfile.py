a047b72249ca943e3d08e69f41b016c8e438e8c1
Alvin <idealvin@qq.com>
2022-09-08 21:58:52 +0800

    Update recipes/coost/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/coost/all/conanfile.py
+++ recipes/coost/all/conanfile.py
@@ -98,6 +98,8 @@ class CoostConan(ConanFile):
         self.cpp_info.components["co"].libs = ["co"]
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["co"].system_libs = ["pthread", "dl"]
+        elif self.settings.os == "Windows":
+            self.cpp_info.components["co"].system_libs = ["ws2_32"]
 
         # TODO: to remove in conan v2 once legacy generators removed
         self.cpp_info.components["co"].set_property("cmake_target_name", "coost::co")
