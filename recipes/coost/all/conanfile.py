c9494054e55335c3f59c6bd95efca0328a0884b1
Alvin <idealvin@qq.com>
2022-09-08 16:54:48 +0800

    Update recipes/coost/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/coost/all/conanfile.py
+++ recipes/coost/all/conanfile.py
@@ -94,6 +94,8 @@ class CoostConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "coost::co")
         # TODO: back to global scope in conan v2 once legacy generators removed
         self.cpp_info.components["co"].libs = ["co"]
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.components["co"].system_libs = ["pthread", "dl"]
 
         # TODO: to remove in conan v2 once legacy generators removed
         self.cpp_info.components["co"].set_property("cmake_target_name", "coost::co")
