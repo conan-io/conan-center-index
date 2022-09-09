0366be5a84ceb0c88d99b803942fbee736781f1b
toge <toge.mail@gmail.com>
2022-09-09 21:45:35 +0900

    don't use tools.cross_building
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/imutils-cpp/all/test_v1_package/conanfile.py
+++ recipes/imutils-cpp/all/test_v1_package/conanfile.py
@@ -13,6 +13,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
