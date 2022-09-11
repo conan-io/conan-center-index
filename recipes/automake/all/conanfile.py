a973261f95c586379c4064d990745fe47b00751c
jelle Spijker <j.spijker@ultimaker.com>
2022-09-11 19:07:14 +0200

    Cleaned up code
    Contributes to CURA-8831

--- recipes/automake/all/conanfile.py
+++ recipes/automake/all/conanfile.py
@@ -1,223 +1,174 @@
-from functools import lru_cache
-from os import environ, path, listdir
-
 from conan import ConanFile
-from conan.errors import ConanException
-from conan.tools.build import build_jobs
-from conan.tools.files import get, apply_conandata_patches, rmdir, copy, replace_in_file, rename
-from conan.tools.gnu import Autotools
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import apply_conandata_patches, copy, get, replace_in_file, rmdir
+from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import unix_path
+from conan.tools.microsoft import is_msvc, unix_path
 from conan.tools.scm import Version
-from conans.client import subsystems
-from conans.client.build import join_arguments
-from conans.client.subsystems import subsystem_path, deduce_subsystem, MSYS2, _escape_windows_cmd
-from conans.tools import args_to_string
 
-required_conan_version = ">=1.50.0"
-
-
-# Duck typing the subsystems._windows_bash_wrapper method, so it wil use the correct path for activation of the environments in the bash subsystem
-# issue conan-io/conan#11980
-def _windows_bash_wrapper_fix(conanfile, command, env, envfiles_folder):
-    from conan.tools.env import Environment
-    from conan.tools.env.environment import environment_wrap_command
-    """ Will wrap a unix command inside a bash terminal It requires to have MSYS2, CYGWIN, or WSL"""
-
-    subsystem = conanfile.conf.get("tools.microsoft.bash:subsystem")
-    shell_path = conanfile.conf.get("tools.microsoft.bash:path")
-    if not subsystem or not shell_path:
-        raise ConanException("The config 'tools.microsoft.bash:subsystem' and "
-                             "'tools.microsoft.bash:path' are "
-                             "needed to run commands in a Windows subsystem")
-    env = env or []
-    if subsystem == MSYS2:
-        # Configure MSYS2 to inherith the PATH
-        msys2_mode_env = Environment()
-        _msystem = {"x86": "MINGW32"}.get(conanfile.settings.get_safe("arch"), "MINGW64")
-        msys2_mode_env.define("MSYSTEM", _msystem)
-        msys2_mode_env.define("MSYS2_PATH_TYPE", "inherit")
-        msys2_mode_bat = path.join(conanfile.generators_folder, "msys2_mode.bat")
-        msys2_mode_env.vars(conanfile, "build").save_bat(msys2_mode_bat)
-        env.append(msys2_mode_bat)
-
-    wrapped_shell = '"%s"' % shell_path if " " in shell_path else shell_path
-    wrapped_shell = environment_wrap_command(env, envfiles_folder, wrapped_shell,
-                                             accepted_extensions=("bat", "ps1"))
-
-    # Wrapping the inside_command enable to prioritize our environment, otherwise /usr/bin go
-    # first and there could be commands that we want to skip
-    wrapped_user_cmd = environment_wrap_command(env, envfiles_folder, command,
-                                                subsystem=subsystem,  # Fix from main branch, see issue conan-io/conan#11980
-                                                accepted_extensions=("sh",))
-    wrapped_user_cmd = _escape_windows_cmd(wrapped_user_cmd)
-
-    final_command = '{} -c {}'.format(wrapped_shell, wrapped_user_cmd)
-    return final_command
-
-
-subsystems._windows_bash_wrapper = _windows_bash_wrapper_fix
-
-
-# Fixing Autotools on Windows with a subsystem
-class AutotoolsWinBash(Autotools):
-
-    def configure(self, build_script_folder=None, args=None):
-        """
-        http://jingfenghanmax.blogspot.com.es/2010/09/configure-with-host-target-and-build.html
-        https://gcc.gnu.org/onlinedocs/gccint/Configure-Terms.html
-        """
-        script_folder = path.join(self._conanfile.source_folder, build_script_folder) \
-            if build_script_folder else self._conanfile.source_folder
-
-        configure_args = []
-        configure_args.extend(args or [])
-
-        self._configure_args = "{} {}".format(self._configure_args, args_to_string(configure_args))
-
-        configure_cmd = "{}/configure".format(script_folder)
-        subsystem = deduce_subsystem(self._conanfile, scope="build")
-        configure_cmd = subsystem_path(subsystem, configure_cmd)
-        cmd = '"{}" {}'.format(configure_cmd, self._configure_args)
-        self._conanfile.output.info("Calling:\n > %s" % cmd)
-        self._conanfile.run(cmd, run_environment=True)
-
-    def make(self, target=None, args=None):
-        make_program = self._conanfile.conf.get("tools.gnu:make_program",
-                                                default="mingw32-make" if self._use_win_mingw() else "make")
-        str_args = self._make_args
-        str_extra_args = " ".join(args) if args is not None else ""
-        jobs = ""
-        if "-j" not in str_args and "nmake" not in make_program.lower():
-            njobs = build_jobs(self._conanfile)
-            if njobs:
-                jobs = "-j{}".format(njobs)
-        command = join_arguments([make_program, target, str_args, str_extra_args, jobs])
-        self._conanfile.run(command, run_environment=True)
+required_conan_version = ">=1.51.3"
 
 
 class AutomakeConan(ConanFile):
     name = "automake"
+    description = "Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards."
+    license = ("GPL-2.0-or-later", "GPL-3.0-or-later")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.gnu.org/software/automake/"
-    description = "Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards."
     topics = ("conan", "automake", "configure", "build")
-    license = ("GPL-2.0-or-later", "GPL-3.0-or-later")
     settings = "os", "arch", "compiler", "build_type"
-    generators = "AutotoolsDeps", "AutotoolsToolchain", "VirtualBuildEnv"
-    win_bash = True
-
-    exports_sources = "patches/*"
 
     @property
     def _settings_build(self):
+        # TODO: Remove for Conan v2
         return getattr(self, "settings_build", self.settings)
 
+    @property
+    def win_bash(self):
+        return self._settings_build.os == "Windows"
+
+    def export_sources(self):
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
+
     def configure(self):
-        del self.settings.compiler.cppstd
-        del self.settings.compiler.libcxx
+        try:
+            del self.settings.compiler.libcxx  # for plain C projects only
+        except ValueError:
+            pass
+        try:
+            del self.settings.compiler.cppstd  # for plain C projects only
+        except ValueError:
+            pass
+
+    def layout(self):
+        basic_layout(self, src_folder="automake")
 
     def requirements(self):
         self.requires("autoconf/2.71")
         self.requires("m4/1.4.19")
 
+    def package_id(self):
+        try:
+            del self.info.settings.arch
+        except ValueError:
+            pass
+        try:
+            del self.info.settings.compiler
+        except ValueError:
+            pass
+        try:
+            del self.info.settings.build_type
+        except ValueError:
+            pass
+
     def build_requirements(self):
-        if hasattr(self, "settings_build"):
-            self.tool_requires("autoconf/2.71")
-            self.tool_requires("m4/1.4.19")
-        if self._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
             self.tool_requires("msys2/cci.latest")
 
-    def package_id(self):
-        del self.info.settings.arch
-        del self.info.settings.compiler
-        del self.info.settings.build_type
-
-    def layout(self):
-        basic_layout(self, src_folder="source")
-        self.cpp.package.includedirs = []  # KB-H071: It is a tool that doesn't contain headers, removing the include directory.
-
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
 
-    @property
-    def _datarootdir(self):
-        return path.join(self.package_folder, "bin", "share")
+    def generate(self):
+        tc = AutotoolsToolchain(self)
+        tc.configure_args.extend([
+            "--datarootdir=${prefix}/res",
+        ])
 
-    @property
-    def _automake_libdir(self):
-        version = Version(self.version)
-        return path.join(self._datarootdir, f"automake-{version.major}.{version.minor}")
+        if is_msvc(self):
+            build = "{}-{}-{}".format(
+                "x86_64" if self._settings_build.arch == "x86_64" else "i686",
+                "pc" if self._settings_build.arch == "x86" else "win64",
+                "mingw32")
+            host = "{}-{}-{}".format(
+                "x86_64" if self.settings.arch == "x86_64" else "i686",
+                "pc" if self.settings.arch == "x86" else "win64",
+                "mingw32")
+            tc.configure_args.append(f"--build={build}")
+            tc.configure_args.append(f"--host={host}")
+        tc.generate()
 
-    @lru_cache(1)
-    def _autotools(self):
-        autotool = AutotoolsWinBash(self)
-        autotool.configure()
-        autotool.make()
-        return autotool
+        deps = AutotoolsDeps(self)
+        deps.generate()
+
+        ms = VirtualBuildEnv(self)
+        ms.generate(scope="build")
 
     def build(self):
         apply_conandata_patches(self)
         if self.settings.os == "Windows":
             # tracing using m4 on Windows returns Windows paths => use cygpath to convert to unix paths
-            replace_in_file(self, path.join(self.source_path, "bin", "aclocal.in"),
+            replace_in_file(self, self.source_path.joinpath("bin", "aclocal.in"),
                             "          $map_traced_defs{$arg1} = $file;",
                             "          $file = `cygpath -u $file`;\n"
                             "          $file =~ s/^\\s+|\\s+$//g;\n"
                             "          $map_traced_defs{$arg1} = $file;")
 
-        _ = self._autotools()
+        autotools = Autotools(self)
+        autotools.configure()
+        autotools.make()
 
     def package(self):
-        autotools = self._autotools()
-        # KB-H013 we're packaging an application, place everything under bin
-        autotools.install(args=[f"DESTDIR={unix_path(self, path.join(self.package_folder, 'bin'))}"])
+        autotools = Autotools(self)
+        autotools.install(args=[f"DESTDIR={unix_path(self, self.package_folder)}"])  # Need to specify the `DESTDIR` as a Unix path, aware of the subsystem
 
-        if self._settings_build.os == "Windows":
-            bin_dir = path.join(self.package_folder, "bin", "bin")
-            for filename in listdir(bin_dir):
-                fullpath = path.join(bin_dir, filename)
-                if not path.isfile(fullpath):
-                    continue
-                rename(self, fullpath, f"{fullpath}.exe")
-
-        copy(self, "COPYING*", src=self.source_folder, dst=path.join(self.package_folder, "licenses"))
-        rmdir(self, path.join(self._datarootdir, "info"))
-        rmdir(self, path.join(self._datarootdir, "man"))
-        rmdir(self, path.join(self._datarootdir, "doc"))
-
-    def _set_env(self, var_name, var_path):
-        self.output.info(f"Setting {var_name} to {var_path}")
-        self.buildenv_info.define_path(var_name, var_path)
-        self.runenv_info.define_path(var_name, var_path)
-        setattr(self.env_info, var_name, var_path)
+        copy(self, "COPYING*", src=self.source_folder, dst=self.package_path.joinpath("licenses"))
+        rmdir(self, self.package_path.joinpath("res", "info"))
+        rmdir(self, self.package_path.joinpath("res", "man"))
+        rmdir(self, self.package_path.joinpath("res", "doc"))
 
     def package_info(self):
-        # KB-H013 we're packaging an application, hence the nested bin
-        bin_dir = path.join(self.package_folder, "bin", "bin")
-        self.output.info(f"Appending PATH environment variable:: {bin_dir}")
-        self.buildenv_info.prepend_path("PATH", bin_dir)
-        self.runenv_info.prepend_path("PATH", bin_dir)
-        self.env_info.PATH.append(bin_dir)
+        self.cpp_info.libdirs = []
+        self.cpp_info.includedirs = []
 
-        bin_ext = ".exe" if self.settings.os == "Windows" else ""
-        for var in [("ACLOCAL", path.join(bin_dir, f"aclocal{bin_ext}")),
-                    ("AUTOMAKE_DATADIR", self._datarootdir),
-                    ("AUTOMAKE_LIBDIR", self._automake_libdir),
-                    ("AUTOMAKE_PERLLIBDIR", self._automake_libdir),
-                    ("AUTOMAKE", path.join(bin_dir, f"automake{bin_ext}"))]:
-            self._set_env(*var)
+        bin_path = self.package_path.joinpath("bin")
+        self.output.info(f"Appending PATH environment variable: {bin_path}")
+        self.env_info.PATH.append(str(bin_path))
 
-        compile_bin = path.join(self._automake_libdir, "compile")
+        dataroot_path = self.package_path.joinpath("res")
+        self.output.info(f"Defining AUTOMAKE_DATADIR environment variable: {dataroot_path}")
+        self.env_info.AUTOMAKE_DATADIR = str(dataroot_path)
+        self.buildenv_info.define_path("AUTOMAKE_DATADIR", str(dataroot_path))
+
+        version = Version(self.version)
+        automake_dataroot_path = dataroot_path.joinpath(f"automake-{version.major}.{version.minor}")
+        self.output.info(f"Defining AUTOMAKE_LIBDIR environment variable: {automake_dataroot_path}")
+        self.env_info.AUTOMAKE_LIBDIR = str(automake_dataroot_path)
+        self.buildenv_info.define_path("AUTOMAKE_LIBDIR", str(automake_dataroot_path))
+
+        self.output.info(f"Defining AUTOMAKE_PERLLIBDIR environment variable: {automake_dataroot_path}")
+        self.env_info.AUTOMAKE_PERLLIBDIR = str(automake_dataroot_path)
+        self.buildenv_info.define_path("AUTOMAKE_PERLLIBDIR", str(automake_dataroot_path))
+
+        aclocal_bin = bin_path.joinpath("aclocal")
+        self.output.info(f"Defining ACLOCAL environment variable: {aclocal_bin}")
+        self.env_info.ACLOCAL = str(aclocal_bin)
+        self.buildenv_info.define_path("ACLOCAL", str(aclocal_bin))
+
+        aclocal_bin_conf_key = "tools.automake:aclocal"
+        self.output.info(f"Defining path to aclocal binary in configuration as `{aclocal_bin_conf_key}` with value: {aclocal_bin}")
+        self.conf_info.define(aclocal_bin_conf_key, str(aclocal_bin))
+
+        automake_bin = bin_path.joinpath("automake")
+        self.output.info(f"Defining AUTOMAKE environment variable: {automake_bin}")
+        self.env_info.AUTOMAKE = str(automake_bin)
+        self.buildenv_info.define_path("AUTOMAKE", str(automake_bin))
+
+        automake_bin_conf_key = "tools.automake:automake"
+        self.output.info(f"Defining path to automake binary in configuration as `{automake_bin_conf_key}` with value: {automake_bin}")
+        self.conf_info.define(automake_bin_conf_key, str(automake_bin))
+
+        compile_bin = bin_path.joinpath("compile")
         self.output.info(f"Define path to `compile` binary in user_info as: {compile_bin}")
         self.user_info.compile = compile_bin
         compile_conf_key = "tools.automake:compile"
         self.output.info(f"Defining path to `compile` binary in configuration as `{compile_conf_key}` with value: {compile_bin}")
         self.conf_info.define(compile_conf_key, compile_bin)
 
-        ar_lib_bin = path.join(self._automake_libdir, "ar-lib")
-        self.output.info(f"Define path to `ar_lib` binary in user_info as: {ar_lib_bin}")
+        ar_lib_bin = bin_path.joinpath("ar-lib")
+        self.output.info(f"Define path to ar_lib binary in user_info as: {ar_lib_bin}")
         self.user_info.ar_lib = ar_lib_bin
         ar_lib_conf_key = "tools.automake:ar-lib"
-        self.output.info(f"Defining path to `ar-lib` binary in configuration as `{ar_lib_conf_key}` with value: {ar_lib_bin}")
+        self.output.info(f"Defining path to ar-lib binary in configuration as `{ar_lib_conf_key}` with value: {ar_lib_bin}")
         self.conf_info.define(ar_lib_conf_key, ar_lib_bin)
--- recipes/automake/all/test_package/conanfile.py
+++ recipes/automake/all/test_package/conanfile.py
@@ -1,81 +1,10 @@
 import shutil
-from os import environ, path
 
 from conan import ConanFile
-from conan.tools.build import cross_building
-from conan.tools.files import chdir
+from conan.tools.build import can_run
 from conan.tools.gnu import Autotools
-from conan.tools.microsoft import is_msvc
-from conans.client.build import join_arguments
-from conans.client.subsystems import subsystem_path
-from conans.tools import vcvars, no_op, args_to_string
-
-
-def _subsystem(conanfile):
-    if conanfile._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
-        return "msys2"
-    return None
-
-
-def _env_activate_sh_path(conanfile):
-    return subsystem_path(_subsystem(conanfile), path.join(conanfile.build_folder, "conanbuild.sh"))
-
-
-class AutotoolsWinBash(Autotools):
-    def configure(self, build_script_folder=None, args=None):
-        # Workaround for conan-io/conan#11975
-        if self._conanfile._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
-            from conans.tools import args_to_string
-            from conans.client.subsystems import subsystem_path
-            script_folder = path.join(self._conanfile.source_folder, build_script_folder) \
-                if build_script_folder else self._conanfile.source_folder
-
-            configure_args = []
-            configure_args.extend(args or [])
-
-            self._configure_args = "{} {}".format(self._configure_args, args_to_string(configure_args))
-
-            configure_cmd = "{}/configure".format(script_folder)
-            configure_cmd = subsystem_path(_subsystem(self._conanfile), configure_cmd)
-            cmd = '"{}" {}'.format(configure_cmd, self._configure_args)
-            self._conanfile.output.info("Calling:\n > %s" % cmd)
-            with vcvars(self._conanfile) if is_msvc(self._conanfile) else no_op():
-                self._conanfile.run(
-                    '. "{}" && "{}" {}'.format(_env_activate_sh_path(self._conanfile), subsystem_path(_subsystem(self._conanfile), path.join(self._conanfile.build_folder, "configure")),
-                                               self._configure_args), run_environment=True, win_bash=True)
-        else:
-            super(AutotoolsWinBash, self).configure(build_script_folder=build_script_folder, args=args)
-
-    def make(self, target=None, args=None):
-        # Workaround for conan-io/conan#11975
-        if self._conanfile._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
-            from conan.tools.build import build_jobs
-            make_program = self._conanfile.conf.get("tools.gnu:make_program",
-                                                    default="mingw32-make" if self._use_win_mingw() else "make")
-            str_args = self._make_args
-            str_extra_args = " ".join(args) if args is not None else ""
-            jobs = ""
-            if "-j" not in str_args and "nmake" not in make_program.lower():
-                njobs = build_jobs(self._conanfile)
-                if njobs:
-                    jobs = "-j{}".format(njobs)
-            command = " ".join(filter(None, [make_program, target, str_args, str_extra_args, jobs]))
-            with vcvars(self._conanfile) if is_msvc(self._conanfile) else no_op():
-                self._conanfile.run(f". {_env_activate_sh_path(self._conanfile)} && {command}", run_environment=True, win_bash=True)
-        else:
-            super(AutotoolsWinBash, self).make(target=target, args=args)
-
-    def autoreconf(self, args=None):
-        # Workaround for conan-io/conan#11975
-        if self._conanfile._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
-            args = args or []
-            command = join_arguments(["autoreconf", self._autoreconf_args, args_to_string(args)])
-            with chdir(self, self._conanfile.build_folder):
-                with vcvars(self._conanfile) if is_msvc(self._conanfile) else no_op():
-                    self._conanfile.run(f". {_env_activate_sh_path(self._conanfile)} && {command}", run_environment=True, win_bash=True)
-        else:
-            super(AutotoolsWinBash, self).autoreconf(args=args)
-
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import unix_path
 
 required_conan_version = ">=1.50.0"
 
@@ -85,34 +14,46 @@ class TestPackageConan(ConanFile):
     exports_sources = "configure.ac", "Makefile.am", "test_package_1.c", "test_package.cpp", "extra.m4"
     # DON'T COPY extra.m4 TO BUILD FOLDER!!!
     generators = "AutotoolsDeps", "AutotoolsToolchain", "VirtualBuildEnv"
-    win_bash = True
     test_type = "explicit"
 
     @property
     def _settings_build(self):
+        # TODO: Remove for Conan v2
         return getattr(self, "settings_build", self.settings)
 
+    @property
+    def win_bash(self):
+        return self._settings_build.os == "Windows"
+
+    def layout(self):
+        basic_layout(self, src_folder=".")
+        self.folders.build = "build"
+        self.folders.generators = "build/conan"
+
+
     def build_requirements(self):
-        if self._settings_build.os == "Windows" and not environ.get("CONAN_BASH_PATH"):
-            self.tool_requires("msys2/cci.latest")
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
+            self.tool_requires("msys2/cci.latest")  # The conf `tools.microsoft.bash:path` and `tools.microsoft.bash:subsystem` aren't injected for test_package
         self.tool_requires(self.tested_reference_str)
 
     def build(self):
-        for src in self.exports_sources:
-            shutil.copy(path.join(self.source_folder, src), self.build_folder)
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
+            return  # autoconf needs a bash if there isn't a bash no need to build
 
-        # Workaround for conan-io/conan#11975
-        autotools = AutotoolsWinBash(self)
-        autotools.autoreconf()
-        autotools.configure()
+        for src in self.exports_sources:
+            shutil.copy(self.source_path.joinpath(src), self.build_folder)
+
+        autotools = Autotools(self)
+        self.run("autoreconf -fiv", cwd = self.build_path)  # Workaround for since the method `autoreconf()` will always run from source
+        autotools.configure(build_script_folder=self.build_path)
         autotools.make()
 
     def test(self):
-        if not cross_building(self):
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
+            return  # autoconf needs a bash if there isn't a bash no need to build
+
+        if can_run(self):
             ext = ".exe" if self.settings.os == "Windows" else ""
+            test_cmd = unix_path(self, self.build_path.joinpath(f"test_package{ext}"))
 
-            # Workaround for conan-io/conan#11975
-            from conans.client.subsystems import subsystem_path
-            test_cmd = subsystem_path(_subsystem(self), path.join(self.build_folder, f"test_package{ext}"))
-
-            self.run(test_cmd, run_environment=True, win_bash=self.settings.os == "Windows")
+            self.run(test_cmd, run_environment=True)
