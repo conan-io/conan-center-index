91af365669847f6ab48ae812a71133de1e6a2b2f
Artem <bladegreat@yandex.ru>
2022-09-13 21:04:24 +0300

    Modernize

--- recipes/opencore-amr/all/conanfile.py
+++ recipes/opencore-amr/all/conanfile.py
@@ -1,8 +1,11 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conan import ConanFile, tools
+from conan.tools.microsoft import is_msvc
+from conans import AutoToolsBuildEnvironment
+from conans.tools import environment_append, get_env, os_info, vcvars, unix_path
 from contextlib import contextmanager
 import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.47.0"
 
 
 class OpencoreAmrConan(ConanFile):
@@ -18,7 +21,7 @@ class OpencoreAmrConan(ConanFile):
         "fPIC": [True, False],
     }
     default_options = {
-        "shared": False, 
+        "shared": False,
         "fPIC": True,
     }
 
@@ -41,26 +44,26 @@ class OpencoreAmrConan(ConanFile):
             del self.options.fPIC
 
     def build_requirements(self):
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/cci.latest")
         if self.settings.compiler == "Visual Studio":
             self.build_requires("automake/1.16.5")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        tools.files.get(self, **self.conan_data["sources"][self.version],
+                        destination=self._source_subfolder, strip_root=True)
 
     @contextmanager
     def _build_context(self):
-        if self.settings.compiler == "Visual Studio":
-            with tools.vcvars(self):
+        if is_msvc(self):
+            with vcvars(self):
                 env = {
                     "CC": "cl -nologo",
                     "CXX": "cl -nologo",
                     "LD": "link -nologo",
-                    "AR": "{} lib".format(tools.unix_path(self.deps_user_info["automake"].ar_lib)),
+                    "AR": "{} lib".format(unix_path(self.deps_user_info["automake"].ar_lib)),
                 }
-                with tools.environment_append(env):
+                with environment_append(env):
                     yield
         else:
             yield
@@ -68,17 +71,20 @@ class OpencoreAmrConan(ConanFile):
     def _configure_autotools(self):
         if self._autotools:
             return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        yes_no = lambda v: "yes" if v else "no"
+        self._autotools = AutoToolsBuildEnvironment(
+            self, win_bash=os_info.is_windows)
+
+        def yes_no(v): return "yes" if v else "no"
         args = [
             "--enable-shared={}".format(yes_no(self.options.shared)),
             "--enable-static={}".format(yes_no(not self.options.shared)),
         ]
-        if self.settings.compiler == "Visual Studio":
+        if is_msvc(self):
             self._autotools.cxx_flags.append("-EHsc")
-            if tools.Version(self.settings.compiler.version) >= "12":
+            if tools.scm.Version(self.settings.compiler.version) >= "12":
                 self._autotools.flags.append("-FS")
-        self._autotools.configure(args=args, configure_dir=self._source_subfolder)
+        self._autotools.configure(
+            args=args, configure_dir=self._source_subfolder)
         return self._autotools
 
     def build(self):
@@ -89,18 +95,20 @@ class OpencoreAmrConan(ConanFile):
     def package(self):
         self._autotools.install()
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.files.rm(self, os.path.join(self.package_folder, "lib"), "*.la")
+        tools.files.rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         if self.settings.compiler == "Visual Studio" and self.options.shared:
             for lib in ("opencore-amrwb", "opencore-amrnb"):
-                tools.rename(os.path.join(self.package_folder, "lib", "{}.dll.lib".format(lib)),
-                             os.path.join(self.package_folder, "lib", "{}.lib".format(lib)))
+                tools.files.rename(self, os.path.join(self.package_folder, "lib", "{}.dll.lib".format(lib)),
+                                   os.path.join(self.package_folder, "lib", "{}.lib".format(lib)))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "opencore-amr")
-        self.cpp_info.set_property("cmake_target_name", "opencore-amr::opencore-amr")
+        self.cpp_info.set_property(
+            "cmake_target_name", "opencore-amr::opencore-amr")
         for lib in ("opencore-amrwb", "opencore-amrnb"):
-            self.cpp_info.components[lib].set_property("cmake_target_name", f'opencore-amr::{lib}')
+            self.cpp_info.components[lib].set_property(
+                "cmake_target_name", f'opencore-amr::{lib}')
             self.cpp_info.components[lib].set_property("pkg_config_name", lib)
             self.cpp_info.components[lib].libs = [lib]
             # TODO: to remove in conan v2 once cmake_find_package* & pkg_config generator removed
