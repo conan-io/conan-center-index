000d92763f928ea71bcc1f011a6ab3b9cd9f0aa9
Christopher McArthur <christopherm@jfrog.com>
2022-09-14 13:30:20 -0600

    add layout

--- recipes/rapidjson/all/conanfile.py
+++ recipes/rapidjson/all/conanfile.py
@@ -1,5 +1,6 @@
 from conan import ConanFile
 from conan.tools.files import get, copy
+from conan.tools.layout import basic_layout
 import os
 
 required_conan_version = ">=1.50.0"
@@ -14,6 +15,9 @@ class RapidjsonConan(ConanFile):
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
+
+    def layout(self):
+        basic_layout(self)
 
     def source(self):
        get(self, **self.conan_data["sources"][self.version], strip_root=True,
