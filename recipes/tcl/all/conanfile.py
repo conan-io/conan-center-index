1165b5bc23e86d91813664264c0267fb4f93f560
Jelle Spijker <j.spijker@ultimaker.com>
2022-09-14 15:14:12 +0200

    Manage fPIC with shared
    Contribute to CURA-9614

--- recipes/tcl/all/conanfile.py
+++ recipes/tcl/all/conanfile.py
@@ -42,7 +42,7 @@ class TclConan(ConanFile):
             copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def configure(self):
-        if self.settings.os == "Windows":
+        if self.settings.os == "Windows" or self.options.shared:
             try:
                 del self.options.fPIC
             except ValueError:
