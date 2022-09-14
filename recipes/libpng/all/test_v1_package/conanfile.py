0900bbc224f642c0cf50006543b5c4677423b28e
Uilian Ries <uilianries@gmail.com>
2022-09-14 19:04:56 +0200

    (#12386) [libpng] Conan v2 support
    * Conan v2 support
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * update generators
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Link libpng
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * required conan version
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * use self.conf
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add 1.5.30
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove symlink
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove symlink
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Use symlink as main library
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * add lib prefix and suffix
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * remove suffix
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix patch file name
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * skip Mac m1 for 1.5
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * skip cross build on mac
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Improve message error
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * fix lib name on Windows^
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Support cross on Mac
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix debug suffix on Windows
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * skip Mac M1 on 1.5
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * fix build_type
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Requires Conan 1.51.3
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Remove pkg_config_name legacy
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Enforce source folder
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

---
+++ recipes/libpng/all/test_v1_package/conanfile.py
@@ -0,0 +1,29 @@
+from conans import ConanFile, CMake, tools
+from conan.tools.build import cross_building
+import os
+import re
+import subprocess
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not cross_building(self):
+            if "arm" in self.settings.arch and not tools.os_info.is_macos:
+                self.test_arm()
+            else:
+                bin_path = os.path.join("bin", "test_package")
+                self.run(bin_path, run_environment=True)
+
+    def test_arm(self):
+        file_ext = "so" if self.options["libpng"].shared else "a"
+        lib_path = os.path.join(self.deps_cpp_info["libpng"].libdirs[0], "libpng.%s" % file_ext)
+        output = subprocess.check_output(["readelf", "-h", lib_path]).decode()
+        assert re.search(r"Machine:\s+ARM", output)
