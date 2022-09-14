4ff8cb5ad45f5c7c67af04225967d89e6e173771
Martin Delille <martin@delille.org>
2022-09-14 10:18:38 +0200

    Apply suggestions from code review
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/bandit/all/conanfile.py
+++ recipes/bandit/all/conanfile.py
@@ -13,7 +13,6 @@ class BanditConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/banditcpp/bandit"
     license = "MIT"
-    no_copy_source = True
 
     def export_sources(self):
         for p in self.conan_data.get("patches", {}).get(self.version, []):
@@ -38,3 +37,10 @@ class BanditConan(ConanFile):
 
     def package_id(self):
         self.info.clear()
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
+
--- recipes/bandit/all/test_package/conanfile.py
+++ recipes/bandit/all/test_package/conanfile.py
@@ -7,6 +7,7 @@ import os
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
