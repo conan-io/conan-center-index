efcf5e1ad0d609b62970ea519501bfccca191b4c
toge <toge.mail@gmail.com>
2022-09-15 02:21:55 +0900

    remove unused modules
    Co-authored-by: Daniel <danimanzaneque@gmail.com>

--- recipes/hazelcast-cpp-client/all/test_package/conanfile.py
+++ recipes/hazelcast-cpp-client/all/test_package/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.env import VirtualRunEnv
 import os
 
