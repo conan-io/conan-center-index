03011532cfdf9957419db47d4ccfd6d4e8f4437e
Chris Mc <christopherm@jfrog.com>
2022-09-14 16:24:23 -0600

    Update recipes/rapidjson/all/conanfile.py
    linter

--- recipes/rapidjson/all/conanfile.py
+++ recipes/rapidjson/all/conanfile.py
@@ -20,7 +20,7 @@ class RapidjsonConan(ConanFile):
         basic_layout(self)
 
     def source(self):
-       get(self, **self.conan_data["sources"][self.version], strip_root=True,
+        get(self, **self.conan_data["sources"][self.version], strip_root=True,
                     destination=self.source_folder)
 
     def package(self):
