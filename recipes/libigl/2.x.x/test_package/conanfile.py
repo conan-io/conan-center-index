7a97f9f305d4043133568ca6849932ca20339b12
Lefteris Kotsonis <lefteris.kotsonis@gmail.com>
2022-09-15 17:16:12 +0300

    [libigl] fix cross building syntax in test package

--- recipes/libigl/2.x.x/test_package/conanfile.py
+++ recipes/libigl/2.x.x/test_package/conanfile.py
@@ -13,7 +13,7 @@ class LibiglTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "example")
             self.run(bin_path, run_environment=True)
 
