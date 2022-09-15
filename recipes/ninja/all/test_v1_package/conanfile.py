fa44c591ab0a5dbc2460a78d1aa80f9f1ffc5372
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-15 03:04:18 +0200

    (#12918) ninja: conan v2 support & drop 1.9.0
    * conan v2 support
    
    * drop 1.9.0

--- recipes/ninja/all/test_v1_package/conanfile.py
+++ recipes/ninja/all/test_v1_package/conanfile.py
@@ -1,14 +1,12 @@
 from conans import ConanFile
-import os
 
 
-class TestPackage(ConanFile):
-    settings = "os", "arch"
-
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
     test_type = "explicit"
-    
+
     def build_requirements(self):
         self.build_requires(self.tested_reference_str)
-        
+
     def test(self):
         self.run("ninja --version", run_environment=True)
