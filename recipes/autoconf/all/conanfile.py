ad45b97bc0534bbd907d6bc51d9c6c6348af02d7
j.spijker@ultimaker.com <jelle spijker>
2022-09-15 14:32:49 +0200

    Set win_bash in configure stage
    Contributes to CURA-8831

--- recipes/autoconf/all/conanfile.py
+++ recipes/autoconf/all/conanfile.py
@@ -22,13 +22,12 @@ class AutoconfConan(ConanFile):
         # TODO: Remove for Conan v2
         return getattr(self, "settings_build", self.settings)
 
-    @property
-    def win_bash(self):
-        return self._settings_build.os == "Windows"
-
     def export_sources(self):
         for p in self.conan_data.get("patches", {}).get(self.version, []):
             copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
+
+    def configure(self):
+        self.win_bash = self._settings_build.os == "Windows"
 
     def layout(self):
         basic_layout(self, src_folder="autoconf")
