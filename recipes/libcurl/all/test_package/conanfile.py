07572dc1a674f58409b81dee65d688d525b29210
Martin Delille <martin@delille.org>
2022-09-16 07:54:48 +0200

    Fix test_package

--- recipes/libcurl/all/test_package/conanfile.py
+++ recipes/libcurl/all/test_package/conanfile.py
@@ -2,9 +2,10 @@ from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake
 import os
+import subprocess
+import re
 
 
-# It will become the standard on Conan 2.x
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
