27308008bc21f0361280ae7b1a395d572befa338
toge <toge.mail@gmail.com>
2022-09-16 19:18:39 +0900

    revise test_package.cpp

--- recipes/xsimd/all/test_package/test_package.cpp
+++ recipes/xsimd/all/test_package/test_package.cpp
@@ -7,12 +7,9 @@ int main(int argc, char *argv[]) {
 #if XSIMD_VERSION_MAJOR < 8
   xs::batch<double, 4> a(1.5, 2.5, 3.5, 4.5);
   xs::batch<double, 4> b(2.5, 3.5, 4.5, 5.5);
-#elif XSIMD_VERSION_MAJOR < 9
-  xs::batch<double> a({1.5, 2.5, 3.5, 4.5});
-  xs::batch<double> b({2.5, 3.5, 4.5, 5.5});
 #else
-  xs::batch<double, xs::default_arch> a{1.5, 2.5, 3.5, 4.5};
-  xs::batch<double, xs::default_arch> b{2.5, 3.5, 4.5, 5.5};
+  xs::batch<double> a{1.5, 2.5, 3.5, 4.5};
+  xs::batch<double> b{2.5, 3.5, 4.5, 5.5};
 #endif
 
   auto mean = (a + b) / 2;
