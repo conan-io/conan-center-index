c51e973003a50f3f6c695ce04f87d3ff0606a69b
Martin Delille <martin@delille.org>
2022-09-16 07:44:56 +0200

    Add test_type and tested_reference_str

--- recipes/libcurl/all/test_package/conanfile.py
+++ recipes/libcurl/all/test_package/conanfile.py
@@ -1,15 +1,17 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, cmake_layout
-
+from conan.tools.cmake import cmake_layout, CMake
 import os
-import subprocess
-import re
 
 
+# It will become the standard on Conan 2.x
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def layout(self):
         cmake_layout(self)
