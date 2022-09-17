5fec90a5c8abbb0a323a2e50b65f32606ff17c6e
toge <toge.mail@gmail.com>
2022-09-17 17:12:50 +0900

    fix constructor call

--- recipes/xsimd/all/test_package/test_package.cpp
+++ recipes/xsimd/all/test_package/test_package.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[]) {
   xs::batch<double, 4> a(1.5, 2.5, 3.5, 4.5);
   xs::batch<double, 4> b(2.5, 3.5, 4.5, 5.5);
 #else
-  xs::batch<double> a{1.5, 2.5, 3.5, 4.5};
-  xs::batch<double> b{2.5, 3.5, 4.5, 5.5};
+  xs::batch<double> a(1.5, 2.5, 3.5, 4.5);
+  xs::batch<double> b(2.5, 3.5, 4.5, 5.5);
 #endif
 
   auto mean = (a + b) / 2;
