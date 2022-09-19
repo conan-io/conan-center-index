68b929a73281056d39094ce07a064c60f3f676d3
toge <toge.mail@gmail.com>
2022-09-19 20:27:52 +0900

    base64: add version 0.5.0 and support conan v2

--- recipes/base64/all/conanfile.py
+++ recipes/base64/all/conanfile.py
@@ -1,16 +1,22 @@
-import os
-from conans import ConanFile, AutoToolsBuildEnvironment, CMake, tools
+from conan import ConanFile
+from conan.tools.env import Environment
+from conan.tools.files import copy, get, apply_conandata_patches, chdir
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc
 
+import os
+
+required_conan_version = ">=1.51.3"
 
 class Base64Conan(ConanFile):
     name = "base64"
+    description = "Fast Base64 stream encoder/decoder"
     license = "BSD-2-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/aklomp/base64"
-    description = "Fast Base64 stream encoder/decoder"
-    topics = ("base64", "codec")
-    exports_sources = "patches/**", "CMakeLists.txt"
-    generators = "cmake"
+    topics = ("base64", "codec", "encoder", "decoder")
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "fPIC": [True, False]
@@ -19,67 +25,69 @@ class Base64Conan(ConanFile):
         "fPIC": True
     }
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
+    def export_sources(self):
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
-        del self.settings.compiler.cppstd
-        del self.settings.compiler.libcxx
+
+    def configure(self):
+        try:
+            del self.settings.compiler.libcxx
+        except Exception:
+            pass
+        try:
+            del self.settings.compiler.cppstd
+        except Exception:
+            pass
+
+    def layout(self):
+        if is_msvc(self):
+            cmake_layout(self, src_folder="src")
+        else:
+            basic_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version],
+                  destination=self.source_folder, strip_root=True)
 
-    def _patch_sources(self):
-        for patch in self.conan_data["patches"][self.version]:
-            tools.patch(**patch)
-
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
-
-    def _build_cmake(self):
-        cmake = self._configure_cmake()
-        cmake.build(target="base64")
-
-    def _build_autotools(self):
-        autotools = AutoToolsBuildEnvironment(self)
-        if self.settings.arch == "x86" or self.settings.arch == "x86_64":
-            extra_env = {
-                "AVX2_CFLAGS": "-mavx2",
-                "SSSE3_CFLAGS": "-mssse3",
-                "SSE41_CFLAGS": "-msse4.1",
-                "SSE42_CFLAGS": "-msse4.2",
-                "AVX_CFLAGS": "-mavx"
-            }
+    def generate(self):
+        if is_msvc(self):
+            tc = CMakeToolchain(self)
+            tc.generate()
         else:
-            # ARM-specific instructions can be enabled here
-            extra_env = {}
-        with tools.environment_append(extra_env):
-            with tools.chdir(self._source_subfolder):
-                autotools.make(target="lib/libbase64.a")
+            tc = AutotoolsToolchain(self)
+            tc.generate()
 
     def build(self):
-        self._patch_sources()
-        if self.settings.compiler == "Visual Studio":
-            self._build_cmake()
+        apply_conandata_patches(self)
+        if is_msvc(self):
+            cmake = CMake(self)
+            cmake.configure(os.path.join(self.source_folder, ""))
+            cmake.build(target="base64")
         else:
-            self._build_autotools()
+            env = Environment()
+            if self.settings.arch == "x86" or self.settings.arch == "x86_64":
+                env.append("AVX2_CFLAGS", "-mavx2")
+                env.append("SSSE3_CFLAGS", "-mssse3")
+                env.append("SSE41_CFLAGS", "-msse4.1")
+                env.append("SSE42_CFLAGS", "-msse4.2")
+                env.append("AVX_CFLAGS", "-mavx")
+            else:
+                # ARM-specific instructions can be enabled here
+                pass
+            with env.vars(self).apply(), \
+                 chdir(self, self.source_folder):
+                autotools = Autotools(self)
+                autotools.make(target="lib/libbase64.a")
 
     def package(self):
-        self.copy(pattern="*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, pattern="*.h", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "include"))
+        copy(self, pattern="*.a", dst=os.path.join(self.package_folder, "lib"), src=self.source_folder, keep_path=False)
+        copy(self, pattern="*.lib", dst=os.path.join(self.package_folder, "lib"), src=self.source_folder, keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["base64"]
