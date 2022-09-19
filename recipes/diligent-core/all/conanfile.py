04a851f16e3131da6aff58b013b14a91bc45c5d3
andrei malashkin <malashkin.andrey@gmail.com>
2022-09-19 11:08:37 +0200

    remove pdb files recursively

--- recipes/diligent-core/all/conanfile.py
+++ recipes/diligent-core/all/conanfile.py
@@ -181,7 +181,7 @@ class DiligentCoreConan(ConanFile):
         self.copy(pattern="*.fxh", dst="res", keep_path=False)
 
         self.copy("File2String*", src=os.path.join(self._build_subfolder, "bin"), dst="bin", keep_path=False)
-        rm(self, "*.pdb", os.path.join(self.package_folder, "lib"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "lib"), recursive=True)
         # MinGw creates many invalid files, called objects.a, remove them here:
         rm(self, "objects.a", self.package_folder, recursive=True)
 
