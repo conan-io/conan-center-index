d9f9eadfd15a156140100267a2e652565cf6c3b2
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-09-20 14:48:05 +0200

    [Catch2] adapt recipe for Conan v2

--- recipes/catch2/3.x.x/conanfile.py
+++ recipes/catch2/3.x.x/conanfile.py
@@ -1,9 +1,10 @@
 import os
 import functools
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile, tools
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.51.3"
 
 class Catch2Conan(ConanFile):
     name = "catch2"
