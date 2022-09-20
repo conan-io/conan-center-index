33247b60ce5a08f2f2500a232beac30708d54b48
dotchris90 <christian.kahr@gmx.de>
2022-09-20 18:05:42 +0200

    restructure tests for v1 and v2

--- recipes/cyclonedds/all/test_package/conanfile.py
+++ recipes/cyclonedds/all/test_package/conanfile.py
@@ -1,11 +1,18 @@
 import os
 from conan import ConanFile
-from conans import CMake
-from conan.tools import build
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
 
 class CycloneDDSTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = ["cmake", "cmake_find_package_multi"]
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -13,6 +20,6 @@ class CycloneDDSTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not build.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
