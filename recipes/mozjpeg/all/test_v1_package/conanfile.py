b06e4ddf2aef3f082133c2f2a1e29ef84a11d751
toge <toge.mail@gmail.com>
2022-09-20 22:34:27 +0900

    fix bug in bin_path

--- recipes/mozjpeg/all/test_v1_package/conanfile.py
+++ recipes/mozjpeg/all/test_v1_package/conanfile.py
@@ -14,6 +14,6 @@ class TestPackageV1Conan(ConanFile):
 
     def test(self):
         if not cross_building(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            bin_path = os.path.join("bin", "test_package")
             img_name = os.path.join(self.source_folder, "testimg.jpg")
             self.run(f"{bin_path} {img_name}", run_environment=True)
