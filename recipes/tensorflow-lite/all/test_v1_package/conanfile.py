c9f46fb2fc737808d4957fdc842ce1bd0d290e90
talyz <kim.lindberger@gmail.com>
2022-09-21 10:38:09 +0200

    [tensorflow-lite] Change back to using source_folder in test_v1

--- recipes/tensorflow-lite/all/test_v1_package/conanfile.py
+++ recipes/tensorflow-lite/all/test_v1_package/conanfile.py
@@ -14,6 +14,6 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not cross_building(self):
-            model_path = os.path.join(self.recipe_folder, os.pardir, "test_package", "model.tflite")
+            model_path = os.path.join(self.source_folder, os.pardir, "test_package", "model.tflite")
             command = os.path.join("bin", "test_package")
             self.run(f"{command} {model_path}", run_environment=True)
