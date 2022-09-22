7fd0578543c68710c5dbdc5341c68be63fe45787
Uilian Ries <uilianries@gmail.com>
2022-09-22 13:28:57 +0200

    Prepare for Conan 2.0
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ recipes/libigl/2.x.x/test_v1_package/conanfile.py
@@ -0,0 +1,20 @@
+import os
+
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conans import CMake
+
+
+class LibiglTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "cmake_find_package_multi"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
