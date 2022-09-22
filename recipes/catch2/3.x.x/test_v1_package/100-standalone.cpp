190da8c7d2bf7b4d4df2bc3f81e0cd1698377160
Uilian Ries <uilianries@gmail.com>
2022-09-22 09:43:22 +0200

    Move test package files (#2)
    * move gtest
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Delete test_all.sh
    
    * Use cmake targets
    
    * Do not use settings
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/catch2/3.x.x/test_v1_package/100-standalone.cpp
+++
@@ -1,5 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE( "compiles and runs" ) {
-    REQUIRE( true == !false );
-}
--- recipes/catch2/3.x.x/test_v1_package/300-standalone-with-prefix.cpp
+++
@@ -1,5 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-CATCH_TEST_CASE( "compiles and runs" ) {
-    CATCH_REQUIRE( true == !false );
-}
