fd68f395789c47203f9360d882b8d92bb0b8c454
Uilian Ries <uilianries@gmail.com>
2022-09-22 15:43:25 +0200

    Prepare for Conan v2
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/jfalcou-eve/all/conanfile.py
+++ recipes/jfalcou-eve/all/conanfile.py
@@ -1,9 +1,16 @@
 from conan import ConanFile
-from conan.tools import files, build
-from conan.tools.cmake import CMake
+from conan.tools import files
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy
+from conan.tools.scm import Version
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc
 from conan.errors import ConanInvalidConfiguration
+import os
 
-required_conan_version = ">=1.33.0"
+
+required_conan_version = ">=1.50.0"
+
 
 class JfalcouEveConan(ConanFile):
     name = "jfalcou-eve"
@@ -11,16 +18,12 @@ class JfalcouEveConan(ConanFile):
                    "Boost.SIMD on C++20"
                    )
     homepage = "https://jfalcou.github.io/eve/"
-    topics = ("c++", "simd")
+    topics = ("cpp", "simd")
     license = ("BSL-1.0")
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os", "arch", "compiler", "build_type"
-
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
     def _min_cppstd(self):
@@ -30,22 +33,24 @@ class JfalcouEveConan(ConanFile):
     def _compilers_minimum_version(self):
         return {"gcc": "11",
                 "Visual Studio": "16.9",
+                "msvc": "1928",
                 "clang": "13",
-                "apple-clang": "13",
-                }
+                "apple-clang": "13"}
 
     def configure(self):
-        ll = self.version.strip("v")
-        lv = [int(v) for v in ll.split(".")]
-        info = lv[1:2]
-        self.license = "BSL-1.0" if (info[0]>=2022 and info[1]>=9) else "MIT"
+        version = Version(self.version.strip("v"))
+        if version.major <= 2022 and version.minor < 9:
+            self.license = "MIT"
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            build.check_min_cppstd(self, self._min_cppstd)
-        if self.settings.compiler == "Visual Studio":
+        if self.info.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+        if is_msvc(self):
             raise ConanInvalidConfiguration("EVE does not support MSVC yet (https://github.com/jfalcou/eve/issues/1022).")
-        if self.settings.compiler == "apple-clang":
+        if self.info.settings.compiler == "apple-clang":
             raise ConanInvalidConfiguration("EVE does not support apple Clang due to an incomplete libcpp.")
 
         def lazy_lt_semver(v1, v2):
@@ -56,21 +61,22 @@ class JfalcouEveConan(ConanFile):
 
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if not minimum_version:
-            self.output.warn("{} {} requires C++20. Your compiler is unknown. Assuming it supports C++20.".format(self.name, self.version))
+            self.output.warn(f"{self.ref} requires C++{self._min_cppstd}. Your compiler is unknown. Assuming it supports C++{self._min_cppstd}.")
         elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
-            raise ConanInvalidConfiguration("{} {} requires C++20, which your compiler does not support.".format(self.name, self.version))
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def source(self):
-        files.get(self, **self.conan_data["sources"][self.version], strip_root=True,
-                destination=self._source_subfolder)
+        files.get(self, **self.conan_data["sources"][self.version], strip_root=True, destination=self.source_folder)
 
     def package(self):
-        self.copy(pattern="include/*", src=self._source_subfolder)
-        self.copy("LICENSE.md", dst="licenses", src=self._source_subfolder)
+        copy(self, pattern="*.hpp", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+        copy(self, "LICENSE.md", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "eve"
         self.cpp_info.names["cmake_find_package_multi"] = "eve"
+        self.cpp_info.set_property("cmake_file_name", "eve")
+        self.cpp_info.set_property("cmake_target_name", "eve::eve")
