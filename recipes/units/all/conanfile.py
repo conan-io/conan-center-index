93c5dd063813e536eb2f1f826a5f0a4a005cfab9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-22 10:36:10 +0200

    typo

--- recipes/units/all/conanfile.py
+++ recipes/units/all/conanfile.py
@@ -64,4 +64,4 @@ class UnitsConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "units::units")
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
-        self.cpp_info.resdirs - []
+        self.cpp_info.resdirs = []
