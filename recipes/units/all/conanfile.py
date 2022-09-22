ef8fc99459bf78c446455168fd5712c818cd8603
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-22 10:13:33 +0200

    conan v2 support

--- recipes/units/all/conanfile.py
+++ recipes/units/all/conanfile.py
@@ -1,51 +1,67 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.scm import Version
 import os
 
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
-from conans.tools import Version
+required_conan_version = ">=1.50.0"
 
 
 class UnitsConan(ConanFile):
     name = "units"
-    url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/nholthaus/units"
-    description = "A compile-time, header-only, dimensional analysis and unit conversion library built on c++14 with no dependencies"
+    description = (
+        "A compile-time, header-only, dimensional analysis and unit conversion "
+        "library built on c++14 with no dependencies"
+    )
+    license = "MIT"
     topics = ("unit-conversion", "dimensional-analysis", "cpp14",
               "template-metaprogramming", "compile-time", "header-only",
               "no-dependencies")
-    license = "MIT"
+    homepage = "https://github.com/nholthaus/units"
+    url = "https://github.com/conan-io/conan-center-index"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
-    settings = "compiler"
 
-    _source_subfolder = "source_subfolder"
+    @property
+    def _min_cppstd(self):
+        return "14"
 
-    def configure(self):
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, "14")
-        minimum_version = {
-            "clang": 3.4,
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "clang": "3.4",
             "gcc": "4.9.3",
-            "Visual Studio": 14.0,
-        }.get(str(self.settings.compiler))
-        if not minimum_version:
-            self.output.warn(
-                "Unknown compiler: assuming compiler supports C++14")
-        else:
-            if Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration(
-                    "Compiler does not support C++14")
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
-
-    def package(self):
-        self.copy(
-            "units.h",
-            dst="include",
-            src=os.path.join(self._source_subfolder, "include"))
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+            "Visual Studio": "14",
+            "msvc": "190",
+        }
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._minimum_compilers_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.",
+            )
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def build(self):
+        pass
+
+    def package(self):
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "units.h", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "units")
+        self.cpp_info.set_property("cmake_target_name", "units::units")
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs - []
