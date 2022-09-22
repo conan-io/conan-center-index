2be0e71b74c91f1816caf962b48c7961886f80ac
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-22 18:45:14 +0200

    (#13067) tsl-array-hash: conan v2 support

--- recipes/tsl-array-hash/all/conanfile.py
+++ recipes/tsl-array-hash/all/conanfile.py
@@ -1,7 +1,10 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
 import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.50.0"
 
 
 class TslArrayHashConan(ConanFile):
@@ -14,24 +17,33 @@ class TslArrayHashConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 11)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def build(self):
+        pass
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.h", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "tsl-array-hash")
         self.cpp_info.set_property("cmake_target_name", "tsl::array_hash")
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
 
         # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "tsl-array-hash"
@@ -41,3 +53,6 @@ class TslArrayHashConan(ConanFile):
         self.cpp_info.components["array_hash"].names["cmake_find_package"] = "array_hash"
         self.cpp_info.components["array_hash"].names["cmake_find_package_multi"] = "array_hash"
         self.cpp_info.components["array_hash"].set_property("cmake_target_name", "tsl::array_hash")
+        self.cpp_info.components["array_hash"].bindirs = []
+        self.cpp_info.components["array_hash"].libdirs = []
+        self.cpp_info.components["array_hash"].resdirs = []
