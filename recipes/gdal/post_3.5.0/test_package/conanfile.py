2376f5e7e876da84fe549c7dfd55c26eba6fa191
Julien Pilet <julien.pilet@invision.ai>
2022-09-22 17:44:36 +0200

    (#11694) gdal: add new CMake based conanfile.py for version 3.5.1
    * [WIP] Add gdal 3.5.1
    
    * [WIP] treat all dependencies in the same way
    
    * works as shared lib
    
    * Works with several deps, formatted
    
    * Format strings with double quotes
    
    * Fix libkml dependency linking
    
    * More fixes, add README.md
    
    * Remove blank line
    
    * Fix build and lint issues
    
    * Fix wrong include dir
    
    * fix requirements component reference
    
    * disable fPIC option with shared
    
    * Check if fPIC options exists before evaluating it
    
    * Updated readme
    
    * Update recipes/gdal/post_3.5.0/conanfile.py
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * removed imports: conan.tools.cmake, conans.tools
    
    * Simplify patch
    
    * Move build in its own folder
    
    * Fix windows lib name
    
    * import is_msvc
    
    * Remove dependency array indirection, replace CONAN_PKG::, address other comments
    
    * Support with_jpeg=libjpeg-turbo
    
    * support with_mysql
    
    * Remove useless line
    
    * use get_safe("fPIC")
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Try to make linter happy
    
    * Revert changes on pre-3.5.0 conanfile
    
    * Fix cmake error
    
    * Remove windows suffixes
    
    * Add 'd' suffix for windows debug builds
    
    * Address comments
    
    * Remove --with_crypto, explain cmake patches
    
    * deprecate with_crypto properly
    
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

---
+++ recipes/gdal/post_3.5.0/test_package/conanfile.py
@@ -0,0 +1,23 @@
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conans import CMake
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "cmake_find_package"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not cross_building(self):
+            if self.options["gdal"].tools:
+                self.run("gdal_translate --formats", run_environment=True)
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
+            bin_path_c = os.path.join("bin", "test_package_c")
+            self.run(bin_path_c, run_environment=True)
