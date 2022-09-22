15caf7b028a2456e575a84ca1deb9c2fae096e4b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-09-22 08:58:44 +0200

    test package changes

--- recipes/catch2/3.x.x/test_package/conanfile.py
+++ recipes/catch2/3.x.x/test_package/conanfile.py
@@ -1,18 +1,31 @@
-from conans import ConanFile, CMake, tools
-from conans.tools import Version
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout
 import os
 
 class TestPackageConan(ConanFile):
-    settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["WITH_PREFIX"] = self.dependencies[self.tested_reference_str].options.with_prefix
+        tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["WITH_PREFIX"] = self.options["catch2"].with_prefix
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            self.run(os.path.join("bin", "standalone"), run_environment=True)
-            self.run(os.path.join("bin", "benchmark"), run_environment=True)
+        if can_run(self):
+            self.run(os.path.join(self.cpp.build.bindirs[0], "standalone"), env="conanrun")
+            self.run(os.path.join(self.cpp.build.bindirs[0], "benchmark"), env="conanrun")
