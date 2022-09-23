cbe329417a7ed4514d534cf4368b84e2868297b0
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-09-23 11:05:53 +0200

    (#13037) [Catch2] adapt recipe for Conan v2
    * [Catch2] adapt recipe for Conan v2
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes 4
    
    * fixes 5
    
    * fixes 6
    
    * Update recipes/catch2/3.x.x/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/catch2/3.x.x/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/catch2/3.x.x/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/catch2/3.x.x/conanfile.py
    
    * fix patches
    
    * fix
    
    * fix
    
    * remove comments
    
    * Update recipes/catch2/3.x.x/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update recipes/catch2/3.x.x/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * test package changes
    
    * Move test package files (#2)
    
    * move gtest
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Delete test_all.sh
    
    * Use cmake targets
    
    * Do not use settings
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/catch2/3.x.x/conanfile.py
+++ recipes/catch2/3.x.x/conanfile.py
@@ -1,9 +1,12 @@
 import os
-import functools
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.files import apply_conandata_patches, copy, get, rmdir
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.52.0"
 
 class Catch2Conan(ConanFile):
     name = "catch2"
@@ -23,24 +26,14 @@ class Catch2Conan(ConanFile):
         "with_prefix": False,
         "default_reporter": None,
     }
-    generators = "cmake"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
 
     @property
     def _default_reporter_str(self):
         return '"{}"'.format(str(self.options.default_reporter).strip('"'))
 
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            copy(self, p["patch_file"], self.recipe_folder, self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -55,12 +48,15 @@ class Catch2Conan(ConanFile):
             "apple-clang": "10",
         }
 
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "14")
+            check_min_cppstd(self, "14")
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if minimum_version:
-            if tools.Version(self.settings.compiler.version) < minimum_version:
+            if Version(self.settings.compiler.version) < minimum_version:
                 raise ConanInvalidConfiguration("{}/{}: Unsupported compiler: {}-{} "
                                                 "(https://github.com/p-ranav/structopt#compiler-compatibility)."
                                                 .format(self.name, self.version, self.settings.compiler, self.settings.compiler.version))
@@ -68,39 +64,36 @@ class Catch2Conan(ConanFile):
             self.output.warn("{}/{} requires C++14. Your compiler is unknown. Assuming it supports C++14.".format(self.name, self.version))
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    @functools.lru_cache(1)
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = "OFF"
-        cmake.definitions["CATCH_INSTALL_DOCS"] = "OFF"
-        cmake.definitions["CATCH_INSTALL_HELPERS"] = "ON"
-        cmake.definitions["CATCH_CONFIG_PREFIX_ALL"] = self.options.with_prefix
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_TESTING"] = False
+        tc.variables["CATCH_INSTALL_DOCS"] = False
+        tc.variables["CATCH_INSTALL_HELPERS"] = True
+        tc.variables["CATCH_CONFIG_PREFIX_ALL"] = self.options.with_prefix
         if self.options.default_reporter:
-            cmake.definitions["CATCH_CONFIG_DEFAULT_REPORTER"] = self._default_reporter_str
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
-
-    def _patch_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+            tc.variables["CATCH_CONFIG_DEFAULT_REPORTER"] = self._default_reporter_str
+        tc.generate()
 
     def build(self):
-        self._patch_sources()
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
         for cmake_file in ["ParseAndAddCatchTests.cmake", "Catch.cmake", "CatchAddTests.cmake"]:
             self.copy(
                 cmake_file,
-                src=os.path.join(self._source_subfolder, "extras"),
+                src=os.path.join(self.source_folder, "extras"),
                 dst=os.path.join("lib", "cmake", "Catch2"),
             )
 
