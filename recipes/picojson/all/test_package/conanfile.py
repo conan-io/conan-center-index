6d3ff4f06000ecfd8247a9eab3b2b2d147c82a79
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-23 17:20:22 +0200

    conan v2 support

--- recipes/picojson/all/test_package/conanfile.py
+++ recipes/picojson/all/test_package/conanfile.py
@@ -1,26 +1,26 @@
-from conans import ConanFile, CMake, tools
-
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, cmake_layout
 import os
 
-class PicoJSONTestConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package"
 
-    @property
-    def _is_multi_configuration(self):
-        cmake = CMake(self)
-        return cmake.is_multi_configuration
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            bin_path = "example"
-            if self._is_multi_configuration:
-                bin_path = os.path.join(str(self.settings.build_type), bin_path)
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
