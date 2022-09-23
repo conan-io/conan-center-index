e66f8bbb6e045ae8f1e784038e112c085bd4fef6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-23 20:48:13 +0200

    tiny improvements

--- recipes/nlohmann_json/all/conanfile.py
+++ recipes/nlohmann_json/all/conanfile.py
@@ -1,8 +1,8 @@
-import os
-
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
+import os
 
 required_conan_version = ">=1.50.0"
 
@@ -23,17 +23,25 @@ class NlohmannJsonConan(ConanFile):
     def package_id(self):
         self.info.clear()
 
-    def generate(self):
-        pass
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 11)
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def build(self):
+        pass
 
     def package(self):
         copy(self, "LICENSE*", self.source_folder, os.path.join(self.package_folder, "licenses"))
         copy(self, "*", os.path.join(self.source_folder, "include"), os.path.join(self.package_folder, "include"))
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "nlohmann_json"
-        self.cpp_info.names["cmake_find_package_multi"] = "nlohmann_json"
-        self.cpp_info.names["pkg_config"] = "nlohmann_json"
+        self.cpp_info.set_property("cmake_file_name", "nlohmann_json")
+        self.cpp_info.set_property("cmake_target_name", "nlohmann_json::nlohmann_json")
+        self.cpp_info.set_property("pkg_config_name", "nlohmann_json")
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
