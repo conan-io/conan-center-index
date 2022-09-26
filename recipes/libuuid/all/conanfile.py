8a5818bf2e79712d9ec1753bfa5aeda618f85d21
Uilian Ries <uilianries@gmail.com>
2022-09-26 12:54:58 +0200

    Prepare for Conan v2
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libuuid/all/conanfile.py
+++ recipes/libuuid/all/conanfile.py
@@ -1,10 +1,13 @@
 from conan import ConanFile
-from conan.tools import files
+from conan.tools.files import export_conandata_patches, apply_conandata_patches, get, rm , rmdir, copy
 from conan.errors import ConanInvalidConfiguration
-from conans import AutoToolsBuildEnvironment, tools
+from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.tools.build import cross_building
+from conan.tools.layout import basic_layout
 import os
 
-required_conan_version = ">=1.50.0"
+required_conan_version = ">=1.52.0"
 
 
 class LibuuidConan(ConanFile):
@@ -15,66 +18,66 @@ class LibuuidConan(ConanFile):
     license = "BSD-3-Clause"
     topics = ("conan", "libuuid", "uuid", "unique-id", "unique-identifier")
     settings = "os", "arch", "compiler", "build_type"
-    exports_sources = "patches/**"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
-    _autotools = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+        try:
+            del self.settings.compiler.libcxx
+        except Exception:
+            pass
+        try:
+            del self.settings.compiler.cppstd
+        except Exception:
+            pass
+
+    def layout(self):
+        basic_layout(self)
 
     def validate(self):
-        if self.settings.os == "Windows":
+        if self.info.settings.os == "Windows":
             raise ConanInvalidConfiguration("libuuid is not supported on Windows")
 
     def build_requirements(self):
-        self.build_requires("libtool/2.4.7")
+        self.tool_requires("libtool/2.4.7")
 
     def source(self):
-        files.get(self, **self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+                destination=self.source_folder, strip_root=True)
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self)
-        yes_no = lambda v: "yes" if v else "no"
-        configure_args = [
-            "--enable-shared={}".format(yes_no(self.options.shared)),
-            "--enable-static={}".format(yes_no(not self.options.shared)),
-        ]
+    def generate(self):
+        tc = AutotoolsToolchain(self)
         if "x86" in self.settings.arch:
-            self._autotools.flags.append('-mstackrealign')
-        self._autotools.configure(args=configure_args, configure_dir=self._source_subfolder)
-        return self._autotools
+            tc.extra_cflags = ['-mstackrealign']
+        tc.generate()
+        tc = AutotoolsDeps(self)
+        tc.generate()
+        env = VirtualBuildEnv(self)
+        env.generate()
+        if not cross_building(self):
+            env = VirtualRunEnv(self)
+            env.generate(scope="build")
 
     def build(self):
-        files.apply_conandata_patches(self)
-        with files.chdir(self, self._source_subfolder):
-            self.run("{} -fiv".format(tools.get_env("AUTORECONF")), run_environment=True)
-        autotools = self._configure_autotools()
-        autotools.make()
+        apply_conandata_patches(self)
+        autotools = Autotools(self)
+        autotools.autoreconf()
+        autotools.configure()
+        autotools.make
 
     def package(self):
-        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
-        autotools = self._configure_autotools()
+        copy(self, "COPYING", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        autotools = Autotools(self)
         autotools.install()
-        files.rm(self, "*.la", os.path.join(self.package_folder, "lib"))
-        files.rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
-        self.cpp_info.names["pkg_config"] = "uuid"
+        self.cpp_info.set_property("pkg_config_name", "uuid")
         self.cpp_info.libs = ["uuid"]
         self.cpp_info.includedirs.append(os.path.join("include", "uuid"))
