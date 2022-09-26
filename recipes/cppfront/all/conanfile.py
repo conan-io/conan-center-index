e1c2cf538690ab39d502f044487017a3c142029a
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-09-26 12:18:24 -0300

    Update recipes/cppfront/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/cppfront/all/conanfile.py
+++ recipes/cppfront/all/conanfile.py
@@ -1,5 +1,4 @@
 from conan import ConanFile
-from conan.tools.files import save
 
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
