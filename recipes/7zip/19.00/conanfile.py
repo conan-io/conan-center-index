2dc4875edcdf183ccea2663c124f52ac29be8018
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-09-26 17:08:41 +0200

    Update conanfile.py

--- recipes/7zip/19.00/conanfile.py
+++ recipes/7zip/19.00/conanfile.py
@@ -96,3 +96,6 @@ class SevenZipConan(ConanFile):
         bin_path = os.path.join(self.package_folder, "bin")
         self.output.info(f"Appending PATH environment variable: {bin_path}")
         self.env_info.path.append(bin_path)
+        
+        self.cpp_info.includedirs = []
+        self.cpp_info.libdirs = []
