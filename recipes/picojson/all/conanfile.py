e93894cff1a6ac2d1777a24fd2dbfcce1ef2852c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-26 17:05:10 +0200

    (#13112) picojson: conan v2 support

--- recipes/picojson/all/conanfile.py
+++ recipes/picojson/all/conanfile.py
@@ -1,5 +1,9 @@
-from conans import ConanFile, tools
-import os, glob
+from conan import ConanFile
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+import os
+
+required_conan_version = ">=1.50.0"
 
 
 class PicoJSONConan(ConanFile):
@@ -8,18 +12,28 @@ class PicoJSONConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/kazuho/picojson"
     description = "A C++ JSON parser/serializer"
-    topics = ("picojson", "json", "header-only", "conan-recipe")
+    topics = ("picojson", "json", "header-only")
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    _source_subfolder = "source_subfolder"
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename(glob.glob("picojson-*")[0], self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def build(self):
+        pass
 
     def package(self):
-        self.copy("{}.h".format(self.name), dst="include", src=self._source_subfolder)
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        copy(self, "picojson.h", src=self.source_folder, dst=os.path.join(self.package_folder, "include"))
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
-        self.info.header_only()
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
