5975fa0d6f3dd12895485ada95ac6724c02f772d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-09-26 20:09:42 +0200

    [catch2] fix recipe

--- recipes/catch2/3.x.x/conanfile.py
+++ recipes/catch2/3.x.x/conanfile.py
@@ -52,7 +52,7 @@ class Catch2Conan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
+        if self.info.settings.compiler.cppstd:
             check_min_cppstd(self, "14")
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if minimum_version:
