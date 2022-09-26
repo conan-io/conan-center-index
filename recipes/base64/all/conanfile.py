32fb9d86cb021fe7b4100dc5c8c087633e5013ff
toge <toge.mail@gmail.com>
2022-09-26 14:44:41 +0900

    add validation for shared

--- recipes/base64/all/conanfile.py
+++ recipes/base64/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.env import Environment
 from conan.tools.files import copy, get, apply_conandata_patches, chdir, export_conandata_patches, rmdir
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
@@ -20,12 +21,12 @@ class Base64Conan(ConanFile):
     topics = ("base64", "codec", "encoder", "decoder")
     settings = "os", "arch", "compiler", "build_type"
     options = {
-        "fPIC": [True, False],
         "shared": [True, False],
+        "fPIC": [True, False],
     }
     default_options = {
-        "fPIC": True,
         "shared": False,
+        "fPIC": True,
     }
 
     def export_sources(self):
@@ -34,8 +35,6 @@ class Base64Conan(ConanFile):
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
-        if Version(self.version) < "0.5.0":
-            del self.options.shared
 
     def configure(self):
         try:
@@ -48,17 +47,25 @@ class Base64Conan(ConanFile):
             pass
 
     def layout(self):
-        if is_msvc(self):
+        if self._use_cmake:
             cmake_layout(self, src_folder="src")
         else:
             basic_layout(self, src_folder="src")
+
+    def validate(self):
+        if Version(self.version) < "0.5.0" and self.info.options.shared:
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support build shared.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version],
                   destination=self.source_folder, strip_root=True)
 
+    @property
+    def _use_cmake(self):
+        return is_msvc(self) or Version(self.version) >= "0.5.0"
+
     def generate(self):
-        if is_msvc(self):
+        if self._use_cmake:
             tc = CMakeToolchain(self)
             tc.generate()
         else:
@@ -67,10 +74,13 @@ class Base64Conan(ConanFile):
 
     def build(self):
         apply_conandata_patches(self)
-        if is_msvc(self):
+        if self._use_cmake:
             cmake = CMake(self)
             cmake.configure()
-            cmake.build(target="base64")
+            if Version(self.version) >= "0.5.0":
+                cmake.build()
+            else:
+                cmake.build(target="base64")
         else:
             env = Environment()
             if self.settings.arch == "x86" or self.settings.arch == "x86_64":
@@ -88,18 +98,23 @@ class Base64Conan(ConanFile):
                 autotools.make(target="lib/libbase64.a")
 
     def package(self):
-        if is_msvc(self):
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        if self._use_cmake:
             cmake = CMake(self)
             cmake.install()
-            rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+            if Version(self.version) >= "0.5.0":
+                rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+            else:
+                rmdir(self, os.path.join(self.package_folder, "cmake"))
+                rmdir(self, os.path.join(self.package_folder, "lib"))
+                copy(self, pattern="*.lib", dst=os.path.join(self.package_folder, "lib"), src=self.build_folder, keep_path=False)
         else:
-            copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
             copy(self, pattern="*.h", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "include"))
-            copy(self, pattern="*.a", dst=os.path.join(self.package_folder, "lib"), src=self.build_folder, keep_path=False)
+            copy(self, pattern="*.a", dst=os.path.join(self.package_folder, "lib"), src=self.source_folder, keep_path=False)
             copy(self, pattern="*.lib", dst=os.path.join(self.package_folder, "lib"), src=self.build_folder, keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["base64"]
 
-        if Version(self.version) >= "0.5.0" and not self.options.shared:
+        if Version(self.version) >= "0.5.0" and self.options.shared:
             self.cpp_info.defines.append("BASE64_STATIC_DEFINE")
