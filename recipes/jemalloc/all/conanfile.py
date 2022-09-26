dafd47d3f5cf02ddd0c6d425f229faf01b2fee9b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-09-26 11:35:42 -0300

    Update recipes/jemalloc/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/jemalloc/all/conanfile.py
+++ recipes/jemalloc/all/conanfile.py
@@ -94,7 +94,7 @@ class JemallocConan(ConanFile):
         return getattr(self, "settings_build", self.settings)
 
     def build_requirements(self):
-        if self._settings_build.os == "Windows" and not tools_legacy.get_env("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
             self.build_requires("msys2/cci.latest")
 
     def source(self):
