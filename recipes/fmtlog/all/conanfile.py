d4d88fcaa6fe995f289abb7177ecf413a6a66f13
toge <toge.mail@gmail.com>
2022-09-28 00:59:58 +0900

    use `self.info.settings` part 2
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/fmtlog/all/conanfile.py
+++ recipes/fmtlog/all/conanfile.py
@@ -75,8 +75,8 @@ class PackageConan(ConanFile):
     def validate(self):
         if self.info.settings.get_safe("compiler.cppstd"):
             check_min_cppstd(self, self._minimum_cpp_standard)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+        minimum_version = self._compilers_minimum_version.get(str(self.info.settings.compiler), False)
+        if minimum_version and Version(self.info.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._minimum_cpp_standard}, which your compiler does not support.")
 
     def source(self):
