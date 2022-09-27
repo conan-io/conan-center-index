14924bd051a0c1e56717175ab05a3a91fa226132
Andrew Marshall <planetmarshalluk@gmail.com>
2022-09-27 13:58:07 +0100

    [gdk-pixbuf] update to new meson toolchain

--- recipes/gdk-pixbuf/all/conanfile.py
+++ recipes/gdk-pixbuf/all/conanfile.py
@@ -1,11 +1,14 @@
 from conan import ConanFile
+from conan.tools.cmake import CMake
+from conan.tools.meson import MesonToolchain, Meson
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.layout import basic_layout
 from conan.tools import files, scm, microsoft
 from conan.errors import ConanInvalidConfiguration, ConanException
-from conans import CMake, Meson, tools
 from tempfile import TemporaryDirectory
 import functools
 import os
-import shutil
 
 required_conan_version = ">=1.50.2"
 
@@ -36,16 +39,7 @@ class GdkPixbufConan(ConanFile):
         "with_introspection": False,
     }
 
-    generators = "pkg_config"
     exports_sources = "patches/**"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -58,6 +52,82 @@ class GdkPixbufConan(ConanFile):
         del self.settings.compiler.cppstd
         if self.options.shared:
             self.options["glib"].shared = True
+
+    def layout(self):
+        basic_layout(self, src_folder="source")
+
+    @property
+    def _requires_compiler_rt(self):
+        return self.settings.compiler == "clang" and self.settings.build_type == "Debug"
+
+    def _test_for_compiler_rt(self):
+        cmake = CMake(self)
+        with TemporaryDirectory() as tmp:
+            def open_temp_file(file_name):
+                return open(os.path.join(tmp, file_name), "w", encoding="utf-8")
+
+            with open_temp_file("CMakeLists.txt") as cmake_file:
+                cmake_file.write(r"""
+                        cmake_minimum_required(VERSION 3.16)
+                        project(compiler_rt_test)
+                        try_compile(HAS_COMPILER_RT ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/test.c OUTPUT_VARIABLE OUTPUT)
+                        if(NOT HAS_COMPILER_RT)
+                        message(FATAL_ERROR compiler-rt not present)
+                        endif()""")
+            with open_temp_file("test.c") as test_source:
+                test_source.write(r"""
+                        extern __int128_t __muloti4(__int128_t a, __int128_t b, int* overflow);
+                        int main() {
+                            __int128_t a;
+                            __int128_t b;
+                            int overflow;
+                            __muloti4(a, b, &overflow);
+                            return 0;
+                        }""")
+            cmake.definitions["CMAKE_EXE_LINKER_FLAGS"] = "-rtlib=compiler-rt"
+            try:
+                cmake.configure(build_script_folder=tmp)
+            except ConanException as ex:
+                raise ConanInvalidConfiguration("LLVM Compiler RT is required to link gdk-pixbuf in debug mode") from ex
+
+    def generate(self):
+        deps = PkgConfigDeps(self)
+        deps.generate()
+
+        tc = MesonToolchain(self)
+        defs = {}
+        if scm.Version(self.version) >= "2.42.0":
+            defs["introspection"] = "false"
+        else:
+            defs["gir"] = "false"
+        defs["docs"] = "false"
+        defs["man"] = "false"
+        defs["installed_tests"] = "false"
+        if scm.Version(self.version) >= "2.42.8":
+            defs["png"] = "enabled" if self.options.with_libpng else "disabled"
+            defs["tiff"] = "enabled" if self.options.with_libtiff else "disabled"
+            defs["jpeg"] = "enabled" if self.options.with_libjpeg else "disabled"
+        else:
+            defs["png"] = "true" if self.options.with_libpng else "false"
+            defs["tiff"] = "true" if self.options.with_libtiff else "false"
+            defs["jpeg"] = "true" if self.options.with_libjpeg else "false"
+
+        defs["builtin_loaders"] = "all"
+        defs["gio_sniffing"] = "false"
+        defs["introspection"] = "enabled" if self.options.with_introspection else "disabled"
+        tc.project_options.update(defs)
+
+        # Workaround for https://bugs.llvm.org/show_bug.cgi?id=16404
+        # Only really for the purposes of building on CCI - end users can
+        # workaround this by appropriately setting global linker flags in their profile
+        if self._requires_compiler_rt:
+            self._test_for_compiler_rt()
+            tc.c_link_args.append("-rtlib=compiler-rt")
+
+        tc.generate()
+
+        venv = VirtualBuildEnv(self)
+        venv.generate()
 
     def requirements(self):
         self.requires("glib/2.74.0")
@@ -81,19 +151,18 @@ class GdkPixbufConan(ConanFile):
             raise ConanInvalidConfiguration("This package does not support Macos currently")
 
     def build_requirements(self):
-        self.build_requires("meson/0.63.2")
-        self.build_requires("pkgconf/1.9.3")
+        self.tool_requires("meson/0.63.2")
+        self.tool_requires("pkgconf/1.9.3")
         if self.options.with_introspection:
-            self.build_requires("gobject-introspection/1.72.0")
+            self.tool_requires("gobject-introspection/1.72.0")
 
     def source(self):
-        files.get(self, **self.conan_data["sources"][self.version],
-                  strip_root=True, destination=self._source_subfolder)
+        files.get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def _patch_sources(self):
         files.apply_conandata_patches(self)
 
-        meson_build = os.path.join(self._source_subfolder, "meson.build")
+        meson_build = os.path.join(self.source_folder, "meson.build")
         files.replace_in_file(self, meson_build, "subdir('tests')", "#subdir('tests')")
         files.replace_in_file(self, meson_build, "subdir('thumbnailer')", "#subdir('thumbnailer')")
         files.replace_in_file(self, meson_build,
@@ -101,95 +170,29 @@ class GdkPixbufConan(ConanFile):
                               else "gmodule_dep.get_pkgconfig_variable('gmodule_supported')", "'true'")
         # workaround https://gitlab.gnome.org/GNOME/gdk-pixbuf/-/issues/203
         if scm.Version(self.version) >= "2.42.6":
-            files.replace_in_file(self, os.path.join(self._source_subfolder, "build-aux", "post-install.py"),
+            files.replace_in_file(self, os.path.join(self.source_folder, "build-aux", "post-install.py"),
                                   "close_fds=True", "close_fds=(sys.platform != 'win32')")
         if scm.Version(self.version) >= "2.42.9":
             files.replace_in_file(self, meson_build, "is_msvc_like ? 'png' : 'libpng'", "'libpng'")
             files.replace_in_file(self, meson_build, "is_msvc_like ? 'jpeg' : 'libjpeg'", "'libjpeg'")
             files.replace_in_file(self, meson_build, "is_msvc_like ? 'tiff' : 'libtiff-4'", "'libtiff-4'")
 
-    @property
-    def _requires_compiler_rt(self):
-        return self.settings.compiler == "clang" and self.settings.build_type == "Debug"
-
-    def _test_for_compiler_rt(self):
-        cmake = CMake(self)
-        with TemporaryDirectory() as tmp:
-            def open_temp_file(file_name):
-                return open(os.path.join(tmp, file_name), "w", encoding="utf-8")
-
-            with open_temp_file("CMakeLists.txt") as cmake_file:
-                cmake_file.write(r"""
-                    cmake_minimum_required(VERSION 3.16)
-                    project(compiler_rt_test)
-                    try_compile(HAS_COMPILER_RT ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/test.c OUTPUT_VARIABLE OUTPUT)
-                    if(NOT HAS_COMPILER_RT)
-                    message(FATAL_ERROR compiler-rt not present)
-                    endif()""")
-            with open_temp_file("test.c") as test_source:
-                test_source.write(r"""
-                    extern __int128_t __muloti4(__int128_t a, __int128_t b, int* overflow);
-                    int main() {
-                        __int128_t a;
-                        __int128_t b;
-                        int overflow;
-                        __muloti4(a, b, &overflow);
-                        return 0;
-                    }""")
-            cmake.definitions["CMAKE_EXE_LINKER_FLAGS"] = "-rtlib=compiler-rt"
-            try:
-                cmake.configure(source_folder=tmp)
-            except ConanException as ex:
-                raise ConanInvalidConfiguration("LLVM Compiler RT is required to link gdk-pixbuf in debug mode") from ex
-
     @functools.lru_cache(1)
     def _configure_meson(self):
         meson = Meson(self)
-        defs = {}
-        if scm.Version(self.version) >= "2.42.0":
-            defs["introspection"] = "false"
-        else:
-            defs["gir"] = "false"
-        defs["docs"] = "false"
-        defs["man"] = "false"
-        defs["installed_tests"] = "false"
-        if scm.Version(self.version) >= "2.42.8":
-            defs["png"] = "enabled" if self.options.with_libpng else "disabled"
-            defs["tiff"] = "enabled" if self.options.with_libtiff else "disabled"
-            defs["jpeg"] = "enabled" if self.options.with_libjpeg else "disabled"
-        else:
-            defs["png"] = "true" if self.options.with_libpng else "false"
-            defs["tiff"] = "true" if self.options.with_libtiff else "false"
-            defs["jpeg"] = "true" if self.options.with_libjpeg else "false"
-
-        defs["builtin_loaders"] = "all"
-        defs["gio_sniffing"] = "false"
-        defs["introspection"] = "enabled" if self.options.with_introspection else "disabled"
-        args = []
-        # Workaround for https://bugs.llvm.org/show_bug.cgi?id=16404
-        # Ony really for the purporses of building on CCI - end users can
-        # workaround this by appropriately setting global linker flags in their profile
-        if self._requires_compiler_rt:
-            args.append('-Dc_link_args="-rtlib=compiler-rt"')
-        args.append("--wrap-mode=nofallback")
-        meson.configure(defs=defs, build_folder=self._build_subfolder, source_folder=self._source_subfolder, pkg_config_paths=".", args=args)
+        meson.configure()
         return meson
 
     def build(self):
-        if self._requires_compiler_rt:
-            self._test_for_compiler_rt()
-
         self._patch_sources()
-        if self.options.with_libpng:
-            shutil.copy("libpng.pc", "libpng16.pc")
         meson = self._configure_meson()
         meson.build()
 
     def package(self):
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        with tools.environment_append({"LD_LIBRARY_PATH": os.path.join(self.package_folder, "lib")}):
-            meson = self._configure_meson()
-            meson.install()
+        files.copy(self, "COPYING", self.source_folder, "licenses")
+        meson = self._configure_meson()
+        meson.install()
+
         if microsoft.is_msvc(self) and not self.options.shared:
             files.rename(self, os.path.join(self.package_folder, "lib", "libgdk_pixbuf-2.0.a"), os.path.join(self.package_folder, "lib", "gdk_pixbuf-2.0.lib"))
         files.rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
@@ -217,4 +220,5 @@ class GdkPixbufConan(ConanFile):
         self.cpp_info.names["pkg_config"] = "gdk-pixbuf-2.0"
 
     def package_id(self):
-        self.info.requires["glib"].full_package_mode()
+        if not self.options["glib"].shared:
+            self.info.requires["glib"].full_package_mode()
