ecd97e6b8fd6f75497995d886e466b6bd11a8e74
Uilian Ries <uilianries@gmail.com>
2022-09-27 15:29:35 +0200

    nettle: support Conan 2.0
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/nettle/all/conanfile.py
+++ recipes/nettle/all/conanfile.py
@@ -1,15 +1,23 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
+from conan.tools.files import get, replace_in_file, copy, rmdir
+from conan.tools.layout import basic_layout
+from conan.tools.build import cross_building
+from conan.tools.microsoft import unix_path
+from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain, PkgConfigDeps
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.47.0"
 
 
 class NettleTLS(ConanFile):
     name = "nettle"
     description = "The Nettle and Hogweed low-level cryptographic libraries"
     homepage = "https://www.lysator.liu.se/~nisse/nettle"
-    topics = ("conan", "nettle", "crypto", "low-level-cryptographic", "cryptographic")
+    topics = ("crypto", "low-level-cryptographic", "cryptographic")
     license = ("GPL-2.0-or-later", "GPL-3.0-or-later")
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os", "arch", "compiler", "build_type"
@@ -30,12 +38,6 @@ class NettleTLS(ConanFile):
         "x86_shani": False,
     }
 
-    _autotools = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -47,18 +49,32 @@ class NettleTLS(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            try:
+                del self.options.fPIC
+            except Exception:
+                pass
+        try:
+            del self.settings.compiler.libcxx
+        except Exception:
+            pass
+        try:
+            del self.settings.compiler.cppstd
+        except Exception:
+            pass
+        if self.settings.os == "Windows":
+            self.win_bash = True
 
     def requirements(self):
         if self.options.public_key:
             self.requires("gmp/6.2.1")
 
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
     def validate(self):
-        if self.settings.compiler == "Visual Studio":
-            raise ConanInvalidConfiguration("Nettle cannot be built using Visual Studio")
-        if tools.Version(self.version) < "3.6" and self.options.get_safe("fat") and self.settings.arch == "x86_64":
+        if is_msvc(self):
+            raise ConanInvalidConfiguration(f"{self.ref} cannot be built using '{self.settings.compiler}'")
+        if Version(self.version) < "3.6" and self.info.options.get_safe("fat") and self.info.settings.arch == "x86_64":
             raise ConanInvalidConfiguration("fat support is broken on this nettle release (due to a missing x86_64/sha_ni/sha1-compress.asm source)")
 
     @property
@@ -66,65 +82,81 @@ class NettleTLS(ConanFile):
         return getattr(self, "settings_build", self.settings)
 
     def build_requirements(self):
-        self.build_requires("libtool/2.4.6")
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/cci.latest")
+        self.tool_requires("libtool/2.4.6")
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
+            self.tool_requires("msys2/cci.latest")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+    def generate(self):
+        tc = AutotoolsToolchain(self)
         conf_args = [
             "--enable-public-key" if self.options.public_key else "--disable-public-key",
             "--enable-fat" if self.options.get_safe("fat") else "--disable-fat",
             "--enable-x86-aesni" if self.options.get_safe("x86_aesni") else "--disable-x86-aesni",
             "--enable-x86_sshni" if self.options.get_safe("x86_sshni") else "--disable-x86_sshni",
         ]
-        if self.options.shared:
-            conf_args.extend(["--enable-shared", "--disable-static"])
-        else:
-            conf_args.extend(["--disable-shared", "--enable-static"])
-        self._autotools.configure(args=conf_args, configure_dir=self._source_subfolder)
-        # srcdir in unix path causes some troubles in asm files on Windows
-        if self.settings.os == "Windows":
-            tools.replace_in_file(os.path.join(self.build_folder, "config.m4"),
-                                  tools.unix_path(os.path.join(self.build_folder, self._source_subfolder)),
-                                  os.path.join(self.build_folder, self._source_subfolder).replace("\\", "/"))
-        return self._autotools
+        tc.configure_args.extend(conf_args)
+        tc.generate()
+        tc = AutotoolsDeps(self)
+        tc.generate()
+        tc = PkgConfigDeps(self)
+        tc.generate()
+        env = VirtualBuildEnv(self)
+        env.generate()
+        if not cross_building(self):
+            env = VirtualRunEnv(self)
+            env.generate(scope="build")
 
     def _patch_sources(self):
-        makefile_in = os.path.join(self._source_subfolder, "Makefile.in")
-        tools.replace_in_file(makefile_in,
+        makefile_in = os.path.join(self.source_folder, "Makefile.in")
+        # discard subdirs
+        replace_in_file(self, makefile_in,
                               "SUBDIRS = tools testsuite examples",
                               "SUBDIRS = ")
         # Fix broken tests for compilers like apple-clang with -Werror,-Wimplicit-function-declaration
-        tools.replace_in_file(os.path.join(self._source_subfolder, "aclocal.m4"),
+        replace_in_file(self, os.path.join(self.source_folder, "aclocal.m4"),
                               "cat >conftest.c <<EOF",
                               "cat >conftest.c <<EOF\n#include <stdlib.h>")
 
     def build(self):
         self._patch_sources()
-        with tools.chdir(self._source_subfolder):
-            self.run("{} -fiv".format(tools.get_env("AUTORECONF")), win_bash=tools.os_info.is_windows)
-        autotools = self._configure_autotools()
+        autotools = Autotools(self)
+        autotools.autoreconf()
+        # srcdir in unix path causes some troubles in asm files on Windows
+        if self.settings.os == "Windows":
+            replace_in_file(self, os.path.join(self.build_folder, "config.m4"),
+                                  unix_path(self, os.path.join(self.build_folder, self.source_folder)),
+                                  os.path.join(self.build_folder, self.source_folder).replace("\\", "/"))
+        autotools.configure()
         autotools.make()
 
     def package(self):
-        self.copy(pattern="COPYING*", src=self._source_subfolder, dst="licenses")
-        autotools = self._configure_autotools()
+        copy(self, pattern="COPYING*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        autotools = Autotools(self)
         autotools.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
-        self.cpp_info.components["hogweed"].names["pkgconfig"] = "hogweed"
+        self.cpp_info.set_property("cmake_file_name", "Nettle")
+
+        self.cpp_info.components["hogweed"].set_property("pkg_config_name", "hogweed")
+        self.cpp_info.components["hogweed"].set_property("cmake_target_name", "Nettle::Hogweed")
         self.cpp_info.components["hogweed"].libs = ["hogweed"]
         if self.options.public_key:
             self.cpp_info.components["hogweed"].requires.append("gmp::libgmp")
 
-        self.cpp_info.components["libnettle"].libs = ["nettle"]
-        self.cpp_info.components["libnettle"].requires = ["hogweed"]
-        self.cpp_info.components["libnettle"].names["pkgconfig"] = "nettle"
+        self.cpp_info.components["nettle"].libs = ["nettle"]
+        self.cpp_info.components["nettle"].requires = ["hogweed"]
+        self.cpp_info.components["nettle"].set_property("pkg_config_name", "nettle")
+        self.cpp_info.components["nettle"].set_property("cmake_target_name", "Nettle::Nettle")
+
+        # TODO: Remove after Conan 2.0
+        self.cpp_info.names["cmake_find_package"] = "Nettle"
+        self.cpp_info.names["cmake_find_package_multi"] = "Nettle"
+        self.cpp_info.components["hogweed"].names["cmake_find_package"] = "Hogweed"
+        self.cpp_info.components["hogweed"].names["cmake_find_package_multi"] = "Hogweed"
+        self.cpp_info.components["nettle"].names["cmake_find_package"] = "Nettle"
+        self.cpp_info.components["nettle"].names["cmake_find_package_multi"] = "Nettle"
