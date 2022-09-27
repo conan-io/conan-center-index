cc396eaeb8c6403aedbd3ded0f93210f4eab2025
toge <toge.mail@gmail.com>
2022-09-27 15:45:38 +0900

    remove unused function

--- recipes/svector/all/conanfile.py
+++ recipes/svector/all/conanfile.py
@@ -1,14 +1,13 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
+from conan.tools.files import get, copy
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
 from conan.tools.layout import basic_layout
+
 import os
 
-
 required_conan_version = ">=1.52.0"
-
 
 class PackageConan(ConanFile):
     name = "svector"
