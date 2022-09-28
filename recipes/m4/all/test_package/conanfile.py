e55d46e8f9baf2a6f031fba21df4f527b2c5e4cc
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-09-28 15:47:14 +0200

    (#13135) m4: conan v2 support
    * conan v2 support
    
    * fix mingw build for 1.4.19
    
    * also add M4 to runenv_info
    
    * set win_bash in build_requirements
    
    * call VirtualBuildEnv before AutotoolsToolchain
    
    * move env vars generation before AutotoolsToolchain

--- recipes/m4/all/test_package/conanfile.py
+++ recipes/m4/all/test_package/conanfile.py
@@ -1,5 +1,5 @@
-from conans import ConanFile, tools
-from conans.errors import ConanException
+from conan import ConanFile
+from conan.tools.files import save
 from io import StringIO
 import os
 import textwrap
@@ -7,13 +7,18 @@ import textwrap
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
+    generators = "VirtualBuildEnv"
+    test_type = "explicit"
 
     @property
     def _m4_input_path(self):
         return os.path.join(self.build_folder, "input.m4")
 
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
+
     def build(self):
-        tools.save(self._m4_input_path, textwrap.dedent("""\
+        save(self, self._m4_input_path, textwrap.dedent("""\
             m4_define(NAME1, `Harry, Jr.')
             m4_define(NAME2, `Sally')
             m4_define(MET, `$1 met $2')
@@ -21,21 +26,11 @@ class TestPackageConan(ConanFile):
         """))
 
     def test(self):
-        if hasattr(self, "settings_build"):
-            exe_suffix = ".exe" if self.settings.os == "Windows" else ""
-            m4_bin = os.path.join(self.deps_cpp_info["m4"].rootpath, "bin", "m4" + exe_suffix)
-        else:
-            m4_bin = tools.get_env("M4")
-            if m4_bin is None or not m4_bin.startswith(self.deps_cpp_info["m4"].rootpath):
-                raise ConanException("M4 environment variable not set")
+        self.run("m4 --version")
+        self.run(f"m4 -P {self._m4_input_path}")
 
-        if not tools.cross_building(self, skip_x64_x86=True):
-            self.run("{} --version".format(m4_bin), run_environment=True)
-            self.run("{} -P {}".format(m4_bin, self._m4_input_path))
+        self.run(f"m4 -R {self.source_folder}/frozen.m4f {self.source_folder}/test.m4")
 
-            self.run("m4 -R {0}/frozen.m4f {0}/test.m4".format(self.source_folder), run_environment=True)
-
-            output = StringIO()
-            self.run("{} -P {}".format(m4_bin, self._m4_input_path), output=output)
-
-            assert "Harry, Jr. met Sally" in output.getvalue()
+        output = StringIO()
+        self.run(f"m4 -P {self._m4_input_path}", output=output)
+        assert "Harry, Jr. met Sally" in output.getvalue()
