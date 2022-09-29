91b2316616faa248058500c36a43feff2e6e4ab0
andrei malashkin <malashkin.andrey@gmail.com>
2022-09-29 11:49:15 +0200

    modernize make

--- recipes/make/all/conanfile.py
+++ recipes/make/all/conanfile.py
@@ -1,8 +1,14 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
+from conan import ConanFile
+from conans import AutoToolsBuildEnvironment, tools
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import is_msvc, VCVars
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.microsoft import is_msvc
+from conans.client.tools.env import no_op
 import os
 
-required_conan_version = ">=1.33.0"
-
+required_conan_version = ">=1.52.0"
 
 class MakeConan(ConanFile):
     name = "make"
@@ -13,45 +19,38 @@ class MakeConan(ConanFile):
     license = "GPL-3.0-or-later"
     settings = "os", "arch", "compiler", "build_type"
 
-    exports_sources = "patches/*"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def configure(self):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
 
     def package_id(self):
         del self.info.settings.compiler
 
     def build(self):
-        for patch in self.conan_data.get("patches").get(self.version, []):
-            tools.patch(**patch)
-
-        with tools.chdir(self._source_subfolder):
-            # README.W32
-            if tools.os_info.is_windows:
-                if self.settings.compiler == "Visual Studio":
-                    command = "build_w32.bat --without-guile"
-                else:
-                    command = "build_w32.bat --without-guile gcc"
+        # README.W32
+        if is_msvc(self):
+            if self.settings.compiler == "Visual Studio":
+                command = "build_w32.bat --without-guile"
             else:
-                env_build = AutoToolsBuildEnvironment(self)
-                env_build.configure()
-                command = "./build.sh"
-            with tools.vcvars(self.settings) if self.settings.compiler == "Visual Studio" else tools.no_op():
-                self.run(command)
+                command = "build_w32.bat --without-guile gcc"
+        else:
+            env_build = AutoToolsBuildEnvironment(self)
+            env_build.configure()
+            command = "./build.sh"
+        with tools.vcvars(self.settings) if self.settings.compiler == "Visual Studio" else no_op():
+            self.run(command)
 
     def package(self):
-        self.copy(pattern="COPYING", src=self._source_subfolder, dst="licenses")
-        self.copy(pattern="make", src=self._source_subfolder, dst="bin", keep_path=False)
-        self.copy(pattern="*gnumake.exe", src=self._source_subfolder, dst="bin", keep_path=False)
+        self.copy(pattern="COPYING", dst="licenses")
+        self.copy(pattern="make", dst="bin", keep_path=False)
+        self.copy(pattern="*gnumake.exe", dst="bin", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libdirs = []
