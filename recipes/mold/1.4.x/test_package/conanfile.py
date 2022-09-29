fcc5aae0f89a3ffea34d9fb051abe96d412af5cb
andrei malashkin <malashkin.andrey@gmail.com>
2022-09-29 15:09:39 +0200

    use build_requirements instead of requirements in test

--- recipes/mold/1.4.x/test_package/conanfile.py
+++ recipes/mold/1.4.x/test_package/conanfile.py
@@ -8,15 +8,13 @@ class MoldTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     # VirtualRunEnv can be avoided if "tools.env.virtualenv:auto_use" is defined
     # (it will be defined in Conan 2.0)
-    generators = "VirtualRunEnv"
+    generators = "VirtualBuildEnv"
     test_type = "explicit"
 
-    def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def layout(self):
-        basic_layout(self)
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def test(self):
         if not cross_building(self):
-            self.run("mold -v", env="conanrun")
+            self.run("mold -v")
+
