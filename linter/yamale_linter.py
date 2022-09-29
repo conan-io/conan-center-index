4b4b0ac31cbdf7996e590b01fc480b3658e2b523
Christopher McArthur <christopherm@jfrog.com>
2022-09-29 23:14:15 +0200

    This is less strict but has more information

--- linter/yamale_linter.py
+++ linter/yamale_linter.py
@@ -1,5 +1,4 @@
-import yamale
-from yamale import YamaleError
+from strictyaml import load, Map, Str, Int, Seq, YAMLError, MapPattern
 import argparse
 
 def main():
@@ -10,20 +9,30 @@ def main():
                         help='filename of schema. Default is schema.yaml.')
     args = parser.parse_args()
 
+
+
+    schema = Map({"versions": MapPattern(Str(), Map({"folder": Str()}), minimum_keys=1)})
+
     try:
-        schema = yamale.make_schema(args.schema)
-        data = yamale.make_data(args.path)
-        yamale.validate(schema, data)
-    except YamaleError as e:
-        for r in e.results:
-            file_path = r.data
-            for e in r.errors:
-                print(f"::warning file={file_path},line=1::{e}")
-        exit(2)
-    except (SyntaxError, NameError, TypeError, ValueError) as e:
-        print('Validation failed!\n%s' % str(e))
-        exit(1)
-    print('Validation success!')
+        with open(args.path) as f:
+            person = load(f.read(), schema)
+    except YAMLError as error:
+        print(error)
+
+    # try:
+    #     schema = yamale.make_schema(args.schema)
+    #     data = yamale.make_data(args.path)
+    #     yamale.validate(schema, data)
+    # except YamaleError as e:
+    #     for r in e.results:
+    #         file_path = r.data
+    #         for e in r.errors:
+    #             print(f"::warning file={file_path},line=1::{e}")
+    #     exit(2)
+    # except (SyntaxError, NameError, TypeError, ValueError) as e:
+    #     print('Validation failed!\n%s' % str(e))
+    #     exit(1)
+    # print('Validation success!')
 
 
 
