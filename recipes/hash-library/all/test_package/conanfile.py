7837a5a3683fbeadccc9e38ab23b2297c5e6855c
Daniel <danimanzaneque@gmail.com>
2022-09-30 13:02:21 +0200

    Update recipes/hash-library/all/test_package/conanfile.py

--- recipes/hash-library/all/test_package/conanfile.py
+++ recipes/hash-library/all/test_package/conanfile.py
@@ -1,7 +1,8 @@
 import os
 
-from conan import ConanFile, tools
-from conans import CMake
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 
 
 class HashLibraryTestConan(ConanFile):
