93fd9b64f4f6d10b7c5a6789948ad15b0d66a547
Christopher McArthur <christopherm@jfrog.com>
2022-09-30 15:51:09 +0200

    fixup message

--- linter/yamale_linter.py
+++ linter/yamale_linter.py
@@ -1,40 +1,38 @@
-from strictyaml import load, Map, Str, Int, Seq, YAMLError, MapPattern
+from strictyaml import load, Map, Str, Int, Seq, YAMLValidationError, MapPattern
 import argparse
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Validate yaml files.')
-    parser.add_argument('path', metavar='PATH', default='./', nargs='?',
-                        help='folder to validate. Default is current directory.')
-    parser.add_argument('-s', '--schema', default='schema.yaml',
-                        help='filename of schema. Default is schema.yaml.')
+    parser = argparse.ArgumentParser(description="Validate yaml files.")
+    parser.add_argument(
+        "path",
+        metavar="PATH",
+        default="./",
+        nargs="?",
+        help="folder to validate. Default is current directory.",
+    )
+    parser.add_argument(
+        "-s",
+        "--schema",
+        default="schema.yaml",
+        help="filename of schema. Default is schema.yaml.",
+    )
     args = parser.parse_args()
 
-
-
-    schema = Map({"versions": MapPattern(Str(), Map({"folder": Str()}), minimum_keys=1)})
+    schema = Map(
+        {"versions": MapPattern(Str(), Map({"folder": Str()}), minimum_keys=1)}
+    )
 
     try:
         with open(args.path) as f:
             person = load(f.read(), schema)
-    except YAMLError as error:
-        print(error)
-
-    # try:
-    #     schema = yamale.make_schema(args.schema)
-    #     data = yamale.make_data(args.path)
-    #     yamale.validate(schema, data)
-    # except YamaleError as e:
-    #     for r in e.results:
-    #         file_path = r.data
-    #         for e in r.errors:
-    #             print(f"::warning file={file_path},line=1::{e}")
-    #     exit(2)
-    # except (SyntaxError, NameError, TypeError, ValueError) as e:
-    #     print('Validation failed!\n%s' % str(e))
-    #     exit(1)
-    # print('Validation success!')
+    except YAMLValidationError as error:
+        print(
+            f"::error file={args.path},line={error.context_mark.line},endline={error.problem_mark.line},"
+            f"title={error.context}, {error.problem}"
+            f"::{error}\n"
+        )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
