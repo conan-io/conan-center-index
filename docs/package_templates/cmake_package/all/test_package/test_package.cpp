96d7b71965729093448292cbaec98f732873377a
Jordan Williams <jordan@jwillikers.com>
2022-09-30 12:21:49 -0500

    Condense output to a single statement

--- docs/package_templates/cmake_package/all/test_package/test_package.cpp
+++ docs/package_templates/cmake_package/all/test_package/test_package.cpp
@@ -1,16 +1,16 @@
 #include <cstdlib>
 #include <iostream>
+
 #include "package/foobar.hpp"
 
-
 int main(void) {
-    std::cout << "Create a minimal usage for the target project here.\n";
-    std::cout << "Avoid big examples, bigger than 100 lines.\n";
-    std::cout << "Avoid networking connections.\n";
-    std::cout << "Avoid background apps or servers.\n";
-    std::cout << "The propose is testing the generated artifacts only.\n";
+  std::cout << "Create a minimal usage for the target project here.\n"
+               "Avoid big examples, bigger than 100 lines.\n"
+               "Avoid networking connections.\n"
+               "Avoid background apps or servers.\n"
+               "The propose is testing the generated artifacts only.\n";
 
-    foobar.print_version();
+  foobar.print_version();
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
