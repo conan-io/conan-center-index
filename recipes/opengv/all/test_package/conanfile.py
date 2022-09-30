8780c364831d1f68ba8bab4ee445d2467d86ae72
Riff, Eric <ericriff@gmail.com>
2022-09-29 23:10:09 -0300

    Modernize test package

--- recipes/opengv/all/test_package/conanfile.py
+++ recipes/opengv/all/test_package/conanfile.py
@@ -1,11 +1,19 @@
 import os
 from conan import ConanFile
 from conan.tools.build import can_run
-from conans import CMake
+from conan.tools.cmake import CMake
+from conan.tools.layout import cmake_layout
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps", "CMakeToolchain"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
@@ -14,5 +22,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path)
