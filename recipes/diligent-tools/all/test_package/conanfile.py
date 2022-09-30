ddd39eb0831312ded257db79859f359a7b64fef9
andrei malashkin <malashkin.andrey@gmail.com>
2022-09-30 08:30:39 +0200

    Revert "migrade test package to conan v2"
    This reverts commit dd7020afdbb5d222eba637786cf7a504c38ee07f.

--- recipes/diligent-tools/all/test_package/conanfile.py
+++ recipes/diligent-tools/all/test_package/conanfile.py
@@ -1,18 +1,10 @@
-from conan import ConanFile
-from conan.tools.build import cross_building
-from conan.tools.cmake import CMake, cmake_layout
+from conan import ConanFile, CMake, tools
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "VirtualRunEnv"
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def layout(self):
-        cmake_layout(self)
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -20,7 +12,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not cross_building(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, env="conanrun")
-
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
