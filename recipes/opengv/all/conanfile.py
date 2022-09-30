214bc6a8e3b4a7f266f10072cb0f7441503aa29e
Riff, Eric <ericriff@gmail.com>
2022-09-29 23:00:53 -0300

    Fix license

--- recipes/opengv/all/conanfile.py
+++ recipes/opengv/all/conanfile.py
@@ -107,7 +107,7 @@ class opengvConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "LICENSE.txt", dst="licenses", src=self.source_folder)
+        copy(self, "License.txt", dst="licenses", src=self.source_folder)
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
