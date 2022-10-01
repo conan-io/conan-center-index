540cc008b053e1468643dddfa51ad739316c1680
Dmitry Baryshev <dima8w@gmail.com>
2022-09-30 19:45:01 -0700

    [sail] Require conan >= 1.52

--- recipes/sail/all/conanfile.py
+++ recipes/sail/all/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, get, rename, rmdir
 import os
 
-required_conan_version = ">=1.51.3"
+required_conan_version = ">=1.52.0"
 
 class SAILConan(ConanFile):
     name = "sail"
