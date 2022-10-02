0356461fa0c96f4afe8013f742015f25d474ee64
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-02 23:17:24 +0200

    conan v2 support

--- recipes/libmetalink/all/conanfile.py
+++ recipes/libmetalink/all/conanfile.py
@@ -1,10 +1,15 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.errors import ConanInvalidConfiguration
-import functools
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import copy, get, rm, rmdir
+from conan.tools.gnu import Autotools, AutotoolsToolchain, PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, unix_path
 import os
 import shutil
 
-required_conan_version = ">=1.36.0"
+required_conan_version = ">=1.51.1"
 
 
 class LibmetalinkConan(ConanFile):
@@ -30,16 +35,6 @@ class LibmetalinkConan(ConanFile):
         "xml_backend": "expat",
     }
 
-    generators = "pkg_config"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _is_msvc(self):
-        return str(self.settings.compiler) in ["Visual Studio", "msvc"]
-
     @property
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
@@ -54,72 +49,82 @@ class LibmetalinkConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            try:
+                del self.options.fPIC
+            except Exception:
+                pass
+        try:
+            del self.settings.compiler.libcxx
+        except Exception:
+            pass
+        try:
+            del self.settings.compiler.cppstd
+        except Exception:
+            pass
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def requirements(self):
         if self.options.xml_backend == "expat":
-            self.requires("expat/2.4.6")
-        if self.options.xml_backend == "libxml2":
-            self.requires("libxml2/2.9.12")
+            self.requires("expat/2.4.9")
+        elif self.options.xml_backend == "libxml2":
+            self.requires("libxml2/2.9.14")
 
     def validate(self):
-        if self._is_msvc:
+        if is_msvc(self):
             raise ConanInvalidConfiguration("libmetalink does not support Visual Studio yet")
 
     def build_requirements(self):
-        self.build_requires("gnu-config/cci.20201022")
-        self.build_requires("pkgconf/1.7.4")
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/cci.latest")
+        self.tool_requires("gnu-config/cci.20210814")
+        self.tool_requires("pkgconf/1.9.3")
+        if self._settings_build.os == "Windows":
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
+                self.tool_requires("msys2/cci.latest")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+
+        tc = AutotoolsToolchain(self)
+        yes_no = lambda v: "yes" if v else "no"
+        tc.configure_args.extend([
+            f"--with-libexpat={yes_no(self.options.xml_backend == 'expat')}",
+            f"--with-libxml2={yes_no(self.options.xml_backend == 'libxml2')}",
+            "ac_cv_func_malloc_0_nonnull=yes",
+        ])
+        tc.generate()
+
+        pkg = PkgConfigDeps(self)
+        pkg.generate()
 
     def _patch_sources(self):
         # Support more configurations
         shutil.copy(self._user_info_build["gnu-config"].CONFIG_SUB,
-                    os.path.join(self._source_subfolder, "config.sub"))
+                    os.path.join(self.source_folder, "config.sub"))
         shutil.copy(self._user_info_build["gnu-config"].CONFIG_GUESS,
-                    os.path.join(self._source_subfolder, "config.guess"))
-        # Relocatable shared lib for Apple platforms
-        tools.replace_in_file(
-            os.path.join(self._source_subfolder, "configure"),
-            "-install_name \\$rpath/",
-            "-install_name @rpath/",
-        )
-
-    @functools.lru_cache(1)
-    def _configure_autotools(self):
-        autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        autotools.libs = []
-        yes_no = lambda v: "yes" if v else "no"
-        args = [
-            "--enable-static={}".format(yes_no(not self.options.shared)),
-            "--enable-shared={}".format(yes_no(self.options.shared)),
-            "--with-libexpat={}".format(yes_no(self.options.xml_backend == "expat")),
-            "--with-libxml2={}".format(yes_no(self.options.xml_backend == "libxml2")),
-            "ac_cv_func_malloc_0_nonnull=yes",
-        ]
-        autotools.configure(configure_dir=self._source_subfolder, args=args)
-        return autotools
+                    os.path.join(self.source_folder, "config.guess"))
 
     def build(self):
         self._patch_sources()
-        with tools.run_environment(self):
-            autotools = self._configure_autotools()
-            autotools.make()
+        autotools = Autotools(self)
+        autotools.configure()
+        autotools.make()
 
     def package(self):
-        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
-        with tools.run_environment(self):
-            autotools = self._configure_autotools()
-            autotools.install()
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        autotools = Autotools(self)
+        # TODO: replace by autotools.install() once https://github.com/conan-io/conan/issues/12153 fixed
+        autotools.install(args=[f"DESTDIR={unix_path(self, self.package_folder)}"])
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        fix_apple_shared_install_name(self)
 
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "libmetalink")
