52713677131acb72e6f39a9884def112f3c3fd1d
Andrei Malashkin <malashkin.andrey@gmail.com>
2022-10-02 18:08:51 +0200

    Apply suggestions from code review
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/mold/1.4.x/test_package/conanfile.py
+++ recipes/mold/1.4.x/test_package/conanfile.py
@@ -1,20 +1,17 @@
 import os
 from conan import ConanFile
-from conan.tools.build import cross_building
-from conan.tools.layout import basic_layout
+from conan.tools.build import can_run
 
 
 class MoldTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    # VirtualRunEnv can be avoided if "tools.env.virtualenv:auto_use" is defined
-    # (it will be defined in Conan 2.0)
-    generators = "VirtualBuildEnv"
+    generators = "VirtualRunEnv"
     test_type = "explicit"
 
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
 
     def test(self):
-        if not cross_building(self):
-            self.run("mold -v")
+        if can_run(self):
+            self.run("mold -v", env="conanrun")
 
