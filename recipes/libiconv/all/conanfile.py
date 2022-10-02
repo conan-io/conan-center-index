5c3105932360bdfeecf1106108b08a0ad5c89fd5
Andrew Marshall <planetmarshalluk@gmail.com>
2022-10-02 23:39:02 +0100

    [libiconv] add destination to source method

--- recipes/libiconv/all/conanfile.py
+++ recipes/libiconv/all/conanfile.py
@@ -101,7 +101,7 @@ class LibiconvConan(ConanFile):
             self.win_bash = True
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def _patch_sources(self):
         apply_conandata_patches(self)
