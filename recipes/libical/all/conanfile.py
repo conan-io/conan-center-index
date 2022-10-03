ad4bab3ffa977f9ce922342e1be3c27fbc890a77
Allen Winter <winter@kde.org>
2022-10-03 12:54:24 -0400

    add libical/3.0.14 recipe

--- recipes/libical/all/conanfile.py
+++ recipes/libical/all/conanfile.py
@@ -1,5 +1,11 @@
-from conan import ConanFile, CMake, tools
+from conan import ConanFile, CMake
 from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import is_msvc_static_runtime, is_msvc
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.cmake import CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv
 import os
 
 
