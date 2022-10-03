1b55e32e123c1a718544c4b9257bd420ff9d6827
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-03 09:44:36 +0200

    (#13242) libmodbus: several improvements
    * several improvements
    
    - remove useless generators
    - do not test cppstd, it's a C library
    - restore includedir to include/modbus
    - fix compiler=msvc & shared
    
    * fix 2 profile with msvc
    
    * do not redefine compile

--- recipes/libmodbus/all/test_v1_package/conanfile.py
+++ recipes/libmodbus/all/test_v1_package/conanfile.py
@@ -1,11 +1,9 @@
-from conans import ConanFile, CMake
-from conans.tools import cross_building
+from conans import ConanFile, CMake, tools
 import os
 
 
-# legacy validation with Coann 1.x
-class TestPackageV1Conan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
@@ -14,7 +12,7 @@ class TestPackageV1Conan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
 
