47974ca12ace2dd54e3be17d33e4f2466af0f413
Jordan Williams <jordan@jwillikers.com>
2022-10-03 16:40:52 -0500

    Revert test_package.cpp

--- docs/package_templates/cmake_package/all/test_package/test_package.cpp
+++ docs/package_templates/cmake_package/all/test_package/test_package.cpp
@@ -4,11 +4,11 @@
 
 
 int main(void) {
-    std::cout << "Create a minimal usage for the target project here.\n";
-    std::cout << "Avoid big examples, bigger than 100 lines.\n";
-    std::cout << "Avoid networking connections.\n";
-    std::cout << "Avoid background apps or servers.\n";
-    std::cout << "The propose is testing the generated artifacts only.\n";
+    std::cout << "Create a minimal usage for the target project here." << std::endl;
+    std::cout << "Avoid big examples, bigger than 100 lines" << std::endl;
+    std::cout << "Avoid networking connections." << std::endl;
+    std::cout << "Avoid background apps or servers." << std::endl;
+    std::cout << "The propose is testing the generated artifacts only." << std::endl;
 
     foobar.print_version();
 
