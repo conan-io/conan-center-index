8c3e690cd0ab649ddd8b78944791603972c8953f
Riff, Eric <ericriff@gmail.com>
2022-10-04 12:19:30 -0300

    Extract souurces into src subfolder and be explicit about the destination on get

--- recipes/opengv/all/conanfile.py
+++ recipes/opengv/all/conanfile.py
@@ -52,10 +52,10 @@ class opengvConan(ConanFile):
             raise ConanInvalidConfiguration("Clang 12 builds fail on Conan CI.")
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def _patch_sources(self):
         # Use conan's Eigen
