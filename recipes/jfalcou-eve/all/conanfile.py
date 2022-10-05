cc357e6647c09486fb3c53aedb80f310f86b28a7
Joel Falcou <joel.falcou@lri.fr>
2022-10-05 18:12:39 +0200

    Update recipes/jfalcou-eve/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/jfalcou-eve/all/conanfile.py
+++ recipes/jfalcou-eve/all/conanfile.py
@@ -63,7 +63,7 @@ class JfalcouEveConan(ConanFile):
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if not minimum_version:
             self.output.warn(f"{self.ref} requires C++{self._min_cppstd}. Your compiler is unknown. Assuming it supports C++{self._min_cppstd}.")
-        elif lazy_lt_semver(str(self.info.settings.compiler.version), minimum_version):
+        elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
             raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
 
     def package_id(self):
