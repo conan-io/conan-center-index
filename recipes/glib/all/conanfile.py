3c6201ff4018890eaa157e6bc4305d5eaf03987b
SSE4 <tomskside@gmail.com>
2022-10-06 23:47:30 +0700

    Update recipes/glib/all/conanfile.py
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/glib/all/conanfile.py
+++ recipes/glib/all/conanfile.py
@@ -108,7 +108,8 @@ class GLibConan(ConanFile):
         self.tool_requires("pkgconf/1.9.3")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
 
     def layout(self):
         self.folders.build = self._build_subfolder
