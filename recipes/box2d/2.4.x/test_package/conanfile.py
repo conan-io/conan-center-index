96ab8f804898378c8ca174b57a0f2245fc64dfb6
Daniel <danimanzaneque@gmail.com>
2022-10-06 15:46:12 +0200

    Update recipes/box2d/2.4.x/test_package/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/box2d/2.4.x/test_package/conanfile.py
+++ recipes/box2d/2.4.x/test_package/conanfile.py
@@ -7,6 +7,7 @@ from conan.tools.cmake import CMake, cmake_layout
 class Box2DTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
 
     def layout(self):
         cmake_layout(self)
