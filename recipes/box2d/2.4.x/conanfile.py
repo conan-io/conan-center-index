51fd05547d4495db97acefd6bc4f942367811299
Daniel <danimanzaneque@gmail.com>
2022-10-06 15:45:47 +0200

    Update recipes/box2d/2.4.x/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/box2d/2.4.x/conanfile.py
+++ recipes/box2d/2.4.x/conanfile.py
@@ -37,7 +37,7 @@ class Box2dConan(ConanFile):
                 pass
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
