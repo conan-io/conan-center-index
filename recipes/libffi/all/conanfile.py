1658a0c1c5dbde68ab641203c5fb14f16fa14d43
Jordan Williams <jordan@jwillikers.com>
2022-10-08 09:32:56 -0500

    Evaluate ar-lib path

--- recipes/libffi/all/conanfile.py
+++ recipes/libffi/all/conanfile.py
@@ -1,11 +1,12 @@
 from conan import ConanFile
-from conan.tools.apple import fix_apple_shared_install_name, is_apple_os
+from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc, msvc_runtime_flag, unix_path
 from conan.tools.scm import Version
+import os
 
 required_conan_version = ">=1.52.0"
 
@@ -31,10 +32,6 @@ class PackageConan(ConanFile):
     def _settings_build(self):
         # TODO: Remove for Conan v2
         return getattr(self, "settings_build", self.settings)
-
-    @property
-    def win_bash(self):
-        return self._settings_build.os == "Windows"
 
     def export_sources(self):
         export_conandata_patches(self)
@@ -62,8 +59,10 @@ class PackageConan(ConanFile):
         basic_layout(self, src_folder="libffi")
 
     def build_requirements(self):
-        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
-            self.tool_requires("msys2/cci.latest")
+        if self._settings_build.os == "Windows":
+            if not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
+                self.tool_requires("msys2/cci.latest")
+            self.win_bash = True
         self.tool_requires("automake/1.16.5")
         self.tool_requires("libtool/2.4.7")
 
@@ -76,7 +75,6 @@ class PackageConan(ConanFile):
         tc = AutotoolsToolchain(self)
         tc.configure_args.extend([
             f"--enable-debug={yes_no(self.settings.build_type == 'Debug')}",
-            "--datarootdir=${prefix}/res",
             "--enable-builddir=no",
             "--enable-docs=no",
         ])
@@ -96,12 +94,10 @@ class PackageConan(ConanFile):
             ])
 
         if self._settings_build.compiler == "apple-clang":
-            tc.configure_args.extend([
-                "--disable-multi-os-directory",
-            ])
+            tc.configure_args.append("--disable-multi-os-directory")
 
         if is_msvc(self):
-            msvcc = unix_path(self, str(self.source_path.joinpath("msvcc.sh")))
+            msvcc = unix_path(self, os.path.join(self.source_folder, "msvcc.sh"))
             msvcc_args = []
             if self.settings.arch == "x86_64":
                 msvcc_args.append("-m64")
@@ -127,16 +123,20 @@ class PackageConan(ConanFile):
 
         env = tc.environment()
         if is_msvc(self):
-            env.define("LD", "link")
             env.define_path("CXX", msvcc)
             env.define_path("CC", msvcc)
             env.define("CXXCPP", "cl -nologo -EP")
             env.define("CPP", "cl -nologo -EP")
-            env.define("AR", f"{unix_path(self, self.conf.get('tools.automake:ar-lib'))} lib")  # FIXME: Use the conf once https://github.com/conan-io/conan-center-index/pull/12898 is merged
-            #env.define("AR", f"{unix_path(self, self.deps_user_info['automake'].ar_lib)} lib")  #  FIXME: deps_user_info only seems to work with run scoped dependencies
+
+            # FIXME: Use the conf once https://github.com/conan-io/conan-center-index/pull/12898 is merged
+            # env.define("AR", f"{unix_path(self, self.conf.get('tools.automake:ar-lib'))}")
+            [version_major, version_minor, _] = self.dependencies.direct_build['automake'].ref.version.split(".", 2)
+            automake_version = f"{version_major}.{version_minor}"
+            ar_lib = unix_path(self, os.path.join(self.dependencies.direct_build['automake'].cpp_info.resdirs[0], f"automake-{automake_version}", "ar-lib"))
+            env.define("AR", f"{ar_lib} lib")
             env.define("LD", "link")
-            env.define("LIBTOOL", unix_path(self, str(self.source_path.joinpath("ltmain.sh"))))
-            env.define("INSTALL", unix_path(self, str(self.source_path.joinpath("install-sh"))))
+            env.define("LIBTOOL", unix_path(self, os.path.join(self.source_folder, "ltmain.sh")))
+            env.define("INSTALL", unix_path(self, os.path.join(self.source_folder, "install-sh")))
             env.define("NM", "dumpbin -symbols")
             env.define("OBJDUMP", ":")
             env.define("RANLIB", ":")
@@ -153,7 +153,7 @@ class PackageConan(ConanFile):
         if Version(self.version) < "3.3":
             if self.settings.compiler == "clang" and Version(str(self.settings.compiler.version)) >= 7.0:
                 # https://android.googlesource.com/platform/external/libffi/+/ca22c3cb49a8cca299828c5ffad6fcfa76fdfa77
-                sysv_s_src = self.source_path.joinpath("src", "arm", "sysv.S")
+                sysv_s_src = os.path.join(self.source_folder, "src", "arm", "sysv.S")
                 replace_in_file(self, sysv_s_src, "fldmiad", "vldmia")
                 replace_in_file(self, sysv_s_src, "fstmiad", "vstmia")
                 replace_in_file(self, sysv_s_src, "fstmfdd\tsp!,", "vpush")
@@ -163,7 +163,6 @@ class PackageConan(ConanFile):
 
     def build(self):
         self._patch_source()
-
         autotools = Autotools(self)
         autotools.configure()
         autotools.make()
@@ -171,16 +170,12 @@ class PackageConan(ConanFile):
     def package(self):
         autotools = Autotools(self)
         autotools.install(args=[f"DESTDIR={unix_path(self, self.package_folder)}"])  # Need to specify the `DESTDIR` as a Unix path, aware of the subsystem
-
-        if is_apple_os(self):
-            fix_apple_shared_install_name(self)
-
-        copy(self, pattern="*.dll", dst=self.package_path.joinpath("bin"), src=self.package_path.joinpath("lib"))
-        copy(self, pattern="LICENSE", dst=self.package_path.joinpath("licenses"), src=self.source_folder)
-
-        rm(self, "*.la", self.package_path.joinpath("lib"), recursive=True)
-        rmdir(self, self.package_path.joinpath("lib", "pkgconfig"))
-        rmdir(self, self.package_path.joinpath("share"))
+        fix_apple_shared_install_name(self)
+        copy(self, "*.dll", os.path.join(self.package_folder, "lib"), os.path.join(self.package_folder, "bin"))
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"), recursive=True)
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.libs = ["{}ffi".format("lib" if is_msvc(self) else "")]
