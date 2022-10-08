97d7ad04879f0d1bb42146fc26190575005a2a56
toge <toge.mail@gmail.com>
2022-10-09 03:38:32 +0900

    remove unused functions

--- recipes/rapidyaml/all/conanfile.py
+++ recipes/rapidyaml/all/conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.microsoft import check_min_vs, is_msvc
+from conan.tools.microsoft import check_min_vs
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
