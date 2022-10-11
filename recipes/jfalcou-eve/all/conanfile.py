e4b2df4d01c689bd6a6288d06f2b4aedaf11efbf
Joel Falcou <joel.falcou@lri.fr>
2022-10-12 00:55:22 +0200

    (#13303) Update/jfalcou eve
    * Update EVE to v2022.09
    
    * Proper license
    
    * Prepare for Conan v2
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * requires conan 1.51.3
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix version
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Move to use get_safe
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Another changes for header only compielr version detection
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/jfalcou-eve/all/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/jfalcou-eve/all/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix test_package path
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix licensing 1/2
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Fix licensing 2/2
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/jfalcou-eve/all/conanfile.py
+++ recipes/jfalcou-eve/all/conanfile.py
@@ -1,25 +1,29 @@
-from conan import ConanFile, tools
+from conan import ConanFile
+from conan.tools import files
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy
 from conan.tools.scm import Version
-from conans.errors import ConanInvalidConfiguration
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc
+from conan.errors import ConanInvalidConfiguration
+import os
 
-required_conan_version = ">=1.33.0"
+
+required_conan_version = ">=1.51.3"
+
 
 class JfalcouEveConan(ConanFile):
     name = "jfalcou-eve"
     description = ("Expressive Velocity Engine - reimplementation of the old "
                    "Boost.SIMD on C++20"
                    )
-    homepage = "https://github.com/jfalcou/eve"
-    topics = ("c++", "simd")
-    license = "MIT"
+    homepage = "https://jfalcou.github.io/eve/"
+    topics = ("cpp", "simd")
+    license = ("MIT", "BSL-1.0")
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os", "arch", "compiler", "build_type"
-
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
     def _min_cppstd(self):
@@ -29,14 +33,25 @@ class JfalcouEveConan(ConanFile):
     def _compilers_minimum_version(self):
         return {"gcc": "11",
                 "Visual Studio": "16.9",
+                "msvc": "1928",
                 "clang": "13",
-                "apple-clang": "13",
-                }
+                "apple-clang": "13"}
+
+    def configure(self):
+        version = Version(self.version.strip("v"))
+        if version.major < 2022 or (version.major == 2022 and version.minor < 9):
+            self.license = "MIT"
+        else:
+            self.license = "BSL-1.0"
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.build.check_min_cppstd(self, self._min_cppstd)
-        if self.settings.compiler == "Visual Studio":
+        # FIXME: Need to use self.info.settings for header-only
+        if self.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+        if is_msvc(self):
             raise ConanInvalidConfiguration("EVE does not support MSVC yet (https://github.com/jfalcou/eve/issues/1022).")
         if self.settings.compiler == "apple-clang":
             raise ConanInvalidConfiguration("EVE does not support apple Clang due to an incomplete libcpp.")
@@ -49,21 +64,22 @@ class JfalcouEveConan(ConanFile):
 
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if not minimum_version:
-            self.output.warn("{} {} requires C++20. Your compiler is unknown. Assuming it supports C++20.".format(self.name, self.version))
+            self.output.warn(f"{self.ref} requires C++{self._min_cppstd}. Your compiler is unknown. Assuming it supports C++{self._min_cppstd}.")
         elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
-            raise ConanInvalidConfiguration("{} {} requires C++20, which your compiler does not support.".format(self.name, self.version))
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def source(self):
-        tools.files.get(self, **self.conan_data["sources"][self.version], strip_root=True,
-                destination=self._source_subfolder)
+        files.get(self, **self.conan_data["sources"][self.version], strip_root=True, destination=self.source_folder)
 
     def package(self):
-        self.copy(pattern="include/*", src=self._source_subfolder)
-        self.copy("LICENSE.md", dst="licenses", src=self._source_subfolder)
+        copy(self, pattern="*.hpp", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+        copy(self, "LICENSE.md", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "eve"
         self.cpp_info.names["cmake_find_package_multi"] = "eve"
+        self.cpp_info.set_property("cmake_file_name", "eve")
+        self.cpp_info.set_property("cmake_target_name", "eve::eve")
