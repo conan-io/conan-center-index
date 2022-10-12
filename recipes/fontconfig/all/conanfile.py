231c8071c3e5de87a73c2afc4ba0b9ca9e4cea05
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-10-12 11:47:25 +0200

    fontconfig: bump expat
    fixes https://github.com/conan-io/conan-center-index/issues/13436

--- recipes/fontconfig/all/conanfile.py
+++ recipes/fontconfig/all/conanfile.py
@@ -56,7 +56,7 @@ class FontconfigConan(ConanFile):
 
     def requirements(self):
         self.requires("freetype/2.12.1")
-        self.requires("expat/2.4.8")
+        self.requires("expat/2.4.9")
         if self.settings.os == "Linux":
             self.requires("libuuid/1.0.3")
 
