ce8fc46e3fe54abb728ca0ac665fe3ddc3368c1c
Vinay Varma <vin1990@gmail.com>
2022-10-13 12:18:45 +0800

    libssh: new recipe with v2 support

---
+++ recipes/libssh/all/conanfile.py
@@ -0,0 +1,83 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.files import collect_libs, get
+
+
+class libsshRecipe(ConanFile):
+    name = "libssh"
+    version = "0.10.4"
+    license = "LGPL-2.1"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://www.libssh.org/"
+    description = "multiplatform C library implementing the SSHv2 protocol on client and server side"
+    topics = ("libssh", "ssh", "shell", "ssh2", "connection")
+
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_zlib": [True, False],
+        "crypto_backend": ["openssl", "gcrypt"],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_zlib": True,
+        "crypto_backend": "openssl",
+    }
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def requirements(self):
+        if self.options.with_zlib:
+            self.requires("zlib/1.2.12")
+        if self.options.crypto_backend =="openssl":
+            self.requires("openssl/1.1.1q")
+        elif self.options.crypto_backend == "gcrypt":
+            self.requires("libgcrypt/1.8.4")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.variables["CLIENT_TESTING"] = False
+        tc.variables["SERVER_TESTING"] = False
+        tc.variables["WITH_ZLIB"] = self.options.with_zlib
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "libssh")
+        self.cpp_info.set_property("cmake_target_name", "libssh::libssh")
+        self.cpp_info.set_property("pkg_config_name", "libssh")
+        # TODO: back to global scope once cmake_find_package* generators removed
+        self.cpp_info.components["_libssh"].libs = collect_libs(self)
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.names["cmake_find_package"] = "libssh"
+        self.cpp_info.names["cmake_find_package_multi"] = "libssh"
+        self.cpp_info.components["_libssh"].names["cmake_find_package"] = "libssh"
+        self.cpp_info.components["_libssh"].names["cmake_find_package_multi"] = "libssh"
+        self.cpp_info.components["_libssh"].set_property("cmake_target_name", "libssh::libssh")
+        self.cpp_info.components["_libssh"].set_property("pkg_config_name", "libssh")
+        if self.options.with_zlib:
+            self.cpp_info.components["_libssh"].requires.append("zlib::zlib")
+        if self.options.crypto_backend == "openssl":
+            self.cpp_info.components["_libssh"].requires.append("openssl::openssl")
+        elif self.options.crypto_backend == "gcrypt":
+            self.cpp_info.components["_libssh"].requires.append("libgcrypt::libgcrypt")
---
+++ recipes/libssh/all/test_package/conanfile.py
@@ -0,0 +1,26 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import cross_building
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def test(self):
+        if not cross_building(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
