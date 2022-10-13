e2695c251310224a83ce082ba346d4ab7a596c18
Jason Green <jave27@gmail.com>
2022-10-13 16:56:20 -0500

    Update recipes/p7zip/all/test_package/conanfile.py
    👍
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/p7zip/all/test_package/conanfile.py
+++ recipes/p7zip/all/test_package/conanfile.py
@@ -5,6 +5,10 @@ from conan.tools.build import can_run
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "VirtualRunEnv"
+    test_type = "explicit"
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def test(self):
         if can_run(self):
