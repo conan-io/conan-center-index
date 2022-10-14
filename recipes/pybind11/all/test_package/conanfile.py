bce6624d6f4209ba2a8ad6bdea011fe4d67dc4d5
Andrew Marshall <planetmarshall@users.noreply.github.com>
2022-10-14 18:44:34 +0100

    (#13283) Pybind11 fix linkage
    * [pybind11] update to new toolchain
    
    * remove old cmakelists
    
    * [pybind11] update test packages
    
    * [pybind11] fix missing source in v1 test package
    
    * [pybind11] use legacy env tools in test package
    
    * [pybind] remove win_bash requirement from test package
    
    * [pybind11] fix component linkage order
    
    * [pybind11] remove reference to unsupported versions
    
    * [pybind11] fix include dir
    
    * [pybind11] exception handling not needed
    
    * Update recipes/pybind11/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update recipes/pybind11/all/test_package/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * [pybind11] fix typo
    
    * [pybind11] ensure translation of win32 path in cmake toolchain file
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/pybind11/all/test_package/conanfile.py
+++ recipes/pybind11/all/test_package/conanfile.py
@@ -1,16 +1,38 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
+from conan.tools.env import VirtualRunEnv
+from conan.tools.build import can_run
+from conan.tools.layout import cmake_layout
+from conans.tools import environment_append
+
 import os
+from pathlib import PurePath
 import sys
-from platform import python_version
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package_multi"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        toolchain = CMakeToolchain(self)
+        toolchain.variables["PYTHON_EXECUTABLE"] = PurePath(self._python_interpreter).as_posix()
+        toolchain.generate()
+
+        run = VirtualRunEnv(self)
+        run.generate()
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["PYTHON_EXECUTABLE"] = self._python_interpreter
         cmake.configure()
         cmake.build()
 
@@ -21,7 +43,8 @@ class TestPackageConan(ConanFile):
         return sys.executable
 
     def test(self):
-        if not tools.cross_building(self):
-            with tools.environment_append({"PYTHONPATH": "lib"}):
-                self.run("{} {}".format(self._python_interpreter, os.path.join(
-                    self.source_folder, "test.py")), run_environment=True)
+        if can_run(self):
+            python_path = os.path.join(self.build_folder, self.cpp.build.libdirs[0])
+            with environment_append({"PYTHONPATH": python_path}):
+                module_path = os.path.join(self.source_folder, "test.py")
+                self.run(f"{self._python_interpreter} {module_path}", env="conanrun")
