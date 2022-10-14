5246a0a49d117c986ba71dc71413d4bf0e783da8
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-10-14 18:44:24 +0200

    (#13399) qt5: bump reqs

--- recipes/qt/5.x.x/conanfile.py
+++ recipes/qt/5.x.x/conanfile.py
@@ -161,12 +161,12 @@ class QtConan(ConanFile):
         if self._settings_build.os == "Windows" and self._is_msvc:
             self.build_requires("jom/1.1.3")
         if self.options.qtwebengine:
-            self.build_requires("ninja/1.11.0")
+            self.build_requires("ninja/1.11.1")
             self.build_requires("nodejs/16.3.0")
             self.build_requires("gperf/3.1")
             # gperf, bison, flex, python >= 2.7.5 & < 3
             if self.settings.os != "Windows":
-                self.build_requires("bison/3.7.6")
+                self.build_requires("bison/3.8.2")
                 self.build_requires("flex/2.6.4")
             else:
                 self.build_requires("winflexbison/2.5.24")
@@ -362,7 +362,7 @@ class QtConan(ConanFile):
             if is_apple_os(self):
                 self.requires("moltenvk/1.1.10")
         if self.options.with_glib:
-            self.requires("glib/2.73.3")
+            self.requires("glib/2.74.0")
         # if self.options.with_libiconv: # QTBUG-84708
         #     self.requires("libiconv/1.16")# QTBUG-84708
         if self.options.with_doubleconversion and not self.options.multiconfiguration:
@@ -374,24 +374,24 @@ class QtConan(ConanFile):
         if self.options.get_safe("with_icu", False):
             self.requires("icu/71.1")
         if self.options.get_safe("with_harfbuzz", False) and not self.options.multiconfiguration:
-            self.requires("harfbuzz/5.1.0")
+            self.requires("harfbuzz/5.3.0")
         if self.options.get_safe("with_libjpeg", False) and not self.options.multiconfiguration:
             if self.options.with_libjpeg == "libjpeg-turbo":
                 self.requires("libjpeg-turbo/2.1.4")
             else:
-                self.requires("libjpeg/9d")
+                self.requires("libjpeg/9e")
         if self.options.get_safe("with_libpng", False) and not self.options.multiconfiguration:
-            self.requires("libpng/1.6.37")
+            self.requires("libpng/1.6.38")
         if self.options.with_sqlite3 and not self.options.multiconfiguration:
-            self.requires("sqlite3/3.39.2")
+            self.requires("sqlite3/3.39.4")
             self.options["sqlite3"].enable_column_metadata = True
         if self.options.get_safe("with_mysql", False):
-            self.requires("libmysqlclient/8.0.29")
+            self.requires("libmysqlclient/8.0.30")
         if self.options.with_pq:
-            self.requires("libpq/14.2")
+            self.requires("libpq/14.5")
         if self.options.with_odbc:
             if self.settings.os != "Windows":
-                self.requires("odbc/2.3.9")
+                self.requires("odbc/2.3.11")
         if self.options.get_safe("with_openal", False):
             self.requires("openal/1.22.2")
         if self.options.get_safe("with_libalsa", False):
@@ -404,11 +404,11 @@ class QtConan(ConanFile):
         if self.options.with_zstd:
             self.requires("zstd/1.5.2")
         if self.options.qtwebengine and self.settings.os in ["Linux", "FreeBSD"]:
-            self.requires("expat/2.4.8")
+            self.requires("expat/2.4.9")
             self.requires("opus/1.3.1")
-            self.requires("xorg-proto/2021.4")
+            self.requires("xorg-proto/2022.2")
             self.requires("libxshmfence/1.3")
-            self.requires("nss/3.77")
+            self.requires("nss/3.83")
             self.requires("libdrm/2.4.109")
             self.requires("egl/system")
         if self.options.get_safe("with_gstreamer", False):
@@ -416,13 +416,13 @@ class QtConan(ConanFile):
         if self.options.get_safe("with_pulseaudio", False):
             self.requires("pulseaudio/14.2")
         if self.options.with_dbus:
-            self.requires("dbus/1.12.20")
+            self.requires("dbus/1.15.2")
         if self.options.qtwayland:
             self.requires("wayland/1.21.0")
         if self.settings.os in ['Linux', 'FreeBSD'] and self.options.with_gssapi:
             self.requires("krb5/1.18.3") # conan-io/conan-center-index#4102
         if self.options.get_safe("with_atspi"):
-            self.requires("at-spi2-core/2.45.90")
+            self.requires("at-spi2-core/2.46.0")
         if self.options.get_safe("with_md4c", False):
             self.requires("md4c/0.4.8")
 
