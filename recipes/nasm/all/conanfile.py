8831e9185e3092bd6de0064fd971d2b59c705ce5
Paul Harris <harris.pc@gmail.com>
2022-10-15 08:51:02 +0800

    nasm - reenable autoreconf()

--- recipes/nasm/all/conanfile.py
+++ recipes/nasm/all/conanfile.py
@@ -93,7 +93,7 @@ class NASMConan(ConanFile):
                 shutil.copy("ndisasm.exe", "ndisasmw.exe")
         else:
             autotools = Autotools(self)
-            # TODO Do we need this?  autotools.autoreconf()
+            autotools.autoreconf()
             autotools.configure()
 
             # GCC9 - "pure" attribute on function returning "void"
