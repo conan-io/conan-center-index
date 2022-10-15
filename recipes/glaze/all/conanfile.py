a15261a6fcba0a65b57282ef4c6d3769850f233d
toge <toge.mail@gmail.com>
2022-10-16 02:51:17 +0900

    update required conan version
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/glaze/all/conanfile.py
+++ recipes/glaze/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.scm import Version
 from conan.tools.layout import basic_layout
 import os
 
-required_conan_version = ">=1.47.0"
+required_conan_version = ">=1.50.0"
 
 class PackageConan(ConanFile):
     name = "glaze"
