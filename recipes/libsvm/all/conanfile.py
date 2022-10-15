9803ef04954abcbeeaaa19e7b94634793946987f
Riff, Eric <ericriff@gmail.com>
2022-10-14 23:21:20 -0300

    Modernize recipe

--- recipes/libsvm/all/conanfile.py
+++ recipes/libsvm/all/conanfile.py
@@ -1,8 +1,11 @@
-from conans import ConanFile, tools, CMake
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import get, copy
 
-required_conan_version = ">=1.33.0"
+import os
 
+required_conan_version = ">=1.52.0"
 
 class libsvmConan(ConanFile):
     name = "libsvm"
@@ -10,17 +13,16 @@ class libsvmConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.csie.ntu.edu.tw/~cjlin/libsvm/"
     license = "BSD-3-Clause"
-    topics = ("conan", "svm", "vector")
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+    topics = "conan", "svm", "vector"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False]
+        }
+    default_options = {
+        "shared": False,
+        "fPIC": True
+        }
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -43,23 +45,28 @@ class libsvmConan(ConanFile):
                 )
             )
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+    def layout(self):
+        cmake_layout(self, src_folder="source_subfolder")
 
-    def _configure_cmake(self):
-        if not self._cmake:
-            self._cmake = CMake(self)
-            self._cmake.configure()
-        return self._cmake
+    def export_sources(self):
+        copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version],
+                  destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
         cmake.build()
 
     def package(self):
-        self.copy("COPYRIGHT", src=self._source_subfolder, dst="licenses")
-        cmake = self._configure_cmake()
+        self.copy("COPYRIGHT", src=self.source_folder, dst="licenses")
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
