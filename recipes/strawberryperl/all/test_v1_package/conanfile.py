baec1b02c429b97a0d8fcd6aeeab14c00179a6ad
Chris Mc <christopherm@jfrog.com>
2022-10-15 20:55:02 -0700

    Update conanfile.py

--- recipes/strawberryperl/all/test_v1_package/conanfile.py
+++ recipes/strawberryperl/all/test_v1_package/conanfile.py
@@ -8,5 +8,5 @@ class DefaultNameConan(ConanFile):
     def test(self):
         if not tools.cross_building(self):
             self.run("perl --version", run_environment=True)
-            perl_script = os.path.join(self.source_folder, "list_files.pl")
+            perl_script = os.path.join(self.source_folder, os.pardir, "test_package", "list_files.pl")
             self.run(f"perl {perl_script}", run_environment=True)
