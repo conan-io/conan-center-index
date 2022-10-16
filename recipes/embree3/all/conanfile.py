5ae36168d9ccb8dd978440ae905083029c2f440f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-17 00:35:15 +0200

    remove vc runtime files

--- recipes/embree3/all/conanfile.py
+++ recipes/embree3/all/conanfile.py
@@ -192,7 +192,7 @@ class EmbreeConan(ConanFile):
 
         if self.settings.os == "Windows" and self.options.shared:
             for dll_pattern_to_remove in ["concrt*.dll", "msvcp*.dll", "vcruntime*.dll"]:
-                rm(self, dll_pattern_to_remove, os.path.join(self.package_folder))
+                rm(self, dll_pattern_to_remove, os.path.join(self.package_folder, "bin"))
         else:
             rmdir(self, os.path.join(self.package_folder, "bin"))
 
