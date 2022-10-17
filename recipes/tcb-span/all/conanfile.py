92c28f403d388d00b227af810f6da24e8959e74f
toge <toge.mail@gmail.com>
2022-10-18 07:30:10 +0900

    remove Version
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/tcb-span/all/conanfile.py
+++ recipes/tcb-span/all/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
 from conan.tools.files import get, copy
 from conan.tools.build import check_min_cppstd
-from conan.tools.scm import Version
 from conan.tools.layout import basic_layout
 import os
 
