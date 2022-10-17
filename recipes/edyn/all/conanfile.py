27660c5c3b1bf48980e718013d4e42c98c9321c9
Raziel Alphadios <raziely@gmail.com>
2022-10-18 00:05:45 +0300

    Conan 2.0 migration and other fixes

--- recipes/edyn/all/conanfile.py
+++ recipes/edyn/all/conanfile.py
@@ -1,13 +1,13 @@
-from conans import CMake, ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
-import functools
+from conan import ConanFile
+from conan.tools import files, build, scm, layout
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
 import os
 
 required_conan_version = ">=1.45.0"
 
 class EdynConan(ConanFile):
     name = "edyn"
-    version = "1.1.0"
     description = "Edyn is a real-time physics engine organized as an ECS"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
@@ -25,53 +25,65 @@ class EdynConan(ConanFile):
         "fPIC": True,
         "floating_type": "float",
     }
-    generators = "cmake", "cmake_find_package_multi"
 
     def requirements(self):
-        self.requires("entt/3.9.0")
+        self.requires("entt/3.10.1")
 
     @property
     def _compiler_required(self):
         return {
             "gcc": "9.3", # GCC 9.3 started supporting attributes in constructor arguments
+            "clang": "8",
+            "Visual Studio": "16",
+            "msvc": "192"
         }
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 17)
+            build.check_min_cppstd(self, 17)
         try:
             minimum_required_compiler_version = self._compiler_required[str(self.settings.compiler)]
-            if tools.Version(self.settings.compiler.version) < minimum_required_compiler_version:
+            if scm.Version(self.settings.compiler.version) < minimum_required_compiler_version:
                 raise ConanInvalidConfiguration("This package requires C++17 support. The current compiler does not support it.")
         except KeyError:
             self.output.warn("This recipe has no support for the current compiler. Please consider adding it.")
 
-    def source(self):
-        self.run("git clone https://github.com/xissburg/edyn.git")
-        self.run("cd edyn && git checkout v" + self.version)
+    def layout(self):
+        layout.cmake_layout(self, src_folder="edyn")
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["EDYN_INSTALL"] = True
-        cmake.configure(source_folder="edyn")
-        return cmake
+    def source(self):
+        files.get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["EDYN_INSTALL"] = True
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src="edyn")
-        cmake = self._configure_cmake()
+        #cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "bin"), "*.pdb")
+        files.rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        files.rmdir(self, os.path.join(self.package_folder, "share"))
+        files.rm(self, os.path.join(self.package_folder, "bin"), "*.pdb")
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = files.collect_libs(self)
 
         self.cpp_info.set_property("cmake_find_mode", "both")
         self.cpp_info.set_property("cmake_module_file_name", "Edyn")
--- recipes/edyn/all/test_package/conanfile.py
+++ recipes/edyn/all/test_package/conanfile.py
@@ -1,9 +1,13 @@
-from conans import ConanFile, CMake, tools
-import os
+from conan import ConanFile
+from conan.tools import build, layout
+from conan.tools.cmake import CMake
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def layout(self):
+        layout.cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
@@ -11,5 +15,5 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not build.cross_building(self):
             self.run("test_package", run_environment=True)
