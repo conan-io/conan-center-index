4a125fb0692486dae496698eef90be449bf22276
toge <toge.mail@gmail.com>
2022-10-17 16:07:22 +0900

    fix ConanInvalidConfiguration path

--- recipes/usockets/all/conanfile.py
+++ recipes/usockets/all/conanfile.py
@@ -1,9 +1,9 @@
 from conan import ConanFile
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file, chdir
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir, chdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
+from conan.errors import ConanInvalidConfiguration
 from conans import MSBuild, AutoToolsBuildEnvironment
-from conans.errors import ConanInvalidConfiguration
 
 import os
 
