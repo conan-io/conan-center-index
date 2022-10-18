8c77bc42efd524755f62e632dac348932c87ef03
toge <toge.mail@gmail.com>
2022-10-18 21:24:41 +0900

    update required recipe version

--- recipes/aws-c-s3/all/conanfile.py
+++ recipes/aws-c-s3/all/conanfile.py
@@ -47,12 +47,14 @@ class AwsCS3(ConanFile):
 
     def requirements(self):
         self.requires("aws-c-common/0.8.2")
-        if Version(self.version) >= "0.1.49":
-            self.requires("aws-c-io/0.13.4")
-        else:
+        if Version(self.version) < "0.1.49":
             self.requires("aws-c-io/0.10.20")
-        self.requires("aws-c-http/0.6.22")
-        self.requires("aws-c-auth/0.6.17")
+            self.requires("aws-c-http/0.6.13")
+            self.requires("aws-c-auth/0.6.11")
+        else:
+            self.requires("aws-c-io/0.13.4")
+            self.requires("aws-c-http/0.6.22")
+            self.requires("aws-c-auth/0.6.17")
         if Version(self.version) >= "0.1.36":
             self.requires("aws-checksums/0.1.13")
 
