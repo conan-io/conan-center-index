34334d8800766b51a87a42b1d9f48006337631e8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-18 11:54:07 +0200

    fix MinGW

--- recipes/yasm/all/conanfile.py
+++ recipes/yasm/all/conanfile.py
@@ -1,14 +1,14 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
-from conan.tools.layout import basic_layout, cmake_layout
-from conan.tools.microsoft import is_msvc
-
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, unix_path
 import os
 
-
 required_conan_version = ">=1.52.0"
+
 
 class YASMConan(ConanFile):
     name = "yasm"
@@ -27,23 +27,38 @@ class YASMConan(ConanFile):
         export_conandata_patches(self)
 
     def configure(self):
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+        try:
+            del self.settings.compiler.libcxx
+        except Exception:
+            pass
+        try:
+            del self.settings.compiler.cppstd
+        except Exception:
+            pass
 
     def layout(self):
         if is_msvc(self):
-            cmake_layout(self)
+            cmake_layout(self, src_folder="src")
         else:
-            basic_layout(self)
+            basic_layout(self, src_folder="src")
 
     def package_id(self):
         del self.info.settings.compiler
+
+    def build_requirements(self):
+        if self._settings_build.os == "Windows" and not is_msvc(self):
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
+                self.tool_requires("msys2/cci.latest")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version][0],
                   destination=self.source_folder, strip_root=True)
 
     def _generate_autotools(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+
         tc = AutotoolsToolchain(self)
         enable_debug = "yes" if self.settings.build_type == "Debug" else "no"
         tc.configure_args.extend([
@@ -85,7 +100,8 @@ class YASMConan(ConanFile):
             rmdir(self, os.path.join(self.package_folder, "lib"))
         else:
             autotools = Autotools(self)
-            autotools.install()
+            # TODO: replace by autotools.install() once https://github.com/conan-io/conan/issues/12153 fixed
+            autotools.install(args=[f"DESTDIR={unix_path(self, self.package_folder)}"])
             rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
