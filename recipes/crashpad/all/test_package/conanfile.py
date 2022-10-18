ec8456c0e9eb865760b7cd461acccd8aad6f11f7
Tamás PEREGI <petamas@gmail.com>
2022-10-18 20:27:26 +0200

    Fix linter errors in test_package/conanfile.py

--- recipes/crashpad/all/test_package/conanfile.py
+++ recipes/crashpad/all/test_package/conanfile.py
@@ -1,4 +1,9 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conan.tools.files import mkdir
+
+from conans import CMake
+
 import os
 
 
@@ -12,9 +17,9 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not cross_building(self):
             test_env_dir = "test_env"
-            tools.mkdir(test_env_dir)
+            mkdir(self, test_env_dir)
             bin_path = os.path.join("bin", "test_package")
             handler_exe = "crashpad_handler.exe" if self.settings.os == "Windows" else "crashpad_handler"
             handler_bin_path = os.path.join(self.deps_cpp_info["crashpad"].rootpath, "bin", handler_exe)
