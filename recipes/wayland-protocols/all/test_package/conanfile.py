566d352c9db71b0a82053cbb2349d700dde2a85e
Jordan Williams <jordan@jwillikers.com>
2022-10-18 09:16:07 -0500

    Add check for build profile

--- recipes/wayland-protocols/all/test_package/conanfile.py
+++ recipes/wayland-protocols/all/test_package/conanfile.py
@@ -11,6 +11,10 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     test_type = "explicit"
 
+    @property
+    def _has_build_profile(self):
+        return hasattr(self, "settings_build")
+
     def requirements(self):
         self.requires(self.tested_reference_str)
         self.requires("wayland/1.21.0")
@@ -26,10 +30,12 @@ class TestPackageConan(ConanFile):
     def generate(self):
         tc = MesonToolchain(self)
         tc.project_options["build.pkg_config_path"] = self.generators_folder
+        tc.project_options["has_build_profile"] = self._has_build_profile
         tc.generate()
         pkg_config_deps = PkgConfigDeps(self)
-        pkg_config_deps.build_context_activated = ["wayland"]
-        pkg_config_deps.build_context_suffix = {"wayland": "_BUILD"}
+        if self._has_build_profile:
+            pkg_config_deps.build_context_activated = ["wayland"]
+            pkg_config_deps.build_context_suffix = {"wayland": "_BUILD"}
         pkg_config_deps.generate()
         virtual_build_env = VirtualBuildEnv(self)
         virtual_build_env.generate()
