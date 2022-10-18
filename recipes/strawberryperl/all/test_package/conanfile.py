4117a1f75917ca4b4dab02820de2738228d57204
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-19 00:56:43 +0200

    self.conf.get instead of self.conf_info.get

--- recipes/strawberryperl/all/test_package/conanfile.py
+++ recipes/strawberryperl/all/test_package/conanfile.py
@@ -12,7 +12,6 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         self.run("perl --version")
-        conf_perl = self.conf_info.get("user.strawberryperl:perl")
-        self.run(f"{conf_perl} --version")
+        self.run(f"{self.conf.get('user.strawberryperl:perl')} --version")
         perl_script = os.path.join(self.source_folder, "list_files.pl")
         self.run(f"perl {perl_script}")
