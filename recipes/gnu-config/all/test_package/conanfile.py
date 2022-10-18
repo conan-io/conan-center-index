361fea5e6770ad8ed519cc40278a1ff8e190a676
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-18 18:33:55 +0200

    conan v2 support

--- recipes/gnu-config/all/test_package/conanfile.py
+++ recipes/gnu-config/all/test_package/conanfile.py
@@ -1,22 +1,28 @@
-from conans import ConanFile, tools
-from conans.errors import ConanException
+from conan import ConanFile
+from conan.errors import ConanException
+from conans import tools as tools_legacy
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
+    generators = "VirtualBuildEnv"
+    test_type = "explicit"
 
     @property
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
     def build_requirements(self):
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/cci.latest")
+        self.tool_requires(self.tested_reference_str)
+        if self._settings_build.os == "Windows":
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
+                self.tool_requires("msys2/cci.latest")
 
     def test(self):
-        triplet = tools.get_gnu_triplet(str(self.settings.os), str(self.settings.arch), str(self.settings.compiler))
-        self.run("config.guess", run_environment=True, win_bash=tools.os_info.is_windows)
+        triplet = tools_legacy.get_gnu_triplet(str(self.settings.os), str(self.settings.arch), str(self.settings.compiler))
+        self.run("config.guess")
         try:
-            self.run("config.sub {}".format(triplet), run_environment=True, win_bash=tools.os_info.is_windows)
+            self.run(f"config.sub {triplet}")
         except ConanException:
             self.output.info("Current configuration is not supported by GNU config.\nIgnoring...")
