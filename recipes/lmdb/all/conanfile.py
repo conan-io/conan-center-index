9dde9b5bf4ede395780aec43e391abbab0574748
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-18 11:08:08 +0200

    bum required_conan_version
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/lmdb/all/conanfile.py
+++ recipes/lmdb/all/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
 import os
 
-required_conan_version = ">=1.29.0"
+required_conan_version = ">=1.46.0"
 
 
 class lmdbConan(ConanFile):
