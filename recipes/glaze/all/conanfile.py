88594aece5c2b13144054e4011620fe2b31aa3a6
toge <toge.mail@gmail.com>
2022-10-18 21:14:14 +0900

    only libdirs is set empty
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/glaze/all/conanfile.py
+++ recipes/glaze/all/conanfile.py
@@ -72,6 +72,4 @@ class PackageConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.bindirs = []
-        self.cpp_info.frameworkdirs = []
         self.cpp_info.libdirs = []
-        self.cpp_info.resdirs = []
