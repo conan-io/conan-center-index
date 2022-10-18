f5db67ad5e26b2612313b684aba58937449a6f8b
Paul Harris <harris.pc@gmail.com>
2022-10-18 17:23:20 +0800

    Update recipes/theora/all/conanfile.py
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/theora/all/conanfile.py
+++ recipes/theora/all/conanfile.py
@@ -51,7 +51,7 @@ class TheoraConan(ConanFile):
         self.requires("ogg/1.3.5")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
