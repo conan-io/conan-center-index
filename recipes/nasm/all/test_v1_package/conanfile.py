f07530b3eada4b62352ec149392a8b831050f8c4
Paul Harris <harris.pc@gmail.com>
2022-10-19 12:35:31 +0800

    nasm - fix test_v1_package

--- recipes/nasm/all/test_v1_package/conanfile.py
+++ recipes/nasm/all/test_v1_package/conanfile.py
@@ -11,7 +11,7 @@ class TestPackageConan(ConanFile):
             asm_file = os.path.join(self.source_folder, "hello_linux.asm")
             out_file = os.path.join(self.build_folder, "hello_linux.o")
             bin_file = os.path.join(self.build_folder, "hello_linux")
-            self.run("nasm -felf64 {asm_file} -o {out_file}", run_environment=True)
+            self.run(f"nasm -felf64 {asm_file} -o {out_file}", run_environment=True)
             if self.settings.os == "Linux" and self.settings.arch == "x86_64":
                 ld = tools.get_env("LD", "ld")
                 self.run(f"{ld} hello_linux.o -o {bin_file}", run_environment=True)
