e4b648a9744ff53f8c86d8725603ed02f478b242
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-10-19 17:30:04 +0200

    Prepare for next orcania release that will ship CMake config files

--- recipes/orcania/all/conanfile.py
+++ recipes/orcania/all/conanfile.py
@@ -80,6 +80,8 @@ class OrcaniaConan(ConanFile):
         cmake = CMake(self)
         cmake.install()
 
+        rmdir(self, os.path.join(self.package_folder, "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         rmdir(self, os.path.join(self.package_folder, "share"))
 
