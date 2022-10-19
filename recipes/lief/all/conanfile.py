843317aed0272023e4be86eb6616c645f8f9ff8e
toge <toge.mail@gmail.com>
2022-10-19 14:43:49 +0900

    (#13526) lief: support conan v2 and update dependencies
    * lief: support conan v2
    
    * fix settings order in test_v1_package
    
    * fix validation
    
    * add filename definition
    
    * don't use collect_libs
    
    * don't use collect_libs
    
    * use cmake_find_package_multi
    
    * drop support libstdc++
    
    * drop support clang and libstdc++
    
    * link math lib
    
    * add msvc version check

--- recipes/lief/all/conanfile.py
+++ recipes/lief/all/conanfile.py
@@ -1,18 +1,23 @@
-import os
-from conans import ConanFile, tools, CMake
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import is_msvc
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
+import os
+
+required_conan_version = ">=1.52.0"
 
 class LiefConan(ConanFile):
     name = "lief"
     description = "Library to Instrument Executable Formats"
-    topics = ("conan", "lief", "executable", "elf")
+    license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/lief-project/LIEF"
-    license = "Apache-2.0"
-    exports_sources = "CMakeLists.txt", "patches/*"
-    generators = "cmake",
-    settings = "os", "compiler", "build_type", "arch"
+    topics = ("executable", "elf", "pe", "mach-o")
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
@@ -42,80 +47,104 @@ class LiefConan(ConanFile):
         "with_vdex": True,
     }
 
-    _cmake = None
-
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _minimum_cpp_standard(self):
+        return 11
 
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
-        if self.settings.compiler == "Visual Studio" and tools.Version(self.settings.compiler.version.value) <= 14 and self.options.shared:
-            raise ConanInvalidConfiguration("{} {} does not support Visual Studio <= 14 with shared:True".format(self.name, self.version))
+        if self.options.shared:
+            try:
+                del self.options.fPIC
+            except Exception:
+                pass
+
+    def validate(self):
+        if self.info.settings.compiler.cppstd:
+            check_min_cppstd(self, self._minimum_cpp_standard)
+
+        if ((self.info.settings.compiler == "Visual Studio" and Version(self.info.settings.compiler.version) <= "14")
+            or
+            (self.info.settings.compiler == "msvc" and Version(self.info.settings.compiler.version) <= "190")) \
+            and self.options.shared:
+            raise ConanInvalidConfiguration(f"{self.ref} does not support Visual Studio <= 14 with shared:True")
+
+        if self.info.settings.compiler.get_safe("libcxx") == "libstdc++":
+            raise ConanInvalidConfiguration(f"{self.ref} does not support libstdc++")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("rang/3.1.0")
-        self.requires("mbedtls/2.16.3-apache")
+        self.requires("rang/3.2")
+        self.requires("mbedtls/3.2.1")
         if self.options.with_json:
-            self.requires("nlohmann_json/3.9.1")
+            self.requires("nlohmann_json/3.11.2")
         if self.options.with_frozen:
-            self.requires("frozen/1.0.0")
+            self.requires("frozen/1.1.1")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "LIEF-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["LIEF_ART"] = self.options.with_art
-        self._cmake.definitions["LIEF_DEX"] = self.options.with_dex
-        self._cmake.definitions["LIEF_ELF"] = self.options.with_elf
-        self._cmake.definitions["LIEF_OAT"] = self.options.with_oat
-        self._cmake.definitions["LIEF_PE"] = self.options.with_pe
-        self._cmake.definitions["LIEF_VDEX"] = self.options.with_vdex
-        self._cmake.definitions["LIEF_MACHO"] = self.options.with_macho
-        self._cmake.definitions["LIEF_ENABLE_JSON"] = self.options.with_json
-        self._cmake.definitions["LIEF_DISABLE_FROZEN"] = not self.options.with_frozen
-        self._cmake.definitions["LIEF_C_API"] = self.options.with_c_api
-        self._cmake.definitions["LIEF_EXAMPLES"] = False
-        self._cmake.definitions["LIEF_TESTS"] = False
-        self._cmake.definitions["LIEF_DOC"] = False
-        self._cmake.definitions["LIEF_LOGGING"] = False
-        self._cmake.definitions["LIEF_PYTHON_API"] = False
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["LIEF_ART"] = self.options.with_art
+        tc.variables["LIEF_DEX"] = self.options.with_dex
+        tc.variables["LIEF_ELF"] = self.options.with_elf
+        tc.variables["LIEF_OAT"] = self.options.with_oat
+        tc.variables["LIEF_PE"] = self.options.with_pe
+        tc.variables["LIEF_VDEX"] = self.options.with_vdex
+        tc.variables["LIEF_MACHO"] = self.options.with_macho
+        tc.variables["LIEF_ENABLE_JSON"] = self.options.with_json
+        tc.variables["LIEF_DISABLE_FROZEN"] = not self.options.with_frozen
+        tc.variables["LIEF_C_API"] = self.options.with_c_api
+        tc.variables["LIEF_EXAMPLES"] = False
+        tc.variables["LIEF_TESTS"] = False
+        tc.variables["LIEF_DOC"] = False
+        tc.variables["LIEF_LOGGING"] = False
+        tc.variables["LIEF_PYTHON_API"] = False
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
-        self.cpp_info.names["pkg_config"] = "lief"
-        self.cpp_info.names["cmake_find_package"] = "LIEF"
-        self.cpp_info.names["cmake_find_package_multi"] = "LIEF"
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.defines = ["_GLIBCXX_USE_CXX11_ABI=1"]
+        self.cpp_info.libs = ["LIEF"]
+
+        self.cpp_info.set_property("cmake_file_name", "LIEF")
+        self.cpp_info.set_property("cmake_target_name", "LIEF::LIEF")
+        self.cpp_info.set_property("pkg_config_name", "LIEF")
+
         if self.options.shared:
             self.cpp_info.defines.append("LIEF_IMPORT")
-        if self.settings.compiler == "Visual Studio":
+        if is_msvc(self):
             self.cpp_info.cxxflags += ["/FIiso646.h"]
         if self.settings.os == "Windows" and self.options.shared:
             self.cpp_info.system_libs = ["ws2_32"]
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.append("m")
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.filenames["cmake_find_package"] = "LIEF"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "LIEF"
+        self.cpp_info.names["cmake_find_package"] = "LIEF"
+        self.cpp_info.names["cmake_find_package_multi"] = "LIEF"
+        self.cpp_info.names["pkg_config"] = "LIEF"
