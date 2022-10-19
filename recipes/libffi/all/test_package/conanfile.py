adf40f0aef65b7108d1f92a63c2d24a6eab1519a
Jordan Williams <jordan@jwillikers.com>
2022-10-18 21:16:23 -0500

    Be sure to set DISABLE_FFI_CALL properly

--- recipes/libffi/all/test_package/conanfile.py
+++ recipes/libffi/all/test_package/conanfile.py
@@ -1,12 +1,13 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.cmake import cmake_layout, CMake, CMakeDeps, CMakeToolchain
+from conan.tools.env import VirtualRunEnv
+from conan.tools.microsoft import msvc_runtime_flag
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
@@ -14,6 +15,16 @@ class TestPackageConan(ConanFile):
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        cmake_deps = CMakeDeps(self)
+        cmake_deps.generate()
+        tc = CMakeToolchain(self)
+        if "d" in msvc_runtime_flag(self):
+            tc.preprocessor_definitions["DISABLE_FFI_CALL"] = 1
+        tc.generate()
+        virtual_run_env = VirtualRunEnv(self)
+        virtual_run_env.generate()
 
     def build(self):
         cmake = CMake(self)
