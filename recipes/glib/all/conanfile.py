55b89aced6d61e7dc46e7f08e3365aa27eab03d0
Jordan Williams <jordan@jwillikers.com>
2022-10-20 13:12:38 -0500

    Check exceptions in configure

--- recipes/glib/all/conanfile.py
+++ recipes/glib/all/conanfile.py
@@ -107,7 +107,8 @@ class GLibConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("meson/0.63.3")
-        self.tool_requires("pkgconf/1.9.3")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/1.9.3")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version],
