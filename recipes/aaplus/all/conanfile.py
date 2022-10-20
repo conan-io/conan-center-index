908ed625c381b9dbe586749b53ea2f1204fde6d4
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-10-20 08:03:39 +0000

    add error in aaplus

--- recipes/aaplus/all/conanfile.py
+++ recipes/aaplus/all/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
+from conans.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, get, load, save
