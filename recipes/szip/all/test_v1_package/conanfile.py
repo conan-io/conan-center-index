44b3533a912afe315431a1561202a01bc8337049
Paul Harris <harris.pc@gmail.com>
2022-10-21 09:56:34 +0800

    Fixed tests - one won't pass

--- recipes/szip/all/test_v1_package/conanfile.py
+++ recipes/szip/all/test_v1_package/conanfile.py
@@ -15,3 +15,5 @@ class TestPackageConan(ConanFile):
         if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
+            bin_path = os.path.join("bin", "test_package-b")
+            self.run(bin_path, run_environment=True)
