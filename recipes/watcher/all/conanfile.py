936adf45d6c3352b7260064bb020c1b9d50fedef
toge <toge.mail@gmail.com>
2022-10-21 16:07:01 +0900

    update compiler version check

--- recipes/watcher/all/conanfile.py
+++ recipes/watcher/all/conanfile.py
@@ -2,14 +2,12 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import get, copy
 from conan.tools.build import check_min_cppstd
-from conan.tools.scm import Version
 from conan.tools.apple import is_apple_os
+from conan.tools.microsoft import check_min_vs
 from conan.tools.layout import basic_layout
 import os
 
-
-required_conan_version = ">=1.47.0"
-
+required_conan_version = ">=1.49.0"
 
 class WatcherConan(ConanFile):
     name = "watcher"
@@ -28,11 +26,9 @@ class WatcherConan(ConanFile):
     @property
     def _compilers_minimum_version(self):
         return {
-            "Visual Studio": "16",
-            "msvc": "192",
             "gcc": "11",
             "clang": "13",
-            "apple-clang": "13",
+            "apple-clang": "13.1",
         }
 
     def layout(self):
@@ -44,10 +40,18 @@ class WatcherConan(ConanFile):
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
             check_min_cppstd(self, self._minimum_cpp_standard)
+        check_min_vs(self, 192)
+
+        def loose_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
+
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.get_safe("compiler.version")) < minimum_version:
+        if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
             raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._minimum_cpp_standard}, which your compiler does not support."
+                f"{self.name} {self.version} requires C++{self._minimum_cpp_standard}, which your compiler does not support.",
             )
 
     def source(self):
