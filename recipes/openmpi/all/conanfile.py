00147dd99f99a459a24c42b3193aa694c7ffdac3
Paul Harris <harris.pc@gmail.com>
2022-10-21 14:01:36 +0800

    Continuing the upgrade

--- recipes/openmpi/all/conanfile.py
+++ recipes/openmpi/all/conanfile.py
@@ -1,16 +1,19 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
-from conans.errors import ConanInvalidConfiguration
-import functools
+from conan import ConanFile
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.tools.build import check_min_cppstd, cross_building
+from conan.tools.files import copy, get, rm, rmdir, apply_conandata_patches, export_conandata_patches
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps, PkgConfigDeps
+from conan.tools.layout import basic_layout
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.52.0"
 
 
 class OpenMPIConan(ConanFile):
     name = "openmpi"
     homepage = "https://www.open-mpi.org"
     url = "https://github.com/conan-io/conan-center-index"
-    topics = ("mpi", "openmpi")
+    topics = "mpi", "openmpi"
     description = "A High Performance Message Passing Library"
     license = "BSD-3-Clause"
 
@@ -26,62 +29,80 @@ class OpenMPIConan(ConanFile):
         "fortran": "no",
     }
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            try:
+                del self.options.fPIC
+            except Exception:
+                pass
+        try:
+            del self.settings.compiler.libcxx
+        except Exception:
+            pass
+        try:
+            del self.settings.compiler.cppstd
+        except Exception:
+            pass
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def requirements(self):
         # FIXME : self.requires("libevent/2.1.12") - try to use libevent from conan
-        self.requires("zlib/1.2.12")
+        self.requires("zlib/1.2.13")
+        # used for hwloc component...
+        self.requires("libudev/system")
+        self.requires("libpciaccess/0.16")
 
     def validate(self):
         if self.settings.os == "Windows":
             raise ConanInvalidConfiguration("OpenMPI doesn't support Windows")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+                  destination=self.source_folder, strip_root=True)
 
-    @functools.lru_cache(1)
-    def _configure_autotools(self):
-        autotools = AutoToolsBuildEnvironment(self)
+    def generate(self):
+        tc = AutotoolsToolchain(self)
         yes_no = lambda v: "yes" if v else "no"
-        args = [
-            "--disable-wrapper-rpath",
-            "--disable-wrapper-runpath",
-            "--enable-shared={}".format(yes_no(self.options.shared)),
-            "--enable-static={}".format(yes_no(not self.options.shared)),
-            "--with-pic={}".format(yes_no(self.options.get_safe("fPIC", True))),
-            "--enable-mpi-fortran={}".format(str(self.options.fortran)),
-            "--with-zlib={}".format(self.deps_cpp_info["zlib"].rootpath),
-            "--with-zlib-libdir={}".format(self.deps_cpp_info["zlib"].lib_paths[0]),
-            "--datarootdir=${prefix}/res",
-        ]
+        tc.configure_args.append("--disable-wrapper-rpath")
+        tc.configure_args.append("--disable-wrapper-runpath")
+        tc.configure_args.append("--enable-shared={}".format(yes_no(self.options.shared)))
+        tc.configure_args.append("--enable-static={}".format(yes_no(not self.options.shared)))
+        tc.configure_args.append("--with-pic={}".format(yes_no(self.options.get_safe("fPIC", True))))
+        tc.configure_args.append("--enable-mpi-fortran={}".format(str(self.options.fortran)))
+        tc.configure_args.append("--with-zlib={}".format(self.deps_cpp_info["zlib"].rootpath))
+        tc.configure_args.append("--with-zlib-libdir={}".format(self.deps_cpp_info["zlib"].lib_paths[0]))
+        tc.configure_args.append("--datarootdir=${prefix}/res")
         if self.settings.build_type == "Debug":
-            args.append("--enable-debug")
-        autotools.configure(args=args)
-        return autotools
+            tc.configure_args.append("--enable-debug")
+        tc.generate()
+
+        tc = AutotoolsDeps(self)
+        tc.generate()
+
+        env = VirtualBuildEnv(self)
+        env.generate()
+
+        if not cross_building(self):
+            env = VirtualRunEnv(self)
+            env.generate(scope="build")
 
     def build(self):
-        with tools.chdir(self._source_subfolder):
-            autotools = self._configure_autotools()
-            autotools.make()
+        autotools = Autotools(self)
+        autotools.autoreconf()
+        autotools.configure()
+        autotools.make()
 
     def package(self):
-        self.copy(pattern="LICENSE", src=self._source_subfolder, dst="licenses")
-        with tools.chdir(self._source_subfolder):
-            autotools = self._configure_autotools()
-            autotools.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        tools.rmdir(os.path.join(self.package_folder, "etc"))
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
+        copy(self, pattern="LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        autotools = Autotools(self)
+        autotools.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "etc"))
+        remove_files_by_mask(self, os.path.join(self.package_folder, "lib"), "*.la")
 
     def package_info(self):
         self.cpp_info.libs = ["mpi", "open-rte", "open-pal"]
--- recipes/openmpi/all/test_package/conanfile.py
+++ recipes/openmpi/all/test_package/conanfile.py
@@ -1,10 +1,19 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
@@ -12,7 +21,8 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if can_run(self):
             mpiexec = os.path.join(os.environ['MPI_BIN'], 'mpiexec')
-            command = '%s -mca plm_rsh_agent yes -np 2 %s' % (mpiexec, os.path.join("bin", "test_package"))
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            command = '%s -mca plm_rsh_agent yes -np 2 %s' % (mpiexec, bin_path)
             self.run(command, run_environment=True)
