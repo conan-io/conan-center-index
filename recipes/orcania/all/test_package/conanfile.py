52f8fafac702cada351b088da030f2d9438c97dd
Anonymous Maarten <madebr@users.noreply.github.com>
2022-10-21 16:37:30 +0200

    Think conan 2.0
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/orcania/all/test_package/conanfile.py
+++ recipes/orcania/all/test_package/conanfile.py
@@ -17,7 +17,7 @@ class TestPackageConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["ORCANIA_SHARED"] = self.options["orcania"].shared
+        tc.variables["ORCANIA_SHARED"] = self.dependencies["orcania"].options.shared
         tc.generate()
 
     def build(self):
