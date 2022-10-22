464e5265b59f72e76d91cf1c623b32afecac145d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-22 22:15:13 +0200

    conan v2 support

--- recipes/tinyobjloader/all/test_package/test_package.cpp
+++ recipes/tinyobjloader/all/test_package/test_package.cpp
@@ -1,20 +1,35 @@
 #include <tiny_obj_loader.h>
-#include <vector>
-#include <iostream>
 
-int main (void)
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char **argv)
 {
+    if (argc < 3) {
+        std::cerr << "Need at least two arguments" << std::endl;
+        return 1;
+    }
+
     tinyobj::attrib_t attrib;
     std::vector<tinyobj::shape_t> shapes;
     std::vector<tinyobj::material_t> materials;
+#ifdef TINYOBJLOADER_GE_1_0_7
     std::string warn;
+#endif
     std::string err;
     bool ret = tinyobj::LoadObj(
-        &attrib, &shapes, &materials, &warn, &err, "cube.obj");
+        &attrib, &shapes, &materials,
+#ifdef TINYOBJLOADER_GE_1_0_7
+        &warn,
+#endif
+        &err, argv[1], argv[2]);
 
+#ifdef TINYOBJLOADER_GE_1_0_7
     if (!warn.empty()) {
         std::cout << "WARN: " << warn << std::endl;
     }
+#endif
 
     if (!err.empty()) {
         std::cerr << "ERR: " << err << std::endl;
