0b2f83e9c7f69e1982ceff0ded071be443378cc1
toge <toge.mail@gmail.com>
2022-10-24 14:44:46 +0900

    (#13685) daw_utf_range: update dependencies and support conan v2

--- recipes/daw_utf_range/all/conanfile.py
+++ recipes/daw_utf_range/all/conanfile.py
@@ -1,9 +1,14 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import check_min_vs, is_msvc
+from conan.tools.files import get, copy, rmdir
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
 import os
 
-from conans.errors import ConanInvalidConfiguration
-from conans import ConanFile, CMake, tools
-
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.47.0"
 
 class DawUtfRangeConan(ConanFile):
     name = "daw_utf_range"
@@ -13,62 +18,74 @@ class DawUtfRangeConan(ConanFile):
     homepage = "https://github.com/beached/utf_range/"
     topics = ("utf", "validator", "iterator")
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi", "cmake_find_package"
     no_copy_source = True
 
-    _compiler_required_cpp17 = {
-        "Visual Studio": "16",
-        "gcc": "8",
-        "clang": "7",
-        "apple-clang": "12.0",
-    }
+    @property
+    def _minimum_cpp_standard(self):
+        return 17
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "8",
+            "clang": "7",
+            "apple-clang": "12.0",
+        }
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("daw_header_libraries/2.72.0")
+
+    def package_id(self):
+        self.info.clear()
 
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
-            tools.check_min_cppstd(self, "17")
-
-        minimum_version = self._compiler_required_cpp17.get(str(self.settings.compiler), False)
-        if minimum_version:
-            if tools.Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not support.".format(self.name))
-        else:
-            self.output.warn("{} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name))
-
-    def requirements(self):
-        self.requires("daw_header_libraries/2.68.1")
+            check_min_cppstd(self, self._minimum_cpp_standard)
+        check_min_vs(self, 192)
+        if not is_msvc(self):
+            minimum_version = self._compilers_minimum_version.get(str(self.info.settings.compiler), False)
+            if minimum_version and Version(self.info.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration(
+                    f"{self.ref} requires C++{self._minimum_cpp_standard}, which your compiler does not support."
+                )
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    def _configure_cmake(self):
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["DAW_USE_PACKAGE_MANAGEMENT"] = True
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
         cmake = CMake(self)
-        cmake.definitions["DAW_USE_PACKAGE_MANAGEMENT"] = True
-        cmake.configure(source_folder=self._source_subfolder)
-        return cmake
+        cmake.configure()
 
     def package(self):
-        self.copy("LICENSE*", "licenses", self._source_subfolder)
-
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE*", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
-
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-
-    def package_id(self):
-        self.info.header_only()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.filenames["cmake_find_package"] = "daw-utf-range"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "daw-utf-range"
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
         self.cpp_info.set_property("cmake_file_name", "daw-utf-range")
-        self.cpp_info.names["cmake_find_package"] = "daw"
-        self.cpp_info.names["cmake_find_package_multi"] = "daw"
         self.cpp_info.set_property("cmake_target_name", "daw::daw-utf-range")
-        self.cpp_info.components["daw"].names["cmake_find_package"] = "daw-utf-range"
-        self.cpp_info.components["daw"].names["cmake_find_package_multi"] = "daw-utf-range"
         self.cpp_info.components["daw"].set_property("cmake_target_name", "daw::daw-utf-range")
         self.cpp_info.components["daw"].requires = ["daw_header_libraries::daw"]
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.filenames["cmake_find_package"] = "daw-utf-range"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "daw-utf-range"
+        self.cpp_info.names["cmake_find_package"] = "daw"
+        self.cpp_info.names["cmake_find_package_multi"] = "daw"
+        self.cpp_info.components["daw"].names["cmake_find_package"] = "daw-utf-range"
+        self.cpp_info.components["daw"].names["cmake_find_package_multi"] = "daw-utf-range"
