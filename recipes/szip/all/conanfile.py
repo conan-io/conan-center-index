87754ca97e8487c92ab635f587ad80bd5c65e882
Paul Harris <harris.pc@gmail.com>
2022-10-26 06:42:03 +0800

    Put finder module back in

--- recipes/szip/all/conanfile.py
+++ recipes/szip/all/conanfile.py
@@ -87,6 +87,22 @@ class SzipConan(ConanFile):
         copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"szip-shared" if self.options.shared else "szip-static": "szip::szip"}
+        )
+
+    def _create_cmake_module_alias_targets(self, module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        save(self, module_file, content)
 
     @property
     def _module_file_rel_path(self):
@@ -102,3 +118,7 @@ class SzipConan(ConanFile):
 
         if self.options.shared:
             self.cpp_info.defines.append("SZ_BUILT_AS_DYNAMIC_LIB=1")
+
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
