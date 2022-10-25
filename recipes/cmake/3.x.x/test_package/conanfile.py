31ab1a3e5f604aab5aea5be471b860a9167aeac1
System-Arch <33330183+System-Arch@users.noreply.github.com>
2022-10-25 07:08:38 -0400

    Handle Conan version disparities

--- recipes/cmake/3.x.x/test_package/conanfile.py
+++ recipes/cmake/3.x.x/test_package/conanfile.py
@@ -1,6 +1,7 @@
 from six import StringIO
-from conan import ConanFile
+from conan import ConanFile, conan_version
 from conan.tools.build import can_run
+from conan.tools.scm import Version
 
 
 class TestPackageConan(ConanFile):
@@ -17,7 +18,10 @@ class TestPackageConan(ConanFile):
             self.run("cmake --version", output, env="conanrun")
             output_str = str(output.getvalue())
             self.output.info("Installed version: {}".format(output_str))
-            require_version = str(self.dependencies["cmake"].ref.version)
+            if Version(conan_version).major < 2:
+                require_version = str(self.deps_cpp_info["cmake"].version)
+            else:
+                require_version = str(self.dependencies["cmake"].ref.version)
             self.output.info("Expected version: {}".format(require_version))
             assert_cmake_version = "cmake version %s" % require_version
             assert(assert_cmake_version in output_str)
