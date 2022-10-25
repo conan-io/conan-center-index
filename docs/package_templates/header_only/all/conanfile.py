26401a8b883f54dd34a30b790dd09c239692b195
Chris Mc <christopherm@jfrog.com>
2022-10-25 13:05:49 -0700

    docs: remove workaround for info object in validate

--- docs/package_templates/header_only/all/conanfile.py
+++ docs/package_templates/header_only/all/conanfile.py
@@ -1,4 +1,4 @@
-from conan import ConanFile, conan_version
+from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
@@ -57,22 +57,18 @@ class PackageConan(ConanFile):
     def package_id(self):
         self.info.clear()
 
-    @property
-    def _info(self):
-        return self if Version(conan_version).major < 2 else self.info
-
     def validate(self):
         # compiler subsettings are not available when building with self.info.clear()
-        if self._info.settings.compiler.get_safe("cppstd"):
+        if self.info.settings.compiler.get_safe("cppstd"):
             # validate the minimum cpp standard supported when installing the package. For C++ projects only
             check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self._info.settings.compiler), False)
-        if minimum_version and Version(self._info.settings.compiler.version) < minimum_version:
+        minimum_version = self._compilers_minimum_version.get(str(self.info.settings.compiler), False)
+        if minimum_version and Version(self.info.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
         # in case it does not work in another configuration, it should validated here too
-        if self._info.settings.os == "Windows":
+        if self.info.settings.os == "Windows":
             raise ConanInvalidConfiguration(f"{self.ref} can not be used on Windows.")
 
     def source(self):
