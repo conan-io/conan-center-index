64d2691e777c342ba5f2eef5940998bcf4e99ae3
Jordan Williams <jordan@jwillikers.com>
2022-10-26 09:56:03 -0500

    Remove version import

--- recipes/ninja/all/conanfile.py
+++ recipes/ninja/all/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.52.0"
