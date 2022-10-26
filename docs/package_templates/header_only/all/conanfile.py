92d8d94b6b2451dc9b97f87488de9c6a295e6868
Chris Mc <christopherm@jfrog.com>
2022-10-26 11:20:08 -0700

    make it 1.x valid until migration Q's are A'd

--- docs/package_templates/header_only/all/conanfile.py
+++ docs/package_templates/header_only/all/conanfile.py
@@ -58,17 +58,18 @@ class PackageConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        # compiler subsettings are not available when building with self.info.clear()
-        if self.info.settings.compiler.get_safe("cppstd"):
+        # FIXME: `self.settings` is not available in 2.0 but there are plenty of open issues about
+        # the migration point. For now we are only going to write valid 1.x recipes until we have a proper answer
+        if self.settings.compiler.get_safe("cppstd"):
             # validate the minimum cpp standard supported when installing the package. For C++ projects only
             check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.info.settings.compiler), False)
-        if minimum_version and Version(self.info.settings.compiler.version) < minimum_version:
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
         # in case it does not work in another configuration, it should validated here too
-        if self.info.settings.os == "Windows":
+        if self.settings.os == "Windows":
             raise ConanInvalidConfiguration(f"{self.ref} can not be used on Windows.")
 
     def source(self):
