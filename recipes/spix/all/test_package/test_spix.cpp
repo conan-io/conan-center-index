8d9a9622e644ffdc7363896badc70b2adc5889ff
Florian de Gaulejac <florian.degaulejac@stilla.fr>
2022-10-26 10:58:56 +0200

    Fix test_package thread issue

--- recipes/spix/all/test_package/test_spix.cpp
+++ recipes/spix/all/test_package/test_spix.cpp
@@ -1,14 +1,18 @@
 #include <cstdlib>
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 #include "Spix/QtQmlBot.h"
+
+std::mutex m;
+std::condition_variable cv;
 
 class MyTests : public spix::TestServer
 {
 protected:
-    void executeTest() override
-    {
-    }
+    void executeTest() override { cv.notify_one(); }
 };
 
 int main(void)
@@ -16,6 +20,11 @@ int main(void)
     spix::QtQmlBot bot;
     MyTests tests;
     bot.runTestServer(tests);
+
+    // wait for the test to begin before exiting main,
+    // destroying the server before the test is launched
+    std::unique_lock lk(m);
+    cv.wait(lk);
 
     return EXIT_SUCCESS;
 }
