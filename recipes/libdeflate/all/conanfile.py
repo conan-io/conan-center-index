26841074ad8ebea357a0f540f1b32d72d83ce920
Paul Harris <harris.pc@gmail.com>
2022-10-26 12:11:44 +0800

    libdeflate - update for conan v2

--- recipes/libdeflate/all/conanfile.py
+++ recipes/libdeflate/all/conanfile.py
@@ -1,9 +1,10 @@
 from conan import ConanFile
-from conan.tools.microsoft import is_msvc
+from conan.tools.microsoft import is_msvc, is_msvc_static_runtime, unix_path, VCVars
 from conan.tools.files import export_conandata_patches, apply_conandata_patches, get, chdir, rmdir, copy, rm
-from conan.tools.env import Environment
-from conans import MSBuild, AutoToolsBuildEnvironment, VisualStudioBuildEnvironment
-from conans.tools import vcvars, environment_append
+from conan.tools.env import Environment, VirtualBuildEnv, VirtualRunEnv
+from conan.tools.layout import basic_layout
+# from conans import MSBuild, AutoToolsBuildEnvironment, VisualStudioBuildEnvironment
+# from conans.tools import vcvars, environment_append
 import os
 
 required_conan_version = ">=1.52.0"
@@ -25,10 +26,6 @@ class LibdeflateConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
     def _is_clangcl(self):
@@ -60,39 +57,90 @@ class LibdeflateConan(ConanFile):
         except Exception:
             pass
 
+    def layout(self):
+        # src_folder must use the same source folder name the project
+        basic_layout(self, src_folder="src")
+
     def build_requirements(self):
         if self._settings_build.os == "Windows" and not is_msvc(self):
             if "CONAN_BASH_PATH" not in Environment().vars(self, scope="build").keys():
                 self.tool_requires("msys2/cci.latest")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    def _build_msvc(self):
-        with chdir(self, self._source_subfolder):
-            with vcvars(self), environment_append(VisualStudioBuildEnvironment(self).vars):
-                target = "libdeflate.dll" if self.options.shared else "libdeflatestatic.lib"
-                self.run("nmake /f Makefile.msc {}".format(target))
+    def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+
+        if is_msvc(self) or self._is_clang_cl:
+            vc = VCVars(self)
+            vc.generate()
+            env = Environment()
+            # env.define("PROFILE", None)
+            # env.define("TUNE", None)
+            # env.define("NODEBUG", None)
+            env.vars(self).save_script("conanbuildenv_nmake")
+        else:
+            tc = AutotoolsToolchain(self)
+            # yes_no = lambda v: "yes" if v else "no"
+            # tc.configure_args.extend([
+                # f"--with-foobar={yes_no(self.options.with_foobar)}",
+                # "--enable-tools=no",
+                # "--enable-manpages=no",
+            # ])
+            tc.generate()
+            # generate pkg-config files of dependencies (useless if upstream configure.ac doesn't rely on PKG_CHECK_MODULES macro)
+            tc = PkgConfigDeps(self)
+            tc.generate()
+
+#        # If Visual Studio is supported
+#        if is_msvc(self):
+#            env = Environment()
+#            # get compile & ar-lib from automake (or eventually lib source code if available)
+#            # it's not always required to wrap CC, CXX & AR with these scripts, it depends on how much love was put in
+#            # upstream build files
+#            compile_wrapper = unix_path(self, self._user_info_build["automake"].compile)
+#            ar_wrapper = unix_path(self, self._user_info_build["automake"].ar_lib)
+#            env.define("CC", f"{compile_wrapper} cl -nologo")
+#            env.define("CXX", f"{compile_wrapper} cl -nologo")
+#            env.define("LD", "link -nologo")
+#            env.define("AR", f"{ar_wrapper} \"lib -nologo\"")
+#            env.define("NM", "dumpbin -symbols")
+#            env.define("OBJDUMP", ":")
+#            env.define("RANLIB", ":")
+#            env.define("STRIP", ":")
+#            env.vars(self).save_script("conanbuild_msvc")
+
+    def _build_nmake(self):
+        with chdir(self, self.source_folder):
+            # with vcvars(self), environment_append(VisualStudioBuildEnvironment(self).vars):
+            target = "libdeflate.dll" if self.options.shared else "libdeflatestatic.lib"
+            self.run(f"nmake /f Makefile.msc {target}")
 
     def _build_make(self):
-        autotools = AutoToolsBuildEnvironment(self, win_bash=(self._settings_build.os == "Windows"))
-        with chdir(self, self._source_subfolder):
-            autotools.make()
+        # autotools = AutoToolsBuildEnvironment(self, win_bash=(self._settings_build.os == "Windows"))
+        # with chdir(self, self._source_subfolder):
+            # autotools.make()
+        autotools = Autotools(self)
+        autotools.autoreconf()
+        autotools.configure()
+        autotools.make()
 
     def build(self):
         apply_conandata_patches(self)
         if is_msvc(self) or self._is_clangcl:
-            self._build_msvc()
+            self._build_nmake()
         else:
             self._build_make()
 
     def _package_windows(self):
-        self.copy("libdeflate.h", dst="include", src=self._source_subfolder)
+        copy(self, "libdeflate.h", dst=os.path.join(self.package_folder, "include"), src=self.source_folder)
         if self.options.shared:
-            self.copy("*deflate.lib", dst="lib", src=self._source_subfolder)
-            self.copy("*deflate.dll", dst="bin", src=self._source_subfolder)
+            copy(self, "*deflate.lib", dst=os.path.join(self.package_folder, "lib"), src=self.source_folder)
+            copy(self, "*deflate.dll", dst=os.path.join(self.package_folder, "bin"), src=self.source_folder)
         else:
-            self.copy("*deflatestatic.lib", dst="lib", src=self._source_subfolder)
+            copy(self, "*deflatestatic.lib", dst=os.path.join(self.package_folder, "lib"), src=self.source_folder)
 
     def _package_make(self):
         autotools = AutoToolsBuildEnvironment(self, win_bash=(self._settings_build.os == "Windows"))
@@ -104,7 +152,7 @@ class LibdeflateConan(ConanFile):
 
     def package(self):
         copy(self, "COPYING", 
-            src=os.path.join(self.source_folder, self._source_subfolder), 
+            src=os.path.join(self.source_folder, self.source_folder), 
             dst=os.path.join(self.package_folder, "licenses"
         ))
         if self.settings.os == "Windows":
