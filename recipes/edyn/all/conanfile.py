167f2553f5b2926da3225bc98a4e3b67b0aa56cf
Raziel Alphadios <raziely@gmail.com>
2022-10-26 10:59:06 +0300

    Remove blocking of gcc11-release-shared as it should be fixed upstream

--- recipes/edyn/all/conanfile.py
+++ recipes/edyn/all/conanfile.py
@@ -53,8 +53,6 @@ class EdynConan(ConanFile):
         self.requires("entt/3.10.1")
 
     def validate(self):
-        if self.options.shared and self.settings.compiler == "gcc" and Version(self.settings.compiler.version) == "11" and self.settings.build_type == "Release":
-            raise ConanInvalidConfiguration(f"{self.ref} does not build in C3i as shared with gcc-11 in release. Please submit a PR to fix this https://github.com/conan-io/conan-center-index/pull/13562#issuecomment-1284854656")
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, 17)
         check_min_vs(self, 192)
