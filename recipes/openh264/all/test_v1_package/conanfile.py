c958fc0d53cdb52b07e57c6e20eb7207fd1ee273
Dallas Hart <nomalahvids@gmail.com>
2022-10-28 18:42:39 -0400

    Fix tests

--- recipes/openh264/all/test_v1_package/conanfile.py
+++ recipes/openh264/all/test_v1_package/conanfile.py
@@ -1,19 +1,10 @@
-from conan import ConanFile
-from conan.tools.build import cross_building
-from conan.tools.cmake import cmake_layout, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
-    test_type = "explicit"
-
-    def layout(self):
-        cmake_layout(self)
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
+    generators = "cmake", "cmake_find_package_multi"
 
     def build_requirements(self):
         if self.settings.os == "Macos" and self.settings.arch == "armv8":
@@ -30,6 +21,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not cross_building(self, skip_x64_x86=True):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, env="conanrun")
+        if not tools.cross_building(self, skip_x64_x86=True):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
