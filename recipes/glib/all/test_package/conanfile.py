97e9aa5917f3cc46de35f03e72c3092bed3ad154
Jordan Williams <jordan@jwillikers.com>
2022-10-29 03:25:16 -0500

    (#13347) glib: Fix test package to allow cross-compilation
    * remove unused patch
    
    * glib: Fix test package to allow cross-compilation
    
    CMake's FindPkgConfig module doesn't behave correctly when cross-compiling.
    The test package has been modified to use CMake config modules when cross-compiling.
    This allows cross-compiling the glib package.
    
    * Use PKG_CONFIG_PATH workaround for CMake
    
    * Fix test_v1_package
    
    * Use pkgconf in test package
    
    * Workaround libdir / libdir1 inconsistency  in pkg_config / PkgConfigDeps
    
    * Fix minimum CMake version for pkg_check_modules
    
    * Fix code warning from apple-clang
    
    * Fix Apple shared install name?
    
    * Simplify boolean print out
    
    * Check exceptions in configure
    
    * Check pkg_config conf variable
    
    * Use SPDX identifier for license
    
    * Organize imports
    
    * Remove workarounds for MesonToolchain generator
    
    * Fix SHA
    
    * Fix merge
    
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>

--- recipes/glib/all/test_package/conanfile.py
+++ recipes/glib/all/test_package/conanfile.py
@@ -1,14 +1,13 @@
 from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeDeps, cmake_layout
-from conan.tools.env import Environment, VirtualBuildEnv
+from conan.tools.build import can_run, cross_building
+from conan.tools.cmake import cmake_layout, CMake, CMakeDeps, CMakeToolchain
+from conan.tools.env import Environment, VirtualBuildEnv, VirtualRunEnv
 from conan.tools.gnu import PkgConfig, PkgConfigDeps
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
 
     def layout(self):
@@ -18,22 +17,31 @@ class TestPackageConan(ConanFile):
         self.requires(self.tested_reference_str)
 
     def build_requirements(self):
-        if self.settings.os != "Windows":
+        if self.settings.os != "Windows" and not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/1.9.3")
 
     def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_CROSSCOMPILING"] = cross_building(self)
+        tc.generate()
         if self.settings.os == "Windows":
             deps = CMakeDeps(self)
             deps.generate()
         else:
-            env = VirtualBuildEnv(self)
-            env.generate()
-            pkg = PkgConfigDeps(self)
-            pkg.generate()
-            # TODO: to remove when properly handled by conan (see https://github.com/conan-io/conan/issues/11962)
+            # todo Remove the following workaround after https://github.com/conan-io/conan/issues/11962 is fixed.
             env = Environment()
             env.prepend_path("PKG_CONFIG_PATH", self.generators_folder)
-            env.vars(self).save_script("conanbuildenv_pkg_config_path")
+            envvars = env.vars(self)
+            envvars.save_script("pkg_config")
+
+            virtual_build_env = VirtualBuildEnv(self)
+            virtual_build_env.generate()
+            virtual_run_env = VirtualRunEnv(self)
+            virtual_run_env.generate()
+            pkg_config_deps = PkgConfigDeps(self)
+            pkg_config_deps.generate()
+            cmake_deps = CMakeDeps(self)
+            cmake_deps.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -49,3 +57,4 @@ class TestPackageConan(ConanFile):
                 pkg_config = PkgConfig(self, "gio-2.0", pkg_config_path=self.generators_folder)
                 gdbus_codegen = pkg_config.variables["gdbus_codegen"]
                 self.run(f"{gdbus_codegen} -h", env="conanrun")
+
