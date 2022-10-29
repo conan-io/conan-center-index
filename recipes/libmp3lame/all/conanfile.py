32ceac0e9f8eeb3ca087c59874150f24272301d6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-29 12:47:29 +0200

    fix mingw cross-build from linux to windows

--- recipes/libmp3lame/all/conanfile.py
+++ recipes/libmp3lame/all/conanfile.py
@@ -60,7 +60,7 @@ class LibMP3LameConan(ConanFile):
     def build_requirements(self):
         if not is_msvc(self) and not self._is_clang_cl:
             self.build_requires("gnu-config/cci.20201022")
-            if self.settings.os == "Windows":
+            if self._settings_build.os == "Windows":
                 self.win_bash = True
                 if not self.conf.get("tools.microsoft.bash:path", default=False, check_type=str):
                     self.tool_requires("msys2/cci.latest")
