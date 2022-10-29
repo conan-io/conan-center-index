6839129b93f616bfd2b9a075fadce91b82ab8b5d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-10-29 12:46:38 +0200

    modernize more

--- recipes/libmp3lame/all/conanfile.py
+++ recipes/libmp3lame/all/conanfile.py
@@ -8,7 +8,7 @@ from conans import VisualStudioBuildEnvironment
 import os
 import shutil
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class LibMP3LameConan(ConanFile):
@@ -50,18 +50,9 @@ class LibMP3LameConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
-        try:
-            del self.settings.compiler.libcxx
-        except Exception:
-            pass
-        try:
-            del self.settings.compiler.cppstd
-        except Exception:
-            pass
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
 
     def layout(self):
         basic_layout(self, src_folder="src")
