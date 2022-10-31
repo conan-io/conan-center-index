c049990a7515f9b90aea7059d5b2ebc80699856e
Rene Rivera <grafikrobot@gmail.com>
2022-10-30 23:37:37 -0500

    More tweaks from feedback. * Add test_v2_package to test all the combinations. * Add requirements test stuff in the three test variants.

--- recipes/b2/portable/test_v1_package/conanfile.py
+++ recipes/b2/portable/test_v1_package/conanfile.py
@@ -3,6 +3,10 @@ from conan import ConanFile
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch"
+    test_type = "explicit"
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def test(self):
         self.run("b2 -v", run_environment=True)
--- recipes/b2/portable/test_v2_package/conanfile.py
+++ recipes/b2/portable/test_v2_package/conanfile.py
@@ -3,7 +3,6 @@ from conan import ConanFile
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch"
-    test_type = "explicit"
 
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
