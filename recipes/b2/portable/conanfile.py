e1a79e3fda162c5fc9651d13901c3574430d86f9
Rene Rivera <grafikrobot@gmail.com>
2022-10-31 07:07:25 -0500

    Lost the list in the env_info.path mutations.

--- recipes/b2/portable/conanfile.py
+++ recipes/b2/portable/conanfile.py
@@ -165,7 +165,7 @@ class B2Conan(ConanFile):
         self.cpp_info.libdirs = []
         self.cpp_info.bindirs = ["bin"]
         self.buildenv_info.prepend_path("PATH", self._pkg_bin_dir)
-        self.env_info.path = self._pkg_bin_dir
+        self.env_info.path = [self._pkg_bin_dir]
 
     def package_id(self):
         del self.info.options.use_cxx_env
