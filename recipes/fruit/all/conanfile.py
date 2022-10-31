100fd9a4c2e9c44910e5d38c48f979415fe8b162
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-31 01:08:44 +0000

    Adding tarfile member sanitization to extractall()

--- recipes/fruit/all/conanfile.py
+++ recipes/fruit/all/conanfile.py
@@ -79,7 +79,26 @@ class FruitConan(ConanFile):
                 exclude_pattern = "%s/extras/bazel_root/third_party/fruit/build" % (self._extracted_dir,)
                 members = list(filter(lambda m: not fnmatch(m.name, exclude_pattern),
                                     tarredgzippedFile.getmembers()))
-                tarredgzippedFile.extractall(".", members=members)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tarredgzippedFile, ".", members=members)
         else:
             tools.get(**self.conan_data["sources"][self.version])
 
