340f035c88aa159464a5b1d26c8a91cad0c268ab
Jordan Williams <jordan@jwillikers.com>
2022-10-31 09:40:32 -0500

    Fix finding pcre2

--- recipes/libselinux/all/conanfile.py
+++ recipes/libselinux/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import apply_conandata_patches, chdir, copy, export_conandata_patches, get, rename, replace_in_file, rm, rmdir
-from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain, PkgConfigDeps
+from conan.tools.files import apply_conandata_patches, chdir, copy, export_conandata_patches, get
+from conan.tools.gnu import Autotools, AutotoolsToolchain, PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
@@ -20,7 +20,7 @@ class LibSELinuxConan(ConanFile):
     topics = ("selinux", "security-enhanced linux")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/SELinuxProject/selinux"
-    license = "Unlicense"  # This library (libselinux) is public domain software, i.e. not copyrighted
+    license = "Unlicense"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -55,6 +55,8 @@ class LibSELinuxConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("flex/2.6.4")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/1.9.3")
 
     def layout(self):
         basic_layout(self, src_folder="src")
@@ -90,15 +92,14 @@ class LibSELinuxConan(ConanFile):
     def generate(self):
         virtual_build_env = VirtualBuildEnv(self)
         virtual_build_env.generate()
-        pkg_config_deps = AutotoolsDeps(self)
+        pkg_config_deps = PkgConfigDeps(self)
         pkg_config_deps.generate()
         tc = AutotoolsToolchain(self)
-        tc.extra_defines.append("USE_PCRE2=1")
-        tc.extra_defines.append("PCRE2_CODE_UNIT_WIDTH=8")
         sepol_include_folder = os.path.join(self._sepol_source_folder, "include")
         tc.extra_cflags.append(f"-I{sepol_include_folder}")
-        # Remove the "/usr" prefix from the install destination.
-        tc.make_args.append("PREFIX=")
+        sepol_lib_folder = os.path.join(self._sepol_source_folder, "src")
+        tc.extra_ldflags.append(f"-L{sepol_lib_folder}")
+        tc.make_args.append("USE_PCRE2=y")
         tc.generate()
 
     def build(self):
@@ -106,15 +107,17 @@ class LibSELinuxConan(ConanFile):
         autotools = Autotools(self)
         with chdir(self, os.path.join(self._sepol_source_folder, "src")):
             autotools.make(self._sepol_library_target)
-        with chdir(self, os.path.join(self._selinux_source_folder, "src")):
-            autotools.make(self._selinux_library_target)
+        with chdir(self, os.path.join(self._selinux_source_folder)):
+            autotools.make()
 
     def package(self):
         copy(self, "LICENSE", self._selinux_source_folder, os.path.join(self.package_folder, "licenses"))
         for library in [self._sepol_source_folder, self._selinux_source_folder]:
             copy(self, "*.h", os.path.join(library, "include"), os.path.join(self.package_folder, "include"))
-            copy(self, "*.so*", library, os.path.join(self.package_folder, "lib"), keep_path=False)
-            copy(self, "*.a", library, os.path.join(self.package_folder, "lib"), keep_path=False)
+            if self.options.shared:
+                copy(self, "*.so*", library, os.path.join(self.package_folder, "lib"), keep_path=False)
+            else:
+                copy(self, "*.a", library, os.path.join(self.package_folder, "lib"), keep_path=False)
 
     def package_info(self):
         self.cpp_info.components["selinux"].set_property("pkg_config_name", "libselinux")
