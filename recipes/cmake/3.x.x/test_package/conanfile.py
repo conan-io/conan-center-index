cb56f4713c851eeace02ec0c2e3674e2e786c37e
System-Arch <33330183+System-Arch@users.noreply.github.com>
2022-11-02 19:15:21 -0400

    Determine require_version in a version-agnostic manner

--- recipes/cmake/3.x.x/test_package/conanfile.py
+++ recipes/cmake/3.x.x/test_package/conanfile.py
@@ -1,7 +1,6 @@
 from six import StringIO
-from conan import ConanFile, conan_version
+from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.scm import Version
 
 
 class TestPackageConan(ConanFile):
@@ -18,10 +17,7 @@ class TestPackageConan(ConanFile):
             self.run("cmake --version", output, env="conanrun")
             output_str = str(output.getvalue())
             self.output.info("Installed version: {}".format(output_str))
-            if Version(conan_version).major < 2:
-                require_version = str(self.deps_cpp_info["cmake"].version)
-            else:
-                require_version = str(self.dependencies["cmake"].ref.version)
+            name, require_version = self.tested_reference_str.split("/", 1)
             self.output.info("Expected version: {}".format(require_version))
             assert_cmake_version = "cmake version %s" % require_version
             assert(assert_cmake_version in output_str)
