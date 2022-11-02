37d20da7c6e01ea6d6ae7620d1ae8fd8e7f7e7f4
Rene Rivera <grafikrobot@gmail.com>
2022-11-02 15:54:17 -0500

    Add full settings for test packages (they are ignore anyway).

--- recipes/b2/portable/test_package/conanfile.py
+++ recipes/b2/portable/test_package/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     generators = "VirtualBuildEnv"
     test_type = "explicit"
 
--- recipes/b2/portable/test_v1_package/conanfile.py
+++ recipes/b2/portable/test_v1_package/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "arch"
+    settings = "os", "arch", "compiler", "build_type"
 
     def test(self):
         self.run("b2 -v", run_environment=True)
