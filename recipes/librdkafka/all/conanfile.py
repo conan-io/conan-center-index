fd28191e41cfbb451d62f42739bd4fe539b32385
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-02 22:36:16 +0100

    use self.options.rm_safe

--- recipes/librdkafka/all/conanfile.py
+++ recipes/librdkafka/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.gnu import PkgConfigDeps
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class LibrdkafkaConan(ConanFile):
@@ -56,10 +56,7 @@ class LibrdkafkaConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
