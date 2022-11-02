f7d94e1a4a8cbbe9b7e93f93b70b9718b9d5b29a
toge <toge.mail@gmail.com>
2022-11-02 10:49:28 +0900

    fix license issue

--- recipes/plutovg/all/conanfile.py
+++ recipes/plutovg/all/conanfile.py
@@ -64,7 +64,7 @@ class PlutoVGConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, pattern="LICENSE", dst="licenses", src=self.source_folder)
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         copy(self, pattern="*.h", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "include"))
         copy(self, pattern="*.a", dst=os.path.join(self.package_folder, "lib"), src=self.build_folder, keep_path=False)
         copy(self, pattern="*.so", dst=os.path.join(self.package_folder, "lib"), src=self.build_folder, keep_path=False)
