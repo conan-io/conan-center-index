0984cd95878dba4a7427517bedece971bab6212c
Rene Rivera <grafikrobot@gmail.com>
2022-11-01 21:49:04 -0500

    More tweaks for CCI testing feedback.

--- recipes/b2/portable/test_package/conanfile.py
+++ recipes/b2/portable/test_package/conanfile.py
@@ -3,10 +3,11 @@ from conan import ConanFile
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch"
+    generators = "VirtualBuildEnv"
     test_type = "explicit"
 
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
 
     def test(self):
-        self.run("b2 -v", run_environment=True)
+        self.run("b2 -v")
