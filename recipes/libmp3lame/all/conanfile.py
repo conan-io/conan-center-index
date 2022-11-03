fc8d3eaeb18f6a45d2853b6968d4590d3f971055
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-03 20:26:57 +0100

    use rm_safe

--- recipes/libmp3lame/all/conanfile.py
+++ recipes/libmp3lame/all/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.microsoft import is_msvc, unix_path, VCVars
 import os
 import shutil
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class LibMP3LameConan(ConanFile):
@@ -45,18 +45,9 @@ class LibMP3LameConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
-        try:
-            del self.settings.compiler.libcxx
-        except Exception:
-            pass
-        try:
-            del self.settings.compiler.cppstd
-        except Exception:
-            pass
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
         basic_layout(self, src_folder="src")
