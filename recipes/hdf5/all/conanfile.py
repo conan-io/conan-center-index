30fd448cc05d179cbe3d39a9461577e1cc599e62
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-11-03 14:52:10 +0100

    Bump zlib

--- recipes/hdf5/all/conanfile.py
+++ recipes/hdf5/all/conanfile.py
@@ -71,7 +71,7 @@ class Hdf5Conan(ConanFile):
 
     def requirements(self):
         if self.options.with_zlib:
-            self.requires("zlib/1.2.12")
+            self.requires("zlib/1.2.13")
         if self.options.szip_support == "with_libaec":
             self.requires("libaec/1.0.6")
         elif self.options.szip_support == "with_szip":
