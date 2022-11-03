8576b67a8ea633bd086a22b8f683b2b83bd56785
Paul Harris <harris.pc@gmail.com>
2022-11-03 20:27:59 +0800

    libharu - support conan v2

--- recipes/libharu/all/conanfile.py
+++ recipes/libharu/all/conanfile.py
@@ -1,16 +1,23 @@
-from conan.tools.microsoft import is_msvc
-from conans import CMake, ConanFile, tools
-import functools
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime, is_msvc
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file, load, save
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv
 import os
 import re
 
-required_conan_version = ">=1.45.0"
+
+required_conan_version = ">=1.53.0"
+
 
 
 class LibharuConan(ConanFile):
     name = "libharu"
     description = "Haru is a free, cross platform, open-sourced software library for generating PDF."
-    topics = ("libharu", "pdf", "generate", "generator")
+    topics = "pdf", "generate", "generator"
     license = "ZLIB"
     homepage = "http://libharu.org/"
     url = "https://github.com/conan-io/conan-center-index"
@@ -25,20 +32,8 @@ class LibharuConan(ConanFile):
         "fPIC": True,
     }
 
-    generators = "cmake", "cmake_find_package"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -46,47 +41,49 @@ class LibharuConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+
+    def layout(self):
+        # src_folder must use the same source folder name the project
+        cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("zlib/1.2.12")
-        self.requires("libpng/1.6.37")
+        self.requires("zlib/1.2.13")
+        self.requires("libpng/1.6.38")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    @functools.lru_cache(1)
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["LIBHPDF_SHARED"] = self.options.shared
-        cmake.definitions["LIBHPDF_STATIC"] = not self.options.shared
-        # To install relocatable shared lib on Macos
-        cmake.definitions["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["LIBHPDF_SHARED"] = self.options.shared
+        tc.variables["LIBHPDF_STATIC"] = not self.options.shared
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def _extract_license(self):
-        readme = tools.load(os.path.join(self._source_subfolder, "README"))
+        readme = load(save, os.path.join(self.source_folder, "README"))
         match = next(re.finditer("\n[^\n]*license[^\n]*\n", readme, flags=re.I | re.A))
         return readme[match.span()[1]:].strip("*").strip()
 
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
-        for fn in ("CHANGES", "INSTALL", "README"):
-            os.unlink(os.path.join(self.package_folder, fn))
-        tools.rmdir(os.path.join(self.package_folder, "if"))
-        tools.save(os.path.join(self.package_folder, "licenses", "LICENSE"), self._extract_license())
+        rm(self, "CHANGES", os.path.join(self.package_folder))
+        rm(self, "INSTALL", os.path.join(self.package_folder))
+        rm(self, "README", os.path.join(self.package_folder))
+        rmdir(self, os.path.join(self.package_folder, "if"))
+        save(self, os.path.join(self.package_folder, "licenses", "LICENSE"), self._extract_license())
 
     def package_info(self):
         libprefix = "lib" if is_msvc(self) else ""
