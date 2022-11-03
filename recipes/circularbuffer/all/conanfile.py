28a94b3993275d7cf7e2818aef131c1ee7f09638
Harald <harald.achitz@gmail.com>
2022-11-03 07:45:29 +0100

    Remove TODO comment
    Co-authored-by: Pau Farré <Hopobcn@users.noreply.github.com>

--- recipes/circularbuffer/all/conanfile.py
+++ recipes/circularbuffer/all/conanfile.py
@@ -44,6 +44,7 @@ class CircularBufferConan(ConanFile):
         self.cpp_info.set_property(
             "cmake_target_name", "CircularBuffer::CircularBuffer")
 
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "CircularBuffer"
         self.cpp_info.filenames["cmake_find_package_multi"] = "CircularBuffer"
         self.cpp_info.names["cmake_find_package"] = "CircularBuffer"
