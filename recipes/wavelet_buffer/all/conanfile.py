7bf6f802f09c12832222d2cddf8e743ac1380735
Kataev Victor <kataev.victor.1234@gmail.com>
2022-11-03 12:07:53 +0000

    Remove rm import

--- recipes/wavelet_buffer/all/conanfile.py
+++ recipes/wavelet_buffer/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
-from conan.tools.files import get, copy, rm, rmdir
+from conan.tools.files import get, copy, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
