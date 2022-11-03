894a1685147736664452225bd8bce7fb86f4bb27
toge <toge.mail@gmail.com>
2022-11-04 01:37:38 +0900

    libunwind: support conan v2, update zlib

--- recipes/libunwind/all/conanfile.py
+++ recipes/libunwind/all/conanfile.py
@@ -1,17 +1,24 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.build import check_min_cppstd, cross_building
+from conan.tools.env import Environment, VirtualBuildEnv, VirtualRunEnv
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps, PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, unix_path
+
 import os
 
-required_conan_version = ">=1.33.0"
-
+required_conan_version = ">=1.53.0"
 
 class LiunwindConan(ConanFile):
     name = "libunwind"
     description = "Manipulate the preserved state of each call-frame and resume the execution at any point."
-    topics = ("conan", "libunwind", "unwind", "debuggers", "exception-handling", "introspection", "setjmp")
+    license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/libunwind/libunwind"
-    license = "MIT"
+    topics = ("unwind", "debuggers", "exception-handling", "introspection", "setjmp")
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -32,60 +39,73 @@ class LiunwindConan(ConanFile):
         "zlibdebuginfo": True,
     }
 
-    exports_sources = "patches/**"
-    _autotools = None
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _user_info_build(self):
+        return getattr(self, "user_info_build", self.deps_user_info)
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def configure(self):
-        if self.settings.os not in ["Linux", "FreeBSD"]:
-            raise ConanInvalidConfiguration("libunwind is only supported on Linux and FreeBSD")
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def requirements(self):
         if self.options.minidebuginfo:
             self.requires("xz_utils/5.2.5")
         if self.options.zlibdebuginfo:
-            self.requires("zlib/1.2.12")
+            self.requires("zlib/1.2.13")
+
+    def validate(self):
+        if self.settings.os not in ["Linux", "FreeBSD"]:
+            raise ConanInvalidConfiguration("libunwind is only supported on Linux and FreeBSD")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    def _configure_autotools(self):
-        if not self._autotools:
-            self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-            args = [
-                "--enable-shared={}".format("yes" if self.options.shared else "no"),
-                "--enable-static={}".format("no" if self.options.shared else "yes"),
-                "--enable-coredump={}".format("yes" if self.options.coredump else "no"),
-                "--enable-ptrace={}".format("yes" if self.options.ptrace else "no"),
-                "--enable-setjmp={}".format("yes" if self.options.setjmp else "no"),
-                "--enable-minidebuginfo={}".format("yes" if self.options.minidebuginfo else "no"),
-                "--enable-zlibdebuginfo={}".format("yes" if self.options.zlibdebuginfo else "no"),
-                "--disable-tests",
-                "--disable-documentation"
-            ]
-            self._autotools.configure(configure_dir=self._source_subfolder, args=args)
-        return self._autotools
+    def generate(self):
+        tc = AutotoolsToolchain(self)
+        yes_no = lambda v: "yes" if v else "no"
+        tc.configure_args.extend([
+            f"--enable-shared={yes_no(self.options.shared)}",
+            f"--enable-static={yes_no(not self.options.shared)}",
+            f"--enable-coredump={yes_no(self.options.coredump)}",
+            f"--enable-ptrace={yes_no(self.options.ptrace)}",
+            f"--enable-setjmp={yes_no(self.options.setjmp)}",
+            f"--enable-minidebuginfo={yes_no(self.options.minidebuginfo)}",
+            f"--enable-zlibdebuginfo={yes_no(self.options.zlibdebuginfo)}",
+            "--disable-tests",
+            "--disable-documentation",
+        ])
+        tc.generate()
+
+        tc = PkgConfigDeps(self)
+        tc.generate()
+
+        tc = AutotoolsDeps(self)
+        tc.generate()
 
     def build(self):
-        for patch_data in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch_data)
-        autotools = self._configure_autotools()
+        apply_conandata_patches(self)
+        autotools = Autotools(self)
+        autotools.configure()
         autotools.make()
 
     def package(self):
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        autotools = self._configure_autotools()
+        copy(self, pattern="COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        autotools = Autotools(self)
         autotools.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
 
     def package_info(self):
         self.cpp_info.components["unwind"].names["pkg_config"] = "libunwind"
