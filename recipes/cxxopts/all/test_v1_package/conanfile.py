d5125588b32ceeadb5d9863c134a5331d3e405c4
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-03 20:26:50 +0100

    (#13922) cxxopts: conan v2 support
    * conan v2 support
    
    * download tar.gz tarball instead of .zip

--- recipes/cxxopts/all/test_v1_package/conanfile.py
+++ recipes/cxxopts/all/test_v1_package/conanfile.py
@@ -1,20 +1,11 @@
-from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import CMake, cmake_layout
+from conans import ConanFile, CMake, tools
 from io import StringIO
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
-    test_type = "explicit"
-
-    def layout(self):
-        cmake_layout(self)
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -22,13 +13,13 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if can_run(self):
+        if not tools.cross_building(self):
             output = StringIO()
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            bin_path = os.path.join("bin", "test_package")
             option_string = "-f 41 --bar baria --baz"
             if self.options["cxxopts"].unicode:
                 option_string += " -q quxis"
-            self.run(f"{bin_path} {option_string}", env="conanrun", output=output)
+            self.run(f"{bin_path} {option_string}", run_environment=True, output=output)
             output_lines = set(output.getvalue().splitlines())
             expected_lines = {"foo:41", "bar:baria", "baz:1"}
             if self.options["cxxopts"].unicode:
