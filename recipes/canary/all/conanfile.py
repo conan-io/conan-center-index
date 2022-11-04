e39bbda65302fd1e103f6f50951fb20ddefbb7f7
Alexey Klimkin <klimkin@gmail.com>
2022-11-04 15:11:37 -0700

    Use new imports

--- recipes/canary/all/conanfile.py
+++ recipes/canary/all/conanfile.py
@@ -1,11 +1,11 @@
 import os
 
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
 from conan.tools.files import copy, get, rmdir
 from conan.tools.scm import Version
-from conans.errors import ConanInvalidConfiguration
 
 required_conan_version = ">=1.53.0"
 
