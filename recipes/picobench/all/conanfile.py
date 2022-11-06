1a9aa692c7d80810a1c3dfa44292882bdb4b3c2c
doome161 <schulze.dominik@gmx.de>
2022-11-06 18:28:32 +0100

    picobench: copy license file

--- recipes/picobench/all/conanfile.py
+++ recipes/picobench/all/conanfile.py
@@ -50,7 +50,7 @@ class PicobenchConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, pattern="LICENSE.txt", dst="licenses", src=self.source_folder)
+        copy(self, pattern="LICENSE.txt", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         cmake = CMake(self)
         cmake.configure()
         cmake.install()
