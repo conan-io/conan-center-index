061d1270e6a046bda24d8fb1dfd2823df6e9ad68
Alexey Kreshchuk <akrsch@gmail.com>
2022-11-06 16:30:08 +0200

    [bacnet-stack] migrate to conan v2

--- recipes/bacnet-stack/all/conanfile.py
+++ recipes/bacnet-stack/all/conanfile.py
@@ -1,5 +1,7 @@
 import os
-from conans import CMake, ConanFile, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import get, copy, rmdir
 from conans.errors import ConanInvalidConfiguration
 
 
@@ -12,7 +14,6 @@ class BacnetStackConan(ConanFile):
         BACnet Protocol Stack library provides a BACnet application layer,
         network layer and media access (MAC) layer communications services."""
     topics = ("bacnet")
-    exports_sources = ['CMakeLists.txt']
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
@@ -22,17 +23,6 @@ class BacnetStackConan(ConanFile):
         "shared": False,
         "fPIC": True
     }
-    generators = "cmake", "cmake_find_package"
-
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -45,34 +35,28 @@ class BacnetStackConan(ConanFile):
         if self.settings.os == "Windows" and self.options.shared:
             raise ConanInvalidConfiguration("Windows shared builds are not supported right now, see issue https://github.com/bacnet-stack/bacnet-stack/issues/49")
 
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        if self.version.startswith("2020"):
-            extracted_dir = self.name + "-" + os.path.basename(self.conan_data["sources"][self.version]["url"]).split(".")[0]
-        else:
-            extracted_dir = self.name + "-" + self.name + "-" + self.version
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-        os.rename(extracted_dir, self._source_subfolder)
-
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["BACNET_STACK_BUILD_APPS"] = False
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BACNET_STACK_BUILD_APPS"] = False
+        tc.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("gpl-2.txt", dst='licenses', src=os.path.join(self._source_subfolder, "license"))
-        cmake = self._configure_cmake()
+        copy(self, pattern="gpl-2.txt", dst=os.path.join(self.package_folder, "licenses"), src=os.path.join(self.source_folder, "license"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder,
-                                 "lib", "bacnet-stack", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "bacnet-stack", "cmake"))
 
     def package_info(self):
         self.cpp_info.libs = ["bacnet-stack"]
@@ -82,3 +66,5 @@ class BacnetStackConan(ConanFile):
             self.cpp_info.system_libs = ["ws2_32"]
         if not self.options.shared:
             self.cpp_info.defines = ["BACNET_STACK_STATIC_DEFINE"]
+        self.cpp_info.set_property("cmake_file_name", "bacnet-stack")
+        self.cpp_info.set_property("cmake_target_name", "bacnet-stack::bacnet-stack")
