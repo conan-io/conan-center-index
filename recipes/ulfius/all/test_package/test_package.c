65f964f1e7d18c60562d334fe8bc35ba044fdcd7
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-11-07 14:13:42 +0100

    ulfius: hide all networking behind the if

--- recipes/ulfius/all/test_package/test_package.c
+++ recipes/ulfius/all/test_package/test_package.c
@@ -25,14 +25,20 @@ int callback_hello_world (const struct _u_request * request, struct _u_response 
 int main(int argc, char *argv[]) {
     struct _u_instance instance;
 
-    // Initialize instance with the port number
-    if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
-        fprintf(stderr, "Error ulfius_init_instance, abort\n");
+    if (ulfius_global_init() != U_OK) {
+        fprintf(stderr, "Error ulfius_global_init, abort\n");
         return(1);
     }
 
     // Avoid running a server on CI
     if (argc > 1 && strcmp(argv[1], "run") == 0) {
+
+        // Initialize instance with the port number
+        if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
+            fprintf(stderr, "Error ulfius_init_instance, abort\n");
+            return(1);
+        }
+
         // Endpoint list declaration
         ulfius_add_endpoint_by_val(&instance, "GET", "/helloworld", NULL, 0, &callback_hello_world, NULL);
 
@@ -48,9 +54,11 @@ int main(int argc, char *argv[]) {
         printf("End framework\n");
 
         ulfius_stop_framework(&instance);
+
+        ulfius_clean_instance(&instance);
     }
 
-    ulfius_clean_instance(&instance);
+    ulfius_global_close();
 
     return 0;
 }
