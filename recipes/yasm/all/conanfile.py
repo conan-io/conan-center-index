d9744a597e05cb471e1bbcbddc65ff8ff3eaabc5
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-11-07 01:55:24 +0100

    yasm: don't build shared libraries

--- recipes/yasm/all/conanfile.py
+++ recipes/yasm/all/conanfile.py
@@ -71,6 +71,10 @@ class YASMConan(ConanFile):
     def _generate_cmake(self):
         tc = CMakeToolchain(self)
         tc.cache_variables["YASM_BUILD_TESTS"] = False
+        # Don't build shared libraries because:
+        # 1. autotools doesn't build shared libs either
+        # 2. the shared libs don't support static libc runtime (MT and such)
+        tc.cache_variables["BUILD_SHARED_LIBS"] = False
         tc.generate()
 
     def generate(self):
