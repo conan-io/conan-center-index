61ed945dd2181f09b5a1644afe967cbb5ab127a8
Chris Mc <christopherm@jfrog.com>
2022-11-06 17:44:02 -0800

    more percent encoding

--- linter/conandata_yaml_linter.py
+++ linter/conandata_yaml_linter.py
@@ -71,7 +71,7 @@ def main():
         print(
             f"::error file={args.path},line={error.context_mark.line},endline={error.problem_mark.line+1},"
             f"title=conandata.yml schema error"
-            f"::{error.problem} in %0A{error.context_mark.get_snippet()}\n"
+            f"::{error.problem} in %0A%60%60%60yml%0A{error.context_mark.get_snippet().replace("\n", "%0A")}%0A%60%60%60\n"
         )
     except BaseException as error:
         e = error.__str__().replace("\n", "%0A")
