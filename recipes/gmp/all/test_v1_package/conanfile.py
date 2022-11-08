fe08dbc6580ad90a929dea52f877c7e95c189104
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-08 02:08:20 +0100

    conan v2 support

---
+++ recipes/gmp/all/test_v1_package/conanfile.py
@@ -0,0 +1,22 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["ENABLE_CXX"] = self.options["gmp"].enable_cxx
+        cmake.definitions["TEST_PIC"] = "fPIC" in self.options["gmp"] and self.options["gmp"].fPIC
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
+            if self.options["gmp"].enable_cxx:
+                bin_path = os.path.join("bin", "test_package_cpp")
+                self.run(bin_path, run_environment=True)
