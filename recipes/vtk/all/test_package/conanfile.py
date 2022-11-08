29157dc4fe32ca4ce17e3192a256fc8bb1f87d76
Paul Harris <harris.pc@gmail.com>
2022-11-08 23:27:54 +0800

    Fixes for building vtk 9.2.2

--- recipes/vtk/all/test_package/conanfile.py
+++ recipes/vtk/all/test_package/conanfile.py
@@ -1,11 +1,19 @@
-from os import path
-from conan import ConanFile #, tools
-from conan.tools.cmake import CMakeToolchain, CMake
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -18,4 +26,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        self.run(path.join(".","test_package"), run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
