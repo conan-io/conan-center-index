1c0e4aeeeb2c8537d0508ae6404efff3212912d8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-10 21:08:03 +0100

    (#14118) autoconf: fix package info on windows
    * fix autoconf usage on windows
    
    * no win_bash in test method

--- recipes/autoconf/all/test_package/conanfile.py
+++ recipes/autoconf/all/test_package/conanfile.py
@@ -1,53 +1,51 @@
-import shutil
-from os import path
-
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.gnu import Autotools
-from conan.tools.microsoft import unix_path
-
-required_conan_version = ">=1.50.0"
+from conan.tools.env import Environment, VirtualBuildEnv
+from conan.tools.files import copy
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc
+import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    exports_sources = "configure.ac", "config.h.in", "Makefile.in", "test_package_c.c", "test_package_cpp.cpp",
-    generators = "AutotoolsDeps", "AutotoolsToolchain", "VirtualBuildEnv"
+    settings = "os", "arch", "compiler", "build_type"
     test_type = "explicit"
+    win_bash = True
 
     @property
     def _settings_build(self):
-        # TODO: Remove for Conan v2
         return getattr(self, "settings_build", self.settings)
 
-    @property
-    def win_bash(self):
-        return self._settings_build.os == "Windows"
+    def layout(self):
+        basic_layout(self)
 
     def build_requirements(self):
-        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
-            self.tool_requires("msys2/cci.latest")  # The conf `tools.microsoft.bash:path` and `tools.microsoft.bash:subsystem` aren't injected for test_package
         self.tool_requires(self.tested_reference_str)
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", check_type=str):
+            self.tool_requires("msys2/cci.latest")
+
+    def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+        tc = AutotoolsToolchain(self)
+        tc.generate()
+        if is_msvc(self):
+            env = Environment()
+            env.define("CC", "cl -nologo")
+            env.define("CXX", "cl -nologo")
+            env.vars(self).save_script("conanbuild_msvc")
 
     def build(self):
-        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
-            return  # autoconf needs a bash if there isn't a bash no need to build
-
-        for src in self.exports_sources:
-            shutil.copy(path.join(self.source_folder, src), self.build_folder)
-
+        for src in ("configure.ac", "config.h.in", "Makefile.in", "test_package_c.c", "test_package_cpp.cpp"):
+            copy(self, src, self.source_folder, self.build_folder)
         self.run("autoconf --verbose")
-
         autotools = Autotools(self)
         autotools.configure(build_script_folder=self.build_folder)
         autotools.make()
 
     def test(self):
-        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", default=False, check_type=bool):
-            return  # autoconf needs a bash if there isn't a bash no need to build
-
+        self.win_bash = None
         if can_run(self):
-            ext = ".exe" if self.settings.os == "Windows" else ""
-            test_cmd = unix_path(self, path.join(self.build_folder, f"test_package{ext}"))
-
-            self.run(test_cmd, env="conanbuild")
+            bin_path = os.path.join(self.build_folder, "test_package")
+            self.run(bin_path, env="conanrun")
