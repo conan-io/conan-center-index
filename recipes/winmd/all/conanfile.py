382d48a74023d67354f4d61b5e361281e57e2311
dvirtz <dvirtz@gmail.com>
2022-11-11 14:12:25 +0000

    fix gcc

--- recipes/winmd/all/conanfile.py
+++ recipes/winmd/all/conanfile.py
@@ -1,11 +1,12 @@
+import os
+
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
+from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
-import os
-
+from conan.tools.microsoft import is_msvc
 
 required_conan_version = ">=1.52.0"
 
@@ -71,3 +72,8 @@ class WinMDConan(ConanFile):
             dst=os.path.join(self.package_folder, "include"),
             src=os.path.join(self.source_folder, "src"),
         )
+
+    def package_info(self):
+        if not is_msvc(self):
+            # ignore shadowing errors
+            self.cpp_info.cppflags = ['-fpermissive']
