803ab4cb3701b3486ae0fef373625f54889505e1
dvirtz <dvirtz@gmail.com>
2022-11-11 14:24:49 +0000

    align test package to templates

--- recipes/polymorphic_value/all/test_package/conanfile.py
+++ recipes/polymorphic_value/all/test_package/conanfile.py
@@ -1,14 +1,20 @@
+import os
+
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake
+from conan.tools.cmake import CMake, cmake_layout
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
@@ -17,5 +23,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            cmake = CMake(self)
-            cmake.test()
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
--- recipes/polymorphic_value/all/test_v1_package/conanfile.py
+++ recipes/polymorphic_value/all/test_v1_package/conanfile.py
@@ -1,8 +1,11 @@
-from conans import ConanFile, CMake, tools
 import os
 
+from conan.tools.build import cross_building
+from conans import CMake, ConanFile
 
-class TestPackageConan(ConanFile):
+
+# legacy validation with Conan 1.x
+class TestPackageV1Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
@@ -12,5 +15,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            self.run(os.path.join("bin", "test_package"), run_environment=True)
+        if not cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
