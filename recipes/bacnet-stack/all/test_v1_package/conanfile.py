119a449ed234e0dfc2909989e819a14cee14ac65
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-15 00:46:54 +0100

    C test package

--- recipes/bacnet-stack/all/test_v1_package/conanfile.py
+++ recipes/bacnet-stack/all/test_v1_package/conanfile.py
@@ -13,6 +13,6 @@ class TestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             bin_path = self.run(bin_path, run_environment=True)
