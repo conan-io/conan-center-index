9e7d40ef86890879c21ac697aa5ad50eb82cbd93
vhorvath-gs <vhorvath@graphisoft.com>
2022-11-14 14:17:57 +0100

    Tidy up the conanfile.py
    The methods are defined in the order the create command calls them and
    unused setting were removed.

--- recipes/archicad-apidevkit/all/conanfile.py
+++ recipes/archicad-apidevkit/all/conanfile.py
@@ -1,29 +1,42 @@
 from conan import ConanFile
 from conan.tools.files import copy, get
+from conan.tools.microsoft import check_min_vs, is_msvc
 from conan.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.52.0"
 
 
-class ArchicadApidevkitConan (ConanFile):
+class ArchicadApidevkitConan(ConanFile):
     name = "archicad-apidevkit"
     description = "The General API Development Kit enables software developers to extend the functionality of Archicad"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://archicadapi.graphisoft.com/"
     license = "LicenseRef-LICENSE"
-    settings = "os", "arch", "build_type", "compiler"
+    settings = "os", "arch"
     no_copy_source = True
     topics = "api", "archicad", "development"
     short_paths = True
 
-    @property
-    def _acdevkit_arch(self):
-        return str(self.settings.arch)
+    def validate(self):
+        if is_msvc(self):
+            # Approximate requirement for toolset >= v142
+            check_min_vs(self, "192")
+        if not self.info.settings.os in ("Macos", "Windows"):
+            raise ConanInvalidConfiguration(
+                f"{self.ref} is not supported by the OS {self.info.settings.os}")
+        if not str(self.settings.arch) in ("x86_64"):
+            raise ConanInvalidConfiguration(
+                f"{self.ref} is not supported yet.")
 
-    def package_id(self):
-        del self.info.settings.compiler
-        del self.info.settings.build_type
+    def build(self):
+        devkit, licenses = self.conan_data["sources"][self.version][str(self.settings.os)][str(self.settings.arch)]
+        get(self, **devkit, destination=os.path.join(self.package_folder, "bin"), strip_root=True)
+        get(self, **licenses, destination=os.path.join(self.package_folder, "licenses"), strip_root=True)
+
+    def package(self):
+        copy(self, "bin", src=self.build_folder, dst=self.package_folder)
+        copy(self, "licenses", src=self.build_folder, dst=self.package_folder)
 
     def package_info(self):
         self.cpp_info.bindirs = []
@@ -35,22 +48,3 @@ class ArchicadApidevkitConan (ConanFile):
         self.output.info(f"Setting AC_API_DEVKIT_DIR environment variable: {devkit_dir}")
         self.env_info.AC_API_DEVKIT_DIR = devkit_dir
         self.buildenv_info.define("AC_API_DEVKIT_DIR", devkit_dir)
-
-    def validate(self):
-        if not self.info.settings.os in ("Macos", "Windows"):
-            raise ConanInvalidConfiguration(
-                f"{self.ref} is not supported by the OS {self.info.settings.os}")
-        if not str(self.settings.arch) in ("x86_64"):
-            raise ConanInvalidConfiguration(
-                f"{self.ref} is not supported yet.")
-
-    def build(self):
-        get(self, **self.conan_data["sources"][self.version][str(self.settings.os)]
-            [self._acdevkit_arch][0], destination=self.build_folder, strip_root=True)
-        get(self, **self.conan_data["sources"][self.version][str(self.settings.os)]
-            [self._acdevkit_arch][1], destination=self.build_folder, strip_root=True)
-
-    def package(self):
-        copy(self, "*", src=self.build_folder, dst=os.path.join(self.package_folder, "bin"))
-        copy(self, "LICENSE", src=self.build_folder, dst=os.path.join(self.package_folder, "licenses"))
-        os.remove(os.path.join(self.package_folder, "bin/LICENSE"))
