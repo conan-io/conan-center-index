16532b82d9e679399f033f6aaf313697a06851ee
Christopher McArthur <christopherm@jfrog.com>
2022-11-14 15:36:04 -0800

    v2 fixes

--- recipes/catch2/2.x.x/test_package/conanfile.py
+++ recipes/catch2/2.x.x/test_package/conanfile.py
@@ -3,6 +3,7 @@ from conan.tools.cmake import CMake, CMakeToolchain
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout
 import os
+import yaml
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -10,6 +11,10 @@ class TestPackageConan(ConanFile):
     test_type = "explicit"
 
     _tests_todo = []
+
+    @property
+    def _todos_filename(self):
+        return os.path.join(self.recipe_folder, self.folders.test_output, self.folders.generators,"catch2_test_to_do.yml")
 
     def requirements(self):
         self.requires(self.tested_reference_str)
@@ -29,6 +34,9 @@ class TestPackageConan(ConanFile):
         if not catch_opts.with_prefix and catch_opts.with_main and catch_opts.with_benchmark:
             self._tests_todo.append("benchmark")
 
+        with open(self._todos_filename, "w") as file:
+            yaml.dump(self._tests_todo, file)
+
     def layout(self):
         cmake_layout(self)
 
@@ -38,6 +46,8 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
+        with open(self._todos_filename, "r") as file:
+            self._tests_todo = yaml.safe_load(file)
         if can_run(self):
             for test_name in self._tests_todo:
                 self.run(os.path.join(self.cpp.build.bindirs[0], test_name), env="conanrun")
