4eb2a544c5df60ccacd520a81cf711255286d171
toge <toge.mail@gmail.com>
2022-11-17 00:12:36 +0900

    import basic_layout
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/scnlib/all/conanfile.py
+++ recipes/scnlib/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.tools.microsoft import check_min_vs
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
+from conan.tools.layout import basic_layout
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
 import os
