a4481d58763c4344052c1037298d5efb370a2983
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-11-18 17:08:54 +0100

    civetweb: bump openssl

--- recipes/civetweb/all/conanfile.py
+++ recipes/civetweb/all/conanfile.py
@@ -87,7 +87,7 @@ class CivetwebConan(ConanFile):
 
     def requirements(self):
         if self.options.with_ssl:
-            self.requires("openssl/1.1.1q")
+            self.requires("openssl/1.1.1s")
         if self.options.get_safe("with_zlib"):
             self.requires("zlib/1.2.13")
 
