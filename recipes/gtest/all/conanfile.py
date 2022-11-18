cebd8992374ef3be83c69b11461dcba7c3c1afa6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-18 11:38:30 +0100

    modernize more

--- recipes/gtest/all/conanfile.py
+++ recipes/gtest/all/conanfile.py
@@ -1,14 +1,13 @@
-import os
-
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, get, replace_in_file, rm, rmdir
-from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 from conan.tools.scm import Version
+import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class GTestConan(ConanFile):
