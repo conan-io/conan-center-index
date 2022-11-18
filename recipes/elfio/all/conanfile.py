d9bf58c371c3fedb2ba02f94e8991b6c7e2ffa2a
Serge Lamikhov-Center <to_serge@hotmail.com>
2022-11-18 16:04:52 +0000

    elfio: Add version 3.11

--- recipes/elfio/all/conanfile.py
+++ recipes/elfio/all/conanfile.py
@@ -1,5 +1,8 @@
+from conan import ConanFile
+from conan.tools.layout import basic_layout
+from conan.tools.files import get, copy, rename
+from conan.tools.build import check_min_cppstd
 import os
-from conans import ConanFile, tools
 
 
 class ElfioConan(ConanFile):
@@ -7,26 +10,25 @@ class ElfioConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "http://elfio.sourceforge.net"
     description = "A header-only C++ library that provides a simple interface for reading and generating files in ELF binary format."
-    topics = ("conan", "elfio", "elf")
+    topics = ("elfio", "elf")
     license = "MIT"
     settings = "compiler"
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
     def configure(self):
         if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, 11)
+            check_min_cppstd(self, 14)
+
+    def layout(self):
+       basic_layout(self, src_folder="source")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("elfio-{}".format(self.version), self._source_subfolder)
+        get(**self.conan_data["sources"][self.version])
+        rename("elfio-{}".format(self.version), self.source_folder)
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def package(self):
-        self.copy("COPYING", src=self._source_subfolder, dst="licenses")
-        self.copy(pattern=os.path.join("elfio", "*.hpp"), src=self._source_subfolder, dst="include")
+        copy(self, "COPYING", src=self.source_folder, dst="licenses")
+        copy(self, pattern=os.path.join("elfio", "*.hpp"), src=self.source_folder, dst="include")
