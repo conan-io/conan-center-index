76d39ab6e780fb470d0bc9dd3d64056af716294a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-19 20:21:26 +0100

    fix CMake imported target

--- recipes/gcem/all/conanfile.py
+++ recipes/gcem/all/conanfile.py
@@ -1,8 +1,9 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import copy, get
+from conan.tools.files import copy, get, save
 from conan.tools.layout import basic_layout
 import os
+import textwrap
 
 required_conan_version = ">=1.50.0"
 
@@ -39,6 +40,33 @@ class GcemConan(ConanFile):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         copy(self, "*", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
 
+        # TODO: to remove in conan v2
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"gcem": "gcem::gcem"},
+        )
+
+    def _create_cmake_module_alias_targets(self, module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent(f"""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """)
+        save(self, module_file, content)
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join("lib", "cmake", f"conan-official-{self.name}-targets.cmake")
+
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "gcem")
+        self.cpp_info.set_property("cmake_target_name", "gcem")
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
+
+        # TODO: to remove in conan v2
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
