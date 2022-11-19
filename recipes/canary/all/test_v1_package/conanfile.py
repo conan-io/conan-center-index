e8f33505a344ad5b7603df84efbd26128d21856e
Alexey Klimkin <klimkin@gmail.com>
2022-11-19 15:11:33 -0800

    Update recipes/canary/all/test_v1_package/conanfile.py
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/canary/all/test_v1_package/conanfile.py
+++ recipes/canary/all/test_v1_package/conanfile.py
@@ -14,5 +14,5 @@ class TestPackage(ConanFile):
 
     def test(self):
         if not tools.cross_building(self):
-            bin_path = os.path.join(".", "test_package")
+            bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
