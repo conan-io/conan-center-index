637452d109637ec284b854301728e1459a58e27c
toge <toge.mail@gmail.com>
2022-11-21 03:40:54 +0900

    drop support apple-clang 11.0

--- recipes/cpp-lazy/all/conanfile.py
+++ recipes/cpp-lazy/all/conanfile.py
@@ -33,8 +33,10 @@ class CpplazyConan(ConanFile):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
 
-        if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "6.0.0":
-            raise ConanInvalidConfiguration(f"{self.ref} doesn't support gcc < 6.0.0.")
+        if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "6.0":
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support gcc < 6.0.")
+        if self.settings.compiler == "apple-clang" and Version(self.settings.compiler.version) < "12.0":
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support apple-clang < 12.0.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder)
