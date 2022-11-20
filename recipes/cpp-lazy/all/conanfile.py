288f0daf9212f8721d3f9636401699e278202139
toge <toge.mail@gmail.com>
2022-11-20 10:25:24 +0900

    drop support gcc 5

--- recipes/cpp-lazy/all/conanfile.py
+++ recipes/cpp-lazy/all/conanfile.py
@@ -1,12 +1,13 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import get, copy
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
+
 import os
 
-
 required_conan_version = ">=1.52.0"
-
 
 class CpplazyConan(ConanFile):
     name = "cpp-lazy"
@@ -31,6 +32,9 @@ class CpplazyConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
+
+        if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "6.0.0":
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support gcc < 6.0.0.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder)
