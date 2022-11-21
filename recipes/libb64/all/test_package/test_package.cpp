d9db4e846d71cee63bc26243e7210e9c518a35b8
KGrzeg <kupczykgrzeg@gmail.com>
2022-11-21 10:45:18 +0100

    Typo in example code

--- recipes/libb64/all/test_package/test_package.cpp
+++ recipes/libb64/all/test_package/test_package.cpp
@@ -8,7 +8,7 @@ extern "C" {
 int main() {
     const char str[] = "Hello world!";
     const unsigned len = sizeof(str);
-    char out[len*2] {0}, rev[len*2] = {0};
+    char out[len*2] = {0}, rev[len*2] = {0};
     
     base64_encodestate E;
     base64_init_encodestate(&E);
