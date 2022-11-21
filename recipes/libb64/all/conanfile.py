5c3e3ae3d9a8e8fb311e2e71462f9da45b5e80e4
KGrzeg <kupczykgrzeg@gmail.com>
2022-11-21 09:18:26 +0100

    libb64: add recipe

---
+++ recipes/libb64/all/conanfile.py
@@ -0,0 +1,48 @@
+from conan import ConanFile
+from conan.tools.files import get, copy
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+import os
+
+
+required_conan_version = ">=1.53.0"
+
+class Libb64Conan(ConanFile):
+    name = "libb64"    
+    description = "A library of ANSI C routines for fast encoding/decoding data into and from a base64-encoded format."
+    author = "Chris Venter"
+    license = "CC0-1.0"
+    url = "https://github.com/KGrzeg/libb64-conan"
+    homepage = "https://libb64.sourceforge.net/"
+    topics = ("base64", "codec", "encoder", "decoder")
+    settings = "os", "arch", "compiler", "build_type"
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(
+            self,
+            pattern="*.h",
+            dst=os.path.join(self.package_folder, "include"),
+            src=os.path.join(self.source_folder, "include"),
+        )
+
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["libb64"]
