aba2bde0914953f945b31cfaeff166e9b69bac13
Rainer Sabelka <saba@sabanet.at>
2022-11-22 20:46:56 +0100

    simplify recipe and adapt for 2.0

--- recipes/libftdi/1.x/conanfile.py
+++ recipes/libftdi/1.x/conanfile.py
@@ -1,6 +1,8 @@
 import os
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.files import get, patch, rmdir
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.errors import ConanInvalidConfiguration
 
 
 class LibFtdiConan(ConanFile):
@@ -11,7 +13,7 @@ class LibFtdiConan(ConanFile):
     homepage = "https://www.intra2net.com/en/developer/libftdi/"
     url = "https://github.com/conan-io/conan-center-index"
     exports_sources = ["CMakeLists.txt", "patches/*"]
-    generators = "cmake", "cmake_find_package", "pkg_config"
+    generators = "CMakeDeps", "CMakeToolchain"
     settings = "os", "arch", "compiler", "build_type"
     options = {
             "shared"             : [True, False], 
@@ -29,20 +31,18 @@ class LibFtdiConan(ConanFile):
     }
     _cmake = None
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "libftdi1-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version],
+            strip_root=True)
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
             if self.settings.compiler == "Visual Studio":
                 self.options.use_streaming = False
+
+    def layout(self):
+        cmake_layout(self)
 
     def configure(self):
         if self.options.shared:
@@ -62,8 +62,7 @@ class LibFtdiConan(ConanFile):
             "ENABLE_STREAMING": self.options.use_streaming,
             "LIB_SUFFIX": "",
         }
-        self._cmake.definitions.update(options)
-        self._cmake.configure()
+        self._cmake.configure(variables = options)
         return self._cmake
 
     def requirements(self):
@@ -76,30 +75,26 @@ class LibFtdiConan(ConanFile):
             raise ConanInvalidConfiguration("VS doesn't not compile with enabled option use_streaming")
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        for p in self.conan_data.get("patches", {}).get(self.version, []):
+            patch(self, **p)
         cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="LICENSE", dst="licenses")
         cmake = self._configure_cmake()
         cmake.install()
         lib_folder = os.path.join(self.package_folder, "lib",)
-        tools.rmdir(os.path.join(lib_folder, "cmake"))
-        tools.rmdir(os.path.join(lib_folder, "pkgconfig"))
+        rmdir(self, os.path.join(lib_folder, "cmake"))
+        rmdir(self, os.path.join(lib_folder, "pkgconfig"))
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "LibFTDI1"
-        self.cpp_info.names["cmake_find_package_multi"] = "LibFTDI1"
-        self.cpp_info.names["pkgconfig"] = "libftdi1"
-
-        self.cpp_info.components["ftdi"].names["pkg_config"] = "libftdi1"
+        self.cpp_info.names['cmake_find_package'] = "LibFTDI1"
+        self.cpp_info.names['cmake_find_package_multi'] = "LibFTDI1"
         self.cpp_info.components["ftdi"].libs = ["ftdi1"]
         self.cpp_info.components["ftdi"].requires = ["libusb::libusb"]
         self.cpp_info.components["ftdi"].includedirs.append(os.path.join("include", "libftdi1"))
 
         if self.options.enable_cpp_wrapper:
-            self.cpp_info.components["ftdipp"].names["pkg_config"] = "libftdi1pp"
             self.cpp_info.components["ftdipp"].libs = ["ftdipp1"]
             self.cpp_info.components["ftdipp"].requires = ["ftdi", "boost::headers"]
