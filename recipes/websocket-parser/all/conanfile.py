3d064c69511baeb3cce1c1acbc77b0c082b5364c
Gary Reynolds <gary.reynolds@optiver.com.au>
2022-11-23 16:21:49 +1100

    Update to satisfy hooks while testing on Macos

--- recipes/websocket-parser/all/conanfile.py
+++ recipes/websocket-parser/all/conanfile.py
@@ -1,6 +1,8 @@
-from conan import ConanFile
-from conans.client.build.autotools_environment import AutoToolsBuildEnvironment
-from conan.tools.files import apply_conandata_patches, chdir, get
+from conan import ConanFile, tools
+from conan.tools.apple import is_apple_os
+from conan.tools.files import apply_conandata_patches, collect_libs, get
+from conans import AutoToolsBuildEnvironment
+
 
 class WebsocketParserConan(ConanFile):
     name = "websocket-parser"
@@ -13,20 +15,33 @@ class WebsocketParserConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def build(self):
         apply_conandata_patches(self)
         buildenv = AutoToolsBuildEnvironment(self)
-        buildenv.fpic = self.options.fPIC
         buildenv.flags = ["-std=gnu99", "-pedantic"]
-        buildenv.make(target="solib" if self.options.shared else "alib")
+        if not self.options.shared:
+            buildenv.fpic = self.options.fPIC
+            buildenv.make(target="alib")
+        elif is_apple_os(self):
+            buildenv.make(target="dylib")
+        else:
+            buildenv.make(target="solib")
 
     def package(self):
+        self.copy("LICENSE", dst="licenses", keep_path=False)
         self.copy("*.h", dst="include", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["libwebsocket_parser"]
+        self.cpp_info.libs = collect_libs(self)
