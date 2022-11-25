22ce88b583d078f5b93baac673c6d8802d12c2e0
Jan Niklas Grieb (home) <jan.grieb@gmail.com>
2022-11-25 22:37:01 +0100

    openssl 3.x.x test package: do not skip test for certain MacOS variants
    removed the _skip_test part taken from "v2 test package" for 1.x.x
    as it was not in "v1 test package" for 3.x.x

--- recipes/openssl/3.x.x/test_package/conanfile.py
+++ recipes/openssl/3.x.x/test_package/conanfile.py
@@ -13,18 +13,6 @@ class TestPackageConan(ConanFile):
     test_type = "explicit"
 
     @property
-    def _skip_test(self):
-        # Attempting to use @rpath without CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG being
-        # set. This could be because you are using a Mac OS X version less than 10.5
-        # or because CMake's platform configuration is corrupt.
-        # FIXME: Remove once CMake on macOS/M1 CI runners is upgraded.
-        # Actually the workaround should be to add cmake/3.22.0 to build requires,
-        # but for the specific case of openssl it fails because it is also a requirement of cmake.
-        # see https://github.com/conan-io/conan/pull/9839
-        return self.settings.os == "Macos" and self.settings.arch == "armv8" \
-               and self.options["openssl"].shared
-
-    @property
     def _with_legacy(self):
         openssl = self.dependencies["openssl"]
         return (not openssl.options.no_legacy and
@@ -51,13 +39,12 @@ class TestPackageConan(ConanFile):
 
 
     def build(self):
-        if not self._skip_test:
-            cmake = CMake(self)
-            cmake.configure()
-            cmake.build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
-        if not self._skip_test and can_run(self):
+        if can_run(self):
             bin_path = os.path.join(self.cpp.build.bindirs[0], "digest")
             self.run(bin_path, env="conanrun")
 
