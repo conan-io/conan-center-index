de70882935cc1aea7f6fc0469c4a94fe1a5f818b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-25 13:57:04 +0100

    modernize more

--- recipes/vulkan-loader/all/conanfile.py
+++ recipes/vulkan-loader/all/conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.gnu import PkgConfigDeps
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class VulkanLoaderConan(ConanFile):
@@ -60,18 +60,9 @@ class VulkanLoaderConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
-        try:
-            del self.settings.compiler.libcxx
-        except Exception:
-            pass
-        try:
-            del self.settings.compiler.cppstd
-        except Exception:
-            pass
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
