14e2e8d6caff5f43019fa142d3961de9721996c4
Jihadist <tomasiche@gmail.com>
2022-11-26 17:40:10 +0300

    libev for msvc

--- recipes/libev/all/conanfile.py
+++ recipes/libev/all/conanfile.py
@@ -2,8 +2,9 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import copy, get, rm, rmdir
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, get, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc, unix_path
 import os
@@ -33,6 +34,9 @@ class LibevConan(ConanFile):
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -47,14 +51,12 @@ class LibevConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def validate(self):
-        if is_msvc(self):
-            raise ConanInvalidConfiguration(f"{self.ref} is not supported by Visual Studio")
         if self.info.settings.os == "Windows" and self.info.options.shared:
             # libtool:   error: can't build i686-pc-mingw32 shared library unless -no-undefined is specified
             raise ConanInvalidConfiguration(f"{self.ref} can't be built as shared on Windows")
 
     def build_requirements(self):
-        if self._settings_build.os == "Windows":
+        if self._settings_build.os == "Windows" and not is_msvc(self):
             self.win_bash = True
             if not self.conf.get("tools.microsoft.bash:path", check_type=str):
                 self.tool_requires("msys2/cci.latest")
@@ -64,21 +66,37 @@ class LibevConan(ConanFile):
             destination=self.source_folder, strip_root=True)
 
     def generate(self):
-        env = VirtualBuildEnv(self)
-        env.generate()
-        tc = AutotoolsToolchain(self)
-        tc.generate()
+        if is_msvc(self):
+            tc = CMakeToolchain(self)
+            tc.generate()
+            tc = CMakeDeps(self)
+            tc.generate()
+        else:    
+            env = VirtualBuildEnv(self)
+            env.generate()
+            tc = AutotoolsToolchain(self)
+            tc.generate()
 
     def build(self):
-        autotools = Autotools(self)
-        autotools.configure()
-        autotools.make()
+        if is_msvc(self):
+            apply_conandata_patches(self)
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
+        else:    
+            autotools = Autotools(self)
+            autotools.configure()
+            autotools.make()
 
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
-        autotools = Autotools(self)
         # TODO: replace by autotools.install() once https://github.com/conan-io/conan/issues/12153 fixed
-        autotools.install(args=[f"DESTDIR={unix_path(self, self.package_folder)}"])
+        if is_msvc(self):
+            cmake = CMake(self)
+            cmake.install()
+        else:    
+            autotools = Autotools(self)
+            autotools.install(args=[f"DESTDIR={unix_path(self, self.package_folder)}"])
         rmdir(self, os.path.join(self.package_folder, "share"))
         rm(self, "*.la", os.path.join(self.package_folder, "lib"))
         fix_apple_shared_install_name(self)
