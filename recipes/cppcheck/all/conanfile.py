7a3131c1884c2287eeb119d17f6dccddfaf87f38
Sneder89 <45610045+Sneder89@users.noreply.github.com>
2022-11-27 19:38:56 +0100

    Update recipes/cppcheck/all/conanfile.py
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/cppcheck/all/conanfile.py
+++ recipes/cppcheck/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.files import apply_conandata_patches, copy, export_conandata_pa
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.46.0"
+required_conan_version = ">=1.52.0"
 
 
 class CppcheckConan(ConanFile):
