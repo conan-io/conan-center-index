89abbfb78ee7bad8b191f558565293cfeb528c59
toge <toge.mail@gmail.com>
2022-11-29 00:50:13 +0900

    update msvc version

--- recipes/homog2d/all/conanfile.py
+++ recipes/homog2d/all/conanfile.py
@@ -45,7 +45,7 @@ class Homog2dConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
-        check_min_vs(self, 191)
+        check_min_vs(self, 192)
         if not is_msvc(self):
             minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
             if minimum_version and Version(self.settings.compiler.version) < minimum_version:
