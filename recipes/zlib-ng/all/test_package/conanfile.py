fe3886c60c66f70f77432b98b903f243c085d9c0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-28 07:44:57 +0100

    (#14303) zlib-ng: modernize more

--- recipes/zlib-ng/all/test_package/conanfile.py
+++ recipes/zlib-ng/all/test_package/conanfile.py
@@ -1,13 +1,16 @@
-import os
-
 from conan import ConanFile
+from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.build import cross_building
+import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
 
     def requirements(self):
         self.requires(self.tested_reference_str)
@@ -17,10 +20,7 @@ class TestPackageConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def layout(self):
-        cmake_layout(self)
-
     def test(self):
-        if not cross_building(self):
+        if can_run(self):
             cmd = os.path.join(self.cpp.build.bindirs[0], "test_package")
             self.run(cmd, env="conanrun")
