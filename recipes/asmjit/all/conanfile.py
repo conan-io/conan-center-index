2822fb56b4323f56d81c069e7ee973e78b8823d9
toge <toge.mail@gmail.com>
2022-11-28 12:25:23 +0900

    (#14364) asmjit: add version cci.20221111
    * asmjit: add version cci.20221111
    
    * improve recipe
    
    * link math lib

--- recipes/asmjit/all/conanfile.py
+++ recipes/asmjit/all/conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir
 import os
 
-required_conan_version = ">=1.50.0"
+required_conan_version = ">=1.53.0"
 
 
 class AsmjitConan(ConanFile):
@@ -32,7 +32,7 @@ class AsmjitConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
 
     def validate(self):
         if self.info.settings.compiler.cppstd:
@@ -77,4 +77,4 @@ class AsmjitConan(ConanFile):
         if not self.options.shared:
             self.cpp_info.defines = ["ASMJIT_STATIC"]
         if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs = ["pthread", "rt"]
+            self.cpp_info.system_libs = ["pthread", "rt", "m"]
