e73da71242ebb6cec7251e50db47dfdb59f593e1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-28 06:44:57 +0100

    (#14300) taocpp-sequences: conan v2 support

--- recipes/taocpp-sequences/all/conanfile.py
+++ recipes/taocpp-sequences/all/conanfile.py
@@ -1,7 +1,10 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
 import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.50.0"
 
 
 class TaoCPPSequencesonan(ConanFile):
@@ -14,28 +17,32 @@ class TaoCPPSequencesonan(ConanFile):
     no_copy_source = True
     settings = "os", "arch", "compiler", "build_type"
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 11)
-
-    def package_id(self):
-        self.info.header_only()
+            check_min_cppstd(self, 11)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def build(self):
+        pass
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("*", dst="include", src=os.path.join(self._source_subfolder, "include"))
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "taocpp-sequences")
         self.cpp_info.set_property("cmake_target_name", "taocpp::sequences")
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
 
         # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "taocpp-sequences"
@@ -45,3 +52,5 @@ class TaoCPPSequencesonan(ConanFile):
         self.cpp_info.components["_taocpp-sequences"].names["cmake_find_package"] = "sequences"
         self.cpp_info.components["_taocpp-sequences"].names["cmake_find_package_multi"] = "sequences"
         self.cpp_info.components["_taocpp-sequences"].set_property("cmake_target_name", "taocpp::sequences")
+        self.cpp_info.components["_taocpp-sequences"].bindirs = []
+        self.cpp_info.components["_taocpp-sequences"].libdirs = []
