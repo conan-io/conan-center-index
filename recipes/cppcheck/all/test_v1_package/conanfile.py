afcf3bad66e2834792e5f2107d3bb16812275832
Sneder89 <45610045+Sneder89@users.noreply.github.com>
2022-11-28 21:25:25 +0100

    (#14149) [cppcheck] Add version 2.9.1
    * Update conandata.yml
    
    * Update config.yml
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conandata.yml
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Martin Delille <martin@delille.org>
    
    * Update conanfile.py
    
    * Update conandata.yml
    
    * Update conanfile.py
    
    * Update conandata.yml
    
    * Update conanfile.py
    
    * Update config.yml
    
    * Delete recipes/cppcheck/all/patches directory
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update recipes/cppcheck/all/test_package/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/cppcheck/all/test_package/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update recipes/cppcheck/all/test_package/conanfile.py
    
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update config.yml
    
    * Update conandata.yml
    
    * changes
    
    * changes
    
    * Update conandata.yml
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update conanfile.py
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Martin Delille <martin@delille.org>
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * get test package working
    
    * add v1 test package
    
    * moved changes from replace to file to patches
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update 0003-pcre-debuglib-name-2.7.patch
    
    * Update 0003-pcre-debuglib-name-2.7.patch
    
    * Update 0003-pcre-debuglib-name-2.7.patch
    
    * Update 0003-pcre-debuglib-name-2.7.patch
    
    * Update 0003-pcre-debuglib-name-2.7.patch
    
    * Update 0003-pcre-debuglib-name-2.7.patch
    
    * Update 0003-pcre-debuglib-name-2.7.patch
    
    * Update 0003-pcre-debuglib-name-2.7.patch
    
    * Update conanfile.py
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update recipes/cppcheck/all/conanfile.py
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * use tool_requires in test package, add required fields in conandata.yml file
    
    Co-authored-by: Martin Delille <martin@delille.org>
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/cppcheck/all/test_v1_package/conanfile.py
+++ recipes/cppcheck/all/test_v1_package/conanfile.py
@@ -6,16 +6,13 @@ import sys
 class TestPackageConan(ConanFile):
     settings = "os", "arch"
     generators = "VirtualRunEnv"
-    test_type = "explicit"
-
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
 
     def test(self):
         if can_run(self):
             self.run("cppcheck --enable=warning,style,performance --std=c++11 .",
                      cwd=self.source_folder, run_environment=True)
             if self.settings.os == "Windows":
+                # Unable to work with Environment variable CPPCHECK_HTML_REPORT
                 self.run(f"{sys.executable} %CPPCHECK_HTMLREPORT% -h", run_environment=True)
             else:
                 self.run("cppcheck-htmlreport -h", run_environment=True)
