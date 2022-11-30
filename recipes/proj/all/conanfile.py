057ad3f1a15ce5a5d8afe800d7e5de5ac0504641
Paul Harris <harris.pc@gmail.com>
2022-11-30 23:33:47 +0800

    Revert previous commit, this time just remove TODO and if

--- recipes/proj/all/conanfile.py
+++ recipes/proj/all/conanfile.py
@@ -98,6 +98,22 @@ class ProjConan(ConanFile):
                                   "set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)",
                                   "")
 
+        # Aggressive workaround against SIP on macOS, to handle sqlite3 executable
+        # linked to shared sqlite3 lib
+        if is_apple_os(self):
+            if Version(self.version) < "8.1.0":
+                cmake_sqlite_call = "CMakeLists.txt"
+                pattern = "${EXE_SQLITE3}"
+            else:
+                cmake_sqlite_call = "generate_proj_db.cmake"
+                pattern = "\"${EXE_SQLITE3}\""
+            lib_paths = self.dependencies.build["sqlite3"].cpp_info.libdirs
+            replace_in_file(self,
+                os.path.join(self.source_folder, "data", cmake_sqlite_call),
+                f"COMMAND {pattern}",
+                f"COMMAND ${{CMAKE_COMMAND}} -E env \"DYLD_LIBRARY_PATH={':'.join(lib_paths)}\" {pattern}"
+            )
+
         # unvendor nlohmann_json
         if Version(self.version) < "8.1.0":
             rmdir(self, os.path.join(self.source_folder, "include", "proj", "internal", "nlohmann"))
