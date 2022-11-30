8d4b103b747d61b3bba06c5dd4bdbaeb01276042
Sneder89 <45610045+Sneder89@users.noreply.github.com>
2022-11-30 14:41:38 +0100

    return to old codebase

--- recipes/doxygen/all/test_package/conanfile.py
+++ recipes/doxygen/all/test_package/conanfile.py
@@ -1,32 +1,22 @@
-from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeDeps
-from conan.errors import ConanException
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanException
 import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "arch"
-    generators = "VirtualRunEnv"
-    test_type = "explicit"
-
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
-
-    def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake"
 
     def build(self):
-        if can_run(self):
-            tc = CMake(self)
-            tc.configure()
-            tc.build()
+        if not tools.cross_building(self, skip_x64_x86=True):
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
 
     def test(self):
-        if can_run(self):
+        if not tools.cross_building(self, skip_x64_x86=True):
             if not os.path.isdir(os.path.join(self.build_folder, "html")):
                 raise ConanException("doxygen did not create html documentation directory")
+
             self.output.info("Version:")
             self.run("doxygen --version", run_environment=True)
-
