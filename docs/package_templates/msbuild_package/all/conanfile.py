87f2a846343cd46188b7607dbaa634b3cb56462e
Chris Mc <prince.chrismc@gmail.com>
2022-12-01 14:51:31 -0800

    templates: bunch of small cleanups

--- docs/package_templates/msbuild_package/all/conanfile.py
+++ docs/package_templates/msbuild_package/all/conanfile.py
@@ -61,7 +61,7 @@ class PackageConan(ConanFile):
         self.tool_requires("tool/x.y.z")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = MSBuildToolchain(self)
