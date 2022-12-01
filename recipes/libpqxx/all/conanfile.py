630d4a4b9c2458099884399100000ea6f4a6b2b3
toge <toge.mail@gmail.com>
2022-12-02 01:08:25 +0900

    use msvc_runtime_flag

--- recipes/libpqxx/all/conanfile.py
+++ recipes/libpqxx/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
-from conan.tools.microsoft import check_min_vs, is_msvc
+from conan.tools.microsoft import check_min_vs, is_msvc, msvc_runtime_flag
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
@@ -79,7 +79,7 @@ class LibpqxxConan(ConanFile):
         else:
             check_min_vs(self, 192)
 
-        if is_msvc(self) and self.options.shared and self.settings.compiler.runtime == "MTd":
+        if is_msvc(self) and self.options.shared and msvc_runtime_flag(self) == "MTd":
             raise ConanInvalidConfiguration(f"{self.ref} recipes does not support build shared library with MTd runtime.")
 
         if not is_msvc(self):
