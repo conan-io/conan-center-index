3da99bbdcec5f876888cfdf1858835997cf4bd79
toge <toge.mail@gmail.com>
2022-12-01 15:00:21 +0900

    cassandra-cpp-driver: add version 2.16.2, support conan v2

--- recipes/cassandra-cpp-driver/all/conanfile.py
+++ recipes/cassandra-cpp-driver/all/conanfile.py
@@ -1,19 +1,19 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, replace_in_file
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 import os
 
-required_conan_version = ">=1.33.0"
-
+required_conan_version = ">=1.53.0"
 
 class CassandraCppDriverConan(ConanFile):
     name = "cassandra-cpp-driver"
+    description = "DataStax C/C++ Driver for Apache Cassandra and DataStax Products"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://docs.datastax.com/en/developer/cpp-driver/"
-    description = "DataStax C/C++ Driver for Apache Cassandra and DataStax Products"
-    topics = ("cassandra", "cpp-driver", "database", "conan-recipe")
-
-    settings = "os", "compiler", "build_type", "arch"
+    topics = ("cassandra", "cpp-driver", "database",)
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
@@ -34,19 +34,10 @@ class CassandraCppDriverConan(ConanFile):
         "with_kerberos": False,
         "use_timerfd": True,
     }
-
     short_paths = True
-    generators = "cmake"
-    exports_sources = [
-        "CMakeLists.txt",
-        "patches/*"
-    ]
 
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -55,22 +46,25 @@ class CassandraCppDriverConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("libuv/1.44.1")
+        self.requires("libuv/1.44.2")
         self.requires("http_parser/2.9.4")
-        self.requires("rapidjson/cci.20211112")
+        self.requires("rapidjson/cci.20220822")
 
         if self.options.with_openssl:
-            self.requires("openssl/1.1.1q")
+            self.requires("openssl/1.1.1s")
 
         if self.options.with_zlib:
-            self.requires("minizip/1.2.12")
-            self.requires("zlib/1.2.12")
+            self.requires("minizip/1.2.13")
+            self.requires("zlib/1.2.13")
 
         if self.options.use_atomic == "boost":
-            self.requires("boost/1.79.0")
+            self.requires("boost/1.80.0")
 
     def validate(self):
         if self.options.use_atomic == "boost":
@@ -84,69 +78,66 @@ class CassandraCppDriverConan(ConanFile):
                 "Kerberos is not supported at the moment")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["VERSION"] = self.version
+        tc.variables["CASS_BUILD_EXAMPLES"] = False
+        tc.variables["CASS_BUILD_INTEGRATION_TESTS"] = False
+        tc.variables["CASS_BUILD_SHARED"] = self.options.shared
+        tc.variables["CASS_BUILD_STATIC"] = not self.options.shared
+        tc.variables["CASS_BUILD_TESTS"] = False
+        tc.variables["CASS_BUILD_UNIT_TESTS"] = False
+        tc.variables["CASS_DEBUG_CUSTOM_ALLOC"] = False
+        tc.variables["CASS_INSTALL_HEADER_IN_SUBDIR"] = self.options.install_header_in_subdir
+        tc.variables["CASS_INSTALL_PKG_CONFIG"] = False
+
+        if self.options.use_atomic == "boost":
+            tc.variables["CASS_USE_BOOST_ATOMIC"] = True
+            tc.variables["CASS_USE_STD_ATOMIC"] = False
+
+        elif self.options.use_atomic == "std":
+            tc.variables["CASS_USE_BOOST_ATOMIC"] = False
+            tc.variables["CASS_USE_STD_ATOMIC"] = True
+        else:
+            tc.variables["CASS_USE_BOOST_ATOMIC"] = False
+            tc.variables["CASS_USE_STD_ATOMIC"] = False
+
+        tc.variables["CASS_USE_OPENSSL"] = self.options.with_openssl
+        tc.variables["CASS_USE_STATIC_LIBS"] = False
+        tc.variables["CASS_USE_ZLIB"] = self.options.with_zlib
+        tc.variables["CASS_USE_LIBSSH2"] = False
+
+        # FIXME: To use kerberos, its conan package is needed. Uncomment this when kerberos conan package is ready.
+        # tc.variables["CASS_USE_KERBEROS"] = self.options.with_kerberos
+
+        if self.settings.os == "Linux":
+            tc.variables["CASS_USE_TIMERFD"] = self.options.use_timerfd
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def _patch_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+        apply_conandata_patches(self)
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
                               "\"${CMAKE_CXX_COMPILER_ID}\" STREQUAL \"Clang\"",
                               "\"${CMAKE_CXX_COMPILER_ID}\" STREQUAL \"Clang\" OR \"${CMAKE_CXX_COMPILER_ID}\" STREQUAL \"AppleClang\"")
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-
-        self._cmake = CMake(self)
-        self._cmake.definitions["VERSION"] = self.version
-        self._cmake.definitions["CASS_BUILD_EXAMPLES"] = False
-        self._cmake.definitions["CASS_BUILD_INTEGRATION_TESTS"] = False
-        self._cmake.definitions["CASS_BUILD_SHARED"] = self.options.shared
-        self._cmake.definitions["CASS_BUILD_STATIC"] = not self.options.shared
-        self._cmake.definitions["CASS_BUILD_TESTS"] = False
-        self._cmake.definitions["CASS_BUILD_UNIT_TESTS"] = False
-        self._cmake.definitions["CASS_DEBUG_CUSTOM_ALLOC"] = False
-        self._cmake.definitions["CASS_INSTALL_HEADER_IN_SUBDIR"] = self.options.install_header_in_subdir
-        self._cmake.definitions["CASS_INSTALL_PKG_CONFIG"] = False
-
-        if self.options.use_atomic == "boost":
-            self._cmake.definitions["CASS_USE_BOOST_ATOMIC"] = True
-            self._cmake.definitions["CASS_USE_STD_ATOMIC"] = False
-
-        elif self.options.use_atomic == "std":
-            self._cmake.definitions["CASS_USE_BOOST_ATOMIC"] = False
-            self._cmake.definitions["CASS_USE_STD_ATOMIC"] = True
-        else:
-            self._cmake.definitions["CASS_USE_BOOST_ATOMIC"] = False
-            self._cmake.definitions["CASS_USE_STD_ATOMIC"] = False
-
-        self._cmake.definitions["CASS_USE_OPENSSL"] = self.options.with_openssl
-        self._cmake.definitions["CASS_USE_STATIC_LIBS"] = False
-        self._cmake.definitions["CASS_USE_ZLIB"] = self.options.with_zlib
-        self._cmake.definitions["CASS_USE_LIBSSH2"] = False
-
-        # FIXME: To use kerberos, its conan package is needed. Uncomment this when kerberos conan package is ready.
-        # self._cmake.definitions["CASS_USE_KERBEROS"] = self.options.with_kerberos
-
-        if self.settings.os == "Linux":
-            self._cmake.definitions["CASS_USE_TIMERFD"] = self.options.use_timerfd
-
-        self._cmake.configure()
-        return self._cmake
-
     def build(self):
         self._patch_sources()
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE.txt", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["cassandra"]
 
         if self.settings.os == "Windows":
             self.cpp_info.system_libs.extend(["iphlpapi", "psapi", "wsock32",
