c713fd25c875ca050d8476a0807e138c50f5f7f4
toge <toge.mail@gmail.com>
2022-12-03 19:17:55 +0900

    fix compiler version check

--- recipes/zpp_bits/all/conanfile.py
+++ recipes/zpp_bits/all/conanfile.py
@@ -4,7 +4,6 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.files import get, copy
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc, check_min_vs
-from conan.tools.scm import Version
 import os
 
 
@@ -29,8 +28,8 @@ class ZppBitsConan(ConanFile):
     def _compilers_minimum_version(self):
         return {
             "gcc": "11",
-            "clang": "11",
-            "apple-clang": "13",
+            "clang": "12",
+            "apple-clang": "13.1",
         }
 
     def layout(self):
@@ -44,10 +43,16 @@ class ZppBitsConan(ConanFile):
             check_min_cppstd(self, self._min_cppstd)
         check_min_vs(self, 192)
         if not is_msvc(self):
+            def loose_lt_semver(v1, v2):
+                lv1 = [int(v) for v in v1.split(".")]
+                lv2 = [int(v) for v in v2.split(".")]
+                min_length = min(len(lv1), len(lv2))
+                return lv1[:min_length] < lv2[:min_length]
+
             minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-            if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
                 raise ConanInvalidConfiguration(
-                    f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+                    f"{self.name} {self.version} requires C++{self._minimum_cpp_standard}, which your compiler does not support.",
                 )
 
     def source(self):
