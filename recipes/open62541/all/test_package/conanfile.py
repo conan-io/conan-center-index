06fc6bc12ffcf01f1497cb08191c2ad70dc36782
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-12-07 18:05:15 +0100

    use v2 conf_info value for tools_dir

--- recipes/open62541/all/test_package/conanfile.py
+++ recipes/open62541/all/test_package/conanfile.py
@@ -20,7 +20,8 @@ class TestPackageConan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["open62541_NODESET_DIR"] = self.deps_user_info["ua-nodeset"].nodeset_dir
-        tc.variables["open62541_TOOLS_DIR"] = self.deps_user_info["open62541"].tools_dir
+        tc.variables["open62541_TOOLS_DIR"] = self.dependencies["open62541"].conf_info.get(
+            "user.myconf:tools_dir")
         tc.generate()
 
     def build(self):
