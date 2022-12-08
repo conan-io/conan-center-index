0b4887c864f6bcb85438bcaadb2f1bcd62904ac6
toge <toge.mail@gmail.com>
2022-12-08 23:57:41 +0900

    remove rm

--- recipes/libavif/all/conanfile.py
+++ recipes/libavif/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rmdir, save, rm
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rmdir, save
 import os
 import textwrap
 
