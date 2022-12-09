ba89487b95537fa645ba4dfb500c93932d9801d5
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-12-09 15:17:10 +0100

    add cmake to tool_requires

--- recipes/wavelet_buffer/all/conanfile.py
+++ recipes/wavelet_buffer/all/conanfile.py
@@ -81,6 +81,21 @@ class WaveletBufferConan(ConanFile):
         if is_msvc(self) and self.options.shared:
             raise ConanInvalidConfiguration(f"{self.ref} can not be built as shared with Visual Studio.")
 
+    def _cmake_new_enough(self, required_version):
+        try:
+            import re
+            from io import StringIO
+            output = StringIO()
+            self.run("cmake --version", output=output)
+            m = re.search(r'cmake version (\d+\.\d+\.\d+)', output.getvalue())
+            return Version(m.group(1)) >= required_version
+        except:
+            return False
+
+    def build_requirements(self):
+        if not self._cmake_new_enough("3.16"):
+            self.tool_requires("cmake/3.25.0")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version],
             destination=self.source_folder, strip_root=True)
