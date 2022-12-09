2328a83a5c57c84c69e080eca63f9665578df116
Igor Mironchik <igor.mironchik@gmail.com>
2022-12-09 17:37:30 +0300

    cfgfile: add Conan v2 support, add cfgfile library version 0.2.11.

--- recipes/cfgfile/all/conanfile.py
+++ recipes/cfgfile/all/conanfile.py
@@ -1,7 +1,13 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 import os
-import textwrap
+
+required_conan_version = ">=1.50.0"
 
 
 class CfgfileConan(ConanFile):
@@ -10,56 +16,37 @@ class CfgfileConan(ConanFile):
     homepage = "https://github.com/igormironchik/cfgfile.git"
     license = "MIT"
     description = "Header-only library for reading/saving configuration files with schema defined in sources."
-    exports_sources = "CMakeLists.txt"
-    generators = "cmake", "cmake_find_package"
     topics = ("cfgfile", "configuration", "file")
     settings = "os", "arch", "compiler", "build_type"
 
-    _cmake = None
-
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
+    def _min_cppstd(self):
+        return "14"
 
     @property
     def _compilers_minimum_version(self):
         return {
             "Visual Studio": "15",
+            "msvc": "191",
             "gcc": "5",
             "clang": "3.5",
-            "apple-clang": "10"
+            "apple-clang": "10",
         }
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["BUILD_EXAMPLES"] = False
-        self._cmake.definitions["BUILD_TESTS"] = False
-        if tools.Version(self.version) >= "0.2.9":
-            self._cmake.definitions["USE_INTERNAL_ARGS_PARSER"] = False
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+    def package_id(self):
+        del self.info.settings.compiler
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "14")
-
-        compiler = str(self.settings.compiler)
-        if compiler not in self._compilers_minimum_version:
-            self.output.warn("Unknown compiler, assuming it supports at least C++14")
-            return
-
-        version = tools.Version(self.settings.compiler.version)
-        if version < self._compilers_minimum_version[compiler]:
-            raise ConanInvalidConfiguration("cfgfile requires a compiler that supports at least C++14")
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.name} {self.version} requires C++{self._min_cppstd}, which your compiler does not support.",
+            )
 
     def requirements(self):
-        if tools.Version(self.version) >= "0.2.10":
+        if Version(self.version) >= "0.2.10":
             self.requires("args-parser/6.2.0.1")
         elif self.version == "0.2.9.1":
             self.requires("args-parser/6.2.0.1")
@@ -67,25 +54,37 @@ class CfgfileConan(ConanFile):
             self.requires("args-parser/6.0.1.0")
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=self.source_folder)
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_EXAMPLES"] = False
+        tc.variables["BUILD_TESTS"] = False
+        if Version(self.version) >= "0.2.9":
+            tc.variables["USE_INTERNAL_ARGS_PARSER"] = False
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def package(self):
-        self.copy("COPYING", src=self._source_subfolder, dst="licenses")
-        self.copy("*.hpp", src=os.path.join(self._source_subfolder, "cfgfile"), dst=os.path.join("include", "cfgfile"))
-        cmake = self._configure_cmake()
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.hpp", src=os.path.join(self.source_folder, "cfgfile"), dst=os.path.join(self.package_folder, "include", "cfgfile"))
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "cfgfile")
+        self.cpp_info.set_property("cmake_target_name", "cfgfile::cfgfile")
+        self.cpp_info.includedirs.append(os.path.join("include", "cfgfile"))
         bin_path = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH env var with : {}".format(bin_path))
         self.env_info.PATH.append(bin_path)
-        self.cpp_info.names["cmake_find_package"] = "cfgfile"
-        self.cpp_info.names["cmake_find_package_multi"] = "cfgfile"
-        self.cpp_info.includedirs.append(os.path.join("include", "cfgfile"))
-
-    def package_id(self):
-        del self.info.settings.compiler
