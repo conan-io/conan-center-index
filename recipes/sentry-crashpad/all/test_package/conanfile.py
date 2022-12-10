cd0d6f377098d90289cff1dc32b40e239f61b390
Martin Delille <martin@delille.org>
2022-12-10 12:08:26 +0100

    deps_cpp_info => dependencies

--- recipes/sentry-crashpad/all/test_package/conanfile.py
+++ recipes/sentry-crashpad/all/test_package/conanfile.py
@@ -27,5 +27,5 @@ class TestPackageConan(ConanFile):
             mkdir(self, test_env_dir)
             bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
             handler_exe = "crashpad_handler.exe" if self.settings.os == "Windows" else "crashpad_handler"
-            handler_bin_path = os.path.join(self.deps_cpp_info["sentry-crashpad"].bin_paths[0], handler_exe)
+            handler_bin_path = os.path.join(self.dependencies["sentry-crashpad"].bin_paths[0], handler_exe)
             self.run(f"{bin_path} {test_env_dir} {handler_bin_path}", run_environment=True)
