3faccb31ee9e4f3d35b24cc36faaaf4d6e9b5435
System-Arch <33330183+System-Arch@users.noreply.github.com>
2022-12-11 16:50:32 -0500

    Need to import VirtualBuildEnv when restoring its use

--- recipes/nasm/all/conanfile.py
+++ recipes/nasm/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, chdir, copy, export_conandata_patches, get, replace_in_file, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
