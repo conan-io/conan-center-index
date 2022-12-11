808c7244c032dfdeac97ab8fb863582c26750acf
System-Arch <33330183+System-Arch@users.noreply.github.com>
2022-12-11 15:54:46 -0500

    MPFR Conan 2.0 Compatibility

--- recipes/mpfr/all/conanfile.py
+++ recipes/mpfr/all/conanfile.py
@@ -1,15 +1,21 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, CMake, tools
-from conans.errors import ConanException
+from conan import ConanFile
+from conan.tools.files import save, load, chdir, patch, copy, rmdir, rm, replace_in_file, apply_conandata_patches
+from conan.tools.layout import basic_layout
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps
+from conan.tools.microsoft import unix_path, is_msvc
+from conan.errors import ConanException
 import contextlib
 import os
 import re
 import shlex
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.53.0"
 
 
 class MpfrConan(ConanFile):
     name = "mpfr"
+    package_type = "library"
     description = "The MPFR library is a C library for multiple-precision floating-point computations with " \
                   "correct rounding"
     topics = ("mpfr", "multiprecision", "math", "mathematics")
@@ -29,14 +35,9 @@ class MpfrConan(ConanFile):
     }
 
     exports_sources = "CMakeLists.txt.in", "patches/**"
-    generators = "cmake"
 
     _autotools = None
     _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
     def _settings_build(self):
@@ -44,133 +45,130 @@ class MpfrConan(ConanFile):
 
     def config_options(self):
         if self.settings.os == "Windows":
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
 
     def requirements(self):
         if self.options.exact_int == "gmp":
-            self.requires("gmp/6.2.1")
+            self.requires("gmp/6.2.1", transitive_headers=True)
         elif self.options.exact_int == "mpir":
             self.requires("mpir/3.0.0")
 
     def build_requirements(self):
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not os.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/cci.latest")
 
+    def layout(self):
+        if self.settings.os == "Windows":
+            cmake_layout(self, src_folder="src")
+        else:
+            basic_layout(self, src_folder="src")
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(**self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        yes_no = lambda v: "yes" if v else "no"
-        args = [
-            "--enable-thread-safe",
-            "--with-gmp-include={}".format(tools.unix_path(os.path.join(self.deps_cpp_info[str(self.options.exact_int)].rootpath, "include"))),
-            "--with-gmp-lib={}".format(tools.unix_path(os.path.join(self.deps_cpp_info[str(self.options.exact_int)].rootpath, "lib"))),
-            "--enable-shared={}".format(yes_no(self.options.shared)),
-            "--enable-static={}".format(yes_no(not self.options.shared)),
-        ]
-        if self.settings.compiler == "clang":
-            # warning: optimization flag '-ffloat-store' is not supported
-            args.append("mpfr_cv_gcc_floatconv_bug=no")
-            if self.settings.arch == "x86":
-                # fatal error: error in backend: Unsupported library call operation!
-                args.append("--disable-float128")
-        if self.options.exact_int == "mpir":
-            self._autotools.include_paths.append(self.build_folder)
-        if self.settings.compiler == "Visual Studio":
-            self._autotools.flags.append("-FS")
-        self._autotools.libs = []
-        self._autotools.configure(args=args, configure_dir=self._source_subfolder)
-        return self._autotools
+    def generate(self):
+        if self.settings.os == "Windows":
+            tc = CMakeToolchain(self)
+            tc.generate()
+        else:
+            tc = AutotoolsToolchain(self)
+            yes_no = lambda v: "yes" if v else "no"
+            tc.configure_args.append("--enable-thread-safe")
+            tc.configure_args.append(f"--with-gmp={unix_path(self, self.dependencies[str(self.options.exact_int)].package_folder)}")
+            tc.configure_args.append(f"--enable-shared={yes_no(self.options.shared)}")
+            tc.configure_args.append(f"--enable-static={yes_no(not self.options.shared)}")
+            if self.settings.compiler == "clang":
+                # warning: optimization flag '-ffloat-store' is not supported
+                tc.configure_args.append("mpfr_cv_gcc_floatconv_bug=no")
+                if self.settings.arch == "x86":
+                    # fatal error: error in backend: Unsupported library call operation!
+                    tc.configure_args.append("--disable-float128")
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.configure(source_dir=os.path.join(self._source_subfolder, "src"))
-        return self._cmake
+            if self.options.exact_int == "mpir":
+                tc.extra_cflags.append(f"-I{self.build_folder}")
+            if self.settings.compiler == "msvc":
+                tc.extra_cflags.append("-FS")
+
+            env = tc.environment()
+            if self.settings.compiler == "msvc":
+                env.define("AR", "lib")
+                env.define("CC", "cl -nologo")
+                env.define("CXX", "cl -nologo")
+                env.define("LD", "link")
+                env.define("NM", "dumpbin -symbols")
+                env.define("OBJDUMP", ":")
+                env.define("RANLIB", ":")
+                env.define("STRIP", ":")
+            tc.generate(env) # Create conanbuild.conf
+            tc = AutotoolsDeps(self)
+            tc.generate()
 
     def _extract_makefile_variable(self, makefile, variable):
-        makefile_contents = tools.load(makefile)
+        makefile_contents = load(self, makefile)
         match = re.search("{}[ \t]*=[ \t]*((?:(?:[a-zA-Z0-9 \t.=/_-])|(?:\\\\\"))*(?:\\\\\n(?:(?:[a-zA-Z0-9 \t.=/_-])|(?:\\\"))*)*)\n".format(variable), makefile_contents)
         if not match:
-            raise ConanException("Cannot extract variable {} from {}".format(variable, makefile_contents))
+            raise ConanException(f"Cannot extract variable {variable} from {makefile_contents}")
         lines = [line.strip(" \t\\") for line in match.group(1).split()]
         return [item for line in lines for item in shlex.split(line) if item]
 
     def _extract_mpfr_autotools_variables(self):
-        makefile_am = os.path.join(self._source_subfolder, "src", "Makefile.am")
+        makefile_am = os.path.join(self.source_folder, "src", "Makefile.am")
         makefile = os.path.join("src", "Makefile")
         sources = self._extract_makefile_variable(makefile_am, "libmpfr_la_SOURCES")
         headers = self._extract_makefile_variable(makefile_am, "include_HEADERS")
         defs = self._extract_makefile_variable(makefile, "DEFS")
         return sources, headers, defs
 
-    @contextlib.contextmanager
-    def _build_context(self):
-        if self.settings.compiler == "Visual Studio":
-            with tools.vcvars(self):
-                env = {
-                    "AR": "lib",
-                    "CC": "cl -nologo",
-                    "CXX": "cl -nologo",
-                    "LD": "link",
-                    "NM": "dumpbin -symbols",
-                    "OBJDUMP": ":",
-                    "RANLIB": ":",
-                    "STRIP": ":",
-                }
-                with tools.environment_append(env):
-                    yield
-        else:
-            yield
-
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        with chdir(self, self.source_folder):
+            command = "./autogen.sh"
+            if os.path.exists(command):
+                self.run(command)
+        apply_conandata_patches(self)
         if self.options.exact_int == "mpir":
-            tools.replace_in_file(os.path.join(self._source_subfolder, "configure"),
+            replace_in_file(self, os.path.join(self.source_folder, "configure"),
                                        "-lgmp", "-lmpir")
-            tools.replace_in_file(os.path.join(self._source_subfolder, "src", "mpfr.h"),
+            replace_in_file(os.path.join(self.source_folder, "src", "mpfr.h"),
                                        "<gmp.h>", "<mpir.h>")
-            tools.save("gmp.h", "#pragma once\n#include <mpir.h>\n")
-        with self._build_context():
-            autotools = self._configure_autotools()
+            save(self, "gmp.h", "#pragma once\n#include <mpir.h>\n")
+
         if self.settings.os == "Windows":
-            cmakelists_in = tools.load("CMakeLists.txt.in")
+            cmakelists_in = load(self, "CMakeLists.txt.in")
             sources, headers, definitions = self._extract_mpfr_autotools_variables()
-            tools.save(os.path.join(self._source_subfolder, "src", "CMakeLists.txt"), cmakelists_in.format(
+            save(self, os.path.join(self.source_folder, "src", "CMakeLists.txt"), cmakelists_in.format(
                 mpfr_sources=" ".join(sources),
                 mpfr_headers=" ".join(headers),
                 definitions=" ".join(definitions),
             ))
-            cmake = self._configure_cmake()
+            cmake = Cmake(self)
             cmake.build()
         else:
+            autotools = Autotools(self)
+            autotools.configure()
             autotools.make(args=["V=0"])
 
     def package(self):
-        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
+        copy(self, "COPYING", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         if self.settings.os == "Windows":
-            cmake = self._configure_cmake()
+            cmake = CMake(self)
             cmake.install()
         else:
-            autotools = self._configure_autotools()
+            autotools = Autotools(self)
             autotools.install()
             os.unlink(os.path.join(self.package_folder, "lib", "libmpfr.la"))
-            tools.rmdir(os.path.join(self.package_folder, "share"))
-            tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+            rmdir(self, os.path.join(self.package_folder, "share"))
+            rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "MPFR")
+        self.cpp_info.set_property("cmake_target_name", "MPFR::MPFR")
         self.cpp_info.libs = ["mpfr"]
         if self.settings.os == "Windows" and self.options.shared:
             self.cpp_info.defines = ["MPFR_DLL"]
