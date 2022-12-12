0bfa66c32f55a0cf95d21dd36302bba38fa1f4af
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-12-12 23:21:17 +0100

    conan v2 support

--- recipes/bitserializer/all/test_package/conanfile.py
+++ recipes/bitserializer/all/test_package/conanfile.py
@@ -1,20 +1,33 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["WITH_CPPRESTSDK"] = self.dependencies["bitserializer"].options.with_cpprestsdk
+        tc.variables["WITH_RAPIDJSON"] = self.dependencies["bitserializer"].options.with_rapidjson
+        tc.variables["WITH_PUGIXML"] = self.dependencies["bitserializer"].options.with_pugixml
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["WITH_CPPRESTSDK"] = self.options["bitserializer"].with_cpprestsdk
-        cmake.definitions["WITH_RAPIDJSON"] = self.options["bitserializer"].with_rapidjson
-        cmake.definitions["WITH_PUGIXML"] = self.options["bitserializer"].with_pugixml
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
