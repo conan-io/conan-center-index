5752a03896836384f33c0a07259e4e08a9cefa5f
Jordan Williams <jordan@jwillikers.com>
2022-12-12 16:53:58 -0600

    jom: Download binaries in build method and fix test packages
    Use conan.tools.files.copy.
    Update license to be GPL-3.0-only.

---
+++ recipes/jom/all/test_v1_package/conanfile.py
@@ -0,0 +1,10 @@
+from conans import ConanFile
+from conan.tools.build import can_run
+
+
+class TestPackageV1Conan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+
+    def test(self):
+        if can_run(self):
+            self.run("jom /VERSION", run_environment=True)
