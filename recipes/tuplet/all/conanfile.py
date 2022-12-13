e05ac437ed826d0f5399c65dd42e6224e8827926
toge <toge.mail@gmail.com>
2022-12-13 17:56:40 +0900

    remove microsoft module
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/tuplet/all/conanfile.py
+++ recipes/tuplet/all/conanfile.py
@@ -3,7 +3,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import check_min_vs
 import os
 
 required_conan_version = ">=1.50.0"
