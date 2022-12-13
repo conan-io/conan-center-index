638dbfd191601764ad1792cc9f0fb2cb2f5fa04a
toge <toge.mail@gmail.com>
2022-12-13 14:36:34 +0900

    wavelet_buffer: add version 0.5.0

--- recipes/wavelet_buffer/all/conanfile.py
+++ recipes/wavelet_buffer/all/conanfile.py
@@ -145,6 +145,21 @@ class WaveletBufferConan(ConanFile):
                 f"{self.ref} requires the option 'cimg:enable_opencv=False'"
             )
 
+    def _cmake_new_enough(self, required_version):
+        try:
+            import re
+            from io import StringIO
+            output = StringIO()
+            self.run("cmake --version", output=output)
+            m = re.search(r'cmake version (\d+\.\d+\.\d+)', output.getvalue())
+            return Version(m.group(1)) >= required_version
+        except:
+            return False
+
+    def build_requirements(self):
+        if not self._cmake_new_enough("3.16"):
+            self.tool_requires("cmake/3.25.0")
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
