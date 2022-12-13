2fa1e90dacb530fbd7c11633b3b38a8a041b878a
toge <toge.mail@gmail.com>
2022-12-13 10:24:25 +0900

    revert validate logic

--- recipes/glaze/all/conanfile.py
+++ recipes/glaze/all/conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.files import get, copy
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import check_min_vs, is_msvc
+from conan.tools.microsoft import check_min_vs
 import os
 
 required_conan_version = ">=1.51.1"
@@ -50,18 +50,17 @@ class GlazeConan(ConanFile):
             check_min_cppstd(self, self._minimum_cpp_standard)
         check_min_vs(self, 192)
 
-        if not is_msvc(self):
-            def loose_lt_semver(v1, v2):
-                lv1 = [int(v) for v in v1.split(".")]
-                lv2 = [int(v) for v in v2.split(".")]
-                min_length = min(len(lv1), len(lv2))
-                return lv1[:min_length] < lv2[:min_length]
+        def loose_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
 
-            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-            if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
-                raise ConanInvalidConfiguration(
-                    f"{self.name} {self.version} requires C++{self._minimum_cpp_standard}, which your compiler does not support.",
-                )
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration(
+                f"{self.name} {self.version} requires C++{self._minimum_cpp_standard}, which your compiler does not support.",
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
