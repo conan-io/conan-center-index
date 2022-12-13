b002f294039c2902a0f4e354b52ec713114d468d
Paul Harris <harris.pc@gmail.com>
2022-12-13 11:36:40 +0800

    Reverse 1.54 upgrade, CI is still on 1.53

--- recipes/proj/all/conanfile.py
+++ recipes/proj/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.apple import is_apple_os
-from conan.tools.build import stdcpp_library
+from conans.tools import stdcpp_library
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir, replace_in_file, collect_libs, rm, rename
@@ -9,7 +9,7 @@ from conan.tools.scm import Version
 import os
 
 
-required_conan_version = ">=1.54.0"
+required_conan_version = ">=1.53.0"
 
 
 class ProjConan(ConanFile):
