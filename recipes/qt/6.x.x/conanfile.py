3d7e852909142f5544efb74f129b806a584ae521
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-12-14 10:36:39 +0100

    re-fixup

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -252,7 +252,7 @@ class QtConan(ConanFile):
         if os.getenv('NOT_ON_C3I', '0') == '0':
             if self.info.settings.compiler == "gcc" and Version(self.info.settings.compiler.version) >= "11" or \
                 self.info.settings.compiler == "clang" and Version(self.info.settings.compiler.version) >= "12":
-                raise ConanInvalidConfiguration("qt is not supported on gcc11 and clang >= 12 on C3I until conan-io/conan-center-index#13472 is fixed\n"/
+                raise ConanInvalidConfiguration("qt is not supported on gcc11 and clang >= 12 on C3I until conan-io/conan-center-index#13472 is fixed\n"\
                                                 "If your distro is modern enough (xcb >= 1.12), set environment variable NOT_ON_C3I=1")
 
         # C++ minimum standard required
