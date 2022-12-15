97daa99ef846fac6cbfd783aa589a9a935153856
System-Arch <33330183+System-Arch@users.noreply.github.com>
2022-12-15 18:02:50 -0500

    MPC Conan 2.0 Compatibility

--- recipes/mpc/all/conanfile.py
+++ recipes/mpc/all/conanfile.py
@@ -1,11 +1,17 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.files import save, load, chdir, copy, rmdir, rm, apply_conandata_patches
+from conan.tools.layout import basic_layout
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps
+from conan.tools.microsoft import unix_path, VCVars, is_msvc
+from conan.tools.apple import is_apple_os
+from conan.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.53.0"
 
 class MpcConan(ConanFile):
     name = "mpc"
+    package_type = "library"
     description = "GNU MPC is a C library for the arithmetic of complex numbers with arbitrarily high precision " \
                   "and correct rounding of the result"
     topics = ("conan", "mpc", "multiprecision", "math", "mathematics")
@@ -16,65 +22,74 @@ class MpcConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     exports_sources = "patches/**"
-    _autotools = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     def config_options(self):
         if self.settings.os == 'Windows':
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
 
     def requirements(self):
-        self.requires("mpfr/4.1.0")
+        self.requires("gmp/6.2.1", transitive_headers=True)
+        self.requires("mpfr/4.1.0", transitive_headers=True)
 
     def validate(self):
-        # FIXME: add Visual Studio support, upstream has a makefile.vc
-        if self.settings.compiler == "Visual Studio":
-            raise ConanInvalidConfiguration("mpc can be built with Visual Studio, but it's not supported yet in this recipe.")
+        # FIXME: add msvc support, upstream has a makefile.vc
+        if self.info.settings.compiler == "msvc":
+            raise ConanInvalidConfiguration("mpc can be built with msvc, but it's not supported yet in this recipe.")
 
     @property
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
     def build_requirements(self):
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not os.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/cci.latest")
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        args = []
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        self.win_bash = self._settings_build.os == "Windows"
+        tc = AutotoolsToolchain(self)
+        tc.configure_args.append(f'--with-gmp={unix_path(self, self.dependencies["gmp"].package_folder)}')
+        tc.configure_args.append(f'--with-mpfr={unix_path(self, self.dependencies["mpfr"].package_folder)}')
         if self.options.shared:
-            args.extend(["--disable-static", "--enable-shared"])
+            tc.configure_args.extend(["--disable-static", "--enable-shared"])
         else:
-            args.extend(["--disable-shared", "--enable-static"])
-        self._autotools.configure(args=args, configure_dir=self._source_subfolder)
-        return self._autotools
+            tc.configure_args.extend(["--disable-shared", "--enable-static"])
+        tc.generate() # Create conanbuild.conf
+        tc = AutotoolsDeps(self)
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        autotools = self._configure_autotools()
+        apply_conandata_patches(self)
+        with chdir(self, self.source_folder):
+            if not os.path.exists("configure"):
+                command = "autoreconf -i"
+                self.run(command)
+        autotools = Autotools(self)
+        autotools.configure()
         autotools.make()
 
     def package(self):
-        self.copy(pattern="COPYING.LESSER", dst="licenses", src=self._source_subfolder)
-        autotools = self._configure_autotools()
+        copy(self, pattern="COPYING.LESSER", dst="licenses", src=self.source_folder)
+        autotools = Autotools(self)
         autotools.install()
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"), recursive=True)
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "MPC")
+        self.cpp_info.set_property("cmake_module_file_name", "mpc")
+        self.cpp_info.set_property("cmake_target_name", "MPC::MPC")
         self.cpp_info.libs = ["mpc"]
