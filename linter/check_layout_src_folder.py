3737fe71a1e5e4f16773b5b7ff036abd1719b5b3
Christopher McArthur <christopherm@jfrog.com>
2022-12-19 14:31:59 -0800

    make the error message more general for different layouts

--- linter/check_layout_src_folder.py
+++ linter/check_layout_src_folder.py
@@ -2,6 +2,10 @@ from pylint.checkers import BaseChecker
 from pylint.interfaces import IAstroidChecker
 from astroid import nodes
 
+WHY_SRC_FOLDER = "Setting the `src_folder` for layouts will help keep an organized and clean workspace when developing recipes locally. " \
+                 "The extra folder will help ensure there are no collisions between the upstream sources and recipe's exports - which " \
+                 "also extends to what happens in the cache when creating packages"
+
 
 class LayoutSrcFolder(BaseChecker):
     """
@@ -13,10 +17,15 @@ class LayoutSrcFolder(BaseChecker):
     name = "conan-layout-src-folder"
     msgs = {
         "E9012": (
-            "cmake_layout should set `src_folder` to src",
-            "conan-cmake-layout-src-folder",
-            "Setting the `src_folder` for layouts will help keep an organized and clean workspace when developing recipes locally. The extra folder will help ensure there are no collisions between the upstream sources and recipe's exports - which also extends to what happens in the cache when creating packages",
-        )
+            "layout is missing `src_folder` argument which should be to `src`",
+            "conan-missing-layout-src-folder",
+            WHY_SRC_FOLDER,
+        ),
+        "E9013": (
+            "layout should set `src_folder` to `src`",
+            "conan-layout-src-folder-is-src",
+            WHY_SRC_FOLDER,
+        ),
     }
 
     def visit_call(self, node: nodes.Call) -> None:
@@ -25,9 +34,11 @@ class LayoutSrcFolder(BaseChecker):
 
         if node.func.name in ["cmake_layout", "vs_layout", "basic_layout"]:
             for kw in node.keywords:
-                if kw.arg == 'src_folder':
-                    if not kw.value or kw.value.as_string().strip('"\'') != 'src':
-                        self.add_message("conan-cmake-layout-src-folder", node=node, line=node.lineno)
+                if kw.arg == "src_folder":
+                    if not kw.value or kw.value.as_string().strip("\"'") != "src":
+                        self.add_message(
+                            "conan-layout-src-folder-is-src", node=node, line=node.lineno
+                        )
                     break
             else:
-                self.add_message("conan-cmake-layout-src-folder", node=node, line=node.lineno)
+                self.add_message("conan-missing-layout-src-folder", node=node, line=node.lineno)
