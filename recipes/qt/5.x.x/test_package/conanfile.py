91f5235bef82dcf6f7973fe80d22e2b3eca635c9
Esteban Dugueperoux <43169544+EstebanDugueperoux2@users.noreply.github.com>
2022-12-20 18:29:33 +0000

    (#14820) Migrate qt5 test_package to v2

--- recipes/qt/5.x.x/test_package/conanfile.py
+++ recipes/qt/5.x.x/test_package/conanfile.py
@@ -1,8 +1,11 @@
 import os
 import shutil
 
-from conans import ConanFile, tools, Meson, RunEnvironment, CMake
-from conans.errors import ConanException
+from conan import ConanFile
+from conans import tools, Meson, RunEnvironment, CMake
+from conan.tools.build import cross_building
+from conan.errors import ConanInvalidConfiguration
+
 
 
 class TestPackageConan(ConanFile):
@@ -24,7 +27,7 @@ class TestPackageConan(ConanFile):
 
     def _meson_supported(self):
         return self.options["qt"].shared and\
-            not tools.cross_building(self) and\
+            not cross_building(self) and\
             not tools.os_info.is_macos and\
             not self._is_mingw()
 
@@ -77,7 +80,7 @@ class TestPackageConan(ConanFile):
                 meson = Meson(self)
                 try:
                     meson.configure(build_folder="meson_folder", defs={"cpp_std": "c++11"})
-                except ConanException:
+                except ConanInvalidConfiguration:
                     self.output.info(open("meson_folder/meson-logs/meson-log.txt", 'r').read())
                     raise
                 meson.build()
@@ -120,7 +123,7 @@ class TestPackageConan(ConanFile):
         self.run(os.path.join("bin", "test_package"), run_environment=True)
 
     def test(self):
-        if not tools.cross_building(self, skip_x64_x86=True):
+        if not cross_building(self, skip_x64_x86=True):
             self._test_with_qmake()
             self._test_with_meson()
             self._test_with_cmake_find_package_multi()
