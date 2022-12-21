7088e035cf664a03e314743f5197e588ddfe4584
toge <toge.mail@gmail.com>
2022-12-21 18:27:55 +0900

    packio: add version 2.3.0, support conan v2

--- recipes/packio/all/conanfile.py
+++ recipes/packio/all/conanfile.py
@@ -1,18 +1,22 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
-from conans.tools import Version
 
+required_conan_version = ">=1.52.0"
 
 class PackioConan(ConanFile):
     name = "packio"
     license = "MPL-2.0"
+    description = "An asynchronous msgpack-RPC and JSON-RPC library built on top of Boost.Asio."
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/qchateau/packio"
-    description = "An asynchronous msgpack-RPC and JSON-RPC library built on top of Boost.Asio."
     topics = ("rpc", "msgpack", "json", "asio", "async", "cpp17", "cpp20", "coroutines")
-    settings = "compiler"
-    no_copy_source = True
+    settings = "os", "arch", "compiler", "build_type"
+
     options = {
         "standalone_asio": [True, False],
         "msgpack": [True, False],
@@ -27,8 +31,8 @@ class PackioConan(ConanFile):
     }
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _min_cppstd(self):
+        return 17
 
     @property
     def _compilers_minimum_version(self):
@@ -36,21 +40,28 @@ class PackioConan(ConanFile):
             "apple-clang": 10,
             "clang": 6,
             "gcc": 7,
+            "msvc": 192,
             "Visual Studio": 16,
         }
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def config_options(self):
-        if tools.Version(self.version) < "1.2.0":
+        if Version(self.version) < "1.2.0":
             del self.options.standalone_asio
-        if tools.Version(self.version) < "2.0.0":
+        if Version(self.version) < "2.0.0":
             del self.options.msgpack
             del self.options.nlohmann_json
-        if tools.Version(self.version) < "2.1.0":
+        if Version(self.version) < "2.1.0":
             del self.options.boost_json
 
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
     def requirements(self):
-        if self.options.get_safe("msgpack") or tools.Version(self.version) < "2.0.0":
-            self.requires("msgpack/3.2.1")
+        if self.options.get_safe("msgpack") or Version(self.version) < "2.0.0":
+            self.requires("msgpack/3.3.0")
 
         if self.options.get_safe("nlohmann_json"):
             self.requires("nlohmann_json/3.9.1")
@@ -60,36 +71,40 @@ class PackioConan(ConanFile):
             self.options.boost_json = not self.options.standalone_asio
 
         if self.options.get_safe("boost_json") or not self.options.get_safe("standalone_asio"):
-            self.requires("boost/1.75.0")
+            self.requires("boost/1.80.0")
 
         if self.options.get_safe("standalone_asio"):
-            self.requires("asio/1.18.1")
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "packio-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
-
-    def configure(self):
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, "17")
-
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version:
-            if tools.Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration("packio requires C++17, which your compiler does not support.")
-        else:
-            self.output.warn("packio requires C++17. Your compiler is unknown. Assuming it supports C++17.")
-
-    def package(self):
-        self.copy("LICENSE.md", dst="licenses", src=self._source_subfolder)
-        self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
+            self.requires("asio/1.24.0")
 
     def package_id(self):
         self.info.header_only()
 
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version:
+            if Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
+        else:
+            self.output.warn(f"{self.ref} requires C++{self._min_cppstd}. Your compiler is unknown. Assuming it supports C++{self._min_cppstd}.")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def build(self):
+        apply_conandata_patches(self)
+
+    def package(self):
+        copy(self, pattern="LICENSE.md", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, "*.h", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "include"))
+
     def package_info(self):
-        if tools.Version(self.version) < "2.1.0":
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
+        if Version(self.version) < "2.1.0":
             if self.options.get_safe("standalone_asio"):
                 self.cpp_info.defines.append("PACKIO_STANDALONE_ASIO")
         else:
--- recipes/packio/all/test_package/conanfile.py
+++ recipes/packio/all/test_package/conanfile.py
@@ -1,18 +1,26 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 import os
-
-from conans import ConanFile, CMake, tools
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
-        packio_version = self.deps_cpp_info["packio"].version
         cmake = CMake(self)
-        cmake.configure(defs={"PACKIO_VERSION": packio_version})
+        cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            self.run(os.path.join("bin", "main"), run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
