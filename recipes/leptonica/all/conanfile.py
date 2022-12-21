680c86ee8ffa4da184a8ae60373547a125b58503
theirix <theirix@gmail.com>
2022-12-21 11:28:25 +0300

    Modernize with rm_safe

--- recipes/leptonica/all/conanfile.py
+++ recipes/leptonica/all/conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.scm import Version
 import os
 import textwrap
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class LeptonicaConan(ConanFile):
@@ -55,18 +55,9 @@ class LeptonicaConan(ConanFile):
         if self.options.with_tiff:
             self.options["libtiff"].jpeg = self.options.with_jpeg
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
-        try:
-            del self.settings.compiler.libcxx
-        except Exception:
-            pass
-        try:
-            del self.settings.compiler.cppstd
-        except Exception:
-            pass
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
