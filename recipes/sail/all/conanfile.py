54d618df463a2a25ea2af8f9b71615db1b3b4b17
Dmitry Baryshev <dima8w@gmail.com>
2022-12-22 07:21:51 -0800

    Update recipes/sail/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/sail/all/conanfile.py
+++ recipes/sail/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, get, rename, rmdir
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, get, rename, rmdir
 from conan.tools.microsoft import is_msvc
 import os
 
