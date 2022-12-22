8d753adc81e9238a8630bdc43adca8694de5e1b0
Marcin Zdun <mzdun@midnightbits.com>
2022-12-22 12:05:59 +0100

    revert check_min_vs removal

--- recipes/mbits-args/all/conanfile.py
+++ recipes/mbits-args/all/conanfile.py
@@ -67,6 +67,7 @@ class MBitsArgsConan(ConanFile):
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
             check_min_cppstd(self, self._min_cppstd)
+        check_min_vs(self, 192)
         if not is_msvc(self):
             minimum_version = self._compilers_minimum_version.get(
                 str(self.settings.compiler), False
