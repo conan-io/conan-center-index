9db53b00b29b704c40125fd0a815d7ed73f3c882
czoido <mrgalleta@gmail.com>
2022-12-24 11:23:33 +0100

    sdl v2

--- recipes/sdl/all/test_v1_package/conanfile.py
+++ recipes/sdl/all/test_v1_package/conanfile.py
@@ -1,20 +1,11 @@
-from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
+from conans import ConanFile, CMake
+from conan.tools.build import cross_building
 import os
 
 
-# It will become the standard on Conan 2.x
 class TestPackageConan(ConanFile):
-    settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
-    test_type = "explicit"
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def layout(self):
-        cmake_layout(self)
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -31,6 +22,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, env="conanrun")
+        if not cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
