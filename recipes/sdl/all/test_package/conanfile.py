f919739b23b45a6feb1841d791f7402fc60857ef
czoido <mrgalleta@gmail.com>
2022-12-24 11:32:46 +0100

    fix test_package

--- recipes/sdl/all/test_package/conanfile.py
+++ recipes/sdl/all/test_package/conanfile.py
@@ -1,13 +1,13 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 import os
 
 
 # It will become the standard on Conan 2.x
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
@@ -16,17 +16,22 @@ class TestPackageConan(ConanFile):
     def layout(self):
         cmake_layout(self)
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if self.settings.os == "Linux":
+            tc.variables["WITH_X11"] = self.options["sdl"].x11
+            tc.variables["WITH_ALSA"] = self.options["sdl"].alsa
+            tc.variables["WITH_PULSE"] = self.options["sdl"].pulse
+            tc.variables["WITH_ESD"] = self.options["sdl"].esd
+            tc.variables["WITH_ARTS"] = self.options["sdl"].arts
+            tc.variables["WITH_DIRECTFB"] = self.options["sdl"].directfb
+        if self.settings.os == "Windows":
+            tc.variables["WITH_DIRECTX"] = self.options["sdl"].directx
+        tc.generate()
+
+
     def build(self):
         cmake = CMake(self)
-        if self.settings.os == "Linux":
-            cmake.definitions["WITH_X11"] = self.options["sdl"].x11
-            cmake.definitions["WITH_ALSA"] = self.options["sdl"].alsa
-            cmake.definitions["WITH_PULSE"] = self.options["sdl"].pulse
-            cmake.definitions["WITH_ESD"] = self.options["sdl"].esd
-            cmake.definitions["WITH_ARTS"] = self.options["sdl"].arts
-            cmake.definitions["WITH_DIRECTFB"] = self.options["sdl"].directfb
-        if self.settings.os == "Windows":
-            cmake.definitions["WITH_DIRECTX"] = self.options["sdl"].directx
         cmake.configure()
         cmake.build()
 
