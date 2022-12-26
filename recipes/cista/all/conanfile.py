d0fc30e8b97c96b3705f66fd23ac02b3d1988764
toge <toge.mail@gmail.com>
2022-12-27 00:47:08 +0900

    cista: add version 0.11

--- recipes/cista/all/conanfile.py
+++ recipes/cista/all/conanfile.py
@@ -22,9 +22,14 @@ class CistaConan(ConanFile):
     no_copy_source = True
 
     @property
+    def _min_cppstd(self):
+        return 17
+
+    @property
     def _compilers_minimum_version(self):
         return {
             "Visual Studio": "15.7",
+            "msvc": "191",
             "gcc": "8",
             "clang": "6",
             "apple-clang": "9.1"
@@ -35,7 +40,7 @@ class CistaConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, 17)
+            check_min_cppstd(self, self._min_cppstd)
 
         def loose_lt_semver(v1, v2):
             lv1 = [int(v) for v in v1.split(".")]
@@ -46,7 +51,7 @@ class CistaConan(ConanFile):
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), None)
         if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
             raise ConanInvalidConfiguration(
-                f"{self.name} {self.version} requires C++17, which your compiler does not support.",
+                f"{self.name} {self.version} requires C++{self._min_cppstd}, which your compiler does not support.",
             )
 
     def layout(self):
@@ -64,7 +69,6 @@ class CistaConan(ConanFile):
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "cista")
         self.cpp_info.set_property("cmake_target_name", "cista::cista")
+
         self.cpp_info.bindirs = []
-        self.cpp_info.frameworkdirs = []
         self.cpp_info.libdirs = []
-        self.cpp_info.resdirs = []
