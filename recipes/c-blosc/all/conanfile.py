3e0a9ccdc5e060daf298da1d9cae755a536fd5ae
toge <toge.mail@gmail.com>
2022-12-27 20:19:52 +0900

    update conan version

--- recipes/c-blosc/all/conanfile.py
+++ recipes/c-blosc/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class CbloscConan(ConanFile):
