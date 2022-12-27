45cee960185f4b3150dfb4204d5816cf167e68e0
memsharded <james@conan.io>
2022-12-27 15:57:55 +0100

    prepare zulu-openjdk for 2.0, remove old patch version

--- recipes/zulu-openjdk/all/test_package/conanfile.py
+++ recipes/zulu-openjdk/all/test_package/conanfile.py
@@ -1,26 +1,29 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.build import cross_building
 from io import StringIO
 
 
 class TestPackage(ConanFile):
     settings = "os", "arch"
+    test_type = "explicit"
+    generators = "VirtualRunEnv"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         pass # nothing to build, but tests should not warn
 
     def test(self):
-        if tools.cross_building(self):
+        if cross_building(self):
             return
             # OK, this needs some explanation
             # You basically do not crosscompile that package, never
             # But C3I does, Macos x86_64 to M1,
             # and this is why there is some cross compilation going on
             # The test will not work in that environment, so .... don't test
-        test_cmd = ['java', '--version']
         output = StringIO()
-        self.run(test_cmd, output=output, run_environment=True)
+        self.run("java --version", output, env="conanrun")
         version_info = output.getvalue()
-        if "Zulu" in version_info:
-            pass
-        else:
+        if "Zulu" not in version_info:
             raise Exception("java call seems not use the Zulu bin")
