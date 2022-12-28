da860ca63d13865a3a93960eca3df3ef1744855c
Linus Klöckner <lkloeckner@audeering.com>
2022-12-28 09:44:16 +0100

    Fix test_package for  Conan v1

--- recipes/soxr/all/test_package/conanfile.py
+++ recipes/soxr/all/test_package/conanfile.py
@@ -2,6 +2,7 @@ import os
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
+import yaml
 
 
 class TestPackageConan(ConanFile):
@@ -9,8 +10,23 @@ class TestPackageConan(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
 
+    _tests_todo = []
+
+    @property
+    def _todos_filename(self):
+        return os.path.join(self.recipe_folder, self.folders.generators, "catch2_test_to_do.yml")
+
     def requirements(self):
         self.requires(self.tested_reference_str)
+
+    def generate(self):
+        # note: this is required as self.dependencies is not available in test()
+        self._tests_todo.append("test_package_core")
+        if self.dependencies[self.tested_reference_str].options.with_lsr_bindings:
+            self._tests_todo.append("test_package_lsr")
+
+        with open(self._todos_filename, "w", encoding="utf-8") as file:
+            yaml.dump(self._tests_todo, file)
 
     def layout(self):
         cmake_layout(self)
@@ -21,11 +37,8 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
+        with open(self._todos_filename, "r", encoding="utf-8") as file:
+            self._tests_todo = yaml.safe_load(file)
         if can_run(self):
-            # core component
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package_core")
-            self.run(bin_path, env="conanrun")
-            # lsr component
-            if self.dependencies[self.tested_reference_str].options.with_lsr_bindings:
-                bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package_lsr")
-                self.run(bin_path, env="conanrun")
+            for test_name in self._tests_todo:
+                self.run(os.path.join(self.cpp.build.bindirs[0], test_name), env="conanrun")
