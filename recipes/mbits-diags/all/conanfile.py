3029b4af686b537362fbbbfce551db81629723a6
Marcin Zdun <mzdun@midnightbits.com>
2022-12-28 14:02:17 +0100

    fix linter comments

--- recipes/mbits-diags/all/conanfile.py
+++ recipes/mbits-diags/all/conanfile.py
@@ -1,11 +1,10 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.microsoft import check_min_vs, is_msvc
-from conan.tools.files import export_conandata_patches, get, copy, rm, rmdir
+from conan.tools.files import export_conandata_patches, get, copy, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.env import VirtualBuildEnv
 import os
 
 
