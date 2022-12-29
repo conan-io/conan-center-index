2d643e3c5f72f60db54d3d4c7b9db962d1f66048
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-12-29 16:12:48 +0100

    modernize more

--- recipes/libsodium/all/conanfile.py
+++ recipes/libsodium/all/conanfile.py
@@ -1,13 +1,13 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration, ConanException
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import export_conandata_patches, apply_conandata_patches, get, rmdir, copy, rm, replace_in_file
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import is_msvc, is_msvc_static_runtime, MSBuildDeps, MSBuildToolchain, MSBuild, VCVars, unix_path, msvc_runtime_flag, vs_layout
+from conan.tools.microsoft import is_msvc, is_msvc_static_runtime, MSBuild, MSBuildToolchain
 import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.54.0"
 
 
 class LibsodiumConan(ConanFile):
@@ -49,24 +49,12 @@ class LibsodiumConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
-        try:
-            del self.settings.compiler.libcxx
-        except Exception:
-            pass
-        try:
-            del self.settings.compiler.cppstd
-        except Exception:
-            pass
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
-        if is_msvc(self):
-            vs_layout(self)
-        else:
-            basic_layout(self, src_folder="src")
+        basic_layout(self, src_folder="src")
 
     def validate(self):
         if self.options.shared and is_msvc(self) and is_msvc_static_runtime(self):
@@ -81,40 +69,28 @@ class LibsodiumConan(ConanFile):
                 if not self.conf.get("tools.microsoft.bash:path", check_type=str):
                     self.tool_requires("msys2/cci.latest")
 
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
     def generate(self):
         if is_msvc(self):
             tc = MSBuildToolchain(self)
             tc.generate()
-            tc = MSBuildDeps(self)
-            tc.generate()
-            tc = VCVars(self)
-            tc.generate()
         else:
-            tc = AutotoolsToolchain(self)
+            env = VirtualBuildEnv(self)
+            env.generate()
 
+            tc = AutotoolsToolchain(self)
             yes_no = lambda v: "yes" if v else "no"
             tc.configure_args.append("--enable-soname-versions={}".format(yes_no(self.options.use_soname)))
             tc.configure_args.append("--enable-pie={}".format(yes_no(self.options.PIE)))
-
-            env = tc.environment()
-
-            # if self._is_mingw:
-                # add libssp (gcc support library) for some missing symbols (e.g. __strcpy_chk)
-                # FIXME how do I do this in conan v2?
-                # autotools.libs.append("ssp")
-
+            if self._is_mingw:
+                tc.extra_ldflags.append("-lssp")
             if self.settings.os == "Emscripten":
                 # FIXME: this is an old comment/test, has not been re-tested with conan2 upgrade
                 self.output.warn("os=Emscripten is not tested/supported by this recipe")
                 # FIXME: ./dist-build/emscripten.sh does not respect options of this recipe
-
-            tc.generate(env)
-
-            env = VirtualBuildEnv(self)
-            env.generate()
-
-    def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+            tc.generate()
 
     @property
     def _msvc_sln_folder(self):
@@ -211,7 +187,6 @@ class LibsodiumConan(ConanFile):
         msbuild.platform = platform
         msbuild.build(msvc_sln_path)
 
-
     def build(self):
         apply_conandata_patches(self)
         if is_msvc(self):
@@ -222,16 +197,15 @@ class LibsodiumConan(ConanFile):
             autotools.make()
 
     def package(self):
-        copy(self, "*LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"), keep_path=False)
+        copy(self, "*LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         if is_msvc(self):
             copy(self, "*.lib", src=self.source_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
             copy(self, "*.dll", src=self.source_folder, dst=os.path.join(self.package_folder, "bin"), keep_path=False)
-            inc_src = os.path.join(self.source_folder, "src", self.name, "include")
-            copy(self, "*.h", src=inc_src, dst=os.path.join(self.package_folder, "include"), keep_path=True, excludes=("*/private/*"))
+            inc_src = os.path.join(self.source_folder, "src", "libsodium", "include")
+            copy(self, "*.h", src=inc_src, dst=os.path.join(self.package_folder, "include"), excludes=("*/private/*"))
         else:
             autotools = Autotools(self)
-            # TODO: replace by autotools.install() once https://github.com/conan-io/conan/issues/12153 fixed
-            autotools.install(args=[f"DESTDIR={unix_path(self, self.package_folder)}"])
+            autotools.install()
             rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
             rm(self, "*.la", os.path.join(self.package_folder, "lib"))
 
