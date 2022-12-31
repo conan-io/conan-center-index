fe4966ccd5cde00b6131bd4cbe6751eed44a6cd0
Sneder89 <45610045+Sneder89@users.noreply.github.com>
2022-12-31 10:17:54 +0100

    Update conanfile.py

--- recipes/doxygen/all/test_package/conanfile.py
+++ recipes/doxygen/all/test_package/conanfile.py
@@ -5,7 +5,7 @@ from conan.errors import ConanException
 import os
 
 
-# It will become the standard on Conan 2
+# It will become the standard on Conan 2.0
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
