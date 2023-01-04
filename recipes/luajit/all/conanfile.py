ae97bc1697f5369a320f9167e863725cfbe9df71
Uilian Ries <uilianries@gmail.com>
2023-01-04 17:15:23 +0100

    Do not support Mac M1
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/luajit/all/conanfile.py
+++ recipes/luajit/all/conanfile.py
@@ -41,7 +41,7 @@ class LuajitConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def validate(self):
-        if Version(self.version) <= "2.1.0-beta3" and self.settings.os == "Macos" and self.settings.arch == "armv8" and cross_building(self):
+        if self.settings.os == "Macos" and self.settings.arch == "armv8" and cross_building(self):
             raise ConanInvalidConfiguration(f"{self.ref} can not be cross-built to Mac M1. Please, try any version >=2.1")
         elif Version(self.version) <= "2.1.0-beta1" and self.settings.os == "Macos" and self.settings.arch == "armv8":
             raise ConanInvalidConfiguration(f"{self.ref} is not supported by Mac M1. Please, try any version >=2.1")
