a863d3f0db31641114f4c65ab5bc7cea4bbc1de2
toge <toge.mail@gmail.com>
2023-01-05 14:33:56 +0900

    drop support clang 13 in 0.2.4

--- recipes/glaze/all/conanfile.py
+++ recipes/glaze/all/conanfile.py
@@ -27,7 +27,7 @@ class GlazeConan(ConanFile):
             "Visual Studio": "16",
             "msvc": "192",
             "gcc": "11" if Version(self.version) < "0.2.4" else "12",
-            "clang": "12",
+            "clang": "12" if Version(self.version) < "0.2.4" else "13",
             "apple-clang": "13.1",
         }
 
