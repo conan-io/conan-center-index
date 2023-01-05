fa27bc77a88bc22f210e8a758839b7f45aa919b3
Andrew Marshall <planetmarshalluk@gmail.com>
2023-01-05 08:40:48 +0000

    Revert update of autotools build to new toolchain

--- recipes/cairo/meson/conanfile.py
+++ recipes/cairo/meson/conanfile.py
@@ -111,7 +111,7 @@ class CairoConan(ConanFile):
         if self.options.with_png:
             self.requires("libpng/1.6.39")
         if self.options.with_glib:
-            self.requires("glib/2.75.1")
+            self.requires("glib/2.75.0")
         if self.settings.os == "Linux":
             if self.options.with_xlib or self.options.with_xlib_xrender or self.options.with_xcb:
                 self.requires("xorg/system")
