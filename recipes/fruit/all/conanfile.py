25952813e6f2fe9cd7442bf3944fb101b31357b5
Timo Lange <tiolangit@outlook.com>
2023-01-05 09:31:30 +0100

    Revert "remove deletion of fPIC"
    This reverts commit a1641234e085f38b9bb040dc476381bdb1400de1.

--- recipes/fruit/all/conanfile.py
+++ recipes/fruit/all/conanfile.py
@@ -32,6 +32,10 @@ class FruitConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
     def layout(self):
         cmake_layout(self, src_folder="src")
 
