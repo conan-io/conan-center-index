dbb2ab86154a3f8e8561cd9193424d2deed0ba33
Jihadist <tomasiche@gmail.com>
2023-01-05 16:36:44 +0300

    update recipe

--- recipes/limereport/all/conanfile.py
+++ recipes/limereport/all/conanfile.py
@@ -1,5 +1,10 @@
-from conans import CMake, ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conans import tools
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, get, rm, rmdir
+from conan import ConanFile
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, unix_path
+from conan.tools.apple import fix_apple_shared_install_name
 import os
 
 
@@ -10,8 +15,6 @@ class LimereportConan(ConanFile):
     topics = ("conan", "limereport", "pdf", "report","qt")
     license = "LGPL-3.0", "GPL-3.0"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = "CMakeLists.txt"
-    generators = "cmake", "cmake_find_package"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -19,29 +22,22 @@ class LimereportConan(ConanFile):
         "with_zint": [True, False]
     }
     default_options = {
-        "shared": False,
+        "shared": True,
         "fPIC": True,
-        "with_zint": True,
-        "qt:qtscript": True,
+        "with_zint": False,
         "qt:qtquickcontrols": True,
         "qt:qtquickcontrols2": True,
         "qt:qtsvg": True,
-        "qt:qttools": True,
-        "qt:qtwinextras": True
+        "qt:qttools": True
     }
-    _cmake = None
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     @property
     def _minimum_compilers_version(self):
@@ -56,46 +52,49 @@ class LimereportConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
     def build_requirements(self):
-        self.build_requires("libpng/1.6.37")
+        self.build_requires("libpng/1.6.38")
 
 
     def requirements(self):
-        self.requires("qt/5.15.2")
+        self.requires("qt/5.15.7")
 
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("limereport-{}".format(self.version), self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
 
-
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        if self.options.shared == False:
-            self._cmake.definitions["LIMEREPORT_STATIC"] = True
-        if self.settings.os == "Windows":
-            self._cmake.definitions["WINDOWS_BUILD"] = True
-
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
-
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["LIMEREPORT_STATIC"] = not self.options.shared
+        if is_msvc(self):
+            tc.variables["WINDOWS_BUILD"] = True
+        qt_major = tools.Version(self.deps_cpp_info["qt"].version).major
+        if qt_major == 6: 
+            tc.variables["USE_QT6"] = True
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
 
     def build(self):
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
-
+            
     def package(self):
-        self.copy("COPYING*", src=self._source_subfolder, dst="licenses")
-        cmake = self._configure_cmake()
+        copy(self, "COPYING*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        fix_apple_shared_install_name(self)
 
     def package_info(self):
         qt_major = tools.Version(self.deps_cpp_info["qt"].version).major
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.names["cmake_find_package"] = ["Limereport-qt{}".format(qt_major)]
-        self.cpp_info.names["cmake_find_package_multi"] = ["Limereport-qt{}".format(qt_major)]
+        self.cpp_info.libs = ["limereport-qt{}".format(qt_major)]
+        # self.cpp_info.names["cmake_find_package"] = ["limereport-qt{}".format(qt_major)]
+        # self.cpp_info.names["cmake_find_package_multi"] = ["limereport-qt{}".format(qt_major)]
 
