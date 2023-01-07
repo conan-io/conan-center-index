97912b15c731931fda049c44f2363714a81823e6
Jordan Williams <jordan@jwillikers.com>
2023-01-06 18:54:09 -0600

    Remove -hostprefix option to ensure mkspec exists

--- recipes/qt/5.x.x/conanfile.py
+++ recipes/qt/5.x.x/conanfile.py
@@ -563,8 +563,7 @@ class QtConan(ConanFile):
         args = ["-confirm-license", "-silent", "-nomake examples", "-nomake tests",
                 f"-prefix {self.package_folder}"]
         if cross_building(self):
-            native_prefix = os.path.join(self.package_folder, "native")
-            args.extend([f"-extprefix {self.package_folder}", f"-hostprefix {native_prefix}"])
+            args.append(f"-extprefix {self.package_folder}")
         args.append("-v")
         args.append("-archdatadir  %s" % os.path.join(self.package_folder, "bin", "archdatadir"))
         args.append("-datadir  %s" % os.path.join(self.package_folder, "bin", "datadir"))
@@ -804,7 +803,6 @@ class QtConan(ConanFile):
     def package(self):
         with chdir(self, "build_folder"):
             self.run(f"{self._make_program()} install")
-        rmdir(self, os.path.join(self.package_folder, "native"))
         save(self, os.path.join(self.package_folder, "bin", "qt.conf"), """[Paths]
 Prefix = ..
 ArchData = bin/archdatadir
