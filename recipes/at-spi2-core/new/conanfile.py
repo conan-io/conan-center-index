3ac7cc6e756d31dbadd2ab089795501101501118
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-01-09 10:35:01 +0100

    at-spi2-core 2.47.1

--- recipes/at-spi2-core/new/conanfile.py
+++ recipes/at-spi2-core/new/conanfile.py
@@ -1,5 +1,9 @@
-from conans import ConanFile, Meson, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, replace_in_file, rmdir
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.meson import Meson, MesonToolchain
 import os
 
 
@@ -10,7 +14,6 @@ class AtSpi2CoreConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://gitlab.gnome.org/GNOME/at-spi2-core/"
     license = "LGPL-2.1-or-later"
-    generators = "pkg_config"
 
     provides = "at-spi2-atk", "atk"
 
@@ -26,19 +29,8 @@ class AtSpi2CoreConan(ConanFile):
         "with_x11": False,
         }
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
-    _meson = None
-
     def export_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        export_conandata_patches(self)
 
     def configure(self):
         if self.options.shared:
@@ -49,14 +41,15 @@ class AtSpi2CoreConan(ConanFile):
             self.options["glib"].shared = True
 
     def build_requirements(self):
-        self.build_requires("meson/0.62.2")
-        self.build_requires("pkgconf/1.7.4")
+        self.build_requires("meson/1.0.0")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.build_requires("pkgconf/1.9.3")
 
     def requirements(self):
-        self.requires("glib/2.73.0")
+        self.requires("glib/2.75.2")
         if self.options.with_x11:
             self.requires("xorg/system")
-        self.requires("dbus/1.12.20")
+        self.requires("dbus/1.15.2")
 
     def validate(self):
         if self.options.shared and not self.options["glib"].shared:
@@ -66,46 +59,43 @@ class AtSpi2CoreConan(ConanFile):
         if self.settings.os != "Linux":
             raise ConanInvalidConfiguration("only linux is supported by this recipe")
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                    strip_root=True, destination=self._source_subfolder)
+    def layout(self):
+        # src_folder must use the same source folder name the project
+        basic_layout(self, src_folder="src")
 
-    def _configure_meson(self):
-        if self._meson:
-            return self._meson
-        self._meson = Meson(self)
-        defs = {}
-        defs["introspection"] = "no"
-        defs["docs"] = "false"
-        defs["x11"] = "yes" if self.options.with_x11 else "no"
-        args=[]
-        args.append("--datadir=%s" % os.path.join(self.package_folder, "res"))
-        args.append("--localedir=%s" % os.path.join(self.package_folder, "res"))
-        args.append("--wrap-mode=nofallback")
-        self._meson.configure(defs=defs, build_folder=self._build_subfolder, source_folder=self._source_subfolder, pkg_config_paths=".", args=args)
-        return self._meson
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = MesonToolchain(self)
+        tc.project_options["introspection"] = "no"
+        tc.project_options["docs"] = "false"
+        tc.project_options["x11"] = "yes" if self.options.with_x11 else "no"
+        tc.generate()
+        tc = PkgConfigDeps(self)
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        tools.replace_in_file(os.path.join(self._source_subfolder, "bus", "meson.build"),
+        apply_conandata_patches(self)
+        replace_in_file(self, os.path.join(self.source_folder, "bus", "meson.build"),
                                 "if x11_dep.found()",
                                 "if x11_option == 'yes'")
-        tools.replace_in_file(os.path.join(self._source_subfolder, 'meson.build'),
+        replace_in_file(self, os.path.join(self.source_folder, 'meson.build'),
             "subdir('tests')",
             "#subdir('tests')")
-        tools.replace_in_file(os.path.join(self._source_subfolder, 'meson.build'),
+        replace_in_file(self, os.path.join(self.source_folder, 'meson.build'),
             "libxml_dep = dependency('libxml-2.0', version: libxml_req_version)",
             "#libxml_dep = dependency('libxml-2.0', version: libxml_req_version)")
-        meson = self._configure_meson()
+        meson = Meson(self)
+        meson.configure()
         meson.build()
 
     def package(self):
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        meson = self._configure_meson()
+        self.copy(pattern="COPYING", dst="licenses", src=self.source_folder)
+        meson = Meson(self)
         meson.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "etc"))
+        rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(os.path.join(self.package_folder, "etc"))
 
 
     def package_info(self):
