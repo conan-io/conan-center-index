5fb484cea7786f42816f9a3d75bb950daa73f394
Sneder89 <45610045+Sneder89@users.noreply.github.com>
2023-01-09 10:05:56 +0100

    Update conanfile.py

--- recipes/doxygen/all/conanfile.py
+++ recipes/doxygen/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.55.0"
+required_conan_version = ">=1.50.2"
 
 
 class DoxygenConan(ConanFile):
