02aa23334f0247c8a54ec5c5151e7681606ecaf3
toge <toge.mail@gmail.com>
2023-01-11 05:10:05 +0900

    libucl: add version 0.8.2, support conan v2

---
+++ recipes/libucl/all/test_v1_package/conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile, CMake
+from conan.tools.build import cross_building
+import os
+
+
+class TestPackageV1Conan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            sample_conf = os.path.join(self.source_folder, os.pardir, "test_package", "sample.conf")
+            self.run(f"{bin_path} {sample_conf}", run_environment=True)
