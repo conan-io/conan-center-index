16dd12b3c983187417711331b4e3213e70a5dc02
werto87 <huhulasmichinruhe@web.de>
2023-01-10 16:15:44 +0900

    new version. support for c++17

--- recipes/confu_json/all/conanfile.py
+++ recipes/confu_json/all/conanfile.py
@@ -20,10 +20,6 @@ class ConfuJson(ConanFile):
         return "source_subfolder"
 
     @property
-    def _minimum_cpp_standard(self):
-        return 20
-
-    @property
     def _minimum_compilers_version(self):
         return {
             "Visual Studio": "17",
@@ -34,29 +30,32 @@ class ConfuJson(ConanFile):
 
        
     def configure(self):
-        if is_msvc(self) and Version(self.version) < "0.0.9":
-            raise ConanInvalidConfiguration(
-                "Visual Studio is not supported in versions before confu_json/0.0.9")
         if self.settings.compiler == "apple-clang":
             raise ConanInvalidConfiguration(
                 "apple-clang is not supported. Pull request welcome")
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.build.check_min_cppstd(self, self._minimum_cpp_standard)
-
-        min_version = self._minimum_compilers_version.get(
-            str(self.settings.compiler))
-        if not min_version:
-            self.output.warn("{} recipe lacks information about the {} "
-                             "compiler support.".format(
-                                 self.name, self.settings.compiler))
+        if is_msvc(self) and Version(self.version) <= "1.0.0":
+            if self.settings.compiler.get_safe("cppstd"):
+                tools.build.check_min_cppstd(self, 17)
         else:
-            if Version(self.settings.compiler.version) < min_version:
+            if is_msvc(self) and Version(self.version) < "0.0.9":
                 raise ConanInvalidConfiguration(
-                    "{} requires C++{} support. "
-                    "The current compiler {} {} does not support it.".format(
-                        self.name, self._minimum_cpp_standard,
-                        self.settings.compiler,
-                        self.settings.compiler.version))
+                    "Visual Studio is not supported in versions before confu_json/0.0.9")
+            if self.settings.compiler.get_safe("cppstd"):
+                tools.build.check_min_cppstd(self, 20)
+            min_version = self._minimum_compilers_version.get(
+                str(self.settings.compiler))
+            if not min_version:
+                self.output.warn("{} recipe lacks information about the {} "
+                                "compiler support.".format(
+                                    self.name, self.settings.compiler))
+            else:
+                if Version(self.settings.compiler.version) < min_version:
+                    raise ConanInvalidConfiguration(
+                        "{} requires C++{} support. "
+                        "The current compiler {} {} does not support it.".format(
+                            self.name, self._minimum_cpp_standard,
+                            self.settings.compiler,
+                            self.settings.compiler.version))
 
     def requirements(self):
         self.requires("boost/1.79.0")
