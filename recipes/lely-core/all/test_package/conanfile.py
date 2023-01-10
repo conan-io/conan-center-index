ead6aba4df918cbe2df4f43bf8d594bb51e036ac
Uy Ha <hchanuy@gmail.com>
2023-01-10 15:50:21 +0000

    fix v2 test

--- recipes/lely-core/all/test_package/conanfile.py
+++ recipes/lely-core/all/test_package/conanfile.py
@@ -7,7 +7,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
