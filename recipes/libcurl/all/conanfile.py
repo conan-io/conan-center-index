8524b53706c04928af7c74c06c8f424b1ec564c0
SSE4 <tomskside@gmail.com>
2023-01-11 16:29:26 +0700

    (#15195) - [libcurl] enable Unicode
    * - [libcurl] enable Unicode
    
    Signed-off-by: SSE4 <tomskside@gmail.com>
    
    * - update sha for cacert
    
    Signed-off-by: SSE4 <tomskside@gmail.com>
    
    * - use a versioned cacert URL to avoid sudden SHA changes in future
    
    Signed-off-by: SSE4 <tomskside@gmail.com>
    
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- recipes/libcurl/all/conanfile.py
+++ recipes/libcurl/all/conanfile.py
@@ -226,7 +226,7 @@ class LibcurlConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version],
                   destination=self.source_folder, strip_root=True)
-        download(self, "https://curl.haxx.se/ca/cacert.pem", "cacert.pem", verify=True, sha256="2cff03f9efdaf52626bd1b451d700605dc1ea000c5da56bd0fc59f8f43071040")
+        download(self, "https://curl.se/ca/cacert-2023-01-10.pem", "cacert.pem", verify=True, sha256="fb1ecd641d0a02c01bc9036d513cb658bbda62a75e246bedbc01764560a639f0")
 
     def generate(self):
         env = VirtualBuildEnv(self)
@@ -529,6 +529,7 @@ class LibcurlConan(ConanFile):
             tc = CMakeToolchain(self, generator="Ninja")
         else:
             tc = CMakeToolchain(self)
+        tc.variables["ENABLE_UNICODE"] = True
         tc.variables["BUILD_TESTING"] = False
         tc.variables["BUILD_CURL_EXE"] = False
         tc.variables["CURL_DISABLE_LDAP"] = not self.options.with_ldap
