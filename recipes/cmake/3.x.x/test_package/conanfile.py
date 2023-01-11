182c7d474ad40462e032d264c75de8aa64bf0da9
System-Arch <33330183+System-Arch@users.noreply.github.com>
2023-01-11 09:02:14 -0500

    Replace VirtualRunEnv with VirtualBuildEnv
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/cmake/3.x.x/test_package/conanfile.py
+++ recipes/cmake/3.x.x/test_package/conanfile.py
@@ -5,7 +5,8 @@ import re
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "VirtualRunEnv"
+    generators = "VirtualBuildEnv"
+    test_type = "explicit"
 
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
