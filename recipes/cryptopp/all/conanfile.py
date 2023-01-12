e6733d79fb9c48ccb184f8b8363f1ce9cd6a640a
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-01-12 10:27:17 +0000

    cryptopp: pass output to self.run() as positional argument

--- recipes/cryptopp/all/conanfile.py
+++ recipes/cryptopp/all/conanfile.py
@@ -41,7 +41,7 @@ class CryptoPPConan(ConanFile):
             import re
             from io import StringIO
             output = StringIO()
-            self.run("cmake --version", output=output)
+            self.run("cmake --version", output)
             m = re.search(r'cmake version (\d+\.\d+\.\d+)', output.getvalue())
             return Version(m.group(1)) >= required_version
         except:
