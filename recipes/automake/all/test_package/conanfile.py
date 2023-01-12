b675d62a4851255ec17f7a0599f905eaa6acb5f6
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-01-12 15:26:20 +0000

    Automake: port to v2 and make relocatable

--- recipes/automake/all/test_package/conanfile.py
+++ recipes/automake/all/test_package/conanfile.py
@@ -1,8 +1,12 @@
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
-from conan.tools.microsoft import is_msvc
-from contextlib import contextmanager
+from conan import ConanFile
+
+from conan.tools.env import Environment, VirtualBuildEnv
+from conan.tools.files import copy, chdir
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, unix_path
+
 import os
-import shutil
 
 required_conan_version = ">=1.45.0"
 
@@ -12,6 +16,7 @@ class TestPackageConan(ConanFile):
     exports_sources = "configure.ac", "Makefile.am", "test_package_1.c", "test_package.cpp"
     # DON'T COPY extra.m4 TO BUILD FOLDER!!!
     test_type = "explicit"
+    win_bash = True
 
     @property
     def _settings_build(self):
@@ -22,17 +27,28 @@ class TestPackageConan(ConanFile):
 
     def build_requirements(self):
         self.build_requires(self.tested_reference_str)
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", check_type=str):
             self.build_requires("msys2/cci.latest")
 
-    @contextmanager
-    def _build_context(self):
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+
+        env = Environment()
+
         if is_msvc(self):
-            with tools.vcvars(self):
-                with tools.environment_append({"CC": "cl -nologo", "CXX": "cl -nologo",}):
-                    yield
-        else:
-            yield
+            env.define("CC", "cl -nologo")
+            env.define("CXX", "cl -nologo")
+        
+        compile_script = unix_path(self, self.dependencies["automake"].conf_info.get("user.automake:compile-wrapper"))
+
+        env.define("COMPILE",compile_script )
+        env.define("ACLOCAL_PATH", unix_path(self, os.path.join(self.source_folder)))
+        env.vars(self, scope="build").save_script("automake_build_test")
+
 
     _default_cc = {
         "gcc": "gcc",
@@ -48,42 +64,56 @@ class TestPackageConan(ConanFile):
             system_cc = self._default_cc.get(str(self.settings.compiler))
         return system_cc
     
-    @property
-    def _user_info(self):
-        return getattr(self, "user_info_build", self.deps_user_info)
+    # def _build_scripts(self):
 
-    def _build_scripts(self):
-        """Test compile script of automake"""
-        compile_script = self._user_info["automake"].compile
-        ar_script = self._user_info["automake"].ar_lib
-        assert os.path.isfile(ar_script)
-        assert os.path.isfile(compile_script)
 
-        if self._system_cc:
-            with tools.vcvars(self) if is_msvc(self) else tools.no_op():
-                self.run("{} {} test_package_1.c -o script_test".format(tools.unix_path(compile_script), self._system_cc), win_bash=tools.os_info.is_windows)
+    #     if self._system_cc:
+    #         with tools.vcvars(self) if is_msvc(self) else tools.no_op():
+    #             self.run("{} {} test_package_1.c -o script_test".format(tools.unix_path(compile_script), self._system_cc), win_bash=tools.os_info.is_windows)
 
-    def _build_autotools(self):
-        """Test autoreconf + configure + make"""
-        with tools.environment_append({"AUTOMAKE_CONAN_INCLUDES": [tools.unix_path(self.source_folder)]}):
-            self.run("{} -fiv".format(os.environ["AUTORECONF"]), win_bash=tools.os_info.is_windows)
-        self.run("{} --help".format(os.path.join(self.build_folder, "configure").replace("\\", "/")), win_bash=tools.os_info.is_windows)
-        autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        with self._build_context():
-            autotools.configure()
-            autotools.make()
+    # def _build_autotools(self):
+    #     """Test autoreconf + configure + make"""
+    #     with tools.environment_append({"AUTOMAKE_CONAN_INCLUDES": [tools.unix_path(self.source_folder)]}):
+    #         self.run("{} -fiv".format(os.environ["AUTORECONF"]), win_bash=tools.os_info.is_windows)
+    #     self.run("{} --help".format(os.path.join(self.build_folder, "configure").replace("\\", "/")), win_bash=tools.os_info.is_windows)
+    #     autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+    #     with self._build_context():
+    #         autotools.configure()
+    #         autotools.make()
 
     def build(self):
-        for src in self.exports_sources:
-            shutil.copy(os.path.join(self.source_folder, src), self.build_folder)
+        #Copy files to build folder
+        for item in self.exports_sources:
+            copy(self, item, self.source_folder, self.build_folder)
+        # Test the compile wrapper for cl
+        compiler_name = self._system_cc
+        self.output.warning(f"source folder: {self.source_folder}")
+        self.run("pwd")
+        self.run("echo $COMPILE")
+        self.run(f"$COMPILE {compiler_name} {self.source_folder}/test_package_1.c -o {self.build_folder}/script_test")
+        
 
-        self._build_scripts()
-        self._build_autotools()
+        # # Build test project
+        with chdir(self, self.build_folder):
+            self.run("autoreconf -fiv")
+            self.run("./configure --help")
+            self.run("./configure")
+            self.run("make")
+        # self.run("autoreconf ")
+        # autotools = Autotools(self)
+        # self.run("which autoreconf")
+        # autotools.autoreconf()
+        # self.run(f"{self.source_folder}/configure --help")
+        # autotools.configure()
+        # autotools.make()
 
     def test(self):
-        if self._system_cc:
-            if not tools.cross_building(self):
-                self.run(os.path.join(".", "script_test"), run_environment=True)
+        self.run(f"{self.build_folder}/script_test")
+        self.run(f"{self.build_folder}/test_package")
+        pass
+        # if self._system_cc:
+        #     if not tools.cross_building(self):
+        #         self.run(os.path.join(".", "script_test"), run_environment=True)
 
-        if not tools.cross_building(self):
-            self.run(os.path.join(".", "test_package"), run_environment=True)
+        # if not tools.cross_building(self):
+        #     self.run(os.path.join(".", "test_package"), run_environment=True)
