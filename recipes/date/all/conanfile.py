db508f783a988f65a718eaff753b4da6ab28c41b
Samuel Dowling <samuel.dowling@protonmail.com>
2023-01-14 00:02:33 +1030

    [date] Fix linting issues by adding src_folder arg to layout method

--- recipes/date/all/conanfile.py
+++ recipes/date/all/conanfile.py
@@ -65,7 +65,7 @@ class DateConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
 
     def generate(self):
         tc = CMakeToolchain(self)
