b5cf16f7c07c13a5a6c4e7dc1de92c8248e0bb22
System-Arch <33330183+System-Arch@users.noreply.github.com>
2023-01-12 23:45:18 -0500

    Bump required_conan_version to trigger rebuild now that autoconf has been updated

--- recipes/automake/all/conanfile.py
+++ recipes/automake/all/conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 
-required_conan_version = ">=1.54"
+required_conan_version = ">=1.55"
 
 
 class AutomakeConan(ConanFile):
