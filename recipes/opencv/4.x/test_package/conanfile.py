10bebc3f385cb7c53440ea32d7a1a0b53585592b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-01-14 21:43:20 +0100

    parallel ctest in test package

--- recipes/opencv/4.x/test_package/conanfile.py
+++ recipes/opencv/4.x/test_package/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.build import can_run
+from conan.tools.build import build_jobs, can_run
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import chdir
 
@@ -40,4 +40,4 @@ class TestPackageConan(ConanFile):
     def test(self):
         if can_run(self):
             with chdir(self, self.build_folder):
-                self.run(f"ctest --output-on-failure -C {self.settings.build_type}", env="conanrun")
+                self.run(f"ctest --output-on-failure -C {self.settings.build_type} -j {build_jobs(self)}", env="conanrun")
