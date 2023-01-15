9d530f5739cebd861407f082091b80fa15b87ad2
System-Arch <33330183+System-Arch@users.noreply.github.com>
2023-01-15 14:37:02 -0500

    Removed environment variables per discussion in conan-io/conan:#12812

--- recipes/automake/all/conanfile.py
+++ recipes/automake/all/conanfile.py
@@ -108,48 +108,25 @@ class AutomakeConan(ConanFile):
         self.output.info(f"Appending PATH environment variable: {bin_path}")
         self.env_info.PATH.append(bin_path)
 
-        dataroot_path = path.join(self.package_folder, "res")
-        self.output.info(f"Defining AUTOMAKE_DATADIR environment variable: {dataroot_path}")
-        self.env_info.AUTOMAKE_DATADIR = dataroot_path
-        self.buildenv_info.define_path("AUTOMAKE_DATADIR", dataroot_path)
-
-        version = Version(self.version)
-        automake_dataroot_path = path.join(dataroot_path, f"automake-{version.major}.{version.minor}")
-        self.output.info(f"Defining AUTOMAKE_LIBDIR environment variable: {automake_dataroot_path}")
-        self.env_info.AUTOMAKE_LIBDIR = automake_dataroot_path
-        self.buildenv_info.define_path("AUTOMAKE_LIBDIR", automake_dataroot_path)
-
-        self.output.info(f"Defining AUTOMAKE_PERLLIBDIR environment variable: {automake_dataroot_path}")
-        self.env_info.AUTOMAKE_PERLLIBDIR = automake_dataroot_path
-        self.buildenv_info.define_path("AUTOMAKE_PERLLIBDIR", automake_dataroot_path)
-
-        aclocal_bin = path.join(bin_path, "aclocal")
-        self.output.info(f"Defining ACLOCAL environment variable: {aclocal_bin}")
-        self.env_info.ACLOCAL = aclocal_bin
-        self.buildenv_info.define_path("ACLOCAL", aclocal_bin)
-
-        automake_bin = path.join(bin_path, "automake")
-        self.output.info(f"Defining AUTOMAKE environment variable: {automake_bin}")
-        self.env_info.AUTOMAKE = automake_bin
-        self.buildenv_info.define_path("AUTOMAKE", automake_bin)
-
         compile_bin = path.join(automake_dataroot_path, "compile")
-        self.output.info(f"Define path to `compile` binary in user_info as: {compile_bin}")
-        self.user_info.compile = compile_bin  # FIXME: Conan V2 will use conf_key instead of user_info
         compile_conf_key = "user.automake:compile"
         self.output.info(f"Defining path to `compile` binary in configuration as `{compile_conf_key}` with value: {compile_bin}")
         self.conf_info.define(compile_conf_key, compile_bin)
 
         ar_lib_bin = path.join(automake_dataroot_path, "ar-lib")
-        self.output.info(f"Define path to ar_lib binary in user_info as: {ar_lib_bin}")
-        self.user_info.ar_lib = ar_lib_bin  # FIXME: Conan V2 will use conf_key instead of user_info
         ar_lib_conf_key = "user.automake:ar-lib"
         self.output.info(f"Defining path to ar-lib binary in configuration as `{ar_lib_conf_key}` with value: {ar_lib_bin}")
         self.conf_info.define(ar_lib_conf_key, ar_lib_bin)
 
         install_sh_bin = path.join(automake_dataroot_path, "install-sh")
-        self.output.info(f"Define path to install_sh binary in user_info as: {install_sh_bin}")
-        self.user_info.install_sh = install_sh_bin  # FIXME: Conan  V2 will use conf_key instead of user_info
         install_sh_conf_key = "user.automake:install-sh"
         self.output.info(f"Defining path to install_sh binary in configuration as `{install_sh_conf_key}` with value: {install_sh_bin}")
         self.conf_info.define(install_sh_conf_key, install_sh_bin)
+
+        # Legacy v1.x support: Conan V2 will use conf_key instead of user_info
+        self.output.info(f"Define path to `compile` binary in user_info as: {compile_bin}")
+        self.user_info.compile = compile_bin
+        self.output.info(f"Define path to ar_lib binary in user_info as: {ar_lib_bin}")
+        self.user_info.ar_lib = ar_lib_bin
+        self.output.info(f"Define path to install_sh binary in user_info as: {install_sh_bin}")
+        self.user_info.install_sh = install_sh_bin
