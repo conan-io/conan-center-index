2b4b11c8dcebd822c9dfa8cb067b1c5377ed94a1
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-01-16 11:11:41 +0000

    bear 3.0.21

---
+++ recipes/bear/all/test_package/conanfile.py
@@ -0,0 +1,14 @@
+from conan import ConanFile
+
+
+# It will become the standard on Conan 2.x
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "VirtualBuildEnv"
+    test_type = "explicit"
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
+
+    def test(self):
+        self.run("bear --version")
