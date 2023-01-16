84eded15408934deae3bef82c2d5592815570610
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-01-16 01:38:09 +0100

    modernize more for conan v2

--- recipes/libsvm/all/test_package/conanfile.py
+++ recipes/libsvm/all/test_package/conanfile.py
@@ -5,15 +5,15 @@ from conan.tools.cmake import CMake, cmake_layout
 import os
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
 
-    def requirements(self):
-        self.requires(self.tested_reference_str)
-
     def layout(self):
         cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
