8e2f41c0bd31938e71cba201e7329d3ae4a54c3e
Jordan Williams <jordan@jwillikers.com>
2023-01-16 06:46:23 -0600

    (#15135) libcap: Modernize
    * libcap: Modernize
    
    * Add VirtualBuildEnv too

--- recipes/libcap/all/test_package/conanfile.py
+++ recipes/libcap/all/test_package/conanfile.py
@@ -2,23 +2,20 @@ import os
 
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake
+from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.env import Environment
-from conan.tools.cmake import cmake_layout
-
-required_conan_version = ">=1.38.0"
 
 
 class LibcapTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "PkgConfigDeps", "VirtualBuildEnv"
+    generators = "CMakeToolchain", "PkgConfigDeps", "VirtualBuildEnv", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
     def build_requirements(self):
-        self.tool_requires("pkgconf/1.7.4")
+        self.tool_requires("pkgconf/1.9.3")
 
     def layout(self):
         cmake_layout(self)
