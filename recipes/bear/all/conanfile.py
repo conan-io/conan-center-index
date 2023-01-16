6145bca0a62ccd67c7ee3b98ab4e7be41053f4bb
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-01-16 15:54:58 +0100

    Update conanfile.py

--- recipes/bear/all/conanfile.py
+++ recipes/bear/all/conanfile.py
@@ -46,7 +46,7 @@ class BearConan(ConanFile):
         self.requires("nlohmann_json/3.11.2")
 
     def build_requirements(self):
-        self.tool_requires("protobuf/3.21.9")
+        self.tool_requires("protobuf/3.21.4")
 
     def package_id(self):
         del self.info.settings.compiler
