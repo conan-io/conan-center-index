b890aecb4e76545fe173eb688b2f988cea8c0df8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-01-17 20:23:54 +0100

    simplify test package even more

--- recipes/opencv/4.x/test_package/conanfile.py
+++ recipes/opencv/4.x/test_package/conanfile.py
@@ -9,6 +9,20 @@ class TestPackageConan(ConanFile):
     generators = "CMakeDeps", "VirtualRunEnv"
     test_type = "explicit"
 
+    @property
+    def _tested_modules(self):
+        return [
+            # Main modules
+            "calib3d", "core", "features2d", "flann", "gapi", "highgui", "imgcodecs",
+            "imgproc", "ml", "objdetect", "photo", "stitching", "video", "videoio",
+            # Extra modules
+            "alphamat", "aruco", "bgsegm", "bioinspired", "ccalib", "dnn_superres", "face",
+            "freetype", "fuzzy", "hfs", "img_hash", "intensity_transform", "line_descriptor",
+            "mcc", "optflow", "phase_unwrapping", "quality", "reg", "rgbd", "saliency", "sfm",
+            "shape", "structured_light", "superres", "surface_matching", "text", "tracking",
+            "wechat_qrcode", "xfeatures2d", "ximgproc", "xobjdetect", "xphoto",
+        ]
+
     def layout(self):
         cmake_layout(self)
 
@@ -17,51 +31,16 @@ class TestPackageConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["OPENCV_WITH_CALIB3D"] = self.dependencies["opencv"].options.calib3d
-        tc.variables["OPENCV_WITH_FEATURES2D"] = self.dependencies["opencv"].options.features2d
-        tc.variables["OPENCV_WITH_FLANN"] = self.dependencies["opencv"].options.flann
-        tc.variables["OPENCV_WITH_GAPI"] = self.dependencies["opencv"].options.gapi
-        tc.variables["OPENCV_WITH_HIGHGUI"] = self.dependencies["opencv"].options.highgui
-        tc.variables["OPENCV_WITH_IMGCODECS"] = self.dependencies["opencv"].options.imgcodecs and self.dependencies["opencv"].options.with_png
-        tc.variables["OPENCV_WITH_IMGPROC"] = self.dependencies["opencv"].options.imgproc
-        tc.variables["OPENCV_WITH_ML"] = self.dependencies["opencv"].options.ml
-        tc.variables["OPENCV_WITH_OBJDETECT"] = self.dependencies["opencv"].options.objdetect
-        tc.variables["OPENCV_WITH_PHOTO"] = self.dependencies["opencv"].options.photo
-        tc.variables["OPENCV_WITH_STITCHING"] = self.dependencies["opencv"].options.stitching
-        tc.variables["OPENCV_WITH_VIDEO"] = self.dependencies["opencv"].options.video
-        tc.variables["OPENCV_WITH_VIDEOIO"] = self.dependencies["opencv"].options.videoio and self.dependencies["opencv"].options.with_ffmpeg
-        tc.variables["OPENCV_WITH_ALPHAMAT"] = self.dependencies["opencv"].options.get_safe("alphamat", False)
-        tc.variables["OPENCV_WITH_ARUCO"] = self.dependencies["opencv"].options.aruco
-        tc.variables["OPENCV_WITH_BGSEGM"] = self.dependencies["opencv"].options.bgsegm
-        tc.variables["OPENCV_WITH_BIOINSPIRED"] = self.dependencies["opencv"].options.bioinspired
-        tc.variables["OPENCV_WITH_CCALIB"] = self.dependencies["opencv"].options.ccalib
-        tc.variables["OPENCV_WITH_DNN_SUPERRES"] = self.dependencies["opencv"].options.dnn_superres
-        tc.variables["OPENCV_WITH_FACE"] = self.dependencies["opencv"].options.face
-        tc.variables["OPENCV_WITH_FREETYPE"] = self.dependencies["opencv"].options.freetype
-        tc.variables["OPENCV_WITH_FUZZY"] = self.dependencies["opencv"].options.fuzzy
-        tc.variables["OPENCV_WITH_HFS"] = self.dependencies["opencv"].options.hfs
-        tc.variables["OPENCV_WITH_IMG_HASH"] = self.dependencies["opencv"].options.img_hash
-        tc.variables["OPENCV_WITH_INTENSITY_TRANSFORM"] = self.dependencies["opencv"].options.get_safe("intensity_transform", False)
-        tc.variables["OPENCV_WITH_LINE_DESCRIPTOR"] = self.dependencies["opencv"].options.line_descriptor
-        tc.variables["OPENCV_WITH_MCC"] = self.dependencies["opencv"].options.get_safe("mcc", False)
-        tc.variables["OPENCV_WITH_OPTFLOW"] = self.dependencies["opencv"].options.optflow
-        tc.variables["OPENCV_WITH_PHASE_UNWRAPPING"] = self.dependencies["opencv"].options.phase_unwrapping
-        tc.variables["OPENCV_WITH_QUALITY"] = self.dependencies["opencv"].options.quality
-        tc.variables["OPENCV_WITH_REG"] = self.dependencies["opencv"].options.reg
-        tc.variables["OPENCV_WITH_RGBD"] = self.dependencies["opencv"].options.rgbd
-        tc.variables["OPENCV_WITH_SALIENCY"] = self.dependencies["opencv"].options.saliency
-        tc.variables["OPENCV_WITH_SFM"] = self.dependencies["opencv"].options.sfm
-        tc.variables["OPENCV_WITH_SHAPE"] = self.dependencies["opencv"].options.shape
-        tc.variables["OPENCV_WITH_STRUCTURED_LIGHT"] = self.dependencies["opencv"].options.structured_light
-        tc.variables["OPENCV_WITH_SUPERRES"] = self.dependencies["opencv"].options.get_safe("superres", False)
-        tc.variables["OPENCV_WITH_SURFACE_MATCHING"] = self.dependencies["opencv"].options.surface_matching
-        tc.variables["OPENCV_WITH_TEXT"] = self.dependencies["opencv"].options.text
-        tc.variables["OPENCV_WITH_TRACKING"] = self.dependencies["opencv"].options.tracking
-        tc.variables["OPENCV_WITH_WECHAT_QRCODE"] = self.dependencies["opencv"].options.get_safe("wechat_qrcode", False)
-        tc.variables["OPENCV_WITH_XFEATURES2D"] = self.dependencies["opencv"].options.xfeatures2d
-        tc.variables["OPENCV_WITH_XIMGPROC"] = self.dependencies["opencv"].options.ximgproc
-        tc.variables["OPENCV_WITH_XOBJDETECT"] = self.dependencies["opencv"].options.xobjdetect
-        tc.variables["OPENCV_WITH_XPHOTO"] = self.dependencies["opencv"].options.xphoto
+        for module in self._tested_modules:
+            cmake_option = f"OPENCV_WITH_{module.upper()}"
+            if module == "core":
+                tc.variables[cmake_option] = True
+            elif module == "imgcodecs":
+                tc.variables[cmake_option] = self.dependencies["opencv"].options.imgcodecs and self.dependencies["opencv"].options.with_png
+            elif module == "videoio":
+                tc.variables[cmake_option] = self.dependencies["opencv"].options.videoio and self.dependencies["opencv"].options.with_ffmpeg
+            else:
+                tc.variables[cmake_option] = self.dependencies["opencv"].options.get_safe(module, False)
         tc.generate()
 
     def build(self):
--- recipes/opencv/4.x/test_v1_package/conanfile.py
+++ recipes/opencv/4.x/test_v1_package/conanfile.py
@@ -6,6 +6,20 @@ class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
+    @property
+    def _tested_modules(self):
+        return [
+            # Main modules
+            "calib3d", "core", "features2d", "flann", "gapi", "highgui", "imgcodecs",
+            "imgproc", "ml", "objdetect", "photo", "stitching", "video", "videoio",
+            # Extra modules
+            "alphamat", "aruco", "bgsegm", "bioinspired", "ccalib", "dnn_superres", "face",
+            "freetype", "fuzzy", "hfs", "img_hash", "intensity_transform", "line_descriptor",
+            "mcc", "optflow", "phase_unwrapping", "quality", "reg", "rgbd", "saliency", "sfm",
+            "shape", "structured_light", "superres", "surface_matching", "text", "tracking",
+            "wechat_qrcode", "xfeatures2d", "ximgproc", "xobjdetect", "xphoto",
+        ]
+
     def _opencv_option(self, name, default):
         try:
             return getattr(self.options["opencv"], name, default)
@@ -14,51 +28,16 @@ class TestPackageConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["OPENCV_WITH_CALIB3D"] = self.options["opencv"].calib3d
-        cmake.definitions["OPENCV_WITH_FEATURES2D"] = self.options["opencv"].features2d
-        cmake.definitions["OPENCV_WITH_FLANN"] = self.options["opencv"].flann
-        cmake.definitions["OPENCV_WITH_GAPI"] = self.options["opencv"].gapi
-        cmake.definitions["OPENCV_WITH_HIGHGUI"] = self.options["opencv"].highgui
-        cmake.definitions["OPENCV_WITH_IMGCODECS"] = self.options["opencv"].imgcodecs and self.options["opencv"].with_png
-        cmake.definitions["OPENCV_WITH_IMGPROC"] = self.options["opencv"].imgproc
-        cmake.definitions["OPENCV_WITH_ML"] = self.options["opencv"].ml
-        cmake.definitions["OPENCV_WITH_OBJDETECT"] = self.options["opencv"].objdetect
-        cmake.definitions["OPENCV_WITH_PHOTO"] = self.options["opencv"].photo
-        cmake.definitions["OPENCV_WITH_STITCHING"] = self.options["opencv"].stitching
-        cmake.definitions["OPENCV_WITH_VIDEO"] = self.options["opencv"].video
-        cmake.definitions["OPENCV_WITH_VIDEOIO"] = self.options["opencv"].videoio and self.options["opencv"].with_ffmpeg
-        cmake.definitions["OPENCV_WITH_ALPHAMAT"] = self._opencv_option("alphamat", False)
-        cmake.definitions["OPENCV_WITH_ARUCO"] = self.options["opencv"].aruco
-        cmake.definitions["OPENCV_WITH_BGSEGM"] = self.options["opencv"].bgsegm
-        cmake.definitions["OPENCV_WITH_BIOINSPIRED"] = self.options["opencv"].bioinspired
-        cmake.definitions["OPENCV_WITH_CCALIB"] = self.options["opencv"].ccalib
-        cmake.definitions["OPENCV_WITH_DNN_SUPERRES"] = self.options["opencv"].dnn_superres
-        cmake.definitions["OPENCV_WITH_FACE"] = self.options["opencv"].face
-        cmake.definitions["OPENCV_WITH_FREETYPE"] = self.options["opencv"].freetype
-        cmake.definitions["OPENCV_WITH_FUZZY"] = self.options["opencv"].fuzzy
-        cmake.definitions["OPENCV_WITH_HFS"] = self.options["opencv"].hfs
-        cmake.definitions["OPENCV_WITH_IMG_HASH"] = self.options["opencv"].img_hash
-        cmake.definitions["OPENCV_WITH_INTENSITY_TRANSFORM"] = self._opencv_option("intensity_transform", False)
-        cmake.definitions["OPENCV_WITH_LINE_DESCRIPTOR"] = self.options["opencv"].line_descriptor
-        cmake.definitions["OPENCV_WITH_MCC"] = self._opencv_option("mcc", False)
-        cmake.definitions["OPENCV_WITH_OPTFLOW"] = self.options["opencv"].optflow
-        cmake.definitions["OPENCV_WITH_PHASE_UNWRAPPING"] = self.options["opencv"].phase_unwrapping
-        cmake.definitions["OPENCV_WITH_QUALITY"] = self.options["opencv"].quality
-        cmake.definitions["OPENCV_WITH_REG"] = self.options["opencv"].reg
-        cmake.definitions["OPENCV_WITH_RGBD"] = self.options["opencv"].rgbd
-        cmake.definitions["OPENCV_WITH_SALIENCY"] = self.options["opencv"].saliency
-        cmake.definitions["OPENCV_WITH_SFM"] = self.options["opencv"].sfm
-        cmake.definitions["OPENCV_WITH_SHAPE"] = self.options["opencv"].shape
-        cmake.definitions["OPENCV_WITH_STRUCTURED_LIGHT"] = self.options["opencv"].structured_light
-        cmake.definitions["OPENCV_WITH_SUPERRES"] = self._opencv_option("superres", False)
-        cmake.definitions["OPENCV_WITH_SURFACE_MATCHING"] = self.options["opencv"].surface_matching
-        cmake.definitions["OPENCV_WITH_TEXT"] = self.options["opencv"].text
-        cmake.definitions["OPENCV_WITH_TRACKING"] = self.options["opencv"].tracking
-        cmake.definitions["OPENCV_WITH_WECHAT_QRCODE"] = self._opencv_option("wechat_qrcode", False)
-        cmake.definitions["OPENCV_WITH_XFEATURES2D"] = self.options["opencv"].xfeatures2d
-        cmake.definitions["OPENCV_WITH_XIMGPROC"] = self.options["opencv"].ximgproc
-        cmake.definitions["OPENCV_WITH_XOBJDETECT"] = self.options["opencv"].xobjdetect
-        cmake.definitions["OPENCV_WITH_XPHOTO"] = self.options["opencv"].xphoto
+        for module in self._tested_modules:
+            cmake_option = f"OPENCV_WITH_{module.upper()}"
+            if module == "core":
+                cmake.definitions[cmake_option] = True
+            elif module == "imgcodecs":
+                cmake.definitions[cmake_option] = self.options["opencv"].imgcodecs and self.options["opencv"].with_png
+            elif module == "videoio":
+                cmake.definitions[cmake_option] = self.options["opencv"].videoio and self.options["opencv"].with_ffmpeg
+            else:
+                cmake.definitions[cmake_option] = self._opencv_option(module, False)
         cmake.configure()
         cmake.build()
 
