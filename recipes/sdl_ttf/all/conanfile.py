8314047064039237a599e404b16569a60f141138
toge <toge.mail@gmail.com>
2023-01-18 11:31:16 +0900

    remove share folder

--- recipes/sdl_ttf/all/conanfile.py
+++ recipes/sdl_ttf/all/conanfile.py
@@ -107,6 +107,7 @@ class SdlttfConan(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         rmdir(self, os.path.join(self.package_folder, "SDL2_ttf.framework"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "SDL2_ttf")
