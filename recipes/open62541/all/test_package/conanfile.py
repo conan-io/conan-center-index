f9e4df685a6d3a4aed15b2c3e18638e6a348433b
Dovydas Girdvainis <Dovydas.Girdvainis@Hahn-Schickard.de>
2023-01-19 12:11:16 +0100

    sanitize open62541_NODESET_DIR variable path

--- recipes/open62541/all/test_package/conanfile.py
+++ recipes/open62541/all/test_package/conanfile.py
@@ -19,7 +19,7 @@ class TestPackageConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["open62541_NODESET_DIR"] = self.deps_user_info["ua-nodeset"].nodeset_dir
+        tc.variables["open62541_NODESET_DIR"] = self.deps_user_info["ua-nodeset"].nodeset_dir.replace("\", "/")
         tc.variables["open62541_TOOLS_DIR"] = self.dependencies["open62541"].conf_info.get(
             "user.open62541:tools_dir").replace("\\", "/")
         tc.generate()
