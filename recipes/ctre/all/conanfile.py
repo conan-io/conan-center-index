1e3bd1a518f626bcacc80862ce4399784d21acf5
Stella Smith <stellasmith3@outlook.com>
2023-01-20 18:29:17 -0300

    Bump required_conan_version
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/ctre/all/conanfile.py
+++ recipes/ctre/all/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.layout import basic_layout
 from conan.tools.files import get, copy
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.50.0"
 
 
 class CtreConan(ConanFile):
