7abaebcd3a322b8c23bcdefe44763a174d4c0c12
toge <toge.mail@gmail.com>
2023-01-22 03:05:09 +0900

    zpp_bits: add 4.4.13, remove wrong version

--- recipes/zpp_bits/all/conanfile.py
+++ recipes/zpp_bits/all/conanfile.py
@@ -3,7 +3,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import get, copy
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import is_msvc, check_min_vs
 import os
 
 
@@ -30,6 +29,8 @@ class ZppBitsConan(ConanFile):
             "gcc": "11",
             "clang": "12",
             "apple-clang": "13.1",
+            "Visual Studio": "17",
+            "msvc": "193",
         }
 
     def layout(self):
@@ -41,19 +42,17 @@ class ZppBitsConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
-        check_min_vs(self, 193)
-        if not is_msvc(self):
-            def loose_lt_semver(v1, v2):
-                lv1 = [int(v) for v in v1.split(".")]
-                lv2 = [int(v) for v in v2.split(".")]
-                min_length = min(len(lv1), len(lv2))
-                return lv1[:min_length] < lv2[:min_length]
+        def loose_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
 
-            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-            if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
-                raise ConanInvalidConfiguration(
-                    f"{self.name} {self.version} requires C++{self._min_cppstd}, which your compiler does not support.",
-                )
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration(
+                f"{self.name} {self.version} requires C++{self._min_cppstd}, which your compiler does not support.",
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
