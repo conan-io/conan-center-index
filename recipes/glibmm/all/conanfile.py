7e5972685cb6f537fc217542a68bba5bdfefb08f
Andrew Marshall <planetmarshall@users.noreply.github.com>
2023-01-22 18:54:29 +0000

    Update recipes/glibmm/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/glibmm/all/conanfile.py
+++ recipes/glibmm/all/conanfile.py
@@ -4,6 +4,7 @@ import shutil
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
+from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.build import check_min_cppstd, cross_building
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import (
