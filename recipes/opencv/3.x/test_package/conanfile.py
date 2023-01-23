23fed2d8a3a2733a2ce41d55ca94f2ee0503ab6b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-01-24 00:27:14 +0100

    (#14949) opencv 3.x: conan v2 support
    * bump dependencies
    
    * allow to use libjpeg implementation from mozjpeg
    
    * conan v2 support

--- recipes/opencv/3.x/test_package/conanfile.py
+++ recipes/opencv/3.x/test_package/conanfile.py
@@ -1,18 +1,31 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["built_contrib"] = self.dependencies["opencv"].options.contrib
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["built_contrib"] = self.options["opencv"].contrib
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
