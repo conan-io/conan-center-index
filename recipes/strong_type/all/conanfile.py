402cbabf30ad69ddbf85c9f84319b44bb98241bc
a4z <harald.achitz@gmail.com>
2023-01-24 08:51:03 +0100

    Set minimum GCC to 7

--- recipes/strong_type/all/conanfile.py
+++ recipes/strong_type/all/conanfile.py
@@ -2,6 +2,8 @@ from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
+from conan.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.50.0"
@@ -23,6 +25,8 @@ class StrongTypeConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, 14)
+        if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "7":
+            raise ConanInvalidConfiguration("GCC < version 7 is not supported")
 
     def layout(self):
         basic_layout(self, src_folder="src")
@@ -35,12 +39,15 @@ class StrongTypeConan(ConanFile):
         pass
 
     def package(self):
-        copy(self, "*", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
-        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*", src=os.path.join(self.source_folder, "include"),
+             dst=os.path.join(self.package_folder, "include"))
+        copy(self, "LICENSE", src=self.source_folder,
+             dst=os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "strong_type")
-        self.cpp_info.set_property("cmake_target_name", "rollbear::strong_type")
+        self.cpp_info.set_property(
+            "cmake_target_name", "rollbear::strong_type")
         self.cpp_info.bindirs = []
         self.cpp_info.frameworkdirs = []
         self.cpp_info.libdirs = []
@@ -53,7 +60,8 @@ class StrongTypeConan(ConanFile):
         self.cpp_info.names["cmake_find_package_multi"] = "rollbear"
         self.cpp_info.components["strong_type"].names["cmake_find_package"] = "strong_type"
         self.cpp_info.components["strong_type"].names["cmake_find_package_multi"] = "strong_type"
-        self.cpp_info.components["strong_type"].set_property("cmake_target_name", "rollbear::strong_type")
+        self.cpp_info.components["strong_type"].set_property(
+            "cmake_target_name", "rollbear::strong_type")
         self.cpp_info.components["strong_type"].bindirs = []
         self.cpp_info.components["strong_type"].frameworkdirs = []
         self.cpp_info.components["strong_type"].libdirs = []
