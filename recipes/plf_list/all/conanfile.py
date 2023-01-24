bc276e20f3bf423cd3852f639e8fc33b59c0b673
toge <toge.mail@gmail.com>
2023-01-24 21:44:01 +0900

    plf_list: add version 2.57, support apple-clang 14

--- recipes/plf_list/all/conanfile.py
+++ recipes/plf_list/all/conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
-from conan.tools.files import copy, get
+from conan.tools.files import copy, get, replace_in_file
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.50.0"
@@ -10,9 +11,9 @@ class PlflistConan(ConanFile):
     name = "plf_list"
     description = "plf::list is a drop-in higher-performance replacement for std::list"
     license = "Zlib"
-    topics = ("plf_list", "container", "linked-list", "list")
-    homepage = "https://github.com/mattreecebentley/plf_list"
+    topics = ("container", "linked-list", "list", "header-only")
     url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/mattreecebentley/plf_list"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
@@ -27,7 +28,13 @@ class PlflistConan(ConanFile):
             destination=self.source_folder, strip_root=True)
 
     def build(self):
-        pass
+        # apple-clang 14 defaults C++ 20, but it doesn't support C++20 features little
+        # https://www.reddit.com/r/cpp/comments/v6ydea/xcode_now_defaults_to_c20/
+        if Version(self.version) >= "2.50":
+            replace_in_file(self, os.path.join(self.source_folder, "plf_list.h"),
+            "if __cplusplus > 201704L && ((defined(_LIBCPP_VERSION) && _LIBCPP_VERSION >= 13) || !defined(_LIBCPP_VERSION)) && ((defined(__clang__) && (__clang_major__ >= 13)) || (defined(__GNUC__) && __GNUC__ >= 10) || (!defined(__clang__) && !defined(__GNUC__)))",
+            "if __cplusplus > 201704L && ((defined(_LIBCPP_VERSION) && _LIBCPP_VERSION >= 13) || !defined(_LIBCPP_VERSION)) && (!defined(__APPLE_CC__) && (defined(__clang__) && (__clang_major__ >= 13)) || (defined(__GNUC__) && __GNUC__ >= 10) || (!defined(__clang__) && !defined(__GNUC__)))"
+            )
 
     def package(self):
         copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
