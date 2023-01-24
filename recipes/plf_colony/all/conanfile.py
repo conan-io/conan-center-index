9b0d3c6abc75aa9a96e4078ad1db0c12bf11ff54
toge <toge.mail@gmail.com>
2023-01-25 02:20:54 +0900

    create patch files for apple-clang 14

--- recipes/plf_colony/all/conanfile.py
+++ recipes/plf_colony/all/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
-from conan.tools.files import copy, get, replace_in_file
+from conan.tools.files import copy, get, apply_conandata_patches, export_conandata_patches
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.50.0"
@@ -17,6 +16,9 @@ class PlfcolonyConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os", "arch", "compiler", "build_type"
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def package_id(self):
         self.info.clear()
 
@@ -28,13 +30,7 @@ class PlfcolonyConan(ConanFile):
             destination=self.source_folder, strip_root=True)
 
     def build(self):
-        # apple-clang 14 defaults C++ 20, but it doesn't support C++20 features little
-        # https://www.reddit.com/r/cpp/comments/v6ydea/xcode_now_defaults_to_c20/
-        if Version(self.version) >= "7.00":
-            replace_in_file(self, os.path.join(self.source_folder, "plf_colony.h"),
-            "if __cplusplus > 201704L && ((defined(_LIBCPP_VERSION) && _LIBCPP_VERSION >= 13) || !defined(_LIBCPP_VERSION)) && ((defined(__clang__) && (__clang_major__ >= 13)) || (defined(__GNUC__) && __GNUC__ >= 10) || (!defined(__clang__) && !defined(__GNUC__)))",
-            "if __cplusplus > 201704L && ((defined(_LIBCPP_VERSION) && _LIBCPP_VERSION >= 13) || !defined(_LIBCPP_VERSION)) && (!defined(__APPLE_CC__) && (defined(__clang__) && (__clang_major__ >= 13)) || (defined(__GNUC__) && __GNUC__ >= 10) || (!defined(__clang__) && !defined(__GNUC__)))"
-            )
+        apply_conandata_patches(self)
 
     def package(self):
         copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
