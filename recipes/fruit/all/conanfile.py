997d326d9745e5532f219bd0cc33dc279ef31527
Chris Mc <prince.chrismc@gmail.com>
2023-01-24 14:51:35 -0800

    Update recipes/fruit/all/conanfile.py

--- recipes/fruit/all/conanfile.py
+++ recipes/fruit/all/conanfile.py
@@ -34,7 +34,7 @@ class FruitConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
