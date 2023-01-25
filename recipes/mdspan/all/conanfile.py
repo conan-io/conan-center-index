6978bcea8a4c13ff02769886c8a91dc7039e0403
toge <toge.mail@gmail.com>
2023-01-25 17:17:31 +0900

    mdspan: add version 0.5.0, support conan v2

--- recipes/mdspan/all/conanfile.py
+++ recipes/mdspan/all/conanfile.py
@@ -1,23 +1,22 @@
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import get, copy
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.33.0"
-
+required_conan_version = ">=1.52.0"
 
 class MDSpanConan(ConanFile):
     name = "mdspan"
-    homepage = "https://github.com/kokkos/mdspan"
     description = "Production-quality reference implementation of mdspan"
-    topics = ("multi-dimensional", "array", "span")
     license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
-    settings = "compiler"
+    homepage = "https://github.com/kokkos/mdspan"
+    topics = ("multi-dimensional", "array", "span", "header-only")
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
     def _minimum_cpp_standard(self):
@@ -32,35 +31,46 @@ class MDSpanConan(ConanFile):
             "apple-clang": "5.1"
         }
 
-    def configure(self):
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
+
+    def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+            check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(
             str(self.settings.compiler))
         if not min_version:
-            self.output.warn("{} recipe lacks information about the {} "
-                             "compiler support.".format(
-                                 self.name, self.settings.compiler))
+            self.output.warn(f"{self.ref} recipe lacks information about the {self.settings.compiler} "
+                             "compiler support.")
         else:
-            if tools.Version(self.settings.compiler.version) < min_version:
+            if Version(self.settings.compiler.version) < min_version:
                 raise ConanInvalidConfiguration(
-                    "{} requires C++{} support. "
-                    "The current compiler {} {} does not support it.".format(
-                        self.name, self._minimum_cpp_standard,
-                        self.settings.compiler,
-                        self.settings.compiler.version))
+                    f"{self.ref} requires C++{self._minimum_cpp_standard} support. "
+                    "The current compiler {self.settings.compiler} {self.settings.compiler.version} does not support it.")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy(pattern="*", dst="include", src=os.path.join(self._source_subfolder, "include"))
-        self.copy("*LICENSE", dst="licenses", keep_path=False)
-
-    def package_id(self):
-        self.info.header_only()
+        copy(self, pattern="*LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(
+            self,
+            pattern="*",
+            dst=os.path.join(self.package_folder, "include"),
+            src=os.path.join(self.source_folder, "include"),
+        )
 
     def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
+        self.cpp_info.set_property("cmake_file_name", "mdspan")
+        self.cpp_info.set_property("cmake_target_name", "std::mdspan")
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "mdspan"
         self.cpp_info.filenames["cmake_find_package_multi"] = "mdspan"
         self.cpp_info.names["cmake_find_package"] = "std"
