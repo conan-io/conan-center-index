da37e6a208d77f5732e6bb22604db4a80d9d672b
Linus Klöckner <mail@linus-kloeckner.de>
2023-01-25 04:46:40 +0100

    (#14489) Fix soxr for Apple Silicon architecture
    * Fix soxr for Apple Silicon architecture
    
    * Prepare for Conan v2
    
    * Set test_type explicit for the new test_package
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Use `can_run` instead of `not cross_building` in the test_package
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Remove wrapper CMakeLists.txt
    
    * Declare some CMake policies
    
    * Fix test_package for  Conan v1
    
    * Add patch descriptions and types
    
    * Change todos filename
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * Update minimum required Conan version
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/soxr/all/conanfile.py
+++ recipes/soxr/all/conanfile.py
@@ -1,10 +1,12 @@
-import functools
 import os
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, load, save, rmdir
 from conan.tools.microsoft import msvc_runtime_flag, is_msvc
+from conan.tools.scm import Version
 
 
-required_conan_version = ">=1.45.0"
+required_conan_version = ">=1.53.0"
 
 
 class SoxrConan(ConanFile):
@@ -27,20 +29,9 @@ class SoxrConan(ConanFile):
         "with_openmp": False,
         "with_lsr_bindings": True
     }
-    generators = "cmake"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
 
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -48,48 +39,58 @@ class SoxrConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.cppstd
-        del self.settings.compiler.libcxx
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-            destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
 
-    @functools.lru_cache(1)
-    def _configure_cmake(self):
-        cmake = CMake(self)
+    def generate(self):
+        tc = CMakeToolchain(self)
+        # Honor BUILD_SHARED_LIBS from conan_toolchain (see https://github.com/conan-io/conan/issues/11840) 
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+        if Version(self.version) < "3.21":
+            # silence warning
+            tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0115"] = "OLD"
         if is_msvc(self):
-            cmake.definitions["BUILD_SHARED_RUNTIME"] = msvc_runtime_flag(self) == "MD"
-        cmake.definitions["BUILD_TESTS"] = False
-        cmake.definitions["WITH_OPENMP"] = self.options.with_openmp
-        cmake.definitions["WITH_LSR_BINDINGS"] = self.options.with_lsr_bindings
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+            tc.variables["BUILD_SHARED_RUNTIME"] = msvc_runtime_flag(self) == "MD"
+        # Disable SIMD based resample engines for Apple Silicon architecture
+        if self.settings.os == "Macos" and self.settings.arch == "armv8":
+            tc.variables["WITH_CR32S"] = False
+            tc.variables["WITH_CR64S"] = False
+        tc.variables["BUILD_TESTS"] = False
+        tc.variables["WITH_OPENMP"] = self.options.with_openmp
+        tc.variables["WITH_LSR_BINDINGS"] = self.options.with_lsr_bindings
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def _extract_pffft_license(self):
-        pffft_c = tools.load(os.path.join(self._source_subfolder, "src", "pffft.c"))
+        pffft_c = load(self, os.path.join(self.source_folder, "src", "pffft.c"))
         license_contents = pffft_c[pffft_c.find("/* Copyright")+3:pffft_c.find("modern CPUs.")+13]
-        tools.save(os.path.join(self.package_folder, "licenses", "LICENSE"), license_contents)
+        save(self, os.path.join(self.package_folder, "licenses", "LICENSE"), license_contents)
 
     def package(self):
-        self.copy("LICENCE", dst="licenses", src=self._source_subfolder)
+        copy(self, "LICENCE", dst="licenses", src=self.source_folder)
         self._extract_pffft_license()
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "doc"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "doc"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         # core component
-        self.cpp_info.components["core"].names["pkg_config"] = "soxr"
+        self.cpp_info.components["core"].set_property("pkg_config_name", "soxr")
         self.cpp_info.components["core"].libs = ["soxr"]
         if self.settings.os in ("FreeBSD", "Linux"):
             self.cpp_info.components["core"].system_libs = ["m"]
@@ -108,7 +109,7 @@ class SoxrConan(ConanFile):
             self.cpp_info.components["core"].sharedlinkflags = openmp_flags
         # lsr component
         if self.options.with_lsr_bindings:
-            self.cpp_info.components["lsr"].names["pkg_config"] = "soxr-lsr"
+            self.cpp_info.components["lsr"].set_property("pkg_config_name", "soxr-lsr")
             self.cpp_info.components["lsr"].libs = ["soxr-lsr"]
             if self.settings.os == "Windows" and self.options.shared:
                 self.cpp_info.components["lsr"].defines.append("SOXR_DLL")
