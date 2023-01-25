36d7085d0cc2f9cf5988c7695b0cecb13b4e2b9f
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-01-25 15:01:08 +0000

    google-apis: modernise test package

--- recipes/googleapis/all/test_package/conanfile.py
+++ recipes/googleapis/all/test_package/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.build import can_run
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake_find_package", "CMakeDeps", "VirtualRunEnv"
+    generators = "CMakeDeps", "VirtualRunEnv"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
