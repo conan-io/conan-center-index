7fa1b396190e9c4d49f6ce56cae5d02cefbed89d
Chris Mc <prince.chrismc@gmail.com>
2023-01-25 08:16:34 -0800

    Update recipes/automake/all/conanfile.py

--- recipes/automake/all/conanfile.py
+++ recipes/automake/all/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
 
-required_conan_version = ">=1.55.0"
+required_conan_version = ">=1.53.0"
 
 
 class AutomakeConan(ConanFile):
