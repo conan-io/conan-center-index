47612d2e98ad6e35b5baf3dd1f0cf9c18e9f228b
Francisco Ramírez <franchuti688@gmail.com>
2023-01-25 16:10:57 +0100

    (#15419) flex: Using again official Flex macros
    * Reverted Conan creating Flex cmake config files
    
    * Removed useless variable
    
    * cross_compiling not working with legacy CMake
    
    * wip
    
    * dummy

--- recipes/flex/all/test_v1_package/conanfile.py
+++ recipes/flex/all/test_v1_package/conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, tools, CMake
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package"
+    generators = "cmake"
     test_type = "explicit"
 
     def requirements(self):
@@ -24,26 +24,21 @@ class TestPackageConan(ConanFile):
 
         output = StringIO()
         self.run(f"flex --version", output, run_environment=False)
-        output_str = str(output.getvalue()) 
+        output_str = str(output.getvalue())
         self.output.info("Installed version: {}".format(output_str))
         expected_version = tested_reference_version()
         self.output.info("Expected version: {}".format(expected_version))
         assert_flex_version = "flex {}".format(expected_version)
         assert(assert_flex_version in output_str)
 
-    def _create_cpp_file(self):
-        l_file = os.path.join(self.source_folder, "basic_nr.l")
-        output_file = os.path.join(self.build_folder, "basic_nr.cpp")
-        self.run(f"flex --outfile={output_file} --c++ {l_file}", run_environment=False)
-        assert os.path.exists(output_file)
-
     def build(self):
-        # Let's check flex version installed and create the basic_nr.cpp file
+        # Let's check flex version installed
         self._assert_expected_version()
-        self._create_cpp_file()
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        # FIXME: CMake legacy is not working as expected when cross-compiling
+        if not tools.cross_building(self, skip_x64_x86=True):
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
 
     def test(self):
         if not tools.cross_building(self, skip_x64_x86=True):
