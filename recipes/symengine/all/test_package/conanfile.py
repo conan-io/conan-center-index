1f66bc8c4a5675bd8a0b3724bbf378d2add355b6
Alec Edgington <alec.edgington@cambridgequantum.com>
2023-01-27 07:59:06 +0000

    Set CMAKE_CXX_STANDARD using CMakeToolChain.

--- recipes/symengine/all/test_package/conanfile.py
+++ recipes/symengine/all/test_package/conanfile.py
@@ -1,12 +1,12 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "VirtualRunEnv"
     test_type = "explicit"
 
     def layout(self):
@@ -15,9 +15,14 @@ class TestPackageConan(ConanFile):
     def requirements(self):
         self.requires(self.tested_reference_str)
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_CXX_STANDARD"] = "11"
+        tc.generate()
+
     def build(self):
         cmake = CMake(self)
-        cmake.configure(variables={"CMAKE_CXX_STANDARD": "11"})
+        cmake.configure()
         cmake.build()
 
     def test(self):
