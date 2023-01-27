c1eb2bac36c3b2439851cf3e4fe6717c3edf2215
Sneder89 <45610045+Sneder89@users.noreply.github.com>
2023-01-27 13:26:27 +0100

    (#14490) [doxygen] Add Version 1.9.5 and bump dependency versions
    * add 1.9.5 entries and bump dependeny versions
    
    * Update conanfile.py
    
    * remove unnecessary patch files
    
    * Update conanfile.py
    
    * add m4
    
    * add v1 test package
    
    * remove deps generator
    
    * return to old codebase
    
    * apply patches
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * remove weird cmake configure function
    
    * Update conandata.yml
    
    * remove CONAN_PKG
    
    * remove unused patches
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * use visualmstudio 16
    
    * Update conanfile.py
    
    * remove version 1.9.5
    
    * Update conanfile.py
    
    use 2.0 cmake tools
    
    * use patch file
    
    * Update conanfile.py
    
    * remove unused imports
    
    * make the package v2 compatible
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update recipes/doxygen/all/conanfile.py
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Update conanfile.py
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/doxygen/all/test_v1_package/conanfile.py
+++ recipes/doxygen/all/test_v1_package/conanfile.py
@@ -1,29 +1,20 @@
-from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
-from conan.errors import ConanException
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanException
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
-    test_type = "explicit"
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def layout(self):
-        cmake_layout(self)
+    generators = "cmake"
 
     def build(self):
-        if can_run(self):
+        if not tools.cross_building(self, skip_x64_x86=True):
             cmake = CMake(self)
             cmake.configure()
             cmake.build()
 
     def test(self):
-        if can_run(self):
+        if not tools.cross_building(self, skip_x64_x86=True):
             if not os.path.isdir(os.path.join(self.build_folder, "html")):
                 raise ConanException("doxygen did not create html documentation directory")
 
