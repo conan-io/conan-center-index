e58014f26e5dcb9deed5c2917556d1c386b96c1b
toge <toge.mail@gmail.com>
2023-01-29 20:22:29 +0900

    stduuid: add version 1.2.3

--- recipes/stduuid/all/conanfile.py
+++ recipes/stduuid/all/conanfile.py
@@ -12,7 +12,7 @@ required_conan_version = ">=1.50.0"
 class StduuidConan(ConanFile):
     name = "stduuid"
     description = "A C++17 cross-platform implementation for UUIDs"
-    topics = ("uuid", "guid")
+    topics = ("uuid", "guid", "header-only")
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/mariusbancila/stduuid"
@@ -44,7 +44,7 @@ class StduuidConan(ConanFile):
 
     def requirements(self):
         if not self.options.with_cxx20_span:
-            self.requires("ms-gsl/3.1.0", transitive_headers=True)
+            self.requires("ms-gsl/4.0.0", transitive_headers=True)
         if self.settings.os == "Linux" and Version(self.version) <= "1.0":
             self.requires("libuuid/1.0.3", transitive_headers=True, transitive_libs=True)
 
