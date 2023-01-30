087a411d8f23b505dedf7122392e1d577e6c5571
toge <toge.mail@gmail.com>
2023-01-31 01:44:21 +0900

    update 1.5.10

--- recipes/trantor/all/conanfile.py
+++ recipes/trantor/all/conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
 import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 class TrantorConan(ConanFile):
     name = "trantor"
