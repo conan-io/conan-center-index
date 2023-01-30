97721f5322342512b93167c19bc16ed3db845dc7
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-01-30 12:46:55 +0000

    (#15450) googleapis: use modern CMake integrations
    * googleapis: use modern CMake integrations
    
    * Add package_type attribute
    
    * Fix applying patches
    
    * google-apis: modernise test package
    
    * googleapis: use resdirs in components and use shorten cmake target names when building
    
    * Apply conandata patches

--- recipes/googleapis/all/test_package/conanfile.py
+++ recipes/googleapis/all/test_package/conanfile.py
@@ -1,13 +1,12 @@
 import os
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain
-from conan.tools.build import cross_building as tools_cross_building
-from conan.tools.layout import cmake_layout
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+from conan.tools.build import can_run
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake_find_package", "CMakeDeps", "VirtualRunEnv"
+    generators = "CMakeDeps", "VirtualRunEnv"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
@@ -25,5 +24,5 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools_cross_building(self):
+        if can_run(self):
             self.run(os.path.join(self.cpp.build.bindirs[0], "test_package"), env="conanrun")
