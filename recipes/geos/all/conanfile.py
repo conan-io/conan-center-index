b65ad5f586876829be819cc9bd7d0412b892d2d1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-01-31 12:18:44 +0100

    modernize more

--- recipes/geos/all/conanfile.py
+++ recipes/geos/all/conanfile.py
@@ -1,12 +1,11 @@
 from conan import ConanFile
-from conan.tools.build import check_min_cppstd
+from conan.tools.build import check_min_cppstd, stdcpp_library
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir
 from conan.tools.scm import Version
-from conans import tools as tools_legacy
 import os
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=1.54.0"
 
 
 class GeosConan(ConanFile):
@@ -45,16 +44,15 @@ class GeosConan(ConanFile):
         if self.options.shared:
             self.options.rm_safe("fPIC")
 
-    def validate(self):
-        if self.info.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, 11)
-
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 11)
+
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -108,9 +106,9 @@ class GeosConan(ConanFile):
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["geos_cpp"].system_libs.append("m")
         if not self.options.shared:
-            stdcpp_library = tools_legacy.stdcpp_library(self)
-            if stdcpp_library:
-                self.cpp_info.components["geos_cpp"].system_libs.append(stdcpp_library)
+            libcxx = stdcpp_library(self)
+            if libcxx:
+                self.cpp_info.components["geos_cpp"].system_libs.append(libcxx)
         self.cpp_info.components["geos_cpp"].requires = ["geos_cxx_flags"]
 
         # GEOS::geos_c
