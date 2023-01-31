08deb37f3475dbdd8f3a21569edc65f479886aa4
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-01-31 14:20:08 +0100

    minor change

--- recipes/libwebp/all/conanfile.py
+++ recipes/libwebp/all/conanfile.py
@@ -49,8 +49,7 @@ class LibwebpConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
