0badc30a5232548f7fe77188c914d98f41f39b6a
toge <toge.mail@gmail.com>
2023-02-01 00:58:36 +0900

    use f-string

--- recipes/ctre/all/conanfile.py
+++ recipes/ctre/all/conanfile.py
@@ -33,21 +33,21 @@ class CtreConan(ConanFile):
             check_min_cppstd(self, "17")
         if is_msvc(self):
             if compiler_version < "15":
-                raise ConanInvalidConfiguration("{}/{} doesn't support MSVC < 15".format(self.name, self.version))
+                raise ConanInvalidConfiguration(f"{self.ref} doesn't support MSVC < 15")
             if ctre_version >= "3.7" and compiler_version < 16:
-                raise ConanInvalidConfiguration("{}/{} doesn't support MSVC < 16".format(self.name, self.version))
+                raise ConanInvalidConfiguration(f"{self.ref} doesn't support MSVC < 16")
         elif compiler == "gcc" and compiler_version < min_gcc:
-            raise ConanInvalidConfiguration("{}/{} doesn't support gcc < {}".format(self.name, self.version, min_gcc))
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support gcc < {min_gcc}")
         elif compiler == "clang" and compiler_version < "6.0":
-            raise ConanInvalidConfiguration("{}/{} doesn't support clang < 6.0".format(self.name, self.version))
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support clang < 6.0")
         elif compiler == "apple-clang":
             if compiler_version < "10.0":
-                raise ConanInvalidConfiguration("{}/{} doesn't support Apple clang < 10.0".format(self.name, self.version))
+                raise ConanInvalidConfiguration(f"{self.ref} doesn't support Apple clang < 10.0")
             # "library does not compile with (at least) Xcode 12.0-12.4"
             # https://github.com/hanickadot/compile-time-regular-expressions/issues/188
             # it's also occurred in Xcode 13.
             if ctre_version.major == "3" and ctre_version.minor == "4" and compiler_version >= "12":
-                raise ConanInvalidConfiguration("{}/{} doesn't support Apple clang".format(self.name, self.version))
+                raise ConanInvalidConfiguration(f"{self.ref} doesn't support Apple clang")
 
     def package_id(self):
         self.info.clear()
