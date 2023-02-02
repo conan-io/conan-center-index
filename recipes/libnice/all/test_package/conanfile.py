d98c95be4c387223fec5384852ef4d3bfe3505af
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2023-02-02 20:21:20 +0800

    libnice: change test packages to use CMake instead of Meson

--- recipes/libnice/all/test_package/conanfile.py
+++ recipes/libnice/all/test_package/conanfile.py
@@ -1,32 +1,27 @@
 import os
 from conan import ConanFile
-from conan.tools.meson import Meson
-from conan.tools.layout import basic_layout
+from conan.tools.cmake import CMake
+from conan.tools.layout import cmake_layout
 from conan.tools.build import cross_building
 
 
 class LibniceTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "PkgConfigDeps", "MesonToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
-        
-    def build_requirements(self):
-        self.tool_requires("meson/0.64.1")
-        self.tool_requires("pkgconf/1.9.3")
-        self.tool_requires("meson/0.64.1")
 
     def build(self):
-        meson = Meson(self)
-        meson.configure()
-        meson.build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def layout(self):
-        basic_layout(self)
+        cmake_layout(self)
 
     def test(self):
         if not cross_building(self, skip_x64_x86=True):
             cmd = os.path.join(self.cpp.build.bindirs[0], "example")
-            self.run(cmd, run_environment=True, env="conanrun")
+            self.run(cmd, env="conanrun")
--- recipes/libnice/all/test_v1_package/conanfile.py
+++ recipes/libnice/all/test_v1_package/conanfile.py
@@ -1,24 +1,19 @@
 import os
 from conan import ConanFile
-from conans import Meson
+from conans import CMake
 from conan.tools.build import cross_building
 
 
 class LibniceTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "pkg_config"
-        
-    def build_requirements(self):
-        self.tool_requires("meson/0.64.1")
-        self.tool_requires("pkgconf/1.9.3")
-        self.tool_requires("meson/0.64.1")
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
-        meson = Meson(self)
-        meson.configure(build_folder="bin", source_folder="../test_package")
-        meson.build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
-        if not cross_building(self, skip_x64_x86=True):
-            cmd = os.path.join("bin", "example")
-            self.run(cmd, run_environment=True)
+        if not cross_building(self):
+            bin_path = os.path.join("bin", "example")
+            self.run(bin_path, run_environment=True)
