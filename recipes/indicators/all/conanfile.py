2093ec4efd63340afaed357b698841efd2a6ab80
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-02-03 16:02:36 +0100

    Update recipes/indicators/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/indicators/all/conanfile.py
+++ recipes/indicators/all/conanfile.py
@@ -7,6 +7,7 @@ from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 
+required_conan_version = ">=1.53.0"
 
 class IndicatorsConan(ConanFile):
     name = "indicators"
