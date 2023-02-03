d4b6ddcfcbb81d4377ffc017ebed5c1816ed91f0
Andrew Marshall <planetmarshalluk@gmail.com>
2023-02-03 11:35:38 +0000

    [glibmm] reorder methods

--- recipes/glibmm/all/conanfile.py
+++ recipes/glibmm/all/conanfile.py
@@ -5,7 +5,7 @@ import shutil
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.build import check_min_cppstd, cross_building
+from conan.tools.build import check_min_cppstd
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import (
     apply_conandata_patches,
@@ -50,6 +50,33 @@ class GlibmmConan(ConanFile):
     def _giomm_lib(self):
         return f"giomm-{self._abi_version}"
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        if self.options.shared:
+            self.options["glib"].shared = True
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("glib/2.75.2")
+        if self._abi_version == "2.68":
+            self.requires("libsigcpp/3.0.7")
+        else:
+            self.requires("libsigcpp/2.10.8")
+
+    def package_id(self):
+        if not self.options["glib"].shared:
+            self.info.requires["glib"].full_package_mode()
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             if self._abi_version == "2.68":
@@ -65,31 +92,18 @@ class GlibmmConan(ConanFile):
         if self.dependencies["glib"].options.shared and is_msvc_static_runtime(self):
             raise ConanInvalidConfiguration("Linking shared glib with the MSVC static runtime is not supported")
 
-    def export_sources(self):
-        export_conandata_patches(self)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def layout(self):
-        basic_layout(self, src_folder="src")
-
     def build_requirements(self):
         self.tool_requires("meson/1.0.0")
-        self.tool_requires("pkgconf/1.9.3")
-
-    def requirements(self):
-        self.requires("glib/2.75.2")
-        if self._abi_version == "2.68":
-            self.requires("libsigcpp/3.0.7")
-        else:
-            self.requires("libsigcpp/2.10.8")
+        if not self.conf.get("tools.gnu:pkg_config", check_type=str):
+            self.tool_requires("pkgconf/1.9.3")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True, destination=self.source_folder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+
         deps = PkgConfigDeps(self)
         deps.generate()
 
@@ -101,9 +115,6 @@ class GlibmmConan(ConanFile):
             "default_library": "shared" if self.options.shared else "static"
         })
         tc.generate()
-
-        env = VirtualBuildEnv(self)
-        env.generate()
 
     def _patch_sources(self):
         apply_conandata_patches(self)
@@ -129,12 +140,6 @@ class GlibmmConan(ConanFile):
             # https://developercommunity.visualstudio.com/t/error-c2760-in-combaseapih-with-windows-sdk-81-and/185399
             replace_in_file(self, meson_build, "cpp_std=c++", "cpp_std=vc++")
 
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        if self.options.shared:
-            self.options["glib"].shared = True
-
     def build(self):
         self._patch_sources()
         meson = Meson(self)
@@ -144,9 +149,12 @@ class GlibmmConan(ConanFile):
     def package(self):
         def rename_msvc_static_libs():
             lib_folder = os.path.join(self.package_folder, "lib")
-            rename(self, os.path.join(lib_folder, f"libglibmm-{self._abi_version}.a"), os.path.join(lib_folder, f"{self._glibmm_lib}.lib"))
-            rename(self, os.path.join(lib_folder, f"libgiomm-{self._abi_version}.a"), os.path.join(lib_folder, f"{self._giomm_lib}.lib"))
-            rename(self, os.path.join(lib_folder, f"libglibmm_generate_extra_defs-{self._abi_version}.a"), os.path.join(lib_folder, f"glibmm_generate_extra_defs-{self._abi_version}.lib"))
+            rename(self, os.path.join(lib_folder, f"libglibmm-{self._abi_version}.a"),
+                   os.path.join(lib_folder, f"{self._glibmm_lib}.lib"))
+            rename(self, os.path.join(lib_folder, f"libgiomm-{self._abi_version}.a"),
+                   os.path.join(lib_folder, f"{self._giomm_lib}.lib"))
+            rename(self, os.path.join(lib_folder, f"libglibmm_generate_extra_defs-{self._abi_version}.a"),
+                   os.path.join(lib_folder, f"glibmm_generate_extra_defs-{self._abi_version}.lib"))
 
         meson = Meson(self)
         meson.install()
@@ -187,7 +195,3 @@ class GlibmmConan(ConanFile):
         self.cpp_info.components[giomm_component].libs = [giomm_component]
         self.cpp_info.components[giomm_component].includedirs = [os.path.join("include", giomm_component)]
         self.cpp_info.components[giomm_component].requires = [glibmm_component, "glib::gio-2.0"]
-
-    def package_id(self):
-        if not self.options["glib"].shared:
-            self.info.requires["glib"].full_package_mode()
