676b25734859c079a73496e30d6e3fa83c164f6c
Andrew Marshall <planetmarshalluk@gmail.com>
2023-02-04 11:14:11 +0000

    [cairomm] update test packages

--- recipes/cairomm/all/test_v1_package/conanfile.py
+++ recipes/cairomm/all/test_v1_package/conanfile.py
@@ -1,18 +1,22 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, Meson
+from conan.tools.build import cross_building
 import os
 
 
-class TestPackageConan(ConanFile):
+class TestPackageV1Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "pkg_config"
+
+    def build_requirements(self):
+        self.build_requires("meson/0.63.3")
+        self.build_requires("pkgconf/1.9.3")
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        meson = Meson(self)
+        meson.configure(build_folder="bin", source_folder="../test_package")
+        meson.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
-
