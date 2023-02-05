3391effeb3454399d605ac47fe8612797097a390
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-02-05 14:50:42 +0100

    conan v2 support

--- recipes/apriltag/all/conanfile.py
+++ recipes/apriltag/all/conanfile.py
@@ -1,6 +1,10 @@
-from conans import ConanFile, tools, CMake
+from conan import ConanFile
 from conans.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 import os
+
+required_conan_version = ">=1.54.0"
 
 
 class ApriltagConan(ConanFile):
@@ -8,9 +12,12 @@ class ApriltagConan(ConanFile):
     description = ("AprilTag is a visual fiducial system, useful for a wide variety of tasks \
                     including augmented reality, robotics, and camera calibration")
     homepage = "https://april.eecs.umich.edu/software/apriltag"
-    topics = ("conan", "apriltag", "robotics")
+    topics = ("robotics")
     license = "BSD-2-Clause"
     url = "https://github.com/conan-io/conan-center-index"
+
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
@@ -19,16 +26,9 @@ class ApriltagConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    settings = "os", "arch", "compiler", "build_type"
 
-    generators = "cmake"
-    exports_sources = "CMakeLists.txt", "patches/*"
-
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -36,45 +36,43 @@ class ApriltagConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def validate(self):
         if self.settings.os != "Linux":
             raise ConanInvalidConfiguration("Apriltag officially supported only on Linux")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.configure()
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE.md", src=self._source_subfolder, dst="licenses")
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "bin"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "apriltag"
-        self.cpp_info.names["cmake_find_package_multi"] = "apriltag"
-
+        self.cpp_info.set_property("cmake_file_name", "apriltag")
+        self.cpp_info.set_property("cmake_target_name", "apriltag::apriltag")
+        self.cpp_info.set_property("pkg_config_name", "apriltag")
         self.cpp_info.libs = ["apriltag"]
+        self.cpp_info.includedirs.append(os.path.join("include", "apriltag"))
         if self.settings.os == "Linux":
             self.cpp_info.system_libs = ["m", "pthread"]
-        self.cpp_info.includedirs.append(os.path.join("include","apriltag"))
