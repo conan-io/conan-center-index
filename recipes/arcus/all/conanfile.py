7a040015441d9e2ef65ea87970a59bcac1dc5b11
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-02-05 19:29:45 +0100

    add libm to system libs

--- recipes/arcus/all/conanfile.py
+++ recipes/arcus/all/conanfile.py
@@ -103,7 +103,7 @@ class ArcusConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "Arcus")
         self.cpp_info.libs = ["Arcus"]
         if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs.append("pthread")
+            self.cpp_info.system_libs.extend(["m", "pthread"])
         elif self.settings.os == "Windows":
             self.cpp_info.system_libs.append("ws2_32")
 
