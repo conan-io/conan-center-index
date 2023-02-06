c1fc4d0e37db7eaca12c5b460bd6661fb27e7cb3
toge <toge.mail@gmail.com>
2023-02-06 23:40:33 +0900

    remove unused import

--- recipes/pistache/all/conanfile.py
+++ recipes/pistache/all/conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime, is_msvc
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file, collect_libs
 from conan.tools.build import check_min_cppstd
 from conan.tools.apple import fix_apple_shared_install_name
