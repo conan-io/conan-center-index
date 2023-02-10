f7ec4b623cf9355382568889ee69a4fa40076ab6
Stella Smith <40411082+StellaSmith@users.noreply.github.com>
2023-02-10 15:07:32 -0300

    (#15314) sol2: conan v2 support
    * sol2: conan v2 support
    
    * Fix missing attribute warn
    
    * Fix include in test_package
    
    * Remove older 3.2.x releases
    
    * Fix max lua allowed
    
    * Remove older 3.2.x releases pt2
    
    * Apply suggested changes from review
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    
    * review
    
    - add layout
    - more elegant check of min compiler versions
    - sort methods by order of execution
    - decrease min conan version
    
    * empty bindirs & libdirs
    
    ---------
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/sol2/all/test_package/test_package.cpp
+++ recipes/sol2/all/test_package/test_package.cpp
@@ -1,9 +1,14 @@
-
-#include "sol_test.hpp"
 #include <cstdlib>
 
-int main() {
+#include <limits>
 
+#ifdef SOL2_V3
+#include <sol/sol.hpp>
+#else
+#include <sol.hpp>
+#endif
+
+int main() {
 	sol::state lua;
 
 	auto bob_table = lua.create_table("bob");
@@ -13,9 +18,8 @@ int main() {
 	int is_not_set = bob_table.get_or("is_not_set", 22);
 
 	if (is_set != 42 || is_not_set != 22) {
-        return EXIT_FAILURE ;
+        return EXIT_FAILURE;
     }
 	
-    return EXIT_SUCCESS ;
-
+    return EXIT_SUCCESS;
 }
