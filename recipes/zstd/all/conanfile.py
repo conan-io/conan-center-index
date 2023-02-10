9ba115d16c0a1ea25871d484bfc5ce11e3cd5b9e
toge <toge.mail@gmail.com>
2023-02-10 14:58:06 +0900

    zstd: add version 1.5.4

--- recipes/zstd/all/conanfile.py
+++ recipes/zstd/all/conanfile.py
@@ -44,8 +44,7 @@ class ZstdConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
