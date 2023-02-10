4f0b1b42883f7737411eca95c876cd9f59d4767a
Chris Mc <prince.chrismc@gmail.com>
2023-02-09 19:54:05 -0800

    Update recipes/coin-clp/all/conanfile.py

--- recipes/coin-clp/all/conanfile.py
+++ recipes/coin-clp/all/conanfile.py
@@ -57,12 +57,8 @@ class CoinClpConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        if self.version == "1.17.6":
-            self.requires("coin-utils/2.11.4")
-            self.requires("coin-osi/0.108.6")
-        else:
-            self.requires("coin-utils/2.11.6")
-            self.requires("coin-osi/0.108.7")
+        self.requires("coin-utils/2.11.6")
+        self.requires("coin-osi/0.108.7")
 
     def validate(self):
         if self.settings.os == "Windows" and self.options.shared:
