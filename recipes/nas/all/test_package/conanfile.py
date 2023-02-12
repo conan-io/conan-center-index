ed75ce5f6789cc7cfbbf43cca3a2eeb20551e868
Chris Mc <christopherm@jfrog.com>
2023-02-11 20:19:55 -0800

    fix test package paths

--- recipes/nas/all/test_package/conanfile.py
+++ recipes/nas/all/test_package/conanfile.py
@@ -6,7 +6,6 @@ from conan.tools.cmake import CMake, cmake_layout
 
 class NasTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
 
@@ -23,5 +22,5 @@ class NasTestConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
             self.run(bin_path, env="conanrun")
