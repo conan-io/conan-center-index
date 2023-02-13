5aadd3639294707be46353e5b05bc3118c6a6043
toge <toge.mail@gmail.com>
2023-02-13 15:08:56 +0900

    remove unused import
    Co-authored-by: Chris Mc <christopherm@jfrog.com>

--- recipes/paho-mqtt-c/all/test_package/conanfile.py
+++ recipes/paho-mqtt-c/all/test_package/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
 
 class TestPackageConan(ConanFile):
