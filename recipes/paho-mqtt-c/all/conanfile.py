3e206a9f0e263fb3b31c1d02cc8d04851afbcae2
toge <toge.mail@gmail.com>
2023-02-13 15:07:47 +0900

    remove destination arg in `self.get()`
    Co-authored-by: Chris Mc <christopherm@jfrog.com>

--- recipes/paho-mqtt-c/all/conanfile.py
+++ recipes/paho-mqtt-c/all/conanfile.py
@@ -65,7 +65,7 @@ class PahoMqttcConan(ConanFile):
         del self.info.options.samples
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
