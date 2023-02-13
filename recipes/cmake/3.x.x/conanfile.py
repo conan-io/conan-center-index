d3f557569a81ea252f37a0c16f6968d51ea3eb7a
Michael Keck <git@cr0ydon.com>
2023-02-13 20:09:30 +0100

    (#15864) cmake: switch to official release tarballs again + openssl update
    * cmake: switch to official release tarballs again
    
    * cmake: update to openssl/1.1.1t

--- recipes/cmake/3.x.x/conanfile.py
+++ recipes/cmake/3.x.x/conanfile.py
@@ -36,7 +36,7 @@ class CMakeConan(ConanFile):
 
     def requirements(self):
         if self.options.with_openssl:
-            self.requires("openssl/1.1.1s")
+            self.requires("openssl/1.1.1t")
 
     def validate(self):
         if self.settings.os == "Windows" and self.options.bootstrap:
