411ac24f6179f07f66f0914f0831417b40b1a0f3
toge <toge.mail@gmail.com>
2023-02-13 15:06:23 +0900

    remove unused import
    Co-authored-by: Chris Mc <christopherm@jfrog.com>

--- recipes/paho-mqtt-c/all/conanfile.py
+++ recipes/paho-mqtt-c/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, replace_in_file
 from conan.tools.scm import Version
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
