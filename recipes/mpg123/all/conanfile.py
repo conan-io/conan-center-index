2725bc5da1457bd8d22e588c1967c23cc0b1f9b8
Uilian Ries <uilianries@gmail.com>
2023-02-14 10:32:09 +0100

    Fix cmake target per module
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/mpg123/all/conanfile.py
+++ recipes/mpg123/all/conanfile.py
@@ -180,19 +180,21 @@ class Mpg123Conan(ConanFile):
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "mpg123")
-        self.cpp_info.set_property("cmake_target_name", "MPG123::libmpg123")
 
         self.cpp_info.components["libmpg123"].libs = ["mpg123"]
         self.cpp_info.components["libmpg123"].set_property("pkg_config_name", "libmpg123")
+        self.cpp_info.components["libmpg123"].set_property("cmake_target_name", "MPG123::libmpg123")
         if self.settings.os == "Windows" and self.options.shared:
             self.cpp_info.components["libmpg123"].defines.append("LINK_MPG123_DLL")
 
         self.cpp_info.components["libout123"].libs = ["out123"]
         self.cpp_info.components["libout123"].set_property("pkg_config_name", "libout123")
+        self.cpp_info.components["libmpg123"].set_property("cmake_target_name", "MPG123::libout123")
         self.cpp_info.components["libout123"].requires = ["libmpg123"]
 
         self.cpp_info.components["libsyn123"].libs = ["syn123"]
         self.cpp_info.components["libsyn123"].set_property("pkg_config_name", "libsyn123")
+        self.cpp_info.components["libmpg123"].set_property("cmake_target_name", "MPG123::libsyn123")
         self.cpp_info.components["libsyn123"].requires = ["libmpg123"]
 
         if self.settings.os == "Linux":
