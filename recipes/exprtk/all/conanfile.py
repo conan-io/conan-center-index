7b5c3c907db4e5e6a9dddb78b57a4c288935d696
toge <toge.mail@gmail.com>
2023-02-14 10:05:54 +0900

    exprtk: add version 0.0.2, support conan v2

--- recipes/exprtk/all/conanfile.py
+++ recipes/exprtk/all/conanfile.py
@@ -1,42 +1,52 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, load, save
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
-from conans import ConanFile, tools
 
-required_conan_version = ">=1.33.0"
-
+required_conan_version = ">=1.52.0"
 
 class ExprTkConan(ConanFile):
     name = "exprtk"
     description = "C++ Mathematical Expression Parsing And Evaluation Library ExprTk"
     license = ("MIT")
-    topics = ("exprtk", "cpp", "math", "mathematics", "parser", "lexer", "numerical")
-    homepage = "https://www.partow.net/programming/exprtk/index.html"
     url = "https://github.com/conan-io/conan-center-index"
-    settings = "compiler"
+    homepage = "https://www.partow.net/programming/exprtk/index.html"
+    topics = ("math", "mathematics", "parser", "lexer", "numerical", "header-only")
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 11)
-
-    def package_id(self):
-        self.info.header_only()
+            check_min_cppstd(self, 11)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def _extract_license(self):
-        exprtk_header_file = "exprtk.hpp"
-        file = os.path.join(self.source_folder, self._source_subfolder, exprtk_header_file)
-        file_content = tools.load(file)
+        source_file = os.path.join(self.source_folder, self.source_folder, "exprtk.hpp")
+        file_content = load(self, source_file)
         license_end = "/MIT                        *"
         license_contents = file_content[2:file_content.find(license_end) + len(license_end)]
-        tools.save(os.path.join(self.package_folder, "licenses", "LICENSE"), license_contents)
+        save(self, os.path.join(self.package_folder, "licenses", "LICENSE"), license_contents)
 
     def package(self):
         self._extract_license()
-        self.copy("exprtk.hpp", dst="include" , src=self._source_subfolder)
+        copy(
+            self,
+            pattern="exprtk.hpp",
+            dst=os.path.join(self.package_folder, "include"),
+            src=self.source_folder,
+        )
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
