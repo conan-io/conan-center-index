5e629dbf603001937612c9d2ac320ea8b88b5f46
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-02-14 10:10:52 +0100

    Initial sketch for xorg-macros v2 migration

--- recipes/xorg-macros/all/conanfile.py
+++ recipes/xorg-macros/all/conanfile.py
@@ -1,76 +1,70 @@
 from conan import ConanFile
-from conan.tools import files
-from conan.tools import build
-from conans import AutoToolsBuildEnvironment, tools
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import unix_path, unix_path_package_info_legacy
 import os
 import textwrap
 
-required_conan_version = ">=1.50.2"
+required_conan_version = ">=1.53.2"
 
 
 class XorgMacrosConan(ConanFile):
     name = "xorg-macros"
     description = "GNU autoconf macros shared across X.Org projects"
-    topics = ("conan", "autoconf", "macros", "build", "system", "m4")
+    topics = ("autoconf", "macros", "build", "system", "m4")
     license = "MIT"
     homepage = "https://gitlab.freedesktop.org/xorg/util/macros"
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os"
 
-    _autotools = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def layout(self):
+        basic_layout(self)
 
     @property
     def _settings_build(self):
         return self.settings_build if hasattr(self, "settings_build") else self.settings
 
     def export_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        export_conandata_patches(self)
 
     def source(self):
-        files.get(self, **self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def build_requirements(self):
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", check_type=str):
             self.build_requires("msys2/cci.latest")
         self.build_requires("automake/1.16.3")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     @property
     def _datarootdir(self):
         return os.path.join(self.package_folder, "bin", "share")
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=self._settings_build.os == "Windows")
+    def generate(self):
+        tc = AutotoolsToolchain(self)
         conf_args = [
-            f"--datarootdir={tools.unix_path(self._datarootdir)}",
+            f"--datarootdir={unix_path(self._datarootdir)}",
         ]
-        self._autotools.configure(args=conf_args, configure_dir=self._source_subfolder)
-        return self._autotools
+        tc.configure_args(args=conf_args)
 
     def build(self):
-        files.apply_conandata_patches(self)
+        apply_conandata_patches(self)
         with files.chdir(self, self._source_subfolder):
             self.run("{} -fiv".format(tools.get_env("AUTORECONF")), run_environment=True, win_bash=self._settings_build.os == "Windows")
-        autotools = self._configure_autotools()
+        autotools = Autotools(self)
+        autotools.configure()
         autotools.make()
 
     def package(self):
-        self.copy("COPYING", src=self._source_subfolder, dst="licenses")
-        autotools = self._configure_autotools()
+        copy(self, "COPYING", src=self.source_folder, dst="licenses")
+        autotools = Autotools(self)
         autotools.install()
 
-        files.rmdir(self, os.path.join(self._datarootdir, "pkgconfig"))
-        files.rmdir(self, os.path.join(self._datarootdir, "util-macros"))
+        rmdir(self, os.path.join(self._datarootdir, "pkgconfig"))
+        rmdir(self, os.path.join(self._datarootdir, "util-macros"))
 
     def package_info(self):
         self.cpp_info.names["pkg_config"] = "xorg-macros"
@@ -87,6 +81,6 @@ class XorgMacrosConan(ConanFile):
             name="util-macros",
         ))
 
-        aclocal = tools.unix_path(os.path.join(self._datarootdir, "aclocal"))
+        aclocal = unix_path_package_info_legacy(os.path.join(self._datarootdir, "aclocal"))
         self.output.info("Appending AUTOMAKE_CONAN_INCLUDES environment variable: {}".format(aclocal))
         self.env_info.AUTOMAKE_CONAN_INCLUDES.append(aclocal)
