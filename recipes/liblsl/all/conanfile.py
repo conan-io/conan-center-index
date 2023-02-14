6fe5e4b52091d58caa4f7cf77a2d7d0c96202f70
chausner <chausner@users.noreply.github.com>
2023-02-14 22:46:41 +0100

    Fix license file copy

--- recipes/liblsl/all/conanfile.py
+++ recipes/liblsl/all/conanfile.py
@@ -80,7 +80,7 @@ class LiblslConan(ConanFile):
         rm(self, "lslver*", os.path.join(self.package_folder, "bin"))
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
-        copy(self, "LICENSE", src=self.source_folder, dst="licenses")
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
 
         self._create_cmake_module_alias_targets(
             os.path.join(self.package_folder, self._module_file_rel_path),
