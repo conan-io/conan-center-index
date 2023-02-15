caf23d63b96f9ad4f7a57efb7c31f25dcd11f08a
chausner <chausner@users.noreply.github.com>
2023-02-15 20:41:26 +0100

    Fix CMake module file name

--- recipes/liblsl/all/conanfile.py
+++ recipes/liblsl/all/conanfile.py
@@ -84,7 +84,7 @@ class LiblslConan(ConanFile):
 
         self._create_cmake_module_alias_targets(
             os.path.join(self.package_folder, self._module_file_rel_path),
-            {"lsl": "LSL::lsl"}
+            {"LSL": "LSL::lsl"}
         )
 
     def _create_cmake_module_alias_targets(self, module_file, targets):
