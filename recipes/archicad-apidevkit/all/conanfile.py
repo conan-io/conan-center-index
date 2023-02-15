f551238393e88f6e1b572c31a2d0b3635daa9e12
Andras Kiss <anerbota@gmail.com>
2023-02-15 15:28:47 +0200

    is_msvc instead of Visual Studio

--- recipes/archicad-apidevkit/all/conanfile.py
+++ recipes/archicad-apidevkit/all/conanfile.py
@@ -32,7 +32,7 @@ class ArchicadApidevkitConan(ConanFile):
             if not str(self.settings.arch) in ("x86_64"):
                 raise ConanInvalidConfiguration(
                     f"{self.ref} is not supported yet.")
-        if self.settings.compiler == "Visual Studio" and Version(self.settings.compiler.version) < "16":
+        if is_msvc(self) and Version(self.settings.compiler.version) < "16":
             raise ConanInvalidConfiguration(
                 "This recipe does not support this compiler version")
 
