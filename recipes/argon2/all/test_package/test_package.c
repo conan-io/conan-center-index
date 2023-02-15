9739889143f258d0c6640556c1e9ff9bd186c614
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-02-15 17:08:09 +0100

    (#15744) argon2: conan v2 support

--- recipes/argon2/all/test_package/test_package.c
+++ recipes/argon2/all/test_package/test_package.c
@@ -5,7 +5,7 @@ int main() {
     const char *salt = "SALTSTR";
     const char *pwd = "password";
 
-    char encoded[97] = {};
+    char encoded[97];
 
     // high-level API
     argon2id_hash_encoded(2, 1<<16, 2, pwd, sizeof(pwd), salt, sizeof(salt), 32, encoded, sizeof(encoded));
