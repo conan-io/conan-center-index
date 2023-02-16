c0b4e61ee1b2a1695137778b2a0ed5f0a723014a
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-02-16 17:54:15 +0100

    Fix test

--- recipes/xorg-macros/all/test_package/conanfile.py
+++ recipes/xorg-macros/all/test_package/conanfile.py
@@ -2,6 +2,7 @@ from conan import ConanFile
 from conan.tools.gnu import AutotoolsToolchain, Autotools
 from conan.tools.layout import basic_layout
 
+
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     test_type = "explicit"
@@ -12,7 +13,7 @@ class TestPackageConan(ConanFile):
         return self.settings_build if hasattr(self, "settings_build") else self.settings
 
     def build_requirements(self):
-        self.tool_requires(self.tested_reference_str, run=True)
+        self.tool_requires(self.tested_reference_str)
         self.build_requires("automake/1.16.5")
         if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", check_type=str):
             self.tool_requires("msys2/cci.latest")
@@ -23,7 +24,6 @@ class TestPackageConan(ConanFile):
     def generate(self):
         tc = AutotoolsToolchain(self)
         tc.generate()
-
 
     def build(self):
         # Only make sure the project configures correctly, as these are build scripts
