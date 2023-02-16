dcd57a7f50f2388ee92a6a89e09e79bc2a0b6a93
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-02-16 14:54:29 +0100

    bump libalsa

--- recipes/rtmidi/all/conanfile.py
+++ recipes/rtmidi/all/conanfile.py
@@ -46,7 +46,7 @@ class RtMidiConan(ConanFile):
 
     def requirements(self):
         if self._with_alsa:
-            self.requires("libalsa/1.2.4")
+            self.requires("libalsa/1.2.7.2")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
