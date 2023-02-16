4d5da765d5140163f69939e9a28fa3a035b549b3
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-02-16 17:16:28 +0100

    Properly copy license

--- recipes/xorg-macros/all/conanfile.py
+++ recipes/xorg-macros/all/conanfile.py
@@ -59,7 +59,7 @@ class XorgMacrosConan(ConanFile):
         autotools.make()
 
     def package(self):
-        copy(self, "COPYING", src=self.source_folder, dst="licenses")
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         autotools = Autotools(self)
         autotools.install()
 
