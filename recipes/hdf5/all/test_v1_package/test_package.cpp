4974d46182f31443a46ece0c461ddbf92c162f4a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-02-17 02:35:44 +0100

    modernize more

--- recipes/hdf5/all/test_v1_package/test_package.cpp
+++
@@ -1,9 +0,0 @@
-#include <H5Cpp.h>
-
-extern "C" void test_cxx_api()
-{
-    hsize_t dimensions[] = {4, 6};
-	H5::H5File file("dataset.h5", H5F_ACC_TRUNC);
-	H5::DataSpace dataspace(2, dimensions);
-	H5::DataSet dataset = file.createDataSet("dataset", H5::PredType::STD_I32BE, dataspace);
-}
