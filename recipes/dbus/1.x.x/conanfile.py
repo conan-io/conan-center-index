d1522e7cee0c6c6398a383a7bd60a4d3e88f9ccd
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-02-18 17:55:10 +0100

    modernize more

--- recipes/dbus/1.x.x/conanfile.py
+++ recipes/dbus/1.x.x/conanfile.py
@@ -65,15 +65,10 @@ class DbusConan(ConanFile):
         else:
             cmake_layout(self, src_folder="src")
 
-    def build_requirements(self):
-        if self._meson_available:
-            self.tool_requires("meson/1.0.0")
-            self.tool_requires("pkgconf/1.9.3")
-
     def requirements(self):
         self.requires("expat/2.5.0")
         if self.options.with_glib:
-            self.requires("glib/2.75.2")
+            self.requires("glib/2.75.3")
         if self.options.get_safe("with_systemd"):
             self.requires("libsystemd/252.4")
         if self.options.with_selinux:
@@ -83,18 +78,25 @@ class DbusConan(ConanFile):
 
     def validate(self):
         if Version(self.version) >= "1.14.0":
-            if self.info.settings.compiler == "gcc" and Version(self.info.settings.compiler.version) < 7:
+            if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < 7:
                 raise ConanInvalidConfiguration(f"{self.ref} requires at least gcc 7.")
 
-        if not self._meson_available and self.info.settings.os == "Windows":
+        if not self._meson_available and self.settings.os == "Windows":
             raise ConanInvalidConfiguration(f"{self.ref} does not support Windows. Contributions welcome.")
 
+    def build_requirements(self):
+        if self._meson_available:
+            self.tool_requires("meson/1.0.0")
+            if not self.conf.get("tools.gnu:pkg_config",check_type=str):
+                self.tool_requires("pkgconf/1.9.3")
+
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         if self._meson_available:
+            env = VirtualBuildEnv(self)
+            env.generate()
             tc = MesonToolchain(self)
             tc.project_options["asserts"] = not is_apple_os(self)
             tc.project_options["checks"] = False
@@ -111,8 +113,8 @@ class DbusConan(ConanFile):
             tc.project_options["x11_autolaunch"] = "enabled" if self.options.get_safe("with_x11", False) else "disabled"
             tc.project_options["xml_docs"] = "disabled"
             tc.generate()
-            env = VirtualBuildEnv(self)
-            env.generate(scope="build")
+            deps = PkgConfigDeps(self)
+            deps.generate()
         else:
             tc = CMakeToolchain(self)
             tc.variables["DBUS_BUILD_TESTS"] = False
@@ -135,8 +137,6 @@ class DbusConan(ConanFile):
             tc.generate()
             cmake_deps = CMakeDeps(self)
             cmake_deps.generate()
-        pkg_config_deps = PkgConfigDeps(self)
-        pkg_config_deps.generate()
 
     def build(self):
         apply_conandata_patches(self)
