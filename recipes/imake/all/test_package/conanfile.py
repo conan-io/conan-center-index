09b6f6270bbae050b79f9b905f647b613749e018
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-02-20 13:11:04 +0100

    Fix test_package

--- recipes/imake/all/test_package/conanfile.py
+++ recipes/imake/all/test_package/conanfile.py
@@ -1,10 +1,9 @@
 from conan import ConanFile
 from conan.tools.build import can_run
+from conan.tools.files import copy
 from conan.tools.microsoft import is_msvc
 from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps
 
-import os
-import shutil
 
 required_conan_version = ">=1.54.0"
 
@@ -29,10 +28,11 @@ class TestPackageConan(ConanFile):
         tc.generate(env)
 
     def build(self):
-        for src in self.exports_sources:
-            copy(self, "*", os.path.join(self.source_folder, src), os.path.join(self.build_folder, src)
+        copy(self, "Imake*", self.source_folder, self.build_folder)
         if can_run(self):
             self.run("imake", env="conanrun")
+        autotools = Autotools(self)
+        autotools.make()
 
     def test(self):
         if can_run(self):
