876be3c7f360beba30ab999e384fbdaca300ac12
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-02-20 13:36:47 +0000

    egl: touch up for Conan 2.0 compatibility

--- recipes/egl/system/conanfile.py
+++ recipes/egl/system/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanException, ConanInvalidConfiguration
+from conan.tools.gnu import PkgConfig
 from conan.tools.system import package_manager
-from conans import tools
 
 required_conan_version = ">=1.47"
 
@@ -21,27 +21,7 @@ class SysConfigEGLConan(ConanFile):
             raise ConanInvalidConfiguration("This recipes supports only Linux and FreeBSD")
 
     def package_id(self):
-        self.info.header_only()
-
-    def _fill_cppinfo_from_pkgconfig(self, name):
-        pkg_config = tools.PkgConfig(name)
-        if not pkg_config.provides:
-            raise ConanException("EGL development files aren't available, give up")
-        libs = [lib[2:] for lib in pkg_config.libs_only_l]
-        lib_dirs = [lib[2:] for lib in pkg_config.libs_only_L]
-        ldflags = [flag for flag in pkg_config.libs_only_other]
-        include_dirs = [include[2:] for include in pkg_config.cflags_only_I]
-        cflags = [flag for flag in pkg_config.cflags_only_other if not flag.startswith("-D")]
-        defines = [flag[2:] for flag in pkg_config.cflags_only_other if flag.startswith("-D")]
-
-        self.cpp_info.system_libs.extend(libs)
-        self.cpp_info.libdirs.extend(lib_dirs)
-        self.cpp_info.sharedlinkflags.extend(ldflags)
-        self.cpp_info.exelinkflags.extend(ldflags)
-        self.cpp_info.defines.extend(defines)
-        self.cpp_info.includedirs.extend(include_dirs)
-        self.cpp_info.cflags.extend(cflags)
-        self.cpp_info.cxxflags.extend(cflags)
+        self.info.clear()
 
     def system_requirements(self):
         dnf = package_manager.Dnf(self)
@@ -51,13 +31,7 @@ class SysConfigEGLConan(ConanFile):
         yum.install(["mesa-libEGL-devel"], update=True, check=True)
 
         apt = package_manager.Apt(self)
-        ubuntu_20_or_later = tools.os_info.linux_distro == "ubuntu" and tools.os_info.os_version >= "20"
-        debian_11_or_later = tools.os_info.linux_distro == "debian" and tools.os_info.os_version >= "11"
-        pop_os_20_or_later = tools.os_info.linux_distro == "pop" and tools.os_info.os_version >= "20"
-        if ubuntu_20_or_later or debian_11_or_later or pop_os_20_or_later:
-            apt.install(["libegl-dev"], update=True, check=True)
-        else:
-            apt.install(["libegl1-mesa-dev"], update=True, check=True)
+        apt.install_substitutes(["libegl-dev", "libegl1-mesa-dev"], update=True, check=True)
 
         pacman = package_manager.PacMan(self)
         pacman.install(["libglvnd"], update=True, check=True)
@@ -75,4 +49,6 @@ class SysConfigEGLConan(ConanFile):
 
         self.cpp_info.includedirs = []
         self.cpp_info.libdirs = []
-        self._fill_cppinfo_from_pkgconfig('egl')
+        pkg_config = PkgConfig(self, "egl")
+        pkg_config.fill_cpp_info(self.cpp_info, is_system=True)
+
