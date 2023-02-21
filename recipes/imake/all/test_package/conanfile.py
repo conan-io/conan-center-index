00099f6b5487cb82a54db2e90ecf0707961acc7e
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-02-21 20:07:07 +0000

    imake: test it as a regular requirement instead

--- recipes/imake/all/test_package/conanfile.py
+++ recipes/imake/all/test_package/conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc
@@ -14,9 +13,10 @@ class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     test_type = "explicit"
 
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
+    def build_requirements(self):
         if not self.conf.get("tools.gnu:make_program", check_type=str):
             self.tool_requires("make/4.3")
 
@@ -31,14 +31,12 @@ class TestPackageConan(ConanFile):
             env.define("CC", "cl -nologo")
         tc.generate(env)
 
-        buildenv = VirtualBuildEnv(self)
-        buildenv.generate()
-
     def build(self):
-        copy(self, "Imake*", self.source_folder, self.build_folder)
-        self.run("imake", env="conanbuild")
-        autotools = Autotools(self)
-        autotools.make()
+        if can_run(self):
+            copy(self, "Imake*", self.source_folder, self.build_folder)
+            self.run("imake", env="conanrun")
+            autotools = Autotools(self)
+            autotools.make()
 
     def test(self):
         # test is successful if we can invoke make in the build step
