a547f58a9ee702ef66cf708061d358269849c0d8
Francisco Ramirez de Anton <franchuti688@gmail.com>
2023-02-21 13:44:36 +0100

    empty self.cpp_info.includedirs

--- recipes/xorg-gccmakedep/all/conanfile.py
+++ recipes/xorg-gccmakedep/all/conanfile.py
@@ -75,6 +75,7 @@ class XorgGccmakedep(ConanFile):
 
     def package_info(self):
         self.cpp_info.libdirs = []
+        self.cpp_info.includedirs = []
 
         bin_path = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH environment variable: {}".format(bin_path))
