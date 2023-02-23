85cb443a2ac2ab739b095d6a0a72366faa74aafe
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-02-23 15:52:11 +0100

    fix _cmake_new_enough() for conan v2

--- recipes/glog/all/conanfile.py
+++ recipes/glog/all/conanfile.py
@@ -63,7 +63,7 @@ class GlogConan(ConanFile):
             import re
             from io import StringIO
             output = StringIO()
-            self.run("cmake --version", output=output)
+            self.run("cmake --version", output)
             m = re.search(r"cmake version (\d+\.\d+\.\d+)", output.getvalue())
             return Version(m.group(1)) >= required_version
         except:
