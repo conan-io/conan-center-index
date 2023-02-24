9ac4b8b3e6b9fae6f48c3e5cc426cea2480bca72
toge <toge.mail@gmail.com>
2023-02-25 01:54:58 +0900

    hyperscan: add version 5.4.1

--- recipes/hyperscan/all/conanfile.py
+++ recipes/hyperscan/all/conanfile.py
@@ -16,6 +16,7 @@ class HyperscanConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.hyperscan.io"
     topics = ("regex", "regular expressions")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
