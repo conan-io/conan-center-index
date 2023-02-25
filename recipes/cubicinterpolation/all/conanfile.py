e8ff226812156603d481654422b0b2ff297cb83e
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2023-02-25 16:25:24 +0100

    compatability of cubic_interpolation with Conan 2.0

--- recipes/cubicinterpolation/all/conanfile.py
+++ recipes/cubicinterpolation/all/conanfile.py
@@ -1,5 +1,8 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile, tools
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import get, copy, rmdir
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
 import os
 
 required_conan_version = ">=1.43.0"
@@ -23,7 +26,6 @@ class CubicInterpolationConan(ConanFile):
         "fPIC": True,
     }
 
-    generators = "cmake", "cmake_find_package"
     _cmake = None
 
     @property
@@ -61,53 +63,45 @@ class CubicInterpolationConan(ConanFile):
         return ["filesystem", "math", "serialization"]
 
     def validate(self):
-        miss_boost_required_comp = any(getattr(self.options["boost"], "without_{}".format(boost_comp), True) for boost_comp in self._required_boost_components)
-        if self.options["boost"].header_only or miss_boost_required_comp:
-            raise ConanInvalidConfiguration("{0} requires non header-only boost with these components: {1}".format(self.name, ", ".join(self._required_boost_components)))
+
+        miss_boost_required_comp = any(getattr(self.dependencies["boost"].options, f"without_{boost_comp}", True) for boost_comp in self._required_boost_components)
+        if self.dependencies["boost"].options.header_only or miss_boost_required_comp:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires non header-only boost with these components: "
+                f"{', '.join(self._required_boost_components)}",
+            )
 
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "14")
-
-        minimum_version = self._minimum_compilers_version.get(
-            str(self.settings.compiler), False
-        )
-        if not minimum_version:
-            self.output.warn(
-                "CubicInterpolation requires C++14. Your compiler is unknown. Assuming it supports C++14."
-            )
-        elif tools.Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                "CubicInterpolation requires C++14, which your compiler does not support."
-            )
+            check_min_cppstd(self, "14")
 
         if self._is_msvc and self.options.shared:
             raise ConanInvalidConfiguration("cubicinterpolation shared is not supported with Visual Studio")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.definitions["BUILD_EXAMPLE"] = False
-        self._cmake.definitions["BUILD_DOCUMENTATION"] = False
-        self._cmake.configure()
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)  
+        tc.variables["BUILD_EXAMPLE"] = False
+        tc.variables["BUILD_DOCUMENTATION"] = False
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
 
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "CubicInterpolation")
