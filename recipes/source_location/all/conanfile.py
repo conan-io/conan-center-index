c586b2ccfc066e4d5ee98bae544bc2eb10718b80
Walenciak, Dawid <dawid.walenciak.sp@lhsystems.com>
2023-02-28 13:01:00 +0100

    Modernize source_location package

--- recipes/source_location/all/conanfile.py
+++ recipes/source_location/all/conanfile.py
@@ -1,8 +1,12 @@
 import os
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import get, copy
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.53.0"
 
 
 class SourceLocationConan(ConanFile):
@@ -15,19 +19,12 @@ class SourceLocationConan(ConanFile):
     settings = ["compiler"]
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  strip_root=True, destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses",
-                  src=self._source_subfolder)
-        self.copy(pattern="*", dst="include",
-                  src=os.path.join(self._source_subfolder, "include"))
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "*", os.path.join(self.source_folder, "include"), os.path.join(self.package_folder, "include"), keep_path=False)
 
     @property
     def _minimum_compilers_version(self):
@@ -40,15 +37,12 @@ class SourceLocationConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "11")
-        minimum_version = self._minimum_compilers_version.get(
-            str(self.settings.compiler), False)
+            check_min_cppstd(self, "11")
+        minimum_version = self._minimum_compilers_version.get(str(self.settings.compiler), False)
         if not minimum_version:
-            self.output.warn(
-                "source_location requires C++11. Your compiler is unknown. Assuming it supports C++11 and required functionality.")
-        elif tools.Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                "source_location requires C++11 and some embedded functionality, which your compiler does not support.")
+            self.output.warn("source_location requires C++11. Your compiler is unknown. Assuming it supports C++11 and required functionality.")
+        elif Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration("source_location requires C++11 and some embedded functionality, which your compiler does not support.")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
