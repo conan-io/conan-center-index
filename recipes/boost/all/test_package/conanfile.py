f68817425014ce4344eeea9da71d8ab94190b7ce
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-03-02 15:58:49 +0000

    boost test package: add comment with clarification

--- recipes/boost/all/test_package/conanfile.py
+++ recipes/boost/all/test_package/conanfile.py
@@ -58,5 +58,14 @@ class TestPackageConan(ConanFile):
         if not can_run(self):
             return
         with chdir(self, self.folders.build_folder):
+            # When boost and its dependencies are built as shared libraries,
+            # the test executables need to locate them. Typically the 
+            # `conanrun` env should be enough, but this may cause problems on macOS
+            # where the CMake installation has dependencies on Apple-provided
+            # system libraries that are incompatible with Conan-provided ones. 
+            # When `conanrun` is enabled, DYLD_LIBRARY_PATH will also apply
+            # to ctest itself. Given that CMake already embeds RPATHs by default, 
+            # we can bypass this by using the `conanbuild` environment on
+            # non-Windows platforms, while still retaining the correct behaviour.
             env = "conanrun" if self.settings.os == "Windows" else "conanbuild"
             self.run(f"ctest --output-on-failure -C {self.settings.build_type}", env=env)
