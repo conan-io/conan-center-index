7f10b1a293a982bf3f24794b016d462147eb5037
Antony Peacock <ant.peacock@gmail.com>
2023-03-05 12:37:11 +0000

    Linter fixes

--- recipes/wg21-linear_algebra/all/conanfile.py
+++ recipes/wg21-linear_algebra/all/conanfile.py
@@ -2,8 +2,8 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.files import copy, get, rmdir
+from conan.tools.cmake import cmake_layout
+from conan.tools.files import copy, get
 from conan.tools.scm import Version
 
 required_conan_version = ">=2.0.0"
@@ -34,24 +34,34 @@ class LAConan(ConanFile):
             "clang": "8",
             "apple-clang": "11"
         }
+    
+    @property
+    def _full_compiler_version(self):
+        compiler = self.settings.compiler
+        if compiler == "msvc":
+            if compiler.update:
+                return int(f"{compiler.version}{compiler.update}")
+            else:
+                return int(f"{compiler.version}0")
+        else:
+            return compiler.version
 
-    def validate(self):
+    def configure(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(
             str(self.settings.compiler))
         if not min_version:
-            self.output.warn("{} recipe lacks information about the {} "
-                             "compiler support.".format(
-                                 self.name, self.settings.compiler))
+            self.output.warn(f"{self.name} recipe lacks information about the "
+                             f"{self.settings.compiler} compiler support.")
         else:
             if Version(self.settings.compiler.version) < min_version:
                 raise ConanInvalidConfiguration(
-                    "{} requires C++{} support. "
-                    "The current compiler {} {} does not support it.".format(
-                        self.name, self._minimum_cpp_standard,
-                        self.settings.compiler,
-                        self.settings.compiler.version))
+                    f"{self.name} requires C++{self._minimum_cpp_standard} support. "
+                    f"The current compiler {self._full_compiler_version} does not support it.")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def package_id(self):
         self.info.clear()
