9cd765fe5fbddbca5a6abe77332826de57323e16
Antony Peacock <ant.peacock@gmail.com>
2023-03-05 12:27:22 +0000

    Fix lint warnings

--- recipes/mdspan/all/conanfile.py
+++ recipes/mdspan/all/conanfile.py
@@ -34,26 +34,33 @@ class MDSpanConan(ConanFile):
             "apple-clang": "5.1"
         }
 
+    @property
+    def _full_compiler_version(self):
+        compiler = self.settings.compiler
+        if compiler == "msvc":
+            if compiler.update:
+                return int(f"{compiler.version}{compiler.update}")
+            else:
+                return int(f"{compiler.version}0")
+        else:
+            return compiler.version
+
     def configure(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(
             str(self.settings.compiler))
         if not min_version:
-            self.output.warn("{} recipe lacks information about the {} "
-                             "compiler support.".format(
-                                 self.name, self.settings.compiler))
+            self.output.warn(f"{self.name} recipe lacks information about the "
+                             f"{self.settings.compiler} compiler support.")
         else:
             if Version(self.settings.compiler.version) < min_version:
                 raise ConanInvalidConfiguration(
-                    "{} requires C++{} support. "
-                    "The current compiler {} {} does not support it.".format(
-                        self.name, self._minimum_cpp_standard,
-                        self.settings.compiler,
-                        self.settings.compiler.version))
+                    f"{self.name} requires C++{self._minimum_cpp_standard} support. "
+                    f"The current compiler {self._full_compiler_version} does not support it.")
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
