530bcf07dc9e60be68bc9452144d35a6614b648c
Ahmed Abd El-Hamed <42156372+ahmed192a@users.noreply.github.com>
2023-03-07 21:26:29 +0200

    Add files via upload

---
+++ recipes/mysql-connector-cpp/all/test_package/example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// header for mysql connector cpp library
+#include <mysqlx/xdevapi.h>
+
+
+
+int main() {
+    // create a session to a MySQL server running on localhost
+    std::cout << "Connecting to the MySQL server..." << std::endl;
+
+}
