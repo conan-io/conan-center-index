8ca59549eef63c9d8ed157cb849bf4535b14e3c6
Radek Brich <radek.brich@devl.cz>
2023-03-09 08:07:05 +0100

    update also pegtl 2.x.x

--- recipes/taocpp-pegtl/3.x.x/test_package/conanfile.py
+++ recipes/taocpp-pegtl/3.x.x/test_package/conanfile.py
@@ -9,10 +9,8 @@ class TestPackageConan(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
 
-
     def requirements(self):
         self.requires(self.tested_reference_str)
-
 
     def layout(self):
         cmake_layout(self)
