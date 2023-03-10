32e785dc595b2e2e2e8e2e378e124acac59fc9f4
Joel Lefkowitz <joellefkowitz@hotmail.com>
2023-03-10 17:55:08 +0000

    Add detect v2.0.1

---
+++ recipes/detect/all/conanfile.py
@@ -0,0 +1,37 @@
+import os 
+from conan import ConanFile, tools
+from glob import glob
+
+required_conan_version = ">=1.52.0"
+
+class PackageConan(ConanFile):
+    name = "detect"
+    description = "A single header library to detect the OS at compile time."
+    license = "MIT"
+
+    author = "Joel Lefkowitz joellefkowitz@hotmail.com"
+    url = "https://github.com/JoelLefkowitz/detect"
+    homepage = "https://github.com/project/package"
+
+    topics = ("os", "header-only")
+    settings = ()
+    
+    no_copy_source = True
+    exports_sources = "src/*"
+
+    @property
+    def _min_cppstd(self):
+        return 11
+
+    def source(self):
+        tools.files.get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def package(self):
+        tools.files.copy(self, "*.hpp", os.path.join(self.source_folder, "src"), os.path.join(self.package_folder, "include"))
+        tools.files.copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+
+    def package_id(self):
+        self.info.clear()
+
+    def package_info(self):
+        self.cpp_info.libs = ["detect"]
