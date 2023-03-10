f4e0af80b8601e9ced0ebb54a84c3ea4a9c7b448
Kleto Zan <kletoz@gmail.com>
2023-03-10 10:14:58 -0300

    Update recipes/readerwriterqueue/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/readerwriterqueue/all/conanfile.py
+++ recipes/readerwriterqueue/all/conanfile.py
@@ -19,8 +19,7 @@ class ReaderWriterQueue(ConanFile):
         basic_layout(self, src_folder="src")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def build(self):
         pass
