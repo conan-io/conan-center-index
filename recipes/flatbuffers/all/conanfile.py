51901a42b333043df6096cffbadb8ebe6112343a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-03-12 22:38:46 +0100

    Revert "add package_type"
    This reverts commit 23a0e4ef0c15f62fddbf34e17f66fced706d619a.

--- recipes/flatbuffers/all/conanfile.py
+++ recipes/flatbuffers/all/conanfile.py
@@ -58,7 +58,6 @@ class FlatbuffersConan(ConanFile):
             self.tool_requires("cmake/3.25.2")
 
     def configure(self):
-        self.package_type = "header-library" if self.options.header_only else "library"
         if self.options.shared or self.options.header_only:
             self.options.rm_safe("fPIC")
         if self.options.header_only:
