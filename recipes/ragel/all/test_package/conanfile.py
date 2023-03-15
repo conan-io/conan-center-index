ff9980f0f65523e218eef5a4dc1d25d3d6f25bee
toge <toge.mail@gmail.com>
2023-03-15 22:20:07 +0900

    ragel: support conan v2

--- recipes/ragel/all/test_package/conanfile.py
+++ recipes/ragel/all/test_package/conanfile.py
@@ -1,10 +1,19 @@
-from conans import ConanFile, tools
-
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
+import os
 
 class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
 
-    settings = "os", "arch", "build_type", "compiler"
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def test(self):
-        if not tools.cross_building(self):
-            self.run("ragel --version", run_environment=True)
+        if can_run(self):
+            self.run("ragel --version", env="conanrun")
