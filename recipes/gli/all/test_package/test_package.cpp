a5d41573a85414e1ce8c5369adcf406713296c33
Luiz Henrique Laurini <luizhenriquelaurini@gmail.com>
2023-03-16 21:07:45 +0100

    Simplify test

--- recipes/gli/all/test_package/test_package.cpp
+++ recipes/gli/all/test_package/test_package.cpp
@@ -1,15 +1,8 @@
 #include <cstdlib>
+#include <iostream>
 #include "gli/gli.hpp"
 
 int main (int argc, char * argv[]) {
-    auto texture = gli::texture1d(gli::FORMAT_RGBA8_UNORM_PACK8, gli::extent1d(10), 1);
-
-    for (int i = 0; i < 10; i++)
-    {
-        texture.store(gli::extent1d(i), 0, glm::u8vec4(i * 255 / 9));
-    }
-
-    gli::save(texture, "test.dds");
-
+    std::cout << GLI_VERSION << "\n";
     return EXIT_SUCCESS;
 }
