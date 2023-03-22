8f1a43d3ab38e5ee35997d69dfb8469260340167
Jordan Williams <jordan@jwillikers.com>
2023-03-22 09:07:10 -0500

    Add package type

--- recipes/gstreamer/all/conanfile.py
+++ recipes/gstreamer/all/conanfile.py
@@ -20,6 +20,7 @@ class GStreamerConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://gstreamer.freedesktop.org/"
     license = "LGPL-2.0-or-later"
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
