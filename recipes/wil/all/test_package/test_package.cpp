222b3c7aaf0bdd11d0615881c03d0d0ba2c71066
elejke <german.novikov@phystech.edu>
2023-03-25 14:48:38 +0300

    wil recipe added

---
+++ recipes/wil/all/test_package/test_package.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+#include "wil/resource.h"
+
+
+int main(void) {
+    std::cout << "wil/resource.h included correctly" std::endl;"
+
+    return EXIT_SUCCESS;
+}
