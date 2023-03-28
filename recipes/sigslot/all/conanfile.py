c340ede8115734e483b054daf3b1690564bc3701
Uilian Ries <uilianries@gmail.com>
2023-03-28 16:49:34 +0200

    port sigslot recipe to Conan 2.0
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/sigslot/all/conanfile.py
+++ recipes/sigslot/all/conanfile.py
@@ -1,8 +1,14 @@
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc
+from conan.tools.files import get, copy
+from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.43.0"
+
+required_conan_version = ">=1.53.0"
 
 
 class SigslotConan(ConanFile):
@@ -13,58 +19,57 @@ class SigslotConan(ConanFile):
     homepage = "https://github.com/palacaze/sigslot"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
+    package_type = "header-library"
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
-    def _is_msvc(self):
-        return str(self.settings.compiler) in ["Visual Studio", "msvc"]
+    def _min_cppstd(self):
+        return 14
 
-    def validate(self):
-        minimal_cpp_standard = "14"
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, minimal_cpp_standard)
-        minimal_version = {
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "Visual Studio": "15",
+            "msvc": "14.1",
             "gcc": "5",
             "clang": "3.4",
             "apple-clang": "10",
-            "Visual Studio": "15"  # 14 is not supported by the library
         }
-        compiler = str(self.settings.compiler)
-        if compiler not in minimal_version:
-            self.output.warn(
-                "%s recipe lacks information about the %s compiler standard version support" % (self.name, compiler))
-            self.output.warn(
-                "%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
-            return
-        version = tools.Version(self.settings.compiler.version)
-        if version < minimal_version[compiler]:
-            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s" % (self.name, minimal_cpp_standard))
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy(pattern="LICENSE", src=self._source_subfolder, dst="licenses")
-        self.copy(pattern="signal.hpp", src=os.path.join(self._source_subfolder, "include", "sigslot"), dst=os.path.join("include", "sigslot"))
+        copy(self, pattern="LICENSE", src=self.source_folder, dst="licenses")
+        copy(self, pattern="signal.hpp", src=os.path.join(self.source_folder, "include", "sigslot"), dst=os.path.join("include", "sigslot"))
 
     def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
         self.cpp_info.set_property("cmake_file_name", "PalSigslot")
         self.cpp_info.set_property("cmake_target_name", "Pal::Sigslot")
+
         # TODO: back to global scope in conan v2 once cmake_find_package* generators removed
         self.cpp_info.components["_sigslot"].libs = []
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["_sigslot"].system_libs.append("pthread")
-        elif self.settings.os == "Windows":
-            if self._is_msvc or self.settings.compiler == "clang":
-                self.cpp_info.components["_sigslot"].exelinkflags.append('-OPT:NOICF')
+        elif self.settings.os == "Windows" and (is_msvc(self) or self.settings.compiler == "clang"):
+            self.cpp_info.components["_sigslot"].exelinkflags.append('-OPT:NOICF')
 
         # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "PalSigslot"
