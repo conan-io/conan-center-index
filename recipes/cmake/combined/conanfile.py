d2d3abc5fcd305f6576545f63088d3d24512344b
Kevin A. Mitchell <kam@datalogics.com>
2023-03-28 13:47:13 -0500

    cmake: Separate sections in conandata for sources and binaries
    - Binary part of the recipe gets the binaries from the binaries:
      dictionary.

--- recipes/cmake/combined/conanfile.py
+++ recipes/cmake/combined/conanfile.py
@@ -27,7 +27,7 @@ class CMakeConan(ConanFile):
 
     def build(self):
         arch = str(self.settings.arch) if self.settings.os != "Macos" else "universal"
-        get(self, **self.conan_data["sources"][self.version][str(self.settings.os)][arch],
+        get(self, **self.conan_data["binaries"][self.version][str(self.settings.os)][arch],
             destination=self.source_folder, strip_root=True)
 
     def package_id(self):
