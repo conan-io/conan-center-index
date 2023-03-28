0f5fb6aeaf3780fda3cb42bf87cefe7ef9790e6a
Cam Mannett <camden.mannett@protonmail.ch>
2023-03-28 09:24:32 +0100

    Updates from review round 1
    * Fixed linting errors
    * Add CMake build requirement as the CI's version is too old
    * Used standard CMake layout
    * Clear all settings

--- recipes/arg_router/all/conanfile.py
+++ recipes/arg_router/all/conanfile.py
@@ -3,20 +3,23 @@ import os
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import get, rmdir
-from conan.tools.cmake import CMakeDeps, CMakeToolchain, CMake
+from conan.tools.cmake import CMake, cmake_layout
 
 
 class arg_routerRecipe(ConanFile):
     name = "arg_router"
     license = "BSL-1.0"
-    author = "Camden Mannett"
-    url = "https://github.com/cmannett85/arg_router"
+    homepage = "https://github.com/cmannett85/arg_router"
+    url = "https://github.com/conan-io/conan-center-index"
     description = "C++ command line argument parsing and routing."
     topics = ("cpp", "command-line", "argument-parser", "header-only")
 
+    # CMake >= 3.18 is required but the recipe for it is not compatible with Conan v2, so just use
+    # the latest at the time of writing
+    tool_requires = "cmake/3.25.3"
     requires = "boost/1.74.0", "span-lite/0.10.3"
 
-    settings = "build_type", "compiler"
+    settings = "os", "arch", "compiler", "build_type"
     default_options = {"boost/*:header_only": True}
     package_type = "header-library"
     generators = "CMakeDeps", "CMakeToolchain"
@@ -29,9 +32,7 @@ class arg_routerRecipe(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def layout(self):
-        self.folders.build = "."
-        self.folders.generators = "."
-        self.cpp.source.includedirs = ["include"]
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
@@ -55,4 +56,4 @@ class arg_routerRecipe(ConanFile):
 
     def package_id(self):
         # build_type and compiler are needed for the Conan's CMake tools but are not actually used
-        self.info.settings.clear()
+        self.info.clear()
