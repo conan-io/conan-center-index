9d684fd5a9307802e9a38e85c862d3590e2ddb17
Samuel Dowling <samuel.dowling@protonmail.com>
2023-03-29 23:11:54 +1030

    [tz] Save dependency options to file in generate for 1.x compatibility
    * Remove test_v1_package as it's redundant after these changes
    * Save dependencies to json file in generate() for a cross-version
      compatible recipe

--- recipes/tz/all/test_package/conanfile.py
+++ recipes/tz/all/test_package/conanfile.py
@@ -1,6 +1,8 @@
 from conan import ConanFile
 from conan.tools.build import cross_building
 from conan.tools.layout import basic_layout
+import json
+import os
 
 
 class TzTestConan(ConanFile):
@@ -14,9 +16,23 @@ class TzTestConan(ConanFile):
     def layout(self):
         basic_layout(self)
 
+    def generate(self):
+        # Save dependencies to dependencies.json for consumption in test()
+        deps = {"tz": {key: value == "True" for key, value in self.dependencies["tz"].options.items()}}
+        with open('dependencies.json', 'w') as f:
+            f.write(json.dumps(deps))
+
     def test(self):
         if not cross_building(self):
-            if not self.dependencies["tz"].options.data_only:
+            # Read dependency options in from dependencies.json
+            # This is achievable in conan >2.0 using self.dependencies["tz"].options.data_only,
+            # however this is not a cross-version compatible approach and so this is used
+            # while support for conan 1.x is ongoing.
+            deps = {}
+            with open(os.path.join(self.generators_folder, 'dependencies.json'), 'r') as f:
+                deps = json.loads(f.read())
+
+            if not deps["tz"]["data_only"]:
                 which = "where" if self.settings.os == "Windows" else "which"
                 cmd = f"{which} zdump"
                 self.output.info(f"Executing '{cmd}'")
