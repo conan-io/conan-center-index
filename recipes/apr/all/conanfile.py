0b11e420e80c882c137abe003375f1fd4de52adb
Kevin Puetz <PuetzKevinA@JohnDeere.com>
2023-03-29 17:57:57 -0500

    apr: Loosen the cross-building restriction
    From all the discussion I found in #6622, the issue which motivated a
    "doesn't support cross-build yet" is the use of is AC_TRY_RUN checks
    in ./configure. That's not an issue when consuming existing binaries,
    so this check belongs in validate_build (since conan 1.51.0).
    
    It also doesn't apply to MSVC which uses CMake rather than autoconf;
    There's no use of try_compile or try_run in the CMake project.

--- recipes/apr/all/conanfile.py
+++ recipes/apr/all/conanfile.py
@@ -67,9 +67,9 @@ class AprConan(ConanFile):
         else:
             basic_layout(self, src_folder="src")
 
-    def validate(self):
-            raise ConanInvalidConfiguration("apr recipe doesn't support cross-build yet due to runtime checks")
-        if cross_building(self):
+    def validate_build(self):
+        if cross_building(self) and not is_msvc(self):
+            raise ConanInvalidConfiguration("apr recipe doesn't support cross-build yet due to runtime checks in autoconf")
 
     def build_requirements(self):
         if not is_msvc(self):
