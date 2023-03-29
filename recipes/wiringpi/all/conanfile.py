a0c8d13d278ea1341d6653e97c186c8182ab9ad7
danimtb <danimanzaneque@gmail.com>
2023-03-29 09:43:03 +0200

    up conan version

--- recipes/wiringpi/all/conanfile.py
+++ recipes/wiringpi/all/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import get
 from conan.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.54.0"
 
 class WiringpiConan(ConanFile):
     name = "wiringpi"
