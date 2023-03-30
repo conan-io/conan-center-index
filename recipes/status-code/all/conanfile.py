a71fb88a43f08ec9959b8cae46124da51c9efbae
Chris Mc <christopherm@jfrog.com>
2023-03-30 14:43:27 -0700

    chore: updates

--- recipes/status-code/all/conanfile.py
+++ recipes/status-code/all/conanfile.py
@@ -1,7 +1,9 @@
 import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conans import tools
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import get, copy
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.45.0"
 
@@ -11,16 +13,8 @@ class StatusCodeConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/ned14/status-code"
     description = "Proposed SG14 status_code for the C++ standard"
-    topics = ("header-only", "proposal")
+    topics = ("header-only", "proposal", "status", "return code")
     settings = "os", "arch", "build_type", "compiler"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _cppstd_required_version(self):
-        return 11
 
     @property
     def _compiler_required_version(self):
@@ -34,34 +28,27 @@ class StatusCodeConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._cppstd_required_version)
+            check_min_cppstd(self, "11")
 
         min_version = self._compiler_required_version.get(str(self.settings.compiler))
         if min_version:
-            if tools.Version(self.settings.compiler.version) < min_version:
+            if Version(self.settings.compiler.version) < min_version:
                 raise ConanInvalidConfiguration("This package requires c++11 support. The current compiler does not support it.")
         else:
-            self.output.warn("This recipe has no support for the current compiler. Please consider adding it.")
+            self.output.warning("This recipe has no support for the current compiler. Please consider adding it.")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy("Licence.txt", dst="licenses", src=self._source_subfolder)
-        self.copy("*.hpp", dst="include", src=os.path.join(self._source_subfolder, "include"))
-        self.copy("*.ipp", dst="include", src=os.path.join(self._source_subfolder, "include"))
+        copy(self, "Licence.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.hpp", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+        copy(self, "*.ipp", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
 
     def package_info(self):
         # See https://github.com/ned14/status-code/blob/38e1e862386cb38d577664fd681ef829b0e03fba/CMakeLists.txt#L126
         self.cpp_info.set_property("cmake_file_name", "status-code")
         self.cpp_info.set_property("cmake_target_name", "status-code::hl")
-
-        # TODO: to remove in conan v2 once cmake_find_package* & pkg_config generators removed
-        self.cpp_info.filenames["cmake_find_package"] = "status-code"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "status-code"
-        self.cpp_info.names["cmake_find_package"] = "status-code::hl"
-        self.cpp_info.names["cmake_find_package_multi"] = "status-code::hl"
