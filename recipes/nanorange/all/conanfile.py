a1e5ca30237853e84c459ffba3472248ab4e8f76
Chris Mc <prince.chrismc@gmail.com>
2023-03-30 15:25:06 -0700

    fix: imports and rm extra file

--- recipes/nanorange/all/conanfile.py
+++ recipes/nanorange/all/conanfile.py
@@ -1,8 +1,10 @@
-import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import get
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
+from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
+import os
 
 
 class NanorangeConan(ConanFile):
--- recipes/nanorange/all/test_package/example.cpp
+++
@@ -1,7 +0,0 @@
-#include <nanorange.hpp>
-#include <iostream>
-
-int main() {
-    int elems[] = { 0, 42, 666};
-    return *nano::min_element( elems );
-}
