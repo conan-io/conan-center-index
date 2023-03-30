8b310763e909a97f461e3fe22cf42d22192949c5
Rubén Rincón <rubenrb@jfrog.com>
2023-03-31 00:15:34 +0200

    Use self.settings in package()

--- recipes/openapi-generator/all/conanfile.py
+++ recipes/openapi-generator/all/conanfile.py
@@ -48,7 +48,7 @@ class OpenApiGeneratorConan(ConanFile):
     def package(self):
         copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         copy(self, pattern="openapi-generator.jar", dst=os.path.join(self.package_folder, "res"), src=self.source_folder)
-        if self.info.settings.os == "Windows":
+        if self.settings.os == "Windows":
             save(self,
                  path=os.path.join(self.package_folder, "bin", "openapi-generator.bat"),
                  content="""\
