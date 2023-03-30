9e711960588e375985db7ea2fcfff9456f741501
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-03-30 14:06:28 +0100

    ruy: fix test_package when cppstd is not defined in the profile (conan 1.x)

--- recipes/ruy/all/test_package/conanfile.py
+++ recipes/ruy/all/test_package/conanfile.py
@@ -1,11 +1,11 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
@@ -13,6 +13,13 @@ class TestPackageConan(ConanFile):
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if not self.settings.compiler.get_safe("cppstd"):
+            # Conan 1.x profiles that may not have cppstd defined
+            tc.variables['CMAKE_CXX_STANDARD'] = "11"
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
