cb5a968c168881ead6ea901abf9f3bb46241856a
Nicolas Morales <n.morales.0@proton.me>
2023-03-31 13:19:00 -0700

    rmlui: use old generators in test_v1_package

--- recipes/rmlui/4.x/test_v1_package/conanfile.py
+++ recipes/rmlui/4.x/test_v1_package/conanfile.py
@@ -1,12 +1,11 @@
-from conan import ConanFile
-from conan.tools.cmake import CMake
+from conans import ConanFile, CMake
 from conan.tools.build import cross_building
 import os
 
 # legacy validation with Conan 1.x
 class ConanRmluiTestV1Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
