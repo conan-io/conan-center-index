a0e20f6c88ff86f5769ab35ff1173d1e8ba52875
edisonhello <edisonhello@hotmail.com>
2023-03-31 06:17:41 +0000

    Trying to debug in CI

--- recipes/tabulate/all/test_package/conanfile.py
+++ recipes/tabulate/all/test_package/conanfile.py
@@ -1,11 +1,19 @@
 import os
 from conan.tools.build import can_run
+from conan.tools.cmake import CMake, cmake_layout
 from conans import ConanFile, CMake
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
