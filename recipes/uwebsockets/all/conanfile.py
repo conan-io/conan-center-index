0aae5ceeca6e1a01f89879a67f512b7cab59c11e
Chris Mc <christopherm@jfrog.com>
2023-03-31 13:31:06 -0700

    fix: type

--- recipes/uwebsockets/all/conanfile.py
+++ recipes/uwebsockets/all/conanfile.py
@@ -15,7 +15,7 @@ class UwebsocketsConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/uNetworking/uWebSockets"
     topics = ("websocket", "network", "server", "header-only")
-    package_type = "header-library
+    package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "with_zlib": [True, False],
