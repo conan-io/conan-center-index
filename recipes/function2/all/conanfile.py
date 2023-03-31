4a5654586b15ff50153416ee29db91475c61d0b9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-03-31 18:12:05 +0200

    modernize more

--- recipes/function2/all/conanfile.py
+++ recipes/function2/all/conanfile.py
@@ -16,12 +16,13 @@ class Function2Conan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/Naios/function2"
     license = "BSL-1.0"
+    package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
     def _min_cppstd(self):
-        return 14
+        return "14"
 
     @property
     def _compiler_minimal_version(self):
@@ -29,7 +30,8 @@ class Function2Conan(ConanFile):
             "gcc": "5",
             "clang": "3.4",
             "apple-clang": "10",
-            "Visual Studio": "14"
+            "Visual Studio": "14",
+            "msvc": "14",
         }
 
     def layout(self):
@@ -41,20 +43,15 @@ class Function2Conan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
-        compiler = str(self.settings.compiler)
-        if compiler not in self._compiler_minimal_version:
-            self.output.warn(
-                "%s recipe lacks information about the %s compiler standard version support" % (self.name, compiler))
-            self.output.warn(
-                "%s requires a compiler that supports at least C++%s" % (self.name, self._min_cppstd))
-            return
-        version = Version(self.settings.compiler.version)
-        if version < self._compiler_minimal_version[compiler]:
-            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s" % (self.name, self._min_cppstd))
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.",
+            )
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def build(self):
         pass
