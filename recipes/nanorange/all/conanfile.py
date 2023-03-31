aafbea7e32776eae622e60b2eae8d077b182e259
Rubén Rincón Blanco <git@rinconblanco.es>
2023-03-31 12:45:23 +0200

    Update recipes/nanorange/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/nanorange/all/conanfile.py
+++ recipes/nanorange/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
+from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
 import os
