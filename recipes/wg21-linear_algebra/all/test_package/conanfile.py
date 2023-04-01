0bf2e01c55182939a8feaa4ea084b9bb9c4b2f9e
Antony Peacock <ant.peacock@gmail.com>
2023-04-01 19:07:19 +0100

    Explicit test required during mirgration

--- recipes/wg21-linear_algebra/all/test_package/conanfile.py
+++ recipes/wg21-linear_algebra/all/test_package/conanfile.py
@@ -6,7 +6,8 @@ from conan.tools.cmake import CMake, cmake_layout
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
-
+    test_type="explicit"
+    
     def requirements(self):
         self.requires(self.tested_reference_str)
 
