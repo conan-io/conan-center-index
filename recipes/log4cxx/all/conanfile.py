c7ce0a061b8922f4dbef9a3f9802da2179d9e06f
Stephen Webb <stephen.webb@sabreautonomous.com.au>
2023-04-03 09:55:26 +1000

    Prevent 'odbc/2.3.11' requires 'libiconv/1.17' while 'apr-util/1.6.1' requires 'libiconv/1.16'

--- recipes/log4cxx/all/conanfile.py
+++ recipes/log4cxx/all/conanfile.py
@@ -23,6 +23,7 @@ class Log4cxx(ConanFile):
         "fPIC": [True, False],
         "with_networking": [True, False],
         "with_wchar_t": [True, False],
+        "with_odbc": [True, False],
         "with_multiprocess_rolling_file_appender": [True, False],
         "with_qt": [True, False],
     }
@@ -31,6 +32,7 @@ class Log4cxx(ConanFile):
         "fPIC": True,
         "with_networking": True,
         "with_wchar_t": False,
+        "with_odbc": False,
         "with_multiprocess_rolling_file_appender": False,
         "with_qt": False,
     }
@@ -53,7 +55,7 @@ class Log4cxx(ConanFile):
         self.requires("apr/[>=1.6]")
         self.requires("apr-util/[>=1.6]")
         self.requires("expat/[>=2.4]")
-        if self.settings.os != "Windows":
+        if self.options.get_safe("with_odbc") and self.settings.os != "Windows":
             self.requires("odbc/[>=2.3]")
         if self.options.get_safe("with_qt"):
             self.requires("qt/[>=5.15 <6]")
@@ -77,7 +79,7 @@ class Log4cxx(ConanFile):
             if not minimum_version:
                 self.output.warning("Your compiler is unknown. Assuming it supports C++17.")
             elif compiler_version < minimum_version:
-                raise ConanInvalidConfiguration(f"{self.settings.compiler} {compiler_version} does not support C++17: Requies {minimum_version}")
+                raise ConanInvalidConfiguration(f"{self.settings.compiler} {compiler_version} does not support C++17: {minimum_version} required.")
             if self.settings.compiler.get_safe("cppstd"):
                 check_min_cppstd(self, "17")
 
@@ -100,6 +102,7 @@ class Log4cxx(ConanFile):
         if Version(self.version) >= "1.0.0":
             tc.variables["LOG4CXX_NETWORKING_SUPPORT"] = self.options.with_networking
             tc.variables["LOG4CXX_MULTIPROCESS_ROLLING_FILE_APPENDER"] = self.options.with_multiprocess_rolling_file_appender
+        tc.variables["LOG4CXX_ENABLE_ODBC"] = self.options.with_odbc
         tc.variables["LOG4CXX_WCHAR_T"] = self.options.with_wchar_t
         tc.variables["LOG4CXX_QT_SUPPORT"] = self.options.with_qt
         if self.settings.os == "Windows":
