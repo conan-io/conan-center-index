c77c4c6a2354272045430d836708abfbc269ad85
Antony Peacock <ant.peacock@gmail.com>
2023-04-02 09:07:10 +0100

    Code review feedback

--- recipes/wg21-linear_algebra/all/test_v1_package/conanfile.py
+++ recipes/wg21-linear_algebra/all/test_v1_package/conanfile.py
@@ -1,17 +1,11 @@
+from conans import ConanFile, CMake
+from conan.tools.build import cross_building
 import os
-from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import CMake, cmake_layout
 
-class TestPackageConan(ConanFile):
+# legacy validation with Conan 1.x
+class TestPackageV1Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def layout(self):
-        cmake_layout(self)
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
@@ -19,6 +13,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, env="conanrun")
+        if not cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
