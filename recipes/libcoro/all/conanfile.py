7ec210fb48dd8ca18f796ab575901fd9f80d70bb
chausner <chausner@users.noreply.github.com>
2023-04-04 01:08:56 +0200

    Remove pkgconfig

--- recipes/libcoro/all/conanfile.py
+++ recipes/libcoro/all/conanfile.py
@@ -92,6 +92,7 @@ class LibcoroConan(ConanFile):
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "libcoro")
