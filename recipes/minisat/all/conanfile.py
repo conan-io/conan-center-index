9fa0759337a658034abfbac91427fb44798d2cef
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-03 21:07:02 +0200

    refactoring

--- recipes/minisat/all/conanfile.py
+++ recipes/minisat/all/conanfile.py
@@ -1,10 +1,7 @@
 from conan import ConanFile
-from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
-from conan.tools.microsoft import is_msvc
-from os.path import join
+import os
 
 required_conan_version = ">=1.53.0"
 
@@ -27,17 +24,9 @@ class MiniSatConan(ConanFile):
     }
     package_type = "library"
 
-    def validate(self):
-        if is_msvc(self) and self.options.shared:
-            raise ConanInvalidConfiguration(f"{self.ref} can not be built as shared on Visual Studio and msvc.")
-
-    def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True,
-            destination=join(self.source_folder, "minisat"))
-
     def export_sources(self):
         export_conandata_patches(self)
-        copy(self, "CMakeLists.txt", self.recipe_folder, join(self.export_sources_folder, "src"))
+        copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -47,33 +36,36 @@ class MiniSatConan(ConanFile):
         if self.options.shared:
             self.options.rm_safe("fPIC")
 
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
     def requirements(self):
         # https://github.com/niklasso/minisat/blob/37dc6c67e2af26379d88ce349eb9c4c6160e8543/minisat/utils/ParseUtils.h#L27
         self.requires("zlib/1.2.13", transitive_headers=True, transitive_libs=True)
 
-    def layout(self):
-        cmake_layout(self, src_folder="src")
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
+        tc.variables["MINISATE_SRC_DIR"] = self.source_folder.replace("\\", "/")
         tc.generate()
-        tc = CMakeDeps(self)
-        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         apply_conandata_patches(self)
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
         cmake.build()
 
     def package(self):
-        copy(self, pattern="LICENSE", src=join(self.source_folder, "minisat"), dst=join(self.package_folder, "licenses"))
+        copy(self, pattern="LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
-        fix_apple_shared_install_name(self)
 
     def package_info(self):
         self.cpp_info.libs = ["minisat"]
+        self.cpp_info.includedirs.append(os.path.join("include", "minisat"))
         if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs.append("m")
-            self.cpp_info.system_libs.append("pthread")
+            self.cpp_info.system_libs.extend(["m", "pthread"])
