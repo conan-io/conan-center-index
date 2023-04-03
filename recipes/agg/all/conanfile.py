090804a53b4963655b25319cc7fd0ba136f94d63
Jihadist <tomasiche@gmail.com>
2023-04-03 13:36:20 +0300

    fix requires

--- recipes/agg/all/conanfile.py
+++ recipes/agg/all/conanfile.py
@@ -105,7 +105,7 @@ class AggConan(ConanFile):
             self.cpp_info.components["fontfreetype"].set_property("cmake_target_name", "agg::fontfreetype")
             self.cpp_info.components["fontfreetype"].libs = ["aggfontfreetype" + lib_suffix]
             self.cpp_info.components["fontfreetype"].includedirs = [os.path.join("include", "agg","fontfreetype")]
-            self.cpp_info.components["2d"].requires = ["agg"]
+            self.cpp_info.components["fontfreetype"].requires = ["agg", "freetype::freetype"]
             
         if self.options.with_gpc:
             self.cpp_info.components["gpc"].set_property("cmake_target_name", "agg::gpc")
