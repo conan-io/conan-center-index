ede44c0e10d1b432fa3c3228e7b9c188f7c705d2
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2023-04-04 15:29:57 +0200

    add suggestions by @prince-chrismc

--- recipes/cubicinterpolation/all/conanfile.py
+++ recipes/cubicinterpolation/all/conanfile.py
@@ -4,9 +4,10 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.files import get, copy, rmdir, patch, apply_conandata_patches, export_conandata_patches
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.scm import Version
+from conan.tools.microsoft import is_msvc, check_min_vs
 import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.53.0"
 
 
 class CubicInterpolationConan(ConanFile):
@@ -16,6 +17,7 @@ class CubicInterpolationConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     description = "Leightweight interpolation library based on boost and eigen."
     topics = ("interpolation", "splines", "cubic", "bicubic", "boost", "eigen3")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -28,27 +30,16 @@ class CubicInterpolationConan(ConanFile):
 
     _cmake = None
 
-    @property
-    def _is_msvc(self):
-        return str(self.settings.compiler) in ["Visual Studio", "msvc"]
-
     def export_sources(self):
-        #self.copy("CMakeLists.txt")
         export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
+            self.options.rm_safe("fPIC")
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -74,10 +65,10 @@ class CubicInterpolationConan(ConanFile):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, "14")
 
-        if str(self.settings.compiler) == "Visual Studio" and Version(self.settings.compiler.version) < "16":    
-            raise ConanInvalidConfiguration("Visual Studio < 2019 not yet supported in this recipe")
+        if not check_min_vs(self, 192, raise_invalid=False):    
+            raise ConanInvalidConfiguration(f"{self.ref} currently Visual Studio < 2019 not yet supported in this recipe. Contributions are welcome")
 
-        if self._is_msvc and self.options.shared:
+        if is_msvc(self) and self.options.shared:
             raise ConanInvalidConfiguration("cubicinterpolation shared is not supported with Visual Studio")
 
     def source(self):
