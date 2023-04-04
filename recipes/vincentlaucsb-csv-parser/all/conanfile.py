7795a4ad026df20a2a1fd943c3f8eee4da2bb2b7
czoido <mrgalleta@gmail.com>
2023-04-04 16:22:31 +0200

    some fixes

--- recipes/vincentlaucsb-csv-parser/all/conanfile.py
+++ recipes/vincentlaucsb-csv-parser/all/conanfile.py
@@ -1,12 +1,13 @@
-from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools import build, scm
-from conan.tools.files import copy, get
-from conan.tools.layout import basic_layout
-
 import os
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.52.0"
+
 
 class VincentlaucsbCsvParserConan(ConanFile):
     name = "vincentlaucsb-csv-parser"
@@ -16,32 +17,32 @@ class VincentlaucsbCsvParserConan(ConanFile):
     homepage = "https://github.com/vincentlaucsb/csv-parser"
     license = "MIT"
     settings = "os", "compiler"
+    package_type = "header-library"
     no_copy_source = True
 
     def validate(self):
-        if self.settings.get_safe("compiler.cppstd"):
-            build.check_min_cppstd(self, 11)
-
+        # C++17 recommended: https://github.com/vincentlaucsb/csv-parser/blob/2.1.3/README.md
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 14)
         compiler = self.settings.compiler
-        compiler_version = scm.Version(self.settings.compiler.version)
-
+        compiler_version = Version(self.settings.compiler.version)
         if compiler == "gcc" and compiler_version < "7":
             raise ConanInvalidConfiguration("gcc version < 7 not supported")
 
     def package_id(self):
         self.info.clear()
-        
-    def layout(self):
-        basic_layout(self, src_folder="src")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
         copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
-        copy(self, pattern="*", src=os.path.join(self.source_folder, "single_include"), dst=os.path.join(self.package_folder, "include"))
+        copy(self, pattern="*",
+             dst=os.path.join(self.package_folder, "include"),
+             src=os.path.join(self.source_folder, "single_include"))
 
     def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
         if self.settings.os in ("Linux", "FreeBSD"):
             self.cpp_info.system_libs.append("pthread")
-
--- recipes/vincentlaucsb-csv-parser/all/test_package/conanfile.py
+++ recipes/vincentlaucsb-csv-parser/all/test_package/conanfile.py
@@ -1,25 +1,26 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import cmake_layout, CMake
 import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain" 
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-        
     def layout(self):
         cmake_layout(self)
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
     def test(self):
         if can_run(self):
-            cmd = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(cmd, env="conanrun")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
