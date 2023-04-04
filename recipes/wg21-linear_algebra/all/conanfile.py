058d8e90406edf6f7bf5272b748a45ecbc0b5d49
Antony Peacock <ant.peacock@gmail.com>
2023-04-04 19:03:43 +0100

    Less is more :)

--- recipes/wg21-linear_algebra/all/conanfile.py
+++ recipes/wg21-linear_algebra/all/conanfile.py
@@ -65,7 +65,7 @@ class LAConan(ConanFile):
         self.info.clear()
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
         copy(self, "LICENSE.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
