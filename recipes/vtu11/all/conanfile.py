366256665b05dc2e29d117ccb62838514c09398f
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-04-05 10:42:36 +0200

    Use patch for filesystem when compiling with C++17
    Removing the filesystem.hpp failed when compiling on some Windows systems
    MSVC needs this to actually report the right value when using the __cplusplus pre-definition macro.
    See here for details: https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus

--- recipes/vtu11/all/conanfile.py
+++ recipes/vtu11/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd, valid_min_cppstd
-from conan.tools.files import get, copy, rm
+from conan.tools.files import get, copy, export_conandata_patches, apply_conandata_patches
 from conan.tools.layout import basic_layout
 import os
 
@@ -16,6 +16,7 @@ class PackageConan(ConanFile):
     homepage = "https://github.com/phmkopp/vtu11"
     topics = ("vtu", "c++11")
     settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
     options = {
         "with_zlib": [True, False],
     }
@@ -26,6 +27,9 @@ class PackageConan(ConanFile):
     @property
     def _min_cppstd(self):
         return 11
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def layout(self):
         basic_layout(self, src_folder="src")
@@ -44,11 +48,9 @@ class PackageConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _patch_sources(self):
-        rm(self, "filesystem.hpp", os.path.join(self.source_folder, "vtu11", "inc"))
-
     def build(self):
-        self._patch_sources()
+        if valid_min_cppstd(self, 17):
+            apply_conandata_patches(self)
 
     def package(self):
         copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
