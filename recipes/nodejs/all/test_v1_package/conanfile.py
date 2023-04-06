d1f3b3730208e5212662a93c3ef6ba09574b7cb2
Stefan Pantic <stefan.pantic@smallpdf.com>
2023-04-06 15:02:51 +0200

    fixup! feat(nodejs): make recipe Conan 2.0 ready
    Add test_v1_package

---
+++ recipes/nodejs/all/test_v1_package/conanfile.py
@@ -0,0 +1,11 @@
+from conans import ConanFile, tools
+
+
+class TestPackageConan(ConanFile):
+
+    settings = "os", "arch"
+
+    def test(self):
+        if not tools.cross_building(self):
+            self.output.info("Node version:")
+            self.run("node --version", run_environment=True)
