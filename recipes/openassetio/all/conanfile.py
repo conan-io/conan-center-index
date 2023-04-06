8e1df1538e61072d98377ee41d5edc1a97194f04
David Feltell <7827094+feltech@users.noreply.github.com>
2023-04-06 13:37:59 +0100

    Add missing `package_type` metadata
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/openassetio/all/conanfile.py
+++ recipes/openassetio/all/conanfile.py
@@ -22,6 +22,7 @@ class PackageConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/OpenAssetIO/OpenAssetIO"
     topics = ("asset-pipeline", "vfx", "cg", "assetmanager", "vfx-pipeline")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
