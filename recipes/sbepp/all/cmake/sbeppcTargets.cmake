4cf7f301afd95d085c182ddf64b1b73377c49adc
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2023-04-07 17:01:37 +0300

    recipe for `sbepp/1.0.0`
    Fixes #16936

---
+++ recipes/sbepp/all/cmake/sbeppcTargets.cmake
@@ -0,0 +1,34 @@
+# mimicking flatbuffers/all/cmake/FlatcTargets.cmake
+if(NOT TARGET sbepp::sbeppc)
+    if(CMAKE_CROSSCOMPILING)
+        find_program(SBEPP_SBEPPC_EXECUTABLE
+            NAMES sbeppc
+            PATHS ENV PATH
+            NO_DEFAULT_PATH
+        )
+    else()
+        find_program(SBEPP_SBEPPC_EXECUTABLE
+            NAMES sbeppc
+            PATHS "${CMAKE_CURRENT_LIST_DIR}/../../../bin/"
+            NO_DEFAULT_PATH
+        )
+    endif()
+    # TODO: In conan v2 with CMakeToolchain, can be replaced by:
+    # find_program(SBEPP_SBEPPC_EXECUTABLE NAMES sbeppc)
+    # # Nice enough to handle sbeppc not in build_requires for native build
+    # if(NOT SBEPP_SBEPPC_EXECUTABLE AND NOT CMAKE_CROSSCOMPILING)
+    #     find_program(SBEPP_SBEPPC_EXECUTABLE
+    #         NAMES flatc
+    #         PATHS "${CMAKE_CURRENT_LIST_DIR}/../../../bin/"
+    #         NO_DEFAULT_PATH
+    #     )
+    # endif()
+
+    if(SBEPP_SBEPPC_EXECUTABLE)
+        get_filename_component(
+            SBEPP_SBEPPC_EXECUTABLE "${SBEPP_SBEPPC_EXECUTABLE}" ABSOLUTE)
+        add_executable(sbepp::sbeppc IMPORTED)
+        set_property(TARGET sbepp::sbeppc
+            PROPERTY IMPORTED_LOCATION ${SBEPP_SBEPPC_EXECUTABLE})
+    endif()
+endif()
---
+++ recipes/sbepp/all/conanfile.py
@@ -0,0 +1,130 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime, is_msvc
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv
+import os
+
+
+required_conan_version = ">=1.53.0"
+
+
+class PackageConan(ConanFile):
+    name = "sbepp"
+    description = "C++ implementation of the FIX Simple Binary Encoding"
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/OleksandrKvl/sbepp"
+    topics = ("trading", "fix", "sbe")
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "with_sbeppc": [True, False]
+    }
+    default_options = {
+        "with_sbeppc": True
+    }
+
+    @property
+    def _min_cppstd(self):
+        if self.options.with_sbeppc:
+            return 17
+        else:
+            return 11
+
+    @property
+    def _compilers_minimum_version(self):
+        if self.options.with_sbeppc:
+            return {
+                "gcc": "7.1",
+                "clang": "5",
+                "apple-clang": "5"
+            }
+        else:
+            return {
+                "gcc": "4.8.1",
+                "clang": "3.3",
+                "apple-clang": "9.4"
+            }
+
+    def export_sources(self):
+        copy(self, os.path.join("cmake", "sbeppcTargets.cmake"),
+            self.recipe_folder, self.export_sources_folder)
+        export_conandata_patches(self)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def package_id(self):
+        if not self.info.options.with_sbeppc:
+            self.info.clear()
+
+    def requirements(self):
+        if self.options.with_sbeppc:
+            self.requires("fmt/9.1.0")
+            self.requires("pugixml/1.12.1")
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+
+        check_min_vs(self, 191)
+        if not is_msvc(self):
+            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+            if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration(
+                    f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+                )
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if not self.options.with_sbeppc:
+            tc.variables["SBEPP_BUILD_SBEPPC"] = False
+
+        tc.generate()
+
+        if self.options.with_sbeppc:
+            tc = CMakeDeps(self)
+            tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, pattern="LICENSE.md", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
+        cmake.install()
+        copy(self, "sbeppcTargets.cmake",
+            src=os.path.join(self.source_folder, "cmake"),
+            dst=os.path.join(self.package_folder, self._module_path))
+    
+    @property
+    def _module_path(self):
+        return os.path.join("lib", "cmake", "sbepp")
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "sbepp")
+        self.cpp_info.set_property("cmake_target_name", "sbepp::sbepp")
+
+        # provide sbepp::sbeppc target
+        build_modules = [
+            os.path.join(self._module_path, "sbeppcTargets.cmake")
+        ]
+        self.cpp_info.set_property("cmake_build_modules", build_modules)
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.filenames["cmake_find_package"] = "sbepp"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "sbepp"
+        self.cpp_info.names["cmake_find_package"] = "sbepp"
+        self.cpp_info.names["cmake_find_package_multi"] = "sbepp"
+
+        # TODO: to remove in conan v2
+        if self.options.with_sbeppc:
+            self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
