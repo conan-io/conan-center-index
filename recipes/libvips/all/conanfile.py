bf78ba8e66f79aee9e713d3879c6048676012fe9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-08 11:07:54 +0200

    bump dependencies

--- recipes/libvips/all/conanfile.py
+++ recipes/libvips/all/conanfile.py
@@ -117,7 +117,7 @@ class LibvipsConan(ConanFile):
 
     def requirements(self):
         self.requires("expat/2.5.0")
-        self.requires("glib/2.76.0", transitive_headers=True, transitive_libs=True, run=can_run(self))
+        self.requires("glib/2.76.1", transitive_headers=True, transitive_libs=True, run=can_run(self))
         if self.options.with_cfitsio:
             self.requires("cfitsio/4.1.0")
         if self.options.with_cgif:
@@ -127,7 +127,7 @@ class LibvipsConan(ConanFile):
         if self.options.with_fftw:
             self.requires("fftw/3.3.10")
         if self.options.with_fontconfig:
-            self.requires("fontconfig/2.13.93")
+            self.requires("fontconfig/2.14.2")
         if self.options.with_heif:
             self.requires("libheif/1.13.0")
         if self.options.with_jpeg == "libjpeg":
@@ -198,13 +198,13 @@ class LibvipsConan(ConanFile):
             raise ConanInvalidConfiguration("librsvg recipe not available in conancenter yet")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.0.0")
+        self.tool_requires("meson/1.0.1")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/1.9.3")
         if self.options.introspection:
             self.tool_requires("gobject-introspection/1.72.0")
         if not can_run(self):
-            self.tool_requires("glib/2.76.0")
+            self.tool_requires("glib/2.76.1")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
