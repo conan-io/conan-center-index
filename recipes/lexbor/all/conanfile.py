271636a5b7212801dcd9407a10bf64c70609dcc5
toge <toge.mail@gmail.com>
2023-04-10 01:12:30 +0900

    lexbor: add version 2.2.0, support conan v2

--- recipes/lexbor/all/conanfile.py
+++ recipes/lexbor/all/conanfile.py
@@ -1,33 +1,31 @@
-from conans import ConanFile, CMake, tools
-from conan.tools.microsoft import is_msvc
-import functools
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime, is_msvc
+from conan.tools.files import get, copy, rm
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.53.0"
 
 class LexborConan(ConanFile):
     name = "lexbor"
     description = "Lexbor is development of an open source HTML Renderer library"
-    topics = ("html5", "css", "parser", "renderer")
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/lexbor/lexbor/"
+    topics = ("html5", "css", "parser", "renderer")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
-        "shared": [True, False], 
+        "shared": [True, False],
         "fPIC": [True, False],
         "build_separately": [True, False],
     }
     default_options = {
-        "shared": False, 
+        "shared": False,
         "fPIC": True,
         "build_separately": False,
     }
-    generators = "cmake"
-    exports_sources = ["CMakeLists.txt", ]
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
     def _is_mingw(self):
@@ -39,53 +37,51 @@ class LexborConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.cppstd
-        del self.settings.compiler.libcxx
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def validate(self):
         # static build on Windows will be support by future release. (https://github.com/lexbor/lexbor/issues/69)
         if str(self.version) == "2.1.0" and self.options.shared == False and (is_msvc(self) or self._is_mingw):
-            raise tools.ConanInvalidConfiguration("{}/{} doesn't support static build on Windows(please use cci.20220301).".format(self.name, self.version))
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support static build on Windows(please use 2.2.0).")
 
         if self.options.build_separately:
-            raise tools.ConanInvalidConfiguration("{}/{} doesn't support build_separately option(yet).".format(self.name, self.version))
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support build_separately option(yet).")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-            destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    @functools.lru_cache(1)
-    def _configure_cmake(self):
-        cmake = CMake(self)
-
-        cmake.definitions["LEXBOR_BUILD_SHARED"] = self.options.shared
-        cmake.definitions["LEXBOR_BUILD_STATIC"] = not self.options.shared
-        cmake.definitions["LEXBOR_TESTS_CPP"] = False
-        # TODO: enable build_separately option
-        cmake.definitions["LEXBOR_BUILD_SEPARATELY"] = self.options.build_separately
-        cmake.definitions["LEXBOR_INSTALL_HEADERS"] = True
-
-        cmake.configure()
-        return cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["LEXBOR_BUILD_SHARED"] = self.options.shared
+        tc.cache_variables["LEXBOR_BUILD_STATIC"] = not self.options.shared
+        tc.variables["LEXBOR_TESTS_CPP"] = False
+        tc.variables["LEXBOR_BUILD_SEPARATELY"] = self.options.build_separately
+        tc.variables["LEXBOR_INSTALL_HEADERS"] = True
+        tc.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
         target = "lexbor" if self.options.shared else "lexbor_static"
         self.cpp_info.set_property("cmake_file_name", "lexbor")
-        self.cpp_info.set_property("cmake_target_name", "lexbor::{}".format(target))
+        self.cpp_info.set_property("cmake_target_name", f"lexbor::{target}")
         self.cpp_info.names["cmake_find_package"] = "lexbor"
         self.cpp_info.names["cmake_find_package_multi"] = "lexbor"
 
-        self.cpp_info.components["_lexbor"].set_property("cmake_target_name", "lexbor::{}".format(target))
+        self.cpp_info.components["_lexbor"].set_property("cmake_target_name", f"lexbor::{target}")
         self.cpp_info.components["_lexbor"].names["cmake_find_package"] = target
         self.cpp_info.components["_lexbor"].names["cmake_find_package_multi"] = target
 
