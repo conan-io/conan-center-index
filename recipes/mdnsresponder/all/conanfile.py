1819f57e39c599ec50127f209f311c48a8bf9184
xuzhenbao <xuzhenbao133@163.com>
2023-04-11 14:29:50 +0800

    Support conan v2

--- recipes/mdnsresponder/all/conanfile.py
+++ recipes/mdnsresponder/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import symlinks
+from conan.tools.files import symlinks, rmdir, get, replace_in_file, rename, chdir, patch, mkdir
 from conan.tools.scm import Version
 from conans import tools, AutoToolsBuildEnvironment, MSBuild
 import os
@@ -62,7 +62,7 @@ class MdnsResponderConan(ConanFile):
             raise ConanInvalidConfiguration("Visual Studio 2017 is not supported in CCI (yet).")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        get(self, **self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     @property
@@ -111,7 +111,7 @@ class MdnsResponderConan(ConanFile):
         return self._autotools
 
     def _build_make(self):
-        with tools.chdir(self._posix_folder):
+        with chdir(self, self._posix_folder):
             autotools = self._configure_autotools()
             autotools.make(args=self._make_build_args, target=self._make_build_targets)
 
@@ -139,24 +139,24 @@ class MdnsResponderConan(ConanFile):
             dllstub_vcxproj = os.path.join(self._source_subfolder, "mDNSWindows", "DLLStub", "DLLStub.vcxproj")
             dns_sd_vcxproj = os.path.join(self._source_subfolder, "Clients", "DNS-SD.VisualStudio", "dns-sd.vcxproj")
             for vcxproj in [dll_vcxproj, dllstub_vcxproj, dns_sd_vcxproj]:
-                tools.replace_in_file(vcxproj, "<RuntimeLibrary>MultiThreaded</RuntimeLibrary>", "<RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>")
-                tools.replace_in_file(vcxproj, "<RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>", "<RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>")
+                replace_in_file(self, vcxproj, "<RuntimeLibrary>MultiThreaded</RuntimeLibrary>", "<RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>")
+                replace_in_file(self, vcxproj, "<RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>", "<RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>")
 
         # could use glob and replace_in_file(strict=False, ...)
         dll_rc = os.path.join(self._source_subfolder, "mDNSWindows", "DLL", "dll.rc")
         dns_sd_rc = os.path.join(self._source_subfolder, "Clients", "DNS-SD.VisualStudio", "dns-sd.rc")
         for rc in [dll_rc, dns_sd_rc]:
-            tools.replace_in_file(rc, "afxres.h", "winres.h")
+            replace_in_file(self, rc, "afxres.h", "winres.h")
 
         msbuild = MSBuild(self)
         msbuild.build(sln, targets=self._msvc_targets, platforms=self._msvc_platforms, definitions=self._msvc_definitions)
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        for patchfile in self.conan_data.get("patches", {}).get(self.version, []):
+            patch(self, **patchfile)
         if self.options.with_opt_patches:
-            for patch in self.conan_data.get("patches", {}).get("{}-opt".format(self.version), []):
-                tools.patch(**patch)
+            for patchfile in self.conan_data.get("patches", {}).get("{}-opt".format(self.version), []):
+                patch(self, **patchfile)
         if self.settings.os == "Linux":
             self._build_make()
         elif self.settings.os == "Windows":
@@ -164,14 +164,14 @@ class MdnsResponderConan(ConanFile):
 
     def _install_make(self):
         for dir in ["bin", "include", "lib", "sbin"]:
-            tools.mkdir(os.path.join(self.package_folder, dir))
-        with tools.chdir(self._posix_folder):
+            mkdir(self, os.path.join(self.package_folder, dir))
+        with chdir(self, self._posix_folder):
             autotools = self._configure_autotools()
             autotools.make(args=self._make_install_args, target=self._make_install_targets)
         # package the daemon in bin too
-        tools.rename(os.path.join(self.package_folder, "sbin", "mdnsd"),
+        rename(self, os.path.join(self.package_folder, "sbin", "mdnsd"),
                      os.path.join(self.package_folder, "bin", "mdnsd"))
-        tools.rmdir(os.path.join(self.package_folder, "sbin"))
+        rmdir(self, os.path.join(self.package_folder, "sbin"))
         symlinks.absolute_to_relative_symlinks(self, self.package_folder)
 
     def _msvc_build_folder(self, *argv):
@@ -183,7 +183,7 @@ class MdnsResponderConan(ConanFile):
         self.copy("dnssd.dll", dst="bin", src=self._msvc_build_folder("mDNSWindows", "DLL"))
         self.copy("dnssdStatic.lib", dst="lib", src=self._msvc_build_folder("mDNSWindows", "DLLStub"))
         # rename consistently with Bonjour SDK
-        tools.rename(src=os.path.join(self.package_folder, "lib", "dnssdStatic.lib"),
+        rename(self, src=os.path.join(self.package_folder, "lib", "dnssdStatic.lib"),
                      dst=os.path.join(self.package_folder, "lib", "dnssd.lib"))
         self.copy("dns-sd.exe", dst="bin", src=self._msvc_build_folder("Clients", "DNS-SD.VisualStudio"))
 
