59c6071aa6b78032c7c11041e03d8ff879cf937a
Seadex-GmbH <125141609+Seadex-GmbH@users.noreply.github.com>
2023-04-12 08:21:53 +0200

    Minimum Conan version set to 1.53.0
    Co-authored-by: Jordan Williams <jordan@jwillikers.com>

--- recipes/seadex-essentials/all/conanfile.py
+++ recipes/seadex-essentials/all/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.scm import Version
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 import os
 
-required_conan_version = ">=1.59"
+required_conan_version = ">=1.53.0"
 
 class SeadexEssentialsConan(ConanFile):
     name = "seadex-essentials"
