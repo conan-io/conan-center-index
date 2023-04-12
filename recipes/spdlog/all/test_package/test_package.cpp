68737ca9adc7ed84ec79c1f44830876eb5de1834
Chris Mc <christopherm@jfrog.com>
2023-04-12 14:19:24 -0700

    fixup: make sure to test bundle is missing

--- recipes/spdlog/all/test_package/test_package.cpp
+++ recipes/spdlog/all/test_package/test_package.cpp
@@ -1,6 +1,10 @@
 #include <cstdlib>
 #include "spdlog/spdlog.h"
 
+#if __has_include("spdlog/fmt/bundled/core.h")
+   #error "bundled fmt within spdlog should not be present"
+#endif
+
 int main(void) {
     spdlog::info("Welcome to spdlog version {}.{}.{}  !", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
     return EXIT_SUCCESS;
