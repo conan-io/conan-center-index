37984d7b38588037f1284d298b531ead679eb044
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-04-12 09:31:47 +0100

    Update recipes/ruy/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/ruy/all/conanfile.py
+++ recipes/ruy/all/conanfile.py
@@ -66,8 +66,7 @@ class RuyConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-                destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
