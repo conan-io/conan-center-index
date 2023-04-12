3d92de4727d26ee89b7a7b4f10deabf82838b56b
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2023-04-12 13:26:16 +0300

    try `192` as `min_vs`

--- recipes/sbepp/all/conanfile.py
+++ recipes/sbepp/all/conanfile.py
@@ -77,7 +77,7 @@ class PackageConan(ConanFile):
         if self.settings.compiler.cppstd:
             check_min_cppstd(self, self._min_cppstd)
 
-        check_min_vs(self, 191)
+        check_min_vs(self, 192)
         if not is_msvc(self):
             minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
             if minimum_version and Version(self.settings.compiler.version) < minimum_version:
