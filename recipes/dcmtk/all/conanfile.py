e70754033dde7291882586c8fa464de9b5e00e2f
Rasmus Thomsen <rasmus.thomsen@dampsoft.de>
2023-04-14 13:19:10 +0200

    dcmtk: remove get_safe

--- recipes/dcmtk/all/conanfile.py
+++ recipes/dcmtk/all/conanfile.py
@@ -126,7 +126,7 @@ class DCMTKConan(ConanFile):
         tc.variables["DCMTK_WITH_XML"] = self.options.with_libxml2
         if self.options.with_libxml2:
             tc.variables["WITH_LIBXMLINC"] = self.dependencies["libxml2"].package_folder
-            tc.variables["WITH_LIBXML_SHARED"] = self.options["libxml2"].get_safe("shared")
+            tc.variables["WITH_LIBXML_SHARED"] = self.options["libxml2"].shared
         tc.variables["DCMTK_WITH_ZLIB"] = self.options.with_zlib
         if self.options.with_zlib:
             tc.variables["WITH_ZLIBINC"] = self.dependencies["zlib"].package_folder
