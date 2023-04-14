c51eb419ff976a320856fe26ac686a57f4c43903
Jan Vcelak <jv@fcelda.cz>
2023-04-14 13:34:55 +0200

    ldns: add test_v1_package

---
+++ recipes/ldns/all/test_v1_package/conanfile.py
@@ -0,0 +1,17 @@
+from conans import ConanFile, CMake
+from conans.tools.build import cross_building
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "pkg_config"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not cross_building(self):
+            self.run(os.path.join(".", "example"))
