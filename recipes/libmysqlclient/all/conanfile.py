a58cdf3c3fec755c3b128bd23b49cb87ec0606c0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-17 23:06:53 +0200

    bump openssl

--- recipes/libmysqlclient/all/conanfile.py
+++ recipes/libmysqlclient/all/conanfile.py
@@ -62,7 +62,7 @@ class LibMysqlClientCConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("openssl/1.1.1t")
+        self.requires("openssl/3.1.0")
         self.requires("zlib/1.2.13")
         self.requires("zstd/1.5.5")
         self.requires("lz4/1.9.4")
