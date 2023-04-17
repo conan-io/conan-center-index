58317efc049161a9ef30947d7aedfcf2e7ed92b6
Federico De Felici <fdf8210@gmail.com>
2023-04-17 14:50:23 +0200

    Bump Clove-Unit to v2.4.0 and migration to conan2

--- recipes/clove-unit/all/conanfile.py
+++ recipes/clove-unit/all/conanfile.py
@@ -1,4 +1,7 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.layout import basic_layout
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
+import os
 
 required_conan_version = ">=1.43.0"
 
@@ -10,21 +13,37 @@ class CloveUnitConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     license = "MIT"
     no_copy_source = True
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    # Use the export_sources(self) method instead of the exports_sources attribute.
+    # This allows finer grain exportation of patches per version
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def layout(self):
+       basic_layout(self, src_folder=".")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+       get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="clove-unit.h", dst="include", src=self._source_subfolder)
+        #copy(pattern="LICENSE", dst="licenses", src=self.source_folder)
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, pattern="clove-unit.h", 
+             dst=os.path.join(self.package_folder, "include"),
+            src=os.path.join(self.source_folder, "include")
+        )
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def package_info(self):
+        # Folders not used for header-only
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.filenames["cmake_find_package"] = "CloveUnit"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "CloveUnit"
         self.cpp_info.names["cmake_find_package"] = "CloveUnit"
         self.cpp_info.names["cmake_find_package_multi"] = "CloveUnit"
