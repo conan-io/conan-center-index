426302bd75251e190401526b60d4804a20fd15af
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-18 11:28:42 +0200

    bump openssl

--- recipes/opusfile/all/conanfile.py
+++ recipes/opusfile/all/conanfile.py
@@ -58,7 +58,7 @@ class OpusFileConan(ConanFile):
         self.requires("ogg/1.3.5")
         self.requires("opus/1.3.1")
         if self.options.http:
-            self.requires("openssl/1.1.1s")
+            self.requires("openssl/3.1.0")
 
     def validate(self):
         if is_msvc(self) and self.options.shared:
