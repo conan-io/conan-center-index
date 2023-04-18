1f1504aa641ab155568274d36f98d7171c759ca2
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-18 11:29:15 +0200

    add package_type

--- recipes/opusfile/all/conanfile.py
+++ recipes/opusfile/all/conanfile.py
@@ -18,6 +18,7 @@ class OpusFileConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/xiph/opusfile"
     license = "BSD-3-Clause"
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
