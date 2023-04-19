2c9643abbac18887a66e8e8ba564a51704909798
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-19 10:19:03 +0200

    modernize more

--- recipes/aws-c-sdkutils/all/conanfile.py
+++ recipes/aws-c-sdkutils/all/conanfile.py
@@ -1,17 +1,19 @@
 from conan import ConanFile
-from conan.tools.files import get, copy, rmdir
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import get, copy, rmdir
 import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
+
 
 class AwsCSDKUtils(ConanFile):
     name = "aws-c-sdkutils"
     description = "C99 library implementing AWS SDK specific utilities. Includes utilities for ARN parsing, reading AWS profiles, etc..."
-    license = "Apache-2.0",
+    license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/awslabs/aws-c-sdkutils"
     topics = ("aws", "amazon", "cloud", "utility", "ARN")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -28,18 +30,9 @@ class AwsCSDKUtils(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
-        try:
-            del self.settings.compiler.libcxx
-        except Exception:
-            pass
-        try:
-            del self.settings.compiler.cppstd
-        except Exception:
-            pass
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -48,8 +41,7 @@ class AwsCSDKUtils(ConanFile):
         self.requires("aws-c-common/0.8.2")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-                  destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
