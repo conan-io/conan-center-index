4d60d7a3485cf3a3e66b18029c3612172cf2f69f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-19 11:24:00 +0200

    more elegant way to define target for legacy generators

--- recipes/aws-c-auth/all/conanfile.py
+++ recipes/aws-c-auth/all/conanfile.py
@@ -1,9 +1,10 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import get, copy, rmdir
+from conan.tools.files import get, copy, rmdir, save
 from conan.tools.scm import Version
 
 import os
+import textwrap
 
 required_conan_version = ">=1.53.0"
 
@@ -76,27 +77,34 @@ class AwsCAuth(ConanFile):
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "aws-c-auth"))
 
+        # TODO: to remove in conan v2 once legacy generators removed
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"AWS::aws-c-auth": "aws-c-auth::aws-c-auth"}
+        )
+
+    def _create_cmake_module_alias_targets(self, module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent(f"""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """)
+        save(self, module_file, content)
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join("lib", "cmake", f"conan-official-{self.name}-targets.cmake")
+
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "aws-c-auth")
         self.cpp_info.set_property("cmake_target_name", "AWS::aws-c-auth")
-        # TODO: back to global scope once conan v1 support dropped
-        self.cpp_info.components["aws-c-auth-lib"].libs = ["aws-c-auth"]
+        self.cpp_info.libs = ["aws-c-auth"]
         if self.options.shared:
-            self.cpp_info.components["aws-c-auth-lib"].defines.append("AWS_AUTH_USE_IMPORT_EXPORT")
+            self.cpp_info.defines.append("AWS_AUTH_USE_IMPORT_EXPORT")
 
-        # TODO: to remove once conan v1 support dropped
-        self.cpp_info.filenames["cmake_find_package"] = "aws-c-auth"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "aws-c-auth"
-        self.cpp_info.names["cmake_find_package"] = "AWS"
-        self.cpp_info.names["cmake_find_package_multi"] = "AWS"
-        self.cpp_info.components["aws-c-auth-lib"].names["cmake_find_package"] = "aws-c-auth"
-        self.cpp_info.components["aws-c-auth-lib"].names["cmake_find_package_multi"] = "aws-c-auth"
-        self.cpp_info.components["aws-c-auth-lib"].set_property("cmake_target_name", "AWS::aws-c-auth")
-        self.cpp_info.components["aws-c-auth-lib"].requires = [
-            "aws-c-common::aws-c-common",
-            "aws-c-cal::aws-c-cal",
-            "aws-c-io::aws-c-io",
-            "aws-c-http::aws-c-http",
-        ]
-        if Version(self.version) >= "0.6.5":
-            self.cpp_info.components["aws-c-auth-lib"].requires.append("aws-c-sdkutils::aws-c-sdkutils")
+        # TODO: to remove in conan v2 once cmake_find_package* generators removed
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
