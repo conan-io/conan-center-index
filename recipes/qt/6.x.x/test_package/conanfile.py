76f16e92c4885423e308c6c0c87099177db5f676
Martin Delille <martin@delille.org>
2023-04-19 18:09:40 +0200

    Apply suggestions from code review
    Co-authored-by: ericLemanissier <ericLemanissier@users.noreply.github.com>

--- recipes/qt/6.x.x/test_package/conanfile.py
+++ recipes/qt/6.x.x/test_package/conanfile.py
@@ -51,18 +51,18 @@ class TestPackageConan(ConanFile):
 
                 value = _getenvpath('CC')
                 if value:
-                    args.append(f"QMAKE_CC={value}")
+                    args.append(f"QMAKE_CC=\"{value}\"")
 
                 value = _getenvpath('CXX')
                 if value:
-                    args.append(f"QMAKE_CXX={value}")
+                    args.append(f"QMAKE_CXX=\"{value}\"")
 
                 value = _getenvpath('LD')
                 if value:
-                    args.append(f"QMAKE_LINK_C={value}")
-                    args.append(f"QMAKE_LINK_C_SHLIB={value}")
-                    args.append(f"QMAKE_LINK={value}")
-                    args.append(f"QMAKE_LINK_SHLIB={value}")
+                    args.append(f"QMAKE_LINK_C=\"{value}\"")
+                    args.append(f"QMAKE_LINK_C_SHLIB=\"{value}\"")
+                    args.append(f"QMAKE_LINK=\"{value}\"")
+                    args.append(f"QMAKE_LINK_SHLIB=\"{value}\"")
 
                 self.run("qmake %s" % " ".join(args), run_environment=True)
                 if tools.os_info.is_windows:
