ac94233345843eff33b945bde3968a96d12f2f41
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-21 00:54:59 +0200

    fix traits

--- recipes/aws-crt-cpp/all/conanfile.py
+++ recipes/aws-crt-cpp/all/conanfile.py
@@ -47,22 +47,23 @@ class AwsCrtCpp(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
+        self.requires("aws-c-cal/0.5.13", transitive_headers=True)
         self.requires("aws-c-common/0.8.2", transitive_headers=True)
         self.requires("aws-checksums/0.1.13")
         if Version(self.version) < "0.17.29":
-            self.requires("aws-c-io/0.10.20")
-            self.requires("aws-c-http/0.6.13")
             self.requires("aws-c-auth/0.6.11", transitive_headers=True)
-            self.requires("aws-c-s3/0.1.37")
-            self.requires("aws-c-mqtt/0.7.10", transitive_headers=True)
             self.requires("aws-c-event-stream/0.2.7")
+            self.requires("aws-c-http/0.6.13", transitive_headers=True)
+            self.requires("aws-c-io/0.10.20", transitive_headers=True)
+            self.requires("aws-c-mqtt/0.7.10", transitive_headers=True)
+            self.requires("aws-c-s3/0.1.37")
         else:
-            self.requires("aws-c-io/0.13.4")
-            self.requires("aws-c-http/0.6.22")
             self.requires("aws-c-auth/0.6.17", transitive_headers=True)
-            self.requires("aws-c-s3/0.1.49")
-            self.requires("aws-c-mqtt/0.7.12", transitive_headers=True)
             self.requires("aws-c-event-stream/0.2.15")
+            self.requires("aws-c-http/0.6.22", transitive_headers=True)
+            self.requires("aws-c-io/0.13.4", transitive_headers=True)
+            self.requires("aws-c-mqtt/0.7.12", transitive_headers=True)
+            self.requires("aws-c-s3/0.1.49")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
