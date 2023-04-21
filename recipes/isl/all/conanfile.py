0718fcf8f8a2763f7fe33e5787a94eb97f5aaff9
doocman <rvonsonsnadtz@gmail.com>
2023-04-21 21:48:21 +0200

    fix ISL to build under conan 2.0

--- recipes/isl/all/conanfile.py
+++ recipes/isl/all/conanfile.py
@@ -1,6 +1,9 @@
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from conan import ConanFile, tools
+from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conans.errors import ConanInvalidConfiguration
+from conan.tools.env import VirtualBuildEnv
 from contextlib import contextmanager
+from conan.tools.files import get, copy, rmdir
 import os
 
 required_conan_version = ">=1.33.0"
@@ -49,7 +52,7 @@ class IslConan(ConanFile):
         if self.options.with_int != "gmp":
             # FIXME: missing imath recipe
             raise ConanInvalidConfiguration("imath is not (yet) available on cci")
-        if self.settings.compiler == "Visual Studio" and tools.Version(self.settings.compiler.version) < 16 and self.settings.compiler.runtime == "MDd":
+        if self.settings.compiler == "msvc" and tools.Version(self.settings.compiler.version) < 16 and self.settings.compiler.runtime == "MDd":
             # gmp.lib(bdiv_dbm1c.obj) : fatal error LNK1318: Unexpected PDB error; OK (0)
             raise ConanInvalidConfiguration("isl fails to link with this version of visual studio and MDd runtime")
 
@@ -64,16 +67,20 @@ class IslConan(ConanFile):
     def build_requirements(self):
         if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/cci.latest")
-        if self.settings.compiler == "Visual Studio":
+        if self.settings.compiler == "msvc":
             self.build_requires("automake/1.16.4")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        get(self, **self.conan_data["sources"][self.version],
                   strip_root=True, destination=self._source_subfolder)
+    
+    def layout(self):
+        self.folders.source = self._source_subfolder
+        self.folders.build = self._source_subfolder
 
     @contextmanager
     def _build_context(self):
-        if self.settings.compiler == "Visual Studio":
+        if self.settings.compiler == "msvc":
             with tools.vcvars(self.settings):
                 env = {
                     "AR": "{} lib".format(tools.unix_path(self.deps_user_info["automake"].ar_lib)),
@@ -88,45 +95,51 @@ class IslConan(ConanFile):
                     yield
         else:
             yield
-
-    def _configure_autotools(self):
+    
+    def generate(self):
         if self._autotools:
             return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        # TODO: Remove when conan 2.0 is released as this will be default behaviour
+        buildenv = VirtualBuildEnv(self)
+        buildenv.generate()
+        
+        self._autotools = AutotoolsToolchain(self)
+
+        # self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
         self._autotools.libs = []
         yes_no = lambda v: "yes" if v else "no"
-        conf_args = [
+        self._autotools.configure_args.extend([
             "--with-int={}".format(self.options.with_int),
             "--enable-portable-binary",
             "--enable-shared={}".format(yes_no(self.options.shared)),
             "--enable-static={}".format(yes_no(not self.options.shared)),
-        ]
+        ])
         if self.options.with_int == "gmp":
-            conf_args.extend([
+            self._autotools.configure_args.extend([
                 "--with-gmp=system",
-                "--with-gmp-prefix={}".format(self.deps_cpp_info["gmp"].rootpath.replace("\\", "/")),
+                #"--with-gmp-prefix={}".format(self.deps_cpp_info["gmp"].rootpath.replace("\\", "/")),
+                "--with-gmp-prefix={}".format(self.dependencies["gmp"].package_folder.replace("\\", "/")),
             ])
-        if self.settings.compiler == "Visual Studio":
+        if self.settings.compiler == "msvc":
             if tools.Version(self.settings.compiler.version) >= 15:
-                self._autotools.flags.append("-Zf")
+                self._autotools.cflags.append("-Zf")
             if tools.Version(self.settings.compiler.version) >= 12:
-                self._autotools.flags.append("-FS")
-        self._autotools.configure(args=conf_args, configure_dir=self._source_subfolder)
-        return self._autotools
+                self._autotools.cflags.append("-FS")
+        self._autotools.generate()
 
     def build(self):
-        with self._build_context():
-            autotools = self._configure_autotools()
-            autotools.make()
+        autotools = Autotools(self)
+        autotools.autoreconf()
+        autotools.configure()
+        autotools.make()
 
     def package(self):
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
-        with self._build_context():
-            autotools = self._configure_autotools()
-            autotools.install()
+        copy(self, "LICENSE", src=self._source_subfolder, dst="licenses")
+        autotools = Autotools(self)
+        autotools.install()
 
         os.unlink(os.path.join(os.path.join(self.package_folder, "lib", "libisl.la")))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.names["pkg_config"] = "isl"
--- recipes/isl/all/test_package/conanfile.py
+++ recipes/isl/all/test_package/conanfile.py
@@ -1,17 +1,22 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile, tools
+from conan.tools.cmake import CMake
+from conan.tools.build import cross_building
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "CMakeToolchain", 'CMakeDeps'
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
     def test(self):
-        if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if not cross_building(self):
+            bin_path = os.path.join(".", "test_package")
+            self.run(bin_path, env='conanrun')
