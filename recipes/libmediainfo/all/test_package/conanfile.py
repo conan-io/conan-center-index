ab6a4f9e9a56edc026c8246293cd81e0c41ddd8f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-22 01:18:22 +0200

    conan v2 support

--- recipes/libmediainfo/all/test_package/conanfile.py
+++ recipes/libmediainfo/all/test_package/conanfile.py
@@ -1,4 +1,6 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
 
 # testsrc.mp4 generated with:
@@ -6,17 +8,28 @@ import os
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package_multi"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["LIBMEDIAINFO_SHARED"] = self.dependencies["libmediainfo"].options.shared
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["LIBMEDIAINFO_SHARED"] = self.options["libmediainfo"].shared
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
             testsrc = os.path.join(self.source_folder, "testsrc.mp4")
-            self.run("{} {}".format(bin_path, testsrc), run_environment=True)
+            self.run(f"{bin_path} {testsrc}", env="conanrun")
