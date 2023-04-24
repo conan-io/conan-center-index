cd2651a86a16c1d3888062c24f2bec044a196106
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-24 21:50:50 +0200

    version range workaround of openssl 1.x.x for conan v1

--- recipes/libmysqlclient/all/conanfile.py
+++ recipes/libmysqlclient/all/conanfile.py
@@ -1,4 +1,4 @@
-from conan import ConanFile
+from conan import ConanFile, conan_version
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
 from conan.tools.build import check_min_cppstd, cross_building, stdcpp_library
@@ -63,7 +63,7 @@ class LibMysqlClientCConan(ConanFile):
 
     def requirements(self):
         if Version(self.version) < "8.0.30":
-            self.requires("openssl/[>=1.1 <2]")
+            self.requires("openssl/{}".format("1.1.1t" if Version(conan_version).major < "2" else "[>=1.1 <2]"))
         else:
             self.requires("openssl/[>=1.1 <4]")
         self.requires("zlib/1.2.13")
@@ -112,7 +112,7 @@ class LibMysqlClientCConan(ConanFile):
 
     def build_requirements(self):
         if is_apple_os(self) and not self._cmake_new_enough("3.18"):
-            self.tool_requires("cmake/[>=3.18 <4]")
+            self.tool_requires("cmake/[^3.18]")
         if self.settings.os == "FreeBSD" and not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/1.9.3")
 
