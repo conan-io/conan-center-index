5e8a6961939d28408d37bbf4da1b3adc067554ce
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-24 20:01:15 +0200

    use version range for openssl

--- recipes/amqp-cpp/all/conanfile.py
+++ recipes/amqp-cpp/all/conanfile.py
@@ -44,7 +44,7 @@ class AmqpcppConan(ConanFile):
 
     def requirements(self):
         if self.options.get_safe("linux_tcp_module"):
-            self.requires("openssl/3.1.0")
+            self.requires("openssl/[>=1.1 <4]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
