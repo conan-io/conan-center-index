b07d3379162f10c34574cf12c76ac6e4888a4fe9
Martin Valgur <martin.valgur@gmail.com>
2023-04-24 23:22:42 +0300

    dlpack: fix test_package.cpp output

--- recipes/dlpack/all/test_package/test_package.cpp
+++ recipes/dlpack/all/test_package/test_package.cpp
@@ -4,9 +4,16 @@
 
 int main()
 {
-    int major = (DLPACK_VERSION >> 6) & 7;
-    int minor = (DLPACK_VERSION >> 3) & 7;
-    int patch = DLPACK_VERSION & 7;
+    int major, minor, patch;
+    if (DLPACK_VERSION < 48) {
+        major = (DLPACK_VERSION >> 6) & 7;
+        minor = (DLPACK_VERSION >> 3) & 7;
+        patch = DLPACK_VERSION & 7;
+    } else {
+        major = 0;
+        minor = DLPACK_VERSION / 10;
+        patch = DLPACK_VERSION % 10;
+    }
     std::cout << "dlpack version: " << major << "." << minor << "." << patch << std::endl;
     return EXIT_SUCCESS;
 }
