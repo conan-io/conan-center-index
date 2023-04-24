cc02602cdfec574fa0fe8194f40002fbd7cc85ca
Martin Valgur <martin.valgur@gmail.com>
2023-04-24 23:13:11 +0300

    dlpack: migrate to Conan 2.0

--- recipes/dlpack/all/conanfile.py
+++ recipes/dlpack/all/conanfile.py
@@ -1,27 +1,32 @@
 import os
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.files import copy, get
 
 required_conan_version = ">=1.33.0"
 
 
 class DlpackConan(ConanFile):
     name = "dlpack"
+    package_type = "header-library"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/dmlc/dlpack"
     description = "RFC for common in-memory tensor structure and operator interface for deep learning system"
-    topics = ("conan", "dlpack", "tensor", "operator")
+    topics = ("dlpack", "tensor", "operator")
     no_copy_source = True
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy("LICENSE", dst="licenses")
-        self.copy("*dlpack.h", keep_path=True)
-
-    def package_id(self):
-        self.info.header_only()
+        copy(self, pattern="LICENSE", src=self.source_folder, dst="licenses")
+        copy(
+            self,
+            pattern="*.h",
+            src=os.path.join(self.source_folder, "include"),
+            dst=os.path.join(self.package_folder, "include"),
+        )
 
     def package_info(self):
+        self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
--- recipes/dlpack/all/test_package/conanfile.py
+++ recipes/dlpack/all/test_package/conanfile.py
@@ -1,17 +1,26 @@
-from conans import ConanFile, CMake, tools
 import os
 
+from conan import ConanFile
+from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, cmake_layout
 
 class TestPackageConan(ConanFile):
+    test_type = "explicit"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if not cross_building(self):
+            cmd = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(cmd, env="conanrun")
