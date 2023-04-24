24ab717c1544a3ba0cf9f562787e935842c53dbb
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-24 20:42:30 +0200

    version range workaround for conan v1 client

--- recipes/civetweb/all/conanfile.py
+++ recipes/civetweb/all/conanfile.py
@@ -83,7 +83,7 @@ class CivetwebConan(ConanFile):
     def requirements(self):
         if self.options.with_ssl:
             if Version(self.version) < "1.16":
-                self.requires("openssl/[^1]")
+                self.requires("openssl/[>=1 < 2]")
             else:
                 self.requires("openssl/[>=1 <4]")
         if self.options.get_safe("with_zlib"):
