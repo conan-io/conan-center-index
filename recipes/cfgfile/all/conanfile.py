111561bd17fe8abda04cd16d1943dfc6974bdefd
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-04-25 18:40:43 +0200

    use version range for cmake

--- recipes/cfgfile/all/conanfile.py
+++ recipes/cfgfile/all/conanfile.py
@@ -54,20 +54,8 @@ class CfgfileConan(ConanFile):
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.",
             )
 
-    def _cmake_new_enough(self, required_version):
-        try:
-            import re
-            from io import StringIO
-            output = StringIO()
-            self.run("cmake --version", output)
-            m = re.search(r"cmake version (\d+\.\d+\.\d+)", output.getvalue())
-            return Version(m.group(1)) >= required_version
-        except:
-            return False
-
     def build_requirements(self):
-        if Version(self.version) >= "0.2.11" and not self._cmake_new_enough("3.19"):
-            self.tool_requires("cmake/3.25.2")
+        self.tool_requires("cmake/[>=3.19 <4]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
