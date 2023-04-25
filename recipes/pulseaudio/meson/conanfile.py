5352a060c54b7a0b62db6d269b77b570780b084f
Esteve Soria <e.soria@lumicks.com>
2023-04-25 11:59:44 +0200

    pulseaudio: Add version 16.1

---
+++ recipes/pulseaudio/meson/conanfile.py
@@ -0,0 +1,173 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import cross_building
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.tools.files import copy, get, rm, rmdir
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.meson import Meson, MesonToolchain
+from conan.tools.layout import basic_layout
+import os
+
+required_conan_version = ">=1.53.0"
+
+
+class PulseAudioConan(ConanFile):
+    name = "pulseaudio"
+    description = "PulseAudio is a sound system for POSIX OSes, meaning that it is a proxy for sound applications."
+    topics = ("sound",)
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "http://pulseaudio.org/"
+    license = "LGPL-2.1"
+
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_alsa": [True, False],
+        "with_glib": [True, False],
+        "with_fftw": [True, False],
+        "with_x11": [True, False],
+        "with_openssl": [True, False],
+        "with_dbus": [True, False],
+        "with_gdbm": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_alsa": True,
+        "with_glib": True,
+        "with_fftw": False,
+        "with_x11": True,
+        "with_openssl": True,
+        "with_dbus": False,
+        "with_gdbm": False,
+    }
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+        if not self.options.with_dbus:
+            del self.options.with_fftw
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("libiconv/1.17")
+        self.requires("libsndfile/1.2.0")
+        self.requires("libcap/2.66")
+        self.requires("libtool/2.4.7")
+        if self.options.with_alsa:
+            self.requires("libalsa/1.2.7.2")
+        if self.options.with_glib:
+            self.requires("glib/2.76.1")
+        if self.options.get_safe("with_fftw"):
+            self.requires("fftw/3.3.10")
+        if self.options.with_x11:
+            self.requires("xorg/system")
+        if self.options.with_openssl:
+            self.requires("openssl/3.1.0")
+        if self.options.with_dbus:
+            self.requires("dbus/1.15.2")
+        if self.options.with_gdbm:
+            self.requires("gdbm/1.19")
+
+    def validate(self):
+        if self.settings.os != "Linux":
+            raise ConanInvalidConfiguration("pulseaudio supports only linux currently")
+
+        if self.options.get_safe("with_fftw"):
+            fftw_precision = self.dependencies["fftw"].options.precision
+            if fftw_precision != "single":
+                raise ConanInvalidConfiguration(
+                    f"Pulse audio cannot use fftw {fftw_precision} precision. "
+                    "Either set option fftw:precision=single or pulseaudio:with_fftw=False"
+                )
+
+    def build_requirements(self):
+        self.tool_requires("gettext/0.21")
+        self.tool_requires("libtool/2.4.7")
+        # CCI policy assumes that Meson may not be installed on consumers machine
+        self.tool_requires("meson/0.63.3")
+        if not self.conf.get("tools.gnu:pkg_config", check_type=str):
+            self.tool_requires("pkgconf/1.9.3")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = MesonToolchain(self)
+
+        yes_no = lambda v: "enabled" if v else "disabled"
+        tc.project_options["daemon"] = False
+        tc.project_options["tests"] = False
+        tc.project_options["doxygen"] = False
+        tc.project_options["systemduserunitdir"] = os.path.join(self.build_folder, 'ignore')
+        tc.project_options["udevrulesdir"] = "${prefix}/bin/udev/rules.d"
+        for lib in ["alsa", "x11", "openssl", "dbus"]:
+            tc.project_options[lib] = yes_no(getattr(self.options, f'with_{lib}'))
+
+        # TODO: Default is tdb but the package is not available in conan
+        tc.project_options["database"] = "gdbm" if self.options.with_gdbm else "simple"
+        tc.generate()
+        # In case there are dependencies listed on requirements, PkgConfigDeps should be used
+        tc = PkgConfigDeps(self)
+        tc.generate()
+        # In case there are dependencies listed on build_requirements, VirtualBuildEnv should be used
+        tc = VirtualBuildEnv(self)
+        tc.generate()
+
+    def build(self):
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
+
+    def package(self):
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        meson = Meson(self)
+        meson.install()
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "lib"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
+
+        rmdir(self, os.path.join(self.package_folder, "etc"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"), recursive=True)
+
+    def package_info(self):
+        self.cpp_info.components["pulse"].set_property("pkg_config_name", "libpulse")
+        self.cpp_info.components["pulse"].libs = ["pulse", f"pulsecommon-{self.version}"]
+        self.cpp_info.components["pulse"].libdirs.append(os.path.join("lib", "pulseaudio"))
+        self.cpp_info.components["pulse"].requires = ["libiconv::libiconv", "libsndfile::libsndfile", "libcap::libcap", "libtool::libtool"]
+        if self.options.with_alsa:
+            self.cpp_info.components["pulse"].requires.append("libalsa::libalsa")
+        if self.options.get_safe("with_fftw"):
+            self.cpp_info.components["pulse"].requires.append("fftw::fftw")
+        if self.options.with_x11:
+            self.cpp_info.components["pulse"].requires.append("xorg::xorg")
+        if self.options.with_openssl:
+            self.cpp_info.components["pulse"].requires.append("openssl::openssl")
+        if self.options.with_dbus:
+            self.cpp_info.components["pulse"].requires.append("dbus::dbus")
+
+        self.cpp_info.components["pulse-simple"].set_property("pkg_config_name", "libpulse-simple")
+        self.cpp_info.components["pulse-simple"].libs = ["pulse-simple"]
+        self.cpp_info.components["pulse-simple"].defines.append("_REENTRANT")
+        self.cpp_info.components["pulse-simple"].requires = ["pulse"]
+
+        if self.options.with_glib:
+            self.cpp_info.components["pulse-mainloop-glib"].set_property("pkg_config_name", "libpulse-mainloop-glib")
+            self.cpp_info.components["pulse-mainloop-glib"].libs = ["pulse-mainloop-glib"]
+            self.cpp_info.components["pulse-mainloop-glib"].defines.append("_REENTRANT")
+            self.cpp_info.components["pulse-mainloop-glib"].requires = ["pulse", "glib::glib-2.0"]
+
+        # FIXME: add cmake generators when conan can generate PULSEAUDIO_INCLUDE_DIR PULSEAUDIO_LIBRARY vars
