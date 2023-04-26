1be2f130dc8dc1fad4c4e3d196a35223991ff61a
Chris Mc <christopherm@jfrog.com>
2023-04-26 10:00:19 -0700

    Update recipes/c-blosc2/all/conanfile.py
    Co-authored-by: Rubén Rincón Blanco <git@rinconblanco.es>

--- recipes/c-blosc2/all/conanfile.py
+++ recipes/c-blosc2/all/conanfile.py
@@ -75,7 +75,7 @@ class CBlosc2Conan(ConanFile):
 
     def build_requirements(self):
         if Version(self.version) >= "2.4.1":
-            self.tool_requires("cmake/[>=3.16 <4]")
+            self.tool_requires("cmake/[>=3.16.3 <4]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
