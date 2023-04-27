412dbdb7f9c7f46da7fe17654ad93d1cefc98542
FireWolf <austere.j@gmail.com>
2023-04-26 23:39:40 -0700

    elfutils: Run compiled utilities under the build environment.

--- recipes/elfutils/all/test_package/conanfile.py
+++ recipes/elfutils/all/test_package/conanfile.py
@@ -21,11 +21,11 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        self.run("eu-ar --version", env="conanrun")
-        bin_path = os.path.join("bin", "test_package")
+        self.run("eu-ar --version", env="conanbuild")
+        bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
         archive_path = "archive.a"
-        self.run(f"eu-ar r {archive_path} {bin_path}", env="conanrun")
-        self.run(f"eu-objdump -d {bin_path}", env="conanrun")
+        self.run(f"eu-ar r {archive_path} {bin_path}", env="conanbuild")
+        self.run(f"eu-objdump -d {bin_path}", env="conanbuild")
         if can_run(self):
             self.run(f"{bin_path} {bin_path}", env="conanrun")
             self.run(f"{bin_path} {archive_path}", env="conanrun")
