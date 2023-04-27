b7b9ce77d543ffab0d76529459044a234d039ed8
FireWolf <austere.j@gmail.com>
2023-04-26 22:20:09 -0700

    elfutils: Fix an issue that the library `bz2` cannot be found.

--- recipes/elfutils/all/conanfile.py
+++ recipes/elfutils/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile, Version
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps
 from conan.tools.layout import basic_layout
 from conan.tools.files import export_conandata_patches, apply_conandata_patches, copy, get, rm, rmdir
 from conan.tools.microsoft import unix_path
@@ -120,6 +120,8 @@ class ElfutilsConan(ConanFile):
             tc.configure_args.append("--enable-libdebuginfod" if self.options.libdebuginfod else "--disable-libdebuginfod")
         tc.configure_args.append(f"BUILD_STATIC={'0' if self.options.shared else '1'}")
         tc.generate()
+        deps = AutotoolsDeps(self)
+        deps.generate()
 
     def build(self):
         apply_conandata_patches(self)
