bd01a2128c17d3138dc8a189ee410d8cf2f4d855
Loic Gouarin <loic.gouarin@gmail.com>
2023-04-27 15:13:12 +0200

    update config

--- recipes/samurai/all/conanfile.py
+++ recipes/samurai/all/conanfile.py
@@ -14,9 +14,9 @@ class PackageConan(ConanFile):
     name = "samurai"
     description = "Intervals coupled with algebra of set to handle adaptive mesh refinement and operators on it"
     license = "BSD-3-Clause"
-    url = "https://github.com/hpc-maths/samurai"
-    homepage = "https://github.com/project/package"
-    topics = ("scientific computing", "adaptive mesh refinement", "AMR", "MRA", "header-only")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/hpc-maths/samurai"
+    topics = ("scientific computing", "adaptive mesh refinement", "header-only")
     package_type = "header-library"
     # Keep these or explain why it's not required for this particular case
     settings = "os", "arch", "compiler", "build_type"
