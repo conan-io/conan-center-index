2646a69f1966cbfc2cd303b625a8fef0faf55ea8
Joris Putcuyps <joris.putcuyps@gmail.com>
2023-04-29 14:04:00 +0200

    (#17250) Improve ArduinoJson and add 6.21.2
    * Add package_type and safer cppstd check for v1
    
    * Add version 6.21.2
    
    * Workaround for v1 C++11 check
    
    * Disable apple-clang for now
    
    * Allow apple-clang but require the cppstd
    
    * Fix github warnings
    
    * fixup: format prefix was missing
    
    ---------
    
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- recipes/arduinojson/all/conanfile.py
+++ recipes/arduinojson/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.tools.files import copy, get, save
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
 from conan.tools.build import check_min_cppstd
+from conan.errors import ConanInvalidConfiguration
 import os
 import textwrap
 
@@ -16,12 +17,29 @@ class ArduinojsonConan(ConanFile):
     topics = ("json", "arduino", "iot", "embedded", "esp8266")
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
+    package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "Visual Studio": "16",
+            "msvc": "192",
+            "gcc": "6",
+            "clang": "6",
+        }
+
     def validate(self):
         if Version(self.version) >= "6.21.0":
-            check_min_cppstd(self, 11)
+            if self.settings.compiler.get_safe("cppstd"):
+                check_min_cppstd(self, 11)
+            elif str(self.settings.compiler) == 'apple-clang':
+                raise ConanInvalidConfiguration("cppstd needs to be set on apple-clang to activate c++11.")
+            else:
+                minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+                if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+                    raise ConanInvalidConfiguration(f"{self.ref} requires C++11, which your compiler does not support.")
 
     def package_id(self):
         self.info.clear()
