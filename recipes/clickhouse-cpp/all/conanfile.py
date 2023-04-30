4e54da77f906f340c3d52d4ca7a5a0a5e12a0d9c
Jihadist <tomasiche@gmail.com>
2023-04-30 10:43:23 +0300

    compiler restrictions

--- recipes/clickhouse-cpp/all/conanfile.py
+++ recipes/clickhouse-cpp/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
 from conan.tools.build import check_min_cppstd
 from conan.errors import ConanInvalidConfiguration
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.51.3"
@@ -45,12 +46,20 @@ class ClickHouseCppConan(ConanFile):
             self.requires("benchmark/1.6.0")
 
     @property
-    def _min_cppstd(self):
-        return "17"
+    def _compilers_minimum_version(self):
+        return {
+            "Visual Studio": "15",
+            "msvc": "191",
+            "gcc": "7",
+            "clang": "6",
+        }
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++17, which your compiler does not support.")    
         if self.settings.os == "Windows" and self.options.shared:
             raise ConanInvalidConfiguration("Invalid configuration")
             # look at https://github.com/ClickHouse/clickhouse-cpp/pull/226
