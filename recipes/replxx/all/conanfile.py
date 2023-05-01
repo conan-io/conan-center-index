2c168f80aea64acd72b7c8507a09f65ae3463b56
toge <toge.mail@gmail.com>
2023-05-02 00:18:49 +0900

    remove unused modules

--- recipes/replxx/all/conanfile.py
+++ recipes/replxx/all/conanfile.py
@@ -1,11 +1,8 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime, is_msvc
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file
+from conan.tools.files import get, copy, rmdir, replace_in_file
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.env import VirtualBuildEnv
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
 
 required_conan_version = ">=1.53.0"
