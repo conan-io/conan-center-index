b1e03ad9aa441298f6839b5cab0c5a3f638221bc
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-05-01 09:24:01 +0200

    (#17314) arduinojson: fix cppstd check logic

--- recipes/arduinojson/all/conanfile.py
+++ recipes/arduinojson/all/conanfile.py
@@ -1,9 +1,8 @@
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get, save
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
-from conan.tools.build import check_min_cppstd
-from conan.errors import ConanInvalidConfiguration
 import os
 import textwrap
 
@@ -22,35 +21,22 @@ class ArduinojsonConan(ConanFile):
     no_copy_source = True
 
     @property
-    def _compilers_minimum_version(self):
-        return {
-            "Visual Studio": "16",
-            "msvc": "192",
-            "gcc": "6",
-            "clang": "6",
-        }
-
-    def validate(self):
-        if Version(self.version) >= "6.21.0":
-            if self.settings.compiler.get_safe("cppstd"):
-                check_min_cppstd(self, 11)
-            elif str(self.settings.compiler) == 'apple-clang':
-                raise ConanInvalidConfiguration("cppstd needs to be set on apple-clang to activate c++11.")
-            else:
-                minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-                if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-                    raise ConanInvalidConfiguration(f"{self.ref} requires C++11, which your compiler does not support.")
-
-    def package_id(self):
-        self.info.clear()
+    def _min_cppstd(self):
+        return "98" if Version(self.version) < "6.21.0" else "11"
 
     def layout(self):
         basic_layout(self, src_folder="src")
 
+    def package_id(self):
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
     def source(self):
         has_arduinojson_root=Version(self.version) < "6.18.2"
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=has_arduinojson_root)
+        get(self, **self.conan_data["sources"][self.version], strip_root=has_arduinojson_root)
 
     def build(self):
         pass
@@ -85,9 +71,7 @@ class ArduinojsonConan(ConanFile):
         self.cpp_info.set_property("cmake_file_name", "ArduinoJson")
         self.cpp_info.set_property("cmake_target_name", "ArduinoJson")
         self.cpp_info.bindirs = []
-        self.cpp_info.frameworkdirs = []
         self.cpp_info.libdirs = []
-        self.cpp_info.resdirs = []
 
         # TODO: to remove in conan v2 once cmake_find_package* generators removed
         self.cpp_info.names["cmake_find_package"] = "ArduinoJson"
