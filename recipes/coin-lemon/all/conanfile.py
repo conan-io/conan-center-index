458d46456639e3984f893599898700932f91b391
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-05-01 16:23:26 +0200

    allow to consume coin-lemon with C++20 or higher

--- recipes/coin-lemon/all/conanfile.py
+++ recipes/coin-lemon/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rmdir
 import os
@@ -38,6 +39,10 @@ class CoinLemonConan(ConanFile):
 
     def layout(self):
         cmake_layout(self, src_folder="src")
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, "11")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
