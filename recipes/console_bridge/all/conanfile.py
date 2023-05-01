0fabee9fc9bec048b70cbd32a581ed8d4cf601c8
Martin Valgur <martin.valgur@gmail.com>
2023-05-01 20:32:35 +0300

    console_bridge: use description from GitHub project summary

--- recipes/console_bridge/all/conanfile.py
+++ recipes/console_bridge/all/conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.53.0"
 
 class PackageConan(ConanFile):
     name = "console_bridge"
-    description = "A ROS-independent library that provides logging calls which mirror those found in rosconsole"
+    description = "A ROS-independent library for logging that seamlessly pipes into rosconsole/rosout for ROS-dependent packages"
     license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/ros/console_bridge"
