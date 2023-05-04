2f004646bc07a62d7b49188f71e0e92a2c8be093
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-05-04 13:09:55 +0200

    fix conditional set of shared & ssl options for conan v1 client

--- recipes/paho-mqtt-cpp/all/conanfile.py
+++ recipes/paho-mqtt-cpp/all/conanfile.py
@@ -1,4 +1,4 @@
-from conan import ConanFile
+from conan import ConanFile, conan_version
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, rmdir, get
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
@@ -44,8 +44,9 @@ class PahoMqttCppConan(ConanFile):
         if self.options.shared:
             self.options.rm_safe("fPIC")
 
-        self.options["paho-mqtt-c/*"].shared = self.options.shared
-        self.options["paho-mqtt-c/*"].ssl = self.options.ssl
+        suffix = "" if Version(conan_version).major < "2" else "/*"
+        self.options[f"paho-mqtt-c{suffix}"].shared = self.options.shared
+        self.options[f"paho-mqtt-c{suffix}"].ssl = self.options.ssl
 
     def layout(self):
         cmake_layout(self, src_folder="src")
