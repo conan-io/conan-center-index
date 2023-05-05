5c8c6989a8f2522f8bb6b37329784a2092ff603e
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-05-05 23:31:13 +0300

    ade: added build_requirements

--- recipes/ade/all/conanfile.py
+++ recipes/ade/all/conanfile.py
@@ -35,6 +35,9 @@ class AdeConan(ConanFile):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, 11)
 
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.2 <4]")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
