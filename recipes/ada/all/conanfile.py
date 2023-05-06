64c4f1e3ffe9ac845cd972fe15e79c6ba1976db2
toge <toge.mail@gmail.com>
2023-05-07 03:17:01 +0900

    rm fPIC on shared build

--- recipes/ada/all/conanfile.py
+++ recipes/ada/all/conanfile.py
@@ -44,6 +44,10 @@ class AdaConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
     def layout(self):
         cmake_layout(self, src_folder="src")
 
