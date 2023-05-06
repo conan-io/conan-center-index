c6b21c6c7bcbd785f7c09aef35131fb2aa869791
toge <toge.mail@gmail.com>
2023-05-07 00:54:39 +0900

    ada: add version 2.3.1, enable shared build

--- recipes/ada/all/conanfile.py
+++ recipes/ada/all/conanfile.py
@@ -15,13 +15,15 @@ class AdaConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/ada-url/ada"
     topics = ("url", "parser", "WHATWG")
-    package_type = "static-library"
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "fPIC": [True, False],
+        "shared": [True, False],
     }
     default_options = {
         "fPIC": True,
+        "shared": False,
     }
 
     @property
