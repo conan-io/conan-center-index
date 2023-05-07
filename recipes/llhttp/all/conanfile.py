97b961ada30b32c1100497a68dcb32efa3fde7f6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-05-07 23:51:38 +0200

    conan v2 support

--- recipes/llhttp/all/conanfile.py
+++ recipes/llhttp/all/conanfile.py
@@ -1,19 +1,20 @@
-from conans import ConanFile, tools, CMake
-from conan.tools.files import patch
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 import os
 
-required_conan_version = ">=1.43.0"
+required_conan_version = ">=1.53.0"
 
 
 class LlhttpParserConan(ConanFile):
     name = "llhttp"
-    description = "http request/response parser for c "
+    description = "http request/response parser for c"
     topics = ("http", "parser")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/nodejs/llhttp"
-    license = ("MIT",)
-    generators = ("cmake",)
-    settings = "os", "compiler", "build_type", "arch"
+    license = "MIT"
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
@@ -22,17 +23,9 @@ class LlhttpParserConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    exports_sources = "CMakeLists.txt", "patches/*"
 
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _is_msvc(self):
-        return str(self.settings.compiler) in ["Visual Studio", "msvc"]
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -40,48 +33,35 @@ class LlhttpParserConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.cppstd
-        del self.settings.compiler.libcxx
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-
-        self._cmake = CMake(self)
-        self._cmake.configure()
-        return self._cmake
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(
-            **self.conan_data["sources"][self.version],
-            destination=self._source_subfolder,
-            strip_root=True,
-        )
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(
-            "LICENSE-MIT",
-            src=os.path.join(self.source_folder, self._source_subfolder),
-            dst="licenses",
-        )
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE-MIT", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "llhttp")
         self.cpp_info.set_property("cmake_target_name", "llhttp::llhttp")
         self.cpp_info.set_property("pkg_config_name", "libllhttp")
         self.cpp_info.libs = ["llhttp"]
-
-        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
-        self.cpp_info.names["cmake_find_package"] = "llhttp"
-        self.cpp_info.names["cmake_find_package_multi"] = "llhttp"
-        self.cpp_info.names["pkg_config"] = "libllhttp"
