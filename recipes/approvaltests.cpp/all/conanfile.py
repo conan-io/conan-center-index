9efc6921e4d8ec1c0ed2d471ea01650723e7da67
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-05-09 09:10:57 +0000

    approvaltests.cpp/all/: bump deps

--- recipes/approvaltests.cpp/all/conanfile.py
+++ recipes/approvaltests.cpp/all/conanfile.py
@@ -52,11 +52,11 @@ class ApprovalTestsCppConan(ConanFile):
         if self.options.get_safe("with_boosttest"):
             self.requires("boost/1.81.0")
         if self.options.with_catch2:
-            self.requires("catch2/2.13.10")
+            self.requires("catch2/3.3.2")
         if self.options.with_gtest:
-            self.requires("gtest/1.12.1")
+            self.requires("gtest/1.13.0")
         if self.options.with_doctest:
-            self.requires("doctest/2.4.10")
+            self.requires("doctest/2.4.11")
         if self.options.get_safe("with_cpputest"):
             self.requires("cpputest/4.0")
 
