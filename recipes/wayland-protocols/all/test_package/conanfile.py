ecd7453a2b719017b93e76da6696c2f191bc03e7
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-05-09 09:39:09 +0000

    wayland-protocols/all/: bump deps

--- recipes/wayland-protocols/all/test_package/conanfile.py
+++ recipes/wayland-protocols/all/test_package/conanfile.py
@@ -20,7 +20,7 @@ class TestPackageConan(ConanFile):
         self.requires("wayland/1.21.0")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.0.0")
+        self.tool_requires("meson/1.1.0")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/1.9.3")
         self.tool_requires("wayland/1.21.0")
