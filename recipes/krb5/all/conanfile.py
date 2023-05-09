896992c0ac5e15a0c6cb9f535aba7b7b703fbac1
Jihadist <tomasiche@gmail.com>
2023-05-09 14:57:53 +0300

    fix openssl?

--- recipes/krb5/all/conanfile.py
+++ recipes/krb5/all/conanfile.py
@@ -113,7 +113,7 @@ class Krb5Conan(ConanFile):
         if not is_msvc(self):
             self.requires("libverto/0.3.2")
         if self.options.get_safe("with_tls") == "openssl":
-            self.requires("openssl/3.1.0")
+            self.requires("openssl/[>=1.1 <4]")
         if self.options.get_safe("with_tcl"):
             self.requires("tcl/8.6.10")
 
