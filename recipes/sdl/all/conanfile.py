12c1eac44a384eca1251e96552d22d7fc40ee485
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-05-09 09:34:45 +0000

    sdl/all/: bump deps

--- recipes/sdl/all/conanfile.py
+++ recipes/sdl/all/conanfile.py
@@ -153,7 +153,7 @@ class SDLConan(ConanFile):
                 self.requires("nas/1.9.5")
             if self.options.wayland:
                 self.requires("wayland/1.21.0")
-                self.requires("xkbcommon/1.4.1")
+                self.requires("xkbcommon/1.5.0")
                 self.requires("egl/system")
             if self.options.libunwind:
                 self.requires("libunwind/1.6.2")
@@ -184,7 +184,7 @@ class SDLConan(ConanFile):
             # set. This could be because you are using a Mac OS X version less than 10.5
             # or because CMake's platform configuration is corrupt.
             # FIXME: Remove once CMake on macOS/M1 CI runners is upgraded.
-            self.tool_requires("cmake/3.25.3")
+            self.tool_requires("cmake/3.26.3")
         if self.settings.os == "Linux" and not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/1.9.3")
         if hasattr(self, "settings_build") and self.options.get_safe("wayland"):
