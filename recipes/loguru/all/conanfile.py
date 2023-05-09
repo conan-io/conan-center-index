9c1aabfaf09d7fcc01edc90f4525df06e873bb88
Uilian Ries <uilianries@gmail.com>
2023-05-09 16:25:49 +0200

    Update recipes/loguru/all/conanfile.py

--- recipes/loguru/all/conanfile.py
+++ recipes/loguru/all/conanfile.py
@@ -74,7 +74,7 @@ class LoguruConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder='src')
 
     def generate(self):
         tc = CMakeToolchain(self)
