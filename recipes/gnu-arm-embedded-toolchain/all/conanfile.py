fdd859d7e39ce4839df941d8f91992511d4a6236
Khalil Estell <kammce@google.com>
2023-05-13 10:37:42 -0700

    Fix comments made by reviewers
    - Add ARM processor license ID
    - Add validation step, raise an exception if the OS & Arch settings are not
      supported
    - Define conf_info attributes for CMake Toolchain
    - Move toolchain.cmake to test_package
    - Remove unnecessary code from toolchain.cmake
    - Get test_package to build

--- recipes/gnu-arm-embedded-toolchain/all/conanfile.py
+++ recipes/gnu-arm-embedded-toolchain/all/conanfile.py
@@ -1,5 +1,6 @@
 from conan import ConanFile
 from conan.tools.files import get, copy, check_sha256, unzip
+from conan.errors import ConanException
 from urllib.parse import urlparse
 import urllib.request
 import os
@@ -21,35 +22,42 @@ class ArmGnuToolchain(ConanFile):
     settings = "os", "arch"
     short_paths = True
 
+    @property
+    def download_info(self):
+        version = self.version
+        os = str(self.settings.os)
+        arch = str(self.settings.arch)
+        return self.conan_data.get("sources", {}).get(version, {}).get(os, {}).get(arch)
+
     def validate(self):
-        pass
+        if not self.download_info:
+            raise ConanException(
+                "This package is not available for this operating system and architecture.")
 
     def source(self):
         pass
 
     def build(self):
-        download_info = self.conan_data["sources"][self.version][str(
-            self.settings.os)][str(self.settings.arch)]
-        url = download_info["url"]
-        url = url + "?rev=" + download_info["rev"]
-        url = url + "&hash=" + download_info["hash"]
+        url = self.download_info["url"]
+        url = url + "?rev=" + self.download_info["rev"]
+        url = url + "&hash=" + self.download_info["hash"]
         filename = os.path.basename(urlparse(url).path)
 
         # using urlretrieve over conan.tools.files.get because get results in
         # a 405 forbidden error whereas urlretrieve does not
         urllib.request.urlretrieve(url, filename)
-        check_sha256(self, filename, download_info["sha256"])
+        check_sha256(self, filename, self.download_info["sha256"])
         unzip(self, filename, strip_root=True)
 
         '''
         NOTE: I'm not sure how exactly I should go about handling this:
         The link is:
-        https://developer.arm.com/GetEula?Id=2821586b-44d0-4e75-a06d-4279cd97eaae
+        https://developer.arm.com/GetEula?Id=<ID>
 
         But it returns a number of licenses concatenated together as HTML.
         '''
         # get(self, self.conan_data["sources"]
-        #     [self.version]["License"], strip_root=True)
+        #     [self.version]["License"])
 
     def package(self):
         destination = os.path.join(self.package_folder, "bin/")
@@ -74,5 +82,23 @@ class ArmGnuToolchain(ConanFile):
     def package_info(self):
         # Add bin directory to PATH
         bin_folder = os.path.join(self.package_folder, "bin/bin")
+
         self.buildenv_info.append_path("PATH", bin_folder)
+
         self.cpp_info.bindirs = [bin_folder]
+
+        self.conf_info.define(
+            "tools.cmake.cmaketoolchain:system_name", "GENERIC")
+        self.conf_info.define(
+            "tools.cmake.cmaketoolchain:system_processor", "ARM")
+        self.conf_info.define("tools.build.cross_building:can_run", False)
+
+        tc_path = os.path.join(self.package_folder, "toolchain.cmake")
+        self.conf_info.append(
+            "tools.cmake.cmaketoolchain:user_toolchain", [tc_path])
+
+        self.conf_info.define("tools.build:compiler_executables", {
+            "c": "arm-none-eabi-gcc",
+            "cpp": "arm-none-eabi-g++",
+            "asm": "arm-none-eabi-as",
+        })
---
+++ recipes/gnu-arm-embedded-toolchain/all/test_package/toolchain.cmake
@@ -0,0 +1,28 @@
+# Append current directory to CMAKE_MODULE_PATH for making device specific
+# cmake modules visible
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+# Target definition
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+
+set(TOOLCHAIN arm-none-eabi)
+
+# Perform compiler test with static library
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# ------------------------------------------------------------------------------
+# Set compilers
+# ------------------------------------------------------------------------------
+set(CMAKE_C_COMPILER ${TOOLCHAIN}-gcc${CMAKE_EXECUTABLE_SUFFIX})
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN}-g++${CMAKE_EXECUTABLE_SUFFIX})
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN}-gcc${CMAKE_EXECUTABLE_SUFFIX})
+set(CMAKE_SIZE_UTIL ${TOOLCHAIN}-size${CMAKE_EXECUTABLE_SUFFIX})
+set(CMAKE_OBJDUMP ${TOOLCHAIN}-objdump${CMAKE_EXECUTABLE_SUFFIX})
+set(CMAKE_OBJCOPY ${TOOLCHAIN}-objcopy${CMAKE_EXECUTABLE_SUFFIX})
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_PREFIX_PATH} ${CMAKE_BINARY_DIR})
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
