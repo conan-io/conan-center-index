90bd98a26cdb3b3b8789588161e65956f111c6fd
Uilian Ries <uilianries@gmail.com>
2023-05-17 17:25:58 +0200

    simplify
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/tz/all/conanfile.py
+++ recipes/tz/all/conanfile.py
@@ -2,35 +2,19 @@ import os
 
 from conan import ConanFile
 from conan.tools.gnu import Autotools, AutotoolsToolchain
-from conan.tools.files import get, copy
+from conan.tools.files import get, copy, rmdir
 from conan.tools.layout import basic_layout
 
 required_conan_version = ">=1.53.0"
 
 class TzConan(ConanFile):
     name = "tz"
-    license = "BSD-3-Clause"
+    license = "Unlicense"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.iana.org/time-zones"
     description = "The Time Zone Database contains data that represent the history of local time for many representative locations around the globe."
     topics = ("tz", "tzdb", "time", "zone", "date")
-
-    # Binary configuration
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"fPIC": [True, False], "data_only": [True, False]}
-    default_options = {"fPIC": True, "data_only": False}
-
-    def package_id(self):
-        if self.info.options.data_only:
-            self.info.clear()
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
+    settings = "os", "build_type", "arch"
 
     def layout(self):
         basic_layout(self, src_folder="src")
@@ -42,55 +26,21 @@ class TzConan(ConanFile):
         tc = AutotoolsToolchain(self)
         tc.generate()
 
+    def build(self):
+        autotools = Autotools(self)
+        autotools.make(args=["-C", self.source_folder])
+
     def package(self):
-        if not self.options.data_only:
-            autotools = Autotools(self)
-            autotools.install(args=["-C", self.source_folder])
-        tzdata = [
-            "africa",
-            "antarctica",
-            "asia",
-            "australasia",
-            "backward",
-            "backzone",
-            "calendars",
-            "checklinks.awk",
-            "checktab.awk",
-            "etcetera",
-            "europe",
-            "factory",
-            "iso3166.tab",
-            "leap-seconds.list",
-            "leapseconds",
-            "leapseconds.awk",
-            "NEWS",
-            "northamerica",
-            "southamerica",
-            "version",
-            "ziguard.awk",
-            "zishrink.awk",
-            "zone.tab",
-            "zone1970.tab",
-        ]
-        for data in tzdata:
-            copy(self, data, dst=os.path.join(self.package_folder, "res", "tzdata"), src=self.source_folder)
-        copy(self, "theory.html", dst=os.path.join(self.package_folder, "res", "docs"), src=self.source_folder)
-        copy(self, "tz-*.html", dst=os.path.join(self.package_folder, "res", "docs"), src=self.source_folder)
         copy(self, "LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        autotools = Autotools(self)
+        autotools.install(args=["-C", self.source_folder, f"DESTDIR={self.package_folder}"])
+        rmdir(self, os.path.join(self.package_folder, "usr", "share", "man"))
+        # INFO: The library does not have a public API, it's used to build the zic and zdump tools
+        rmdir(self, os.path.join(self.package_folder, "usr", "lib"))
 
     def package_info(self):
-        self.cpp_info.resdirs = ["res"]
-        self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
-        self.cpp_info.includedirs = []
-        if not self.options.data_only:
-            self.cpp_info.resdirs.append(os.path.join("usr", "share"))
-            self.cpp_info.libdirs = [os.path.join("usr", "lib")]
-            self.cpp_info.bindirs = [
-                os.path.join("usr", "bin"),
-                os.path.join("usr", "sbin"),
-            ]
-            self.cpp_info.libs = ["tz"]
-
-        self.buildenv_info.define("TZDATA", os.path.join(self.package_folder, "res", "tzdata"))
-        self.runenv_info.define("TZDATA", os.path.join(self.package_folder, "res", "tzdata"))
+        self.cpp_info.resdirs = [os.path.join("usr", "share")]
+        self.cpp_info.bindirs = [os.path.join("usr", "bin"), os.path.join("usr", "sbin")]
+        self.buildenv_info.define("TZDATA", os.path.join(self.package_folder, "usr", "share", "zoneinfo"))
+        self.runenv_info.define("TZDATA", os.path.join(self.package_folder, "usr", "share", "zoneinfo"))
--- recipes/tz/all/test_package/conanfile.py
+++ recipes/tz/all/test_package/conanfile.py
@@ -1,14 +1,12 @@
 from conan import ConanFile
-from conan.tools.build import cross_building
+from conan.tools.build import can_run
 from conan.tools.layout import basic_layout
-import json
-import os
 
 
 class TzTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "VirtualBuildEnv", "VirtualRunEnv"
-    apply_env = False
+    test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
@@ -16,32 +14,6 @@ class TzTestConan(ConanFile):
     def layout(self):
         basic_layout(self)
 
-    def generate(self):
-        # Save dependencies to dependencies.json for consumption in test()
-        deps = {"tz": {key: value == "True" for key, value in self.dependencies["tz"].options.items()}}
-        with open('dependencies.json', 'w') as f:
-            f.write(json.dumps(deps))
-
     def test(self):
-        if not cross_building(self):
-            # Read dependency options in from dependencies.json
-            # This is achievable in conan >2.0 using self.dependencies["tz"].options.data_only,
-            # however this is not a cross-version compatible approach and so this is used
-            # while support for conan 1.x is ongoing.
-            deps = {}
-            with open(os.path.join(self.generators_folder, 'dependencies.json'), 'r') as f:
-                deps = json.loads(f.read())
-
-            if not deps["tz"]["data_only"]:
-                which = "where" if self.settings.os == "Windows" else "which"
-                cmd = f"{which} zdump"
-                self.output.info(f"Executing '{cmd}'")
-                self.run(cmd, env="conanrun")
-
-                cmd = "zdump -v America/Los_Angeles"
-                self.output.info(f"Executing '{cmd}'")
-                self.run(cmd, env="conanrun")
-
-            self.output.info("Test that tzdata is readable")
-            cmd = "python -c 'import os; tzdata = os.environ[\"TZDATA\"]; f=open(os.path.join(tzdata, \"factory\"), \"r\"); s = f.read(); f.close(); print(s)'"
-            self.run(cmd, env="conanrun")
+        if can_run(self):
+            self.run('zdump -v America/Los_Angeles', env="conanrun")
