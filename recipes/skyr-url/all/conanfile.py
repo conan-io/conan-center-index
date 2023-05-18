b3d2c8ab784d1c84703790a45018cadf4bd83dbb
toge <toge.mail@gmail.com>
2023-05-19 01:51:44 +0900

    skyr-url: support conan v2, update dependencies

--- recipes/skyr-url/all/conanfile.py
+++ recipes/skyr-url/all/conanfile.py
@@ -1,10 +1,14 @@
-from conans import ConanFile, tools, CMake
-from conans.errors import ConanInvalidConfiguration
-from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import is_msvc_static_runtime, is_msvc
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file, collect_libs
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv
 import os
-import functools
 
-required_conan_version = ">=1.45.0"
+required_conan_version = ">=1.53.0"
 
 class SkyrUrlConan(ConanFile):
     name = "skyr-url"
@@ -13,6 +17,7 @@ class SkyrUrlConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://cpp-netlib.github.io/url"
     topics = ("whatwg", "url", "parser")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -26,19 +31,9 @@ class SkyrUrlConan(ConanFile):
         "with_json": True,
         "with_fs": True,
     }
-    generators = "cmake", "cmake_find_package_multi"
-    _cmake = None
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder (self):
-        return "build_subfolder"
-
-    @property
-    def _minimum_cpp_standard(self):
+    def _min_cppstd(self):
         return 17
 
     @property
@@ -46,15 +41,15 @@ class SkyrUrlConan(ConanFile):
         # https://github.com/cpp-netlib/url/tree/v1.12.0#requirements
         return {
             "Visual Studio": "16",
+            "msvc": "192",
             "gcc": "7",
-            "clang": "6" if tools.Version(self.version) <= "1.12.0" else "8",
+            "clang": "6" if Version(self.version) <= "1.12.0" else "8",
             "apple-clang": "10",
         }
 
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -64,70 +59,75 @@ class SkyrUrlConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("tl-expected/1.1.0", transitive_headers=True)
+        self.requires("range-v3/0.12.0", transitive_headers=True)
+        if self.options.with_json:
+            self.requires("nlohmann_json/3.11.2")
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+            check_min_cppstd(self, self._min_cppstd)
         min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
         if not min_version:
-            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
-                self.name, self.settings.compiler))
+            self.output.warn(f"{self.ref} recipe lacks information about the {self.settings.compiler} compiler support.")
         else:
-            if tools.Version(self.settings.compiler.version) < min_version:
-                raise ConanInvalidConfiguration("{} requires C++17 support. The current compiler {} {} does not support it.".format(
-                    self.name, self.settings.compiler, self.settings.compiler.version))
+            if Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration(
+                    f"{self.ref} requires C++17 support. "
+                    f"The current compiler {self.settings.compiler} {self.settings.compiler.version} does not support it.")
 
         if self.options.with_fs and self.settings.compiler == "apple-clang":
             raise ConanInvalidConfiguration("apple-clang currently does not support with filesystem")
         if self.settings.compiler.get_safe("libcxx") == "libstdc++":
-            raise ConanInvalidConfiguration("{} supports only libstdc++'s new ABI".format(self.name))
-
-    def requirements(self):
-        self.requires("tl-expected/1.0.0")
-        self.requires("range-v3/0.12.0")
-        if self.options.with_json:
-            self.requires("nlohmann_json/3.10.5")
+            raise ConanInvalidConfiguration(f"{self.ref} supports only libstdc++'s new ABI")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    @functools.lru_cache(1)
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["skyr_BUILD_TESTS"] = False
-        cmake.definitions["skyr_FULL_WARNINGS"] = False
-        cmake.definitions["skyr_WARNINGS_AS_ERRORS"] = False
-        cmake.definitions["skyr_ENABLE_JSON_FUNCTIONS"] = self.options.with_json
-        cmake.definitions["skyr_ENABLE_FILESYSTEM_FUNCTIONS"] = self.options.with_fs
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["skyr_BUILD_TESTS"] = False
+        tc.variables["skyr_FULL_WARNINGS"] = False
+        tc.variables["skyr_WARNINGS_AS_ERRORS"] = False
+        tc.variables["skyr_ENABLE_JSON_FUNCTIONS"] = self.options.with_json
+        tc.variables["skyr_ENABLE_FILESYSTEM_FUNCTIONS"] = self.options.with_fs
+        tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
         if is_msvc(self):
-            cmake.definitions["skyr_USE_STATIC_CRT"] = is_msvc_static_runtime(self)
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+            tc.variables["skyr_USE_STATIC_CRT"] = is_msvc_static_runtime(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE_1_0.txt", src=self._source_subfolder, dst="licenses")
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE_1_0.txt", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "skyr-url")
         self.cpp_info.set_property("cmake_target_name", "skyr::skyr-url")
 
         self.cpp_info.components["url"].name = "skyr-url"
-        self.cpp_info.components["url"].libs = tools.collect_libs(self)
+        self.cpp_info.components["url"].libs = ["skyr-url"]
         self.cpp_info.components["url"].requires = ["tl-expected::tl-expected", "range-v3::range-v3"]
         if self.options.with_json:
             self.cpp_info.components["url"].requires.append("nlohmann_json::nlohmann_json")
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["url"].system_libs.append("m")
 
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "skyr-url"
         self.cpp_info.filenames["cmake_find_package_multi"] = "skyr-url"
         self.cpp_info.names["cmake_find_package"] = "skyr"
