f8719c30a9600e394197717b143a3c7bac9d5996
Rubén Rincón <rubenrb@jfrog.com>
2023-05-19 12:43:28 +0200

    Cert conf for libcurl

--- recipes/libcurl/all/conanfile.py
+++ recipes/libcurl/all/conanfile.py
@@ -226,7 +226,9 @@ class LibcurlConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        download(self, "https://curl.se/ca/cacert-2023-01-10.pem", "cacert.pem", verify=True, sha256="fb1ecd641d0a02c01bc9036d513cb658bbda62a75e246bedbc01764560a639f0")
+        cert_url = self.config.get("user.libcurl.cert:url", check_type=str) or "https://curl.se/ca/cacert-2023-01-10.pem"
+        cert_sha256 = self.config.get("user.libcurl.cert:sha256", check_type=str) or "fb1ecd641d0a02c01bc9036d513cb658bbda62a75e246bedbc01764560a639f0"
+        download(self, cert_url, "cacert.pem", verify=True, sha256=cert_sha256)
 
     def generate(self):
         env = VirtualBuildEnv(self)
