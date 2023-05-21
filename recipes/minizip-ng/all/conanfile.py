17873cada63e9c9a4de04b1f0726882fc7920cf0
toge <toge.mail@gmail.com>
2023-05-21 17:35:10 +0900

    require newer cmake

--- recipes/minizip-ng/all/conanfile.py
+++ recipes/minizip-ng/all/conanfile.py
@@ -99,6 +99,8 @@ class MinizipNgConan(ConanFile):
     def build_requirements(self):
         if self._needs_pkg_config:
             self.tool_requires("pkgconf/1.9.3")
+        if Version(self.version) >= "4.0.0":
+            self.tool_requires("cmake/[>=3.19 <4]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
