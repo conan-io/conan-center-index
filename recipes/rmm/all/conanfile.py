e6d19ef500c84f2b2809f350cbed8795353578d4
Martin Valgur <martin.valgur@gmail.com>
2023-05-23 23:58:03 +0300

    rmm: skip CMake build entirely
    Only the version_config.hpp creation step was relevant for Conan.

--- recipes/rmm/all/conanfile.py
+++ recipes/rmm/all/conanfile.py
@@ -1,9 +1,11 @@
 import os
+import textwrap
+from pathlib import Path
 
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import export_conandata_patches, get, copy, replace_in_file
+from conan.tools.files import export_conandata_patches, get, copy
+from conan.tools.layout import basic_layout
 
 required_conan_version = ">=1.52.0"
 
@@ -26,7 +28,13 @@ class RmmConan(ConanFile):
         export_conandata_patches(self)
 
     def layout(self):
-        cmake_layout(self, src_folder="src")
+        basic_layout(self, src_folder="src")
+
+    def configure(self):
+        # Thrust needs to use CUDA as the backend for RMM.
+        # Disabled for ConanCenter CI.
+        # self.options["thrust"].backend = "cuda"
+        pass
 
     def requirements(self):
         self.requires("thrust/1.17.2", transitive_headers=True, transitive_libs=True)
@@ -40,35 +48,20 @@ class RmmConan(ConanFile):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
 
+    def _write_version_header(self):
+        major, minor, patch = self.version.split(".")[:3]
+        header_path = Path(self.source_folder) / "include" / "rmm" / "version_config.hpp"
+        header_path.parent.mkdir(parents=True, exist_ok=True)
+        header_path.write_text(textwrap.dedent(f"""\
+            #pragma once
+            #define RMM_VERSION_MAJOR {int(major)}
+            #define RMM_VERSION_MINOR {int(minor)}
+            #define RMM_VERSION_PATCH {int(patch)}
+            """))
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.variables["BUILD_TESTS"] = False
-        tc.variables["BUILD_BENCHMARKS"] = False
-        tc.generate()
-        deps = CMakeDeps(self)
-        deps.generate()
-
-    def _patch_sources(self):
-        for pattern, repl in [
-            ("include(rapids-cpm)", ""),
-            ("rapids_cpm_init()", ""),
-            ("include(cmake/thirdparty", "# include(cmake/thirdparty"),
-        ]:
-            replace_in_file(
-                self,
-                os.path.join(self.source_folder, "CMakeLists.txt"),
-                pattern,
-                repl,
-            )
-
-    def build(self):
-        self._patch_sources()
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        self._write_version_header()
 
     def package(self):
         copy(
@@ -77,9 +70,17 @@ class RmmConan(ConanFile):
             dst=os.path.join(self.package_folder, "licenses"),
             src=self.source_folder,
         )
-        cmake = CMake(self)
-        cmake.install()
+        for pattern in ["*.hpp", "*.h"]:
+            copy(
+                self,
+                pattern=pattern,
+                dst=os.path.join(self.package_folder, "include"),
+                src=os.path.join(self.source_folder, "include"),
+            )
 
     def package_info(self):
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
+
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.append("dl")
