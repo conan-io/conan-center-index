d74183414bb5f79e1bd9ba85d3cc053d9a061c17
Gregor Jasny <gjasny@googlemail.com>
2023-05-23 18:44:42 +0200

    lzma_sdk: Conan 2 compatibility

--- recipes/lzma_sdk/9.20/conanfile.py
+++ recipes/lzma_sdk/9.20/conanfile.py
@@ -1,9 +1,12 @@
 from conan import ConanFile
-from conan.tools.files import get, chdir, replace_in_file, rm
-from conans import tools, AutoToolsBuildEnvironment, VisualStudioBuildEnvironment
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import get, chdir, copy, replace_in_file, rm
+from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, NMakeToolchain
 import os
 
-required_conan_version = ">=1.50.0"
+required_conan_version = ">=1.55.0"
 
 
 """ This older lzma release is used to build 7zip (to extract its sources).
@@ -25,36 +28,49 @@ class LzmaSdkConan(ConanFile):
     license = ("LZMA-exception",)
     homepage = "https://www.7-zip.org/sdk.html"
     topics = ("lzma", "zip", "compression", "decompression")
-    settings = "os", "arch", "compiler"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    package_type = "application"
+    settings = "os", "arch", "compiler", "build_type"
 
     @property
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
     def build_requirements(self):
-        if self.settings.compiler != "Visual Studio" and self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/cci.latest")
+        if not is_msvc(self) and self._settings_build.os == "Windows":
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                self.build_requires("msys2/cci.latest")
 
     def package_id(self):
+        del self.info.settings.build_type
         del self.info.settings.compiler
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self._source_subfolder)
-        rm(self, "7zr.exe", self._source_subfolder)
-        rm(self, "lzma.exe", self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version])
+        rm(self, "7zr.exe", self.source_folder)
+        rm(self, "lzma.exe", self.source_folder)
+
+
+    def generate(self):
+        if is_msvc(self):
+            tc = NMakeToolchain(self)
+            tc.generate()
+        else:
+            env = VirtualBuildEnv(self)
+            env.generate()            
+            tc = AutotoolsToolchain(self)
+            tc.generate()
+            deps = AutotoolsDeps(self)
+            deps.generate()
 
     @property
     def _msvc_build_dirs(self):
         return (
-            (os.path.join(self._source_subfolder, "C", "Util", "7z"), "7zDec.exe"),
-            (os.path.join(self._source_subfolder, "C", "Util", "Lzma"), "LZMAc.exe"),
-            (os.path.join(self._source_subfolder, "CPP", "7zip", "UI", "Console"), "7z.exe"),
-            (os.path.join(self._source_subfolder, "CPP", "7zip","Bundles", "Alone7z"), "7zr.exe"),
-            (os.path.join(self._source_subfolder, "CPP", "7zip", "Bundles", "LzmaCon"), "lzma.exe"),
+            (os.path.join(self.source_folder, "C", "Util", "7z"), "7zDec.exe"),
+            (os.path.join(self.source_folder, "C", "Util", "Lzma"), "LZMAc.exe"),
+            (os.path.join(self.source_folder, "CPP", "7zip", "UI", "Console"), "7z.exe"),
+            (os.path.join(self.source_folder, "CPP", "7zip","Bundles", "Alone7z"), "7zr.exe"),
+            (os.path.join(self.source_folder, "CPP", "7zip", "Bundles", "LzmaCon"), "lzma.exe"),
         )
 
     @property
@@ -68,19 +84,16 @@ class LzmaSdkConan(ConanFile):
     def _autotools_build_dirs(self):
         es = ".exe" if self.settings.os == "Windows" else ""
         return (
-            (os.path.join(self._source_subfolder, "C", "Util", "7z"), f"7zDec{es}"),
-            (os.path.join(self._source_subfolder, "CPP", "7zip", "Bundles", "LzmaCon"), f"lzma{es}"),
+            (os.path.join(self.source_folder, "C", "Util", "7z"), f"7zDec{es}"),
+            (os.path.join(self.source_folder, "CPP", "7zip", "Bundles", "LzmaCon"), f"lzma{es}"),
         )
 
     def _build_msvc(self):
         for make_dir, _ in self._msvc_build_dirs:
-            with tools.vcvars(self):
-                with tools.environment_append(VisualStudioBuildEnvironment(self).vars):
-                    with tools.chdir(make_dir):
-                        self.run(f"nmake /f makefile NEW_COMPILER=1 CPU={self._msvc_cpu} NO_BUFFEROVERFLOWU=1")
+            self.run(f"nmake /f makefile NEW_COMPILER=1 CPU={self._msvc_cpu} NO_BUFFEROVERFLOWU=1", cwd=make_dir)
 
     def _build_autotools(self):
-        env_build = AutoToolsBuildEnvironment(self)
+        autotools = Autotools(self)
         with tools.environment_append(env_build.vars):
             for make_dir, _ in self._autotools_build_dirs:
                 with chdir(self, make_dir):
@@ -89,48 +102,47 @@ class LzmaSdkConan(ConanFile):
                     ]
                     if self.settings.os == "Windows":
                         args.append("IS_MINGW=1")
-                    autotools = AutoToolsBuildEnvironment(self)
                     autotools.make(args=args)
 
     def _patch_sources(self):
-        if self.settings.compiler == "Visual Studio":
-            replace_in_file(self, os.path.join(self._source_subfolder, "CPP", "Build.mak"),
+        if is_msvc(self):
+            replace_in_file(self, os.path.join(self.source_folder, "CPP", "Build.mak"),
                                   "-MT\r", "-" + str(self.settings.compiler.runtime))
-            replace_in_file(self, os.path.join(self._source_subfolder, "CPP", "Build.mak"),
+            replace_in_file(self, os.path.join(self.source_folder, "CPP", "Build.mak"),
                                   "-MD\r", "-" + str(self.settings.compiler.runtime))
-            replace_in_file(self, os.path.join(self._source_subfolder, "CPP", "Build.mak"),
+            replace_in_file(self, os.path.join(self.source_folder, "CPP", "Build.mak"),
                                   " -WX ", " ")
 
         # Patches for other build systems
-        replace_in_file(self, os.path.join(self._source_subfolder, "C", "Util", "7z", "makefile.gcc"),
+        replace_in_file(self, os.path.join(self.source_folder, "C", "Util", "7z", "makefile.gcc"),
                               "CFLAGS = ",
                               "CFLAGS = -fpermissive ")
-        replace_in_file(self, os.path.join(self._source_subfolder, "C", "Util", "7z", "makefile.gcc"),
+        replace_in_file(self, os.path.join(self.source_folder, "C", "Util", "7z", "makefile.gcc"),
                               ": 7zAlloc.c",
                               ": ../../7zAlloc.c")
-        replace_in_file(self, os.path.join(self._source_subfolder, "C", "Util", "Lzma", "makefile.gcc"),
+        replace_in_file(self, os.path.join(self.source_folder, "C", "Util", "Lzma", "makefile.gcc"),
                               "CFLAGS = ",
                               "CFLAGS = -fpermissive ")
-        replace_in_file(self, os.path.join(self._source_subfolder, "CPP", "Common", "MyString.h"),
+        replace_in_file(self, os.path.join(self.source_folder, "CPP", "Common", "MyString.h"),
                               "#ifdef _WIN32\r\n",
                               "#ifdef _WIN32\r\n#ifndef UNDER_CE\r\n#include <windows.h>\r\n#endif\r\n")
 
     def build(self):
         self._patch_sources()
-        if self.settings.compiler == "Visual Studio":
+        if is_msvc(self):
             self._build_msvc()
         else:
             self._build_autotools()
 
     def package(self):
-        self.copy("lzma.txt", src=self._source_subfolder, dst="licenses")
-        self.copy("7zC.txt", src=self._source_subfolder, dst="licenses")
-        if self.settings.compiler == "Visual Studio":
+        copy(self, "lzma.txt", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, "7zC.txt", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        if is_msvc(self):
             for make_dir, exe in self._msvc_build_dirs:
-                self.copy(exe, src=os.path.join(make_dir, self._msvc_cpu), dst="bin", keep_path=False)
+                copy(self, exe, dst=os.path.join(self.package_folder, "bin"), src=os.path.join(make_dir, self._msvc_cpu), keep_path=False)
         else:
             for make_dir, exe in self._autotools_build_dirs:
-                self.copy(exe, src=os.path.join(make_dir), dst="bin", keep_path=False)
+                copy(self, exe, dst=os.path.join(self.package_folder, "bin"), src=os.path.join(make_dir), keep_path=False)
 
     def package_info(self):
         self.cpp_info.libdirs = []
