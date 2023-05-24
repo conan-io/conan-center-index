0bbb663e083caae99a60c72e08847d55edc807a9
Uilian Ries <uilianries@gmail.com>
2023-05-24 12:14:50 +0200

    update chaiscript to Conan v2
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/chaiscript/all/conanfile.py
+++ recipes/chaiscript/all/conanfile.py
@@ -1,4 +1,6 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.files import get, rmdir, copy, collect_libs
 import os
 
 
@@ -6,72 +8,75 @@ class ChaiScriptConan(ConanFile):
     name = "chaiscript"
     homepage = "https://github.com/ChaiScript/ChaiScript"
     description = "Embedded Scripting Language Designed for C++."
-    topics = ("conan", "embedded-scripting-language", "language")
+    topics = ("embedded-scripting-language", "language")
     url = "https://github.com/conan-io/conan-center-index"
     license = "BSD-3-Clause"
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"fPIC": [True, False], "dyn_load": [True, False], "use_std_make_shared": [True, False],
+    package_type = "shared-library"
+    options = {"fPIC": [True, False],
+               "dyn_load": [True, False],
+               "use_std_make_shared": [True, False],
                "multithread_support": [True, False],
                "header_only": [True, False]}
-    default_options = {"fPIC": True, "dyn_load": True,
+    default_options = {"fPIC": True,
+                       "dyn_load": True,
                        "use_std_make_shared": True,
                        "multithread_support": True,
                        "header_only": True}
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "ChaiScript-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = False
-        cmake.definitions["BUILD_SAMPLES"] = False
-        cmake.definitions["BUILD_MODULES"] = True
-        cmake.definitions["USE_STD_MAKE_SHARED"] = self.options.use_std_make_shared
-        cmake.definitions["DYNLOAD_ENABLED"] = self.options.dyn_load
-        cmake.definitions["MULTITHREAD_SUPPORT_ENABLED"] = self.options.multithread_support
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+    def configure(self):
+        if self.options.header_only:
+            self.package_type = "header-library"
+            del self.options.fPIC
+            del self.options.dyn_load
+            del self.options.use_std_make_shared
+            del self.options.multithread_support
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def package_id(self):
+        if self.info.options.header_only:
+            self.info.clear()
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_TESTING"] = False
+        tc.variables["BUILD_SAMPLES"] = False
+        tc.variables["BUILD_MODULES"] = True
+        tc.variables["USE_STD_MAKE_SHARED"] = self.options.get_safe("use_std_make_shared")
+        tc.variables["DYNLOAD_ENABLED"] = self.options.get_safe("dyn_load")
+        tc.variables["MULTITHREAD_SUPPORT_ENABLED"] = self.options.get_safe("multithread_support")
+        tc.generate()
 
     def build(self):
         if not self.options.header_only:
-            cmake = self._configure_cmake()
+            cmake = CMake(self)
+            cmake.configure()
             cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        copy(self, "LICENSE", dst="licenses", src=self.source_folder)
         if self.options.header_only:
-            self.copy(pattern="*.hpp", dst="include",
-                      src=os.path.join(self._source_subfolder, 'include'))
+            copy(self, pattern="*.hpp", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, 'include'))
         else:
-            cmake = self._configure_cmake()
+            cmake = CMake(self)
             cmake.install()
-            tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-            tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-            tools.rmdir(os.path.join(self.package_folder, "share"))
-
-    def package_id(self):
-        if self.options.header_only:
-            self.info.header_only()
+            rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+            rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+            rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         if not self.options.header_only:
-            self.cpp_info.libs = tools.collect_libs(self)
+            self.cpp_info.libdirs = ["lib", os.path.join("lib", "chaiscript")]
+            self.cpp_info.libs = [f'chaiscript_stdlib-{self.version}', 'stl_extra']
         if self.options.use_std_make_shared:
             self.cpp_info.defines.append("CHAISCRIPT_USE_STD_MAKE_SHARED")
         if self.settings.os == "Linux":
--- recipes/chaiscript/all/test_package/conanfile.py
+++ recipes/chaiscript/all/test_package/conanfile.py
@@ -1,10 +1,19 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 import os
-from conans import ConanFile, CMake
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package_multi", "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -12,4 +21,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        self.run(os.path.join("bin", "test_package"), run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
