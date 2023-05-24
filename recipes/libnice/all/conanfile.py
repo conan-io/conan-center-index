0807b437ab93d6c95ec5fa5cdc40955ab0fe8466
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-05-24 18:34:16 +0000

    libnice/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libnice/all/conanfile.py
+++ recipes/libnice/all/conanfile.py
@@ -59,7 +59,7 @@ class LibniceConan(ConanFile):
                 "-o glib/*:shared=True with static runtime is not supported")
 
     def requirements(self):
-        self.requires("glib/2.76.2")
+        self.requires("glib/2.76.3")
         if self.options.crypto_library == "openssl":
             self.requires("openssl/3.1.0")
         if self.options.with_gstreamer:
@@ -68,7 +68,7 @@ class LibniceConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("meson/1.1.0")
         self.tool_requires("pkgconf/1.9.3")
-        self.tool_requires("glib/2.76.2")  # for glib-mkenums
+        self.tool_requires("glib/2.76.3")  # for glib-mkenums
         if self.options.with_introspection:
             self.tool_requires("gobject-introspection/1.72.0")
 
