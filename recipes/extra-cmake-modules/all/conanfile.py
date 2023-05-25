088c922983614948f2f2710c517b9add281d9fd2
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2023-05-25 13:33:25 +0100

    setup package_info

--- recipes/extra-cmake-modules/all/conanfile.py
+++ recipes/extra-cmake-modules/all/conanfile.py
@@ -1,8 +1,7 @@
 import os
-#from conans import ConanFile, CMake, tools
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain
-from conan.tools.files import get
+from conan.tools.files import get, copy
 
 class ExtracmakemodulesConan(ConanFile):
     name = "extra-cmake-modules"
@@ -14,7 +13,6 @@ class ExtracmakemodulesConan(ConanFile):
     no_copy_source = False
     package_type = "build-scripts"
     settings = "build_type"
-    
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder)
@@ -28,30 +26,24 @@ class ExtracmakemodulesConan(ConanFile):
         tc.cache_variables["BUILD_TESTING"] = False
         tc.generate()
 
-
     def build(self):
         cm_folder = "{}-{}".format(self.name, self.version)
         cmake = CMake(self)
         cmake.configure(build_script_folder=cm_folder)
 
     def package(self):
+        cm_folder = "{}-{}".format(self.name, self.version)
+        lic_folder = os.path.join(cm_folder, "LICENSES")
+        copy(self, "*", src=lic_folder, dst="licenses")
+
         cmake = CMake(self)
         cmake.install()
 
+
     def package_id(self):
         self.info.settings.clear()
-        
- 
-    def package_old(self):
-        cmake = self._configure_cmake()
-        cmake.install()
-        self.copy("testhelper.h", src=os.path.join(self.source_folder, self._source_subfolder, "tests/ECMAddTests"), dst="res/tests")
-        self.copy("*", src=os.path.join(self.source_folder, self._source_subfolder, "LICENSES"), dst="licenses")
 
     def package_info(self):
-        self.cpp_info.resdirs = ["res"]
-        self.cpp_info.builddirs = ["res/ECM/cmake", "res/ECM/kde-modules", "res/ECM/modules", "res/ECM/test-modules", "res/ECM/toolchain"]
-
-#    def package_id(self):
-#        self.info.header_only()
-
+        #this package is CMake files, it doesn't need an extra one generating
+        self.cpp_info.set_property("cmake_find_mode", "none")
+        self.cpp_info.builddirs.append(os.path.join("share","ECM","cmake"))
