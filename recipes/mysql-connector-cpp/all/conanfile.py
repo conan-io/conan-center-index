d9bd970241c28dafd1d015b3a539337c6f8869ae
samuaz <samuaz@gmail.com>
2023-05-24 21:00:56 -0300

    update minimal gcc version

--- recipes/mysql-connector-cpp/all/conanfile.py
+++ recipes/mysql-connector-cpp/all/conanfile.py
@@ -15,7 +15,8 @@ class MysqlConnectorCPPRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    requires = ("boost/1.81.0", "openssl/[>=1.1 <4]", "libmysqlclient/8.0.31")
+    requires = ("boost/1.81.0", "openssl/[>=1.1 <4]", "protobuf/3.21.9", "libmysqlclient/8.0.31")
+    build_policy = "missing"
 
     @property
     def _min_cppstd(self):
@@ -26,7 +27,7 @@ class MysqlConnectorCPPRecipe(ConanFile):
         return {
             "Visual Studio": "16",
             "msvc": "192",
-            "gcc": "7" if Version(self.version) >= "8.0.27" else "5.3",
+            "gcc": "9" if Version(self.version) >= "8.0.27" else "5.3",
             "clang": "6",
         }
 
@@ -83,6 +84,7 @@ class MysqlConnectorCPPRecipe(ConanFile):
         tc.cache_variables["MYSQL_LIB_DIR"] = self.dependencies["libmysqlclient"].cpp_info.aggregated_components().libdirs[0].replace("\\", "/")
         tc.cache_variables["MYSQL_INCLUDE_DIR"] = self.dependencies["libmysqlclient"].cpp_info.aggregated_components().includedirs[0].replace("\\", "/")
         tc.cache_variables["WITH_SSL"] = self.dependencies["openssl"].package_folder.replace("\\", "/")
+        tc.cache_variables["WITH_PROTOBUF"] = self.dependencies["protobuf"].package_folder.replace("\\", "/")
         tc.generate()
 
     def build(self):
