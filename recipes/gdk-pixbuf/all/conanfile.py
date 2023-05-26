4e691aa855dcb8e76c276878557683215d82489e
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-05-26 06:30:07 +0000

    gdk-pixbuf/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/gdk-pixbuf/all/conanfile.py
+++ recipes/gdk-pixbuf/all/conanfile.py
@@ -90,7 +90,7 @@ class GdkPixbufConan(ConanFile):
             )
 
     def build_requirements(self):
-        self.tool_requires("meson/1.1.0")
+        self.tool_requires("meson/1.1.1")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/1.9.3")
         if not can_run(self):
