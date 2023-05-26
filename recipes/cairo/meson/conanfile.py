639d50605f6ae255000a40ca23359f97ed44af79
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-05-26 06:26:12 +0000

    cairo/meson: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/cairo/meson/conanfile.py
+++ recipes/cairo/meson/conanfile.py
@@ -141,7 +141,7 @@ class CairoConan(ConanFile):
                 )
 
     def build_requirements(self):
-        self.tool_requires("meson/1.1.0")
+        self.tool_requires("meson/1.1.1")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/1.9.3")
 
