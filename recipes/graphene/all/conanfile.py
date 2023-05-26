86eb31886621b6d6ad3e6687eeaa94af7c01c267
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-05-26 06:30:50 +0000

    graphene/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/graphene/all/conanfile.py
+++ recipes/graphene/all/conanfile.py
@@ -70,7 +70,7 @@ class GrapheneConan(ConanFile):
                 )
 
     def build_requirements(self):
-        self.tool_requires("meson/1.1.0")
+        self.tool_requires("meson/1.1.1")
         if not self.conf.get("tools.gnu:pkg_config", default=False):
             self.tool_requires("pkgconf/1.9.3")
 
