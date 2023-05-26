0cbc9a153d81ba762927a9cece2c047e9222d92a
Rubén Rincón <rubenrb@jfrog.com>
2023-05-26 13:20:47 +0200

    Simplify test_package

--- recipes/imgui/all/test_package/test_package.cpp
+++ recipes/imgui/all/test_package/test_package.cpp
@@ -7,23 +7,7 @@
 
 int main(int, char**)
 {
-    ImGuiContext* context =ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-
-    ImGuiTextBuffer textBuffer;
-    textBuffer.append("Hello, ImGui");
-
-    // Build atlas
-    unsigned char* tex_pixels = NULL;
-    int tex_w, tex_h;
-    io.Fonts->GetTexDataAsRGBA32(&tex_pixels, &tex_w, &tex_h);
-
-    for (int n = 0; n < 50; n++)
-    {
-        printf("NewFrame() %d\n", n);
-        io.DisplaySize = ImVec2(1920, 1080);
-        io.DeltaTime = 1.0f / 60.0f;
-        ImGui::NewFrame();
+        printf("IMGUI VERSION: %s\n", IMGUI_VERSION);
 
 #ifdef DOCKING
         auto dockspaceID = ImGui::GetID("MyDockSpace");
@@ -32,18 +16,5 @@ int main(int, char**)
         printf("  with docking\n");
 #endif
 
-        static float f = 0.0f;
-        ImGui::Text("Hello, world!");
-        ImGui::Text("%s", textBuffer.begin());
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-        ImGui::MyFunction("test_package");  // ensure we are using our provided IMGUI_USER_CONFIG
-        ImGui::ShowDemoWindow(NULL);
-
-        ImGui::Render();
-    }
-
-    printf("DestroyContext()\n");
-    ImGui::DestroyContext(context);
     return 0;
 }
