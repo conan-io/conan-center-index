16b38933d1bd2b61759c66256dcf64f41cdee218
Samuel Dowling <samuel.dowling@protonmail.com>
2023-05-29 00:00:13 +0930

    [tz] Fix typo when querying settings_build

--- recipes/tz/all/conanfile.py
+++ recipes/tz/all/conanfile.py
@@ -32,7 +32,7 @@ class TzConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("mawk/1.3.4-20230404")
-        if self.settings_build.os == "Windows":
+        if self._settings_build.os == "Windows":
             self.win_bash = True
             if not self.conf.get("tools.microsoft.bash:path", check_type=str):
                 self.tool_requires("msys2/cci.latest")
