bad37ff28663db8ce4b512a73cfaf07d4b7cbe7f
Samuel Dowling <samuel.dowling@protonmail.com>
2023-05-30 00:13:58 +0930

    [tz] Ensure test_package runs in msys context on Windows

--- recipes/tz/all/test_package/conanfile.py
+++ recipes/tz/all/test_package/conanfile.py
@@ -1,5 +1,7 @@
 from conan import ConanFile
 from conan.tools.build import can_run
+from conan.errors import ConanException
+import os
 
 
 class TzTestConan(ConanFile):
@@ -8,8 +10,14 @@ class TzTestConan(ConanFile):
     test_type = "explicit"
     tzdata = None
 
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
+        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", check_type=str):
+            self.tool_requires("msys2/cci.latest")
 
     def generate(self):
         # INFO: zdump does not consume TZDATA, need to pass absolute path of the zoneinfo directory
@@ -24,4 +32,4 @@ class TzTestConan(ConanFile):
         if can_run(self):
             with open("tzdata.info", "r") as fd:
                 self.tzdata = fd.read()
-            self.run(f"zdump {self.tzdata}/America/Los_Angeles")
+            self.run(f"zdump {os.path.join(self.tzdata, 'America', 'Los_Angeles')}")
