f475f275a0c8a567f81571c709a457f67d8fb43c
Rasmus Thomsen <rasmus.thomsen@dampsoft.de>
2023-05-31 11:03:16 +0200

    turtle: address review comments

--- recipes/turtle/all/conanfile.py
+++ recipes/turtle/all/conanfile.py
@@ -1,16 +1,23 @@
 from conan import ConanFile
 from conan.tools.files import get, copy
+from conan.tools.layout import basic_layout
 import os
 
+required_conan_version = ">=1.53.0"
 
 class TurtleConan(ConanFile):
     name = "turtle"
     description = "Turtle is a C++ mock object library based on Boost with a focus on usability, simplicity and flexibility."
-    topics = ("conan", "turtule", "mock", "test", "boost")
+    topics = ("mock", "test", "boost")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/mat007/turtle"
     license = "BSL-1.0"
+    no_copy_source = True
     package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def layout(self):
+        basic_layout(self, src_folder="include/turtle")
 
     def requirements(self):
         self.requires("boost/1.81.0")
