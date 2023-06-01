ceafb52cef21b529629c1f575695da80729fca82
toge <toge.mail@gmail.com>
2023-06-01 23:42:16 +0900

    fix validateion

--- recipes/glaze/all/conanfile.py
+++ recipes/glaze/all/conanfile.py
@@ -28,7 +28,7 @@ class GlazeConan(ConanFile):
             "Visual Studio": "16",
             "msvc": "192",
             "gcc": "12",
-            "clang": "12" if Version(self.version) > "1.0.0" else "14",
+            "clang": "12" if Version(self.version) > "1.0.0" else "13",
             "apple-clang": "13.1",
         }
 
@@ -55,16 +55,10 @@ class GlazeConan(ConanFile):
         if self.settings.get_safe("compiler.cppstd"):
             check_min_cppstd(self, self._minimum_cpp_standard)
 
-        def loose_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
-
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(
-                f"{self.name} {self.version} requires C++{self._minimum_cpp_standard}, which your compiler does not support.",
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
 
     def source(self):
