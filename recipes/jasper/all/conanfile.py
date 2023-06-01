14e1dbb8cfa60805cb514b72cc05c90f1949b7e8
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-06-01 14:55:30 +0100

    Update conanfile.py

--- recipes/jasper/all/conanfile.py
+++ recipes/jasper/all/conanfile.py
@@ -2,7 +2,6 @@ from conan import ConanFile
 from conan.tools.build import cross_building
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir, save
-from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 import os
 import textwrap
