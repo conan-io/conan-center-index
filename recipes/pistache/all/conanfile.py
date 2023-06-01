cec155ead8c859d1074334206cafaf6e586ea5c3
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-06-01 17:38:45 +0000

    pistache/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/pistache/all/conanfile.py
+++ recipes/pistache/all/conanfile.py
@@ -54,7 +54,7 @@ class PistacheConan(ConanFile):
     def requirements(self):
         self.requires("rapidjson/cci.20220822")
         if self.options.with_ssl:
-            self.requires("openssl/3.1.0")
+            self.requires("openssl/3.1.1")
         if self.version != "cci.20201127":
             self.requires("date/3.0.1")
 
