f96871c011c596e14e1c321c5530aa4d79e909e4
Alexander Shuklin <jasuramme@gmail.com>
2023-06-04 16:18:38 +0600

    Migrate LibXft to Conan2
    - Moved old test_package to test_v1_package
    - Left similar options and changed what migration guide suggests

--- recipes/libxft/all/conanfile.py
+++ recipes/libxft/all/conanfile.py
@@ -1,12 +1,15 @@
 from conan import ConanFile
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, chdir, rm, rmdir
-from conans import AutoToolsBuildEnvironment
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, rm, rmdir, copy
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.layout import basic_layout
 import functools
+import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.57.0"
 
-class libxftConan(ConanFile):
+class LibxftConan(ConanFile):
     name = "libxft"
+    package_type = "library"
     description = 'X FreeType library'
     topics = ("libxft", "x11", "xorg")
     url = "https://github.com/conan-io/conan-center-index"
@@ -16,11 +19,7 @@ class libxftConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "pkg_config"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    generators = "PkgConfigDeps"
 
     def export_sources(self):
         export_conandata_patches(self)
@@ -35,15 +34,21 @@ class libxftConan(ConanFile):
         self.build_requires("xorg-macros/1.19.3")
         self.build_requires("libtool/2.4.7")
 
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = AutotoolsToolchain(self)
+        tc.generate()
 
     def configure(self):
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
     @functools.lru_cache(1)
     def _configure_autotools(self):
@@ -52,22 +57,20 @@ class libxftConan(ConanFile):
             args.extend(["--disable-static", "--enable-shared"])
         else:
             args.extend(["--disable-shared", "--enable-static"])
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.configure(args=args, pkg_config_paths=self.build_folder)
+        autotools = Autotools(self)
+        autotools.configure(args=args)
         return autotools
 
     def build(self):
         apply_conandata_patches(self)
-        with chdir(self, self._source_subfolder):
-            autotools = self._configure_autotools()
-            autotools.make()
+        autotools = self._configure_autotools()
+        autotools.make()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        with chdir(self, self._source_subfolder):
-            autotools = self._configure_autotools()
-            autotools.install(args=["-j1"])
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        autotools = self._configure_autotools()
+        autotools.install(args=["-j1"])
         rm(self, "*.la", f"{self.package_folder}/lib", recursive=True)
         rmdir(self, f"{self.package_folder}/lib/pkgconfig")
         rmdir(self, f"{self.package_folder}/share")
--- recipes/libxft/all/test_package/conanfile.py
+++ recipes/libxft/all/test_package/conanfile.py
@@ -1,10 +1,22 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, cmake_layout
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeToolchain", "CMakeDeps"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self, src_folder="./")
+
+    def requirements(self):
+        self.requires("xorg/system")
+        self.requires("freetype/2.13.0")
+        self.requires("fontconfig/2.14.2")
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -12,6 +24,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
