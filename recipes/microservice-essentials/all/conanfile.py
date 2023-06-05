bf4d1e77eaeed81a1baccaf793e364eddfa87e1d
Sebastian Steger <sebastian@sebastian-steger.com>
2023-06-05 18:06:50 +0200

    hook fixes

--- recipes/microservice-essentials/all/conanfile.py
+++ recipes/microservice-essentials/all/conanfile.py
@@ -1,23 +1,26 @@
 from conans import ConanFile, CMake
+import os
+from conan.tools.files import get, copy
+
 class MicroserviceEssentials(ConanFile):
     name = "microservice-essentials"
     version = "0.1.0"
-    url = "https://github.com/seboste/microservice-essentials"
-    license = "MIT"
-    author = "Sebastian Steger (Sebastian.Steger@gmail.com)"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/seboste/microservice-essentials"
+    license = "MIT"    
     description = """microservice-essentials is a portable, independent C++ library that takes care of typical recurring concerns that occur in microservice development."""
-    settings = "os", "compiler", "build_type", "arch", "cppstd"    
-    generators = "cmake_find_package_multi", "cmake"
-    scm = {
-        "type": "git",        
-        "url": "https://github.com/seboste/microservice-essentials.git",
-        "revision": "auto"
-    }
+    topics = ("microservices", "cloud-native", "request-handling")
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "cmake_find_package_multi", "cmake"    
     options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
         "build_testing": [True, False],
         "build_examples": [True, False]        
     }
     default_options = {
+        "shared": False,
+        "fPIC": True,
         "build_testing": False,
         "build_examples": False
     }
@@ -32,6 +35,9 @@ class MicroserviceEssentials(ConanFile):
             self.requires("catch2/3.3.2")
             self.requires("nlohmann_json/3.11.2")
 
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
     def build(self):
         cmake = CMake(self)
         cmake.definitions['BUILD_TESTING'] = self.options.build_testing
@@ -40,6 +46,7 @@ class MicroserviceEssentials(ConanFile):
         cmake.build()
 
     def package(self):
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
@@ -47,4 +54,3 @@ class MicroserviceEssentials(ConanFile):
         self.cpp_info.libs = ["microservice-essentials"]
         if self.settings.os != "Windows":
             self.cpp_info.system_libs = ["pthread"]
-    
--- recipes/microservice-essentials/all/test_package/conanfile.py
+++ recipes/microservice-essentials/all/test_package/conanfile.py
@@ -12,11 +12,6 @@ class TestTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-
     def test(self):
         if not tools.cross_building(self):
             os.chdir("bin")
