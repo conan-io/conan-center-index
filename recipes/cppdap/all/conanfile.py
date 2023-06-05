a21210d24bd158efaaa730fd89c0a64aacf0d044
Gregor Jasny <gjasny@googlemail.com>
2023-06-05 08:09:50 +0200

    cppdap: correct the order of cmake min and project

--- recipes/cppdap/all/conanfile.py
+++ recipes/cppdap/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import get, copy, rm, rmdir
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 import os
@@ -34,7 +34,7 @@ class PackageConan(ConanFile):
         return 11
 
     def export_sources(self):
-        pass
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -74,6 +74,7 @@ class PackageConan(ConanFile):
         tc.generate()
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
