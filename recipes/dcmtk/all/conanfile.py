5f64f75ede57d7968cbaf56ef2fa771e9c38af0c
Rasmus Thomsen <rasmus.thomsen@dampsoft.de>
2023-06-05 10:24:12 +0200

    dcmtk: delete FindIconv

--- recipes/dcmtk/all/conanfile.py
+++ recipes/dcmtk/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import can_run, check_min_cppstd, valid_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, rmdir, save, copy
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, rmdir, save, copy, rm
 from conan.tools.microsoft import is_msvc, msvc_runtime_flag
 from conan.tools.scm import Version
 import os
@@ -181,6 +181,7 @@ class DCMTKConan(ConanFile):
 
     def build(self):
         apply_conandata_patches(self)
+        rm(self, "FindIconv.cmake", os.path.join(self.source_folder, "CMake"))
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
