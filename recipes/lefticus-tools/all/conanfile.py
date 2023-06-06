84622c49428a62654f92ae2cbf3ff1a07b5f9c8c
Uilian Ries <uilianries@gmail.com>
2023-06-06 10:43:38 +0200

    need C++20
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/lefticus-tools/all/conanfile.py
+++ recipes/lefticus-tools/all/conanfile.py
@@ -1,5 +1,9 @@
 from conan import ConanFile
 from conan.tools.files import get, copy
+from conan.tools.build import check_min_cppstd, check_min_vs
+from conan.tools.microsoft import is_msvc
+from conan.errors import ConanInvalidConfiguration
+from conan import Version
 import os
 
 
@@ -17,8 +21,29 @@ class LefticusToolsConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
+    @property
+    def _min_cppstd(self):
+        return "20"
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "11",
+            "clang": "13",
+            "apple-clang": "14",
+        }
+
     def package_id(self):
         self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        check_min_vs(self, 192)
+        if not is_msvc(self):
+            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+            if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
