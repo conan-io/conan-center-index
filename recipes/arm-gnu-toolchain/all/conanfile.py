7e96199bed1504fe0960125706de552dfab869b1
Khalil Estell <kammce@google.com>
2023-06-05 18:52:23 -0700

    Remove build policy

--- recipes/arm-gnu-toolchain/all/conanfile.py
+++ recipes/arm-gnu-toolchain/all/conanfile.py
@@ -21,7 +21,6 @@ class ArmGnuToolchain(ConanFile):
     exports_sources = "toolchain.cmake"
     package_type = "application"
     short_paths = True
-    build_policy = "missing"
 
     @property
     def download_info(self):
