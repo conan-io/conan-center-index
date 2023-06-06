f790e33723b20f49e530a236d1d4e15ea832a6ea
Uilian Ries <uilianries@gmail.com>
2023-06-06 12:40:52 +0200

    simplify test package due Windows
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/lefticus-tools/all/test_package/test_package.cpp
+++ recipes/lefticus-tools/all/test_package/test_package.cpp
@@ -1,15 +1,15 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "lefticus/tools/non_promoting_ints.hpp"
+#include "lefticus/tools/consteval_invoke.hpp"
 
-using namespace lefticus::tools::literals;
+
+constexpr unsigned int Factorial(unsigned int number) {
+    return number <= 1 ? number : Factorial(number - 1) * number;
+}
+
 
 int main() {
-    const auto npint1 = std::int8_t{ 2 } + 1_np8;
-    if (std::is_same_v<decltype(npint1), const lefticus::tools::int_np<std::int8_t>>) {
-        std::cout << "npint1 is a non-promoting int" << std::endl;
-    }
-
+    std::cout << "Factorial of 3 is: " << lefticus::tools::consteval_invoke(Factorial, 3) << std::endl;
     return EXIT_SUCCESS;
 }
