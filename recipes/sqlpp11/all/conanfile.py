834a02cd97036db81d80578458e878aabc8d5a10
toge <toge.mail@gmail.com>
2023-06-09 08:30:26 +0900

    sqlpp11: add version 0.62, support conan v2

--- recipes/sqlpp11/all/conanfile.py
+++ recipes/sqlpp11/all/conanfile.py
@@ -1,71 +1,79 @@
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime, is_msvc
+from conan.tools.files import get, copy
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.layout import basic_layout
 import os
 
-required_conan_version = ">=1.33.0"
-
+required_conan_version = ">=1.52.0"
 
 class Sqlpp11Conan(ConanFile):
     name = "sqlpp11"
+    description = "A type safe SQL template library for C++"
     license = "BSD-2-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/rbock/sqlpp11"
-    description = "A type safe SQL template library for C++"
-    topics = ("sql", "dsl", "embedded", "data-base")
+    topics = ("sql", "dsl", "embedded", "data-base", "header-only")
+    package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _min_stdcpp_version(self):
-        return 11 if tools.Version(self.version) < "0.61" else 14
+    def _min_cppstd(self):
+        return 11 if Version(self.version) < "0.61" else 14
 
     @property
     def _compilers_minimum_version(self):
         return {
-            "gcc": "5",
-            "Visual Studio": "14",
-            "clang": "3.4",
-            "apple-clang": "10",
-        }
+            "14": {
+                "gcc": "6",
+                "clang": "5",
+                "apple-clang": "10",
+                "Visual Studio": "15",
+                "msvc": "191",
+            },
+        }.get(self._min_cppstd, {})
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def requirements(self):
         self.requires("date/3.0.1")
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, self._min_stdcpp_version)
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
 
-        if self._min_stdcpp_version > 11:
-            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-            if minimum_version:
-                if tools.Version(self.settings.compiler.version) < minimum_version:
-                    raise ConanInvalidConfiguration(f"{self.name} requires C++14, which your compiler does not support.")
-            else:
-                self.output.warn(f"{self.name} requires C++14. Your compiler is unknown. Assuming it supports C++14.")
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version:
+            if Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration(f"{self.name} requires C++{self._min_cppstd}, which your compiler does not support.")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def source(self):
-        tools.get(
-            **self.conan_data["sources"][self.version],
-            destination=self._source_subfolder,
-            strip_root=True
-        )
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
-        self.copy("*", dst="bin", src=os.path.join(self._source_subfolder, "scripts"))
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(
+            self,
+            pattern="*.h",
+            dst=os.path.join(self.package_folder, "include"),
+            src=os.path.join(self.source_folder, "include"),
+        )
+        copy(
+            self,
+            pattern="*",
+            dst=os.path.join(self.package_folder, "bin"),
+            src=os.path.join(self.source_folder, "scripts"),
+        )
 
     def package_info(self):
-        self.cpp_info.filenames["cmake_find_package"] = "Sqlpp11"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "Sqlpp11"
+        self.cpp_info.libdirs = []
 
-        bindir = os.path.join(self.package_folder, "bin")
-        self.output.info("Appending PATH environment variable: {}".format(bindir))
-        self.env_info.PATH.append(bindir)
+        self.cpp_info.set_property("cmake_file_name", "Sqlpp11")
+        self.cpp_info.set_property("cmake_target_name", "sqlpp11::sqlpp11")
