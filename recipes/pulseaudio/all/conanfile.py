530af91a59249237a57c30d2b26f29ba88291980
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-06-09 17:34:13 +0000

    pulseaudio/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/pulseaudio/all/conanfile.py
+++ recipes/pulseaudio/all/conanfile.py
@@ -72,7 +72,7 @@ class PulseAudioConan(ConanFile):
         if self.options.with_openssl:
             self.requires("openssl/[>=1.1 <4]")
         if self.options.with_dbus:
-            self.requires("dbus/1.15.2")
+            self.requires("dbus/1.15.6")
 
     def validate(self):
         if self.settings.os != "Linux":
