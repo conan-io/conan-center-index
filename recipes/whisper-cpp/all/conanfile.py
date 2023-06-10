3a3c8154f259d06e39df5767de104a84fe5bec28
Francesco Corso <fcorso2016@gmail.com>
2023-06-09 21:19:59 -0400

    Added a bunch more library options

--- recipes/whisper-cpp/all/conanfile.py
+++ recipes/whisper-cpp/all/conanfile.py
@@ -24,8 +24,16 @@ class WhisperCppConan(ConanFile):
     homepage = "https://github.com/ggerganov/whisper.cpp"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {"shared": [True, False], "fPIC": [True, False], "sanitize_thread": [True, False],
+               "sanitize_address": [True, False], "sanitize_undefined": [True, False], "with_sdl2": [True, False],
+               "no_avx": [True, False], "no_avx2": [True, False], "no_fma": [True, False], "no_f16c": [True, False],
+               "on_accelerate": [True, False], "with_coreml": [True, False], "coreml_allow_fallback": [True, False],
+               "with_blas": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "sanitize_thread": False,
+                       "sanitize_address": False, "sanitize_undefined": False, "with_sdl2": False,
+                       "no_avx": False, "no_avx2": False, "no_fma": False, "no_f16c": False,
+                       "on_accelerate": False, "with_coreml": False, "coreml_allow_fallback": False,
+                       "with_blas": False}
 
     _cmake = None
 
@@ -49,12 +57,23 @@ class WhisperCppConan(ConanFile):
         export_conandata_patches(self)
 
     def config_options(self):
+        if is_apple_os(self):
+            del self.options.with_blas
+        else:
+            del self.options.no_accelerate
+            del self.options.with_coreml
+            del self.options.coreml_allow_fallback
+
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
         if self.options.shared:
             self.options.rm_safe("fPIC")
+
+        if is_apple_os(self):
+            if not self.options.with_coreml:
+                self.options.rm_safe("coreml_allow_fallback")
 
     def validate(self):
         if self.settings.compiler.cppstd:
@@ -64,6 +83,14 @@ class WhisperCppConan(ConanFile):
             raise ConanInvalidConfiguration(
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
+
+    def requirements(self):
+        if self.options.with_sdl2:
+            self.requires("sdl/[>=2.0.0]")
+
+        if not is_apple_os(self):
+            if self.options.with_blas:
+                self.requires("openblas/[>=0.3.7]")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -75,6 +102,38 @@ class WhisperCppConan(ConanFile):
         tc = CMakeToolchain(self)
         tc.variables["WHISPER_BUILD_TESTS"] = False
         tc.variables["WHISPER_BUILD_EXAMPLES"] = False
+
+        if self.options.shared:
+            tc.variables["BUILD_SHARED_LIBS"] = True
+        if self.options.sanitize_thread:
+            tc.variables["WHISPER_SANITIZE_THREAD"] = True
+        if self.options.sanitize_address:
+            tc.variables["WHISPER_SANITIZE_ADDRESS"] = True
+        if self.options.sanitize_undefined:
+            tc.variables["WHISPER_SANITIZE_UNDEFINED"] = True
+        if self.options.with_sdl2:
+            tc.variables["WHISPER_SDL2"] = True
+        if self.options.no_avx:
+            tc.variables["WHISPER_NO_AVX"] = True
+        if self.options.no_avx2:
+            tc.variables["WHISPER_NO_AVX2"] = True
+        if self.options.no_fma:
+            tc.variables["WHISPER_NO_FMA"] = True
+        if self.options.no_f16c:
+            tc.variables["WHISPER_NO_F16C"] = True
+
+        if is_apple_os(self):
+            if self.options.no_accelerate:
+                tc.variables["WHISPER_NO_ACCELERATE"] = True
+            if self.options.with_coreml:
+                tc.variables["WHISPER_COREML"] = True
+            if self.options.coreml_allow_fallback:
+                tc.variables["WHISPER_COREML_ALLOW_FALLBACK"] = True
+        else:
+            if self.options.with_blas:
+                tc.variables["WHISPER_BLAS"] = True
+                tc.variables["WHISPER_CUBLAS"] = True
+
         tc.generate()
 
     def build(self):
