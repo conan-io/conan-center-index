bca797eea96a7fa0e80260192a847dce0b764cc6
Martin Valgur <martin.valgur@gmail.com>
2023-06-14 16:30:19 +0300

    gtsam: add transitive_headers/libs to deps

--- recipes/gtsam/all/conanfile.py
+++ recipes/gtsam/all/conanfile.py
@@ -86,10 +86,10 @@ class GtsamConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.81.0")
-        self.requires("eigen/3.4.0")
+        self.requires("boost/1.81.0", transitive_headers=True, transitive_libs=True)
+        self.requires("eigen/3.4.0", transitive_headers=True)
         if self.options.with_TBB:
-            self.requires("onetbb/2020.3")
+            self.requires("onetbb/2020.3", transitive_headers=True, transitive_libs=True)
 
     @property
     def _required_boost_components(self):
