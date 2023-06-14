5b9ebc75f96a9c6b7a63242f4b4a2856c3f86ffb
Mateusz Pusz <mateusz.pusz@gmail.com>
2023-06-14 19:35:37 +0300

    `requires` attribute moved to `requirements` + traits added

--- recipes/mp-units/0.7.0/conanfile.py
+++ recipes/mp-units/0.7.0/conanfile.py
@@ -61,8 +61,10 @@ class MPUnitsConan(ConanFile):
         return "clang" in compiler and compiler.libcxx == "libc++" and version < 14
 
     def requirements(self):
+        self.requires("gsl-lite/0.38.0", transitive_headers=True)
+        self.requires("fmt/7.1.3", transitive_libs=True)
         if self._use_range_v3:
-            self.requires("range-v3/0.11.0")
+            self.requires("range-v3/0.11.0", transitive_headers=True)
 
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
