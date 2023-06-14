5418c3145d64fa6e7cd36891c6d80cb0dd1a9ec9
Mateusz Pusz <mateusz.pusz@gmail.com>
2023-06-14 19:35:37 +0300

    `cmake_layout` now properly points to the current directory

--- recipes/mp-units/0.7.0/conanfile.py
+++ recipes/mp-units/0.7.0/conanfile.py
@@ -84,7 +84,7 @@ class MPUnitsConan(ConanFile):
             )
 
     def layout(self):
-        cmake_layout(self, src_folder="src")
+        cmake_layout(self, src_folder=".")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
