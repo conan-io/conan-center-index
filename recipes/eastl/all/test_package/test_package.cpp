416b745ecd606cc28d2378abf7c3eacda689cf4d
Martin Valgur <martin.valgur@gmail.com>
2023-06-18 15:26:36 +0300

    eastl: add required `operator new[]()` to test_package.cpp

--- recipes/eastl/all/test_package/test_package.cpp
+++ recipes/eastl/all/test_package/test_package.cpp
@@ -1,7 +1,15 @@
 #include <EASTL/hash_map.h>
+#include <new>
 
-int main()
-{
+// https://github.com/electronicarts/EASTL/blob/master/doc/FAQ.md#info15-how-hard-is-it-to-incorporate-eastl-into-my-project
+void *operator new[](size_t size, const char *pName, int flags, unsigned debugFlags, const char *file, int line) {
+    return new uint8_t[size];
+}
+void *operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char *pName, int flags, unsigned debugFlags, const char *file, int line) {
+    return new uint8_t[size];
+}
+
+int main() {
     eastl::hash_map<int, int> map;
     map[0] = 1;
     return 0;
