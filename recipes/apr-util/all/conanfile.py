866ce13ad7eebbc624a1a779f836ece51f1a06a2
memsharded <james@conan.io>
2023-06-19 11:01:34 +0200

    new commit

--- recipes/apr-util/all/conanfile.py
+++ recipes/apr-util/all/conanfile.py
@@ -1,3 +1,5 @@
+import os
+
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
@@ -8,7 +10,7 @@ from conan.tools.files import apply_conandata_patches, copy, export_conandata_pa
 from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc
-import os
+
 
 required_conan_version = ">=1.54.0"
 
