819c5cda9a6b31509ba8c02421a4e4fb01c94714
Daulet <uralskdev@gmail.com>
2023-06-20 01:49:16 +0500

    Platform.Hashing package translated to conan 2.0
    Signed-off-by: Daulet <uralskdev@gmail.com>

--- recipes/platform.hashing/all/conanfile.py
+++ recipes/platform.hashing/all/conanfile.py
@@ -1,14 +1,11 @@
 from conan import ConanFile
-try:
-    from conan.tools.build import check_min_cppstd
-except ImportError:
-    from conans.tools import check_min_cppstd  # FIXME : not in 1.49
-from conan.tools.files import get
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import get, copy
 from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=2.0.0"
 
 
 class PlatformInterfacesConan(ConanFile):
@@ -26,8 +23,8 @@ class PlatformInterfacesConan(ConanFile):
     no_copy_source = True
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _subfolder_sources(self):
+        return os.path.join(self.source_folder, "cpp", "Platform.Hashing")
 
     @property
     def _internal_cpp_subfolder(self):
@@ -50,42 +47,55 @@ class PlatformInterfacesConan(ConanFile):
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler))
 
         if not minimum_version:
-            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
-                self.name, self.settings.compiler))
+            self.output.warn(f"{self.name} recipe lacks information about the {self.settings.compiler} compiler support.")
 
         elif Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration("{}/{} requires c++{}, "
-                                            "which is not supported by {} {}.".format(
-                self.name, self.version, self._minimum_cpp_standard, self.settings.compiler,
-                self.settings.compiler.version))
+            raise ConanInvalidConfiguration(f"{self.ref} requires c++{self._minimum_cpp_standard}, "
+                                            f"which is not supported by {self.settings.compiler} {self.settings.compiler.version}.")
 
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._minimum_cpp_standard)
 
         if self.settings.arch in ("x86", ):
-            raise ConanInvalidConfiguration("{} does not support arch={}".format(self.name, self.settings.arch))
+            raise ConanInvalidConfiguration(f"{self.name} does not support arch={self.settings.arch}")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version]["source"], strip_root=True)
 
     def package(self):
-        self.copy("*.h", dst="include", src=self._internal_cpp_subfolder)
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        copy(
+                self,
+                pattern="*.h",
+                dst=os.path.join(self.package_folder, "include"),
+                src=self.source_folder,
+            )
+        copy(self, pattern="LICENSE", dst="licenses", src=self.source_folder)
 
     def package_info(self):
+        self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
+
         suggested_flags = ""
-        if self.settings.compiler != "Visual Studio":
+        if self.settings.compiler != "msvc":
             suggested_flags = {
                 "x86_64": "-march=haswell",
                 "armv7": "-march=armv7",
                 "armv8": "-march=armv8-a",
             }.get(str(self.settings.arch), "")
-        self.user_info.suggested_flags = suggested_flags
+            
+        self.cpp_info.set_property("suggested_flags", suggested_flags)
 
         if "-march" not in "{} {}".format(os.environ.get("CPPFLAGS", ""), os.environ.get("CXXFLAGS", "")):
-            self.output.warn("platform.hashing needs to have `-march=ARCH` added to CPPFLAGS/CXXFLAGS. "
-                             "A suggestion is available in deps_user_info[{name}].suggested_flags.".format(name=self.name))
+            self.output.warning(f"platform.hashing needs to have `-march=ARCH` added to CPPFLAGS/CXXFLAGS. "
+                            f"A suggestion is available in the property '{self.name}:suggested_flags'.")
+
+        self.cpp_info.set_property("cmake_file_name", "Platform.Hashing")
+        self.cpp_info.set_property("cmake_target_name", "Platform.Hashing::Platform.Hashing")
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.names["cmake_find_package"] = "Platform.Hashing"
+        self.cpp_info.names["cmake_find_package_multi"] = "Platform.Hashing"
+
--- recipes/platform.hashing/all/test_package/conanfile.py
+++ recipes/platform.hashing/all/test_package/conanfile.py
@@ -1,28 +1,35 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanException
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMakeToolchain, CMake
 import os
 
-
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
 
     @property
     def _extra_flags(self):
-        return self.deps_user_info["platform.hashing"].suggested_flags
+        return self.dependencies["platform.hashing"].cpp_info.get_property("suggested_flags")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        if str(self.settings.compiler) != "msvc" and self._extra_flags:
+            tc = CMakeToolchain(self)
+            tc.variables["EXTRA_FLAGS"] = self._extra_flags
+            tc.generate()
 
     def build(self):
-        if self.settings.compiler != "Visual Studio":
-            if not self._extra_flags:
-                raise ConanException("Suggested flags are not available for os={}/arch={}".format(self.settings.os, self.settings.arch))
-
         cmake = CMake(self)
-        if self.settings.compiler != "Visual Studio":
-            cmake.definitions["EXTRA_FLAGS"] = self._extra_flags
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
