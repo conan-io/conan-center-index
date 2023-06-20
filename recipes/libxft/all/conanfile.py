05fb2689ed52c4bbe28bf4d9d4be5b2b70733779
memsharded <james@conan.io>
2023-06-21 00:21:22 +0200

    upgrades libxft to conanv2

--- recipes/libxft/all/conanfile.py
+++ recipes/libxft/all/conanfile.py
@@ -1,7 +1,9 @@
 from conan import ConanFile
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, chdir, rm, rmdir
-from conans import AutoToolsBuildEnvironment
-import functools
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, chdir, rm, rmdir, copy
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps, PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.env import VirtualRunEnv, VirtualBuildEnv
+from conan.tools.build import cross_building
 
 required_conan_version = ">=1.52.0"
 
@@ -16,28 +18,26 @@ class libxftConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "pkg_config"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     def export_sources(self):
         export_conandata_patches(self)
 
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
     def requirements(self):
         self.requires("xorg/system")
-        self.requires("freetype/2.13.0")
-        self.requires("fontconfig/2.14.2")
+        self.requires("freetype/2.13.0", transitive_headers=True)
+        self.requires("fontconfig/2.14.2", transitive_headers=True)
 
     def build_requirements(self):
-        self.build_requires("pkgconf/1.9.3")
-        self.build_requires("xorg-macros/1.19.3")
-        self.build_requires("libtool/2.4.7")
+        self.tool_requires("pkgconf/1.9.3")
+        self.tool_requires("xorg-macros/1.19.3")
+        self.tool_requires("libtool/2.4.7")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+                  destination=self.source_folder, strip_root=True)
 
     def configure(self):
         del self.settings.compiler.libcxx
@@ -45,29 +45,45 @@ class libxftConan(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
-    @functools.lru_cache(1)
-    def _configure_autotools(self):
-        args = ["--disable-dependency-tracking"]
+    def generate(self):
+        # inject tool_requires env vars in build scope (not needed if there is no tool_requires)
+        env = VirtualBuildEnv(self)
+        env.generate()
+        # inject requires env vars in build scope
+        # it's required in case of native build when there is AutotoolsDeps & at least one dependency which might be shared, because configure tries to run a test executable
+        if not cross_building(self):
+            env = VirtualRunEnv(self)
+            env.generate(scope="build")
+
+        tc = AutotoolsToolchain(self)
+        tc.configure_args.extend(["--disable-dependency-tracking"])
         if self.options.shared:
-            args.extend(["--disable-static", "--enable-shared"])
+            tc.configure_args.extend(["--disable-static", "--enable-shared"])
         else:
-            args.extend(["--disable-shared", "--enable-static"])
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.configure(args=args, pkg_config_paths=self.build_folder)
-        return autotools
+            tc.configure_args.extend(["--disable-shared", "--enable-static"])
+        tc.generate()
+
+        # generate pkg-config files of dependencies (useless if upstream configure.ac doesn't rely on PKG_CHECK_MODULES macro)
+        tc = PkgConfigDeps(self)
+        tc.generate()
+        # generate dependencies for autotools
+        tc = AutotoolsDeps(self)
+        tc.generate()
+
 
     def build(self):
         apply_conandata_patches(self)
-        with chdir(self, self._source_subfolder):
-            autotools = self._configure_autotools()
-            autotools.make()
+        autotools = Autotools(self)
+        autotools.autoreconf()
+        autotools.configure()
+        autotools.make()
+
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        with chdir(self, self._source_subfolder):
-            autotools = self._configure_autotools()
-            autotools.install(args=["-j1"])
+        copy(self, pattern="LICENSE", dst="licenses", src=self.source_folder)
+        copy(self, pattern="COPYING", dst="licenses", src=self.source_folder)
+        autotools = Autotools(self)
+        autotools.install()
         rm(self, "*.la", f"{self.package_folder}/lib", recursive=True)
         rmdir(self, f"{self.package_folder}/lib/pkgconfig")
         rmdir(self, f"{self.package_folder}/share")
