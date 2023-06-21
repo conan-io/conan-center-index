e0ab189577de52a3a9f23d0aed2c7d53b80b0c72
Martin Valgur <martin.valgur@gmail.com>
2023-06-21 11:26:22 +0300

    rmm: fix .get_safe() in Conan v1

--- recipes/rmm/all/conanfile.py
+++ recipes/rmm/all/conanfile.py
@@ -60,7 +60,7 @@ class RmmConan(ConanFile):
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
 
-        if self.options["thrust"].get_safe("backend") != "cuda":
+        if "backend" not in self.options["thrust"] or self.options["thrust"].backend != "cuda":
             self.output.warning(
                 "RMM requires the CUDA backend to be enabled in Thrust by setting thrust/*:backend=cuda."
             )
