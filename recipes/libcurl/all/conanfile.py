d6fbc96e1be5d8a42a93ce317a064a8c69a1c76b
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-06-21 21:32:27 +0000

    libcurl/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libcurl/all/conanfile.py
+++ recipes/libcurl/all/conanfile.py
@@ -187,7 +187,7 @@ class LibcurlConan(ConanFile):
         if self.options.with_nghttp2:
             self.requires("libnghttp2/1.54.0")
         if self.options.with_libssh2:
-            self.requires("libssh2/1.10.0")
+            self.requires("libssh2/1.11.0")
         if self.options.with_zlib:
             self.requires("zlib/1.2.13")
         if self.options.with_brotli:
