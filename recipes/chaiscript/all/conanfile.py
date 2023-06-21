feb1c7bd8948b9246822882a5aee2aa5d3b09732
holger-detering <github@detering-springhoe.de>
2023-06-21 13:04:03 +0200

    chaiscript: conan v2 support

--- recipes/chaiscript/all/conanfile.py
+++ recipes/chaiscript/all/conanfile.py
@@ -1,5 +1,10 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.files import get, copy, rmdir, collect_libs
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
+
+
+required_conan_version = ">=1.53.0"
 
 
 class ChaiScriptConan(ConanFile):
@@ -10,7 +15,6 @@ class ChaiScriptConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     license = "BSD-3-Clause"
     exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
     options = {"fPIC": [True, False], "dyn_load": [True, False], "use_std_make_shared": [True, False],
                "multithread_support": [True, False],
@@ -20,61 +24,57 @@ class ChaiScriptConan(ConanFile):
                        "multithread_support": True,
                        "header_only": True}
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "ChaiScript-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = False
-        cmake.definitions["BUILD_SAMPLES"] = False
-        cmake.definitions["BUILD_MODULES"] = True
-        cmake.definitions["USE_STD_MAKE_SHARED"] = self.options.use_std_make_shared
-        cmake.definitions["DYNLOAD_ENABLED"] = self.options.dyn_load
-        cmake.definitions["MULTITHREAD_SUPPORT_ENABLED"] = self.options.multithread_support
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        # BUILD_SHARED_LIBS and POSITION_INDEPENDENT_CODE are automatically parsed when self.options.shared or self.options.fPIC exist
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_TESTING"] = False
+        tc.variables["BUILD_SAMPLES"] = False
+        tc.variables["BUILD_MODULES"] = True
+        tc.variables["USE_STD_MAKE_SHARED"] = self.options.use_std_make_shared
+        tc.variables["DYNLOAD_ENABLED"] = self.options.dyn_load
+        tc.variables["MULTITHREAD_SUPPORT_ENABLED"] = self.options.multithread_support
+        tc.generate()
 
     def build(self):
         if not self.options.header_only:
-            cmake = self._configure_cmake()
+            cmake = CMake(self)
+            cmake.configure()
             cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         if self.options.header_only:
-            self.copy(pattern="*.hpp", dst="include",
-                      src=os.path.join(self._source_subfolder, 'include'))
+            copy(self, pattern="*.hpp",
+                dst=os.path.join(self.package_folder, "include"),
+                src=os.path.join(self.source_folder, "include"))
         else:
-            cmake = self._configure_cmake()
+            cmake = CMake(self)
             cmake.install()
-            tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-            tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-            tools.rmdir(os.path.join(self.package_folder, "share"))
+            rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+            rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+            rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_id(self):
-        if self.options.header_only:
-            self.info.header_only()
+        if self.info.options.header_only:
+            self.info.clear()
 
     def package_info(self):
         if not self.options.header_only:
-            self.cpp_info.libs = tools.collect_libs(self)
+            self.cpp_info.libs = collect_libs(self)
         if self.options.use_std_make_shared:
             self.cpp_info.defines.append("CHAISCRIPT_USE_STD_MAKE_SHARED")
-        if self.settings.os == "Linux":
-            self.cpp_info.system_libs = ["dl"]
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.append("m")
+            self.cpp_info.system_libs.append("dl")
             if self.options.multithread_support:
                 self.cpp_info.system_libs.append("pthread")
