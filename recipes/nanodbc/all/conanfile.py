6e5a3f796a2c9c87444daa2c5d8bd48eb4659acf
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-06-22 10:50:51 +0100

    nanodbc: restore compiler version checks for Conan 1.x packages, minor cleanup

--- recipes/nanodbc/all/conanfile.py
+++ recipes/nanodbc/all/conanfile.py
@@ -1,8 +1,8 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
 from conan.tools.files import get, apply_conandata_patches, export_conandata_patches, rmdir, copy
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 from conan.tools.scm import Version
 import os
 
@@ -13,7 +13,6 @@ class NanodbcConan(ConanFile):
     license = "MIT"
     homepage = "https://github.com/nanodbc/nanodbc/"
     url = "https://github.com/conan-io/conan-center-index"
-
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -30,7 +29,6 @@ class NanodbcConan(ConanFile):
         "with_boost": False,
     }
 
-    generators = "CMakeDeps"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -46,8 +44,29 @@ class NanodbcConan(ConanFile):
         if self.options.shared:
             self.options.rm_safe("fPIC")
 
+    @property
+    def _min_cppstd(self):
+        return 14
+    
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "5",
+            "clang": "3.4",
+            "Visual Studio": "14",
+            "msvc": "190",
+            "apple-clang": "9.1",  # FIXME: this is a guess
+        }
+
     def validate(self):
-        check_min_cppstd(self, 14)
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, 14)
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.",
+            )
 
         if self.settings.compiler == "apple-clang" and Version(self.version) != "2.14.0":
             raise ConanInvalidConfiguration("""
@@ -62,23 +81,22 @@ class NanodbcConan(ConanFile):
             self.requires("odbc/2.3.9")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-
         tc.cache_variables["NANODBC_DISABLE_ASYNC"] = not self.options.get_safe("async")
         tc.cache_variables["NANODBC_ENABLE_UNICODE"] = self.options.unicode
         tc.cache_variables["NANODBC_ENABLE_BOOST"] = self.options.with_boost
         tc.cache_variables["NANODBC_DISABLE_LIBCXX"] = self.settings.get_safe("compiler.libcxx") != "libc++"
-
         tc.cache_variables["NANODBC_DISABLE_INSTALL"] = False
         tc.cache_variables["NANODBC_DISABLE_EXAMPLES"] = True
         tc.cache_variables["NANODBC_DISABLE_TESTS"] = True
-
         tc.cache_variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
-
         tc.generate()
+        
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         apply_conandata_patches(self)
@@ -97,8 +115,6 @@ class NanodbcConan(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.set_property("cmake_file_name", "nanodbc")
-        self.cpp_info.set_property("cmake_target_name", "nanodbc::nanodbc")
         self.cpp_info.libs = ["nanodbc"]
 
         if not self.options.shared and self.settings.os == "Windows":
