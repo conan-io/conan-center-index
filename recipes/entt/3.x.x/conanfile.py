923d115402d0e40db583b4c0c248e3ad4d981c68
Raziel <raziel@digdyn.com>
2023-06-22 23:56:58 +0300

    Remove older versions Add cpp_info.names back

--- recipes/entt/3.x.x/conanfile.py
+++ recipes/entt/3.x.x/conanfile.py
@@ -73,3 +73,7 @@ class EnttConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "EnTT::EnTT")
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
+
+        # TODO: to remove in conan v2
+        self.cpp_info.names["cmake_find_package"] = "EnTT"
+        self.cpp_info.names["cmake_find_package_multi"] = "EnTT"
