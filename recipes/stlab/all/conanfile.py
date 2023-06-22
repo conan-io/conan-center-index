ec898c07f67b0574c6cd3a05e1afcdf73b81093c
Francisco Ramirez de Anton <franchuti688@gmail.com>
2023-06-22 12:38:59 +0200

    Refactoring and fixing errors

--- recipes/stlab/all/conanfile.py
+++ recipes/stlab/all/conanfile.py
@@ -1,14 +1,15 @@
-from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import copy, get, rm, rmdir
-from conan.tools.scm import Version
-from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.microsoft import check_min_vs, is_msvc
-
 import os
 
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, rm, rmdir
+from conan.tools.microsoft import check_min_vs, is_msvc
+from conan.tools.scm import Version
+
 required_conan_version = ">=1.52.0"
+
 
 class Stlab(ConanFile):
     name = 'stlab'
@@ -17,26 +18,31 @@ class Stlab(ConanFile):
     homepage = 'https://github.com/stlab/libraries'
     license = 'BSL-1.0'
     topics = 'concurrency', 'futures', 'channels'
-
     settings = "arch", "os", "compiler", "build_type",
-
     options = {
         "with_boost": [True, False],
         "no_std_coroutines": [True, False],
         "future_coroutines": [True, False],
-        "task_system": ["portable", "libdispatch", "emscripten", "pnacl", "windows", "auto"],
-        "thread_system": ["win32", "pthread", "pthread-emscripten", "pthread-apple", "none", "auto"],
+        "task_system": ["portable", "libdispatch", "emscripten", "pnacl", "windows"],
+        "thread_system": ["win32", "pthread", "pthread-emscripten", "pthread-apple", "none"],
     }
-
     default_options = {
         "with_boost": False,
         "no_std_coroutines": True,
-        "future_coroutines": False,
-        "task_system": "auto",
-        "thread_system": "auto",
+        "future_coroutines": False
+        # Handle default value for `thread_system` in `config_options` method
+        # Handle default value for `task_system` in `config_options` method
     }
-
+    package_type = "header-library"
     short_paths = True
+
+    def config_options(self):
+        self.options.thread_system = {"Macos": "pthread-apple",
+                                      "Linux": "pthread",
+                                      "Windows": "win32",
+                                      "Emscripten": "pthread-emscripten"}.get(str(self.settings.os), "none")
+        self.options.task_system = {"Macos": "libdispatch",
+                                    "Windows": "windows"}.get(str(self.settings.os), "portable")
 
     @property
     def _minimum_cpp_standard(self):
@@ -45,122 +51,77 @@ class Stlab(ConanFile):
     @property
     def _compilers_minimum_version(self):
         return {"gcc": "9",
-                "Visual Studio": "15.8",
                 "clang": "8",
-                "apple-clang": "13",
-                }
+                "apple-clang": "13"}
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
-    def _requires_libdispatch(self):
-        # On macOS it is not necessary to use the libdispatch conan package, because the library is
-        # included in the OS.
-        return self.options.task_system == "libdispatch" and self.settings.os != "Macos"
-
     def build_requirements(self):
-        self.build_requires("cmake/3.23.3")
+        self.tool_requires("cmake/[>=3.23.3]")
 
     def requirements(self):
         if self.options.with_boost:
             self.requires("boost/1.80.0")
 
-        if self._requires_libdispatch():
+        # On macOS, it is not necessary to use the libdispatch conan package, because the library is
+        # included in the OS.
+        if self.options.task_system == "libdispatch" and self.settings.os != "Macos":
             self.requires("libdispatch/5.3.2")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
-    def _default_task_system(self):
-        if self.settings.os == "Macos":
-            return "libdispatch"
-
-        if self.settings.os == "Windows":
-            return "windows"
-
-        return "portable"
-
-    def _validate_task_system_libdispatch(self):
-        if self.settings.os == "Linux":
-            if self.settings.compiler != "clang":
-                raise ConanInvalidConfiguration(f"{self.ref} task_system=libdispatch needs Clang compiler when using OS: {self.settings.os}. Use Clang compiler or switch to task_system=portable or task_system=auto")
-        elif self.settings.os != "Macos":
-            raise ConanInvalidConfiguration(f"{self.ref} task_system=libdispatch is not supported on {self.settings.os}. Try using task_system=auto")
-
     def _validate_task_system(self):
         if self.options.task_system == "libdispatch":
-            self._validate_task_system_libdispatch()
-        elif self.options.task_system == "windows":
-            if self.settings.os != "Windows":
-                raise ConanInvalidConfiguration(f"{self.ref} task_system=windows is not supported on {self.settings.os}. Try using task_system=auto")
-
-    def _default_thread_system(self):
-        if self.settings.os == "Macos":
-            return "pthread-apple"
-
-        if self.settings.os == "Linux":
-            return "pthread"
-
-        if self.settings.os == "Windows":
-            return "win32"
-
-        if self.settings.os == "Emscripten":
-            return "pthread-emscripten"
-
-        return "none"
+            if self.settings.os == "Linux" and self.settings.compiler != "clang":
+                raise ConanInvalidConfiguration(f"{self.ref} task_system=libdispatch needs Clang compiler when using OS: {self.settings.os}."
+                                                f" Use Clang compiler or switch to task_system=portable")
+            elif self.settings.os != "Macos":
+                raise ConanInvalidConfiguration(f"{self.ref} task_system=libdispatch is not supported on {self.settings.os}")
+        elif self.options.task_system == "windows" and self.settings.os != "Windows":
+            raise ConanInvalidConfiguration(f"{self.ref} task_system=windows is not supported on {self.settings.os}")
 
     def _validate_thread_system(self):
-        if self.options.thread_system == "pthread-apple" and self.settings.os != "Macos":
-                raise ConanInvalidConfiguration("{}/{} thread_system=pthread-apple is not supported on {}. Try using thread_system=auto".format(self.name, self.version, self.settings.os))
-        elif self.options.thread_system == "pthread" and self.settings.os != "Linux":
-                raise ConanInvalidConfiguration("{}/{} thread_system=pthread is not supported on {}. Try using thread_system=auto".format(self.name, self.version, self.settings.os))
-        elif self.options.thread_system == "win32" and self.settings.os != "Windows":
-                raise ConanInvalidConfiguration("{}/{} thread_system=win32 is not supported on {}. Try using thread_system=auto".format(self.name, self.version, self.settings.os))
-        elif self.options.thread_system == "pthread-emscripten" and self.settings.os != "Emscripten":
-                raise ConanInvalidConfiguration("{}/{} thread_system=pthread-emscripten is not supported on {}. Try using thread_system=auto".format(self.name, self.version, self.settings.os))
+        if any([self.options.thread_system == "pthread-apple" and self.settings.os != "Macos",
+                self.options.thread_system == "pthread" and self.settings.os != "Linux",
+                self.options.thread_system == "win32" and self.settings.os != "Windows",
+                self.options.thread_system == "pthread-emscripten" and self.settings.os != "Emscripten"]):
+            raise ConanInvalidConfiguration(f"{self.ref} thread_system={self.options.thread_system} is not supported on {self.settings.os}")
 
     def _validate_boost_components(self):
-        if self.settings.os != "Macos": return
-        if self.settings.compiler != "apple-clang": return
-        if Version(self.settings.compiler.version) >= "12": return
-        if self.options.with_boost: return
-        #
-        # On Apple we have to force the usage of boost.variant and boost.optional, because Apple's implementation of C++17
-        # is not complete.
-        #
-        raise ConanInvalidConfiguration(f"Apple-Clang versions less than 12 do not correctly support std::optional or std::variant, so we will use boost::optional and boost::variant instead. Try -o {self.name}:with_boost=True.")
+        if not any([self.settings.os != "Macos", self.settings.compiler != "apple-clang",
+                    Version(str(self.settings.compiler.version)) >= "12", self.options.with_boost]):
+            # On Apple we have to force the usage of boost.variant and boost.optional, because Apple's implementation of C++17
+            # is not complete.
+            raise ConanInvalidConfiguration(
+                f"Compiler Apple-Clang < 12 versions do not correctly support std::optional or std::variant, "
+                f"so we will use boost::optional and boost::variant instead. Try -o {self.name}:with_boost=True.")
+
+    def _validate_min_compiler_version(self):
+        if is_msvc(self):
+            check_min_vs(self, "192")
+        else:
+            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+            if not minimum_version:
+                self.output.warn(f"{self.name} {self.version} requires C++20. Your compiler is unknown. Assuming it supports C++20.")
+            elif Version(str(self.settings.compiler.version)) < minimum_version:
+                raise ConanInvalidConfiguration(f"{self.name} {self.version} requires C++20, which your compiler does not support.")
+            if self.info.settings.compiler == "clang" and str(self.info.settings.compiler.version) in ("13", "14"):
+                raise ConanInvalidConfiguration(
+                    f"{self.ref} currently does not work with Clang {self.info.settings.compiler.version} on CCI, it enters "
+                    f"in an infinite build loop (smells like a compiler bug). Contributions are welcomed!")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._minimum_cpp_standard)
 
-        def _lazy_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
-
-        check_min_vs(self, "192")
-        if not is_msvc(self):
-            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-            if not minimum_version:
-                self.output.warn(f"{self.name} {self.version} requires C++20. Your compiler is unknown. Assuming it supports C++20.")
-            elif _lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
-                raise ConanInvalidConfiguration(f"{self.name} {self.version} requires C++20, which your compiler does not support.")
-            if self.info.settings.compiler == "clang" and str(self.info.settings.compiler.version) in ("13", "14"):
-                raise ConanInvalidConfiguration(f"{self.ref} currently does not work with Clang {self.info.settings.compiler.version} on CCI, it enters in an infinite build loop (smells like a compiler bug). Contributions are welcomed!")
-
+        self._validate_min_compiler_version()
         self._validate_task_system()
         self._validate_thread_system()
         self._validate_boost_components()
 
     def configure(self):
-        if self.options.task_system == "auto":
-            self.options.task_system = self._default_task_system()
-
-        if self.options.thread_system == "auto":
-            self.options.thread_system = self._default_thread_system()
-
         self.output.info("STLab With Boost: {}.".format(self.options.with_boost))
         self.output.info("STLab Future Coroutines: {}.".format(self.options.future_coroutines))
         self.output.info("STLab No Standard Coroutines: {}.".format(self.options.no_std_coroutines))
@@ -169,18 +130,15 @@ class Stlab(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        # tc.variables["CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP"] = True
         tc.variables['BUILD_TESTING'] = not self.conf.get("tools.build:skip_test", default=True, check_type=bool)
         tc.variables["STLAB_USE_BOOST_CPP17_SHIMS"] = self.options.with_boost
         tc.variables["STLAB_NO_STD_COROUTINES"] = self.options.no_std_coroutines
         tc.variables["STLAB_THREAD_SYSTEM"] = self.options.thread_system
         tc.variables["STLAB_TASK_SYSTEM"] = self.options.task_system
-
         tc.generate()
 
         tc = CMakeDeps(self)
         tc.generate()
-
 
     def build(self):
         cmake = CMake(self)
@@ -198,7 +156,7 @@ class Stlab(ConanFile):
         rm(self, "vcruntime*.dll", os.path.join(self.package_folder, "bin"))
 
     def package_id(self):
-        #TODO: is header only but needs a header modified by cmake
+        # TODO: is header only but needs a header modified by cmake
         # self.info.settings.clear()
         # self.info.header_only()
         pass
--- recipes/stlab/all/test_package/conanfile.py
+++ recipes/stlab/all/test_package/conanfile.py
@@ -1,12 +1,16 @@
+import os
+
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake
-import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def layout(self):
         cmake_layout(self)
