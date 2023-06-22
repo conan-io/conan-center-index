5e1706693df1808b7eb59be2773d38bd95ab9d7b
CD Clark III <clifton.clark@gmail.com>
2023-06-21 22:02:53 -0500

    [libinterpolate]: added version 2.6.4, which adds Windows support.

--- recipes/libinterpolate/all/conanfile.py
+++ recipes/libinterpolate/all/conanfile.py
@@ -43,9 +43,6 @@ class PackageConan(ConanFile):
         self.requires("eigen/3.3.7", transitive_headers=True)
 
     def validate(self):
-        if self.settings.os != "Linux":
-            raise ConanInvalidConfiguration("libInterpolate currently only supports Linux. Upstream PR's are welcome (https://github.com/CD3/libInterpolate/issues/14).")
-
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
         minimum_version = self._compilers_minimum_version.get(
