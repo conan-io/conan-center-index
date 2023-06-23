2878d71f36cf94e47fce0b04d3541b19320631f6
Pau Farré <Hopobcn@users.noreply.github.com>
2023-06-23 09:03:08 +0000

    Order imports

--- recipes/folly/all/conanfile.py
+++ recipes/folly/all/conanfile.py
@@ -1,10 +1,10 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.microsoft import is_msvc, msvc_runtime_flag
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir, replace_in_file
 from conan.tools.build import can_run, check_min_cppstd
-from conan.tools.scm import Version
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir, replace_in_file
+from conan.tools.microsoft import is_msvc, msvc_runtime_flag
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.53.0"
