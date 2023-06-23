72e1293b7769afce4ab32cc2662bbd5fc26e12a7
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-06-23 15:48:37 +0000

    vulkan-validationlayers/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/vulkan-validationlayers/all/conanfile.py
+++ recipes/vulkan-validationlayers/all/conanfile.py
@@ -106,7 +106,7 @@ class VulkanValidationLayersConan(ConanFile):
         if self.options.get_safe("with_wsi_xcb") or self.options.get_safe("with_wsi_xlib"):
             self.requires("xorg/system")
         if self._needs_wayland_for_build:
-            self.requires("wayland/1.21.0")
+            self.requires("wayland/1.22.0")
 
     def _require(self, recipe_name):
         if recipe_name not in self._dependencies_versions:
