3300547dd74ef0dadea9e53cdeba04ba4a6da477
Rasmus Thomsen <rasmus.thomsen@dampsoft.de>
2023-06-27 14:09:48 +0200

    dcmtk: fixup build with libxml2

--- recipes/dcmtk/all/conanfile.py
+++ recipes/dcmtk/all/conanfile.py
@@ -82,7 +82,7 @@ class DCMTKConan(ConanFile):
         if self.options.with_zlib:
             self.requires("zlib/1.2.13")
         if self.options.with_openssl:
-            self.requires("openssl/1.1.1t")
+            self.requires("openssl/[>=1.1 <4]")
         if self.options.with_libpng:
             self.requires("libpng/1.6.39")
         if self.options.with_libtiff:
