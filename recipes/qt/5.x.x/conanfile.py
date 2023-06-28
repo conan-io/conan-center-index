67820ca933ca0f9149e0032c153d137bece483c9
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-06-28 12:01:55 +0200

    bump at-spi2-core

--- recipes/qt/5.x.x/conanfile.py
+++ recipes/qt/5.x.x/conanfile.py
@@ -410,7 +410,7 @@ class QtConan(ConanFile):
         if self.settings.os in ['Linux', 'FreeBSD'] and self.options.with_gssapi:
             self.requires("krb5/1.18.3") # conan-io/conan-center-index#4102
         if self.options.get_safe("with_atspi"):
-            self.requires("at-spi2-core/2.48.0")
+            self.requires("at-spi2-core/2.48.3")
         if self.options.get_safe("with_md4c", False):
             self.requires("md4c/0.4.8")
 
