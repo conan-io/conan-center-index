150d87c3b8836eb67d7477d32dacad454209217a
Martin Valgur <martin.valgur@gmail.com>
2023-06-28 21:16:08 +0300

    opentelemetry-proto: add package_type, clean up

--- recipes/opentelemetry-proto/all/test_package/conanfile.py
+++ recipes/opentelemetry-proto/all/test_package/conanfile.py
@@ -1,34 +1,19 @@
-from conan import ConanFile
-from conan.tools.cmake import CMakeDeps, CMakeToolchain
-from conan.tools.env import VirtualBuildEnv
-from conan.tools.layout import basic_layout
 import os
+
+from conan import ConanFile
+
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     test_type = "explicit"
 
-    _res_folder = ""
-
     def layout(self):
-        basic_layout(self, src_folder="src")
+        pass
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
-        tc = CMakeDeps(self)
-        tc.generate()
-        tc = VirtualBuildEnv(self)
-        tc.generate(scope="build")
-
-        self._res_folder = self.dependencies["opentelemetry-proto"].conf_info.get("user.opentelemetry-proto:proto_root")
-
     def test(self):
-        # TODO: to remove in conan v2
-        if self._res_folder == "":
-            self._res_folder = self.deps_user_info["opentelemetry-proto"].proto_root
-
-        assert os.path.isfile(os.path.join(self._res_folder, "opentelemetry", "proto", "common", "v1", "common.proto"))
+        res_folder = self.dependencies[self.tested_reference_str].conf_info.get("user.opentelemetry-proto:proto_root")
+        proto_path = os.path.join(res_folder, "opentelemetry", "proto", "common", "v1", "common.proto")
+        assert os.path.isfile(proto_path)
