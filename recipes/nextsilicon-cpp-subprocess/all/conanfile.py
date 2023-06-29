8d6835839924db814a4c5053829f8764480aa19e
Martin Valgur <martin.valgur@gmail.com>
2023-06-29 12:34:00 +0100

    (#18135) nextsilicon-cpp-subprocess: add cppstd check, fix typo
    Follow-up to https://github.com/conan-io/conan-center-index/pull/18127/files#r1246355487

--- recipes/nextsilicon-cpp-subprocess/all/conanfile.py
+++ recipes/nextsilicon-cpp-subprocess/all/conanfile.py
@@ -1,6 +1,7 @@
 import os
 
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
 
@@ -11,8 +12,8 @@ class CppSubprocess(ConanFile):
     name = "nextsilicon-cpp-subprocess"
     description = (
         "Subprocessing with modern C++. "
-        "The only goal was to develop something that is as close as"
-        "Python subprocess module in dealing with processes."
+        "The only goal was to develop something that is as close as possible"
+        " to the Python subprocess module in dealing with processes."
     )
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
@@ -23,18 +24,22 @@ class CppSubprocess(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-
     def layout(self):
         basic_layout(self, src_folder="src")
-
 
     def package_id(self):
         self.info.clear()
 
+    @property
+    def _min_cppstd(self):
+        return 11
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-
 
     def package(self):
         copy(self, "subprocess.hpp",
@@ -43,7 +48,6 @@ class CppSubprocess(ConanFile):
         copy(self, "LICENSE.MIT",
              dst=os.path.join(self.package_folder, "licenses"),
              src=self.source_folder)
-
 
     def package_info(self):
         self.cpp_info.bindirs = []
