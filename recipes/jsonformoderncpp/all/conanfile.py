61ea0e6eee8a9f3b1a2763bfe52bbe6529054937
Martin Valgur <martin.valgur@gmail.com>
2023-06-30 08:50:49 +0300

    jsonformoderncpp: migrate to Conan v2

--- recipes/jsonformoderncpp/all/conanfile.py
+++ recipes/jsonformoderncpp/all/conanfile.py
@@ -1,51 +1,60 @@
-from conans import ConanFile, tools, CMake
 import os
 
-required_conan_version = ">=1.28.0"
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, rmdir, rm
+
+required_conan_version = ">=1.52.0"
 
 
 class JsonformoderncppConan(ConanFile):
     name = "jsonformoderncpp"
-    homepage = "https://github.com/nlohmann/json"
-    description = "JSON for Modern C++ parser and generator."
-    topics = ("conan", "jsonformoderncpp", "json", "header-only")
-    url = "https://github.com/conan-io/conan-center-index"
-    settings = "os", "compiler", "arch", "build_type"
-    no_copy_source = True
-    license = "MIT"
     deprecated = "nlohmann_json"
-    options = {"multiple_headers": [True, False]}
-    default_options = {"multiple_headers": False}
+    description = "JSON for Modern C++ parser and generator."
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/nlohmann/json"
+    topics = ("json", "header-only")
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "multiple_headers": [True, False],
+    }
+    default_options = {
+        "multiple_headers": False,
+    }
+    no_copy_source = True
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "json-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["JSON_BuildTests"] = False
-        cmake.definitions["JSON_MultipleHeaders"] = self.options.multiple_headers
-        cmake.configure(source_folder=self._source_subfolder)
-        return cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["JSON_BuildTests"] = False
+        tc.variables["JSON_MultipleHeaders"] = self.options.multiple_headers
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE*", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE*", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib"))
-        try:
-            os.remove(os.path.join(self.package_folder, "nlohmann_json.natvis"))
-        except FileNotFoundError:
-            pass
+        rmdir(self, os.path.join(self.package_folder, "lib"))
+        rm(self, "nlohmann_json.natvis", self.package_folder)
 
-    def package_id(self):
-        self.info.header_only()
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
