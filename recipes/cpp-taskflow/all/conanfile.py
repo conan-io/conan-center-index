ec6de94e66ed7780512b0b2b66d4b9dd38071d5c
Martin Valgur <martin.valgur@gmail.com>
2023-06-30 14:04:53 +0300

    cpp-taskflow: migrate to Conan v2

--- recipes/cpp-taskflow/all/conanfile.py
+++ recipes/cpp-taskflow/all/conanfile.py
@@ -1,73 +1,92 @@
 import os
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.28.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.52.0"
+
 
 class CppTaskflowConan(ConanFile):
     name = "cpp-taskflow"
     description = "A fast C++ header-only library to help you quickly write parallel programs with complex task dependencies."
-    topics = ("conan", "cpp-taskflow", "taskflow", "tasking", "parallelism")
+    license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/cpp-taskflow/cpp-taskflow"
-    license = "MIT"
+    topics = ("taskflow", "tasking", "parallelism", "header-only")
+
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
-    settings = "os", "compiler"
     deprecated = "taskflow"
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _min_cppstd(self):
+        return 17 if Version(self.version) <= "2.2.0" else 14
 
-    def configure(self):
-        compiler = str(self.settings.compiler)
-        compiler_version = tools.Version(self.settings.compiler.version)
-        min_req_cppstd = "17" if tools.Version(self.version) <= "2.2.0" else "14"
-
-        if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, min_req_cppstd)
-        else:
-            self.output.warn("%s recipe lacks information about the %s compiler"
-                             " standard version support" % (self.name, compiler))
-
-        minimal_version = {
-            "17": {
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            17: {
                 "Visual Studio": "16",
                 "gcc": "7.3",
                 "clang": "6.0",
-                "apple-clang": "10.0"
+                "apple-clang": "10.0",
             },
-            "14": {
+            14: {
                 "Visual Studio": "15",
                 "gcc": "5",
                 "clang": "4.0",
-                "apple-clang": "8.0"
+                "apple-clang": "8.0",
             }
-        }
+        }[self._min_cppstd]
 
-        if compiler not in minimal_version[min_req_cppstd]:
-            self.output.info("%s requires a compiler that supports at least C++%s" % (self.name, min_req_cppstd))
+    def validate(self):
+        compiler = str(self.settings.compiler)
+        compiler_version = Version(self.settings.compiler.version)
+
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        else:
+            self.output.warning(f"{self.name} recipe lacks information about the {compiler} compiler standard version support")
+
+        if compiler not in self._compilers_minimum_version:
+            self.output.info(f"{self.name} requires a compiler that supports at least C++{self._min_cppstd}")
             return
 
         # Exclude compilers not supported by cpp-taskflow
-        if compiler_version < minimal_version[min_req_cppstd][compiler]:
-            raise ConanInvalidConfiguration("%s requires a compiler that supports"
-                                            " at least C++%s. %s %s is not"
-                                            " supported." % (self.name, min_req_cppstd, compiler, tools.Version(self.settings.compiler.version.value)))
+        if compiler_version < self._compilers_minimum_version[compiler]:
+            raise ConanInvalidConfiguration(
+                f"{self.name} requires a compiler that supports at least C++{self._min_cppstd}."
+                f" {compiler} {Version(self.settings.compiler.version.value)} is not supported."
+            )
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("taskflow-" + self.version, self._source_subfolder)
-
-    def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="*", dst="include/taskflow", src=os.path.join(self._source_subfolder, "taskflow"))
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def package(self):
+        copy(self, "LICENSE",
+             dst=os.path.join(self.package_folder, "licenses"),
+             src=self.source_folder)
+        copy(self, "*",
+             dst=os.path.join(self.package_folder, "include/taskflow"),
+             src=os.path.join(self.source_folder, "taskflow"))
 
     def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("pthread")
-        if self.settings.compiler == "Visual Studio":
+        if is_msvc(self):
             self.cpp_info.defines.append("_ENABLE_EXTENDED_ALIGNED_STORAGE")
--- recipes/cpp-taskflow/all/test_package/test_package.cpp
+++ recipes/cpp-taskflow/all/test_package/test_package.cpp
@@ -9,16 +9,16 @@ int main()
     auto B = taskflow.emplace([](){ std::cout << "Task B\n"; });
     auto C = taskflow.emplace([](){ std::cout << "Task C\n"; });
     auto D = taskflow.emplace([](){ std::cout << "Task D\n"; });
-                                            
-                                               //                                 
-                                               //          +---+                  
-                                               //    +---->| B |-----+            
-                                               //    |     +---+     |            
-                                               //  +---+           +-v-+          
-                                               //  | A |           | D |          
-    A.precede(B);    // B runs after A         //  +---+           +-^-+          
-    A.precede(C);    // C runs after A         //    |     +---+     |            
-    B.precede(D);    // D runs after B         //    +---->| C |-----+            
+
+                                               //
+                                               //          +---+
+                                               //    +---->| B |-----+
+                                               //    |     +---+     |
+                                               //  +---+           +-v-+
+                                               //  | A |           | D |
+    A.precede(B);    // B runs after A         //  +---+           +-^-+
+    A.precede(C);    // C runs after A         //    |     +---+     |
+    B.precede(D);    // D runs after B         //    +---->| C |-----+
     C.precede(D);    // D runs after C         //          +---+
 
     executor.run(taskflow).get();
