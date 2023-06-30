e143562da744aee55b9dde0f868a947f31e8fd1c
Martin Valgur <martin.valgur@gmail.com>
2023-06-30 14:05:00 +0300

    diligentgraphics-vulkan-headers: migrate to Conan v2

--- recipes/diligentgraphics-vulkan-headers/all/conanfile.py
+++ recipes/diligentgraphics-vulkan-headers/all/conanfile.py
@@ -1,49 +1,68 @@
-from conans import ConanFile, tools
-import glob
 import os
 
-required_conan_version = ">=1.33"
+from conan import ConanFile
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+
+required_conan_version = ">=1.52.0"
 
 
 class VulkanHeadersConan(ConanFile):
     name = "diligentgraphics-vulkan-headers"
     description = "Diligent fork of Vulkan Header files."
     license = "Apache-2.0"
-    topics = ("vulkan-headers", "vulkan")
-    homepage = "https://github.com/DiligentGraphics/Vulkan-Headers"
     url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/DiligentGraphics/Vulkan-Headers"
+    topics = ("vulkan-headers", "vulkan", "header-only")
+
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
     provides = "vulkan-headers"
     deprecated = "vulkan-headers"
-    no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
-        self.copy("*", dst="include", src=os.path.join(self.source_folder, self._source_subfolder, "include"))
-        self.copy("*", dst=os.path.join("res", "vulkan", "registry"),
-                       src=os.path.join(self.source_folder, self._source_subfolder, "registry"))
+        copy(self, "LICENSE.txt",
+             dst=os.path.join(self.package_folder, "licenses"),
+             src=self.source_folder)
+        copy(self, "*",
+             dst=os.path.join(self.package_folder, "include"),
+             src=os.path.join(self.source_folder, "include"))
+        copy(self, "*",
+             dst=os.path.join("res", "vulkan", "registry"),
+             src=os.path.join(self.source_folder, "registry"))
 
     def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
+        self.cpp_info.set_property("cmake_file_name", "VulkanHeaders")
+        self.cpp_info.set_property("cmake_target_name", "Vulkan::Vulkan")
+
+        self.cpp_info.components["vulkanheaders"].set_property("cmake_target_name", "Vulkan::Headers")
+        self.cpp_info.components["vulkanheaders"].bindirs = []
+        self.cpp_info.components["vulkanheaders"].libdirs = []
+        self.cpp_info.components["vulkanregistry"].set_property("cmake_target_name", "Vulkan::Registry")
+        self.cpp_info.components["vulkanregistry"].includedirs = [os.path.join("res", "vulkan", "registry")]
+        self.cpp_info.components["vulkanregistry"].bindirs = []
+        self.cpp_info.components["vulkanregistry"].libdirs = []
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "VulkanHeaders"
         self.cpp_info.filenames["cmake_find_package_multi"] = "VulkanHeaders"
         self.cpp_info.names["cmake_find_package"] = "Vulkan"
         self.cpp_info.names["cmake_find_package_multi"] = "Vulkan"
+
         self.cpp_info.components["vulkanheaders"].names["cmake_find_package"] = "Headers"
         self.cpp_info.components["vulkanheaders"].names["cmake_find_package_multi"] = "Headers"
-        self.cpp_info.components["vulkanheaders"].bindirs = []
-        self.cpp_info.components["vulkanheaders"].libdirs = []
         self.cpp_info.components["vulkanregistry"].names["cmake_find_package"] = "Registry"
         self.cpp_info.components["vulkanregistry"].names["cmake_find_package_multi"] = "Registry"
-        self.cpp_info.components["vulkanregistry"].includedirs = [os.path.join("res", "vulkan", "registry")]
-        self.cpp_info.components["vulkanregistry"].bindirs = []
-        self.cpp_info.components["vulkanregistry"].libdirs = []
