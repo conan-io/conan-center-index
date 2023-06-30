dce2e1d5e420b3a70394dca231549726ac19c771
Martin Valgur <martin.valgur@gmail.com>
2023-06-30 08:53:08 +0300

    maddy: migrate to Conan v2

--- recipes/maddy/all/conanfile.py
+++ recipes/maddy/all/conanfile.py
@@ -1,47 +1,42 @@
-from conans import ConanFile, tools
 import os
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+
+required_conan_version = ">=1.52.0"
 
 
 class MaddyConan(ConanFile):
     name = "maddy"
+    description = "open-source, maddy is a C++ Markdown to HTML header-only parser library."
+    license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/progsource/maddy"
-    description = (
-        "open-source, maddy is a C++ Markdown to HTML header-only parser library."
-    )
-    topics = ("maddy", "markdown", "header-only")
-    license = "MIT"
+    topics = ("markdown", "header-only")
+
+    package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 14)
+            check_min_cppstd(self, 14)
 
     def source(self):
-        tools.get(
-            **self.conan_data["sources"][self.version],
-            destination=self._source_subfolder,
-            strip_root=True
-        )
-
-    def package_id(self):
-        self.info.header_only()
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy(
-            "LICENSE",
-            src=os.path.join(self.source_folder, self._source_subfolder),
-            dst="licenses",
-        )
-        self.copy(
-            pattern="maddy/*.h",
-            src=os.path.join(self.source_folder, self._source_subfolder, "include"),
-            dst="include",
-        )
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, pattern="maddy/*.h", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
