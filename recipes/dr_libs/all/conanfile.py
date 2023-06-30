8e71956b46ae6429abceb3adbd3aa419a1c8be50
czoido <mrgalleta@gmail.com>
2023-06-30 10:03:43 +0200

    fix imports

--- recipes/dr_libs/all/conanfile.py
+++ recipes/dr_libs/all/conanfile.py
@@ -2,6 +2,10 @@ from conan import ConanFile
 from conan.tools.files import get, copy
 from conan.tools.build import check_min_cppstd
 from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, check_min_vs
+from conan.tools.scm import Version
+from conan.errors import ConanInvalidConfiguration
+
 import os
 
 required_conan_version = ">=1.52.0"
@@ -35,7 +39,8 @@ class DrLibsConan(ConanFile):
             check_min_cppstd(self, self._min_cppstd)
         check_min_vs(self, 180)
         if not is_msvc(self):
-            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+            minimum_version = self._compilers_minimum_version.get(
+                str(self.settings.compiler), False)
             if minimum_version and Version(self.settings.compiler.version) < minimum_version:
                 raise ConanInvalidConfiguration(
                     f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
@@ -51,7 +56,8 @@ class DrLibsConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, pattern="LICENSE", dst=os.path.join(
+            self.package_folder, "licenses"), src=self.source_folder)
         copy(
             self,
             pattern="*.h",
