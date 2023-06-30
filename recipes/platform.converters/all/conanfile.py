11be7fbbfc3224d70cad2c0b68a69722f77a45cf
Martin Valgur <martin.valgur@gmail.com>
2023-06-30 08:55:36 +0300

    platform.converters: migrate to Conan v2

--- recipes/platform.converters/all/conanfile.py
+++ recipes/platform.converters/all/conanfile.py
@@ -1,28 +1,30 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.52.0"
 
 
 class PlatformConvertersConan(ConanFile):
     name = "platform.converters"
+    description = "platform.converters is one of the libraries of the LinksPlatform modular framework, to provide conversions between different types"
     license = "MIT"
-    homepage = "https://github.com/linksplatform/Converters"
     url = "https://github.com/conan-io/conan-center-index"
-    description = "platform.converters is one of the libraries of the LinksPlatform modular framework, " \
-                  "to provide conversions between different types"
-    topics = ("linksplatform", "cpp20", "converters", "any", "native")
-    settings = "compiler", "arch"
+    homepage = "https://github.com/linksplatform/Converters"
+    topics = ("linksplatform", "cpp20", "converters", "any", "native", "header-only")
+
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _internal_cpp_subfolder(self):
-        return os.path.join(self._source_subfolder, "cpp", "Platform.Converters")
+    def _minimum_cpp_standard(self):
+        return 20
 
     @property
     def _compilers_minimum_version(self):
@@ -30,36 +32,41 @@ class PlatformConvertersConan(ConanFile):
             "gcc": "10",
             "Visual Studio": "16",
             "clang": "14",
-            "apple-clang": "14"
+            "apple-clang": "14",
         }
 
-    @property
-    def _minimum_cpp_standard(self):
-        return 20
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
 
     def validate(self):
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler))
 
         if not minimum_version:
-            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
-                self.name, self.settings.compiler))
+            self.output.warning(f"{self.name} recipe lacks information about the {self.settings.compiler} compiler support.")
 
-        elif tools.Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration("{}/{} requires c++{}, "
-                                            "which is not supported by {} {}.".format(
-                self.name, self.version, self._minimum_cpp_standard, self.settings.compiler,
-                self.settings.compiler.version))
+        elif Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.name}/{self.version} requires c++{self._minimum_cpp_standard}, "
+                f"which is not supported by {self.settings.compiler} {self.settings.compiler.version}."
+            )
 
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
-
-    def package_id(self):
-        self.info.header_only()
+            check_min_cppstd(self, self._minimum_cpp_standard)
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    @property
+    def _internal_cpp_subfolder(self):
+        return os.path.join(self.source_folder, "cpp", "Platform.Converters")
 
     def package(self):
-        self.copy("*.h", dst="include", src=self._internal_cpp_subfolder)
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        copy(self, "*.h", dst=os.path.join(self.package_folder, "include"), src=self._internal_cpp_subfolder)
+        copy(self, "LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
