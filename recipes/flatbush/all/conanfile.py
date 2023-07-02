3aec652f5ce3a044ff47d2fc38eeea0f56b455f4
Alex E <chusitoo@gmail.com>
2023-07-01 20:50:53 -0400

    Migrate to Conan v2

--- recipes/flatbush/all/conanfile.py
+++ recipes/flatbush/all/conanfile.py
@@ -1,25 +1,46 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd, valid_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+import os
+
+required_conan_version = ">=1.50.0"
 
 class FlatbushConan(ConanFile):
     name = "flatbush"
-    license = "MIT"
-    homepage = "https://github.com/chusitoo/flatbush"
     description = "Flatbush for C++"
+    homepage = "https://github.com/chusitoo/flatbush"
+    license = "MIT"
     topics = ("header-only", "flatbush", "r-tree", "hilbert", "zero-copy", "spatial-index")
     url = "https://github.com/conan-io/conan-center-index"
-    settings = "os", "compiler", "build_type", "arch"
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 11)
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
     def source(self):
-       tools.get(**self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        pass
+
+    def build(self):
+        pass
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses")
-        self.copy(pattern="flatbush.h", dst="include")
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "flatbush.h", src=self.source_folder, dst=os.path.join(self.package_folder, "include"))
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def package_info(self):
-        if not tools.valid_min_cppstd(self, "20"):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        if not valid_min_cppstd(self, "20"):
             self.cpp_info.defines = ["FLATBUSH_SPAN"]
