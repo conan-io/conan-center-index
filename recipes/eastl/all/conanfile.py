a3b7c379ff0475f179d85c3ff3f1b927aab5eaf9
toge <toge.mail@gmail.com>
2023-07-03 11:49:35 +0900

    eastl: add version 3.21.12, add patch descriptions

--- recipes/eastl/all/conanfile.py
+++ recipes/eastl/all/conanfile.py
@@ -79,12 +79,7 @@ class EastlConan(ConanFile):
             )
 
     def source(self):
-        get(
-            self,
-            **self.conan_data["sources"][self.version],
-            destination=self.source_folder,
-            strip_root=True,
-        )
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
