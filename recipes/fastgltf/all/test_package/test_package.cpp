bf21a590f04767793c5e456158f9024ccc252d2f
toge <toge.mail@gmail.com>
2023-07-04 00:42:52 +0900

    fastgltf: add version 0.5.0, update simdjson, small improvements

--- recipes/fastgltf/all/test_package/test_package.cpp
+++ recipes/fastgltf/all/test_package/test_package.cpp
@@ -1,9 +1,13 @@
 #include <cstdio>
-#include <fastgltf_parser.hpp>
+#ifdef FASTGLTF_0_5_0_LATER
+#  include <fastgltf/parser.hpp>
+#else
+#  include <fastgltf_parser.hpp>
+#endif
 
 int main() {
     fastgltf::Parser parser;
-    printf("Version: " FASTGLTF_QUOTE(FASTGLTF_VERSION));
-    printf("C++17: " FASTGLTF_QUOTE(FASTGLTF_CPP_17));
-    printf("C++20: " FASTGLTF_QUOTE(FASTGLTF_CPP_20));
+    printf("Version: " FASTGLTF_QUOTE(FASTGLTF_VERSION) "\n");
+    printf("C++17: " FASTGLTF_QUOTE(FASTGLTF_CPP_17) "\n");
+    printf("C++20: " FASTGLTF_QUOTE(FASTGLTF_CPP_20) "\n");
 }
