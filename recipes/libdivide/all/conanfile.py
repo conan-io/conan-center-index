c550d3549cee3549e79f99470b6fc8b6cd64b67c
Rubén Rincón Blanco <git@rinconblanco.es>
2023-07-06 01:21:27 +0200

    Update recipes/libdivide/all/conanfile.py

--- recipes/libdivide/all/conanfile.py
+++ recipes/libdivide/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class LibdivideConan(ConanFile):
