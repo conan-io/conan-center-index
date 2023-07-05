cbef5f3a122866c5ed2d6cb3d6c9825094ed71dd
Julien Marrec <julien.marrec@gmail.com>
2023-07-05 15:24:14 +0200

    Ruby: support conan v2 and expose more configuration options for native extension gems. Reboots #12456 and #12208
    * conan v2 support (work done by myself + a merge of @SpaceIm 's branch from PR #12208 to reconcile both)
    
    * Extend testing to require one of the native extensions
    
    * ```
    post_package(): WARN: [MISSING SYSTEM LIBS (KB-H043)] Library './lib/ruby/3.1.0/x86_64-linux/pty.so' links to system library 'util' but it is not in cpp_info.system_libs.
    ```
    
    * Use `--with-opt-dir` instead of `--with-xx-dir` that ruby isn't respecting.
    
    conanio/gccXX (eg 10) removed the libxxx-dev (eg libgmp-dev) from the image. This made me realize that the conan deps weren't being picked up by the build. The openssl was, because --with-openssl-dir is explicitly used in ruby config.
    
    So here, we rely on --with-opt-dir.
    
    cf: https://bugs.ruby-lang.org/issues/19014#change-99241 (--with-gmp-dir was added on September 14, 2022, wrote this in Sep 22, 2022)
    
    * Pass both --with-opt-dir and --with-xxx-dir
    
    https://gist.github.com/mrkn/6647630

--- recipes/ruby/all/test_package/conanfile.py
+++ recipes/ruby/all/test_package/conanfile.py
@@ -1,21 +1,38 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake
-from conans import tools
+from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, cmake_layout
 import os
 
+
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure()
+        # when --static-linked-ext is used, ruby defines EXTSTATIC as 1
+        # But when ruby itself is static there's nothing, so:
+        # We define RUBY_STATIC_RUBY when ruby itself is static
+        # We define RUBY_STATIC_LINKED_EXT when the ruby extensions are static (same as EXTSTATIC but clearer)
+        defs = {}
+        if not self.options['ruby'].shared:
+            defs['RUBY_STATIC_RUBY'] = 1
+            if self.options['ruby'].with_static_linked_ext:
+                defs['RUBY_STATIC_LINKED_EXT'] = 1
+        cmake.configure(variables=defs)
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not cross_building(self):
             # test executable
-            self.run("ruby --version", run_environment=True)
+            self.run("ruby --version", env="conanrun")
 
             # test library
-            self.run(os.path.join("bin", "test_package"), run_environment=True)
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "bin", "test_package")
+            self.run(bin_path, env="conanrun")
