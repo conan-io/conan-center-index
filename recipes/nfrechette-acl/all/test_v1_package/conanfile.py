3d5d0d4441eb6e36485aa76c2f1c0e7c904b644c
Martin Valgur <martin.valgur@gmail.com>
2023-07-05 09:16:58 +0200

    nfrechette-acl: add cmake_find_package_multi generator to test_v1_package

--- recipes/nfrechette-acl/all/test_v1_package/conanfile.py
+++ recipes/nfrechette-acl/all/test_v1_package/conanfile.py
@@ -5,7 +5,7 @@ from conans import CMake, tools
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
