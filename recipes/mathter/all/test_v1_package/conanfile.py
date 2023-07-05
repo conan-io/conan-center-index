38946bedbc3c1585202804213a69b39d8a687eb9
Martin Valgur <martin.valgur@gmail.com>
2023-07-05 09:16:57 +0200

    mathter: add cmake_find_package_multi generator to test_v1_package

--- recipes/mathter/all/test_v1_package/conanfile.py
+++ recipes/mathter/all/test_v1_package/conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class MathterTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
