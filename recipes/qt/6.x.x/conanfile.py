4704e45ba22ebd0c6946ca545c905a565c0895c1
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-07-06 09:19:05 +0200

    Use self.info.settings.os in source() method
    stolen from @EstebanDugueperoux2

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -595,7 +595,7 @@ class QtConan(ConanFile):
 
     def source(self):
         destination = self.source_folder
-        if os.name == 'nt':
+        if self.info.settings.os == "Windows":
             # Don't use os.path.join, or it removes the \\?\ prefix, which enables long paths
             destination = rf"\\?\{self.source_folder}"
         get(self, **self.conan_data["sources"][self.version],
