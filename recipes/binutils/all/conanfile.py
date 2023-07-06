6409eac1c3366afafc302004ceb138a2eda40fe8
Gregor Jasny <gjasny@googlemail.com>
2023-07-06 09:37:41 +0200

    binutils: set GPROFNG_SYSCONFDIR
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/binutils/all/conanfile.py
+++ recipes/binutils/all/conanfile.py
@@ -205,7 +205,8 @@ class BinutilsConan(ConanFile):
         # Add recipe path to enable running the self test in the test package.
         # Don't use this property in production code. It's unsupported.
         self.user_info.recipe_path = os.path.realpath(__file__)
-
+        self.cpp_info.resdirs = ["etc"]
+        self.cpp_info.buildenv_info.define("GPROFNG_SYSCONFDIR", os.path.join(self.package_folder, "etc"))
         if self.settings.os in ("FreeBSD", "Linux"):
             self.cpp_info.system_libs = ["dl", "rt"]
 
