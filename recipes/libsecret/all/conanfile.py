452947b526cef598ebd9624d13889bd3d773cc15
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-07-07 18:37:38 +0000

    libsecret/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libsecret/all/conanfile.py
+++ recipes/libsecret/all/conanfile.py
@@ -66,7 +66,7 @@ class LibsecretConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("meson/1.1.1")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/1.9.3")
+            self.tool_requires("pkgconf/1.9.5")
         self.tool_requires("glib/<host_version>")
 
         if self.settings.os == "Macos":
