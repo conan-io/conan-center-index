049c49a1c4c09b62aa0f6397d05a78155cb27207
Martin Valgur <martin.valgur@gmail.com>
2023-07-08 11:19:48 +0200

    Update recipes/influxdb-cpp/all/test_v1_package/conanfile.py

--- recipes/influxdb-cpp/all/test_v1_package/conanfile.py
+++ recipes/influxdb-cpp/all/test_v1_package/conanfile.py
@@ -14,5 +14,5 @@ class InfluxDBCppTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "example")
+            bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
