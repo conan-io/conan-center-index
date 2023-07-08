88e23f65521724d61615ac8ffb74a1f0e2c512db
Martin Valgur <martin.valgur@gmail.com>
2023-07-08 09:16:13 +0200

    Update recipes/alac/all/conanfile.py

--- recipes/alac/all/conanfile.py
+++ recipes/alac/all/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
 import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class AlacConan(ConanFile):
