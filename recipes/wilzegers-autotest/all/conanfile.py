2543e7b263ed66b3c23bf76cdf99df9075ff6bb7
Martin Valgur <martin.valgur@gmail.com>
2023-07-08 08:57:57 +0200

    add min msvc version for C++17

--- recipes/wilzegers-autotest/all/conanfile.py
+++ recipes/wilzegers-autotest/all/conanfile.py
@@ -30,6 +30,7 @@ class WilzegersAutotestConan(ConanFile):
     def _compilers_minimum_version(self):
         return {
             "Visual Studio": "15",
+            "msvc": "191",
             "gcc": "7",
             "clang": "5.0",
             "apple-clang": "9.1",
