5fff340e0fc896076ff0d1c3e0ef3af7cae89ada
Stephen Webb <swebb2066@gmail.com>
2023-07-14 17:44:25 +1000

    Prevent 'Version conflict: apr-util/1.6.1->apr/1.7.0, None->apr/1.7.4'

--- recipes/log4cxx/all/conanfile.py
+++ recipes/log4cxx/all/conanfile.py
@@ -60,7 +60,7 @@ class Log4cxxConan(ConanFile):
             self.options.rm_safe("fPIC")
 
     def requirements(self):
-        self.requires("apr/[>=1.6]")
+        self.requires("apr/1.7.0")
         self.requires("apr-util/[>=1.6]")
         if self.options.get_safe("with_odbc_appender") and self.settings.os != "Windows":
             self.requires("odbc/[>=2.3]")
