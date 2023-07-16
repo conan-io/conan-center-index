45d0296fb33ffe90c2edf47e69b0867dd19e94fc
Martin Delille <martin@delille.org>
2023-07-16 18:28:20 +0200

    Set CMAKE_CXX_STANDARD to minimum CPP standard if it is not already set

--- recipes/qtawesome/all/conanfile.py
+++ recipes/qtawesome/all/conanfile.py
@@ -79,7 +79,8 @@ class QtAwesomeConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["CMAKE_CXX_STANDARD"] = self._min_cppstd
+        if not self.settings.compiler.cppstd:
+            tc.variables["CMAKE_CXX_STANDARD"] = self._min_cppstd
         if is_msvc(self):
             tc.variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
