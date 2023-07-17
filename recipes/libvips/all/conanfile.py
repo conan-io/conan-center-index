bbd7f61235b688bccc46fa7fbb6746e0d0c5e1af
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-07-17 06:39:46 +0000

    libvips/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libvips/all/conanfile.py
+++ recipes/libvips/all/conanfile.py
@@ -200,7 +200,7 @@ class LibvipsConan(ConanFile):
             raise ConanInvalidConfiguration("librsvg recipe not available in conancenter yet")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.1.1")
+        self.tool_requires("meson/1.2.0")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/1.9.5")
         if self.options.introspection:
