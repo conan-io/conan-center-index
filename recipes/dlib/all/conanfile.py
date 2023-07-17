a03d2b2c026253db5f1d110cf2b4db9ff3531129
Uilian Ries <uilianries@gmail.com>
2023-07-17 13:25:32 +0200

    Update recipes/dlib/all/conanfile.py

--- recipes/dlib/all/conanfile.py
+++ recipes/dlib/all/conanfile.py
@@ -2,7 +2,9 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import get, replace_in_file, copy, rmdir
 from conan.tools.build import check_min_cppstd
+from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
+from conan.errors import ConanInvalidConfiguration
 import os
 
 
