7bc47c3769e4a51e6cfdfa3a7e07e70d1db89712
toge <toge.mail@gmail.com>
2023-07-18 01:13:23 +0900

    simplify test_package.cpp

--- recipes/wasm-micro-runtime/all/test_package/test_package.cpp
+++ recipes/wasm-micro-runtime/all/test_package/test_package.cpp
@@ -7,15 +7,6 @@
 
 #define own
 
-// A function to be called from Wasm code.
-own wasm_trap_t* hello_callback(
-  const wasm_val_vec_t* args, wasm_val_vec_t* results
-) {
-  printf("Calling back...\n");
-  printf("> Hello World!\n");
-  return NULL;
-}
-
 int main(int argc, const char* argv[]) {
   // Initialize.
   printf("Initializing...\n");
@@ -70,64 +61,5 @@ int main(int argc, const char* argv[]) {
 
   wasm_byte_vec_delete(&binary);
 
-  // Create external print functions.
-  printf("Creating callback...\n");
-  own wasm_functype_t* hello_type = wasm_functype_new_0_0();
-  own wasm_func_t* hello_func =
-    wasm_func_new(store, hello_type, hello_callback);
-
-  wasm_functype_delete(hello_type);
-
-  // Instantiate.
-  printf("Instantiating module...\n");
-  wasm_extern_t* externs[] = { wasm_func_as_extern(hello_func) };
-  wasm_extern_vec_t imports = WASM_ARRAY_VEC(externs);
-  own wasm_instance_t* instance =
-    wasm_instance_new(store, module, &imports, NULL);
-  if (!instance) {
-    printf("> Error instantiating module!\n");
-    return 1;
-  }
-
-  wasm_func_delete(hello_func);
-
-  // Extract export.
-  printf("Extracting export...\n");
-  own wasm_extern_vec_t exports;
-  wasm_instance_exports(instance, &exports);
-  if (exports.size == 0) {
-    printf("> Error accessing exports!\n");
-    return 1;
-  }
-
-  const wasm_func_t* run_func = wasm_extern_as_func(exports.data[0]);
-  if (run_func == NULL) {
-    printf("> Error accessing export!\n");
-    return 1;
-  }
-
-  wasm_module_delete(module);
-  wasm_instance_delete(instance);
-
-  // Call.
-  printf("Calling export...\n");
-  wasm_val_vec_t args = WASM_EMPTY_VEC;
-  wasm_val_vec_t results = WASM_EMPTY_VEC;
-  wasm_trap_t *trap = wasm_func_call(run_func, &args, &results);
-  if (trap) {
-      printf("> Error calling function!\n");
-      wasm_trap_delete(trap);
-      return 1;
-  }
-
-  wasm_extern_vec_delete(&exports);
-
-  // Shut down.
-  printf("Shutting down...\n");
-  wasm_store_delete(store);
-  wasm_engine_delete(engine);
-
-  // All done.
-  printf("Done.\n");
   return 0;
 }
