b6f9363ac78a606ffd612175286b294c979c5a72
Rubén Rincón Blanco <git@rinconblanco.es>
2023-07-17 05:55:10 +0200

    Dont import every tool

--- recipes/tree-sitter/all/conanfile.py
+++ recipes/tree-sitter/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake
 from conan.tools.layout import basic_layout
-import conan.tools.files as tools
+from conan.toools.files import get, copy
 import os
 
 required_conan_version = ">=1.53.0"
@@ -14,9 +14,7 @@ class TreeSitterConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://tree-sitter.github.io/tree-sitter"
     license = "MIT"
-    
     package_type = "library"
-    
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "fPIC": [True, False],
@@ -44,7 +42,7 @@ class TreeSitterConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
 
     def build(self):
@@ -53,7 +51,7 @@ class TreeSitterConan(ConanFile):
         cmake.build()
 
     def package(self):
-        tools.copy(
+        copy(
             self,
             "LICENSE",
             src=self.source_folder,
