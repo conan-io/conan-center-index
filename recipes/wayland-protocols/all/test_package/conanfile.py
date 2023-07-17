e7aee8425432fdc152096d2ce565623dda9b6a38
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-07-17 06:48:53 +0000

    wayland-protocols/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/wayland-protocols/all/test_package/conanfile.py
+++ recipes/wayland-protocols/all/test_package/conanfile.py
@@ -20,7 +20,7 @@ class TestPackageConan(ConanFile):
         self.requires("wayland/1.22.0")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.1.1")
+        self.tool_requires("meson/1.2.0")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/1.9.5")
         self.tool_requires("wayland/1.22.0")
