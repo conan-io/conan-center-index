cb1967d695faa1f22e7b4a2488cd33c7d70814ce
Martin Valgur <martin.valgur@gmail.com>
2023-07-18 21:59:08 +0300

    giflib/5.1.x: migrate to Conan v2

--- recipes/giflib/5.1.x/test_v1_package/conanfile.py
+++ recipes/giflib/5.1.x/test_v1_package/conanfile.py
@@ -1,16 +1,10 @@
-from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
-    test_type = "explicit"
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
+    generators = "cmake", "cmake_find_package_multi"
 
     def build_requirements(self):
         if self.settings.os == "Macos" and self.settings.arch == "armv8":
@@ -19,10 +13,7 @@ class TestPackageConan(ConanFile):
             # set. This could be because you are using a Mac OS X version less than 10.5
             # or because CMake's platform configuration is corrupt.
             # FIXME: Remove once CMake on macOS/M1 CI runners is upgraded.
-            self.tool_requires("cmake/3.22.0")
-
-    def layout(self):
-        cmake_layout(self)
+            self.build_requires("cmake/3.22.0")
 
     def build(self):
         cmake = CMake(self)
@@ -30,7 +21,8 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if can_run(self):
+        if not tools.cross_building(self):
             img_name = os.path.join(self.source_folder, "testimg.gif")
-            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
-            self.run(f"{bin_path} {img_name}", env="conanrun")
+            bin_path = os.path.join("bin", "test_package")
+            command = "{} {}".format(bin_path, img_name)
+            self.run(command, run_environment=True)
