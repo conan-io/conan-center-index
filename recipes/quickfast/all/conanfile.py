db33de66c2ea8072b2eae41cdb792a74de7c921f
Martin Valgur <martin.valgur@gmail.com>
2023-07-18 15:35:35 +0300

    quickfast: migrate to Conan v2

--- recipes/quickfast/all/conanfile.py
+++ recipes/quickfast/all/conanfile.py
@@ -1,69 +1,75 @@
-from conans import ConanFile, CMake, tools
 import os
-import shutil
-import glob
+
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, collect_libs, copy, export_conandata_patches, get
+
+required_conan_version = ">=1.53.0"
 
 
 class QuickfastConan(ConanFile):
     name = "quickfast"
+    description = "QuickFAST is an Open Source native C++ implementation of the FAST Protocol"
     license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://objectcomputing.com/"
-    description = "QuickFAST is an Open Source native C++ implementation of the FAST Protocol"
-    topics = ("conan", "QuickFAST", "FAST", "FIX", "Fix Adapted for STreaming", "Financial Information Exchange",
-              "libraries", "cpp")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"fPIC": [True, False],
-               "shared": [True, False]}
-    default_options = {"fPIC": True,
-                       "shared": False}
-    requires = ["boost/1.75.0", "xerces-c/3.2.3"]
-    generators = "cmake"
-    exports_sources = "CMakeLists.txt", "patches/**"
-    _cmake = None
+    topics = ("conan", "QuickFAST", "FAST", "FIX", "Fix Adapted for STreaming", "Financial Information Exchange", "libraries", "cpp")
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
 
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
-    def _configure_cmake(self):
-        if not self._cmake:
-            self._cmake = CMake(self)
-            self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = glob.glob("quickfast-*")[0]
-        os.rename(extracted_dir, self._source_subfolder)
+    def export_sources(self):
+        copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def configure(self):
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("boost/1.83.0")
+        self.requires("xerces-c/3.2.4")
+
+    def validate(self):
         if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, "11")
+            check_min_cppstd(self, 11)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
-        patches = self.conan_data["patches"][self.version]
-        for patch in patches:
-            tools.patch(**patch)
-
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=self.export_sources_folder)
         cmake.build(target="quickfast")
 
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
-        self.copy("license.txt", dst="licenses", src=self._source_subfolder)
+        copy(self, "license.txt",
+             dst=os.path.join(self.package_folder, "licenses"),
+             src=self.source_folder)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
         self.cpp_info.includedirs.append(os.path.join("include", "quickfast"))
 
         if not self.options.shared:
