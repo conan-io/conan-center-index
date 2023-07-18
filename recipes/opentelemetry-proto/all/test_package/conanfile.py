5d1974d052c6b28937024da158fad40ba4630d2b
Uilian Ries <uilianries@gmail.com>
2023-07-18 08:54:35 +0200

    Update recipes/opentelemetry-proto/all/test_package/conanfile.py

--- recipes/opentelemetry-proto/all/test_package/conanfile.py
+++ recipes/opentelemetry-proto/all/test_package/conanfile.py
@@ -19,6 +19,6 @@ class TestPackageConan(ConanFile):
         save(self, os.path.join(self.build_folder, "proto_root"), self.dependencies["opentelemetry-proto"].conf_info.get("user.opentelemetry-proto:proto_root"))
 
     def test(self):
-        res_folder = load(self, "proto_root")
+        res_folder = load(self, os.path.join(self.build_folder, "proto_root"))
         proto_path = os.path.join(res_folder, "opentelemetry", "proto", "common", "v1", "common.proto")
         assert os.path.isfile(proto_path)
