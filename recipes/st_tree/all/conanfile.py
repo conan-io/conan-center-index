d33ef2106b0d96e297e52aa661e7a632dda90ddc
Uilian Ries <uilianries@gmail.com>
2023-07-18 09:25:28 +0200

    Simplify st_tree Conan recipe
    The upstream cmake is very simple, only copies headers files to include
    folder. Plus, it has testing and install steps, but we can simplify a
    lot by copying directly to the include folder without CMake steps.
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/st_tree/all/conanfile.py
+++ recipes/st_tree/all/conanfile.py
@@ -1,8 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import get, copy, rmdir
-from conan.tools.layout import basic_layout
-from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.files import get, copy
 import os
 
 
@@ -19,39 +17,19 @@ class STTreeConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    @property
-    def _min_cppstd(self):
-        return 11
-
-    def layout(self):
-        basic_layout(self, src_folder="src")
-
     def package_id(self):
         self.info.clear()
 
     def validate(self):
         if self.settings.compiler.cppstd:
-            check_min_cppstd(self, self._min_cppstd)
+            check_min_cppstd(self, 11)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.variables["ENABLE_TESTS"] = False
-        tc.generate()
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-
     def package(self):
         copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
-        cmake = CMake(self)
-        cmake.install()
-
-        rmdir(self, os.path.join(self.package_folder, "lib"))
+        copy(self, pattern="*.h", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "include"))
 
     def package_info(self):
         self.cpp_info.bindirs = []
