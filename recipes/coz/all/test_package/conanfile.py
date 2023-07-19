4189e81f9f61fa20292b74e3ad8957edde363667
Martin Valgur <martin.valgur@gmail.com>
2023-07-19 23:22:28 +0300

    coz: migrate to Conan v2

--- recipes/coz/all/test_package/conanfile.py
+++ recipes/coz/all/test_package/conanfile.py
@@ -1,19 +1,31 @@
-from conans import ConanFile, CMake, tools
 import os
+
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def build(self):
-        cmake = CMake(self, build_type="RelWithDebInfo") # To work properly Coz tool requires debug information https://github.com/plasma-umass/coz
+        # FIXME: To work properly Coz tool requires debug information https://github.com/plasma-umass/coz
+        # cmake = CMake(self, build_type="RelWithDebInfo")
+        cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if tools.cross_building(self.settings):
-            return
-        bin_path = os.path.join("bin", "test_package")
-        self.run("coz run --- " + bin_path, run_environment=True)
-        print(open("profile.coz").read())
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
+            self.run("coz run --- " + bin_path, env="conanrun")
+            print(open("profile.coz").read())
