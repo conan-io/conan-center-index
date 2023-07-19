17466b3bf74cfa94b4e1169eb5fdfeb38abd102a
Martin Valgur <martin.valgur@gmail.com>
2023-07-19 21:26:05 +0300

    giflib/5.1.x: add automake

--- recipes/giflib/5.1.x/conanfile.py
+++ recipes/giflib/5.1.x/conanfile.py
@@ -63,6 +63,8 @@ class GiflibConan(ConanFile):
             self.win_bash = True
             if not self.conf.get("tools.microsoft.bash:path", check_type=str):
                 self.tool_requires("msys2/cci.latest")
+            if is_msvc(self):
+                self.tool_requires("automake/1.16.5")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
