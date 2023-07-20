b496bfa97ce73d99e9e2e69d441e89eff2511642
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 12:59:55 +0300

    nodesoup: migrate to Conan v2

--- recipes/nodesoup/all/conanfile.py
+++ recipes/nodesoup/all/conanfile.py
@@ -1,17 +1,24 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.53.0"
 
 
 class NodesoupConan(ConanFile):
     name = "nodesoup"
     description = "Force-directed graph layout with Fruchterman-Reingold"
-    license = "MIT",
-    topics = ("graph", "visualization", "layout", "kamada", "kawai", "fruchterman", "reingold")
-    homepage = "https://github.com/olvb/nodesoup"
+    license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/olvb/nodesoup"
+    topics = ("graph", "visualization", "layout", "kamada", "kawai", "fruchterman", "reingold")
+
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -22,18 +29,8 @@ class NodesoupConan(ConanFile):
         "fPIC": True,
     }
 
-    generators = "cmake"
-    exports_sources = "CMakeLists.txt", "patches/*"
-
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -41,41 +38,45 @@ class NodesoupConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def validate(self):
         if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, 14)
+            check_min_cppstd(self, 14)
         if self.settings.compiler == "clang":
-            if tools.Version(self.settings.compiler.version) < "5.0" and self.settings.compiler.libcxx in ("libstdc++", "libstdc++11"):
-                raise ConanInvalidConfiguration("The version of libstdc++(11) of the current compiler does not support building nodesoup")
+            if (Version(self.settings.compiler.version) < "5.0" and
+                    self.settings.compiler.libcxx in ("libstdc++", "libstdc++11")):
+                raise ConanInvalidConfiguration(
+                    "The version of libstdc++(11) of the current compiler does not support building nodesoup"
+                )
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = self.options.shared
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE",
+             dst=os.path.join(self.package_folder, "licenses"),
+             src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
-
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.includedirs.append(os.path.join("include", "nodesoup"))
         self.cpp_info.libs = ["nodesoup"]
-        self.cpp_info.names["cmake_find_package"] = "nodesoup"
-        self.cpp_info.names["cmake_find_package_multi"] = "nodesoup"
