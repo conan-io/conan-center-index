4b4f7e74148ffe950c491ae476265f3eea14ce8d
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 23:17:04 +0300

    net-snmp: migrate to Conan v2

--- recipes/net-snmp/all/test_v1_package/conanfile.py
+++ recipes/net-snmp/all/test_v1_package/conanfile.py
@@ -1,16 +1,11 @@
-from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
 import os
+
+from conans import ConanFile, CMake, tools
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
-    test_type = "explicit"
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
+    generators = "cmake", "cmake_find_package_multi"
 
     def build_requirements(self):
         if self.settings.os == "Macos" and self.settings.arch == "armv8":
@@ -18,10 +13,7 @@ class TestPackageConan(ConanFile):
             # CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG being set. This could be
             # because you are using a Mac OS X version less than 10.5 or
             # because CMake's platform configuration is corrupt.
-            self.tool_requires("cmake/[>=3.20]")
-
-    def layout(self):
-        cmake_layout(self)
+            self.build_requires("cmake/3.20.1")
 
     def build(self):
         cmake = CMake(self)
@@ -29,6 +21,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
-            self.run(bin_path, env="conanrun")
+        if not tools.cross_building(self):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
