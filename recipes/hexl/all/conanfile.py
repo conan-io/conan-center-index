1380fc4aa2740aef201be85e6968877282054d83
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 23:15:56 +0300

    hexl: migrate to Conan v2

--- recipes/hexl/all/conanfile.py
+++ recipes/hexl/all/conanfile.py
@@ -1,82 +1,86 @@
-from conans import CMake, tools
-from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.microsoft import is_msvc
-from conan.tools.files import get, rmdir
-from conan.tools.scm import Version
-from conan.tools.build import cross_building
-
 import os
 
-required_conan_version = ">=1.43.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches
+from conan.tools.files import get, rmdir
+from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.53.0"
+
 
 class HexlConan(ConanFile):
     name = "hexl"
-    license = "Apache-2.0"
-    homepage = "https://github.com/intel/hexl"
-    url = "https://github.com/conan-io/conan-center-index"
     description = "Intel Homomorphic Encryption (HE) Acceleration Library"
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/intel/hexl"
     topics = ("homomorphic", "encryption", "privacy")
-    settings = "os", "compiler", "build_type", "arch"
-    exports_sources = ["CMakeLists.txt", "patches/**"]
-    generators = "cmake", "cmake_find_package"
 
-    _cmake = None
-
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
         "experimental": [True, False],
         "fpga_compatibility_dyadic_multiply": [True, False],
-        "fpga_compatibility_keyswitch":  [True, False]
+        "fpga_compatibility_keyswitch": [True, False],
     }
-
     default_options = {
         "shared": False,
         "fPIC": True,
         "experimental": False,
         "fpga_compatibility_dyadic_multiply": False,
-        "fpga_compatibility_keyswitch": False
+        "fpga_compatibility_keyswitch": False,
     }
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
-    def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
-
-    def build_requirements(self):
-        self.build_requires("cmake/3.22.0")
-
-    def requirements(self):
-        self.requires("cpu_features/0.7.0")
-
-        if self.settings.build_type == "Debug":
-            self.requires("easyloggingpp/9.97.0")
 
     @property
     def _compilers_minimum_version(self):
         return {
             "gcc": "7",
             "Visual Studio": "15.7",
+            "msvc": "191",
             "clang": "7",
             "apple-clang": "11",
         }
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("cpu_features/0.8.0")
+
+        if self.settings.build_type == "Debug":
+            self.requires("easyloggingpp/9.97.0")
+
     def validate(self):
         if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, 17)
+            check_min_cppstd(self, 17)
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if minimum_version:
             if Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration("{} requires C++17, which your compiler does not support.".format(self.name))
+                raise ConanInvalidConfiguration(
+                    f"{self.name} requires C++17, which your compiler does not support."
+                )
         else:
-            self.output.warn("{} requires C++17. Your compiler is unknown. Assuming it supports C++17.".format(self.name))
+            self.output.warning(
+                f"{self.name} requires C++17. Your compiler is unknown. Assuming it supports C++17."
+            )
 
         if self.settings.arch not in ["x86", "x86_64"]:
             raise ConanInvalidConfiguration("Hexl only supports x86 architecture")
@@ -84,50 +88,46 @@ class HexlConan(ConanFile):
         if self.options.shared and is_msvc(self):
             raise ConanInvalidConfiguration("Hexl only supports static linking with msvc")
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.24]")
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self);
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
+    def generate(self):
+        tc = CMakeToolchain(self)
 
-        self._cmake.definitions["HEXL_BENCHMARK"] = False
-        self._cmake.definitions["HEXL_TESTING"] = False
-        self._cmake.definitions["HEXL_EXPERIMENTAL"] = self.options.experimental
-
+        tc.variables["HEXL_BENCHMARK"] = False
+        tc.variables["HEXL_TESTING"] = False
+        tc.variables["HEXL_EXPERIMENTAL"] = self.options.experimental
 
         if self.options.fpga_compatibility_dyadic_multiply and self.options.fpga_compatibility_keyswitch:
-            self._cmake.definitions["HEXL_FPGA_COMPATIBILITY"] = 3
+            tc.variables["HEXL_FPGA_COMPATIBILITY"] = 3
         elif self.options.fpga_compatibility_dyadic_multiply:
-            self._cmake.definitions["HEXL_FPGA_COMPATIBILITY"] = 1
+            tc.variables["HEXL_FPGA_COMPATIBILITY"] = 1
         elif self.options.fpga_compatibility_keyswitch:
-            self._cmake.definitions["HEXL_FPGA_COMPATIBILITY"] = 2
+            tc.variables["HEXL_FPGA_COMPATIBILITY"] = 2
         else:
-            self._cmake.definitions["HEXL_FPGA_COMPATIBILITY"] = 0
+            tc.variables["HEXL_FPGA_COMPATIBILITY"] = 0
 
-        self._cmake.definitions["HEXL_SHARED_LIB"] = self.options.shared
-        self._cmake.definitions["HEXL_CROSS_COMPILED"] = cross_building(self)
+        tc.variables["HEXL_SHARED_LIB"] = self.options.shared
+        tc.variables["HEXL_CROSS_COMPILED"] = cross_building(self)
 
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+        tc.generate()
 
-    def configure(self):
-        if self.options.shared:
-            del self.options.fPIC
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        copy(self, "LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
@@ -155,4 +155,3 @@ class HexlConan(ConanFile):
 
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["Hexl"].system_libs = ["pthread", "m"]
-
