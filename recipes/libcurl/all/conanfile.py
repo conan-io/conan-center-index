b0bdb5763053251a9d7afe7c58ead30538bed0d4
toge <toge.mail@gmail.com>
2023-07-20 18:11:26 +0900

    fix brotli patch

--- recipes/libcurl/all/conanfile.py
+++ recipes/libcurl/all/conanfile.py
@@ -334,7 +334,8 @@ class LibcurlConan(ConanFile):
                               "include(CurlSymbolHiding)", "")
 
         # brotli
-        replace_in_file(self, cmakelists, "find_package(Brotli QUIET)", "find_package(brotli REQUIRED CONFIG)")
+        if Version(self.version) < "8.2.0":
+            replace_in_file(self, cmakelists, "find_package(Brotli QUIET)", "find_package(brotli REQUIRED CONFIG)")
         replace_in_file(self, cmakelists, "if(BROTLI_FOUND)", "if(brotli_FOUND)")
         replace_in_file(self, cmakelists, "${BROTLI_LIBRARIES}", "brotli::brotli")
         replace_in_file(self, cmakelists, "${BROTLI_INCLUDE_DIRS}", "${brotli_INCLUDE_DIRS}")
