35a18a825908f832b6c825798aa0a7e1429292d9
Hwa <hwahe.cs@gmail.com>
2023-07-20 18:14:25 +0800

    fix: update grammar lib to dynamic lib

--- recipes/tree-sitter-c/all/conanfile.py
+++ recipes/tree-sitter-c/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake
-from conan.tools.files import get, replace_in_file, copy
+from conan.tools.files import get, copy
 from conan.tools.layout import basic_layout
 import os
 
@@ -15,13 +15,12 @@ class TreeSitterCConan(ConanFile):
     homepage = "https://github.com/tree-sitter/tree-sitter-c"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
+    package_type = "shared-library"
     options = {
         "fPIC": [True, False],
-        "shared": [True, False],
     }
     default_options = {
         "fPIC": True,
-        "shared": False,
     }
 
     generators = "CMakeToolchain", "CMakeDeps"
@@ -35,8 +34,6 @@ class TreeSitterCConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
         self.settings.rm_safe("compiler.cppstd")
         self.settings.rm_safe("compiler.libcxx")
 
@@ -46,16 +43,7 @@ class TreeSitterCConan(ConanFile):
     def requirements(self):
         self.requires("tree-sitter/0.20.6", transitive_headers=True, transitive_libs=True)
 
-    def _patch_sources(self):
-        if not self.options.shared:
-            replace_in_file(
-                self,
-                os.path.join(self.source_folder, "src", "parser.c"),
-                "__declspec(dllexport)", ""
-            )
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
         cmake.build()
