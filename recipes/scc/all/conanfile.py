1f6b32cdf186056b0da508cfb86506a17c7e9eaf
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 13:00:24 +0300

    scc: migrate to Conan v2

--- recipes/scc/all/conanfile.py
+++ recipes/scc/all/conanfile.py
@@ -1,84 +1,97 @@
-from conan import ConanFile
-from conan.tools.scm import Version
-from conan.tools.files import get
-from conans import tools, CMake
-from conan.errors import ConanInvalidConfiguration
-import functools
+import os
 
-required_conan_version = ">=1.50.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.apple import is_apple_os
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.files import copy, get, export_conandata_patches, apply_conandata_patches
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.53.0"
+
 
 class SystemcComponentsConan(ConanFile):
     name = "scc"
-    description = """A light weight productivity library for SystemC and TLM 2.0"""
-    homepage = "https://minres.github.io/SystemC-Components"
-    url = "https://github.com/conan-io/conan-center-index"
+    description = "A light weight productivity library for SystemC and TLM 2.0"
     license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://minres.github.io/SystemC-Components"
     topics = ("systemc", "modeling", "tlm", "scc")
-    settings = "os", "compiler", "build_type", "arch"
+
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
     options = {
+        "shared": [True, False],
         "fPIC": [True, False],
         "enable_phase_callbacks": [True, False],
-        "enable_phase_callbacks_tracing": [True, False]
+        "enable_phase_callbacks_tracing": [True, False],
     }
     default_options = {
+        "shared": False,
         "fPIC": True,
         "enable_phase_callbacks": False,
-        "enable_phase_callbacks_tracing": False
+        "enable_phase_callbacks_tracing": False,
     }
-    generators = "cmake"
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
-    # no exports_sources attribute, but export_sources(self) method instead
-    # this allows finer grain exportation of patches per version
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-            
+        export_conandata_patches(self)
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-            
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("fmt/8.1.1")
+        self.requires("zlib/1.2.13")
+        self.requires("boost/1.82.0")
+        self.requires("gsl-lite/0.40.0")
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 11)
-        if self.settings.os == "Macos":
+            check_min_cppstd(self, 11)
+        if is_apple_os(self):
             raise ConanInvalidConfiguration(f"{self.name} is not suppported on {self.settings.os}.")
         if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "7":
             raise ConanInvalidConfiguration("GCC < version 7 is not supported")
 
-    def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
-
     def build_requirements(self):
-        self.tool_requires("cmake/3.24.0")
+        self.tool_requires("cmake/[>=3.24]")
 
-    @functools.lru_cache(1)
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["SC_WITH_PHASE_CALLBACKS"] = self.options.enable_phase_callbacks
-        cmake.definitions["SC_WITH_PHASE_CALLBACK_TRACING"] = self.options.enable_phase_callbacks_tracing
-        cmake.definitions["BUILD_SCC_DOCUMENTATION"] = False
-        cmake.definitions["SCC_LIB_ONLY"] = True
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["SC_WITH_PHASE_CALLBACKS"] = self.options.enable_phase_callbacks
+        tc.variables["SC_WITH_PHASE_CALLBACK_TRACING"] = self.options.enable_phase_callbacks_tracing
+        tc.variables["BUILD_SCC_DOCUMENTATION"] = False
+        tc.variables["SCC_LIB_ONLY"] = True
+        tc.variables["ENABLE_CONAN"] = False
         if self.settings.os == "Windows":
-            cmake.definitions["SCC_LIMIT_TRACE_TYPE_LIST"] = True
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+            tc.variables["SCC_LIMIT_TRACE_TYPE_LIST"] = True
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses",
-                  src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE",
+             dst=os.path.join(self.package_folder, "licenses"),
+             src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
