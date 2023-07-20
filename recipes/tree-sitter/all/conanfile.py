93203c0754fc8ca6e5a30bbd0a0568b9de0fbb32
Carlos Zoido <mrgalleta@gmail.com>
2023-07-20 16:16:20 +0200

    Update recipes/tree-sitter/all/conanfile.py

--- recipes/tree-sitter/all/conanfile.py
+++ recipes/tree-sitter/all/conanfile.py
@@ -25,7 +25,7 @@ class TreeSitterConan(ConanFile):
         "shared": False,
     }
 
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "CMakeToolchain"
     exports_sources = "CMakeLists.txt"
 
     def config_options(self):
