73ced606ec998e0d02831de07c46f17d4d6579f3
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 14:43:57 +0300

    nsimd: migrate to Conan v2

--- recipes/nsimd/2.x/conanfile.py
+++ recipes/nsimd/2.x/conanfile.py
@@ -1,41 +1,40 @@
 import os
-from conans import ConanFile, CMake, tools
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, export_conandata_patches, get, replace_in_file
+
+required_conan_version = ">=1.53.0"
 
 
 class NsimdConan(ConanFile):
     name = "nsimd"
-    homepage = "https://github.com/agenium-scale/nsimd"
     description = "Agenium Scale vectorization library for CPUs and GPUs"
-    topics = ("hpc", "neon", "cuda", "avx", "simd", "avx2", "sse2", "aarch64", "avx512", "sse42", "rocm", "sve", "neon128")
-    url = "https://github.com/conan-io/conan-center-index"
     license = "MIT"
-    exports_sources = ["CMakeLists.txt", "patches/*"]
-    generators = "cmake"
-    settings = "os", "compiler", "build_type", "arch"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/agenium-scale/nsimd"
+    topics = ("hpc", "neon", "cuda", "avx", "simd", "avx2", "sse2",
+              "aarch64", "avx512", "sse42", "rocm", "sve", "neon128")
+
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
         # This used only when building the library.
         # Most functionality is header only.
-        "simd": [None, "cpu", "sse2", "sse42", "avx", "avx2", "avx512_knl", "avx512_skylake", "neon128", "aarch64", "sve", "sve128", "sve256", "sve512", "sve1024", "sve2048", "cuda", "rocm"]
+        "simd": [None, "cpu", "sse2", "sse42", "avx", "avx2", "avx512_knl",
+                 "avx512_skylake", "neon128", "aarch64", "sve", "sve128",
+                 "sve256", "sve512", "sve1024", "sve2048", "cuda", "rocm"]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "simd": None
+        "simd": None,
     }
 
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -43,47 +42,49 @@ class NsimdConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
         # Most of the library is header only.
         # cpp files do not use STL.
-        del self.settings.compiler.libcxx
+        self.settings.rm_safe("compiler.libcxx")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
+    def generate(self):
+        tc = CMakeToolchain(self)
         if self.options.simd:
-            self._cmake.definitions["simd"] = self.options.simd
+            tc.variables["simd"] = self.options.simd
         if self.settings.arch == "armv7hf":
-            self._cmake.definitions["NSIMD_ARM32_IS_ARMEL"] = False
-        self._cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.get_safe("fPIC", True)
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+            tc.variables["NSIMD_ARM32_IS_ARMEL"] = False
+        tc.generate()
 
     def _patch_sources(self):
-        cmakefile_path = os.path.join(self._source_subfolder, "CMakeLists.txt")
-        tools.replace_in_file(cmakefile_path,
-                              " SHARED ",
-                              " ")
-        tools.replace_in_file(cmakefile_path,
-                              "RUNTIME DESTINATION lib",
-                              "RUNTIME DESTINATION bin")
-        tools.replace_in_file(cmakefile_path,
-                              "set_property(TARGET ${o} PROPERTY POSITION_INDEPENDENT_CODE ON)",
-                              "")
+        cmakefile_path = os.path.join(self.source_folder, "CMakeLists.txt")
+        replace_in_file(self, cmakefile_path,
+                        " SHARED ",
+                        " ")
+        replace_in_file(self, cmakefile_path,
+                        "RUNTIME DESTINATION lib",
+                        "RUNTIME DESTINATION bin")
+        replace_in_file(self, cmakefile_path,
+                        "set_property(TARGET ${o} PROPERTY POSITION_INDEPENDENT_CODE ON)",
+                        "")
 
     def build(self):
         self._patch_sources()
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, "LICENSE",
+             dst=os.path.join(self.package_folder, "licenses"),
+             src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["nsimd_cpu"]
