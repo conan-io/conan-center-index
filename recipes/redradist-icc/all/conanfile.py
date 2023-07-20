1e71cce51c89210287b6935116d0cf8bfa87fab6
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 23:17:42 +0300

    redradist-icc: migrate to Conan v2

--- recipes/redradist-icc/all/conanfile.py
+++ recipes/redradist-icc/all/conanfile.py
@@ -1,33 +1,107 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration, ConanException
 import os
+
+from conan import ConanFile, conan_version
+from conan.errors import ConanInvalidConfiguration, ConanException
+from conan.tools.android import android_abi
+from conan.tools.apple import (
+    XCRun,
+    fix_apple_shared_install_name,
+    is_apple_os,
+    to_apple_arch,
+)
+from conan.tools.build import (
+    build_jobs,
+    can_run,
+    check_min_cppstd,
+    cross_building,
+    default_cppstd,
+    stdcpp_library,
+    valid_min_cppstd,
+)
+from conan.tools.cmake import (
+    CMake,
+    CMakeDeps,
+    CMakeToolchain,
+    cmake_layout,
+)
+from conan.tools.env import (
+    Environment,
+    VirtualBuildEnv,
+    VirtualRunEnv,
+)
+from conan.tools.files import (
+    apply_conandata_patches,
+    chdir,
+    collect_libs,
+    copy,
+    download,
+    export_conandata_patches,
+    get,
+    load,
+    mkdir,
+    patch,
+    patches,
+    rename,
+    replace_in_file,
+    rm,
+    rmdir,
+    save,
+    symlinks,
+    unzip,
+)
+from conan.tools.gnu import (
+    Autotools,
+    AutotoolsDeps,
+    AutotoolsToolchain,
+    PkgConfig,
+    PkgConfigDeps,
+)
+from conan.tools.layout import basic_layout
+from conan.tools.meson import MesonToolchain, Meson
+from conan.tools.microsoft import (
+    MSBuild,
+    MSBuildDeps,
+    MSBuildToolchain,
+    NMakeDeps,
+    NMakeToolchain,
+    VCVars,
+    check_min_vs,
+    is_msvc,
+    is_msvc_static_runtime,
+    msvc_runtime_flag,
+    unix_path,
+    unix_path_package_info_legacy,
+    vs_layout,
+)
+from conan.tools.scm import Version
+from conan.tools.system import package_manager
+import os
+
+required_conan_version = ">=1.53.0"
 
 
 class ICCConan(ConanFile):
-    name = 'redradist-icc'
-    homepage = 'https://github.com/redradist/Inter-Component-Communication'
-    license = 'MIT'
-    url = 'https://github.com/conan-io/conan-center-index'
-    description = "I.C.C. - Inter Component Communication, This is a library created to simplify communication between " \
-                  "components inside of single application. It is thread safe and could be used for creating " \
-                  "components that works in different threads. "
+    name = "redradist-icc"
+    description = (
+        "I.C.C. - Inter Component Communication, This is a library created to simplify communication between "
+        "components inside of single application. It is thread safe and could be used for creating "
+        "components that works in different threads. "
+    )
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/redradist/Inter-Component-Communication"
     topics = ("thread-safe", "active-object", "communication")
-    settings = "os", "compiler", "build_type", "arch"
+
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
-        'fPIC': [True, False],
+        "fPIC": [True, False],
     }
     default_options = {
-        'shared': False,
-        'fPIC': True,
+        "shared": False,
+        "fPIC": True,
     }
-    generators = "cmake", "cmake_find_package", "cmake_find_package_multi"
-
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
     def _minimum_cpp_standard(self):
@@ -39,77 +113,75 @@ class ICCConan(ConanFile):
             "Visual Studio": "15",
             "apple-clang": "9.4",
             "clang": "3.3",
-            "gcc": "4.9.4"
+            "gcc": "4.9.4",
         }
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        cmake = CMake(self)
-        cmake.definitions['ICC_BUILD_SHARED'] = self.options.shared
-        cmake.configure()
-        self._cmake = cmake
-        return self._cmake
-
-    def validate(self):
-        if self.settings.get_safe("compiler.cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
-
-        os = self.settings.os
-        if os not in ("Windows", "Linux"):
-            msg = (
-                "OS {} is not supported !!"
-            ).format(os)
-            raise ConanInvalidConfiguration(msg)
-
-        compiler = self.settings.compiler
-        try:
-            min_version = self._minimum_compilers_version[str(compiler)]
-            if tools.Version(compiler.version) < min_version:
-                msg = (
-                    "{} requires C++{} features which are not supported by compiler {} {} !!"
-                ).format(self.name, self._minimum_cpp_standard, compiler, compiler.version)
-                raise ConanInvalidConfiguration(msg)
-        except KeyError:
-            msg = (
-                "{} recipe lacks information about the {} compiler, "
-                "support for the required C++{} features is assumed"
-            ).format(self.name, compiler, self._minimum_cpp_standard)
-            self.output.warn(msg)
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
-        if self.settings.os == 'Windows':
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
 
-    def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def validate(self):
+        if self.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, self._minimum_cpp_standard)
+
+        os = self.settings.os
+        if os not in ("Windows", "Linux"):
+            raise ConanInvalidConfiguration(f"OS {os} is not supported")
+
+        compiler = self.settings.compiler
+        try:
+            min_version = self._minimum_compilers_version[str(compiler)]
+            if Version(compiler.version) < min_version:
+                raise ConanInvalidConfiguration(
+                    f"{self.name} requires C++{self._minimum_cpp_standard} features "
+                    f"which are not supported by compiler {compiler} {compiler.version}")
+        except KeyError:
+            self.output.warning(
+                f"{self.name} recipe lacks information about the {compiler} compiler, "
+                f"support for the required C++{self._minimum_cpp_standard} features is assumed"
+            )
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["ICC_BUILD_SHARED"] = self.options.shared
+        tc.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.names["cmake_find_package"] = "icc"
-        self.cpp_info.names["cmake_find_package_multi"] = "icc"
+        self.cpp_info.set_property("cmake_file_name", "icc")
+        self.cpp_info.set_property("cmake_target_name", "icc::icc")
+
         if self.options.shared:
             self.cpp_info.libs = ["ICC"]
         else:
             self.cpp_info.libs = ["ICC_static"]
 
-        if self.settings.os == 'Windows':
-            self.cpp_info.system_libs = ['ws2_32', 'wsock32']
-        if self.settings.os == 'Linux':
-            self.cpp_info.system_libs = ['pthread']
+        if self.settings.os == "Windows":
+            self.cpp_info.system_libs = ["ws2_32", "wsock32"]
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["pthread"]
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.names["cmake_find_package"] = "icc"
+        self.cpp_info.names["cmake_find_package_multi"] = "icc"
--- recipes/redradist-icc/all/test_package/example.cpp
+++ recipes/redradist-icc/all/test_package/example.cpp
@@ -1,14 +1,15 @@
 #include <icc/Component.hpp>
 #include <icc/Event.hpp>
 #include <icc/Context.hpp>
-#include <thread>
 #include <icc/os/timer/Timer.hpp>
+
+#include <thread>
 
 int main() {
   auto timer = icc::os::Timer::createTimer();
-  timer->setInterval(std::chrono::seconds(10));
+  timer->setInterval(std::chrono::milliseconds(100));
   timer->enableContinuous();
   timer->start();
-  std::this_thread::sleep_for(std::chrono::seconds(25));
+  std::this_thread::sleep_for(std::chrono::seconds(1));
   return 0;
 }
