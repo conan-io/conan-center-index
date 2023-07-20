00ab13b6863d6c0d28d51683f5f1da0de8ad225e
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 23:18:01 +0300

    stx: migrate to Conan v2

--- recipes/stx/all/conanfile.py
+++ recipes/stx/all/conanfile.py
@@ -1,164 +1,141 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import collect_libs, copy, get
+from conan.tools.microsoft import is_msvc, check_min_vs
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.53.0"
 
 
 class STXConan(ConanFile):
-    name = 'stx'
-    homepage = 'https://github.com/lamarrr/STX'
-    license = 'MIT'
-    url = 'https://github.com/conan-io/conan-center-index'
-    description = 'C++17 & C++ 20 error-handling and utility extensions.'
-    topics = 'error-handling', 'result', 'option', 'backtrace', 'panic'
+    name = "stx"
+    description = "C++17 & C++ 20 error-handling and utility extensions."
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/lamarrr/STX"
+    topics = ("error-handling", "result", "option", "backtrace", "panic")
 
-    settings = 'os', 'compiler', 'build_type', 'arch'
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
     options = {
-        'shared': [True, False],
-        'fPIC': [True, False],
-        'backtrace': [True, False],
-        'panic_handler': [None, 'default', 'backtrace'],
-        'visible_panic_hook': [True, False],
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "backtrace": [True, False],
+        "panic_handler": [None, "default", "backtrace"],
+        "visible_panic_hook": [True, False],
     }
     default_options = {
-        'shared': False,
-        'fPIC': True,
-        'backtrace': False,
-        'panic_handler': 'default',
-        'visible_panic_hook': False,
+        "shared": False,
+        "fPIC": True,
+        "backtrace": False,
+        "panic_handler": "default",
+        "visible_panic_hook": False,
     }
 
-    exports_sources = ['CMakeLists.txt', 'patches/*']
-    generators = 'cmake', 'cmake_find_package'
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
     def config_options(self):
-        if self.settings.os == 'Windows':
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def requirements(self):
         if self.options.backtrace:
-            self.requires('abseil/20200923.1')
+            self.requires("abseil/20230125.3")
 
     def validate(self):
-        if (self.options.panic_handler == 'backtrace' and
-                not self.options.backtrace):
-            raise ConanInvalidConfiguration(
-                'panic_handler=backtrace requires backtrace=True'
-            )
+        if self.options.panic_handler == "backtrace" and not self.options.backtrace:
+            raise ConanInvalidConfiguration("panic_handler=backtrace requires backtrace=True")
 
         compiler = self.settings.compiler
-        compiler_version = tools.Version(self.settings.compiler.version)
+        compiler_version = Version(self.settings.compiler.version)
 
-        if compiler.get_safe('cppstd'):
-            tools.check_min_cppstd(self, 17)
+        if compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 17)
 
-        if compiler == 'Visual Studio' and compiler_version < 16:
+        check_min_vs(self, 192)
+
+        if compiler == "gcc" and compiler_version < 8:
             raise ConanInvalidConfiguration(
-                'STX requires C++17 language and standard library features '
-                'which VS < 2019 lacks'
+                "STX requires C++17 language and standard library features which GCC < 8 lacks"
             )
 
-        if compiler == 'gcc' and compiler_version < 8:
+        if (
+            compiler == "clang"
+            and compiler.libcxx
+            and compiler.libcxx in ["libstdc++", "libstdc++11"]
+            and compiler_version < 9
+        ):
             raise ConanInvalidConfiguration(
-                'STX requires C++17 language and standard library features '
-                'which GCC < 8 lacks'
+                "STX requires C++17 language and standard library features which clang < 9 with libc++ lacks"
             )
 
-        if (compiler == 'clang' and compiler.libcxx and
-                compiler.libcxx in ['libstdc++', 'libstdc++11'] and
-                compiler_version < 9):
+        if compiler == "clang" and compiler.libcxx and compiler.libcxx == "libc++" and compiler_version < 10:
             raise ConanInvalidConfiguration(
-                'STX requires C++17 language and standard library features '
-                'which clang < 9 with libc++ lacks'
+                "STX requires C++17 language and standard library features which clang < 10 with libc++ lacks"
             )
 
-        if (compiler == 'clang' and compiler.libcxx and
-                compiler.libcxx == 'libc++' and
-                compiler_version < 10):
+        if compiler == "apple-clang" and compiler_version < 12:
             raise ConanInvalidConfiguration(
-                'STX requires C++17 language and standard library features '
-                'which clang < 10 with libc++ lacks'
+                "STX requires C++17 language and standard library features "
+                "which apple-clang < 12 with libc++ lacks"
             )
 
-        if compiler == 'apple-clang' and compiler_version < 12:
-            raise ConanInvalidConfiguration(
-                'STX requires C++17 language and standard library features '
-                'which apple-clang < 12 with libc++ lacks'
-            )
-
-        if (compiler == 'Visual Studio' and self.options.shared and
-                tools.Version(self.version) <= '1.0.1'):
-            raise ConanInvalidConfiguration(
-                'shared library build does not work on windows with '
-                'STX version <= 1.0.1'
-            )
+        # if is_msvc(self) and self.options.shared and Version(self.version) <= "1.0.1":
+        #     raise ConanInvalidConfiguration(
+        #         "shared library build does not work on windows with STX version <= 1.0.1"
+        #     )
 
     def source(self):
-        tools.get(**self.conan_data['sources'][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["STX_BUILD_SHARED"] = self.options.shared
+        tc.variables["STX_ENABLE_BACKTRACE"] = self.options.backtrace
+        tc.variables["STX_ENABLE_PANIC_BACKTRACE"] = self.options.panic_handler == "backtrace"
+        tc.variables["STX_OVERRIDE_PANIC_HANDLER"] = self.options.panic_handler == None
+        tc.variables["STX_VISIBLE_PANIC_HOOK"] = self.options.visible_panic_hook
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data.get('patches', {}).get(self.version, []):
-            tools.patch(**patch)
-
         cmake = CMake(self)
-        cmake.definitions['STX_BUILD_SHARED'] = self.options.shared
-        cmake.definitions['STX_ENABLE_BACKTRACE'] = self.options.backtrace
-        cmake.definitions['STX_ENABLE_PANIC_BACKTRACE'] = \
-            self.options.panic_handler == 'backtrace'
-        cmake.definitions['STX_OVERRIDE_PANIC_HANDLER'] = \
-            self.options.panic_handler == None
-        cmake.definitions['STX_VISIBLE_PANIC_HOOK'] = \
-            self.options.visible_panic_hook
-
-        cmake.configure(build_folder=self._build_subfolder)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(
-            '*.h',
-            dst='include',
-            src=os.path.join(self._source_subfolder, 'include')
-        )
-
-        self.copy('*.lib', dst='lib', keep_path=False)
-        self.copy('*.dll', dst='bin', keep_path=False)
-        self.copy('*.so', dst='lib', keep_path=False)
-        self.copy('*.dylib', dst='lib', keep_path=False)
-        self.copy('*.a', dst='lib', keep_path=False)
-
-        self.copy('LICENSE', dst='licenses', src=self._source_subfolder)
+        copy(self, "*.h",
+            dst=os.path.join(self.package_folder, "include"),
+            src=os.path.join(self.source_folder, "include"))
+        for pattern in ["*.lib", "*.so", "*.dylib", "*.a"]:
+            copy(self, pattern, dst=os.path.join(self.package_folder, "lib"), src=self.build_folder, keep_path=False)
+        copy(self, "*.dll", dst=os.path.join(self.package_folder, "bin"), src=self.build_folder, keep_path=False)
+        copy(self, "LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
 
         if self.options.backtrace:
-            self.cpp_info.requires = [
-                'abseil::absl_stacktrace',
-                'abseil::absl_symbolize'
-            ]
+            self.cpp_info.requires = ["abseil::absl_stacktrace", "abseil::absl_symbolize"]
 
         if self.options.visible_panic_hook:
-            self.cpp_info.defines.append('STX_VISIBLE_PANIC_HOOK')
+            self.cpp_info.defines.append("STX_VISIBLE_PANIC_HOOK")
 
         if self.options.panic_handler == None:
-            self.cpp_info.defines.append('STX_OVERRIDE_PANIC_HANDLER')
+            self.cpp_info.defines.append("STX_OVERRIDE_PANIC_HANDLER")
 
-        if self.options.panic_handler == 'backtrace':
-            self.cpp_info.defines.append('STX_ENABLE_PANIC_BACKTRACE')
+        if self.options.panic_handler == "backtrace":
+            self.cpp_info.defines.append("STX_ENABLE_PANIC_BACKTRACE")
 
-        if self.settings.os == 'Android':
-            self.cpp_info.system_libs = ['atomic']
+        if self.settings.os == "Android":
+            self.cpp_info.system_libs = ["atomic"]
