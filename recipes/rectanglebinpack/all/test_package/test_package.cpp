36ba8de926e2650c535dafe6306460e4ee829139
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 13:00:20 +0300

    rectanglebinpack: migrate to Conan v2

--- recipes/rectanglebinpack/all/test_package/test_package.cpp
+++ recipes/rectanglebinpack/all/test_package/test_package.cpp
@@ -5,43 +5,38 @@
 
 #include <cstdint>
 #include <cstdio>
-#include <vector>
 #include <rectanglebinpack/MaxRectsBinPack.h>
+#include <vector>
 
 using namespace rbp;
 
-bool AreDisjoint(const Rect &a, const Rect &b)
-{
-	return a.x >= b.x + b.width || a.x + a.width <= b.x ||
-		a.y >= b.y + b.height || a.y + a.height <= b.y;
+bool AreDisjoint(const Rect &a, const Rect &b) {
+    return a.x >= b.x + b.width || a.x + a.width <= b.x || a.y >= b.y + b.height ||
+           a.y + a.height <= b.y;
 }
 
-bool AllRectsDisjoint(std::vector<Rect> &packed)
-{
-	for(size_t i = 0; i < packed.size(); ++i)
-		for(size_t j = i+1; j < packed.size(); ++j)
-		{
-			if (!AreDisjoint(packed[i], packed[j]))
-				return false;
-		}
-	return true;
+bool AllRectsDisjoint(std::vector<Rect> &packed) {
+    for (size_t i = 0; i < packed.size(); ++i)
+        for (size_t j = i + 1; j < packed.size(); ++j) {
+            if (!AreDisjoint(packed[i], packed[j]))
+                return false;
+        }
+    return true;
 }
 
-int main()
-{
-	MaxRectsBinPack pack(256, 256, true);
+int main() {
+    MaxRectsBinPack pack(256, 256, true);
 
-	std::vector<Rect> packed;
-	srand(12412);
-	for(int i = 1; i < 128; ++i)
-	{
-		int a = (rand() % 128) + 1;
-		int b = (rand() % 128) + 1;
-		Rect r = pack.Insert(a, b, MaxRectsBinPack::RectBestShortSideFit);
-		if (!r.width)
-			break;
-		packed.push_back(r);
-	}
-	printf("Packed %d rectangles. All rects disjoint: %s. Occupancy: %f\n",
-		(int)packed.size(), AllRectsDisjoint(packed) ? "yes" : "NO!", pack.Occupancy());
+    std::vector<Rect> packed;
+    srand(12412);
+    for (int i = 1; i < 128; ++i) {
+        int a = (rand() % 128) + 1;
+        int b = (rand() % 128) + 1;
+        Rect r = pack.Insert(a, b, MaxRectsBinPack::RectBestShortSideFit);
+        if (!r.width)
+            break;
+        packed.push_back(r);
+    }
+    printf("Packed %d rectangles. All rects disjoint: %s. Occupancy: %f\n", (int)packed.size(),
+           AllRectsDisjoint(packed) ? "yes" : "NO!", pack.Occupancy());
 }
