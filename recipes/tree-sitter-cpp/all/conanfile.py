a210e28cecc3e372259822a76afcf6d1d78316a6
Hwa <hwahe.cs@gmail.com>
2023-07-20 17:59:06 +0800

    fix: only provide shared-library for grammar

--- recipes/tree-sitter-cpp/all/conanfile.py
+++ recipes/tree-sitter-cpp/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake
 from conan.tools.layout import basic_layout
-import conan.tools.files as tools
+from conan.tools.files import copy, get, replace_in_file
 import os
 
 required_conan_version = ">=1.53.0"
@@ -15,16 +15,14 @@ class TreeSitterCPPConan(ConanFile):
     homepage = "https://tree-sitter.github.io/tree-sitter"
     license = "MIT"
     
-    package_type = "library"
+    package_type = "shared-library"
     
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "fPIC": [True, False],
-        "shared": [True, False],
     }
     default_options = {
         "fPIC": True,
-        "shared": False,
     }
 
     generators = "CMakeToolchain", "CMakeDeps"
@@ -35,8 +33,6 @@ class TreeSitterCPPConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
         self.settings.rm_safe("compiler.cppstd")
         self.settings.rm_safe("compiler.libcxx")
 
@@ -44,27 +40,18 @@ class TreeSitterCPPConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def requirements(self):
         self.requires("tree-sitter/0.20.6", transitive_headers=True, transitive_libs=True)
 
-    def _patch_sources(self):
-        if not self.options.shared:
-            tools.replace_in_file(
-                self,
-                os.path.join(self.source_folder, "src", "parser.c"),
-                "__declspec(dllexport)", ""
-            )
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
         cmake.build()
 
     def package(self):
-        tools.copy(
+        copy(
             self,
             "LICENSE",
             src=self.source_folder,
