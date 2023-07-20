e06e389ad6868a2bc91d1e4d19d2532a9fd9c029
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 14:44:44 +0300

    unleash-client-cpp: migrate to Conan v2

--- recipes/unleash-client-cpp/all/test_package/test_package.cpp
+++ recipes/unleash-client-cpp/all/test_package/test_package.cpp
@@ -3,8 +3,8 @@
 #include <unleash/unleashclient.h>
 
 int main() {
-    unleash::UnleashClient unleashClient = unleash::UnleashClient::create("production", "https://www.apple.com/%");
+    unleash::UnleashClient unleashClient =
+        unleash::UnleashClient::create("production", "https://www.apple.com/%");
     unleashClient.initializeClient();
     return unleashClient.isEnabled("feature.toogle");
-
 }
