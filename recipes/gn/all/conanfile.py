088059588c98c13303075344c94a0945fb5fcdc7
Martin Valgur <martin.valgur@gmail.com>
2023-07-20 23:15:49 +0300

    gn: migrate to Conan v2

--- recipes/gn/all/conanfile.py
+++ recipes/gn/all/conanfile.py
@@ -1,28 +1,37 @@
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
-from contextlib import contextmanager
-import conan.tools.files as tools_files
-import conan.tools.scm as tools_scm
 import os
 import sys
 import textwrap
 import time
 
-required_conan_version = ">=1.46.0"
+import conan.tools.scm as tools_scm
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.apple import is_apple_os
+from conan.tools.build import check_min_cppstd
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import chdir, copy, get, load, save
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc
+
+required_conan_version = ">=1.47.0"
 
 
 class GnConan(ConanFile):
     name = "gn"
     description = "GN is a meta-build system that generates build files for Ninja."
-    url = "https://github.com/conan-io/conan-center-index"
-    topics = ("gn", "build", "system", "ninja")
     license = "BSD-3-Clause"
+    url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://gn.googlesource.com/"
+    topics = ("build system", "ninja")
+
+    package_type = "application"
     settings = "os", "arch", "compiler", "build_type"
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        del self.info.settings.compiler
 
     @property
     def _minimum_compiler_version_supporting_cxx17(self):
@@ -35,94 +44,80 @@ class GnConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, 17)
+            check_min_cppstd(self, 17)
         else:
             if self._minimum_compiler_version_supporting_cxx17:
-                if tools_scm.Version(self.settings.compiler.version) < self._minimum_compiler_version_supporting_cxx17:
+                if (
+                    tools_scm.Version(self.settings.compiler.version)
+                    < self._minimum_compiler_version_supporting_cxx17
+                ):
                     raise ConanInvalidConfiguration("gn requires a compiler supporting c++17")
             else:
-                self.output.warn("gn recipe does not recognize the compiler. gn requires a compiler supporting c++17. Assuming it does.")
-
-    def package_id(self):
-        del self.info.settings.compiler
-
-    def source(self):
-        tools_files.get(self, **self.conan_data["sources"][self.version], destination=self._source_subfolder)
+                self.output.warning(
+                    "gn recipe does not recognize the compiler. gn requires a compiler supporting c++17."
+                    " Assuming it does."
+                )
 
     def build_requirements(self):
         # FIXME: add cpython build requirements for `build/gen.py`.
         self.build_requires("ninja/1.10.2")
 
-    @contextmanager
-    def _build_context(self):
-        if self.settings.compiler == "Visual Studio":
-            with tools.vcvars(self.settings):
-                yield
-        else:
-            compiler_defaults = {}
-            if self.settings.compiler == "gcc":
-                compiler_defaults = {
-                    "CC": "gcc",
-                    "CXX": "g++",
-                    "AR": "ar",
-                    "LD": "g++",
-                }
-            elif self.settings.compiler == "clang":
-                compiler_defaults = {
-                    "CC": "clang",
-                    "CXX": "clang++",
-                    "AR": "ar",
-                    "LD": "clang++",
-                }
-            env = {}
-            for k in ("CC", "CXX", "AR", "LD"):
-                v = tools.get_env(k, compiler_defaults.get(k, None))
-                if v:
-                    env[k] = v
-            with tools.environment_append(env):
-                yield
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version])
 
-    @staticmethod
-    def _to_gn_platform(os_, compiler):
-        if tools.is_apple_os(os_):
+    def _to_gn_platform(self, os_, compiler):
+        if is_apple_os(self):
             return "darwin"
-        if compiler == "Visual Studio":
+        if is_msvc(self):
             return "msvc"
         # Assume gn knows about the os
         return str(os_).lower()
 
+    def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+        configure_args = [
+            "--no-last-commit-position",
+            "--host={}".format(self._to_gn_platform(self.settings.os, self.settings.compiler)),
+        ]
+        if self.settings.build_type in ["Debug", "RelWithDebInfo"]:
+            configure_args.append("-d")
+        save(self, os.path.join(self.source_folder, "configure_args"), " ".join(configure_args))
+
     def build(self):
-        with tools.chdir(self._source_subfolder):
-            with self._build_context():
-                # Generate dummy header to be able to run `build/ben.py` with `--no-last-commit-position`. This allows running the script without the tree having to be a git checkout.
-                tools.save(os.path.join("src", "gn", "last_commit_position.h"),
-                           textwrap.dedent("""\
-                                #pragma once
-                                #define LAST_COMMIT_POSITION "1"
-                                #define LAST_COMMIT_POSITION_NUM 1
-                                """))
-                conf_args = [
-                    "--no-last-commit-position",
-                    "--host={}".format(self._to_gn_platform(self.settings.os, self.settings.compiler)),
-                ]
-                if self.settings.build_type == "Debug":
-                    conf_args.append("-d")
-                self.run("{} build/gen.py {}".format(sys.executable, " ".join(conf_args)), run_environment=True)
-                # Try sleeping one second to avoid time skew of the generated ninja.build file (and having to re-run build/gen.py)
-                time.sleep(1)
-                build_args = [
-                    "-C", "out",
-                    "-j{}".format(tools.cpu_count()),
-                ]
-                self.run("ninja {}".format(" ".join(build_args)), run_environment=True)
+        with chdir(self, self.source_folder):
+            # Generate dummy header to be able to run `build/ben.py` with `--no-last-commit-position`. This allows running the script without the tree having to be a git checkout.
+            save(
+                self,
+                os.path.join("src", "gn", "last_commit_position.h"),
+                textwrap.dedent("""\
+                    #pragma once
+                    #define LAST_COMMIT_POSITION "1"
+                    #define LAST_COMMIT_POSITION_NUM 1
+                    """),
+            )
+            self.run(f"{sys.executable} build/gen.py " + load(self, "configure_args"))
+            # Try sleeping one second to avoid time skew of the generated ninja.build file (and having to re-run build/gen.py)
+            time.sleep(1)
+            build_args = ["-C", "out", f"-j{os.cpu_count()}"]
+            self.run("ninja " + " ".join(build_args))
 
     def package(self):
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
-        self.copy("gn", src=os.path.join(self._source_subfolder, "out"), dst="bin")
-        self.copy("gn.exe", src=os.path.join(self._source_subfolder, "out"), dst="bin")
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "gn",
+            src=os.path.join(self.source_folder, "out"),
+            dst=os.path.join(self.package_folder, "bin"))
+        copy(self,
+            "gn.exe",
+            src=os.path.join(self.source_folder, "out"),
+            dst=os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
-        bin_path = os.path.join(self.package_folder, "bin")
-        self.output.info("Appending PATH environment variable: {}".format(bin_path))
-        self.env_info.PATH.append(bin_path)
         self.cpp_info.includedirs = []
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
+
+        bin_path = os.path.join(self.package_folder, "bin")
+        self.output.info(f"Appending PATH environment variable: {bin_path}")
+        self.env_info.PATH.append(bin_path)
