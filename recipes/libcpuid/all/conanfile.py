7eaf9d172756af8c7b2bdf5529e84d8d1d554055
Martin Valgur <martin.valgur@gmail.com>
2023-07-21 20:36:33 +0300

    libcpuid: move DLLs from lib to bin

--- recipes/libcpuid/all/conanfile.py
+++ recipes/libcpuid/all/conanfile.py
@@ -3,7 +3,7 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, rename
 
 required_conan_version = ">=1.53.0"
 
@@ -74,6 +74,8 @@ class LibCpuidConan(ConanFile):
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        for dll in (self.package_path / "lib").glob("*.dll"):
+            rename(self, dll, self.package_path / "bin" / dll.name)
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "cpuid")
