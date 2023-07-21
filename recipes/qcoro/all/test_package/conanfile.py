2a81c578192c53da194e3df5098d32dad61927c4
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-07-21 08:43:37 +0000

    qcoro/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/qcoro/all/test_package/conanfile.py
+++ recipes/qcoro/all/test_package/conanfile.py
@@ -9,7 +9,7 @@ class TestPackageConan(ConanFile):
     generators = "cmake", "cmake_find_package"
 
     def build_requirements(self):
-        self.build_requires("cmake/3.26.4")
+        self.build_requires("cmake/3.27.0")
 
     def build(self):
         cmake = CMake(self)
