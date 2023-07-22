47785d5416f4dd8c26789265a9b56be8d968f61d
Petukhov Maksim <Petuhov_M@credo-dialogue.com>
2023-07-22 22:31:29 +0300

    transitive headers

--- recipes/qarchive/all/conanfile.py
+++ recipes/qarchive/all/conanfile.py
@@ -49,7 +49,7 @@ class QarchiveConan(ConanFile):
 
     def requirements(self):
         self.requires("libarchive/3.6.2")
-        self.requires("qt/5.15.10")
+        self.requires("qt/5.15.10", transitive_headers=True)
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.17 <4]")
