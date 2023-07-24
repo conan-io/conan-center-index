2913388dafaf2f41d6806af7c8abc247cdff94ba
pwqbot <ding00000804@gmail.com>
2023-07-24 13:17:17 +0800

    compiler check

--- recipes/intx/all/conanfile.py
+++ recipes/intx/all/conanfile.py
@@ -28,6 +28,17 @@ class IntxConan(ConanFile):
     def layout(self):
         basic_layout(self, src_folder="src")
 
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "Visual Studio": "16",
+            "msvc": "192",
+            "gcc": "12",
+            "clang": "15",
+            "apple-clang": "14.1",
+        }
+
+
     def requirements(self):
         pass
 
@@ -37,6 +48,12 @@ class IntxConan(ConanFile):
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
             check_min_cppstd(self, self._min_cppstd)
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
