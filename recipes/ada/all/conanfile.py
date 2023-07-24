218584a0531cb4e778525625ef73e9136a62be43
toge <toge.mail@gmail.com>
2023-07-24 18:52:51 +0900

    ada: add version 2.6.0

--- recipes/ada/all/conanfile.py
+++ recipes/ada/all/conanfile.py
@@ -69,8 +69,7 @@ class AdaConan(ConanFile):
             )
 
     def build_requirements(self):
-        if Version(self.version) >= "0.6.0":
-            self.tool_requires("cmake/[>=3.16 <4]")
+        self.tool_requires("cmake/[>=3.16 <4]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
