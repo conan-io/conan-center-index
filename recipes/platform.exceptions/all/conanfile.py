11b061055cbed0b0cb644846b843e81c633533dd
Daulet <uralskdev@gmail.com>
2023-07-26 22:41:48 +0500

    added platform.exceptions
    Signed-off-by: Daulet <uralskdev@gmail.com>

--- recipes/platform.exceptions/all/conanfile.py
+++ recipes/platform.exceptions/all/conanfile.py
@@ -1,71 +1,86 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import get, copy
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=2.0"
 
 
 class PlatformExceptionsConan(ConanFile):
     name = "platform.exceptions"
-    license = "MIT"
+    license = "Unlicense"
     homepage = "https://github.com/linksplatform/Exceptions"
     url = "https://github.com/conan-io/conan-center-index"
     description = "platform.exceptions is one of the libraries of the LinksPlatform modular framework, " \
                   "to ensure exceptions"
     topics = ("linksplatform", "cpp20", "exceptions", "any", "ranges", "native")
-    settings = "compiler", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _subfolder_sources(self):
+        return os.path.join(self.source_folder, "cpp", "Platform.Exceptions")
 
     @property
-    def _internal_cpp_subfolder(self):
-        return os.path.join(self._source_subfolder, "cpp", "Platform.Exceptions")
+    def _min_cppstd(self):
+        return "20"
 
     @property
     def _compilers_minimum_version(self):
         return {
             "gcc": "10",
             "Visual Studio": "16",
+            "msvc": "193",
             "clang": "11",
-            "apple-clang": "11"
+            "apple-clang": "12"
         }
 
-    @property
-    def _minimum_cpp_standard(self):
-        return 20
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
-    def requirements(self):
-        if tools.Version(self.version) >= "0.3.0":
-            self.requires("platform.delegates/0.2.7")
+    def package_id(self):
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler == "apple-clang" and Version(self.settings.compiler.version) < "12":
+            raise ConanInvalidConfiguration("platform.exceptions/{} requires C++{}, "
+                                            "which is not supported by apple-clang {}.".format(
+                self.version, self._min_cppstd, self.settings.compiler.version))
+
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration("{}/{} requires C++{}, "
+                                            "which is not supported by {} {}.".format(
+                self.name, self.version, self._min_cppstd, self.settings.compiler, self.settings.compiler.version))
+
+    def build_requirements(self):
+        if Version(self.version) >= "0.3.0":
+            self.requires("platform.delegates/0.3.7")
         else:
             self.requires("platform.delegates/0.1.3")
 
-    def validate(self):
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler))
-
-        if not minimum_version:
-            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
-                self.name, self.settings.compiler))
-
-        elif tools.Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration("{}/{} requires c++{}, "
-                                            "which is not supported by {} {}.".format(
-                self.name, self.version, self._minimum_cpp_standard, self.settings.compiler,
-                self.settings.compiler.version))
-
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
-
-    def package_id(self):
-        self.info.header_only()
-
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version]["source"], strip_root=True)
 
     def package(self):
-        self.copy("*.h", dst="include", src=self._internal_cpp_subfolder)
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        copy(
+                self,
+                pattern="*.h",
+                dst=os.path.join(self.package_folder, "include"),
+                src=self.source_folder,
+            )
+        copy(self, pattern="LICENSE", dst="licenses", src=self.source_folder)
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.set_property("cmake_file_name", "Platform.Exceptions")
+        self.cpp_info.set_property("cmake_target_name", "Platform.Exceptions::Platform.Exceptions")
+        self.cpp_info.names["cmake_find_package"] = "Platform.Exceptions"
+        self.cpp_info.names["cmake_find_package_multi"] = "Platform.Exceptions"
+
