68e101019a14d6bc645979eb84f64742a220e0a9
Martin Valgur <martin.valgur@gmail.com>
2023-07-28 16:26:08 +0300

    isa-l: migrate to Conan v2

--- recipes/isa-l/all/conanfile.py
+++ recipes/isa-l/all/conanfile.py
@@ -1,9 +1,13 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.errors import ConanInvalidConfiguration
-
 import os
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.files import chdir, collect_libs, copy, get
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.layout import basic_layout
+
+required_conan_version = ">=1.53.0"
+
 
 class LibisalConan(ConanFile):
     name = "isa-l"
@@ -11,7 +15,9 @@ class LibisalConan(ConanFile):
     license = "BSD-3"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/intel/isa-l"
-    topics = ("isa-l", "compression")
+    topics = "compression"
+
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -19,54 +25,72 @@ class LibisalConan(ConanFile):
     }
     default_options = {
         "shared": False,
-        "fPIC" :True
+        "fPIC": True,
     }
-    build_requires = (
-        "libtool/2.4.6",
-        "nasm/2.15.05",
-    )
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def configure(self):
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def validate(self):
-        if self.settings.arch not in [ "x86", "x86_64", "armv8" ]:
+        if self.settings.arch not in ["x86", "x86_64", "armv8"]:
             raise ConanInvalidConfiguration("CPU Architecture not supported")
         if self.settings.os not in ["Linux", "FreeBSD"]:
             raise ConanInvalidConfiguration("Only Linux and FreeBSD builds are supported")
 
-    def configure(self):
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
-        if self.options.shared:
-            del self.options.fPIC
+    def build_requirements(self):
+        self.tool_requires("libtool/2.4.7")
+        self.tool_requires("nasm/2.15.05")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = AutotoolsToolchain(self)
+        tc.generate()
 
     def build(self):
-        with tools.chdir(self._source_subfolder):
-            self.run("./autogen.sh")
-            env_build = AutoToolsBuildEnvironment(self)
-            extra_args = list()
-            if self.options.shared:
-                extra_args.extend(('--enable-static=no',))
-            else:
-                extra_args.extend(('--enable-shared=no',))
-            env_build.configure(".", args=extra_args, build=False, host=False, target=False)
-            env_build.make()
+        with chdir(self, self.source_folder):
+            autotools = Autotools(self)
+            autotools.autoreconf()
+            autotools.configure()
+            autotools.make()
 
     def package(self):
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
-        self.copy("*/isa-l.h", dst="include/isa-l", keep_path=False)
-        self.copy("*.h", dst="include/isa-l", src="%s/include" % (self._source_subfolder) , keep_path=False)
+        copy(self, "LICENSE",
+             src=self.source_folder,
+             dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*/isa-l.h",
+             src=self.source_folder,
+             dst=os.path.join(self.package_folder, "include/isa-l"),
+             keep_path=False)
+        copy(self, "*.h",
+             dst=os.path.join(self.package_folder, "include/isa-l"),
+             src=os.path.join(self.source_folder, "include"),
+             keep_path=False)
         if self.options.shared:
-            self.copy("*.dll", dst="bin", keep_path=False)
-            self.copy("*.so*", dst="lib", keep_path=False, symlinks=True)
-            self.copy("*.dylib", dst="lib", keep_path=False)
+            copy(self, "*.dll",
+                 dst=os.path.join(self.package_folder, "bin"),
+                 src=self.source_folder,
+                 keep_path=False)
+            copy(self, "*.so*",
+                 dst=os.path.join(self.package_folder, "lib"),
+                 src=self.source_folder,
+                 keep_path=False)
+            copy(self, "*.dylib",
+                 dst=os.path.join(self.package_folder, "lib"),
+                 src=self.source_folder,
+                 keep_path=False)
         else:
-            self.copy("*.a", dst="lib", keep_path=False)
+            copy(self, "*.a",
+                 dst=os.path.join(self.package_folder, "lib"),
+                 src=self.source_folder,
+                 keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
