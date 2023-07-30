23f5c9e318458dc673fc25fc0ab1bc9aa517bad1
klausholstjacobsen <klaus.holst.jacobsen@gmail.com>
2023-07-30 18:50:13 +0200

    rsync: Added rsync package

---
+++ recipes/rsync/all/test_package/conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.errors import ConanException
+import os
+from io import StringIO
+import re
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def test(self):
+        if not can_run(self):
+            return 
+        
+        self.run("rsync --version", env="conanrun")
