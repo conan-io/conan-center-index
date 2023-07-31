a89b52be383d520470e7e58c1f41a3f3b4ad835e
Martin Valgur <martin.valgur@gmail.com>
2023-07-31 15:09:32 +0300

    libid3tag: migrate to Conan v2

--- recipes/libid3tag/all/conanfile.py
+++ recipes/libid3tag/all/conanfile.py
@@ -1,28 +1,38 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild
-from conans.errors import ConanInvalidConfiguration
 import os
-import shutil
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import chdir, copy, get, replace_in_file, rm
+from conan.tools.gnu import Autotools, AutotoolsToolchain, PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import MSBuild, MSBuildToolchain, is_msvc, MSBuildDeps
+
+required_conan_version = ">=1.53.0"
 
 
 class LibId3TagConan(ConanFile):
     name = "libid3tag"
     description = "ID3 tag manipulation library."
-    topics = ("conan", "mad", "id3", "MPEG", "audio", "decoder")
+    license = "GPL-2.0-or-later"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.underbit.com/products/mad/"
-    license = "GPL-2.0-or-later"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    generator = "pkg_config", "visual_studio"
+    topics = ("mad", "id3", "MPEG", "audio", "decoder")
 
-    _autotools = None
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -30,95 +40,99 @@ class LibId3TagConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("zlib/1.2.11")
-
-    @property
-    def _is_msvc(self):
-        return self.settings.compiler == "Visual Studio" or (
-            self.settings.compiler == "clang" and self.settings.os == "Windows"
-        )
+        self.requires("zlib/1.2.13")
 
     def validate(self):
-        if self._is_msvc and self.options.shared:
+        if is_msvc(self) and self.options.shared:
             raise ConanInvalidConfiguration("libid3tag does not support shared library for MSVC")
 
-    @property
-    def _settings_build(self):
-        return getattr(self, "settings_build", self.settings)
-
     def build_requirements(self):
-        if not self._is_msvc:
-            self.build_requires("gnu-config/cci.20201022")
-            if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-                self.build_requires("msys2/cci.latest")
+        if not is_msvc(self):
+            self.tool_requires("gnu-config/cci.20210814")
+            if self._settings_build.os == "Windows":
+                self.win_bash = True
+                if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                    self.tool_requires("msys2/cci.latest")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+        if is_msvc(self):
+            tc = MSBuildToolchain(self)
+            tc.generate()
+            deps = MSBuildDeps(self)
+            deps.generate()
+        else:
+            tc = AutotoolsToolchain(self)
+            tc.generate()
+            deps = PkgConfigDeps(self)
+            deps.generate()
 
     def build(self):
-        if self._is_msvc:
+        if is_msvc(self):
             self._build_msvc()
         else:
             self._build_autotools()
 
     def _build_msvc(self):
         kwargs = {}
-        with tools.chdir(os.path.join(self._source_subfolder, "msvc++")):
+        with chdir(self, os.path.join(self.source_folder, "msvc++")):
             # cl : Command line error D8016: '/ZI' and '/Gy-' command-line options are incompatible
-            tools.replace_in_file("libid3tag.dsp", "/ZI ", "")
+            replace_in_file(self, "libid3tag.dsp", "/ZI ", "")
             if self.settings.compiler == "clang":
-                tools.replace_in_file("libid3tag.dsp", "CPP=cl.exe", "CPP=clang-cl.exe")
-                tools.replace_in_file("libid3tag.dsp", "RSC=rc.exe", "RSC=llvm-rc.exe")
+                replace_in_file(self, "libid3tag.dsp", "CPP=cl.exe", "CPP=clang-cl.exe")
+                replace_in_file(self, "libid3tag.dsp", "RSC=rc.exe", "RSC=llvm-rc.exe")
                 kwargs["toolset"] = "ClangCl"
             if self.settings.arch == "x86_64":
-                tools.replace_in_file("libid3tag.dsp", "Win32", "x64")
-            with tools.vcvars(self.settings):
-                self.run("devenv /Upgrade libid3tag.dsp")
+                replace_in_file(self, "libid3tag.dsp", "Win32", "x64")
+            self.run("devenv /Upgrade libid3tag.dsp")
             msbuild = MSBuild(self)
-            msbuild.build(project_file="libid3tag.vcxproj", **kwargs)
-
-    def _configure_autotools(self):
-        if not self._autotools:
-            if self.options.shared:
-                args = ["--disable-static", "--enable-shared"]
-            else:
-                args = ["--disable-shared", "--enable-static"]
-            self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-            self._autotools.configure(args=args, configure_dir=self._source_subfolder)
-        return self._autotools
-
-    @property
-    def _user_info_build(self):
-        return getattr(self, "user_info_build", self.deps_user_info)
+            msbuild.build(sln="libid3tag.vcxproj", **kwargs)
 
     def _build_autotools(self):
-        shutil.copy(self._user_info_build["gnu-config"].CONFIG_SUB,
-                    os.path.join(self._source_subfolder, "config.sub"))
-        shutil.copy(self._user_info_build["gnu-config"].CONFIG_GUESS,
-                    os.path.join(self._source_subfolder, "config.guess"))
-        autotools = self._configure_autotools()
-        autotools.make()
+        for gnu_config in [
+            self.conf.get("user.gnu-config:config_guess", check_type=str),
+            self.conf.get("user.gnu-config:config_sub", check_type=str),
+        ]:
+            if gnu_config:
+                copy(self, os.path.basename(gnu_config), src=os.path.dirname(gnu_config), dst=self.source_folder)
+        with chdir(self, self.source_folder):
+            autotools = Autotools(self)
+            autotools.configure()
+            autotools.make()
 
     def _install_autotools(self):
-        autotools = self._configure_autotools()
-        autotools.install()
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
+        with chdir(self, self.source_folder):
+            autotools = Autotools(self)
+            autotools.install()
+        rm(self, "*.la", self.package_folder, recursive=True)
 
     def package(self):
-        self.copy("COPYRIGHT", dst="licenses", src=self._source_subfolder)
-        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
-        self.copy("CREDITS", dst="licenses", src=self._source_subfolder)
-        if self._is_msvc:
-            self.copy(pattern="*.lib", dst="lib", src=self._source_subfolder, keep_path=False)
-            self.copy(pattern="id3tag.h", dst="include", src=self._source_subfolder)
+        for license_file in ["COPYRIGHT", "COPYING", "CREDITS"]:
+            copy(self, license_file,
+                 dst=os.path.join(self.package_folder, "licenses"),
+                 src=self.source_folder)
+        if is_msvc(self):
+            copy(self, "*.lib",
+                 dst=os.path.join(self.package_folder, "lib"),
+                 src=self.source_folder,
+                 keep_path=False)
+            copy(self, "id3tag.h",
+                 dst=os.path.join(self.package_folder, "include"),
+                 src=self.source_folder)
         else:
             self._install_autotools()
 
     def package_info(self):
-        self.cpp_info.libs = ["libid3tag" if self._is_msvc else "id3tag"]
+        self.cpp_info.libs = ["libid3tag" if is_msvc(self) else "id3tag"]
