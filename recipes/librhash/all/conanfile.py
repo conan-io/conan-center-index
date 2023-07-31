00563e9c7f0c3c7ca2d8d1a9627dfcd38eeaa795
Martin Valgur <martin.valgur@gmail.com>
2023-07-31 15:09:38 +0300

    librhash: migrate to Conan v2

--- recipes/librhash/all/conanfile.py
+++ recipes/librhash/all/conanfile.py
@@ -1,17 +1,25 @@
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import apply_conandata_patches, chdir, copy, export_conandata_patches, get, rmdir
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, unix_path
+
+required_conan_version = ">=1.53.0"
 
 
 class LibRHashConan(ConanFile):
     name = "librhash"
     description = "Great utility for computing hash sums"
-    topics = ("rhash", "hash", "checksum")
+    license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "http://rhash.sourceforge.net/"
-    license = "MIT"
+    topics = ("rhash", "hash", "checksum")
+
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -24,16 +32,12 @@ class LibRHashConan(ConanFile):
         "with_openssl": True,
     }
 
-    exports_sources = "patches/*"
-    _autotools = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
     @property
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -41,81 +45,84 @@ class LibRHashConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.cppstd
-        del self.settings.compiler.libcxx
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def requirements(self):
         if self.options.with_openssl:
-            self.requires("openssl/1.1.1q")
-
-    def build_requirements(self):
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/cci.latest")
+            self.requires("openssl/[>=1.1 <4]")
 
     def validate(self):
-        if self.settings.compiler == "Visual Studio":
+        if is_msvc(self):
             raise ConanInvalidConfiguration("Visual Studio is not supported")
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+    def build_requirements(self):
+        if self._settings_build.os == "Windows":
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                self.tool_requires("msys2/cci.latest")
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        if self.settings.compiler in ("apple-clang", ):
-            if self.settings.arch in ("armv7", ):
-                self._autotools.link_flags.append("-arch armv7")
-            elif self.settings.arch in ("armv8", ):
-                self._autotools.link_flags.append("-arch arm64")
-        vars = self._autotools.vars
-        conf_args = [
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+
+        tc = AutotoolsToolchain(self)
+        if self.settings.compiler in ("apple-clang",):
+            if self.settings.arch == "armv7":
+                tc.build_type_link_flags.append("-arch armv7")
+            elif self.settings.arch == "armv8":
+                tc.build_type_link_flags.append("-arch arm64")
+        tc.configure_args = [
             # librhash's configure script does not understand `--enable-opt1=yes`
-            "--enable-openssl" if self.options.with_openssl else "--disable-openssl",
+            "--{}-openssl".format("enable" if self.options.with_openssl else "disable"),
             "--disable-gettext",
             # librhash's configure script is custom and does not understand "--bindir=${prefix}/bin" arguments
-            "--prefix={}".format(tools.unix_path(self.package_folder)),
-            "--bindir={}".format(tools.unix_path(os.path.join(self.package_folder, "bin"))),
-            "--libdir={}".format(tools.unix_path(os.path.join(self.package_folder, "lib"))),
-            # the configure script does not use CPPFLAGS, so add it to CFLAGS/CXXFLAGS
-            "--extra-cflags={}".format("{} {}".format(vars["CFLAGS"], vars["CPPFLAGS"])),
-            "--extra-ldflags={}".format(vars["LDFLAGS"]),
+            f"--prefix={unix_path(self, self.package_folder)}",
+            f"--bindir=/bin",
+            f"--libdir=/lib",
         ]
         if self.options.shared:
-            conf_args.extend(["--enable-lib-shared", "--disable-lib-static"])
+            tc.configure_args += ["--enable-lib-shared", "--disable-lib-static"]
         else:
-            conf_args.extend(["--disable-lib-shared", "--enable-lib-static"])
+            tc.configure_args += ["--disable-lib-shared", "--enable-lib-static"]
+        tc.generate()
 
-        with tools.environment_append({
-            "BUILD_TARGET": tools.get_gnu_triplet(str(self.settings.os), str(self.settings.arch), str(self.settings.compiler)),
-        }):
-            self._autotools.configure(args=conf_args, use_default_install_dirs=False, build=False, host=False)
-        return self._autotools
+        deps = AutotoolsDeps(self)
+        deps.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        with tools.chdir(self._source_subfolder):
-            autotools = self._configure_autotools()
+        apply_conandata_patches(self)
+        with chdir(self, self.source_folder):
+            autotools = Autotools(self)
+            autotools.configure()
             autotools.make()
 
     def package(self):
-        self.copy("COPYING", src=self._source_subfolder, dst="licenses")
-        with tools.chdir(self._source_subfolder):
-            autotools = self._configure_autotools()
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        with chdir(self, self.source_folder):
+            autotools = Autotools(self)
             autotools.install()
             autotools.make(target="install-lib-headers")
-            with tools.chdir("librhash"):
+            with chdir(self, "librhash"):
                 if self.options.shared:
                     autotools.make(target="install-so-link")
-        tools.rmdir(os.path.join(self.package_folder, "bin"))
-        tools.rmdir(os.path.join(self.package_folder, "etc"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        for path in self.package_path.iterdir():
+            if path.is_dir() and path.name not in ["include", "lib", "licenses"]:
+                rmdir(self, path)
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "LibRHash")
+        self.cpp_info.set_property("cmake_target_name", "LibRHash::LibRHash")
+        self.cpp_info.set_property("pkg_config_name", "librhash")
+        self.cpp_info.libs = ["rhash"]
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.names["cmake_find_package"] = "LibRHash"
         self.cpp_info.names["cmake_find_package_multi"] = "LibRHash"
-        self.cpp_info.names["pkg_config"] = "librhash"
-        self.cpp_info.libs = ["rhash"]
