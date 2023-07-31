fb530d1ee1a9846b81e91f59efd662edfdec5573
klausholstjacobsen <klaus.holst.jacobsen@gmail.com>
2023-07-31 19:03:55 +0200

    net-snmp: Updated package for conan 2.0 compliance

--- recipes/net-snmp/all/test_package/conanfile.py
+++ recipes/net-snmp/all/test_package/conanfile.py
@@ -1,11 +1,16 @@
 import os
-
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build_requirements(self):
         if self.settings.os == "Macos" and self.settings.arch == "armv8":
@@ -15,12 +20,18 @@ class TestPackageConan(ConanFile):
             # because CMake's platform configuration is corrupt.
             self.build_requires("cmake/3.20.1")
 
+    def layout(self):
+        cmake_layout(self)
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if not can_run(self):
+            return
+
+
+        bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+        self.run(bin_path, env="conanrun")
