ca587a92109429ae2e004d440ba1520b4156a29f
klausholstjacobsen <klaus.holst.jacobsen@gmail.com>
2023-08-03 13:11:26 +0200

    openpam: Added openpam recipe

---
+++ recipes/openpam/all/test_package/test_package.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <security/pam_types.h>
+#include <security/openpam.h>
+#include <security/pam_constants.h>
+
+
+int main(void) {
+
+    int x = PAM_SUCCESS;
+
+    return EXIT_SUCCESS;
+}
