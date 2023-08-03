07cd5e2603a588567c7ea8436d39d757bdc79b07
pwqbot <ding00000804@gmail.com>
2023-08-03 16:54:53 +0800

    add evmc

---
+++ recipes/evmc/all/conanfile.py
@@ -0,0 +1,67 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMakeToolchain, CMake
+from conan.tools.files import get, copy
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.layout import basic_layout
+import os
+
+required_conan_version = ">=1.51.1"
+
+class EVMCConan(ConanFile):
+    name = "evmc"
+    description = "EVMC – Ethereum Client-VM Connector API"
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/ethereum/evmc"
+    topics = ("ethereum", "wasm", "evm")
+    package_type = "library"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    @property
+    def _min_cppstd(self):
+        return 17
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "Visual Studio": "16",
+            "msvc": "192",
+            "gcc": "8",
+            "clang": "9",
+            "apple-clang": "14.1",
+        }
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def validate(self):
+        if self.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
+        cmake.install()
---
+++ recipes/evmc/all/test_package/test_package.cpp
@@ -0,0 +1,13 @@
+#include <evmc/evmc.h>
+#include <evmc/evmc.hpp>
+#include <evmc/filter_iterator.hpp>
+#include <evmc/helpers.h>
+#include <evmc/hex.hpp>
+#include <evmc/instructions.h>
+#include <evmc/loader.h>
+#include <evmc/mocked_host.hpp>
+#include <evmc/utils.h>
+
+int main(int argc, char **) {
+    return 0;
+}
