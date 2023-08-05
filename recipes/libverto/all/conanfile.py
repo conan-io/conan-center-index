cb16de0978815084cb0aff07d5eed90e8e3e0122
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-08-05 16:34:42 +0000

    libverto/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libverto/all/conanfile.py
+++ recipes/libverto/all/conanfile.py
@@ -75,7 +75,7 @@ class LibVertoConan(ConanFile):
 
     def requirements(self):
         if self.options.with_glib:
-            self.requires("glib/2.77.0")
+            self.requires("glib/2.77.1")
         if self.options.with_libevent:
             self.requires("libevent/2.1.12")
         if self.options.with_libev:
