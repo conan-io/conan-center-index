1617df4150c61a6166f078453b3ebc7acadb36a6
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-08-05 16:31:47 +0000

    keychain/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/keychain/all/conanfile.py
+++ recipes/keychain/all/conanfile.py
@@ -34,7 +34,7 @@ class KeychainConan(ConanFile):
     def requirements(self):
         if self.settings.os == "Linux":
             self.requires("libsecret/0.20.5")
-            self.requires("glib/2.77.0")
+            self.requires("glib/2.77.1")
 
     def build_requirements(self):
         if self.settings.os == "Linux":
