fdbc8c0554f3e49b21c133e3bf65fb266c62ae1a
Martin Valgur <martin.valgur@gmail.com>
2023-08-06 21:39:57 +0300

    ignition-utils: add "gazebo" tag

--- recipes/ignition-utils/all/conanfile.py
+++ recipes/ignition-utils/all/conanfile.py
@@ -18,7 +18,7 @@ class IgnitionUitlsConan(ConanFile):
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://gazebosim.org/libs/utils"
-    topics = ("ignition", "robotics", "utils")
+    topics = ("ignition", "robotics", "utils", "gazebo")
 
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
