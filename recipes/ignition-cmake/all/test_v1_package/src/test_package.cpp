7824411a99351b44501287814fda1c575a1cc1d3
Martin Valgur <martin.valgur@gmail.com>
2023-08-06 21:26:54 +0300

    ignition-cmake: remove redundant test sources

--- recipes/ignition-cmake/all/test_v1_package/src/test_package.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <ignition/utilities/SuppressWarning.hh>
-#include <ignition/utilities/ExtraTestMacros.hh>
-
-int main() {
-    std::cout << "Hello world!\n";
-    return 0;
-}
