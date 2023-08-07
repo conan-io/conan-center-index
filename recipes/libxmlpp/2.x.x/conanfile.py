c980f17274b826c7330680dd3d6c6a24c145d47f
Martin Valgur <martin.valgur@gmail.com>
2023-08-07 14:26:21 +0300

    libxmlpp: bump deps

--- recipes/libxmlpp/2.x.x/conanfile.py
+++ recipes/libxmlpp/2.x.x/conanfile.py
@@ -57,8 +57,9 @@ class LibXMLPlusPlus(ConanFile):
             check_min_cppstd(self, 11)
 
     def build_requirements(self):
-        self.tool_requires("meson/1.1.1")
-        self.tool_requires("pkgconf/1.9.3")
+        self.tool_requires("meson/1.2.0")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/1.9.5")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
