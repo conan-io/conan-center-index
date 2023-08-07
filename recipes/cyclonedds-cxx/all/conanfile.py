8d39a29f88dcb7ef6a50e50f1d7951d0b5d5473d
Wade Hunkapiller <wade.hunk@gmail.com>
2023-08-07 17:08:16 -0500

    Tweak

--- recipes/cyclonedds-cxx/all/conanfile.py
+++ recipes/cyclonedds-cxx/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir, replace_in_file
+from conan.tools.files import copy, export_conandata_patches, get, rm, rmdir, replace_in_file
 from conan.tools.scm import Version
 import os
 
--- recipes/cyclonedds-cxx/all/test_package/test_package.cpp
+++ recipes/cyclonedds-cxx/all/test_package/test_package.cpp
@@ -4,12 +4,10 @@
 
 int main() {
   dds_entity_t participant;
-
   dds::domain::DomainParticipant domain_(0);
 
   conan::Message message;
   std::vector<unsigned char> payload;
   message.payload(std::move(payload));
-
   return 0;
 }
