e24cb0e714d1918ebfff815285104e2e55c5a3a2
Martin Valgur <martin.valgur@gmail.com>
2023-08-07 14:26:21 +0300

    libusb-compat: bump deps

--- recipes/libusb-compat/all/conanfile.py
+++ recipes/libusb-compat/all/conanfile.py
@@ -65,7 +65,8 @@ class LibUSBCompatConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("gnu-config/cci.20210814")
         self.tool_requires("libtool/2.4.7")
-        self.tool_requires("pkgconf/1.9.3")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/2.0.2")
         if self._settings_build.os == "Windows":
             self.win_bash = True
             if not self.conf.get("tools.microsoft.bash:path", check_type=str):
