5faf2c32eaec104eafdf4d998e7f24458a06f2ed
toge <toge.mail@gmail.com>
2023-08-08 23:46:13 +0900

    fix indent

--- recipes/libraw/all/conanfile.py
+++ recipes/libraw/all/conanfile.py
@@ -65,7 +65,7 @@ class LibRawConan(ConanFile):
                 check_max_cppstd(self, 11)
 
     def source(self):
-       get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
