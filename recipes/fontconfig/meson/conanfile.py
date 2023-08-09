42f00d8a4e3a59faf3fd765d1ff970040a9f9c56
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-08-09 01:32:14 +0000

    fontconfig/meson: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/fontconfig/meson/conanfile.py
+++ recipes/fontconfig/meson/conanfile.py
@@ -56,7 +56,7 @@ class FontconfigConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("gperf/3.1")
-        self.tool_requires("meson/1.2.0")
+        self.tool_requires("meson/1.2.1")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/1.9.5")
 
