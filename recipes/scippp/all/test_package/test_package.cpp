9e96a2d79dc61109ba767fadb05324a80d7fe859
Ivo Hedtke <ivo.hedtke@dbschenker.com>
2023-08-10 15:19:47 +0200

    Output version in test

--- recipes/scippp/all/test_package/test_package.cpp
+++ recipes/scippp/all/test_package/test_package.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
 #include <scippp/model.hpp>
+#include <scippp/version.hpp>
 using namespace scippp;
 int main() {
+    std::cout << "This is SCIP++ version " << scippp::getVersion() << std::endl;
     Model model("Simple");
     auto x1 = model.addVar("x_1", 1);
     auto x2 = model.addVar("x_2", 1);
