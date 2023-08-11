4b2c2b54b78eac858e21968b60d93876eb8b6459
Andrei Malashkin <andrei@vikingsoftware.com>
2023-08-11 10:15:31 +0200

    add forgotten import

--- recipes/newmat/all/conanfile.py
+++ recipes/newmat/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, get, load, collect_libs, rmdir
 import os
