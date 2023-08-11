d7bcca58805f29c21cf6718aaa2ee155a8911911
Rubén Rincón Blanco <git@rinconblanco.es>
2023-08-11 13:58:25 +0200

    Use proper conan min version check

--- recipes/at-spi2-core/new/conanfile.py
+++ recipes/at-spi2-core/new/conanfile.py
@@ -9,7 +9,7 @@ from conan.tools.scm import Version
 import os
 
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=1.60.0 <2.0 || >=2.0.5"
 class AtSpi2CoreConan(ConanFile):
     name = "at-spi2-core"
     description = "It provides a Service Provider Interface for the Assistive Technologies available on the GNOME platform and a library against which applications can be linked"
