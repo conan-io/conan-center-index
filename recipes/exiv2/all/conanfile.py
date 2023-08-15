4c366aa99a38f1b9eb9791b320efbbe73aa7764b
Maksim Petukhov <6967052+maksim-petukhov@users.noreply.github.com>
2023-08-15 14:29:13 +0300

    Update recipes/exiv2/all/conanfile.py
    Co-authored-by: Matthieu Darbois <mayeut@users.noreply.github.com>

--- recipes/exiv2/all/conanfile.py
+++ recipes/exiv2/all/conanfile.py
@@ -73,7 +73,7 @@ class Exiv2Conan(ConanFile):
         if self.options.with_xmp == "bundled":
             self.requires("expat/2.5.0")
         if self.options.with_curl:
-            self.requires("libcurl/8.1.2")
+            self.requires("libcurl/8.2.1")
         if self.options.get_safe("with_brotli"):
             self.requires("brotli/1.0.9")
         if self.options.get_safe("with_inih"):
