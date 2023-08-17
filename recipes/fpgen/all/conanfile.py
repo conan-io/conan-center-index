8231b1070f0ad1363c9aa1023282cebc2120de20
Martin Valgur <martin.valgur@gmail.com>
2023-08-17 15:06:27 +0300

    fpgen: remove unused imports

--- recipes/fpgen/all/conanfile.py
+++ recipes/fpgen/all/conanfile.py
@@ -3,9 +3,8 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import copy, get, replace_in_file
+from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 
 required_conan_version = ">=1.52.0"
 
