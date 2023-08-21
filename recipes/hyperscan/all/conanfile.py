2aeae89d874000594340eb3d995197524381b42f
toge <toge.mail@gmail.com>
2023-08-21 14:45:17 +0900

    update boost/1.83.0

--- recipes/hyperscan/all/conanfile.py
+++ recipes/hyperscan/all/conanfile.py
@@ -58,7 +58,7 @@ class HyperscanConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.82.0")
+        self.requires("boost/1.83.0")
         if self.options.build_chimera:
             self.requires("pcre/8.45")
 
