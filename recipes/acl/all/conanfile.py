6a5ba04256dae6b18786d468bdf3c94d27ca3903
Wade Hunkapiller <wade.hunk@gmail.com>
2023-08-22 15:17:52 -0500

    Add cmake targets

--- recipes/acl/all/conanfile.py
+++ recipes/acl/all/conanfile.py
@@ -71,4 +71,10 @@ class AclConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "libacl")
+        self.cpp_info.set_property("cmake_file_name", "acl")
+        self.cpp_info.set_property("cmake_target_name", "acl::acl")
+        self.cpp_info.set_property("cmake_target_aliases", ["acl"])
         self.cpp_info.libs = ["acl"]
+        # TODO: to remove in conan v2
+        self.cpp_info.names["cmake_find_package"] = "acl"
+        self.cpp_info.names["cmake_find_package_multi"] = "acl"
