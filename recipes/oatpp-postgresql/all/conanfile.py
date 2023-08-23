9c98c8abc53fd104c9238b752132283701280a25
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-08-23 09:33:49 +0000

    oatpp-postgresql/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/oatpp-postgresql/all/conanfile.py
+++ recipes/oatpp-postgresql/all/conanfile.py
@@ -47,7 +47,7 @@ class OatppPostgresqlConan(ConanFile):
 
     def requirements(self):
         self.requires(f"oatpp/{self.version}")
-        self.requires("libpq/15.3")
+        self.requires("libpq/15.4")
 
     def validate(self):
         if self.info.settings.compiler.get_safe("cppstd"):
