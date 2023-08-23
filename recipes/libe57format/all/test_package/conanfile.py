3a80fd6bae6418a27aa275d48d19b72dc2294326
Martin Liska <martin.liska@melowntech.com>
2023-08-23 19:01:33 +0200

    disable tests in version 3.0.0+

--- recipes/libe57format/all/test_package/conanfile.py
+++ recipes/libe57format/all/test_package/conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.scm import Version
 import os
 
 
@@ -22,5 +23,9 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, env="conanrun")
+            tested_version = self.tested_reference_str.split('/')[1].split('@')[0]
+            if Version(tested_version) >= Version("3.0.0"):
+                print(f'Skipping tests in version {tested_version}')
+            else:
+                bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+                self.run(bin_path, env="conanrun")
