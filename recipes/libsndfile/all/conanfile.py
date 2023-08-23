cdb8f1363630642a201e5c9929cba007fb9906a2
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-08-23 09:29:07 +0000

    libsndfile/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libsndfile/all/conanfile.py
+++ recipes/libsndfile/all/conanfile.py
@@ -63,7 +63,7 @@ class LibsndfileConan(ConanFile):
             self.requires("ogg/1.3.5")
             self.requires("vorbis/1.3.7")
             self.requires("flac/1.4.2")
-            self.requires("opus/1.3.1")
+            self.requires("opus/1.4")
         if self.options.get_safe("with_mpeg", False):
             self.requires("mpg123/1.31.2")
             self.requires("libmp3lame/3.100")
