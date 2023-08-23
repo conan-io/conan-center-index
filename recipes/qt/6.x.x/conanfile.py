6f7ee59863a27e6a3596b227e705f3ecd0399c8a
Jordan Williams <jordan@jwillikers.com>
2023-08-23 07:44:50 -0500

    Disable Qt 6.5.0 with GCC 9 in C3I

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -237,6 +237,9 @@ class QtConan(ConanFile):
                 self.info.settings.compiler == "clang" and Version(self.info.settings.compiler.version) >= "12":
                 raise ConanInvalidConfiguration("qt is not supported on gcc11 and clang >= 12 on C3I until conan-io/conan-center-index#13472 is fixed\n"\
                                                 "If your distro is modern enough (xcb >= 1.12), set environment variable NOT_ON_C3I=1")
+            if Version(self.version) >= "6.5.0" and self.settings.compiler == "gcc" and Version(self.settings.compiler.version) == "9":
+                raise ConanInvalidConfiguration("qt 6.5.0 and newer is not supported with gcc 9 on C3I until gcc 9.3 or newer is used, cf QTBUG-112920\n"\
+                                                "If your using gcc 9.3 or newer, set environment variable NOT_ON_C3I=1")
 
         # C++ minimum standard required
         if self.settings.compiler.get_safe("cppstd"):
