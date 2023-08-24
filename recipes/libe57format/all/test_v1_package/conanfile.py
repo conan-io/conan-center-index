4b13d73f788d15e66cf2f442ba8ae70f96581680
Martin Liska <martin.liska@melowntech.com>
2023-08-24 21:40:39 +0200

    skip tests in conan 1.x

--- recipes/libe57format/all/test_v1_package/conanfile.py
+++ recipes/libe57format/all/test_v1_package/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+from conans.tools import Version
 import os
 
 
@@ -12,6 +13,11 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
+        tested_version = self.tested_reference_str.split('/')[1].split('@')[0].split('#')[0]
+        if Version(tested_version) >= Version("3.0.0"):
+            print(f'Skipping tests in version {tested_version}')
+            return
+
         if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
