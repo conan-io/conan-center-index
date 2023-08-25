9e4487cc1630958ab9ed292bbd8eafb7bc7414bd
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-08-25 14:43:18 +0000

    qt/6.x.x: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -324,7 +324,7 @@ class QtConan(ConanFile):
         if self.options.get_safe("with_libpng", False) and not self.options.multiconfiguration:
             self.requires("libpng/1.6.40")
         if self.options.with_sqlite3 and not self.options.multiconfiguration:
-            self.requires("sqlite3/3.42.0")
+            self.requires("sqlite3/3.43.0")
         if self.options.get_safe("with_mysql", False):
             self.requires("libmysqlclient/8.1.0")
         if self.options.with_pq:
