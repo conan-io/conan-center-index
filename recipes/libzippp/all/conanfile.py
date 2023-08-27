84dbdbfb37a39ebb99e9d7c903e4f1beea04d76e
toge <toge.mail@gmail.com>
2023-08-27 22:37:43 +0900

    add build_require for 6.1

--- recipes/libzippp/all/conanfile.py
+++ recipes/libzippp/all/conanfile.py
@@ -64,6 +64,10 @@ class LibZipppConan(ConanFile):
         if self.settings.compiler == "clang" and self.settings.compiler.get_safe("libcxx") == "libc++":
             raise ConanInvalidConfiguration(f"{self.ref} does not support clang with libc++. Use libstdc++ instead.")
 
+    def build_requirements(self):
+        if Version(self.version) >= "6.1":
+            self.tool_requires("cmake/[>=3.16 <4]")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
