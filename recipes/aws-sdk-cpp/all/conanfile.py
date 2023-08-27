5a98674b9d8a9c54cdea8c6f0dba2765e99c66cc
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-08-28 01:43:22 +0200

    bump libcurl

--- recipes/aws-sdk-cpp/all/conanfile.py
+++ recipes/aws-sdk-cpp/all/conanfile.py
@@ -351,7 +351,7 @@ class AwsSdkCppConan(ConanFile):
             self.requires("aws-crt-cpp/0.17.23", transitive_headers=True)
         if self.settings.os != "Windows":
             self.requires("openssl/[>=1.1 <4]")
-            self.requires("libcurl/8.0.1")
+            self.requires("libcurl/8.2.1")
         if self.settings.os in ["Linux", "FreeBSD"]:
             if self.options.get_safe("text-to-speech"):
                 self.requires("pulseaudio/14.2")
