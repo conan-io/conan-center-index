42e2d2e7302eba944909da23a04aebaa3eab0fcc
toge <toge.mail@gmail.com>
2023-08-27 22:39:08 +0900

    import sys

--- recipes/libraw/all/conanfile.py
+++ recipes/libraw/all/conanfile.py
@@ -4,6 +4,7 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.scm import Version
 import os
+import sys
 
 required_conan_version = ">=1.53.0"
 
