b4c23acddaa0dd56088003b3c53890d0d3954a73
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2023-08-28 09:51:53 +0200

    Add ohNet test package

--- recipes/ohnet/all/conanfile.py
+++ recipes/ohnet/all/conanfile.py
@@ -1,7 +1,9 @@
 from conan import ConanFile
-from conan.tools.files import get, chdir
+from conan.tools.files import get, chdir, copy, mkdir
+from conan.tools.apple import is_apple_os
 from conan.tools.gnu import Autotools, AutotoolsToolchain
-
+from conan.tools.microsoft import is_msvc, NMakeToolchain
+import os
 
 required_conan_version = ">=1.53.0"
 
@@ -24,23 +26,68 @@ class OhNetConan(ConanFile):
         "fPIC": True,
     }
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
+    def _get_openhome_architecture(self, args):
+        if is_apple_os(self):
+            if str(self.settings.arch).startswith("armv8"):
+                openhome_architecture = "arm64"
+                args.extend([f"openhome_architecture={openhome_architecture}", f"detected_openhome_architecture={openhome_architecture}"])
+        return args
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        tc = AutotoolsToolchain(self)
-        tc.make_args.append("-j1")
-        tc.generate()
+        if is_msvc(self):
+            tc = NMakeToolchain(self)
+            tc.generate()
+        else:
+            tc = AutotoolsToolchain(self)
+            tc.make_args.append("-j1")
+            tc.generate()
 
     def build(self):
+        targets = "ohNetDll TestsNative proxies devices"
+
         with chdir(self, self.source_folder):
-            autotools = Autotools(self)
-            autotools.make(target="ohNetDll TestsNative proxies devices")
+            if is_msvc(self):
+                self.run(f"nmake /f {targets}")
+            else:
+                args = []
+                args = self._get_openhome_architecture(args)
+                autotools = Autotools(self)
+                autotools.make(args=args, target=targets)
+
+    def package(self):
+        installlibdir = os.path.join(self.package_folder, "lib")
+        installincludedir = os.path.join(self.package_folder, "include")
+
+        copy(self, "License.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+
+        copy(self, "*", src=os.path.join(self.source_folder, "OpenHome", "Net", "ServiceGen"), dst=os.path.join(self.package_folder, "lib", "ServiceGen"))
+        mkdir(self, os.path.join(self.package_folder, "lib", "t4"))
+
+        with chdir(self, self.source_folder):
+            if is_msvc(self):
+                self.run(f"nmake /f OhNet.mak install installdir={self.package_folder} installlibdir={installlibdir} installincludedir={installincludedir}")
+            else:
+                args = [f"prefix={self.package_folder}", f"installlibdir={installlibdir}", f"installincludedir={installincludedir}"]
+                args = self._get_openhome_architecture(args)
+                autotools = Autotools(self)
+                autotools.make(args=args, target="install-libs install-includes")
+
+    def package_info(self):
+        self.cpp_info.components["ohNet"].libs = ["ohNet"]
+        self.cpp_info.components["ohNet"].set_property("cmake_target_name", "ohNet")
+
+        self.cpp_info.components["ohNetCore"].libs = ["ohNetCore"]
+        self.cpp_info.components["ohNetCore"].frameworks.extend(["CoreFoundation", "IOKit", "SystemConfiguration"])
+        self.cpp_info.components["ohNetCore"].set_property("cmake_target_name", "ohNetCore")
+
+        self.cpp_info.components["ohNetDevices"].libs = ["ohNetDevices"]
+        self.cpp_info.components["ohNetDevices"].set_property("cmake_target_name", "ohNetDevices")
+
+        self.cpp_info.components["ohNetProxies"].libs = ["ohNetProxies"]
+        self.cpp_info.components["ohNetProxies"].set_property("cmake_target_name", "ohNetProxies")
+
+        self.cpp_info.components["TestFramework"].libs = ["TestFramework"]
+        self.cpp_info.components["TestFramework"].set_property("cmake_target_name", "TestFramework")
