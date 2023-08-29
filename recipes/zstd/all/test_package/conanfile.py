786a3f7f15a0836dd1050b3536d6f8ba07c37dc2
klausholstjacobsen <klaus.holst.jacobsen@gmail.com>
2023-08-29 11:42:05 +0200

    Updated test package to use build_env instead of run_env to run the newly built ztsd executable

--- recipes/zstd/all/test_package/conanfile.py
+++ recipes/zstd/all/test_package/conanfile.py
@@ -1,14 +1,12 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
-from io import StringIO
-from conan.tools.files import load, save
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualBuildEnv", "VirtualRunEnv"
     test_type = "explicit"
 
     def layout(self):
@@ -17,10 +15,8 @@ class TestPackageConan(ConanFile):
     def requirements(self):
         self.requires(self.tested_reference_str)
 
-    def generate(self):
-        zstd_version = self.dependencies["zstd"].ref.version
-        save(self, "zstd_version", f"{zstd_version}")
-
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -35,10 +31,4 @@ class TestPackageConan(ConanFile):
         png_file = os.path.join(self.source_folder, "logo.png")
         self.run(f"{bin_path} {png_file}", env="conanrun")
 
-        zstd_version = load(self, os.path.join(self.generators_folder, "zstd_version"))
-
-        output = StringIO()
-        self.run(f"zstd --version", output, env="conanrun")
-        output_str = str.strip(output.getvalue())
-        assert zstd_version in output_str, f"zstd command output '{output_str}' should contain version string '{zstd_version}'"
-        self.output.info(f"Version verified: '{zstd_version}'")
+        self.run(f"zstd --version")
