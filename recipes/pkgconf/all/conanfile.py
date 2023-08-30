1893f9e1c03652d1a0f709d5367df9ef9c66b111
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-08-30 11:42:38 +0200

    bump meson

--- recipes/pkgconf/all/conanfile.py
+++ recipes/pkgconf/all/conanfile.py
@@ -52,7 +52,7 @@ class PkgConfConan(ConanFile):
         self.settings.rm_safe("compiler.cppstd")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.0.0")
+        self.tool_requires("meson/1.2.1")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
