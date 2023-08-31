563dd3eb2b64aaaf118831f9f028164d22f86831
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-08-31 10:25:14 +0200

    libnice: bump glib

--- recipes/libnice/all/conanfile.py
+++ recipes/libnice/all/conanfile.py
@@ -59,7 +59,7 @@ class LibniceConan(ConanFile):
                 "-o glib/*:shared=True with static runtime is not supported")
 
     def requirements(self):
-        self.requires("glib/2.75.2")
+        self.requires("glib/2.77.2")
         if self.options.crypto_library == "openssl":
             self.requires("openssl/1.1.1t")
         if self.options.with_gstreamer:
@@ -68,7 +68,7 @@ class LibniceConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("meson/1.0.0")
         self.tool_requires("pkgconf/1.9.3")
-        self.tool_requires("glib/2.75.2")  # for glib-mkenums
+        self.tool_requires("glib/2.77.2")  # for glib-mkenums
         if self.options.with_introspection:
             self.tool_requires("gobject-introspection/1.72.0")
 
