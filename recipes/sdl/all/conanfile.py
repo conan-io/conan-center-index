f57dc96d0b9ed8c984985bb3652a009c55c31d7d
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-09-01 20:40:54 +0000

    sdl/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/sdl/all/conanfile.py
+++ recipes/sdl/all/conanfile.py
@@ -186,7 +186,7 @@ class SDLConan(ConanFile):
             # FIXME: Remove once CMake on macOS/M1 CI runners is upgraded.
             self.tool_requires("cmake/3.27.1")
         if self.settings.os == "Linux" and not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/1.9.5")
+            self.tool_requires("pkgconf/2.0.2")
         if hasattr(self, "settings_build") and self.options.get_safe("wayland"):
             self.build_requires("wayland/1.22.0")  # Provides wayland-scanner
 
