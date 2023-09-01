7d90b430b21ade9682e09f2695e8065996da7041
Romain BOULLARD <r.boullard@ntymail.com>
2023-09-01 18:57:50 +0200

    Fix constexpr on clang

--- recipes/eastl/all/test_package/test_package.cpp
+++ recipes/eastl/all/test_package/test_package.cpp
@@ -1,4 +1,5 @@
 #include <EASTL/hash_map.h>
+#include <EASTL/string_view.h>
 #include <new>
 
 // https://github.com/electronicarts/EASTL/blob/master/doc/FAQ.md#info15-how-hard-is-it-to-incorporate-eastl-into-my-project
@@ -10,6 +11,7 @@ void *operator new[](size_t size, size_t alignment, size_t alignmentOffset, cons
 }
 
 int main() {
+    constexpr eastl::string_view string = "string_view";
     eastl::hash_map<int, int> map;
     map[0] = 1;
     return 0;
