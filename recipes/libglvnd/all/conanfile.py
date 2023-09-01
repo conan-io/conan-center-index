65eaa6fe9d2852bc913e3802ad6d713e865c3ea6
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-09-01 20:29:26 +0000

    libglvnd/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libglvnd/all/conanfile.py
+++ recipes/libglvnd/all/conanfile.py
@@ -69,7 +69,7 @@ class LibGlvndConan(ConanFile):
 
     def build_requirements(self):
         self.build_requires("meson/1.2.1")
-        self.build_requires("pkgconf/1.9.5")
+        self.build_requires("pkgconf/2.0.2")
 
     def layout(self):
         basic_layout(self)
