591bf235f9fc12febe22c8b0240c32f1ee6de26f
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-09-01 20:45:44 +0000

    xorg-makedepend/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/xorg-makedepend/all/conanfile.py
+++ recipes/xorg-makedepend/all/conanfile.py
@@ -30,7 +30,7 @@ class XorgMakedepend(ConanFile):
         self.requires("xorg-proto/2022.2")
 
     def build_requirements(self):
-        self.build_requires("pkgconf/1.9.5")
+        self.build_requires("pkgconf/2.0.2")
 
     def validate(self):
         if self.settings.os == "Windows":
