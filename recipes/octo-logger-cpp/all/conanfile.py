d54e8f10241a0ea73f993bf1227a45be262c601b
toge <toge.mail@gmail.com>
2023-09-03 06:14:27 +0900

    (#19515) octo-logger-cpp: support conan v2, update fmt/10.1.1, add package_type

--- recipes/octo-logger-cpp/all/conanfile.py
+++ recipes/octo-logger-cpp/all/conanfile.py
@@ -4,6 +4,7 @@ from conan.tools.files import get, copy
 from conan.tools.build import check_min_cppstd
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.scm import Version
+from conan.tools.microsoft import is_msvc
 import os
 
 required_conan_version = ">=1.50.0"
@@ -11,18 +12,23 @@ required_conan_version = ">=1.50.0"
 
 class OctoLoggerCPPConan(ConanFile):
     name = "octo-logger-cpp"
+    description = "CPP Stream based logging library"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/ofiriluz/octo-logger-cpp"
-    description = "Octo logger library"
-    topics = ("logging", "cpp")
-    settings = "os", "compiler", "build_type", "arch"
+    topics = ("logging", "stream", "syslog", "cloudwatch")
+    package_type = "static-library"
+    settings = "os", "arch", "compiler", "build_type"
     options = {
-        "with_aws": [True, False]
+        "with_aws": [True, False],
     }
     default_options = {
-        "with_aws": False
+        "with_aws": False,
     }
+
+    @property
+    def _min_cppstd(self):
+        return 17
 
     @property
     def _compilers_minimum_version(self):
@@ -56,37 +62,32 @@ class OctoLoggerCPPConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        if self.info.settings.compiler.cppstd:
-            check_min_cppstd(self, "17")
-
+        if self.info.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
         minimum_version = self._compilers_minimum_version.get(str(self.info.settings.compiler), False)
         if minimum_version and Version(self.info.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(
-                f"{self.name} requires C++17, which your compiler does not support."
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
-        else:
-            self.output.warn(f"{self.name} requires C++17. Your compiler is unknown. Assuming it supports C++17.")
         if self.settings.compiler == "clang" and self.settings.compiler.get_safe("libcxx") == "libc++":
-            raise ConanInvalidConfiguration(f"{self.name} does not support clang with libc++. Use libstdc++ instead.")
-        if self.settings.compiler == "Visual Studio" and self.settings.compiler.runtime in ["MTd", "MT"]:
-            raise ConanInvalidConfiguration(f"{self.name} does not support MSVC MT/MTd configurations, only MD/MDd is supported")
+            raise ConanInvalidConfiguration(f"{self.ref} does not support clang with libc++. Use libstdc++ instead.")
+        if is_msvc(self) and self.settings.compiler.runtime in ["MTd", "MT"]:
+            raise ConanInvalidConfiguration(f"{self.ref} does not support MSVC MT/MTd configurations, only MD/MDd is supported")
         if self.options.get_safe("with_aws"):
             if not self.dependencies["aws-sdk-cpp"].options.logs:
-                raise ConanInvalidConfiguration(f"{self.name} requires the option aws-sdk-cpp:logs=True")
-
+                raise ConanInvalidConfiguration(f"{self.ref} requires the option aws-sdk-cpp:logs=True")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def requirements(self):
-        self.requires("fmt/9.0.0")
+        self.requires("fmt/10.1.1", transitive_headers=True)
         if self.options.get_safe("with_aws"):
             self.requires("nlohmann_json/3.11.2")
             self.requires("aws-sdk-cpp/1.9.234")
 
     def build_requirements(self):
-        self.build_requires("cmake/3.24.0")
+        self.tool_requires("cmake/[>=3.16 <4]")
 
     def build(self):
         cmake = CMake(self)
@@ -99,16 +100,19 @@ class OctoLoggerCPPConan(ConanFile):
         cmake.install()
 
     def package_info(self):
+        self.cpp_info.libs = ["octo-logger-cpp"]
+
         self.cpp_info.set_property("cmake_file_name", "octo-logger-cpp")
         self.cpp_info.set_property("cmake_target_name", "octo::octo-logger-cpp")
         self.cpp_info.set_property("pkg_config_name", "octo-logger-cpp")
-        self.cpp_info.libs = ["octo-logger-cpp"]
-        self.cpp_info.names["cmake_find_package"] = "octo-logger-cpp"
-        self.cpp_info.names["cmake_find_package_multi"] = "octo-logger-cpp"
-        self.cpp_info.names["pkg_config"] = "octo-logger-cpp"
         self.cpp_info.requires = ["fmt::fmt"]
         if self.options.get_safe("with_aws"):
             self.cpp_info.requires.extend([
                 "nlohmann_json::nlohmann_json",
                 "aws-sdk-cpp::monitoring"
             ])
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.names["cmake_find_package"] = "octo-logger-cpp"
+        self.cpp_info.names["cmake_find_package_multi"] = "octo-logger-cpp"
+        self.cpp_info.names["pkg_config"] = "octo-logger-cpp"
