b3166eb1240cc7b846b622ee90b4b52db991cdd2
toge <toge.mail@gmail.com>
2023-09-02 12:28:16 +0900

    use yaml-cpp/0.7.0 on opencolorio<=2.0.0

--- recipes/opencolorio/all/conanfile.py
+++ recipes/opencolorio/all/conanfile.py
@@ -53,11 +53,12 @@ class OpenColorIOConan(ConanFile):
             self.requires("openexr/3.1.9")
             self.requires("imath/3.1.9")
 
-        self.requires("yaml-cpp/0.7.0")
         if Version(self.version) < "2.0.0":
             self.requires("tinyxml/2.6.2")
+            self.requires("yaml-cpp/0.7.0")
         else:
             self.requires("pystring/1.1.4")
+            self.requires("yaml-cpp/0.8.0")
 
         if Version(self.version) >= "2.3.0":
             self.requires("minizip-ng/4.0.1")
