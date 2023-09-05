6291e58e3a0ecbba4bf0650e28651c9d543d5dc1
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-09-05 08:21:00 +0000

    cgal/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/cgal/all/conanfile.py
+++ recipes/cgal/all/conanfile.py
@@ -45,7 +45,7 @@ class CgalConan(ConanFile):
     def requirements(self):
         self.requires("boost/1.83.0")
         self.requires("eigen/3.4.0")
-        self.requires("mpfr/4.2.0")
+        self.requires("mpfr/4.2.1")
 
     def package_id(self):
         self.info.clear()
