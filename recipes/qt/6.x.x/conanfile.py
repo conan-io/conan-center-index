2279559ee0db245f7dceeb2016a88ae5fcaa2269
Alex Maystrenko <alexeytech@gmail.com>
2023-09-06 13:18:56 +0200

    qt: add QT_NO_DEBUG define in release build

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -967,6 +967,8 @@ class QtConan(ConanFile):
         ]
         self.cpp_info.components["qtCore"].set_property("pkg_config_custom_content", "\n".join(pkg_config_vars))
 
+        if self.settings.build_type == "Release":
+            self.cpp_info.components['qtCore'].defines.append('QT_NO_DEBUG')
         if self.settings.os == "Windows":
             self.cpp_info.components["qtCore"].system_libs.append("authz")
         if is_msvc(self):
