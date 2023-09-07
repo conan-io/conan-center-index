bc9390f1b5a7cc4817249f1490cf1e7f2fad5f17
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-07 21:54:57 +0200

    use version range for zlib

--- recipes/pcre2/all/conanfile.py
+++ recipes/pcre2/all/conanfile.py
@@ -65,7 +65,7 @@ class PCRE2Conan(ConanFile):
 
     def requirements(self):
         if self.options.get_safe("with_zlib"):
-            self.requires("zlib/1.2.13")
+            self.requires("zlib/[>=1.2.10 <2]")
         if self.options.get_safe("with_bzip2"):
             self.requires("bzip2/1.0.8")
 
