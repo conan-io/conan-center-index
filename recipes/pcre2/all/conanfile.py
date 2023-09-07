e7364de8471b0be276c2c923bd46a23a56fb47f8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-07 21:55:48 +0200

    add package_type

--- recipes/pcre2/all/conanfile.py
+++ recipes/pcre2/all/conanfile.py
@@ -16,7 +16,7 @@ class PCRE2Conan(ConanFile):
     description = "Perl Compatible Regular Expressions"
     topics = ("regex", "regexp", "perl")
     license = "BSD-3-Clause"
-
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -76,8 +76,7 @@ class PCRE2Conan(ConanFile):
             raise ConanInvalidConfiguration("build_pcre2_8 must be enabled for the pcre2grep program")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
