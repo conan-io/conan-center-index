75e3e42c9722087d08eab12c96ceea276f033312
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-07 19:37:43 +0200

    bump icu & pkgconf

--- recipes/libxml2/all/conanfile.py
+++ recipes/libxml2/all/conanfile.py
@@ -101,13 +101,13 @@ class Libxml2Conan(ConanFile):
         if self.options.iconv:
             self.requires("libiconv/1.17", transitive_headers=True, transitive_libs=True)
         if self.options.icu:
-            self.requires("icu/73.1")
+            self.requires("icu/73.2")
 
     def build_requirements(self):
         if not (is_msvc(self) or self._is_mingw_windows):
             if self.options.zlib or self.options.lzma or self.options.icu:
                 if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-                    self.tool_requires("pkgconf/1.9.3")
+                    self.tool_requires("pkgconf/2.0.2")
             if self._settings_build.os == "Windows":
                 self.win_bash = True
                 if not self.conf.get("tools.microsoft.bash:path", check_type=str):
