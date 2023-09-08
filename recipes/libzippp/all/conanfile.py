fafdff7b8ba516a4b89d91c9d97dbbc158c3bb2e
Rubén Rincón <rubenrb@jfrog.com>
2023-09-08 11:41:41 +0200

    Bump libzippp's zlib requirement to [>=1.2.11 <2] range

--- recipes/libzippp/all/conanfile.py
+++ recipes/libzippp/all/conanfile.py
@@ -45,7 +45,7 @@ class LibZipppConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("zlib/1.2.13")
+        self.requires("zlib/[>=1.2.11 <2]")
         if Version(self.version) == "4.0":
             self.requires("libzip/1.7.3")
         else:
