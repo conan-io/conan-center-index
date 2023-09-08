fed112b45a5a8963bc88088e9359e0afb6965ae9
Uilian Ries <uilianries@gmail.com>
2023-09-08 09:21:18 +0200

    Fix libcurl range version

--- recipes/libgit2/0.27.x/conanfile.py
+++ recipes/libgit2/0.27.x/conanfile.py
@@ -70,7 +70,7 @@ class LibGit2Conan(ConanFile):
         if self.options.with_libssh2:
             self.requires("libssh2/1.11.0")
         if self.settings.os != "Windows":
-            self.requires("libcurl/[>=X.YY <9]")
+            self.requires("libcurl/[>=7.83 <9]")
         if self._need_openssl:
             self.requires("openssl/[>=1.1 <4]")
 
