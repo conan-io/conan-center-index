52c59b5a37a48fc9b19929cb7b731770d49fd83d
mayeut <mayeut@users.noreply.github.com>
2023-09-11 12:44:27 +0200

    bump pkgconf

--- recipes/libvips/all/conanfile.py
+++ recipes/libvips/all/conanfile.py
@@ -201,7 +201,7 @@ class LibvipsConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("meson/1.2.1")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/1.9.5")
+            self.tool_requires("pkgconf/2.0.2")
         if self.options.introspection:
             self.tool_requires("gobject-introspection/1.72.0")
         self.tool_requires("glib/<host_version>")
