7218e6622d82bf15e51cf00c38679bb8b24f137e
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-09-12 08:36:11 +0000

    libuvc/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libuvc/all/conanfile.py
+++ recipes/libuvc/all/conanfile.py
@@ -77,7 +77,7 @@ class LibuvcConan(ConanFile):
 
     def build_requirements(self):
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/2.0.2")
+            self.tool_requires("pkgconf/2.0.3")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
