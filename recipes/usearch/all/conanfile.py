0aced79c815292b3a40d480660bb5258ed8d9cf3
Rubén Rincón Blanco <git@rinconblanco.es>
2023-09-13 16:20:26 +0200

    Update recipes/usearch/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/usearch/all/conanfile.py
+++ recipes/usearch/all/conanfile.py
@@ -20,11 +20,7 @@ class USearchConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     url = "https://github.com/conan-io/conan-center-index"
     package_type = "header-library"
-
-    # No settings/options are necessary, this is header only
-    # Potentially add unit-tests in the future:
-    # https://docs.conan.io/1/howtos/header_only.html#with-unit-tests
-    # no_copy_source = True
+    no_copy_source = True
 
     @property
     def _min_cppstd(self):
