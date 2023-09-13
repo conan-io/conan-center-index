0153938eccf79de6e1315eac3f4d88a0d7b904f3
Martin Valgur <martin.valgur@gmail.com>
2023-09-13 11:12:03 +0300

    redboltz-mqtt_cpp: bump deps

--- recipes/redboltz-mqtt_cpp/all/conanfile.py
+++ recipes/redboltz-mqtt_cpp/all/conanfile.py
@@ -39,7 +39,7 @@ class MqttCPPConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.82.0")
+        self.requires("boost/1.83.0")
 
     def package_id(self):
         self.info.clear()
