ead3e940c3cd64802b1c6495161505d193d9696e
Martin Valgur <martin.valgur@gmail.com>
2023-09-14 11:48:37 +0300

    cairo: add v1.17.8

--- recipes/cairo/meson/conanfile.py
+++ recipes/cairo/meson/conanfile.py
@@ -74,6 +74,8 @@ class CairoConan(ConanFile):
             del self.options.with_symbol_lookup
         if self.settings.os == "Windows" or is_apple_os(self):
             del self.options.with_opengl
+        if Version(self.version) >= "1.17.8":
+            del self.options.with_opengl
 
     def configure(self):
         if self.options.shared:
@@ -156,16 +158,17 @@ class CairoConan(ConanFile):
         else:
             options["xcb"] = "disabled"
             options["xlib"] = "disabled"
-        if self.options.get_safe("with_opengl") == "desktop":
-            options["gl-backend"] = "gl"
-        elif self.options.get_safe("with_opengl") == "gles2":
-            options["gl-backend"] = "glesv2"
-        elif self.options.get_safe("with_opengl") == "gles3":
-            options["gl-backend"] = "glesv3"
-        else:
-            options["gl-backend"] = "disabled"
-        options["glesv2"] = is_enabled(self.options.get_safe("with_opengl") == "gles2")
-        options["glesv3"] = is_enabled(self.options.get_safe("with_opengl") == "gles3")
+        if Version(self.version) < "1.17.8":
+            if self.options.get_safe("with_opengl") == "desktop":
+                options["gl-backend"] = "gl"
+            elif self.options.get_safe("with_opengl") == "gles2":
+                options["gl-backend"] = "glesv2"
+            elif self.options.get_safe("with_opengl") == "gles3":
+                options["gl-backend"] = "glesv3"
+            else:
+                options["gl-backend"] = "disabled"
+            options["glesv2"] = is_enabled(self.options.get_safe("with_opengl") == "gles2")
+            options["glesv3"] = is_enabled(self.options.get_safe("with_opengl") == "gles3")
         options["tee"] = is_enabled(self.options.tee)
         options["symbol-lookup"] = is_enabled(self.options.get_safe("with_symbol_lookup"))
 
