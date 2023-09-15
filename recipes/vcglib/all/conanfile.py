94aa3439cfcc63e9aa9b20747356d3998c646f03
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-15 13:55:00 +0200

    add package_type & modernize more

--- recipes/vcglib/all/conanfile.py
+++ recipes/vcglib/all/conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.53.0"
 
 
 class VcglibConan(ConanFile):
@@ -14,7 +14,7 @@ class VcglibConan(ConanFile):
     topics = ("vcglib", "mesh-processing")
     homepage = "https://github.com/cnr-isti-vclab/vcglib"
     url = "https://github.com/conan-io/conan-center-index"
-
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -35,10 +35,7 @@ class VcglibConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -51,8 +48,7 @@ class VcglibConan(ConanFile):
             check_min_cppstd(self, 11)
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
