ac68aae85c6246a08a438baf862d49c2c17547ea
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-15 11:08:14 +0200

    format imports

--- recipes/tscns/all/conanfile.py
+++ recipes/tscns/all/conanfile.py
@@ -1,12 +1,13 @@
-from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
-from conan.tools.build import check_min_cppstd
-from conan.tools.layout import basic_layout
-
 import os
 
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.layout import basic_layout
+
 required_conan_version = ">=1.52.0"
+
 
 class TscnsConan(ConanFile):
     name = "tscns"
