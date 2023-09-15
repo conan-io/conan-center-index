1a366114f0bb8f215483e7a6506b74f3654829c1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-15 13:23:42 +0200

    add a layout

--- recipes/units/all/conanfile.py
+++ recipes/units/all/conanfile.py
@@ -2,6 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
 import os
 
@@ -36,6 +37,9 @@ class UnitsConan(ConanFile):
             "Visual Studio": "14",
             "msvc": "190",
         }
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def package_id(self):
         self.info.clear()
