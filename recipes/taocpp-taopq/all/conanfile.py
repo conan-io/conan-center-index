bb09a5969edcecf30cf9c6ccf9589e4f4b4eb2c5
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-15 10:36:17 +0200

    fix traits of libpq

--- recipes/taocpp-taopq/all/conanfile.py
+++ recipes/taocpp-taopq/all/conanfile.py
@@ -53,7 +53,8 @@ class TaoCPPTaopqConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("libpq/14.5")
+        # libpq-fe.h is included by many public headers of taocpp-taopq
+        self.requires("libpq/14.5", transitive_headers=True)
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
