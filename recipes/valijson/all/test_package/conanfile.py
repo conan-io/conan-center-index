956dc2c05f28424036912dc8c39bf060bc513938
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-15 13:43:17 +0200

    bump nlohmann_json & rapidjson in test package

--- recipes/valijson/all/test_package/conanfile.py
+++ recipes/valijson/all/test_package/conanfile.py
@@ -12,8 +12,8 @@ class TestPackageConan(ConanFile):
         cmake_layout(self)
 
     def requirements(self):
-        self.requires("nlohmann_json/3.9.1")
-        self.requires("rapidjson/cci.20200410")
+        self.requires("nlohmann_json/3.11.2")
+        self.requires("rapidjson/cci.20220822")
         self.requires("picojson/1.3.0")
         self.requires(self.tested_reference_str)
 
