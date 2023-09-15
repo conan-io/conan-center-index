a6d162b987be3528bc4b9b236645246752241d14
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-15 15:25:06 +0200

    use version range for zlib

--- recipes/zstr/all/conanfile.py
+++ recipes/zstr/all/conanfile.py
@@ -22,7 +22,7 @@ class ZstrConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("zlib/1.2.13")
+        self.requires("zlib/[>=1.2.11 <2]")
 
     def package_id(self):
         self.info.clear()
