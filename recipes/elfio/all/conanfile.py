c0a46775c2c75956fadbb5a4fac23ad5a4e54b42
toge <toge.mail@gmail.com>
2023-09-17 00:51:24 +0900

    elfio/3.12 requires C++17

--- recipes/elfio/all/conanfile.py
+++ recipes/elfio/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
@@ -19,6 +20,28 @@ class ElfioConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
+    @property
+    def _min_cppstd(self):
+        if Version(self.version) < "3.11":
+            return "11"
+        elif Version(self.version) < "3.12":
+            return "14"
+        else:
+            return "17"
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            # while elfio<3.12 requires C++14, elfio still supports GCC 5
+            "17": {
+                "gcc": "8",
+                "clang": "7",
+                "apple-clang": "12",
+                "Visual Studio": "16",
+                "msvc": "192",
+            },
+        }.get(self._min_cppstd, {})
+
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -27,10 +50,12 @@ class ElfioConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            if Version(self.version) < "3.11":
-                check_min_cppstd(self, 11)
-            else:
-                check_min_cppstd(self, 14)
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
