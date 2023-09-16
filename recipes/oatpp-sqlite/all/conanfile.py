e69d35e03a78fcac35ab8961ff7f458a9e87f40e
toge <toge.mail@gmail.com>
2023-09-17 00:57:03 +0900

    update sqlite3/3.43.1

--- recipes/oatpp-sqlite/all/conanfile.py
+++ recipes/oatpp-sqlite/all/conanfile.py
@@ -41,7 +41,7 @@ class OatppsqliteConan(ConanFile):
 
     def requirements(self):
         self.requires(f"oatpp/{self.version}", transitive_headers=True)
-        self.requires("sqlite3/3.43.0")
+        self.requires("sqlite3/3.43.1")
 
     def validate(self):
         if self.info.settings.compiler.get_safe("cppstd"):
