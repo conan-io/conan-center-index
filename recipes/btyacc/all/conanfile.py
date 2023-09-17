dfc15c10135522cfd0092406ce9c4e183e5594b0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-17 17:51:56 +0200

    move cmake file under bin/cmake

--- recipes/btyacc/all/conanfile.py
+++ recipes/btyacc/all/conanfile.py
@@ -48,8 +48,8 @@ class BtyaccConan(ConanFile):
         cmake.build()
 
     @property
-    def _variables(self):
-        return os.path.join("bin", "conan-official-btyacc-variables.cmake")
+    def _cmake_variables(self):
+        return os.path.join("bin", "cmake", f"conan-official-{self.name}-variables.cmake")
 
     def package(self):
         copy(self, "README*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
@@ -62,15 +62,15 @@ class BtyaccConan(ConanFile):
               set(BTYACC_EXECUTABLE "${BTYACC_EXECUTABLE}.exe")
             endif()
         """)
-        save(self, os.path.join(self.package_folder, self._variables), content)
+        save(self, os.path.join(self.package_folder, self._cmake_variables), content)
 
     def package_info(self):
         self.cpp_info.includedirs = []
         self.cpp_info.libdirs = []
-        self.cpp_info.set_property("cmake_build_modules", [self._variables])
+        self.cpp_info.set_property("cmake_build_modules", [self._cmake_variables])
 
         # TODO: to remove after conan v2
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
-        self.cpp_info.build_modules["cmake"] = [self._variables]
-        self.cpp_info.build_modules["cmake_find_package"] = [self._variables]
-        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._variables]
+        self.cpp_info.build_modules["cmake"] = [self._cmake_variables]
+        self.cpp_info.build_modules["cmake_find_package"] = [self._cmake_variables]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._cmake_variables]
