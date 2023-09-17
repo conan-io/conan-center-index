58abf4e09cbbc37dc9b00a62551c9d15a49d9bd3
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-17 18:40:43 +0200

    Revert "workaround in test package"
    This reverts commit ba102b0e0eb88b5a3f9e41d66272258bfa239b4c.

--- recipes/btyacc/all/test_package/conanfile.py
+++ recipes/btyacc/all/test_package/conanfile.py
@@ -1,33 +1,28 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.env import VirtualRunEnv
+from conan.tools.cmake import CMake, CMakeDeps, cmake_layout
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeToolchain", "VirtualBuildEnv"
     test_type = "explicit"
 
     def layout(self):
         cmake_layout(self)
 
-    # FIXME: we should test btyacc as a build requirement and
-    #        add btyacc to build_context_activated of CMakDeps but there is a
-    #        conan client bug: https://github.com/conan-io/conan/issues/14758
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def generate(self):
-        if can_run(self):
-            VirtualRunEnv(self).generate(scope="build")
+        deps = CMakeDeps(self)
+        deps.build_context_activated = ["btyacc"]
+        deps.generate()
 
     def build(self):
-        if can_run(self):
-            cmake = CMake(self)
-            cmake.configure()
-            cmake.build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
         if can_run(self):
