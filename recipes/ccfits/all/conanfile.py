67ebfae8eb210a0f5db081223b49bd451093ffde
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-17 10:46:51 +0200

    bump cfitsio

--- recipes/ccfits/all/conanfile.py
+++ recipes/ccfits/all/conanfile.py
@@ -41,7 +41,7 @@ class CcfitsConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("cfitsio/4.1.0")
+        self.requires("cfitsio/4.2.0")
 
     def validate_build(self):
         if Version(self.version) >= "2.6":
