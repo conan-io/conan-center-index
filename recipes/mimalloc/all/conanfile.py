3f1a165f7c0d4cf7dc0dfd5962da7bda91766112
tankeco <wjn123335@126.com>
2023-09-19 00:17:24 +0800

    remove unused import

--- recipes/mimalloc/all/conanfile.py
+++ recipes/mimalloc/all/conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file, save, collect_libs
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime, VCVars
