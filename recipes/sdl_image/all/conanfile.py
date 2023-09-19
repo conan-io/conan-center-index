f889f2dda9183186691f592c6e66c9513d8d42e1
toge <toge.mail@gmail.com>
2023-09-19 20:08:56 +0900

    fix license filename

--- recipes/sdl_image/all/conanfile.py
+++ recipes/sdl_image/all/conanfile.py
@@ -174,11 +174,14 @@ class SDLImageConan(ConanFile):
 
     def package(self):
         copy(self, "COPYING.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "LICENSE.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
         if Version(self.version) >= "2.6":
             rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+            rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+            rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "SDL2_image")
