afbaaffa0d12181af5bdcbf3b477da60414d8b09
Martin Valgur <martin.valgur@gmail.com>
2023-09-20 12:16:45 +0300

    glslang: add new versions, remove old ones
    Removed versions that are more than a year old.

--- recipes/glslang/all/conanfile.py
+++ recipes/glslang/all/conanfile.py
@@ -2,7 +2,7 @@ import os
 import re
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
+from conan.errors import ConanInvalidConfiguration, ConanException
 from conan.tools.apple import is_apple_os
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
@@ -42,6 +42,10 @@ class GlslangConan(ConanFile):
 
     short_paths = True
 
+    @property
+    def _is_v1(self):
+        return Version(self.version).major == 1
+
     def export_sources(self):
         copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
 
@@ -56,18 +60,9 @@ class GlslangConan(ConanFile):
     def layout(self):
         cmake_layout(self, src_folder="src")
 
-    @property
-    def _get_compatible_spirv_tools_version(self):
-        return {
-            "11.7.0": "2021.4",
-            "11.6.0": "2021.3",
-            "11.5.0": "2021.2",
-            "8.13.3559": "2020.5",
-        }.get(str(self.version), self.version)
-
     def requirements(self):
         if self.options.enable_optimizer:
-            self.requires(f"spirv-tools/{self._get_compatible_spirv_tools_version}")
+            self.requires(f"spirv-tools/{self.version}")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
@@ -75,8 +70,7 @@ class GlslangConan(ConanFile):
 
         # see https://github.com/KhronosGroup/glslang/issues/2283
         if self.options.shared:
-            glslang_version = Version(self.version)
-            if self.settings.os == "Windows" or (is_apple_os(self) and "7.0.0" <= glslang_version < "11.0.0"):
+            if self.settings.os == "Windows":
                 raise ConanInvalidConfiguration(f"{self.ref} shared library build is broken on {self.settings.os}")
 
         if self.options.enable_optimizer and self.dependencies["spirv-tools"].options.shared:
@@ -94,30 +88,24 @@ class GlslangConan(ConanFile):
         tc.variables["SKIP_GLSLANG_INSTALL"] = False
         tc.variables["ENABLE_SPVREMAPPER"] = self.options.spv_remapper
         tc.variables["ENABLE_GLSLANG_BINARIES"] = self.options.build_executables
-        glslang_version = Version(self.version)
-        if not ("7.0.0" <= glslang_version < "8.13.3743"):
-            tc.variables["ENABLE_GLSLANG_JS"] = False
-            tc.variables["ENABLE_GLSLANG_WEBMIN"] = False
-            tc.variables["ENABLE_GLSLANG_WEBMIN_DEVEL"] = False
-        else:
-            tc.variables["ENABLE_GLSLANG_WEB"] = False
-            tc.variables["ENABLE_GLSLANG_WEB_DEVEL"] = False
+        tc.variables["ENABLE_GLSLANG_JS"] = False
+        tc.variables["ENABLE_GLSLANG_WEBMIN"] = False
+        tc.variables["ENABLE_GLSLANG_WEBMIN_DEVEL"] = False
         tc.variables["ENABLE_EMSCRIPTEN_SINGLE_FILE"] = False
         tc.variables["ENABLE_EMSCRIPTEN_ENVIRONMENT_NODE"] = False
         tc.variables["ENABLE_HLSL"] = self.options.hlsl
-        if not ("7.0.0" <= glslang_version < "8.13.3743"):
-            tc.variables["ENABLE_RTTI"] = True
+        tc.variables["ENABLE_RTTI"] = True
         tc.variables["ENABLE_OPT"] = self.options.enable_optimizer
         if self.options.enable_optimizer:
             tc.variables["spirv-tools_SOURCE_DIR"] = self.dependencies["spirv-tools"].package_folder.replace("\\", "/")
         tc.variables["ENABLE_PCH"] = False
         tc.variables["ENABLE_CTEST"] = False
         tc.variables["USE_CCACHE"] = False
-        if not ("7.0.0" <= glslang_version < "11.6.0") and self.settings.os == "Windows":
+        if self.settings.os == "Windows":
             tc.variables["OVERRIDE_MSVCCRT"] = False
         if is_apple_os(self):
             tc.variables["CMAKE_MACOSX_BUNDLE"] = False
-        if not ("1.3.231" <= glslang_version < "7.0.0"):
+        if not self._is_v1:
             # Generate a relocatable shared lib on Macos
             tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
         tc.generate()
@@ -133,6 +121,8 @@ class GlslangConan(ConanFile):
                 content = content.replace("POSITION_INDEPENDENT_CODE ON", "")
                 cmake_file.write_text(content, encoding="utf8")
                 self.output.info(f"Patched fPIC handling in {cmake_file.relative_to(self.source_path)}")
+            if "POSITION_INDEPENDENT_CODE" in content:
+                raise ConanException(f"POSITION_INDEPENDENT_CODE found in {cmake_file}, please update the recipe")
 
     def build(self):
         self._patch_sources()
@@ -153,18 +143,17 @@ class GlslangConan(ConanFile):
 
         lib_suffix = "d" if self.settings.os == "Windows" and self.settings.build_type == "Debug" else ""
 
-        glslang_version = Version(self.version)
-        has_machineindependent = not ("7.0.0" <= glslang_version < "11.0.0") and not self.options.shared
-        has_genericcodegen = not ("7.0.0" <= glslang_version < "11.0.0") and not self.options.shared
-        has_osdependent = not ("1.3.231" <= glslang_version < "7.0.0") or not self.options.shared
-        has_oglcompiler = not ("1.3.231" <= glslang_version < "7.0.0") or not self.options.shared
+        has_machineindependent = not self.options.shared
+        has_genericcodegen = not self.options.shared
+        has_osdependent = not self._is_v1 or not self.options.shared
+        has_oglcompiler = not self._is_v1 or not self.options.shared
 
         # glslang
         self.cpp_info.components["glslang-core"].set_property("cmake_target_name", "glslang::glslang")
         self.cpp_info.components["glslang-core"].names["cmake_find_package"] = "glslang"
         self.cpp_info.components["glslang-core"].names["cmake_find_package_multi"] = "glslang"
         self.cpp_info.components["glslang-core"].libs = [f"glslang{lib_suffix}"]
-        if not ("7.0.0" <= glslang_version < "11.0.0") and self.options.shared:
+        if self.options.shared:
             self.cpp_info.components["glslang-core"].defines.append("GLSLANG_IS_SHARED_LIBRARY")
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["glslang-core"].system_libs.extend(["m", "pthread"])
