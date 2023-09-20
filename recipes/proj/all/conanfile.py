48e4effb4a082b8e43aad2f97e076b96b3a9314e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-20 02:47:34 +0200

    bump dependencies

--- recipes/proj/all/conanfile.py
+++ recipes/proj/all/conanfile.py
@@ -57,9 +57,9 @@ class ProjConan(ConanFile):
 
     def requirements(self):
         self.requires("nlohmann_json/3.11.2")
-        self.requires("sqlite3/3.42.0")
+        self.requires("sqlite3/3.43.1")
         if self.options.get_safe("with_tiff"):
-            self.requires("libtiff/4.5.1")
+            self.requires("libtiff/4.6.0")
         if self.options.get_safe("with_curl"):
             self.requires("libcurl/8.2.1")
 
