6a32493bc0d6c02a7b37e23b005921c2fe81fc19
Martin Valgur <martin.valgur@gmail.com>
2023-09-20 15:33:03 +0300

    open-simulation-interface: update required_conan_version
    for host_version

--- recipes/open-simulation-interface/all/conanfile.py
+++ recipes/open-simulation-interface/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=1.60.0 <2.0 || >=2.0.5"
 
 
 class OpenSimulationInterfaceConan(ConanFile):
