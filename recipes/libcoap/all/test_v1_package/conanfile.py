e041ac834b103aead13d31d8feb2cca2b2706459
Martin Valgur <martin.valgur@gmail.com>
2023-09-20 15:00:48 +0300

    libcoap: fix test_v1_package

--- recipes/libcoap/all/test_v1_package/conanfile.py
+++ recipes/libcoap/all/test_v1_package/conanfile.py
@@ -8,17 +8,13 @@ class TestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-
-        version = self.deps_cpp_info["libcoap"].version;
-        if version == "cci.20200424":
-            cmake.definitions["LIB_VERSION"] = 2
-        else:
-            cmake.definitions["LIB_VERSION"] = 3
-
+        version = self.deps_cpp_info["libcoap"].version
+        lib_version = 2 if version == "cci.20200424" else 3
+        cmake.definitions["CMAKE_CXX_FLAGS"] = f"-DLIB_VERSION={lib_version}"
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self.settings):
             bin_path = os.path.join("bin", "test_package")
-            bin_path = self.run(bin_path, run_environment=True)
+            self.run(bin_path, run_environment=True)
