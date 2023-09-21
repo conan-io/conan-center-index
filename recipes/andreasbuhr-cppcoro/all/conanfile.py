a2a501189a25d01f0fed9d565ddd2c5fe805162c
Martin Valgur <martin.valgur@gmail.com>
2023-09-21 21:46:39 +0300

    andreasbuhr-cppcoro: set min cppstd to 17, tidy

--- recipes/andreasbuhr-cppcoro/all/conanfile.py
+++ recipes/andreasbuhr-cppcoro/all/conanfile.py
@@ -2,7 +2,8 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
@@ -31,6 +32,10 @@ class AndreasbuhrCppCoroConan(ConanFile):
     provides = "cppcoro"
 
     @property
+    def _min_cppstd(self):
+        return 17
+
+    @property
     def _minimum_compilers_version(self):
         return {
             "Visual Studio": "15",
@@ -52,22 +57,20 @@ class AndreasbuhrCppCoroConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+
         # We can't simply check for C++20, because clang and MSVC support the coroutine TS despite not having labeled (__cplusplus macro) C++20 support
         min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
-        if not min_version:
-            self.output.warning(
-                f"{self.name} recipe lacks information about the {self.settings.compiler} compiler support."
+        if min_version and Version(self.settings.compiler.version) < min_version:
+            raise ConanInvalidConfiguration(
+                f"{self.name} requires coroutine TS support. The current compiler"
+                f" {self.settings.compiler} {self.settings.compiler.version} does not support it."
             )
-        else:
-            if Version(self.settings.compiler.version) < min_version:
-                raise ConanInvalidConfiguration(
-                    f"{self.name} requires coroutine TS support. The current compiler"
-                    f" {self.settings.compiler} {self.settings.compiler.version} does not support it."
-                )
 
-        # Currently clang expects coroutine to be implemented in a certain way (under std::experiemental::), while libstdc++ puts them under std::
+        # Currently clang expects coroutine to be implemented in a certain way (under std::experimental::), while libstdc++ puts them under std::
         # There are also other inconsistencies, see https://bugs.llvm.org/show_bug.cgi?id=48172
-        # This should be removed after both gcc and clang implements the final coroutine TS
+        # This should be removed after both gcc and clang implement the final coroutine TS
         if self.settings.compiler == "clang" and self.settings.compiler.get_safe("libcxx") == "libstdc++":
             raise ConanInvalidConfiguration(
                 f"{self.name} does not support clang with libstdc++. Use libc++ instead."
@@ -79,8 +82,6 @@ class AndreasbuhrCppCoroConan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = self.options.shared
-        tc.generate()
-        tc = CMakeDeps(self)
         tc.generate()
 
     def build(self):
@@ -98,27 +99,23 @@ class AndreasbuhrCppCoroConan(ConanFile):
         self.cpp_info.set_property("cmake_file_name", "cppcoro")
         self.cpp_info.set_property("cmake_target_name", "cppcoro::cppcoro")
 
-        comp = self.cpp_info.components["cppcoro"]
-        comp.set_property("cmake_target_name", "cppcoro")
-        comp.libs = ["cppcoro"]
+        self.cpp_info.libs = ["cppcoro"]
 
         if self.settings.os in ["Linux", "FreeBSD"] and self.options.shared:
-            comp.system_libs = ["pthread", "m"]
-        if self.settings.os == "Windows":
-            comp.system_libs = ["synchronization", "mswsock", "ws2_32"]
+            self.cpp_info.system_libs = ["pthread", "m"]
+        elif self.settings.os == "Windows":
+            self.cpp_info.system_libs = ["synchronization", "mswsock", "ws2_32"]
 
         if is_msvc(self):
-            comp.cxxflags.append("/await")
+            self.cpp_info.cxxflags.append("/await")
         elif self.settings.compiler == "gcc":
-            comp.cxxflags.append("-fcoroutines")
-            comp.defines.append("CPPCORO_COMPILER_SUPPORTS_SYMMETRIC_TRANSFER=1")
+            self.cpp_info.cxxflags.append("-fcoroutines")
+            self.cpp_info.defines.append("CPPCORO_COMPILER_SUPPORTS_SYMMETRIC_TRANSFER=1")
         elif self.settings.compiler in ["clang", "apple-clang"]:
-            comp.cxxflags.append("-fcoroutines-ts")
+            self.cpp_info.cxxflags.append("-fcoroutines-ts")
 
         # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "cppcoro"
         self.cpp_info.filenames["cmake_find_package_multi"] = "cppcoro"
         self.cpp_info.names["cmake_find_package"] = "cppcoro"
         self.cpp_info.names["cmake_find_package_multi"] = "cppcoro"
-        comp.names["cmake_find_package"] = "cppcoro"
-        comp.names["cmake_find_package_multi"] = "cppcoro"
