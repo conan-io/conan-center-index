3fb3b25bbad15496f4fcbfae0658dd353cd1b606
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-09-22 09:35:24 +0200

    use version range for zlib

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -295,7 +295,7 @@ class QtConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("zlib/1.2.13")
+        self.requires("zlib/[>=1.2.11 <2]")
         if self.options.openssl:
             self.requires("openssl/[>=1.1 <4]")
         if self.options.with_pcre2:
