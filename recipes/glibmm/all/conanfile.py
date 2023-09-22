430c30820d0207e92938794da951a6013ce0558f
MALASHKIN Andrei <andrei.malashkin-ext@hexagon.com>
2023-09-22 09:00:19 +0200

    bump glibmm dependencies

--- recipes/glibmm/all/conanfile.py
+++ recipes/glibmm/all/conanfile.py
@@ -75,7 +75,7 @@ class GlibmmConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("glib/2.77.2")
+        self.requires("glib/2.78.0")
         if self._abi_version == "2.68":
             self.requires("libsigcpp/3.0.7")
         else:
@@ -99,7 +99,7 @@ class GlibmmConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("meson/1.2.1")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/1.9.5")
+            self.tool_requires("pkgconf/2.0.3")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
