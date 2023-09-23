0fbdcad03221990b5f85e1e593ce35dedc3b230f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-23 22:20:40 +0200

    always use protobuf from build context in case of 2 profiles

--- recipes/onnx/all/conanfile.py
+++ recipes/onnx/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.build import check_min_cppstd, cross_building
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, save
@@ -35,6 +35,10 @@ class OnnxConan(ConanFile):
     }
 
     @property
+    def _is_legacy_one_profile(self):
+        return not hasattr(self, "settings_build")
+
+    @property
     def _min_cppstd(self):
         if Version(self.version) >= "1.13.0" and is_msvc(self):
             return 17
@@ -55,7 +59,7 @@ class OnnxConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("protobuf/3.21.12", run=not cross_building(self), transitive_headers=True, transitive_libs=True)
+        self.requires("protobuf/3.21.12", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
@@ -64,7 +68,7 @@ class OnnxConan(ConanFile):
             raise ConanInvalidConfiguration("onnx shared is broken with Visual Studio")
 
     def build_requirements(self):
-        if hasattr(self, "settings_build") and cross_building(self):
+        if not self._is_legacy_one_profile:
             self.tool_requires("protobuf/<host_version>")
 
     def source(self):
@@ -73,7 +77,7 @@ class OnnxConan(ConanFile):
     def generate(self):
         env = VirtualBuildEnv(self)
         env.generate()
-        if not cross_building(self):
+        if self._is_legacy_one_profile:
             env = VirtualRunEnv(self)
             env.generate(scope="build")
         tc = CMakeToolchain(self)
