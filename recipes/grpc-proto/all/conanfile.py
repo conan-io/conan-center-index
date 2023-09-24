81bb578e6d823c5f9334ed516114f2a067bd0eb8
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-25 01:28:41 +0200

    fix min conan v2 version

--- recipes/grpc-proto/all/conanfile.py
+++ recipes/grpc-proto/all/conanfile.py
@@ -11,7 +11,7 @@ from conan.tools.scm import Version
 
 from helpers import parse_proto_libraries
 
-required_conan_version = ">=1.60.0 <2.0 || >=2.0.6"
+required_conan_version = ">=1.60.0 <2.0 || >=2.0.5"
 
 
 class GRPCProto(ConanFile):
