de71c3ca836d3ef58781cc7490bd61f8e6577c22
Rubén Rincón <rubenrb@jfrog.com>
2023-09-25 17:22:01 +0200

    Bump opencv's zlib requirement to [>=1.2.11 <2] range

--- recipes/opencv/2.x/conanfile.py
+++ recipes/opencv/2.x/conanfile.py
@@ -352,7 +352,7 @@ class OpenCVConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("zlib/1.2.13")
+        self.requires("zlib/[>=1.2.11 <2]")
         if self.options.with_eigen:
             self.requires("eigen/3.4.0")
         if self.options.with_tbb:
