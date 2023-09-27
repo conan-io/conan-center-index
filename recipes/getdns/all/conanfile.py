60a3270894594d4609b6c5ba39070b055a14f6a0
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-09-27 12:31:21 +0000

    getdns/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/getdns/all/conanfile.py
+++ recipes/getdns/all/conanfile.py
@@ -68,7 +68,7 @@ class GetDnsConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def requirements(self):
-        self.requires("openssl/3.1.2")
+        self.requires("openssl/3.1.3")
         if self._with_libev:
             self.requires("libev/4.33")
         if self.options.with_libevent:
