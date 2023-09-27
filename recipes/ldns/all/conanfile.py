06b92132b482c0aaa6df2e078cd627e0ab95a6d2
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-09-27 23:48:28 +0200

    version range for openssl

--- recipes/ldns/all/conanfile.py
+++ recipes/ldns/all/conanfile.py
@@ -43,7 +43,7 @@ class LdnsConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("openssl/1.1.1t")
+        self.requires("openssl/[>=1.1 <4]")
 
     def validate(self):
         if self.settings.os == "Windows":
