ab32a279aef3178830a15bec474833aa250caaec
Rubén Rincón <rubenrb@jfrog.com>
2023-09-28 20:37:02 +0200

    Bump ixwebsocket's zlib requirement to [>=1.2.11 <2] range

--- recipes/ixwebsocket/all/conanfile.py
+++ recipes/ixwebsocket/all/conanfile.py
@@ -56,7 +56,7 @@ class IXWebSocketConan(ConanFile):
 
     def requirements(self):
         if self.options.get_safe("with_zlib", True):
-            self.requires("zlib/1.2.13")
+            self.requires("zlib/[>=1.2.11 <2]")
         if self.options.tls == "openssl":
             self.requires("openssl/1.1.1s")
         elif self.options.tls == "mbedtls":
