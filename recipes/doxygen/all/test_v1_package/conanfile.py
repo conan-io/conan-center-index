c4b8df6a839e51525fc0a665ad3efb24e961ef4e
Uilian Ries <uilianries@gmail.com>
2023-09-29 17:49:49 +0200

    Update recipes/doxygen/all/test_v1_package/conanfile.py

--- recipes/doxygen/all/test_v1_package/conanfile.py
+++ recipes/doxygen/all/test_v1_package/conanfile.py
@@ -11,7 +11,7 @@ class TestPackageConan(ConanFile):
             self.output.info("Doxygen Version:")
             stringio = StringIO()
             try:
-                self.run(f"{self.recipe_folder}/doxygen --version", run_environment=True, output=stringio)
+                self.run(f"doxygen --version", run_environment=True, output=stringio)
             except ConanException:
                 # FIXME: Doxygen is built with glibc 2.29, but the CI machines have 2.23
                 assert "version `GLIBC_2.29' not found" in stringio.getvalue()
