072bf6b002496ec30dbe7a3700b3b8c7e8e605b9
Uilian Ries <uilianries@gmail.com>
2023-09-29 14:16:14 +0200

    Link everthing statically
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/doxygen/all/conanfile.py
+++ recipes/doxygen/all/conanfile.py
@@ -85,7 +85,7 @@ class DoxygenConan(ConanFile):
         tc.variables["win_static"] = is_msvc_static_runtime(self)
         if self.settings.os == "Linux" and "libstdc++" in self.settings.compiler.libcxx:
             # INFO: Link C++ library statically on Linux so that it can run on systems with an older C++ runtime
-            tc.cache_variables["CMAKE_EXE_LINKER_FLAGS"] = "-static-libstdc++ -static-libgcc"
+            tc.cache_variables["CMAKE_EXE_LINKER_FLAGS"] = "-static"
         tc.generate()
 
         deps = CMakeDeps(self)
