758b696f4058285aef85bafac9dab57ddd496a52
toge <toge.mail@gmail.com>
2023-10-01 01:04:02 +0900

    make openssl transitive_headers

--- recipes/libcoro/all/conanfile.py
+++ recipes/libcoro/all/conanfile.py
@@ -64,7 +64,7 @@ class LibcoroConan(ConanFile):
 
     def requirements(self):
         if "with_ssl" not in self.options or self.options.with_ssl:
-            self.requires("openssl/[>=1.1 <4]")
+            self.requires("openssl/[>=1.1 <4]", transitive_headers=True)
         self.requires("c-ares/1.19.1", transitive_headers=True)
         self.requires("tl-expected/1.1.0", transitive_headers=True)
 
