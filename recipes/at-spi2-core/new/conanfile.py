8e3765c02e22b844937733f68eb57280930a7906
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-10-01 11:12:12 +0200

    Update conanfile.py

--- recipes/at-spi2-core/new/conanfile.py
+++ recipes/at-spi2-core/new/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
+from conan.errors import ConanException, ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
