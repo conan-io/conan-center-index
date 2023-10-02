61e96e3f5c38ce6ccd64cc1f221c9086b3bc1bce
klausholstjacobsen <klaus.holst.jacobsen@gmail.com>
2023-10-02 17:54:29 +0200

    net-snmp: Removed unused imports 9in test package

--- recipes/net-snmp/all/test_package/conanfile.py
+++ recipes/net-snmp/all/test_package/conanfile.py
@@ -1,7 +1,7 @@
 import os
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMake, cmake_layout
 
 
 class TestPackageConan(ConanFile):
