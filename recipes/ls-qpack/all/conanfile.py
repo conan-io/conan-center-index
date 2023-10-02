4648c0d6837138913e847b0dcb042bcdbe1a558e
toge <toge.mail@gmail.com>
2023-10-03 00:30:37 +0900

    ls-qpack: add version 2.5.3

--- recipes/ls-qpack/all/conanfile.py
+++ recipes/ls-qpack/all/conanfile.py
@@ -42,7 +42,7 @@ class LsQpackConan(ConanFile):
 
     def requirements(self):
         if self.options.with_xxh:
-            self.requires("xxhash/0.8.1")
+            self.requires("xxhash/0.8.2")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
