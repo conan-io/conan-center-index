27ddfe166d04127c6f1887e71478d976f22d9a3e
Martin Valgur <martin.valgur@gmail.com>
2023-10-02 13:58:28 +0300

    libdrm: add pkgconf dependency

--- recipes/libdrm/all/conanfile.py
+++ recipes/libdrm/all/conanfile.py
@@ -88,7 +88,9 @@ class LibdrmConan(ConanFile):
             raise ConanInvalidConfiguration("libdrm supports only Linux or FreeBSD")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.2.1")
+        self.tool_requires("meson/1.2.2")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/2.0.3")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
