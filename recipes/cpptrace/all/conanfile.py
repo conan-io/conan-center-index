a4b83bc50a7aefbd84b305e66b119e30347b0d74
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2023-10-02 14:16:32 -0400

    Update recipes/cpptrace/all/conanfile.py
    Co-authored-by: Rubén Rincón Blanco <git@rinconblanco.es>

--- recipes/cpptrace/all/conanfile.py
+++ recipes/cpptrace/all/conanfile.py
@@ -55,12 +55,6 @@ class PackageConan(ConanFile):
         if self.settings.compiler.cppstd:
             check_min_cppstd(self, self._min_cppstd)
         check_min_vs(self, 191)
-        if not is_msvc(self):
-            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-            if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration(
-                    f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-                )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
