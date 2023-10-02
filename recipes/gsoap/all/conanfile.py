d7bf5d88c40e9cbc9a62016147836c444be80194
Rubén Rincón Blanco <git@rinconblanco.es>
2023-10-02 20:51:02 +0200

    Add zlib range

--- recipes/gsoap/all/conanfile.py
+++ recipes/gsoap/all/conanfile.py
@@ -52,7 +52,7 @@ class GsoapConan(ConanFile):
     def requirements(self):
         if self.options.with_openssl:
             self.requires("openssl/[>=1.1 <4]", transitive_headers=True)
-            self.requires("zlib/1.2.13")
+            self.requires("zlib/[>=1.2.11 <2]")
 
     def build_requirements(self):
         if cross_building(self, skip_x64_x86=True) and hasattr(self, "settings_build"):
