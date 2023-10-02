52d88ecfa1d41a1dc24f0001640ddc1367301bc3
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-10-02 15:37:59 -0400

    Add period to end of description

--- recipes/cpptrace/all/conanfile.py
+++ recipes/cpptrace/all/conanfile.py
@@ -12,7 +12,7 @@ required_conan_version = ">=1.53.0"
 
 class PackageConan(ConanFile):
     name = "cpptrace"
-    description = "Cpptrace is a lightweight C++ stacktrace library supporting C++11 and greater on Linux, macOS, and Windows including MinGW and Cygwin environments. The goal: Make stack traces simple for once"
+    description = "Cpptrace is a lightweight C++ stacktrace library supporting C++11 and greater on Linux, macOS, and Windows including MinGW and Cygwin environments. The goal: Make stack traces simple for once."
     license = ("MIT", "LGPL-2.1-only", "BSD-2-Clause-Views")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/jeremy-rifkin/cpptrace"
