56e4881510f98d656c028cfa4f391facda1bc5ec
Gareth Lloyd <gareth@ignition-web.co.uk>
2023-10-03 22:39:08 +0100

    Fix libdispatch for conan v2

--- recipes/libdispatch/all/conanfile.py
+++ recipes/libdispatch/all/conanfile.py
@@ -1,6 +1,7 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
-import os
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.files import get, copy
+from conan.errors import ConanInvalidConfiguration
 
 required_conan_version = ">=1.32.0"
 
@@ -11,48 +12,36 @@ class LibDispatchConan(ConanFile):
     topics = ("conan", "libdispatch", "apple", "GCD", "concurrency")
     url = "https://github.com/conan-io/conan-center-index"
     license = "Apache-2.0"
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
 
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
-    _cmake = None
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def validate(self):
         if self.settings.compiler != "clang":
             raise ConanInvalidConfiguration("Clang compiler is required.")
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "swift-corelibs-{}-swift-{}-RELEASE".format(self.name, self.version)
-        os.rename(extracted_dir, self._source_subfolder)
-
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
+        cmake.install()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        copy(self, "LICENSE" , self.source_folder, self.package_folder, keep_path=False)
 
     def package_info(self):
         if self.settings.os == "Macos":
--- recipes/libdispatch/all/test_package/conanfile.py
+++ recipes/libdispatch/all/test_package/conanfile.py
@@ -1,16 +1,25 @@
 import os
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package", "cmake"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        if not tools.cross_building(self.settings):
-            self.run(os.path.join("bin","test_package"), run_environment=True)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(cmd, env="conanrun")
