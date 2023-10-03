c6172966f36a1bc289152e4ef9654d7ae16f54b9
Martin Valgur <martin.valgur@gmail.com>
2023-10-03 11:41:09 +0300

    aeron: bump deps

--- recipes/aeron/all/conanfile.py
+++ recipes/aeron/all/conanfile.py
@@ -17,7 +17,7 @@ class AeronConan(ConanFile):
     description = "Efficient reliable UDP unicast, UDP multicast, and IPC message transport"
     topics = ("udp", "messaging", "low-latency")
     url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/real-logic/aeron/wiki"
+    homepage = "https://github.com/real-logic/aeron"
     license = "Apache-2.0"
 
     package_type = "library"
@@ -72,7 +72,7 @@ class AeronConan(ConanFile):
             raise ConanInvalidConfiguration("This platform (os=Macos arch=armv8) is not yet supported by this recipe")
 
     def build_requirements(self):
-        self.tool_requires("zulu-openjdk/11.0.15")
+        self.tool_requires("zulu-openjdk/11.0.19")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
