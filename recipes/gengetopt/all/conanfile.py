8e138dc4fe41cdf9d73fea059b8ffdcbcde719ca
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2023-10-04 18:26:50 +0100

    conanfile update

--- recipes/gengetopt/all/conanfile.py
+++ recipes/gengetopt/all/conanfile.py
@@ -1,10 +1,9 @@
-
-import os
-
 from conan import ConanFile
 from conan.tools.gnu import AutotoolsToolchain, Autotools, AutotoolsDeps
 from conan.tools.layout import basic_layout
-from conan.tools.files import chdir, get
+from conan.tools.files import get, mkdir
+
+import pathlib
 
 
 class gengetoptConan(ConanFile):
@@ -12,15 +11,18 @@ class gengetoptConan(ConanFile):
     package_type = "application"
 
     # Optional metadata
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of {package_name} here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    license = "GPL-3.0-or-later"
+    url = "https://github.com/conan-io/conan-center-index"
+    description = "A tool to write command line option parsing code for C programs."
+    topics = ("gnu", "generator", "parsing", "command-line")
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
 
+    def build_requirements(self):
+        self.tool_requires("gengen/1.4.2")
+        self.tool_requires("flex/2.6.4")
+        self.tool_requires("bison/3.8.2")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version],
@@ -37,11 +39,19 @@ class gengetoptConan(ConanFile):
         at_toolchain.generate()
 
     def build(self):
+
+        self.run("which gengen")
+
         autotools = Autotools(self)
-        autotools.autoreconf()
+#        autotools.autoreconf()
         autotools.configure()
         autotools.make()
 
     def package(self):
         autotools = Autotools(self)
         autotools.install()
+
+        lic_path = pathlib.Path(self.package_folder) / "licenses"
+        mkdir(lic_path)
+        copy(self, "COPYING", self.source_folder, lic_path)
+        copy(self, "LICENSE", self.source_folder, lic_path)
