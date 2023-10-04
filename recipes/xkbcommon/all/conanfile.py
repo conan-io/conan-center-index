72167de2a47c661b07783cb04a4611c356bb6f76
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-10-04 11:24:00 +0200

    wayland-protocols is a tool_requirement

--- recipes/xkbcommon/all/conanfile.py
+++ recipes/xkbcommon/all/conanfile.py
@@ -78,8 +78,9 @@ class XkbcommonConan(ConanFile):
         self.tool_requires("bison/3.8.2")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/2.0.3")
-        if self._has_build_profile and self.options.get_safe("with_wayland"):
-            self.tool_requires("wayland/<host_version>")
+        if self.options.get_safe("with_wayland"):
+            if self._has_build_profile:
+                self.tool_requires("wayland/<host_version>")
             self.tool_requires("wayland-protocols/1.31")
 
     def source(self):
