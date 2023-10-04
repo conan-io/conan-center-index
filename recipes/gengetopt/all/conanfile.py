7580e1f1e57b073152e49494219cd8bf2170dfc0
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2023-10-04 16:29:33 +0100

    start on gengetopt recipe

---
+++ recipes/gengetopt/all/conanfile.py
@@ -0,0 +1,46 @@
+
+import os
+
+from conan import ConanFile
+from conan.tools.gnu import AutotoolsToolchain, Autotools, AutotoolsDeps
+from conan.tools.layout import basic_layout
+from conan.tools.files import chdir
+
+
+class gengetoptConan(ConanFile):
+    name = "gengetopt"
+    package_type = "application"
+
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of {package_name} here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "configure.ac", "Makefile.am", "src/*"
+
+    
+
+    def layout(self):
+        basic_layout(self)
+
+    def generate(self):
+        deps = AutotoolsDeps(self)
+        deps.generate()
+        at_toolchain = AutotoolsToolchain(self)
+        at_toolchain.generate()
+
+    def build(self):
+        autotools = Autotools(self)
+        autotools.autoreconf()
+        autotools.configure()
+        autotools.make()
+
+    def package(self):
+        autotools = Autotools(self)
+        autotools.install()
