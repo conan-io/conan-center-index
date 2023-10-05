94e5f091603f07281d72b94269dd4453a23a078d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-10-05 13:56:12 +0200

    limit the diff with previous revision

--- recipes/grpc/all/test_package/conanfile.py
+++ recipes/grpc/all/test_package/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
 from conan.tools.microsoft import is_msvc
 import os
@@ -8,7 +8,6 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps"
     test_type = "explicit"
 
     @property
@@ -26,15 +25,19 @@ class TestPackageConan(ConanFile):
         if not self._is_legacy_one_profile:
             # For the grpc-cpp-plugin executable at build time
             self.tool_requires(self.tested_reference_str)
+            # For protoc (through protobuf_generate() function) at build time
             self.tool_requires("protobuf/<host_version>")
 
     def generate(self):
-        VirtualRunEnv(self).generate()
         # Set up environment so that we can run grpc-cpp-plugin at build time
         if self._is_legacy_one_profile:
             VirtualRunEnv(self).generate(scope="build")
         else:
             VirtualBuildEnv(self).generate()
+
+        # Environment so that the compiled test executable can load shared libraries
+        runenv = VirtualRunEnv(self)
+        runenv.generate()
 
         tc = CMakeToolchain(self)
         tc.cache_variables["TEST_ACTUAL_SERVER"] = not (is_msvc(self)
@@ -46,6 +49,9 @@ class TestPackageConan(ConanFile):
         project_include = os.path.join(self.source_folder, "macos_make_override.cmake")
         tc.cache_variables["CMAKE_PROJECT_test_package_INCLUDE"] = project_include
         tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
