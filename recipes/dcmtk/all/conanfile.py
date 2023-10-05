44317d924873ddd76f5e20b8744e22940bf7d10f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-10-05 16:51:28 +0200

    fix enable_stl option mapping

--- recipes/dcmtk/all/conanfile.py
+++ recipes/dcmtk/all/conanfile.py
@@ -144,7 +144,9 @@ class DCMTKConan(ConanFile):
         tc.variables["DCMTK_WITH_DOXYGEN"] = False
         tc.variables["DCMTK_WIDE_CHAR_FILE_IO_FUNCTIONS"] = self.options.wide_io
         tc.variables["DCMTK_WIDE_CHAR_MAIN_FUNCTION"] = self.options.wide_io
-        tc.variables["DCMTK_ENABLE_STL"] = self.options.enable_stl
+        # Upstream CMakeLists really expects ON value and no other value (like TRUE) in its internal logic
+        # to enable STL features (see DCMTK_TEST_ENABLE_STL_FEATURE function in GenerateDCMTKConfigure.cmake())
+        tc.variables["DCMTK_ENABLE_STL"] = "ON" if self.options.enable_stl else "OFF"
         tc.variables["DCMTK_ENABLE_CXX11"] = True
         tc.variables["DCMTK_ENABLE_MANPAGE"] = False
         tc.cache_variables["DCMTK_DEFAULT_DICT"] = self.options.default_dict
