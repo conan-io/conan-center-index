24adb50ef8df782b49c2a5074bfb3899856dd05e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-10-05 04:12:22 +0200

    (#20331) playrho: cleanup recipe in order to remove unnecessay/misleading logic

--- recipes/playrho/all/conanfile.py
+++ recipes/playrho/all/conanfile.py
@@ -3,7 +3,7 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, rename
 from conan.tools.scm import Version
 
@@ -28,6 +28,10 @@ class PlayrhoConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
+
+    @property
+    def _min_cppstd(self):
+        return "17"
 
     @property
     def _compilers_minimum_versions(self):
@@ -55,18 +59,12 @@ class PlayrhoConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, 17)
+            check_min_cppstd(self, self._min_cppstd)
 
-        compilers_minimum_version = self._compilers_minimum_versions
-        minimum_version = compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version:
-            if Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration(
-                    f"{self.name} requires C++17, which your compiler does not support."
-                )
-        else:
-            self.output.warning(
-                f"{self.name} requires C++17. Your compiler is unknown. Assuming it supports C++17."
+        minimum_version = self._compilers_minimum_versions.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
 
     def source(self):
@@ -78,9 +76,6 @@ class PlayrhoConan(ConanFile):
         tc.variables["PLAYRHO_BUILD_SHARED"] = self.options.shared
         tc.variables["PLAYRHO_BUILD_STATIC"] = not self.options.shared
         tc.variables["PLAYRHO_INSTALL"] = True
-        tc.generate()
-
-        tc = CMakeDeps(self)
         tc.generate()
 
     def build(self):
@@ -97,8 +92,6 @@ class PlayrhoConan(ConanFile):
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "lib", "PlayRho"))
-        for dll in (self.package_path / "lib").glob("*.dll"):
-            rename(self, dll, self.package_path / "bin" / dll.name)
 
     def package_info(self):
         self.cpp_info.libs = ["PlayRho"]
