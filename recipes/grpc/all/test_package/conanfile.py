2536354f3d9fba26342db7af7ca5db191ae15e7f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-10-05 13:52:20 +0200

    restore some trick for 1 profile in test package

--- recipes/grpc/all/test_package/conanfile.py
+++ recipes/grpc/all/test_package/conanfile.py
@@ -1,14 +1,19 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
 from conan.tools.microsoft import is_msvc
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "VirtualBuildEnv", "VirtualRunEnv"
+    generators = "CMakeDeps"
     test_type = "explicit"
+
+    @property
+    def _is_legacy_one_profile(self):
+        return not hasattr(self, "settings_build")
 
     def layout(self):
         cmake_layout(self)
@@ -18,11 +23,19 @@ class TestPackageConan(ConanFile):
         self.requires("protobuf/3.21.12")
 
     def build_requirements(self):
-        # For the grpc-cpp-plugin executable at build time
-        self.tool_requires(self.tested_reference_str)
-        self.tool_requires("protobuf/<host_version>")
+        if not self._is_legacy_one_profile:
+            # For the grpc-cpp-plugin executable at build time
+            self.tool_requires(self.tested_reference_str)
+            self.tool_requires("protobuf/<host_version>")
 
     def generate(self):
+        VirtualRunEnv(self).generate()
+        # Set up environment so that we can run grpc-cpp-plugin at build time
+        if self._is_legacy_one_profile:
+            VirtualRunEnv(self).generate(scope="build")
+        else:
+            VirtualBuildEnv(self).generate()
+
         tc = CMakeToolchain(self)
         tc.cache_variables["TEST_ACTUAL_SERVER"] = not (is_msvc(self)
                                                         and str(self.settings.compiler.version) in ("15", "191")
