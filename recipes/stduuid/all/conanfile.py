b44edbedc5bbcf27f27000329ddbd4dd5864f3cb
Alex Trotta <alext@glodonusa.com>
2023-10-09 01:17:36 -0400

    stduuid: use std::span only when available

--- recipes/stduuid/all/conanfile.py
+++ recipes/stduuid/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import copy, get
+from conan.tools.files import copy, get, replace_in_file
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
 import os
@@ -18,12 +18,10 @@ class StduuidConan(ConanFile):
     homepage = "https://github.com/mariusbancila/stduuid"
     settings = "os", "arch", "compiler", "build_type"
     options = {
+        # True: Use std::span
+        # False: Use gsl::span
         "with_cxx20_span": [True, False],
     }
-    default_options = {
-        "with_cxx20_span": False,
-    }
-    no_copy_source = True
 
     @property
     def _min_cppstd(self):
@@ -39,12 +37,50 @@ class StduuidConan(ConanFile):
             "Visual Studio": "15",
         }
 
+    @property
+    def _std_span_minimum_version(self):
+        # TODO double check these
+        return {
+            "apple-clang": "10",
+            "clang": "7",
+            "gcc": "10",
+            "msvc": "192",
+            "Visual Studio": "16",
+        }
+
+    def _supports_std_span(self):
+        # Air on the side of caution, if we are not certain then we should return false.
+        
+        if self.settings.compiler.get_safe("cppstd"):
+            # This first check alone should almost always be correct on its own, as if you're on
+            # C++20, your compiler probably supports std::span.
+            try:
+                check_min_cppstd(self, "20", False)
+            except ConanInvalidConfiguration:
+                return False
+        else:
+            self.output.info("compiler.cppstd not set, assuming std::span not supported")
+            return False
+
+        compiler_minimum_version = self._std_span_minimum_version.get(str(self.settings.compiler), False)
+        if not compiler_minimum_version:
+            self.output.info(f"Unsure if compiler '{self.settings.compiler}' supports std::span, assuming no.")
+            return False
+        if Version(self.settings.compiler.version) < compiler_minimum_version:
+            return False
+
+        return True
+    
+    def config_options(self):
+        # By default, use std::span if we're certain the profile supports it.
+        self.options.with_cxx20_span = self._supports_std_span()
+
     def layout(self):
-        basic_layout(self, src_folder="src")
+        basic_layout(self)
 
     def requirements(self):
         if not self.options.with_cxx20_span:
-            self.requires("ms-gsl/3.1.0", transitive_headers=True)
+            self.requires("ms-gsl/4.0.0", transitive_headers=True)
         if self.settings.os == "Linux" and Version(self.version) <= "1.0":
             self.requires("util-linux-libuuid/2.39", transitive_headers=True, transitive_libs=True)
 
@@ -52,7 +88,7 @@ class StduuidConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppsd"):
+        if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
 
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
@@ -66,7 +102,14 @@ class StduuidConan(ConanFile):
             destination=self.source_folder, strip_root=True)
 
     def build(self):
-        pass
+        uuid_h_file = os.path.join(self.build_folder, "..", "include", "uuid.h")
+        #replace_in_file(self, uuid_h_file, "#ifdef LIBUUID_CPP20_OR_GREATER", "#if __cpp_lib_span")
+        if self.options.with_cxx20_span:
+            replace_in_file(self, uuid_h_file, "#ifdef LIBUUID_CPP20_OR_GREATER", "#if 1")
+            replace_in_file(self, uuid_h_file, "#ifdef __cpp_lib_span", "#if 1")
+        else:
+            replace_in_file(self, uuid_h_file, "#ifdef LIBUUID_CPP20_OR_GREATER", "#if 0")
+            replace_in_file(self, uuid_h_file, "#ifdef __cpp_lib_span", "#if 0")
 
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
@@ -75,5 +118,3 @@ class StduuidConan(ConanFile):
     def package_info(self):
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
-        if not self.options.with_cxx20_span:
-            self.cpp_info.includedirs.append(os.path.join(self.dependencies["ms-gsl"].cpp_info.includedirs[0], "gsl"))
