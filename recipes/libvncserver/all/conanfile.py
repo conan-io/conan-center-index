09f025d3dc28cb2025287b82c34b1d9aab61c553
MALASHKIN Andrei <andrei.malashkin-ext@hexagon.com>
2023-10-09 14:04:07 +0200

    add forgotten include

--- recipes/libvncserver/all/conanfile.py
+++ recipes/libvncserver/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, load, replace_in_file, copy, rmdir
 from conan.tools.scm import Version
