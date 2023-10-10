c23d2c46b495e83880bd8d8ab7408682cefd43fd
Martin Delille <martin@delille.org>
2023-10-10 19:11:24 +0200

    Use uncrustify as a build_requirement in test_package

--- recipes/uncrustify/all/test_package/conanfile.py
+++ recipes/uncrustify/all/test_package/conanfile.py
@@ -4,12 +4,11 @@ from conan.tools.build import can_run
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "VirtualRunEnv"
+    generators = "VirtualBuildEnv"
     test_type = "explicit"
 
-    def requirements(self):
-        self.requires(self.tested_reference_str)
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def test(self):
-        if can_run(self):
-            self.run("uncrustify --version", env="conanrun")
+        self.run("uncrustify --version", env="conanrun")
