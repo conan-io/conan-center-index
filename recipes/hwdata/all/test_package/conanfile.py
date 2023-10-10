75d651da56f4f085be46da86e722bdd7ded73994
Jordan Williams <jordan@jwillikers.com>
2023-10-10 08:41:49 -0500

    Remove dependency on Meson in the test package

--- recipes/hwdata/all/test_package/conanfile.py
+++ recipes/hwdata/all/test_package/conanfile.py
@@ -1,7 +1,8 @@
+import os
+
 from conan import ConanFile
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.gnu import PkgConfigDeps
-from conan.tools.meson import Meson, MesonToolchain
+from conan.tools.gnu import PkgConfig, PkgConfigDeps
 from conan.tools.layout import basic_layout
 
 
@@ -12,25 +13,22 @@ class TestPackageConan(ConanFile):
     def layout(self):
         basic_layout(self)
 
-    def requirements(self):
-        self.requires(self.tested_reference_str)
-
     def build_requirements(self):
-        self.tool_requires("meson/1.2.2")
+        self.tool_requires(self.tested_reference_str)
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/2.0.3")
 
     def generate(self):
-        meson_toolchain = MesonToolchain(self)
-        meson_toolchain.generate()
         pkg_config_deps = PkgConfigDeps(self)
+        pkg_config_deps.build_context_activated = ["hwdata"]
         pkg_config_deps.generate()
         virtual_build_env = VirtualBuildEnv(self)
         virtual_build_env.generate()
 
     def build(self):
-        meson = Meson(self)
-        meson.configure()
+        pkg_config = PkgConfig(self, "hwdata", self.generators_folder)
+        self.output.info(pkg_config.variables["pkgdatadir"])
+        assert len(pkg_config.variables["pkgdatadir"]) > 0
 
     def test(self):
         pass
