55586a09b0f54cd735e0fa06a3754df6b19af2fd
toge <toge.mail@gmail.com>
2023-10-11 00:58:38 +0900

    update ffmpeg/5.1.3

--- recipes/avcpp/all/conanfile.py
+++ recipes/avcpp/all/conanfile.py
@@ -56,7 +56,7 @@ class AvcppConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("ffmpeg/5.0")
+        self.requires("ffmpeg/5.1.3")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
