3af06d52efd4d7f3f5955cd8a875ead0855ed03b
Jordan Williams <jordan@jwillikers.com>
2023-10-11 09:41:48 -0500

    Update recipes/hwdata/all/test_package/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/hwdata/all/test_package/conanfile.py
+++ recipes/hwdata/all/test_package/conanfile.py
@@ -1,32 +1,16 @@
 from conan import ConanFile
-from conan.tools.env import VirtualBuildEnv
-from conan.tools.gnu import PkgConfig, PkgConfigDeps
-from conan.tools.layout import basic_layout
+from conan.tools.files import save, load
+import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     test_type = "explicit"
 
-    def layout(self):
-        basic_layout(self)
-
     def requirements(self):
         self.requires(self.tested_reference_str)
-
-    def build_requirements(self):
-        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.0.3")
-
-    def generate(self):
-        pkg_config_deps = PkgConfigDeps(self)
-        pkg_config_deps.generate()
-        virtual_build_env = VirtualBuildEnv(self)
-        virtual_build_env.generate()
-
-    def build(self):
-        pkg_config = PkgConfig(self, "hwdata", self.generators_folder)
-        assert len(pkg_config.variables["pkgdatadir"]) > 0
+        save(self, "hwdata_pkg_dir", self.dependencies[self.tested_reference_str].package_folder)
 
     def test(self):
-        pass
+        pkg_dir = load(self, "hwdata_pkg_dir")
+        assert os.path.isfile(os.path.join(pkg_dir, "res", "hwdata", "usb.ids"))
