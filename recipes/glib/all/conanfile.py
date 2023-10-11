49c3522013ee39a70498f43a9c5cfe6721ec5b12
MALASHKIN Andrei <andrei.malashkin-ext@hexagon.com>
2023-10-11 14:11:03 +0200

    bump glib dependencies

--- recipes/glib/all/conanfile.py
+++ recipes/glib/all/conanfile.py
@@ -72,7 +72,7 @@ class GLibConan(ConanFile):
         if self.options.get_safe("with_mount"):
             self.requires("libmount/2.39")
         if self.options.get_safe("with_selinux"):
-            self.requires("libselinux/3.3")
+            self.requires("libselinux/3.5")
         if self.settings.os != "Linux":
             # for Linux, gettext is provided by libc
             self.requires("libgettext/0.22", transitive_headers=True, transitive_libs=True)
@@ -81,9 +81,9 @@ class GLibConan(ConanFile):
             self.requires("libiconv/1.17")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.2.1")
+        self.tool_requires("meson/[>=1 <2]")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/2.0.3")
+            self.tool_requires("pkgconf/[>=2 <3]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
