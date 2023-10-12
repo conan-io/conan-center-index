79b408e995cb18fff78ea0132d6a07fc705b14e3
seladb <pcapplusplus@gmail.com>
2023-10-11 22:33:51 -0700

    Update get source

--- recipes/pcapplusplus/cmake/conanfile.py
+++ recipes/pcapplusplus/cmake/conanfile.py
@@ -34,9 +34,7 @@ class PcapplusplusConan(ConanFile):
             raise ConanInvalidConfiguration(f"{self.settings.os} is not supported")
 
     def source(self):
-        get(
-            self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True
-        )
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
