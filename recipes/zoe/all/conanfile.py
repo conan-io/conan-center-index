56013fc4df898c44153da00af2ca548741023fb4
toge <toge.mail@gmail.com>
2023-10-14 11:38:48 +0900

    add package_type

--- recipes/zoe/all/conanfile.py
+++ recipes/zoe/all/conanfile.py
@@ -16,6 +16,7 @@ class ZoeConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/winsoft666/zoe"
     topics = ("downloader", "libcurl", "speed-limit", "ftp", "http")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
