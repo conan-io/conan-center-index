ba13a6c28ae6b6b331c4a08bc2aeee4a6a9d61aa
toge <toge.mail@gmail.com>
2023-10-16 01:28:12 +0900

    link functions, but don't execute functions

--- recipes/clipboard_lite/all/test_package/test_package.cpp
+++ recipes/clipboard_lite/all/test_package/test_package.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include "Clipboard_Lite.h"
 
-int main(void) {
+// just compile it, don't execute it.
+// clipboard_lite execute `assert()` with false when test_package runs in headless environment.
+auto create_impl() {
     auto clipboard = SL::Clipboard_Lite::CreateClipboard()
-        ->onText([](const std::string& text) {
-            std::cout << text << std::endl;
-        })
-        ->onImage([&](const SL::Clipboard_Lite::Image& image) {
-            std::cout << "onImage Height=" << image.Height << " Width=" << image.Width << std::endl;
-        })
-        ->run();
+    ->onText([](const std::string& text) {
+        std::cout << text << std::endl;
+    })
+    ->onImage([&](const SL::Clipboard_Lite::Image& image) {
+        std::cout << "onImage Height=" << image.Height << " Width=" << image.Width << std::endl;
+    })
+    ->run();
+
+    return clipboard;
+}
+
+int main(void) {
+    auto* create = create_impl;
 
     return 0;
 }
