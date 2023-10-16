bd7d4b143ceb16ee431e782e2e01f4a8fa02c433
Jordan Williams <jordan@jwillikers.com>
2023-10-16 06:55:41 -0500

    Link transtively against Qt's headers

--- recipes/qcustomplot/all/conanfile.py
+++ recipes/qcustomplot/all/conanfile.py
@@ -46,9 +46,9 @@ class QCustomPlotConan(ConanFile):
 
     def requirements(self):
         if Version(self.version) >= "2.0.0":
-            self.requires("qt/6.6.0")
+            self.requires("qt/6.6.0", transitive_headers=True)
         else:
-            self.requires("qt/5.15.11")
+            self.requires("qt/5.15.11", transitive_headers=True)
         if self.options.with_opengl and self.settings.os == "Windows":
             self.requires("opengl/system")
 
