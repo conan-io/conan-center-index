5c8464914230efebd9ea26b5c138ba1cd41d8615
toge <toge.mail@gmail.com>
2023-10-17 01:53:24 +0900

    remove unused imports

--- recipes/nghttp3/all/conanfile.py
+++ recipes/nghttp3/all/conanfile.py
@@ -1,11 +1,9 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import get, rmdir, copy
 from conan.tools.microsoft import is_msvc
-from conan.tools.scm import Version
 import os
 
 
