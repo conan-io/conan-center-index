fe6cb43b68ca8f801922847bffe65fdc4dda1b3c
Alex Trotta <alext@glodonusa.com>
2023-10-17 01:58:39 -0400

    Lint fixes

--- recipes/andreasbuhr-cppcoro/all/conanfile.py
+++ recipes/andreasbuhr-cppcoro/all/conanfile.py
@@ -36,7 +36,7 @@ class AndreasbuhrCppCoroConan(ConanFile):
         }
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -46,18 +46,18 @@ class AndreasbuhrCppCoroConan(ConanFile):
         # We can't simply check for C++20, because clang and MSVC support the coroutine TS despite not having labeled (__cplusplus macro) C++20 support
         min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
         if not min_version:
-            self.output.warning("{} recipe lacks information about the {} compiler support.".format(
-                self.name, self.settings.compiler))
+            self.output.warning(f"{self.name} recipe lacks information about the {self.settings.compiler} compiler support.")
         else:
             if Version(self.settings.compiler.version) < min_version:
-                raise ConanInvalidConfiguration("{} requires coroutine TS support. The current compiler {} {} does not support it.".format(
-                    self.name, self.settings.compiler, self.settings.compiler.version))
+                raise ConanInvalidConfiguration(
+                    f"{self.name} requires coroutine TS support. The current compiler {self.settings.compiler} {self.settings.compiler.version} does not support it."
+                )
 
         # Currently clang expects coroutine to be implemented in a certain way (under std::experiemental::), while libstdc++ puts them under std::
         # There are also other inconsistencies, see https://bugs.llvm.org/show_bug.cgi?id=48172
         # This should be removed after both gcc and clang implements the final coroutine TS
         if self.settings.compiler == "clang" and self.settings.compiler.get_safe("libcxx") == "libstdc++":
-            raise ConanInvalidConfiguration("{} does not support clang with libstdc++. Use libc++ instead.".format(self.name))
+            raise ConanInvalidConfiguration(f"{self.name} does not support clang with libstdc++. Use libc++ instead.")
 
     def configure(self):
         if self.options.shared:
@@ -96,7 +96,7 @@ class AndreasbuhrCppCoroConan(ConanFile):
         elif self.settings.compiler == "gcc":
             self.cpp_info.cxxflags.append("-fcoroutines")
             self.cpp_info.defines.append("CPPCORO_COMPILER_SUPPORTS_SYMMETRIC_TRANSFER=1")
-        elif self.settings.compiler == "clang" or self.settings.compiler == "apple-clang":
+        elif self.settings.compiler in ["clang", "apple-clang"]:
             self.cpp_info.cxxflags.append("-fcoroutines-ts")
 
         self.cpp_info.set_property("cmake_file_name", "cppcoro")
