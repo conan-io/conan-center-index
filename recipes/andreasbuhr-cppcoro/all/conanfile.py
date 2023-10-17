a9099c04e6646f92ca93f63e9bbfb9acb38ac185
Alex Trotta <alext@glodonusa.com>
2023-10-17 11:03:24 -0400

    cppcoro: Review suggestions and other PR changes

--- recipes/andreasbuhr-cppcoro/all/conanfile.py
+++ recipes/andreasbuhr-cppcoro/all/conanfile.py
@@ -1,11 +1,12 @@
 import os
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import rmdir, get, copy
 from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.49.0"
+required_conan_version = ">=1.53.0"
 
 class AndreasbuhrCppCoroConan(ConanFile):
     name = "andreasbuhr-cppcoro"
@@ -16,6 +17,7 @@ class AndreasbuhrCppCoroConan(ConanFile):
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
     provides = "cppcoro"
+    package_type = "library"
 
     options = {
         "shared": [True, False],
@@ -30,10 +32,15 @@ class AndreasbuhrCppCoroConan(ConanFile):
     def _minimum_compilers_version(self):
         return {
             "Visual Studio": "15",
+            "msvc": "191",
             "gcc": "10",
             "clang": "8",
             "apple-clang": "10",
         }
+
+    @property
+    def _min_cppstd(self):
+        return 17
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -43,6 +50,9 @@ class AndreasbuhrCppCoroConan(ConanFile):
             self.options.rm_safe("fPIC")
 
     def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        
         # We can't simply check for C++20, because clang and MSVC support the coroutine TS despite not having labeled (__cplusplus macro) C++20 support
         min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
         if not min_version:
@@ -52,12 +62,6 @@ class AndreasbuhrCppCoroConan(ConanFile):
                 raise ConanInvalidConfiguration(
                     f"{self.name} requires coroutine TS support. The current compiler {self.settings.compiler} {self.settings.compiler.version} does not support it."
                 )
-
-        # Currently clang expects coroutine to be implemented in a certain way (under std::experiemental::), while libstdc++ puts them under std::
-        # There are also other inconsistencies, see https://bugs.llvm.org/show_bug.cgi?id=48172
-        # This should be removed after both gcc and clang implements the final coroutine TS
-        if self.settings.compiler == "clang" and self.settings.compiler.get_safe("libcxx") == "libstdc++":
-            raise ConanInvalidConfiguration(f"{self.name} does not support clang with libstdc++. Use libc++ instead.")
 
     def configure(self):
         if self.options.shared:
@@ -78,7 +82,7 @@ class AndreasbuhrCppCoroConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "LICENSE.txt", dst="licenses", src=self.source_folder)
+        copy(self, "LICENSE.txt", dst=os.path.join(self.package_folder,"licenses"), src=self.source_folder)
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
@@ -96,7 +100,8 @@ class AndreasbuhrCppCoroConan(ConanFile):
         elif self.settings.compiler == "gcc":
             self.cpp_info.cxxflags.append("-fcoroutines")
             self.cpp_info.defines.append("CPPCORO_COMPILER_SUPPORTS_SYMMETRIC_TRANSFER=1")
-        elif self.settings.compiler in ["clang", "apple-clang"]:
+        elif (self.settings.compiler == "clang" and self.settings.compiler.version < Version("16")) or \
+             (self.settings.compiler == "apple-clang"):
             self.cpp_info.cxxflags.append("-fcoroutines-ts")
 
         self.cpp_info.set_property("cmake_file_name", "cppcoro")
