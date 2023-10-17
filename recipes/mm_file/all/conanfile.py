e699fbf187e4fee5a130a98a0b913e8fd65bcccf
toge <toge.mail@gmail.com>
2023-10-17 11:28:24 +0900

    specify C++11

--- recipes/mm_file/all/conanfile.py
+++ recipes/mm_file/all/conanfile.py
@@ -18,11 +18,19 @@ class MMFileConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
+    @property
+    def _min_cppstd(self):
+        return 11
+
     def layout(self):
         basic_layout(self, src_folder="src")
 
     def package_id(self):
         self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
 
     def validate(self):
         if self.settings.os == "Windows":
