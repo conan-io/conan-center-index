4ae00660bec80f517bec0db41bbc3ffd9273958a
Jordan Williams <jordan@jwillikers.com>
2023-10-18 08:16:13 -0500

    Simplify copy command

--- docs/package_templates/meson_package/all/conanfile.py
+++ docs/package_templates/meson_package/all/conanfile.py
@@ -135,7 +135,7 @@ class PackageConan(ConanFile):
         meson.build()
 
     def package(self):
-        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
         meson = Meson(self)
         meson.install()
 
