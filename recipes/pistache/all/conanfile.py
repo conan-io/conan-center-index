aa1dd8fbe8eba5e4bc9cb20e83f3cef1db3b6e97
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-10-21 03:32:55 +0000

    pistache/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericlemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/pistache/all/conanfile.py
+++ recipes/pistache/all/conanfile.py
@@ -80,7 +80,7 @@ class PistacheConan(ConanFile):
 
     def build_requirements(self):
         if self.version != "cci.20201127":
-            self.tool_requires("meson/1.2.2")
+            self.tool_requires("meson/1.2.3")
             if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
                 self.tool_requires("pkgconf/2.0.3")
 
