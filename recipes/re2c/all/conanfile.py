677706b2235183f3215fac999d619d99a7db8959
Martin Valgur <martin.valgur@gmail.com>
2023-10-23 17:28:08 +0300

    re2c: drop message for PATH

--- recipes/re2c/all/conanfile.py
+++ recipes/re2c/all/conanfile.py
@@ -105,5 +105,4 @@ class Re2CConan(ConanFile):
 
         # TODO: to remove in conan v2
         bin_path = os.path.join(self.package_folder, "bin")
-        self.output.info(f"Appending PATH environment variable: {bin_path}")
         self.env_info.PATH.append(bin_path)
