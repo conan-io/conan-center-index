892d65984353c17b9616a6fd427515ca8b4bb969
toge <toge.mail@gmail.com>
2023-10-24 01:21:42 +0900

    add build_requirements

--- recipes/escape/all/conanfile.py
+++ recipes/escape/all/conanfile.py
@@ -54,6 +54,9 @@ class EscapeConan(ConanFile):
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
 
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.16 <4]")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
