b3bb95be8e41d8bb9e86a7da91c0157b873e4b08
Martin Valgur <martin.valgur@gmail.com>
2023-10-25 02:11:23 +0300

    apriltag: fix shared build

--- recipes/apriltag/all/conanfile.py
+++ recipes/apriltag/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, replace_in_file
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 import os
@@ -69,8 +69,16 @@ class ApriltagConan(ConanFile):
             deps = CMakeDeps(self)
             deps.generate()
 
-    def build(self):
+    def _patch_sources(self):
         apply_conandata_patches(self)
+        # Fix DLL installation
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}",
+                        "ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}\n"
+                        "RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}")
+
+    def build(self):
+        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
