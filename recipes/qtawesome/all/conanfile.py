0feee2a341b0cdc7624d40f580d88bfaea9a9c72
Daniel <danimanzaneque@gmail.com>
2023-10-24 12:54:41 +0200

    Update recipes/qtawesome/all/conanfile.py
    Co-authored-by: Francisco Ramírez <franchuti688@gmail.com>

--- recipes/qtawesome/all/conanfile.py
+++ recipes/qtawesome/all/conanfile.py
@@ -60,7 +60,7 @@ class QtAwesomeConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("qt/5.15.9", transitive_headers=True, transitive_libs=True)
+        self.requires("qt/5.15.9", transitive_headers=True)
 
     def validate(self):
         if self.settings.compiler.cppstd:
