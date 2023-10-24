696a61eb57c2f73a526096fcdae9933161c22f99
Martin Valgur <martin.valgur@gmail.com>
2023-10-24 21:44:28 +0300

    libcheck: adjust required_conan_version

--- recipes/libcheck/all/conanfile.py
+++ recipes/libcheck/all/conanfile.py
@@ -2,10 +2,10 @@ import os
 
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, save
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.microsoft import is_msvc
 
-required_conan_version = ">=1.47.0"
+required_conan_version = ">=1.52.0"
 
 
 class LibCheckConan(ConanFile):
