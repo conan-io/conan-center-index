93256fde233b93db55319e553efe9af43593e817
Martin Valgur <martin.valgur@gmail.com>
2023-10-24 09:49:12 +0300

    (#19096) acado: migrate to Conan v2, unvendor qpOASES-3
    * acado: migrate to Conan v2, unvendor qpOASES-3
    
    * acado: clean up a bit
    
    * acado: fix cmake_find_package support
    
    * acado: fixes
    
    * acado: remove logging for set env var
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/acado/all/conanfile.py
+++ recipes/acado/all/conanfile.py
@@ -1,19 +1,24 @@
 import os
-import glob
 
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile, conan_version
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
+from conan.tools.microsoft import is_msvc
+
+required_conan_version = ">=1.53.0"
 
 
 class AcadoConan(ConanFile):
     name = "acado"
-    description = "ACADO Toolkit is a software environment and algorithm collection for automatic control and dynamic optimization."
+    description = ("ACADO Toolkit is a software environment and algorithm "
+                   "collection for automatic control and dynamic optimization.")
     license = "LGPL-3.0"
-    topics = ("conan", "acado", "control", "optimization", "mpc")
-    homepage = "https://github.com/acado/acado"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = ["CMakeLists.txt", "cmake/qpoases.cmake", "patches/**"]
-    generators = "cmake"
+    homepage = "https://github.com/acado/acado"
+    topics = ("control", "optimization", "mpc")
+
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -25,12 +30,11 @@ class AcadoConan(ConanFile):
         "fPIC": True,
         "codegen_only": True,
     }
+    short_paths = True
 
-    _cmake = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def export_sources(self):
+        export_conandata_patches(self)
+        copy(self, "cmake/qpoases.cmake", src=self.recipe_folder, dst=self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -38,93 +42,120 @@ class AcadoConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = glob.glob("acado-*/")[0]
-        os.rename(extracted_dir, self._source_subfolder)
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-
-        self._cmake.definitions["ACADO_BUILD_SHARED"] = self.options.shared
-        self._cmake.definitions["ACADO_BUILD_STATIC"] = not self.options.shared
-
-        self._cmake.definitions["ACADO_WITH_EXAMPLES"] = False
-        self._cmake.definitions["ACADO_WITH_TESTING"] = False
-        self._cmake.definitions["ACADO_DEVELOPER"] = False
-        self._cmake.definitions["ACADO_INTERNAL"] = False
-        self._cmake.definitions["ACADO_BUILD_CGT_ONLY"] = self.options.codegen_only
-
-        # ACADO logs 170.000 lines of warnings, so we disable them
-        self._cmake.definitions["CMAKE_C_FLAGS"] = "-w"
-        self._cmake.definitions["CMAKE_CXX_FLAGS"] = "-w"
-
-        self._cmake.configure()
-        return self._cmake
-
-    def _patch_sources(self):
-        for patch in self.conan_data["patches"][self.version]:
-            tools.patch(**patch)
-
-    def build(self):
-        self._patch_sources()
-        cmake = self._configure_cmake()
-        cmake.build()
-
-    @property
-    def _qpoases_sources(self):
-        return os.path.join("lib", "cmake", "qpoases")
-
-    def package(self):
-        self.copy("LICENSE.txt", src=self._source_subfolder, dst="licenses")
-        cmake = self._configure_cmake()
-        cmake.install()
-
-        self.copy("*", src="lib", dst="lib")
-        self.copy("qpoases.cmake", src="cmake", dst="lib/cmake")
-        qpoases_sources_from = os.path.join(self.package_folder, "share", "acado", "external_packages", "qpoases")
-        self.copy("*", src=qpoases_sources_from, dst=self._qpoases_sources)
-
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        tools.remove_files_by_mask(self.package_folder, "*.pdb")
-
-    def package_info(self):
-        acado_template_paths = os.path.join(self.package_folder, "include", "acado", "code_generation", "templates")
-        self.output.info("Setting ACADO_TEMPLATE_PATHS environment variable: {}".format(acado_template_paths))
-        self.env_info.ACADO_TEMPLATE_PATHS = acado_template_paths
-
-        if self.options.shared:
-            self.cpp_info.libs = ["acado_toolkit_s", "acado_casadi"]
-        else:
-            self.cpp_info.libs = ["acado_toolkit", "acado_casadi"]
-
-        self.cpp_info.names["cmake_find_package"] = "ACADO"
-        self.cpp_info.names["cmake_find_package_multi"] = "ACADO"
-
-        self.cpp_info.builddirs.append(os.path.join("lib", "cmake"))
-        self.cpp_info.build_modules.append(os.path.join("lib", "cmake", "qpoases.cmake"))
-
-        self.cpp_info.includedirs.append(os.path.join("include", "acado"))
-        self.cpp_info.includedirs.append(self._qpoases_sources)
-        self.cpp_info.includedirs.append(os.path.join(self._qpoases_sources, "INCLUDE"))
-        self.cpp_info.includedirs.append(os.path.join(self._qpoases_sources, "SRC"))
+    def requirements(self):
+        # Note: ACADO also separately vendors and exports qpOASES v1.3 for code generation.
+        self.requires("qpoases/3.2.1")
 
     def validate(self):
-        if self.settings.compiler == "Visual Studio" and self.options.shared:
+        if is_msvc(self) and self.options.shared:
             # https://github.com/acado/acado/blob/b4e28f3131f79cadfd1a001e9fff061f361d3a0f/CMakeLists.txt#L77-L80
             raise ConanInvalidConfiguration("Acado does not support shared builds on Windows.")
         if self.settings.compiler == "apple-clang":
             raise ConanInvalidConfiguration("apple-clang not supported")
         if self.settings.compiler == "clang" and self.settings.compiler.version == "9":
             raise ConanInvalidConfiguration("acado can not be built by Clang 9.")
+        if conan_version.major < 2 and self.settings.compiler in ["clang", "gcc"] and self.settings.compiler.libcxx != "libstdc++11":
+            raise ConanInvalidConfiguration("libstdc++11 required")
 
-        # acado requires libstdc++11 for shared builds
-        # https://github.com/conan-io/conan-center-index/pull/3967#issuecomment-752985640
-        if self.options.shared and self.settings.compiler == "clang" and self.settings.compiler.libcxx != "libstdc++11":
-            raise ConanInvalidConfiguration("libstdc++11 required")
-        if self.options.shared and self.settings.compiler == "gcc" and self.settings.compiler.libcxx != "libstdc++11":
-            raise ConanInvalidConfiguration("libstdc++11 required")
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["ACADO_BUILD_SHARED"] = self.options.shared
+        tc.cache_variables["ACADO_BUILD_STATIC"] = not self.options.shared
+        tc.cache_variables["ACADO_BUILD_CGT_ONLY"] = self.options.codegen_only
+        tc.cache_variables["ACADO_WITH_EXAMPLES"] = False
+        tc.cache_variables["ACADO_WITH_TESTING"] = False
+        tc.cache_variables["ACADO_DEVELOPER"] = False
+        tc.cache_variables["ACADO_INTERNAL"] = False
+        # The build logs 170,000 lines of warnings otherwise
+        tc.variables["CMAKE_C_FLAGS"] = "-w"
+        tc.variables["CMAKE_CXX_FLAGS"] = "-w"
+        tc.variables["CMAKE_CXX_STANDARD"] = 11
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def _patch_sources(self):
+        apply_conandata_patches(self)
+        # TODO: Use Eigen from Conan. Failed with static assert errors even if using matching eigen/3.2.0 version for some reason.
+        # rmdir(self, os.path.join(self.source_folder, "external_packages", "eigen3"))
+        # replace_in_file(self, os.path.join(self.source_folder, "acado/matrix_vector/matrix_vector_tools.hpp"),
+        #                 "external_packages/eigen3/", "")
+
+    def build(self):
+        self._patch_sources()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    @property
+    def _qpoases_sources(self):
+        return os.path.join(self.package_folder, "lib", "cmake", "qpoases")
+
+    def package(self):
+        copy(self, "LICENSE.txt",
+             src=self.source_folder,
+             dst=os.path.join(self.package_folder, "licenses"))
+
+        cmake = CMake(self)
+        cmake.install()
+
+        if not self.options.shared:
+            # Copy internal static acado_casadi lib
+            copy(self, "*acado_casadi*",
+                 src=os.path.join(self.build_folder, "lib"),
+                 dst=os.path.join(self.package_folder, "lib"),
+                 keep_path=False)
+
+        # Copy embedded qpOASES v1.3 sources for code generation
+        copy(self, "qpoases.cmake",
+             src=os.path.join(self.export_sources_folder, "cmake"),
+             dst=os.path.join(self.package_folder, "lib", "cmake"))
+        copy(self, "*",
+             src=os.path.join(self.package_folder, "share", "acado", "external_packages", "qpoases"),
+             dst=self._qpoases_sources)
+
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rm(self, "*.pdb", self.package_folder, recursive=True)
+        rm(self, "*.exp", self.package_folder, recursive=True)
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "ACADO")
+        self.cpp_info.set_property("cmake_target_name", "ACADO::ACADO")
+
+        if self.options.shared:
+            self.cpp_info.libs = ["acado_toolkit_s"]
+        else:
+            self.cpp_info.libs = ["acado_toolkit", "acado_casadi"]
+
+        self.cpp_info.includedirs.append(os.path.join("include", "acado"))
+
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.append("m")
+
+        self.cpp_info.builddirs.append(os.path.join("lib", "cmake"))
+        self.cpp_info.set_property("cmake_build_modules", [os.path.join("lib", "cmake", "qpoases.cmake")])
+        self.cpp_info.includedirs += [
+            self._qpoases_sources,
+            os.path.join(self._qpoases_sources, "INCLUDE"),
+            os.path.join(self._qpoases_sources, "SRC"),
+        ]
+
+        acado_template_paths = os.path.join(self.package_folder, "include", "acado", "code_generation", "templates")
+        self.conf_info.define("user.acado:template_paths", acado_template_paths)
+        self.buildenv_info.define_path("ACADO_TEMPLATE_PATHS", acado_template_paths)
+
+        # TODO: to remove in conan v2
+        self.env_info.ACADO_TEMPLATE_PATHS = acado_template_paths
+        self.cpp_info.names["cmake_find_package"] = "ACADO"
+        self.cpp_info.names["cmake_find_package_multi"] = "ACADO"
+        self.cpp_info.build_modules["cmake_find_package"].append(os.path.join("lib", "cmake", "qpoases.cmake"))
+        self.cpp_info.build_modules["cmake_find_package_multi"].append(os.path.join("lib", "cmake", "qpoases.cmake"))
