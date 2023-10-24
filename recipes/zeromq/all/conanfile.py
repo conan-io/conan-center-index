dccb40f4b2c5c8e13489e8b980c767ad3887a22e
toge <toge.mail@gmail.com>
2023-10-24 22:06:38 +0900

    copy license file for each versions
    Co-authored-by: Jordan Williams <jordan@jwillikers.com>

--- recipes/zeromq/all/conanfile.py
+++ recipes/zeromq/all/conanfile.py
@@ -122,8 +122,10 @@ class ZeroMQConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "COPYING*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
-        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        if Version(self.version) >= "4.3.5":
+            copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        else:
+            copy(self, "COPYING*", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
         rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
