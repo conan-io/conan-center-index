8a5b17152a3d3e91beb0868d8da450d2af098f24
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-10-25 23:50:25 +0200

    simplify source()

--- recipes/netcdf/all/conanfile.py
+++ recipes/netcdf/all/conanfile.py
@@ -74,7 +74,7 @@ class NetcdfConan(ConanFile):
             self.requires("libcurl/[>=7.78.0 <9]")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
