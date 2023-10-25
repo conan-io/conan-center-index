35c9527cda00910a1749bf6f6c4352818fab4b04
Martin Valgur <martin.valgur@gmail.com>
2023-10-25 15:15:11 +0300

    subunit: add /FS to CFLAGS

--- recipes/subunit/all/conanfile.py
+++ recipes/subunit/all/conanfile.py
@@ -75,7 +75,6 @@ class SubunitConan(ConanFile):
                 self.tool_requires("msys2/cci.latest")
         if is_msvc(self):
             self.tool_requires("automake/1.16.5")
-
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
@@ -89,6 +88,7 @@ class SubunitConan(ConanFile):
 
         tc = AutotoolsToolchain(self)
         if is_msvc(self):
+            tc.extra_cflags.append("-FS")
             tc.extra_cxxflags.append("-FS")
             tc.extra_cxxflags.append("-EHsc")
         tc.configure_args.append("CHECK_CFLAGS= ")
