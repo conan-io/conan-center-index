f634cac8cf533bf559c73b5c233969634d1c5aaf
Martin Valgur <martin.valgur@gmail.com>
2023-10-25 21:43:18 +0300

    hayai: make test_package faster

--- recipes/hayai/all/test_package/test_package.cpp
+++ recipes/hayai/all/test_package/test_package.cpp
@@ -3,6 +3,6 @@
 #include <chrono>
 #include <thread>
 
-BENCHMARK(SomeSleep, Sleep10ms, 5, 10) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+BENCHMARK(SomeSleep, Sleep1ms, 5, 10) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
 }
