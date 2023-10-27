b891f1a33f152905988c1b477e846dd7821bf9ea
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-10-28 01:29:14 +0200

    fix logic of test package

--- recipes/extra-cmake-modules/all/test_package/conanfile.py
+++ recipes/extra-cmake-modules/all/test_package/conanfile.py
@@ -1,22 +1,19 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.build import can_run
+from conan.tools.cmake import CMake, cmake_layout
 import os
 
-class ExtraCMakeModulesTestConan(ConanFile):
-    settings = "os", "compiler", "arch", "build_type"
 
-    def build_requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
-        deps = CMakeDeps(self)
-        deps.generate()
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
 
     def layout(self):
         cmake_layout(self)
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -25,5 +22,5 @@ class ExtraCMakeModulesTestConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            runpath = os.path.join(self.cpp.build.bindir, "example")
-            self.run(runpath, env="conanrun")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
