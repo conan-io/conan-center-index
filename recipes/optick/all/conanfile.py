1470c5ba2c8efc45f748dcc805daed085448ab3a
Luiz Feldmann <gustavo.feldmann@hotmail.com>
2023-10-28 12:07:07 -0300

    Code review: removing unused code; adding v1 test package

--- recipes/optick/all/conanfile.py
+++ recipes/optick/all/conanfile.py
@@ -1,12 +1,12 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.microsoft import is_msvc_static_runtime, is_msvc
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir
+from conan.tools.files import get, copy, rm, rmdir
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
 
-required_conan_version = ">=2.0.0"
+required_conan_version = ">=1.54.0"
 
 class PackageConan(ConanFile):
     name = "optick"
@@ -17,21 +17,10 @@ class PackageConan(ConanFile):
     topics = ("profiler", "games")
     package_type = "shared-library"
     settings = "os", "arch", "compiler", "build_type"
-    options = {}
-    default_options = {}
 
     @property
     def _min_cppstd(self):
         return 11
-
-    def export_sources(self):
-        export_conandata_patches(self)
-
-    def config_options(self):
-        pass
-
-    def configure(self):
-        pass
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -45,9 +34,6 @@ class PackageConan(ConanFile):
         if not is_msvc(self):
             raise ConanInvalidConfiguration(f"{self.ref} can be built only by Visual Studio and msvc.")
 
-    def build_requirements(self):
-        pass
-
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
@@ -59,14 +45,7 @@ class PackageConan(ConanFile):
 
         tc.generate()
 
-        tc = CMakeDeps(self)
-        tc.generate()
-
-    def _patch_sources(self):
-        apply_conandata_patches(self)
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -76,14 +55,9 @@ class PackageConan(ConanFile):
         cmake = CMake(self)
         cmake.install()
 
-        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
-        rmdir(self, os.path.join(self.package_folder, "share"))
-        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rm(self, "*.pdb", os.path.join(self.package_folder, "lib"))
         rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
         self.cpp_info.libs = ["OptickCore" + ("d" if self.settings.build_type == "Debug" else "")]
-
-        self.cpp_info.set_property("cmake_file_name", "Optick")
-        self.cpp_info.set_property("cmake_target_name", "Optick::OptickCore")
