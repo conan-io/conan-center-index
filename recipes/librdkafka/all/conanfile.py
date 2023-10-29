da9b7a5e081513ce631b1bd3030b05ff60dfce5a
Eric Pederson <ericacm+github@gmail.com>
2023-10-29 11:04:19 -0400

    Update recipes/librdkafka/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/librdkafka/all/conanfile.py
+++ recipes/librdkafka/all/conanfile.py
@@ -2,6 +2,7 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, replace_in_file
+from conan.tools.microsoft import is_msvc
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.scm import Version
 import os
