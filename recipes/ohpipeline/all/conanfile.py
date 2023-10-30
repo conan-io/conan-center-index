6feb31b0fcef87d36c76aa0bce9ab4cab7f2ae96
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2023-10-30 11:14:53 +0100

    Add CMake to build requirements

--- recipes/ohpipeline/all/conanfile.py
+++ recipes/ohpipeline/all/conanfile.py
@@ -29,6 +29,9 @@ class OhPipelineConan(ConanFile):
         copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=os.path.join(self.export_sources_folder, "src"))
         export_conandata_patches(self)
 
+    def build_requirements(self):
+        self.tool_requires("cmake/[>3.23 <4]")
+
     def requirements(self):
         self.requires("ohnet/1.36.5182", transitive_headers=True, transitive_libs=True)
         self.requires("libressl/3.5.3", transitive_headers=True, transitive_libs=True)
