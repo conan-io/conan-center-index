a3bed4816e016738aedeadc82ddfac807744125d
Martin Valgur <martin.valgur@gmail.com>
2023-10-31 09:55:31 +0200

    dlib: bump sqlite3

--- recipes/dlib/all/conanfile.py
+++ recipes/dlib/all/conanfile.py
@@ -78,7 +78,7 @@ class DlibConan(ConanFile):
         if self.options.get_safe("with_webp"):
             self.requires("libwebp/1.3.2")
         if self.options.with_sqlite3:
-            self.requires("sqlite3/3.43.1")
+            self.requires("sqlite3/3.43.2")
         if self.options.with_openblas:
             self.requires("openblas/0.3.20")
 
