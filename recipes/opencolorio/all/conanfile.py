86d028d0a9765d4d237dfffde164d6b216725044
Ingmar Rieger <git@irieger.net>
2023-10-31 23:04:25 +0100

    Add configuration check for MSVC 2022 with OCIO < 2.0

--- recipes/opencolorio/all/conanfile.py
+++ recipes/opencolorio/all/conanfile.py
@@ -77,6 +77,11 @@ class OpenColorIOConan(ConanFile):
             Version(self.settings.compiler.version) < "6.0":
             raise ConanInvalidConfiguration(f"{self.ref} requires gcc >= 6.0")
 
+        if Version(self.version) < "2.0.0" and \
+            self.settings.compiler.value == "msvc" and \
+            self.settings.compiler.version >= "17.0":
+            raise ConanInvalidConfiguration(f"{self.ref} < 2.0 not building on MSVC 2022")
+
         if Version(self.version) >= "2.3.0" and \
             self.settings.compiler == "clang" and \
             self.settings.compiler.libcxx == "libc++":
