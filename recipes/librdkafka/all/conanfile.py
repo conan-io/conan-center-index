4074c6948d5673ff6b950cf726eebe9906d496b4
Uilian Ries <uilianries@gmail.com>
2023-10-31 17:05:54 +0100

    Use source folder instead of build folder

--- recipes/librdkafka/all/conanfile.py
+++ recipes/librdkafka/all/conanfile.py
@@ -117,7 +117,7 @@ class LibrdkafkaConan(ConanFile):
         # There are references to libcrypto.lib and libssl.lib in rdkafka_ssl.c for versions >= 1.8.0
         if Version(self.version) >= "1.8.0" and is_msvc(self) and \
                 self.settings.build_type == "Debug" and self.options.get_safe("ssl", False):
-            rdkafka_ssl_path = os.path.join(self.build_folder, "..", "src", "src", "rdkafka_ssl.c")
+            rdkafka_ssl_path = os.path.join(self.source_folder, "src", "rdkafka_ssl.c")
             replace_in_file(self, rdkafka_ssl_path, "libcrypto.lib", "libcryptod.lib")
             replace_in_file(self, rdkafka_ssl_path, "libssl.lib", "libssld.lib")
         cmake = CMake(self)
