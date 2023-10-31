8ef938dc33f8f1bb42a0121469351cc2f7abd92e
Jordan Williams <jordan@jwillikers.com>
2023-10-31 10:47:47 -0500

    Bump Meson version

--- recipes/libdisplay-info/all/conanfile.py
+++ recipes/libdisplay-info/all/conanfile.py
@@ -45,7 +45,7 @@ class LibdisplayInfoConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("hwdata/0.374")
-        self.tool_requires("meson/1.2.2")
+        self.tool_requires("meson/1.2.3")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/2.0.3")
 
