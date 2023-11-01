7d3783629a4c69df9468b29ee8e1a33ceb39f5f5
Martin Valgur <martin.valgur@gmail.com>
2023-11-01 13:55:14 +0200

    hlslpp: tidy

--- recipes/hlslpp/all/conanfile.py
+++ recipes/hlslpp/all/conanfile.py
@@ -45,8 +45,7 @@ class HlslppConan(ConanFile):
             )
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def build(self):
         pass
