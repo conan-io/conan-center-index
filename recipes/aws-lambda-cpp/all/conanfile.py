5f5c874d3dc065094a210b6ddc305cf9ab4986ee
toge <toge.mail@gmail.com>
2023-11-01 10:57:51 +0900

    fix conan.errors

--- recipes/aws-lambda-cpp/all/conanfile.py
+++ recipes/aws-lambda-cpp/all/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.error import ConanInvalidConfiguration
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
