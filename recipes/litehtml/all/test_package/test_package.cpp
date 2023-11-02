070d594b63449f9d34940aa62c2039872ae44b11
Martin Valgur <martin.valgur@gmail.com>
2023-11-02 22:47:53 +0200

    litehtml: simplify test_package.cpp
    For compatibility across versions

--- recipes/litehtml/all/test_package/test_package.cpp
+++ recipes/litehtml/all/test_package/test_package.cpp
@@ -5,14 +5,6 @@
 #include <string>
 
 int main() {
-    constexpr size_t offset = 5;
-    constexpr size_t length = 10;
-
-    std::string string = "the quick brown fox jumps over the lazy dog";
-    litehtml::tstring_view view(static_cast<litehtml::tstring_view::const_pointer>(string.c_str() + offset), length);
-
-    for (auto c : view) {
-        std::cout << c << std::endl;
-    }
+  litehtml::tstring_view view;
   return 0;
 }
