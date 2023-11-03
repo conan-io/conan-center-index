26692ad4781733f373b505dd5eb144932a912ffd
Carlos Zoido <mrgalleta@gmail.com>
2023-11-03 10:32:55 +0100

    Update recipes/aws-lambda-cpp/all/conanfile.py

--- recipes/aws-lambda-cpp/all/conanfile.py
+++ recipes/aws-lambda-cpp/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir
+from conan.tools.files import get, copy, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 import os
