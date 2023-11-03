f0f961a4ea733eff14a07c0d9fb678549159e861
Uilian Ries <uilianries@gmail.com>
2023-11-03 19:19:11 +0100

    Update recipes/mariadb-connector-c/all/conanfile.py
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/mariadb-connector-c/all/conanfile.py
+++ recipes/mariadb-connector-c/all/conanfile.py
@@ -96,7 +96,8 @@ class MariadbConnectorcConan(ConanFile):
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
         tc.generate()
         deps = CMakeDeps(self)
-        deps.set_property("zstd", "cmake_file_name", "ZSTD")
+        if Version(self.version) >= "3.3":
+            deps.set_property("zstd", "cmake_file_name", "ZSTD")
         deps.generate()
 
     def _patch_sources(self):
