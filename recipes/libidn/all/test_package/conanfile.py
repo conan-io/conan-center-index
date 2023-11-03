03ac3afb4ed46a825b07fae9607d27101a183a05
Martin Valgur <martin.valgur@gmail.com>
2023-11-03 14:17:57 +0200

    libidn: fix shared lib not being found in v1

--- recipes/libidn/all/test_package/conanfile.py
+++ recipes/libidn/all/test_package/conanfile.py
@@ -1,12 +1,14 @@
+import os
+
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake
-import os
+from conan.tools.env import VirtualRunEnv
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "CMakeToolchain"
     test_type = "explicit"
 
     def requirements(self):
@@ -17,6 +19,10 @@ class TestPackageConan(ConanFile):
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        VirtualRunEnv(self).generate(scope="build")
+        VirtualRunEnv(self).generate(scope="run")
 
     def build(self):
         cmake = CMake(self)
