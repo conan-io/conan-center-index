330335a3fb513aa1720e31b416eab9b9099d843c
Martin Valgur <martin.valgur@gmail.com>
2023-11-03 17:20:56 +0200

    libcpuid: fix test_package

--- recipes/libcpuid/all/test_package/conanfile.py
+++ recipes/libcpuid/all/test_package/conanfile.py
@@ -1,22 +1,25 @@
+import os
+
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake
-import os
+from conan.tools.env import VirtualRunEnv
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "CMakeToolchain"
     test_type = "explicit"
 
     def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+        self.requires(self.tested_reference_str, run=True)
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        VirtualRunEnv(self).generate(scope="build")
+        VirtualRunEnv(self).generate(scope="run")
 
     def build(self):
         cmake = CMake(self)
