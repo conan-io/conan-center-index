e4178b0be0c31a0a21366ace7908794cbaa9fbfb
Jordan Williams <jordan@jwillikers.com>
2023-11-03 14:25:35 -0500

    Fix remove of with_x11 option

--- recipes/glfw/all/conanfile.py
+++ recipes/glfw/all/conanfile.py
@@ -50,7 +50,7 @@ class GlfwConan(ConanFile):
             self.options.rm_safe("fPIC")
         if self.settings.os != "Linux":
             self.options.rm_safe("with_wayland")
-        if not (self.settings.os in ["Linux", "FreeBSD"] or self.version <= Version("3.3.8")):
+        if not self.settings.os in ["Linux", "FreeBSD"] or self.version <= Version("3.3.8"):
             self.options.rm_safe("with_x11")
 
     def configure(self):
