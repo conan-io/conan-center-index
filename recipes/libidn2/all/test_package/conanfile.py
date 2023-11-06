46c8b4a9b59a23f9d27dc607a1205dc7c5c25b17
Martin Valgur <martin.valgur@gmail.com>
2023-11-06 12:39:18 +0200

    libidn2: fix test_package

--- recipes/libidn2/all/test_package/conanfile.py
+++ recipes/libidn2/all/test_package/conanfile.py
@@ -3,20 +3,23 @@ from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake
 import os
 
+from conan.tools.env import VirtualRunEnv
+
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "CMakeToolchain"
     test_type = "explicit"
 
     def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+        self.requires(self.tested_reference_str, run=True)
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        VirtualRunEnv(self).generate(scope="run")
+        VirtualRunEnv(self).generate(scope="build")
 
     def build(self):
         cmake = CMake(self)
@@ -25,7 +28,7 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            self.run("idn2 --help", env="conanrun")
+            self.run("idn2 --help")
 
             bin_path = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(bin_path, env="conanrun")
