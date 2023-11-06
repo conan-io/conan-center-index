779ca88fb3e78cfcc482bef514ee45949164a93f
Luiz Feldmann <gustavo.feldmann@hotmail.com>
2023-11-06 09:24:36 -0300

    cmake_find_package: Will produce Findxxxx.cmake cmake_find_package_multi: Produces xxxxConfig.cmake

--- recipes/optick/all/test_v1_package/conanfile.py
+++ recipes/optick/all/test_v1_package/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package"
+    generators = "cmake", "cmake_find_package_multi"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
