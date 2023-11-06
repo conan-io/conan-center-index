9b0b8cef60e24bf63895fb86b57572bb04f0b90d
Martin Valgur <martin.valgur@gmail.com>
2023-11-06 18:17:46 +0200

    pbc: fix apple cross building bug

--- recipes/pbc/all/conanfile.py
+++ recipes/pbc/all/conanfile.py
@@ -74,7 +74,7 @@ class PbcConan(ConanFile):
         # No idea why this is necessary, but if you don't set CC this way, then
         # configure complains that it can't find gmp.
         if cross_building(self) and self.settings.compiler == "apple-clang":
-            xcr = XCRun(self.settings)
+            xcr = XCRun(self)
             target = to_apple_arch(self.settings.arch) + "-apple-darwin"
             min_ios = ""
             if self.settings.os == "iOS":
