c37adc71239e8f0d52f490160c66ddb08e18ee78
Martin Valgur <martin.valgur@gmail.com>
2023-11-07 10:14:12 +0200

    tpl: replace the autotools build scripts with a simple CMakeLists.txt

--- recipes/tpl/all/conanfile.py
+++ recipes/tpl/all/conanfile.py
@@ -1,12 +1,9 @@
 import os
 
 from conan import ConanFile
-from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.env import Environment, VirtualBuildEnv
-from conan.tools.files import apply_conandata_patches, copy, get, rm
-from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.cmake import CMakeToolchain, CMake
+from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import is_msvc, unix_path
 
 required_conan_version = ">=1.54.0"
 
@@ -30,6 +27,9 @@ class PackageConan(ConanFile):
         "fPIC": True,
     }
 
+    def export_sources(self):
+        copy(self, "CMakeLists.txt", self.recipe_folder, os.path.join(self.export_sources_folder, "src"))
+
     @property
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
@@ -47,47 +47,22 @@ class PackageConan(ConanFile):
     def layout(self):
         basic_layout(self, src_folder="src")
 
-    def build_requirements(self):
-        self.tool_requires("libtool/2.4.7")
-        if self._settings_build.os == "Windows":
-            self.win_bash = True
-            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
-                self.tool_requires("msys2/cci.latest")
-        if is_msvc(self):
-            self.tool_requires("automake/1.16.5")
-
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        env = VirtualBuildEnv(self)
-        env.generate()
-        tc = AutotoolsToolchain(self)
+        tc = CMakeToolchain(self)
         tc.generate()
 
-        if is_msvc(self):
-            env = Environment()
-            automake_conf = self.dependencies.build["automake"].conf_info
-            compile_wrapper = unix_path(self, automake_conf.get("user.automake:compile-wrapper", check_type=str))
-            ar_wrapper = unix_path(self, automake_conf.get("user.automake:lib-wrapper", check_type=str))
-            env.define("CC", f"{compile_wrapper} cl -nologo")
-            env.define("LD", "link -nologo")
-            env.define("AR", f'{ar_wrapper} "lib -nologo"')
-            env.vars(self).save_script("conanbuild_msvc")
-
     def build(self):
-        apply_conandata_patches(self)
-        autotools = Autotools(self)
-        autotools.autoreconf()
-        autotools.configure()
-        autotools.make()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
         copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
-        autotools = Autotools(self)
-        autotools.install()
-        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
-        fix_apple_shared_install_name(self)
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["tpl"]
