6eb45fbd33603e4f20e85767f0a972f6159f1365
Jordan Williams <jordan@jwillikers.com>
2023-11-08 11:55:17 -0600

    Remove destination argument to get method

--- recipes/glew/all/conanfile.py
+++ recipes/glew/all/conanfile.py
@@ -57,8 +57,7 @@ class GlewConan(ConanFile):
         self.requires("glu/system")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
