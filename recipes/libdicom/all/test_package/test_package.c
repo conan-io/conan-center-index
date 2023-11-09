25b92983d3a98afe2dd04e14790954ae517a4730
Martin Valgur <martin.valgur@gmail.com>
2023-11-09 19:13:51 +0200

    libdicom: fix test_package

--- recipes/libdicom/all/test_package/test_package.c
+++ recipes/libdicom/all/test_package/test_package.c
@@ -2,6 +2,6 @@
 
 int main(void) {
     DcmError *error = NULL;
-    DcmFilehandle *filehandle = dcm_filehandle_create_from_file(&error, "xyz");
+    dcm_filehandle_create_from_file(&error, "xyz");
     return 0;
 }
