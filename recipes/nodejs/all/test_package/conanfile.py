54116157e1a7b7346b8c793822a67b74bbb23266
Martin Valgur <martin.valgur@gmail.com>
2023-11-12 16:41:01 +0200

    nodejs: update recipe for Conan v2

--- recipes/nodejs/all/test_package/conanfile.py
+++ recipes/nodejs/all/test_package/conanfile.py
@@ -1,16 +1,19 @@
 from conan import ConanFile
 from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout
 
 
 class TestPackageConan(ConanFile):
-
-    settings = "os", "arch"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "VirtualRunEnv"
     test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
 
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
 
     def test(self):
         if can_run(self):
-            self.output.info("Node version:")
             self.run("node --version")
