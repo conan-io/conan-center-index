73e9c7a863a7c0d05f314b6f62930453425b312d
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-11-12 23:27:59 +0000

    libvips/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericLemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libvips/all/conanfile.py
+++ recipes/libvips/all/conanfile.py
@@ -132,7 +132,7 @@ class LibvipsConan(ConanFile):
         if self.options.with_jpeg == "libjpeg":
             self.requires("libjpeg/9e")
         elif self.options.with_jpeg == "libjpeg-turbo":
-            self.requires("libjpeg-turbo/3.0.0")
+            self.requires("libjpeg-turbo/3.0.1")
         elif self.options.with_jpeg == "mozjpeg":
             self.requires("mozjpeg/4.1.3")
         if self.options.with_jpeg_xl:
