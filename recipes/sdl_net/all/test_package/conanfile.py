71d0fbdecf900c23f9633a5010ef8a79b6b128fa
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-11-13 20:50:38 +0300

    (#19823) sdl_net: add package_type + bump sdl + fix transitive_headers of sdl
    * bump sdl
    
    * add package_type
    
    * add transitive_headers to sdl dependency
    
    * link to sdl in test package since it's a direct dependency of test_package.c
    
    * typo
    
    * simplify souce()

--- recipes/sdl_net/all/test_package/conanfile.py
+++ recipes/sdl_net/all/test_package/conanfile.py
@@ -14,6 +14,7 @@ class TestPackageConan(ConanFile):
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+        self.requires("sdl/2.28.2")
 
     def build(self):
         cmake = CMake(self)
