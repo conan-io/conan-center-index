88b943638fb8005890c05a30487fba5f63ef7c22
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-11-13 11:30:30 +0100

    tsil: use rm_safe

--- recipes/tsil/all/conanfile.py
+++ recipes/tsil/all/conanfile.py
@@ -40,18 +40,9 @@ class TsilConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except Exception:
-                pass
-        try:
-            del self.settings.compiler.libcxx
-        except Exception:
-            pass
-        try:
-            del self.settings.compiler.cppstd
-        except Exception:
-            pass
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
