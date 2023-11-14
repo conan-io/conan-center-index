382545c898576f7681dad630324dfb933e40f03a
Uilian Ries <uilianries@gmail.com>
2023-11-14 11:21:10 +0100

    Simplify recipe without patches
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/freealut/all/conanfile.py
+++ recipes/freealut/all/conanfile.py
@@ -1,19 +1,18 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, get
+from conan.tools.files import copy, get, rm, rmdir
 from conan.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.61"
+required_conan_version = ">=1.54"
 
 class FreeAlutConan(ConanFile):
     name = "freealut"
     description = "freealut is a free implementation of OpenAL's ALUT standard."
-    topics = ("freealut", "openal", "audio", "api")
+    topics = ("openal", "audio", "api")
     url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://directory.fsf.org/wiki/Freealut"
+    homepage = "https://openal.org"
     license = "LGPL-2.0-or-later"
-
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -23,9 +22,6 @@ class FreeAlutConan(ConanFile):
         "shared": False,
         "fPIC": True
     }
-
-    def export_sources(self):
-        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -37,12 +33,12 @@ class FreeAlutConan(ConanFile):
             self.options["openal-soft"].shared = True
         self.settings.rm_safe("compiler.cppstd")
         self.settings.rm_safe("compiler.libcxx")
-    
+
     def validate(self):
         if self.options.shared and \
-           (not self.dependencies["openal-soft"].options.shared):
+           not self.dependencies["openal-soft"].options.shared:
             raise ConanInvalidConfiguration(
-                "If built as shared openal-soft must be shared as well. " 
+                "If built as shared openal-soft must be shared as well. "
                 "Please, use `openal-soft/*:shared=True`.",
             )
 
@@ -54,15 +50,19 @@ class FreeAlutConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
 
     def generate(self):
+        tc = CMakeDeps(self)
+        tc.generate()
         tc = CMakeToolchain(self)
         # Honor BUILD_SHARED_LIBS from conan_toolchain (see https://github.com/conan-io/conan/issues/11840)
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
-        tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0003"] = "NEW"
+        tc.variables["BUILD_STATIC"] = not self.options.shared
+        # INFO: CMakeDeps generates CamelCase variables
+        tc.variables["OPENAL_LIB_DIR"] = os.path.join(self.dependencies["openal-soft"].package_folder, "lib")
+        tc.variables["OPENAL_INCLUDE_DIR"] = os.path.join(self.dependencies["openal-soft"].package_folder, "include")
         tc.generate()
-        CMakeDeps(self).generate()
 
     def build(self):
         cmake = CMake(self)
@@ -73,9 +73,13 @@ class FreeAlutConan(ConanFile):
         copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        if not self.options.shared:
+            rm(self, "*.so*", os.path.join(self.package_folder, "lib"))
 
     def package_info(self):
         self.cpp_info.libs = ['alut']
-        self.cpp_info.includedirs.append(os.path.join("include", "AL"))
-        if(not self.options.shared):
-            self.cpp_info.defines.append("ALUT_LIBTYPE_STATIC")
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs = ["m", "pthread"]
+        if self.options.shared:
+            self.cpp_info.defines.append("ALUT_BUILD_LIBRARY")
