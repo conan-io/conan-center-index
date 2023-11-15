a154dddcc26de1a2a2148721fd1b833ed7cf3920
Martin Valgur <martin.valgur@gmail.com>
2023-11-15 20:18:05 +0200

    ags: recipe tweaks

--- recipes/ags/all/conanfile.py
+++ recipes/ags/all/conanfile.py
@@ -3,7 +3,7 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import collect_libs, copy, get
+from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc, msvc_runtime_flag, check_min_vs
 from conan.tools.scm import Version
@@ -21,7 +21,7 @@ class AGSConan(ConanFile):
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/GPUOpen-LibrariesAndSDKs/AGS_SDK"
-    topics = ("amd", "gpu")
+    topics = ("amd", "gpu", "pre-built")
 
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
@@ -40,17 +40,13 @@ class AGSConan(ConanFile):
         return ["x86_64", "x86"]
 
     def validate(self):
-        if self.settings.os != "Windows":
-            raise ConanInvalidConfiguration(f"ags doesn't support OS: {self.settings.os}.")
         if not is_msvc(self):
-            raise ConanInvalidConfiguration(
-                f"ags doesn't support compiler: {self.settings.compiler} on OS: {self.settings.os}."
-            )
+            raise ConanInvalidConfiguration("AGS SDK only supports MSVC and Windows")
         check_min_vs(self, 190)
         if Version(self.version) < "6.1.0" and check_min_vs(self, 193, raise_invalid=False):
             raise ConanInvalidConfiguration(f"Visual Studio 2019 or older is required for v{self.version}")
         if self.settings.arch not in self._supported_archs:
-            raise ConanInvalidConfiguration(f"ags doesn't support arch: {self.settings.arch}")
+            raise ConanInvalidConfiguration(f"AGS SDK doesn't support arch: {self.settings.arch}")
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, 11)
 
@@ -82,6 +78,12 @@ class AGSConan(ConanFile):
             "x86": "x86",
         }[str(self.settings.arch)]
 
+    @property
+    def _lib_name(self):
+        if self.options.shared:
+            return f"amd_ags_{self._win_arch}"
+        return f"amd_ags_{self._win_arch}_{self._vs_ide_year}_{msvc_runtime_flag(self)}"
+
     def package(self):
         ags_lib_path = os.path.join(self.source_folder, "ags_lib")
         copy(self, "LICENSE.txt",
@@ -90,21 +92,13 @@ class AGSConan(ConanFile):
         copy(self, "*.h",
              dst=os.path.join(self.package_folder, "include"),
              src=os.path.join(ags_lib_path, "inc"))
-
-        if is_msvc(self):
-            win_arch = self._win_arch
-            if self.options.get_safe("shared"):
-                copy(self, f"amd_ags_{win_arch}.dll",
-                     dst=os.path.join(self.package_folder, "bin"),
-                     src=os.path.join(ags_lib_path, "lib"))
-                copy(self, f"amd_ags_{win_arch}.lib",
-                     dst=os.path.join(self.package_folder, "lib"),
-                     src=os.path.join(ags_lib_path, "lib"))
-            else:
-                static_lib = f"amd_ags_{win_arch}_{self._vs_ide_year}_{msvc_runtime_flag(self)}.lib"
-                copy(self, static_lib,
-                     dst=os.path.join(self.package_folder, "lib"),
-                     src=os.path.join(ags_lib_path, "lib"))
+        copy(self, f"{self._lib_name}.lib",
+             dst=os.path.join(self.package_folder, "lib"),
+             src=os.path.join(ags_lib_path, "lib"))
+        if self.options.shared:
+            copy(self, f"{self._lib_name}.dll",
+                 dst=os.path.join(self.package_folder, "bin"),
+                 src=os.path.join(ags_lib_path, "lib"))
 
     def package_info(self):
-        self.cpp_info.libs = collect_libs(self)
+        self.cpp_info.libs = [self._lib_name]
