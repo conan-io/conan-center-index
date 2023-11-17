c13f2bd178e154bddf5b31e1b008bff3795820af
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-11-17 17:45:26 +0300

    better way to handle CMake variables related to x11 and wayland and fix version comparison related to these options

--- recipes/glfw/all/conanfile.py
+++ recipes/glfw/all/conanfile.py
@@ -93,9 +93,12 @@ class GlfwConan(ConanFile):
         tc.cache_variables["GLFW_BUILD_DOCS"] = False
         tc.cache_variables["GLFW_BUILD_EXAMPLES"] = False
         tc.cache_variables["GLFW_BUILD_TESTS"] = False
-        tc.cache_variables["GLFW_BUILD_X11"] = self.options.get_safe("with_x11")
         tc.cache_variables["GLFW_INSTALL"] = True
-        tc.variables["GLFW_USE_WAYLAND" if self.version <= "3.3.8" else "GLFW_BUILD_WAYLAND"] = self.options.get_safe("with_wayland")
+        if Version(self.version) > "3.3.8":
+            tc.cache_variables["GLFW_BUILD_X11"] = self.options.get_safe("with_x11", False)
+            tc.cache_variables["GLFW_BUILD_WAYLAND"] = self.options.get_safe("with_wayland", False)
+        else:
+            tc.cache_variables["GLFW_USE_WAYLAND"] = self.options.get_safe("with_wayland", False)
         tc.variables["GLFW_VULKAN_STATIC"] = self.options.vulkan_static
         if is_msvc(self):
             tc.cache_variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
