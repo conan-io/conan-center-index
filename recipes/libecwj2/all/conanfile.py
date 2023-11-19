531ca1b9312467b80d5a05f126c4a4f785757f5e
Martin Valgur <martin.valgur@gmail.com>
2023-11-19 21:54:54 +0200

    libecwj2: convert to autotools

--- recipes/libecwj2/all/conanfile.py
+++ recipes/libecwj2/all/conanfile.py
@@ -1,10 +1,13 @@
 import os
 
 from conan import ConanFile
+from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.build import check_min_cppstd, stdcpp_library
-from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.env import Environment, VirtualBuildEnv
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, chdir, replace_in_file
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps
 from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, unix_path
 
 required_conan_version = ">=1.54.0"
 
@@ -17,16 +20,18 @@ class PackageConan(ConanFile):
     homepage = "https://trac.osgeo.org/gdal/wiki/ECW"
     topics = ("image", "ecw", "jp2", "jpeg2000")
 
-    package_type = "library"
+    package_type = "static-library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
-        "shared": [True, False],
         "fPIC": [True, False],
     }
     default_options = {
-        "shared": False,
         "fPIC": True,
     }
+
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
 
     def export_sources(self):
         copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=os.path.join(self.export_sources_folder, "src", "Source"))
@@ -36,44 +41,82 @@ class PackageConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-
     def layout(self):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("libjpeg/9e", transitive_headers=True, transitive_libs=True)
+        self.requires("libjpeg/9e")
         self.requires("tinyxml/2.6.2", transitive_headers=True, transitive_libs=True)
-        # self.requires("lcms/2.14")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, 98)
 
+    def build_requirements(self):
+        self.tool_requires("libtool/2.4.7")
+        if self._settings_build.os == "Windows":
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                self.tool_requires("msys2/cci.latest")
+        if is_msvc(self):
+            self.tool_requires("automake/1.16.5")
+
+    @staticmethod
+    def _chmod_plus_x(filename):
+        if os.name == "posix":
+            os.chmod(filename, os.stat(filename).st_mode | 0o111)
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        self._chmod_plus_x(os.path.join(self.source_folder, "configure"))
 
     def generate(self):
-        tc = CMakeToolchain(self)
+        env = VirtualBuildEnv(self)
+        env.generate()
+        tc = AutotoolsToolchain(self)
         tc.generate()
-        deps = CMakeDeps(self)
+        deps = AutotoolsDeps(self)
         deps.generate()
 
-    def build(self):
+        if is_msvc(self):
+            env = Environment()
+            automake_conf = self.dependencies.build["automake"].conf_info
+            compile_wrapper = unix_path(self, automake_conf.get("user.automake:compile-wrapper", check_type=str))
+            ar_wrapper = unix_path(self, automake_conf.get("user.automake:lib-wrapper", check_type=str))
+            env.define("CC", f"{compile_wrapper} cl -nologo")
+            env.define("CXX", f"{compile_wrapper} cl -nologo")
+            env.define("LD", "link -nologo")
+            env.define("AR", f"{ar_wrapper} \"lib -nologo\"")
+            env.define("NM", "dumpbin -symbols")
+            env.define("OBJDUMP", ":")
+            env.define("RANLIB", ":")
+            env.define("STRIP", ":")
+            env.vars(self).save_script("conanbuild_msvc")
+
+    def _patch_sources(self):
         apply_conandata_patches(self)
-        cmake = CMake(self)
-        cmake.configure(build_script_folder=os.path.join(self.source_folder, "Source"))
-        cmake.build()
+        rmdir(self, os.path.join(self.source_folder, "Source", "C", "libjpeg"))
+        rmdir(self, os.path.join(self.source_folder, "Source", "C", "tinyxml"))
+        replace_in_file(self, os.path.join(self.source_folder, "configure.in"), " -fpic", "")
+
+    def build(self):
+        self._patch_sources()
+        with chdir(self, self.source_folder):
+            autotools = Autotools(self)
+            autotools.autoreconf()
+            autotools.configure()
+            autotools.make()
 
     def package(self):
         copy(self, "License.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
-        cmake = CMake(self)
-        cmake.install()
+        copy(self, "*", os.path.join(self.source_folder, "lib"), os.path.join(self.package_folder, "lib"))
+        copy(self, "*", os.path.join(self.source_folder, "Source", "include"), os.path.join(self.package_folder, "include"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        fix_apple_shared_install_name(self)
 
     def package_info(self):
-        self.cpp_info.libs = ["libecwj2"]
+        self.cpp_info.libs = ["ecwj2"]
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.extend(["dl", "rt", "m", "pthread"])
         if stdcpp_library(self):
