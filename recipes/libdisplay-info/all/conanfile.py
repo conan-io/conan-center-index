7413aa2c47bc1e994cd9a5317c5dd4723c056b33
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-11-19 17:51:09 +0300

    move hwdata to build requirements unconditionally

--- recipes/libdisplay-info/all/conanfile.py
+++ recipes/libdisplay-info/all/conanfile.py
@@ -2,11 +2,12 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import cross_building
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import copy, get, replace_in_file, rm, rmdir
+from conan.tools.files import copy, get, replace_in_file, rm, rmdir, save
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.meson import Meson, MesonToolchain
 import os
+import textwrap
 
 
 required_conan_version = ">=1.53.0"
@@ -47,35 +48,48 @@ class LibdisplayInfoConan(ConanFile):
     def layout(self):
         basic_layout(self, src_folder="src")
 
-    def requirements(self):
-        if not self._has_build_profile:
-            self.requires("hwdata/0.374")
-
     def validate(self):
         if not self.settings.os in ["FreeBSD", "Linux"]:
             raise ConanInvalidConfiguration(f"{self.ref} is not supported on {self.settings.os}")
 
     def build_requirements(self):
-        if self._has_build_profile:
-            self.tool_requires("hwdata/0.374")
+        self.tool_requires("hwdata/0.376")
         self.tool_requires("meson/1.2.3")
-        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+        if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/2.0.3")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
         tc = MesonToolchain(self)
         if cross_building(self):
+            # https://mesonbuild.com/Builtin-options.html#specifying-options-per-machine
             tc.project_options["build.pkg_config_path"] = self.generators_folder
         tc.generate()
-        pkg_config_deps = PkgConfigDeps(self)
+
         if self._has_build_profile:
+            pkg_config_deps = PkgConfigDeps(self)
             pkg_config_deps.build_context_activated = ["hwdata"]
-        pkg_config_deps.generate()
-        virtual_build_env = VirtualBuildEnv(self)
-        virtual_build_env.generate()
+            pkg_config_deps.generate()
+        else:
+            # Manually generate pkgconfig file of hwdata since
+            # PkgConfigDeps.build_context_activated can't work with legacy 1 profile.
+            # We must use legacy conan v1 deps_cpp_info because self.dependencies doesn't
+            # contain build requirements when using 1 profile.
+            hwdata_prefix = self.deps_cpp_info["hwdata"].rootpath
+            hwdata_version = self.deps_cpp_info["hwdata"].version
+            hwdata_pkg_content = textwrap.dedent(f"""\
+                prefix={hwdata_prefix}
+                datadir=${{prefix}}/res
+                pkgdatadir=${{datadir}}/hwdata
+                Name: hwdata
+                Description: Hardware identification and configuration data
+                Version: {hwdata_version}
+            """)
+            save(self, os.path.join(self.generators_folder, "hwdata.pc"), hwdata_pkg_content)
 
     def _patch_sources(self):
         replace_in_file(self, os.path.join(self.source_folder, "meson.build"), "subdir('test')", "# subdir('test')")
