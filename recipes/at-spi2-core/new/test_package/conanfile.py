dbeaba8ea14ed6095def73be98a5a625eb2cb055
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-11-20 09:16:29 +0000

    at-spi2-core/new: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericLemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/at-spi2-core/new/test_package/conanfile.py
+++ recipes/at-spi2-core/new/test_package/conanfile.py
@@ -17,7 +17,7 @@ class TestPackageConan(ConanFile):
         self.requires(self.tested_reference_str)
 
     def build_requirements(self):
-        self.tool_requires("meson/1.2.3")
+        self.tool_requires("meson/1.3.0")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/2.0.3")
 
