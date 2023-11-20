11bd46364e7349667efd3da990dada3900a59028
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-11-20 22:58:36 +0300

    bump c-ares

--- recipes/trantor/all/conanfile.py
+++ recipes/trantor/all/conanfile.py
@@ -61,7 +61,7 @@ class TrantorConan(ConanFile):
     def requirements(self):
         self.requires("openssl/[>=1.1 <4]")
         if self.options.with_c_ares:
-            self.requires("c-ares/1.19.1")
+            self.requires("c-ares/1.22.0")
 
     def validate(self):
         if self.info.settings.compiler.get_safe("cppstd"):
