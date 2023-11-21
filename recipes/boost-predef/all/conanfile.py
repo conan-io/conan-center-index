60cdc074b2a769ac6cae006ff122e26a96a6497e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-11-21 16:23:34 +0100

    fix recipe

--- recipes/boost-predef/all/conanfile.py
+++ recipes/boost-predef/all/conanfile.py
@@ -47,11 +47,6 @@ class BoostPredefConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
 
     def requirements(self):
         pass
