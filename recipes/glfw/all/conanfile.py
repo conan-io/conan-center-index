f97f30d248fca951e3e6ebda18c6be044f3fefbd
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-11-22 09:20:53 +0000

    glfw/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericLemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/glfw/all/conanfile.py
+++ recipes/glfw/all/conanfile.py
@@ -68,7 +68,7 @@ class GlfwConan(ConanFile):
     def requirements(self):
         self.requires("opengl/system")
         if self.options.vulkan_static:
-            self.requires("vulkan-loader/1.3.243.0")
+            self.requires("vulkan-loader/1.3.268.0")
         if self.settings.os in ["Linux", "FreeBSD"]:
             if self.options.get_safe("with_x11", True):
                 self.requires("xorg/system")
