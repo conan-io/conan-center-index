conan-io/conan-center-index

commit 067d4e191d3e83742e2e8dcbade15d6ac2972ac7

(#20913) glfw: Add support for Wayland
* glfw: Add support for Wayland

Adds with_wayland option to enable Wayland support in GLFW.
In the current release, there is no option to toggle X11 support.
The current master branch allows choosing between X11, Wayland, or both.
I include the logic for enabling X11 / Wayland for the next release.
I can remove it of course, but figured it might save future work.

Update vulkan-loader.
Add patch to workaround issues with ECM not generating sources from Wayland Protocols.

* Add patch to use dependencies from Conan

* Fix remove of with_x11 option

* Update patch for 3.3.5

* Rename the XKBCommon CMakeDeps file to match the one looked for

This reduces the amount of patching required.

* Default Wayland off as the upstream project does

* Use GLFW_BUILD_WAYLAND for newer versions of GLFW

* Change patch type from portability to upstream

* Fix patch type

recipes/glfw/all/conanfile.py
@@ -1,7 +1,11 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rmdir, save
+from conan.tools.gnu import PkgConfigDeps
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
+from conan.tools.scm import Version
 import os
 import textwrap
 
@@ -23,19 +27,31 @@ class GlfwConan(ConanFile):
         "shared": [True, False],
         "fPIC": [True, False],
         "vulkan_static": [True, False],
+        "with_x11": [True, False],
+        "with_wayland": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
         "vulkan_static": False,
+        "with_x11": True,
+        "with_wayland": False,
     }
+
+    @property
+    def _has_build_profile(self):
+        return hasattr(self, "settings_build")
 
     def export_sources(self):
         export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
+        if self.settings.os != "Linux":
+            self.options.rm_safe("with_wayland")
+        if not self.settings.os in ["Linux", "FreeBSD"] or self.version <= Version("3.3.8"):
+            self.options.rm_safe("with_x11")
 
     def configure(self):
         if self.options.shared:
@@ -43,29 +59,53 @@ class GlfwConan(ConanFile):
         self.settings.rm_safe("compiler.cppstd")
         self.settings.rm_safe("compiler.libcxx")
 
+        if self.options.get_safe("with_wayland"):
+            self.options["xkbcommon"].with_wayland = True
+
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
         self.requires("opengl/system")
         if self.options.vulkan_static:
-            self.requires("vulkan-loader/1.3.239.0")
-        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.requires("vulkan-loader/1.3.243.0")
+        if self.options.get_safe("with_x11") or self.version <= Version("3.3.8"):
             self.requires("xorg/system")
+        if self.options.get_safe("with_wayland"):
+            self.requires("wayland/1.22.0")
+            self.requires("wayland-protocols/1.32")
+            self.requires("xkbcommon/1.6.0")
+
+    def validate(self):
+        if self.options.get_safe("with_wayland") and not self.dependencies["xkbcommon"].options.with_wayland:
+            raise ConanInvalidConfiguration(f"{self.ref} requires the with_wayland option in xkbcommon to be enabled when the with_wayland option is enabled")
+
+    def build_requirements(self):
+        if self.options.get_safe("with_wayland"):
+            self.tool_requires("wayland/<host_version>")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["GLFW_BUILD_EXAMPLES"] = False
-        tc.variables["GLFW_BUILD_TESTS"] = False
-        tc.variables["GLFW_BUILD_DOCS"] = False
-        tc.variables["GLFW_INSTALL"] = True
+        tc.cache_variables["GLFW_BUILD_DOCS"] = False
+        tc.cache_variables["GLFW_BUILD_EXAMPLES"] = False
+        tc.cache_variables["GLFW_BUILD_TESTS"] = False
+        tc.cache_variables["GLFW_BUILD_X11"] = self.options.get_safe("with_x11")
+        tc.cache_variables["GLFW_INSTALL"] = True
+        tc.variables["GLFW_USE_WAYLAND" if self.version <= "3.3.8" else "GLFW_BUILD_WAYLAND"] = self.options.get_safe("with_wayland")
         tc.variables["GLFW_VULKAN_STATIC"] = self.options.vulkan_static
         if is_msvc(self):
-            tc.variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
+            tc.cache_variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
         tc.generate()
+        cmake_deps = CMakeDeps(self)
+        cmake_deps.set_property("xkbcommon", "cmake_file_name", "XKBCommon")
+        cmake_deps.generate()
+        pkg_config_deps = PkgConfigDeps(self)
+        pkg_config_deps.generate()
+        virtual_build_env = VirtualBuildEnv(self)
+        virtual_build_env.generate()
 
     def _patch_sources(self):
         apply_conandata_patches(self)
@@ -100,7 +140,7 @@ class GlfwConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "LICENSE*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "LICENSE*", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))

recipes/glfw/all/conandata.yml
@@ -21,6 +21,36 @@ sources:
     url: "https://github.com/glfw/glfw/releases/download/3.3.2/glfw-3.3.2.zip"
     sha256: "08a33a512f29d7dbf78eab39bd7858576adcc95228c9efe8e4bc5f0f3261efc7"
 patches:
+  "3.3.8":
+    - patch_file: "patches/3.3.5-0001-Wayland-Remove-extra-cmake-modules-dependency.patch"
+      patch_description: "Remove dependency on extra-cmake-modules to fix generation of Wayland Protocols"
+      patch_type: "official"
+      patch_source: "https://github.com/glfw/glfw/commit/2747e47393cbca2d09db56223e735bd94b21e2eb"
+  "3.3.7":
+    - patch_file: "patches/3.3.5-0001-Wayland-Remove-extra-cmake-modules-dependency.patch"
+      patch_description: "Remove dependency on extra-cmake-modules to fix generation of Wayland Protocols"
+      patch_type: "official"
+      patch_source: "https://github.com/glfw/glfw/commit/2747e47393cbca2d09db56223e735bd94b21e2eb"
+  "3.3.6":
+    - patch_file: "patches/3.3.5-0001-Wayland-Remove-extra-cmake-modules-dependency.patch"
+      patch_description: "Remove dependency on extra-cmake-modules to fix generation of Wayland Protocols"
+      patch_type: "official"
+      patch_source: "https://github.com/glfw/glfw/commit/2747e47393cbca2d09db56223e735bd94b21e2eb"
+  "3.3.5":
+    - patch_file: "patches/3.3.5-0001-Wayland-Remove-extra-cmake-modules-dependency.patch"
+      patch_description: "Remove dependency on extra-cmake-modules to fix generation of Wayland Protocols"
+      patch_type: "official"
+      patch_source: "https://github.com/glfw/glfw/commit/2747e47393cbca2d09db56223e735bd94b21e2eb"
+  "3.3.4":
+    - patch_file: "patches/3.3.3-0001-Wayland-Remove-extra-cmake-modules-dependency.patch"
+      patch_description: "Remove dependency on extra-cmake-modules to fix generation of Wayland Protocols"
+      patch_type: "official"
+      patch_source: "https://github.com/glfw/glfw/commit/2747e47393cbca2d09db56223e735bd94b21e2eb"
+  "3.3.3":
+    - patch_file: "patches/3.3.3-0001-Wayland-Remove-extra-cmake-modules-dependency.patch"
+      patch_description: "Remove dependency on extra-cmake-modules to fix generation of Wayland Protocols"
+      patch_type: "official"
+      patch_source: "https://github.com/glfw/glfw/commit/2747e47393cbca2d09db56223e735bd94b21e2eb"
   "3.3.2":
     - patch_file: "patches/3.3.2-0001-fix-objc-cmake3.19+.patch"
       patch_description: "Fix ObjC handling while using recent CMake versions"
@@ -30,3 +60,7 @@ patches:
       patch_description: "Relocatable shared lib on macOS"
       patch_type: "portability"
       patch_source: "https://github.com/glfw/glfw/commit/5a15d8a7842fbc4b5260eac2335a6691bca9be4b"
+    - patch_file: "patches/3.3.2-0003-Wayland-Remove-extra-cmake-modules-dependency.patch"
+      patch_description: "Remove dependency on extra-cmake-modules to fix generation of Wayland Protocols"
+      patch_type: "official"
+      patch_source: "https://github.com/glfw/glfw/commit/2747e47393cbca2d09db56223e735bd94b21e2eb"

recipes/glfw/all/patches/3.3.2-0003-Wayland-Remove-extra-cmake-modules-dependency.patch
@@ -1,0 +1,122 @@
+From 8622c81fe5271d10650e4007e96bc5342ac87ec1 Mon Sep 17 00:00:00 2001
+From: Jordan Williams <jordan@jwillikers.com>
+Date: Fri, 3 Nov 2023 08:51:04 -0500
+Subject: [PATCH] Wayland: Remove extra-cmake-modules dependency
+
+Fixes #1774.
+---
+ CMakeLists.txt     | 11 ++++----
+ src/CMakeLists.txt | 67 +++++++++++++++++++++++++++++-----------------
+ 2 files changed, 48 insertions(+), 30 deletions(-)
+
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index e9888624..33d1a0c1 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -237,12 +237,13 @@ endif()
+ # Use Wayland for window creation
+ #--------------------------------------------------------------------
+ if (_GLFW_WAYLAND)
+-    find_package(ECM REQUIRED NO_MODULE)
+-    list(APPEND CMAKE_MODULE_PATH "${ECM_MODULE_PATH}")
+ 
+-    find_package(Wayland REQUIRED Client Cursor Egl)
+-    find_package(WaylandScanner REQUIRED)
+-    find_package(WaylandProtocols 1.15 REQUIRED)
++    include(FindPkgConfig)
++    pkg_check_modules(Wayland REQUIRED
++        wayland-client>=0.2.7
++        wayland-cursor>=0.2.7
++        wayland-egl>=0.2.7
++        xkbcommon)
+ 
+     list(APPEND glfw_PKG_DEPS "wayland-egl")
+ 
+diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
+index e3463639..76c25251 100644
+--- a/src/CMakeLists.txt
++++ b/src/CMakeLists.txt
+@@ -30,31 +30,6 @@ elseif (_GLFW_WAYLAND)
+     set(glfw_SOURCES ${common_SOURCES} wl_init.c wl_monitor.c wl_window.c
+                      posix_time.c posix_thread.c xkb_unicode.c
+                      egl_context.c osmesa_context.c)
+-
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/stable/xdg-shell/xdg-shell.xml"
+-        BASENAME xdg-shell)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/unstable/xdg-decoration/xdg-decoration-unstable-v1.xml"
+-        BASENAME xdg-decoration)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/stable/viewporter/viewporter.xml"
+-        BASENAME viewporter)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/unstable/relative-pointer/relative-pointer-unstable-v1.xml"
+-        BASENAME relative-pointer-unstable-v1)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/unstable/pointer-constraints/pointer-constraints-unstable-v1.xml"
+-        BASENAME pointer-constraints-unstable-v1)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/unstable/idle-inhibit/idle-inhibit-unstable-v1.xml"
+-        BASENAME idle-inhibit-unstable-v1)
+ elseif (_GLFW_OSMESA)
+     set(glfw_HEADERS ${common_HEADERS} null_platform.h null_joystick.h
+                      posix_time.h posix_thread.h osmesa_context.h)
+@@ -62,6 +37,48 @@ elseif (_GLFW_OSMESA)
+                      null_joystick.c posix_time.c posix_thread.c osmesa_context.c)
+ endif()
+ 
++if (_GLFW_WAYLAND)
++    find_program(WAYLAND_SCANNER_EXECUTABLE NAMES wayland-scanner)
++    pkg_check_modules(WAYLAND_PROTOCOLS REQUIRED wayland-protocols>=1.15)
++    pkg_get_variable(WAYLAND_PROTOCOLS_BASE wayland-protocols pkgdatadir)
++
++    macro(wayland_generate protocol_file output_file)
++          add_custom_command(OUTPUT ${output_file}.h
++              COMMAND ${WAYLAND_SCANNER_EXECUTABLE} client-header
++              < ${protocol_file} > ${output_file}.h
++              DEPENDS ${protocol_file})
++          list(APPEND GLFW_WAYLAND_PROTOCOL_SOURCES ${output_file}.h)
++
++          add_custom_command(OUTPUT ${output_file}.c
++              COMMAND ${WAYLAND_SCANNER_EXECUTABLE} private-code
++              < ${protocol_file} > ${output_file}.c
++              DEPENDS ${protocol_file})
++          list(APPEND GLFW_WAYLAND_PROTOCOL_SOURCES ${output_file}.c)
++    endmacro()
++
++    set(GLFW_WAYLAND_PROTOCOL_SOURCES)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/stable/xdg-shell/xdg-shell.xml
++        ${GLFW_BINARY_DIR}/src/wayland-xdg-shell-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/unstable/xdg-decoration/xdg-decoration-unstable-v1.xml
++        ${GLFW_BINARY_DIR}/src/wayland-xdg-decoration-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/stable/viewporter/viewporter.xml
++        ${GLFW_BINARY_DIR}/src/wayland-viewporter-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/unstable/relative-pointer/relative-pointer-unstable-v1.xml
++        ${GLFW_BINARY_DIR}/src/wayland-relative-pointer-unstable-v1-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/unstable/pointer-constraints/pointer-constraints-unstable-v1.xml
++        ${GLFW_BINARY_DIR}/src/wayland-pointer-constraints-unstable-v1-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/unstable/idle-inhibit/idle-inhibit-unstable-v1.xml
++        ${GLFW_BINARY_DIR}/src/wayland-idle-inhibit-unstable-v1-client-protocol)
++
++    list(APPEND glfw_SOURCES ${GLFW_WAYLAND_PROTOCOL_SOURCES})
++endif()
++
+ if (_GLFW_X11 OR _GLFW_WAYLAND)
+     if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
+         set(glfw_HEADERS ${glfw_HEADERS} linux_joystick.h)
+-- 
+2.41.0
+

recipes/glfw/all/patches/3.3.3-0001-Wayland-Remove-extra-cmake-modules-dependency.patch
@@ -1,0 +1,122 @@
+From 95e47a77801c9ab91ebfcb740d9ad6ce9861f2ab Mon Sep 17 00:00:00 2001
+From: Jordan Williams <jordan@jwillikers.com>
+Date: Fri, 3 Nov 2023 08:51:04 -0500
+Subject: [PATCH] Wayland: Remove extra-cmake-modules dependency
+
+Fixes #1774.
+---
+ CMakeLists.txt     | 11 ++++----
+ src/CMakeLists.txt | 67 +++++++++++++++++++++++++++++-----------------
+ 2 files changed, 48 insertions(+), 30 deletions(-)
+
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index f4f30293..097e5931 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -244,12 +244,13 @@ endif()
+ # Use Wayland for window creation
+ #--------------------------------------------------------------------
+ if (_GLFW_WAYLAND)
+-    find_package(ECM REQUIRED NO_MODULE)
+-    list(APPEND CMAKE_MODULE_PATH "${ECM_MODULE_PATH}")
+ 
+-    find_package(Wayland REQUIRED Client Cursor Egl)
+-    find_package(WaylandScanner REQUIRED)
+-    find_package(WaylandProtocols 1.15 REQUIRED)
++    include(FindPkgConfig)
++    pkg_check_modules(Wayland REQUIRED
++        wayland-client>=0.2.7
++        wayland-cursor>=0.2.7
++        wayland-egl>=0.2.7
++        xkbcommon)
+ 
+     list(APPEND glfw_PKG_DEPS "wayland-client")
+ 
+diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
+index a409459b..03aaacf6 100644
+--- a/src/CMakeLists.txt
++++ b/src/CMakeLists.txt
+@@ -30,31 +30,6 @@ elseif (_GLFW_WAYLAND)
+     set(glfw_SOURCES ${common_SOURCES} wl_init.c wl_monitor.c wl_window.c
+                      posix_time.c posix_thread.c xkb_unicode.c
+                      egl_context.c osmesa_context.c)
+-
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/stable/xdg-shell/xdg-shell.xml"
+-        BASENAME xdg-shell)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/unstable/xdg-decoration/xdg-decoration-unstable-v1.xml"
+-        BASENAME xdg-decoration)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/stable/viewporter/viewporter.xml"
+-        BASENAME viewporter)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/unstable/relative-pointer/relative-pointer-unstable-v1.xml"
+-        BASENAME relative-pointer-unstable-v1)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/unstable/pointer-constraints/pointer-constraints-unstable-v1.xml"
+-        BASENAME pointer-constraints-unstable-v1)
+-    ecm_add_wayland_client_protocol(glfw_SOURCES
+-        PROTOCOL
+-        "${WAYLAND_PROTOCOLS_PKGDATADIR}/unstable/idle-inhibit/idle-inhibit-unstable-v1.xml"
+-        BASENAME idle-inhibit-unstable-v1)
+ elseif (_GLFW_OSMESA)
+     set(glfw_HEADERS ${common_HEADERS} null_platform.h null_joystick.h
+                      posix_time.h posix_thread.h osmesa_context.h)
+@@ -62,6 +37,48 @@ elseif (_GLFW_OSMESA)
+                      null_joystick.c posix_time.c posix_thread.c osmesa_context.c)
+ endif()
+ 
++if (_GLFW_WAYLAND)
++    find_program(WAYLAND_SCANNER_EXECUTABLE NAMES wayland-scanner)
++    pkg_check_modules(WAYLAND_PROTOCOLS REQUIRED wayland-protocols>=1.15)
++    pkg_get_variable(WAYLAND_PROTOCOLS_BASE wayland-protocols pkgdatadir)
++
++    macro(wayland_generate protocol_file output_file)
++          add_custom_command(OUTPUT ${output_file}.h
++              COMMAND ${WAYLAND_SCANNER_EXECUTABLE} client-header
++              < ${protocol_file} > ${output_file}.h
++              DEPENDS ${protocol_file})
++          list(APPEND GLFW_WAYLAND_PROTOCOL_SOURCES ${output_file}.h)
++
++          add_custom_command(OUTPUT ${output_file}.c
++              COMMAND ${WAYLAND_SCANNER_EXECUTABLE} private-code
++              < ${protocol_file} > ${output_file}.c
++              DEPENDS ${protocol_file})
++          list(APPEND GLFW_WAYLAND_PROTOCOL_SOURCES ${output_file}.c)
++    endmacro()
++
++    set(GLFW_WAYLAND_PROTOCOL_SOURCES)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/stable/xdg-shell/xdg-shell.xml
++        ${GLFW_BINARY_DIR}/src/wayland-xdg-shell-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/unstable/xdg-decoration/xdg-decoration-unstable-v1.xml
++        ${GLFW_BINARY_DIR}/src/wayland-xdg-decoration-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/stable/viewporter/viewporter.xml
++        ${GLFW_BINARY_DIR}/src/wayland-viewporter-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/unstable/relative-pointer/relative-pointer-unstable-v1.xml
++        ${GLFW_BINARY_DIR}/src/wayland-relative-pointer-unstable-v1-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/unstable/pointer-constraints/pointer-constraints-unstable-v1.xml
++        ${GLFW_BINARY_DIR}/src/wayland-pointer-constraints-unstable-v1-client-protocol)
++    wayland_generate(
++        ${WAYLAND_PROTOCOLS_BASE}/unstable/idle-inhibit/idle-inhibit-unstable-v1.xml
++        ${GLFW_BINARY_DIR}/src/wayland-idle-inhibit-unstable-v1-client-protocol)
++
++    list(APPEND glfw_SOURCES ${GLFW_WAYLAND_PROTOCOL_SOURCES})
++endif()
++
+ if (_GLFW_X11 OR _GLFW_WAYLAND)
+     if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
+         set(glfw_HEADERS ${glfw_HEADERS} linux_joystick.h)
+-- 
+2.41.0
+

commit 50b5b1f3db1ae934da49373d7dc17b2f6453664a

(#21199) glfw: fix regressions introduced by #20913
* do not require xorg for non-Linux platforms

it fixes a regression introduced by https://github.com/conan-io/conan-center-index/pull/20913

* update required_conan_version due to host_version in build requirements

* better way to handle CMake variables related to x11 and wayland and fix version comparison related to these options

* do not add wayland in build requirements if 1 profile as it may lead to conflicts impossible to resolve for consumers

* move wayland-protocols to build requirements

* use legacy deps_cpp_info for wayland-protocols workaround with 1 profile

* more elegant workaround for wayland-protocols and 1 profile

* reduce PR diff

recipes/glfw/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.env import VirtualBuildEnv
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rmdir, save
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
@@ -9,7 +9,7 @@ from conan.tools.scm import Version
 import os
 import textwrap
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=1.60.0 <2 || >=2.0.5"
 
 
 class GlfwConan(ConanFile):
@@ -50,7 +50,7 @@ class GlfwConan(ConanFile):
             self.options.rm_safe("fPIC")
         if self.settings.os != "Linux":
             self.options.rm_safe("with_wayland")
-        if not self.settings.os in ["Linux", "FreeBSD"] or self.version <= Version("3.3.8"):
+        if self.settings.os not in ["Linux", "FreeBSD"] or Version(self.version) <= "3.3.8":
             self.options.rm_safe("with_x11")
 
     def configure(self):
@@ -69,11 +69,11 @@ class GlfwConan(ConanFile):
         self.requires("opengl/system")
         if self.options.vulkan_static:
             self.requires("vulkan-loader/1.3.243.0")
-        if self.options.get_safe("with_x11") or self.version <= Version("3.3.8"):
-            self.requires("xorg/system")
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            if self.options.get_safe("with_x11", True):
+                self.requires("xorg/system")
         if self.options.get_safe("with_wayland"):
             self.requires("wayland/1.22.0")
-            self.requires("wayland-protocols/1.32")
             self.requires("xkbcommon/1.6.0")
 
     def validate(self):
@@ -82,30 +82,61 @@ class GlfwConan(ConanFile):
 
     def build_requirements(self):
         if self.options.get_safe("with_wayland"):
-            self.tool_requires("wayland/<host_version>")
+            self.tool_requires("wayland-protocols/1.32")
+            if self._has_build_profile:
+                self.tool_requires("wayland/<host_version>")
+            if not self.conf.get("tools.gnu:pkg_config", check_type=str):
+                self.tool_requires("pkgconf/2.0.3")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+        if self.options.get_safe("with_wayland") and not self._has_build_profile:
+            env = VirtualRunEnv(self)
+            env.generate(scope="build")
+
         tc = CMakeToolchain(self)
         tc.cache_variables["GLFW_BUILD_DOCS"] = False
         tc.cache_variables["GLFW_BUILD_EXAMPLES"] = False
         tc.cache_variables["GLFW_BUILD_TESTS"] = False
-        tc.cache_variables["GLFW_BUILD_X11"] = self.options.get_safe("with_x11")
         tc.cache_variables["GLFW_INSTALL"] = True
-        tc.variables["GLFW_USE_WAYLAND" if self.version <= "3.3.8" else "GLFW_BUILD_WAYLAND"] = self.options.get_safe("with_wayland")
+        if Version(self.version) > "3.3.8":
+            tc.cache_variables["GLFW_BUILD_X11"] = self.options.get_safe("with_x11", False)
+            tc.cache_variables["GLFW_BUILD_WAYLAND"] = self.options.get_safe("with_wayland", False)
+        else:
+            tc.cache_variables["GLFW_USE_WAYLAND"] = self.options.get_safe("with_wayland", False)
         tc.variables["GLFW_VULKAN_STATIC"] = self.options.vulkan_static
         if is_msvc(self):
             tc.cache_variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
         tc.generate()
         cmake_deps = CMakeDeps(self)
-        cmake_deps.set_property("xkbcommon", "cmake_file_name", "XKBCommon")
+        if self.options.get_safe("with_wayland"):
+            cmake_deps.set_property("xkbcommon", "cmake_file_name", "XKBCommon")
         cmake_deps.generate()
-        pkg_config_deps = PkgConfigDeps(self)
-        pkg_config_deps.generate()
-        virtual_build_env = VirtualBuildEnv(self)
-        virtual_build_env.generate()
+        if self.options.get_safe("with_wayland"):
+            pkg_config_deps = PkgConfigDeps(self)
+            if self._has_build_profile:
+                pkg_config_deps.build_context_activated = ["wayland-protocols"]
+            else:
+                # Manually generate pkgconfig file of wayland-protocols since
+                # PkgConfigDeps.build_context_activated can't work with legacy 1 profile
+                # We must use legacy conan v1 deps_cpp_info because self.dependencies doesn't
+                # contain build requirements when using 1 profile.
+                wp_prefix = self.deps_cpp_info["wayland-protocols"].rootpath
+                wp_version = self.deps_cpp_info["wayland-protocols"].version
+                wp_pkg_content = textwrap.dedent(f"""\
+                    prefix={wp_prefix}
+                    datarootdir=${{prefix}}/res
+                    pkgdatadir=${{datarootdir}}/wayland-protocols
+                    Name: Wayland Protocols
+                    Description: Wayland protocol files
+                    Version: {wp_version}
+                """)
+                save(self, os.path.join(self.generators_folder, "wayland-protocols.pc"), wp_pkg_content)
+            pkg_config_deps.generate()
 
     def _patch_sources(self):
         apply_conandata_patches(self)