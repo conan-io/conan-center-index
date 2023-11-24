a7a76c3e60889319dccae6e0838eba294e0dd76a
Uilian Ries <uilianries@gmail.com>
2023-11-24 13:24:36 +0100

    first step
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/gettext/all/conanfile.py
+++ recipes/gettext/all/conanfile.py
@@ -3,23 +3,38 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.env import Environment, VirtualBuildEnv
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, chdir, rm, rename
 from conan.tools.gnu import AutotoolsToolchain, Autotools
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import check_min_vs, is_msvc, unix_path, unix_path_package_info_legacy
+from conan.tools.apple import is_apple_os
 from conan.tools.scm import Version
 
 required_conan_version = ">=1.57.0"
 
 class GetTextConan(ConanFile):
     name = "gettext"
-    package_type = "application"
     description = "An internationalization and localization system for multilingual programs"
     topics = ("intl", "libintl", "i18n")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.gnu.org/software/gettext"
-    license = "GPL-3.0-or-later"
+    license = ("GPL-3.0-or-later", "LGPL-2.1-or-later")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "threads": ["posix", "solaris", "pth", "windows", "disabled"],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        # INFO: Handle default value for `threads` in `config_options` method
+    }
+
+    @property
+    def _gettext_folder(self):
+        return "gettext-tools"
 
     @property
     def _settings_build(self):
@@ -28,7 +43,14 @@ class GetTextConan(ConanFile):
     def export_sources(self):
         export_conandata_patches(self)
 
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+        self.options.threads = {"Solaris": "solaris", "Windows": "windows"}.get(str(self.settings.os), "posix")
+
     def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
         self.settings.rm_safe("compiler.libcxx")
         self.settings.rm_safe("compiler.cppstd")
 
@@ -37,13 +59,6 @@ class GetTextConan(ConanFile):
 
     def requirements(self):
         self.requires("libiconv/1.17")
-
-    def package_id(self):
-        del self.info.settings.compiler
-
-    def validate(self):
-        if Version(self.version) < "0.21" and is_msvc(self):
-            raise ConanInvalidConfiguration("MSVC builds of gettext for versions < 0.21 are not supported.")  # FIXME: it used to be possible. What changed?
 
     def build_requirements(self):
         if self._settings_build.os == "Windows":
@@ -68,8 +83,6 @@ class GetTextConan(ConanFile):
             "EMACS=no",
             "--datarootdir=${prefix}/res",
             "--with-libiconv-prefix={}".format(libiconv_root),
-            "--disable-shared",
-            "--disable-static",
             "--disable-nls",
             "--disable-dependency-tracking",
             "--enable-relocatable",
@@ -77,7 +90,21 @@ class GetTextConan(ConanFile):
             "--disable-java",
             "--disable-csharp",
             "--disable-libasprintf",
+            "--disable-openmp",
             "--disable-curses",
+            "--disable-threads" if self.options.threads == "disabled" else ("--enable-threads=" + str(self.options.threads)),
+            "--with-included-glib",
+            "--with-included-libxml",
+            "--with-included-libunistring",
+            "--with-installed-libtextstyle=no",
+            "--without-cvs",
+            "--without-emacs",
+            "--without-git",
+            "--without-libcurses-prefix",
+            "--without-libncurses-prefix",
+            "--without-libtermcap-prefix",
+            "--without-libxcurses-prefix",
+            "--without-included-gettext",
         ])
 
         if is_msvc(self):
@@ -114,33 +141,50 @@ class GetTextConan(ConanFile):
 
     def build(self):
         apply_conandata_patches(self)
-
         autotools = Autotools(self)
         autotools.configure()
         autotools.make()
+        #with chdir(self, os.path.join(self.build_folder, self._gettext_folder)):
+        #    autotools.make(args=["-C", "intl"])
 
     def package(self):
         autotools = Autotools(self)
         autotools.install()
 
         copy(self, pattern="COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
-
-        rmdir(self, os.path.join(self.package_folder, "lib"))
-        rmdir(self, os.path.join(self.package_folder, "include"))
         rmdir(self, os.path.join(self.package_folder, "share", "doc"))
         rmdir(self, os.path.join(self.package_folder, "share", "info"))
         rmdir(self, os.path.join(self.package_folder, "share", "man"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        dest_lib_dir = os.path.join(self.package_folder, "lib")
+        dest_runtime_dir = os.path.join(self.package_folder, "bin")
+        dest_include_dir = os.path.join(self.package_folder, "include")
+        copy(self, "*gnuintl*.dll", self.build_folder, dest_runtime_dir, keep_path=False)
+        copy(self, "*gnuintl*.lib", self.build_folder, dest_lib_dir, keep_path=False)
+        copy(self, "*gnuintl*.a", self.build_folder, dest_lib_dir, keep_path=False)
+        copy(self, "*gnuintl*.so*", self.build_folder, dest_lib_dir, keep_path=False)
+        copy(self, "*gnuintl*.dylib", self.build_folder, dest_lib_dir, keep_path=False)
+        copy(self, "*libgnuintl.h", self.build_folder, dest_include_dir, keep_path=False)
+        rename(self, os.path.join(dest_include_dir, "libgnuintl.h"), os.path.join(dest_include_dir, "libintl.h"))
 
     def package_info(self):
-        self.cpp_info.libdirs = []
-        self.cpp_info.includedirs = []
-
         aclocal = os.path.join(self.package_folder, "res", "aclocal")
         autopoint = os.path.join(self.package_folder, "bin", "autopoint")
         self.buildenv_info.append_path("ACLOCAL_PATH", aclocal)
         self.buildenv_info.define_path("AUTOPOINT", autopoint)
 
+        # TODO: Generate FindGettext.cmake: https://cmake.org/cmake/help/latest/module/FindGettext.html
+
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "Intl")
+        self.cpp_info.set_property("cmake_target_name", "Intl::Intl")
+        self.cpp_info.libs = ["gnuintl"]
+        if is_apple_os(self):
+            self.cpp_info.frameworks.append("CoreFoundation")
+
         # TODO: the following can be removed when the recipe supports Conan >= 2.0 only
+        self.cpp_info.names["cmake_find_package"] = "Intl"
+        self.cpp_info.names["cmake_find_package_multi"] = "Intl"
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
         self.env_info.AUTOMAKE_CONAN_INCLUDES.append(unix_path_package_info_legacy(self, aclocal))
         self.env_info.AUTOPOINT = unix_path_package_info_legacy(self, autopoint)
