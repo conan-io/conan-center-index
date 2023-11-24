1f757c50df8bf6995300c6c6f628f5f1b01970dd
Uilian Ries <uilianries@gmail.com>
2023-11-24 17:22:05 +0100

    Simplify test package
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/gettext/all/test_package/conanfile.py
+++ recipes/gettext/all/test_package/conanfile.py
@@ -1,58 +1,28 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.env import Environment, VirtualRunEnv, VirtualBuildEnv
-from conan.tools.gnu import Autotools, AutotoolsToolchain
-from conan.tools.layout import basic_layout
-from conan.tools.microsoft import is_msvc
-
+from conan.tools.cmake import cmake_layout, CMake
+import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    exports_sources = "configure.ac",
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
-    win_bash = True
-
-    @property
-    def _settings_build(self):
-        return getattr(self, "settings_build", self.settings)
 
     def requirements(self):
-         self.requires(self.tested_reference_str)
-
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
-        self.tool_requires("automake/1.16.5")
-        if self._settings_build.os == "Windows" and not self.conf.get("tools.microsoft.bash:path", check_type=str):
-            self.tool_requires("msys2/cci.latest")
+        self.requires(self.tested_reference_str)
 
     def layout(self):
-        basic_layout(self, src_folder="src")
-        
-    def generate(self):
-        buildenv = VirtualBuildEnv(self)
-        buildenv.generate()
-
-        at = AutotoolsToolchain(self)
-        at.generate()
-
-        if is_msvc(self):
-            env = Environment()
-            env.define("CC", "cl -nologo")
-            env.define("LD", "link -nologo")
-            env.vars(self).save_script("conanbuild_libsmacker_msvc")
-
-        runenv = VirtualRunEnv(self)
-        runenv.generate()
+        cmake_layout(self)
 
     def build(self):
-
-        autotools = Autotools(self)
-        autotools.autoreconf()
-        autotools.configure()
-
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
         if can_run(self):
-            for exe in ["gettext", "ngettext", "msgcat", "msgmerge"]:
-                self.run("{} --version".format(exe), env="conanrun")
+            for exe in ["gettext", "ngettext", "msgcat", "msgmerge", "msgfmt"]:
+                self.run(f"{exe} --version", env="conanrun")
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
--- recipes/gettext/all/test_v1_package/conanfile.py
+++ recipes/gettext/all/test_v1_package/conanfile.py
@@ -1,51 +1,19 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
-import contextlib
+from conans import ConanFile, CMake, tools
 import os
-import shutil
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "arch", "compiler"
-    exports_sources = "configure.ac",
-    test_type = "explicit"
-
-    @property
-    def _settings_build(self):
-        return getattr(self, "settings_build", self.settings)
-
-    def requirements(self):
-         self.requires(self.tested_reference_str)
-
-    def build_requirements(self):
-        self.build_requires(self.tested_reference_str)
-        self.build_requires("automake/1.16.4")
-        if self._settings_build.os == "Windows" and not tools.get_env("CONAN_BASH_PATH"):
-            self.build_requires("msys2/cci.latest")
-
-    @contextlib.contextmanager
-    def _build_context(self):
-        if self.settings.compiler == "Visual Studio":
-            with tools.vcvars(self):
-                env = {
-                    "CC": "cl -nologo",
-                    "LD": "link -nologo",
-                }
-                with tools.environment_append(env):
-                    yield
-        else:
-            yield
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
-        for src in self.exports_sources:
-            shutil.copy(os.path.join(self.source_folder, src), os.path.join(self.build_folder, src))
-
-        self.run("{} -fiv".format(tools.get_env("AUTORECONF")), win_bash=tools.os_info.is_windows, run_environment=True)
-        with self._build_context():
-            autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-            autotools.libs = []
-            autotools.configure()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
-        if not tools.cross_building(self, skip_x64_x86=True):
-            for exe in ["gettext", "ngettext", "msgcat", "msgmerge"]:
-                self.run("{} --version".format(exe), run_environment=True)
+        if not tools.cross_building(self):
+            for exe in ["gettext", "ngettext", "msgcat", "msgmerge", "msgfmt"]:
+                bin_path = os.path.join("bin", exe)
+                self.run(f"{bin_path} --version", run_environment=True)
+            self.run(os.path.join("bin", "test_package"), run_environment=True)
