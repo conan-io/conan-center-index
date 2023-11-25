ff7564966efcc8976795afce230ebdd31180b9a6
toge <toge.mail@gmail.com>
2023-11-25 09:26:58 +0900

    make aws-c-io transitive_libs

--- recipes/aws-c-auth/all/conanfile.py
+++ recipes/aws-c-auth/all/conanfile.py
@@ -50,14 +50,14 @@ class AwsCAuth(ConanFile):
         if Version(self.version) <= "0.6.17":
             self.requires("aws-c-common/0.8.2", transitive_headers=True, transitive_libs=True)
             self.requires("aws-c-cal/0.5.13")
-            self.requires("aws-c-io/0.10.20", transitive_headers=True)
+            self.requires("aws-c-io/0.10.20", transitive_headers=True, transitive_libs=True)
             self.requires("aws-c-http/0.6.13", transitive_headers=True)
             if Version(self.version) >= "0.6.5":
                 self.requires("aws-c-sdkutils/0.1.3", transitive_headers=True)
         else:
             self.requires("aws-c-common/0.9.6", transitive_headers=True, transitive_libs=True)
             self.requires("aws-c-cal/0.6.9")
-            self.requires("aws-c-io/0.13.32", transitive_headers=True)
+            self.requires("aws-c-io/0.13.32", transitive_headers=True, transitive_libs=True)
             self.requires("aws-c-http/0.7.14", transitive_headers=True)
             self.requires("aws-c-sdkutils/0.1.12", transitive_headers=True)
 
