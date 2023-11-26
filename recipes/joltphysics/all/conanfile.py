15d6195bafddb7da6c44b4fd2bbf125aa199c727
toge <toge.mail@gmail.com>
2023-11-26 15:39:29 +0900

    rm Jolt.cmake

--- recipes/joltphysics/all/conanfile.py
+++ recipes/joltphysics/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 from conan.tools.scm import Version
 import os
@@ -151,6 +151,7 @@ class JoltPhysicsConan(ConanFile):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
+        rm(self, "*.cmake", os.path.join(self.package_folder, "include", "Jolt"))
 
     def package_info(self):
         self.cpp_info.libs = ["Jolt"]
