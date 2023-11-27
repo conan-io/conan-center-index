7409b7c97ef7386fb17fc5a3f59cfdc8fec07f80
Andrew Marshall <andrew.marshall3@sky.uk>
2023-11-27 16:50:02 +0000

    ENTUI-527: simplify boost dependency options

--- recipes/launchdarkly/all/conanfile.py
+++ recipes/launchdarkly/all/conanfile.py
@@ -28,28 +28,7 @@ class LaunchDarklyConan(ConanFile):
     }
     default_options = {
         "shared": False,
-        "fPIC": True,
-        "boost/*:without_contract": True,
-        "boost/*:without_fiber": True,
-        "boost/*:without_filesystem": True,
-        "boost/*:without_graph": True,
-        "boost/*:without_graph_parallel": True,
-        "boost/*:without_iostreams": True,
-        "boost/*:without_locale": True,
-        "boost/*:without_log": True,
-        "boost/*:without_math": True,
-        "boost/*:without_mpi": True,
-        "boost/*:without_nowide": True,
-        "boost/*:without_program_options": True,
-        "boost/*:without_python": True,
-        "boost/*:without_random": True,
-        "boost/*:without_regex": True,
-        "boost/*:without_serialization": True,
-        "boost/*:without_stacktrace": True,
-        "boost/*:without_test": True,
-        "boost/*:without_timer": True,
-        "boost/*:without_type_erasure": True,
-        "boost/*:without_wave": True
+        "fPIC": True
     }
 
     @property
