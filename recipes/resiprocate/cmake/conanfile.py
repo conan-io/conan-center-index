dd938f478f1f2312da5ddcc967f2ee04832e069d
Gregor Jasny <gjasny@googlemail.com>
2023-11-27 08:06:45 +0100

    Update recipes/resiprocate/cmake/conanfile.py
    Co-authored-by: Martin Valgur <martin.valgur@gmail.com>

--- recipes/resiprocate/cmake/conanfile.py
+++ recipes/resiprocate/cmake/conanfile.py
@@ -2,11 +2,13 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
 
