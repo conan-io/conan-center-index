16fa97f9d2040aef29c3e46f2f30c2b0e4199d5d
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-11-27 16:31:52 +0000

    opencv/4.x: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericLemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/opencv/4.x/conanfile.py
+++ recipes/opencv/4.x/conanfile.py
@@ -1113,7 +1113,7 @@ class OpenCVConan(ConanFile):
         elif self.options.get_safe("with_jpeg") == "libjpeg-turbo":
             self.requires("libjpeg-turbo/3.0.1")
         elif self.options.get_safe("with_jpeg") == "mozjpeg":
-            self.requires("mozjpeg/4.1.3")
+            self.requires("mozjpeg/4.1.5")
         if self.options.get_safe("with_jpeg2000") == "jasper":
             self.requires("jasper/4.0.0")
         elif self.options.get_safe("with_jpeg2000") == "openjpeg":
