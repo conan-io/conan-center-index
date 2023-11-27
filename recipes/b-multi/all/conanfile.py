dc92aadc396a2aa578febeb99206163426f099a6
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-11-27 08:05:10 -0800

    Update recipes/b-multi/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/b-multi/all/conanfile.py
+++ recipes/b-multi/all/conanfile.py
@@ -16,6 +16,33 @@ class MultiConan(ConanFile):
     package_type = "header-library"
     no_copy_source = True
 
+	@property
+    def _min_cppstd(self):
+        return 17
+
+    @property
+    def _compilers_minimum_version(self):
+        return {"gcc": "8",
+                "clang": "7",
+                "apple-clang": "12",
+                "Visual Studio": "16",
+                "msvc": "192"}
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
     def package(self):
         # This will also copy the "include" folder
         copy(self, "*.hpp", self.source_folder, self.package_folder)
