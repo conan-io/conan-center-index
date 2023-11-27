ab0ace11621b8b9e5c72c6c8a1a44d26b4eea76d
toge <toge.mail@gmail.com>
2023-11-27 11:36:52 +0900

    drop support gcc5 in 0.172.0

--- recipes/jsoncons/all/conanfile.py
+++ recipes/jsoncons/all/conanfile.py
@@ -1,6 +1,9 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import get, copy
 from conan.tools.layout import basic_layout
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.50.0"
@@ -23,11 +26,22 @@ class JsonconsConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
+    @property
+    def _min_cppstd(self):
+        return 11
+
     def layout(self):
         basic_layout(self, src_folder="src")
 
     def package_id(self):
         self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        if Version(self.version) >= "0.172.0" and \
+            self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "6":
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support GCC < 6.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
