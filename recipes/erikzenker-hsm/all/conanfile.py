8b67f3dfac5df7720552bea1c7f5f7006b73bb18
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-11-28 20:50:10 +0100

    bump boost

--- recipes/erikzenker-hsm/all/conanfile.py
+++ recipes/erikzenker-hsm/all/conanfile.py
@@ -29,7 +29,7 @@ class HsmConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.81.0")
+        self.requires("boost/1.83.0")
 
     def package_id(self):
         self.info.clear()
