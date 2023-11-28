035b58e24d851dbd2ea2f541fd5c9500c0d58ee9
Nelles, David <david.nelles@dlr.de>
2023-11-28 13:05:06 +0100

    refactor: Slitted genration and build steps

--- recipes/cantera/all/conanfile.py
+++ recipes/cantera/all/conanfile.py
@@ -2,12 +2,13 @@ import os
 
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import get
+from conan.tools.files import get, save, load, chdir
 from conan.tools.layout import basic_layout
 
 class canteraRecipe(ConanFile):
     name = "cantera"
     tool_requires="scons/4.3.0"
+    package_type="library"
     options = {
         "shared": [True, False]
     }
@@ -43,75 +44,65 @@ class canteraRecipe(ConanFile):
 
     def requirements(self):
         self.requires("boost/1.83.0", headers=True, libs=False, build=False, visible=False)
-        self.requires("fmt/10.1.1")
+        self.requires("fmt/10.1.1", headers=True)
         self.requires("yaml-cpp/0.7.0")
         self.requires("eigen/3.4.0")
         self.requires("sundials/5.4.0")
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def generate(self):
-        self.scons_extra_inc_dirs = \
-            self.dependencies["fmt"].cpp_info.includedirs +\
-            self.dependencies["yaml-cpp"].cpp_info.includedirs +\
-            self.dependencies["eigen"].cpp_info.includedirs
-        
-        self.scons_extra_lib_dirs = \
-            self.dependencies["fmt"].cpp_info.libdirs +\
-            self.dependencies["yaml-cpp"].cpp_info.libdirs +\
-            self.dependencies["eigen"].cpp_info.libdirs
-        
-        self.scons_sundials_include = self.dependencies["sundials"].cpp_info.includedirs[0]
-        self.scons_sundials_libdir = self.dependencies["sundials"].cpp_info.libdirs[0]
-        
-        self.scons_boost_inc_dir = self.dependencies["boost"].cpp_info.includedirs[0]
+        libs = ["fmt", "yaml-cpp", "eigen"]
+        include_paths = []
+        lib_paths = []
+        for lib in libs:
+            lib_info = self.dependencies[lib].cpp_info
+            include_paths = include_paths + lib_info.includedirs
+            lib_paths = lib_paths + lib_info.libdirs
 
-    def layout(self):
-        basic_layout(self, src_folder="src")
+        sundials_info = self.dependencies["sundials"].cpp_info
 
-    def build(self):
+        opitons = {
+            "libdirname": "lib",
+            "python_package": "none",
+            "f90_interface": "n",
+            "googletest": "none",
+            "versioned_shared_library": "yes",
+            "prefix": self.package_folder,
+            "extra_inc_dirs": os.pathsep.join(include_paths),
+            "extra_lib_dirs": os.pathsep.join(lib_paths),
+            "boost_inc_dir": self.dependencies["boost"].cpp_info.includedirs[0],
+            "sundials_include": sundials_info.includedirs[0],
+            "sundials_libdir": sundials_info.libdirs[0]
+        }
 
         if self.settings.os == "Windows":
-            opt = "toolchain=msvc "
-            cd_modifier = "/d"
-        else:
-            opt = ""
-            cd_modifier = ""
-
-        opt = opt +\
-              "prefix={} ".format(self.package_folder) +\
-              "libdirname=lib " \
-              "python_package=none " \
-              "f90_interface=n " \
-              "googletest=none " \
-              "versioned_shared_library=yes " \
-              "extra_inc_dirs={} ".format(os.pathsep.join(self.scons_extra_inc_dirs)) +\
-              "extra_lib_dirs={} ".format(os.pathsep.join(self.scons_extra_lib_dirs)) +\
-              "boost_inc_dir={} ".format(self.scons_boost_inc_dir) +\
-              "sundials_include={} ".format(self.scons_sundials_include) +\
-              "sundials_libdir={} ".format(self.scons_sundials_libdir)
+            opitons["toolchain"] = "msvc"
 
         if self.settings.build_type == "Debug":
-            opt = opt + "optimize=no "
+            opitons["optimize"] = "no"
         else:
-            opt = opt + "debug=no "
+            opitons["debug"] = "no"
 
-        self.run("cd {} {} && scons build -j4 {}".format(cd_modifier, self.source_folder, opt))
+        escape_str = lambda x: f'"{x}"'
+        scons_args = ' '.join([f"{key}={escape_str(option)}" for key, option in opitons.items()])
+        save(self, os.path.join(self.source_folder, "scons_args"), scons_args)
+
+    def build(self):
+        with chdir(self, self.source_folder):
+            options = load(self, "scons_args")
+            self.run(f'scons build -j4 -Y "{self.source_folder}" {options}')
 
     def package(self):
-        if self.settings.os == "Windows":
-            cd_modifier = "/d"
-        else:
-            cd_modifier = ""
-        
-        self.run("cd {} {} && scons install".format(cd_modifier, self.source_folder))
+        with chdir(self, self.source_folder):
+            self.run(f'scons install -Y "{self.source_folder}"')
 
-    
     def package_info(self):
         self.cpp_info.libs = ["cantera_shared"] if self.options.shared else ["cantera"]
-        self.cpp_info.includedirs = ["include"]
-        self.cpp_info.bindirs = ["bin"]
         self.cpp_info.resdirs = ["data"]
 
         if self.options.shared:
