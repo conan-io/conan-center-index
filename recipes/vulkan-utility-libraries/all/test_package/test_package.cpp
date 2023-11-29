68b624cc4d39f56b78aa2b02f0ae234930e4b412
Martin Valgur <martin.valgur@gmail.com>
2023-11-29 15:08:03 +0200

    vulkan-utility-libraries: handle API change in test_package.cpp

--- recipes/vulkan-utility-libraries/all/test_package/test_package.cpp
+++ recipes/vulkan-utility-libraries/all/test_package/test_package.cpp
@@ -2,8 +2,13 @@
 #include <iostream>
 
 int main() {
+#ifdef USE_PRE_268_API
+    VlLayerSettingSet layerSettingSet = VK_NULL_HANDLE;
+    vlCreateLayerSettingSet("VK_LAYER_LUNARG_conan", nullptr, nullptr, nullptr, &layerSettingSet);
+#else
     VkuLayerSettingSet layerSettingSet = VK_NULL_HANDLE;
     vkuCreateLayerSettingSet("VK_LAYER_LUNARG_conan", nullptr, nullptr, nullptr, &layerSettingSet);
+#endif
     std::cout << VK_EXT_LAYER_SETTINGS_EXTENSION_NAME << std::endl;
     return 0;
 }
