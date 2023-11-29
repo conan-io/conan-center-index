9113788cef9bf11f01bbf6d370ae7676e15c3fab
Martin Valgur <martin.valgur@gmail.com>
2023-11-29 13:26:07 +0200

    Improve test_package.cpp
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/vulkan-utility-libraries/all/test_package/test_package.cpp
+++ recipes/vulkan-utility-libraries/all/test_package/test_package.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 
 int main() {
+    VkuLayerSettingSet layerSettingSet = VK_NULL_HANDLE;
+    vkuCreateLayerSettingSet("VK_LAYER_LUNARG_conan", nullptr, nullptr, nullptr, &layerSettingSet);
     std::cout << VK_EXT_LAYER_SETTINGS_EXTENSION_NAME << std::endl;
     return 0;
 }
