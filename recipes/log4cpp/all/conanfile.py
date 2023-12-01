edcb363c24f22128293f18005710e1a6cfa25b34
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-12-01 17:02:33 +0100

    fix copy of license file

--- recipes/log4cpp/all/conanfile.py
+++ recipes/log4cpp/all/conanfile.py
@@ -70,7 +70,7 @@ class Log4cppConan(ConanFile):
         autotools.make()
 
     def package(self):
-        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "COPYING", src=os.path.join(self.source_folder, "log4cpp"), dst=os.path.join(self.package_folder, "licenses"))
         autotools = Autotools(self)
         autotools.install()
         rmdir(self, os.path.join(self.package_folder, "bin"))
