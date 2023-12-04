148ab6d1853a87fb543dce53683f4f397ba873d0
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2023-12-04 20:25:09 +0000

    libdrm/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericLemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/libdrm/all/conanfile.py
+++ recipes/libdrm/all/conanfile.py
@@ -90,7 +90,7 @@ class LibdrmConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("meson/1.3.0")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.0.3")
+            self.tool_requires("pkgconf/2.1.0")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
