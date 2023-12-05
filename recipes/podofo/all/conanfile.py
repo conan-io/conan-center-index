64587bd9860c0d7a13c463be2deb67d85b52deac
Martin Valgur <martin.valgur@gmail.com>
2023-12-05 15:27:56 +0200

    podofo: add v0.10.2, switch source URLs to the new GitHub project, add package_type

--- recipes/podofo/all/conanfile.py
+++ recipes/podofo/all/conanfile.py
@@ -1,12 +1,13 @@
-from conan import ConanFile, conan_version
-from conan.tools.build import check_min_cppstd, valid_min_cppstd
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
-from conan.tools.microsoft import is_msvc
-from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.53.0"
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd, valid_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, replace_in_file
+from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
+
+required_conan_version = ">=2.0"
 
 
 class PodofoConan(ConanFile):
@@ -17,6 +18,7 @@ class PodofoConan(ConanFile):
     description = "PoDoFo is a library to work with the PDF file format."
     topics = ("pdf")
 
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -45,6 +47,7 @@ class PodofoConan(ConanFile):
 
     def export_sources(self):
         export_conandata_patches(self)
+        copy(self, "conan_deps.cmake", self.recipe_folder, os.path.join(self.export_sources_folder, "src"))
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -53,6 +56,8 @@ class PodofoConan(ConanFile):
             # libunistring recipe raises for Visual Studio
             # TODO: Enable again when fixed?
             self.options.with_unistring = False
+        if Version(self.version) >= "0.10":
+            del self.options.with_openssl
 
     def configure(self):
         if self.options.shared:
@@ -62,11 +67,14 @@ class PodofoConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
+        # None of the dependencies are used in public headers
         self.requires("freetype/2.13.2")
         self.requires("zlib/[>=1.2.11 <2]")
+        if Version(self.version) >= "0.10":
+            self.requires("libxml2/2.11.5")  # 2.12+ has minor incompatibilities
         if self.settings.os != "Windows":
             self.requires("fontconfig/2.15.0")
-        if self.options.with_openssl:
+        if self.options.get_safe("with_openssl", True):
             self.requires("openssl/[>=1.1 <4]")
         if self.options.with_libidn:
             self.requires("libidn/1.36")
@@ -80,25 +88,31 @@ class PodofoConan(ConanFile):
             self.requires("libunistring/0.9.10")
 
     def validate(self):
-        if self.info.settings.compiler.get_safe("cppstd") and Version(self.version) >= "0.9.7":
+        if Version(self.version) >= "0.10":
+            check_min_cppstd(self, 17)
+        else:
             check_min_cppstd(self, 11)
-        if conan_version.major >= 2 and Version(self.version) < "0.9.7":
-            # FIXME: linter complains, but function is there
-            # https://docs.conan.io/2.0/reference/tools/build.html?highlight=check_min_cppstd#conan-tools-build-check-max-cppstd
-            import sys
-            check_max_cppstd = getattr(sys.modules['conan.tools.build'], 'check_max_cppstd')
-            # INFO: error: no template named 'auto_ptr' in namespace 'std'. Removed in C++17.
-            check_max_cppstd(self, 14)
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-                  destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
+        if Version(self.version) >= "0.10":
+            replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                            "(PoDoFo)",
+                            "(PoDoFo)\n\ninclude(conan_deps.cmake)")
+            # TODO: also fix libxml2 v2.12.x incompatibilities and submit a patch
+            replace_in_file(self, os.path.join(self.source_folder, "src/podofo/private/XmlUtils.h"),
+                            "#include <libxml/tree.h>",
+                            "#include <libxml/tree.h>\n#include <libxml/xmlerror.h>")
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["PODOFO_BUILD_TOOLS"] = self.options.with_tools
-        tc.variables["PODOFO_BUILD_SHARED"] = self.options.shared
         tc.variables["PODOFO_BUILD_STATIC"] = not self.options.shared
+        if Version(self.version) < "0.10":
+            tc.variables["PODOFO_BUILD_SHARED"] = self.options.shared
+        tc.variables["PODOFO_BUILD_TOOLS"] = self.options.with_tools
+        tc.variables["PODOFO_BUILD_TEST"] = False
+        tc.variables["PODOFO_BUILD_EXAMPLES"] = False
         if not self.options.threadsafe:
             tc.variables["PODOFO_NO_MULTITHREAD"] = True
         if Version(self.version) >= "0.9.7" and not valid_min_cppstd(self, 11):
@@ -108,22 +122,27 @@ class PodofoConan(ConanFile):
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
 
         # Custom CMake options injected in our patch, required to ensure reproducible builds
-        tc.variables["PODOFO_WITH_OPENSSL"] = self.options.with_openssl
-        tc.variables["PODOFO_WITH_LIBIDN"] = self.options.with_libidn
-        tc.variables["PODOFO_WITH_LIBJPEG"] = self.options.with_jpeg
-        tc.variables["PODOFO_WITH_TIFF"] = self.options.with_tiff
-        tc.variables["PODOFO_WITH_PNG"] = self.options.with_png
-        tc.variables["PODOFO_WITH_UNISTRING"] = self.options.with_unistring
+        tc.variables["WITH_ZLIB"] = True
+        tc.variables["WITH_FREETYPE"] = True
+        tc.variables["WITH_FONTCONFIG"] = self.settings.os != "Windows"
+        tc.variables["WITH_OPENSSL"] = self.options.get_safe("with_openssl", True)
+        tc.variables["WITH_LIBIDN"] = self.options.with_libidn
+        tc.variables["WITH_LIBXML2"] = Version(self.version) >= "0.10"
+        tc.variables["WITH_JPEG"] = self.options.with_jpeg
+        tc.variables["WITH_LIBJPEG"] = self.options.with_jpeg
+        tc.variables["WITH_TIFF"] = self.options.with_tiff
+        tc.variables["WITH_PNG"] = self.options.with_png
+        tc.variables["WITH_UNISTRING"] = self.options.with_unistring
         tc.variables["PODOFO_HAVE_OPENSSL_1_1"] = Version(self.dependencies["openssl"].ref.version) >= "1.1"
-        if self.options.with_openssl and ("no_rc4" in self.dependencies["openssl"].options):
+        if self.options.get_safe("with_openssl", True) and ("no_rc4" in self.dependencies["openssl"].options):
             tc.variables["PODOFO_HAVE_OPENSSL_NO_RC4"] = self.dependencies["openssl"].options.no_rc4
+        tc.variables["PODOFO_V10"] = Version(self.version) >= "0.10"
         tc.generate()
 
         deps = CMakeDeps(self)
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
