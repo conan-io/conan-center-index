55ae84113d438d0431399109fda484911e33413f
Martin Valgur <martin.valgur@gmail.com>
2023-12-06 21:18:39 +0200

    seqan3: add v3.3.0

--- recipes/seqan3/all/conanfile.py
+++ recipes/seqan3/all/conanfile.py
@@ -44,11 +44,8 @@ class Seqan3Conan(ConanFile):
             check_min_cppstd(self, self._min_cppstd)
 
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version:
-            if Version(self.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration("SeqAn3 requires C++20, which your compiler does not fully support.")
-        else:
-            self.output.warning("SeqAn3 requires C++20. Your compiler is unknown. Assuming it supports C++20.")
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration("SeqAn3 requires C++20, which your compiler does not fully support.")
 
         if self.settings.compiler == "gcc" and self.settings.compiler.libcxx != "libstdc++11":
             self.output.warning("SeqAn3 does not actively support libstdc++, consider using libstdc++11 instead.")
