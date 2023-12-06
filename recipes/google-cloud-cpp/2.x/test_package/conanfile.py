8c66565e4f4bcc00db88d0157b31dff37f28d103
Carlos O'Ryan <coryan@google.com>
2023-12-06 19:11:36 +0000

    Tested locally with Conan v1

--- recipes/google-cloud-cpp/2.x/test_package/conanfile.py
+++ recipes/google-cloud-cpp/2.x/test_package/conanfile.py
@@ -6,6 +6,7 @@ from conan.tools.build import can_run
 from conan.tools.env import VirtualRunEnv
 from conan.tools.scm import Version
 
+
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     test_type = "explicit"
@@ -15,10 +16,19 @@ class TestPackageConan(ConanFile):
 
     def layout(self):
         cmake_layout(self)
+    
+    def _supports_compute(self):
+        if not hasattr(self, "dependencies"):
+            # This is typically a Conan v1 build. We skip the test for compute
+            # because it is difficult to establish the `google-cloud-cpp`
+            # version, and Conan v1 is being retired, and the support is tested
+            # as part of the Conan v2 build.
+            return False
+        return Version(self.dependencies["google-cloud-cpp"].ref.version) >= "2.19.0"
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["WITH_COMPUTE"] = Version(self.dependencies["google-cloud-cpp"].ref.version) >= '2.19.0'
+        tc.variables["WITH_COMPUTE"] = self._supports_compute()
         tc.generate()
         # Environment so that the compiled test executable can load shared libraries
         runenv = VirtualRunEnv(self)
@@ -37,6 +47,6 @@ class TestPackageConan(ConanFile):
         for test in ["bigtable", "pubsub", "spanner", "speech", "storage"]:
             cmd = os.path.join(self.cpp.build.bindir, test)
             self.run(cmd, env="conanrun")
-        if Version(self.dependencies["google-cloud-cpp"].ref.version) >= '2.19.0':
+        if self._supports_compute():
             cmd = os.path.join(self.cpp.build.bindir, "compute")
             self.run(cmd, env="conanrun")
