b339966a68dfae5c4242b5a0f00dde04f1343dc7
Martin Valgur <martin.valgur@gmail.com>
2023-12-09 22:56:46 +0200

    imagemagick: fix CMake dependency handling

--- recipes/imagemagick/7.1/conanfile.py
+++ recipes/imagemagick/7.1/conanfile.py
@@ -1,10 +1,9 @@
 import os
 
 from conan import ConanFile
-from conan.tools.build import stdcpp_library
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import copy, get, rmdir
+from conan.tools.files import get, rmdir, replace_in_file
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
@@ -26,66 +25,74 @@ class ImageMagicConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "cipher_support": [True, False],
+        "exclude_deprecated": [True, False],
         "hdri": [True, False],
+        "magickpp": [True, False],
         "quantum_depth": [8, 16, 32],
-        "with_zlib": [True, False],
+        "utilities": [True, False],
         "with_bzlib": [True, False],
-        "with_zstd": [True, False],
-        "with_lzma": [True, False],
-        "with_lcms": [True, False],
-        "with_openexr": [True, False],
+        "with_djvu": [True, False],
+        "with_freetype": [True, False],
         "with_heic": [True, False],
         "with_jbig": [True, False],
         "with_jpeg": [None, "libjpeg", "libjpeg-turbo"],
         "with_jxl": [True, False],
+        "with_lcms": [True, False],
+        "with_lzma": [True, False],
+        "with_openexr": [True, False],
         "with_openjp2": [True, False],
+        "with_openmp": [True, False],
         "with_pango": [True, False],
         "with_png": [True, False],
+        "with_raw": [True, False],
+        "with_threads": [True, False],
         "with_tiff": [True, False],
         "with_webp": [True, False],
+        "with_x11": [True, False],
         "with_xml2": [True, False],
-        "with_freetype": [True, False],
-        "with_djvu": [True, False],
-        "with_raw": [True, False],
-        "with_openmp": [True, False],
-        "with_threads": [True, False],
-        "magickpp": [True, False],
-        "utilities": [True, False],
+        "with_zlib": [True, False],
+        "with_zstd": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
+        "cipher_support": True,
+        "exclude_deprecated": True,
         "hdri": True,
+        "magickpp": True,
         "quantum_depth": 16,
-        "with_zlib": True,
+        "utilities": True,
         "with_bzlib": True,
-        "with_zstd": False,
-        "with_lzma": True,
-        "with_lcms": True,
-        "with_openexr": True,
+        "with_djvu": False,
+        "with_freetype": True,
         "with_heic": True,
         "with_jbig": True,
         "with_jpeg": "libjpeg",
         "with_jxl": False,
+        "with_lcms": True,
+        "with_lzma": True,
+        "with_openexr": True,
         "with_openjp2": True,
+        "with_openmp": True,
         "with_pango": False,  # FIXME: re-enable once migrated
         "with_png": True,
-        "with_tiff": True,
-        "with_webp": False,
-        "with_xml2": True,
-        "with_freetype": True,
-        "with_djvu": False,
         "with_raw": False,
-        "with_openmp": True,
         "with_threads": True,
-        "magickpp": True,
-        "utilities": True,
+        "with_tiff": True,
+        "with_webp": True,
+        "with_x11": True,
+        "with_xml2": True,
+        "with_zlib": True,
+        "with_zstd": True,
     }
     short_paths = True
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+        if self.settings.os not in ["Linux", "FreeBSD"]:
+            del self.options.with_x11
 
     def configure(self):
         if self.options.shared:
@@ -99,18 +106,10 @@ class ImageMagicConan(ConanFile):
 
     def requirements(self):
         # None of the dependencies need transitive_headers=True
-        if self.options.with_zlib:
-            self.requires("zlib/1.3")
         if self.options.with_bzlib:
             self.requires("bzip2/1.0.8")
-        if self.options.with_zstd:
-            self.requires("zstd/1.5.5")
-        if self.options.with_lzma:
-            self.requires("xz_utils/5.4.5")
-        if self.options.with_lcms:
-            self.requires("lcms/2.14")
-        if self.options.with_openexr:
-            self.requires("openexr/3.2.1")
+        if self.options.with_freetype:
+            self.requires("freetype/2.13.2")
         if self.options.with_heic:
             self.requires("libheif/1.16.2")
         if self.options.with_jbig:
@@ -121,27 +120,37 @@ class ImageMagicConan(ConanFile):
             self.requires("libjpeg-turbo/3.0.1")
         if self.options.with_jxl:
             self.requires("libjxl/0.6.1")
+        if self.options.with_lcms:
+            self.requires("lcms/2.14")
+        if self.options.with_lzma:
+            self.requires("xz_utils/5.4.5")
+        if self.options.with_openexr:
+            self.requires("openexr/3.2.1")
         if self.options.with_openjp2:
             self.requires("openjpeg/2.5.0")
+        if self.options.with_openmp:
+            if self.settings.compiler in ["clang", "apple-clang"]:
+                self.requires("llvm-openmp/17.0.4")
         if self.options.with_pango:
             self.requires("pango/1.48.5")
         if self.options.with_png:
             self.requires("libpng/1.6.40")
+        if self.options.with_raw:
+            self.requires("libraw/0.21.1")
         if self.options.with_tiff:
             self.requires("libtiff/4.6.0")
         if self.options.with_webp:
             self.requires("libwebp/1.3.2")
         if self.options.with_xml2:
             self.requires("libxml2/2.12.2")
-        if self.options.with_freetype:
-            self.requires("freetype/2.13.2")
-        if self.options.with_raw:
-            self.requires("libraw/0.21.1")
+        if self.options.with_zlib:
+            self.requires("zlib/1.3")
+        if self.options.with_zstd:
+            self.requires("zstd/1.5.5")
         if self.options.with_djvu:
             self.output.warning("Conan package for djvu is not available, this package will be used from system.")
-        if self.options.with_openmp:
-            if self.settings.compiler in ["clang", "apple-clang"]:
-                self.requires("llvm-openmp/17.0.4")
+        if self.options.get_safe("with_x11"):
+            self.requires("xorg/system")
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.19 <4]")
@@ -165,52 +174,138 @@ class ImageMagicConan(ConanFile):
         venv.generate()
 
         tc = CMakeToolchain(self)
-        tc.variables["MAGICK_BUILD_STATIC"] = not self.options.shared
-        tc.variables["MAGICK_HDRI_ENABLE"] = self.options.hdri
-        tc.variables["MAGICKCORE_QUANTUM_DEPTH"] = self.options.quantum_depth
-        tc.variables["BUILD_MAGICKPP"] = self.options.magickpp
-        tc.variables["BUILD_UTILITIES"] = self.options.utilities
-        tc.variables["OPENMP_SUPPORT"] = self.options.with_openmp
-        tc.variables["THREADS_SUPPORT"] = self.options.with_threads
+        tc.cache_variables["MAGICK_BUILD_STATIC"] = not self.options.shared
+        tc.cache_variables["BUILD_MAGICKPP"] = self.options.magickpp
+        tc.cache_variables["BUILD_UTILITIES"] = self.options.utilities
+        tc.cache_variables["CIPHER_SUPPORT"] = self.options.cipher_support
+        tc.cache_variables["EXCLUDE_DEPRECATED"] = self.options.exclude_deprecated
+        tc.cache_variables["MAGICKCORE_QUANTUM_DEPTH"] = self.options.quantum_depth
+        tc.cache_variables["MAGICK_HDRI_ENABLE"] = self.options.hdri
 
-        tc.variables["BZLIB_DELEGATE"] = self.options.with_bzlib
-        tc.variables["LZMA_DELEGATE"] = self.options.with_lzma
-        tc.variables["ZLIB_DELEGATE"] = self.options.with_zlib
-        tc.variables["ZSTD_DELEGATE"] = self.options.with_zstd
-        tc.variables["LCMS_DELEGATE"] = self.options.with_lcms
-        tc.variables["FREETYPE_DELEGATE"] = self.options.with_freetype
-        tc.variables["XML_DELEGATE"] = self.options.with_xml2
-        tc.variables["TIFF_DELEGATE"] = self.options.with_tiff
-        tc.variables["HEIC_DELEGATE"] = self.options.with_heic
-        tc.variables["JBIG_DELEGATE"] = self.options.with_jbig
-        tc.variables["JPEG_DELEGATE"] = self.options.with_jpeg
-        tc.variables["LIBOPENJP2_DELEGATE"] = self.options.with_openjp2
-        tc.variables["OPENEXR_DELEGATE"] = self.options.with_openexr
-        tc.variables["PNG_DELEGATE"] = self.options.with_png
-        tc.variables["RAW_R_DELEGATE"] = self.options.with_raw
-        tc.variables["WEBP_DELEGATE"] = self.options.with_webp
-        tc.variables["JXL_DELEGATE"] = self.options.with_jxl
+        tc.cache_variables["AUTOTRACE_DELEGATE"] = False
+        tc.cache_variables["BZLIB_DELEGATE"] = self.options.with_bzlib
+        tc.cache_variables["CAIRO_DELEGATE"] = False
+        tc.cache_variables["DJVU_DELEGATE"] = False
+        tc.cache_variables["DPS_DELEGATE"] = False
+        tc.cache_variables["FFTW_DELEGATE"] = False
+        tc.cache_variables["FLIF_DELEGATE"] = False
+        tc.cache_variables["FONTCONFIG_DELEGATE"] = False
+        tc.cache_variables["FPX_DELEGATE"] = False
+        tc.cache_variables["FREETYPE_DELEGATE"] = self.options.with_freetype
+        tc.cache_variables["GS_DELEGATE"] = False
+        tc.cache_variables["GVC_DELEGATE"] = False
+        tc.cache_variables["HasJEMALLOC"] = False
+        tc.cache_variables["HasUMEM"] = False
+        tc.cache_variables["HEIC_DELEGATE"] = self.options.with_heic
+        tc.cache_variables["JBIG_DELEGATE"] = self.options.with_jbig
+        tc.cache_variables["JPEG_DELEGATE"] = self.options.with_jpeg
+        tc.cache_variables["JXL_DELEGATE"] = self.options.with_jxl
+        tc.cache_variables["LCMS_DELEGATE"] = self.options.with_lcms
+        tc.cache_variables["LIBOPENJP2_DELEGATE"] = self.options.with_openjp2
+        tc.cache_variables["LQR"] = False
+        tc.cache_variables["LTDL_DELEGATE"] = False
+        tc.cache_variables["LZMA_DELEGATE"] = self.options.with_lzma
+        tc.cache_variables["OPENCLLIB_DELEGATE"] = False
+        tc.cache_variables["OPENEXR_DELEGATE"] = self.options.with_openexr
+        tc.cache_variables["OPENMP_SUPPORT"] = self.options.with_openmp
+        tc.cache_variables["PANGO_DELEGATE"] = self.options.with_pango
+        tc.cache_variables["PANGOCAIRO_DELEGATE"] = False
+        tc.cache_variables["PNG_DELEGATE"] = self.options.with_png
+        tc.cache_variables["RAQM_DELEGATE"] = False
+        tc.cache_variables["RAW_R_DELEGATE"] = self.options.with_raw
+        tc.cache_variables["RSVG_DELEGATE"] = False
+        tc.cache_variables["THREADS_SUPPORT"] = self.options.with_threads
+        tc.cache_variables["TIFF_DELEGATE"] = self.options.with_tiff
+        tc.cache_variables["WEBP_DELEGATE"] = self.options.with_webp
+        tc.cache_variables["WEBPMUX_DELEGATE"] = False
+        tc.cache_variables["WMF_DELEGATE"] = False
+        tc.cache_variables["X11_DELEGATE"] = self.options.get_safe("with_x11", False)
+        tc.cache_variables["XML_DELEGATE"] = self.options.with_xml2
+        tc.cache_variables["ZLIB_DELEGATE"] = self.options.with_zlib
+        tc.cache_variables["ZSTD_DELEGATE"] = self.options.with_zstd
         tc.generate()
 
+        # TODO: fix this bug in the libheif recipe
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.dependencies["libheif"].cpp_info.components["heif"].system_libs.append("dl")
+
+        cmake_names = {
+            "autotrace": "AUTOTRACE",
+            "bzip2": "BZip2",
+            "cairo": "Cairo",
+            "djvu": "DJVU",
+            "dps": "DPS",
+            "fftw": "FFTW",
+            "flashpix": "FlashPIX",
+            "flif": "FLIF",
+            "fontconfig": "Fontconfig",
+            "freetype": "Freetype",
+            "ghostscript": "Ghostscript",
+            "gvc": "GVC",
+            "jbig": "JBIG",
+            "jemalloc": "Jemalloc",
+            "jpeg": "JPEG",
+            "lcms": "lcms",
+            "libheif": "libheif",
+            "libjxl": "libjxl",
+            "libpng": "PNG",
+            "libraw": "libraw",
+            "libtiff": "TIFF",
+            "libwebp": "WebP",
+            "libxml2": "LibXml2",
+            "lqr": "Lqr",
+            "ltdl": "LTDL",
+            "opencl": "OpenCL",
+            "openexr": "OpenEXR",
+            "openjpeg": "OpenJPEG",
+            "pango": "Pango",
+            "raqm": "RAQM",
+            "rsvg": "Rsvg",
+            "umem": "UMEM",
+            "webpmux": "WEBPMUX",
+            "wmf": "WMF",
+            "xz_utils": "LibLZMA",
+            "zlib": "ZLIB",
+            "zstd": "zstd",
+        }
         deps = CMakeDeps(self)
+        for dep, cmake_name in cmake_names.items():
+            deps.set_property(dep, "cmake_file_name", cmake_name)
+            deps.set_property(dep, "cmake_target_name", f"{cmake_name}::{cmake_name}")
         deps.generate()
 
         deps = PkgConfigDeps(self)
         deps.generate()
 
+    def _patch_sources(self):
+        # Fix mismatch with Conan's CMake variable capitalization.
+        # Also add REQUIRED since we explicitly enable/disable all dependencies.
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "    find_package(${MAGICK_FIND_NAME} QUIET)\n",
+                        "    find_package(${MAGICK_FIND_NAME} REQUIRED)\n"
+                        "    string(TOUPPER ${MAGICK_FIND_NAME} name_upper)\n"
+                        "    set(${name_upper}_FOUND ${${MAGICK_FIND_NAME}_FOUND})\n")
+        # Fix incorrect .so install location
+        for subdir in ["MagickCore", "MagickWand", "Magick++"]:
+            replace_in_file(self, os.path.join(self.source_folder, subdir, "CMakeLists.txt"),
+                            "LIBRARY DESTINATION bin\n",
+                            "LIBRARY DESTINATION lib\n")
+
     def build(self):
+        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
-    def _libname(self, library):
-        suffix = "HDRI" if self.options.hdri else ""
-        return f"{library}-{Version(self, self.version).major}.Q{self.options.quantum_depth}{suffix}"
-
     def package(self):
-        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        # License files are installed by the project
         cmake = CMake(self)
         cmake.install()
+
+    def _libname(self, library):
+        suffix = "HDRI" if self.options.hdri else ""
+        major_version = Version(self.version).major
+        return f"{library}-{major_version}.Q{self.options.quantum_depth}{suffix}"
 
     def package_info(self):
         # FIXME: fully model the official FindImageMagick https://cmake.org/cmake/help/latest/module/FindImageMagick.html
@@ -220,18 +315,10 @@ class ImageMagicConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "ImageMagick::ImageMagick")
 
         core_requires = []
-        if self.options.with_zlib:
-            core_requires.append("zlib::zlib")
         if self.options.with_bzlib:
             core_requires.append("bzip2::bzip2")
-        if self.options.with_zstd:
-            core_requires.append("zstd::zstd")
-        if self.options.with_lzma:
-            core_requires.append("xz_utils::xz_utils")
-        if self.options.with_lcms:
-            core_requires.append("lcms::lcms")
-        if self.options.with_openexr:
-            core_requires.append("openexr::openexr")
+        if self.options.with_freetype:
+            core_requires.append("freetype::freetype")
         if self.options.with_heic:
             core_requires.append("libheif::libheif")
         if self.options.with_jbig:
@@ -242,22 +329,34 @@ class ImageMagicConan(ConanFile):
             core_requires.append("libjpeg-turbo::jpeg")
         if self.options.with_jxl:
             core_requires.append("libjxl::libjxl")
+        if self.options.with_lcms:
+            core_requires.append("lcms::lcms")
+        if self.options.with_lzma:
+            core_requires.append("xz_utils::xz_utils")
+        if self.options.with_openexr:
+            core_requires.append("openexr::openexr")
         if self.options.with_openjp2:
             core_requires.append("openjpeg::openjpeg")
+        if self.options.with_openmp and self.settings.compiler in ["clang", "apple-clang"]:
+            core_requires.append("llvm-openmp::llvm-openmp")
         if self.options.with_pango:
             core_requires.append("pango::pango")
         if self.options.with_png:
             core_requires.append("libpng::libpng")
+        if self.options.with_raw:
+            core_requires.append("libraw::libraw")
         if self.options.with_tiff:
             core_requires.append("libtiff::libtiff")
         if self.options.with_webp:
             core_requires.append("libwebp::libwebp")
         if self.options.with_xml2:
             core_requires.append("libxml2::libxml2")
-        if self.options.with_freetype:
-            core_requires.append("freetype::freetype")
-        if self.options.with_raw:
-            core_requires.append("libraw::libraw")
+        if self.options.with_zlib:
+            core_requires.append("zlib::zlib")
+        if self.options.with_zstd:
+            core_requires.append("zstd::zstd")
+        if self.options.get_safe("with_x11"):
+            core_requires.append("xorg::xorg")
 
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["MagickCore"].system_libs.append("pthread")
@@ -283,9 +382,6 @@ class ImageMagicConan(ConanFile):
                 openmp_flags = ["-fopenmp"]
             elif self.settings.compiler in ["clang", "apple-clang"]:
                 openmp_flags = ["-Xpreprocessor", "-fopenmp"]
-            # TODO: check if OpenMP pargmas are used in exported headers
-            # self.cpp_info.components["MagickCore"].cflags.extend(openmp_flags)
-            # self.cpp_info.components["MagickCore"].cxxflags.extend(openmp_flags)
             self.cpp_info.components["MagickCore"].sharedlinkflags = openmp_flags
             self.cpp_info.components["MagickCore"].exelinkflags = openmp_flags
 
