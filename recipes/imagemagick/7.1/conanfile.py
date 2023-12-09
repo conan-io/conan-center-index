65f95f12903abbf2228911b12c030acc59f77b8a
Martin Valgur <martin.valgur@gmail.com>
2023-12-09 18:49:29 +0200

    imagemagick: migrate and update

--- recipes/imagemagick/7.1/conanfile.py
+++ recipes/imagemagick/7.1/conanfile.py
@@ -1,23 +1,28 @@
-from conans import ConanFile, tools, CMake
-from conans.errors import ConanInvalidConfiguration
 import os
-import glob
 
-required_conan_version = ">=1.33.0"
+from conan import ConanFile
+from conan.tools.build import stdcpp_library
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import copy, get, rmdir
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.53.0"
 
 
 class ImageMagicConan(ConanFile):
     name = "imagemagick"
-    description = (
-        "ImageMagick is a free and open-source software suite for displaying, converting, and editing "
-        "raster image and vector image files"
-    )
-    topics = ("imagemagick", "images", "manipulating")
+    description = ("ImageMagick is a free and open-source software suite for displaying, "
+                   "converting, and editing raster image and vector image files")
+    license = "ImageMagick"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://imagemagick.org"
-    license = "ImageMagick"
+    topics = ("images", "manipulating")
+
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
-    generators = "pkg_config", "cmake", "cmake_find_package"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
@@ -63,7 +68,7 @@ class ImageMagicConan(ConanFile):
         "with_jpeg": "libjpeg",
         "with_jxl": False,
         "with_openjp2": True,
-        "with_pango": True,
+        "with_pango": False,  # FIXME: re-enable once migrated
         "with_png": True,
         "with_tiff": True,
         "with_webp": False,
@@ -76,63 +81,7 @@ class ImageMagicConan(ConanFile):
         "magickpp": True,
         "utilities": True,
     }
-    exports_sources = "patches/*"
-
-    _cmake = None
-
     short_paths = True
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "ImageMagick"
-
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-
-        self._cmake = CMake(self, generator="Ninja")
-
-        self._cmake.definitions["MAGICK_BUILD_STATIC"] = not self.options.shared
-        self._cmake.definitions["MAGICK_HDRI_ENABLE"] = self.options.hdri
-        self._cmake.definitions["MAGICKCORE_QUANTUM_DEPTH"] = self.options.quantum_depth
-        self._cmake.definitions["BUILD_MAGICKPP"] = self.options.magickpp
-        self._cmake.definitions["BUILD_UTILITIES"] = self.options.utilities
-        self._cmake.definitions["OPENMP_SUPPORT"] = self.options.with_openmp
-        self._cmake.definitions["THREADS_SUPPORT"] = self.options.with_threads
-
-        self._cmake.definitions["BZLIB_DELEGATE"] = self.options.with_bzlib
-        self._cmake.definitions["LZMA_DELEGATE"] = self.options.with_lzma
-        self._cmake.definitions["ZLIB_DELEGATE"] = self.options.with_zlib
-        self._cmake.definitions["ZSTD_DELEGATE"] = self.options.with_zstd
-        self._cmake.definitions["LCMS_DELEGATE"] = self.options.with_lcms
-        self._cmake.definitions["FREETYPE_DELEGATE"] = self.options.with_freetype
-        self._cmake.definitions["XML_DELEGATE"] = self.options.with_xml2
-        self._cmake.definitions["TIFF_DELEGATE"] = self.options.with_tiff
-        self._cmake.definitions["HEIC_DELEGATE"] = self.options.with_heic
-        self._cmake.definitions["JBIG_DELEGATE"] = self.options.with_jbig
-        self._cmake.definitions["JPEG_DELEGATE"] = self.options.with_jpeg
-        self._cmake.definitions["LIBOPENJP2_DELEGATE"] = self.options.with_openjp2
-        self._cmake.definitions["OPENEXR_DELEGATE"] = self.options.with_openexr
-        self._cmake.definitions["PNG_DELEGATE"] = self.options.with_png
-        self._cmake.definitions["RAW_R_DELEGATE"] = self.options.with_raw
-        self._cmake.definitions["WEBP_DELEGATE"] = self.options.with_webp
-        self._cmake.definitions["JXL_DELEGATE"] = self.options.with_jxl
-
-        self._cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
-        return self._cmake
-
-    def _libname(self, library):
-        suffix = "HDRI" if self.options.hdri else ""
-        return "%s-%s.Q%s%s" % (
-            library,
-            tools.Version(self.version).major,
-            self.options.quantum_depth,
-            suffix,
-        )
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -140,29 +89,36 @@ class ImageMagicConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
+        if not self.options.magickpp:
+            self.settings.rm_safe("compiler.cppstd")
+            self.settings.rm_safe("compiler.libcxx")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def requirements(self):
+        # None of the dependencies need transitive_headers=True
         if self.options.with_zlib:
-            self.requires("zlib/1.2.12")
+            self.requires("zlib/1.3")
         if self.options.with_bzlib:
             self.requires("bzip2/1.0.8")
         if self.options.with_zstd:
-            self.requires("zstd/1.5.2")
+            self.requires("zstd/1.5.5")
         if self.options.with_lzma:
-            self.requires("xz_utils/5.2.5")
+            self.requires("xz_utils/5.4.5")
         if self.options.with_lcms:
-            self.requires("lcms/2.13.1")
+            self.requires("lcms/2.14")
         if self.options.with_openexr:
-            self.requires("openexr/2.5.7")
+            self.requires("openexr/3.2.1")
         if self.options.with_heic:
-            self.requires("libheif/1.12.0")
+            self.requires("libheif/1.16.2")
         if self.options.with_jbig:
             self.requires("jbig/20160605")
         if self.options.with_jpeg == "libjpeg":
-            self.requires("libjpeg/9d")
+            self.requires("libjpeg/9e")
         elif self.options.with_jpeg == "libjpeg-turbo":
-            self.requires("libjpeg-turbo/2.1.2")
+            self.requires("libjpeg-turbo/3.0.1")
         if self.options.with_jxl:
             self.requires("libjxl/0.6.1")
         if self.options.with_openjp2:
@@ -170,54 +126,98 @@ class ImageMagicConan(ConanFile):
         if self.options.with_pango:
             self.requires("pango/1.48.5")
         if self.options.with_png:
-            self.requires("libpng/1.6.37")
+            self.requires("libpng/1.6.40")
         if self.options.with_tiff:
-            self.requires("libtiff/4.3.0")
+            self.requires("libtiff/4.6.0")
         if self.options.with_webp:
-            self.requires("libwebp/1.2.2")
+            self.requires("libwebp/1.3.2")
         if self.options.with_xml2:
-            self.requires("libxml2/2.9.12")
+            self.requires("libxml2/2.12.2")
         if self.options.with_freetype:
-            self.requires("freetype/2.10.4")
+            self.requires("freetype/2.13.2")
         if self.options.with_raw:
-            self.requires("libraw/0.20.2")
+            self.requires("libraw/0.21.1")
         if self.options.with_djvu:
-            self.output.warn("Conan package for djvu is not available, this package will be used from system.")
+            self.output.warning("Conan package for djvu is not available, this package will be used from system.")
         if self.options.with_openmp:
-            self.output.warn("Conan package for OpenMP is not available, this package will be used from system.")
+            if self.settings.compiler in ["clang", "apple-clang"]:
+                self.requires("llvm-openmp/17.0.4")
 
     def build_requirements(self):
-        self.build_requires("cmake/3.22.0")
-        self.build_requires("ninja/1.10.2")
-        self.build_requires("pkgconf/1.7.4")
+        self.tool_requires("cmake/[>=3.19 <4]")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/2.1.0")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version]["source"], destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version]["source"], strip_root=True)
+
+        # Copy CMake build scripts from https://github.com/Cyriuz/ImageMagick
+        cmake_sources = self.source_path.joinpath("_cmake")
+        get(self, **self.conan_data["sources"][self.version]["cmake"], strip_root=True, destination=cmake_sources)
+        for path in sorted(cmake_sources.rglob("CMakeLists.txt")) + sorted(cmake_sources.rglob("*.cmake")):
+            new_path = self.source_path.joinpath(path.relative_to(cmake_sources))
+            new_path.parent.mkdir(parents=True, exist_ok=True)
+            path.rename(new_path)
+        rmdir(self, cmake_sources)
+
+    def generate(self):
+        venv = VirtualBuildEnv(self)
+        venv.generate()
+
+        tc = CMakeToolchain(self)
+        tc.variables["MAGICK_BUILD_STATIC"] = not self.options.shared
+        tc.variables["MAGICK_HDRI_ENABLE"] = self.options.hdri
+        tc.variables["MAGICKCORE_QUANTUM_DEPTH"] = self.options.quantum_depth
+        tc.variables["BUILD_MAGICKPP"] = self.options.magickpp
+        tc.variables["BUILD_UTILITIES"] = self.options.utilities
+        tc.variables["OPENMP_SUPPORT"] = self.options.with_openmp
+        tc.variables["THREADS_SUPPORT"] = self.options.with_threads
+
+        tc.variables["BZLIB_DELEGATE"] = self.options.with_bzlib
+        tc.variables["LZMA_DELEGATE"] = self.options.with_lzma
+        tc.variables["ZLIB_DELEGATE"] = self.options.with_zlib
+        tc.variables["ZSTD_DELEGATE"] = self.options.with_zstd
+        tc.variables["LCMS_DELEGATE"] = self.options.with_lcms
+        tc.variables["FREETYPE_DELEGATE"] = self.options.with_freetype
+        tc.variables["XML_DELEGATE"] = self.options.with_xml2
+        tc.variables["TIFF_DELEGATE"] = self.options.with_tiff
+        tc.variables["HEIC_DELEGATE"] = self.options.with_heic
+        tc.variables["JBIG_DELEGATE"] = self.options.with_jbig
+        tc.variables["JPEG_DELEGATE"] = self.options.with_jpeg
+        tc.variables["LIBOPENJP2_DELEGATE"] = self.options.with_openjp2
+        tc.variables["OPENEXR_DELEGATE"] = self.options.with_openexr
+        tc.variables["PNG_DELEGATE"] = self.options.with_png
+        tc.variables["RAW_R_DELEGATE"] = self.options.with_raw
+        tc.variables["WEBP_DELEGATE"] = self.options.with_webp
+        tc.variables["JXL_DELEGATE"] = self.options.with_jxl
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        deps = PkgConfigDeps(self)
+        deps.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, {}):
-            tools.patch(**patch)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
-                "magick_check_env()",
-                "include(${CMAKE_BINARY_DIR}/../conanbuildinfo.cmake)\n"
-                "conan_basic_setup(KEEP_RPATHS)\n"
-                "magick_check_env()")
-
-        with tools.vcvars(self.settings) if self.settings.compiler == "Visual Studio" else tools.no_op():
-            cmake = self._configure_cmake()
-            with tools.run_environment(self):
-                cmake.build()
+    def _libname(self, library):
+        suffix = "HDRI" if self.options.hdri else ""
+        return f"{library}-{Version(self, self.version).major}.Q{self.options.quantum_depth}{suffix}"
 
     def package(self):
-        cmake = self._configure_cmake()
-        cmake.install(build_dir=self._build_subfolder)
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
-        # FIXME model official FindImageMagick https://cmake.org/cmake/help/latest/module/FindImageMagick.html
-        bin_path = os.path.join(self.package_folder, "bin")
-        self.output.info("Appending PATH environment variable: {}".format(bin_path))
-        self.env_info.PATH.append(bin_path)
+        # FIXME: fully model the official FindImageMagick https://cmake.org/cmake/help/latest/module/FindImageMagick.html
+
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "ImageMagick")
+        self.cpp_info.set_property("cmake_target_name", "ImageMagick::ImageMagick")
 
         core_requires = []
         if self.options.with_zlib:
@@ -236,8 +236,10 @@ class ImageMagicConan(ConanFile):
             core_requires.append("libheif::libheif")
         if self.options.with_jbig:
             core_requires.append("jbig::jbig")
-        if self.options.with_jpeg:
-            core_requires.append("{0}::{0}".format(self.options.with_jpeg))
+        if self.options.with_jpeg == "libjpeg":
+            core_requires.append("libjpeg::libjpeg")
+        elif self.options.with_jpeg == "libjpeg-turbo":
+            core_requires.append("libjpeg-turbo::jpeg")
         if self.options.with_jxl:
             core_requires.append("libjxl::libjxl")
         if self.options.with_openjp2:
@@ -257,73 +259,59 @@ class ImageMagicConan(ConanFile):
         if self.options.with_raw:
             core_requires.append("libraw::libraw")
 
-        if self.settings.os == "Linux":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["MagickCore"].system_libs.append("pthread")
 
-        self.cpp_info.components["MagickCore"].defines.append(
-            "MAGICKCORE_QUANTUM_DEPTH=%s" % self.options.quantum_depth
-        )
-        self.cpp_info.components["MagickCore"].defines.append(
-            "MAGICKCORE_HDRI_ENABLE=%s" % int(bool(self.options.hdri))
-        )
-        self.cpp_info.components["MagickCore"].defines.append(
-            "_MAGICKDLL_=1" if self.options.shared else "_MAGICKLIB_=1"
-        )
+        self.cpp_info.components["MagickCore"].defines.append(f"MAGICKCORE_QUANTUM_DEPTH={self.options.quantum_depth}")
+        self.cpp_info.components["MagickCore"].defines.append(f"MAGICKCORE_HDRI_ENABLE={int(bool(self.options.hdri))}")
+        self.cpp_info.components["MagickCore"].defines.append("_MAGICKDLL_=1" if self.options.shared else "_MAGICKLIB_=1")
 
-        imagemagick_include_dir = (
-            "include/ImageMagick-%s" % tools.Version(self.version).major
-        )
-
-        self.cpp_info.components["MagickCore"].includedirs = [imagemagick_include_dir]
+        include_dir_root = os.path.join("include", f"ImageMagick-{Version(self.version).major}")
+        self.cpp_info.components["MagickCore"].includedirs = [include_dir_root]
         self.cpp_info.components["MagickCore"].libs.append(self._libname("MagickCore"))
         self.cpp_info.components["MagickCore"].requires = core_requires
-        self.cpp_info.components["MagickCore"].names["pkg_config"] = ["MagicCore"]
+        self.cpp_info.components["MagickCore"].set_property("pkg_config_name", "MagicCore")
 
         self.cpp_info.components[self._libname("MagickCore")].requires = ["MagickCore"]
-        self.cpp_info.components[self._libname("MagickCore")].names["pkg_config"] = [
-            self._libname("MagickCore")
-        ]
+        self.cpp_info.components[self._libname("MagickCore")].set_property("pkg_config_name", self._libname("MagickCore"))
 
         if not self.options.shared and self.options.with_openmp:
             openmp_flags = []
-            if self.settings.compiler in ("Visual Studio", "msvc"):
+            if is_msvc(self):
                 openmp_flags = ["-openmp"]
-            elif self.settings.compiler in ("gcc", "clang"):
+            elif self.settings.compiler == "gcc":
                 openmp_flags = ["-fopenmp"]
-            elif self.settings.compiler == "apple-clang":
+            elif self.settings.compiler in ["clang", "apple-clang"]:
                 openmp_flags = ["-Xpreprocessor", "-fopenmp"]
-
+            # TODO: check if OpenMP pargmas are used in exported headers
+            # self.cpp_info.components["MagickCore"].cflags.extend(openmp_flags)
+            # self.cpp_info.components["MagickCore"].cxxflags.extend(openmp_flags)
             self.cpp_info.components["MagickCore"].sharedlinkflags = openmp_flags
             self.cpp_info.components["MagickCore"].exelinkflags = openmp_flags
 
-        self.cpp_info.components["MagickWand"].includedirs = [
-            imagemagick_include_dir + "/MagickWand"
-        ]
+        self.cpp_info.components["MagickWand"].includedirs = [os.path.join(include_dir_root, "MagickWand")]
         self.cpp_info.components["MagickWand"].libs = [self._libname("MagickWand")]
         self.cpp_info.components["MagickWand"].requires = ["MagickCore"]
-        self.cpp_info.components["MagickWand"].names["pkg_config"] = ["MagickWand"]
+        self.cpp_info.components["MagickWand"].set_property("pkg_config_name", "MagickWand")
 
         self.cpp_info.components[self._libname("MagickWand")].requires = ["MagickWand"]
-        self.cpp_info.components[self._libname("MagickWand")].names[
-            "pkg_config"
-        ] = self._libname("MagickWand")
+        self.cpp_info.components[self._libname("MagickWand")].set_property("pkg_config_name", "MagickWand")
 
         if self.options.magickpp:
-            self.cpp_info.components["Magick++"].includedirs = [
-                imagemagick_include_dir + "/Magick++"
-            ]
+            self.cpp_info.components["Magick++"].includedirs = [os.path.join(include_dir_root, "Magick++")]
             self.cpp_info.components["Magick++"].libs = [self._libname("Magick++")]
             self.cpp_info.components["Magick++"].requires = ["MagickWand"]
-            self.cpp_info.components["Magick++"].names["pkg_config"] = [
-                "Magick++",
-                self._libname("Magick++"),
-            ]
+            self.cpp_info.components["Magick++"].set_property("pkg_config_name", ["Magick++", self._libname("Magick++")])
 
             self.cpp_info.components[self._libname("Magick++")].requires = ["Magick++"]
-            self.cpp_info.components[self._libname("Magick++")].names[
-                "pkg_config"
-            ] = self._libname("Magick++")
+            self.cpp_info.components[self._libname("Magick++")].set_property("pkg_config_name", self._libname("Magick++"))
 
             if not self.options.shared:
                 self.cpp_info.components["MagickCore"].defines.append("STATIC_MAGICK=1")
                 self.cpp_info.components["MagickCore"].defines.append("NOAUTOLINK_MAGICK=1")
+
+        # TODO: Legacy, to be removed on Conan 2.0
+        self.cpp_info.names["cmake_find_package"] = "ImageMagick"
+        self.cpp_info.names["cmake_find_package_multi"] = "ImageMagick"
+        bin_path = os.path.join(self.package_folder, "bin")
+        self.env_info.PATH.append(bin_path)
--- recipes/imagemagick/7.1/test_package/conanfile.py
+++ recipes/imagemagick/7.1/test_package/conanfile.py
@@ -1,10 +1,25 @@
-from conans import ConanFile, CMake, tools
+import json
 import os
+
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        dependency_opts = {k: str(v) == "True" for k, v in self.dependencies["imagemagick"].options.items() if k.startswith("with_")}
+        self.generators_path.joinpath("dependencies.json").write_text(json.dumps(dependency_opts, indent=2, sort_keys=True))
 
     def build(self):
         cmake = CMake(self)
@@ -12,31 +27,34 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
-            with open('delegates.txt') as f:
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
+
+            dependency_opts = json.loads(self.generators_path.joinpath("dependencies.json").read_text())
+
+            with open("delegates.txt") as f:
                 content = f.read()
 
-                def check(option, token):
-                    self.output.info('checking feature %s...' % token)
-                    if option:
-                        if token not in content.split():
-                            raise Exception("feature %s wasn't enabled!" % token)
-                    self.output.info('checking feature %s... OK!' % token)
+            def check(option, token):
+                self.output.info(f"checking feature {token}...")
+                if dependency_opts[option]:
+                    if token not in content.split():
+                        raise Exception(f"feature {token} wasn't enabled!")
+                self.output.info(f"checking feature {token}... OK!")
 
-                check(self.options['imagemagick'].with_zlib, 'zlib')
-                check(self.options['imagemagick'].with_bzlib, 'bzlib')
-                check(self.options['imagemagick'].with_lzma, 'lzma')
-                check(self.options['imagemagick'].with_lcms, 'lcms')
-                check(self.options['imagemagick'].with_openexr, 'openexr')
-                check(self.options['imagemagick'].with_heic, 'heic')
-                check(self.options['imagemagick'].with_jbig, 'jbig')
-                check(self.options['imagemagick'].with_jpeg, 'jpeg')
-                check(self.options['imagemagick'].with_openjp2, 'jp2')
-                check(self.options['imagemagick'].with_pango, 'pangocairo')
-                check(self.options['imagemagick'].with_png, 'png')
-                check(self.options['imagemagick'].with_tiff, 'tiff')
-                check(self.options['imagemagick'].with_webp, 'webp')
-                check(self.options['imagemagick'].with_freetype, 'freetype')
-                check(self.options['imagemagick'].with_xml2, 'xml')
+            check("with_zlib", "zlib")
+            check("with_bzlib", "bzlib")
+            check("with_lzma", "lzma")
+            check("with_lcms", "lcms")
+            check("with_openexr", "openexr")
+            check("with_heic", "heic")
+            check("with_jbig", "jbig")
+            check("with_jpeg", "jpeg")
+            check("with_openjp2", "jp2")
+            check("with_pango", "pangocairo")
+            check("with_png", "png")
+            check("with_tiff", "tiff")
+            check("with_webp", "webp")
+            check("with_freetype", "freetype")
+            check("with_xml2", "xml")
