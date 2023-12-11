ea2d016bc7347be389ec38e6364d1d835fbeccc7
Martin Valgur <martin.valgur@gmail.com>
2023-12-11 17:54:36 +0200

    openldap: bump cyrus-sasl

--- recipes/openldap/all/conanfile.py
+++ recipes/openldap/all/conanfile.py
@@ -44,7 +44,7 @@ class OpenldapConan(ConanFile):
     def requirements(self):
         self.requires("openssl/[>=1.1 <4]")
         if self.options.with_cyrus_sasl:
-            self.requires("cyrus-sasl/2.1.27")
+            self.requires("cyrus-sasl/2.1.28")
 
     def validate(self):
         if self.settings.os not in ["Linux", "FreeBSD"]:
