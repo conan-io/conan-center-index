cdf07ccae09849af93bbc6ce415b98f59acdc264
Rubén Rincón Blanco <git@rinconblanco.es>
2023-12-12 12:14:55 +0100

    Apply suggestions from code review
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>

--- recipes/sqlite3mc/all/conanfile.py
+++ recipes/sqlite3mc/all/conanfile.py
@@ -133,7 +133,7 @@ class sqlite3mc(ConanFile):
         if self.options.require_zlib:
             self.requires("zlib/[>=1.2.11 <2]")
         if self.options.with_icu:
-            self.requires("icu/[74.1]")
+            self.requires("icu/74.1")
 
     def generate(self):
         deps = CMakeDeps(self)
