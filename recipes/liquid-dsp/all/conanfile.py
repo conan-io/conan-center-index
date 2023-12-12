bec663783e722d5c2cde755b852971db8bc57084
Martin Valgur <martin.valgur@gmail.com>
2023-12-13 01:56:07 +0200

    liquid-dsp: fix MSVC shared build, disable MSVC static

--- recipes/liquid-dsp/all/conanfile.py
+++ recipes/liquid-dsp/all/conanfile.py
@@ -1,14 +1,15 @@
 import os
+from io import StringIO
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
 from conan.tools.build import cross_building
-from conan.tools.env import Environment
+from conan.tools.env import Environment, VirtualBuildEnv, VirtualRunEnv
 from conan.tools.files import apply_conandata_patches, chdir, copy, export_conandata_patches, get, rename
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import is_msvc, unix_path
+from conan.tools.microsoft import is_msvc
 
 required_conan_version = ">=1.53.0"
 
@@ -38,9 +39,32 @@ class LiquidDspConan(ConanFile):
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
+    @property
+    def _libname(self):
+        if self.settings.os == "Windows":
+            return "libliquid"
+        return "liquid"
+
+    @property
+    def _target_name(self):
+        if is_apple_os(self):
+            if not self.options.shared:
+                return "libliquid.ar"
+            return "libliquid.dylib"
+        if not self.options.shared:
+            return "libliquid.a"
+        return "libliquid.so"
+
+    @property
+    def _lib_pattern(self):
+        if is_apple_os(self) and not self.options.shared:
+            return "libliquid.a"
+        if self.settings.os != "Windows":
+            return self._target_name
+        return "libliquid.lib"
+
     def export_sources(self):
         export_conandata_patches(self)
-        copy(self, "generate_link_library.bat", src=self.recipe_folder, dst=self.export_sources_folder)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -58,6 +82,9 @@ class LiquidDspConan(ConanFile):
     def validate(self):
         if hasattr(self, "settings_build") and cross_building(self):
             raise ConanInvalidConfiguration("Cross building is not yet supported. Contributions are welcome")
+        if is_msvc(self) and not self.options.shared:
+            # FIXME: export the correct libgcc library files for a static build
+            raise ConanInvalidConfiguration("Static builds are not supported on MSVC. Contributions are welcome")
 
     def build_requirements(self):
         if self._settings_build.os == "Windows":
@@ -65,6 +92,7 @@ class LiquidDspConan(ConanFile):
             if not self.conf.get("tools.microsoft.bash:path", check_type=str):
                 self.tool_requires("msys2/cci.latest")
         if is_msvc(self):
+            # GCC from MinGW is required due to MSVC C99 not supporting complex float
             self.tool_requires("mingw-builds/12.2.0")
             self.tool_requires("automake/1.16.5")
 
@@ -72,53 +100,51 @@ class LiquidDspConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
+        venv = VirtualBuildEnv(self)
+        venv.generate()
+
+        if not cross_building(self):
+            venv = VirtualRunEnv(self)
+            venv.generate(scope="build")
+
+        gcc_env = Environment()
+        if is_msvc(self):
+            gcc_env.define("CC", "gcc")
+            gcc_env.define("CXX", "g++")
+            gcc_env.define("LD", "ld")
+            gcc_env.define("AR", "ar")
+        if self.settings.build_type == "Debug":
+            cflags = "-g -O0"
+        else:
+            cflags = "-s -O2 -DNDEBUG"
+        gcc_env.append("CFLAGS", cflags)
+        gcc_env.vars(self, scope="gcc").save_script("conan_gcc_env")
+
         tc = AutotoolsToolchain(self)
-        tc.extra_cflags += ["-static-libgcc"]
         if self.settings.build_type == "Debug":
             tc.configure_args.append("--enable-debug-messages")
-            tc.extra_cflags += ["-g", "-O0"]
-        else:
-            tc.extra_cflags += ["-s", "-O2", "-DNDEBUG"]
         if self.options.simdoverride:
             tc.configure_args.append("--enable-simdoverride")
-        tc.generate()
-
-        if is_msvc(self):
-            env = Environment()
-            automake_conf = self.dependencies.build["automake"].conf_info
-            compile_wrapper = unix_path(self, automake_conf.get("user.automake:compile-wrapper", check_type=str))
-            ar_wrapper = unix_path(self, automake_conf.get("user.automake:lib-wrapper", check_type=str))
-            env.define("CC", f"{compile_wrapper} cl -nologo")
-            env.define("CXX", f"{compile_wrapper} cl -nologo")
-            env.define("LD", "link -nologo")
-            env.define("AR", f'{ar_wrapper} "lib -nologo"')
-            env.define("NM", "dumpbin -symbols")
-            env.define("OBJDUMP", ":")
-            env.define("RANLIB", ":")
-            env.define("STRIP", ":")
-            env.vars(self).save_script("conanbuild_msvc")
+        tc.generate(gcc_env)
 
     def _patch_sources(self):
         if self.settings.os == "Windows":
             apply_conandata_patches(self)
 
-    @property
-    def _target_name(self):
-        if is_apple_os(self):
-            if not self.options.shared:
-                return "libliquid.ar"
-            return "libliquid.dylib"
-        if not self.options.shared:
-            return "libliquid.a"
-        return "libliquid.so"
-
     def _gen_link_library(self):
         if is_msvc(self) and self.options.shared:
-            self.run("cmd /c generate_link_library.bat")
             with chdir(self, self.source_folder):
-                self.run("{} /def:libliquid.def /out:libliquid.lib /machine:{}".format(
-                    os.getenv("AR"), "X86" if self.settings.arch == "x86" else "X64")
-                )
+                stdout = StringIO()
+                self.run("dumpbin -EXPORTS libliquid.dll", stdout)
+                lines = stdout.getvalue().splitlines()
+                with open("libliquid.def", "w") as f:
+                    f.write("EXPORTS\n")
+                    for line in lines[19:]:
+                        tokens = line.split()
+                        if len(tokens) > 3:
+                            f.write(tokens[3] + "\n")
+                arch = "X86" if self.settings.arch == "x86" else "X64"
+                self.run(f"lib /def:libliquid.def /out:libliquid.lib /machine:{arch}")
 
     def _rename_libraries(self):
         with chdir(self, self.source_folder):
@@ -132,20 +158,12 @@ class LiquidDspConan(ConanFile):
     def build(self):
         self._patch_sources()
         with chdir(self, self.source_folder):
-            self.run("./bootstrap.sh")
+            self.run("./bootstrap.sh", env=["conanbuild", "gcc"])
             autotools = Autotools(self)
             autotools.configure()
             autotools.make(self._target_name)
         self._rename_libraries()
         self._gen_link_library()
-
-    @property
-    def _lib_pattern(self):
-        if is_apple_os(self) and not self.options.shared:
-            return "libliquid.a"
-        if self.settings.os != "Windows":
-            return self._target_name
-        return "libliquid.lib"
 
     def package(self):
         copy(self, "LICENSE",
@@ -160,12 +178,11 @@ class LiquidDspConan(ConanFile):
         copy(self, self._lib_pattern,
              dst=os.path.join(self.package_folder, "lib"),
              src=self.source_folder)
-
-    @property
-    def _libname(self):
-        if self.settings.os == "Windows":
-            return "libliquid"
-        return "liquid"
+        if is_msvc(self):
+            copy(self, "libgcc_*.dll",
+                 os.path.join(self.dependencies.build["mingw-builds"].package_folder, "bin"),
+                 os.path.join(self.package_folder, "bin"),
+                 keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = [self._libname]
