beff34aef2c26b904c61b5ccf0427c6f747937d9
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-12-14 14:35:00 +0100

    Update conanfile.py

--- recipes/cpp-httplib/all/conanfile.py
+++ recipes/cpp-httplib/all/conanfile.py
@@ -1,9 +1,7 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.50.0"
