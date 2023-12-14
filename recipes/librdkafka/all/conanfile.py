622b48ef320c9dfb0d1acfcd0fab0758c2f9efe7
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-12-14 14:36:23 +0100

    Update conanfile.py

--- recipes/librdkafka/all/conanfile.py
+++ recipes/librdkafka/all/conanfile.py
@@ -4,7 +4,6 @@ from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, replace_in_file
 from conan.tools.microsoft import is_msvc
 from conan.tools.gnu import PkgConfigDeps
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.55.0"
