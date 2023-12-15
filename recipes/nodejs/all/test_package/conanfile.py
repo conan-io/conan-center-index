d3529b58571d16fc8a470334e81ddeb12bf7e067
Martin Valgur <martin.valgur@gmail.com>
2023-12-15 10:17:30 +0200

    nodejs: skip test on old libc versions (i.e. CI)

--- recipes/nodejs/all/test_package/conanfile.py
+++ recipes/nodejs/all/test_package/conanfile.py
@@ -1,6 +1,9 @@
+from io import StringIO
+
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout
+from conan.tools.scm import Version
 
 
 class TestPackageConan(ConanFile):
@@ -14,6 +17,16 @@ class TestPackageConan(ConanFile):
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
 
+    def _get_libc_version(self):
+        stdout = StringIO()
+        self.run("ldd --version", stdout)
+        return Version(stdout.getvalue().splitlines()[0].split()[-1])
+
     def test(self):
         if can_run(self):
+            if self.settings.os in ["Linux", "FreeBSD"]:
+                libc_version = self._get_libc_version()
+                if libc_version < "2.29":
+                    self.output.warn(f"System libc version {libc_version} < 2.29, skipping test_package")
+                    return
             self.run("node --version")
