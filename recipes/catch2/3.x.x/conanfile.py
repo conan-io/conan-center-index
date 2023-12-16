8a2d61305bc7dc6726e97aaed1199a00f4361f13
Antony Peacock <ant.peacock@gmail.com>
2023-12-16 18:12:09 +0000

    Clean up

--- recipes/catch2/3.x.x/conanfile.py
+++ recipes/catch2/3.x.x/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd, supported_cppstd
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, save
 from conan.tools.scm import Version
@@ -86,7 +86,7 @@ class Catch2Conan(ConanFile):
 
     def layout(self):
         cmake_layout(self, src_folder="src")
-    
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
