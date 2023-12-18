7886a29c8f9a47913fc22ec86774482bfc82e86d
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-12-18 01:31:15 +0100

    Update conanfile.py

--- recipes/hazelcast-cpp-client/all/conanfile.py
+++ recipes/hazelcast-cpp-client/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 
 import os
