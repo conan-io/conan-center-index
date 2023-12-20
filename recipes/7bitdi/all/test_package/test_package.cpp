27b9618c1355f697e6d8618940a2013d10cb06ad
7bitcoder <Sylwo1003@gmail.com>
2023-12-20 20:22:55 +0100

    add 7bitdi 2.0.0 version

--- recipes/7bitdi/all/test_package/test_package.cpp
+++ recipes/7bitdi/all/test_package/test_package.cpp
@@ -1,7 +1,24 @@
-#include <SevenBit/DI/LibraryConfig.hpp>
 #include <iostream>
+#include <SevenBit/DI.hpp>
+
+struct LibInfo {
+    std::string getVersion() const { return _7BIT_DI_VERSION; }
+
+    std::string getInfo() const { return "7bitdi version: " + getVersion(); }
+};
+
+template<class TProvider>
+auto& extractProvider(TProvider& provider) {
+    if constexpr (_7BIT_DI_VERSION_AS_NUMBER > 10000) {
+        return provider;
+    } else {
+        return *provider;
+    }
+}
 
 int main() {
-  std::cout << "7bitdi version: " << _7BIT_DI_VERSION;
-  return 0;
+    auto provider = sb::di::ServiceCollection{}.addSingleton<LibInfo>().buildServiceProvider();
+
+    std::cout << extractProvider(provider).getService<LibInfo>().getInfo() << std::endl;
+    return 0;
 }
