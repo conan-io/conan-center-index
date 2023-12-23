4c2c5167059e903fd9f080903668a837dcffcfeb
Martin Valgur <martin.valgur@gmail.com>
2023-12-23 15:15:11 +0200

    coz: static build is not supported, mark as shared-library

--- recipes/coz/all/conanfile.py
+++ recipes/coz/all/conanfile.py
@@ -20,24 +20,8 @@ class CozConan(ConanFile):
     homepage = "http://coz-profiler.org"
     topics = ("profiler", "causal")
 
-    package_type = "library"
+    package_type = "shared-library"
     settings = "os", "arch", "compiler", "build_type"
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
-    }
-    default_options = {
-        "shared": False,
-        "fPIC": True,
-    }
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
