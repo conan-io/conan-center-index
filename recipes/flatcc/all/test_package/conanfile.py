52a97438a2ed1270c578241c5ec65ad35886dddf
Martin Valgur <martin.valgur@gmail.com>
2023-12-25 19:33:30 +0200

    flatcc: skip macOS shared test on Conan v1

--- recipes/flatcc/all/test_package/conanfile.py
+++ recipes/flatcc/all/test_package/conanfile.py
@@ -3,14 +3,13 @@ import os
 from conan import ConanFile, conan_version
 from conan.tools.apple import is_apple_os
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+from conan.tools.cmake import cmake_layout, CMake
 from conan.tools.env import VirtualRunEnv
-from conan.tools.files import mkdir
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps"
+    generators = "CMakeDeps", "CMakeToolchain"
     test_type = "explicit"
 
     def requirements(self):
@@ -23,31 +22,24 @@ class TestPackageConan(ConanFile):
         cmake_layout(self)
 
     @property
-    def _macos_workaround(self):
+    def _skip_shared_macos(self):
         return conan_version.major == 1 and self.options["flatcc"].shared and is_apple_os(self)
 
     def generate(self):
         VirtualRunEnv(self).generate(scope="build")
         VirtualRunEnv(self).generate(scope="run")
-        tc = CMakeToolchain(self)
-        tc.variables["MACOS_SIP_WORKAROUND"] = self._macos_workaround
-        tc.generate()
 
 
     def build(self):
+        if self._skip_shared_macos:
+            return
         cmake = CMake(self)
-        if  self._macos_workaround:
-            # Because of MacOS System Integrity Protection it is currently not possible to run the flatcc
-            # executable from cmake if it is linked shared. As a temporary work-around run flatcc here in
-            # the build function.
-            gen_dir = os.path.join(self.build_folder, "generated")
-            mkdir(self, gen_dir)
-            fbs_file = os.path.join(self.source_folder, os.pardir, "test_package", "monster.fbs")
-            self.run(f"flatcc -a -o {gen_dir} {fbs_file}")
         cmake.configure()
         cmake.build()
 
     def test(self):
+        if self._skip_shared_macos:
+            return
         if can_run(self):
             self.run("flatcc --version")
             bin_path = os.path.join(self.cpp.build.bindir, "monster")
--- recipes/flatcc/all/test_v1_package/conanfile.py
+++ recipes/flatcc/all/test_v1_package/conanfile.py
@@ -8,25 +8,29 @@ class FlatccTestConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
+    @property
+    def _skip_shared_macos(self):
+        # Because of MacOS System Integraty Protection it is currently not possible to run the flatcc
+        # executable from cmake if it is linked shared. As a temporary work-around run flatcc here in
+        # the build function.
+        return self.options["flatcc"].shared and tools.os_info.is_macos
+
     def build(self):
         if tools.cross_building(self):
+            return
+
+        if self._skip_shared_macos:
             return
 
         env_build = RunEnvironment(self)
         with tools.environment_append(env_build.vars):
             cmake = CMake(self)
-            if tools.os_info.is_macos and self.options["flatcc"].shared:
-                # Because of MacOS System Integrity Protection it is currently not possible to run the flatcc
-                # executable from cmake if it is linked shared. As a temporary work-around run flatcc here in
-                # the build function.
-                gen_dir = os.path.join(self.build_folder, "generated")
-                tools.mkdir(gen_dir)
-                fbs_file = os.path.join(self.source_folder, "monster.fbs")
-                self.run(f"flatcc -a -o {gen_dir} {fbs_file}", run_environment=True)
             cmake.configure()
             cmake.build()
 
     def test(self):
+        if self._skip_shared_macos:
+            return
         if tools.cross_building(self):
             bin_path = os.path.join(self.deps_cpp_info["flatcc"].rootpath, "bin", "flatcc")
             if not os.path.isfile(bin_path) or not os.access(bin_path, os.X_OK):
