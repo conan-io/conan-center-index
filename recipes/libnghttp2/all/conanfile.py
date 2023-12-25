4e885359ebe91ccba575eff91f0c9ed4aa00c1ef
mayeut <mayeut@users.noreply.github.com>
2023-12-25 18:53:56 +0100

    libnghttp2: bump deps

--- recipes/libnghttp2/all/conanfile.py
+++ recipes/libnghttp2/all/conanfile.py
@@ -60,10 +60,10 @@ class Nghttp2Conan(ConanFile):
         if self.options.with_app or self.options.get_safe("with_asio"):
             self.requires("openssl/[>=1.1 <4]")
         if self.options.with_app:
-            self.requires("c-ares/1.20.1")
+            self.requires("c-ares/1.22.0")
             self.requires("libev/4.33")
             self.requires("libevent/2.1.12")
-            self.requires("libxml2/2.11.5")
+            self.requires("libxml2/2.12.2")
             self.requires("zlib/[>=1.2.11 <2]")
             if self.options.with_jemalloc:
                 self.requires("jemalloc/5.3.0")
