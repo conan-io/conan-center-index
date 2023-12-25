865bfcd2cda72b4aabf8e5b331aaf5b74b36f61e
Martin Valgur <martin.valgur@gmail.com>
2023-12-25 20:21:49 +0200

    sdl_mixer: fix license install

--- recipes/sdl_mixer/cmake/conanfile.py
+++ recipes/sdl_mixer/cmake/conanfile.py
@@ -178,7 +178,7 @@ class SDLMixerConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "COPYING.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "LICENSE.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
