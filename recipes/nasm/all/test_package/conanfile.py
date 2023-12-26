aade2524b7bdce5c01b70b821d1bf9bac3daa026
Martin Valgur <martin.valgur@gmail.com>
2023-12-26 02:16:15 +0200

    nasm: add v2.16.01

--- recipes/nasm/all/test_package/conanfile.py
+++ recipes/nasm/all/test_package/conanfile.py
@@ -1,6 +1,8 @@
+import os
+
 from conan import ConanFile
 from conan.tools.build import can_run
-import os
+from conan.tools.cmake import cmake_layout
 
 
 class TestPackageConan(ConanFile):
@@ -10,6 +12,9 @@ class TestPackageConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
 
     def test(self):
         self.run("nasm --version")
