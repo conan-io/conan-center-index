1850aecbfde995b2f21d59a977109c6c085aec36
Martin Valgur <martin.valgur@gmail.com>
2023-12-26 20:36:11 +0200

    libsigcpp: add v3.6.0, v2.12.1

--- recipes/libsigcpp/3.x.x/conanfile.py
+++ recipes/libsigcpp/3.x.x/conanfile.py
@@ -2,10 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import (
-    apply_conandata_patches, collect_libs, copy, export_conandata_patches, get,
-    rename, replace_in_file, rmdir, save
-)
+from conan.tools.files import collect_libs, copy, get, rename, replace_in_file, rmdir, save
 import glob
 import os
 import textwrap
@@ -45,9 +42,6 @@ class LibSigCppConan(ConanFile):
             "apple-clang": "10",
         }
 
-    def export_sources(self):
-        export_conandata_patches(self)
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -64,10 +58,7 @@ class LibSigCppConan(ConanFile):
             check_min_cppstd(self, self._min_cppstd)
 
         def loose_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
+            return all(int(p1) < int(p2) for p1, p2 in zip(str(v1).split("."), str(v2).split(".")))
 
         minimum_version = self._minimum_compilers_version.get(str(self.settings.compiler), False)
         if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
@@ -76,18 +67,27 @@ class LibSigCppConan(ConanFile):
             )
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.generate()
 
     def _patch_sources(self):
-        apply_conandata_patches(self)
         if not self.options.shared:
             replace_in_file(self, os.path.join(self.source_folder, "sigc++config.h.cmake"),
                                   "define SIGC_DLL 1", "undef SIGC_DLL")
+        # Disable subdirs
+        save(self, os.path.join(self.source_folder, "examples", "CMakeLists.txt"), "")
+        save(self, os.path.join(self.source_folder, "tests", "CMakeLists.txt"), "")
+        # Enable static builds
+        cmakelists = os.path.join(self.source_folder, "sigc++", "CMakeLists.txt")
+        replace_in_file(self, cmakelists, " SHARED ", " ")
+        # Fix install paths
+        replace_in_file(self, cmakelists,
+                        'LIBRARY DESTINATION "lib"',
+                        "LIBRARY DESTINATION lib ARCHIVE DESTINATION lib RUNTIME DESTINATION bin")
+
 
     def build(self):
         self._patch_sources()
