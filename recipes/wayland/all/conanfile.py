2a35197a505726c81b6b59dfad0c54c5402f7593
Matthieu Darbois <mayeut@users.noreply.github.com>
2023-12-27 23:08:45 +0100

    (#21977) wayland: bump deps

--- recipes/wayland/all/conanfile.py
+++ recipes/wayland/all/conanfile.py
@@ -50,7 +50,7 @@ class WaylandConan(ConanFile):
         if self.options.enable_libraries:
             self.requires("libffi/3.4.4")
         if self.options.enable_dtd_validation:
-            self.requires("libxml2/2.12.2")
+            self.requires("libxml2/2.12.3")
         self.requires("expat/2.5.0")
 
     def validate(self):
