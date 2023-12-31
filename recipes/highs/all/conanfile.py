0bcb41ae5c87f34cc6393d4cdd2240164aeb778f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2023-12-31 14:12:05 +0100

    rely on CMake for installation

--- recipes/highs/all/conanfile.py
+++ recipes/highs/all/conanfile.py
@@ -1,12 +1,12 @@
+import os
+
 from conan import ConanFile
-from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, collect_libs, copy, export_conandata_patches, get
+from conan.tools.files import copy, get, rmdir
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
-from os.path import join
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=1.54.0"
 
 
 class HiGHSConan(ConanFile):
@@ -16,6 +16,7 @@ class HiGHSConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.highs.dev/"
     topics = ("simplex", "interior point", "solver", "linear", "programming")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -26,12 +27,6 @@ class HiGHSConan(ConanFile):
         "fPIC": True,
     }
 
-    def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
-
-    def export_sources(self):
-        export_conandata_patches(self)
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -40,11 +35,14 @@ class HiGHSConan(ConanFile):
         if self.options.shared:
             self.options.rm_safe("fPIC")
 
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
     def requirements(self):
         self.requires("zlib/[>=1.2.11 <2]")
 
-    def layout(self):
-        cmake_layout(self, src_folder="src")
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -53,33 +51,22 @@ class HiGHSConan(ConanFile):
         tc.variables["PYTHON"] = False
         tc.variables["FORTRAN"] = False
         tc.variables["CSHARP"] = False
+        tc.variables["BUILD_EXAMPLES"] = False
         tc.generate()
-        tc = CMakeDeps(self)
-        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
-        cmake.build(target="highs")
+        cmake.build()
 
     def package(self):
-        copy(self, pattern="LICENSE", src=self.source_folder, dst=join(self.package_folder, "licenses"))
-        copy(self, pattern="*.h", src=join(self.source_folder, "src"), dst=join(self.package_folder, "include"))
-        copy(self, pattern="HConfig.h", src=self.build_folder, dst=join(self.package_folder, "include"))
-        if self.options.shared:
-            copy(self, pattern="*.so*", src=join(self.build_folder, "lib"), dst=join(self.package_folder, "lib"))
-            copy(self, pattern="*.dylib*", src=join(self.build_folder, "lib"), dst=join(self.package_folder, "lib"))
-            copy(self, pattern="*.lib", src=self.build_folder, dst=join(self.package_folder, "lib"), keep_path=False)
-            copy(self, pattern="*.dll", src=self.build_folder, dst=join(self.package_folder, "bin"), keep_path=False)
-        else:
-            copy(self, pattern="*.a", src=join(self.build_folder, "lib"), dst=join(self.package_folder, "lib"))
-            if Version(self.version) >= Version("1.5.3"):
-                # https://github.com/ERGO-Code/HiGHS/commit/2c24b4cb6ecece98ed807dbeff9b27a2fbba8d37
-                copy(self, pattern="*.lib", src=self.build_folder, dst=join(self.package_folder, "lib"), keep_path=False)
-            else:
-                copy(self, pattern="*.lib", src=join(self.build_folder, "lib"), dst=join(self.package_folder, "lib"), keep_path=False)
-        fix_apple_shared_install_name(self)
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.libs = collect_libs(self)
