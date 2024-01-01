234cdd09e23d96044c9be04a47683d222b84dc03
Martin Valgur <martin.valgur@gmail.com>
2024-01-01 11:25:52 +0200

    mingw-w64: fix test_v1_package

--- recipes/mingw-w64/linux/test_v1_package/conanfile.py
+++ recipes/mingw-w64/linux/test_v1_package/conanfile.py
@@ -8,8 +8,8 @@ class MinGWTestConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
     def build(self):
-        source_file = os.path.join(self.source_folder, "main.cpp")
-        self.run("x86_64-w64-mingw32-g++ {} @conanbuildinfo.gcc -lstdc++ -o main".format(source_file), run_environment=True)
+        source_file = os.path.join(self.source_folder, os.pardir, "test_package", "main.cpp")
+        self.run(f"x86_64-w64-mingw32-g++ {source_file} @conanbuildinfo.gcc -lstdc++ -o main", run_environment=True)
 
     def test(self):
         if not cross_building(self):
