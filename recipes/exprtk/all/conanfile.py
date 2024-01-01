90fb1916aa7e63d52abb5c0011b5165b99a57f7b
Arash Partow <partow@gmail.com>
2024-01-01 00:00:00 +0000

    Revert "exprtk: add version 0.0.3 (#25832)"
    This reverts commit 1ab848bed5d0ea3f857c78d08f033d6d399c44de.

--- recipes/exprtk/all/conanfile.py
+++ recipes/exprtk/all/conanfile.py
@@ -2,7 +2,6 @@ from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.layout import basic_layout
 from conan.tools.files import get, copy, load, save
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.53.0"
@@ -12,9 +11,9 @@ class ExprTkConan(ConanFile):
     name = "exprtk"
     description = "C++ Mathematical Expression Parsing And Evaluation Library ExprTk"
     license = "MIT"
-    url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://www.partow.net/programming/exprtk/index.html"
     topics = ("math", "mathematics", "parser", "lexer", "numerical", "header-only")
+    homepage = "https://www.partow.net/programming/exprtk/index.html"
+    url = "https://github.com/conan-io/conan-center-index"
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
@@ -45,10 +44,7 @@ class ExprTkConan(ConanFile):
         save(self, os.path.join(self.package_folder, "licenses", "LICENSE"), license_contents)
 
     def package(self):
-        if Version(self.version) < "0.0.3":
-            self._extract_license()
-        else:
-            copy(self, "license.txt", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        self._extract_license()
         copy(self, "exprtk.hpp", dst=os.path.join(self.package_folder, "include"), src=self.source_folder)
 
     def package_info(self):
