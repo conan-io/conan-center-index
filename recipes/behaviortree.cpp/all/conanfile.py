a5e03f7f9919c4afa748456cdadad36b48dc3ae1
Martin Valgur <martin.valgur@gmail.com>
2024-01-02 09:02:07 +0200

    behaviortree.cpp: expose all build options

--- recipes/behaviortree.cpp/all/conanfile.py
+++ recipes/behaviortree.cpp/all/conanfile.py
@@ -23,14 +23,28 @@ class BehaviorTreeCPPConan(ConanFile):
         "fPIC": [True, False],
         "with_tools": [True, False],
         "with_coroutines": [True, False],
-        "with_manual_selector": [True, False],
+        "enable_groot_interface": [True, False],
+        "enable_sqlite_logging": [True, False],
+        "enable_manual_selector": [True, False],
+        "use_v3_compatible_names": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
         "with_tools": False,
         "with_coroutines": False,
-        "with_manual_selector": False,
+        "enable_groot_interface": True,
+        "enable_sqlite_logging": True,
+        "enable_manual_selector": False,
+        "use_v3_compatible_names": False,
+    }
+    options_description = {
+        "with_tools": "Build commandline tools",
+        "with_coroutines": "Enable Boost coroutines",
+        "enable_groot_interface": "Add Groot2 connection (requires ZeroMQ)",
+        "enable_sqlite_logging": "Add SQLite logging",
+        "enable_manual_selector": "Build manual selector node",
+        "use_v3_compatible_names": "Use v3 compatible names",
     }
 
     @property
@@ -64,8 +78,15 @@ class BehaviorTreeCPPConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+        if Version(self.version) < "4.0":
+            del self.options.use_v3_compatible_names
+        if Version(self.version) >= "4.1.0":
+            del self.options.with_coroutines
         if Version(self.version) >= "4.1.1":
-            del self.options.with_manual_selector
+            del self.options.enable_manual_selector
+        else:
+            del self.options.enable_groot_interface
+            del self.options.enable_sqlite_logging
 
     def configure(self):
         if self.options.shared:
@@ -75,26 +96,41 @@ class BehaviorTreeCPPConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     @property
-    def _with_tinyxml2(self):
-        return Version(self.version) >= "4.0.0"
+    def _with_boost(self):
+        return self.options.get_safe("with_coroutines", False)
 
     @property
     def _with_lexy(self):
-        return Version(self.version) >= "4.0.0"
-
-    @property
-    def _with_sqlite3(self):
-        return Version(self.version) >= "4.1.1"
+        # FIXME: using vendored version temporarily due to a missing CCI binary
+        # return Version(self.version) >= "4.0.0"
+        return False
 
     @property
     def _with_minitrace(self):
         return Version(self.version) >= "4.3.4"
 
+    @property
+    def _with_ncurses(self):
+        return self.options.get_safe("enable_manual_selector", False)
+
+    @property
+    def _with_sqlite3(self):
+        return self.options.get_safe("enable_sqlite_logging", False)
+
+    @property
+    def _with_tinyxml2(self):
+        return Version(self.version) >= "4.0.0"
+
+    @property
+    def _with_zeromq(self):
+        if Version(self.version) >= "4.1.1":
+            return self.options.enable_groot_interface
+        return Version(self.version) >= "4.0.0"
+
     def requirements(self):
-        self.requires("zeromq/4.3.5")
-        if self.options.with_coroutines:
+        if self._with_boost:
             self.requires("boost/1.83.0")
-        if self.options.get_safe("with_manual_selector"):
+        if self._with_ncurses:
             self.requires("ncurses/6.4")
         if self._with_lexy:
             self.requires("foonathan-lexy/2022.12.1")
@@ -104,6 +140,8 @@ class BehaviorTreeCPPConan(ConanFile):
             self.requires("sqlite3/3.44.2")
         if self._with_tinyxml2:
             self.requires("tinyxml2/10.0.0")
+        if self._with_zeromq:
+            self.requires("zeromq/4.3.5")
 
         # TODO: other vendored dependencies
         # - cppzmq is customized and not compatible with Conan version
@@ -147,7 +185,7 @@ class BehaviorTreeCPPConan(ConanFile):
         tc.variables["WITH_LEXY"] = self._with_lexy
         tc.variables["WITH_MINITRACE"] = self._with_minitrace
         tc.variables["WITH_TINYXML2"] = self._with_tinyxml2
-        if not self.options.get_safe("with_manual_selector"):
+        if not self.options.get_safe("enable_manual_selector"):
             # Avoid accidental use of system ncurses
             tc.variables["CMAKE_DISABLE_FIND_PACKAGE_Curses"] = True
         if Version(self.version) < "4.0":
@@ -155,14 +193,17 @@ class BehaviorTreeCPPConan(ConanFile):
             tc.variables["BUILD_UNIT_TESTS"] = False
             tc.variables["BUILD_TOOLS"] = self.options.with_tools
             tc.variables["ENABLE_COROUTINES"] = self.options.with_coroutines
-            tc.variables["BUILD_MANUAL_SELECTOR"] = self.options.get_safe("with_manual_selector", False)
+            tc.variables["BUILD_MANUAL_SELECTOR"] = self.options.get_safe("enable_manual_selector", False)
         else:
             tc.variables["BTCPP_SHARED_LIBS"] = self.options.shared
             tc.variables["BTCPP_EXAMPLES"] = False
             tc.variables["BTCPP_UNIT_TESTS"] = False
             tc.variables["BTCPP_BUILD_TOOLS"] = self.options.with_tools
-            tc.variables["BTCPP_ENABLE_COROUTINES"] = self.options.with_coroutines
-            tc.variables["BTCPP_MANUAL_SELECTOR"] = self.options.get_safe("with_manual_selector", False)
+            tc.variables["BTCPP_ENABLE_COROUTINES"] = self.options.get_safe("with_coroutines", False)
+            tc.variables["BTCPP_MANUAL_SELECTOR"] = self.options.get_safe("enable_manual_selector", False)
+            tc.variables["BTCPP_GROOT_INTERFACE"] = self.options.get_safe("enable_groot_interface", False)
+            tc.variables["BTCPP_SQLITE_LOGGING"] = self.options.get_safe("enable_sqlite_logging", False)
+            tc.variables["USE_V3_COMPATIBLE_NAMES"] = self.options.use_v3_compatible_names
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
         tc.generate()
 
@@ -223,19 +264,21 @@ class BehaviorTreeCPPConan(ConanFile):
         self.cpp_info.set_property("cmake_file_name", cmake_file_name)
         self.cpp_info.set_property("cmake_target_name", f"BT::{libname}")
 
-        requires = ["zeromq::zeromq"]
-        if self.options.get_safe("with_manual_selector"):
-            requires.append("ncurses::ncurses")
-        if self.options.with_coroutines:
+        requires = []
+        if self._with_boost:
             requires.append("boost::coroutine")
         if self._with_lexy:
             requires.append("foonathan-lexy::foonathan-lexy")
         if self._with_minitrace:
             requires.append("minitrace::minitrace")
+        if self._with_ncurses:
+            requires.append("ncurses::ncurses")
         if self._with_sqlite3:
             requires.append("sqlite3::sqlite3")
         if self._with_tinyxml2:
             requires.append("tinyxml2::tinyxml2")
+        if self._with_zeromq:
+            requires.append("zeromq::zeromq")
 
         postfix = "d" if self.settings.os == "Windows" and self.settings.build_type == "Debug" else ""
         # TODO: back to global scope in conan v2 once cmake_find_package* generators removed
