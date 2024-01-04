fb10750660c79548de3e69b0fb84d06a1dcc6827
Anton Piliugin <anton.piliugin@icloud.com>
2024-01-04 21:22:07 +0500

    Updated recipe

--- recipes/libsrtp/all/conanfile.py
+++ recipes/libsrtp/all/conanfile.py
@@ -1,7 +1,8 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import collect_libs, copy, get, replace_in_file
+from conan.tools.files import collect_libs, copy, get, replace_in_file, save
 from conan.tools.scm import Version
+import textwrap
 import os
 
 required_conan_version = ">=1.53.0"
@@ -24,11 +25,15 @@ class LibsrtpRecipe(ConanFile):
         "shared": [True, False],
         "fPIC": [True, False],
         "with_openssl": [True, False],
+        "with_nss": [True, False],
+        "with_mbedtls": [True, False]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
         "with_openssl": False,
+        "with_nss": False,
+        "with_mbedtls": False
     }
 
     def config_options(self):
@@ -48,6 +53,13 @@ class LibsrtpRecipe(ConanFile):
         if self.options.with_openssl:
             self.requires("openssl/[>=1.1 <4]")
 
+        if self.options.with_nss:
+            self.requires("nss3")
+            self.requires("nspr")
+
+        if self.options.with_mbedtls:
+            self.requires("mbedtls")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
@@ -62,7 +74,7 @@ class LibsrtpRecipe(ConanFile):
         deps = CMakeDeps(self)
         deps.generate()
 
-    def build(self):
+    def _patch_sources(self):
         if Version(self.version) < "2.5.0":
             replace_in_file(
                 self, os.path.join(self.source_folder, "CMakeLists.txt"),
@@ -74,7 +86,24 @@ class LibsrtpRecipe(ConanFile):
                     "LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}\n"
                     "ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})"
                 ),
-        )
+            )
+        else:
+            replace_in_file(
+                self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                "install(TARGETS srtp2 DESTINATION lib\n  EXPORT libSRTPTargets\n)",
+                (
+                    "include(GNUInstallDirs)\n"
+                    "install(TARGETS srtp2\n"
+                    "EXPORT libSRTPTargets\n"
+                    "RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}\n"
+                    "LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}\n"
+                    "ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})"
+                ),
+            )
+
+    def build(self):
+        self._patch_sources()
+
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -84,8 +113,44 @@ class LibsrtpRecipe(ConanFile):
         cmake = CMake(self)
         cmake.install()
 
+        self._create_cmake_module_variables(
+            os.path.join(self.package_folder, self._module_vars_file)
+        )
+
+    def _create_cmake_module_variables(self, module_file):
+        content = textwrap.dedent("""\
+            set(libsrtp_FOUND ${libsrtp_FOUND})
+            if(DEFINED libsrtp_INCLUDE_DIR)
+                set(libsrtp_INCLUDE_DIR ${libsrtp_INCLUDE_DIR})
+            endif()
+            if(DEFINED libsrtp_LIBRARIES)
+                set(libsrtp_LIBRARIES ${libsrtp_LIBRARIES})
+            endif()
+        """)
+        save(self, module_file, content)
+
+    @property
+    def _module_vars_file(self):
+        return os.path.join("lib", "cmake", f"conan-official-{self.name}-variables.cmake")
+
+    @property
+    def _module_target_file(self):
+        return os.path.join("lib", "cmake", f"conan-official-{self.name}-targets.cmake")
+
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", f"libsrtp{Version(self.version).major}")
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "libsrtp")
+        self.cpp_info.set_property("cmake_target_name", "libsrtp")
+        self.cpp_info.set_property("cmake_target_aliases", ["srtp2"])
+        self.cpp_info.set_property("cmake_build_modules", [self._module_vars_file])
+
+        self.cpp_info.names["cmake_find_package"] = "libsrtp"
+        self.cpp_info.names["cmake_find_package_multi"] = "libsrtp"
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_vars_file]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_target_file]
+
         self.cpp_info.libs = collect_libs(self)
+
         if self.settings.os == "Windows":
             self.cpp_info.system_libs = ["ws2_32"]
