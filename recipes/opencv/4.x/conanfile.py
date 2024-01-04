3c36bb86edf99b4c1d18511263417a3682c679ba
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-01-04 06:36:56 +0100

    revert gdal

--- recipes/opencv/4.x/conanfile.py
+++ recipes/opencv/4.x/conanfile.py
@@ -1127,7 +1127,7 @@ class OpenCVConan(ConanFile):
         if self.options.get_safe("with_webp"):
             self.requires("libwebp/1.3.2")
         if self.options.get_safe("with_gdal"):
-            self.requires("gdal/3.4.3")
+            self.requires("gdal/3.7.0")
         if self.options.get_safe("with_gdcm"):
             self.requires("gdcm/3.0.21")
         # objdetect module dependencies
