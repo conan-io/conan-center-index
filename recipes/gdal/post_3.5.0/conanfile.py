2a2e3929b303925a6c4ebc0bd51a526a1fa9e2cc
Martin Valgur <martin.valgur@gmail.com>
2024-01-04 08:20:22 +0200

    gdal: re-enable libiconv for apple-clang

--- recipes/gdal/post_3.5.0/conanfile.py
+++ recipes/gdal/post_3.5.0/conanfile.py
@@ -166,12 +166,6 @@ class GdalConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        if is_apple_os(self):
-            # FIXME: compilation fails on macOS due to difference in ICONV_CONST value in iconv() signature
-            # https://github.com/conan-io/conan-center-index/pull/19298#issuecomment-1840433415
-            # https://github.com/roboticslibrary/libiconv/blob/v1.17/include/iconv.h.in#L82
-            # https://lists.gnu.org/archive/html/bug-gnu-libiconv/2008-07/msg00005.html
-            self.options.with_libiconv = False
         if Version(self.version) < "3.7":
             # Latest versions of Arrow are no longer compatible with GDAL 3.5
             self.options.with_arrow = False
