79732737304c192f17ebc32972342c64f1e8e9b9
Martin Valgur <martin.valgur@gmail.com>
2024-01-05 13:16:03 +0200

    nodejs: use platform.libc_ver() for libc version detection

--- recipes/nodejs/all/test_package/conanfile.py
+++ recipes/nodejs/all/test_package/conanfile.py
@@ -1,4 +1,4 @@
-from io import StringIO
+import platform
 
 from conan import ConanFile
 from conan.tools.build import can_run
@@ -17,21 +17,11 @@ class TestPackageConan(ConanFile):
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
 
-    def _get_libc_version(self):
-        stdout = StringIO()
-        self.run("ldd --version 2>&1", stdout, env="buildenv")
-        stdout = stdout.getvalue()
-        try:
-            return Version(stdout.splitlines()[0].split()[-1])
-        except IndexError:
-            self.output.warning("Failed to parse libc version from 'ldd --version' output:\n" + stdout)
-            return None
-
     def test(self):
         if can_run(self):
             if self.settings.os in ["Linux", "FreeBSD"]:
-                libc_version = self._get_libc_version()
-                if libc_version and libc_version < "2.29":
+                libc_version = Version(platform.libc_ver()[1])
+                if libc_version < "2.29":
                     self.output.warning(f"System libc version {libc_version} < 2.29, skipping test_package")
                     return
             self.run("node --version")
