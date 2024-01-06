3111a5626cb9c715326525620ba61d70568dea8a
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2024-01-06 11:27:24 +0000

    pdcurses/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericLemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/pdcurses/all/conanfile.py
+++ recipes/pdcurses/all/conanfile.py
@@ -77,7 +77,7 @@ class PDCursesConan(ConanFile):
     def build_requirements(self):
         if not is_msvc(self):
             if not self.conf.get("tools.gnu:make_program", check_type=str):
-                self.tool_requires("make/4.4")
+                self.tool_requires("make/4.4.1")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
