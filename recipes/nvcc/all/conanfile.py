8df850362e9a23e8e9eba1b8bba3862896dadff0
rymut <boguslaw@rymut.org>
2024-01-07 18:56:35 +0100

    fix: linux package fix

--- recipes/nvcc/all/conanfile.py
+++ recipes/nvcc/all/conanfile.py
@@ -1,8 +1,9 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import get, save, copy, symlinks, patch, export_conandata_patches, rm
+from conan.tools.files import get, save, copy, symlinks, patch, export_conandata_patches, rm, rmdir, rename
 from conan.tools.scm import Version
 import os
+from os import chmod
 from os.path import isdir, join, exists
 
 required_conan_version = ">=1.47.0"
@@ -21,6 +22,7 @@ class NvccConan(ConanFile):
     license = "Nvidia CUDA Toolkit EULA"
     topics = ("nvcc", "compiler", "cuda", "nvidia", "SDK")
     exports_sources = ["nvcc_toolchain.cmake"]
+    package_type = "application"
     settings = "os", "arch"
     options = {}
     default_options = {}
@@ -37,13 +39,18 @@ class NvccConan(ConanFile):
 
     def build(self):
         srcs = self.conan_data["sources"][self.version][str(self.settings.os)][str(self.settings.arch)]
-        if isinstance(srcs, list):
-            for src in srcs:
-                get(self, **src, strip_root=True)
-        else:
-            get(self, **srcs, strip_root=True)
-        copy(self, "*.dll", join(self.build_folder, "lib"), join(self.build_folder, "bin"))
-        rm(self, "*.dll", join(self.build_folder, "lib"))
+        license_path = join(self.build_folder, "LICENSE")
+        for src in srcs:
+            get(self, **src, strip_root=True)
+            if exists(license_path):
+                chmod(license_path, 0o666)
+        # nvcc package
+        nvvm_root = join(self.build_folder, "nvvm")
+        if exists(join(nvvm_root, "lib64")):
+            rename(self, join(nvvm_root, "lib64"), join(nvvm_root, "lib"))
+        if exists(join(nvvm_root, "nvvm-samples")):
+            rmdir(self, join(self, nvvm_root, "nvvm-samples"))
+        # patching
         for it in self.conan_data.get("patches", {}).get(str(self.version), []):
             if str(self.settings.os).lower() in it.get("patch_file", ""):
                 patch(self, **it, base_path=self.build_folder)
@@ -51,14 +58,13 @@ class NvccConan(ConanFile):
     def package(self):
         copy(self, "nvcc_toolchain.cmake", self.export_sources_folder, self.package_folder)
         copy(self, "LICENSE", self.build_folder, join(self.package_folder, "licenses"))
-        dirs = ["bin", "lib", "lib64", "include", "nvvm"]
+        dirs = ["bin", "lib", "include", "nvvm"]
         for name in dirs:
             if exists(join(self.build_folder, name)):
                 copy(self, "*", join(self.build_folder, name), join(self.package_folder, name))
         if exists(join(self.build_folder, "visual_studio_integration")):
             copy(self, "*", join(self.build_folder, "visual_studio_integration"), join(self.package_folder, "CUDAVisualStudioIntegration", "extras", "visual_studio_integration"))
         symlinks.absolute_to_relative_symlinks(self, self.package_folder)
-
 
     def package_info(self):
         def lib(name):
@@ -96,16 +102,12 @@ class NvccConan(ConanFile):
             if libname is None:
                 continue
             self.cpp_info.components[component].set_property("cmake_target_aliases", [f"CUDA::{component}"])
-            self.cpp_info.components[component].libdirs = [libdir]
+            self.cpp_info.components[component].libdirs = ['lib']
             self.cpp_info.components[component].resdirs = []
-            self.cpp_info.components[component].bindirs = bin_dir()
             self.cpp_info.components[component].includedirs = [incdir]
             self.cpp_info.components[component].libs = [libname]
-        if bin_dir():
-            self.runenv_info.prepend_path("PATH", join(self.package_folder, bin_dir()[0]))
+        run_extension = ".exe" if self.settings.os == "Windows" else ""
+        self.conf_info.update("tools.build:compiler_executables", { "cuda": join(self.package_folder, "bin", "nvcc" + run_extension) })
         self.conf_info.append("tools.cmake.cmaketoolchain:user_toolchain", join(self.package_folder, "nvcc_toolchain.cmake"))
         if self.settings.os == "Windows":
-            self.conf_info.update("tools.build:compiler_executables", { "cuda": join(self.package_folder, "bin", "nvcc.exe") })
             self.conf_info.define("tools.cmake.cmaketoolchain:toolset_arch", f"x64,cuda={self.package_folder.replace(os.sep, '/')}")
-        else:
-            self.conf_info.update("tools.build:compiler_executables", { "cuda": join(self.package_folder, "bin", "nvcc") })
