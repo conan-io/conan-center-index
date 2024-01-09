07e88a2fc8ee3d37a937d44be1b4ac482761c6e1
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2024-01-09 11:32:18 +0200

    import `CMakeToolchain` from `conan.tools.cmake`

--- recipes/sbepp/all/test_v1_package/conanfile.py
+++ recipes/sbepp/all/test_v1_package/conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, CMake, CMakeToolchain, CMakeDeps
+from conans import ConanFile, CMake
+from conan.tools.cmake import CMakeToolchain
 from conan.tools.build import cross_building
 import os
 
