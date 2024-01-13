e0163268f3145237b30714cd5b9885f4d6021528
Martin Valgur <martin.valgur@gmail.com>
2024-01-13 21:14:23 +0200

    osmanip: drop older versions

--- recipes/osmanip/all/conanfile.py
+++ recipes/osmanip/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.files import copy, get
 from conan.tools.scm import Version
 
 import os
@@ -33,7 +33,6 @@ class OsmanipConan(ConanFile):
     def export_sources(self):
         if Version(self.version) < "4.5.0":
             copy(self, "CMakeLists.txt", self.recipe_folder, os.path.join(self.export_sources_folder, "src"))
-        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -91,7 +90,6 @@ class OsmanipConan(ConanFile):
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
