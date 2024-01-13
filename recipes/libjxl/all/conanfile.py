c7ac8a2c7771af627cce1089e2a63d1feee433c8
Martin Valgur <martin.valgur@gmail.com>
2024-01-13 18:54:32 +0200

    jxl: add v0.9.1, v0.6.1, simplify patching

--- recipes/libjxl/all/conanfile.py
+++ recipes/libjxl/all/conanfile.py
@@ -3,7 +3,9 @@ import os
 from conan import ConanFile
 from conan.tools.build import cross_building, stdcpp_library, check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import replace_in_file, copy, get, rmdir, save, rm
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import copy, get, rmdir, save, rm, replace_in_file
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
 
@@ -27,6 +29,9 @@ class LibjxlConan(ConanFile):
         "fPIC": True,
     }
 
+    def export_sources(self):
+        copy(self, "conan_deps.cmake", self.recipe_folder, os.path.join(self.export_sources_folder, "src"))
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -40,34 +45,48 @@ class LibjxlConan(ConanFile):
 
     def requirements(self):
         self.requires("brotli/1.1.0")
-        self.requires("highway/1.0.7")
-        self.requires("lcms/2.14")
+        if Version(self.version) >= "0.7":
+            self.requires("highway/1.0.7")
+        else:
+            self.requires("highway/0.12.2")
+        self.requires("lcms/2.16")
 
     def validate(self):
         if self.settings.compiler.cppstd:
             check_min_cppstd(self, 11)
 
+    def build_requirements(self):
+        if Version(self.version) >= "0.9":
+            self.tool_requires("cmake/[>=3.16 <4]")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
+        VirtualBuildEnv(self).generate()
+
         tc = CMakeToolchain(self)
-        tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.shared
-        tc.cache_variables["BUILD_TESTING"] = False
-        tc.cache_variables["JPEGXL_STATIC"] = not self.options.shared  # applies to tools only
-        tc.cache_variables["JPEGXL_ENABLE_BENCHMARK"] = False
-        tc.cache_variables["JPEGXL_ENABLE_EXAMPLES"] = False
-        tc.cache_variables["JPEGXL_ENABLE_MANPAGES"] = False
-        tc.cache_variables["JPEGXL_ENABLE_SJPEG"] = False
-        tc.cache_variables["JPEGXL_ENABLE_OPENEXR"] = False
-        tc.cache_variables["JPEGXL_ENABLE_SKCMS"] = False
-        tc.cache_variables["JPEGXL_ENABLE_TCMALLOC"] = False
-        tc.cache_variables["JPEGXL_FORCE_SYSTEM_BROTLI"] = True
-        tc.cache_variables["JPEGXL_FORCE_SYSTEM_LCMS2"] = True
-        tc.cache_variables["JPEGXL_FORCE_SYSTEM_HWY"] = True
-        tc.cache_variables["JPEGXL_FORCE_SYSTEM_GTEST"] = True
+        tc.variables["CMAKE_PROJECT_LIBJXL_INCLUDE"] = "conan_deps.cmake"
+        tc.variables["BUILD_TESTING"] = False
+        tc.variables["JPEGXL_STATIC"] = not self.options.shared  # applies to tools only
+        tc.variables["JPEGXL_ENABLE_BENCHMARK"] = False
+        tc.variables["JPEGXL_ENABLE_EXAMPLES"] = False
+        tc.variables["JPEGXL_ENABLE_MANPAGES"] = False
+        tc.variables["JPEGXL_ENABLE_SJPEG"] = False
+        tc.variables["JPEGXL_ENABLE_OPENEXR"] = False
+        tc.variables["JPEGXL_ENABLE_SKCMS"] = False
+        tc.variables["JPEGXL_ENABLE_TCMALLOC"] = False
+        tc.variables["JPEGXL_FORCE_SYSTEM_BROTLI"] = True
+        tc.variables["JPEGXL_FORCE_SYSTEM_LCMS2"] = True
+        tc.variables["JPEGXL_FORCE_SYSTEM_HWY"] = True
+        tc.variables["JPEGXL_FORCE_SYSTEM_GTEST"] = True
         if cross_building(self):
             tc.variables["CMAKE_SYSTEM_PROCESSOR"] = str(self.settings.arch)
+        # Allow non-cache_variables to be used
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+        # Skip the buggy custom FindAtomic and force the use of atomic library directly for libstdc++
+        tc.variables["ATOMICS_FOUND"] = True
+        tc.variables["ATOMICS_LIBRARIES"] = "atomic" if self._atomic_required else ""
         tc.generate()
 
         deps = CMakeDeps(self)
@@ -81,42 +100,15 @@ class LibjxlConan(ConanFile):
         return self.settings.get_safe("compiler.libcxx") in ["libstdc++", "libstdc++11"]
 
     def _patch_sources(self):
-        # Disable tools and extras
+        # Disable tools, extras and third_party
         save(self, os.path.join(self.source_folder, "tools", "CMakeLists.txt"), "")
         save(self, os.path.join(self.source_folder, "lib", "jxl_extras.cmake"), "")
-        # Inject Conan dependencies
-        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-                        "add_subdirectory(third_party)",
-                        ("find_package(LCMS2 REQUIRED CONFIG)\n"
-                         "find_package(Brotli REQUIRED CONFIG)\n"
-                         "find_package(HWY REQUIRED CONFIG)\n"
-                         "link_libraries(brotli::brotli highway::highway lcms::lcms)\n")
-                        )
-        # Do not link directly against libraries
-        for cmake in [
-            self.source_path.joinpath("lib", "jxl.cmake"),
-            self.source_path.joinpath("lib", "jpegli.cmake"),
-        ]:
-            content = cmake.read_text(encoding="utf8")
-            for lib, replacement in [
-                ("hwy", "highway::highway"),
-                ("brotlicommon-static", "brotli::brotli"),
-                ("brotlienc-static", "brotli::brotli"),
-                ("brotlidec-static", "brotli::brotli"),
-                ("lcms2", "lcms::lcms"),
-            ]:
-                content = content.replace(lib, replacement)
-            cmake.write_text(content, encoding="utf8")
-        # Avoid "INTERFACE_LIBRARY targets may only have whitelisted properties" error with Conan v1
-        jxl_cmake = os.path.join(self.source_folder, "lib", "jxl.cmake")
-        replace_in_file(self, jxl_cmake, '"$<BUILD_INTERFACE:$<TARGET_PROPERTY', "# ", strict=False)
-        replace_in_file(self, jxl_cmake, "$<TARGET_PROPERTY", "# ", strict=False)
-        # Skip custom FindAtomic and force the use of atomic library directly for libstdc++
-        atomic_lib = "atomic" if self._atomic_required else ""
-        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-                        "find_package(Atomics REQUIRED)",
-                        "set(ATOMICS_FOUND TRUE)\n"
-                        f"set(ATOMICS_LIBRARIES {atomic_lib})")
+        save(self, os.path.join(self.source_folder, "third_party", "CMakeLists.txt"), "")
+
+        if Version(self.version) < "0.7":
+            replace_in_file(self, os.path.join(self.source_folder, "lib", "jxl.cmake"),
+                            "  DESTINATION ${CMAKE_INSTALL_LIBDIR}",
+                            "  RUNTIME DESTINATION bin LIBRARY DESTINATION lib ARCHIVE DESTINATION lib")
 
     def build(self):
         self._patch_sources()
