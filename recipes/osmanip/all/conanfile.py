4a5183d6b4e2653a3baac0a362f0b24c5460f38e
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-01-15 11:34:20 +0000

    osmanip: bump arsenalgear

--- recipes/osmanip/all/conanfile.py
+++ recipes/osmanip/all/conanfile.py
@@ -48,10 +48,7 @@ class OsmanipConan(ConanFile):
     def requirements(self):
         self.requires("boost/1.83.0")
         # osmanip/progressbar/progress_bar.hpp includes arsenalgear/constants.hpp
-        if Version(self.version) < "4.2.0":
-            self.requires("arsenalgear/1.2.2", transitive_headers=True)
-        else:
-            self.requires("arsenalgear/2.0.1", transitive_headers=True)
+        self.requires("arsenalgear/2.1.0", transitive_headers=True)
 
     @property
     def _min_cppstd(self):
