a5fe2c8792e69c09e9ae512beb2f9fceb971b965
DavidAce <aceituno@kth.se>
2024-01-15 08:03:36 +0100

    Bumped dependency version

--- recipes/h5pp/all/conanfile.py
+++ recipes/h5pp/all/conanfile.py
@@ -73,7 +73,7 @@ class H5ppConan(ConanFile):
         if Version(self.version) < "1.10.0" or self.options.get_safe('with_eigen'):
             self.requires("eigen/3.4.0", transitive_headers=True)
         if Version(self.version) < "1.10.0" or self.options.get_safe('with_spdlog'):
-            self.requires("spdlog/[>=1.11.0 <1.13]", transitive_headers=True, transitive_libs=True)
+            self.requires("spdlog/[>=1.11.0 <1.14]", transitive_headers=True, transitive_libs=True)
         if Version(self.version) >= "1.10.0" and self.options.with_zlib:
             self.requires("zlib/[>=1.2.11 <2]", transitive_headers=True, transitive_libs=True)
 
