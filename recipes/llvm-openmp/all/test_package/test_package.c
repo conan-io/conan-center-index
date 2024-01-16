29b48abf51cb6d60a043a9bf5d2e2e189441ed08
Martin Valgur <martin.valgur@gmail.com>
2024-01-16 19:20:43 +0200

    llvm-openmp: print runtime library info in test_package

--- recipes/llvm-openmp/all/test_package/test_package.c
+++ recipes/llvm-openmp/all/test_package/test_package.c
@@ -21,6 +21,6 @@ int main()
         printf("There are probably missing compiler flags.\n");
         return 1;
     }
-    printf("OpenMP year-month version: %d", _OPENMP);
+    printf("OpenMP year-month version: %d\n", _OPENMP);
     return 0;
 }
