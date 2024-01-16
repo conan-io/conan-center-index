8c3d63ad2ae6a1c64fd07f1d2dbaf43b52e826f1
Martin Valgur <martin.valgur@gmail.com>
2024-01-16 19:20:43 +0200

    llvm-openmp: print runtime library info in test_package

--- recipes/llvm-openmp/all/test_package/conanfile.py
+++ recipes/llvm-openmp/all/test_package/conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.env import Environment
 import os
 
 
@@ -14,6 +15,12 @@ class TestPackageConan(ConanFile):
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        env = Environment()
+        # Trigger printing of runtime version info on startup
+        env.define("KMP_VERSION", "TRUE")
+        env.vars(self, scope="run").save_script("conan_openmp_version")
 
     def build(self):
         cmake = CMake(self)
