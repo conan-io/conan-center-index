ca20a234f59277b222ab89967a74299ffff14367
Martin Valgur <martin.valgur@gmail.com>
2024-01-16 10:02:09 +0200

    vk-bootstrap: fix shared build in v1.3.x

--- recipes/vk-bootstrap/all/conanfile.py
+++ recipes/vk-bootstrap/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd, stdcpp_library
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 import os
@@ -12,7 +12,7 @@ required_conan_version = ">=1.54.0"
 
 class VkBootstrapConan(ConanFile):
     name = "vk-bootstrap"
-    description = "Vulkan bootstraping library."
+    description = "Vulkan bootstrapping library."
     license = "MIT"
     topics = ("vulkan", "bootstrap", "setup")
     homepage = "https://github.com/charles-lunarg/vk-bootstrap"
@@ -115,8 +115,14 @@ class VkBootstrapConan(ConanFile):
             tc.variables["VK_BOOTSTRAP_WERROR"] = False
         tc.generate()
 
-    def build(self):
+    def _patch_sources(self):
         apply_conandata_patches(self)
+        if Version(self.version) >= "1.3":
+            replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                            "add_library(vk-bootstrap STATIC ", "add_library(vk-bootstrap ")
+
+    def build(self):
+        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
