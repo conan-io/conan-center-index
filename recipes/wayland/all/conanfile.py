f34396d0709015eeaefe0e0c9512d08eca60ed56
Eric Lemanissier (bot) <eric.lemanissier@gmail.com>
2024-01-17 01:16:00 +0000

    wayland/all: bump deps
    Generated and committed by [Conan Center Bump Deps](https://github.com/ericLemanissier/conan-center-index-bump-deps)
    Find more updatable recipes in the [GitHub Pages](https://ericLemanissier.github.io/conan-center-index-bump-deps/)

--- recipes/wayland/all/conanfile.py
+++ recipes/wayland/all/conanfile.py
@@ -50,7 +50,7 @@ class WaylandConan(ConanFile):
         if self.options.enable_libraries:
             self.requires("libffi/3.4.4")
         if self.options.enable_dtd_validation:
-            self.requires("libxml2/2.12.3")
+            self.requires("libxml2/2.12.4")
         self.requires("expat/2.5.0")
 
     def validate(self):
@@ -58,7 +58,7 @@ class WaylandConan(ConanFile):
             raise ConanInvalidConfiguration(f"{self.ref} only supports Linux")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.3.0")
+        self.tool_requires("meson/1.3.1")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/2.1.0")
         if not can_run(self):
