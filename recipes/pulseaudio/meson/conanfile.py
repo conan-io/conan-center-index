667e54d1abf3a5066844b5813d1196c1ece131af
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-01-19 15:42:07 +0000

    remove gettext

--- recipes/pulseaudio/meson/conanfile.py
+++ recipes/pulseaudio/meson/conanfile.py
@@ -87,7 +87,6 @@ class PulseAudioConan(ConanFile):
                 )
 
     def build_requirements(self):
-        self.tool_requires("gettext/0.21")
         self.tool_requires("meson/1.3.1")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/2.0.3")
