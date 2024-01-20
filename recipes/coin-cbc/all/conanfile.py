b239f2bbbfe3f01d43ac90a3a8566920b023987f
Martin Valgur <martin.valgur@gmail.com>
2024-01-20 16:52:05 +0200

    coin-cbc: bump deps

--- recipes/coin-cbc/all/conanfile.py
+++ recipes/coin-cbc/all/conanfile.py
@@ -60,7 +60,7 @@ class CoinCbcConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("gnu-config/cci.20210814")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.0.3")
+            self.tool_requires("pkgconf/2.1.0")
         if self._settings_build.os == "Windows":
             self.win_bash = True
             if not self.conf.get("tools.microsoft.bash:path", check_type=str):
