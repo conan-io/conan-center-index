e965a444851c1cad3f387b84b857f7c2e77a8b3b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2024-01-21 20:55:04 +0100

    support mingw

--- recipes/isa-l/all/conanfile.py
+++ recipes/isa-l/all/conanfile.py
@@ -30,6 +30,10 @@ class LibisalConan(ConanFile):
         "fPIC": True,
     }
 
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -51,8 +55,12 @@ class LibisalConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("nasm/2.15.05")
-        if self.settings.os != "Windows":
+        if not is_msvc(self):
             self.tool_requires("libtool/2.4.7")
+            if self._settings_build.os == "Windows":
+                self.win_bash = True
+                if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                    self.tool_requires("msys2/cci.latest")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
