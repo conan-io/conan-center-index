879498daf7b55be2bb1915a7b8280d64ef6876b0
Uilian Ries <uilianries@gmail.com>
2024-01-25 16:48:44 +0100

    Revert "protobuf: improve discovery of protoc executable in build context"

--- recipes/protobuf/all/test_package/conanfile.py
+++ recipes/protobuf/all/test_package/conanfile.py
@@ -1,12 +1,13 @@
 from conan import ConanFile
-from conan.tools.build import can_run
+from conan.tools.build import can_run, cross_building
 from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "VirtualBuildEnv", "VirtualRunEnv"
+    generators = "CMakeDeps"
     test_type = "explicit"
 
     def layout(self):
@@ -16,9 +17,15 @@ class TestPackageConan(ConanFile):
         self.requires(self.tested_reference_str)
 
     def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+        if cross_building(self) and hasattr(self, "settings_build"):
+            self.tool_requires(self.tested_reference_str)
 
     def generate(self):
+        VirtualRunEnv(self).generate()
+        if cross_building(self) and hasattr(self, "settings_build"):
+            VirtualBuildEnv(self).generate()
+        else:
+            VirtualRunEnv(self).generate(scope="build")
         tc = CMakeToolchain(self)
         tc.cache_variables["protobuf_LITE"] = self.dependencies[self.tested_reference_str].options.lite
         tc.generate()
