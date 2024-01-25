b4db0f696e29966375869247af42b3a1d2be0e95
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-01-25 18:00:28 +0000

    make change - do not merge

--- recipes/zstr/all/conanfile.py
+++ recipes/zstr/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
-import os
+import os #comment
 
 required_conan_version = ">=1.51.1"
 
