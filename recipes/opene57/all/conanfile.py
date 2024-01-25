41061a98b09c2710ab548208e92b530902568464
Michele Adduci <madduci@users.noreply.github.com>
2024-01-25 16:10:24 +0100

    Update Boost
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/opene57/all/conanfile.py
+++ recipes/opene57/all/conanfile.py
@@ -66,7 +66,7 @@ class Opene57Conan(ConanFile):
 
     def requirements(self):
         if self.options.with_tools:
-            self.requires("boost/1.83.0")
+            self.requires("boost/1.84.0")
 
         if self.options.with_docs:
             self.requires("doxygen/1.9.4")
