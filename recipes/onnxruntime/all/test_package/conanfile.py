08f4f49f887d19e5a5d03a31b5b0d9071f2ce2aa
Florian de Gaulejac <florian.degaulejac@stilla.fr>
2024-01-26 11:45:01 +0100

    Test CUDA in the test_package

--- recipes/onnxruntime/all/test_package/conanfile.py
+++ recipes/onnxruntime/all/test_package/conanfile.py
@@ -1,13 +1,14 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+from conan.tools.files import copy
 import os
 
 
 # It will become the standard on Conan 2.x
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
@@ -15,6 +16,15 @@ class TestPackageConan(ConanFile):
 
     def layout(self):
         cmake_layout(self, src_folder=".")
+        
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["WITH_CUDA"] = self.dependencies["onnxruntime"].options.with_cuda
+        tc.generate()
+        
+        # on windows the system dll C:\WINDOWS\system32\onnxruntime.dll may be loaded instead even if the conan lib is first in the PATH, see https://learn.microsoft.com/en-us/windows/win32/dlls/dynamic-link-library-search-order
+        for bindir in self.dependencies[self.tested_reference_str].cpp_info.bindirs:
+            copy(self, "*.dll", bindir, self.build_folder)
 
     def build(self):
         cmake = CMake(self)
