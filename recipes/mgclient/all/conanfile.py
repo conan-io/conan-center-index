b6153a7a6fedad9a097bd0e394de249e47c73bd9
toge <toge.mail@gmail.com>
2024-01-27 23:06:24 +0900

    add CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS

--- recipes/mgclient/all/conanfile.py
+++ recipes/mgclient/all/conanfile.py
@@ -78,6 +78,7 @@ class MGClientConan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["BUILD_CPP_BINDINGS"] = self.options.with_cpp
+        tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
