c60ba068ebe52a8f9156531ebd13aa411db78974
Ahajha <ahajha@gmail.com>
2024-01-26 22:36:22 -0500

    Revert "Try different test package"
    This reverts commit 940f87493c882efd9b43e2719e416714f144340c.

--- recipes/tk/all/test_package/src/test_package.c
+++ recipes/tk/all/test_package/src/test_package.c
@@ -4,24 +4,21 @@
 #include <tcl.h>
 #include <tk.h>
 
-int app_init(Tcl_Interp *interp) {
+int main (int argc ,char *argv[]) {
+    Tcl_FindExecutable(argv[0]);
+    Tcl_Interp *interp = Tcl_CreateInterp();
     if (Tcl_Init(interp) != TCL_OK) {
         fprintf(stderr ,"Tcl_Init error: %s\n" ,Tcl_GetStringResult(interp));
-        exit(EXIT_SUCCESS);
-        return TCL_OK;
+        return EXIT_FAILURE;
     }
 
     if (Tk_Init(interp) != TCL_OK) {
         fprintf(stderr, "Tk_Init failed: %s\n", Tcl_GetStringResult(interp));
         fprintf(stderr, "But ignore it: there may not be a X server running.\n");
-        exit(EXIT_SUCCESS);
-        return TCL_OK;
+        return EXIT_SUCCESS;
     }
-    exit(EXIT_SUCCESS);
-    return TCL_OK;
-}
 
-int main (int argc ,char *argv[]) {
-    Tk_Main(argc, argv, app_init);
+    Tcl_Finalize();
+    fprintf(stderr, "Test package success.\n");
     return EXIT_SUCCESS;
 }
