3ca83bc0fd8dd8d1b1b967a1dee333d538c17de8
Rubén Rincón Blanco <git@rinconblanco.es>
2024-02-01 12:37:53 +0100

    Update recipes/eventpp/all/conanfile.py

--- recipes/eventpp/all/conanfile.py
+++ recipes/eventpp/all/conanfile.py
@@ -21,9 +21,6 @@ class EventppConan(ConanFile):
     def _min_cppstd(self):
         return 11
 
-    def export_sources(self):
-        export_conandata_patches(self)
-
     def layout(self):
         basic_layout(self, src_folder="src")
            
