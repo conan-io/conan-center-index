e210bafd87e61844ad44ef58c43f783ca7e8fa26
Alex Trotta <ahajha@gmail.com>
2024-02-01 01:04:04 -0500

    Remove Py_SetProgramName (deprecated in 3.11)

--- recipes/cpython/all/test_package/py3/test_package.c
+++ recipes/cpython/all/test_package/py3/test_package.c
@@ -9,7 +9,6 @@ main(int argc, char *argv[])
         fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
         exit(1);
     }
-    Py_SetProgramName(program);  /* optional but recommended */
     Py_Initialize();
     PyRun_SimpleString("from time import time, ctime\n"
                        "print('Today is', ctime(time()))\n");
