6cb6428466e948dbdb3311fe5955b4abaa112dfb
Martin Delille <martin@delille.org>
2024-02-05 11:28:39 +0100

    [llvm-core] Conan v2

--- recipes/llvm-core/all/conanfile.py
+++ recipes/llvm-core/all/conanfile.py
@@ -1,9 +1,9 @@
 from conan.errors import ConanInvalidConfiguration
 from conan import ConanFile
 from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, chdir, collect_libs, get, load, rename, replace_in_file, rm, rmdir, save
 from conan.tools.scm import Version
-from conans import CMake
 from collections import defaultdict
 import json
 import re
