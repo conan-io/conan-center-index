c87261beb253946fb335713983ca7c30d5f75f89
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-02-05 14:46:51 +0100

    Update conanfile.py

--- recipes/libsquish/all/conanfile.py
+++ recipes/libsquish/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, collect_libs, copy, export_conandata_patches, get
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 import os
 
 required_conan_version = ">=1.53.0"
