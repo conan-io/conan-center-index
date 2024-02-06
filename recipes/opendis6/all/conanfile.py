4c5032e211e49c2b53caf0ae12deaab2eeb3e50a
Cameron Howell <crhowell3@gmail.com>
2024-02-06 13:28:29 -0600

    Bumped min compiler versions

--- recipes/opendis6/all/conanfile.py
+++ recipes/opendis6/all/conanfile.py
@@ -36,9 +36,9 @@ class OpenDis6Conan(ConanFile):
         return {
             "Visual Studio": "15",
             "msvc": "191",
-            "gcc": "7",
+            "gcc": "8.5",
             "clang": "6",
-            "apple-clang": "10",
+            "apple-clang": "14",
         }
 
     def export_sources(self):
