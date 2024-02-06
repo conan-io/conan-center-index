eb0e4da37d06b8da94f1c8fae1999ecaf63a5c87
Uilian Ries <uilianries@gmail.com>
2024-02-06 12:31:50 +0100

    remove not used import
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/etcd-cpp-apiv3/all/conanfile.py
+++ recipes/etcd-cpp-apiv3/all/conanfile.py
@@ -1,5 +1,4 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.files import export_conandata_patches, get, copy, rmdir
 from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
