0a176758007198a04ce416c3f703771d8cbd936f
MelamudMichael <melamudmichael@gmail.com>
2024-02-06 19:33:42 +0000

    msg

--- recipes/up-cpp/all/test_package/test_package.cpp
+++ recipes/up-cpp/all/test_package/test_package.cpp
@@ -1,14 +1,14 @@
 #include <cstdlib>
-#include <up-cpp/uri/serializer/LongUriSerializer.h>
+//#include <up-cpp/uri/serializer/LongUriSerializer.h>
 
-using namespace uprotocol::v1;
-using namespace uprotocol::uri;
+// using namespace uprotocol::v1;
+// using namespace uprotocol::uri;
 
 int main(void) {
 
-    auto uri = LongUriSerializer::deserialize("/test.app/1/milliseconds");
-    auto uriStr = LongUriSerializer::serialize(timeUri);
+    // auto uri = LongUriSerializer::deserialize("/test.app/1/milliseconds");
+    // auto uriStr = LongUriSerializer::serialize(timeUri);
 
-    std::cout << "serialized " << uriStr << std::endl;
+    std::cout << "serialized " << std::endl;
     return EXIT_SUCCESS;
 }
