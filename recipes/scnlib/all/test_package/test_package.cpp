52526d5dad66ef846f3452c5f4e8b5d44aee3c18
toge <toge.mail@gmail.com>
2024-02-06 12:47:57 +0900

    (#22455) scnlib: add version 2.0.0, remove older version
    * scnlib: add version 2.0.0
    
    * support other options
    
    * remove 0.4, preserve header_only option
    
    * add build_requirements
    
    * drop support gcc11 in 2.0.0
    
    * drop support apple-clang 12, 13 due to std::regex_constants::multiline
    
    * update msvc version
    
    * drop support msvc on C++17
    
    * update fast_float/6.1.0

--- recipes/scnlib/all/test_package/test_package.cpp
+++ recipes/scnlib/all/test_package/test_package.cpp
@@ -1,9 +1,27 @@
-#include <scn/scn.h>
-
+#include <iostream>
 #include <string>
 
+#ifndef SCNLIB_V2
+
+#include <scn/scn.h>
+
 int main() {
-    std::string s{"conan-center-index"};
-    auto span = scn::make_span(s);
-    return 0;
+    std::string word;
+    auto result = scn::scan("Hello world", "{}", word);
+    std::cout << word << '\n';
+    std::cout << result.range_as_string() << '\n';
 }
+
+#else
+
+#include <scn/scan.h>
+
+int main() {
+    if (auto result = scn::scan<std::string>("Hello world!", "{}")) {
+        std::cout << result->value() << '\n';
+    } else {
+        std::cout << "Couldn't parse a word: " << result.error().msg() << '\n';
+    }
+}
+
+#endif
