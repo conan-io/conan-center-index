d3263b68bc853da9b568e392cb5a2b2af0364f9e
toge <toge.mail@gmail.com>
2024-02-08 03:47:07 +0900

    stringzilla: add version 3.0.0

--- recipes/stringzilla/all/test_package/test_package.cpp
+++ recipes/stringzilla/all/test_package/test_package.cpp
@@ -1,8 +1,30 @@
 #include <cstdlib>
 #include <iostream>
+#if STRINGZILLA_API >= 3
+#include <cstdint>
+#include <vector>
+#include "stringzilla/stringzilla.hpp"
+#else
 #include "stringzilla.h"
+#endif
 
-#ifdef STRINGZILLA_LESS_2_0
+#if STRINGZILLA_API >= 3
+
+namespace sz = ashvardanian::stringzilla;
+
+int main(void) {
+  sz::string haystack = "some string";
+  sz::string_view needle = sz::string_view(haystack).substr(0, 4);
+
+  auto substring_position = haystack.find(needle); // Or `rfind`
+
+  haystack.end() - haystack.begin() == haystack.size(); // Or `rbegin`, `rend`
+  haystack.find_first_of(" \w\t") == 4; // Or `find_last_of`, `find_first_not_of`, `find_last_not_of`
+  haystack.starts_with(needle) == true; // Or `ends_with`
+  haystack.remove_prefix(needle.size()); // Why is this operation in-place?!
+}
+
+#elif STRINGZILLA_API == 2
 
 int main(void) {
   // Initialize your haystack and needle
