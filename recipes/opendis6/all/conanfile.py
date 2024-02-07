57ecfa0afd5d541d36460d7360ab9be3f577e2fe
Rubén Rincón Blanco <git@rinconblanco.es>
2024-02-07 11:36:56 +0100

    Update recipes/opendis6/all/conanfile.py

--- recipes/opendis6/all/conanfile.py
+++ recipes/opendis6/all/conanfile.py
@@ -2,7 +2,7 @@ import os
 
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, export_conandata_patches, get, rmdir
+from conan.tools.files import copy, get, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
