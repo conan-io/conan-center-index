3dc803d9073abf0d903a8d2fbd5f4348ca1f1020
MelamudMichael <melamudmichael@gmail.com>
2024-02-07 03:43:06 -0500

    adding license

--- recipes/up-cpp/all/conanfile.py
+++ recipes/up-cpp/all/conanfile.py
@@ -70,6 +70,7 @@ class UpCpp(ConanFile):
         cmake.build()
 
     def package(self):
+        copy(self, "LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         cmake = CMake(self)
         cmake.install()
 
