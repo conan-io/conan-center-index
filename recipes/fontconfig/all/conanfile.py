0fd9a8bfed45ed171d717e9aebf77df19babcef8
Ahajha <ahajha@gmail.com>
2024-02-09 12:09:57 -0500

    Revert expat on fontconfig<2.13.93

--- recipes/fontconfig/all/conanfile.py
+++ recipes/fontconfig/all/conanfile.py
@@ -48,7 +48,8 @@ class FontconfigConan(ConanFile):
 
     def requirements(self):
         self.requires("freetype/2.13.2")
-        self.requires("expat/2.6.0")
+        # Expat >2.5.0 does not work for versions <2.13.93, which are exactly the versions this recipe serves.
+        self.requires("expat/2.5.0")
         if self.settings.os == "Linux":
             self.requires("util-linux-libuuid/2.39.2")
 
