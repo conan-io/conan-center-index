6f2504b30cb8f20239252f0543089f013ce85187
rturrado <68099809+rturrado@users.noreply.github.com>
2024-02-09 17:39:14 +0100

    Change package_test to use tree-gen in the expected way. - generate_tree_py should exercise the tree-gen executable. - Then example links against the tree-gen library. - Add all/test_package/CMakeLists.txt. - Implement example.cpp.
    Reorganize folder structure:
    - Move golden files to res.
    - Expect actual files to be written out to output.
    
    TODO: conan create all/conanfile.py --version 1.0.1 cannot find tree-gen package.

--- recipes/tree-gen/all/conanfile.py
+++ recipes/tree-gen/all/conanfile.py
@@ -4,6 +4,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get
 from conan.tools.scm import Version
 
@@ -22,19 +23,35 @@ class TreeGenConan(ConanFile):
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
+    package_type = "library"
     options = {
+        "shared": [True, False],
         "fPIC": [True, False],
-        "asan_enabled": [True, False],
-        "build_tests": [True, False]
+        "asan_enabled": [True, False]
     }
     default_options = {
+        "shared": False,
         "fPIC": True,
-        "asan_enabled": False,
-        "build_tests": False
+        "asan_enabled": False
     }
 
-    exports = "version.py", "include/version.hpp"
-    exports_sources = "CMakeLists.txt", "cmake/*", "include/*", "src/*", "test/*"
+    @property
+    def _should_build_test(self):
+        return not self.conf.get("tools.build:skip_test", default=True, check_type=bool)
+
+    @property
+    def _min_cpp_std(self):
+        return 17
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "gcc": "10",
+            "clang": "13",
+            "apple-clang": "14",
+            "Visual Studio": "16",
+            "msvc": "192"
+        }
 
     def build_requirements(self):
         self.tool_requires("m4/1.4.19")
@@ -48,12 +65,16 @@ class TreeGenConan(ConanFile):
     def requirements(self):
         self.requires("fmt/10.1.1")
         self.requires("range-v3/0.12.0")
-        if self.options.build_tests:
+        if self._should_build_test:
             self.requires("gtest/1.14.0")
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -66,8 +87,10 @@ class TreeGenConan(ConanFile):
         deps.generate()
         tc = CMakeToolchain(self)
         tc.variables["ASAN_ENABLED"] = self.options.asan_enabled
-        tc.variables["TREE_GEN_BUILD_TESTS"] = self.options.build_tests
+        tc.variables["TREE_GEN_BUILD_TESTS"] = self._should_build_test
         tc.generate()
+        env = VirtualBuildEnv(self)
+        env.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -75,26 +98,23 @@ class TreeGenConan(ConanFile):
         cmake.build()
 
     def validate(self):
-        compiler = self.settings.compiler
+        compiler = str(self.settings.compiler)
         version = Version(self.settings.compiler.version)
-        if compiler == "apple-clang":
-            if version < "14":
-                raise ConanInvalidConfiguration("tree-gen requires at least apple-clang++ 14")
-        elif compiler == "clang":
-            if version < "13":
-                raise ConanInvalidConfiguration("tree-gen requires at least clang++ 13")
-        elif compiler == "gcc":
-            if version < "10.0":
-                raise ConanInvalidConfiguration("tree-gen requires at least g++ 10.0")
-        elif compiler == "msvc":
-            if version < "19.29":
-                raise ConanInvalidConfiguration("tree-gen requires at least msvc 19.29")
+        if compiler in self._minimum_compilers_version:
+            min_version = self._minimum_compilers_version[compiler]
+            if version < min_version:
+                raise ConanInvalidConfiguration(f"tree-gen requires at least {compiler} {min_version}")
         else:
             raise ConanInvalidConfiguration("Unsupported compiler")
-        if compiler.get_safe("cppstd"):
-            check_min_cppstd(self, "17")
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cpp_std)
 
     def package(self):
         copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.cmake", src=os.path.join(self.source_folder, "cmake"),
+             dst=os.path.join(self.package_folder, "cmake"))
         cmake = CMake(self)
         cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["tree-gen"]
--- recipes/tree-gen/all/test_package/conanfile.py
+++ recipes/tree-gen/all/test_package/conanfile.py
@@ -1,40 +1,28 @@
-import filecmp
 import os
 
 from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import cross_building
 
 
 class TreeGenTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "VirtualBuildEnv"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv", "VirtualRunEnv"
+    apply_env = False
     test_type = "explicit"
 
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
 
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        output_files_folder = os.path.dirname(__file__)
-        tree_file_path = os.path.join(output_files_folder, "directory.tree")
-        actual_header_file_path = os.path.join(output_files_folder, "directory.actual.hpp")
-        actual_source_file_path = os.path.join(output_files_folder, "directory.actual.cpp")
-        actual_python_file_path = os.path.join(output_files_folder, "directory.actual.py")
-        golden_header_file_path = os.path.join(output_files_folder, "directory.golden.hpp")
-        golden_source_file_path = os.path.join(output_files_folder, "directory.golden.cpp")
-        golden_python_file_path = os.path.join(output_files_folder, "directory.golden.py")
-
-        self.run(f"tree-gen {tree_file_path} {actual_header_file_path} {actual_source_file_path} {actual_python_file_path}")
-
-        header_cmp_result = filecmp.cmp(actual_header_file_path, golden_header_file_path, shallow=False)
-        source_cmp_result = filecmp.cmp(actual_source_file_path, golden_source_file_path, shallow=False)
-        python_cmp_result = filecmp.cmp(actual_python_file_path, golden_python_file_path, shallow=False)
-        if not header_cmp_result:
-            self.output.error(f"Error: header files '{actual_header_file_path}' and '{golden_header_file_path}' differ")
-            return False
-        if not source_cmp_result:
-            self.output.error(f"Error: source files '{actual_source_file_path}' and '{golden_source_file_path}' differ")
-            return False
-        if not python_cmp_result:
-            self.output.error(f"Error: python files '{actual_python_file_path}' and '{golden_python_file_path}' differ")
-            return False
-        self.output.error(f"Success: output header, source, and python files at '{output_files_folder}' are OK")
-        return True
+        if not cross_building(self):
+            cmd = os.path.join(self.cpp.build.bindirs[0], "example")
+            self.run(cmd, env="conanrun")
