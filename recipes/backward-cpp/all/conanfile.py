f85d052baff67db68a96d2a9441c8520ea188b41
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-02-11 12:54:18 -0600

    Use libdwarf 0.9.1

--- recipes/backward-cpp/all/conanfile.py
+++ recipes/backward-cpp/all/conanfile.py
@@ -81,7 +81,7 @@ class BackwardCppConan(ConanFile):
             if self._has_stack_walking("libunwind"):
                 self.requires("libunwind/1.7.2", transitive_headers=True)
             if self._has_stack_details("dwarf"):
-                self.requires("libdwarf/20191104", transitive_headers=True, transitive_libs=True)
+                self.requires("libdwarf/0.9.1", transitive_headers=True, transitive_libs=True)
                 self.requires("libelf/0.8.13")
             if self._has_stack_details("dw"):
                 self.requires("elfutils/0.190", transitive_headers=True)
