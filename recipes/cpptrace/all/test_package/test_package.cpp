c10c85bf25da48ce3a84a6c3b7e8a3218c74c0b0
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-02-11 12:02:24 -0600

    rc2

--- recipes/cpptrace/all/test_package/test_package.cpp
+++ recipes/cpptrace/all/test_package/test_package.cpp
@@ -1,9 +1,13 @@
 #include <cstdlib>
 #include <iostream>
+#include <limits.h>
 #include <cpptrace/cpptrace.hpp>
+#include <ctrace/ctrace.h>
 
 int main() {
     cpptrace::generate_trace().print();
+    
+    ctrace_stacktrace c_trace = ctrace_generate_trace(0, SIZE_MAX);
 
     return EXIT_SUCCESS;
 }
