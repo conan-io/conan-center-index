b038d7258d90c3d2a74ba54dcf428837c69fc0dc
toge <toge.mail@gmail.com>
2024-02-12 16:16:42 +0900

    drop support gcc 5

--- recipes/bezier/all/conanfile.py
+++ recipes/bezier/all/conanfile.py
@@ -1,7 +1,9 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
+from conan.tools.files import get, copy
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
 
@@ -23,6 +25,12 @@ class BezierConan(ConanFile):
     def _min_cppstd(self):
         return 11
 
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "6",
+        }
+
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -32,6 +40,11 @@ class BezierConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
