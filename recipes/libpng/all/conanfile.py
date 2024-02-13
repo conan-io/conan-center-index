c0271eda6d649a4d9c290702deb87c8d80cf4b3e
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-02-13 15:54:42 +0000

    Add comment to conanfile

--- recipes/libpng/all/conanfile.py
+++ recipes/libpng/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
-import os
+import os #comment
 
 required_conan_version = ">=1.53.0"
 
