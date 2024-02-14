a21fd195405224af86d4652071fdfef4bd4cfcdf
Martin Delille <martin@delille.org>
2024-02-14 14:57:01 +0100

    Apply suggestions from code review
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/mocknetworkaccessmanager/all/conanfile.py
+++ recipes/mocknetworkaccessmanager/all/conanfile.py
@@ -40,7 +40,7 @@ class MockNetworkAccessManagerConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, "17")
+            check_min_cppstd(self, "11")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
