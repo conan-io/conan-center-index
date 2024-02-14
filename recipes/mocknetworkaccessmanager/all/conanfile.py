a05670c222ac909883c45cdb80bcbe6002154aec
Martin Delille <martin@delille.org>
2024-02-14 18:50:26 +0100

    Update recipes/mocknetworkaccessmanager/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/mocknetworkaccessmanager/all/conanfile.py
+++ recipes/mocknetworkaccessmanager/all/conanfile.py
@@ -1,5 +1,4 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
