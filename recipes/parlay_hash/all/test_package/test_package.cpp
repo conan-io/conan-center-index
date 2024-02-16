a9a1a20c58b99632b790da75ec66172c5f460956
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-02-16 11:29:09 +0100

    fix test_package

--- recipes/parlay_hash/all/test_package/test_package.cpp
+++ recipes/parlay_hash/all/test_package/test_package.cpp
@@ -1,13 +1,22 @@
+#include <iostream>
+#include <string>
 #include <parlay/unordered_map.h>
 
-#include <iostream>
+using K = std::string;
+using V = unsigned long;
+using map_type = parlay::unordered_map<K,V>;
 
-auto main() -> int {
-    auto map = parlay::unordered_map<int, std::string>();
-    map[123] = "hello";
-    map[987] = "world!";
+int main() {
+  map_type my_map(100);
+  my_map.insert("sue", 1);
+  my_map.insert("sam", 5);
 
-    for (auto const& [key, val] : map) {
-        std::cout << key << " => " << val << std::endl;
-    }
+  std::cout << "value before increment: " << *my_map.find("sue") << std::endl;
+  auto increment = [] (std::optional<V> v) -> V {return v.has_value() ? 1 + *v : 1;};
+  my_map.upsert("sue", increment);
+  std::cout << "value after increment: " << *my_map.find("sue") << std::endl;
+
+  std::cout << "size before remove: " << my_map.size() << std::endl;
+  my_map.remove("sue");
+  std::cout << "size after remove: " << my_map.size() << std::endl;
 }
