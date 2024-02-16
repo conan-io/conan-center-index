fb7a2082b9b2fb930fe6aa87901853f0b1ee4716
toge <toge.mail@gmail.com>
2024-02-17 00:22:05 +0900

    cctz: add version 2.4

--- recipes/cctz/all/conanfile.py
+++ recipes/cctz/all/conanfile.py
@@ -10,12 +10,11 @@ required_conan_version = ">=1.53.0"
 
 class CCTZConan(ConanFile):
     name = "cctz"
+    description = "C++ library for translating between absolute and civil times"
+    license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/google/cctz"
-    description = "C++ library for translating between absolute and civil times"
     topics = ("time", "timezones")
-    license = "Apache-2.0"
-
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
