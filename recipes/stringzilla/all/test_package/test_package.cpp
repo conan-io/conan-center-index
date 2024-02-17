666329104c5f80253fecb7ea1ba90b87b1bc1815
toge <toge.mail@gmail.com>
2024-02-17 15:24:22 +0900

    simplify test_package.cpp

--- recipes/stringzilla/all/test_package/test_package.cpp
+++ recipes/stringzilla/all/test_package/test_package.cpp
@@ -1,31 +1,34 @@
 #include <cstdlib>
 #include <iostream>
-#if STRINGZILLA_API >= 3
-#include <array>
-#include <cstdint>
-#include <vector>
-#include "stringzilla/stringzilla.hpp"
-#else
+
+#if STRINGZILLA_API == 1
+
 #include "stringzilla.h"
-#endif
-
-#if STRINGZILLA_API >= 3
-
-namespace sz = ashvardanian::stringzilla;
 
 int main(void) {
-  sz::string haystack = "some string";
-  sz::string_view needle = sz::string_view(haystack).substr(0, 4);
+  // Initialize your haystack and needle
+  strzl_haystack_t haystack = {
+    "Fastest string sort, search, split, "
+    "and shuffle for long strings and multi-gigabyte files in Python and C, "
+    "leveraging SIMD with Arm Neon and x86 AVX2 & AVX-512 intrinsics.",
+    171};
+  strzl_needle_t needle = {"SIMD", 4};
 
-  auto substring_position = haystack.find(needle); // Or `rfind`
+  // Count occurrences of a character like a boss 😎
+  size_t count = strzl_naive_count_char(haystack, 'a');
 
-  haystack.end() - haystack.begin() == haystack.size(); // Or `rbegin`, `rend`
-  haystack.find_first_of(" \w\t") == 4; // Or `find_last_of`, `find_first_not_of`, `find_last_not_of`
-  haystack.starts_with(needle) == true; // Or `ends_with`
-  haystack.remove_prefix(needle.size()); // Why is this operation in-place?!
+  // Find a character like you're searching for treasure 🏴‍☠️
+  size_t position = strzl_naive_find_char(haystack, 'a');
+
+  // Find a substring like it's Waldo 🕵️‍♂️
+  size_t substring_position = strzl_naive_find_substr(haystack, needle);
+
+  return EXIT_SUCCESS;
 }
 
 #elif STRINGZILLA_API == 2
+
+#include "stringzilla.h"
 
 int main(void) {
   // Initialize your haystack and needle
@@ -51,23 +54,23 @@ int main(void) {
 
 #else
 
+#include <array>
+#include <cstdint>
+#include <vector>
+#include "stringzilla/stringzilla.hpp"
+
+namespace sz = ashvardanian::stringzilla;
+
 int main(void) {
-  // Initialize your haystack and needle
-  strzl_haystack_t haystack = {
-    "Fastest string sort, search, split, "
-    "and shuffle for long strings and multi-gigabyte files in Python and C, "
-    "leveraging SIMD with Arm Neon and x86 AVX2 & AVX-512 intrinsics.",
-    171};
-  strzl_needle_t needle = {"SIMD", 4};
+  sz::string haystack = "some string";
+  sz::string_view needle = sz::string_view(haystack).substr(0, 4);
 
-  // Count occurrences of a character like a boss 😎
-  size_t count = strzl_naive_count_char(haystack, 'a');
+  auto substring_position = haystack.find(needle); // Or `rfind`
 
-  // Find a character like you're searching for treasure 🏴‍☠️
-  size_t position = strzl_naive_find_char(haystack, 'a');
-
-  // Find a substring like it's Waldo 🕵️‍♂️
-  size_t substring_position = strzl_naive_find_substr(haystack, needle);
+  haystack.end() - haystack.begin() == haystack.size(); // Or `rbegin`, `rend`
+  haystack.find_first_of(" \w\t") == 4; // Or `find_last_of`, `find_first_not_of`, `find_last_not_of`
+  haystack.starts_with(needle) == true; // Or `ends_with`
+  haystack.remove_prefix(needle.size()); // Why is this operation in-place?!
 
   return EXIT_SUCCESS;
 }
