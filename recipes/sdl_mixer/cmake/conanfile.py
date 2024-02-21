ca61538dd7e13dd9819b22f0289ad9b2b992ae2c
Martin Valgur <martin.valgur@gmail.com>
2024-02-21 12:08:44 +0200

    sdl_mixer: revert flac version bump

--- recipes/sdl_mixer/cmake/conanfile.py
+++ recipes/sdl_mixer/cmake/conanfile.py
@@ -78,7 +78,7 @@ class SDLMixerConan(ConanFile):
     def requirements(self):
         self.requires("sdl/2.28.5", transitive_headers=True, transitive_libs=True)
         if self.options.flac:
-            self.requires("flac/1.4.3")
+            self.requires("flac/1.4.2")
         elif self.options.gme:
             # TODO: not available on CCI
             self.requires("gme/x.y.z")
