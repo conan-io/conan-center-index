8a9e9a27eb18a4f380b2d9143cb3c89249fa680a
toge <toge.mail@gmail.com>
2024-02-23 03:17:50 +0900

    miniscript: add version 1.6.2

--- recipes/miniscript/all/conanfile.py
+++ recipes/miniscript/all/conanfile.py
@@ -1,7 +1,9 @@
 from conan import ConanFile
-from conan.tools.files import get, copy
+from conan.tools.files import get, copy, apply_conandata_patches, export_conandata_patches
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.53.0"
@@ -24,6 +26,23 @@ class MiniscriptConan(ConanFile):
     }
     exports_sources = ["CMakeLists.txt"]
 
+    @property
+    def _min_cppstd(self):
+        return 14
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "6",
+            "clang": "5",
+            "apple-clang": "10",
+            "Visual Studio": "15",
+            "msvc": "191",
+        }	
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
@@ -35,6 +54,17 @@ class MiniscriptConan(ConanFile):
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    def validate(self):
+        if Version(self.version) < "1.6.2":
+            return
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
@@ -44,8 +74,12 @@ class MiniscriptConan(ConanFile):
         tc.generate()
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
-        cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
+        if Version(self.version) >= "1.6.2":
+            cmake.configure()
+        else:
+            cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
         cmake.build()
 
     def package(self):
@@ -54,7 +88,7 @@ class MiniscriptConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["miniscript"]
+        self.cpp_info.libs = ["miniscript-cpp"]
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.append("m")
             self.cpp_info.system_libs.append("pthread")
