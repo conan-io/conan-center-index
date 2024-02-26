33994398c11f6b2802bf3f7e494986b24749c35c
Tom Deblauwe <tom.deblauwe@eeve.com>
2024-02-26 09:37:11 +0100

    Bases the package file on cmake_package template.

--- recipes/libq/all/conanfile.py
+++ recipes/libq/all/conanfile.py
@@ -1,58 +1,113 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake
-from conan.tools.files import collect_libs
-from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.files import get
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
+from conan.tools.microsoft import check_min_vs, is_msvc, is_msvc_static_runtime
+from conan.tools.scm import Version
+import os
 
-required_conan_version = ">=2.0.0"
+required_conan_version = ">=1.53.0"
 
 class libqConan(ConanFile):
     name = "libq"
     description = "A platform-independent promise library for C++, implementing asynchronous continuations."
-    license = "Apache license 2.0"
-    url = "https://github.com/grantila/q"
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/grantila/q"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
     options = {
-        "shared"            : [True, False],
-        "qtest"            : [True, False]
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "qtest": [True, False]
     }
     default_options = {
-        "shared"            : False,
-        "qtest"            : False
+        "shared": False,
+        "fPIC": True,
+        "qtest": False
     }
+
+    @property
+    def _min_cppstd(self):
+        return 17
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "apple-clang": "10",
+            "clang": "7",
+            "gcc": "7",
+            "msvc": "191",
+            "Visual Studio": "15",
+        }
+    
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def validate(self):
         if self.settings.compiler.cppstd:
-            check_min_cppstd(self, 17)
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
+        if is_msvc(self) and self.options.shared:
+            raise ConanInvalidConfiguration(f"{self.ref} can not be built as shared on Visual Studio and msvc.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def optionBool(self, b):
-        if b:
-            return "ON"
-        else:
-            return "OFF"
-
-    def parseOptionsToCMake(self):
-        cmakeOpts = {
-            "q_BUILD_TESTS" : "OFF"
-        }
-
-        cmakeOpts["BUILD_SHARED_LIBS"] = self.optionBool(self.options.shared)
-        cmakeOpts["q_BUILD_QTEST"] = self.optionBool(self.options.qtest)
-
-        return cmakeOpts
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.variables["q_BUILD_TESTS"] = self.options.qtest
+        if is_msvc(self):
+            tc.variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
+        
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(variables=self.parseOptionsToCMake())
+        cmake.configure()
         cmake.build()
+
+    def package(self):
+        copy(self, "LICENSE.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        fix_apple_shared_install_name(self)
+
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "lib"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
-        self.cpp_info.libs = collect_libs(self)
+        self.cpp_info.libs = ["package_lib"]
+
+        self.cpp_info.set_property("cmake_module_file_name", "LIBQ")
+        self.cpp_info.set_property("cmake_module_target_name", "LIBQ::LIBQ")
+        self.cpp_info.set_property("cmake_file_name", "libq")
+        self.cpp_info.set_property("cmake_target_name", "libq::libq")
+
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.append("m")
+            self.cpp_info.system_libs.append("pthread")
+            self.cpp_info.system_libs.append("dl")
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.filenames["cmake_find_package"] = "LIBQ"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "libq"
+        self.cpp_info.names["cmake_find_package"] = "LIBQ"
+        self.cpp_info.names["cmake_find_package_multi"] = "libq"
