707e83b60544134946a98a32fe3d0ba638ec22f4
Gang Wu <ustcwg@gmail.com>
2024-02-27 18:04:37 +0800

    use generator expression

--- recipes/orc/all/ConanThirdpartyToolchain.cmake
+++ recipes/orc/all/ConanThirdpartyToolchain.cmake
@@ -5,8 +5,7 @@ find_package (Snappy REQUIRED)
 
 add_library (orc_snappy INTERFACE)
 add_library (orc::snappy ALIAS orc_snappy)
-target_link_libraries(orc_snappy INTERFACE ${Snappy_LIBRARIES})
-target_include_directories (orc_snappy SYSTEM INTERFACE ${Snappy_INCLUDE_DIR})
+target_link_libraries(orc_snappy INTERFACE Snappy::snappy)
 
 # ----------------------------------------------------------------------
 # ZLIB
@@ -15,8 +14,7 @@ find_package (ZLIB REQUIRED)
 
 add_library (orc_zlib INTERFACE)
 add_library (orc::zlib ALIAS orc_zlib)
-target_link_libraries (orc_zlib INTERFACE ${ZLIB_LIBRARIES})
-target_include_directories (orc_zlib SYSTEM INTERFACE ${ZLIB_INCLUDE_DIR})
+target_link_libraries (orc_zlib INTERFACE ZLIB::ZLIB)
 
 # ----------------------------------------------------------------------
 # Zstd
@@ -25,8 +23,10 @@ find_package (ZSTD REQUIRED)
 
 add_library (orc_zstd INTERFACE)
 add_library (orc::zstd ALIAS orc_zstd)
-target_link_libraries (orc_zstd INTERFACE ${zstd_LIBRARIES})
-target_include_directories (orc_zstd SYSTEM INTERFACE ${zstd_INCLUDE_DIR})
+target_link_libraries (orc_zstd INTERFACE
+  $<TARGET_NAME_IF_EXISTS:zstd::libzstd_static>
+  $<TARGET_NAME_IF_EXISTS:zstd::libzstd_shared>
+)
 
 # ----------------------------------------------------------------------
 # LZ4
@@ -35,8 +35,10 @@ find_package (LZ4 REQUIRED)
 
 add_library (orc_lz4 INTERFACE)
 add_library (orc::lz4 ALIAS orc_lz4)
-target_link_libraries (orc_lz4 INTERFACE ${lz4_LIBRARIES})
-target_include_directories (orc_lz4 SYSTEM INTERFACE ${lz4_INCLUDE_DIR})
+target_link_libraries (orc_lz4 INTERFACE
+  $<TARGET_NAME_IF_EXISTS:LZ4::lz4_shared>
+  $<TARGET_NAME_IF_EXISTS:LZ4::lz4_static>
+)
 
 # ----------------------------------------------------------------------
 # Protobuf
@@ -45,5 +47,4 @@ find_package (Protobuf REQUIRED)
 
 add_library (orc_protobuf INTERFACE)
 add_library (orc::protobuf ALIAS orc_protobuf)
-target_link_libraries (orc_protobuf INTERFACE ${protobuf_LIBRARIES})
-target_include_directories (orc_protobuf SYSTEM INTERFACE ${protobuf_INCLUDE_DIR})
+target_link_libraries (orc_protobuf INTERFACE protobuf::protobuf)
