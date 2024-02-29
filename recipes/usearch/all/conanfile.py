6cfa04d372a455c27d753d6fb397191210571825
Martin Valgur <martin.valgur@gmail.com>
2024-02-29 10:50:00 +0200

    usearch: require GCC 6+

--- recipes/usearch/all/conanfile.py
+++ recipes/usearch/all/conanfile.py
@@ -1,10 +1,11 @@
 import os
 
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import get, copy
 from conan.tools.layout import basic_layout
-
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
 
@@ -24,6 +25,12 @@ class USearchConan(ConanFile):
     def _min_cppstd(self):
         return 11
 
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "6",
+        }
+
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -36,6 +43,11 @@ class USearchConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
