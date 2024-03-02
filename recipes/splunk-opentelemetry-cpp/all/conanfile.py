43e27d86439bf720a04b639b1e3dd8cb17e4f4f8
Martin Valgur <martin.valgur@gmail.com>
2024-03-02 18:57:25 +0200

    bump nlohmann_json

--- recipes/splunk-opentelemetry-cpp/all/conanfile.py
+++ recipes/splunk-opentelemetry-cpp/all/conanfile.py
@@ -47,7 +47,7 @@ class SplunkOpentelemetryConan(ConanFile):
     def requirements(self):
         self.requires("opentelemetry-cpp/1.9.1", transitive_headers=True) # v1.12 is not compatible
         self.requires("grpc/1.54.3")
-        self.requires("nlohmann_json/3.11.2")
+        self.requires("nlohmann_json/3.11.3")
         if self.options.build_jaeger_exporter:
             self.requires("thrift/0.17.0")
             self.requires("libcurl/[>=7.78.0 <9]")
