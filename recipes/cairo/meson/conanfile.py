0fc00924125f1a13e6b2615c4920cd52a0cd1352
Matthieu Darbois <mayeut@users.noreply.github.com>
2024-03-04 10:50:23 +0100

    Update recipes/cairo/meson/conanfile.py
    Co-authored-by: Martin Valgur <martin.valgur@gmail.com>

--- recipes/cairo/meson/conanfile.py
+++ recipes/cairo/meson/conanfile.py
@@ -88,7 +88,7 @@ class CairoConan(ConanFile):
     def requirements(self):
         self.requires("pixman/0.43.0")
         if self.options.with_zlib and self.options.with_png:
-            self.requires("expat/2.5.0")
+            self.requires("expat/2.6.0")
         if self.options.with_lzo:
             self.requires("lzo/2.10")
         if self.options.with_zlib:
