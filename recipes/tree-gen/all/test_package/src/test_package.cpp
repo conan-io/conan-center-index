16834a6f2a55ab2b2f90a90db27ffa7cc02bc3c6
rturrado <68099809+rturrado@users.noreply.github.com>
2024-03-04 10:12:19 +0100

    Change apple-clang compiler minimum version back to 14. There seems to be a bug in the Conan CI system when generating ARM binaries.
    Update to tree-gen version 1.0.6.

--- recipes/tree-gen/all/test_package/src/test_package.cpp
+++ recipes/tree-gen/all/test_package/src/test_package.cpp
@@ -2,15 +2,12 @@
 #include "utils.hpp"
 #include "version.hpp"  // tree-gen version
 
-#include <cstdio>
 #include <iostream>
-#include <sstream>  // ostringstream
-#include <stdexcept>
 
 
 void print_tree_gen_version() {
-    std::printf("tree-gen version: %s\n", TREE_GEN_VERSION);
-    std::printf("tree-gen release year: %s\n", TREE_GEN_RELEASE_YEAR);
+    std::cout << "tree-gen version: " << get_version() << "\n";
+    std::cout << "tree-gen release year: " << get_release_year() << "\n";
 }
 
 int main() {
