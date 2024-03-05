4a1d57b5a8eb1808d5a8a11332f802a5900076f9
Martin Delille <martin@delille.org>
2024-03-05 07:40:18 +0100

    Remove whenGet() from test_package.cpp

--- recipes/mocknetworkaccessmanager/all/test_package/test_package.cpp
+++ recipes/mocknetworkaccessmanager/all/test_package/test_package.cpp
@@ -3,10 +3,5 @@
 int main(void) {
     MockNetworkAccess::Manager< QNetworkAccessManager > mnam;
 
-    mnam.whenGet( QUrl( "http://example.com/hello" ) )
-        .has( MockNetworkAccess::Predicates::HeaderMatching( QNetworkRequest::UserAgentHeader,
-                    QRegularExpression( ".*MyNetworkClient/.*" ) ) )
-        .reply().withBody( QJsonDocument::fromJson( "{\"hello\":\"world\"}" ) );
-
     return 0;
 }
