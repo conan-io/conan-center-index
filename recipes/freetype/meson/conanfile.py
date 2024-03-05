4dda8dfd66a97468abb8a15d9073cccde8ab08c6
Jordan Williams <jordan@jwillikers.com>
2024-03-05 08:13:44 -0600

    Set pkg-config version appropriately

--- recipes/freetype/meson/conanfile.py
+++ recipes/freetype/meson/conanfile.py
@@ -222,8 +222,7 @@ class FreetypeConan(ConanFile):
 
         libtool_version = load(self, self._libtool_version_txt).strip()
         self.conf_info.define("user.freetype:libtool_version", libtool_version)
-        # FIXME: need to do override the pkg_config version (pkg_config_custom_content does not work)
-        # self.cpp_info.version["pkg_config"] = pkg_config_version
+        self.cpp_info.set_property("system_package_version", libtool_version)
 
         # TODO: to remove in conan v2 once cmake_find_package* & pkg_config generators removed
         self.cpp_info.filenames["cmake_find_package"] = "Freetype"
