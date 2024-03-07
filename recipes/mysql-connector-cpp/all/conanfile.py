057ae9c1461c2c6df330ecc34b5cf9311cf74f18
Samuel Azcona <samuaz@gmail.com>
2024-03-07 02:44:23 +0100

    Update recipes/mysql-connector-cpp/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/mysql-connector-cpp/all/conanfile.py
+++ recipes/mysql-connector-cpp/all/conanfile.py
@@ -56,7 +56,6 @@ class MysqlConnectorCPPRecipe(ConanFile):
     def validate(self):
         if self.settings.compiler.cppstd:
             check_min_cppstd(self, self._min_cppstd)
-        check_min_vs(self, 192)
         if not is_msvc(self):
             minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
             if minimum_version and Version(self.settings.compiler.version) < minimum_version:
