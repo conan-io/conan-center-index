3916a164bde21d5f87fb387dd3e15496713ad741
Ahajha <ahajha@gmail.com>
2024-03-15 04:22:54 -0400

    Bump xz_utils

--- recipes/cpython/all/conanfile.py
+++ recipes/cpython/all/conanfile.py
@@ -176,7 +176,7 @@ class CPythonConan(ConanFile):
         if self.options.get_safe("with_bsddb", False):
             self.requires("libdb/5.3.28")
         if self.options.get_safe("with_lzma", False):
-            self.requires("xz_utils/5.6.0")
+            self.requires("xz_utils/5.6.1")
 
     def package_id(self):
         del self.info.options.env_vars
