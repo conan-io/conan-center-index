1296085d11b8a3a3b4f7bce9bd8e5c164027ec53
toge <toge.mail@gmail.com>
2024-03-16 23:14:22 +0900

    drop apple-clang/13

--- recipes/iguana/all/conanfile.py
+++ recipes/iguana/all/conanfile.py
@@ -28,7 +28,7 @@ class IguanaConan(ConanFile):
         return {
             "gcc": "11",
             "clang": "12",
-            "apple-clang": "13",
+            "apple-clang": "14",
             "Visual Studio": "16",
             "msvc": "192",
         }
