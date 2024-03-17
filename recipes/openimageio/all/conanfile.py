154d1041519ef8092266712f7e3e87c3a742618d
mayeut <mayeut@users.noreply.github.com>
2024-03-17 12:01:07 +0100

    openimageio: use libpng version range

--- recipes/openimageio/all/conanfile.py
+++ recipes/openimageio/all/conanfile.py
@@ -97,7 +97,7 @@ class OpenImageIOConan(ConanFile):
 
         # Optional libraries
         if self.options.with_libpng:
-            self.requires("libpng/1.6.42")
+            self.requires("libpng/[>=1.6 <2]")
         if self.options.with_freetype:
             self.requires("freetype/2.13.2")
         if self.options.with_hdf5:
