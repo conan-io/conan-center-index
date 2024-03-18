e254c573558e732b7dc85a9bc4e6762ea9a3fd58
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-03-18 16:09:16 +0100

    simplify test

--- recipes/qcustomplot/all/test_package/test_package.cpp
+++ recipes/qcustomplot/all/test_package/test_package.cpp
@@ -1,27 +1,11 @@
 #include <qcustomplot.h>
+#include <iostream>
 
-#include <QApplication>
-#include <QMainWindow>
-#include <QVector>
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QMainWindow window;
-
-    QCustomPlot customPlot;
-    window.setCentralWidget(&customPlot);
-
-    QVector<double> x(101), y(101);
-    for (int i = 0; i < 101; ++i) {
-        x[i] = i / 50.0 - 1;
-        y[i] = x[i] * x[i];
-    }
-    customPlot.addGraph();
-    customPlot.graph(0)->setData(x, y);
-    customPlot.xAxis->setLabel("x");
-    customPlot.yAxis->setLabel("y");
-    customPlot.rescaleAxes();
-
-    window.setGeometry(100, 100, 500, 400);
+    QCPVector2D vec(1,2);
+    vec = vec.normalized();
+    vec *= 2;
+    cout << vec.length();
     return 0;
 }
