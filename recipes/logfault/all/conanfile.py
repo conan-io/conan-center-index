9677ed62b8a3c950c5f6e1f17aa831765669fc7b
Jarle Aase <jarle@jgaa.com>
2024-03-18 18:24:47 +0200

    Update recipes/logfault/all/conanfile.py
    Co-authored-by: Rubén Rincón Blanco <git@rinconblanco.es>

--- recipes/logfault/all/conanfile.py
+++ recipes/logfault/all/conanfile.py
@@ -77,9 +77,6 @@ class PackageConan(ConanFile):
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
 
-        # In case this library does not work in some another configuration, it should be validated here too
-        if self.settings.os == "Windows":
-            raise ConanInvalidConfiguration(f"{self.ref} can not be used on Windows.")
 
     def source(self):
         # Download source package and extract to source folder
