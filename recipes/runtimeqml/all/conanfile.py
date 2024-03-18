9da3e7c5c0a6d2c4af75ad71c4fdecb45cd1403b
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-03-18 12:38:51 +0100

    runtimeqml: bump qt

--- recipes/runtimeqml/all/conanfile.py
+++ recipes/runtimeqml/all/conanfile.py
@@ -60,9 +60,9 @@ class RuntimeQml(ConanFile):
 
     def requirements(self):
         if Version(self.version) <= "cci.20211220":
-            self.requires("qt/5.15.5")
+            self.requires("qt/5.15.13")
         else:
-            self.requires("qt/6.3.1")
+            self.requires("qt/6.6.2")
 
     def validate(self):
         if self.info.settings.compiler.cppstd:
