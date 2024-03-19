3304b848a83da5606df08c92f8b473ce60271a4a
Martin Valgur <martin.valgur@gmail.com>
2024-03-19 19:09:17 +0200

    gici: require GCC v6

--- recipes/gici/all/conanfile.py
+++ recipes/gici/all/conanfile.py
@@ -1,9 +1,11 @@
 import os
 
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd, valid_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
 
@@ -32,6 +34,13 @@ class GiciConan(ConanFile):
     @property
     def _min_cppstd(self):
         return 11
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            # For atomic_uint64_t
+            "gcc": "6",
+        }
 
     def export_sources(self):
         export_conandata_patches(self)
@@ -69,6 +78,11 @@ class GiciConan(ConanFile):
     def validate(self):
         if self.settings.compiler.cppstd:
             check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
