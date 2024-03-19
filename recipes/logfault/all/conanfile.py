bce3c08c24b3ec1ce0683aa04a8cabdd4d0c3f2b
Jarle Aase <jarle@jgaa.com>
2024-03-19 18:26:06 +0200

    Update recipes/logfault/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/logfault/all/conanfile.py
+++ recipes/logfault/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
 from conan.tools.cmake import CMake, CMakeToolchain
