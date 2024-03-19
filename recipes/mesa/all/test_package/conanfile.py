ef8876bdc361e770f8dbce71637548fcd8f0ef2f
Jordan Williams <jordan@jwillikers.com>
2024-03-19 10:37:11 -0500

    Disable EGL for test package on Windows

--- recipes/mesa/all/test_package/conanfile.py
+++ recipes/mesa/all/test_package/conanfile.py
@@ -24,7 +24,7 @@ class TestPackageConan(ConanFile):
 
     def generate(self):
         tc = MesonToolchain(self)
-        tc.project_options["egl"] = "enabled" if self.dependencies[self.tested_reference_str].options.get_safe("egl") else "disabled"
+        tc.project_options["egl"] = "enabled" if self.dependencies[self.tested_reference_str].options.get_safe("egl") and not self.settings.os == "Windows" else "disabled"
         tc.project_options["gbm"] = "enabled" if self.dependencies[self.tested_reference_str].options.get_safe("gbm") else "disabled"
         tc.project_options["glvnd"] = "enabled" if self.dependencies[self.tested_reference_str].options.get_safe("with_libglvnd") else "disabled"
         tc.project_options["osmesa"] = "enabled" if self.dependencies[self.tested_reference_str].options.get_safe("osmesa") else "disabled"
