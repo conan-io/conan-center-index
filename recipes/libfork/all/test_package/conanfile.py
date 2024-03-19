69213f199f30e0472da2c1e686d02f9ab4865fa2
Uilian Ries <uilianries@gmail.com>
2024-03-19 15:58:00 +0100

    update recipe to 3.7.2
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libfork/all/test_package/conanfile.py
+++ recipes/libfork/all/test_package/conanfile.py
@@ -1,21 +1,21 @@
 import os
 
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.build import can_run
 
 
 class LibforkPackageTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
 
     def requirements(self):
-        # self.requires("libfork/2.1.1")
         self.requires(self.tested_reference_str)
 
     def layout(self):
         cmake_layout(self)
-        
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
@@ -23,5 +23,5 @@ class LibforkPackageTestConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            cmd = os.path.join(self.cpp.build.bindir, "libfork_test_package")
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(cmd, env="conanrun")
--- recipes/libfork/all/test_package/test_package.cpp
+++ recipes/libfork/all/test_package/test_package.cpp
@@ -1,34 +1,24 @@
+#include <cstdlib>
 #include <iostream>
-#include <thread>
 
-#include "libfork/task.hpp"
-#include "libfork/schedule/busy_pool.hpp"
+#include "libfork.hpp"
 
-
-/// Short-hand for a task that uses the busy_pool scheduler.
-template <typename T>
-using pool_task = lf::basic_task<T, lf::busy_pool::context>;
-
-/// Compute the n'th fibonacci number
-auto fib(int n) -> pool_task<int> { 
-
-  if (n < 2) {
-    co_return n;
-  }
-
-  auto a = co_await fib(n - 1).fork(); // Spawn a child task.
-  auto b = co_await fib(n - 2);        // Execute inline.
-
-  co_await lf::join();                 // Wait for children.
-
-  co_return *a + b;                    // Use * to dereference a future.
+namespace {
+    constexpr auto hello_async_world = [](auto /* self */) -> lf::task<int> {
+        std::cout << "Hello, async world!" << std::endl;
+        co_return 0;
+    };
 }
 
-
 auto main() -> int {
-    lf::busy_pool pool;
-
-    int fib_10 = pool.schedule(fib(10));
-
-    std::cout << "Test package output fib10 = " << fib_10 << std::endl;
+    try {
+        return lf::sync_wait(lf::lazy_pool{}, hello_async_world);
+    } catch (std::exception const &e) {
+        std::cerr << "Caught exception: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    } catch (...) {
+        std::cerr << "Caught unknown exception." << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
