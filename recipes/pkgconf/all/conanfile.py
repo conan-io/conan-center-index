b958b8ff6472bec0e28605eb0d8d7c47dc86c483
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-03-21 10:27:15 +0100

    pkgconf: set tools.gnu:pkg_config
    It is used by conan's PkgConfig helper
    https://github.com/conan-io/conan/blob/59029095e86eacd402083fca7c308b06889f6301/conan/tools/gnu/pkgconfig.py#L25
    
    unblocks https://github.com/conan-io/conan-center-index/pull/23030

--- recipes/pkgconf/all/conanfile.py
+++ recipes/pkgconf/all/conanfile.py
@@ -138,6 +138,8 @@ class PkgConfConan(ConanFile):
         self.buildenv_info.prepend_path("ACLOCAL_PATH", pkgconf_aclocal)
         # TODO: evaluate if `ACLOCAL_PATH` is enough and we can stop using `AUTOMAKE_CONAN_INCLUDES`
         self.buildenv_info.prepend_path("AUTOMAKE_CONAN_INCLUDES", pkgconf_aclocal)
+        
+        self.conf_info.define("tools.gnu:pkg_config", "pkgconf")
 
         # TODO: remove in conanv2
         automake_extra_includes = unix_path_package_info_legacy(self, pkgconf_aclocal.replace("\\", "/"))
