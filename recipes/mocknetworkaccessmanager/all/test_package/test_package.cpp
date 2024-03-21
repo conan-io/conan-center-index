2e0be31cf0e1f96d9185e4bf124c58d5a6fdc5dc
Martin Delille <martin@delille.org>
2024-03-21 10:00:37 +0100

    Revert "Remove whenGet() from test_package.cpp"
    This reverts commit 4a1d57b5a8eb1808d5a8a11332f802a5900076f9.

--- recipes/mocknetworkaccessmanager/all/test_package/test_package.cpp
+++ recipes/mocknetworkaccessmanager/all/test_package/test_package.cpp
@@ -3,5 +3,10 @@
 int main(void) {
     MockNetworkAccess::Manager< QNetworkAccessManager > mnam;
 
+    mnam.whenGet( QUrl( "http://example.com/hello" ) )
+        .has( MockNetworkAccess::Predicates::HeaderMatching( QNetworkRequest::UserAgentHeader,
+                    QRegularExpression( ".*MyNetworkClient/.*" ) ) )
+        .reply().withBody( QJsonDocument::fromJson( "{\"hello\":\"world\"}" ) );
+
     return 0;
 }
