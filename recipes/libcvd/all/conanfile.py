a92c02c0539c97615e3f86514db5e677aa5794bb
Martin Valgur <martin.valgur@gmail.com>
2024-03-24 13:57:46 +0200

    libcvd: fix Windows build

--- recipes/libcvd/all/conanfile.py
+++ recipes/libcvd/all/conanfile.py
@@ -4,7 +4,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, save, rmdir, export_conandata_patches, apply_conandata_patches
+from conan.tools.files import copy, get, save, rmdir, export_conandata_patches, apply_conandata_patches, replace_in_file
 from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
@@ -91,7 +91,7 @@ class LibCVDConan(ConanFile):
             self.requires("libpng/[>=1.6 <2]")
         if self.options.with_libtiff:
             self.requires("libtiff/4.6.0")
-        if self.options.get_safe("with_opengl"):
+        if self.options.get_safe("with_opengl", True):
             # https://github.com/edrosten/libcvd/blob/RELEASE_2_5_0/cvd/videodisplay.h#L18-L20
             self.requires("opengl/system", transitive_headers=True, transitive_libs=True)
             if self.settings.os in ["Linux", "FreeBSD"]:
@@ -122,9 +122,10 @@ class LibCVDConan(ConanFile):
         tc.variables["CMAKE_DISABLE_FIND_PACKAGE_JPEG"] = not self.options.with_libjpeg
         tc.variables["CMAKE_DISABLE_FIND_PACKAGE_PNG"] = not self.options.with_libpng
         tc.variables["CMAKE_DISABLE_FIND_PACKAGE_TIFF"] = not self.options.with_libtiff
-        tc.variables["CMAKE_DISABLE_FIND_PACKAGE_OpenGL"] = not self.options.get_safe("with_opengl", False)
-        tc.variables["CMAKE_DISABLE_FIND_PACKAGE_X11"] = not self.options.get_safe("with_opengl", False)
+        tc.variables["CMAKE_DISABLE_FIND_PACKAGE_OpenGL"] = not self.options.get_safe("with_opengl", True)
+        tc.variables["CMAKE_DISABLE_FIND_PACKAGE_X11"] = not self.options.get_safe("with_opengl", True)
         tc.variables["CMAKE_DISABLE_FIND_PACKAGE_TooN"] = True
+        tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
         tc.generate()
 
@@ -136,6 +137,7 @@ class LibCVDConan(ConanFile):
 
     def _patch_sources(self):
         apply_conandata_patches(self)
+
         # Use deps from Conan
         save(self, os.path.join(self.source_folder, "cmake", "CVDFindFFMPEG.cmake"),
              "find_package(CVD_FFMPEG REQUIRED)\n" if self.options.with_ffmpeg else "")
@@ -143,9 +145,16 @@ class LibCVDConan(ConanFile):
              "find_package(CVD_dc1394v2 REQUIRED)\n" if self.options.with_libdc1394 else "")
         save(self, os.path.join(self.source_folder, "cmake", "CVDFindTooN.cmake"),
             "set(CVD_TooN_FOUND FALSE)\n")
+
         # For debugging
         save(self, os.path.join(self.source_folder, "cmake", "CVDFindAllDeps.cmake"),
              '\nmessage(INFO "CVD_DEP_LIBS: ${CVD_DEP_LIBS}")\n', append=True)
+
+        # Install DLL
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "install(TARGETS ${PROJECT_NAME} ARCHIVE DESTINATION lib LIBRARY DESTINATION lib)",
+                        "install(TARGETS ${PROJECT_NAME} ARCHIVE DESTINATION lib LIBRARY DESTINATION lib RUNTIME DESTINATION bin)")
+
 
     def build(self):
         self._patch_sources()
