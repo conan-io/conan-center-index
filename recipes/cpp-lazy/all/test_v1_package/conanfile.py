f7565ac168f283761ff7a1814d4c507d8e84173d
toge <toge.mail@gmail.com>
2024-03-25 16:50:53 +0900

    add requirements in test_v1_package

--- recipes/cpp-lazy/all/test_v1_package/conanfile.py
+++ recipes/cpp-lazy/all/test_v1_package/conanfile.py
@@ -7,6 +7,10 @@ class TestPackageV1Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake", "cmake_find_package_multi"
 
+    def requirements(self):
+        # cpp-lazy>=8.0.0 requires fmt library when neither LZ_STANDALONE or LZ_MODULE_EXPORT are not defined
+        self.requires("fmt/10.2.1")
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
