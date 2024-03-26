0f5a4787a395b28eddaef79d0cc7960c7c716512
Matthieu Darbois <mayeut@users.noreply.github.com>
2024-03-26 08:07:44 +0100

    bump meson
    Co-authored-by: Martin Valgur <martin.valgur@gmail.com>

--- recipes/cairo/meson/conanfile.py
+++ recipes/cairo/meson/conanfile.py
@@ -128,7 +128,7 @@ class CairoConan(ConanFile):
                 )
 
     def build_requirements(self):
-        self.tool_requires("meson/1.3.1")
+        self.tool_requires("meson/1.4.0")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/2.1.0")
 
