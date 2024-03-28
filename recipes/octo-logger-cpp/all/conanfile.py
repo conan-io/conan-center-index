d95dc9246583ab41fbbd98efb407dd9883f76279
toge <toge.mail@gmail.com>
2024-03-28 12:31:18 +0900

    revert nlohmann_json
    Co-authored-by: Carlos Zoido <mrgalleta@gmail.com>

--- recipes/octo-logger-cpp/all/conanfile.py
+++ recipes/octo-logger-cpp/all/conanfile.py
@@ -54,7 +54,7 @@ class OctoLoggerCPPConan(ConanFile):
     def requirements(self):
         self.requires("fmt/10.1.1", transitive_headers=True)
         if self.options.get_safe("with_aws"):
-            self.requires("nlohmann_json/3.11.3")
+            self.requires("nlohmann_json/3.11.2")
             self.requires("aws-sdk-cpp/1.9.234")
 
     def validate(self):
