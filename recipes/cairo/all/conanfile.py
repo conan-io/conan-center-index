1815229055395570890cf422bfdb57ff44f0d46e
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-04-02 10:18:04 +0200

    Update conanfile.py

--- recipes/cairo/all/conanfile.py
+++ recipes/cairo/all/conanfile.py
@@ -92,7 +92,7 @@ class CairoConan(ConanFile):
                 self.requires("xorg/system")
         if self.options.get_safe("with_glib", True):
             self.requires("glib/2.76.3")
-        self.requires("zlib/[>=1.2.11 <2]3")
+        self.requires("zlib/[>=1.2.11 <2]")
         self.requires("pixman/0.43.4")
         self.requires("libpng/[>=1.6 <2]")
 
