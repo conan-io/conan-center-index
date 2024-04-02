54ba6cc096d06c7ada51301684800afebf07b3f9
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-04-02 17:12:35 +0300

    Compiling a test package using C++11

--- recipes/ipaddress/all/test_package/test_package.cpp
+++ recipes/ipaddress/all/test_package/test_package.cpp
@@ -4,7 +4,7 @@
 
 
 int main(void) {
-    constexpr auto ip = ipaddress::ipv6_address::parse("fec0::1ff:fe23:4567:890a%eth2");
+    auto ip = ipaddress::ipv6_address::parse("fec0::1ff:fe23:4567:890a%eth2");
     std::cout << "Parsing ipv6: " << ip << std::endl;
 
     return EXIT_SUCCESS;
