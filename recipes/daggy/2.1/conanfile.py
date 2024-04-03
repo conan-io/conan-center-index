2c1e82f642b0fb798d813f87c20ee457f6267874
Martin Valgur <martin.valgur@gmail.com>
2024-04-03 12:36:55 +0300

    daggy: don't force C++ standard

--- recipes/daggy/2.1/conanfile.py
+++ recipes/daggy/2.1/conanfile.py
@@ -104,8 +104,6 @@ class DaggyConan(ConanFile):
         env.generate(scope="build")
 
         tc = CMakeToolchain(self)
-        tc.variables["CMAKE_CXX_STANDARD"] = 17
-        tc.variables["CMAKE_CXX_STANDARD_REQUIRED"] = True
         tc.variables["SSH2_SUPPORT"] = self.options.with_ssh2
         tc.variables["YAML_SUPPORT"] = self.options.with_yaml
         tc.variables["CONSOLE"] = self.options.with_console
