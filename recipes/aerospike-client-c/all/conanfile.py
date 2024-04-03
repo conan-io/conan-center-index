49090b43679e23949ea085b907d94ae0487df105
Yury Bogomolov <cleveryura@gmail.com>
2024-04-03 14:46:45 +0000

    remove cpp compiler settings

--- recipes/aerospike-client-c/all/conanfile.py
+++ recipes/aerospike-client-c/all/conanfile.py
@@ -37,6 +37,8 @@ class AerospikeConan(ConanFile):
     def configure(self):
         if self.options.shared:
             self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
 
     def requirements(self):
         self.requires("openssl/[>=1.1 <4]")
