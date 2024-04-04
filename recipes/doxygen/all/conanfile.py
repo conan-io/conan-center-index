ec98dd6e0ab0cf07ed37054c565b7eddfd02c4d6
Uilian Ries <uilianries@gmail.com>
2024-04-04 11:13:28 +0200

    Remove unused import

--- recipes/doxygen/all/conanfile.py
+++ recipes/doxygen/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime
 from conan.tools.scm import Version
 import os
