74217adb766b5654c7d1b225fc5a95c9bfa6db9f
Martin Valgur <martin.valgur@gmail.com>
2024-04-05 21:33:18 +0300

    gnuradio-volk: install mako from pip

--- recipes/gnuradio-volk/all/conanfile.py
+++ recipes/gnuradio-volk/all/conanfile.py
@@ -4,6 +4,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv, Environment
 from conan.tools.files import copy, get, rm, save, rmdir
 from conan.tools.scm import Version
 
@@ -50,6 +51,8 @@ class GnuradioVolkConan(ConanFile):
     def configure(self):
         if self.options.shared:
             self.options.rm_safe("fPIC")
+        # To avoid ConanInvalidConfiguration on Windows
+        self.options["cpython"].shared = True
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -67,10 +70,22 @@ class GnuradioVolkConan(ConanFile):
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
 
+    def build_requirements(self):
+        self.tool_requires("cpython/3.10.0")
+        # To avoid a conflict with CMake installed via pip on the system Python
+        self.tool_requires("cmake/[>=3.15 <4]")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
+    @property
+    def _site_packages_dir(self):
+        return os.path.join(self.build_folder, "site-packages")
+
     def generate(self):
+        venv = VirtualBuildEnv(self)
+        venv.generate()
+
         tc = CMakeToolchain(self)
         tc.variables["ENABLE_TESTING"] = False
         tc.variables["ENABLE_MODTOOL"] = False  # Requires Python
@@ -78,15 +93,24 @@ class GnuradioVolkConan(ConanFile):
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
         tc.generate()
 
-        tc = CMakeDeps(self)
-        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        env = Environment()
+        env.append_path("PYTHONPATH", self._site_packages_dir)
+        env.append_path("PATH", os.path.join(self._site_packages_dir, "bin"))
+        env.vars(self).save_script("pythonpath")
 
     def _patch_sources(self):
         # Disable apps
         save(self, os.path.join(self.source_folder, "apps", "CMakeLists.txt"), "")
 
+    def _pip_install(self, packages):
+        self.run(f"python -m pip install {' '.join(packages)} --no-cache-dir --target={self._site_packages_dir}")
+
     def build(self):
         self._patch_sources()
+        self._pip_install(["mako"])
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -101,7 +125,6 @@ class GnuradioVolkConan(ConanFile):
         rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
-
         self.cpp_info.set_property("cmake_file_name", "Volk")
         self.cpp_info.set_property("cmake_target_name", "Volk::volk")
         self.cpp_info.set_property("pkg_config_name", "volk")
