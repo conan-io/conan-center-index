3ffdb9aea5d7773c275a1fd6aecea35d48ac4cef
Martin Valgur <martin.valgur@gmail.com>
2024-04-07 17:09:48 +0300

    f2c: new recipe

---
+++ recipes/f2c/all/conanfile.py
@@ -0,0 +1,93 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake
+from conan.tools.files import get, load, save, copy, download, export_conandata_patches, apply_conandata_patches
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc
+
+required_conan_version = ">=1.54.0"
+
+class F2cConan(ConanFile):
+    name = "f2c"
+    description = "Fortran 77 to C/C++ translator and compiler wrapper"
+    license = "SMLNJ"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://netlib.org/f2c/"
+    topics = ("fortran", "translator", "tool")
+
+    package_type = "application"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "fc_wrapper": [True, False],
+    }
+    default_options = {
+        "fc_wrapper": True,
+    }
+
+    def export_sources(self):
+        copy(self, "CMakeLists.txt", self.recipe_folder, os.path.join(self.export_sources_folder, "src"))
+        export_conandata_patches(self)
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        del self.info.settings.compiler
+        del self.info.options.fc_wrapper
+
+    @staticmethod
+    def _chmod_plus_x(name):
+        if os.name == "posix":
+            os.chmod(name, os.stat(name).st_mode | 0o111)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version]["f2c"], strip_root=True)
+        download(self, **self.conan_data["sources"][self.version]["fc"], filename="fc")
+        self._chmod_plus_x("fc")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def _read_license(self):
+        lines = load(self, os.path.join(self.source_folder, "main.c")).splitlines()
+        return "\n".join(lines[1:21]) + "\n"
+
+    def package(self):
+        save(self, os.path.join(self.package_folder, "licenses", "LICENSE"), self._read_license())
+        cmake = CMake(self)
+        cmake.install()
+        copy(self, "fc", self.source_folder, os.path.join(self.package_folder, "bin"))
+
+    def package_info(self):
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
+
+        f2c_path = os.path.join(self.package_folder, "bin", "f2c")
+        self.buildenv_info.define_path("F2C", f2c_path)
+
+        if self.options.fc_wrapper:
+            fc_path = os.path.join(self.package_folder, "bin", "fc")
+            includedir = os.path.join(self.package_folder, "include")
+            libdir = os.path.join(self.package_folder, "lib")
+            if is_msvc(self):
+                cflags = f"-I{includedir} -link /LIBPATH:{libdir} f2c.lib"
+            else:
+                cflags = f"-I{includedir} -L{libdir} -lf2c"
+            self.buildenv_info.define_path("FC", fc_path)
+            self.buildenv_info.define("CFLAGSF2C", cflags)
+            self.env_info.FC = fc_path
+            self.env_info.CFLAGSF2C = cflags
+
+        # TODO: Legacy, to be removed on Conan 2.0
+        bin_folder = os.path.join(self.package_folder, "bin")
+        self.env_info.PATH.append(bin_folder)
+        self.env_info.F2C = f2c_path
