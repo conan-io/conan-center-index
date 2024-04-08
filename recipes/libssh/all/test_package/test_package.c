b8b9f094a60a389287122ce68eed378ed28a1f9a
Noah Miller <mike@stealthwing.com>
2024-04-08 21:58:07 +1200

    libssh: simplify test_package

--- recipes/libssh/all/test_package/test_package.c
+++ recipes/libssh/all/test_package/test_package.c
@@ -1,14 +1,10 @@
 #include <libssh/libssh.h>
 
 int main(int argc, char *argv[]) {
-  ssh_init();
   ssh_session session = ssh_new();
-  if (session == NULL)
+  if (session == NULL) {
     return -1;
+  }
 
-  const char* host = "localhost";
-  ssh_options_set(session, SSH_OPTIONS_HOST, host);
-  ssh_options_set(session, SSH_OPTIONS_USER, "user");
-  int rc = ssh_connect(session);
   return 0;
 }
