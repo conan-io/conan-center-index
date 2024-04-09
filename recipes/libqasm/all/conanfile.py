eee1c42dccd069ab11d32d7f2c94c5eed8067555
rturrado <68099809+rturrado@users.noreply.github.com>
2024-04-09 16:51:22 +0200

    Update libqasm recipe. I've mainly mimicked the tree-gen recipe, which @uilianries helped  me build recently. But also followed @valgur's code review comments.

--- recipes/libqasm/all/conanfile.py
+++ recipes/libqasm/all/conanfile.py
@@ -3,39 +3,36 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get
 from conan.tools.scm import Version
+
+required_conan_version = ">=1.53.0"
 
 
 class LibqasmConan(ConanFile):
     name = "libqasm"
-
-    # Optional metadata
     license = "Apache-2.0"
     homepage = "https://github.com/QuTech-Delft/libqasm"
     url = "https://github.com/conan-io/conan-center-index"
     description = "Library to parse cQASM files"
     topics = ("code generation", "parser", "compiler", "quantum compilation", "quantum simulation")
-
-    # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "build_python": [True, False],
-        "build_tests": [True, False],
-        "compat": [True, False],
-        "tree_gen_build_tests": [True, False]
+        "build_python": [True, False]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "build_python": False,
-        "build_tests": False,
-        "compat": False,
-        "tree_gen_build_tests": False
+        "build_python": False
     }
+
+    @property
+    def _should_build_test(self):
+        return not self.conf.get("tools.build:skip_test", default=True, check_type=bool)
 
     @property
     def _min_cppstd(self):
@@ -44,20 +41,12 @@ class LibqasmConan(ConanFile):
     @property
     def _compilers_minimum_version(self):
         return {
-            "apple-clang": "13",
-            "clang": "13",
             "gcc": "10",
-            "msvc": "192",
-            "Visual Studio": "16"
+            "clang": "13",
+            "apple-clang": "14",
+            "Visual Studio": "16",
+            "msvc": "192"
         }
-
-    def build_requirements(self):
-        self.tool_requires("m4/1.4.19")
-        if self.settings.os == "Windows":
-            self.tool_requires("winflexbison/2.5.24")
-        else:
-            self.tool_requires("flex/2.6.4")
-            self.tool_requires("bison/3.8.2")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -68,16 +57,29 @@ class LibqasmConan(ConanFile):
             self.options.rm_safe("fPIC")
 
     def layout(self):
-        self.folders.source = "."
-        self.folders.build = os.path.join("build", str(self.settings.build_type))
-        self.folders.generators = os.path.join(self.folders.build, "generators")
+        cmake_layout(self, src_folder="src")
 
-        self.cpp.package.libs = ["cqasm"]
-        self.cpp.package.includedirs = ["include"]
-        self.cpp.package.libdirs = ["lib"]
+    def build_requirements(self):
+        self.tool_requires("tree-gen/1.0.7")
+        self.tool_requires("zulu-openjdk/11.0.19")
+        if self.settings.arch == "wasm":
+            self.tool_requires("emsdk/3.1.50")
+        if self._should_build_test:
+            self.test_requires("gtest/1.14.0")
 
-        self.cpp.source.includedirs = ["include"]
-        self.cpp.build.libdirs = ["."]
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
+
+    def requirements(self):
+        self.requires("fmt/10.2.1", transitive_headers=True)
+        self.requires("range-v3/0.12.0", transitive_headers=True)
+        self.requires("tree-gen/1.0.7", transitive_headers=True)
+        if not self.settings.arch == "wasm":
+            self.requires("antlr4-cppruntime/4.13.1", transitive_headers=True)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -88,24 +90,14 @@ class LibqasmConan(ConanFile):
         tc = CMakeToolchain(self)
         tc.variables["LIBQASM_BUILD_PYTHON"] = self.options.build_python
         tc.variables["LIBQASM_BUILD_TESTS"] = self.options.build_tests
-        tc.variables["LIBQASM_COMPAT"] = self.options.compat
-        tc.variables["TREE_GEN_BUILD_TESTS"] = self.options.tree_gen_build_tests
         tc.generate()
+        env = VirtualBuildEnv(self)
+        env.generate()
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-
-    def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
-
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
 
     def package(self):
         copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
