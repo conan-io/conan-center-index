fe9665d83bf71b8c622937bf96f1ab35b9a1ce70
Jordan Williams <jordan@jwillikers.com>
2024-04-11 13:40:30 -0500

    Bump Qt

--- recipes/qcustomplot/all/conanfile.py
+++ recipes/qcustomplot/all/conanfile.py
@@ -46,9 +46,9 @@ class QCustomPlotConan(ConanFile):
 
     def requirements(self):
         if Version(self.version) >= "2.0.0":
-            self.requires("qt/6.6.1", transitive_headers=True, transitive_libs=True)
+            self.requires("qt/6.7.0", transitive_headers=True, transitive_libs=True)
         else:
-            self.requires("qt/5.15.12", transitive_headers=True, transitive_libs=True)
+            self.requires("qt/5.15.13", transitive_headers=True, transitive_libs=True)
         if self.options.with_opengl and self.settings.os == "Windows":
             self.requires("opengl/system")
 
