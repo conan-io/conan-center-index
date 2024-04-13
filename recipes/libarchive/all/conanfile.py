3b7857b83e979cfcc81288b6572674341554827c
mayeut <mayeut@users.noreply.github.com>
2024-04-13 12:10:12 +0200

    libarchive: remove vulnerable unused versions
    All versions<3.7.0 have some CVEs, let's remove all versions not used in CCI which are vulnerable.

--- recipes/libarchive/all/conanfile.py
+++ recipes/libarchive/all/conanfile.py
@@ -69,8 +69,6 @@ class LibarchiveConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        if Version(self.version) < "3.4.2":
-            del self.options.with_mbedtls
         if Version(self.version) < "3.7.3":
             del self.options.with_pcre2
 
@@ -158,8 +156,7 @@ class LibarchiveConan(ConanFile):
         tc.variables["ENABLE_UNZIP"] = False
         # too strict check
         tc.variables["ENABLE_WERROR"] = False
-        if Version(self.version) >= "3.4.2":
-            tc.variables["ENABLE_MBEDTLS"] = self.options.with_mbedtls
+        tc.variables["ENABLE_MBEDTLS"] = self.options.with_mbedtls
         if Version(self.version) >= "3.7.3":
             tc.variables["ENABLE_PCRE2POSIX"] = self.options.with_pcre2
         tc.variables["ENABLE_XATTR"] = self.options.with_xattr
