b63ddb1dc4b41689ff195a3657f5ac8c56282637
mayeut <mayeut@users.noreply.github.com>
2024-04-13 14:16:41 +0200

    bump openexr

--- recipes/libvips/all/conanfile.py
+++ recipes/libvips/all/conanfile.py
@@ -159,7 +159,7 @@ class LibvipsConan(ConanFile):
         if self.options.with_matio:
             self.requires("matio/1.5.24")
         if self.options.with_openexr:
-            self.requires("openexr/3.2.1")
+            self.requires("openexr/3.2.3")
         if self.options.with_openjpeg:
             self.requires("openjpeg/2.5.2")
         if self.options.with_pangocairo:
