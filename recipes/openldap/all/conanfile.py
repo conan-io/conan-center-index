ec56a93ae80671475c8a9dc3cc70d53357bd6b51
SSE4 <tomskside@gmail.com>
2024-04-14 19:24:09 +0700

    [openldap] fix package

--- recipes/openldap/all/conanfile.py
+++ recipes/openldap/all/conanfile.py
@@ -1,6 +1,11 @@
 import os
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps, PkgConfigDeps
+from conan.tools.env import Environment, VirtualBuildEnv, VirtualRunEnv
+from conan.tools.files import rmdir, get, copy, rm, apply_conandata_patches, export_conandata_patches
+from conan.tools.build import cross_building
+from conan.tools.layout import basic_layout
+from conan.errors import ConanInvalidConfiguration
 required_conan_version = ">=1.43.0"
 
 
@@ -12,7 +17,7 @@ class OpenldapConan(ConanFile):
     license = "OLDAP-2.8"
     topics = ("ldap", "load-balancer", "directory-access")
     exports_sources = ["patches/*"]
-    settings = settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
@@ -22,88 +27,74 @@ class OpenldapConan(ConanFile):
         "shared": False,
         "fPIC": True,
         "with_cyrus_sasl": True
-
     }
-    _autotools = None
-    _configure_vars = None
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def layout(self):
+        # src_folder must use the same source folder name the project
+        basic_layout(self, src_folder="src")
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  strip_root=True, destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def requirements(self):
-        self.requires("openssl/1.1.1q")
+        self.requires("openssl/3.2.1")
         if self.options.with_cyrus_sasl:
-            self.requires("cyrus-sasl/2.1.27")
+            self.requires("cyrus-sasl/2.1.28")
 
     def validate(self):
         if self.settings.os != "Linux":
             raise ConanInvalidConfiguration(
                 f"{self.name} is only supported on Linux")
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-
+    def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
+        if not cross_building(self):
+            env = VirtualRunEnv(self)
+            env.generate(scope="build")
+        tc = AutotoolsToolchain(self)
         def yes_no(v): return "yes" if v else "no"
-        self._autotools = AutoToolsBuildEnvironment(self)
-        configure_args = [
-            "--enable-shared={}".format(yes_no(self.options.shared)),
-            "--enable-static={}".format(yes_no(not self.options.shared)),
-            "--with-cyrus_sasl={}".format(yes_no(self.options.with_cyrus_sasl)),
-            "--with-pic={}".format(yes_no(self.options.get_safe("fPIC", True))),
+        tc.configure_args.extend([
+            f"--with-cyrus_sasl={yes_no(self.options.with_cyrus_sasl)}",
+            f"--with-pic={yes_no(self.options.get_safe('fPIC', True))}",
             "--without-fetch",
             "--with-tls=openssl",
-            "--enable-auditlog"]
-        self._configure_vars = self._autotools.vars
-        self._configure_vars["systemdsystemunitdir"] = os.path.join(
-            self.package_folder, "res")
-
-        # Need to link to -pthread instead of -lpthread for gcc 8 shared=True
-        # on CI job. Otherwise, linking fails.
-        self._autotools.libs.remove("pthread")
-        self._configure_vars["LIBS"] = self._configure_vars["LIBS"].replace(
-            "-lpthread", "-pthread")
-
-        self._autotools.configure(
-            args=configure_args,
-            configure_dir=self._source_subfolder,
-            vars=self._configure_vars)
-        return self._autotools
+            "--enable-auditlog"
+        ])
+        tc.generate()
+        tc = PkgConfigDeps(self)
+        tc.generate()
+        tc = AutotoolsDeps(self)
+        tc.generate()
 
     def build(self):
-        for patch in self.conan_data["patches"][self.version]:
-            tools.patch(**patch)
-        autotools = self._configure_autotools()
-
-        autotools.make(vars=self._configure_vars)
+        apply_conandata_patches(self)
+        autotools = Autotools(self)
+        autotools.configure()
+        autotools.make()
 
     def package(self):
-        autotools = self._configure_autotools()
-        autotools.install(vars=self._configure_vars)
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("COPYRIGHT", dst="licenses", src=self._source_subfolder)
-        for folder in ["var", "share", "etc", "lib/pkgconfig", "res"]:
-            tools.rmdir(os.path.join(self.package_folder, folder))
-        tools.remove_files_by_mask(
-            os.path.join(
-                self.package_folder,
-                "lib"),
-            "*.la")
+        autotools = Autotools(self)
+        autotools.install()
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "COPYRIGHT", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        for folder in ["var", "share", "etc", "lib/pkgconfig", "res", "libexec"]:
+            rmdir(self, os.path.join(self.package_folder, folder))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        rm(self, "*slap*", os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
         bin_path = os.path.join(self.package_folder, "bin")
         self.env_info.PATH.append(bin_path)
         self.output.info(
-            "Appending PATH environment variable: {}".format(bin_path))
+            f"Appending PATH environment variable: {bin_path}")
 
         self.cpp_info.libs = ["ldap", "lber"]
         if self.settings.os in ["Linux", "FreeBSD"]:
