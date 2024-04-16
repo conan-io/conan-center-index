3c026f689256d9fc76e561c79538f1a5623a4727
Jordan Williams <jordan@jwillikers.com>
2024-04-16 08:32:19 -0500

    mold: Remove unused OpenSSL dependency

--- recipes/mold/all/conanfile.py
+++ recipes/mold/all/conanfile.py
@@ -40,7 +40,6 @@ class MoldConan(ConanFile):
 
     def requirements(self):
         self.requires("zlib/[>=1.2.11 <2]")
-        self.requires("openssl/[>=1.1 <4]")
         self.requires("xxhash/0.8.2")
         self.requires("onetbb/2021.10.0")
         if self.options.with_mimalloc:
