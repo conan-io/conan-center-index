02fbd30ccebf45deb15cac477bc4599459b9bf78
Martin Valgur <martin.valgur@gmail.com>
2024-04-16 20:35:10 +0300

    protobuf: avoid tool_requires() in test_package

--- recipes/protobuf/all/test_package/conanfile.py
+++ recipes/protobuf/all/test_package/conanfile.py
@@ -3,24 +3,32 @@ from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 import os
 
+from conan.tools.env import VirtualRunEnv
+
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "VirtualBuildEnv", "VirtualRunEnv"
+    generators = "CMakeDeps"
     test_type = "explicit"
 
     def layout(self):
         cmake_layout(self)
 
     def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+        # A workaround for CI only.
+        # https://github.com/conan-io/conan-center-index/pull/23573#issue-2246020949
+        # Should normally be added without run=True and with a self.tool_requires("protobuf/...") instead
+        # to avoid propagating run=True in the host context in the graph.
+        self.requires(self.tested_reference_str, run=True)
 
     def generate(self):
+        venv = VirtualRunEnv(self)
+        venv.generate(scope="run")
+        # Needed for the CI workaround.
+        venv.generate(scope="build")
+
         tc = CMakeToolchain(self)
-        tc.cache_variables["protobuf_LITE"] = self.dependencies[self.tested_reference_str].options.lite
+        tc.variables["protobuf_LITE"] = self.dependencies[self.tested_reference_str].options.lite
         tc.generate()
 
     def build(self):
@@ -30,5 +38,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(bin_path, env="conanrun")
