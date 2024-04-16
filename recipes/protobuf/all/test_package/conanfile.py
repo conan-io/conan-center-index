4529c72a1eae02feddce0a0f4f52b3c4d1a6e73c
Martin Valgur <martin.valgur@gmail.com>
2024-04-16 21:03:24 +0300

    protobuf: format imports

--- recipes/protobuf/all/test_package/conanfile.py
+++ recipes/protobuf/all/test_package/conanfile.py
@@ -1,9 +1,8 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
-import os
-
 from conan.tools.env import VirtualRunEnv
+import os
 
 
 class TestPackageConan(ConanFile):
