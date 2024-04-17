8d46d34bb7b775aae833f2a410812425aebe17bd
toge <toge.mail@gmail.com>
2024-04-17 23:15:22 +0900

    don't use network layer

--- recipes/reduct-cpp/all/test_package/test_package.cpp
+++ recipes/reduct-cpp/all/test_package/test_package.cpp
@@ -5,7 +5,9 @@
 using reduct::IBucket;
 using reduct::IClient;
 
-int main() {
+// Don't call this function. we should avoid hitting the network layer.
+extern
+void dummy() {
     auto client = IClient::Build("http://127.0.0.1:8383");
 
     // Get information about the server
@@ -13,6 +15,9 @@ int main() {
     if (err) {
         std::cerr << "Error: " << err << '\n';
     }
+}
+
+int main() {
 
     return 0;
 }
