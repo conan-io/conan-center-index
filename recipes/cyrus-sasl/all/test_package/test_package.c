19ffd1d194afb1312c0502a2a0809e040eb2eee2
adamws <adamws@users.noreply.github.com>
2024-04-17 17:22:30 +0200

    cyrus-sasl: add extra header to `test_package`

--- recipes/cyrus-sasl/all/test_package/test_package.c
+++ recipes/cyrus-sasl/all/test_package/test_package.c
@@ -1,4 +1,5 @@
 #include <sasl/sasl.h>
+#include <sasl/saslplug.h>
 
 #include <stdio.h>
 
@@ -8,6 +9,7 @@ int main(int argc, char *argv[])
     sasl_version_info(&implementation, &version, NULL, NULL, NULL, NULL);
     printf("--------------------------->Tests are done.<--------------------------\n");
     printf("SASL Using implementation: %s, version: %s\n", implementation, version);
+    printf("SASL Utils version: %d\n", SASL_UTILS_VERSION);
     printf("//////////////////////////////////////////////////////////////////////\n");
     return 0;
 }
