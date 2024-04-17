fec5b03946f83101646d406537692ab567b43ad0
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-04-17 13:23:51 +0200

    fix conflict with mpc

--- recipes/gcc/all/conanfile.py
+++ recipes/gcc/all/conanfile.py
@@ -40,7 +40,7 @@ class GccConan(ConanFile):
     def requirements(self):
         self.requires("mpc/1.2.0")
         self.requires("mpfr/4.1.0")
-        self.requires("gmp/6.2.1")
+        self.requires("gmp/6.3.0")
         self.requires("zlib/[>=1.2.13 <2]")
         self.requires("isl/0.24")
 
