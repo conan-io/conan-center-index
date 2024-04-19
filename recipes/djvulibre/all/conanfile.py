4f5a2570c2a33c80f82f474721680daea715bfa1
Martin Valgur <martin.valgur@gmail.com>
2024-04-19 16:08:22 +0300

    djvulibre: fix MSVC build

--- recipes/djvulibre/all/conanfile.py
+++ recipes/djvulibre/all/conanfile.py
@@ -1,11 +1,12 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name, is_apple_os
 from conan.tools.build import cross_building, stdcpp_library
 from conan.tools.env import Environment, VirtualBuildEnv, VirtualRunEnv
 from conan.tools.files import copy, get, rm, rmdir, save, rename, export_conandata_patches, apply_conandata_patches
-from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import is_msvc, unix_path
+from conan.tools.microsoft import is_msvc, unix_path, check_min_vs
 import os
 
 required_conan_version = ">=1.54.0"
@@ -62,6 +63,11 @@ class DjVuLibreConan(ConanFile):
             self.requires("mozjpeg/4.1.5")
         self.requires("libtiff/4.6.0")
 
+    def validate(self):
+        if self.settings.os == "Windows" and not self.options.shared:
+            # __declspec(dllimport) or __declspec(dllexport) is always set for symbols
+            raise ConanInvalidConfiguration("Static linking is not supported on Windows")
+
     def build_requirements(self):
         self.tool_requires("libtool/2.4.7")
         if self._settings_build.os == "Windows":
@@ -80,6 +86,12 @@ class DjVuLibreConan(ConanFile):
             VirtualRunEnv(self).generate(scope="build")
 
         tc = AutotoolsToolchain(self)
+        if is_msvc(self):
+            tc.extra_cxxflags.append("-EHsc")
+            if check_min_vs(self, "180", raise_invalid=False):
+                tc.extra_cflags.append("-FS")
+                tc.extra_cxxflags.append("-FS")
+            tc.extra_ldflags.append("-ladvapi32")
         tc.generate()
 
         if is_msvc(self):
@@ -96,6 +108,38 @@ class DjVuLibreConan(ConanFile):
             env.define("RANLIB", ":")
             env.define("STRIP", ":")
             env.vars(self).save_script("conanbuild_msvc")
+
+        # FIXME: "JPEG support.......: no"
+        if is_msvc(self):
+            # Custom AutotoolsDeps for cl-like compilers
+            # workaround for https://github.com/conan-io/conan/issues/12784
+            includedirs = []
+            defines = []
+            libs = []
+            libdirs = []
+            linkflags = []
+            cxxflags = []
+            cflags = []
+            for dependency in self.dependencies.values():
+                deps_cpp_info = dependency.cpp_info.aggregated_components()
+                includedirs.extend(deps_cpp_info.includedirs)
+                defines.extend(deps_cpp_info.defines)
+                libs.extend(deps_cpp_info.libs + deps_cpp_info.system_libs)
+                libdirs.extend(deps_cpp_info.libdirs)
+                linkflags.extend(deps_cpp_info.sharedlinkflags + deps_cpp_info.exelinkflags)
+                cxxflags.extend(deps_cpp_info.cxxflags)
+                cflags.extend(deps_cpp_info.cflags)
+
+            env = Environment()
+            env.append("CPPFLAGS", [f"-I{unix_path(self, p)}" for p in includedirs] + [f"-D{d}" for d in defines])
+            env.append("_LINK_", [lib if lib.endswith(".lib") else f"{lib}.lib" for lib in libs])
+            env.append("LDFLAGS", [f"-L{unix_path(self, p)}" for p in libdirs] + linkflags)
+            env.append("CXXFLAGS", cxxflags)
+            env.append("CFLAGS", cflags)
+            env.vars(self).save_script("conanautotoolsdeps_cl_workaround")
+        else:
+            deps = AutotoolsDeps(self)
+            deps.generate()
 
     def _patch_sources(self):
         apply_conandata_patches(self)
@@ -120,10 +164,12 @@ class DjVuLibreConan(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "share", "icons"))
         rename(self, os.path.join(self.package_folder, "share"), os.path.join(self.package_folder, "res"))
         fix_apple_shared_install_name(self)
+        if is_msvc(self) and self.options.shared:
+            rename(self, os.path.join(self.package_folder, "lib", "djvulibre.dll.lib"),
+                         os.path.join(self.package_folder, "lib", "djvulibre.lib"))
 
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "ddjvuapi")
-
         self.cpp_info.libs = ["djvulibre"]
         self.cpp_info.resdirs = ["res"]
 
@@ -131,6 +177,8 @@ class DjVuLibreConan(ConanFile):
             self.cpp_info.system_libs.extend(["m", "pthread"])
         elif is_apple_os(self):
             self.cpp_info.frameworks.extend(["CoreFoundation"])
+        elif self.settings.os == "Windows":
+            self.cpp_info.system_libs.append("advapi32")
 
         # Implemented in C++ but exports a pure C API
         if stdcpp_library(self):
