c4a1ffde2bed13c2f3e89fbe389b8e4456e2f923
Rubén Rincón Blanco <git@rinconblanco.es>
2024-04-19 08:36:53 +0200

    Fix Version import

--- recipes/aws-kvs-pic/all/conanfile.py
+++ recipes/aws-kvs-pic/all/conanfile.py
@@ -4,6 +4,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir, replace_in_file
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
 
