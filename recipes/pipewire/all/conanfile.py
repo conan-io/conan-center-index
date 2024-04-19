06a1e85ffa8c0fa24d8d3d5982789a0cbbdfb84e
Jordan Williams <jordan@jwillikers.com>
2024-04-19 15:57:54 -0500

    Bump Meson

--- recipes/pipewire/all/conanfile.py
+++ recipes/pipewire/all/conanfile.py
@@ -154,7 +154,7 @@ class PipeWireConan(ConanFile):
             )
 
     def build_requirements(self):
-        self.tool_requires("meson/1.3.2")
+        self.tool_requires("meson/1.4.0")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/2.1.0")
 
