b984d85cd7ab539fb9ae69d5f1b3c7bcc973bc59
deniskovalchuk <denis.kovalchuk.main@gmail.com>
2024-04-23 01:02:53 +0300

    libftp: revert 2a515545
    The test package must be compatible with v0.4.1.

--- recipes/libftp/all/test_package/test_package.cpp
+++ recipes/libftp/all/test_package/test_package.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-#include "ftp/ftp.hpp"
+#include "ftp/client.hpp"
 
 int main(void) {
     try {
-        /* Test that libftp provides OpenSSL routines. */
-        ftp::ssl::context_ptr ssl_context
-            = std::make_unique<ftp::ssl::context>(ftp::ssl::context::tlsv13_client);
-        SSL_CTX_set_session_cache_mode(ssl_context->native_handle(), SSL_SESS_CACHE_CLIENT);
-
-        ftp::client client(std::move(ssl_context));
+        ftp::client client;
     }
     catch (const std::exception & ex) {
         std::cerr << ex.what() << std::endl;
