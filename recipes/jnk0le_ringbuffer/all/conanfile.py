60e4daedd3bf9e11eff3a6261aa06ef718e1ae89
toge <toge.mail@gmail.com>
2024-04-23 11:05:40 +0900

    update cci.20240422

--- recipes/jnk0le_ringbuffer/all/conanfile.py
+++ recipes/jnk0le_ringbuffer/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import get, copy, apply_conandata_patches, export_conandata_patches
+from conan.tools.files import get, copy
 from conan.tools.layout import basic_layout
 import os
 
@@ -21,9 +21,6 @@ class RingBufferConan(ConanFile):
     def _min_cppstd(self):
         return 11
 
-    def export_sources(self):
-        export_conandata_patches(self)
-
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -36,9 +33,6 @@ class RingBufferConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-
-    def build(self):
-        apply_conandata_patches(self)
 
     def package(self):
         copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
