fdf2e838650d6d33a69a7017513003750b95a24d
Jordan Williams <jordan@jwillikers.com>
2024-04-23 12:12:57 -0500

    Bump build dependencies

--- recipes/pipewire/all/conanfile.py
+++ recipes/pipewire/all/conanfile.py
@@ -156,7 +156,7 @@ class PipeWireConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("meson/1.4.0")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.1.0")
+            self.tool_requires("pkgconf/2.2.0")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
