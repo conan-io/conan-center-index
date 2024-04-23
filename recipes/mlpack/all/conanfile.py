e5812b64e540c99201aa53d876b14c1c4b73fa7d
Rubén Rincón Blanco <git@rinconblanco.es>
2024-04-23 11:10:07 +0200

    Update recipes/mlpack/all/conanfile.py

--- recipes/mlpack/all/conanfile.py
+++ recipes/mlpack/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import copy, get, replace_in_file, rm
+from conan.tools.files import copy, get, replace_in_file
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
