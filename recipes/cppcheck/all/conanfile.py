693e5ab2fb96d5cd2bb684fa7c94fa60930dd462
Rubén Rincón Blanco <git@rinconblanco.es>
2024-04-23 11:01:54 +0200

    Remove unused imports

--- recipes/cppcheck/all/conanfile.py
+++ recipes/cppcheck/all/conanfile.py
@@ -1,6 +1,4 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 from conan.tools.scm import Version
