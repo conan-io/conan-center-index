e502321e2a68259029f2a56056e11d899a6e0b92
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-04-24 16:49:13 +0200

    fix

--- recipes/open62541/all/test_package/test_package.c
+++ recipes/open62541/all/test_package/test_package.c
@@ -1,5 +1,9 @@
+#ifdef UA_ENABLE_AMALGAMATION
+#include <open62541.h>
+#else
 #include <open62541/server.h>
 #include <open62541/server_config_default.h>
+#endif
 #include <stdlib.h>
 
 int main(void) {
