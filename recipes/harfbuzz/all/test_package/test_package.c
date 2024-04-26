7f82a028266d443f1c7252556424a10207e316e4
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-04-26 11:24:24 +0200

    not const

--- recipes/harfbuzz/all/test_package/test_package.c
+++ recipes/harfbuzz/all/test_package/test_package.c
@@ -3,7 +3,7 @@
 #include <hb.h>
 
 int main() {
-    const char *my_string = "my-string";
+    char *my_string = "my-string";
     size_t length = strlen(my_string);
 
     hb_blob_t* blob = hb_blob_create(my_string, length, HB_MEMORY_MODE_READONLY, NULL, NULL);
