40c3d4bc71c851df8f380d4c895c20aaa5498e89
Martin Valgur <martin.valgur@gmail.com>
2024-04-27 15:17:02 +0300

    pangolin: improve example

--- recipes/pangolin/all/test_package/test_package.cpp
+++ recipes/pangolin/all/test_package/test_package.cpp
@@ -5,9 +5,8 @@
 
 int main()
 {
-    static const int w = 640;
-    static const int h = 480;
-    pangolin::GlTexture color_buffer(w, h);
-    pangolin::GlRenderBuffer depth_buffer(w, h);
-    pangolin::GlFramebuffer fbo_buffer(color_buffer, depth_buffer);
+    pangolin::OpenGlRenderState s_cam(
+        pangolin::ProjectionMatrix(640,480,420,420,320,240,0.2,100),
+        pangolin::ModelViewLookAt(-2,2,-2, 0,0,0, pangolin::AxisY)
+    );
 }
