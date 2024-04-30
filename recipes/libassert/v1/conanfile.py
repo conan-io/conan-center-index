79171010e0950a10df3ac807344b69d4e73f18c6
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-04-29 21:48:52 -0500

    Undo v1 side, I don't want to mess with that

--- recipes/libassert/v1/conanfile.py
+++ recipes/libassert/v1/conanfile.py
@@ -140,3 +140,9 @@ class LibassertConan(ConanFile):
                 self.cpp_info.system_libs.append("dl")
             if self.settings.os == "Windows":
                 self.cpp_info.system_libs.append("dbghelp")
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.filenames["cmake_find_package"] = "assert"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "assert"
+        self.cpp_info.names["cmake_find_package"] = "assert"
+        self.cpp_info.names["cmake_find_package_multi"] = "assert"
