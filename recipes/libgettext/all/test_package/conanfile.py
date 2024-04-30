a805273e535c9a9bc220bdbc70744d2e9f86daf2
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-04-30 10:04:28 +0200

    virtualrunenv

--- recipes/libgettext/all/test_package/conanfile.py
+++ recipes/libgettext/all/test_package/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMake, cmake_layout
 
 import os
 
@@ -8,7 +8,7 @@ import os
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     test_type = "explicit"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
