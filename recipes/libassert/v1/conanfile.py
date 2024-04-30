75672dc22ffe8415ead3d5a7c8a657b6b6265921
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-04-29 21:32:36 -0500

    Remove cpp_info.names/.filenames from libassert's conanfiles

--- recipes/libassert/v1/conanfile.py
+++ recipes/libassert/v1/conanfile.py
@@ -130,12 +130,6 @@ class LibassertConan(ConanFile):
         # the first version of this library used assert/assert as include folder
         # appending this one but not removing the default to not break consumers
         self.cpp_info.includedirs.append(os.path.join("include", "assert"))
-
-        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
-        self.cpp_info.filenames["cmake_find_package"] = "assert"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "assert"
-        self.cpp_info.names["cmake_find_package"] = "assert"
-        self.cpp_info.names["cmake_find_package_multi"] = "assert"
         
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.append("m")
