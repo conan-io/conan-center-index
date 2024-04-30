7dd20562db4b6e99d13eeaa16943164c552e653b
Martin Valgur <martin.valgur@gmail.com>
2024-04-30 14:11:03 +0300

    gdalcpp: new recipe

---
+++ recipes/gdalcpp/all/conanfile.py
@@ -0,0 +1,45 @@
+import os
+
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+
+required_conan_version = ">=1.52.0"
+
+class GdalcppConan(ConanFile):
+    name = "gdalcpp"
+    description = "C++11 wrapper classes for GDAL/OGR"
+    license = "BSL-1.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/joto/gdalcpp"
+    topics = ("gdal", "osgeo", "geospatial", "raster", "vector", "gis", "header-only")
+    package_type = "header-library"
+
+    settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("gdal/3.8.3")
+
+    def package_id(self):
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, 11)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+
+    def package(self):
+        copy(self, "LICENSE.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "gdalcpp.hpp", self.source_folder, os.path.join(self.package_folder, "include"))
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
