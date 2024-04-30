8fd9b253815aeeda25351c75aa03f0ee465560f4
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-04-29 20:54:36 -0500

    Try to remove cpp_info.names/.filenames

--- recipes/cpptrace/all/conanfile.py
+++ recipes/cpptrace/all/conanfile.py
@@ -110,9 +110,3 @@ class CpptraceConan(ConanFile):
 
         if not self.options.shared:
             self.cpp_info.defines.append("CPPTRACE_STATIC_DEFINE")
-
-        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
-        self.cpp_info.filenames["cmake_find_package"] = "CPPTRACE"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "cpptrace"
-        self.cpp_info.names["cmake_find_package"] = "CPPTRACE"
-        self.cpp_info.names["cmake_find_package_multi"] = "cpptrace"
