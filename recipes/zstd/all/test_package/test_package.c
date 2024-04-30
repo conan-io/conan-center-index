4f1114628df846f3ee7f534c06e1c62675dcd7b8
Rubén Rincón Blanco <git@rinconblanco.es>
2024-04-30 10:26:49 +0200

    Update recipes/zstd/all/test_package/test_package.c

--- recipes/zstd/all/test_package/test_package.c
+++ recipes/zstd/all/test_package/test_package.c
@@ -6,6 +6,7 @@
 int main() {
     const char* originalData = "Sample text";
     size_t compressedSize = ZSTD_compressBound(strlen(originalData) + 1);
+    printf("%zu\n", compressedSize);
 
     return 0;
 }
