732c16cef143f9a07416809531b128b7d11ada85
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-04-29 21:36:11 -0500

    Defer libassert changes to another pr

--- recipes/libassert/v2/conanfile.py
+++ recipes/libassert/v2/conanfile.py
@@ -123,6 +123,12 @@ class LibassertConan(ConanFile):
         # appending this one but not removing the default to not break consumers
         self.cpp_info.includedirs.append(os.path.join("include", "libassert"))
 
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.filenames["cmake_find_package"] = "libassert"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "libassert"
+        self.cpp_info.names["cmake_find_package"] = "libassert"
+        self.cpp_info.names["cmake_find_package_multi"] = "libassert"
+
         self.cpp_info.components["assert"].names["cmake_find_package"] = "assert"
         self.cpp_info.components["assert"].names["cmake_find_package_multi"] = "assert"
         self.cpp_info.components["assert"].requires = ["cpptrace::cpptrace"]
