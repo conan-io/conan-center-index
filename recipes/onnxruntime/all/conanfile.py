b23744b99cd4a77a32545a3eb24b5de8b8342096
mayeut <mayeut@users.noreply.github.com>
2024-05-03 21:25:56 +0200

    bump boost to 1.85.0

--- recipes/onnxruntime/all/conanfile.py
+++ recipes/onnxruntime/all/conanfile.py
@@ -90,7 +90,7 @@ class OnnxRuntimeConan(ConanFile):
         self.requires("date/3.0.1")
         self.requires("re2/20231101")
         self.requires("flatbuffers/1.12.0")  # v1.* is required, newer versions are not compatible
-        self.requires("boost/1.84.0", headers=True, libs=False)  # for mp11, header only, no need for libraries
+        self.requires("boost/1.85.0", headers=True, libs=False)  # for mp11, header only, no need for libraries
         self.requires("safeint/3.0.28")
         self.requires("nlohmann_json/3.11.3")
         self.requires("eigen/3.4.0")
