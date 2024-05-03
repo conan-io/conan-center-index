5b0d65f09bbbe937dbef7325a93c5082c2d1e6a7
Croydon <git@cr0ydon.com>
2024-05-03 03:22:48 +0200

    Fix pdb removal

--- recipes/openssl/3.x.x/conanfile.py
+++ recipes/openssl/3.x.x/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name, is_apple_os, XCRun
 from conan.tools.build import build_jobs
-from conan.tools.files import chdir, copy, get, replace_in_file, rmdir, save
+from conan.tools.files import chdir, copy, get, replace_in_file, rm, rmdir, save
 from conan.tools.gnu import AutotoolsToolchain
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc, msvc_runtime_flag, unix_path
