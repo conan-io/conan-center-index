724d4d9cd290cd8b3ac8e119ce6e6ade53f338fd
toge <toge.mail@gmail.com>
2024-05-04 00:09:13 +0900

    link pthread

--- recipes/cxxgraph/all/conanfile.py
+++ recipes/cxxgraph/all/conanfile.py
@@ -71,3 +71,6 @@ class CxxgraphConan(ConanFile):
     def package_info(self):
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
+
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.extend(["pthread"])
