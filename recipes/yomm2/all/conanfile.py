fba35db108d58bbd0ab987dcfbcd5df7bf3b04bc
Francisco Ramirez de Anton <franchuti688@gmail.com>
2024-05-08 12:52:36 +0200

    Fixed static vs shared mode

--- recipes/yomm2/all/conanfile.py
+++ recipes/yomm2/all/conanfile.py
@@ -12,13 +12,13 @@ required_conan_version = ">=1.53.0"
 
 class yomm2Recipe(ConanFile):
     name = "yomm2"
-    package_type = "library"
+    package_type = "library"  # if static, it's a header-only one
     # Optional metadata
     license = "BSL-1.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/jll63/yomm2"
     description = "Fast, orthogonal, open multi-methods. Solve the Expression Problem in C++17"
-    topics = ("multi-methods", "library", "expressions", "c++17")
+    topics = ("multi-methods", "library", "header-only", "expressions", "c++17")
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
     options = {
@@ -26,7 +26,8 @@ class yomm2Recipe(ConanFile):
         "fPIC": [True, False]
     }
     default_options = {
-        "shared": False, "fPIC": True
+        "shared": False,
+        "fPIC": True
     }
 
     @property
@@ -36,10 +37,10 @@ class yomm2Recipe(ConanFile):
     @property
     def _compilers_minimum_version(self):
         return {
-            "gcc": "9",
+            "gcc": "8",
             "clang": "5",
-            "apple-clang": "10",
-            "msvc": "193",
+            "apple-clang": "12",
+            "msvc": "192"
         }
 
     def config_options(self):
@@ -63,7 +64,10 @@ class yomm2Recipe(ConanFile):
         self.tool_requires("cmake/[>=3.20 <4]")
 
     def requirements(self):
-        self.requires("boost/1.74.0")
+        # Upstream requires Boost 1.74
+        # Using more modern Boost version to avoid issues like the one commented here:
+        # - https://github.com/conan-io/conan/issues/15977#issuecomment-2098003085
+        self.requires("boost/1.85.0", transitive_headers=True)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -90,9 +94,16 @@ class yomm2Recipe(ConanFile):
         cmake = CMake(self)
         cmake.install()
         rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
-        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        if self.options.shared:
+            rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        else:  # header-only one
+            rmdir(self, os.path.join(self.package_folder, "lib"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "YOMM2")
         self.cpp_info.set_property("cmake_target_name", "YOMM2::yomm2")
-        self.cpp_info.libs = ["yomm2"]
+        if self.options.shared:
+            self.cpp_info.libs = ["yomm2"]
+        else:  # header-only one
+            self.cpp_info.bindirs = []
+            self.cpp_info.libdirs = []
