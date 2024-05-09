aa3f7f88a3d7f75ab4097388187d4c20a0bc300d
Francisco Ramirez de Anton <franchuti688@gmail.com>
2024-05-09 14:50:58 +0200

    Simplified test

--- recipes/yomm2/all/test_package/test_package.cpp
+++ recipes/yomm2/all/test_package/test_package.cpp
@@ -1,50 +1,25 @@
-// Copyright (c) 2018-2021 Jean-Louis Leroy
+// Copyright (c) 2021 Jean-Louis Leroy
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <iostream>
-#include <string>
-
-using std::string;
-
 #include <yorel/yomm2/keywords.hpp>
-
-using yorel::yomm2::virtual_;
-
-struct A {
-    virtual ~A() {
-    }
-};
-struct B : A {};
-struct C : A {};
-struct D : B, C {};
-
-register_class(A);
-register_class(B, A);
-register_class(C, A);
-register_class(D, B, C);
-
-declare_method(string, foobar, (virtual_<A&>));
-
-define_method(string, foobar, (A&)) {
-    return "foobar(A)";
-}
-
-define_method(string, foobar, (C&)) {
-    return "foobar(C)";
-}
 
 #include <iostream>
 #include <memory>
+#include <string>
+
+using std::cout;
+using yorel::yomm2::virtual_;
+
+// register_class(classes);
+
+// declare_method(return, name, (params));
+
+// define_method(return, name, (params)) {
+// }
 
 int main() {
     yorel::yomm2::update();
-
-    D d;
-    B& db = d; // B part of D
-    C& dc = d; // C part of D
-    std::cout << foobar(db) << "\n";
-    std::cout << foobar(dc) << "\n";
-    //std::cout << foobar(d) << "\n";
+    return 0;
 }
