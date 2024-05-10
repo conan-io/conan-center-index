30a0c02f547b73dd4bff897d6bcfa764840e7043
klausholstjacobsen <klaus.holst.jacobsen@gmail.com>
2024-05-10 10:35:39 +0200

    openssh: Allow all openssl 3.1.x releases

--- recipes/openssh/all/conanfile.py
+++ recipes/openssh/all/conanfile.py
@@ -52,7 +52,7 @@ class PackageConan(ConanFile):
     def requirements(self):
         self.requires("zlib/[>=1.2.11 <2]")
         if self.options.with_libcrypto == "openssl":
-            self.requires("openssl/[>=1.1 <=3.1]")
+            self.requires("openssl/[>=1.1 <3.2]")
         elif self.options.with_libcrypto == "libressl":
             self.requires("libressl/3.9.1")
         if self.options.with_pam == "openpam":
