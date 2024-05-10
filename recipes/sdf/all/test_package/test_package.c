b05511fb578f80734c7b54b0b3c01a56053d75d7
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-05-10 11:20:29 +0200

    evade UB

--- recipes/sdf/all/test_package/test_package.c
+++ recipes/sdf/all/test_package/test_package.c
@@ -10,6 +10,9 @@ int main(int argc, char **argv)
 {
     int width, height, bpp;
     unsigned char* img_data = stbi_load("not a valid/path/fake_img.png", &width, &height, &bpp, 0);
-    printf("img_data: %p\n", img_data);
+    if (img_data == NULL)
+    {
+        printf("Test\n"); // This should be printed always
+    }
     return 0;
 }
