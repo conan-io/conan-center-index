af78e6495c9c2bc9fd978fe8db4c5728181b41fa
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-05-10 12:20:08 +0200

    use sdf function

--- recipes/sdf/all/test_package/test_package.c
+++ recipes/sdf/all/test_package/test_package.c
@@ -8,11 +8,20 @@
 
 int main(int argc, char **argv)
 {
+
     int width, height, bpp;
+    unsigned char* dest_data = malloc(width * height * bpp);
     unsigned char* img_data = stbi_load("not a valid/path/fake_img.png", &width, &height, &bpp, 0);
     if (img_data == NULL)
     {
         printf("Test\n"); // This should be printed always
     }
+
+    int result = sdfBuildDistanceField(dest_data, width, 2.0f, img_data, width, height, width);
+    printf("Result: %d\n", result);
+
+    free(dest_data);
+    stbi_image_free(img_data);
+
     return 0;
 }
