949d50494c2295960e7fb1940c3a1f735ec40748
Sahnvour <sahnvour@pm.me>
2024-05-12 14:00:38 +0200

    add wgpu-native 0.19.4.1

---
+++ recipes/wgpu_native/all/test_package/conanfile.py
@@ -0,0 +1,29 @@
+import os
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.meson import Meson
+from conan.tools.layout import basic_layout
+
+
+class wgpu_nativeTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "PkgConfigDeps", "MesonToolchain"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+    
+    def build_requirements(self):
+        self.tool_requires("meson/[>1 <2]")
+
+    def build(self):
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
+
+    def layout(self):
+        basic_layout(self)
+
+    def test(self):
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "example")
+            self.run(cmd, env="conanrun")
