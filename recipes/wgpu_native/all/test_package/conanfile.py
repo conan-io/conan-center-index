acdc5ecc9edd43be33a30a5da586f5da58f63bc0
Sahnvour <sahnvour@pm.me>
2024-05-13 20:25:08 +0200

    wgpu_native: test_package with cmake instead of meson

--- recipes/wgpu_native/all/test_package/conanfile.py
+++ recipes/wgpu_native/all/test_package/conanfile.py
@@ -1,29 +1,25 @@
 import os
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.meson import Meson
-from conan.tools.layout import basic_layout
+from conan.tools.cmake import cmake_layout, CMake
 
 
-class wgpu_nativeTestConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "PkgConfigDeps", "MesonToolchain"
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
-    
-    def build_requirements(self):
-        self.tool_requires("meson/[>1 <2]")
-
-    def build(self):
-        meson = Meson(self)
-        meson.configure()
-        meson.build()
 
     def layout(self):
-        basic_layout(self)
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
         if can_run(self):
-            cmd = os.path.join(self.cpp.build.bindir, "example")
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(cmd, env="conanrun")
