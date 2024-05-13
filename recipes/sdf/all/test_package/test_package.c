85890f8616189885f74e2b023d1535cef1d6e601
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-05-13 13:27:43 +0200

    minimal test with img_data null

--- recipes/sdf/all/test_package/test_package.c
+++ recipes/sdf/all/test_package/test_package.c
@@ -3,25 +3,14 @@
 #define SDF_IMPLEMENTATION
 #include "sdf.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
 int main(int argc, char **argv)
 {
-
-    int width, height, bpp;
-    unsigned char* dest_data = malloc(width * height * bpp);
-    unsigned char* img_data = stbi_load("not a valid/path/fake_img.png", &width, &height, &bpp, 0);
-    if (img_data == NULL)
-    {
-        printf("Test\n"); // This should be printed always
-    }
-
-    int result = sdfBuildDistanceField(dest_data, width, 2.0f, img_data, width, height, width);
+    unsigned char* dest_data = malloc(0);
+    int result = sdfBuildDistanceField(dest_data, 0, 2.0f, NULL, 0, 0, 0);
     printf("Result: %d\n", result);
+    printf("Test");
 
     free(dest_data);
-    stbi_image_free(img_data);
 
     return 0;
 }
