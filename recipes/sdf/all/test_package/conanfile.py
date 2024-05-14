44781b06d0afe2a82dfddadd7305fa6d2ca89696
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-05-14 15:05:38 +0200

    remove stb from conanfile

--- recipes/sdf/all/test_package/conanfile.py
+++ recipes/sdf/all/test_package/conanfile.py
@@ -14,7 +14,6 @@ class TestPackageConan(ConanFile):
 
     def requirements(self):
         self.requires(self.tested_reference_str)
-        self.requires("stb/cci.20210910")
 
     def build(self):
         cmake = CMake(self)
