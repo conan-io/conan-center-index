c83f1cdda6567fb58bb8d1357bb50d2d90cf28f6
Martin Valgur <martin.valgur@gmail.com>
2024-05-16 15:15:09 +0300

    protobuf: restore "linkage" test for CMake functions in test_package

--- recipes/protobuf/all/test_package/conanfile.py
+++ recipes/protobuf/all/test_package/conanfile.py
@@ -1,12 +1,13 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+from conan.tools.env import VirtualRunEnv
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "VirtualBuildEnv", "VirtualRunEnv"
+    generators = "CMakeDeps", "VirtualBuildEnv"
     test_type = "explicit"
 
     def layout(self):
@@ -17,8 +18,11 @@ class TestPackageConan(ConanFile):
         self.requires(self.tested_reference_str, run=True)
 
     def generate(self):
+        venv = VirtualRunEnv(self)
+        venv.generate(scope="build")
+        venv.generate(scope="run")
         tc = CMakeToolchain(self)
-        tc.cache_variables["protobuf_LITE"] = self.dependencies[self.tested_reference_str].options.lite
+        tc.variables["protobuf_LITE"] = self.dependencies[self.tested_reference_str].options.lite
         tc.generate()
 
     def build(self):
@@ -28,10 +32,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(bin_path, env="conanrun")
-
-            # Invoke protoc in the same way CMake would
-            self.run(f"protoc --proto_path={self.source_folder} --cpp_out={self.build_folder} {self.source_folder}/addressbook.proto", env="conanrun")
-            assert os.path.exists(os.path.join(self.build_folder,"addressbook.pb.cc"))
-            assert os.path.exists(os.path.join(self.build_folder,"addressbook.pb.h"))
--- recipes/protobuf/all/test_package/test_package.cpp
+++ recipes/protobuf/all/test_package/test_package.cpp
@@ -4,8 +4,15 @@
 #include <google/protobuf/timestamp.pb.h>
 #include <google/protobuf/util/time_util.h>
 
+#include "addressbook.pb.h"
+
 int main()
 {
+	tutorial::Person p;
+	p.set_id(21);
+	p.set_name("conan-center-index");
+	p.set_email("info@conan.io");
+
 	google::protobuf::Timestamp ts;
 	google::protobuf::util::TimeUtil::FromString("1972-01-01T10:00:20.021Z", &ts);
 	const auto nanoseconds = ts.nanos();
