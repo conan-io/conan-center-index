05451cc76d27c08dd7017cd885d99e7f5f27b91e
Martin Valgur <martin.valgur@gmail.com>
2024-05-19 12:16:39 +0300

    imagemagick: fix v1 test_package

--- recipes/imagemagick/all/test_package/conanfile.py
+++ recipes/imagemagick/all/test_package/conanfile.py
@@ -3,6 +3,8 @@ from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake
 import os
 
+from conan.tools.files import save, load
+
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
@@ -15,6 +17,36 @@ class TestPackageConan(ConanFile):
     def layout(self):
         cmake_layout(self)
 
+    def generate(self):
+        opts = self.dependencies["imagemagick"].options
+        delegates = [
+            (opts.with_zlib, "zlib"),
+            (opts.with_bzlib, "bzlib"),
+            (opts.with_lzma, "lzma"),
+            (opts.with_lcms, "lcms"),
+            (opts.with_openexr, "openexr"),
+            (opts.with_heic, "heic"),
+            (opts.with_jbig, "jbig"),
+            (opts.with_jpeg, "jpeg"),
+            (opts.with_openjp2, "jp2"),
+            (opts.with_pango, "pangocairo"),
+            (opts.with_png, "png"),
+            (opts.with_tiff, "tiff"),
+            (opts.with_webp, "webp"),
+            (opts.with_freetype, "freetype"),
+            (opts.with_xml2, "xml"),
+            (opts.with_djvu, "djvu"),
+        ]
+        enabled = []
+        disabled = []
+        for option, delegate in delegates:
+            if option:
+                enabled.append(delegate)
+            else:
+                disabled.append(delegate)
+        save(self, os.path.join(self.generators_folder, "enabled"), " ".join(enabled))
+        save(self, os.path.join(self.generators_folder, "disabled"), " ".join(disabled))
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
@@ -25,30 +57,13 @@ class TestPackageConan(ConanFile):
             bin_path = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(bin_path, env="conanrun")
 
-            with open("delegates.txt") as f:
-                content = f.read()
+            delegates = set(load(self, "delegates.txt").strip().split())
+            expect_enabled = set(load(self, os.path.join(self.generators_folder, "enabled")).strip().split())
+            expect_disabled = set(load(self, os.path.join(self.generators_folder, "disabled")).strip().split())
 
-            def check(option, token):
-                self.output.info(f"checking feature {token}...")
-                if option:
-                    if token not in content.split():
-                        raise Exception(f"feature {token} wasn't enabled!")
-                self.output.info(f"checking feature {token}... OK!")
+            if expect_enabled - delegates:
+                raise Exception("Some expected image formats are missing: " + " ".join(expect_enabled - delegates))
+            if expect_disabled & delegates:
+                raise Exception("Some unexpected image formats are available: " + " ".join(expect_disabled & delegates))
 
-            opts = self.dependencies["imagemagick"].options
-            check(opts.with_zlib, "zlib")
-            check(opts.with_bzlib, "bzlib")
-            check(opts.with_lzma, "lzma")
-            check(opts.with_lcms, "lcms")
-            check(opts.with_openexr, "openexr")
-            check(opts.with_heic, "heic")
-            check(opts.with_jbig, "jbig")
-            check(opts.with_jpeg, "jpeg")
-            check(opts.with_openjp2, "jp2")
-            check(opts.with_pango, "pangocairo")
-            check(opts.with_png, "png")
-            check(opts.with_tiff, "tiff")
-            check(opts.with_webp, "webp")
-            check(opts.with_freetype, "freetype")
-            check(opts.with_xml2, "xml")
-            check(opts.with_djvu, "djvu")
+            print("All expected image formats are available")
