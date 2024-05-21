6bc88a6f1a8fdc988942bbc88f9bcd412c2ee37e
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-05-21 16:49:06 +0200

    new test

--- recipes/artery-font-format/all/test_package/test_package.cpp
+++ recipes/artery-font-format/all/test_package/test_package.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 
-#include <artery-font/structures.h>
+#include <artery-font/std-artery-font.h>
 
 int main(int argc, char *argv[]) {
-    artery_font::KernPair<float> kp;
-    kp.codepoint1 = 65; // ASCII value for 'A'
-    kp.codepoint2 = 66; // ASCII value for 'B'
-    kp.advance.h = 1.0f;
-    kp.advance.v = 0.0f;
-    std::cout << "Test: " << sizeof(kp) << std::endl;
-
+    auto list = artery_font::StdList<int>(5);
+    std::cout << "There are " << list.length() << " elements in the list" << std::endl;
     return EXIT_SUCCESS;
 }
