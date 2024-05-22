841fef51034338b3c9aa5885562932b06a88bd55
Croydon <git@cr0ydon.com>
2024-05-22 20:26:09 +0200

    Replace Iconv patches with replace_in_files that work in all versions
    + drop their custom FindIconv file

--- recipes/doxygen/all/conanfile.py
+++ recipes/doxygen/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm
 from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime
 from conan.tools.scm import Version
 import os
@@ -92,6 +92,12 @@ class DoxygenConan(ConanFile):
         deps.generate()
 
     def build(self):
+        rm(self, "FindIconv.cmake", os.path.join(self.source_folder, "cmake"))
+        for cmake_file in ["CMakeLists.txt", os.path.join("src", "CMakeLists.txt"), os.path.join("addon", "doxyapp", "CMakeLists.txt"), os.path.join("addon", "doxyparse", "CMakeLists.txt")]:
+            cmake_file = os.path.join(self.source_folder, cmake_file)
+            replace_in_file(self, cmake_file, "find_package(Iconv REQUIRED)\n", "find_package(Iconv REQUIRED)\nget_target_property(ICONV_INCLUDE_DIR Iconv::Iconv INTERFACE_INCLUDE_DIRECTORIES)\n", strict=False)
+            replace_in_file(self, cmake_file, "find_package(Iconv)\\n", "find_package(Iconv)\nget_target_property(ICONV_INCLUDE_DIR Iconv::Iconv INTERFACE_INCLUDE_DIRECTORIES)\n", strict=False)
+            replace_in_file(self, cmake_file, "${ICONV_LIBRARIES}", "Iconv::Iconv", strict=False)
         apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
