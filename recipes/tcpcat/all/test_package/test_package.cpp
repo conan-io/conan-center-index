bc471c4c29de931784224080a40dc7d409c2e493
ydcpp <ydemir9191@gmail.com>
2024-05-26 14:30:48 +0300

    CI test error fix

--- recipes/tcpcat/all/test_package/test_package.cpp
+++ recipes/tcpcat/all/test_package/test_package.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-   auto session = tcpcat::TcpSession(nullptr, nullptr, 0);
+   tcpcat::TcpSession session(nullptr, nullptr, 0);
    std::cout << "session id: " << session.GetId() << std::endl;
    return 0;
 }
