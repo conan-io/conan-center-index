cc35a96445e9c9e77a9873322ce928d3e4eb6659
Martin Valgur <martin.valgur@gmail.com>
2024-05-26 18:29:03 +0300

    iridescence: MSVC is not supported

--- recipes/iridescence/all/conanfile.py
+++ recipes/iridescence/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir, rename, replace_in_file
+from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 import os
 
@@ -82,6 +83,9 @@ class IridescenceConan(ConanFile):
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
 
+        if is_msvc(self):
+            # Build fails with several compilation errors
+            raise ConanInvalidConfiguration("MSVC is not supported")
         if self.settings.os == "Windows" and self.options.shared:
             raise ConanInvalidConfiguration("Shared builds are not supported on Windows")
 
