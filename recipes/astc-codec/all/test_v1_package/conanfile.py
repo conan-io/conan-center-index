04bf1a683f1e585dcdc3878c570d9446e0994d03
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-05-27 23:59:59 +0200

    astc-codec: refactor test package

--- recipes/astc-codec/all/test_v1_package/conanfile.py
+++ recipes/astc-codec/all/test_v1_package/conanfile.py
@@ -14,5 +14,4 @@ class TestPackageConan(ConanFile):
     def test(self):
         if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
-            bees = os.path.join(self.source_folder, os.pardir, "test_package", "atlas_small_4x4.astc")
-            self.run(f"{bin_path} {bees} 256 256", run_environment=True)
+            self.run(bin_path, run_environment=True)
