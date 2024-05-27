8a8c8864acfb4ba59b561f47f9745c794b091cf5
Martin Valgur <martin.valgur@gmail.com>
2024-05-27 15:09:53 +0300

    vlfeat: remove unused imports

--- recipes/vlfeat/all/conanfile.py
+++ recipes/vlfeat/all/conanfile.py
@@ -3,7 +3,7 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, export_conandata_patches, apply_conandata_patches
+from conan.tools.files import copy, get
 from conan.tools.microsoft import is_msvc
 
 required_conan_version = ">=1.53.0"
