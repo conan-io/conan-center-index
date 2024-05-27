897b89ad31aa8a9779d73b09df4225899d1c9a61
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-05-27 16:58:40 +0200

    jxrlib: refactor test package

--- recipes/jxrlib/all/test_package/test_package.c
+++ recipes/jxrlib/all/test_package/test_package.c
@@ -4,27 +4,14 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2) {
-        fprintf(stderr, "Need at least one argument\n");
-        return 1;
-    }
-
-    const char *jxr_path = argv[1];
-
     {
         ERR err = WMP_errSuccess;
-
         PKCodecFactory* pCodecFactory = NULL;
-        PKImageDecode* pDecoder = NULL;
 
         Call(PKCreateCodecFactory(&pCodecFactory, WMP_SDK_VERSION));
-        Call(pCodecFactory->CreateDecoderFromFile(jxr_path, &pDecoder));
-
-        PKPixelFormatGUID pix_frmt;
-        Call(pDecoder->GetPixelFormat(pDecoder, &pix_frmt));
+        printf("Test");
 
     Cleanup:
-        if(pDecoder) pDecoder->Release(&pDecoder);
         if(pCodecFactory) pCodecFactory->Release(&pCodecFactory);
     }
 
