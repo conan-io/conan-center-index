9edc181b7ef1d169a979efb99596812b8c0a74bd
Ernesto de Gracia Herranz <vivalaburocracia@hotmail.com>
2024-05-28 17:26:12 +0200

    laslib: refactor test package

--- recipes/laslib/all/test_package/test_package.cpp
+++ recipes/laslib/all/test_package/test_package.cpp
@@ -4,10 +4,9 @@
 int main(void) {
 
     LASreadOpener readOpener;
-    readOpener.set_file_name("test.laz");
-    LASreader* lasreader = readOpener.open();
+    readOpener.set_file_name("fake-file.laz");
 
-    std::cout << "test.laz number of points : " << lasreader->header.number_of_point_records << std::endl;
+    std::cout << "Test: " << readOpener.get_file_name() << std::endl;
 
     return EXIT_SUCCESS;
 }
