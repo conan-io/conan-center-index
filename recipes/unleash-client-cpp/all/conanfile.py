0d39aae6c6cc9f8bd0db219c0160726864b19eb5
PerseoGI <perseog@jfrog.com>
2024-05-30 14:28:47 +0200

    Removed support for v1.1.1 and cleaned old conan 1 cruft

--- recipes/unleash-client-cpp/all/conanfile.py
+++ recipes/unleash-client-cpp/all/conanfile.py
@@ -2,8 +2,10 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
+from conan.tools.files import copy, get, rmdir
 import os
+
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
 
@@ -40,16 +42,13 @@ class UnleashConan(ConanFile):
             "intel": "17",
         }
 
-    def export_sources(self):
-        export_conandata_patches(self)
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -62,21 +61,10 @@ class UnleashConan(ConanFile):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
 
-        def loose_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
-
         min_version = self._compilers_min_version.get(str(self.settings.compiler), False)
-        if min_version and loose_lt_semver(str(self.settings.compiler.version), min_version):
+        if min_version and Version(self.settings.compiler.version) < min_version:
             raise ConanInvalidConfiguration(
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
-
-        if self.settings.compiler in ["clang", "apple-clang"] and self.settings.compiler.libcxx == "libc++":
-            raise ConanInvalidConfiguration(
-                f"{self.ref} doesn't support clang with libc++. Use libstdc++ instead."
             )
 
     def source(self):
@@ -91,7 +79,6 @@ class UnleashConan(ConanFile):
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
