e5c63d01a78f864f13694ea0d0994a1481b3a33c
Uilian Ries <uilianries@gmail.com>
2024-05-30 12:17:21 +0200

    Do not build shared on Windows
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libprotobuf-mutator/all/conanfile.py
+++ recipes/libprotobuf-mutator/all/conanfile.py
@@ -49,9 +49,10 @@ class LibProtobufMutatorConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+            del self.options.shared
 
     def configure(self):
-        if self.options.shared:
+        if self.options.get_safe("shared"):
             self.options.rm_safe("fPIC")
 
     def layout(self):
