6f4bf6564e158d70dab0e96fa7a2983675db8636
toge <toge.mail@gmail.com>
2024-05-30 12:01:43 +0900

    update test_package.cpp

--- recipes/bredis/all/test_package/test_package.cpp
+++ recipes/bredis/all/test_package/test_package.cpp
@@ -8,23 +8,21 @@
 namespace r = bredis;
 namespace asio = boost::asio;
 
-static void dummy() {
+int main(int argc, const char** argv) {
   using socket_t = asio::ip::tcp::socket;
   using next_layer_t = socket_t;
 
   asio::io_context io_service;
-  auto             ip_address = asio::ip::make_address("127.0.0.1");
-  auto             port       = boost::lexical_cast<std::uint16_t>("6379");
-  std::cout << "connecting to " << ip_address << "\n";
+
+  auto ip_address = asio::ip::make_address("127.0.0.1");
+  auto port = boost::lexical_cast<std::uint16_t>("6379");
+
   asio::ip::tcp::endpoint end_point(ip_address, port);
-  socket_t                socket(io_service, end_point.protocol());
-  socket.connect(end_point);
-  std::cout << "connected\n";
+
+  socket_t socket(io_service, end_point.protocol());
 
   // wrap it into bredis connection
   r::Connection<next_layer_t> c(std::move(socket));
-}
 
-int main(int argc, const char** argv) {
   return 0;
 }
