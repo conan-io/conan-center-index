64690eba2cd7b07277fdbb527e907b1bc15f87ea
Martin Valgur <martin.valgur@gmail.com>
2024-05-30 14:33:50 +0300

    pagmo2: bump boost and onetbb versions

--- recipes/pagmo2/all/conanfile.py
+++ recipes/pagmo2/all/conanfile.py
@@ -58,8 +58,8 @@ class Pagmo2Conan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.83.0", transitive_headers=True)
-        self.requires("onetbb/2021.10.0")
+        self.requires("boost/1.85.0", transitive_headers=True)
+        self.requires("onetbb/2021.12.0")
         if self.options.with_eigen:
             self.requires("eigen/3.4.0", transitive_headers=True)
         if self.options.with_nlopt:
