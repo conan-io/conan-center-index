43cfe13eecb915c558cdf603409fa4e277f2c3ab
toge <toge.mail@gmail.com>
2024-06-01 13:03:27 +0900

    drop support gcc<7 in cci.20240531

--- recipes/asmjit/all/conanfile.py
+++ recipes/asmjit/all/conanfile.py
@@ -2,6 +2,8 @@ from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir
+from conan.tools.scm import Version
+from conan.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.53.0"
@@ -27,6 +29,16 @@ class AsmjitConan(ConanFile):
         "fPIC": True,
     }
 
+    @property
+    def _min_cppstd(self):
+        return 11
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "7",
+        }
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -40,7 +52,14 @@ class AsmjitConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, 11)
+            check_min_cppstd(self, self._min_cppstd)
+
+        if self.version >= "cci.20240531":
+            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+            if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration(
+                    f"{self.ref} does not support {self.settings.compiler}/{self.settings.compiler.version}."
+                )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
