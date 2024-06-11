67fa281c3595daf6a873f672bd67b9cb06a8e41c
Rubén Rincón Blanco <git@rinconblanco.es>
2024-06-11 16:20:33 +0200

    Remove windows support

--- recipes/rebound/all/conanfile.py
+++ recipes/rebound/all/conanfile.py
@@ -1,10 +1,9 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.env import Environment
 from conan.tools.files import chdir, copy, get, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import is_msvc, unix_path
 import os
 
 
@@ -26,46 +25,22 @@ class ReboundConan(ConanFile):
         return getattr(self, "settings_build", self.settings)
 
     def configure(self):
-        # for plain C projects only
         self.settings.rm_safe("compiler.cppstd")
         self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
         basic_layout(self, src_folder="src")
 
+    def validate(self):
+        if self.settings.os == "Windows":
+            raise ConanInvalidConfiguration(f"{self.ref} recipe does not support Windows, contributions welcomed!")
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-
-    def build_requirements(self):
-        if self._settings_build.os == "Windows":
-            self.win_bash = True
-            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
-                self.tool_requires("msys2/cci.latest")
-        if is_msvc(self):
-            self.tool_requires("automake/1.16.5")
 
     def generate(self):
         tc = AutotoolsToolchain(self)
         tc.generate()
-
-        # If Visual Studio is supported
-        if is_msvc(self):
-            env = Environment()
-            # get compile & ar-lib from automake (or eventually lib source code if available)
-            # it's not always required to wrap CC, CXX & AR with these scripts, it depends on how much love was put in
-            # upstream build files
-            automake_conf = self.dependencies.build["automake"].conf_info
-            compile_wrapper = unix_path(self, automake_conf.get("user.automake:compile-wrapper", check_type=str))
-            ar_wrapper = unix_path(self, automake_conf.get("user.automake:lib-wrapper", check_type=str))
-            env.define("CC", f"{compile_wrapper} cl -nologo")
-            env.define("CXX", f"{compile_wrapper} cl -nologo")
-            env.define("LD", "link -nologo")
-            env.define("AR", f"{ar_wrapper} \"lib -nologo\"")
-            env.define("NM", "dumpbin -symbols")
-            env.define("OBJDUMP", ":")
-            env.define("RANLIB", ":")
-            env.define("STRIP", ":")
-            env.vars(self).save_script("conanbuild_msvc")
 
     def build(self):
         with chdir(self, self.source_folder):
