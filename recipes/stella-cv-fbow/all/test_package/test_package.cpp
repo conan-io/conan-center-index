1ee9bd1d37084e9eb5c523268d3036d716a5586b
PerseoGI <perseog@jfrog.com>
2024-06-11 12:55:45 +0200

    Print fbow output on test_package avoiding possible compiler optimization

--- recipes/stella-cv-fbow/all/test_package/test_package.cpp
+++ recipes/stella-cv-fbow/all/test_package/test_package.cpp
@@ -1,5 +1,10 @@
 #include <fbow/fbow.h>
+#include <fbow/cpu.h>
+#include <iostream>
 
 int main() {
-    fbow::cpu().detect_host();
+    auto features = fbow::cpu();
+    features.detect_host();
+    std::cout << "CPU Vendor String: " << fbow::cpu::get_vendor_string() << '\n';
+    std::cout << "64-bit = " << features.OS_x64 << '\n';
 }
