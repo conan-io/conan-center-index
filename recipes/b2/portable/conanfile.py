89ccc2a8fb02b7ba103e86c7a56e840a91fe40a1
Rene Rivera <grafikrobot@gmail.com>
2024-06-11 08:33:46 -0500

    B2 5.1.0 and 5.2.0

--- recipes/b2/portable/conanfile.py
+++ recipes/b2/portable/conanfile.py
@@ -193,6 +193,7 @@ class B2Conan(ConanFile):
 
     def package(self):
         copy(self, "LICENSE.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.LICENSE", src=self._b2_engine_dir, dst=os.path.join(self.package_folder, "licenses"))
         copy(self, "*b2", dst=self._pkg_bin_dir, src=self._b2_output_dir)
         copy(self, "*b2.exe", dst=self._pkg_bin_dir, src=self._b2_output_dir)
         copy(self, "*.jam", dst=self._pkg_bin_dir, src=self._b2_output_dir)
