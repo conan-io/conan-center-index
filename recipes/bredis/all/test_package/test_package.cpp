e46b54eed4e075d8eb80520cfd461115c532bbfa
PerseoGI <perseog@jfrog.com>
2024-06-11 15:59:59 +0200

    Remove network access in test_package

--- recipes/bredis/all/test_package/test_package.cpp
+++ recipes/bredis/all/test_package/test_package.cpp
@@ -1,28 +1,12 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "boost/asio.hpp"
-#include "bredis/MarkerHelpers.hpp"
 #include "bredis/Connection.hpp"
+#include "bredis/MarkerHelpers.hpp"
 
-namespace r = bredis;
-namespace asio = boost::asio;
-
-int main(int argc, const char** argv) {
-  using socket_t = asio::ip::tcp::socket;
-  using next_layer_t = socket_t;
-
-  asio::io_context io_service;
-
-  auto ip_address = asio::ip::make_address("127.0.0.1");
-  auto port = boost::lexical_cast<std::uint16_t>("6379");
-
-  asio::ip::tcp::endpoint end_point(ip_address, port);
-
-  socket_t socket(io_service, end_point.protocol());
-
-  // wrap it into bredis connection
-  r::Connection<next_layer_t> c(std::move(socket));
-
+int main(int argc, const char **argv) {
+  // Basic test avoiding any kind of access to network layer
+  bredis::bredis_category category;
+  std::cout << category.name() << '\n';
   return 0;
 }
