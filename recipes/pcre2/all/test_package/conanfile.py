f517621b78167b7a54c4eb1cfda1fbcdd1c044cc
Daniel <danimanzaneque@gmail.com>
2024-06-12 17:00:32 +0200

    Update recipes/pcre2/all/test_package/conanfile.py

--- recipes/pcre2/all/test_package/conanfile.py
+++ recipes/pcre2/all/test_package/conanfile.py
@@ -2,7 +2,6 @@ from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
 import os
-from io import StringIO
 
 
 class TestPackageConan(ConanFile):
