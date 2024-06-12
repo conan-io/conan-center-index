bcd3701c7943a493f6f6292413c5a7650310d480
Daniel <danimanzaneque@gmail.com>
2024-06-12 17:00:16 +0200

    Update recipes/pcre2/all/test_package/conanfile.py

--- recipes/pcre2/all/test_package/conanfile.py
+++ recipes/pcre2/all/test_package/conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.files import save, load
 from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.scm import Version
 import os
