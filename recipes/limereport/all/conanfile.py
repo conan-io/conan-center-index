d34c4a8328edf273e55a34c9ee73bf85cc6b88d9
Uilian Ries <uilianries@gmail.com>
2024-06-12 12:41:02 +0200

    copy all licenses files
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/limereport/all/conanfile.py
+++ recipes/limereport/all/conanfile.py
@@ -104,7 +104,8 @@ class LimereportConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "COPYING*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
