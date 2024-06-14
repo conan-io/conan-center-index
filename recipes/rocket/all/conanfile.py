b0988e46768eeaf7a66934e5dd29f5e8697ec09c
PerseoGI <perseog@jfrog.com>
2024-06-14 14:10:34 +0200

    Added support for conan V1, add verify and license

--- recipes/rocket/all/conanfile.py
+++ recipes/rocket/all/conanfile.py
@@ -1,29 +1,48 @@
 from conan import ConanFile
-from conan.tools.files import copy
-from conan.tools.scm import Git
+from conan.tools.files import copy, get, save, load
+from conan.tools.build import check_min_cppstd
 import os
 
-required_conan_version = ">=2.0.0"
+required_conan_version = ">=1.53.0"
+
 
 class RocketConan(ConanFile):
     name = "rocket"
     description = "Fast single header signal/slots library for C++"
-    license = "public domain"
-    topics = ("signal-slots", "observer-pattern")
-    homepage = "https://github.com/tripleslash/rocket"
+    license = "DocumentRef-README.md:LicenseRef-Rocket-public-domain"
     url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/tripleslash/rocket"
+    topics = ("signal-slots", "observer-pattern")
     package_type = "header-library"
+    settings = "os", "compiler", "build_type", "arch"
+
+    @property
+    def _min_cppstd(self):
+        return 11
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
 
     def source(self):
-        git = Git(self)
-        git.clone(url="https://github.com/tripleslash/rocket.git", target=".")
-        git.checkout(**self.conan_data["sources"][self.version])
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def package_id(self):
+        self.info.clear()
 
     def build(self):
         pass
 
+    def _extract_license(self):
+        readme_content = load(self, os.path.join(self.source_folder, "README.md"))
+        license_content = "\n".join(readme_content.splitlines()[:6])
+        save(self, os.path.join(self.package_folder, "licenses", "LICENSE"), license_content)
+
     def package(self):
+        self._extract_license()
         copy(self, "rocket.hpp", self.build_folder, os.path.join(self.package_folder, "include"))
 
     def package_info(self):
-        self.cpp_info.libs = ["rocket"]
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
--- recipes/rocket/all/test_package/conanfile.py
+++ recipes/rocket/all/test_package/conanfile.py
@@ -1,13 +1,16 @@
-import os
-
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.build import can_run
+from conan.tools.cmake import CMake, cmake_layout
+import os
 
 
 class RocketTestConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
 
     def requirements(self):
         self.requires(self.tested_reference_str)
@@ -17,10 +20,7 @@ class RocketTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def layout(self):
-        cmake_layout(self)
-
     def test(self):
         if can_run(self):
-            cmd = os.path.join(self.cpp.build.bindir, "example")
-            self.run(cmd, env="conanrun")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
