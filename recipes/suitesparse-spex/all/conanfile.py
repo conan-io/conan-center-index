7ef4a902ea3bc63ef635fc1759928c49dc67af5e
Martin Valgur <martin.valgur@gmail.com>
2024-06-14 14:04:08 +0300

    suitesparse-spex: fix license id

--- recipes/suitesparse-spex/all/conanfile.py
+++ recipes/suitesparse-spex/all/conanfile.py
@@ -11,7 +11,7 @@ required_conan_version = ">=1.53.0"
 class SuiteSparseSpexConan(ConanFile):
     name = "suitesparse-spex"
     description = "SPEX: Software package for SParse EXact algebra in SuiteSparse"
-    license = "GPL-2.0-or-later or LGPL-3.0-or-later"
+    license = "GPL-2.0-or-later OR LGPL-3.0-or-later"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://people.engr.tamu.edu/davis/suitesparse.html"
     topics = ("mathematics", "sparse-matrix", "linear-algebra", "exact-linear-algebra")
