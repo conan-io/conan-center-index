a1ef89403bd43ebe3c04d8a506d5a3c4119e8006
Jak_o_Shadows <jak-o-shadows@users.noreply.github.com>
2024-06-30 14:51:48 +0930

    Change to only latest version, add newlines to end of files, changes to make CIwork, responding to feedback on PR#22647

--- recipes/hfsm2/all/conanfile.py
+++ recipes/hfsm2/all/conanfile.py
@@ -6,20 +6,19 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.layout import basic_layout
 from conan.tools.files import get
 
-required_conan_version = ">=2.0.16"
+required_conan_version = ">=1.54.0"
 
 class Hfsm2Conan(ConanFile):
     name = "hfsm2"
     description = "High-Performance Hierarchical Finite State Machine Framework"
     license = "MIT"
-    topics = ("embedded", "fsm", "state-machine", "cpp" "modern-cpp", "game-development",
+    topics = ("embedded", "fsm", "state-machine", "cpp", "modern-cpp", "game-development",
               "cpp11", "embedded-systems", "template-metaprogramming", "header-only",
               "mit-license", "fsm-library", "hierarchical-state-machine", "game-dev", "hfsm")
     homepage = "https://hfsm.dev/"
     url = "https://github.com/conan-io/conan-center-index"
 
     package_type = "header-library"
-    exports_sources = "include/*"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
@@ -34,8 +33,8 @@ class Hfsm2Conan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        # This will also copy the "include" folder
-        copy(self, os.path.join(self.exports_sources, "*.hpp"), self.source_folder, self.package_folder)
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.hpp", self.source_folder, self.package_folder)
 
     def package_info(self):
         # For header-only packages, libdirs and bindirs are not used
