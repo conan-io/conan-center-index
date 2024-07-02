4f5e0eab92ddbbfb0a5502aa42fbc3c6f6afac41
Andrew Marshall <andrew.marshall3@sky.uk>
2024-07-02 12:03:25 +0100

    [pact_ffi] error if ninja generator, support static and shared libs

--- recipes/pact_ffi/all/conanfile.py
+++ recipes/pact_ffi/all/conanfile.py
@@ -1,10 +1,13 @@
 from conan import ConanFile
-from conan.errors import ConanException, ConanInvalidConfiguration
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.files import get, copy, replace_in_file
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
+from conan.tools.files import get, copy, replace_in_file, rmdir, rm
+from conan.tools.layout import basic_layout
 
+from pathlib import Path
 import os
+
 
 required_conan_version = ">=2.0.0"
 
@@ -27,6 +30,24 @@ class PactFFIConan(ConanFile):
         "shared": False
     }
 
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def validate(self):
+        if self.settings.build_type in ["RelWithDebInfo", "MinSizeRel"]:
+            raise ConanInvalidConfiguration("build_type must be Release or Debug")
+        if self.conf.get("tools.cmake.cmaketoolchain:generator") == "Ninja":
+            raise ConanInvalidConfiguration("The Ninja generator is not supported. Define "
+                                            "'pact_ffi/*:tools.cmake.cmaketoolchain:generator=!' in a profile to unset it")
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
     def source(self):
         get(self, f"https://github.com/pact-foundation/pact-reference/archive/refs/tags/libpact_ffi-v{self.version}.tar.gz",
             strip_root=True)
@@ -41,7 +62,7 @@ class PactFFIConan(ConanFile):
     def build(self):
         replace_in_file(self, os.path.join(self.source_folder, "rust", "pact_ffi", "CMakeLists.txt"),
                         'set(CARGO_TARGET_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../target")',
-                        'set(CARGO_TARGET_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../target" CACHE STRING "")')
+                        'set(CARGO_TARGET_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../target" CACHE STRING "")', strict=False)
         cmake = CMake(self)
         cmake.configure(build_script_folder=os.path.join(self.source_folder, "rust", "pact_ffi"))
         cmake.build()
@@ -60,20 +81,27 @@ class PactFFIConan(ConanFile):
                  cwd=os.path.join(self.source_folder, "rust", "pact_ffi"))
 
     def package(self):
-        copy(self,
-             "libpact_ffi*",
-             os.path.join(self.build_folder, "rust", "target", "release"),
-             os.path.join(self.package_folder, "lib")
-             )
+        cmake = CMake(self)
+        cmake.install()
 
-        copy(self,
-             "pact*.h",
-             os.path.join(self.source_folder, "rust", "pact_ffi", "include"),
-             os.path.join(self.package_folder, "include"))
+        # Pact will install shared libraries itself, but if we want static libs we have to do it manually
+        package_folder = Path(self.package_folder)
+        lib_folder = package_folder / "lib"
+        if not self.options.shared:
+            target_folder = Path(self.build_folder) / "rust" / "target" / str(self.settings.build_type).lower()
+            copy(self, pattern="*.a", src=target_folder, dst=lib_folder)
+            rm(self, pattern="*.so", folder=lib_folder)
+            rm(self, pattern="*.dylib", folder=lib_folder)
+        else:
+            fix_apple_shared_install_name(self)
 
-        fix_apple_shared_install_name(self)
+        # Don't include package generated cmake files as conan will generate its own
+        rmdir(self, lib_folder / "cmake")
+        copy(self, pattern="LICENSE", dst=package_folder / "licenses", src=self.source_folder)
 
     def package_info(self):
         self.cpp_info.libs = ["pact_ffi"]
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("m")
+        elif self.settings.os == "Macos":
+            self.cpp_info.frameworks.extend(["Security", "IOKit", "CoreFoundation", "SystemConfiguration"])
