2e1d41fc5cf951aa7ae2eda5a9eb87d419b2da38
Martin Valgur <martin.valgur@gmail.com>
2024-07-04 13:53:17 +0300

    reproc: require GCC 6

--- recipes/reproc/all/conanfile.py
+++ recipes/reproc/all/conanfile.py
@@ -1,9 +1,11 @@
 import os
 
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rm, rmdir
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
 
@@ -48,6 +50,9 @@ class PackageConan(ConanFile):
     def validate(self):
         if self.options.cxx and self.settings.compiler.cppstd:
             check_min_cppstd(self, 11)
+        if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "6":
+            # drain.hpp:55:43: error: ‘stream’ is not a class, namespace, or enumeration
+            raise ConanInvalidConfiguration("GCC < 6 is not supported")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
