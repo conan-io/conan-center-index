99a6cf5de31737b0eae0e300d137dc8568b94303
Ingmar Rieger <git@irieger.net>
2024-07-04 18:40:08 +0200

    Cleanup & first checks

--- recipes/libsvtjpegxs/all/conanfile.py
+++ recipes/libsvtjpegxs/all/conanfile.py
@@ -1,5 +1,7 @@
 import os
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps, CMake
 from conan.tools.files import copy, get, rmdir, apply_conandata_patches, export_conandata_patches
 from conan.tools.scm import Version
@@ -41,13 +43,21 @@ class SvtJpegXsConan(ConanFile):
 
     def requirements(self):
         pass
+        # if not self.settings.arch in ("x86", "x86_64"):
+        #     self.tool_requires("simde/0.8.2")
 
     def build_requirements(self):
-        if Version(self.version) >= "1.3.0":
-            self.tool_requires("cmake/[>=3.16 <4]")
+        self.tool_requires("cmake/[>=3.16 <4]")
         # if self.settings.arch in ("x86", "x86_64"):
         #     self.tool_requires("nasm/2.15.05")
         self.tool_requires("yasm/1.3.0")
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 11)
+
+        if not self.settings.arch in ("x86", "x86_64"):
+            raise ConanInvalidConfiguration("libsvtjpegxs currently only supports x86 with SSE or AVX support")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
