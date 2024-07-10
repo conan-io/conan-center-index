6dea8d86a1641aea8108b74e24d06686c6a19881
Guillaume Egles <gegles@us.ibm.com>
2024-07-10 13:47:11 -0700

    restinio: bump 3ps

--- recipes/restinio/v0.7/conanfile.py
+++ recipes/restinio/v0.7/conanfile.py
@@ -36,13 +36,13 @@ class RestinioConan(ConanFile):
 
     def requirements(self):
         self.requires("llhttp/9.1.3")
-        self.requires("fmt/10.2.1")
+        self.requires("fmt/11.0.2")
         self.requires("expected-lite/0.8.0")
 
         if self.options.asio == "standalone":
-            self.requires("asio/1.30.2")
+            self.requires("asio/1.31.0")
         else:
-            self.requires("boost/1.85.0")
+            self.requires("boost/1.86.0")
 
         if self.options.with_openssl:
             self.requires("openssl/[>=1.1 <4]")
