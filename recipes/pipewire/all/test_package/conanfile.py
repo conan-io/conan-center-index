406ee4c5abefe5684657b3349d4da0faac1b6019
Jordan Williams <jordan@jwillikers.com>
2024-07-15 08:32:47 -0500

    Use version range for Meson

--- recipes/pipewire/all/test_package/conanfile.py
+++ recipes/pipewire/all/test_package/conanfile.py
@@ -19,7 +19,7 @@ class TestPackageConan(ConanFile):
         self.requires(self.tested_reference_str)
 
     def build_requirements(self):
-        self.tool_requires("meson/1.4.0")
+        self.tool_requires("meson/[>=1.2.0 <2]")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/2.2.0")
 
