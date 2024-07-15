01b4b93f1198442bcd9461c9736c7d4ee6d4a294
Martin Valgur <martin.valgur@gmail.com>
2024-07-15 13:36:23 +0300

    nativefiledialog: downgrade premake dep

--- recipes/nativefiledialog/all/conanfile.py
+++ recipes/nativefiledialog/all/conanfile.py
@@ -49,7 +49,7 @@ class NativefiledialogConan(ConanFile):
             raise ConanInvalidConfiguration(f"{self.settings.arch} architecture is not supported")
 
     def build_requirements(self):
-        self.tool_requires("premake/5.0.0-beta2")
+        self.tool_requires("premake/5.0.0-alpha15")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
