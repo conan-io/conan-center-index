ed3d1fbcb6e5b1873460d983ffebe82c468d48c9
Martin Valgur <martin.valgur@gmail.com>
2024-07-15 16:05:18 +0300

    lerc: fix test_package.c

--- recipes/lerc/all/test_package/test_package.c
+++ recipes/lerc/all/test_package/test_package.c
@@ -4,7 +4,7 @@
 
 int main() {
   const uint32_t numBytesBlob = 275714;
-  uint8_t pLercBlob[numBytesBlob];
+  uint8_t pLercBlob[1000000];
   uint32_t infoArr[10];
   double dataRangeArr[3];
   lerc_getBlobInfo(&pLercBlob[0], numBytesBlob, infoArr, dataRangeArr, 10, 3);
