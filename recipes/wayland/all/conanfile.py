021d92799ac329cbdfdce64ecf73bae0a48a5df9
Jordan Williams <jordan@jwillikers.com>
2024-07-15 09:54:22 -0500

    Bump pkgconf

--- recipes/wayland/all/conanfile.py
+++ recipes/wayland/all/conanfile.py
@@ -60,7 +60,7 @@ class WaylandConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("meson/1.4.0")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.1.0")
+            self.tool_requires("pkgconf/2.2.0")
         if not can_run(self):
             self.tool_requires(str(self.ref))
 
