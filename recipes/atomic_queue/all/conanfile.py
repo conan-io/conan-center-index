3585cf5e06a603ec54212dbac245e16899c9204e
Luiz Feldmann <gustavo.feldmann@hotmail.com>
2024-07-15 17:13:23 -0300

    (#24625) atomic_queue: add recipe
    * New recipe for library atomic_queue/1.6.3
    
    * The entire test_v1_package folder can be removed. It's no longer needed for new recipes in CCI.

---
+++ recipes/atomic_queue/all/conanfile.py
@@ -0,0 +1,46 @@
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+import os
+
+required_conan_version = ">=1.50.0"
+
+class CxxOptsConan(ConanFile):
+    name = "atomic_queue"
+    homepage = "https://github.com/max0x7ba/atomic_queue"
+    url = "https://github.com/conan-io/conan-center-index"
+    description = "Multiple-producer-multiple-consumer lock-free queues based on circular buffer and std::atomic."
+    license = "MIT"
+    topics = ("queue", "lockfree", "atomic")
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
+
+    @property
+    def _min_cppstd(self):
+        return 14
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def build(self):
+        pass
+
+    def package(self):
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, pattern="*", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
