6fb764eb28f63d71947b0108ba32f0ce4f1ed463
Martin Valgur <martin.valgur@gmail.com>
2024-07-17 19:04:05 +0300

    openmp: test package_info() output via PkgConfigDeps

--- recipes/openmp/all/test_package/conanfile.py
+++ recipes/openmp/all/test_package/conanfile.py
@@ -7,11 +7,15 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "CMakeToolchain", "PkgConfigDeps", "VirtualBuildEnv", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+
+    def build_requirements(self):
+        if not self.conf.get("tools.gnu:pkg_config", check_type=str):
+            self.tool_requires("pkgconf/2.2.0")
 
     def layout(self):
         cmake_layout(self)
@@ -33,6 +37,6 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            for executable in ["test_package_cxx", "test_package_c"]:
+            for executable in ["test_package_cxx", "test_package_c", "test_package_pkgconfig"]:
                 bin_path = os.path.join(self.cpp.build.bindir, executable)
                 self.run(bin_path, env="conanrun")
