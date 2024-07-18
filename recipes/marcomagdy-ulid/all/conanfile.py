47d2bc89f09956f8e2a3fca2d3131442820265e4
Abril Rincón Blanco <git@rinconblanco.es>
2024-07-18 10:54:46 +0200

    Refactor, rename and fix

--- recipes/marcomagdy-ulid/all/conanfile.py
+++ recipes/marcomagdy-ulid/all/conanfile.py
@@ -1,20 +1,20 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.microsoft import check_min_vs, is_msvc_static_runtime, is_msvc
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file
+from conan.tools.microsoft import check_min_vs, is_msvc
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir
 from conan.tools.build import check_min_cppstd
 from conan.tools.scm import Version
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.env import VirtualBuildEnv
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
 
 
 required_conan_version = ">=1.53.0"
 
+
 class UlidConan(ConanFile):
-    name = "ulid"
+    name = "marcomagdy-ulid"
     description = "Lightweight ulid implementation in C++ suitable for embedded environments"
-    license = "The Unlicense"
+    license = "Unlicense"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/marcomagdy/ulid"
     topics = ("ulid", "id", "embedded")
@@ -47,9 +47,11 @@ class UlidConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+            del self.options.shared
+            self.package_type = "static-library"
 
     def configure(self):
-        if self.options.shared:
+        if self.options.get_safe("shared"):
             self.options.rm_safe("fPIC")
 
     def layout(self):
@@ -65,8 +67,6 @@ class UlidConan(ConanFile):
                 raise ConanInvalidConfiguration(
                     f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
                 )
-        if is_msvc(self) and self.options.shared:
-            raise ConanInvalidConfiguration(f"{self.ref} can not be built as shared on Visual Studio and msvc.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -87,8 +87,7 @@ class UlidConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, pattern="LICENSE", dst=os.path.join(
-            self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         cmake = CMake(self)
         cmake.install()
 
@@ -101,7 +100,5 @@ class UlidConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["ulid"]
-        self.cpp_info.filenames["cmake_find_package"] = "ULID"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "ulid"
-        self.cpp_info.names["cmake_find_package"] = "ULID"
-        self.cpp_info.names["cmake_find_package_multi"] = "ulid"
+        self.cpp_info.set_property("cmake_file_name", "ulid")
+        self.cpp_info.set_property("cmake_target_name", "ULID::ulid")
