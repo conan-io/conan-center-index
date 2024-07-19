2c6444423609c6459874197bbb9feb45ee16b8b8
Sven Roederer <S.Roederer@colvistec.de>
2024-07-19 15:38:47 +0200

    qt: QML-test: drop creating a log file

--- recipes/qt/6.x.x/test_package/test_qml_modules.cpp
+++ recipes/qt/6.x.x/test_package/test_qml_modules.cpp
@@ -14,50 +14,39 @@ int main(int argc, char *argv[])
     QStringList modulePaths = engine.importPathList();
     QString rootQMLModules;
     QString logMessage;
-    QFile file("test_qml_module_output.txt");
-    if (file.open(QIODevice::Text | QIODevice::ReadWrite))
+
+    for (auto &modulePath: modulePaths)
     {
-        QTextStream stream(&file);
-        for (auto &modulePath: modulePaths)
+        if (QDir(modulePath).exists())
         {
-            if (QDir(modulePath).exists())
+            if (QDir(modulePath).dirName() == "qml")
             {
-                if (QDir(modulePath).dirName() == "qml")
-                {
-                    logMessage = "QML Module Directory: ";
-                    qDebug() <<  logMessage << modulePath;
-                    stream << logMessage << modulePath << "\n";
+                logMessage = "QML Module Directory: ";
+                qDebug() <<  logMessage << modulePath;
 
-                    rootQMLModules = modulePath;
-                    break;
-                }
-            }
-        }
-
-        QDir dir (rootQMLModules);
-        QFileInfoList list = dir.entryInfoList(QDir::Dirs| QDir::NoSymLinks | QDir::NoDotAndDotDot);
-
-        if (list.empty())
-        {
-            logMessage = "No QML Modules are found!";
-            qDebug() << logMessage;
-            stream << logMessage  << "\n";
-        }
-        else
-        {
-            logMessage = "List of QML Modules: ";
-            qDebug() << logMessage;
-            stream << logMessage << "\n";
-            for (auto &l : list)
-            {
-                qDebug() << l.baseName();
-                stream << l.baseName() << "\n";
+                rootQMLModules = modulePath;
+                break;
             }
         }
     }
 
-    file.close();
+    QDir dir (rootQMLModules);
+    QFileInfoList list = dir.entryInfoList(QDir::Dirs| QDir::NoSymLinks | QDir::NoDotAndDotDot);
 
+    if (list.empty())
+    {
+        logMessage = "No QML Modules are found!";
+        qDebug() << logMessage;
+    }
+    else
+    {
+        logMessage = "List of QML Modules: ";
+        qDebug() << logMessage;
+        for (auto &l : list)
+        {
+            qDebug() << l.baseName();
+        }
+    }
 
     return a.exec();
 }
