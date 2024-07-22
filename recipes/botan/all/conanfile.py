8169b4694401e43941611351dc54d9b50ae17fe7
René Meusel <rene.meusel@rohde-schwarz.com>
2024-07-22 11:15:34 +0200

    (#24638) Botan: add version 3.5.0
    * botan: add 3.5.0
    
    * botan: support --{enable/disable}-{experimental/deprecated}-featurs

--- recipes/botan/all/conanfile.py
+++ recipes/botan/all/conanfile.py
@@ -53,6 +53,8 @@ class BotanConan(ConanFile):
         "with_powercrypto": [True, False],
         "enable_modules": [None, "ANY"],
         "disable_modules": [None, "ANY"],
+        "enable_experimental_features": [True, False],
+        "enable_deprecated_features": [True, False],
         "system_cert_bundle": [None, "ANY"],
         "module_policy": [None, "bsi", "modern", "nist"],
     }
@@ -81,6 +83,8 @@ class BotanConan(ConanFile):
         "with_powercrypto": True,
         "enable_modules": None,
         "disable_modules": None,
+        "enable_experimental_features": False,
+        "enable_deprecated_features": True,
         "system_cert_bundle": None,
         "module_policy": None,
     }
@@ -335,6 +339,20 @@ class BotanConan(ConanFile):
 
         if self._extra_cxxflags:
             botan_extra_cxx_flags.append(self._extra_cxxflags)
+
+        if Version(self.version) >= '3.4':
+            # Botan 3.4.0 introduced a 'module life cycle' feature, before that
+            # the experimental/deprecated feature switches are ignored.
+
+            if self.options.enable_experimental_features:
+                build_flags.append('--enable-experimental-features')
+            else:
+                build_flags.append('--disable-experimental-features')
+
+            if self.options.enable_deprecated_features:
+                build_flags.append('--enable-deprecated-features')
+            else:
+                build_flags.append('--disable-deprecated-features')
 
         if self.options.enable_modules:
             build_flags.append('--minimized-build')
