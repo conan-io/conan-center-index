9ec5cb87f2b462bf011129ec07b1c0cd02282f9e
toge <toge.mail@gmail.com>
2024-07-29 00:12:47 +0900

    librdkafka: add version 2.5.0

--- recipes/librdkafka/all/conanfile.py
+++ recipes/librdkafka/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.gnu import PkgConfigDeps
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.55.0"
