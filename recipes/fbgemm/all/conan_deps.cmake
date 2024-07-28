c60215bbab404837bc163872de381ab240d6064a
Martin Valgur <martin.valgur@gmail.com>
2024-07-28 21:35:43 +0300

    fbgemm: bump to v0.8.0

--- recipes/fbgemm/all/conan_deps.cmake
+++ recipes/fbgemm/all/conan_deps.cmake
@@ -1,2 +1,3 @@
 find_package(asmjit REQUIRED CONFIG)
 find_package(cpuinfo REQUIRED CONFIG)
+link_libraries(asmjit::asmjit cpuinfo::cpuinfo)
--- recipes/fbgemm/all/conanfile.py
+++ recipes/fbgemm/all/conanfile.py
@@ -2,10 +2,10 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd
+from conan.tools.build import check_min_cppstd, valid_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import copy, export_conandata_patches, get, apply_conandata_patches, replace_in_file, save, rmdir
+from conan.tools.files import copy, export_conandata_patches, get, apply_conandata_patches, rmdir, replace_in_file
 from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
@@ -62,8 +62,9 @@ class FbgemmConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("asmjit/cci.20210306")
+        self.requires("asmjit/cci.20240531")
         self.requires("cpuinfo/cci.20231129")
+        # TODO: #22360
         # self.requires("openmp/system")
 
     def validate(self):
@@ -85,40 +86,24 @@ class FbgemmConan(ConanFile):
         tc = CMakeToolchain(self)
         tc.variables["CMAKE_PROJECT_fbgemm_INCLUDE"] = "conan_deps.cmake"
         tc.variables["FBGEMM_LIBRARY_TYPE"] = "shared" if self.options.shared else "static"
+        tc.variables["FBGEMM_BUILD_FBGEMM_GPU"] = False
         tc.variables["FBGEMM_BUILD_TESTS"] = False
         tc.variables["FBGEMM_BUILD_BENCHMARKS"] = False
         tc.variables["FBGEMM_BUILD_DOCS"] = False
-        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+        tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_OpenMP"] = True
+        if not valid_min_cppstd(self, self._min_cppstd):
+            tc.variables["CMAKE_CXX_STANDARD"] = self._min_cppstd
         tc.generate()
 
         deps = CMakeDeps(self)
-        deps.set_property("asmjit", "cmake_target_name", "asmjit")
-        deps.set_property("cpuinfo", "cmake_target_name", "cpuinfo")
         deps.generate()
 
         VirtualBuildEnv(self).generate()
 
     def _patch_sources(self):
         apply_conandata_patches(self)
-
-        cmakelists = os.path.join(self.source_folder, "CMakeLists.txt")
-        # Don't inject definitions from upstream dependencies
-        replace_in_file(self, cmakelists, "target_compile_definitions(fbgemm_generic PRIVATE ASMJIT_STATIC)", "")
-        replace_in_file(self, cmakelists, "target_compile_definitions(fbgemm_avx2 PRIVATE ASMJIT_STATIC)", "")
-        replace_in_file(self, cmakelists, "target_compile_definitions(fbgemm_avx512 PRIVATE ASMJIT_STATIC)", "")
-        # Honor runtime from profile
-        replace_in_file(self, cmakelists, 'if(${flag_var} MATCHES "/MD")', "if(0)")
-        # Do not install stuff related to dependencies
-        replace_in_file(self, cmakelists, 'if(MSVC)\n  if(FBGEMM_LIBRARY_TYPE STREQUAL "shared")', "if(0)\nif(0)")
-        # Properly link to obj targets
-        replace_in_file(self, cmakelists,
-                              "add_dependencies(fbgemm asmjit cpuinfo)",
-                              "target_link_libraries(fbgemm_generic asmjit cpuinfo)\n"
-                              "target_link_libraries(fbgemm_avx2 asmjit cpuinfo)\n"
-                              "target_link_libraries(fbgemm_avx512 asmjit cpuinfo)")
-        # Disable third-party subdirs
-        save(self, os.path.join(self.source_folder, "third_party", "asmjit", "CMakeLists.txt"), "")
-        save(self, os.path.join(self.source_folder, "third_party", "cpuinfo", "CMakeLists.txt"), "")
+        rmdir(self, os.path.join(self.source_folder, "third_party"))
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"), "-Werror", "")
 
     def build(self):
         self._patch_sources()
