d1437a1e082c77a75dc76ca98ebb2d4302767792
Martin Valgur <martin.valgur@gmail.com>
2024-07-29 12:59:42 +0300

    Use pkgconf/[>=2.2 <3]

--- recipes/pangolin/all/conanfile.py
+++ recipes/pangolin/all/conanfile.py
@@ -203,7 +203,7 @@ class PangolinConan(ConanFile):
     def build_requirements(self):
         if self.options.get_safe("with_wayland"):
             if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-                self.tool_requires("pkgconf/2.2.0")
+                self.tool_requires("pkgconf/[>=2.2 <3]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
