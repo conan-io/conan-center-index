dcaabf45d5a2b44c42a7ec22c8b3b18d71ca102b
Martin Valgur <martin.valgur@gmail.com>
2024-07-29 12:59:42 +0300

    Use pkgconf/[>=2.2 <3]

--- recipes/librasterlite/all/conanfile.py
+++ recipes/librasterlite/all/conanfile.py
@@ -64,7 +64,7 @@ class LibrasterliteConan(ConanFile):
         if not is_msvc(self):
             self.tool_requires("libtool/2.4.7")
             if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-                self.tool_requires("pkgconf/1.9.3")
+                self.tool_requires("pkgconf/[>=2.2 <3]")
             if self._settings_build.os == "Windows":
                 self.win_bash = True
                 if not self.conf.get("tools.microsoft.bash:path", check_type=str):
