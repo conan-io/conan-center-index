66d1f3547393be10f692c72ae3d1503c017d3291
memsharded <james@conan.io>
2024-07-29 17:45:42 +0200

    try to pass the build

--- recipes/hermes/all/conanfile.py
+++ recipes/hermes/all/conanfile.py
@@ -1,11 +1,14 @@
+import os
+import sys
+
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import get, copy
+from conan.tools.files import get, copy, replace_in_file
 from conan.tools.build import check_min_cppstd
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.scm import Version
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-import os
+
 
 required_conan_version = ">=1.53.0"
 
@@ -16,16 +19,9 @@ class HermesConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/facebook/hermes"
     topics = ("javascript", "ahead-of-time", "react native")
-    package_type = "library"
+    package_type = "shared-library"
     settings = "os", "arch", "compiler", "build_type"
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
-    }
-    default_options = {
-        "shared": False,
-        "fPIC": True,
-    }
+
     short_paths = True
 
     @property
@@ -42,14 +38,6 @@ class HermesConan(ConanFile):
             "msvc": "191",
         }
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-
     def layout(self):
         cmake_layout(self, src_folder="src")
 
@@ -65,17 +53,20 @@ class HermesConan(ConanFile):
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
 
-    def build_requirements(self):
-        self.tool_requires("cpython/3.12.2")
+    def validate_build(self):
+        if getattr(sys, "frozen", False):
+            raise ConanInvalidConfiguration("This package cannot be built with a Conan self-contained executable, "
+                                            "needs full Python installation")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.shared
-        tc.variables["HERMES_USE_STATIC_ICU"] = not self.dependencies["icu"].options.shared
-        tc.variables["HERMES_ENABLE_TEST_SUITE"] = False
+        tc.cache_variables["HERMES_USE_STATIC_ICU"] = not self.dependencies["icu"].options.shared
+        tc.cache_variables["HERMES_ENABLE_TEST_SUITE"] = False
+        tc.cache_variables["HERMES_IS_ANDROID"] = self.settings.os == "Android"
+        tc.cache_variables["HERMES_ENABLE_WIN10_ICU_FALLBACK"] = False
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
@@ -83,6 +74,9 @@ class HermesConan(ConanFile):
         venv.generate(scope="build")
 
     def build(self):
+        if self.settings.os == "Windows":
+            replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                            "target_link_libraries(${target_name} dl pthread)", "")
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
