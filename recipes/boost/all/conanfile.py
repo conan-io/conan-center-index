396d8dd2c3845d4c32d9e14c74a6197756431a74
Uilian Ries <uilianries@gmail.com>
2024-07-30 13:20:59 +0200

    Use version range for zstd
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/boost/all/conanfile.py
+++ recipes/boost/all/conanfile.py
@@ -651,9 +651,9 @@ class BoostConan(ConanFile):
         if self._with_bzip2:
             self.requires("bzip2/1.0.8")
         if self._with_lzma:
-            self.requires("xz_utils/5.4.5")
+            self.requires("xz_utils/[>=5.4.5 <6]")
         if self._with_zstd:
-            self.requires("zstd/1.5.6")
+            self.requires("zstd/[>=1.5 <1.6]")
         if self._with_stacktrace_backtrace:
             self.requires("libbacktrace/cci.20210118", transitive_headers=True, transitive_libs=True)
 
