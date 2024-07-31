92b9e334f10a8d179148feaa3e9536e2516916b8
toge <toge.mail@gmail.com>
2024-08-01 08:43:13 +0900

    remove destination in self.get()

--- recipes/libxlsxwriter/all/conanfile.py
+++ recipes/libxlsxwriter/all/conanfile.py
@@ -65,8 +65,7 @@ class LibxlsxwriterConan(ConanFile):
             raise ConanInvalidConfiguration(f"{self.name}:md5=openssl is not suppported in {self.ref}")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version],
-            destination=self.source_folder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
