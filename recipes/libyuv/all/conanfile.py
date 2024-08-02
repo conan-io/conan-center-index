7b0d7b7b64510adc958084ac8068150970fa59df
Nerixyz <nerixdev@outlook.de>
2024-08-02 15:37:44 +0200

    deps: loosen requirement on libjpeg-turbo and mozjpeg

--- recipes/libyuv/all/conanfile.py
+++ recipes/libyuv/all/conanfile.py
@@ -50,9 +50,9 @@ class LibyuvConan(ConanFile):
         if self.options.with_jpeg == "libjpeg":
             self.requires("libjpeg/9f")
         elif self.options.with_jpeg == "libjpeg-turbo":
-            self.requires("libjpeg-turbo/3.0.3")
+            self.requires("libjpeg-turbo/[~3.0.3]")
         elif self.options.with_jpeg == "mozjpeg":
-            self.requires("mozjpeg/4.1.5")
+            self.requires("mozjpeg/[~4.1.5]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version])
