804f02a9d3c406945300402940a8b88e339c4ac3
Esteban DUGUEPEROUX <esteban.dugueperoux@gmail.com>
2024-08-04 19:22:36 +0200

    Try to fix test_package CI

--- recipes/libmeshb/all/test_package/test_package.c
+++ recipes/libmeshb/all/test_package/test_package.c
@@ -1,30 +1,12 @@
-#include "libmeshb7.h"
+#include <libmeshb7.h>
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char **argv) {
-    int64_t LibIdx;
-    int i, ver, dim, NmbVer, NmbTri, (*Nodes)[4], *Domains;
-    float (*Coords)[3];
-
-    // Open the mesh file for reading
-    LibIdx = GmfOpenMesh( "test", GmfRead, &ver, &dim );
-
-    // Get the number of vertices and triangles
-    NmbVer = GmfStatKwd( LibIdx, GmfVertices  );
-    NmbTri = GmfStatKwd( LibIdx, GmfTriangles );
-
-    // Allocate memory accordingly
-    Nodes   = malloc( NmbTri * 4 * sizeof(int)   );
-    Coords  = malloc( NmbVer * 3 * sizeof(float) );
-    Domains = malloc( NmbVer     * sizeof(int)   );
-
-    // Move the file pointer to the vertices keyword
-    GmfGotoKwd( LibIdx, GmfVertices );
-
-    // Close the mesh file !
-    GmfCloseMesh( LibIdx );
-
-    return EXIT_SUCCESS;
+int main() {
+   int64_t OutMsh;
+   if(!(OutMsh = GmfOpenMesh("test.meshb", GmfWrite, 2, 3)))
+      return 1;
+   GmfCloseMesh(OutMsh);
+   puts("Success! Created test.meshb");
+   return 0;
 }
