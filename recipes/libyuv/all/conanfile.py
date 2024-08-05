8152ed286d1b1e87b2777e07a7d330259b216eee
nerix <nero.9@hotmail.de>
2024-08-05 14:36:01 +0200

    Update recipes/libyuv/all/conanfile.py
    Co-authored-by: PerseoGI <perseog@jfrog.com>

--- recipes/libyuv/all/conanfile.py
+++ recipes/libyuv/all/conanfile.py
@@ -53,7 +53,7 @@ class LibyuvConan(ConanFile):
         elif self.options.with_jpeg == "libjpeg-turbo":
             self.requires("libjpeg-turbo/[>=3.0.3 <4]")
         elif self.options.with_jpeg == "mozjpeg":
-            self.requires("mozjpeg/[~4.1.5]")
+            self.requires("mozjpeg/4.1.5")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version])
