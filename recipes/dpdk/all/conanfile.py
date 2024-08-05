d00d5921d5d0cae329c99d2d4f79383f21a4c1b1
Martin Valgur <martin.valgur@gmail.com>
2024-08-05 22:39:17 +0300

    dpdk: fix linux-headers-generic not being used

--- recipes/dpdk/all/conanfile.py
+++ recipes/dpdk/all/conanfile.py
@@ -120,6 +120,7 @@ class DpdkConan(ConanFile):
         tc.project_options["enable_stdatomic"] = "true" if self.options.enable_stdatomic else "false"
         tc.project_options["ibverbs_link"] = "shared"  # rdma-core is a shared-library package
         tc.project_options["disable_drivers"] = "bus"
+        tc.extra_cflags.append(f"-I{self.dependencies['linux-headers-generic'].cpp_info.includedir}")
         tc.generate()
 
         deps = PkgConfigDeps(self)
