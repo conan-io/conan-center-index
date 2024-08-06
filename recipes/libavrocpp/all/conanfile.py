5f1b083270accc13619baba70cac7acd15de560b
toge <toge.mail@gmail.com>
2024-08-07 03:44:18 +0900

    1.12.0 requires C++17

--- recipes/libavrocpp/all/conanfile.py
+++ recipes/libavrocpp/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, replace_in_file
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
@@ -30,7 +31,20 @@ class LibavrocppConan(ConanFile):
 
     @property
     def _min_cppstd(self):
-        return 11
+        # 1.12.0 or later requires C++17 https://github.com/apache/avro/pull/2949
+        return "11" if Version(self.version) < "1.12.0" else "17"
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "17": {
+                "gcc": "8",
+                "clang": "7",
+                "apple-clang": "12",
+                "Visual Studio": "16",
+                "msvc": "192",
+            },
+        }.get(self._min_cppstd, {})
 
     def export_sources(self):
         export_conandata_patches(self)
@@ -47,15 +61,22 @@ class LibavrocppConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        # boost upper to 1.81.0 requires C++14 minimum
-        self.requires("boost/1.81.0", transitive_headers=True)
-        self.requires("snappy/1.1.9")
-        if Version(self.version) >= "1.12.0":
+        if Version(self.version) < "1.12.0":
+            # boost upper to 1.81.0 requires C++14 minimum
+            self.requires("boost/1.81.0", transitive_headers=True)
+        else:
+            self.requires("boost/1.85.0", transitive_headers=True)
             self.requires("fmt/10.2.1", transitive_headers=True)
+        self.requires("snappy/1.1.9")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
