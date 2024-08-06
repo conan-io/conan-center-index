c9ee4a45863e515572bdbf734f0dc5b22a4e6614
Patrick Urbanke <patrick@getml.com>
2024-08-06 23:07:11 +0200

    Adapted the conanfile in the test package as well

--- recipes/reflectcpp/all/test_package/conanfile.py
+++ recipes/reflectcpp/all/test_package/conanfile.py
@@ -6,7 +6,16 @@ from conan.tools.build import can_run
 
 
 class reflect_cppTestConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = {
+        "os": ["Linux", "Windows", "Macos"],
+        "compiler": {
+            "gcc": {"cppstd": [20]},
+            "msvc": {"cppstd": [20]},
+            "clang": {"cppstd": [20]},
+        },
+        "arch": None,
+        "build_type": None,
+    }
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
@@ -24,3 +33,17 @@ class reflect_cppTestConan(ConanFile):
         if can_run(self):
             cmd = os.path.join(self.cpp.build.bindir, "reflectcpp-test")
             self.run(cmd, env="conanrun")
+
+    @property
+    def _min_cppstd(self):
+        return 20
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "Visual Studio": "17",
+            "msvc": "193",
+            "gcc": "11.4",
+            "clang": "16",
+            "apple-clang": "15",
+        }
