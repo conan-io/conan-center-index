d057aba63eba3394e0e35d247e89e919e02e2ae2
Martin Valgur <martin.valgur@gmail.com>
2024-08-06 15:35:23 +0300

    tiledb: update pkgconf

--- recipes/tiledb/all/conanfile.py
+++ recipes/tiledb/all/conanfile.py
@@ -145,7 +145,7 @@ class TileDBConan(ConanFile):
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.21 <4]")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.1.0")
+            self.tool_requires("pkgconf/[>=2.2.0 <3]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
