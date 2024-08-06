f3fe59667c1be3698eb5011b8010e176c34aa14a
Luiz Gustavo Pfitscher e Feldmann <lfeldmann@ael.com.br>
2024-08-06 15:12:27 -0300

    Simplifying the test package even more

--- recipes/cigi-cl/all/test_package/test_package.cpp
+++ recipes/cigi-cl/all/test_package/test_package.cpp
@@ -1,32 +1,7 @@
-#include <cigicl/CigiIGSession.h>
-#include <cigicl/CigiHostSession.h>
-#include <cigicl/CigiSOFV4.h>
+#include <cigicl/CigiOutgoingMsg.h>
 
 int main(void) {
-    // Test constructing sessions
-    CigiIGSession* pIgSession = new CigiIGSession();
-    CigiHostSession* pHostSession = new CigiHostSession();
-
-    // Write outgoing messages to buffer
-    CigiSOFV4 sofMsg;
-
-    CigiOutgoingMsg& msgOut = pIgSession->GetOutgoingMsgMgr();
-    msgOut.BeginMsg();
-    msgOut.pack(sofMsg);
-
-    Cigi_uint8* rawMsgData = NULL;
-    int msgLength = 0;
-    msgOut.PackageMsg(&rawMsgData, msgLength);
-
-    // Test reading back the message
-    CigiIncomingMsg& msgIn = pHostSession->GetIncomingMsgMgr();
-    int status = msgIn.ProcessIncomingMsg(rawMsgData, msgLength);
-
-    // Cleanup
-    msgOut.FreeMsg();
-    delete pIgSession;
-    delete pHostSession;
-
-    // Test results
-    return (status == CIGI_SUCCESS) ? 0 : -1;
+    CigiOutgoingMsg message{};
+    message.CreateBuffer();
+    return 0;
 }
