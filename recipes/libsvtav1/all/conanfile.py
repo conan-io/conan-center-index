da0363f3182f95c1f3bfa5983bc66ac347c6b1f8
Uilian Ries <uilianries@gmail.com>
2024-08-07 09:02:50 +0100

    (#24797) [libsvtav1] Disable Android build for version < 2.x
    * Disable Android build for older versions
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix required version for android
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add version 2.1.2
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    ---------
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libsvtav1/all/conanfile.py
+++ recipes/libsvtav1/all/conanfile.py
@@ -3,8 +3,9 @@ from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps, CMake
 from conan.tools.files import copy, get, rmdir, apply_conandata_patches, export_conandata_patches
 from conan.tools.scm import Version
+from conan.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=1.54.0"
 
 
 class SVTAV1Conan(ConanFile):
@@ -39,6 +40,9 @@ class SVTAV1Conan(ConanFile):
             self.options.rm_safe("fPIC")
         if Version(self.version) < "2.0.0":
             del self.options.minimal_build
+        if Version(self.version) >= "2.1.1":
+            # https://gitlab.com/AOMediaCodec/SVT-AV1/-/blob/c949fe4f14fe288a9b2b47aa3e61335422a83645/CHANGELOG.md#211---2024-06-25
+            del self.options.build_decoder
 
     def configure(self):
         if self.options.shared:
@@ -50,11 +54,17 @@ class SVTAV1Conan(ConanFile):
     def requirements(self):
         self.requires("cpuinfo/cci.20231129")
 
+    def validate(self):
+        # https://gitlab.com/AOMediaCodec/SVT-AV1/-/issues/2081
+        # https://gitlab.com/AOMediaCodec/SVT-AV1/-/commit/800a81b09db1cf8c9c289ecf6f70381d7888b98c
+        if Version(self.version) < "1.9.0" and self.settings.os == "Android":
+            raise ConanInvalidConfiguration(f"{self.ref} does not support Android before version 1.9.0.")
+
     def build_requirements(self):
         if Version(self.version) >= "1.3.0":
             self.tool_requires("cmake/[>=3.16 <4]")
         if self.settings.arch in ("x86", "x86_64"):
-            self.tool_requires("nasm/2.15.05")
+            self.tool_requires("nasm/2.16.01")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -62,7 +72,8 @@ class SVTAV1Conan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["BUILD_APPS"] = False
-        tc.variables["BUILD_DEC"] = self.options.build_decoder
+        if Version(self.version) < "2.1.1":
+            tc.variables["BUILD_DEC"] = self.options.build_decoder
         tc.variables["BUILD_ENC"] = self.options.build_encoder
         tc.variables["USE_EXTERNAL_CPUINFO"] = True
         if self.settings.arch in ("x86", "x86_64"):
@@ -94,7 +105,7 @@ class SVTAV1Conan(ConanFile):
             self.cpp_info.components["encoder"].requires = ["cpuinfo::cpuinfo"]
             if self.settings.os in ("FreeBSD", "Linux"):
                 self.cpp_info.components["encoder"].system_libs = ["pthread", "dl", "m"]
-        if self.options.build_decoder:
+        if self.options.get_safe("build_decoder"):
             self.cpp_info.components["decoder"].libs = ["SvtAv1Dec"]
             self.cpp_info.components["decoder"].includedirs = ["include/svt-av1"]
             self.cpp_info.components["decoder"].set_property("pkg_config_name", "SvtAv1Dec")
