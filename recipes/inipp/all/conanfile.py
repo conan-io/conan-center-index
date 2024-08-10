c2b1883460d3167fee1e7a6e38ca34205bb69160
toge <toge.mail@gmail.com>
2024-08-10 18:05:41 +0900

    require C++14

--- recipes/inipp/all/conanfile.py
+++ recipes/inipp/all/conanfile.py
@@ -1,7 +1,9 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.52.0"
@@ -19,7 +21,18 @@ class InippConan(ConanFile):
 
     @property
     def _min_cppstd(self):
-        return 11
+        # inipp requires `deduced return type` which is provided in C++14
+        return 14
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "apple-clang": "10",
+            "clang": "7",
+            "gcc": "7",
+            "msvc": "191",
+            "Visual Studio": "15",
+        }
 
     def layout(self):
         basic_layout(self, src_folder="src")
@@ -28,8 +41,13 @@ class InippConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
+        if self.settings.compiler.cppstd:
             check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
