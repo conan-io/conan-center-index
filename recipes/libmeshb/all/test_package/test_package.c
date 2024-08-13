32a7326ca52edeb2d58bd049b5c02aabde02c273
Esteban Dugueperoux <43169544+EstebanDugueperoux2@users.noreply.github.com>
2024-08-13 13:54:39 +0200

    Update recipes/libmeshb/all/test_package/test_package.c
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libmeshb/all/test_package/test_package.c
+++ recipes/libmeshb/all/test_package/test_package.c
@@ -1,8 +1,7 @@
 #include <libmeshb7.h>
 
-#include <stdio.h>
-
 int main() {
-   GmfCloseMesh(NULL);
+   int ver, dim;
+   int64_t LibIdx = GmfOpenMesh("/dev/null", GmfRead, &ver, &dim);
    return 0;
 }
