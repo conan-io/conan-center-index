29e2b6437ee959163bfc8aabff696b61771a0c3a
toge <toge.mail@gmail.com>
2024-08-13 09:35:40 +0900

    remove replace_in_file

--- recipes/fixed_math/all/conanfile.py
+++ recipes/fixed_math/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rmdir, replace_in_file
+from conan.tools.files import copy, get, rmdir
 from conan.tools.scm import Version
 from conan.tools.layout import basic_layout
 from conan.tools.env import VirtualBuildEnv
