093ba128e32dc954a3fd193e12896111bfefef34
danimtb <danimanzaneque@gmail.com>
2024-08-19 17:29:41 +0200

    fix

--- recipes/mysql-connector-cpp/all/test_package/test_package.cpp
+++ recipes/mysql-connector-cpp/all/test_package/test_package.cpp
@@ -4,8 +4,9 @@
 
 
 int main() {
-    Session sess("mysqlx://root@127.0.0.1");
-    RowResult res = sess.sql("show variables like 'version'").execute();
+    mysqlx::Session sess("mysqlx://root@127.0.0.1");
+    mysqlx::RowResult res = sess.sql("show variables like 'version'").execute();
+    std::stringstream version;
     version << res.fetchOne().get(1).get<std::string>();
     std::cout << "mysqlx version: " << version << std::endl;
     return EXIT_SUCCESS;
