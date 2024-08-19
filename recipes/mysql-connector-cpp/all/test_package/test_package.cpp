8e471a00543fbfc4828f5df811f3adb871ccf5f5
danimtb <danimanzaneque@gmail.com>
2024-08-19 17:40:45 +0200

    other

--- recipes/mysql-connector-cpp/all/test_package/test_package.cpp
+++ recipes/mysql-connector-cpp/all/test_package/test_package.cpp
@@ -8,6 +8,8 @@ int main() {
     mysqlx::RowResult res = sess.sql("show variables like 'version'").execute();
     std::stringstream version;
     version << res.fetchOne().get(1).get<std::string>();
-    std::cout << "mysqlx version: " << version << std::endl;
+    int major_version;
+    version >> major_version;
+    std::cout << "mysqlx version: " << major_version << std::endl;
     return EXIT_SUCCESS;
 }
