16870ab68601c8dece83306a6f55ecec4abd4ba0
Abril Rincón Blanco <git@rinconblanco.es>
2024-08-21 00:12:31 +0200

    Bump more version ranges

--- recipes/openslide/all/conanfile.py
+++ recipes/openslide/all/conanfile.py
@@ -75,9 +75,9 @@ class OpenSlideConan(ConanFile):
             raise ConanInvalidConfiguration("OpenSlide requires GNU C extensions support and is not compatible with MSVC")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.4.0")
+        self.tool_requires("meson/[>=1.2.3 <2]")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.2.0")
+            self.tool_requires("pkgconf/[>=2.2 <3]")
         self.tool_requires("glib/<host_version>")
 
     def source(self):
