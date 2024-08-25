c816a60aca4eb5ac4e8c524a3c23b51450dd4633
toge <toge.mail@gmail.com>
2024-08-26 08:52:52 +0900

    make libunwind transitive_lib for shared build

--- recipes/tracy/all/conanfile.py
+++ recipes/tracy/all/conanfile.py
@@ -96,7 +96,7 @@ class TracyConan(ConanFile):
 
     def requirements(self):
         if self.options.get_safe("libunwind_backtrace"):
-            self.requires("libunwind/1.8.1", transitive_headers=True)
+            self.requires("libunwind/1.8.1", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
         if self.info.settings.compiler.get_safe("cppstd"):
