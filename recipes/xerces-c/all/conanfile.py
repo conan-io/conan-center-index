758361d235c595a0d8e97889fcc71fa6861d26e3
PerseoGI <perseog@jfrog.com>
2024-08-27 11:26:13 +0200

    Fix clang compilation issue on Windows, close #24796

--- recipes/xerces-c/all/conanfile.py
+++ recipes/xerces-c/all/conanfile.py
@@ -131,6 +131,12 @@ class XercesCConan(ConanFile):
         # avoid picking up system dependency
         tc.variables["CMAKE_DISABLE_FIND_PACKAGE_CURL"] = self.options.get_safe("network_accessor") != "curl"
         tc.variables["CMAKE_DISABLE_FIND_PACKAGE_ICU"] = "icu" not in (self.options.transcoder, self.options.message_loader)
+
+        # Fix compatibility with Clang on Windows
+        # https://issues.apache.org/jira/browse/XERCESC-2252
+        if self.settings.os == "Windows" and self.settings.compiler == "Clang":
+            tc.cache_variables["CMAKE_RC_FLAGS"] = "-C 1252"
+
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
