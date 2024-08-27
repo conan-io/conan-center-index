3d238b2022e6b1a25bb4bfb758eef7b4f30d9ff0
Martin Valgur <martin.valgur@gmail.com>
2024-08-27 19:08:30 +0300

    splinter: new recipe

---
+++ recipes/splinter/all/conanfile.py
@@ -0,0 +1,85 @@
+import os
+
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
+from conan.tools.scm import Version
+
+required_conan_version = ">=1.53.0"
+
+
+class SplinterConan(ConanFile):
+    name = "splinter"
+    description = ("SPLINTER (SPLine INTERpolation) is a library for multivariate function approximation with splines. "
+                   "The library can be used for function approximation, regression, data smoothing, data reduction, and much more.")
+    license = "MPL-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/bgrimstad/splinter"
+    topics = ("splines", "interpolation", "smoothing", "function-approximation")
+
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("eigen/3.4.0", transitive_headers=True, transitive_libs=True)
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, 11)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def _patch_sources(self):
+        apply_conandata_patches(self)
+        rmdir(self, os.path.join(self.source_folder, "thirdparty"))
+
+    def build(self):
+        self._patch_sources()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        # The project does not install any CMake config or pkg-config files
+        suffix = "" if self.options.shared else "-static"
+        version = Version(self.version)
+        self.cpp_info.libs = [f"splinter{suffix}-{version.major}-{version.minor}"]
+        self.cpp_info.includedirs.append(os.path.join("include", "SPLINTER"))
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.append("m")
