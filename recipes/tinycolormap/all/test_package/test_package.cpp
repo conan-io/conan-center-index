49cd43592673ace07c1edc1ab421e08bb7bb70f6
Martin Valgur <martin.valgur@gmail.com>
2024-08-28 15:52:12 +0300

    tinycolormap: use a version range for Qt dependencies

--- recipes/tinycolormap/all/test_package/test_package.cpp
+++ recipes/tinycolormap/all/test_package/test_package.cpp
@@ -7,4 +7,9 @@ int main() {
     auto color = tinycolormap::GetColor(value, tinycolormap::ColormapType::Viridis);
     std::cout << "Viridis RGB values at " << value << ": "
               << (int)color.ri() << " " << (int)color.gi() << " " << (int)color.bi() << std::endl;
+#ifdef TINYCOLORMAP_WITH_QT5
+    auto qt_color = color.ConvertToQColor();
+    std::cout << "Viridis QColor values at " << value << ": "
+              << qt_color.red() << " " << qt_color.green() << " " << qt_color.blue() << std::endl;
+#endif
 }
