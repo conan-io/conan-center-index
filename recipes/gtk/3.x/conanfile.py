f030b67b22ab4dcc8c6e2cdb5a5670c1e2d45992
Martin Valgur <martin.valgur@gmail.com>
2024-08-30 16:03:46 +0300

    gtk3: add a note about missing extra .pc file params

--- recipes/gtk/3.x/conanfile.py
+++ recipes/gtk/3.x/conanfile.py
@@ -367,3 +367,8 @@ class GtkConan(ConanFile):
         if self.options.with_introspection:
             self.buildenv_info.append_path("GI_GIR_PATH", os.path.join(self.package_folder, "res", "share", "gir-1.0"))
             self.env_info.GI_GIR_PATH.append(os.path.join(self.package_folder, "res", "share", "gir-1.0"))
+
+        # TODO: add the following info to all generated .pc files:
+        # targets=wayland x11
+        # gtk_binary_version=3.0.0
+        # gtk_host=x86_64-linux
