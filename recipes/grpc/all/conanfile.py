b95bef4e21f610c1282cb722f0dee38d1f80f69e
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-08-31 15:07:32 +0200

    grpc: fix transitive_libs grpc regression

--- recipes/grpc/all/conanfile.py
+++ recipes/grpc/all/conanfile.py
@@ -104,9 +104,9 @@ class GrpcConan(ConanFile):
         # abseil is public. See https://github.com/conan-io/conan-center-index/pull/17284#issuecomment-1526082638
         if Version(self.version) >= "1.62.0":
             self.requires("protobuf/5.27.0", transitive_headers=True)
-            self.requires("abseil/[>=20240116.1 <20240117.0]", transitive_headers=True)
+            self.requires("abseil/[>=20240116.1 <20240117.0]", transitive_headers=True, transitive_libs=True)
         else:
-            self.requires("abseil/[>=20230125.3 <=20230802.1]", transitive_headers=True)
+            self.requires("abseil/[>=20230125.3 <=20230802.1]", transitive_headers=True, transitive_libs=True)
             self.requires("protobuf/3.21.12", transitive_headers=True)
         self.requires("c-ares/[>=1.19.1 <2]")
         self.requires("openssl/[>=1.1 <4]")
