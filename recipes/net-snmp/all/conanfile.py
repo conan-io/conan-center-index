e4adab69a2d5cb74575dedcc0accbf27c0acf071
Uilian Ries <uilianries@gmail.com>
2024-09-02 08:52:21 +0200

    Install pkgconfig to find OpenSSL when cross-building
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/net-snmp/all/conanfile.py
+++ recipes/net-snmp/all/conanfile.py
@@ -73,6 +73,8 @@ class NetSnmpConan(ConanFile):
             self.tool_requires("autoconf/2.71")
             self.tool_requires("automake/1.16.5")
             self.tool_requires("libtool/2.4.7")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/2.2.0")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
