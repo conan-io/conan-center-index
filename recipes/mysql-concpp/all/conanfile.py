171fbcd70fc12c9dba385a387a015d56158c41bc
Hussein Itawi <26905155+husitawi@users.noreply.github.com>
2024-09-02 21:46:44 +0300

    V1 pipeline

--- recipes/mysql-concpp/all/conanfile.py
+++ recipes/mysql-concpp/all/conanfile.py
@@ -62,6 +62,7 @@ class MysqlCppConnRecipe(ConanFile):
     def requirements(self):
         self.requires("openssl/1.0.2u")
         self.requires("boost/1.85.0")
+        self.requires("zlib/[>=1.2.11 <2]")
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.24 <4]")
@@ -101,7 +102,7 @@ class MysqlCppConnRecipe(ConanFile):
         # LZ4 patches
         tc.cache_variables["WITH_LZ4"] = "TRUE"
         # ZLIB patches
-        tc.cache_variables["WITH_ZLIB"] = "TRUE"
+        tc.cache_variables["WITH_ZLIB"] = "SYSTEM"
         # ZSTD patches
         tc.cache_variables["WITH_ZSTD"] = "TRUE"
         # Build patches
