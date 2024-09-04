bf3963265357e282f0896f76e17dc999eb83a85b
husitawi <26905155+husitawi@users.noreply.github.com>
2024-09-04 13:02:41 +0300

    Fix example

--- recipes/mysql-concpp/all/test_package/src/example.cpp
+++ recipes/mysql-concpp/all/test_package/src/example.cpp
@@ -1,36 +1,7 @@
 #include <mysqlx/xdevapi.h>
-#include <iostream>
 
-using ::std::cout;
-using ::std::endl;
-using namespace ::mysqlx;
-
-int main(int argc, const char *argv[])
+int main()
 {
-    try
-    {
-        const char *url = (argc > 1 ? argv[1] : "mysqlx://root:password@127.0.0.1");
-
-        cout << "Creating session on " << url
-             << " ..." << endl;
-
-        Session sess(url);
-        cout << "Session accepted." << endl;
-    }
-    catch (const mysqlx::Error &err)
-    {
-        cout << "ERROR: " << err << endl;
-    }
-    catch (std::exception &ex)
-    {
-        cout << "STD EXCEPTION: " << ex.what() << endl;
-    }
-    catch (const char *ex)
-    {
-        cout << "EXCEPTION: " << ex << endl;
-    }
-
-    cout << "Test ended." << endl;
-
+    mysqlx::SessionSettings from_url("mysqlx://user:pwd@127.0.0.1:1234/db?ssl-mode=required");
     return 0;
 }
