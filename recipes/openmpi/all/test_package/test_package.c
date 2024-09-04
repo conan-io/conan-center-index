2b728b50b4ca678884d0b199e3e648067220e918
Uilian Ries <uilianries@gmail.com>
2024-09-04 08:54:05 +0200

    Simplify test package to avoid networking usage
    The current openmpi test package tries to use networking,
    and in OS like Mac, the GUI asks about permission to
    execute the test package. It's too much for a simple
    package validation. It would be better keep a version
    check for library linkage validation.
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/openmpi/all/test_package/test_package.c
+++ recipes/openmpi/all/test_package/test_package.c
@@ -1,23 +1,13 @@
-#include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <mpi.h>
 
 int main(int argc, char* argv[])
 {
-  MPI_Init(&argc, &argv);
-
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  if (rank == 0) {
-    int value = 17;
-    int result = MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-    if (result == MPI_SUCCESS)
-      puts("Rank 0 OK!\n");
-  } else if (rank == 1) {
-    int value;
-    int result = MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    if (result == MPI_SUCCESS && value == 17)
-      puts("Rank 1 OK!\n");
-  }
-  MPI_Finalize();
-  return 0;
+  char version[MPI_MAX_LIBRARY_VERSION_STRING] = {0};
+  int len = 0;
+  MPI_Get_library_version(version, &len);
+  printf("MPI version: %s\n", version);
+  return EXIT_SUCCESS;
 }
