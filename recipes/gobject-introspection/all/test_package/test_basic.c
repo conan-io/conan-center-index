1cebce44f4218a6f238f89c84735936496fbe57c
Uilian Ries <uilianries@gmail.com>
2024-09-04 13:22:59 +0200

    (#25015) [gobject-introspection] Migrate to Conan 2.x
    * gobject-introspection: migrate to Conan v2
    
    * gobject-introspection: bump deps
    
    * gobject-introspection: bump deps, transitive_headers=True
    
    * gobject-introspection: use flexible version for glib
    
    * gobject-introspection: fix linker error
    
    * gobject-introspection: drop old versions, add v1.76.1
    
    * gobject-introspection: cannot be built with shared glib
    
    * gobject-introspection: required_conan_version = ">=1.53.0"
    
    For rm_safe()
    
    * gobject-introspection: add a comment for the glib version requirement
    
    * gobject-introspection: fix build, add proper girepository test
    
    * gobject-introspection: set correct min Conan version
    
    * gobject-introspection: skip tests against g-ir tools when cross-compiling
    
    * gobject-introspection: bump to v78.1
    
    * gobject-introspection: bump deps
    
    * gobject-introspection: disable Windows Debug builds
    
    * gobject-introspection: update version
    
    * Drop version 1.80.1
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Simplify test package
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Use version range for pkgconf
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Do not enforce PKG_CONFIG_PATH
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add short_paths due Conan 1.x
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix missing PKG_CONFIG_PATH
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Use generators folder
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix res folder location
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add fix_apple_shared_install_name
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Disable introspection_data when cross-building
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix Conan 1.x test package execution
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix test paths
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Disable introspection_data for osx
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Disable introspection_data for osx
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Enforce using static glib when building
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Enforce glib 2.76 due compatibility
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * use glib 2.78.3
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * avoid introspection data on Mac
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add option for build_introspection_data
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * set glib to shared=True
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add libffi as dependency
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Define GIT PATH
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix typo
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix pkg config path define
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * force glib only when using build_introspection_data
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    ---------
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: Martin Valgur <martin.valgur@gmail.com>

--- recipes/gobject-introspection/all/test_package/test_basic.c
+++ recipes/gobject-introspection/all/test_package/test_basic.c
@@ -1,12 +1,12 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "girepository.h"
 
-int main(int argc, char **argv)
-{
+int main(void) {
     printf("gobject introspection version %d.%d.%d\n",
            gi_get_major_version(),
            gi_get_minor_version(),
            gi_get_micro_version());
-    return 0;
+    return EXIT_SUCCESS;
 }
