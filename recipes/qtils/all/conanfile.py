187774a7ec94877a8c49d5a7cbe348c9c03ae96a
simbahebinbo <lansheng228@163.com>
2024-09-06 18:44:18 +0800

    Modify according to the template

--- recipes/qtils/all/conanfile.py
+++ recipes/qtils/all/conanfile.py
@@ -1,43 +1,90 @@
-import os
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
-from conan.tools.files import copy,get
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
+import os
 
-required_conan_version = ">=2.0.5"
+
+required_conan_version = ">=1.52.0"
 
 class PackageConan(ConanFile):
     name = "qtils"
-    version = "0.0.4"
     description = "Utils for KAGOME C++ projects"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/qdrvm/qtils"
-    topics = ("KAGOME")
+    topics = ("KAGOME", "header-only")
     package_type = "header-library"
     settings = "os", "compiler", "build_type", "arch"
+    no_copy_source = True
+
+    @property
+    def _min_cppstd(self):
+        return 14
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "apple-clang": "10",
+            "clang": "7",
+            "gcc": "7",
+            "msvc": "191",
+            "Visual Studio": "15",
+        }
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def layout(self):
-        cmake_layout(self)
+        basic_layout(self, src_folder="src")
 
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
-        deps = CMakeDeps(self)
-        deps.generate()
+    def requirements(self):
+        self.requires("boost/1.84.0", transitive_headers=True)
+        self.requires("fmt/10.1.1", transitive_headers=True)
+
+    def package_id(self):
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
+        if self.settings.os == "Windows":
+            raise ConanInvalidConfiguration(f"{self.ref} can not be used on Windows.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def build(self):
-        # Only header files, no source files that need to be compiled, so skip the build step
-        pass
+        apply_conandata_patches(self)
 
     def package(self):
-        include_folder = os.path.join(self.source_folder, "src")
-        copy(self, "*.hpp", dst=os.path.join(self.package_folder, "include"), src=include_folder, keep_path=True)
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(
+            self,
+            "*.hpp",
+            os.path.join(self.source_folder, "src"),
+            os.path.join(self.package_folder, "include"),
+        )
 
     def package_info(self):
-        # Only header files, no library files need to be linked
+        self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
-        self.cpp_info.libs = []
-        self.cpp_info.includedirs = ["include"]
+
+        self.cpp_info.set_property("cmake_file_name", "qtils")
+        self.cpp_info.set_property("cmake_target_name", "qtils::qtils")
+        self.cpp_info.set_property("pkg_config_name", "qtils")
+
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.extend(["dl", "m", "pthread"])
+
+        self.cpp_info.filenames["cmake_find_package"] = "qtils"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "qtils"
+        self.cpp_info.names["cmake_find_package"] = "qtils"
+        self.cpp_info.names["cmake_find_package_multi"] = "qtils"
--- recipes/qtils/all/test_package/conanfile.py
+++ recipes/qtils/all/test_package/conanfile.py
@@ -1,11 +1,13 @@
-import os
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
+import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
 
     def layout(self):
         cmake_layout(self)
@@ -19,5 +21,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        demo_path = os.path.join(self.build_folder, "demo")
-        self.run(demo_path, env="conanrun")
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
