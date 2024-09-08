89af984c4050972421e0bb0642f2fa60e88e4934
Martin Valgur <martin.valgur@gmail.com>
2024-09-08 20:06:20 +0300

    drogon: drop the cppstd warning in validate()

--- recipes/drogon/all/conanfile.py
+++ recipes/drogon/all/conanfile.py
@@ -100,11 +100,8 @@ class DrogonConan(ConanFile):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
         minimum_version = self._compilers_minimum_version.get(str(self.info.settings.compiler), False)
-        if minimum_version:
-            if Version(self.info.settings.compiler.version) < minimum_version:
-                raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
-        else:
-            self.output.warn(f"{self.ref} requires C++{self._min_cppstd}. Your compiler is unknown. Assuming it supports C++{self._min_cppstd}.")
+        if minimum_version and Version(self.info.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
 
         if self.settings.compiler.get_safe("cppstd") == "14" and not self.options.with_boost:
             raise ConanInvalidConfiguration(f"{self.ref} requires boost on C++14")
