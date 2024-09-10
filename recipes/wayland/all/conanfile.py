2aeea70aa6dadd2ec3a49ab21096a74447a7a95e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2024-09-11 01:17:10 +0200

    bump libffi & use version range for meson & pkgconf

--- recipes/wayland/all/conanfile.py
+++ recipes/wayland/all/conanfile.py
@@ -48,7 +48,7 @@ class WaylandConan(ConanFile):
 
     def requirements(self):
         if self.options.enable_libraries:
-            self.requires("libffi/3.4.4")
+            self.requires("libffi/3.4.6")
         if self.options.enable_dtd_validation:
             self.requires("libxml2/[>=2.12.5 <3]")
         self.requires("expat/[>=2.6.2 <3]")
@@ -58,9 +58,9 @@ class WaylandConan(ConanFile):
             raise ConanInvalidConfiguration(f"{self.ref} only supports Linux")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.4.0")
+        self.tool_requires("meson/[>=1.2.3 <2]")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.1.0")
+            self.tool_requires("pkgconf/[>=2.2 <3]")
         if not can_run(self):
             self.tool_requires(str(self.ref))
 
