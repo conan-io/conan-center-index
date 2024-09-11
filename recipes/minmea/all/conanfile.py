24cc4cf682ee958886983cac444b72dc2b4df1cb
Uilian Ries <uilianries@gmail.com>
2024-09-11 09:33:54 +0200

    Build on Windows
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/minmea/all/conanfile.py
+++ recipes/minmea/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, copy, get, replace_in_file, rename
+from conan.tools.files import copy, copy, get, replace_in_file, rename, apply_conandata_patches, export_conandata_patches
 from conan.tools.microsoft import is_msvc
 from conan.errors import ConanInvalidConfiguration
 import os
@@ -25,6 +25,9 @@ class MinmeaConan(ConanFile):
         "fPIC": True,
     }
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -34,37 +37,27 @@ class MinmeaConan(ConanFile):
             self.options.rm_safe("fPIC")
         self.settings.rm_safe("compiler.cppstd")
         self.settings.rm_safe("compiler.libcxx")
-        if is_msvc(self):
-            del self.options.shared
-            self.package_type = "static-library"
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        # FIXME: Windows supported: https://github.com/kosma/minmea?tab=readme-ov-file#compatibility
+        # INFO: Windows mingw supported: https://github.com/kosma/minmea?tab=readme-ov-file#compatibility
+        # INFO: MSVC fails with error C2011: 'timespec': 'struct' type redefinition
         if is_msvc(self):
-            raise ConanInvalidConfiguration(f"{self.ref} can not be built on Visual Studio and msvc. Contributions are welcome.")
+            raise ConanInvalidConfiguration(f"{self.ref} can not be built on Visual Studio and msvc. Use mingw instead or similar.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        #if is_msvc(self):
-        #    tc.preprocessor_definitions["MINMEA_INCLUDE_COMPAT"] = "1"
+        if self.settings.os == "Windows":
+            tc.preprocessor_definitions["MINMEA_INCLUDE_COMPAT"] = "1"
         tc.generate()
-        deps = CMakeDeps(self)
-        deps.generate()
-
-    def _patch_sources(self):
-        # INFO: Disable pkg-config searching for the test library libcheck
-        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-                         "pkg_check_modules",
-                         "# pkg_check_modules")
 
     def build(self):
-        self._patch_sources()
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build(target="minmea")
@@ -72,17 +65,18 @@ class MinmeaConan(ConanFile):
     def package(self):
         copy(self, "COPYING", self.source_folder, os.path.join(self.package_folder, "licenses"))
         copy(self, "LICENSE.*", self.source_folder, os.path.join(self.package_folder, "licenses"))
-        copy(self, "*.h", self.source_folder, os.path.join(self.package_folder, "include"), keep_path=False)
+        copy(self, "minmea.h", self.source_folder, os.path.join(self.package_folder, "include"))
         copy(self, "libminmea.a", self.build_folder, os.path.join(self.package_folder, "lib"))
         copy(self, "libminmea.so", self.build_folder, os.path.join(self.package_folder, "lib"))
         copy(self, "libminmea.dylib", self.build_folder, os.path.join(self.package_folder, "lib"))
         if self.settings.os == "Windows":
-            copy(self, "minmea.lib", self.build_folder, os.path.join(self.package_folder, "lib"))
-            # INFO: https://github.com/kosma/minmea?tab=readme-ov-file#compatibility Need to rename the file
-            rename(self, os.path.join(self.package_folder, "include", "minmea_compat_windows.h"),
-                    os.path.join(self.package_folder, "include", "minmea_compat.h"))
+            copy(self, "libminmea.dll.a", self.build_folder, os.path.join(self.package_folder, "lib"))
+            copy(self, "libminmea.dll", self.build_folder, os.path.join(self.package_folder, "bin"))
+            copy(self, "minmea_compat.h", self.source_folder, os.path.join(self.package_folder, "include"))
 
     def package_info(self):
         self.cpp_info.libs = ["minmea"]
-        if is_msvc(self):
-            self.cpp_info.defines.append("MINMEA_INCLUDE_COMPAT")
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs = ["m"]
+        if self.settings.os == "Windows":
+            self.cpp_info.defines = ["MINMEA_INCLUDE_COMPAT=1"]
