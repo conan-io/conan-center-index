8cc3e4198849b7b38d37a372682e6fbadddc5ccb
Danny Lihard <danny.lihard@non.se.com>
2024-09-18 09:17:37 +0200

    oatpp: added version 1.3.0-latest
    - Added patch so that `-latest` in the version string does not prevent
    packaging.
    - Renamed CMake target `oatpp-test::oatpp-test` to `oatpp::oatpp-test`.

--- recipes/oatpp/all/conanfile.py
+++ recipes/oatpp/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rmdir
+from conan.tools.files import copy, get, rmdir, export_conandata_patches, apply_conandata_patches
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 from conan.tools.scm import Version
 import os
@@ -28,6 +28,9 @@ class OatppConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -61,6 +64,7 @@ class OatppConan(ConanFile):
         tc.generate()
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -92,7 +96,7 @@ class OatppConan(ConanFile):
         # oatpp-test
         self.cpp_info.components["oatpp-test"].names["cmake_find_package"] = "oatpp-test"
         self.cpp_info.components["oatpp-test"].names["cmake_find_package_multi"] = "oatpp-test"
-        self.cpp_info.components["oatpp-test"].set_property("cmake_target_name", "oatpp-test::oatpp-test")
+        self.cpp_info.components["oatpp-test"].set_property("cmake_target_name", "oatpp::oatpp-test")
         self.cpp_info.components["oatpp-test"].includedirs = [include_dir]
         self.cpp_info.components["oatpp-test"].libdirs = [lib_dir]
         self.cpp_info.components["oatpp-test"].libs = ["oatpp-test"]
