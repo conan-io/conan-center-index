06a7bc5daf227e4b14cb067f4ffa4ec30eb59b44
Uilian Ries <uilianries@gmail.com>
2024-09-19 11:53:10 +0200

    Remove check Conans import
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- linter/conanv2_transition.py
+++ linter/conanv2_transition.py
@@ -6,7 +6,6 @@ Pylint plugin/rules for conanfiles in Conan Center Index
 
 from pylint.lint import PyLinter
 from linter.check_package_name import PackageName
-from linter.check_import_conanfile import ImportConanFile
 from linter.check_import_errors import ImportErrorsConanException, ImportErrorsConanInvalidConfiguration, ImportErrors
 from linter.check_import_tools import ImportTools
 from linter.check_layout_src_folder import LayoutSrcFolder
@@ -15,7 +14,6 @@ from linter.check_version_attribute import VersionAttribute
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(PackageName(linter))
-    linter.register_checker(ImportConanFile(linter))
     linter.register_checker(ImportErrors(linter))
     linter.register_checker(ImportErrorsConanException(linter))
     linter.register_checker(ImportErrorsConanInvalidConfiguration(linter))
