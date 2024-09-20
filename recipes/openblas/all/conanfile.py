31e98507c7ad13bcd9a2884edd6f2e9a6f86895e
Martin Valgur <martin.valgur@gmail.com>
2024-09-20 21:37:43 +0300

    openblas: drop v0.3.20 to get rid of many patches and checks

--- recipes/openblas/all/conanfile.py
+++ recipes/openblas/all/conanfile.py
@@ -3,11 +3,9 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.build import cross_building
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
-from conan.tools.files import copy, get, replace_in_file, rmdir, export_conandata_patches, apply_conandata_patches
+from conan.tools.files import copy, get, rmdir, export_conandata_patches, apply_conandata_patches
 from conan.tools.microsoft import is_msvc_static_runtime, is_msvc
-from conan.tools.scm import Version
 import os
-import textwrap
 
 required_conan_version = ">=1.53.0"
 
@@ -61,7 +59,7 @@ available_openblas_targets = ["P2", "KATMAI", "COPPERMINE", "NORTHWOOD", "PRESCO
 
 class OpenblasConan(ConanFile):
     name = "openblas"
-    description = "An optimized BLAS library based on GotoBLAS2 1.13 BSD version"
+    description = "An optimized BLAS library based on GotoBLAS2, with LAPACK"
     license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.openblas.net"
@@ -126,16 +124,12 @@ class OpenblasConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-        # When no Fortran compiler is available, OpenBLAS builds LAPACK from an f2c-converted copy of LAPACK unless the NO_LAPACK option is specified.
-        # This is not available before v0.3.21.
-        if Version(self.version) < "0.3.21":
-            self.options.build_lapack = False
-            self.options.build_relapack = False
 
     def configure(self):
         if self.options.shared:
             self.options.rm_safe("fPIC")
-
+        if not self.options.build_lapack:
+            del self.options.build_relapack
         if not self.options.use_thread:
             del self.options.max_threads
         if not self.options.use_openmp:
@@ -146,7 +140,11 @@ class OpenblasConan(ConanFile):
             # Try inferring the target from settings.arch
             target = conan_arch_to_openblas_target.get(str(self.settings.arch))
             if target:
-                self.output.warning(f'Setting OpenBLAS TARGET={target} based on settings.arch. This may result in suboptimal performance. Set the "{self.name}/*:target=XXX" option to silence this warning.')
+                self.output.warning(
+                    f"Setting OpenBLAS TARGET={target} based on settings.arch. "
+                    "This may result in suboptimal performance. "
+                    f'Set the "{self.name}/*:target=XXX" option to silence this warning.'
+                )
                 self.options.target = target
 
     def requirements(self):
@@ -154,25 +152,11 @@ class OpenblasConan(ConanFile):
             self.requires("llvm-openmp/17.0.6")
 
     def validate(self):
-        if Version(self.version) < "0.3.24" and self.settings.arch == "armv8":
-            # OpenBLAS fails to detect the appropriate target architecture for armv8 for versions < 0.3.24, as it matches the 32 bit variant instead of 64.
-            # This was fixed in https://github.com/OpenMathLib/OpenBLAS/pull/4142, which was introduced in 0.3.24.
-            # This would be a reasonably trivial hotfix to backport.
-            raise ConanInvalidConfiguration("armv8 builds are not currently supported for versions lower than 0.3.24. Contributions to support this are welcome.")
-
-        if self.options.build_relapack:
-            if not self.options.build_lapack:
-                raise ConanInvalidConfiguration(f'"{self.name}/*:build_relapack=True" option requires "{self.name}/*:build_lapack=True"')
-            if self.settings.compiler not in ["gcc", "clang"]:
-                # ld: unknown option: --allow-multiple-definition on apple-clang
-                raise ConanInvalidConfiguration(f'"{self.name}/*:build_relapack=True" option is only supported for GCC and Clang')
+        if self.options.get_safe("build_relapack") and self.settings.compiler not in ["gcc", "clang"]:
+            # ld: unknown option: --allow-multiple-definition on apple-clang
+            raise ConanInvalidConfiguration(f'"{self.name}/*:build_relapack=True" option is only supported for GCC and Clang')
 
     def validate_build(self):
-        if Version(self.version) < "0.3.22" and cross_building(self, skip_x64_x86=True):
-            # OpenBLAS CMake builds did not support some of the cross-compilation targets in 0.3.20/21 and earlier.
-            # This was fixed in https://github.com/OpenMathLib/OpenBLAS/pull/3714 and https://github.com/OpenMathLib/OpenBLAS/pull/3958
-            raise ConanInvalidConfiguration(f"Cross-building is not supported for {self.name}/0.3.21 and earlier.")
-
         # If we're cross-compiling, and the user didn't provide the target, and
         # we couldn't infer the target from settings.arch, fail
         if cross_building(self, skip_x64_x86=True) and not self.options.target:
@@ -191,17 +175,13 @@ class OpenblasConan(ConanFile):
         tc.variables["BUILD_TESTING"] = False
 
         tc.variables["NOFORTRAN"] = not self.options.build_lapack
-        # This checks explicit user-specified fortran compiler
         if self.options.build_lapack and not self._fortran_compiler:
-            if Version(self.version) < "0.3.21":
-                self.output.warning("Building with LAPACK support requires a Fortran compiler.")
-            else:
-                tc.variables["C_LAPACK"] = True
-                tc.variables["NOFORTRAN"] = True
-                self.output.info("Building LAPACK without a Fortran compiler")
+            tc.variables["C_LAPACK"] = True
+            tc.variables["NOFORTRAN"] = True
+            self.output.info("Building LAPACK without a Fortran compiler")
 
         tc.variables["BUILD_WITHOUT_LAPACK"] = not self.options.build_lapack
-        tc.variables["BUILD_RELAPACK"] = self.options.build_relapack
+        tc.variables["BUILD_RELAPACK"] = self.options.get_safe("build_relapack", False)
         tc.variables["BUILD_BFLOAT16"] = self.options.build_bfloat16
         tc.variables["INTERFACE64"] = self.options.ilp64
         tc.variables["DYNAMIC_ARCH"] = self.options.dynamic_arch
@@ -233,40 +213,6 @@ class OpenblasConan(ConanFile):
 
     def _patch_sources(self):
         apply_conandata_patches(self)
-        if Version(self.version) <= "0.3.15":
-            replace_in_file(self, os.path.join(self.source_folder, "cmake", "utils.cmake"),
-                "set(obj_defines ${defines_in})", textwrap.dedent("""\
-                 set(obj_defines ${defines_in})
-
-                 list(FIND obj_defines "RC" def_idx)
-                 if (${def_idx} GREATER -1)
-                   list(REMOVE_ITEM obj_defines "RC")
-                   list(APPEND obj_defines "RC=RC")
-                 endif ()
-                 list(FIND obj_defines "CR" def_idx)
-                 if (${def_idx} GREATER -1)
-                   list(REMOVE_ITEM obj_defines "CR")
-                   list(APPEND obj_defines "CR=CR")
-                 endif ()"""))
-        if Version(self.version) < "0.3.21":
-            f_check_cmake = os.path.join(self.source_folder, "cmake", "f_check.cmake")
-            if Version(self.version) >= "0.3.12":
-                replace_in_file(self, f_check_cmake,
-                                'message(STATUS "No Fortran compiler found, can build only BLAS but not LAPACK")',
-                                'message(FATAL_ERROR "No Fortran compiler found. Cannot build with LAPACK.")')
-            else:
-                replace_in_file(self, f_check_cmake,
-                    "enable_language(Fortran)",
-                    textwrap.dedent("""\
-                        include(CheckLanguage)
-                        check_language(Fortran)
-                        if(CMAKE_Fortran_COMPILER)
-                          enable_language(Fortran)
-                        else()
-                          message(FATAL_ERROR "No Fortran compiler found. Cannot build with LAPACK.")
-                          set (NOFORTRAN 1)
-                          set (NO_LAPACK 1)
-                        endif()"""))
 
     def build(self):
         self._patch_sources()
