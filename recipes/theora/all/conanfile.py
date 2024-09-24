a1c4656e29b08f198381a99a3daadb13ece2c431
Martin Valgur <martin.valgur@gmail.com>
2024-09-24 23:33:29 +0300

    theora: add package_type, simplify test_package, fix component name

--- recipes/theora/all/conanfile.py
+++ recipes/theora/all/conanfile.py
@@ -1,9 +1,9 @@
+import os
+
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps, cmake_layout
-from conan.tools.files import copy, download, get, replace_in_file
+from conan.tools.files import copy, get
 from conan.tools.microsoft import is_msvc
-from conan.errors import ConanException
-import os
 
 required_conan_version = ">=1.52.0"
 
@@ -13,11 +13,18 @@ class TheoraConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/xiph/theora"
     description = "Theora is a free and open video compression format from the Xiph.org Foundation"
-    topics = "video", "video-compressor", "video-format"
+    topics = ("video", "video-compressor", "video-format")
 
+    package_type = "library"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
 
     def export_sources(self):
         copy(self, "CMakeLists.txt", self.recipe_folder, os.path.join(self.export_sources_folder,"src"))
@@ -28,24 +35,16 @@ class TheoraConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            try:
-                del self.options.fPIC
-            except ConanException:
-                pass
-        try:
-            del self.settings.compiler.libcxx
-        except ConanException:
-            pass
-        try:
-            del self.settings.compiler.cppstd
-        except ConanException:
-            pass
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("ogg/1.3.5")
+        # ogg_packet* and integer types are used in the public headers
+        self.requires("ogg/1.3.5", transitive_headers=True)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
@@ -73,9 +72,9 @@ class TheoraConan(ConanFile):
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "theora_full_package_do_not_use")    # to avoid conflicts with theora component
 
-        self.cpp_info.components["theora"].set_property("pkg_config_name", "theora")
-        self.cpp_info.components["theora"].libs = ["theora"]
-        self.cpp_info.components["theora"].requires = ["ogg::ogg"]
+        self.cpp_info.components["theora_"].set_property("pkg_config_name", "theora")
+        self.cpp_info.components["theora_"].libs = ["theora"]
+        self.cpp_info.components["theora_"].requires = ["ogg::ogg"]
 
         self.cpp_info.components["theoradec"].set_property("pkg_config_name", "theoradec")
         self.cpp_info.components["theoradec"].libs = ["theoradec"]
@@ -84,7 +83,3 @@ class TheoraConan(ConanFile):
         self.cpp_info.components["theoraenc"].set_property("pkg_config_name", "theoraenc")
         self.cpp_info.components["theoraenc"].libs = ["theoraenc"]
         self.cpp_info.components["theoraenc"].requires = ["ogg::ogg"]
-
-
-        # TODO: to remove in conan v2 once pkg_config generator removed
-        self.cpp_info.names["pkg_config"] = "theora_full_package"
