217f42e6b23f814e384e3ee72693e4f32a70c9b1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2024-09-24 23:48:04 +0200

    bump conan min version to 1.55.0 due to set_property in CMakeDeps

--- recipes/sdbus-cpp/all/conanfile.py
+++ recipes/sdbus-cpp/all/conanfile.py
@@ -9,7 +9,7 @@ from conan.tools.files import copy, get, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.scm import Version
 
-required_conan_version = ">=1.51.0"
+required_conan_version = ">=1.55.0"
 
 
 class SdbusCppConan(ConanFile):
