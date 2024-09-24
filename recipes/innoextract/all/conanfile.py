83d4ad08ae60db5163cacfa56f14be68a74f32b2
Abril Rincón Blanco <git@rinconblanco.es>
2024-09-24 17:54:59 +0200

    Fix Boost 1.85 usage

--- recipes/innoextract/all/conanfile.py
+++ recipes/innoextract/all/conanfile.py
@@ -25,7 +25,7 @@ class InnoextractConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.80.0")
+        self.requires("boost/1.85.0")
         self.requires("xz_utils/[>=5.4.5 <6]")
         self.requires("libiconv/1.17")
 
