d87e6499369b4ae0d04e3a46cc0a43ca681e26bb
Martin Valgur <martin.valgur@gmail.com>
2024-09-24 23:58:12 +0300

    samarium: migrate to Conan v2, drop old version

--- recipes/samarium/all/conanfile.py
+++ recipes/samarium/all/conanfile.py
@@ -1,10 +1,11 @@
-from os import path
+import os
 
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.files import patch, copy, get
-from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeDeps
+from conan.tools.files import copy, get
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.47.0"
 
@@ -12,73 +13,84 @@ required_conan_version = ">=1.47.0"
 class SamariumConan(ConanFile):
     name = "samarium"
     description = "2-D physics simulation library"
-    homepage = "https://strangequark1041.github.io/samarium/"
+    homepage = "https://jjbel.github.io/samarium/"
     url = "https://github.com/conan-io/conan-center-index/"
     license = "MIT"
     topics = ("cpp20", "physics", "2d", "simulation")
-    generators = "CMakeDeps", "CMakeToolchain"
-    requires = "fmt/9.0.0", "sfml/2.5.1", "range-v3/0.12.0", "stb/cci.20210910", "tl-expected/20190710"
 
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [
-        True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    @property
+    def _min_cppstd(self):
+        return 20
 
     @property
     def _compilers_minimum_version(self):
         return {
-            "gcc": "11.0",
-            "Visual Studio": "16",
+            "gcc": "11",
             "clang": "13",
             "apple-clang": "13",
+            "msvc": "192",
+            "Visual Studio": "16",
         }
-
-    def source(self):
-        get(self, **self.conan_data["sources"]
-            [str(self.version)], strip_root=True)
-
-    def configure(self):
-        if self.options.shared:
-            del self.options.fPIC
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def validate(self):
-        if self.version == "1.0.0" and self.settings.os == "Macos":
-            raise ConanInvalidConfiguration("Macos not supported for v1.0.0")
-
-        compiler = str(self.settings.compiler)
-        if compiler not in self._compilers_minimum_version:
-            self.output.warn(
-                "Unknown compiler, assuming it supports at least C++20")
-            return
-
-        version = Version(self.settings.compiler.version)
-        if version < self._compilers_minimum_version[compiler]:
-            raise ConanInvalidConfiguration(
-                f"{self.name} requires a compiler that supports at least C++20")
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
-    def export_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+    def requirements(self):
+         self.requires("fmt/10.2.1", transitive_headers=True, transitive_libs=True)
+         self.requires("sfml/2.6.1", transitive_headers=True, transitive_libs=True)
+         self.requires("range-v3/cci.20240905", transitive_headers=True, transitive_libs=True)
+         self.requires("stb/cci.20230920")
+         self.requires("tl-expected/20190710", transitive_headers=True, transitive_libs=True)
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
+
+    def source(self):
+        get(self, **self.conan_data["sources"][str(self.version)], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["RUN_CONAN"] = False
+        tc.cache_variables["BUILD_UNIT_TESTS"] = False
+        tc.cache_variables["BUILD_EXAMPLES"] = False
+        tc.cache_variables["BUILD_DOCS"] = False
+        tc.cache_variables["BUILD_DOCS_TARGET"] = False
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        for patch_ in self.conan_data.get("patches", {}).get(self.version, []):
-            patch(self, **patch_)
-
         cmake = CMake(self)
-        cmake.configure(build_script_folder="src")
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        copy(self, "LICENSE.md", src=self.folders.source_folder,
-             dst=path.join(self.package_folder, "licenses"))
-
+        copy(self, "LICENSE.md", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
--- recipes/samarium/all/test_package/conanfile.py
+++ recipes/samarium/all/test_package/conanfile.py
@@ -23,5 +23,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not cross_building(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, run_environment=True)
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
