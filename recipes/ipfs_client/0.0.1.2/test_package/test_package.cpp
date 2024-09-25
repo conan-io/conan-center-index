195421fb744e04052fa4da56b5ebcdd90cec463a
Uilian Ries <uilianries@gmail.com>
2024-09-25 10:17:00 +0200

    Update test package to follow template
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/ipfs_client/0.0.1.2/test_package/test_package.cpp
+++
@@ -1,8 +0,0 @@
-#include <ipfs_client/opinionated_context.h>
-
-int main(void) {
-  boost::asio::io_context io;
-  auto ctxt = ipfs::start_default(io);
-  auto mime = ctxt->MimeType("html", "a", "ipfs://bafyaafyscefakakvaaawceqgmexgq5dnnqmaccqcbaaq/a.html");
-  return mime.compare("text/html");
-}
--- recipes/ipfs_client/all/conanfile.py
+++ recipes/ipfs_client/all/conanfile.py
@@ -12,7 +12,7 @@ required_conan_version = ">=1.63"
 
 
 class IpfsChromium(ConanFile):
-    name = "ipfs-chromium"
+    name = "ipfs_client"
     description = "Library for acting as a trustless client of IFPS gateway"
     homepage = "https://github.com/little-bear-labs/ipfs-chromium"
     topics = ("ipfs", "ipns", "dweb", "web", "content-addressed", "network", "client", "io", "api", "file-sharing", "gateway", "kubo")
@@ -26,7 +26,7 @@ class IpfsChromium(ConanFile):
         return "20"
 
     @property
-    def _minimum_compilers_version(self):
+    def _compilers_minimum_version(self):
         return {
             "apple-clang": "14",
             "clang": "9",
@@ -36,16 +36,16 @@ class IpfsChromium(ConanFile):
         }
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.85.0")
+        self.requires("boost/1.85.0", transitive_headers=True, transitive_libs=True)
         self.requires("openssl/[>=1.1 <4]")
         self.requires("bzip2/1.0.8")
         self.requires("c-ares/1.33.1")
         self.requires("nlohmann_json/3.11.3")
-        self.requires("abseil/20240722.0")
-        self.requires("protobuf/5.27.0")
+        self.requires("abseil/20240116.2")
+        self.requires("protobuf/3.21.12", transitive_headers=True)
 
     def validate(self):
         if self.settings.compiler.cppstd:
@@ -67,8 +67,8 @@ class IpfsChromium(ConanFile):
         tc = CMakeToolchain(self)
         tc.cache_variables["INSIDE_CONAN"] = True
         tc.generate()
-        d = CMakeDeps(self)
-        d.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -76,9 +76,13 @@ class IpfsChromium(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "LICENSE*", dst=os.path.join(self.source_folder, "licenses"), src=os.path.join(self.source_folder, "library"))
+        copy(self, "LICENSE*", dst=os.path.join(self.package_folder, "licenses"),
+             src=os.path.join(self.source_folder, "library"))
         cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["ipfs_client", "ic_proto"]
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs = ["m"]
+        self.cpp_info.defines = ["HAS_BOOST_BEAST=1", "HAS_ARES=1", "HAS_OPENSSL_EVP=1"]
