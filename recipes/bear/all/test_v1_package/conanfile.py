8b228a7162cd4d3202ad22f36431bc6ae8152464
Abril Rincón Blanco <git@rinconblanco.es>
2024-09-27 22:10:22 +0200

    v1 fix, amybe? Else delete

--- recipes/bear/all/test_v1_package/conanfile.py
+++ recipes/bear/all/test_v1_package/conanfile.py
@@ -5,7 +5,11 @@ from conan.tools.build import can_run
 # legacy validation with Conan 1.x
 class TestPackageV1Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
+    test_type = "explicit"
 
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
+    
     def test(self):
         if can_run(self):
             self.run("bear --version", run_environment=True)
