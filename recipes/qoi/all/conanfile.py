60b98a1aa232702bcb325f10fe2e649d74517735
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2024-09-29 21:47:35 +0200

    remove comments and unused imports

--- recipes/qoi/all/conanfile.py
+++ recipes/qoi/all/conanfile.py
@@ -1,9 +1,6 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 import os
 
 
@@ -31,7 +28,6 @@ class PackageConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    # Not mandatory when there is no patch, but will suppress warning message about missing build() method
     def build(self):
         pass
 
@@ -48,26 +44,3 @@ class PackageConan(ConanFile):
         # Folders not used for header-only
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
-
-        # # Set these to the appropriate values if the package has an official FindPACKAGE.cmake
-        # # listed in https://cmake.org/cmake/help/latest/manual/cmake-modules.7.html#find-modules
-        # # examples: bzip2, freetype, gdal, icu, libcurl, libjpeg, libpng, libtiff, openssl, sqlite3, zlib...
-        # self.cpp_info.set_property("cmake_module_file_name", "PACKAGE")
-        # self.cpp_info.set_property("cmake_module_target_name", "PACKAGE::PACKAGE")
-        # # Set these to the appropriate values if package provides a CMake config file
-        # # (package-config.cmake or packageConfig.cmake, with package::package target, usually installed in <prefix>/lib/cmake/<package>/)
-        # self.cpp_info.set_property("cmake_file_name", "package")
-        # self.cpp_info.set_property("cmake_target_name", "package::package")
-        # # Set this to the appropriate value if the package provides a pkgconfig file
-        # # (package.pc, usually installed in <prefix>/lib/pkgconfig/)
-        # self.cpp_info.set_property("pkg_config_name", "package")
-
-        # # Add m, pthread and dl if needed in Linux/FreeBSD
-        # if self.settings.os in ["Linux", "FreeBSD"]:
-        #     self.cpp_info.system_libs.extend(["dl", "m", "pthread"])
-
-        # # TODO: to remove in conan v2 once cmake_find_package_* generators removed
-        # self.cpp_info.filenames["cmake_find_package"] = "PACKAGE"
-        # self.cpp_info.filenames["cmake_find_package_multi"] = "package"
-        # self.cpp_info.names["cmake_find_package"] = "PACKAGE"
-        # self.cpp_info.names["cmake_find_package_multi"] = "package"
