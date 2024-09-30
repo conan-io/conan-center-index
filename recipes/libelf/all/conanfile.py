ea6446ce5dfe32676265522e60076a210ac2774b
Joachim Danmayr <joachim@danmayr.org>
2024-09-30 20:09:33 +0200

    chore: Apply suggestions from SpaceIm

--- recipes/libelf/all/conanfile.py
+++ recipes/libelf/all/conanfile.py
@@ -1,8 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import copy, get, replace_in_file, rm, rmdir
-from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdirfrom conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import unix_path
 import os
