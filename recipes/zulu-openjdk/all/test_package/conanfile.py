c93b0baf773ccc0dfbee9fb9da40f59b7cae91e7
Robert Boehne <robb@datalogics.com>
2024-10-01 16:19:45 -0700

    [zulu-openjdk] Bugfixes for the Zulu JDK package
    Recently the Zulu package was enhance by
    adding many different binaries for version 11.0.24.
    However, other changes to support these binary packages should
    also have been made at that time, as none of them were functional.
    
    host settings should identfy the binary type of the package:
     https://github.com/conan-io/conan-center-index/issues/25484
    
    validation should support all the new configurations:
     https://github.com/conan-io/conan-center-index/issues/25483
    
    Zip files with executables need an extra flag:
     https://github.com/conan-io/conan-center-index/issues/25482

--- recipes/zulu-openjdk/all/test_package/conanfile.py
+++ recipes/zulu-openjdk/all/test_package/conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
-from conan.errors import ConanException
-from conan.tools.build import can_run
+from conan.tools.build import cross_building
 from io import StringIO
 
 
@@ -13,13 +12,18 @@ class TestPackage(ConanFile):
         self.requires(self.tested_reference_str)
 
     def build(self):
-        pass
+        pass # nothing to build, but tests should not warn
 
     def test(self):
-        if can_run(self):
-            output = StringIO()
-            self.run("java --version", output, env="conanrun")
-            version_info = output.getvalue()
-            self.output.info(f"java --version returned: \n{version_info}")
-            if "Zulu" not in version_info:
-                raise ConanException("zulu-openjdk test package failed: 'Zulu' not found in java --version output")
+        if cross_building(self):
+            return
+            # OK, this needs some explanation
+            # You basically do not crosscompile that package, never
+            # But C3I does, Macos x86_64 to M1,
+            # and this is why there is some cross compilation going on
+            # The test will not work in that environment, so .... don't test
+        output = StringIO()
+        self.run("java --version", output, env="conanrun")
+        version_info = output.getvalue()
+        if "Zulu" not in version_info:
+            raise Exception("java call seems not use the Zulu bin")
