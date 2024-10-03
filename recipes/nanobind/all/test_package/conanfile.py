eb4fcef420e103ab63a5ac062fa3b2e1073aa9c6
Martin Valgur <martin.valgur@gmail.com>
2024-10-03 10:58:42 +0300

    nanobind: fix test_package for shared cpython

--- recipes/nanobind/all/test_package/conanfile.py
+++ recipes/nanobind/all/test_package/conanfile.py
@@ -1,11 +1,12 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.env import VirtualRunEnv
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv", "VirtualBuildEnv"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv"
     test_type = "explicit"
 
     def requirements(self):
@@ -20,6 +21,11 @@ class TestPackageConan(ConanFile):
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        VirtualRunEnv(self).generate()
+        # Required for find_package(Python) to work with cpython/*:shared=True
+        VirtualRunEnv(self).generate(scope="build")
 
     def build(self):
         cmake = CMake(self)
