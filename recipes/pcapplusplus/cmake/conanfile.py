41e842437201ec6f49a0a3ca8a14c7a433d42a20
Abril Rincón Blanco <git@rinconblanco.es>
2024-10-08 09:40:57 +0200

    Let Conan choose fPIC

--- recipes/pcapplusplus/cmake/conanfile.py
+++ recipes/pcapplusplus/cmake/conanfile.py
@@ -1,30 +1,39 @@
 import os
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
-from conan.tools.files import copy, get, rmdir
+from conan.tools.files import copy, get, rmdir, replace_in_file
 from conan.tools.scm import Version
+from conan.tools.microsoft import is_msvc
 from conan.errors import ConanInvalidConfiguration
 
 
 required_conan_version = ">=1.52.0"
 
+
 class PcapplusplusConan(ConanFile):
     name = "pcapplusplus"
-    package_type = "static-library"
     license = "Unlicense"
     description = "PcapPlusPlus is a multiplatform C++ library for capturing, parsing and crafting of network packets"
     topics = ("pcap", "network", "security", "packet")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/seladb/PcapPlusPlus"
     settings = "os", "arch", "build_type", "compiler"
+    package_type = "library"
     options = {
-        "immediate_mode": [True, False],
         "fPIC": [True, False],
+        "shared": [True, False],
+        "immediate_mode": [True, False],
     }
     default_options = {
-        "immediate_mode": False,
         "fPIC": True,
+        "shared": False,
+        "immediate_mode": False,
     }
+
+    @property
+    def _min_cppstd(self):
+        return 11
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -37,9 +46,13 @@ class PcapplusplusConan(ConanFile):
             self.requires("libpcap/1.10.1")
 
     def validate(self):
+        if self.settings.compiler.cppstd:
+            # popen()/pclose() usage
+            check_min_cppstd(self, self._min_cppstd, gnu_extensions=not is_msvc(self))
         if self.settings.os not in ("FreeBSD", "Linux", "Macos", "Windows"):
             raise ConanInvalidConfiguration(f"{self.settings.os} is not supported")
 
+    def validate_build(self):
         compiler_version = Version(self.settings.compiler.version)
         if self.settings.compiler == "gcc" and compiler_version < "5.1":
             raise ConanInvalidConfiguration("PcapPlusPlus requires GCC >= 5.1")
@@ -51,12 +64,22 @@ class PcapplusplusConan(ConanFile):
         tc = CMakeToolchain(self)
         tc.variables["PCAPPP_BUILD_TESTS"] = False
         tc.variables["PCAPPP_BUILD_EXAMPLES"] = False
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
         tc.generate()
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    def _patch_sources(self):
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "set(CMAKE_CXX_STANDARD 11)",
+                        "")
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "set(CMAKE_POSITION_INDEPENDENT_CODE ON)",
+                        "")
+
     def build(self):
+        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -74,5 +97,7 @@ class PcapplusplusConan(ConanFile):
             self.cpp_info.frameworks = ["CoreFoundation", "SystemConfiguration"]
         elif self.settings.os == "Windows":
             self.cpp_info.system_libs = ["ws2_32", "iphlpapi"]
+        elif self.settings.os in ("Linux", "FreeBSD"):
+            self.cpp_info.system_libs = ["pthread"]
         self.cpp_info.set_property("cmake_file_name", "PcapPlusPlus")
         self.cpp_info.set_property("cmake_target_name", "PcapPlusPlus::PcapPlusPlus")
