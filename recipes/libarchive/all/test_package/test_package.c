7f5a228c69a6a42567bb19fd8826e3484cece420
Adrien <Adnn@users.noreply.github.com>
2024-10-08 16:05:03 +0200

    Extend test_package code to trigger linkage against upstreams.

--- recipes/libarchive/all/test_package/test_package.c
+++ recipes/libarchive/all/test_package/test_package.c
@@ -4,5 +4,11 @@
 int main(int argc, char *argv[]) {
     printf("libarchive version: %s\n", archive_version_string());
     printf("libarchive details: %s\n", archive_version_details());
+
+    // Further test correct linkage against upstreams
+    struct archive * a = archive_read_new();
+    archive_read_support_filter_all(a);
+    archive_read_free(a);
+
     return 0;
 }
