51d080ea4ea518cf418ce1bb6ca941a0467b016f
Alec Edgington <alec.edgington@quantinuum.com>
2024-10-10 09:08:40 +0100

    Update boost version.

--- recipes/symengine/all/conanfile.py
+++ recipes/symengine/all/conanfile.py
@@ -62,7 +62,7 @@ class SymengineConan(ConanFile):
     def requirements(self):
         if self.options.integer_class == "boostmp":
             # symengine/mp_class.h:12
-            self.requires("boost/1.83.0", transitive_headers=True)
+            self.requires("boost/1.86.0", transitive_headers=True)
         else:
             self.requires("gmp/6.3.0", transitive_headers=True, transitive_libs=True)
         if self._needs_fast_float:
