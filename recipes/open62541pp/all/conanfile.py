0060c4af4d3ba0cc8c9cc7cd1825ab5300bb95b6
Martin Valgur <martin.valgur@gmail.com>
2024-10-11 12:35:59 +0300

    open62541pp: bump to v0.15.0

--- recipes/open62541pp/all/conanfile.py
+++ recipes/open62541pp/all/conanfile.py
@@ -4,7 +4,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file
+from conan.tools.files import get, copy, rm, rmdir, replace_in_file
 from conan.tools.microsoft import is_msvc_static_runtime, is_msvc
 from conan.tools.scm import Version
 
