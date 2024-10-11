5beba701a62f61f4b08ba950931e96ff8419c062
Martin Valgur <martin.valgur@gmail.com>
2024-10-11 14:03:15 +0300

    open62541pp: improve test_package.cpp

--- recipes/open62541pp/all/test_package/test_package.cpp
+++ recipes/open62541pp/all/test_package/test_package.cpp
@@ -1,6 +1,10 @@
-#include <open62541pp/open62541pp.h>
+#include <open62541pp/node.hpp>
+#include <open62541pp/server.hpp>
 
 int main() {
-    uint16_t port = 0;
+    int port = 0;
     opcua::Server server(port);
+    opcua::Node parentNode(server, opcua::ObjectId::ObjectsFolder);
+    opcua::Node myIntegerNode = parentNode.addVariable({1, 1000}, "TheAnswer");
+    myIntegerNode.writeValueScalar(42);
 }
