df763941e98ca8a81e17394db957068f1a7842cf
Martin Valgur <martin.valgur@gmail.com>
2024-10-11 20:50:17 +0300

    open62541pp: simplify test_package to avoid STATUS_STACK_BUFFER_OVERRUN on MSVC

--- recipes/open62541pp/all/test_package/test_package.cpp
+++ recipes/open62541pp/all/test_package/test_package.cpp
@@ -4,6 +4,4 @@
 int main() {
     opcua::Server server;
     opcua::Node parentNode(server, opcua::ObjectId::ObjectsFolder);
-    opcua::Node myIntegerNode = parentNode.addVariable({1, 1000}, "TheAnswer");
-    myIntegerNode.writeValueScalar(42);
 }
