b74f003d850687849d4118b70dace0f25a240098
Martin Valgur <martin.valgur@gmail.com>
2024-10-16 09:01:50 +0300

    gdal: use a pre-release of qhull

--- recipes/gdal/post_3.5.0/conanfile.py
+++ recipes/gdal/post_3.5.0/conanfile.py
@@ -282,7 +282,7 @@ class GdalConan(ConanFile):
         if self.options.with_poppler:
             self.requires("poppler/21.07.0")
         if self.options.with_qhull:
-            self.requires("qhull/8.0.1")
+            self.requires("qhull/cci.20231130")
         if self.options.with_rasterlite2:
             self.requires("librasterlite2/1.1.0-beta1")
         if self.options.with_spatialite:
