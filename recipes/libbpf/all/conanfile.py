30a858251e85bbbbfe5d58f08953186d9f9074d4
Martin Valgur <martin.valgur@gmail.com>
2024-10-16 13:16:03 +0300

    libbpf: add v1.4.6, add cross-compilation support

--- recipes/libbpf/all/conanfile.py
+++ recipes/libbpf/all/conanfile.py
@@ -1,11 +1,12 @@
+import os
+
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import copy, get, rm, rmdir, chdir
-from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps, PkgConfigDeps
+from conan.tools.gnu import Autotools, PkgConfigDeps, GnuToolchain
 from conan.tools.layout import basic_layout
-import os
 
-required_conan_version = ">=1.54.0"
+required_conan_version = ">=2.3.0"
 
 class LibbpfConan(ConanFile):
     name = "libbpf"
@@ -42,7 +43,7 @@ class LibbpfConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("linux-headers-generic/5.15.128", transitive_headers=True)
+        self.requires("linux-headers-generic/6.5.9", transitive_headers=True)
         self.requires("elfutils/0.190", transitive_headers=True, transitive_libs=True)
         self.requires("zlib/[>=1.2.11 <2]")
 
@@ -51,27 +52,24 @@ class LibbpfConan(ConanFile):
             raise ConanInvalidConfiguration(f"{self.ref} is only available on Linux")
 
     def build_requirements(self):
-        self.tool_requires("make/4.3")
+        self.tool_requires("make/4.4.1")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        tc = AutotoolsToolchain(self)
-        tc.make_args.extend([
-            "PREFIX={}".format(""),
-            "DESTDIR={}".format(self.package_folder),
-            "LIBSUBDIR={}".format("lib"),
-        ])
+        tc = GnuToolchain(self)
+        tc_vars = tc.extra_env.vars(self)
+        tc.make_args["PREFIX"] = "/"
+        tc.make_args["DESTDIR"] = self.package_folder
+        tc.make_args["LIBSUBDIR"] = "lib"
         if not self.options.shared:
-            tc.configure_args.append("BUILD_STATIC_ONLY={}".format(1))
+            tc.make_args["BUILD_STATIC_ONLY"] = 1
+        tc.make_args["CC"] = tc_vars["CC"]
         tc.generate()
 
         pkgdeps = PkgConfigDeps(self)
         pkgdeps.generate()
-
-        autotoolsdeps = AutotoolsDeps(self)
-        autotoolsdeps.generate()
 
     def build(self):
         with chdir(self, os.path.join(self.source_folder, "src")):
@@ -96,7 +94,4 @@ class LibbpfConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = ["bpf"]
         self.cpp_info.set_property("pkg_config_name", "libbpf")
-
-        # TODO: Remove once v1 is no longer needed
-        self.cpp_info.names["pkg_config"] = "libbpf"
 
