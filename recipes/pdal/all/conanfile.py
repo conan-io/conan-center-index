be0760db07dadca69092b1880ca72d635a377c4b
Martin Valgur <martin.valgur@gmail.com>
2024-10-16 11:50:25 +0300

    pdal: adjust versions

--- recipes/pdal/all/conanfile.py
+++ recipes/pdal/all/conanfile.py
@@ -72,12 +72,12 @@ class PdalConan(ConanFile):
         self.requires("openssl/[>=1.1 <4]") # for arbiter
         self.requires("proj/9.3.1", transitive_headers=True, transitive_libs=True)
         self.requires("rapidxml/1.13", transitive_headers=True) # for arbiter
-        self.requires("utfcpp/4.0.4")
+        self.requires("utfcpp/4.0.5")
         self.requires("zlib/[>=1.2.11 <2]", transitive_headers=True, transitive_libs=True) # for arbiter
         if self.options.with_xml:
             self.requires("libxml2/[>=2.12.5 <3]", transitive_headers=True, transitive_libs=True)
         if self.options.with_zstd:
-            self.requires("zstd/[^1.5]")
+            self.requires("zstd/[~1.5]")
         if self.options.with_lzma:
             self.requires("xz_utils/[>=5.4.5 <6]")
         if self.options.get_safe("with_unwind"):
