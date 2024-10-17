a00811cfb664bd7afc3848c1535c0fad86834ace
Sven Roederer <S.Roederer@colvistec.de>
2024-10-18 00:08:53 +0200

    add dependency to "libjpeg/9f"

--- recipes/CopperSpice/all/conanfile.py
+++ recipes/CopperSpice/all/conanfile.py
@@ -55,6 +55,7 @@ class CopperSpiceConan(ConanFile):
 
     def requirements(self):
         self.requires("cs_libguarded/[>=1.1.0 <2]")
+        self.requires("libjpeg/9f")
         self.requires("zlib/[>=1.3 <2]")
         if self.options.with_mysql_plugin:
             self.options.requires("libmysqlclient/[>=8]")
