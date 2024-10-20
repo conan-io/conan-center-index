65776572e6711db7f5838ca1220aee0191660886
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2024-10-20 16:20:21 +0200

    fix msvc

--- recipes/librtmp/all/conanfile.py
+++ recipes/librtmp/all/conanfile.py
@@ -2,7 +2,7 @@ import os
 
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 
 required_conan_version = ">=1.53.0"
 
@@ -27,6 +27,7 @@ class LibrtmpConan(ConanFile):
 
     def export_sources(self):
         copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -56,6 +57,7 @@ class LibrtmpConan(ConanFile):
         tc.generate()
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
         cmake.build()
