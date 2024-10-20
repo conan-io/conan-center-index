ce3a1963d69f8b2a5ee70985c535a66bc15b98ea
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2024-10-20 15:36:46 +0200

    add librtmp/2.6

---
+++ recipes/librtmp/all/conanfile.py
@@ -0,0 +1,72 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get
+
+required_conan_version = ">=1.53.0"
+
+
+class LibrtmpConan(ConanFile):
+    name = "librtmp"
+    description = "RTMPDump Real-Time Messaging Protocol API"
+    license = "GPL-2.0-or-later"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "http://rtmpdump.mplayerhq.hu"
+    topics = ("rtmpdump", "rtmp", "real-time messaging protocol")
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    def export_sources(self):
+        copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("zlib/[>=1.2.11 <2]")
+        self.requires("openssl/[>=1.1 <4]")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["RTMP_SRC_DIR"] = self.source_folder.replace("\\", "/")
+        tc.variables["RTMP_VERSION"] = self.version
+        tc.variables["RTMP_SOVERSION"] = 1
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
+        cmake.build()
+
+    def package(self):
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.set_property("pkg_config_name", "librtmp")
+        self.cpp_info.libs = ["rtmp"]
+        if self.settings.os == "Windows":
+            self.cpp_info.system_libs.append("ws2_32")
