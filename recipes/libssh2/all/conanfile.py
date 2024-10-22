2f4f264c5ed36047d1586b36143a74bfa297cbab
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-10-22 14:40:25 +0100

    Update recipes/libssh2/all/conanfile.py

--- recipes/libssh2/all/conanfile.py
+++ recipes/libssh2/all/conanfile.py
@@ -91,7 +91,7 @@ class Libssh2Conan(ConanFile):
                 self.requires("mbedtls/2.28.4")
 
     def build_requirements(self):
-        self.tool_requires("cmake/3.20.6")
+        self.tool_requires("cmake/[>=3.20 <4]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
