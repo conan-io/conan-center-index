f581d1565f4b8f60680a3b5fd052ce5c1367e79e
Alexander Allman <alex.allman@mo-sys.com>
2024-10-23 15:56:40 +0100

    Small test written up

--- recipes/opentrackio-cpp/all/test_package/test_package.cpp
+++ recipes/opentrackio-cpp/all/test_package/test_package.cpp
@@ -1,16 +1,24 @@
 #include <cstdlib>
 #include <iostream>
-#include "package/foobar.hpp"
+#include <string>
+#include <opentrackio-cpp/OpenTrackIOSample.h>
 
 
 int main(void) {
-    std::cout << "Create a minimal usage for the target project here." << std::endl;
-    std::cout << "Avoid big examples, bigger than 100 lines" << std::endl;
-    std::cout << "Avoid networking connections." << std::endl;
-    std::cout << "Avoid background apps or servers." << std::endl;
-    std::cout << "The propose is testing the generated artifacts only." << std::endl;
-
-    foobar.print_version();
-
+    std::string example = R"({
+      "protocol": {
+        "name": "OpenTrackIO",
+        "version": "0.9.0"
+      },
+      "tracker": {
+        "notes": "Example generated sample.",
+        "recording": false,
+        "slate": "A101_A_4",
+        "status": "Optical Good"
+      }
+    })"
+    
+    opentrackio::OpenTrackIOSample sample{};
+    sample.initialise(example);
     return EXIT_SUCCESS;
 }
