c0ec252f95f728b31b4421870101cfa729424363
Abril Rincón Blanco <git@rinconblanco.es>
2024-10-29 00:33:00 +0100

    Remove all dependencies that crt brings

--- recipes/aws-sdk-cpp/all/conanfile.py
+++ recipes/aws-sdk-cpp/all/conanfile.py
@@ -502,33 +502,10 @@ class AwsSdkCppConan(ConanFile):
         # These versions come from prefetch_crt_dependency.sh,
         # dont bump them independently, check the file
         if self.version == "1.11.352":
-            self.requires("aws-crt-cpp/0.26.9", transitive_headers=True)
-            self.requires("aws-c-auth/0.7.16")
-            self.requires("aws-c-cal/0.6.14")
-            self.requires("aws-c-common/0.9.15")
-            self.requires("aws-c-compression/0.2.18")  # No mention of this in the code
-            self.requires("aws-c-event-stream/0.4.2")
-            self.requires("aws-c-http/0.8.1")
-            self.requires("aws-c-io/0.14.7")
-            self.requires("aws-c-mqtt/0.10.3")
-            if self.options.get_safe("s3-crt"):
-                self.requires("aws-c-s3/0.5.5")
-            self.requires("aws-c-sdkutils/0.1.15")  # No mention of this in the code
-            self.requires("aws-checksums/0.1.18")
+            self.requires("aws-crt-cpp/0.26.9", transitive_headers=True, transitive_libs=True)
             # missing aws-lc, but only needed as openssl replacement if USE_OPENSSL is OFF
         if self.version == "1.9.234":
-            self.requires("aws-crt-cpp/0.17.1a", transitive_headers=True)
-            self.requires("aws-c-auth/0.6.4")
-            self.requires("aws-c-cal/0.5.12")
-            self.requires("aws-c-common/0.6.11")
-            self.requires("aws-c-compression/0.2.14")
-            self.requires("aws-c-event-stream/0.2.7")
-            self.requires("aws-c-http/0.6.7")
-            self.requires("aws-c-io/0.10.9")
-            self.requires("aws-c-mqtt/0.7.8")
-            if self.options.get_safe("s3-crt"):
-                self.requires("aws-c-s3/0.1.26")
-            self.requires("aws-checksums/0.1.12")
+            self.requires("aws-crt-cpp/0.17.1a", transitive_headers=True, transitive_libs=True)
         if self.settings.os != "Windows":
             # Used transitively in core/utils/crypto/openssl/CryptoImpl.h public header
             self.requires("openssl/[>=1.1 <4]", transitive_headers=True)
@@ -691,22 +668,8 @@ class AwsSdkCppConan(ConanFile):
         self.cpp_info.components["core"].libs = ["aws-cpp-sdk-core"]
         self.cpp_info.components["core"].requires = [
             "aws-crt-cpp::aws-crt-cpp",
-            "aws-c-auth::aws-c-auth",
-            "aws-c-cal::aws-c-cal",
-            "aws-c-common::aws-c-common",
-            "aws-c-compression::aws-c-compression",
-            "aws-c-event-stream::aws-c-event-stream",
-            "aws-c-http::aws-c-http",
-            "aws-c-io::aws-c-io",
-            "aws-c-mqtt::aws-c-mqtt",
-            "aws-checksums::aws-checksums",
             "zlib::zlib"
         ]
-
-        if Version(self.version) >= "1.11.352":
-            self.cpp_info.components["core"].requires.extend([
-                "aws-c-sdkutils::aws-c-sdkutils",
-            ])
 
         for sdk, _ in self._enabled_sdks():
             # TODO: there is no way to properly emulate COMPONENTS names for
--- recipes/aws-sdk-cpp/all/test_package/conanfile.py
+++ recipes/aws-sdk-cpp/all/test_package/conanfile.py
@@ -1,12 +1,12 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "VirtualRunEnv"
     test_type = "explicit"
 
     def layout(self):
@@ -14,6 +14,15 @@ class TestPackageConan(ConanFile):
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+
+    @property
+    def _has_s3_dependency(self):
+        return self.dependencies[self.tested_reference_str].options.s3
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["SDK_HAS_S3"] = self._has_s3_dependency
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -24,3 +33,8 @@ class TestPackageConan(ConanFile):
         if can_run(self):
             bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
             self.run(bin_path, env="conanrun")
+
+            if self._has_s3_dependency:
+                s3_bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package_s3")
+                self.run(s3_bin_path, env="conanrun")
+
