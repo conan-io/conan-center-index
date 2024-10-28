ce6c3298ad40d21fba5e02db3fd144338ddce496
Abril Rincón Blanco <git@rinconblanco.es>
2024-10-28 22:31:03 +0100

    Remove unused imports

--- recipes/devil/all/conanfile.py
+++ recipes/devil/all/conanfile.py
@@ -1,11 +1,6 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.env import VirtualBuildEnv
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
-from conan.tools.microsoft import check_min_vs, is_msvc, is_msvc_static_runtime
-from conan.tools.scm import Version
 import os
 
 
