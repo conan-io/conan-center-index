eb33e92ab536946096503d1ff8265df3b2033f2b
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2024-10-29 12:58:28 +0100

    import check_min_cppstd

--- recipes/efsw/all/conanfile.py
+++ recipes/efsw/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rm, rmdir
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
