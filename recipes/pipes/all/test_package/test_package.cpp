c3f90857d688aca1e14ab53cebc5d75ccff4b652
Uilian Ries <uilianries@gmail.com>
2024-10-30 09:17:05 +0100

    Include optional to fix compilation error
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/pipes/all/test_package/test_package.cpp
+++ recipes/pipes/all/test_package/test_package.cpp
@@ -1,9 +1,11 @@
 // Workaround for pipes not finding size_t
 #include <cstddef>
+#include <cstdlib>
+#include <optional>
+#include <vector>
 
 #include <pipes/pipes.hpp>
 
-#include <vector>
 
 int main() {
     auto source = std::vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -13,8 +15,5 @@ int main() {
         >>= pipes::transform([](int i){ return i * 2; })
         >>= pipes::push_back(destination);
 
-    auto expected = std::vector<int>{0, 4, 8, 12, 16};
-    bool success = destination == expected;
-
-    return success ? 0 : 1;
+    return EXIT_SUCCESS;
 }
