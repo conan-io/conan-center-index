efa8a779ca94ab1424a320aeff19c6b3ddc96606
Uilian Ries <uilianries@gmail.com>
2024-10-31 11:59:26 +0100

    Simplify test package

--- recipes/crossdb/all/test_package/test_package.c
+++ recipes/crossdb/all/test_package/test_package.c
@@ -1,10 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <crossdb.h>
 
 int main () {
-	xdb_conn_t	*pConn = xdb_open (":memory:");
-	XDB_CHECK (NULL != pConn, printf ("failed to create DB\n"); return -1;);
-
-	xdb_close (pConn);
-
-    return 0;
+    printf("Cross DB Version: %s\n", xdb_version());
+    return EXIT_SUCCESS;
 }
