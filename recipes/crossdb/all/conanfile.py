e1b87747602f662d30b42cdd3057c6dd387fd8f7
Uilian Ries <uilianries@gmail.com>
2024-10-31 12:00:31 +0100

    Import fix_apple_shared_install_name

--- recipes/crossdb/all/conanfile.py
+++ recipes/crossdb/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
 from conan.tools.microsoft import is_msvc
+from conan.tools.apple import fix_apple_shared_install_name
 import os
 
 required_conan_version = ">=1.53.0"
