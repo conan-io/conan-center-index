fb58337105e4d4ecd50679b8e65402b37c046a7b
Andrew Marshall <planetmarshalluk@gmail.com>
2024-10-31 11:20:05 +0000

    add glad version 2.x

---
+++ recipes/glad/2.x/conanfile.py
@@ -0,0 +1,138 @@
+import os
+import sys
+from pathlib import PurePosixPath
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, collect_libs, copy, export_conandata_patches, get, rmdir
+from conan.errors import ConanInvalidConfiguration
+
+class GladConan(ConanFile):
+    name = "glad"
+    description = "Multi-Language GL/GLES/EGL/GLX/WGL Loader-Generator based on the official specs."
+    topics = ("opengl",)
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/Dav1dde/glad"
+    license = "MIT"
+    settings = "os", "compiler", "build_type", "arch"
+
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "no_loader": [True, False],
+        "spec": ["gl", "egl", "glx", "wgl"], # Not relevant for 2.x. A spec will be included unless its version is None
+        "extensions": ["ANY"], # A comma separated list of extensions, if missing all extensions are included
+
+        "gl_profile": ["compatibility", "core"],
+        "gl_version": ["None", "1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "2.0",
+                       "2.1", "3.0", "3.1", "3.2", "3.3", "4.0", "4.1", "4.2",
+                       "4.3", "4.4", "4.5", "4.6"],
+        "gles1_version": ["None", "1.0"],
+        "gles2_version": ["None", "2.0", "3.0", "3.1", "3.2"],
+        "glsc2_version": ["None", "2.0"],
+        # if specification is egl
+        "egl_version": ["None", "1.0", "1.1", "1.2", "1.3", "1.4", "1.5"],
+        # if specification is glx
+        "glx_version": ["None", "1.0", "1.1", "1.2", "1.3", "1.4"],
+        # if specification is wgl
+        "wgl_version": ["None", "1.0"]
+    }
+
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "no_loader": False,
+        "spec": "gl",
+        "extensions": "",
+        "gl_profile": "compatibility",
+        "gl_version": "3.3",
+        "gles1_version": "None",
+        "gles2_version": "None",
+        "glsc2_version": "None",
+        "egl_version": "None",
+        "glx_version": "None",
+        "wgl_version": "None"
+    }
+
+    def export_sources(self):
+        copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+            del self.options.wgl_version
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+
+    def validate(self):
+        if (self.options.gles1_version != "None" or self.options.gles2_version != None) and self.options.egl_version == "None":
+            raise ConanInvalidConfiguration(f"{self.ref} Generating an OpenGLES spec requires a valid version of EGL")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+    
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables.update({
+                "Python_EXECUTABLE": sys.executable,
+                "GLAD_SOURCES_DIR": self.source_folder,
+                "GLAD_CONAN_LIB_TYPE": "SHARED" if self.options.shared else "STATIC",
+                "GLAD_CONAN_API": self._get_api(),
+                "GLAD_CONAN_EXTENSIONS": ";".join(str(self.options.extensions).split(","))
+        })
+
+        if not self.options.no_loader:
+            tc.cache_variables["GLAD_CONAN_LOADER"] = "LOADER"
+
+        # This is more than just debug symbols - it wraps all gl calls in a debugging layer.
+        # See https://github.com/Dav1dde/glad/wiki/C#debugging
+        if self.settings.build_type == "Debug":
+            tc.cache_variables["GLAD_CONAN_DEBUG"] = "DEBUG"
+
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=PurePosixPath(self.source_folder).parent)
+        cmake.build()
+
+    def _get_api(self):
+        def api(spec):
+            for api_name, api_version in spec.items():
+                if api_version == "None":
+                    continue
+                if api_name == "gl":
+                    yield f"{api_name}:{self.options.gl_profile}={api_version}"
+                else:
+                    yield f"{api_name}={api_version}"
+
+        spec_api = {
+            "gl": self.options.gl_version,
+            "gles1": self.options.gles1_version,
+            "gles2": self.options.gles2_version,
+            "glsc2": self.options.glsc2_version,
+            "egl": self.options.egl_version,
+            "glx": self.options.glx_version,
+            "wgl": self.options.wgl_version
+        }
+
+        return ";".join(api(spec_api))
+
+    def package(self):
+        CMake(self).install()
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+
+    def package_info(self):
+        self.cpp_info.libs = collect_libs(self)
+        if self.options.shared:
+            self.cpp_info.defines = ["GLAD_API_CALL_EXPORT"]
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs.append("dl")
