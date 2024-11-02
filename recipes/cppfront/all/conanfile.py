1c77b1d6c1976003773421700086e71ab58781c9
toge <toge.mail@gmail.com>
2024-11-03 00:22:34 +0900

    cppfront: add version 0.8.0, remove cci snapshots

--- recipes/cppfront/all/conanfile.py
+++ recipes/cppfront/all/conanfile.py
@@ -11,10 +11,11 @@ required_conan_version = ">=1.50.0"
 class CppfrontConan(ConanFile):
     name = "cppfront"
     description = "Cppfront is a experimental compiler from a potential C++ 'syntax 2' (Cpp2) to today's 'syntax 1' (Cpp1)"
-    topics = ("cpp2", "compiler")
+    license = "CC-BY-NC-ND-4.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/hsutter/cppfront"
-    license = "CC-BY-NC-ND-4.0"
+    topics = ("cpp2", "compiler")
+    package_type = "application"
     settings = "os", "arch", "compiler", "build_type"
 
     @property
