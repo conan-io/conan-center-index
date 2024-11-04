c6fd97ab72d3a4468f46b33eb56f41beb23b9f74
Abril Rincón Blanco <git@rinconblanco.es>
2024-11-04 21:24:56 +0100

    Linter warnings

--- recipes/mimalloc/all/conanfile.py
+++ recipes/mimalloc/all/conanfile.py
@@ -1,13 +1,12 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file, save, collect_libs
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file, collect_libs
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime, VCVars
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.scm import Version
 import os
 import shutil
-import textwrap
 
 required_conan_version = ">=2"
 
