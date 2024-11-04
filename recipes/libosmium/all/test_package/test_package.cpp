7be9ad41e7afb8476ace5974befc0d32f3e231cb
Martin Valgur <martin.valgur@gmail.com>
2024-11-04 19:53:34 +0200

    libosmium: simplify test_package, use only the core headers

--- recipes/libosmium/all/test_package/test_package.cpp
+++ recipes/libosmium/all/test_package/test_package.cpp
@@ -1,13 +1,5 @@
-#include <osmium/io/any_input.hpp>
-
-#include <iostream>
+#include <osmium/osm.hpp>
 
 int main() {
-    try {
-        const osmium::io::File input_file{"missing_file.pbf"};
-        osmium::io::Reader reader{input_file};
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << '\n';
-        return 0;
-    }
+    osmium::OSMEntity entity(1, osmium::item_type::node);
 }
