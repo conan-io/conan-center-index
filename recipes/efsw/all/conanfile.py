090f2cad58e3ff234f6032f002f1e45250eaf8b4
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2024-11-04 13:12:23 +0100

    Add efsw (#25448)
    * add efsw
    
    * streamlined recipe more
    
    * fix missing homepage link
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex Trotta <44127594+Ahajha@users.noreply.github.com>
    
    * import check_min_cppstd
    
    * add Cocoa framework and shorten test package
    
    * Update recipes/efsw/all/test_package/test_package.cpp
    
    * fix test package
    
    * Update recipes/efsw/all/conanfile.py
    
    Co-authored-by: Alex Trotta <44127594+Ahajha@users.noreply.github.com>
    
    * Update recipes/efsw/all/conanfile.py
    
    Co-authored-by: Alex Trotta <44127594+Ahajha@users.noreply.github.com>
    
    * Update recipes/efsw/all/test_package/CMakeLists.txt
    
    Co-authored-by: Alex Trotta <44127594+Ahajha@users.noreply.github.com>
    
    * add missing cstdlib include in testpackage
    
    * fix formatting and other minor stuff
    
    ---------
    
    Co-authored-by: Alex Trotta <44127594+Ahajha@users.noreply.github.com>
    Co-authored-by: Abril Rincón Blanco <git@rinconblanco.es>

---
+++ recipes/efsw/all/conanfile.py
@@ -0,0 +1,79 @@
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, rm, rmdir
+from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
+import os
+
+
+required_conan_version = ">=1.53.0"
+
+
+class EfswConan(ConanFile):
+    name = "efsw"
+    description = "efsw is a C++ cross-platform file system watcher and notifier."
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/SpartanJ/efsw"
+    topics = ("file system", "watch", "cross-platform")
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def validate(self):
+        if self.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, 11)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if is_msvc(self):
+            tc.variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
+        tc.variables["BUILD_TEST_APP"] = False
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "lib"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
+
+    def package_info(self):
+        self.cpp_info.libs = ["efsw"]
+
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs = ["m", "pthread"]
+        
+        if self.settings.os == "Macos":
+            self.cpp_info.frameworks = ["Cocoa", "CoreFoundation", "CoreServices"]
