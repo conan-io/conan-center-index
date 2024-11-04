280cfaa58ff1a5eff0cf4e54a4c0231d089042de
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-11-04 16:09:26 +0000

    Update conanfile.py

--- recipes/dpp/all/conanfile.py
+++ recipes/dpp/all/conanfile.py
@@ -1,6 +1,6 @@
 import os
 from conan import ConanFile
-from conan.tools.build import check_min_cppstd
+from conan.tools.build import check_min_cppstd, cross_building
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 from conan.tools.files import get, copy, rmdir
 from conan.tools.scm import Version
