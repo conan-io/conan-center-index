d2505f05e3b34f8cff754a2e3605cf7d685ddc95
Uilian Ries <uilianries@gmail.com>
2024-11-04 15:27:03 +0100

    Remove unused import
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/mimalloc/all/conanfile.py
+++ recipes/mimalloc/all/conanfile.py
@@ -3,7 +3,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file, save, collect_libs
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime, VCVars
-from conan.tools.scm import Version
 from conan.tools.env import VirtualBuildEnv
 import os
 import shutil
