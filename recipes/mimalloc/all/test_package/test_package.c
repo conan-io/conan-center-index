e4c8f2808ce1831a62685ce7f8957d871274f780
PerseoGI <perseog@jfrog.com>
2024-11-05 10:57:35 +0100

    mimalloc: Fix package error when single_object=True (#25250)
    Co-authored-by: Uilian Ries <uilianries@gmail.com>
    Co-authored-by: Abril Rincón Blanco <git@rinconblanco.es>

--- recipes/mimalloc/all/test_package/test_package.c
+++ recipes/mimalloc/all/test_package/test_package.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 int main() {
-    void *data = mi_malloc(1024);
+    void *data = mi_malloc(32);
 
     printf("mimalloc version %d\n", mi_version());
-    return 0;
+    return EXIT_SUCCESS;
 }
