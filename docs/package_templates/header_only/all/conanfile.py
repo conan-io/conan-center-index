151d1fef5a9a1241a639309b1b6ab62b09a962a7
Uilian Ries <uilianries@gmail.com>
2024-11-05 17:06:06 +0100

    Simplify header-only template
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- docs/package_templates/header_only/all/conanfile.py
+++ docs/package_templates/header_only/all/conanfile.py
@@ -3,7 +3,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 import os
 
 
