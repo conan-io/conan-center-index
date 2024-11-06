bdc15a197aa1ecf6b25505de17b10185ad0127ae
toge <toge.mail@gmail.com>
2024-11-07 00:43:24 +0900

    apply review

--- recipes/libpopcnt/all/conanfile.py
+++ recipes/libpopcnt/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
-from conan.tools.layout import basic_layout
 import os
 
 required_conan_version = ">=1.52.0"
@@ -17,7 +17,7 @@ class LibPopcntConan(ConanFile):
     no_copy_source = True
 
     def layout(self):
-        basic_layout(self, src_folder="src")
+        cmake_layout(self, src_folder="src")
 
     def package_id(self):
         self.info.clear()
@@ -25,14 +25,19 @@ class LibPopcntConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
     def package(self):
         copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
-        copy(
-            self,
-            "libpopcnt.h",
-            self.source_folder,
-            os.path.join(self.package_folder, "include"),
-        )
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.bindirs = []
--- recipes/libpopcnt/all/test_package/test_package.c
+++ recipes/libpopcnt/all/test_package/test_package.c
@@ -1,59 +1,17 @@
 #include "libpopcnt.h"
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #include <stdint.h>
 #include <string.h>
 
-/*
- * Count 1 bits from &data[i] till &data[size].
- * @data: An array for testing
- * @size: size of data
- * @i: Array start index
- */
-void test(uint8_t* data, size_t size, size_t i) {
-  uint64_t bits = popcnt(&data[i], size - i);
-  uint64_t bits_verify = 0;
-
-  for (; i < size; i++)
-    bits_verify += popcnt64_bitwise(data[i]);
-
-  if (bits != bits_verify) {
-    printf("\nlibpopcnt test failed!\n");
-    free(data);
-    exit(1);
-  }
-}
-
 int main() {
-  size_t i;
+  int i = 0;
   size_t size = 70000;
-
   uint8_t* data = (uint8_t*) malloc(size);
 
-  if (!data) {
-    printf("Failed to allocate memory!\n");
-    exit(1);
-  }
-
-  /* init array with only 1 bits */
   memset(data, 0xff, size);
-  test(data, size, 0);
 
-  srand((unsigned) time(0));
-
-  /* generate array with random data */
-  for (i = 0; i < size; i++)
-    data[i] = (uint8_t) rand();
-
-  for (i = 0; i < size; i++)
-    test(data, size, i);
-
-  free(data);
-
-  printf("\rStatus: 100%%\n");
-  printf("libpopcnt tested successfully!\n");
+  uint64_t bits = popcnt(&data[i], size - i);
 
   return 0;
 }
