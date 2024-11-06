90c6b17c7e0f58fd16c8eae621f751bddf2ef566
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-11-06 11:10:25 +0000

    Revert "exprtk: add version 0.0.3 (#25832)"
    This reverts commit 1ab848bed5d0ea3f857c78d08f033d6d399c44de.

--- recipes/exprtk/all/test_package/test_package.cpp
+++ recipes/exprtk/all/test_package/test_package.cpp
@@ -1,36 +1,78 @@
+/*
+ **************************************************************
+ *         C++ Mathematical Expression Toolkit Library        *
+ *                                                            *
+ * Exprtk Test Package                                        *
+ * Author: Arash Partow (1999-2022)                           *
+ * URL: https://www.partow.net/programming/exprtk/index.html  *
+ *                                                            *
+ * Copyright notice:                                          *
+ * Free use of the Mathematical Expression Toolkit Library is *
+ * permitted under the guidelines and in accordance with the  *
+ * most current version of the MIT License.                   *
+ * http://www.opensource.org/licenses/MIT                     *
+ *                                                            *
+ **************************************************************
+*/
+
+
 #include <cstdio>
 #include <string>
 
-#include "exprtk.hpp"
+#include <exprtk.hpp>
+
 
 template <typename T>
-void trig_function() {
-   typedef exprtk::symbol_table<T> symbol_table_t;
-   typedef exprtk::expression<T>   expression_t;
-   typedef exprtk::parser<T>       parser_t;
-
-   const std::string expression_string =
-      "clamp(-1.0, sin(2 * pi * x) + cos(x / 2 * pi), +1.0)";
-
-   T x;
+void test_function()
+{
+   typedef exprtk::symbol_table<T>    symbol_table_t;
+   typedef exprtk::expression<T>      expression_t;
+   typedef exprtk::parser<T>          parser_t;
+   typedef exprtk::parser_error::type error_t;
 
    symbol_table_t symbol_table;
-   symbol_table.add_variable("x",x);
-   symbol_table.add_constants();
+   expression_t   expression;
+   parser_t       parser;
 
-   expression_t expression;
+   T x = 1;
+   T y = 2;
+
+   const std::string expression_string = "x * y + 3";
+
+   symbol_table.add_variable("x",x);
+   symbol_table.add_variable("y",y);
+
    expression.register_symbol_table(symbol_table);
 
-   parser_t parser;
-   parser.compile(expression_string,expression);
+   if (!parser.compile(expression_string,expression))
+   {
+      printf("Error: %s\tExpression: %s\n",
+             parser.error().c_str(),
+             expression_string.c_str());
 
-   for (x = T(-5); x <= T(+5); x += T(0.1)) {
-      const T y = expression.value();
-      printf("%19.15f\t%19.15f\n", x, y);
+      for (std::size_t i = 0; i < parser.error_count(); ++i)
+      {
+         const error_t error = parser.get_error(i);
+
+         printf("Error: %02d Position: %02d "
+                "Type: [%s] "
+                "Message: %s "
+                "Expression: %s\n",
+                static_cast<int>(i),
+                static_cast<int>(error.token.position),
+                exprtk::parser_error::to_str(error.mode).c_str(),
+                error.diagnostic.c_str(),
+                expression_string.c_str());
+      }
+
+      return;
    }
+
+   expression.value();
 }
 
-int main() {
-   trig_function<double>();
+int main()
+{
+   test_function<double>();
    return 0;
 }
