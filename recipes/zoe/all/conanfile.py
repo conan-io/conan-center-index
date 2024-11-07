2e1d98e17f8f7f5e60ff2911dd4b410903e608a4
Abril Rincón Blanco <git@rinconblanco.es>
2024-11-07 18:13:12 +0100

    Update recipes/zoe/all/conanfile.py

--- recipes/zoe/all/conanfile.py
+++ recipes/zoe/all/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=2"
+required_conan_version = ">=2.1"
 
 class ZoeConan(ConanFile):
     name = "zoe"
