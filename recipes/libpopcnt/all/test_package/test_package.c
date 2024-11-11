e3f977deb42be76d6ecfbfd0cb8fb1c4d6511b5a
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-11-11 16:19:19 +0000

    minimal test package

--- recipes/libpopcnt/all/test_package/test_package.c
+++ recipes/libpopcnt/all/test_package/test_package.c
@@ -1,19 +1,10 @@
-#include "libpopcnt.h"
-
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <inttypes.h>
+#include <libpopcnt.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 int main() {
-  int i = 0;
-  size_t size = 42;
-  uint8_t* data = (uint8_t*) malloc(size);
-
-  memset(data, 0xff, size);
-
-  uint64_t bits = popcnt(&data[i], size - i);
-  printf(PRIu64 "\n", bits);
+  const char data = 0xff;
+  uint64_t bits = popcnt(&data, sizeof(char));
+  printf("popcnt(0xff) = %" PRIu64 " (expected 8) - Test package successful \n", bits);
   return 0;
 }
