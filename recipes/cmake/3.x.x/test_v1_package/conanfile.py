98d631f008116d272ad648f78320e0684386ffe1
toge <toge.mail@gmail.com>
2024-11-11 11:41:00 +0900

    revert removing test_v1_packages

---
+++ recipes/cmake/3.x.x/test_v1_package/conanfile.py
@@ -0,0 +1,23 @@
+import os
+from six import StringIO
+from conan import ConanFile
+import re
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    test_type = "explicit"
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
+
+    def test(self):
+        output = StringIO()
+        self.run("cmake --version", output=output, run_environment=False)
+        output_str = str(output.getvalue())
+        self.output.info("Installed version: {}".format(output_str))
+        tokens = re.split('[@#]', self.tested_reference_str)
+        require_version = tokens[0].split("/", 1)[1]
+        self.output.info("Expected version: {}".format(require_version))
+        assert_cmake_version = "cmake version %s" % require_version
+        assert(assert_cmake_version in output_str)
