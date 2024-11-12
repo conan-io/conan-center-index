85263c6381165cdf7c8261b3a7239ebb93584c8c
toge <toge.mail@gmail.com>
2024-11-12 18:44:14 +0900

    support shared build

--- recipes/seq/all/conanfile.py
+++ recipes/seq/all/conanfile.py
@@ -1,9 +1,7 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rmdir, replace_in_file
-from conan.tools.scm import Version
+from conan.tools.files import copy, get, rmdir, replace_in_file, rm
 from conan.tools.env import VirtualBuildEnv
 import os
 
@@ -15,38 +13,30 @@ class PackageConan(ConanFile):
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/Thermadiag/seq"
-    topics = ("formatting", "hashmap", "radix", "header-only")
-    package_type = "header-library"
+    topics = ("formatting", "hashmap", "radix")
+    package_type = "shared-library"
     settings = "os", "arch", "compiler", "build_type"
     short_paths = True
+    options = {
+        "header_only": [True, False],
+    }
+    default_options = {
+        "header_only": False,
+    }
 
-    @property
-    def _min_cppstd(self):
-        return 14
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "apple-clang": "10",
-            "clang": "7",
-            "gcc": "7",
-            "msvc": "191",
-            "Visual Studio": "15",
-        }
+    def config_options(self):
+        if self.options.header_only:
+            self.package_type = "header-library"
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def package_id(self):
-        self.info.clear()
+        if self.info.options.header_only:
+            self.info.clear()
 
     def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 14)
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.16 <4]")
@@ -56,7 +46,7 @@ class PackageConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["HEADER_ONLY"] = True
+        tc.variables["HEADER_ONLY"] = self.options.header_only
         tc.generate()
         venv = VirtualBuildEnv(self)
         venv.generate(scope="build")
@@ -74,10 +64,15 @@ class PackageConan(ConanFile):
 
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "share"))
+        if not self.options.header_only:
+            rm(self, "*.cpp", os.path.join(self.package_folder, "include"), recursive=True)
 
     def package_info(self):
-        self.cpp_info.bindirs = []
-        self.cpp_info.libdirs = []
+        if self.options.header_only:
+            self.cpp_info.bindirs = []
+            self.cpp_info.libdirs = []
+        else:
+            self.cpp_info.libs = ["seq"]
 
         self.cpp_info.set_property("cmake_module_file_name", "seq")
         self.cpp_info.set_property("cmake_module_target_name", "seq::seq")
