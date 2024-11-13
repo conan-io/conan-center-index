c8ecb6c61ccfb99cce3b460680b2f6c286396e3e
Martin Valgur <martin.valgur@gmail.com>
2024-11-13 19:00:12 +0200

    Drop all non-migrated deprecated recipes

--- recipes/co/all/test_package/test_package.cc
+++
@@ -1,7 +0,0 @@
-#include "co/str.h"
-#include <iostream>
-
-int main() {
-  std::cout << str::from(123) << std::endl;
-  return 0;
-}
--- recipes/msgpack/all/test_package/test_package.c
+++
@@ -1,8 +0,0 @@
-#include <msgpack.h>
-
-#include <stdio.h>
-
-int main() {
-    printf("msgpack version %s\n", msgpack_version());
-    return 0;
-}
