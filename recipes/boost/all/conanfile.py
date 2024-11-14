b7c75f797e1be001a65c744826ff7559a58b036d
Pavel Koshevoy <pkoshevoy@gmail.com>
2024-11-13 18:10:30 -0700

    Use lowercase advapi32 to avoid linker error on linux with mingw

--- recipes/boost/all/conanfile.py
+++ recipes/boost/all/conanfile.py
@@ -2046,7 +2046,7 @@ class BoostConan(ConanFile):
 
             if not self.options.get_safe("without_process"):
                 if self.settings.os == "Windows":
-                    self.cpp_info.components["process"].system_libs.extend(["ntdll", "shell32", "Advapi32", "user32"])
+                    self.cpp_info.components["process"].system_libs.extend(["ntdll", "shell32", "advapi32", "user32"])
                 if self._shared:
                     self.cpp_info.components["process"].defines.append("BOOST_PROCESS_DYN_LINK")
 
