a778477ca058ff01397621ec12a7dbef98c89191
Martin Valgur <martin.valgur@gmail.com>
2024-11-15 17:21:50 +0200

    gst-plugins-base: drop unused imports

--- recipes/gst-plugins-base/all/conanfile.py
+++ recipes/gst-plugins-base/all/conanfile.py
@@ -1,12 +1,11 @@
 import glob
 import os
 import shutil
-import textwrap
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
-from conan.tools.files import chdir, copy, get, rm, rmdir, save
+from conan.tools.files import chdir, copy, get, rm, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.meson import MesonToolchain, Meson
