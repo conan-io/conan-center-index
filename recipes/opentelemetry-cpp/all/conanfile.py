64ae9297120e07a3a0ca21a54e0892b6a37e78fb
Abril Rincón Blanco <git@rinconblanco.es>
2024-11-18 22:23:10 +0100

    Update recipes/opentelemetry-cpp/all/conanfile.py

--- recipes/opentelemetry-cpp/all/conanfile.py
+++ recipes/opentelemetry-cpp/all/conanfile.py
@@ -184,8 +184,7 @@ class OpenTelemetryCppConan(ConanFile):
         return ["locale"] if self.options.get_safe("with_jaeger") else []
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
+        check_min_cppstd(self, self._min_cppstd)
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if minimum_version and Version(self.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(
