72c08e522136a5a59ab5a6c343a0f62e7b2a9590
Saikari <lin@sz.cn.eu.org>
2024-11-19 13:56:41 +0300

    Update recipes/keystone/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/keystone/all/conanfile.py
+++ recipes/keystone/all/conanfile.py
@@ -79,6 +79,7 @@ class KeystoneConan(ConanFile):
             src=self.source_folder,
             dst=os.path.join(self.package_folder, "licenses"),
         )
+        copy(self, "LICENSE*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
