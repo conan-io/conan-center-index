7c121f1efa216e4080080f91cab3040b968ff55f
Uilian Ries <uilianries@gmail.com>
2024-11-19 17:47:31 +0100

    Import missing check_max_cppstd

--- recipes/keystone/all/conanfile.py
+++ recipes/keystone/all/conanfile.py
@@ -2,6 +2,7 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
+from conan.tools.build import check_max_cppstd
 import os
 
 required_conan_version = ">=2.0"
