c48fe138e81378ecbcf6cb443bb4315edfdf2ed7
PerseoGI <perseog@jfrog.com>
2024-11-20 16:43:19 +0100

    Some updates

--- recipes/libdisplay-info/all/conanfile.py
+++ recipes/libdisplay-info/all/conanfile.py
@@ -2,20 +2,19 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import cross_building
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import copy, get, replace_in_file, rm, rmdir, save
+from conan.tools.files import copy, get, replace_in_file, rm, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.meson import Meson, MesonToolchain
 import os
-import textwrap
 
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.0.9"
 
 
 class LibdisplayInfoConan(ConanFile):
     name = "libdisplay-info"
-    description = "EDID and DisplayID library."
+    description = "EDID and DisplayID library"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://gitlab.freedesktop.org/emersion/libdisplay-info"
@@ -30,14 +29,7 @@ class LibdisplayInfoConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-
-    @property
-    def _has_build_profile(self):
-        return hasattr(self, "settings_build")
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    implements = ["auto_shared_fpic"]
 
     def configure(self):
         if self.options.shared:
@@ -54,9 +46,9 @@ class LibdisplayInfoConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("hwdata/0.376")
-        self.tool_requires("meson/1.2.3")
-        if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/2.0.3")
+        self.tool_requires("meson/[>=1.2.3 <2]")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/[>=2.2 <3]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -70,26 +62,9 @@ class LibdisplayInfoConan(ConanFile):
             tc.project_options["build.pkg_config_path"] = self.generators_folder
         tc.generate()
 
-        if self._has_build_profile:
-            pkg_config_deps = PkgConfigDeps(self)
-            pkg_config_deps.build_context_activated = ["hwdata"]
-            pkg_config_deps.generate()
-        else:
-            # Manually generate pkgconfig file of hwdata since
-            # PkgConfigDeps.build_context_activated can't work with legacy 1 profile.
-            # We must use legacy conan v1 deps_cpp_info because self.dependencies doesn't
-            # contain build requirements when using 1 profile.
-            hwdata_prefix = self.deps_cpp_info["hwdata"].rootpath
-            hwdata_version = self.deps_cpp_info["hwdata"].version
-            hwdata_pkg_content = textwrap.dedent(f"""\
-                prefix={hwdata_prefix}
-                datadir=${{prefix}}/res
-                pkgdatadir=${{datadir}}/hwdata
-                Name: hwdata
-                Description: Hardware identification and configuration data
-                Version: {hwdata_version}
-            """)
-            save(self, os.path.join(self.generators_folder, "hwdata.pc"), hwdata_pkg_content)
+        pkg_config_deps = PkgConfigDeps(self)
+        pkg_config_deps.build_context_activated = ["hwdata"]
+        pkg_config_deps.generate()
 
     def _patch_sources(self):
         replace_in_file(self, os.path.join(self.source_folder, "meson.build"), "subdir('test')", "# subdir('test')")
