05500cde3dd150b643db574099a27b7a3ea7344d
kleto zan <kletoz@gmail.com>
2024-11-21 17:16:17 -0300

    protobuf: add exclusive test to lite mode

--- recipes/protobuf/all/test_package/test_package.cpp
+++ recipes/protobuf/all/test_package/test_package.cpp
@@ -1,15 +1,35 @@
 #include <cstdlib>
 #include <iostream>
 
-#include <google/protobuf/timestamp.pb.h>
-#include <google/protobuf/util/time_util.h>
+#if protobuf_LITE != ON
+#  include <google/protobuf/timestamp.pb.h>
+#  include <google/protobuf/util/time_util.h>
+#  include "addressbook.pb.h"
+#else
+#  include "addressbook-lite.pb.h"
+#endif
+
 
 int main()
 {
+    // example from https://protobuf.dev/getting-started/cpptutorial/
+    tutorial::AddressBook address_book;
+    tutorial::Person* person = address_book.add_people();
+    person->set_email("a@b.c");
+    tutorial::Person::PhoneNumber* phone_number = person->add_phones();
+    phone_number->set_number("123");
+    
+    std::vector<char> buffer(address_book.ByteSizeLong());
+    address_book.SerializeToArray(buffer.data(), static_cast<int>(buffer.size()));
+
+    tutorial::AddressBook address_book2;
+    address_book2.ParseFromArray(buffer.data(), static_cast<int>(buffer.size()));
+
+#if protobuf_LITE != ON
 	google::protobuf::Timestamp ts;
 	google::protobuf::util::TimeUtil::FromString("1972-01-01T10:00:20.021Z", &ts);
 	const auto nanoseconds = ts.nanos();
-
 	std::cout << "1972-01-01T10:00:20.021Z in nanoseconds: " << nanoseconds << "\n";
+#endif
 	return EXIT_SUCCESS;
 }
