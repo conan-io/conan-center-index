74c2e2e8266f78f532f37212f66b8be308625cd8
Uilian Ries <uilianries@gmail.com>
2024-11-21 09:02:48 +0100

    Remove unused import
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/joltphysics/5.x/conanfile.py
+++ recipes/joltphysics/5.x/conanfile.py
@@ -1,5 +1,4 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir, rm
