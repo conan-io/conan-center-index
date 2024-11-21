680f931c9ad9ebf7bd3634fcdf60a48c638d7c5f
Andrey Filipenkov <decapitator@ukr.net>
2024-11-21 12:24:09 +0300

    bump libdeflate

--- recipes/libtiff/all/conanfile.py
+++ recipes/libtiff/all/conanfile.py
@@ -65,7 +65,7 @@ class LibtiffConan(ConanFile):
         if self.options.zlib:
             self.requires("zlib/[>=1.2.11 <2]")
         if self.options.libdeflate:
-            self.requires("libdeflate/1.19")
+            self.requires("libdeflate/1.22")
         if self.options.lzma:
             self.requires("xz_utils/5.4.5")
         if self.options.jpeg == "libjpeg":
