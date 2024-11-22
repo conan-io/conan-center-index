f7ec85eb917ed505173ced1b6bab35f55c98a21c
Uilian Ries <uilianries@gmail.com>
2024-11-22 15:18:24 +0100

    the library is pure-c
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/svtjpegxs/all/conanfile.py
+++ recipes/svtjpegxs/all/conanfile.py
@@ -1,7 +1,6 @@
 import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import cmake_layout, CMakeToolchain, CMake
 from conan.tools.files import copy, get, rmdir, apply_conandata_patches, export_conandata_patches
 from conan.tools.microsoft import is_msvc
@@ -31,6 +30,12 @@ class SvtJpegXsConan(ConanFile):
     def export_sources(self):
         export_conandata_patches(self)
 
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
     def layout(self):
         cmake_layout(self, src_folder="src")
 
@@ -38,8 +43,6 @@ class SvtJpegXsConan(ConanFile):
         self.tool_requires("yasm/1.3.0")
 
     def validate(self):
-        check_min_cppstd(self, 11)
-
         if self.settings.arch not in ["x86", "x86_64"]:
             # INFO: The upstream mention about only supporting x86, SSE and AVX
             # https://github.com/OpenVisualCloud/SVT-JPEG-XS/tree/v0.9.0?tab=readme-ov-file#environment-and-requirements
