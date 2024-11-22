1460400ca308a4b65dc2f34f4a48ff85fceb62df
Uilian Ries <uilianries@gmail.com>
2024-11-22 11:42:14 +0100

    Reduce more test package
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/joltphysics/5.x/test_package/test_package.cpp
+++ recipes/joltphysics/5.x/test_package/test_package.cpp
@@ -1,25 +1,8 @@
 #include <cstdlib>
-
 #include <Jolt/Jolt.h>
-#include <Jolt/RegisterTypes.h>
-#include <Jolt/Core/Factory.h>
-#include <Jolt/Core/TempAllocator.h>
-#include <Jolt/Core/JobSystemThreadPool.h>
-#include <Jolt/Physics/PhysicsSettings.h>
 
 
-int main()
-{
+int main() {
     JPH::RegisterDefaultAllocator();
-
-    JPH::Factory::sInstance = new JPH::Factory();
-
-    JPH::RegisterTypes();
-
-    JPH::TempAllocatorImpl temp_allocator(16);
-    JPH::JobSystemThreadPool job_system(JPH::cMaxPhysicsJobs, JPH::cMaxPhysicsBarriers, std::thread::hardware_concurrency() - 1);
-
-    delete JPH::Factory::sInstance;
-
     return EXIT_SUCCESS;
 }
