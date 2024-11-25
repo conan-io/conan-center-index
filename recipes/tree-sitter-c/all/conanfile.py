aa0f6214000dce616c0c0b4fc6b871895fe6835d
toge <toge.mail@gmail.com>
2024-11-25 18:42:08 +0900

    tree-sitter-c: add version 0.23.2 (#26014)

--- recipes/tree-sitter-c/all/conanfile.py
+++ recipes/tree-sitter-c/all/conanfile.py
@@ -1,6 +1,8 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import get, replace_in_file, copy
+from conan.tools.files import get, replace_in_file, apply_conandata_patches, copy, export_conandata_patches, rmdir
+
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.53.0"
@@ -29,11 +31,14 @@ class TreeSitterCConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def export_sources(self):
-        copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=(self.export_sources_folder + "/src"))
+        export_conandata_patches(self)
+        if Version(self.version) < "0.23.2":
+            copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=os.path.join(self.export_sources_folder, "src"))
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["TREE_SITTER_C_SRC_DIR"] = self.source_folder.replace("\\", "/")
+        if Version(self.version) < "0.23.2":
+            tc.variables["TREE_SITTER_C_SRC_DIR"] = self.source_folder.replace("\\", "/")
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
@@ -50,12 +55,13 @@ class TreeSitterCConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def requirements(self):
         self.requires("tree-sitter/0.24.3", transitive_headers=True, transitive_libs=True)
 
     def _patch_sources(self):
-        if not self.options.shared:
+        if Version(self.version) < "0.23.2" and not self.options.shared:
             replace_in_file(
                 self,
                 os.path.join(self.source_folder, "src", "parser.c"),
@@ -77,6 +83,7 @@ class TreeSitterCConan(ConanFile):
         )
         cmake = CMake(self)
         cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.libs = ["tree-sitter-c"]
