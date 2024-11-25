4322899d724e7032e1e70b4f4a9acc8fb2fb2649
Abril Rincón Blanco <git@rinconblanco.es>
2024-11-25 13:44:38 +0100

    Add missing Version

--- recipes/openimageio/all/conanfile.py
+++ recipes/openimageio/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, get, rm, rmdir
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
+from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
 import os
 
