22888d9abe00b06a600ab88d074b3591d89001a6
Dr. Patrick Urbanke <patrick@getml.com>
2024-11-26 11:39:44 +0100

    Added missing import

--- recipes/reflectcpp/all/conanfile.py
+++ recipes/reflectcpp/all/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.files import get, copy, rmdir
+from conan.tools.files import get, copy, rmdir, replace_in_file
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.build import check_min_cppstd
