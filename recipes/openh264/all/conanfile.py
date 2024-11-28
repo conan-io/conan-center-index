1a2396a16287af38a8d2a890dae8ba4e46b5869c
Abril Rincón Blanco <git@rinconblanco.es>
2024-11-28 11:10:57 +0100

    Simplify

--- recipes/openh264/all/conanfile.py
+++ recipes/openh264/all/conanfile.py
@@ -103,13 +103,9 @@ class OpenH264Conan(ConanFile):
                     # INFO: Preserve same old library name as when building with Make on Windows but using Meson
                     rename(self, os.path.join(self.package_folder, "lib", "openh264.lib"),
                            os.path.join(self.package_folder, "lib", "openh264_dll.lib"))
-                else:
-                    rename(self, os.path.join(self.package_folder, "lib", "libopenh264.a"),
-                           os.path.join(self.package_folder, "lib", "openh264.lib"))
-            else:
-                if not self.options.shared:
-                    rename(self, os.path.join(self.package_folder, "lib", "libopenh264.a"),
-                           os.path.join(self.package_folder, "lib", "openh264.lib"))
+            if not self.options.shared:
+                rename(self, os.path.join(self.package_folder, "lib", "libopenh264.a"),
+                        os.path.join(self.package_folder, "lib", "openh264.lib"))
         fix_apple_shared_install_name(self)
 
     def package_info(self):
