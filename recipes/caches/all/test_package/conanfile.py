31bb423fd6295a04e74a0ad48b564af9586314b4
Joel Lefkowitz <joel.lefkowitz@teya.com>
2024-12-01 18:30:05 +0000

    Remove SCons builder

--- recipes/caches/all/test_package/conanfile.py
+++ recipes/caches/all/test_package/conanfile.py
@@ -1,16 +1,28 @@
 from conan import ConanFile
-from conan.tools.scons import SConsDeps
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
+import os
 
 
 class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    requires = ["gtest/1.12.1"]
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
     def requirements(self):
         self.requires(self.tested_reference_str)
 
-    def build_requirements(self):
-        self.test_requires("gtest/1.12.1")
-
-    def generate(self):
-        SConsDeps(self).generate()
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
-        self.run("scons test")
+        if can_run(self):
+            root = self.cpp.build.bindirs[0]
+            self.run(os.path.join(root, "test_package"))
