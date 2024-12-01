4540cd92469c0ddd9c0fa56af3d436c49f8cdc30
Joel Lefkowitz <joel.lefkowitz@teya.com>
2024-12-01 19:53:17 +0000

    Add gtest dependency

--- recipes/caches/all/test_package/conanfile.py
+++ recipes/caches/all/test_package/conanfile.py
@@ -7,13 +7,13 @@ import os
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain"
-    requires = ["gtest/1.12.1"]
 
     def layout(self):
         cmake_layout(self)
 
-    def requirements(self):
+    def build_requirements(self):
         self.requires(self.tested_reference_str)
+        self.test_requires("gtest/1.12.1")
 
     def build(self):
         cmake = CMake(self)
@@ -22,5 +22,4 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
-            self.run(bin_path, env="conanrun")
+            self.run(os.path.join(self.cpp.build.bindir, "test_package"))
