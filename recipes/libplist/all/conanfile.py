f18e9648d62702aafb98ddadf88e4689514ec217
Abril Rincón Blanco <git@rinconblanco.es>
2024-12-03 10:13:17 +0100

    unused

--- recipes/libplist/all/conanfile.py
+++ recipes/libplist/all/conanfile.py
@@ -3,7 +3,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.build import cross_building
 from conan.tools.env import VirtualRunEnv
-from conan.tools.files import copy, get, rm, rmdir, apply_conandata_patches, export_conandata_patches
+from conan.tools.files import copy, get, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
 import os
