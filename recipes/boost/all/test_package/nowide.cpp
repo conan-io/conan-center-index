4a318277d90e7d08171e813f5b4d1c4688d371dd
Uilian Ries <uilianries@gmail.com>
2024-12-03 17:52:50 +0100

    Fix nowide test
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/boost/all/test_package/nowide.cpp
+++ recipes/boost/all/test_package/nowide.cpp
@@ -1,9 +1,7 @@
 #include <boost/nowide/args.hpp>
-#include <iostream>
 
 int main(int argc,char **argv)
 {
     boost::nowide::args args(argc,argv);
-    std::cout << "Testing Boost::Nowide: " << args.argc() << std::endl;
     return 0;
 }
