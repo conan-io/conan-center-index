1f387010951e94d291c69a1ec6e0c7b60ccf0d77
Farzin Monsef <farzin@inphraz.ir>
2024-12-03 15:28:21 +0000

    Get library version in test_package

--- recipes/libiec61850/all/test_package/test_package.cpp
+++ recipes/libiec61850/all/test_package/test_package.cpp
@@ -2,32 +2,10 @@
 #include <format>
 #include <cstdlib>
 
-#include "libiec61850/iec61850_client.h"
+#include "libiec61850/iec61850_common.h"
 
 int main(int argc, char **argv)
 {
-    std::string hostname("localhost");
-    int tcpPort = 102;
-
-    IedClientError error;
-
-    IedConnection con = IedConnection_create();
-
-    IedConnection_connect(con, &error, hostname.c_str(), tcpPort);
-
-    std::cout << std::format("Connecting to {}:{}\n", hostname, tcpPort);
-
-    if (error == IED_ERROR_OK)
-    {
-        std::cout << "Connected\n";
-        IedConnection_close(con);
-    }
-    else
-    {
-        std::cerr << std::format("Failed to connect to {}:{}\n", hostname, tcpPort);
-    }
-
-    IedConnection_destroy(con);
-
+    std::cout << std::format("libiec61850 v{}\n", LibIEC61850_getVersionString());
     return 0;
 }
