318773086df3b559582b2fcd05d68c03bf6e9077
Farzin Monsef <farzin@inphraz.ir>
2024-12-03 15:40:28 +0000

    simpler test_package

--- recipes/libiec61850/all/test_package/test_package.cpp
+++ recipes/libiec61850/all/test_package/test_package.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <format>
 #include <cstdlib>
 
 #include "libiec61850/iec61850_common.h"
 
 int main(int argc, char **argv)
 {
-    std::cout << std::format("libiec61850 v{}\n", LibIEC61850_getVersionString());
+    std::cout << "libiec61850 v" << LibIEC61850_getVersionString() << std::endl;
     return 0;
 }
