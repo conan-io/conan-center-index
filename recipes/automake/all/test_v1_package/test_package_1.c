63521493aa569366f8bb3ed9260750bb4533f9f4
PerseoGI <perseog@jfrog.com>
2024-12-05 17:13:15 +0100

    Maintenance: remove all test_v1_packages excluding non migrated recipes and recipes with more than one test_ folder

--- recipes/automake/all/test_v1_package/test_package_1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    puts("test_package.c says 'Hello World'!\n");
-    return EXIT_SUCCESS;
-}
--- recipes/kcov/all/test_v1_package/conanfile.py
+++
@@ -1,9 +0,0 @@
-from conans import ConanFile, tools
-
-
-class KcovTestConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-
-    def test(self):
-        if not tools.cross_building(self.settings):
-            self.run("kcov --version", run_environment=True)
