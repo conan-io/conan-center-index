d2c6a879b9f7a21bd3b06ef52f5f3ccc1fbf87e6
Abril Rincón Blanco <git@rinconblanco.es>
2024-12-09 13:56:54 +0100

    Use fork for libid3tag

--- recipes/libid3tag/all/test_package/test_package.c
+++ recipes/libid3tag/all/test_package/test_package.c
@@ -4,13 +4,20 @@
 
 int main()
 {
-    printf("id3tag version: %s\n", id3_version);
-    printf("id3tag copyright: %s\n", id3_copyright);
-    printf("id3tag author: %s\n", id3_author);
+    printf("id3tag version: %s\n", ID3_VERSION);
+    printf("id3tag author: %s\n", ID3_AUTHOR);
+    #ifndef CCI_SKIP_GLOBALS
+    // Ensure extern variables also work.
+    // This is disabled for shared due to Window
+    // WINDOWS_EXPORT_ALL_SYMBOLS still needs __declspec usage
+    // for global data symbols as per CMake docs
+    // but upstream does not use it
     printf("id3tag build: %s\n", id3_build);
+    #endif
 
     struct id3_tag tag;
     id3_tag_version(&tag);
 
+    printf("id3tag default tag version: %d\n", tag.version);
     return 0;
 }
