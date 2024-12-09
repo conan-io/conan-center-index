969aeeda1c7f90c1e67a8af81c5457f9b36bd407
Martin Valgur <martin.valgur@gmail.com>
2024-12-09 11:38:00 +0200

    gnsstk: modernize for Conan v2

--- recipes/gnsstk/all/conanfile.py
+++ recipes/gnsstk/all/conanfile.py
@@ -1,13 +1,12 @@
 import os
 
 from conan import ConanFile
-from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.build import check_min_cppstd, valid_min_cppstd
+from conan.tools.build import check_min_cppstd, check_max_cppstd
 from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain
 from conan.tools.files import get, copy, rmdir, save, replace_in_file, export_conandata_patches, apply_conandata_patches, mkdir, rename
 from conan.tools.scm import Version
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.0.9"
 
 
 class GNSSTkConan(ConanFile):
@@ -36,47 +35,23 @@ class GNSSTkConan(ConanFile):
     options_description = {
         "build_ext": "Build the ext library, in addition to the core library.",
     }
-
-    @property
-    def _min_cppstd(self):
-        return 11
+    implements = ["auto_shared_fpic"]
 
     def export_sources(self):
         export_conandata_patches(self)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            # https://github.com/SGL-UT/gnsstk/blob/v14.0.0/BuildSetup.cmake#L54
-            check_min_cppstd(self, self._min_cppstd)
+        # https://github.com/SGL-UT/gnsstk/blob/v14.0.0/BuildSetup.cmake#L54
+        check_min_cppstd(self, 11)
+        # https://github.com/SGL-UT/gnsstk/pull/8
+        check_max_cppstd(self, 17)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def generate(self):
-        tc = CMakeToolchain(self)
-        # https://github.com/SGL-UT/gnsstk/blob/v14.0.0/CMakeLists.txt#L41-L51
-        tc.variables["BUILD_EXT"] = self.options.build_ext
-        tc.variables["VERSIONED_HEADER_INSTALL"] = True
-        tc.variables["USE_RPATH"] = False  # Adds install directory to RPATH otherwise
-        if not valid_min_cppstd(self, self._min_cppstd):
-            # The C++ standard is not set correctly by the project for apple-clang
-            tc.variables["CMAKE_CXX_STANDARD"] = self._min_cppstd
-        # Relocatable shared libs on macOS
-        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
-        tc.generate()
-
-    def _patch_sources(self):
         apply_conandata_patches(self)
         # Disable examples and tests
         save(self, os.path.join(self.source_folder, "examples", "CMakeLists.txt"), "")
@@ -88,8 +63,17 @@ class GNSSTkConan(ConanFile):
         replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
                         " SHARED ", " ")
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        # https://github.com/SGL-UT/gnsstk/blob/v14.0.0/CMakeLists.txt#L41-L51
+        tc.variables["BUILD_EXT"] = self.options.build_ext
+        tc.variables["VERSIONED_HEADER_INSTALL"] = True
+        tc.variables["USE_RPATH"] = False  # Adds install directory to RPATH otherwise
+        # Relocatable shared libs on macOS
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
+        tc.generate()
+
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -104,7 +88,6 @@ class GNSSTkConan(ConanFile):
             mkdir(self, os.path.join(self.package_folder, "bin"))
             rename(self, os.path.join(self.package_folder, "lib", "gnsstk.dll"),
                    os.path.join(self.package_folder, "bin", "gnsstk.dll"))
-        fix_apple_shared_install_name(self)
 
     def package_info(self):
         # https://github.com/SGL-UT/gnsstk/blob/stable/GNSSTKConfig.cmake.in
