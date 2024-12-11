ac41e6cfb5ea3db0a1e7a2bcf90b77993d33d992
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-12-11 11:56:57 +0000

    test executable, remove redundant properties in test_package

--- recipes/p7zip/all/test_package/conanfile.py
+++ recipes/p7zip/all/test_package/conanfile.py
@@ -6,11 +6,11 @@ from conan.tools.build import can_run
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "VirtualRunEnv", "VirtualBuildEnv"
-    test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
     def test(self):
         assert os.path.exists(os.path.join(self.dependencies[self.tested_reference_str].cpp_info.bindir, "7za"))
+        if can_run(self):
+            self.run("7za", env="conanrun")
