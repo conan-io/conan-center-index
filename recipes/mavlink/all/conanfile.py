77b47d2b51bc733b29aefb0023efc6dd1def1b99
Martin Valgur <martin.valgur@gmail.com>
2024-12-12 17:33:28 +0200

    mavlink: skip CMake, run generator directly from Conan

--- recipes/mavlink/all/conanfile.py
+++ recipes/mavlink/all/conanfile.py
@@ -1,8 +1,9 @@
 import os
 
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rmdir
+from conan.tools.cmake import cmake_layout
+from conan.tools.env import Environment
+from conan.tools.files import copy, get
 
 required_conan_version = ">=2.4"
 
@@ -50,7 +51,7 @@ class MavlinkConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def package_id(self):
-        self.info.clear()
+        self.info.settings.clear()
 
     def build_requirements(self):
         self.tool_requires("cpython/[>=3.12 <4]")
@@ -61,23 +62,33 @@ class MavlinkConan(ConanFile):
         get(self, **info["pymavlink"], strip_root=True, destination="pymavlink")
 
     def generate(self):
-        tc = CMakeToolchain(self)
-        tc.cache_variables["MAVLINK_DIALECT"] = self.options.dialect
-        tc.cache_variables["MAVLINK_VERSION"] = self.options.wire_protocol
-        tc.generate()
-        deps = CMakeDeps(self)
-        deps.generate()
+        # To install pyelftools
+        env = Environment()
+        env.append_path("PYTHONPATH", self._site_packages_dir)
+        env.append_path("PATH", os.path.join(self._site_packages_dir, "bin"))
+        env.vars(self).save_script("pythonpath")
+
+    @property
+    def _site_packages_dir(self):
+        return os.path.join(self.build_folder, "site-packages")
+
+    def _pip_install(self, packages):
+        self.run(f"python -m pip install {' '.join(packages)} --no-cache-dir --target={self._site_packages_dir}",
+                 cwd=self.source_folder)
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        # Reproduce these CMake steps https://github.com/mavlink/mavlink/blob/5e3a42b8f3f53038f2779f9f69bd64767b913bb8/CMakeLists.txt#L32-L39
+        # for a tighter control over the created temporary Python environment.
+        self._pip_install(["-r", "pymavlink/requirements.txt"])
+        self.run("python -m pymavlink.tools.mavgen --lang=C"
+                 f" --wire-protocol={self.options.wire_protocol}"
+                 f" --output {self.build_folder}/include/mavlink/"
+                 f" message_definitions/v1.0/{self.options.dialect}.xml",
+                 cwd=self.source_folder)
 
     def package(self):
         copy(self, "COPYING", self.source_folder, os.path.join(self.package_folder, "licenses"))
-        cmake = CMake(self)
-        cmake.install()
-        rmdir(self, os.path.join(self.package_folder, "lib"))
+        copy(self, "*", os.path.join(self.build_folder, "include"), os.path.join(self.package_folder, "include"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "MAVLink")
