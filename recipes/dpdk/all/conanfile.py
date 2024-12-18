a16e5c8d98709920f660b60ee6da4d1ea3036402
Martin Valgur <martin.valgur@gmail.com>
2024-12-18 19:47:51 +0200

    dpdk: add ARM cross-compilation support

--- recipes/dpdk/all/conanfile.py
+++ recipes/dpdk/all/conanfile.py
@@ -2,9 +2,9 @@ import os
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import check_min_cppstd
+from conan.tools.build import check_min_cppstd, cross_building
 from conan.tools.env import Environment
-from conan.tools.files import copy, get, export_conandata_patches, apply_conandata_patches, collect_libs, rm, rmdir
+from conan.tools.files import copy, get, export_conandata_patches, apply_conandata_patches, collect_libs, rm, rmdir, replace_in_file
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.meson import Meson, MesonToolchain
@@ -26,6 +26,7 @@ class DpdkConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "build_apps": [True, False],
         "enable_stdatomic": [True, False],
         "with_jansson": [True, False],
         "with_libarchive": [True, False],
@@ -34,10 +35,12 @@ class DpdkConan(ConanFile):
         "with_libibverbs": [True, False],
         "with_libpcap": [True, False],
         "with_openssl": [True, False],
+        "platform": ["generic", "generic_aarch32", "ANY"]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
+        "build_apps": False,
         "enable_stdatomic": False,
         "with_jansson": True,
         "with_libarchive": True,
@@ -46,10 +49,17 @@ class DpdkConan(ConanFile):
         "with_libibverbs": True,
         "with_libpcap": True,
         "with_openssl": True,
+        "platform": "generic",
     }
 
     def export_sources(self):
         export_conandata_patches(self)
+
+    def config_options(self):
+        if not str(self.settings.arch).startswith("arm") or not cross_building(self):
+            del self.options.platform
+        elif self.settings.arch != "armv8":
+            self.platform = "generic_aarch32"
 
     def configure(self):
         if self.options.shared:
@@ -107,15 +117,22 @@ class DpdkConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
         apply_conandata_patches(self)
+        # Disable -march, -mcpu and -mtune flags, which produce non-portable binaries.
+        # Also fails when cross-compiling for armv8.
+        replace_in_file(self, os.path.join(self.source_folder, "config", "meson.build"),
+                        "if not is_ms_compiler", "if false")
 
     def generate(self):
         tc = MesonToolchain(self)
         # No good way to enable only static/shared except patching?
         tc.project_options["tests"] = "false"
+        tc.project_options["disable_apps"] = "false" if self.options.build_apps else "true"
         tc.project_options["enable_stdatomic"] = "true" if self.options.enable_stdatomic else "false"
         tc.project_options["ibverbs_link"] = "shared"  # rdma-core is a shared-library package
         tc.project_options["disable_drivers"] = "bus"
         tc.extra_cflags.append(f"-I{self.dependencies['linux-headers-generic'].cpp_info.includedir}")
+        if self.options.get_safe("platform"):
+            tc.properties["platform"] = str(self.options.platform)
         tc.generate()
 
         PkgConfigDeps(self).generate()
