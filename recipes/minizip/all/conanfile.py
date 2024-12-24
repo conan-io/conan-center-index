786324bb5e5a065c3272489294111173c7a98d72
Andrey Filipenkov <decapitator@ukr.net>
2024-12-24 11:56:30 +0300

    apply patches in source()

--- recipes/minizip/all/conanfile.py
+++ recipes/minizip/all/conanfile.py
@@ -52,6 +52,7 @@ class MinizipConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -66,7 +67,6 @@ class MinizipConan(ConanFile):
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
         cmake.build()
