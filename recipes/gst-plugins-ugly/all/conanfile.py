dcb15ffc221eb0cbc79e3bd23e0bc0fb79151f40
Martin Valgur <martin.valgur@gmail.com>
2024-12-31 00:51:54 +0200

    gst-plugins-ugly: update and migrate, add libx264

--- recipes/gst-plugins-ugly/all/conanfile.py
+++ recipes/gst-plugins-ugly/all/conanfile.py
@@ -1,166 +1,182 @@
-from conans import ConanFile, tools, Meson, VisualStudioBuildEnvironment
-from conans.errors import ConanInvalidConfiguration
-from conan.tools.microsoft import msvc_runtime_flag, is_msvc
 import glob
 import os
 import shutil
 
+from conan import ConanFile
+from conan.tools.build import stdcpp_library
+from conan.tools.files import chdir, copy, get, rm, rmdir, rename, replace_in_file
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.meson import MesonToolchain, Meson
+from conan.tools.microsoft import is_msvc, check_min_vs
+
+required_conan_version = ">=2.4"
+
 
 class GStPluginsUglyConan(ConanFile):
     name = "gst-plugins-ugly"
-    description = "GStreamer is a development framework for creating applications like media players, video editors, " \
-                  "streaming media broadcasters and so on"
+    description = "A set of good-quality plug-ins for GStreamer that might pose distribution problems"
     topics = ("gstreamer", "multimedia", "video", "audio", "broadcasting", "framework", "media")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://gstreamer.freedesktop.org/"
+    # Mostly GPL, some LGPL plugins. For details, see:
+    # https://gitlab.freedesktop.org/gstreamer/gstreamer/-/raw/1.24.10/subprojects/gst-plugins-ugly/docs/gst_plugins_cache.json
     license = "GPL-2.0-only"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "with_introspection": [True, False],
-        }
+        "with_libx264": [True, False],
+    }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "with_introspection": False,
-        }
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
-    exports_sources = ["patches/*.patch"]
+        "with_libx264": True,
+    }
+    languages = ["C"]
 
-    generators = "pkg_config"
-
-    def validate(self):
-        if self.options.shared != self.options["gstreamer"].shared or \
-            self.options.shared != self.options["glib"].shared or \
-            self.options.shared != self.options["gst-plugins-base"].shared:
-                # https://gitlab.freedesktop.org/gstreamer/gst-build/-/issues/133
-                raise ConanInvalidConfiguration("GLib, GStreamer and GstPlugins must be either all shared, or all static")
-        if tools.Version(self.version) >= "1.18.2" and\
-           self.settings.compiler == "gcc" and\
-           tools.Version(self.settings.compiler.version) < "5":
-            raise ConanInvalidConfiguration(
-                "gst-plugins-ugly%s does not support gcc older than 5" % self.version
-            )
-        if self.options.shared and str(msvc_runtime_flag(self)).startswith("MT"):
-            raise ConanInvalidConfiguration('shared build with static runtime is not supported due to the FlsAlloc limit')
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
-        self.options['gstreamer'].shared = self.options.shared
-        self.options['gst-plugins-base'].shared = self.options.shared
+            self.options.rm_safe("fPIC")
+        self.options["gstreamer"].shared = self.options.shared
+        self.options["gst-plugins-base"].shared = self.options.shared
 
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("glib/2.70.1")
-        self.requires("gstreamer/1.19.1")
-        self.requires("gst-plugins-base/1.19.1")
+        self.requires(f"gstreamer/{self.version}", transitive_headers=True, transitive_libs=True)
+        self.requires(f"gst-plugins-base/{self.version}", transitive_headers=True, transitive_libs=True)
+        self.requires("glib/2.78.3", transitive_headers=True, transitive_libs=True)
+        if self.options.with_libx264:
+            self.requires("libx264/cci.20240224")
 
     def build_requirements(self):
-        self.build_requires("meson/[>=1.2.3 <2]")
-        if not tools.which("pkg-config"):
-            self.build_requires("pkgconf/[>=2.2 <3]")
-        if self.settings.os == 'Windows':
-            self.build_requires("winflexbison/2.5.24")
-        else:
-            self.build_requires("bison/3.7.6")
-            self.build_requires("flex/2.6.4")
-        if self.options.with_introspection:
-            self.build_requires("gobject-introspection/1.68.0")
+        self.tool_requires("meson/[>=1.2.3 <2]")
+        self.tool_requires("glib/<host_version>")
+        if not self.conf.get("tools.gnu:pkg_config", check_type=str):
+            self.tool_requires("pkgconf/[>=2.2 <3]")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        # libx264 on CCI uses custom versions, which fail version validation
+        replace_in_file(self, os.path.join(self.source_folder, "ext", "x264", "meson.build"),
+                        "dependency('x264', version : '>=0.156'",
+                        "dependency('x264'")
 
-    def _configure_meson(self):
-        defs = dict()
+    def generate(self):
+        tc = MesonToolchain(self)
 
-        def add_flag(name, value):
-            if name in defs:
-                defs[name] += " " + value
-            else:
-                defs[name] = value
+        if is_msvc(self) and not check_min_vs(self, 190, raise_invalid=False):
+            tc.c_link_args.append("-Dsnprintf=_snprintf")
 
-        def add_compiler_flag(value):
-            add_flag("c_args", value)
-            add_flag("cpp_args", value)
+        def feature(value):
+            return "enabled" if value else "disabled"
 
-        def add_linker_flag(value):
-            add_flag("c_link_args", value)
-            add_flag("cpp_link_args", value)
+        # Feature options for plugins without external deps
+        tc.project_options["asfdemux"] = "enabled"
+        tc.project_options["dvdlpcmdec"] = "enabled"
+        tc.project_options["dvdsub"] = "enabled"
+        tc.project_options["realmedia"] = "enabled"
 
-        meson = Meson(self)
-        if is_msvc(self):
-            add_linker_flag("-lws2_32")
-            add_compiler_flag("-%s" % self.settings.compiler.runtime)
-            if int(str(self.settings.compiler.version)) < 14:
-                add_compiler_flag("-Dsnprintf=_snprintf")
-        if self.settings.get_safe("compiler.runtime"):
-            defs["b_vscrt"] = str(self.settings.compiler.runtime).lower()
-        defs["tools"] = "disabled"
-        defs["examples"] = "disabled"
-        defs["benchmarks"] = "disabled"
-        defs["tests"] = "disabled"
-        defs["wrap_mode"] = "nofallback"
-        defs["introspection"] = "enabled" if self.options.with_introspection else "disabled"
-        meson.configure(build_folder=self._build_subfolder,
-                        source_folder=self._source_subfolder,
-                        defs=defs)
-        return meson
+        # Feature options for plugins that need external deps
+        tc.project_options["a52dec"] = "disabled"  # liba52
+        tc.project_options["cdio"] = "disabled"  # libcdio
+        tc.project_options["dvdread"] = "disabled"  # dvdread
+        tc.project_options["mpeg2dec"] = "disabled"  # libmpeg2
+        tc.project_options["sidplay"] = "disabled"  # sidplay
+        tc.project_options["x264"] = feature(self.options.with_libx264)
+
+        # Common options
+        tc.project_options["gpl"] = "enabled"
+        tc.project_options["doc"] = "disabled"
+        tc.project_options["tests"] = "disabled"
+        tc.project_options["nls"] = "disabled"
+        tc.project_options["orc"] = "disabled"
+
+        tc.generate()
+
+        deps = PkgConfigDeps(self)
+        deps.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-
-        with tools.environment_append(VisualStudioBuildEnvironment(self).vars) if is_msvc(self) else tools.no_op():
-            meson = self._configure_meson()
-            meson.build()
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
 
     def _fix_library_names(self, path):
-        # regression in 1.16
         if is_msvc(self):
-            with tools.chdir(path):
+            with chdir(self, path):
                 for filename_old in glob.glob("*.a"):
                     filename_new = filename_old[3:-2] + ".lib"
-                    self.output.info("rename %s into %s" % (filename_old, filename_new))
+                    self.output.info(f"rename {filename_old} into {filename_new}")
                     shutil.move(filename_old, filename_new)
 
     def package(self):
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        with tools.environment_append(VisualStudioBuildEnvironment(self).vars) if is_msvc(self) else tools.no_op():
-            meson = self._configure_meson()
-            meson.install()
-
+        copy(self, pattern="COPYING", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        meson = Meson(self)
+        meson.install()
         self._fix_library_names(os.path.join(self.package_folder, "lib"))
         self._fix_library_names(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "gstreamer-1.0", "pkgconfig"))
-        tools.remove_files_by_mask(self.package_folder, "*.pdb")
+        rename(self, os.path.join(self.package_folder, "share"), os.path.join(self.package_folder, "res"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "gstreamer-1.0", "pkgconfig"))
+        rm(self, "*.pdb", self.package_folder, recursive=True)
 
     def package_info(self):
-
-        plugins = ["asf",
-                   "dvdlpcmdec",
-                   "dvdsub",
-                   "realmedia",
-                   "xingmux"]
-
-        gst_plugin_path = os.path.join(self.package_folder, "lib", "gstreamer-1.0")
         if self.options.shared:
-            self.output.info("Appending GST_PLUGIN_PATH env var : %s" % gst_plugin_path)
-            self.cpp_info.bindirs.append(gst_plugin_path)
-            self.runenv_info.prepend_path("GST_PLUGIN_PATH", gst_plugin_path)
-        else:
-            self.cpp_info.defines.append("GST_PLUGINS_UGLY_STATIC")
-            self.cpp_info.libdirs.append(gst_plugin_path)
-            self.cpp_info.libs.extend(["gst%s" % plugin for plugin in plugins])
+            self.runenv_info.append_path("GST_PLUGIN_PATH", os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
 
-        self.cpp_info.includedirs = ["include", os.path.join("include", "gstreamer-1.0")]
+        def _define_plugin(name, extra_requires, cpp=False):
+            name = f"gst{name}"
+            component = self.cpp_info.components[name]
+            component.requires = [
+                "gstreamer::gstreamer-1.0",
+                "gstreamer::gstreamer-base-1.0",
+                "glib::gobject-2.0",
+                "glib::glib-2.0",
+            ] + extra_requires
+            component.includedirs = []
+            component.bindirs = []
+            if self.options.shared:
+                component.bindirs.append(os.path.join("lib", "gstreamer-1.0"))
+            else:
+                component.libs = [name]
+                component.libdirs = [os.path.join("lib", "gstreamer-1.0")]
+                if self.settings.os in ["Linux", "FreeBSD"]:
+                    component.system_libs = ["m", "dl"]
+                component.defines.append("GST_PLUGINS_UGLY_STATIC")
+            if cpp and not self.options.shared and stdcpp_library(self):
+                component.system_libs.append(stdcpp_library(self))
+            return component
+
+        _define_plugin("asf", [
+            "gst-plugins-base::gstreamer-rtp-1.0",
+            "gst-plugins-base::gstreamer-video-1.0",
+            "gst-plugins-base::gstreamer-audio-1.0",
+            "gst-plugins-base::gstreamer-tag-1.0",
+            "gst-plugins-base::gstreamer-riff-1.0",
+            "gst-plugins-base::gstreamer-rtsp-1.0",
+            "gst-plugins-base::gstreamer-pbutils-1.0",
+            "gst-plugins-base::gstreamer-sdp-1.0",
+        ])
+        _define_plugin("dvdlpcmdec", [
+            "gst-plugins-base::gstreamer-audio-1.0",
+        ])
+        _define_plugin("dvdsub", [
+            "gst-plugins-base::gstreamer-video-1.0",
+        ])
+        _define_plugin("realmedia", [
+            "gst-plugins-base::gstreamer-rtsp-1.0",
+            "gst-plugins-base::gstreamer-pbutils-1.0",
+            "gst-plugins-base::gstreamer-sdp-1.0",
+        ])
+        if self.options.with_libx264:
+            _define_plugin("x264", [
+                "gst-plugins-base::gstreamer-pbutils-1.0",
+                "libx264::libx264",
+            ])
