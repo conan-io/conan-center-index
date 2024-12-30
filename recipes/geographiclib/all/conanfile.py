2ae40751c62142a8e9fece28ff2e70406137b2e4
fredbje <fbjerkas@gmail.com>
2024-12-30 12:46:00 +0100

    geographiclib: add v2.5

--- recipes/geographiclib/all/conanfile.py
+++ recipes/geographiclib/all/conanfile.py
@@ -98,7 +98,9 @@ class GeographiclibConan(ConanFile):
             raise ConanInvalidConfiguration("extended, quadruple and variable precisions not yet supported in this recipe")
 
     def build_requirements(self):
-        if Version(self.version) >= "2.4":
+        if Version(self.version) >= "2.5":
+            self.tool_requires("cmake/[>=3.17 <4]")
+        elif Version(self.version) >= "2.4":
             self.tool_requires("cmake/[>=3.16 <4]")
 
     def source(self):
