a30744dea81462355dd4e7331e4a9cd40d1d9c8a
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-12-31 15:12:18 +0000

    unused import

--- recipes/confu_json/all/conanfile.py
+++ recipes/confu_json/all/conanfile.py
@@ -3,7 +3,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2.0"
