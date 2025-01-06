9aaf998e7f1a159346cff3102bb63fc0f793ca28
Martin Valgur <martin.valgur@gmail.com>
2025-01-06 13:16:09 +0200

    librasterlite2: modernize

--- recipes/librasterlite2/all/conanfile.py
+++ recipes/librasterlite2/all/conanfile.py
@@ -2,14 +2,14 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name, is_apple_os, to_apple_arch
 from conan.tools.build import cross_building
-from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.tools.env import VirtualRunEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain, PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc
 import os
 
-required_conan_version = ">=1.54.0"
+required_conan_version = ">=2.4"
 
 
 class Librasterlite2Conan(ConanFile):
@@ -42,29 +42,17 @@ class Librasterlite2Conan(ConanFile):
         "with_lz4": True,
         "with_zstd": True,
     }
-
-    @property
-    def _settings_build(self):
-        return getattr(self, "settings_build", self.settings)
+    implements = ["auto_shared_fpic"]
+    languages = ["C"]
 
     def export_sources(self):
         export_conandata_patches(self)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("cairo/1.17.4")
+        self.requires("cairo/1.18.0")
         self.requires("freetype/2.13.2")
         self.requires("giflib/5.2.1")
         self.requires("libcurl/[>=7.78 <9]")
@@ -90,25 +78,32 @@ class Librasterlite2Conan(ConanFile):
 
     def validate(self):
         if is_msvc(self):
-            raise ConanInvalidConfiguration("Visual Studio not supported yet")
+            raise ConanInvalidConfiguration("MSVC is not yet supported")
 
     def build_requirements(self):
         self.tool_requires("libtool/2.4.7")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/[>=2.2 <3]")
-        if self._settings_build.os == "Windows":
+        if self.settings_build.os == "Windows":
             self.win_bash = True
             if not self.conf.get("tools.microsoft.bash:path", check_type=str):
                 self.tool_requires("msys2/cci.latest")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
+        # Disable tests, tools and examples
+        replace_in_file(self, os.path.join(self.source_folder, "Makefile.am"),
+                        "SUBDIRS = headers src test tools examples",
+                        "SUBDIRS = headers src")
+        # fix MinGW
+        zlib_lib = self.dependencies["zlib"].cpp_info.aggregated_components().libs[0]
+        replace_in_file(self, os.path.join(self.source_folder, "configure.ac"),
+                        "AC_CHECK_LIB(z,",
+                        f"AC_CHECK_LIB({zlib_lib},")
 
     def generate(self):
-        env = VirtualBuildEnv(self)
-        env.generate()
-        if not cross_building(self) or is_apple_os(self):
-            # Required to find libgnuintl.8.dylib on macOS
+        if not cross_building(self):
             env = VirtualRunEnv(self)
             env.generate(scope="build")
 
@@ -132,30 +127,14 @@ class Librasterlite2Conan(ConanFile):
         deps = PkgConfigDeps(self)
         deps.generate()
 
-    def _patch_sources(self):
-        apply_conandata_patches(self)
-        # Disable tests, tools and examples
-        replace_in_file(self, os.path.join(self.source_folder, "Makefile.am"),
-                              "SUBDIRS = headers src test tools examples",
-                              "SUBDIRS = headers src")
-        # fix MinGW
-        zlib_lib = self.dependencies["zlib"].cpp_info.aggregated_components().libs[0]
-        replace_in_file(self, os.path.join(self.source_folder, "configure.ac"),
-                        "AC_CHECK_LIB(z,",
-                        f"AC_CHECK_LIB({zlib_lib},")
-
     def build(self):
-        self._patch_sources()
         autotools = Autotools(self)
         autotools.autoreconf()
-        # FIXME: remove, added to help debug ./configure failure on apple-clang
-        replace_in_file(self, os.path.join(self.source_folder, "configure"), "2>&5", "")
-        replace_in_file(self, os.path.join(self.source_folder, "configure"), ">&5", "")
         autotools.configure()
         autotools.make()
 
     def package(self):
-        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "COPYING", self.source_folder, os.path.join(self.package_folder, "licenses"))
         autotools = Autotools(self)
         autotools.install()
         rm(self, "*.la", os.path.join(self.package_folder, "lib"))
