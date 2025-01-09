1114f4010ba331a81e2510436ad705a492a36a5c
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-01-09 14:31:29 +0000

    Update recipes/librdata/all/test_package/test_package.c
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/librdata/all/test_package/test_package.c
+++ recipes/librdata/all/test_package/test_package.c
@@ -1,16 +1,10 @@
 #include "rdata.h"
 
 
-int main(int argc, char const *argv[])
+int main()
 {
     rdata_parser_t *parser = rdata_parser_init();
-    if (parser == NULL) {
-        fprintf(stderr, "Failed to initialize rdata_parser.\n");
-        return 1;
-    }
-
     rdata_parser_free(parser);
-    printf("librdata is successfully included, linked, and used.\n");
 
     return 0;
 }
