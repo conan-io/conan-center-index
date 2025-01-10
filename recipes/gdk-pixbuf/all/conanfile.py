b8ee036655fe0bfa453f0bae7280514b2f925655
Martin Valgur <martin.valgur@gmail.com>
2025-01-10 08:55:39 +0200

    gdk-pixbuf: use a version range for libtiff

--- recipes/gdk-pixbuf/all/conanfile.py
+++ recipes/gdk-pixbuf/all/conanfile.py
@@ -68,7 +68,7 @@ class GdkPixbufConan(ConanFile):
         if self.options.with_libpng:
             self.requires("libpng/[>=1.6 <2]")
         if self.options.with_libtiff:
-            self.requires("libtiff/4.6.0")
+            self.requires("libtiff/[>=4.5 <5]")
         if self.options.with_libjpeg == "libjpeg-turbo":
             self.requires("libjpeg-turbo/3.0.2")
         elif self.options.with_libjpeg == "libjpeg":
