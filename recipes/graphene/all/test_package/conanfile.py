82e7c6429df75ba278fc645ecb14456a55df3293
Martin Valgur <martin.valgur@gmail.com>
2025-01-10 08:49:05 +0200

    graphene: drop test_v1_package

--- recipes/graphene/all/test_package/conanfile.py
+++ recipes/graphene/all/test_package/conanfile.py
@@ -7,8 +7,7 @@ from conan.tools.build import can_run
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
-    test_type = "explicit"
+    generators = "CMakeToolchain", "CMakeDeps"
 
     def layout(self):
         cmake_layout(self)
@@ -23,5 +22,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(bin_path, env="conanrun")
