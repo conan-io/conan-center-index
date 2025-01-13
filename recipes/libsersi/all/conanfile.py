706ce252e022d81af0cd4a99419aec0dc4c3928e
Abril Rincón Blanco <git@rinconblanco.es>
2025-01-13 06:06:52 +0100

    Deprecate opendis6 in favour of libsersi

--- recipes/libsersi/all/conanfile.py
+++ recipes/libsersi/all/conanfile.py
@@ -2,18 +2,16 @@ import os
 
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rmdir
+from conan.tools.files import copy, get, rmdir, replace_in_file
 from conan.tools.build import check_min_cppstd
-from conan.tools.scm import Version
-from conan.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2"
 
-class OpenDis6Conan(ConanFile):
-    name = "opendis6"
-    homepage = "https://github.com/crhowell3/opendis6"
+class LibsersiConan(ConanFile):
+    name = "libsersi"
+    homepage = "https://github.com/crhowell3/libsersi"
     description = "Modern C++ implementation of IEEE 1278.1a-1998"
-    topics = ("library", "protocol", "dis")
+    topics = ("dis", "ieee", "1278.1a-1998")
     url = "https://github.com/conan-io/conan-center-index"
     license = "BSD-2-Clause"
     package_type = "library"
@@ -27,26 +25,7 @@ class OpenDis6Conan(ConanFile):
         "fPIC": True
     }
 
-    provides = "libsersi"
-    deprecated = "libsersi"
-
-    @property
-    def _min_cppstd(self):
-        return "14"
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "Visual Studio": "15",
-            "msvc": "191",
-            "gcc": "7",
-            "clang": "7",
-            "apple-clang": "10",
-        }
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    implements = ["auto_shared_fpic"]
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -57,22 +36,16 @@ class OpenDis6Conan(ConanFile):
     def layout(self):
         cmake_layout(self, src_folder="src")
 
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        replace_in_file(self, os.path.join(self.source_folder, "cmake", "libsersi-api.cmake"),
+                        "set(CMAKE_CXX_STANDARD 17)", "")
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.22 <4]")
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
+        check_min_cppstd(self, 14)
 
     def build(self):
         cmake = CMake(self)
@@ -89,10 +62,8 @@ class OpenDis6Conan(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
-        self.cpp_info.libs = ["OpenDIS6"]
-        self.cpp_info.set_property("cmake_file_name", "OpenDIS")
-        self.cpp_info.set_property("cmake_target_name", "OpenDIS::OpenDIS6")
-        self.cpp_info.set_property("cmake_target_aliases", ["OpenDIS::DIS6","OpenDIS6"])
+        self.cpp_info.libs = ["sersi"]
+        self.cpp_info.set_property("cmake_target_name", "libsersi::sersi")
 
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.append("m")
