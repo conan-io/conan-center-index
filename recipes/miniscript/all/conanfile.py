89950aab824a1f821881c465926036fb4f4aa07f
toge <toge.mail@gmail.com>
2025-01-13 14:24:06 +0900

    miniscript: add version 1.6.2 (retry)

--- recipes/miniscript/all/conanfile.py
+++ recipes/miniscript/all/conanfile.py
@@ -1,7 +1,10 @@
 from conan import ConanFile
-from conan.tools.files import get, copy
+from conan.tools.files import get, copy, apply_conandata_patches, export_conandata_patches
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools.microsoft import is_msvc
 import os
 
 required_conan_version = ">=1.53.0"
@@ -14,6 +17,7 @@ class MiniscriptConan(ConanFile):
     homepage = "https://github.com/JoeStrout/miniscript"
     topics = ("script", "embedded", "programming-language")
     settings = "os", "arch", "compiler", "build_type"
+    package_type = "library"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
@@ -23,29 +27,38 @@ class MiniscriptConan(ConanFile):
         "fPIC": True,
     }
     exports_sources = ["CMakeLists.txt"]
+    implements = ["auto_shared_fpic"]
 
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    def validate(self):
+        # miniscript doesn't declare export symbols with __declspec.
+        if is_msvc(self) and self.options.shared:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} doesn't support msvc shared build.(yet)"
+            )
+        check_min_cppstd(self, 11 if Version(self.version) < "1.6.2" else 17)
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["MINISCRIPT_SRC_DIR"] = self.source_folder.replace("\\", "/")
+        if Version(self.version) < "1.6.2":
+            tc.variables["MINISCRIPT_SRC_DIR"] = self.source_folder.replace("\\", "/")
         tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
+        if Version(self.version) < "1.6.2":
+            cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
+        else:
+            cmake.configure()
         cmake.build()
 
     def package(self):
@@ -54,7 +67,7 @@ class MiniscriptConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["miniscript"]
+        self.cpp_info.libs = ["miniscript-cpp"]
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.append("m")
             self.cpp_info.system_libs.append("pthread")
