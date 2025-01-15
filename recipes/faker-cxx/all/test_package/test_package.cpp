ae386ab67c1dff65b0677ea7c7e53a9a5e0c19a8
toge <toge.mail@gmail.com>
2025-01-15 22:11:34 +0900

    faker-cxx: add version 4.0.1

--- recipes/faker-cxx/all/test_package/test_package.cpp
+++ recipes/faker-cxx/all/test_package/test_package.cpp
@@ -11,13 +11,12 @@
 
 int main()
 {
-    const auto id = faker::string::uuid();
     const auto email = faker::internet::email();
     const auto password = faker::internet::password();
     const auto createdAt = faker::date::pastDate(5, faker::date::DateFormat::ISO);
     const auto updatedAt = faker::date::recentDate(2, faker::date::DateFormat::ISO);
 
-    std::cout << "id: " << id << ", email: " << email << ", password: " <<  password << ", createdAt: " << createdAt << ", updatedAt: " << updatedAt << "\n";
+    std::cout << "email: " << email << ", password: " <<  password << ", createdAt: " << createdAt << ", updatedAt: " << updatedAt << "\n";
 
     return 0;
 }
