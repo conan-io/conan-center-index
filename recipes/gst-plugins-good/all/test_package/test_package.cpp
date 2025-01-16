a6cf33bd115a610837aed4c04a4ca21cc1d81e2d
Martin Valgur <martin.valgur@gmail.com>
2025-01-16 20:14:54 +0200

    gst: list found plugins in test_package

--- recipes/gst-plugins-good/all/test_package/test_package.cpp
+++ recipes/gst-plugins-good/all/test_package/test_package.cpp
@@ -2,25 +2,45 @@
 #include <gst/gstplugin.h>
 
 #ifdef GST_PLUGINS_GOOD_STATIC
-
 extern "C"
 {
     GST_PLUGIN_STATIC_DECLARE(wavparse);
 }
-
 #endif
 
 #include <iostream>
+
+void list_plugins()
+{
+    GstRegistry *registry = gst_registry_get();
+    if (!registry) {
+        std::cerr << "Failed to get GStreamer registry" << std::endl;
+        exit(-1);
+    }
+    GList *plugins = gst_registry_get_plugin_list(registry);
+    std::cout << "Found plugins:" << std::endl;
+    for (GList *plugin_item = plugins; plugin_item != nullptr; plugin_item = plugin_item->next) {
+        GstPlugin *plugin = GST_PLUGIN(plugin_item->data);
+        const gchar *name = gst_plugin_get_name(plugin);
+        const gchar *desc = gst_plugin_get_description(plugin);
+        std::cout << "- " << name;
+        if (desc) {
+            std::cout << " (" << desc << ")";
+        }
+        std::cout << std::endl;
+    }
+    gst_plugin_list_free(plugins);
+}
 
 int main(int argc, char * argv[])
 {
     gst_init(&argc, &argv);
 
 #ifdef GST_PLUGINS_GOOD_STATIC
-
     GST_PLUGIN_STATIC_REGISTER(wavparse);
-
 #endif
+
+    list_plugins();
 
     GstElement * wavparse = gst_element_factory_make("wavparse", NULL);
     if (!wavparse) {
