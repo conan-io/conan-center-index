cb37e839ab7742afbd727bcc748b5fe15c127231
Martin Valgur <martin.valgur@gmail.com>
2025-01-16 08:31:07 +0200

    b2: simplify, add cross-compilation support, unvendor nlohmann_json

--- recipes/b2/portable/test_package/conanfile.py
+++ recipes/b2/portable/test_package/conanfile.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
 from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.layout import basic_layout
 
 
 class TestPackageConan(ConanFile):
@@ -6,8 +10,15 @@ class TestPackageConan(ConanFile):
     generators = "VirtualBuildEnv"
     test_type = "explicit"
 
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+    def layout(self):
+        basic_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str, run=True)
 
     def test(self):
-        self.run("b2 -v")
+        if can_run(self):
+            self.run("b2 -v", env="conanrun")
+        else:
+            suffix = ".exe" if self.settings.os == "Windows" else ""
+            assert Path(self.dependencies["b2"].package_folder, "bin", f"b2{suffix}").is_file()
