134050d56cc2427999d9a3ae34085e2e2a863101
Martin Valgur <martin.valgur@gmail.com>
2025-01-17 17:51:36 +0200

    qt-advanced-docking-system: require only tool components from qt when cross-compiling

--- recipes/qt-advanced-docking-system/all/conanfile.py
+++ recipes/qt-advanced-docking-system/all/conanfile.py
@@ -58,7 +58,7 @@ class QtADS(ConanFile):
 
     def build_requirements(self):
         if not can_run(self):
-            self.tool_requires("qt/<host_version>")
+            self.tool_requires("qt/<host_version>", options={"gui": False, "widgets": False})
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
