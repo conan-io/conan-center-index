b70780056846142aae312101de73b1c4d9893dab
Uilian Ries <uilianries@gmail.com>
2025-01-24 16:13:15 +0100

    Skip debug build
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libtommath/all/conanfile.py
+++ recipes/libtommath/all/conanfile.py
@@ -2,9 +2,9 @@ import os
 
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rmdir, replace_in_file
+from conan.tools.files import copy, get, rmdir
 from conan.tools.microsoft import is_msvc
-from conan.tools.scm import Version
+from conan.errors import ConanInvalidConfiguration
 
 required_conan_version = ">=2.0.9"
 
@@ -50,20 +50,15 @@ class LibTomMathConan(ConanFile):
         tc = CMakeToolchain(self)
         tc.generate()
 
-    def _patch_sources(self):
-        if is_msvc(self):
+    def validate(self):
+        if is_msvc(self) and self.settings.build_type == "Debug":
             # libtommath requires /O1 on MSVC Debug builds for dead code elimination.
-            # I could not find a way short of forcing settings.build_type=RelWithDebInfo to get that to work, though.
-            # Disabling the missing symbols directly instead.
+            # Otherwise, the build will fail with error LNK2019: unresolved external symbol s_read_arc4random
             # https://github.com/libtom/libtommath/blob/42b3fb07e7d504f61a04c7fca12e996d76a25251/s_mp_rand_platform.c#L120-L138
-            file = "bn_s_mp_rand_platform.c" if Version(self.version) <= "1.3.0" else "s_mp_rand_platform.c"
-            for symbol in ["s_read_getrandom", "s_read_ltm_rng", "s_read_arc4random", "s_read_urandom"]:
-                replace_in_file(self, os.path.join(self.source_folder, file),
-                                f"if ((err != MP_OKAY) && MP_HAS({symbol.upper()})",
-                                f"// if ((err != MP_OKAY) && MP_HAS({symbol.upper()})")
+            # https://github.com/libtom/libtommath/issues/485
+            raise ConanInvalidConfiguration("Could not build with MSVC Debug build_type due compiler optization requirement. Please, try to build with build_type=RelWithDebInfo")
 
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
