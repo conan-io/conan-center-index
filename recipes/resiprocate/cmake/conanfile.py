cba6710fe1c6fefa937df042d8da0b870875d29c
Francisco Ramírez <franchuti688@gmail.com>
2025-01-28 09:22:30 +0100

    Update recipes/resiprocate/cmake/conanfile.py

--- recipes/resiprocate/cmake/conanfile.py
+++ recipes/resiprocate/cmake/conanfile.py
@@ -5,7 +5,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
 from conan.tools.build import check_min_cppstd, cross_building
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get, rm, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.microsoft import is_msvc
