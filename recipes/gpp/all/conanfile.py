b5cf9144516a94380c22943395db366d28a86e3e
Dan Weatherill <dan.weatherill@cantab.net>
2025-01-30 13:43:05 -0800

    try again with msys build

--- recipes/gpp/all/conanfile.py
+++ recipes/gpp/all/conanfile.py
@@ -5,6 +5,7 @@ from conan import ConanFile
 from conan.tools.gnu import AutotoolsToolchain, Autotools
 from conan.tools.layout import basic_layout
 from conan.tools.files import chdir, get
+from conan.tools.env import VirtualBuildEnv
 
 class GppConan(ConanFile):
     name = "gpp"
@@ -17,7 +18,6 @@ class GppConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://logological.org/gpp"
     description = "A generic preprocessor"
-    
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
 
@@ -34,21 +34,26 @@ class GppConan(ConanFile):
         # The library is C only
         self.settings.rm_safe("compiler.libcxx")
         self.settings.rm_safe("compiler.cppstd")
-        
-    
+
+    def package_id(self):
+        del self.info.settings.arch
+        del self.info.settings.compiler
+        del self.info.settings.build_type
+
     def layout(self):
         basic_layout(self, src_folder="src")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        
+
     def generate(self):
+        env = VirtualBuildEnv(self)
+        env.generate()
         at_toolchain = AutotoolsToolchain(self)
         at_toolchain.generate()
 
     def build(self):
         autotools = Autotools(self)
-
         autotools.autoreconf()        
         autotools.configure()
         autotools.make()
