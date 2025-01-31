cf5696955f8ac9487edeb5332d3fbdeb7eee1877
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-01-31 12:01:50 +0000

    fix test_package

--- recipes/qt-advanced-docking-system/all/test_package/conanfile.py
+++ recipes/qt-advanced-docking-system/all/test_package/conanfile.py
@@ -10,6 +10,7 @@ class TestPackageConan(ConanFile):
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+        self.requires("qt/[*]")
 
     def layout(self):
         cmake_layout(self)
