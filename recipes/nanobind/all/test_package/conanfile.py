142eeadd8c5e2eb2d6c5127d22b608d11baa195f
Martin Valgur <martin.valgur@gmail.com>
2025-02-02 12:24:04 +0200

    nanobind: require newer CMake in test_package

--- recipes/nanobind/all/test_package/conanfile.py
+++ recipes/nanobind/all/test_package/conanfile.py
@@ -14,6 +14,7 @@ class TestPackageConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("cpython/<host_version>")
+        self.tool_requires("cmake/[^3]")
 
     def layout(self):
         cmake_layout(self)
