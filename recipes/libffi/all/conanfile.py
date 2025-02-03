877f065126ab6f96ac99a045d7bbef6493c6c915
Nikita <root.kidik@gmail.com>
2025-02-03 10:55:15 +0700

    fix: libffi - always require as tool `automake`

--- recipes/libffi/all/conanfile.py
+++ recipes/libffi/all/conanfile.py
@@ -62,8 +62,7 @@ class LibffiConan(ConanFile):
             self.win_bash = True
             if not self.conf.get("tools.microsoft.bash:path", default=False, check_type=str):
                 self.tool_requires("msys2/cci.latest")
-        if is_msvc(self) or self.settings.compiler == "clang":
-            self.tool_requires("automake/1.16.5")
+        self.tool_requires("automake/1.16.5")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
