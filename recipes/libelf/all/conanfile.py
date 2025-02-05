6c0ddcc98919ba187f083f88b90c1dd3ae4d52dd
Uilian Ries <uilianries@gmail.com>
2025-02-05 16:58:49 +0100

    Remove unused import
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libelf/all/conanfile.py
+++ recipes/libelf/all/conanfile.py
@@ -1,5 +1,4 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get, replace_in_file, rm, rmdir
