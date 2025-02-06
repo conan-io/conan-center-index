7f0ae7b40b9a2bed839a8e52b81781cbc4439812
Uilian Ries <uilianries@gmail.com>
2025-02-06 10:13:51 +0100

    Simplify test package
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/log4cpp/all/test_package/test_package.cpp
+++ recipes/log4cpp/all/test_package/test_package.cpp
@@ -1,39 +1,14 @@
-#include <log4cpp/Category.hh>
-#include <log4cpp/PropertyConfigurator.hh>
+#include <stdlib>
+#include "log4cpp/Category.hh"
+#include "log4cpp/Appender.hh"
+#include "log4cpp/OstreamAppender.hh"
 
-#include <iostream>
 
-int main(int argc, char **argv)
-{
-    if (argc < 2) {
-        std::cerr << "Need at least one argument" << std::endl;
-        return 1;
-    }
-
-    log4cpp::PropertyConfigurator::configure(argv[1]);
-
+int main(void) {
+    log4cpp::OstreamAppender appender("console", &std::cerr);
     log4cpp::Category& root = log4cpp::Category::getRoot();
-
-    log4cpp::Category& sub1 = log4cpp::Category::getInstance(std::string("sub1"));
-
-    log4cpp::Category& sub2 = log4cpp::Category::getInstance(std::string("sub1.sub2"));
-
-    root.warn("Storm is coming");
-
-    sub1.debug("Received storm warning");
-    sub1.info("Closing all hatches");
-
-    sub2.debug("Hiding solar panels");
-    sub2.error("Solar panels are blocked");
-    sub2.debug("Applying protective shield");
-    sub2.warn("Unfolding protective shield");
-    sub2.info("Solar panels are shielded");
-
-    sub1.info("All hatches closed");
-
-    root.info("Ready for storm.");
-
-    log4cpp::Category::shutdown();
-
-    return 0;
+    root.addAppender(appender);
+    root.info("Conan test package");
+    root.shutdown();
+    return EXIT_SUCCESS;
 }
