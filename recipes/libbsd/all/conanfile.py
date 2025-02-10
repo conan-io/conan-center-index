77b56a133f65ab0ef88608f4d9c4f54ae14dd10d
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 14:28:07 +0200

    libbsd: drop Conan v1 support

--- recipes/libbsd/all/conanfile.py
+++ recipes/libbsd/all/conanfile.py
@@ -65,9 +65,9 @@ class LibBsdConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def build(self):
-        apply_conandata_patches(self)
         autotools = Autotools(self)
         autotools.autoreconf()
         autotools.configure()
