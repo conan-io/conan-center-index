5c465c9b2b7fd61d980f1b04ed079ac3369a38f5
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 17:44:15 +0200

    hscpp: drop Conan v1 support

--- recipes/hscpp/all/conanfile.py
+++ recipes/hscpp/all/conanfile.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from conan import ConanFile
 from conan.tools.apple import is_apple_os
@@ -7,7 +8,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, replace_in_file, rm, rmdir
 from conan.tools.microsoft import is_msvc
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.0.9"
 
 
 class HscppConan(ConanFile):
@@ -28,17 +29,10 @@ class HscppConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
+    implements = ["auto_shared_fpic"]
 
     def export_sources(self):
         copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -50,12 +44,17 @@ class HscppConan(ConanFile):
             self.requires("util-linux-libuuid/2.39.2")
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, 11)
+        check_min_cppstd(self, 11)
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[^3]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
         rmdir(self, "lib")
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "add_subdirectory(lib/filesystem-1.3.4)",
+                        "find_package(ghc_filesystem REQUIRED CONFIG)")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -71,15 +70,9 @@ class HscppConan(ConanFile):
         deps.set_property("ghc-filesystem", "cmake_target_name", "ghc_filesystem")
         deps.generate()
 
-    def _patch_sources(self):
-        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-                        "add_subdirectory(lib/filesystem-1.3.4)",
-                        "find_package(ghc_filesystem REQUIRED CONFIG)")
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
-        cmake.configure(build_script_folder=self.source_path.parent)
+        cmake.configure(build_script_folder=Path(self.source_folder).parent)
         cmake.build()
 
     def package(self):
