a787f9a10a520c42096fd229c95e3e7e166fd8b1
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 22:30:10 +0200

    libbpf: drop Conan v1 support

--- recipes/libbpf/all/conanfile.py
+++ recipes/libbpf/all/conanfile.py
@@ -53,6 +53,8 @@ class LibbpfConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("make/4.4.1")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/[>=2.2 <3]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
