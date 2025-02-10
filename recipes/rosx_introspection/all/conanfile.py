6ab45249a9c12c3d28da033c2039a502bee6fc7d
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 18:13:37 +0200

    rosx_introspection: drop Conan v1 support

--- recipes/rosx_introspection/all/conanfile.py
+++ recipes/rosx_introspection/all/conanfile.py
@@ -1,13 +1,11 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
-from conan.tools.scm import Version
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.0"
 
 
 class RosxIntrospectionConan(ConanFile):
@@ -28,32 +26,17 @@ class RosxIntrospectionConan(ConanFile):
         "fPIC": True,
     }
 
-    @property
-    def _min_cppstd(self):
-        return 17
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "Visual Studio": "16",
-            "msvc": "192",
-            "gcc": "8",
-            "clang": "8",
-            "apple-clang": "10",
-        }
-
     def export_sources(self):
         export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-
-    def configure(self):
-        if self.settings.os == "Windows":
             # DLL symbols are not exported by the project
             del self.options.shared
             self.package_type = "static-library"
+
+    def configure(self):
         if self.options.get_safe("shared"):
             self.options.rm_safe("fPIC")
 
@@ -65,16 +48,11 @@ class RosxIntrospectionConan(ConanFile):
         self.requires("fast-cdr/2.2.0")
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 17)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -85,7 +63,6 @@ class RosxIntrospectionConan(ConanFile):
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
