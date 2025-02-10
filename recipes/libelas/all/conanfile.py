a8a31bc3ab3bb4a24aac5187533c5f82453ed6af
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 17:40:52 +0200

    libelas: drop Conan v1 support

--- recipes/libelas/all/conanfile.py
+++ recipes/libelas/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
 from conan.tools.files import copy, get, rm, download, load, save, export_conandata_patches, apply_conandata_patches
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.0.9"
 
 
 class LibelasConan(ConanFile):
@@ -27,18 +27,11 @@ class LibelasConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
+    implements = ["auto_shared_fpic"]
 
     def export_sources(self):
         export_conandata_patches(self)
         copy(self, "CMakeLists.txt", self.recipe_folder, os.path.join(self.export_sources_folder, "src"))
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -47,14 +40,14 @@ class LibelasConan(ConanFile):
         self.requires("llvm-openmp/17.0.6")
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, 98)
+        check_min_cppstd(self, 98)
         if self.settings.arch not in ["x86", "x86_64"]:
             raise ConanInvalidConfiguration("Only x86 and x86_64 architectures are supported")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version]["sources"], strip_root=True)
         download(self, **self.conan_data["sources"][self.version]["license"], filename="README.TXT")
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -64,7 +57,6 @@ class LibelasConan(ConanFile):
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
