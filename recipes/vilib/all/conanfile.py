79a8a9444b2c10e0fd22baee2d7e30cc03efdf57
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 17:34:10 +0200

    vilib: drop Conan v1 support

--- recipes/vilib/all/conanfile.py
+++ recipes/vilib/all/conanfile.py
@@ -1,12 +1,12 @@
 import os
 
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir, rm, export_conandata_patches, apply_conandata_patches
-from conans.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.0.9"
 
 
 class VilibConan(ConanFile):
@@ -27,18 +27,11 @@ class VilibConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
+    implements = ["auto_shared_fpic"]
 
     def export_sources(self):
         export_conandata_patches(self)
         copy(self, "*.cmake", self.recipe_folder, self.export_sources_folder)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -48,13 +41,13 @@ class VilibConan(ConanFile):
         self.requires("opencv/4.9.0", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, 11)
+        check_min_cppstd(self, 11)
         if self.settings.os == "Windows" and self.options.shared:
             raise ConanInvalidConfiguration("Shared builds on Windows are not supported")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
         rmdir(self, "assets")
         rmdir(self, "ros")
         rmdir(self, os.path.join("visual_lib", "test"))
@@ -67,7 +60,6 @@ class VilibConan(ConanFile):
         tc.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
