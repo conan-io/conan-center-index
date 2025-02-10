a2a9993f07ab183a4f4b90c4fe0ff5256b1c0794
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 19:07:57 +0200

    gl2ps: move apply_conandata_patches()

--- recipes/gl2ps/all/conanfile.py
+++ recipes/gl2ps/all/conanfile.py
@@ -54,6 +54,7 @@ class Gl2psConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -67,7 +68,6 @@ class Gl2psConan(ConanFile):
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
