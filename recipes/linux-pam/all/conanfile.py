8ddcf05daa7083985f82504d6cb6de3e56e3a195
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 22:06:38 +0200

    linux-pam: drop Conan v1 support

--- recipes/linux-pam/all/conanfile.py
+++ recipes/linux-pam/all/conanfile.py
@@ -10,7 +10,7 @@ from conan.tools.gnu import PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.meson import Meson, MesonToolchain
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.4.0"
 
 class LinuxPamConan(ConanFile):
     name = "linux-pam"
@@ -53,19 +53,11 @@ class LinuxPamConan(ConanFile):
         "with_selinux": "Enable SELinux support",
         "with_systemd": "Enable logind support in pam_issue and pam_timestamp",
     }
+    languages = ["C"]
+    implements = ["auto_shared_fpic"]
 
     def export_sources(self):
         export_conandata_patches(self)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
         basic_layout(self, src_folder="src")
