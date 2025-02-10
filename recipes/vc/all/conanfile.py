3f80828bac4a28d21bec7dd46583575cca617d2e
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 15:04:31 +0200

    vc: drop Conan v1 support

--- recipes/vc/all/conanfile.py
+++ recipes/vc/all/conanfile.py
@@ -49,11 +49,12 @@ class VcConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, 11)
+        check_min_cppstd(self, 11)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        cmakelists = os.path.join(self.source_folder, "CMakeLists.txt")
+        replace_in_file(self, cmakelists, 'AddCompilerFlag("-fPIC" CXX_FLAGS libvc_compile_flags)', "")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -62,12 +63,7 @@ class VcConan(ConanFile):
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
         tc.generate()
 
-    def _patch_sources(self):
-        cmakelists = os.path.join(self.source_folder, "CMakeLists.txt")
-        replace_in_file(self, cmakelists, 'AddCompilerFlag("-fPIC" CXX_FLAGS libvc_compile_flags)', "")
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -82,7 +78,3 @@ class VcConan(ConanFile):
         self.cpp_info.set_property("cmake_file_name", "Vc")
         self.cpp_info.set_property("cmake_target_name", "Vc::Vc")
         self.cpp_info.libs = ["Vc"]
-
-        # TODO: to remove in conan v2 once cmake_find_package* generators removed
-        self.cpp_info.names["cmake_find_package"] = "Vc"
-        self.cpp_info.names["cmake_find_package_multi"] = "Vc"
