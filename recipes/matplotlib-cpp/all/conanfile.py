79c16a1bc21416f4783874677b77b5177ca6ae56
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 15:14:12 +0200

    matplotlib-cpp: add cross-compilation support

--- recipes/matplotlib-cpp/all/conanfile.py
+++ recipes/matplotlib-cpp/all/conanfile.py
@@ -30,8 +30,7 @@ class MatplotlibCppConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, self._min_cppstd)
+        check_min_cppstd(self, self._min_cppstd)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
--- recipes/matplotlib-cpp/all/test_package/conanfile.py
+++ recipes/matplotlib-cpp/all/test_package/conanfile.py
@@ -1,19 +1,30 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain, CMakeDeps
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
-    test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+        self.requires("cpython/[~3.12]")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[^3]")
+        self.tool_requires("cpython/<host_version>")
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if not can_run(self):
+            tc.variables["Python_EXECUTABLE"] = os.path.join(self.dependencies.build["cpython"].package_folder, "bin", "python").replace("\\", "/")
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
