ab8b78daf94eed052bb4fc547f39b74bbdc03a0d
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 18:33:54 +0200

    cpython: add cross-compilation support

--- recipes/cpython/all/conanfile.py
+++ recipes/cpython/all/conanfile.py
@@ -5,11 +5,12 @@ import textwrap
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os, fix_apple_shared_install_name
+from conan.tools.build import can_run
 from conan.tools.env import VirtualRunEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, load, mkdir, replace_in_file, rm, rmdir, save, unzip
 from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps, PkgConfigDeps
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import MSBuildDeps, MSBuildToolchain, MSBuild, is_msvc, is_msvc_static_runtime, msvc_runtime_flag, msvs_toolset
+from conan.tools.microsoft import MSBuildDeps, MSBuildToolchain, MSBuild, is_msvc, is_msvc_static_runtime, msvc_runtime_flag, msvs_toolset, unix_path
 from conan.tools.scm import Version
 
 required_conan_version = ">=1.58.0"
@@ -101,7 +102,9 @@ class CPythonConan(ConanFile):
 
     def build_requirements(self):
         if Version(self.version) >= "3.11" and not is_msvc(self) and not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/2.1.0")
+            self.tool_requires("pkgconf/[>=2.2 <3]")
+        if not can_run(self) and not is_msvc(self):
+            self.tool_requires(f"cpython/{self.version}")
 
     def requirements(self):
         self.requires("zlib/[>=1.2.11 <2]")
@@ -128,7 +131,7 @@ class CPythonConan(ConanFile):
         if self.options.get_safe("with_gdbm", False):
             self.requires("gdbm/1.23")
         if self.options.get_safe("with_sqlite3"):
-            self.requires("sqlite3/3.45.2")
+            self.requires("sqlite3/[>=3.45.0 <4]")
         if self.options.get_safe("with_tkinter"):
             self.requires("tk/8.6.10")
         if self.options.get_safe("with_curses", False):
@@ -182,6 +185,7 @@ class CPythonConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def _generate_autotools(self):
         tc = AutotoolsToolchain(self, prefix=self.package_folder)
@@ -224,6 +228,16 @@ class CPythonConan(ConanFile):
             ]
         if not is_apple_os(self):
             tc.extra_ldflags.append('-Wl,--as-needed')
+
+        if not can_run(self):
+            build_python = unix_path(self, os.path.join(self.dependencies.build["cpython"].package_folder, "bin", "python"))
+            tc.configure_args.append(f"--with-build-python={build_python}")
+        # The following are required only when cross-building, but set for all cases for consistency
+        tc.configure_args.append("--enable-ipv6")  # enabled by default, but skip the check
+        dev_ptmx_exists = os.path.exists("/dev/ptmx")
+        dev_ptc_exists = os.path.exists("/dev/ptc")
+        tc.configure_args.append(f"ac_cv_file__dev_ptmx={yes_no(dev_ptmx_exists)}")
+        tc.configure_args.append(f"ac_cv_file__dev_ptc={yes_no(dev_ptc_exists)}")
 
         tc.generate()
 
@@ -397,7 +411,6 @@ class CPythonConan(ConanFile):
         self._inject_conan_props_file("_bsddb", "libdb", self.options.get_safe("with_bsddb"))
 
     def _patch_sources(self):
-        apply_conandata_patches(self)
         # <=3.10 requires a lot of manual injection of dependencies through setup.py
         # 3.12 removes setup.py completely, and uses pkgconfig dependencies
         # 3.11 is an in awkward transition state where some dependencies use pkgconfig, and others use setup.py
@@ -671,7 +684,11 @@ class CPythonConan(ConanFile):
         else()
             set(_CONAN_PYTHON_SUFFIX "")
         endif()
-        set(Python${_CONAN_PYTHON_SUFFIX}_EXECUTABLE @PYTHON_EXECUTABLE@)
+        get_filename_component(_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)
+        # Allow Python_EXECUTABLE to be overridden for cross-compilation support
+        if(NOT DEFINED Python_EXECUTABLE)
+            set(Python${_CONAN_PYTHON_SUFFIX}_EXECUTABLE @PYTHON_EXECUTABLE@)
+        endif()
         set(Python${_CONAN_PYTHON_SUFFIX}_LIBRARY @PYTHON_LIBRARY@)
 
         # Fails if these are set beforehand
@@ -698,11 +715,11 @@ class CPythonConan(ConanFile):
 
         # In order for the package to be relocatable, these variables must be relative to the installed CMake file
         if is_msvc(self):
-            python_exe = "${CMAKE_CURRENT_LIST_DIR}/../../" + self._cpython_interpreter_name
-            python_library = "${CMAKE_CURRENT_LIST_DIR}/../" + self._exact_lib_name
+            python_exe = "${_PREFIX_PATH}/" + self._cpython_interpreter_name
+            python_library = "${_PREFIX_PATH}/lib/" + self._exact_lib_name
         else:
-            python_exe = "${CMAKE_CURRENT_LIST_DIR}/../../bin/" + self._cpython_interpreter_name
-            python_library = "${CMAKE_CURRENT_LIST_DIR}/../" + self._exact_lib_name
+            python_exe = "${_PREFIX_PATH}/bin/" + self._cpython_interpreter_name
+            python_library = "${_PREFIX_PATH}/lib/" + self._exact_lib_name
 
         cmake_file = os.path.join(self.package_folder, self._cmake_module_path, "use_conan_python.cmake")
         content = template.replace("@PYTHON_EXECUTABLE@", python_exe).replace("@PYTHON_LIBRARY@", python_library)
@@ -882,54 +899,23 @@ class CPythonConan(ConanFile):
             self.runenv_info.append_path("PATH", bindir)
             self.buildenv_info.append_path("PATH", bindir)
 
-            # TODO remove once Conan 1.x is no longer supported
-            self.output.info(f"Appending PATH environment variable: {bindir}")
-            self.env_info.PATH.append(bindir)
-
         python = self._cpython_interpreter_path
         self.conf_info.define("user.cpython:python", python)
-        self.user_info.python = python
         if self.options.env_vars:
             self.runenv_info.append_path("PYTHON", python)
             self.buildenv_info.append_path("PYTHON", python)
-
-            # TODO remove once Conan 1.x is no longer supported
-            self.output.info(f"Appending PYTHON environment variable: {python}")
-            self.env_info.PYTHON = python
 
         if is_msvc(self):
             pythonhome = os.path.join(self.package_folder, "bin")
         else:
             pythonhome = self.package_folder
         self.conf_info.define("user.cpython:pythonhome", pythonhome)
-        self.user_info.pythonhome = pythonhome
 
         pythonhome_required = is_msvc(self) or is_apple_os(self)
         self.conf_info.define("user.cpython:module_requires_pythonhome", pythonhome_required)
-        self.user_info.module_requires_pythonhome = pythonhome_required
-
-        if is_msvc(self):
-            if self.options.env_vars:
-                # FIXME: On Windows, defining this breaks the packaged Python executable, but fixes
-                # separately built executables with an embedded interpreter trying to run standard Python
-                # modules. However, NOT defining this reverses the situation, normal Python executables
-                #work, but embedded interpreters break.
-                # The docs at https://python.readthedocs.io/en/latest/using/cmdline.html#envvar-PYTHONHOME
-                # seem to not be accurate to Windows (https://discuss.python.org/t/the-document-on-pythonhome-might-be-wrong/19614/5)
-                #self.runenv_info.append_path("PYTHONHOME", pythonhome)
-                #self.buildenv_info.append_path("PYTHONHOME", pythonhome)
-
-                # TODO remove once Conan 1.x is no longer supported
-                self.output.info(f"Setting PYTHONHOME environment variable: {pythonhome}")
-                self.env_info.PYTHONHOME = pythonhome
 
         python_root = self.package_folder
         if self.options.env_vars:
             self.runenv_info.append_path("PYTHON_ROOT", python_root)
             self.buildenv_info.append_path("PYTHON_ROOT", python_root)
-
-            # TODO remove once Conan 1.x is no longer supported
-            self.output.info(f"Setting PYTHON_ROOT environment variable: {python_root}")
-            self.env_info.PYTHON_ROOT = python_root
         self.conf_info.define("user.cpython:python_root", python_root)
-        self.user_info.python_root = python_root
