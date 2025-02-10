f931a0f0090af1e43fb77bf55e157858f94864ed
Martin Valgur <martin.valgur@gmail.com>
2025-02-10 19:23:17 +0200

    packio: drop Conan v1 support

--- recipes/packio/all/conanfile.py
+++ recipes/packio/all/conanfile.py
@@ -1,12 +1,10 @@
 import os
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get, export_conandata_patches, apply_conandata_patches
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 
-required_conan_version = ">=1.51.1"
+required_conan_version = ">=2.0"
 
 
 class PackioConan(ConanFile):
@@ -31,28 +29,6 @@ class PackioConan(ConanFile):
         "nlohmann_json": True,
         "boost_json": "default",
     }
-
-    @property
-    def _min_cppstd(self):
-        return "17"
-
-    @property
-    def _compilers_minimum_version(self):
-        if Version(self.version) < "2.4.0":
-            return {
-                "apple-clang": "10",
-                "clang": "6",
-                "gcc": "7",
-                "Visual Studio": "16",
-                "msvc": "192",
-            }
-        return {
-            "apple-clang": "13",
-            "clang": "11",
-            "gcc": "9",
-            "Visual Studio": "16",
-            "msvc": "192",
-        }
 
     def export_sources(self):
         export_conandata_patches(self)
@@ -81,22 +57,16 @@ class PackioConan(ConanFile):
             self.requires("asio/1.31.0")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.",
-            )
+        check_min_cppstd(self, 17)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def build(self):
         pass
 
     def package(self):
-        apply_conandata_patches(self)
         copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         copy(self, "*.h", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
 
