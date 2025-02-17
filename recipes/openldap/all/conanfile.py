fe61a7d814be022032b7b03498fd7eee75006a2c
Martin Valgur <martin.valgur@gmail.com>
2025-02-17 20:38:27 +0200

    openldap: drop Conan v1 cruft

--- recipes/openldap/all/conanfile.py
+++ recipes/openldap/all/conanfile.py
@@ -1,12 +1,11 @@
 import os
-import shutil
 
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import cross_building
 from conan.tools.env import VirtualRunEnv
 from conan.tools.files import chdir, copy, get, rm, rmdir, replace_in_file, save
-from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain, GnuToolchain
+from conan.tools.gnu import Autotools, AutotoolsDeps, GnuToolchain
 from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.layout import basic_layout
 
@@ -117,7 +116,3 @@ class OpenldapConan(ConanFile):
         self.cpp_info.components["lber"].libs = ["lber"]
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["lber"].system_libs = ["pthread"]
-
-        # TODO: to remove in conan v2
-        bin_path = os.path.join(self.package_folder, "bin")
-        self.env_info.PATH.append(bin_path)
