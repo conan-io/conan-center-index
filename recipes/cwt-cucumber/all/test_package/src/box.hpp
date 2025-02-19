e32af46c5a21f0cc7536881509f94744bea1340a
Thomas Sedlmair <thomas.sedlmair@googlemail.com>
2025-02-19 07:33:39 +0100

    updated example

--- recipes/cwt-cucumber/all/test_package/src/box.hpp
+++ recipes/cwt-cucumber/all/test_package/src/box.hpp
@@ -1,37 +1,32 @@
 #pragma once
 
+#include <algorithm>
 #include <string>
 #include <vector>
 
-class box
-{
- public:
+class box {
+public:
   box() = default;
 
-  void add_item(const std::string& item) { m_items.push_back(item); }
-  void add_items(const std::string& item, std::size_t count)
-  {
-    for ([[maybe_unused]] int i = 0; i < count; i++)
-    {
+  void add_item(const std::string &item) { m_items.push_back(item); }
+  void add_items(const std::string &item, std::size_t count) {
+    for ([[maybe_unused]] int i = 0; i < count; i++) {
       add_item(item);
     }
   }
-  [[nodiscard]] std::size_t items_count() const noexcept
-  {
+  [[nodiscard]] std::size_t items_count() const noexcept {
     return m_items.size();
   }
-  [[nodiscard]] const std::string& at(const std::size_t idx) const
-  {
+  [[nodiscard]] const std::string &at(const std::size_t idx) const {
     return m_items.at(idx);
   }
   void close() noexcept { m_is_open = false; }
 
-  std::size_t count(std::string_view item)
-  {
+  std::size_t count(std::string_view item) {
     return std::count(m_items.begin(), m_items.end(), item);
   }
 
- private:
+private:
   bool m_is_open{true};
   std::vector<std::string> m_items;
 };
