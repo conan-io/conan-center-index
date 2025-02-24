c8d41b3c340bae4d79ca353cdec751d23d3e07ec
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-02-24 18:45:06 +0100

    Cleanup

--- recipes/au/all/conanfile.py
+++ recipes/au/all/conanfile.py
@@ -3,11 +3,8 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import (
-    apply_conandata_patches,
     copy,
-    export_conandata_patches,
     get,
-    rm,
     rmdir,
 )
 from conan.tools.scm import Version
