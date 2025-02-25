d26cda671249a8a6439f873e19c49adf5bfaeb85
Chip Hogg <charles.r.hogg@gmail.com>
2025-02-25 08:36:38 -0500

    Upgrade au recipe to use conan 2.0 (#26633)
    * au: add version 0.4.1, follow 2.0 template
    
    * Cleanup
    
    * Cleanup
    
    ---------
    
    Co-authored-by: toge <toge.mail@gmail.com>
    Co-authored-by: Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>

--- recipes/au/all/conanfile.py
+++ recipes/au/all/conanfile.py
@@ -2,12 +2,17 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
+from conan.tools.files import (
+    copy,
+    get,
+    rmdir,
+)
 from conan.tools.scm import Version
 import os
 
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.0"
+
 
 class PackageConan(ConanFile):
     name = "au"
@@ -15,26 +20,9 @@ class PackageConan(ConanFile):
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/aurora-opensource/au"
-    topics = ("units", "C++14")
+    topics = ("units", "C++14", "header-only")
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
-
-    @property
-    def _min_cppstd(self):
-        return 14
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "apple-clang": "10",
-            "clang": "7",
-            "gcc": "7",
-            "msvc": "191",
-            "Visual Studio": "15",
-        }
-
-    def export_sources(self):
-        export_conandata_patches(self)
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -43,13 +31,7 @@ class PackageConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 14)
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.24 <4]")
@@ -59,12 +41,11 @@ class PackageConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["AU_EXCLUDE_GTEST_DEPENDENCY"] = "ON"
-        tc.variables["AU_ENABLE_TESTING"] = "OFF"
+        tc.variables["AU_ENABLE_TESTING"] = False
+        tc.variables["AU_EXCLUDE_GTEST_DEPENDENCY"] = True
         tc.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -73,7 +54,12 @@ class PackageConan(ConanFile):
         cmake = CMake(self)
         cmake.install()
 
-        copy(self, "LICENSE.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(
+            self,
+            "LICENSE.txt",
+            self.source_folder,
+            os.path.join(self.package_folder, "licenses"),
+        )
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
@@ -84,9 +70,3 @@ class PackageConan(ConanFile):
             self.cpp_info.system_libs.append("m")
             self.cpp_info.system_libs.append("pthread")
             self.cpp_info.system_libs.append("dl")
-
-        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
-        self.cpp_info.filenames["cmake_find_package"] = "Au"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "Au"
-        self.cpp_info.names["cmake_find_package"] = "Au::au"
-        self.cpp_info.names["cmake_find_package_multi"] = "Au::au"
