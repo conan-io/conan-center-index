d82e15609643e5e09ffcba8c97742d189dd08790
Tomas Krupa <tomas.krupa@innovatrics.com>
2025-02-26 15:56:47 +0100

    fixed code review findings

--- recipes/nfir/all/test_package/conanfile.py
+++ recipes/nfir/all/test_package/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.files import download
 from os import path
 
 class TestPackageConan(ConanFile):
@@ -16,11 +15,6 @@ class TestPackageConan(ConanFile):
         self.requires(self.tested_reference_str)
 
     def build(self):
-        download(
-                self,
-                url="https://raw.githubusercontent.com/tomas-krupa/NFIR/refs/tags/0.2.0/doc/img/f_nist-logo-brand-2c_512wide.png",
-                filename=path.join(self.build_folder, "image.png")
-                )
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -29,4 +23,4 @@ class TestPackageConan(ConanFile):
         if can_run(self):
             self.run(path.join(self.cpp.build.bindir, "test_package"), env="conanrun")
             ext = ".exe" if self.settings.os == "Windows" else ""
-            self.run(f"NFIR{ext} -a 1000 -b 500 -c image.png -d downsampled.png", env="conanrun")
+            self.run(f"NFIR{ext} --version", env="conanrun")
