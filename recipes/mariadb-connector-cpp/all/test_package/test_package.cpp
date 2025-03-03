84b42a1caf8be5756ffe936802a497a27b8056fe
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-03-03 11:55:34 +0000

    Update recipes/mariadb-connector-cpp/all/test_package/test_package.cpp

--- recipes/mariadb-connector-cpp/all/test_package/test_package.cpp
+++ recipes/mariadb-connector-cpp/all/test_package/test_package.cpp
@@ -6,7 +6,6 @@ int main (int argc, char *argv[])
     sql::Driver* driver = sql::mariadb::get_driver_instance();
     sql::Properties properties({{"user", "test"}, {"password", "test"}});
 
-    std::cout << "WELL DONE\n";
 
     return 0;
 }
