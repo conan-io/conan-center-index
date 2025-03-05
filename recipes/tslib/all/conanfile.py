f5c1420c874437738fdbd1e938e584a412ea5d47
Winfried Dobbe <winfried_mb2@xmsnet.nl>
2025-03-05 20:46:19 +0100

    Add recipe for tslib.

---
+++ recipes/tslib/all/conanfile.py
@@ -0,0 +1,75 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
+from conan.tools.scm import Version
+import os
+
+required_conan_version = ">=2.0"
+
+
+class TslibConan(ConanFile):
+    name = "tslib"
+    description = "C library for filtering touchscreen events"
+    license = ("LGPL-2.0", "GPL")
+    topics = ("touchscreen")
+    homepage = "https://github.com/kergoth/tslib.git"
+    url = "https://github.com/conan-io/conan-center-index"
+
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "enable_tools": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "enable_tools": True
+    }
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def validate(self):
+        if self.settings.os != "Linux":
+            raise ConanInvalidConfiguration("tslib is only supported on Linux")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["ENABLE_TOOLS"] = self.options.enable_tools
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "CONTRIBUTING.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "etc"))
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_target_name", "tslib::tslib")
+        self.cpp_info.set_property("pkg_config_name", "tslib")
+        self.cpp_info.libs = ["ts"]
