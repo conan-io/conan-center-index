d95fb9fdc608dbf6b9a832fcd79e25c44bf4116c
Martin Valgur <martin.valgur@gmail.com>
2025-03-05 21:38:46 +0200

    libnice: add introspection support, define components

--- recipes/libnice/all/conanfile.py
+++ recipes/libnice/all/conanfile.py
@@ -13,7 +13,7 @@ required_conan_version = ">=1.60.0 <2 || >=2.0.6"
 class LibniceConan(ConanFile):
     name = "libnice"
     homepage = "https://libnice.freedesktop.org/"
-    license = ("MPL-1.1", "LGPL-2.1-only")
+    license = "LGPL-2.1-or-later OR MPL-1.1"
     url = "https://github.com/conan-io/conan-center-index"
     description = "a GLib ICE implementation"
     topics = ("ice", "stun", "turn")
@@ -25,13 +25,15 @@ class LibniceConan(ConanFile):
         "crypto_library": ["openssl", "win32"],
         "with_gstreamer": [True, False],
         "with_gtk_doc": [True, False],
-        "with_introspection": [True, False]}
+        "with_introspection": [True, False],
+    }
     default_options = {
         "shared": False,
         "fPIC": True,
         "with_gstreamer": False,
         "with_gtk_doc": False,
-        "with_introspection": False}
+        "with_introspection": False,
+    }
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -61,18 +63,20 @@ class LibniceConan(ConanFile):
                 "-o glib/*:shared=True with static runtime is not supported")
 
     def requirements(self):
-        self.requires("glib/2.78.1", transitive_headers=True, transitive_libs=True)
+        self.requires("glib/2.78.3", transitive_headers=True, transitive_libs=True)
         if self.options.crypto_library == "openssl":
             self.requires("openssl/[>=1.1 <4]")
         if self.options.with_gstreamer:
-            self.requires("gstreamer/1.22.3")
+            self.requires("gstreamer/1.24.11")
+        if self.options.with_introspection:
+            self.requires("gobject-introspection/1.78.1")
 
     def build_requirements(self):
-        self.tool_requires("meson/1.2.3")
-        self.tool_requires("pkgconf/2.0.3")
+        self.tool_requires("meson/[>=1.2.3 <2]")
+        self.tool_requires("pkgconf/[>=2.2 <3]")
         self.tool_requires("glib/<host_version>")  # for glib-mkenums
         if self.options.with_introspection:
-            self.tool_requires("gobject-introspection/1.72.0")
+            self.tool_requires("gobject-introspection/<host_version>")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -98,13 +102,15 @@ class LibniceConan(ConanFile):
         meson.build()
 
     def package(self):
-        copy(self, pattern="COPYING*", dst=os.path.join(self.package_folder,
-             "licenses"), src=self.source_folder)
+        copy(self, "COPYING*", self.source_folder, os.path.join(self.package_folder, "licenses"))
         meson = Meson(self)
         meson.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "gstreamer-1.0", "pkgconfig"))
         rm(self, "*.pdb", os.path.join(self.package_folder, "lib"))
         rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
+        if self.options.with_introspection:
+            rename(self, os.path.join(self.package_folder, "share"), os.path.join(self.package_folder, "res"))
         if self.settings.os == "Windows":
             if not self.options.shared:
                 with chdir(self, os.path.join(self.package_folder, "lib")):
@@ -112,6 +118,24 @@ class LibniceConan(ConanFile):
         fix_apple_shared_install_name(self)
 
     def package_info(self):
-        self.cpp_info.libs = ["nice"]
+        self.cpp_info.components["libnice"].set_property("pkg_config_name", "nice")
+        self.cpp_info.components["libnice"].set_property("pkg_config_custom_content", "upnp_enabled=false")
+        self.cpp_info.components["libnice"].libs = ["nice"]
+        self.cpp_info.components["libnice"].includedirs.append(os.path.join("include", "nice"))
+        self.cpp_info.components["libnice"].requires = ["glib::glib-2.0", "glib::gobject-2.0", "glib::gio-2.0", "glib::gthread-2.0"]
+        if self.options.crypto_library == "openssl":
+            self.cpp_info.components["libnice"].requires.append("openssl::openssl")
         if self.settings.os == "Windows":
-            self.cpp_info.system_libs.append("advapi32")
+            self.cpp_info.components["libnice"].system_libs.append("advapi32")
+
+        if self.options.with_gstreamer:
+            self.cpp_info.components["gstnice"].set_property("pkg_config_name", "gstnice")
+            self.cpp_info.components["gstnice"].libs = ["gstnice"]
+            self.cpp_info.components["gstnice"].libdirs = [os.path.join("lib", "gstreamer-1.0")]
+            self.cpp_info.components["gstnice"].requires = ["libnice", "gstreamer::gstreamer-base-1.0"]
+
+        if self.options.with_introspection:
+            self.cpp_info.components["libnice"].resdirs = ["res"]
+            self.cpp_info.components["libnice"].requires.append("gobject-introspection::gobject-introspection")
+            self.buildenv_info.append_path("GI_GIR_PATH", os.path.join(self.package_folder, "res", "gir-1.0"))
+            self.runenv_info.append_path("GI_TYPELIB_PATH", os.path.join(self.package_folder, "lib", "girepository-1.0"))
--- recipes/libnice/all/test_package/conanfile.py
+++ recipes/libnice/all/test_package/conanfile.py
@@ -6,8 +6,7 @@ from conan.tools.build import can_run
 
 class LibniceTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
-    test_type = "explicit"
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
@@ -22,5 +21,5 @@ class LibniceTestConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            cmd = os.path.join(self.cpp.build.bindirs[0], "example")
+            cmd = os.path.join(self.cpp.build.bindir, "example")
             self.run(cmd, env="conanrun")
