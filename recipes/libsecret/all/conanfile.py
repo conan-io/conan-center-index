701e099c102cd31b230be67b4bcce139f7675f8c
Martin Valgur <martin.valgur@gmail.com>
2025-03-05 21:35:00 +0200

    libsecret: add gobject-introspection support

--- recipes/libsecret/all/conanfile.py
+++ recipes/libsecret/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.apple import fix_apple_shared_install_name, is_apple_os
+from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get, rmdir
 from conan.tools.gnu import PkgConfigDeps
@@ -9,7 +9,7 @@ from conan.tools.meson import Meson, MesonToolchain
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.60.0 <2.0 || >=2.0.6"
+required_conan_version = ">=2.0.6"
 
 
 class LibsecretConan(ConanFile):
@@ -27,12 +27,14 @@ class LibsecretConan(ConanFile):
         "fPIC": [True, False],
         "crypto": [False, "libgcrypt", "gnutls"],
         "with_libgcrypt": [True, False, "deprecated"],
+        "with_introspection": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
         "crypto": "libgcrypt",
         "with_libgcrypt": "deprecated",
+        "with_introspection": False,
     }
 
     def config_options(self):
@@ -57,6 +59,8 @@ class LibsecretConan(ConanFile):
             self.requires("libgcrypt/1.10.3")
         elif self.options.get_safe("crypto") == "gnutls":
             self.requires("gnutls/3.8.2")
+        if self.options.with_introspection:
+            self.requires("gobject-introspection/1.78.1")
 
     def validate(self):
         if self.settings.os == "Windows":
@@ -68,17 +72,13 @@ class LibsecretConan(ConanFile):
         del self.info.options.with_libgcrypt
 
     def build_requirements(self):
-        self.tool_requires("meson/1.4.1")
+        self.tool_requires("meson/[>=1.2.3 <2]")
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
-            self.tool_requires("pkgconf/2.2.0")
+            self.tool_requires("pkgconf/[>=2.2 <3]")
         self.tool_requires("glib/<host_version>")
-
-        if is_apple_os(self):
-            # Avoid using gettext from homebrew which may be linked against
-            # a different/incompatible libiconv than the one being exposed
-            # in the runtime environment (DYLD_LIBRARY_PATH)
-            # See https://github.com/conan-io/conan-center-index/pull/17502#issuecomment-1542492466
-            self.tool_requires("gettext/0.22.5")
+        self.tool_requires("gettext/0.22.5")
+        if self.options.with_introspection:
+            self.tool_requires("gobject-introspection/<host_version>")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -87,7 +87,8 @@ class LibsecretConan(ConanFile):
         env = VirtualBuildEnv(self)
         env.generate()
         tc = MesonToolchain(self)
-        tc.project_options["introspection"] = "false"
+        tc.project_options["introspection"] = "true" if self.options.with_introspection else "false"
+        tc.project_options["vapi"] = "false"
         tc.project_options["manpage"] = "false"
         tc.project_options["gtk_doc"] = "false"
         if Version(self.version) >= "0.21.2":
@@ -108,15 +109,21 @@ class LibsecretConan(ConanFile):
         meson = Meson(self)
         meson.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
-        rmdir(self, os.path.join(self.package_folder, "share"))
+        os.rename(os.path.join(self.package_folder, "share"),
+                  os.path.join(self.package_folder, "res"))
         fix_apple_shared_install_name(self)
 
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "libsecret-1")
         self.cpp_info.includedirs = [os.path.join("include", "libsecret-1")]
+        self.cpp_info.resdirs = ["res"]
         self.cpp_info.libs = ["secret-1"]
         self.cpp_info.requires = ["glib::glib-2.0", "glib::gobject-2.0", "glib::gio-2.0"]
         if self.options.get_safe("crypto") == "libgcrypt":
             self.cpp_info.requires.append("libgcrypt::libgcrypt")
         elif self.options.get_safe("crypto") == "gnutls":
             self.cpp_info.requires.append("gnutls::gnutls")
+        if self.options.with_introspection:
+            self.cpp_info.requires.append("gobject-introspection::gobject-introspection")
+            self.buildenv_info.append_path("GI_GIR_PATH", os.path.join(self.package_folder, "res", "gir-1.0"))
+            self.runenv_info.append_path("GI_TYPELIB_PATH", os.path.join(self.package_folder, "lib", "girepository-1.0"))
