6d7dcf54da88829980759965cbc67340a6fa836c
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-03-07 23:36:26 +0100

    Add aws-c-cal 0.8.3, dependency of new aws-crt

--- recipes/aws-c-cal/all/conanfile.py
+++ recipes/aws-c-cal/all/conanfile.py
@@ -1,10 +1,9 @@
 from conan import ConanFile
 from conan.tools.apple import is_apple_os
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, save
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.scm import Version
 import os
-import textwrap
 
 required_conan_version = ">=1.53.0"
 
@@ -27,6 +26,9 @@ class AwsCCal(ConanFile):
         "fPIC": True,
     }
 
+    implements = ["auto_shared_fpic"]
+    languages = "C"
+
     @property
     def _needs_openssl(self):
         return not (self.settings.os == "Windows" or is_apple_os(self))
@@ -34,23 +36,15 @@ class AwsCCal(ConanFile):
     def export_sources(self):
         export_conandata_patches(self)
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
-
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        if self.version == "0.6.14":
+        if self.version == "0.8.3":
+            self.requires("aws-c-common/0.11.0", transitive_headers=True, transitive_libs=True)
+        elif self.version == "0.6.14":
             self.requires("aws-c-common/0.9.15", transitive_headers=True, transitive_libs=True)
-        if self.version == "0.5.12":
+        elif self.version == "0.5.12":
             self.requires("aws-c-common/0.6.11", transitive_headers=True, transitive_libs=True)
         if self._needs_openssl:
             self.requires("openssl/[>=1.1 <4]")
@@ -77,27 +71,6 @@ class AwsCCal(ConanFile):
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "aws-c-cal"))
-
-        # TODO: to remove in conan v2 once legacy generators removed
-        self._create_cmake_module_alias_targets(
-            os.path.join(self.package_folder, self._module_file_rel_path),
-            {"AWS::aws-c-cal": "aws-c-cal::aws-c-cal"}
-        )
-
-    def _create_cmake_module_alias_targets(self, module_file, targets):
-        content = ""
-        for alias, aliased in targets.items():
-            content += textwrap.dedent(f"""\
-                if(TARGET {aliased} AND NOT TARGET {alias})
-                    add_library({alias} INTERFACE IMPORTED)
-                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
-                endif()
-            """)
-        save(self, module_file, content)
-
-    @property
-    def _module_file_rel_path(self):
-        return os.path.join("lib", "cmake", f"conan-official-{self.name}-targets.cmake")
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "aws-c-cal")
@@ -133,7 +106,3 @@ class AwsCCal(ConanFile):
                 crypto_link_flags = "-Wl," + ",".join(f"-u{symbol}" for symbol in crypto_symbols)
                 self.cpp_info.exelinkflags.append(crypto_link_flags)
                 self.cpp_info.sharedlinkflags.append(crypto_link_flags)
-
-        # TODO: to remove in conan v2 once cmake_find_package* generators removed
-        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
-        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
