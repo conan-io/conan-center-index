c06dca40dd4f9b1d465a44f6106a35273ab38dd9
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-03-07 22:05:56 +0100

    Add aws-crt-cpp 0.31.0

--- recipes/aws-crt-cpp/all/conanfile.py
+++ recipes/aws-crt-cpp/all/conanfile.py
@@ -2,13 +2,11 @@ from conan import ConanFile
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir, save
 from conan.tools.build import check_min_cppstd
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
-from conan.tools.scm import Version
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
 import os
-import textwrap
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2"
 
 
 class AwsCrtCpp(ConanFile):
@@ -48,7 +46,19 @@ class AwsCrtCpp(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        if self.version == "0.26.9":
+        if self.version == "0.31.0":
+            self.requires("aws-c-common/0.11.0")
+            self.requires("aws-c-sdkutils/0.2.3")
+            self.requires("aws-c-io/0.15.4", transitive_headers=True)
+            self.requires("aws-c-cal/0.8.3")
+            self.requires("aws-c-compression/0.3.1")
+            self.requires("aws-c-http/0.9.3", transitive_headers=True)
+            self.requires("aws-c-auth/0.8.4", transitive_headers=True)
+            self.requires("aws-c-mqtt/0.12.1", transitive_headers=True)
+            self.requires("aws-checksums/0.2.3")
+            self.requires("aws-c-event-stream/0.5.1")
+            self.requires("aws-c-s3/0.7.11")
+        elif self.version == "0.26.9":
             # From add_subdirectory() calls in https://github.com/awslabs/aws-crt-cpp/blob/v0.26.9/CMakeLists.txt
             self.requires("aws-c-common/0.9.15")
             self.requires("aws-c-sdkutils/0.1.15")
@@ -61,7 +71,7 @@ class AwsCrtCpp(ConanFile):
             self.requires("aws-checksums/0.1.18")
             self.requires("aws-c-event-stream/0.4.2")
             self.requires("aws-c-s3/0.5.5")
-        if self.version == "0.17.1a":
+        elif self.version == "0.17.1a":
             # From add_subdirectory() calls in https://github.com/awslabs/aws-crt-cpp/blob/v0.17.1a/CMakeLists.txt#L95
             self.requires("aws-c-http/0.6.7", transitive_headers=True)
             self.requires("aws-c-mqtt/0.7.8", transitive_headers=True)
@@ -104,34 +114,9 @@ class AwsCrtCpp(ConanFile):
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "aws-crt-cpp"))
 
-        # TODO: to remove in conan v2 once legacy generators removed
-        self._create_cmake_module_alias_targets(
-            os.path.join(self.package_folder, self._module_file_rel_path),
-            {"AWS::aws-crt-cpp": "aws-crt-cpp::aws-crt-cpp"}
-        )
-
-    def _create_cmake_module_alias_targets(self, module_file, targets):
-        content = ""
-        for alias, aliased in targets.items():
-            content += textwrap.dedent(f"""\
-                if(TARGET {aliased} AND NOT TARGET {alias})
-                    add_library({alias} INTERFACE IMPORTED)
-                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
-                endif()
-            """)
-        save(self, module_file, content)
-
-    @property
-    def _module_file_rel_path(self):
-        return os.path.join("lib", "cmake", f"conan-official-{self.name}-targets.cmake")
-
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "aws-crt-cpp")
         self.cpp_info.set_property("cmake_target_name", "AWS::aws-crt-cpp")
         self.cpp_info.libs = ["aws-crt-cpp"]
         if self.options.shared:
             self.cpp_info.defines.append("AWS_CRT_CPP_USE_IMPORT_EXPORT")
-
-        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
-        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
-        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
