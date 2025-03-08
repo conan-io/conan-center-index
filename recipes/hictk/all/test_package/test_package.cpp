8e233b0d2fa93135bce492d57f1dce621bcfeff4
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-03-08 12:56:57 +0100

    Update test to include headers affected by with_{arrow,eigen} options

--- recipes/hictk/all/test_package/test_package.cpp
+++ recipes/hictk/all/test_package/test_package.cpp
@@ -2,6 +2,13 @@
 
 #include "hictk/file.hpp"
 
+#if __has_include("hictk/transformers/to_dataframe.hpp")
+#include "hictk/transformers/to_dataframe.hpp"     // test hictk/*:with_arrow
+#endif
+
+#if __has_include("hictk/transformers/to_dense_matrix.hpp")
+#include "hictk/transformers/to_dense_matrix.hpp"  // test hictk/*:with_eigen
+#endif
 
 int main(int argc, char** argv) {
   try {
