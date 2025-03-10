33a04ddf6d36af3e0e4510507283c94fa460e64c
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-03-10 11:14:55 +0100

    Add aws-c-io 0.15.4, dependency of new aws-crt

--- recipes/aws-c-io/all/conanfile.py
+++ recipes/aws-c-io/all/conanfile.py
@@ -1,10 +1,9 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import get, copy, rmdir, save
+from conan.tools.files import get, copy, rmdir
 import os
-import textwrap
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.4"
 
 
 class AwsCIO(ConanFile):
@@ -25,15 +24,8 @@ class AwsCIO(ConanFile):
         "fPIC": True,
     }
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
+    implements = ["auto_shared_fpic"]
+    languages = "C"
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -41,6 +33,11 @@ class AwsCIO(ConanFile):
     def requirements(self):
         # These versions come from aws-sdl-cpp prefetch_crt_dependency.sh file,
         # dont bump them independently, check the file and update all the dependencies at once
+        if self.version == "0.15.4":
+            self.requires("aws-c-common/0.11.0", transitive_headers=True, transitive_libs=True)
+            self.requires("aws-c-cal/0.8.3")
+            if self.settings.os in ["Linux", "FreeBSD", "Android"]:
+                self.requires("s2n/1.5.9")
         if self.version == "0.14.7":
             self.requires("aws-c-common/0.9.15", transitive_headers=True, transitive_libs=True)
             self.requires("aws-c-cal/0.6.14")
@@ -74,27 +71,6 @@ class AwsCIO(ConanFile):
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "aws-c-io"))
 
-        # TODO: to remove in conan v2 once legacy generators removed
-        self._create_cmake_module_alias_targets(
-            os.path.join(self.package_folder, self._module_file_rel_path),
-            {"AWS::aws-c-io": "aws-c-io::aws-c-io"}
-        )
-
-    def _create_cmake_module_alias_targets(self, module_file, targets):
-        content = ""
-        for alias, aliased in targets.items():
-            content += textwrap.dedent(f"""\
-                if(TARGET {aliased} AND NOT TARGET {alias})
-                    add_library({alias} INTERFACE IMPORTED)
-                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
-                endif()
-            """)
-        save(self, module_file, content)
-
-    @property
-    def _module_file_rel_path(self):
-        return os.path.join("lib", "cmake", f"conan-official-{self.name}-targets.cmake")
-
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "aws-c-io")
         self.cpp_info.set_property("cmake_target_name", "AWS::aws-c-io")
@@ -103,9 +79,7 @@ class AwsCIO(ConanFile):
             self.cpp_info.defines.append("AWS_IO_USE_IMPORT_EXPORT")
         if self.settings.os == "Macos":
             self.cpp_info.frameworks.append("Security")
+            if Version(self.version) >= "0.15.4":
+                self.cpp_info.frameworks.append("Network")
         if self.settings.os == "Windows":
             self.cpp_info.system_libs = ["crypt32", "secur32", "shlwapi"]
-
-        # TODO: to remove in conan v2 once cmake_find_package* generators removed
-        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
-        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
