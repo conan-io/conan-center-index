0ce74f818582c148e995d734b0981ba99c470e51
Uilian Ries <uilianries@gmail.com>
2025-03-11 16:58:44 +0100

    Use elfutils for glib
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/glib/all/conanfile.py
+++ recipes/glib/all/conanfile.py
@@ -51,6 +51,7 @@ class GLibConan(ConanFile):
         if self.settings.os != "Linux":
             del self.options.with_mount
             del self.options.with_selinux
+        self.options.with_elf = self.settings.os == "Linux"
         if is_msvc(self):
             del self.options.with_elf
 
@@ -71,7 +72,7 @@ class GLibConan(ConanFile):
         self.requires("libffi/3.4.4")
         self.requires("pcre2/10.42")
         if self.options.get_safe("with_elf"):
-            self.requires("libelf/0.8.13")
+            self.requires("elfutils/0.190")
         if self.options.get_safe("with_mount"):
             self.requires("libmount/2.39.2")
         if self.options.get_safe("with_selinux"):
