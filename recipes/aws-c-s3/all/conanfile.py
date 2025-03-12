dc3fd6142cb6229cd2ef0a249abea8b1cbaf1dcd
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-03-12 22:09:41 +0100

    Add aws-c-s3/0.7.11, dependency of new aws-crt

--- recipes/aws-c-s3/all/conanfile.py
+++ recipes/aws-c-s3/all/conanfile.py
@@ -1,11 +1,9 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import get, copy, rmdir, save
-from conan.tools.scm import Version
+from conan.tools.files import get, copy, rmdir
 import os
-import textwrap
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.4"
 
 
 class AwsCS3(ConanFile):
@@ -26,20 +24,20 @@ class AwsCS3(ConanFile):
         "fPIC": True,
     }
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
+    implements = ["auto_shared_fpic"]
+    languages = "C"
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
+        if self.version == "0.7.11":
+            self.requires("aws-c-common/0.11.0", transitive_headers=True, transitive_libs=True)
+            self.requires("aws-c-cal/0.8.3")
+            self.requires("aws-c-auth/0.8.4", transitive_headers=True)
+            self.requires("aws-c-http/0.9.3")
+            self.requires("aws-c-io/0.15.4", transitive_headers=True)
+            self.requires("aws-checksums/0.2.3")
         if self.version == "0.5.5":
             self.requires("aws-c-common/0.9.15", transitive_headers=True, transitive_libs=True)
             self.requires("aws-c-cal/0.6.14")
@@ -76,34 +74,9 @@ class AwsCS3(ConanFile):
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "aws-c-s3"))
 
-        # TODO: to remove in conan v2 once legacy generators removed
-        self._create_cmake_module_alias_targets(
-            os.path.join(self.package_folder, self._module_file_rel_path),
-            {"AWS::aws-c-s3": "aws-c-s3::aws-c-s3"}
-        )
-
-    def _create_cmake_module_alias_targets(self, module_file, targets):
-        content = ""
-        for alias, aliased in targets.items():
-            content += textwrap.dedent(f"""\
-                if(TARGET {aliased} AND NOT TARGET {alias})
-                    add_library({alias} INTERFACE IMPORTED)
-                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
-                endif()
-            """)
-        save(self, module_file, content)
-
-    @property
-    def _module_file_rel_path(self):
-        return os.path.join("lib", "cmake", f"conan-official-{self.name}-targets.cmake")
-
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "aws-c-s3")
         self.cpp_info.set_property("cmake_target_name", "AWS::aws-c-s3")
         self.cpp_info.libs = ["aws-c-s3"]
         if self.options.shared:
             self.cpp_info.defines.append("AWS_S3_USE_IMPORT_EXPORT")
-
-        # TODO: to remove in conan v2 once cmake_find_package* generators removed
-        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
-        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
