78625b19e6fe3f9e9e88f2bb4787a12a48803dcc
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-03-13 13:55:22 +0100

    Cleanup

--- recipes/imgui-sfml/all/conanfile.py
+++ recipes/imgui-sfml/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, replace_in_file, rm, rmdir, apply_conandata_patches, export_conandata_patches
+from conan.tools.files import copy, get, rm, rmdir, apply_conandata_patches, export_conandata_patches
 import os
 
 required_conan_version = ">=2.0.9"
