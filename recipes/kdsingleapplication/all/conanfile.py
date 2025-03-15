23564733b516338f6bfa287a6950cc77b4f06d95
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2025-03-15 23:41:44 +0100

    kdsingleapplication: initial

---
+++ recipes/kdsingleapplication/all/conanfile.py
@@ -0,0 +1,60 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+import os
+
+required_conan_version = ">=2.0.9"
+
+class KDSingleApplicationConan(ConanFile):
+    name = "kdsingleapplication"
+    description = "KDAB's helper class for single-instance policy applications."
+    topics = ("qt", "kdab")
+    license = "MIT"
+    homepage = "https://github.com/KDAB/KDSingleApplication"
+    url = "https://github.com/conan-io/conan-center-index"
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": True,
+        "fPIC": True,
+    }
+
+    implements = ["auto_shared_fpic"]
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires("qt/[>=6.7 <7]", options={"with_dbus": True, "widgets": True, "shared": True}, transitive_headers=True, transitive_libs=True)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["KDSingleApplication_QT6"] = True
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "LICENSE.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["KDSingleApplication"]
