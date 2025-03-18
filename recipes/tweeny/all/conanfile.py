65426de9766c3a0dd197519d152ac8bbee73d101
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2025-03-18 12:05:19 +0100

    add new recipe tweeny
    Co-Authored-By: Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>

---
+++ recipes/tweeny/all/conanfile.py
@@ -0,0 +1,58 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
+from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
+import os
+
+
+required_conan_version = ">=2.0.9"
+
+class TweenyConan(ConanFile):
+    name = "tweeny"
+    description = "Tweeny is an inbetweening library designed for the creation of complex animations for games and other beautiful interactive software"
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/mobius3/tweeny"
+    topics = ("animation", "tweening", "easing-functions")
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def validate(self):
+        check_min_cppstd(self, 11)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"  # CMake 4 support
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package_id(self):
+        self.info.clear()
+
+    def package(self):
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.set_property("cmake_file_name", "tweeny")
+        self.cpp_info.set_property("cmake_target_name", "tweeny")
---
+++ recipes/tweeny/all/test_package/test_package.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "tweeny/tweeny.h"
+
+
+int main(void) {
+    auto helloworld = tweeny::from('h','e','l','l','o').to('w','o','r','l','d').during(2);
+    for (int i = 0; i < 2; i++) {
+        for (char c : helloworld.step(1)) { printf("%c", c); }
+        printf("\n");
+    }
+}
