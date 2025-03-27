17d968810633f011cdcf098f3dead21098023d03
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-03-27 17:35:31 +0000

    remove unused import

--- recipes/libnfs/all/conanfile.py
+++ recipes/libnfs/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, rename
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.scm import Version
 import os
 
