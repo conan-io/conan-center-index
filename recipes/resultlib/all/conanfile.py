40a4445a345c3d5393619f9fd5dd7b6616f92170
Guillermo Calvo <guillermo@guillermo.dev>
2025-03-31 14:23:01 +0200

    Bump required version numbers

--- recipes/resultlib/all/conanfile.py
+++ recipes/resultlib/all/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
 import os
 
-required_conan_version = ">=2.0"
+required_conan_version = ">=2.1"
 
 class ResultLibConan(ConanFile):
     name = "resultlib"
