4d41d6e781e57f1090d559de89957ed1b22ee384
toge <toge.mail@gmail.com>
2025-04-04 04:16:11 +0900

    drop support apple-clang <= 14

--- recipes/idna/all/conanfile.py
+++ recipes/idna/all/conanfile.py
@@ -1,8 +1,10 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import get, copy
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2.0.9"
@@ -29,6 +31,9 @@ class IdnaConan(ConanFile):
 
     def validate(self):
         check_min_cppstd(self, 20)
+        # apple-clang <= 14 doesn't support C++20 std::any_of which idna uses
+        if self.settings.compiler == "apple-clang" and Version(self.settings.compiler.version) <= "14":
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support apple-clang <= 14")
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.16 <4]")
