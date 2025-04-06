740c9ba332c7a520e78dec4b01dda8d71be589cd
chausner <chausner@users.noreply.github.com>
2025-04-06 20:01:45 +0200

    Add grpc 1.71.0

--- recipes/grpc/all/conanfile.py
+++ recipes/grpc/all/conanfile.py
@@ -2,11 +2,10 @@ import os
 import yaml
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration, ConanException
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
 from conan.tools.build import cross_building, valid_min_cppstd, check_min_cppstd
 from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain, CMakeDeps
-from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rename, replace_in_file, rmdir
 from conan.tools.microsoft import check_min_vs, is_msvc
 from conan.tools.scm import Version
@@ -66,7 +65,12 @@ class GrpcConan(ConanFile):
 
     @property
     def _cxxstd_required(self):
-        return 14 if Version(self.version) >= "1.47" else 11
+        if Version(self.version) >= "1.70":
+            return 17
+        elif Version(self.version) >= "1.47":
+            return 14
+        else:
+            return 11
 
     @property
     def _supports_libsystemd(self):
