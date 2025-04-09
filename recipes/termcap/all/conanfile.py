57608fab47f55d101acc4241d5e118d7d3e2defa
Breuninger Matthias (ETAS-ICA/XPC-Fe3) <matthias.breuninger@etas.com>
2025-04-09 18:05:39 +0200

    fix: Don't define TERMCAP_FILE
    Instead, by default use default behaviour as provided by the termcap API

--- recipes/termcap/all/conanfile.py
+++ recipes/termcap/all/conanfile.py
@@ -61,7 +61,6 @@ class TermcapConan(ConanFile):
         tc.cache_variables["TERMCAP_HEADERS"] = to_cmake_paths(headers)
         tc.cache_variables["TERMCAP_INC_OPTS"] = to_cmake_paths(optional_headers)
         tc.cache_variables["TERMCAP_CAP_FILE"] = os.path.join(self.source_folder, "termcap.src").replace("\\", "/")
-        tc.cache_variables["CMAKE_INSTALL_SYSCONFDIR"] = os.path.join(self.package_folder, "bin", "etc").replace("\\", "/")
         tc.generate()
 
     def _patch_sources(self):
