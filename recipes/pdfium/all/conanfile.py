ccff857e57d540a400e9fa1407e5b07587c452f2
Hanno <vingine.dev@gmail.com>
2025-04-09 20:21:59 +0200

    Add cstdint patch

--- recipes/pdfium/all/conanfile.py
+++ recipes/pdfium/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
 from conan.tools.build import check_min_cppstd, stdcpp_library
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.scm import Version
 
@@ -32,6 +32,9 @@ class PdfiumConan(ConanFile):
         "fPIC": True,
         "with_libjpeg": "libjpeg",
     }
+
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -96,6 +99,7 @@ class PdfiumConan(ConanFile):
         deps.generate()
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure(build_script_folder=os.path.join("pdfium-cmake", "cmake"))
         cmake.build()
