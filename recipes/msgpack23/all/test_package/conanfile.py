4f500da551f4f4f814075dbb995e851acb3fe074
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-04-23 11:53:36 +0200

    Use the same recipe for both versions

--- recipes/msgpack23/all/test_package/conanfile.py
+++ recipes/msgpack23/all/test_package/conanfile.py
@@ -1,18 +1,24 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps"
 
     def layout(self):
         cmake_layout(self)
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        print(type(self.dependencies[self.tested_reference_str].ref.version))
+        tc.cache_variables["IS_NEW_API"] = self.dependencies[self.tested_reference_str].ref.version >= "3.0"
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
--- recipes/msgpack23/all/test_package/test_package_old.cpp
+++ recipes/msgpack23/all/test_package/test_package_old.cpp
@@ -1,13 +1,10 @@
 #include <msgpack23/msgpack23.h>
-#include <vector>
-#include <iterator>
 
 int main(void) {
-	std::vector<std::byte> data{};
-	auto inserter = std::back_inserter(data);
-    msgpack23::Packer packer {inserter};
+	
+    msgpack23::Packer packer {};
 	std::uint8_t const expected = 42;
-	packer(expected);
+	auto data = packer(expected);
 	msgpack23::Unpacker unpacker {data};
 	std::int8_t actual {};
 	unpacker(actual);
