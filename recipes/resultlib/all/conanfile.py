61ea8ed646b46765e91aa47121766f03f20765b6
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-05-04 21:07:55 +0200

    Cleanups

--- recipes/resultlib/all/conanfile.py
+++ recipes/resultlib/all/conanfile.py
@@ -10,14 +10,17 @@ class ResultLibConan(ConanFile):
     description = "Elegant error handling in C"
     license = "Apache-2.0"
     homepage = "https://result.guillermo.dev/"
+    url = "https://github.com/conan-io/conan-center-index"
     topics = ("header-only", "error-handling", "c-library", "declarative-programming", "monadic-interface", "result")
-    exports_sources = "src/result.h"
     package_type = "header-library"
-    settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
     def layout(self):
         basic_layout(self, src_folder="src")
+    
+    def validate(self):
+        if self.settings.get_safe("compiler.cstd"):
+            check_min_cstd(self, 23)
 
     def package_id(self):
         self.info.clear()
@@ -25,15 +28,9 @@ class ResultLibConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def build(self):
-        pass
-
     def package(self):
-        src_path = os.path.join(self.source_folder, "src")
-        include_path = os.path.join(self.package_folder, os.path.join("include", "resultlib"))
-        licenses_path = os.path.join(self.package_folder, "licenses")
-        copy(self, "result.h", src_path, include_path)
-        copy(self, "LICENSE", self.source_folder, licenses_path)
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "result.h", os.path.join(self.source_folder, "src"), os.path.join(self.package_folder, "include", "resultlib"))
 
     def package_info(self):
         self.cpp_info.bindirs = []
