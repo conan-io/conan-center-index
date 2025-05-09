2d167b63d7b661190aa407dce35bd242764399c5
Ingmar Rieger <git@irieger.net>
2025-05-09 22:16:53 +0200

    Update the fix for macOS

--- recipes/wxwidgets/all/conanfile.py
+++ recipes/wxwidgets/all/conanfile.py
@@ -177,8 +177,10 @@ class wxWidgetsConan(ConanFile):
                         "CMAKE_OSX_DEPLOYMENT_TARGET",
                         "CMAKE_OSX_DEPLOYMENT_TARGET_IGNORED")
         # Fix for strcpy_s (fix upstream?)
-        if is_apple_os(self) and Version(self.version) < "3.2.8":
+        if is_apple_os(self):
             cmake_version = "3.0"
+            if Version(self.version) >= "3.2.7":
+                cmake_version = "3.0...3.31"
             replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
                             f'cmake_minimum_required(VERSION {cmake_version})',
                             f'cmake_minimum_required(VERSION {cmake_version})\nadd_definitions(-D__STDC_WANT_LIB_EXT1__)')
