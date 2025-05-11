623d35252395147bcf5384d76e4bceb807f26b82
Kataev Victor <kataev.victor.1234@gmail.com>
2025-05-12 01:23:49 +0200

    feat(reduct-cpp): add recipe

---
+++ recipes/reduct-cpp/all/conanfile.py
@@ -0,0 +1,81 @@
+from os.path import join
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import copy
+from conan.tools.files import get
+
+
+class ReductCppConan(ConanFile):
+    name = "reduct-cpp"
+    description = "Reduct Storage Client SDK for C++"
+    license = "MIT"
+    url = "https://github.com/reduct-storage/reduct-cpp"
+    homepage = "https://www.reduct.store/docs/getting-started/with-cpp"
+    author = "Alexey Timin"
+    topics = ("reduct-storage", "http-client", "http-api")
+    package_type = "library"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_chrono": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_chrono": False,
+        "cpp-httplib/*:with_openssl": True,
+        "cpp-httplib/*:with_zlib": True,
+        "date/*:header_only": True,
+    }
+
+    requires = (
+        "fmt/11.0.2",
+        "cpp-httplib/0.16.0",
+        "nlohmann_json/3.11.3",
+        "openssl/3.2.2",
+        "concurrentqueue/1.0.4",
+    )
+
+    def config_options(self):
+        if self.settings.get_safe("os") == "Windows":
+            self.options.rm_safe("fPIC")
+            self.options.with_chrono = True
+        elif (
+                self.settings.get_safe("compiler") == "gcc"
+                and self.settings.get_safe("compiler.version") >= "14"
+        ):
+            self.options.with_chrono = True
+
+    def requirements(self):
+        if not self.options.with_chrono:
+            self.requires("date/3.0.1")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        if self.options.with_chrono:
+            tc.variables["REDUCT_CPP_USE_STD_CHRONO"] = "ON"
+        tc.generate()
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+        copy(self, "LICENSE", src=self.source_folder, dst=join(self.package_folder, "licenses"))
+
+    def package_info(self):
+        self.cpp_info.libs = ["reductcpp"]
