05e3fefebd5b223aeb69cf4d82a875c4bf7dcdc4
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-05-18 19:35:36 +0200

    Cleanups

--- recipes/sqlitemap/all/conanfile.py
+++ recipes/sqlitemap/all/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
 import os
 
@@ -19,9 +18,6 @@ class PackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
-    def export_sources(self):
-        export_conandata_patches(self)
-
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -36,9 +32,6 @@ class PackageConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-
-    def build(self):
-        pass
 
     def package(self):
         copy(self, "LICENSE.txt", self.source_folder, os.path.join(self.package_folder, "licenses"))
