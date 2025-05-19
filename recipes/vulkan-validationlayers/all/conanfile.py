56df3cbb6c305d7a485b75048dfb4d1a6c97de15
Andrei Gavrilin <gav2xlin@gmail.com>
2025-05-19 06:42:08 +0200

    vulkan-validationlayers: version 1.4.313.0

--- recipes/vulkan-validationlayers/all/conanfile.py
+++ recipes/vulkan-validationlayers/all/conanfile.py
@@ -100,7 +100,8 @@ class VulkanValidationLayersConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("robin-hood-hashing/3.11.5")
+        if Version(self.version) < "1.4.313.0":
+            self.requires("robin-hood-hashing/3.11.5")
         self.requires(self._require("spirv-headers"))
         if Version(conan_version).major < "2":
             # TODO: set private=True, once the issue is resolved https://github.com/conan-io/conan/issues/9390
