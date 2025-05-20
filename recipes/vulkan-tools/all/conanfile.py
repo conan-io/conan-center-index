7e4a3c0e59643bd7d4cbad1bfd0a15469418e721
Andrei Gavrilin <gav2xlin@gmail.com>
2025-05-20 07:24:18 +0200

    vulkan-tools: version 1.4.313.0

---
+++ recipes/vulkan-tools/all/conanfile.py
@@ -0,0 +1,53 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get
+
+required_conan_version = ">=2.0"
+
+
+class VulkanToolsConan(ConanFile):
+    name = "vulkan-tools"
+    homepage = "https://github.com/KhronosGroup/Vulkan-Tools"
+    description = "Vulkan Development Tools"
+    license = "Apache-2.0"
+    topics = ("vulkan-tools", "vulkan", "gpu")
+    url = "https://github.com/conan-io/conan-center-index"
+    package_type = "application"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        self.requires(f"vulkan-headers/{self.version}", transitive_headers=True)
+        if self.settings.os != "Android":
+            self.requires(f"vulkan-loader/{self.version}", transitive_headers=True)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_TESTS"] = False
+        tc.variables["TOOLS_CODEGEN"] = False
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "LICENSE*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        bin_path = os.path.join(self.package_folder, "bin")
+        self.output.info(f"Appending PATH environment variable: {bin_path}")
+        self.env_info.path.append(bin_path)
+
+        self.cpp_info.includedirs = []
+        self.cpp_info.libdirs = []
