802abce59e2a7f068fe22fcb7984054699669585
Andrei Gavrilin <gav2xlin@gmail.com>
2025-05-20 07:24:18 +0200

    vulkan-profiles: version 1.4.313.0

---
+++ recipes/vulkan-profiles/all/test_package/test_package.cpp
@@ -0,0 +1,11 @@
+//#include <vulkan/utility/vk_safe_struct.hpp>
+//#include <vulkan/layer/vk_layer_settings.h>
+
+int main(int argc, char ** argv)
+{
+    //const vku::safe_VkDescriptorPoolCreateInfo safe_create_info;
+
+    //VkuLayerSettingLogCallback pCallback{nullptr};
+
+    return 0;
+}
