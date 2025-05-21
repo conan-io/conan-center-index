3cc6d01359df86cf4c09d276f57f403bc58932c3
Raúl Cumplido <raulcumplido@gmail.com>
2025-05-21 11:26:48 +0200

    lz4, snappy, zlib and zstd should be enabled if orc is enabled Fix orc with with_orc

--- recipes/arrow/all/conanfile.py
+++ recipes/arrow/all/conanfile.py
@@ -217,6 +217,15 @@ class ArrowConan(ConanFile):
                 raise ConanException("'with_boost' option should be True when 'gandiva=True'")
             if not self.options.with_utf8proc:
                 raise ConanException("'with_utf8proc' option should be True when 'gandiva=True'")
+        if self.options.with_orc:
+            if not self.options.with_lz4:
+                raise ConanException("'with_lz4' option should be True when 'orc=True'")
+            if not self.options.with_snappy:
+                raise ConanException("'with_snappy' option should be True when 'orc=True'")
+            if not self.options.with_zlib:
+                raise ConanException("'with_zlib' option should be True when 'orc=True'")
+            if not self.options.with_zstd:
+                raise ConanException("'with_zstd' option should be True when 'orc=True'")
         if self.options.with_thrift and not self.options.with_boost:
             raise ConanException("'with_boost' option should be True when 'thrift=True'")
         if self.options.parquet:
