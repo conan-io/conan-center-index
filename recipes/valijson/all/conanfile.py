0526e1be5bd310728099e3f5eaf1ec81d988e5b9
Andrei Gavrilin <gav2xlin@gmail.com>
2025-05-22 16:47:19 +0200

    valijson: fix library name and update version 1.0.5

--- recipes/valijson/all/conanfile.py
+++ recipes/valijson/all/conanfile.py
@@ -54,8 +54,8 @@ class ValijsonConan(ConanFile):
         # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         # self.cpp_info.filenames["cmake_find_package"] = "valijson" # TBA: There's no installed config file
         # self.cpp_info.filenames["cmake_find_package_multi"] = "valijson" # TBA: There's no installed config file
-        self.cpp_info.names["cmake_find_package"] = "ValiJSON"
-        self.cpp_info.names["cmake_find_package_multi"] = "ValiJSON"
+        self.cpp_info.names["cmake_find_package"] = "valijson"
+        self.cpp_info.names["cmake_find_package_multi"] = "valijson"
         self.cpp_info.components["libvalijson"].names["cmake_find_package"] = "valijson"
         self.cpp_info.components["libvalijson"].names["cmake_find_package_multi"] = "valijson"
         self.cpp_info.components["libvalijson"].set_property("cmake_target_name", "ValiJSON::valijson")
