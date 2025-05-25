7994443172981ac76015b7eaa10c00cb1f4b1ede
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-05-25 23:04:09 +0200

    Cleanups

--- recipes/pmp/all/conanfile.py
+++ recipes/pmp/all/conanfile.py
@@ -1,7 +1,9 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-from conan.tools.files import collect_libs, get, copy
+from conan.tools.files import collect_libs, get, copy, rmdir
 import os
+
+required_conan_version = ">=2.1"
 
 class PmpConan(ConanFile):
     name = "pmp"
@@ -9,45 +11,44 @@ class PmpConan(ConanFile):
     url = "https://github.com/pmp-library/pmp-library"
     description = "PMP library - a library for processing polygon meshes."
     topics = ("geometry", "mesh processing", "3D", "polygon mesh")
-    package_type = "library" 
+    package_type = "library"
 
     # Binary configuration
     settings = "os", "arch", "compiler", "build_type"
     options = {
-    "shared": [True, False],
-    "examples": [True, False],
-    "tests": [True, False],
-    "docs": [True, False],
-    "vis": [True, False],
-    "regressions": [True, False]
+        "shared": [True, False],
+        "fPIC": [True, False],
     }
     default_options = {
-    "shared": False,
-    "examples": False,
-    "tests": False,
-    "docs": False,
-    "vis": False,
-    "regressions": False    
+        "shared": False,
+        "fPIC": True,
     }
-    
+
+    implements = ["auto_shared_fpic"]
+
     def requirements(self):
-        self.requires("eigen/[>=3.4 <4]", transitive_headers=True)
-    
+        self.requires("eigen/3.4.0", transitive_headers=True)
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.16 <4]")
+
     def layout(self):
         cmake_layout(self, src_folder="src")
-        
+
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)  
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
-        tc.variables["PMP_BUILD_EXAMPLES"] = self.options.examples
-        tc.variables["PMP_BUILD_TESTS"] = self.options.tests
-        tc.variables["PMP_BUILD_DOCS"] = self.options.docs
-        tc.variables["PMP_BUILD_VIS"] = self.options.vis
-        tc.variables["PMP_BUILD_REGRESSIONS"] = self.options.regressions
+        # TODO: Check if VIS tools would be useful in the future
+        tc.cache_variables["PMP_BUILD_VIS"] = False
+        tc.cache_variables["PMP_BUILD_EXAMPLES"] = False
+        tc.cache_variables["PMP_BUILD_TESTS"] = False
+        tc.cache_variables["PMP_BUILD_DOCS"] = False
+        tc.cache_variables["PMP_BUILD_REGRESSIONS"] = False
+        tc.cache_variables["PMP_STRICT_COMPILATION"] = False
         tc.generate()
 
     def build(self):
@@ -57,16 +58,16 @@ class PmpConan(ConanFile):
 
     def package(self):
         copy(self, "LICENSE.txt",
-        dst=os.path.join(self.package_folder, "licenses"),
-        src=self.source_folder)
-    
+            dst=os.path.join(self.package_folder, "licenses"),
+            src=self.source_folder)
+
         cmake = CMake(self)
         cmake.install()
-        
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "pmp")
         self.cpp_info.set_property("cmake_target_name", "pmp::pmp")
         self.cpp_info.set_property("pkg_config_name", "pmp")
-        self.cpp_info.libs = collect_libs(self)
-        self.cpp_info.bindirs = ["bin"]              
+        self.cpp_info.libs = ["pmp"]
         self.cpp_info.requires = ["eigen::eigen"]
