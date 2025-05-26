1b4e13b0f52edfbfff2933b52f6b41e4daa8fc61
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-05-26 12:21:17 +0200

    Fix test_package

--- recipes/pmp/all/test_package/conanfile.py
+++ recipes/pmp/all/test_package/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
 from conan.errors import ConanException
 import os
@@ -19,7 +20,6 @@ class TestPMPConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-        if not os.path.isfile(bin_path):
-            raise ConanException(f"Executable not found: {bin_path}")
-        self.run(bin_path, env="conanrun")
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
