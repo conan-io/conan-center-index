9e08e7cf8f3b435cff02de7f542c839bc8a036ad
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-05-27 10:36:12 +0200

    Update recipes/soci/all/conanfile.py

--- recipes/soci/all/conanfile.py
+++ recipes/soci/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, replace_in_file
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
