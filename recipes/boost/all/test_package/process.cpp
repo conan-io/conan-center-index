4f88ed5823c8e892c9594bedb88d680e245404c6
Uilian Ries <uilianries@gmail.com>
2025-05-30 08:00:06 +0200

    Simplify boost process test
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/boost/all/test_package/process.cpp
+++ recipes/boost/all/test_package/process.cpp
@@ -1,15 +1,9 @@
 #include <boost/version.hpp>
+#include <boost/process.hpp>
 
 #define BOOST_PROCESS_V2_DEFAULT (BOOST_VERSION / 100 % 1000 >= 88)
 
-#if BOOST_PROCESS_V2_DEFAULT
-#include <boost/process/v2.hpp>
-#else
-#include <boost/process.hpp>
-#endif
-
-int main()
-{
+int main() {
 #if BOOST_PROCESS_V2_DEFAULT
     boost::process::v2::shell().empty();
 #else
