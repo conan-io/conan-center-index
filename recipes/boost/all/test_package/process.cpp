b0d1579674db9112b5fd3525dc662d7a1483c4af
Uilian Ries <uilianries@gmail.com>
2025-05-30 10:54:20 +0200

    Fix Windows build for boost process
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/boost/all/test_package/process.cpp
+++ recipes/boost/all/test_package/process.cpp
@@ -1,13 +1,24 @@
 #include <boost/version.hpp>
-#include <boost/process.hpp>
 
 #define BOOST_PROCESS_V2_DEFAULT (BOOST_VERSION / 100 % 1000 >= 88)
 
+#ifdef WIN32
+// Workaround for https://github.com/boostorg/process/issues/429
+// boost/asio/detail/socket_types.hpp(24): fatal error C1189: #error:  WinSock.h has already been included
+#include <boost/asio/read.hpp>
+#endif
+
+#if BOOST_PROCESS_V2_DEFAULT
+#include <boost/process.hpp>
+#else
+#include <boost/process/v2.hpp>
+#endif
+
 int main() {
 #if BOOST_PROCESS_V2_DEFAULT
-    boost::process::v2::shell().empty();
-#else
     boost::process::shell().empty();
+#else
+    boost::process::v2::shell().empty();
 #endif
     return 0;
 }
