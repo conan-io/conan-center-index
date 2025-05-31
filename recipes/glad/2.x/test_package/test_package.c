5a08517a49fe24f8e5e4ca54d231971e6ca9328b
Andrew Marshall <planetmarshalluk@gmail.com>
2025-05-31 10:56:21 +0100

    fix test package for macos

--- recipes/glad/2.x/test_package/test_package.c
+++ recipes/glad/2.x/test_package/test_package.c
@@ -1,11 +1,24 @@
-#include "glad/gl.h"
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 
 #include <stdio.h>
 
 int main() {
+    GLFWwindow* window;
+
+    #ifdef __APPLE__
+    if (!glfwInit()) {
+        return -1;
+    }
+    #endif
+
+    window = glfwCreateWindow(1, 1, "Hello World", NULL, NULL);
+
     int version = gladLoaderLoadGL();
     printf("GL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
     gladLoaderUnloadGL();
+
+    glfwTerminate();
 
     return 0;
 }
