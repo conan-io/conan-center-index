defb48965982af39a0ab833c70703ac10383f2cb
Hubert <trebuh.g@gmail.com>
2025-06-03 21:42:30 +0200

    Fix CMake version misalignment. Fix linter warnings.

--- recipes/ctd_cli/all/conanfile.py
+++ recipes/ctd_cli/all/conanfile.py
@@ -1,8 +1,8 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, rmdir
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 import os
 
