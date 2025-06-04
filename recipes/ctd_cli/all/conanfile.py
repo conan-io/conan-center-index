1284ebc2cdfb76438745354fcd8fba4e3d85ff90
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-06-04 21:07:13 +0200

    Cleanup, validate msvc out, use upstream target names

--- recipes/ctd_cli/all/conanfile.py
+++ recipes/ctd_cli/all/conanfile.py
@@ -2,11 +2,11 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rmdir
-from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
+from conan.tools.files import copy, get, replace_in_file
 import os
 
-required_conan_version = ">=2.0.9"
+
+required_conan_version = ">=2"
 
 class PackageConan(ConanFile):
     name = "ctd_cli"
@@ -27,45 +27,23 @@ class PackageConan(ConanFile):
     }
     implements = ["auto_shared_fpic"]
 
-    @property
-    def _min_cppstd(self):
-        return 20
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "gcc": "12.2",
-            "clang": "14",
-            "apple-clang": "13.1",
-            "Visual Studio": "17",
-            "msvc": "193",
-        }
-
     def layout(self):
         cmake_layout(self, src_folder="src")
 
-    def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
-        def loose_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
+    def validate_build(self):
+        if self.settings.compiler == "msvc":
+            raise ConanInvalidConfiguration("The library does not support building on MSVC")
 
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
-            raise ConanInvalidConfiguration(
-                f"{self.name} {self.version} requires C++{self._min_cppstd}, which your compiler does not support.",
-            )
+    def validate(self):
+        check_min_cppstd(self, 20)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "set(CMAKE_CXX_STANDARD 20)", "")
 
     def generate(self):
         tc = CMakeToolchain(self)
-        if is_msvc(self):
-            tc.cache_variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
         tc.generate()
 
     def build(self):
@@ -77,10 +55,8 @@ class PackageConan(ConanFile):
         copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
-        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
-        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
-        rmdir(self, os.path.join(self.package_folder, "res"))
-        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.libs = ["ctd_cli"]
+        self.cpp_info.set_property("cmake_file_name", "ctd_cli")
+        self.cpp_info.set_property("cmake_target_name", "korgorusze::CLI")
