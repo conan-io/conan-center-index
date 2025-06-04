3c3df1dfc640e47c12a0606e98cb2d5380405a98
Andrew Marshall <planetmarshalluk@gmail.com>
2025-06-04 16:51:11 +0100

    simplify test package

--- recipes/glad/2.x/test_package/test_package.c
+++ recipes/glad/2.x/test_package/test_package.c
@@ -1,24 +1,9 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
-#include <stdio.h>
-
 int main() {
-    GLFWwindow* window;
 
-    #ifdef __APPLE__
-    if (!glfwInit()) {
-        return -1;
-    }
-    #endif
-
-    window = glfwCreateWindow(1, 1, "Hello World", NULL, NULL);
-
-    int version = gladLoaderLoadGL();
-    printf("GL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
-    gladLoaderUnloadGL();
-
-    glfwTerminate();
+    gladLoadGL(glfwGetProcAddress);
 
     return 0;
 }
