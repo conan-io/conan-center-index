016e51b051c1ecd0559f7ab829ef27333bebc62a
Ivet Galabova <galabovaa@gmail.com>
2025-06-06 13:54:11 +0300

    source dir

--- recipes/highs/all/conanfile.py
+++ recipes/highs/all/conanfile.py
@@ -34,7 +34,7 @@ class HiGHSConan(ConanFile):
             self.options.rm_safe("fPIC")
 
     def layout(self):
-        cmake_layout(self, src_folder="highs")
+        cmake_layout(self, src_folder="src")
 
     def requirements(self):
         self.requires("zlib/[>=1.2.11 <2]")
