40e2856af134ea2fc800bd7d5927d02677904ea9
Uilian Ries <uilianries@gmail.com>
2025-06-06 08:46:24 +0200

    Package missing license file
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/ouster_sdk/all/conanfile.py
+++ recipes/ouster_sdk/all/conanfile.py
@@ -136,7 +136,7 @@ class OusterSdkConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, pattern="LICENSE*", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
