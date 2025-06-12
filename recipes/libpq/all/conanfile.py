dcd13c716c7a271005195da9be2744ecf803fa5c
Uilian Ries <uilianries@gmail.com>
2025-06-12 11:55:47 +0200

    Revert non related  changes
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libpq/all/conanfile.py
+++ recipes/libpq/all/conanfile.py
@@ -306,7 +306,7 @@ class LibpqConan(ConanFile):
             self.cpp_info.components["pq"].system_libs = ["pthread"]
             self.cpp_info.components["pgcommon"].system_libs = ["m"]
         elif self.settings.os == "Windows":
-            self.cpp_info.components["pq"].system_libs = ["ws2_32"]
+            self.cpp_info.components["pq"].system_libs = ["ws2_32", "secur32", "advapi32", "shell32", "crypt32", "wldap32"]
 
         self.cpp_info.names["cmake_find_package"] = "PostgreSQL"
         self.cpp_info.names["cmake_find_package_multi"] = "PostgreSQL"
