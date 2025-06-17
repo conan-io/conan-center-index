dc07193b6fb5cb3c4acd94bac8908be46226e580
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-06-17 13:58:13 +0200

    use old test package

--- recipes/gsl-lite/old/test_package/test_package.cpp
+++ recipes/gsl-lite/old/test_package/test_package.cpp
@@ -1,22 +1,9 @@
-
 #include <iostream>
-
 #include <gsl/gsl-lite.hpp>
 
-void printCmdArgs( gsl::span<gsl::zstring const> cmdArgs )
+int main()
 {
-    Expects( !cmdArgs.empty() );
-
-    auto argsWithoutExeName = cmdArgs.subspan( 1 );
-    for ( auto arg : argsWithoutExeName )
-    {
-        std::cout << arg << "\n";
-    }
-}
-
-int main( int argc, char* argv[] )
-{
-    auto numArgs = gsl::narrow<std::size_t>( argc );
-    auto cmdArgs = gsl::make_span( argv, numArgs );
-    printCmdArgs( cmdArgs );
+  char const s[] = "Hello, World!";
+  gsl::span<char const> sp(s) ;
+  std::cout << sp << std::endl;
 }
