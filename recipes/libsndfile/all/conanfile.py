0582710f9d3495e76d26b9ef8a00f28e18ad7b44
Martin Delille <martin@delille.org>
2025-06-23 18:39:00 +0200

    Remove conan 1 support

--- recipes/libsndfile/all/conanfile.py
+++ recipes/libsndfile/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.54.0"
+required_conan_version = ">=2.1.0"
 
 
 class LibsndfileConan(ConanFile):
