ba5579d9f2742a89f972eeb588221fedb13e224a
Mike Carney <mike@carneyweb.com>
2025-06-25 16:49:49 -0400

    got working.

--- recipes/cppmicroservices/3.x.x/conanfile.py
+++ recipes/cppmicroservices/3.x.x/conanfile.py
@@ -27,36 +27,40 @@ class CppMicroServicesConan(ConanFile):
     def source(self):
         # we recover the saved url and commit from conandata.yml and use them to get sources
         git = Git(self)
-        git.clone(url=self.conan_data["sources"][self.version]["url"], args=["--recurse-submodules"], hide_url=False)
+        target = os.path.join(self.source_folder, "target")
+        git.folder = target
+        git.clone(url=self.conan_data["sources"][self.version]["url"], args=["--recurse-submodules"], target=target)
         git.checkout(commit=self.conan_data["sources"][self.version]["sha1"])
 #        replace_in_file(self, os.path.join(self.target, "third_party", "boost", "nowide", "include", "nowide", "detail", "convert.hpp"),
 #                        "::template ", "::")
 
-    # def config_options(self):
-    #     if self.settings.os == "Windows":
-    #         self.options.rm_safe("fPIC")
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
 
-    # def configure(self):
-    #     os.environ["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
-    #     if self.options.shared:
-    #         self.options.rm_safe("fPIC")
+    def configure(self):
+        os.environ["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
 
-    # def generate(self):
-    #     deps = CMakeDeps(self)
-    #     deps.generate()
-    #     tc = CMakeToolchain(self)
-    #     tc.generate()
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
 
-    # def build(self):
-    #     cmake = CMake(self)
-    #     cmake.configure(variables={"US_BUILD_TESTING":"On"},
-    #                     build_script_folder=self.target)
-    #     cmake.build()
-    #     cmake.test()
+    def build(self):
+        print(f"package_folder = {self.package_folder}")
+        cmake = CMake(self)
+        target = os.path.join(self.source_folder, "target")
+        cmake.configure(variables={"US_BUILD_TESTING":"Off"},
+                         build_script_folder=target)
+        cmake.build()
+#         cmake.test()
 
-    # def package(self):
-    #     cmake = CMake(self)
-    #     cmake.install()
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
 
     # def package_info(self):
     #     self.cpp_info.libs = ["cppmicroservices"]
