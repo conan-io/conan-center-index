cc8765ea7dc31d87c1c2f64cb81405d6bbd55a0e
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2025-06-26 13:05:32 +0200

    release decoder

--- recipes/libultrahdr/all/test_package/test_package.cpp
+++ recipes/libultrahdr/all/test_package/test_package.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
-    std::cout << "Creating new decoder " << uhdr_create_decoder() << "\n";
+    uhdr_codec_private_t* decoder = uhdr_create_decoder();
+    std::cout << "Creating new decoder " << decoder << "\n";
+    std::cout << "Releasing decoder " << "\n";
+    uhdr_release_decoder(decoder);
+
     return 0;
 }
