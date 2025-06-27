8acaa680f10bea72368e1de83f21d2c73cdf1bca
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-06-27 19:01:09 +0100

    dcmtk: prevent inadvertently linking against nsl, use libxml from cmake config

--- recipes/dcmtk/all/conanfile.py
+++ recipes/dcmtk/all/conanfile.py
@@ -167,6 +167,10 @@ class DCMTKConan(ConanFile):
             tc.variables["DCMTK_ICONV_FLAGS_ANALYZED"] = True
             tc.variables["DCMTK_COMPILE_WIN32_MULTITHREADED_DLL"] = not is_msvc_static_runtime(self)
 
+        # Prevent libnsl from the system (and not from Conan) from being picked up
+        tc.cache_variables["HAVE_LIBNSL_MAIN"] = False
+        tc.cache_variable["HAVE_LIBNSL"] = False
+
         if Version(self.version) >= "3.6.7" and cross_building(self):
             # See https://support.dcmtk.org/redmine/projects/dcmtk/wiki/Cross_Compiling
             tc.cache_variables["DCMTK_NO_TRY_RUN"] = True
@@ -383,7 +387,7 @@ class DCMTKConan(ConanFile):
         if self.settings.os == "Windows":
             system_libs = ["iphlpapi", "ws2_32", "netapi32", "wsock32"]
         elif self.settings.os in ["Linux", "FreeBSD"]:
-            system_libs = ["m", "nsl"]
+            system_libs = ["m"]
             if self.options.with_multithreading:
                 system_libs.append("pthread")
             if Version(self.version) >= "3.6.8":
