1d493852b0a7e427ed34da0d117671d531849a6d
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-07-01 16:23:50 +0100

    libpq 17.5: fix issue with flex/m4 on Linux

--- recipes/libpq/meson/conanfile.py
+++ recipes/libpq/meson/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.files import copy, get, rm, rmdir
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.meson import Meson, MesonToolchain
@@ -94,8 +94,12 @@ class LibpqConan(ConanFile):
             self.tool_requires("flex/2.6.4")
             self.tool_requires("bison/3.8.2")
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         def feature(v):
