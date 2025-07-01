5cbecf26b3805292aa66d51ad8d71f8fd59afb30
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-07-01 13:17:03 +0200

    Conan 2 changes

--- recipes/ipfs_client/all/conanfile.py
+++ recipes/ipfs_client/all/conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.env import VirtualBuildEnv
 import os
 
 
-required_conan_version = ">=1.63"
+required_conan_version = ">=2"
 
 
 class IpfsChromium(ConanFile):
@@ -18,7 +18,7 @@ class IpfsChromium(ConanFile):
     topics = ("ipfs", "ipns", "dweb", "web", "content-addressed", "network", "client", "io", "api", "file-sharing", "gateway", "kubo")
     license = ("MIT", "Apache-2.0")
     url = 'https://github.com/conan-io/conan-center-index'
-    settings = ["os", "compiler", "build_type", "arch"]
+    settings = "os", "compiler", "build_type", "arch"
     package_type = 'static-library'
 
     @property
@@ -42,14 +42,13 @@ class IpfsChromium(ConanFile):
         self.requires("boost/1.85.0", transitive_headers=True, transitive_libs=True)
         self.requires("openssl/[>=1.1 <4]")
         self.requires("bzip2/1.0.8")
-        self.requires("c-ares/1.33.1")
+        self.requires("c-ares/[>=1.27 <2]")
         self.requires("nlohmann_json/3.11.3")
         self.requires("abseil/20240116.2")
         self.requires("protobuf/3.21.12", transitive_headers=True)
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, self._min_cppstd)
+        check_min_cppstd(self, self._min_cppstd)
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if minimum_version and Version(self.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
@@ -62,8 +61,6 @@ class IpfsChromium(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        env = VirtualBuildEnv(self)
-        env.generate()
         tc = CMakeToolchain(self)
         tc.cache_variables["INSIDE_CONAN"] = True
         cppstd = self.settings.get_safe("compiler.cppstd", self._min_cppstd).replace("gnu", "")
