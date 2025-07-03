2f02fc4e8dc1a018ecd3a1ecc8941a9c59f3c129
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-07-03 16:16:10 +0200

    Remove checks

--- recipes/aws-c-cal/all/test_package/conanfile.py
+++ recipes/aws-c-cal/all/test_package/conanfile.py
@@ -1,8 +1,6 @@
 from conan import ConanFile
-from conan.tools.apple import is_apple_os
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.scm import Version
 import os
 import io
 
@@ -11,10 +9,6 @@ class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
     test_type = "explicit"
-
-    @property
-    def _needs_openssl(self):
-        return not (self.settings.os == "Windows" or is_apple_os(self))
 
     def layout(self):
         cmake_layout(self)
@@ -32,9 +26,3 @@ class TestPackageConan(ConanFile):
             stream = io.StringIO()
             bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
             self.run(bin_path, stream, env="conanrun")
-            self.output.info(stream.getvalue())
-            if self._needs_openssl:
-                if Version(self.version) < "0.9.1":
-                    assert "found static libcrypto" in stream.getvalue()
-                else:
-                    assert "found weak ref libcrypto" in stream.getvalue()
