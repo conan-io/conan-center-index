43c54a7625807af6016d1a67c75bcfa62c35f46b
fischjo3SICKAG <35769851+fischjo3SICKAG@users.noreply.github.com>
2025-07-10 08:40:59 +0200

    Set UA_ENABLE_NODESETLOADER=False explicitly for non-Linux platforms

--- recipes/open62541/all/conanfile.py
+++ recipes/open62541/all/conanfile.py
@@ -348,7 +348,11 @@ class Open62541Conan(ConanFile):
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
 
         if version >= "1.4.11.1":
-            tc.cache_variables["UA_ENABLE_NODESETLOADER"] = self.options.get_safe("nodeset_loader", False)
+            if self.settings.os == "Linux":
+                tc.cache_variables["UA_ENABLE_NODESETLOADER"] = self.options.get_safe("nodeset_loader", False)
+            else:
+                tc.cache_variables["UA_ENABLE_NODESETLOADER"] = False
+
         tc.generate()
         tc = CMakeDeps(self)
         tc.set_property("mbedtls", "cmake_additional_variables_prefixes", ["MBEDTLS"])
