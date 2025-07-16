184686b535270ee9cdc3fb28f0d8c17b8f07046a
Andi Skrgat <andi8647@gmail.com>
2025-07-16 12:31:50 +0200

    Update to files

--- recipes/librdtsc/all/conanfile.py
+++ recipes/librdtsc/all/conanfile.py
@@ -1,17 +1,18 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
+from conan.tools.build import check_min_cppstd
+
 
 import os
 
 
 class Librdtsc(ConanFile):
     name = "librdtsc"
-    version = "v0.3"
     description = "A simple multi-platform library for reading TSC values on x86-64 and ARM architectures"
     license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/gabrieleara/librdtsc.git"
+    homepage = "https://github.com/gabrieleara/librdtsc"
     topics = ("librdtsc", "cpu")
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
@@ -20,14 +21,12 @@ class Librdtsc(ConanFile):
             "fPIC": [True, False],
             "use_hpet": [True, False],
             "use_pmu": [True, False],
-            "arch": ["x86", "arm", "arm64"]
             }
     default_options = {
             "shared": False,
             "fPIC": True,
             "use_hpet": False,
             "use_pmu": False,
-            "arch": "x86"
             }
 
     def export_sources(self):
@@ -39,20 +38,25 @@ class Librdtsc(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            self.options.rm_safe("fPIC")
+            del self.options.fPIC
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
+
+    def validate(self):
+        check_min_cppstd(self, 20)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["LIBRDTSC_OS_LINUX"] = True
-        tc.variables["LIBRDTSC_ARCH_X86"] = self.options.arch == "x86"
-        tc.variables["LIBRDTSC_ARCH_ARM"] = self.options.arch == "arm"
-        tc.variables["LIBRDTSC_ARCH_ARM64"] = self.options.arch == "arm64"
+        tc.variables["LIBRDTSC_OS_LINUX"] = str(self.settings.os) == "Linux"
+        arch = str(self.settings.arch)
+        tc.variables["LIBRDTSC_ARCH_X86"] = arch in ("x86", "x86_64")
+        tc.variables["LIBRDTSC_ARCH_ARM"] = arch == "armv7"
+        tc.variables["LIBRDTSC_ARCH_ARM64"] = arch == "armv8"
         tc.variables["LIBRDTSC_USE_HPET"] = self.options.use_hpet
         tc.variables["LIBRDTSC_USE_PMU"] = self.options.use_pmu
         tc.generate()
@@ -60,7 +64,6 @@ class Librdtsc(ConanFile):
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -71,7 +74,5 @@ class Librdtsc(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.set_property("cmake_file_name", "librdtsc")
-        self.cpp_info.set_property("cmake_target_name", "librdtsc::librdtsc")
         self.cpp_info.libs = ["rdtsc"]
 
