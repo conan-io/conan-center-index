da728c6351ad69ec9319a9455e8e97375c991df5
PerseoGI <perseog@jfrog.com>
2025-07-18 10:23:21 +0200

    Removed network access on test_package

--- recipes/fast-dds/all/conanfile.py
+++ recipes/fast-dds/all/conanfile.py
@@ -81,6 +81,7 @@ class FastDDSConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -95,7 +96,6 @@ class FastDDSConan(ConanFile):
         tc.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
