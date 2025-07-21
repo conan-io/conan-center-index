81018e03e225b9c7ce919aea001adeb61afa0bf5
Ayaz Salikhov <asalikhov@ripple.com>
2025-07-21 13:25:49 +0100

    libarchive: fix cmake 4 support

--- recipes/libarchive/all/conanfile.py
+++ recipes/libarchive/all/conanfile.py
@@ -132,6 +132,9 @@ class LibarchiveConan(ConanFile):
         cmake_deps = CMakeDeps(self)
         cmake_deps.generate()
         tc = CMakeToolchain(self)
+        # CMake 4 support
+        if Version(self.version) < "3.7.9":
+            tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
         # turn off deps to avoid picking up them accidentally
         tc.variables["ENABLE_NETTLE"] = self.options.with_nettle
         tc.variables["ENABLE_OPENSSL"] = self.options.with_openssl
