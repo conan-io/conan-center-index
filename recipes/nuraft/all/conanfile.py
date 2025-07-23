a07271fa84bf708442b4d02d57ba249567d58c91
Andi Skrgat <andi8647@gmail.com>
2025-07-23 19:33:46 +0200

    Remove changes nuraft

--- recipes/nuraft/all/conanfile.py
+++ recipes/nuraft/all/conanfile.py
@@ -1,95 +1,83 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
+from conan.tools.scm import Git
 import os
 
-from conan import ConanFile
-from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain, CMakeDeps
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir
-from conan.tools.scm import Git
+required_conan_version = ">=1.53.0"
 
-required_conan_version = ">=2.12.2"
 
 class NuRaftConan(ConanFile):
-    # Metadata
     name = "nuraft"
-    package_type = "library"
+    homepage = "https://github.com/eBay/NuRaft"
+    description = """Cornerstone based RAFT library."""
+    topics = ("raft",)
+    url = "https://github.com/conan-io/conan-center-index"
     license = "Apache-2.0"
-    description = "RAFT protocol library."
-    homepage = "https://github.com/ebay/NuRaft.git"
 
-    # generators = "CMakeDeps"
-
-    settings = "os", "compiler", "build_type", "arch"
-
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
     options = {
-        "shared": [True, False], 
-        "fPIC": [True, False],                
-        "coverage": [True, False], 
-        "boost_asio":[True, False],
-        "build_tests": [True, False],
-        "build_examples": [True, False]
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "boost_asio": [True, False],
     }
     default_options = {
-        "shared": False, 
-        "fPIC": True, 
-        "coverage": False, 
-        "boost_asio": True, 
-        "build_tests": True, 
-        "build_examples":True
+        "shared": False,
+        "fPIC": True,
+        "boost_asio": True,
     }
 
-    def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def configure(self):
         if self.options.shared:
             self.options.rm_safe("fPIC")
 
-    def requirements(self):
-        if self.options.boost_asio:
-            self.requires("boost/[>=1.8.0]")
-        else:
-            self.requires("asio/[>=1.22.0]")
-
-        self.requires("openssl/[~3]")
-
     def layout(self):
-        cmake_layout(self, generator="CMakeDeps")
-        self.cpp.package.libs = [f"lib{self.name}.so" if self.options.shared else f"lib{self.name}.a"]
+        cmake_layout(self, src_folder="src")
 
-        # For “editable” packages, self.cpp.source describes the artifacts under self.source_folder.
-        self.cpp.source.includedirs = ["include", "include/libnuraft"]
+    def requirements(self):
+        self.requires("openssl/[>=1.1 <4]")
+        if self.options.boost_asio:
+            self.requires("boost/1.81.0")
+        else:
+            self.requires("asio/1.27.0")
 
-        hash = Git(self).get_commit()
-        self.cpp.package.defines = self.cpp.build.defines = ["_RAFT_COMMIT_HASH=%s" % hash]
+    def validate(self):
+        if self.settings.os == "Windows":
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support Windows")
+        if self.settings.os == "Macos" and self.options.shared:
+            raise ConanInvalidConfiguration(f"{self.ref} shared not supported for Macos")
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 11)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["WITH_CONAN"] = True
-        tc.variables["CONAN_BUILD_COVERAGE"] = False
-
-        tc.variables["CODE_COVERAGE"] = self.options.coverage
         tc.variables["BOOST_ASIO"] = self.options.boost_asio
-        tc.variables["BUILD_TESTING"] = self.options.build_tests
-        tc.variables["BUILD_EXAMPLES"] = self.options.build_examples
-        tc.variables["ENABLE_RAFT_STATS"] = True
-
-
-        tc.variables["CMAKE_EXPORT_COMPILE_COMMANDS"] = True
         tc.generate()
-
-        deps=CMakeDeps(self)
-        # deps.build_context_activated = ["boost", "openssl"]
+        deps = CMakeDeps(self)
         deps.generate()
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-        if self.options.build_tests:
-            cmake.ctest()
-        copy(self, "compile_commands.json", self.build_folder, self.source_folder, keep_path=False)
-
 
     def package(self):
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
-        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+
+    def package_info(self):
+        self.cpp_info.libs = ["nuraft"]
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.extend(["m", "pthread"])
