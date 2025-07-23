b94f13eee342f4f012d11cea94541fd8e9c3fcae
Andi Skrgat <andi8647@gmail.com>
2025-07-23 19:34:33 +0200

    Remove import

--- recipes/nuraft/all/conanfile.py
+++ recipes/nuraft/all/conanfile.py
@@ -3,7 +3,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
-from conan.tools.scm import Git
 import os
 
 required_conan_version = ">=1.53.0"
