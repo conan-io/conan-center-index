98ee7ae1ca180cfd708490b1229c387a106fb94f
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-07-23 11:44:01 +0200

    Expose legacy variables following upstream config file

--- recipes/eigen/all/test_package/conanfile.py
+++ recipes/eigen/all/test_package/conanfile.py
@@ -4,6 +4,7 @@ from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
 
+
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
