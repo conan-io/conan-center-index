2a4f3cd556262cdf2a2caa4973c8875916895b6e
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2025-07-24 14:19:41 +0200

    also add Ws2_32

--- recipes/qpid-proton/all/conanfile.py
+++ recipes/qpid-proton/all/conanfile.py
@@ -124,14 +124,14 @@ class QpidProtonConan(ConanFile):
         self.cpp_info.components["proton"].set_property("cmake_target_name", "Proton::proton")
         self.cpp_info.components["proton"].libs = [f"qpid-proton{suffix}"]
         if is_msvc(self):
-            self.cpp_info.components["proton"].system_libs.extend(["secur32", "crypt32"])
+            self.cpp_info.components["proton"].system_libs.extend(["secur32", "crypt32", "Ws2_32"])
         else:
             self.cpp_info.components["proton"].requires = ["openssl::ssl"]
 
         self.cpp_info.components["core"].set_property("cmake_target_name", "Proton::core")
         self.cpp_info.components["core"].libs = [f"qpid-proton-core{suffix}"]
         if is_msvc(self):
-            self.cpp_info.components["core"].system_libs.extend(["secur32", "crypt32"])
+            self.cpp_info.components["core"].system_libs.extend(["secur32", "crypt32", "Ws2_32"])
         else:
             self.cpp_info.components["core"].requires = ["openssl::ssl"]
 
@@ -139,7 +139,7 @@ class QpidProtonConan(ConanFile):
         self.cpp_info.components["proactor"].libs = [f"qpid-proton-proactor{suffix}"]
         self.cpp_info.components["proactor"].requires = ["core"]
         if is_msvc(self):
-            self.cpp_info.components["proactor"].system_libs.extend(["secur32", "crypt32"])
+            self.cpp_info.components["proactor"].system_libs.extend(["secur32", "crypt32", "Ws2_32"])
         else:
             self.cpp_info.components["proactor"].requires = ["openssl::ssl"]
 
