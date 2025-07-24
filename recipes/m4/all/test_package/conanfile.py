298b4cdd7b88dec95cbf8ea43cb030ee15a72019
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-07-24 13:53:39 +0100

    fix

--- recipes/m4/all/test_package/conanfile.py
+++ recipes/m4/all/test_package/conanfile.py
@@ -7,9 +7,9 @@ class TestPackageConan(ConanFile):
     test_type = "explicit"
 
     def requirements(self):
-        self.requirements(self.tested_reference_str)
+        self.requires(self.tested_reference_str)
 
     def test(self):
         if can_run(self):
             extension = ".exe" if self.settings.os == "Windows" else ""
-            self.run(f"m4{extension} --version")
+            self.run(f"m4{extension} --version", env="conanrun")
