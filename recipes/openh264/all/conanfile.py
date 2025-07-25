8f6b9b700b91d58d8e6eeee1b46d9e899c669534
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-07-25 12:15:55 +0100

    fixes

--- recipes/openh264/all/conanfile.py
+++ recipes/openh264/all/conanfile.py
@@ -52,8 +52,8 @@ class OpenH264Conan(ConanFile):
         if self.settings.arch in ["x86", "x86_64"]:
             self.tool_requires("nasm/2.16.01")
         if is_msvc(self) and self.settings.arch == "armv8":
-            self.tool_requires("strawberryperl/5.32.1.1")
-            self.tool_requires("gas-preprocessor/0.0.0.cci20250710")
+            self.tool_requires("strawberryperl/[*]")
+            self.tool_requires("gas-preprocessor/[*]")
 
     def validate(self):
         if Version(self.version) <= "2.1.1" and self.settings.os in ["Android", "Macos"]:
