ecaa6c881e80f960181f73c91bd0d90d606a02cf
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-07-29 12:05:36 +0100

    xlsxio version range for minizip-ng

--- recipes/xlsxio/all/conanfile.py
+++ recipes/xlsxio/all/conanfile.py
@@ -59,7 +59,7 @@ class XlsxioConan(ConanFile):
         if self.options.with_libzip:
             self.requires("libzip/1.10.1")
         elif Version(self.version) >= "0.2.34" and self.options.with_minizip_ng :
-            self.requires("minizip-ng/4.0.1")
+            self.requires("minizip-ng/[>=4.0.1 <5]")
         else:
             self.requires("minizip/1.2.13")
         self.requires("expat/[>=2.6.2 <3]")
