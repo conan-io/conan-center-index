689881f82c2c1efb2891ded3489677528fe08e91
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-07-30 09:43:17 +0100

    libx265: single version, version range

--- recipes/libx265/all/conanfile.py
+++ recipes/libx265/all/conanfile.py
@@ -1,12 +1,10 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.apple import is_apple_os
 from conan.tools.build import cross_building, stdcpp_library
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rename, rm, rmdir
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2.1"
