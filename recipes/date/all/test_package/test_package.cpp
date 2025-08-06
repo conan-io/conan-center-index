5737c88ffcbd5ea2d6b92b57b03807d921780701
danimtb <danimanzaneque@gmail.com>
2025-08-06 10:49:06 +0200

    revert tp

--- recipes/date/all/test_package/test_package.cpp
+++ recipes/date/all/test_package/test_package.cpp
@@ -8,6 +8,10 @@ int main() {
     std::cout << "Conan was released on: " << conan_two << std::endl;
 
 #ifndef DATE_HEADER_ONLY
-    const date::time_zone* tz = date::current_zone();
+    try {
+        const date::time_zone* tz = date::current_zone();
+    }
+    catch (const std::exception & e) {
+    }
 #endif
 }
