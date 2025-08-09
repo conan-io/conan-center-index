1bc12599fc67acdd962def56530d5e4d329b57fd
Ingmar Rieger <git@irieger.net>
2025-08-10 01:04:04 +0200

    Remove unused import

--- recipes/imath/all/conanfile.py
+++ recipes/imath/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, collect_libs, copy, export_conandata_patches, get, rmdir
+from conan.tools.files import apply_conandata_patches, collect_libs, copy, get, rmdir
 from conan.tools.microsoft import is_msvc
 import os
 
