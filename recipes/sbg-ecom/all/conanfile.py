1a29dd250129377265c138a3ed4eddcd6451638c
Alexandre Petitjean <alexandre.petitjean@sbg-systems.com>
2025-08-12 13:14:38 +0200

    sbg-ecom: Update with PR remarks

--- recipes/sbg-ecom/all/conanfile.py
+++ recipes/sbg-ecom/all/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
-from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, rmdir, replace_in_file
 
 import os
 
@@ -11,27 +10,28 @@ required_conan_version = ">=2.1"
 class SbgEComConan(ConanFile):
     name = "sbgecom"
     license = "MIT"
-    author = "Alexandre Petitjean <alexandre.petitjean@sbg-systems.com>"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/SBG-Systems/sbgECom"
     description = "C library used to communicate with SBG Systems IMU, AHRS and INS"
     topics = ("sbg", "imu", "ahrs", "ins")
+    options = {"fPIC": [True, False]}
+    default_options = {"fPIC": True}
     
     package_type = "static-library"
     settings = "os", "arch", "compiler", "build_type"
 
-    def layout(self):
-        cmake_layout(self)
+    languages = "C"
     
-    def validate(self):
-        check_min_cppstd(self, 14)
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "set(CMAKE_POSITION_INDEPENDENT_CODE",
+                        "# set(CMAKE_POSITION_INDEPENDENT_CODE")
 
     def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
         tc = CMakeToolchain(self)
         tc.generate()
 
@@ -44,6 +44,12 @@ class SbgEComConan(ConanFile):
         cmake = CMake(self)
         cmake.install()
         copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.libs = ["sbgECom"]
+        self.cpp_info.set_property("cmake_file_name", "sbgECom")
+        self.cpp_info.set_property("cmake_target_name", "sbgECom::sbgECom")
+        self.cpp_info.defines = ["SBG_COMMON_STATIC_USE"]
+        if self.settings.compiler == "msvc":
+            self.cpp_info.system_libs = ["ws2_32"]
