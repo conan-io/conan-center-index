639e7fd30248101d5713c41ebbdc87287f6cef3a
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-08-18 11:06:26 +0100

    crossdb

--- recipes/crossdb/all/conanfile.py
+++ recipes/crossdb/all/conanfile.py
@@ -4,7 +4,6 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
 from conan.tools.microsoft import is_msvc
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2.1"
@@ -16,7 +15,9 @@ class CrossDBConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/crossdb-org/crossdb"
     topics = ("database", "oltp", "embedded")
-    package_type = "shared-library"
+    package_type = "library"
+    languages = "C"
+    implements = ["auto_shared_fpic"]
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -27,33 +28,18 @@ class CrossDBConan(ConanFile):
         "fPIC": True,
     }
 
-    def config_options(self):
-        # from 0.12.0, it is a static/shared library
-        if Version(self.version) < "0.12.0":
-            del self.options.shared
-        else:
-            self.package_type = "library"
-
-    def configure(self):
-        if self.options.get_safe("shared", True):
-            self.options.rm_safe("fPIC")
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
-
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
         if is_msvc(self):
-            raise ConanInvalidConfiguration(f"${self.ref} does not support MSVC")
+            raise ConanInvalidConfiguration(f"msvc is not supported")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        if Version(self.version) <= "0.11.0":
-            tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5" # CMake 4 support
         tc.generate()
 
     def build(self):
@@ -70,5 +56,5 @@ class CrossDBConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = ["crossdb"]
 
-        if Version(self.version) >= "0.10.0" and self.settings.os in ["Linux", "FreeBSD"]:
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.append("pthread")
