b8b2bedca47cf851e6470d1fe4aa9f2c97527a51
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-08-18 09:25:29 +0100

    remove unused import

--- recipes/pkgconf/all/conanfile.py
+++ recipes/pkgconf/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rename, rm, rmdir, replace_in_file
 from conan.tools.layout import basic_layout
 from conan.tools.meson import Meson, MesonToolchain
-from conan.tools.microsoft import is_msvc, unix_path_package_info_legacy
+from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 
 
