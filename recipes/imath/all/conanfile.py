8c9ff0a0bc8209d2870661fb7325fa428044fcfb
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-08-18 17:03:50 +0200

    Disable tests, dont use collect_libs

--- recipes/imath/all/conanfile.py
+++ recipes/imath/all/conanfile.py
@@ -1,8 +1,9 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, collect_libs, copy, get, rmdir
+from conan.tools.files import copy, get, rmdir
 from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2.0"
@@ -30,26 +31,20 @@ class ImathConan(ConanFile):
         "fPIC": True,
     }
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
+    implements = ["auto_shared_fpic"]
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, 11)
+        check_min_cppstd(self, 11)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
+        tc.cache_variables["BUILD_TESTING"] = False
         if is_msvc(self) and self.settings.compiler.get_safe("cppstd"):
             # when msvc is working with a C++ standard level higher
             # than the default, we need the __cplusplus macro to be correct
@@ -57,8 +52,6 @@ class ImathConan(ConanFile):
         tc.generate()
 
     def build(self):
-        apply_conandata_patches(self)
-
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -85,7 +78,8 @@ class ImathConan(ConanFile):
         imath_lib = self.cpp_info.components["imath_lib"]
         imath_lib.set_property("cmake_target_name", "Imath::Imath")
         imath_lib.set_property("pkg_config_name", "Imath")
-        imath_lib.libs = collect_libs(self)
+        version = Version(self.version)
+        imath_lib.libs = [f"Imath-{version.major}_{version.minor}"]
         imath_lib.requires = ["imath_config"]
         if self.settings.os == "Windows" and self.options.shared:
             imath_lib.defines.append("IMATH_DLL")
