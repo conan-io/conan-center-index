d474e2507e80a676df181e3f21543a87ad254c66
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-08-19 15:24:52 +0100

    b2: changes

--- recipes/b2/standard/test_package/conanfile.py
+++
@@ -1,9 +0,0 @@
-from conans import ConanFile, tools
-import os
-
-
-class TestPackgeConan(ConanFile):
-    settings = "os", "arch"
-
-    def test(self):
-        self.run("b2 -v", run_environment=True)
