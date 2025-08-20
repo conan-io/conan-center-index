2967effc93d6e289cd7b7792de399a1e2b06220e
Mickaël ADRIAO <mickael.adriao@sbg-systems.com>
2025-08-20 12:02:54 +0200

    update kiss-icp conanfile to match template

--- recipes/kiss-icp/all/conanfile.py
+++ recipes/kiss-icp/all/conanfile.py
@@ -1,29 +1,31 @@
 from conan import ConanFile
-from conan.errors import ConanException
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, apply_conandata_patches
+from conan.tools.files import apply_conandata_patches, copy, get, apply_conandata_patches
 import os
-import shutil
 
 required_conan_version = ">=2.1.0"
 
 
 class KissIcpConan(ConanFile):
     name = "kiss-icp"
+    description = "A LiDAR odometry pipeline that just works"
+    license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/PRBonn/kiss-icp"
     topics = ("robotics", "ros", "slam", "ros2", "3d-mapping", "lidar-slam")
-    description = "A LiDAR odometry pipeline that just works"
-    license = "MIT"
     package_type = "shared-library"
     settings = "os", "arch", "compiler", "build_type"
-    exports_sources = "patches/*.patch"
+    options = {
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "fPIC": True,
+    }
+    implements = ["auto_shared_fpic"]
 
-    def requirements(self):
-        self.requires("eigen/3.4.0", transitive_headers=True)
-        self.requires("sophus/1.22.10", transitive_headers=True)
-        self.requires("onetbb/2020.3.3", transitive_headers=True)
-        self.requires("tsl-robin-map/1.3.0", transitive_headers=True)
+    def export_sources(self):
+        apply_conandata_patches(self)
 
     def layout(self):
         cmake_layout(self)
@@ -31,6 +33,15 @@ class KissIcpConan(ConanFile):
         self.folders.source = "."
         self.folders.build = "build"
         self.folders.generators = self.folders.build
+
+    def requirements(self):
+        self.requires("eigen/3.4.0", transitive_headers=True)
+        self.requires("sophus/1.22.10", transitive_headers=True)
+        self.requires("onetbb/2020.3.3", transitive_headers=True)
+        self.requires("tsl-robin-map/1.3.0", transitive_headers=True)
+
+    def validate(self):
+        check_min_cppstd(self, 17)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version],
@@ -61,7 +72,6 @@ class KissIcpConan(ConanFile):
 
 
     def package_info(self):
-        suffix = "d" if self.settings.build_type == "Debug" else ""
         self.cpp_info.libs = ["kiss_icp_core", "kiss_icp_metrics", "kiss_icp_pipeline"]
         self.cpp_info.includedirs = ["include"]
         self.cpp_info.requires = ["eigen::eigen3", "sophus::sophus", "onetbb::onetbb", "tsl-robin-map::tsl-robin-map"]
