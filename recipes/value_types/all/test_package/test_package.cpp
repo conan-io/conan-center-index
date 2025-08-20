65854269270d08bfd4c8ff2f588ba0852276333c
Antony Peacock <ant.peacock@gmail.com>
2025-08-20 21:43:29 +0100

    Initial package for value types proposal

---
+++ recipes/value_types/all/test_package/test_package.cpp
@@ -0,0 +1,27 @@
+#include <indirect.h>
+#include <polymorphic.h>
+
+#include <cstdlib>
+
+class Base
+{
+public:
+    virtual ~Base() {};
+    virtual int getValue() const = 0;
+};
+
+class Derived : public Base
+{
+    int value;
+public:
+    Derived(int value_) : value(value_) {}
+    int getValue() const { return value; } 
+};
+
+int main()
+{
+    xyz::indirect initial(EXIT_SUCCESS);
+    int value = *initial;
+    xyz::polymorphic<Base> result(std::in_place_type<Derived>, value);
+    return result->getValue();
+}
