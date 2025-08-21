7444140a3adac7bb0b02e7c37ddbdaa4482278ca
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-08-21 15:06:02 +0100

    podofo and openslide: fix version conflicts

--- recipes/openslide/all/conanfile.py
+++ recipes/openslide/all/conanfile.py
@@ -64,11 +64,11 @@ class OpenSlideConan(ConanFile):
         self.requires("sqlite3/3.45.3")
         self.requires("zlib/[>=1.2.11 <2]")
         if self.options.jpeg == "libjpeg":
-            self.requires("libjpeg/9e")
+            self.requires("libjpeg/[>=9e]")
         elif self.options.jpeg == "libjpeg-turbo":
-            self.requires("libjpeg-turbo/3.0.2")
+            self.requires("libjpeg-turbo/[>=3.0.2 <4]")
         elif self.options.jpeg == "mozjpeg":
-            self.requires("mozjpeg/4.1.5")
+            self.requires("mozjpeg/[>=4.1.5 <5]")
 
     def validate(self):
         if is_msvc(self):
