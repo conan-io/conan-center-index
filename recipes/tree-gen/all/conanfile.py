757097cd89147d32e3e0c8a1c302ee6b4ed65ba2
PerseoGI <perseog@jfrog.com>
2025-08-26 11:54:04 +0200

    tree-gen: removed compiler restriction which cause invalid configurations on libqasm

--- recipes/tree-gen/all/conanfile.py
+++ recipes/tree-gen/all/conanfile.py
@@ -1,14 +1,12 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get
-from conan.tools.scm import Version
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.1"
 
 
 class TreeGenConan(ConanFile):
@@ -28,39 +26,12 @@ class TreeGenConan(ConanFile):
         "shared": False,
         "fPIC": True
     }
-
-    @property
-    def _should_build_test(self):
-        return not self.conf.get("tools.build:skip_test", default=True, check_type=bool)
-
-    @property
-    def _min_cppstd(self):
-        return 17
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "gcc": "8",
-            "clang": "7",
-            "apple-clang": "14",
-            "Visual Studio": "16",
-            "msvc": "192"
-        }
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
+    implements = ["auto_shared_fpic"]
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def build_requirements(self):
-        if self._should_build_test:
-            self.test_requires("gtest/1.15.0")
         self.tool_requires("m4/1.4.19")
         if self.settings.os == "Windows":
             self.tool_requires("winflexbison/2.5.24")
@@ -69,17 +40,10 @@ class TreeGenConan(ConanFile):
             self.tool_requires("bison/3.8.2")
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.")
+        check_min_cppstd(self, 17)
 
     def requirements(self):
-        if Version(self.version) < "1.0.8":
-            self.requires("fmt/10.2.1", transitive_headers=True)
-        else:
-            self.requires("fmt/11.0.2", transitive_headers=True)
+        self.requires("fmt/11.0.2", transitive_headers=True)
         self.requires("range-v3/0.12.0", transitive_headers=True)
 
     def source(self):
@@ -89,7 +53,6 @@ class TreeGenConan(ConanFile):
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
-        tc.variables["TREE_GEN_BUILD_TESTS"] = self._should_build_test
         tc.generate()
         env = VirtualBuildEnv(self)
         env.generate()
