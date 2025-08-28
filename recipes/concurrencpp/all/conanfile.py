df605bbed966306822f48a237f6154b77b08db2a
PerseoGI <perseog@jfrog.com>
2025-08-28 14:18:38 +0200

    Relax libc++ requirement when using clang

--- recipes/concurrencpp/all/conanfile.py
+++ recipes/concurrencpp/all/conanfile.py
@@ -1,5 +1,4 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
@@ -35,8 +34,6 @@ class ConcurrencppConan(ConanFile):
 
     def validate(self):
         check_min_cppstd(self, 20)
-        if self.settings.compiler == "clang" and self.settings.compiler.libcxx != "libc++":
-            raise ConanInvalidConfiguration("libc++ required")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
