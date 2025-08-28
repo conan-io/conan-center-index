724593a0d1d3d974ab8d71116be6ee5911abb159
PerseoGI <perseog@jfrog.com>
2025-08-28 16:24:15 +0200

    Restrict clang > 14 and gcc > 13

--- recipes/concurrencpp/all/conanfile.py
+++ recipes/concurrencpp/all/conanfile.py
@@ -2,6 +2,8 @@ from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
+from conan.tools.scm import Version
+from conan.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=2.1"
@@ -26,6 +28,13 @@ class ConcurrencppConan(ConanFile):
     }
     implements = ["auto_shared_fpic"]
 
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "clang": "14", # Until clang-14 coroutines are not fully supported
+            "gcc": "13",   # Until gcc-13 alignas specifier is not supported
+        }
+
     def export_sources(self):
         export_conandata_patches(self)
 
@@ -34,6 +43,9 @@ class ConcurrencppConan(ConanFile):
 
     def validate(self):
         check_min_cppstd(self, 20)
+        minimum_version = self._minimum_compilers_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++20, which your compiler does not support.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
