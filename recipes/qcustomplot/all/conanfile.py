c9171810d6fd76d1123230c6514f2746a060522d
Uilian Ries <uilianries@gmail.com>
2025-08-29 12:23:36 +0200

    Requires conan 2.1

--- recipes/qcustomplot/all/conanfile.py
+++ recipes/qcustomplot/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.files import apply_conandata_patches, copy, get, export_conanda
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.1"
 
 
 class QCustomPlotConan(ConanFile):
