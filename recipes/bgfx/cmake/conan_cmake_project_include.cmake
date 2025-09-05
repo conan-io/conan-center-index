bba8c6a088d42513c3b7415387c50b493786574a
Uilian Ries <uilianr@jfrog.com>
2025-09-05 12:37:04 +0200

    Add cmake include
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

---
+++ recipes/bgfx/cmake/conan_cmake_project_include.cmake
@@ -0,0 +1,6 @@
+cmake_minimum_required(VERSION 3.15)
+
+find_package(miniz REQUIRED CONFIG)
+find_package(tinyexr REQUIRED CONFIG)
+find_package(libsquish REQUIRED CONFIG)
+find_package(wayland REQUIRED CONFIG)
