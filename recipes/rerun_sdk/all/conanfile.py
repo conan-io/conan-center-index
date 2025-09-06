1a91d270e36d8df560eecdae8820a4916537e540
Ottmar Zittlau <ozteopat@gmx.net>
2025-09-06 13:28:08 +0200

    Add fpic and shared option, remove options for arrow and add arrow to list of libraries that needs to be linked

--- recipes/rerun_sdk/all/conanfile.py
+++ recipes/rerun_sdk/all/conanfile.py
@@ -1,9 +1,10 @@
-from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
-from conan.tools import files
-from conan.errors import ConanInvalidConfiguration
-
 import os
+
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools import files
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
 
 class Package(ConanFile):
     name = "rerun_sdk"
@@ -15,6 +16,10 @@ class Package(ConanFile):
     license = ("Apache-2.0", "MIT")
 
     settings = "os", "arch", "compiler", "build_type"
+
+    implements = ["auto_shared_fpic"]
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
 
     def _get_additional_lib(self):
         if self.settings.arch == "x86_64":
@@ -29,19 +34,20 @@ class Package(ConanFile):
                 return "librerun_c__linux_arm64.a"
             elif self.settings.os == "Macos":
                 return "librerun_c__macos_arm64.a"
-            
+
         return None
 
     def validate(self):
         if self._get_additional_lib() is None:
-            raise ConanInvalidConfiguration(f"Unsupported combination of architecture {self.settings.arch} and os {self.settings.os}")
-
+            raise ConanInvalidConfiguration(
+                f"Unsupported combination of architecture {self.settings.arch} and os {self.settings.os}"
+            )
 
     def requirements(self):
-        self.requires("arrow/21.0.0", options={"with_thrift": False, "parquet": False})
+        self.requires("arrow/21.0.0")
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
 
     def source(self):
         files.get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -49,10 +55,11 @@ class Package(ConanFile):
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
-        
+
         tc = CMakeToolchain(self)
         tc.variables["RERUN_DOWNLOAD_AND_BUILD_ARROW"] = "OFF"
-        tc.variables["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
+        tc.variables["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options["fPIC"]
+        tc.variables["BUILD_SHARED_LIBS"] = self.options["shared"]
         tc.generate()
 
     def build(self):
@@ -64,8 +71,14 @@ class Package(ConanFile):
         cmake = CMake(self)
         cmake.install()
 
-        # Remove installed cmake config files       
+        # Remove installed cmake config files
         files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.libs = ["rerun_sdk", self._get_additional_lib()]
+        arrow_cmake_suffix = "shared" if self.options["arrow/*"].shared else "static"
+
+        self.cpp_info.libs = [
+            "rerun_sdk",
+            self._get_additional_lib(),
+            f"Arrow::arrow_{arrow_cmake_suffix}",
+        ]
