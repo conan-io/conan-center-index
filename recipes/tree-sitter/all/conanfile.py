0e407851c2c786d8fdfe703c072c98c86a0ddd0c
Uilian Ries <uilianr@jfrog.com>
2025-09-08 13:16:28 +0200

    Remove logic for old versions from recipe
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/tree-sitter/all/conanfile.py
+++ recipes/tree-sitter/all/conanfile.py
@@ -3,7 +3,6 @@ import os
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 from conan.tools.files import get, copy, rmdir
-from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
 
@@ -15,7 +14,7 @@ class TreeSitterConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://tree-sitter.github.io/tree-sitter"
     topics = ("parser", "incremental", "rust")
-    package_type = "library"
+    package_type = "static-library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "fPIC": [True, False],
@@ -26,18 +25,12 @@ class TreeSitterConan(ConanFile):
         "shared": False,
     }
 
-    def export_sources(self):
-        if Version(self.version) < "0.24.1":
-            copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-            if Version(self.version) >= "0.24.1":
-                self.package_type = "static-library"
 
     def configure(self):
-        if Version(self.version) >= "0.24.1" and self.settings.os == "Windows":
+        if self.settings.os == "Windows":
             self.options.rm_safe("shared")
         if self.options.get_safe("shared"):
             self.options.rm_safe("fPIC")
@@ -51,20 +44,13 @@ class TreeSitterConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        tc = CMakeToolchain(self)
-        if Version(self.version) < "0.24.1":
-            tc.variables["TREE_SITTER_SRC_DIR"] = self.source_folder.replace("\\", "/")
-            tc.variables["TREE_SITTER_VERSION"] = str(self.version)
-        else:
-            tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.get_safe("shared", False)
+        tc = CMakeToolchain(self)        
+        tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.get_safe("shared", False)
         tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        if Version(self.version) < "0.24.1":
-            cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
-        else:
-            cmake.configure(build_script_folder=os.path.join(self.source_folder, "lib"))
+        cmake.configure(build_script_folder=os.path.join(self.source_folder, "lib"))
         cmake.build()
 
     def package(self):
