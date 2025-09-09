0ef835e8882218dd6b4ba54405755cedb1b966d0
PerseoGI <perseog@jfrog.com>
2025-09-09 16:18:03 +0200

    SDL_image: added new major 3.x version

---
+++ recipes/sdl_image/3.x/test_package/test_package.c
@@ -0,0 +1,9 @@
+#define SDL_MAIN_HANDLED
+#include "SDL3_image/SDL_image.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("SDL Version: %i\n", SDL_GetVersion());
+    printf("SDL_image Version: %i\n", IMG_Version());
+}
