ee0b71beae8f015af091d2d3619c4673fa8ef56c
Uilian Ries <uilianr@jfrog.com>
2025-09-10 13:22:43 +0200

    Add shared option
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/bgfx/cmake/conanfile.py
+++ recipes/bgfx/cmake/conanfile.py
@@ -21,10 +21,11 @@ class bgfxConan(ConanFile):
     topics = ("rendering", "graphics", "gamedev")
     package_type = "static-library"
     settings = "os", "compiler", "arch", "build_type"
-    options = {"fPIC": [True, False], "tools": [True, False]}
-    default_options = {"fPIC": True, "tools": False}
-    implements = ["auto_shared_fpic"]
+    options = {"fPIC": [True, False], "shared": [True, False], "tools": [True, False]}
+    default_options = {"fPIC": True, "shared": False, "tools": False}
     languages = "C++"
+    # INFO: bgfx supports shared, but only generates bgfx as shared and all others as static
+    implements = ["auto_shared_fpic"]
 
     def export_sources(self):
         copy(self, "conan_cmake_project_include.cmake", self.recipe_folder, os.path.join(self.export_sources_folder, "src"))
@@ -41,9 +42,11 @@ class bgfxConan(ConanFile):
             self.requires("wayland/1.23.92")
         # INFO: miniz, tinyexr and libsquish are vendored in bgfx as well
         self.requires("miniz/3.0.2")
-        self.requires("tinyexr/1.0.7")
-        self.requires("libsquish/1.15")
-        # INFO: glflags and spirv-tools are vendored only
+        if not self.options.shared:
+            # INFO: bimg_encode and bimg_decode are only built for static bgfx
+            self.requires("tinyexr/1.0.7")
+            self.requires("libsquish/1.15")
+        # INFO: glflags and spirv-tools are vendored only. No need to add them as dependencies
 
     def validate(self):
         check_min_cppstd(self, 17)
@@ -70,7 +73,7 @@ class bgfxConan(ConanFile):
         # bgfx does no include dependencis via find_package
         tc.cache_variables["CMAKE_PROJECT_bgfx_INCLUDE"] = os.path.join(self.source_folder, "conan_cmake_project_include.cmake")
         # It supports shared, but only generates bgfx as shared and all others as static
-        tc.cache_variables["BGFX_LIBRARY_TYPE"] = "STATIC"
+        tc.cache_variables["BGFX_LIBRARY_TYPE"] = "SHARED" if self.options.shared else "STATIC"
         tc.cache_variables["BGFX_BUILD_EXAMPLES"] = False
         tc.cache_variables["BGFX_BUILD_EXAMPLE_COMMON"] = False
         tc.cache_variables["BGFX_BUILD_TESTS"] = False
@@ -111,12 +114,13 @@ class bgfxConan(ConanFile):
 
         self.cpp_info.components["bx"].set_property("cmake_target_name", "bgfx::bx")
         self.cpp_info.components["bx"].libs = ["bx"]
+        self.cpp_info.components["bx"].defines = [f"BX_CONFIG_DEBUG={1 if self.settings.build_type == 'Debug' else 0}"]
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.components["bx"].system_libs = ["dl", "rt"]
             self.cpp_info.components["bx"].includedirs.append(os.path.join("include", "bx", "compat", "linux"))
-        self.cpp_info.components["bx"].defines = [f"BX_CONFIG_DEBUG={1 if self.settings.build_type == 'Debug' else 0}"]
-        if is_apple_os(self):
+        elif is_apple_os(self):
             self.cpp_info.components["bx"].frameworks = ["Foundation"]
+            self.cpp_info.components["bx"].includedirs.append(os.path.join("include", "bx", "compat", "osx"))
         elif is_msvc(self):
             # INFO: \bx\platform.h(432): fatal error C1189: #error:  "When using MSVC you must set /Zc:__cplusplus compiler option."
             self.cpp_info.components["bx"].cxxflags = ["/Zc:__cplusplus", "/Zc:preprocessor"]
@@ -127,24 +131,27 @@ class bgfxConan(ConanFile):
         self.cpp_info.components["bimg"].libs = ["bimg"]
         self.cpp_info.components["bimg"].requires = ["bx", "miniz::miniz"]
 
-        self.cpp_info.components["decode"].set_property("cmake_target_name", "bgfx::bimg_decode")
-        self.cpp_info.components["decode"].libs = ["bimg_decode"]
-        self.cpp_info.components["decode"].requires = ["bx", "miniz::miniz", "tinyexr::tinyexr"]
+        if not self.options.shared:
+            self.cpp_info.components["decode"].set_property("cmake_target_name", "bgfx::bimg_decode")
+            self.cpp_info.components["decode"].libs = ["bimg_decode"]
+            self.cpp_info.components["decode"].requires = ["bx", "miniz::miniz", "tinyexr::tinyexr"]
 
-        self.cpp_info.components["encode"].set_property("cmake_target_name", "bgfx::bimg_encode")
-        self.cpp_info.components["encode"].libs = ["bimg_encode"]
-        self.cpp_info.components["encode"].requires = ["bx", "libsquish::libsquish", "tinyexr::tinyexr"]
+            self.cpp_info.components["encode"].set_property("cmake_target_name", "bgfx::bimg_encode")
+            self.cpp_info.components["encode"].libs = ["bimg_encode"]
+            self.cpp_info.components["encode"].requires = ["bx", "libsquish::libsquish", "tinyexr::tinyexr"]
 
         self.cpp_info.components["bgfx"].set_property("cmake_target_name", "bgfx::bgfx")
         self.cpp_info.components["bgfx"].libs = ["bgfx"]
         self.cpp_info.components["bgfx"].requires = ["bx", "bimg"]
+        if self.options.shared:
+            self.cpp_info.components["bgfx"].defines.append("BGFX_SHARED_LIB_BUILD=1")
         if self.settings.os == "Linux":
             self.cpp_info.components["bgfx"].requires.extend(["xorg::xorg", "opengl::opengl", "wayland::wayland"])
-        if is_apple_os(self):
+        elif is_apple_os(self):
             self.cpp_info.components["bgfx"].frameworks = ["Cocoa", "Metal", "QuartzCore", "IOKit", "CoreFoundation"]
         # multithreaded rendering is enabled by default via BGFX_CONFIG_MULTITHREADED
-        self.cpp_info.components["bgfx"].defines = ["BGFX_CONFIG_MULTITHREADED=1",
-                                                   f"BGFX_CONFIG_DEBUG_ANNOTATION={1 if self.settings.build_type == 'Debug' else 0}",]
+        self.cpp_info.components["bgfx"].defines.extend(["BGFX_CONFIG_MULTITHREADED=1",
+                                                   f"BGFX_CONFIG_DEBUG_ANNOTATION={1 if self.settings.build_type == 'Debug' else 0}",])
 
         for tool in ["bin2c", "texturec", "texturev", "geometryc", "geometryv", "shaderc"]:
             self.cpp_info.components[tool].set_property("cmake_target_name", f"bgfx::{tool}")
