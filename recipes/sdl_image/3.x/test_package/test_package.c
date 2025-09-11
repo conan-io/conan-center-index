383215006f8421abd8a3fc575b792d2e472a3966
PerseoGI <perseog@jfrog.com>
2025-09-11 12:51:46 +0200

    librdkafka: removed old versions

--- recipes/sdl_image/3.x/test_package/test_package.c
+++
@@ -1,9 +0,0 @@
-#define SDL_MAIN_HANDLED
-#include "SDL3_image/SDL_image.h"
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-    printf("SDL Version: %i\n", SDL_GetVersion());
-    printf("SDL_image Version: %i\n", IMG_Version());
-}
