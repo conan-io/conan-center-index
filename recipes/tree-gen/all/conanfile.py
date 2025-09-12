36f641eba0014b7efb9c74c9de0c3b64806bbc02
PerseoGI <perseog@jfrog.com>
2025-09-12 14:09:26 +0200

    Restored original tree-gen and only keep basic changes

--- recipes/tree-gen/all/conanfile.py
+++ recipes/tree-gen/all/conanfile.py
@@ -1,12 +1,14 @@
 import os
 
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get
 from conan.tools.scm import Version
 
-required_conan_version = ">=2.1"
+required_conan_version = ">=1.53.0"
 
 
 class TreeGenConan(ConanFile):
@@ -26,12 +28,25 @@ class TreeGenConan(ConanFile):
         "shared": False,
         "fPIC": True
     }
-    implements = ["auto_shared_fpic"]
+
+    @property
+    def _should_build_test(self):
+        return not self.conf.get("tools.build:skip_test", default=True, check_type=bool)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def build_requirements(self):
+        if self._should_build_test:
+            self.test_requires("gtest/1.15.0")
         self.tool_requires("m4/1.4.19")
         if self.settings.os == "Windows":
             self.tool_requires("winflexbison/2.5.24")
@@ -49,11 +64,10 @@ class TreeGenConan(ConanFile):
             check_min_cppstd(self, 17)
 
     def requirements(self):
-        if Version(self.version) <= "1.0.7":
-            self.requires("fmt/[<11]", transitive_headers=True)
+        if Version(self.version) < "1.0.8":
+            self.requires("fmt/10.2.1", transitive_headers=True)
         else:
-            self.requires("fmt/[>=10.2.1]", transitive_headers=True)
-
+            self.requires("fmt/11.0.2", transitive_headers=True)
         self.requires("range-v3/0.12.0", transitive_headers=True)
 
     def source(self):
@@ -63,7 +77,10 @@ class TreeGenConan(ConanFile):
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
+        tc.variables["TREE_GEN_BUILD_TESTS"] = self._should_build_test
         tc.generate()
+        env = VirtualBuildEnv(self)
+        env.generate()
 
     def build(self):
         cmake = CMake(self)
--- recipes/tree-gen/all/test_package/test_package.cpp
+++
@@ -1,8 +0,0 @@
-#include "version.hpp"  // tree-gen version
-#include <iostream>
-
-
-int main() {
-    std::cout << "tree-gen version: " << get_version() << '\n';
-    std::cout << "tree-gen release year: " << get_release_year() << '\n';
-}
