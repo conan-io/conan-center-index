43fb36afdf659dbf6ef122bfc64ba9c75b5963d6
PerseoGI <perseog@jfrog.com>
2025-09-12 14:35:36 +0200

    Revert test_package

--- recipes/tree-gen/all/conanfile.py
+++ recipes/tree-gen/all/conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get
 from conan.tools.scm import Version
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.1"
 
 
 class TreeGenConan(ConanFile):
