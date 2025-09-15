33a3eec411ba05102482ba77b11fe63048eaa50d
PerseoGI <perseog@jfrog.com>
2025-09-15 11:54:24 +0200

    Re-revert test_package

--- recipes/tree-gen/all/test_package/conanfile.py
+++ recipes/tree-gen/all/test_package/conanfile.py
@@ -7,14 +7,10 @@ from conan.tools.cmake import CMake, cmake_layout
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv", "VirtualRunEnv"
-    test_type = "explicit"
-
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str, run=True)
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
 
     def requirements(self):
-        self.requires(self.tested_reference_str, run=True)
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -28,3 +24,5 @@ class TestPackageConan(ConanFile):
         if can_run(self):
             bin_path = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(bin_path, env="conanrun")
+
+        assert os.path.exists(os.path.join(self.dependencies[self.tested_reference_str].cpp_info.builddirs[0], "generate_tree.cmake"))
