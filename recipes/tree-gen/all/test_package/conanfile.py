bbe78c2874152f6c3c30d85afb0abcb4ffaa454c
PerseoGI <perseog@jfrog.com>
2025-09-16 11:04:17 +0200

    Removed os.path.exists

--- recipes/tree-gen/all/test_package/conanfile.py
+++ recipes/tree-gen/all/test_package/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.cmake import CMake, cmake_layout
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
@@ -25,4 +25,3 @@ class TestPackageConan(ConanFile):
             bin_path = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(bin_path, env="conanrun")
 
-        assert os.path.exists(os.path.join(self.dependencies[self.tested_reference_str].cpp_info.builddirs[0], "generate_tree.cmake"))
