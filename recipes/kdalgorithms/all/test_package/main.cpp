751c9387d962950cee4e539f212d146b1c963f1c
Uilian Ries <uilianr@jfrog.com>
2025-09-18 10:19:44 +0200

    Simplify test package
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/kdalgorithms/all/test_package/main.cpp
+++ recipes/kdalgorithms/all/test_package/main.cpp
@@ -1,27 +1,10 @@
-/*
-  This file is part of KDAlgorithms.
-
-  SPDX-FileCopyrightText: 2025 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
-
-  SPDX-License-Identifier: MIT
-
-  Contact KDAB at <info@kdab.com> for commercial licensing options.
-*/
-
+#include <cstdlib>
 #include <iostream>
-#include <kdalgorithms.h>
-#include <string>
-#include <vector>
 
-int main()
-{
-    auto vec = kdalgorithms::iota(1, 100);
-    auto odds = kdalgorithms::filtered(vec, [](int i) { return i % 2 == 1; });
-    auto result = kdalgorithms::accumulate(odds, [](const std::string &partial, int value) {
-        return partial + "," + std::to_string(value);
-    });
+#include <kdalgorithms/kdalgorithms.h>
 
-    std::cout << result << "\n";
-
-    return 0;
+int main() {
+    auto vec = kdalgorithms::iota(1, 10);
+    std::cout << "kdalgorithms::iota: " << vec[0] << "\n";
+    return EXIT_SUCCESS;
 }
