068d4b7156f2c610cc11288ec79133a9145f2720
Uilian Ries <uilianr@jfrog.com>
2025-09-19 08:57:44 +0200

    Apply patches in source step
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/rocksdb/all/conanfile.py
+++ recipes/rocksdb/all/conanfile.py
@@ -111,6 +111,7 @@ class RocksDBConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -164,7 +165,6 @@ class RocksDBConan(ConanFile):
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
