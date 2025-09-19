08da33f623b767fa2415ef0b03ff34621907417f
Uilian Ries <uilianr@jfrog.com>
2025-09-19 09:54:08 +0200

    Simplify test package
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/rocksdb/all/test_package/test_package.cpp
+++ recipes/rocksdb/all/test_package/test_package.cpp
@@ -2,15 +2,8 @@
 #include <iostream>
 #include "rocksdb/db.h"
 
+
 int main() {
-  rocksdb::DB* db;
-  rocksdb::Options options;
-  options.create_if_missing = true;
-  rocksdb::Status status = rocksdb::DB::Open(options, "testdb", &db);
-  
-  if (!status.ok()) {
-    std::cerr << "DB error: " << status.ToString() << std::endl;
-  }
-  delete db;
+  std::cout << "RocksDB version: " << rocksdb::GetRocksVersionAsString() << std::endl;
   return EXIT_SUCCESS;
 }
