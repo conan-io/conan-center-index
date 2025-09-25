f5187a5de8206b9323a83dc63ee75df0708a9e96
Uilian Ries <uilianr@jfrog.com>
2025-09-25 16:12:34 +0200

    Use requirements only for Flex in test package
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/flex/all/test_package/conanfile.py
+++ recipes/flex/all/test_package/conanfile.py
@@ -5,19 +5,14 @@ from io import StringIO
 from conan import ConanFile
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, cmake_layout, CMakeDeps, CMakeToolchain
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "CMakeDeps"
-    test_type = "explicit"
 
     def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+        self.requires(self.tested_reference_str, run=True)
 
     def layout(self):
         cmake_layout(self)
@@ -38,15 +33,23 @@ class TestPackageConan(ConanFile):
         assert_flex_version = "flex {}".format(expected_version)
         assert(assert_flex_version in output_str)
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        # INFO: Pass flex path to CMake, otherwise will need to add tool_requires(flex) for FindFLEX.cmake
+        tc.cache_variables["FLEX_EXECUTABLE"] = os.path.join(self.dependencies["flex"].cpp_info.bindir, "flex")
+        tc.generate()
+        CMakeDeps(self).generate()
+
     def build(self):
         # Let's check flex version installed
         self._assert_expected_version()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+        
 
     def test(self):
-        if can_run(self):
+        if can_run(self):            
             bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
             txt_file = os.path.join(self.source_folder, "basic_nr.txt")
             self.run(f"{bin_path} {txt_file}", env="conanrun")
