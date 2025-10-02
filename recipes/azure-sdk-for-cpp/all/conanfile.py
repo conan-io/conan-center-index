517708e6d43ba7d708efb45ec1e8b3ba84b65cce
Tony Perrie <hoyhoy@gmail.com>
2025-10-02 12:31:35 -0700

    add datalake

--- recipes/azure-sdk-for-cpp/all/conanfile.py
+++ recipes/azure-sdk-for-cpp/all/conanfile.py
@@ -160,6 +160,10 @@ class AzureSDKForCppConan(ConanFile):
         self.cpp_info.components["azure-identity"].set_property("cmake_target_name", "Azure::azure-identity")
         self.cpp_info.components["azure-identity"].libs = ["azure-identity"]
         self.cpp_info.components["azure-identity"].requires = ["azure-core"]
+        
+        self.cpp_info.components["azure-storage-files-datalake"].set_property("cmake_target_name", "Azure::azure-storage-files-datalake")
+        self.cpp_info.components["azure-storage-files-datalake"].libs = ["azure-storage-files-datalake"]
+        self.cpp_info.components["azure-storage-files-datalake"].requires = ["azure-core", "azure-storage-common"]
 
         if self.settings.os == "Windows":
             self.cpp_info.components["azure-identity"].requires.append("wil::wil")
