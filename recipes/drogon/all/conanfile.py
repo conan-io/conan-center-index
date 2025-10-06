3afe6a922cc2465d7006515fc2946b177975c618
Uilian Ries <uilianr@jfrog.com>
2025-10-06 17:07:07 +0200

    Revert version ranges
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/drogon/all/conanfile.py
+++ recipes/drogon/all/conanfile.py
@@ -77,10 +77,8 @@ class DrogonConan(ConanFile):
     def requirements(self):
         if Version(self.version) < "1.9.7":
             self.requires("trantor/1.5.19", transitive_headers=True, transitive_libs=True)
-        elif Version(self.version) <= "1.9.10":
-            self.requires("trantor/1.5.21", transitive_headers=True, transitive_libs=True)
         else:
-            self.requires("trantor/[>=1.5.21]", transitive_headers=True, transitive_libs=True)
+            self.requires("trantor/1.5.21", transitive_headers=True, transitive_libs=True)
         self.requires("jsoncpp/1.9.5", transitive_headers=True, transitive_libs=True)
         self.requires("openssl/[>=1.1 <4]")
         self.requires("zlib/[>=1.2.11 <2]")
