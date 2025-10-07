6e60c6bde94415a25a03ef270709f194f6e3d5b1
Francisco Ramírez <franchuti688@gmail.com>
2025-10-07 10:54:41 +0200

    Apply suggestion from @franramirez688

--- recipes/opencolorio/all/conanfile.py
+++ recipes/opencolorio/all/conanfile.py
@@ -102,7 +102,7 @@ class OpenColorIOConan(ConanFile):
 
         if self.settings.os == "Linux":
             # Workaround for: https://github.com/conan-io/conan/issues/13560
-            libdirs_host = [l for dependency in self.dependencies.host.values() for l in dependency.cpp_info.aggregated_components()]
+            libdirs_host = [l for dependency in self.dependencies.host.values() for l in dependency.cpp_info.aggregated_components().libdirs]
             tc.variables["CMAKE_BUILD_RPATH"] = ";".join(libdirs_host)
 
         tc.generate()
