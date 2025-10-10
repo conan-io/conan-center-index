a7c32f4b758aee0bf3eeffdd86a47f9e102f0816
synacker <milovidovmikhail@gmail.com>
2025-10-10 23:19:45 +0300

    Update recipes/daggy/2.2/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/daggy/2.2/conanfile.py
+++ recipes/daggy/2.2/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
-from conan.tools.files import get, copy, apply_conandata_patches, export_conandata_patches
+from conan.tools.files import get, copy
 from conan.tools.build import check_min_cppstd
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.scm import Version
