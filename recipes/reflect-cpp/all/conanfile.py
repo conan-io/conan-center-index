0241c3ea18643cc3b64361b8a48480c5fc51ec97
mmeisner204 <mmeisner204@gmail.com>
2025-10-15 16:24:10 +0000

    address MR comments

--- recipes/reflect-cpp/all/conanfile.py
+++ recipes/reflect-cpp/all/conanfile.py
@@ -112,7 +112,9 @@ class ReflectCppConan(ConanFile):
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if minimum_version and Version(self.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(f"{self.ref} requires C++20 features, which your compiler does not fully support.")
-
+        if self.settings.compiler == "msvc" and self.option.shared:
+            raise ConanInvalidConfiguration("Old versions of this library do not support MSVC-shared builds")
+    
     def layout(self):
         cmake_layout(self, src_folder="src")
 
@@ -157,6 +159,7 @@ class ReflectCppConan(ConanFile):
         )
         cmake = CMake(self)
         cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.libs = ["reflectcpp"]
