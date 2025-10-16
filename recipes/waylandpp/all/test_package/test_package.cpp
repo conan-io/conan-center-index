ee18c3dbad932277f2c37e8a325d253057a7fd70
Andrew Marshall <planetmarshalluk@gmail.com>
2025-10-16 15:35:31 +0100

    handle expected error in test package

--- recipes/waylandpp/all/test_package/test_package.cpp
+++ recipes/waylandpp/all/test_package/test_package.cpp
@@ -2,13 +2,17 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    const auto display = wayland::display_t{};
-    if (!display) {
-	    std::cerr << "Can't connect to display\n";
+    try {
+        const auto display = wayland::display_t{};
+        if (!display) {
+	        throw std::runtime_error("Can't connect to display");
+        }
+        std::cout << "connected to display fd: " << display.get_fd() << '\n';
+        exit(0);
     }
-    else {
-        std::cout << "connected to display fd: " << display.get_fd() << "\n";
+    catch (std::exception const &ex) {
+        std::cerr << ex.what() << '\n';
     }
 
-    exit(0);
+    return 0;
 }
