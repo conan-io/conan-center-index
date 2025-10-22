9a7d092cc911421b561a9a605c05cb5bbbb19c74
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-10-22 14:32:36 +0100

    remove unused import

--- recipes/flex/all/test_package/conanfile.py
+++ recipes/flex/all/test_package/conanfile.py
@@ -3,7 +3,6 @@ import re
 from io import StringIO
 
 from conan import ConanFile
-from conan.tools.env import VirtualBuildEnv
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout, CMakeDeps, CMakeToolchain
 
