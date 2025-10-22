35be4accb3bdc3d2b53f7e0ea2b92a2e0f8835cc
Saikari <lin@sz.cn.eu.org>
2025-10-22 03:25:20 +0300

    Remove unused import of ConanInvalidConfiguration from omath recipe

--- recipes/omath/all/conanfile.py
+++ recipes/omath/all/conanfile.py
@@ -1,10 +1,8 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
 from conan.tools.microsoft import is_msvc
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.1"
 
