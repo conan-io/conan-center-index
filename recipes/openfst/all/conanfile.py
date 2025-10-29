318f465887de6f29e8c180e5b919c3d35cfd2486
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-10-29 16:02:55 +0100

    Revert dirty

--- recipes/openfst/all/conanfile.py
+++ recipes/openfst/all/conanfile.py
@@ -3,11 +3,10 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.build import check_min_cppstd, cross_building, check_max_cppstd
+from conan.tools.build import check_min_cppstd, cross_building
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rename, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.0.9"
 
