fb5dff8d73be4153e0ddd3cfd86c6f2b772922dd
Ernesto de Gracia Herranz <ernestodegraciah@gmail.com>
2025-10-30 12:05:12 +0100

    Update recipes/libobjc2/all/conanfile.py
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/libobjc2/all/conanfile.py
+++ recipes/libobjc2/all/conanfile.py
@@ -60,7 +60,7 @@ class PackageConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "COPYING", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
