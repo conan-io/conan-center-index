35d9360b19cb95395f1cdfe1a68382733dc99e7b
Francisco Ramirez de Anton <franchuti688@gmail.com>
2025-10-31 15:11:03 +0100

    Building application update-mime-info

--- recipes/shared-mime-info/all/conanfile.py
+++ recipes/shared-mime-info/all/conanfile.py
@@ -1,34 +1,43 @@
-from conan import ConanFile
-from conan.tools.files import copy, get
-from conan.tools.layout import basic_layout
-from conan.tools.meson import Meson, MesonToolchain
-from conan.errors import ConanInvalidConfiguration
-
 import os
 
-required_conan_version = ">=1.55.0"
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get, rmdir
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.layout import basic_layout
+from conan.tools.meson import Meson, MesonToolchain
+
+required_conan_version = ">=2"
 
 
 class SharedMimeInfoConan(ConanFile):
     name = "shared-mime-info"
     url = "https://github.com/conan-io/conan-center-index"
     description = "The freedesktop.org shared MIME database"
-    license = ("GPL-2.0")
+    license = "GPL-2.0"
     homepage = "https://gitlab.freedesktop.org/xdg/shared-mime-info"
-    topics = ("ferrdesktop", "mime")
+    topics = ("freedesktop", "mime")
     package_type = "application"
     settings = "os", "arch", "compiler", "build_type"
 
     def layout(self):
         basic_layout(self, src_folder="src")
 
+    def validate_build(self):
+        check_min_cppstd(self, 17)
+
     def validate(self):
         if self.settings.os not in ("Linux", "FreeBSD"):
-            raise ConanInvalidConfiguration("OS is not supported")
+            raise ConanInvalidConfiguration("Only Linux and FreeBSD are supported")
+
+    def requirements(self):
+        self.requires("libxml2/[>=2.12.5 <3]")
+        self.requires("glib/[>=2.85.3 <3]")
 
     def build_requirements(self):
         self.tool_requires("meson/1.4.0")
-        self.tool_requires("libxml2/[>=2.12.5 <3]")
+        self.tool_requires("libxml2/<host_version>")
         self.tool_requires("gettext/0.22.5")
 
     def source(self):
@@ -36,11 +45,13 @@ class SharedMimeInfoConan(ConanFile):
 
     def generate(self):
         tc = MesonToolchain(self)
-        tc.project_options['update-mimedb'] = "false"
-        tc.project_options['build-tools'] = "false"
+        tc.project_options['build-tools'] = "true"
         tc.project_options['build-translations'] = "false"
         tc.project_options['build-tests'] = "false"
+        tc.project_options['update-mimedb'] = "false"
         tc.generate()
+        deps = PkgConfigDeps(self)
+        deps.generate()
 
     def build(self):
         meson = Meson(self)
@@ -48,7 +59,12 @@ class SharedMimeInfoConan(ConanFile):
         meson.build()
 
     def package(self):
-        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder,"licenses"))
-
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         meson = Meson(self)
         meson.install()
+        rmdir(self, os.path.join(self.package_folder, "share", "man"))
+        rmdir(self, os.path.join(self.package_folder, "share", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.libdirs = []
+        self.cpp_info.includedirs = []
--- recipes/shared-mime-info/all/test_package/conanfile.py
+++ recipes/shared-mime-info/all/test_package/conanfile.py
@@ -4,8 +4,6 @@ from conan.tools.build import can_run
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "VirtualBuildEnv"
-    test_type = "explicit"
 
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
