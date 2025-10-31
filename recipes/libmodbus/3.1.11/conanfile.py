044b0bab222f750d60ed7b92acf3a4a3822efe9e
David Sanchez <davidsf@jfrog.com>
2025-10-31 09:30:38 +0100

    recipe splited

--- recipes/libmodbus/3.1.11/conanfile.py
+++ recipes/libmodbus/3.1.11/conanfile.py
@@ -1,11 +1,9 @@
 from conan import ConanFile
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rename, replace_in_file, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import check_min_vs, is_msvc, unix_path
-from conan.tools.scm import Version
+from conan.tools.microsoft import check_min_vs, is_msvc
 import os
 
 required_conan_version = ">=1.57.0"
@@ -51,6 +49,7 @@ class LibmodbusConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def build_requirements(self):
+        self.tool_requires("libtool/2.4.7")
         if is_msvc(self):
             self.tool_requires("automake/1.16.5")
         if self._settings_build.os == "Windows":
@@ -62,28 +61,13 @@ class LibmodbusConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        env = VirtualBuildEnv(self)
-        env.generate()
-
         tc = AutotoolsToolchain(self)
-        if Version(self.version) < "3.1.8":
-            tc.configure_args.append("--without-documentation")
         tc.configure_args.append("--disable-tests")
         if is_msvc(self) and check_min_vs(self, "180", raise_invalid=False):
             tc.extra_cflags.append("-FS")
-        env = tc.environment()
-        if is_msvc(self):
-            compile_wrapper = unix_path(self, os.path.join(self.source_folder, "build-aux", "compile"))
-            ar_wrapper = unix_path(self, self.conf.get("user.automake:lib-wrapper", check_type=str))
-            env.define("CC", f"{compile_wrapper} cl -nologo")
-            env.define("CXX", f"{compile_wrapper} cl -nologo")
-            env.define("LD", "link -nologo")
-            env.define("AR", f"{ar_wrapper} \"lib -nologo\"")
-            env.define("NM", "dumpbin -symbols")
-            env.define("OBJDUMP", ":")
-            env.define("RANLIB", ":")
-            env.define("STRIP", ":")
-        tc.generate(env)
+            if self.options.shared:
+                tc.extra_defines.append("DLLBUILD")
+        tc.generate()
 
     def _patch_sources(self):
         apply_conandata_patches(self)
@@ -94,6 +78,7 @@ class LibmodbusConan(ConanFile):
     def build(self):
         self._patch_sources()
         autotools = Autotools(self)
+        autotools.autoreconf()
         autotools.configure()
         autotools.make()
 
@@ -107,8 +92,8 @@ class LibmodbusConan(ConanFile):
         fix_apple_shared_install_name(self)
         if is_msvc(self) and self.options.shared:
             rename(self,
-                    os.path.join(self.package_folder, "lib", "modbus.dll.lib"),
-                    os.path.join(self.package_folder, "lib", "modbus.lib"))
+                   os.path.join(self.package_folder, "lib", "modbus.dll.lib"),
+                   os.path.join(self.package_folder, "lib", "modbus.lib"))
 
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "libmodbus")
