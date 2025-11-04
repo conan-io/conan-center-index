ab5523188b8a7706012186051675954f2bfc5347
Francisco Ramirez de Anton <franchuti688@gmail.com>
2025-11-04 17:20:30 +0100

    Keeping it as upstream

--- recipes/cnats/all/conanfile.py
+++ recipes/cnats/all/conanfile.py
@@ -90,18 +90,15 @@ class PackageConan(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
-    @property
-    def _nats_library_name(self):
-        suffix = "" if self.options.shared else "_static"
-        debug = "d" if self.settings.build_type == "Debug" else ""
-        return f"nats{suffix}{debug}"
-
     def package_info(self):
-        self.cpp_info.libs = [self._nats_library_name]
+        suffix = "" if self.options.shared else "_static"
+        lib_name = f"nats{suffix}"
+        self.cpp_info.libs = [lib_name]
         self.cpp_info.set_property("cmake_file_name", "cnats")
-        self.cpp_info.set_property("cmake_target_name", f"cnats::{self._nats_library_name}")
-        # Let's have another target name without suffixes
-        self.cpp_info.set_property("cmake_target_aliases", ["cnats"])
+        self.cpp_info.set_property("cmake_target_name", f"cnats::{lib_name}")
+        # Backward compatible with users using targets with debug suffix
+        debug = "d" if self.settings.build_type == "Debug" else ""
+        self.cpp_info.set_property("cmake_target_aliases", [f"cnats::{lib_name}{debug}"])
         self.cpp_info.set_property("pkg_config_name", "libnats")
 
         if self.options.enable_streaming:
