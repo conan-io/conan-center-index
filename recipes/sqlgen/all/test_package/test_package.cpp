fdafa34b80a5c2b0c94cb4a1e77e7bd4f45e287b
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-11-05 11:54:59 +0100

    Some code for test package

--- recipes/sqlgen/all/test_package/test_package.cpp
+++ recipes/sqlgen/all/test_package/test_package.cpp
@@ -1,5 +1,5 @@
 #include <sqlgen.hpp>
-#ifdef TEST_SQLITE_ENABLED
+#ifdef TEST_SQLITE3_ENABLED
 #include <sqlgen/sqlite.hpp>
 #endif
 #ifdef TEST_MYSQL_ENABLED
@@ -10,8 +10,31 @@
 #endif
 
 #include <iostream>
+struct Person {
+    int id;
+    std::string first_name;
+    std::string last_name;
+    int age;
+};
 
 int main() {
-    // TODO: Example that does not try to connect
+   using namespace sqlgen;
+   using namespace sqlgen::literals;
+
+   // Define a query
+   const auto query = sqlgen::read<std::vector<Person>> |
+                      where("age"_c < 18 and "first_name"_c != "Hugo") |
+                      order_by("age"_c);
+
+    #ifdef TEST_SQLITE3_ENABLED
+    std::cout << "sqlite: " << sqlgen::sqlite::to_sql(query) << std::endl;
+    #endif
+    #ifdef TEST_MYSQL_ENABLED
+    std::cout << "mysql: " << sqlgen::mysql::to_sql(query) << std::endl;
+    #endif
+    #ifdef TEST_POSTGRES_ENABLED
+    std::cout << "postgress: " << sqlgen::postgres::to_sql(query) << std::endl;
+    #endif
+
     return 0;
 }
