a7da6c003471b2a269b8fb1bcf29bcd0196f87d5
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-11-05 07:33:30 +0000

    libavrocpp: add new version, use version ranges, cleanup recipe (#28821)
    * libavrocpp: add version 1.12.1
    
    * remove unused
    
    * removed patches
    
    ---------
    
    Co-authored-by: Francisco Ramirez de Anton <franchuti688@gmail.com>

--- recipes/libavrocpp/all/conanfile.py
+++ recipes/libavrocpp/all/conanfile.py
@@ -1,10 +1,10 @@
 from conan import ConanFile
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, replace_in_file
-from conan.tools.build import check_min_cppstd, valid_min_cppstd
+from conan.tools.files import get, copy, rm, rmdir, replace_in_file
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 import os
 
-required_conan_version = ">=1.54.0"
+required_conan_version = ">=2.1"
 
 
 class LibavrocppConan(ConanFile):
@@ -24,14 +24,6 @@ class LibavrocppConan(ConanFile):
         "shared": False,
         "fPIC": True
     }
-    short_paths = True
-
-    @property
-    def _min_cppstd(self):
-        return 11
-
-    def export_sources(self):
-        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -45,41 +37,32 @@ class LibavrocppConan(ConanFile):
         cmake_layout(self, src_folder="src")
         
     def requirements(self):
-        # boost upper to 1.81.0 requires C++14 minimum
-        self.requires("boost/1.81.0", transitive_headers=True)
-        self.requires("snappy/1.1.9")
+        self.requires("boost/[>=1.81.0 <=1.89.0]", transitive_headers=True)
+        self.requires("snappy/[>=1.1.9 <2]")
+        self.requires("fmt/[>=12 <13]", transitive_headers=True)
+        self.requires("zlib/[>=1.3.1 <2]")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
+        check_min_cppstd(self, 17)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        replace_in_file(self, os.path.join(self.source_folder, "lang", "c++", "CMakeLists.txt"),
+                        "-Werror", "")
 
     def generate(self):
         tc = CMakeToolchain(self)
+        tc.cache_variables["AVRO_BUILD_STATIC"] = not self.options.shared
+        tc.cache_variables["AVRO_BUILD_SHARED"] = self.options.shared
+        tc.cache_variables["AVRO_BUILD_TESTS"] = False
+        tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_zstd"] = True # please open an issue if this is needed
         tc.generate()
 
         deps = CMakeDeps(self)
+        deps.set_property("fmt", "cmake_target_aliases", ["fmt::fmt-header-only"])
         deps.generate()
 
-    def _patch_sources(self):
-        apply_conandata_patches(self)
-        cmakelists = os.path.join(self.source_folder, "lang", "c++", "CMakeLists.txt")
-        # Fix discovery & link to Snappy
-        replace_in_file(self, cmakelists, "SNAPPY_FOUND", "Snappy_FOUND")
-        replace_in_file(self, cmakelists, "${SNAPPY_LIBRARIES}", "Snappy::snappy")
-        replace_in_file(
-            self, cmakelists,
-            "target_include_directories(avrocpp_s PRIVATE ${SNAPPY_INCLUDE_DIR})",
-            "target_link_libraries(avrocpp_s PRIVATE Snappy::snappy)",
-        )
-        # Install either static or shared
-        target = "avrocpp" if self.options.shared else "avrocpp_s"
-        replace_in_file(self, cmakelists, "install (TARGETS avrocpp avrocpp_s" , f"install (TARGETS {target}")
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure(build_script_folder=os.path.join(self.source_folder, "lang", "c++"))
         cmake.build()
@@ -89,6 +72,7 @@ class LibavrocppConan(ConanFile):
         copy(self, pattern="NOTICE*", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         cmake = CMake(self)
         cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
         if self.settings.os == "Windows":
             for dll_pattern_to_remove in ["concrt*.dll", "msvcp*.dll", "vcruntime*.dll"]:
@@ -96,11 +80,16 @@ class LibavrocppConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["avrocpp"] if self.options.shared else ["avrocpp_s"]
+        self.cpp_info.set_property("cmake_file_name", "avro-cpp")
+        self.cpp_info.set_property("cmake_target_name", "avro-cpp::avrocpp" if self.options.shared else "avro-cpp::avrocpp_s")
         if self.options.shared:
             self.cpp_info.defines.append("AVRO_DYN_LINK")
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs.append("m")
+
         self.cpp_info.requires = [
-            "boost::headers", "boost::filesystem", "boost::iostreams", "boost::program_options",
-            "boost::regex", "boost::system", "snappy::snappy",
+            "zlib::zlib",
+            "snappy::snappy",
+            "boost::headers",
+            "fmt::fmt",
         ]
