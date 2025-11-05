e5932dc1044e8a126ef51790d9ef8451b4211347
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-11-05 13:45:14 +0100

    Simplify, make this compile
    Expat symbols are exposed in public headers
    
    Simplify
    
    dl needed
    
    Remove 1.4.0
    
    dont commit the src dummy
    
    Validate bug workaround
    
    back to all, to be squashed

---
+++ recipes/openfx/all/conanfile.py
@@ -0,0 +1,90 @@
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, export_conandata_patches, apply_conandata_patches
+import os
+
+required_conan_version = ">=2.1"
+
+
+class Openfx(ConanFile):
+    name = "openfx"
+    license = "BSD-3-Clause"
+    description = "OpenFX image processing plug-in standard"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/AcademySoftwareFoundation/openfx"
+    topics = ("graphics", "vfx", "image-processing", "plugins")
+    package_type = "static-library"
+
+    options = {
+        "fPIC": [True, False],
+    }
+
+    settings = "os", "arch", "compiler", "build_type"
+    default_options = {
+        "fPIC": True,
+        # "expat/*:shared": True,
+    }
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
+
+    def requirements(self):
+        # Symbols used in public headers
+        self.requires("expat/[>=2.6.2 <3]", transitive_headers=True)
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.16]")
+
+    def validate(self):
+        check_min_cppstd(self, 17)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        tc = CMakeToolchain(self)
+        if self.settings.os == "Windows":
+            tc.preprocessor_definitions["WINDOWS"] = 1
+            tc.preprocessor_definitions["NOMINMAX"] = 1
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_build_modules", [os.path.join("lib", "cmake", "OpenFX", "OpenFX.cmake")])
+
+        self.cpp_info.components["Api"].includedirs = ["include"]
+        self.cpp_info.components["HostSupport"].libs = ["OfxHost"]
+        self.cpp_info.components["HostSupport"].includedirs = [os.path.join("include", "HostSupport")]
+        self.cpp_info.components["HostSupport"].requires = ["Api", "expat::expat"]
+        self.cpp_info.components["Support"].libs = ["OfxSupport"]
+        self.cpp_info.components["Support"].includedirs = [os.path.join("include", "Support")]
+        self.cpp_info.components["Support"].requires = ["Api"]
+
+        if self.settings.os == "Windows":
+            win_defines = ["WINDOWS", "NOMINMAX"]
+            self.cpp_info.components["Api"].defines = win_defines
+            self.cpp_info.components["HostSupport"].defines = win_defines
+            self.cpp_info.components["Support"].defines = win_defines
+        elif self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.components["HostSupport"].system_libs = ["dl"]
