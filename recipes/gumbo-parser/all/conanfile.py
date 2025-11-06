23ea560487543015aee99a6e9978166aa5ecd900
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-11-06 09:31:31 +0000

    gumbo parser: add missing import

--- recipes/gumbo-parser/all/conanfile.py
+++ recipes/gumbo-parser/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get, rm, rmdir
