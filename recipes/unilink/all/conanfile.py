d290ee6943c738d04902cd06bdf0227fab3105a9
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-11-06 11:05:40 +0100

    Cleanup, fix sha

--- recipes/unilink/all/conanfile.py
+++ recipes/unilink/all/conanfile.py
@@ -1,7 +1,9 @@
 from conan import ConanFile
-from conan.tools.files import copy, get
-from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
+from conan.tools.build import check_min_cppstd
 import os
 
 required_conan_version = ">=2.0"
@@ -14,8 +16,8 @@ class UnilinkConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/jwsung91/unilink"
     topics = ("async", "communication", "tcp", "serial", "networking", "c++17")
+    settings = "compiler"
     package_type = "header-library"
-    settings = None
     options = {
         "enable_config": [True, False],
         "enable_memory_tracking": [True, False],
@@ -24,47 +26,41 @@ class UnilinkConan(ConanFile):
         "enable_config": True,
         "enable_memory_tracking": True,
     }
-    
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            from conan.tools.build import check_min_cppstd
-            check_min_cppstd(self, 17)
-        
+        check_min_cppstd(self, 17)
+
         if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "7":
             raise ConanInvalidConfiguration(f"{self.ref} requires at least GCC 7")
-        
+
         if self.settings.compiler == "clang" and Version(self.settings.compiler.version) < "5":
             raise ConanInvalidConfiguration(f"{self.ref} requires at least Clang 5")
-    
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-    
+
     def requirements(self):
-        # Boost system library - use stable version
-        self.requires("boost/1.74.0")
-    
+        self.requires("boost/[>=1.83.0 <2]")
+
     def package(self):
         # Copy license files
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         copy(self, "NOTICE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
-        
+
         # Copy headers
         copy(self, "*.hpp", src=os.path.join(self.source_folder, "unilink"), dst=os.path.join(self.package_folder, "include", "unilink"))
         copy(self, "*.h", src=os.path.join(self.source_folder, "unilink"), dst=os.path.join(self.package_folder, "include", "unilink"))
-    
+
     def package_info(self):
         # Set target name
         self.cpp_info.set_property("cmake_target_name", "unilink::unilink")
         self.cpp_info.set_property("cmake_file_name", "unilink")
-        
-        # Set include directories
-        self.cpp_info.includedirs = ["include"]
-        
+
         # Set compile definitions
         if self.options.enable_config:
             self.cpp_info.defines.append("UNILINK_ENABLE_CONFIG=1")
         if self.options.enable_memory_tracking:
             self.cpp_info.defines.append("UNILINK_ENABLE_MEMORY_TRACKING=1")
-        
-        # Add Boost dependency
-        self.cpp_info.requires = ["boost::boost"]
--- recipes/unilink/all/test_package/conanfile.py
+++ recipes/unilink/all/test_package/conanfile.py
@@ -1,5 +1,8 @@
 from conan import ConanFile
+from conan.tools.build import can_run
 from conan.tools.cmake import CMake, cmake_layout
+
+import os
 
 
 class UnilinkTestConan(ConanFile):
@@ -19,5 +22,6 @@ class UnilinkTestConan(ConanFile):
         cmake_layout(self)
 
     def test(self):
-        cmake = CMake(self)
-        cmake.test()
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
