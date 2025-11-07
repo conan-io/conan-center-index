28d3a2c622cb2204362c5bd26b5f93954a84072d
Tolker-KU <rasmus@tolker.dk>
2025-11-07 16:46:17 +0100

    Allow for a range of abseil version in RE2

--- recipes/re2/all/conanfile.py
+++ recipes/re2/all/conanfile.py
@@ -38,7 +38,7 @@ class Re2Conan(ConanFile):
         if self.options.get_safe("with_icu"):
             self.requires("icu/73.2")
         if Version(self.version) >= "20230601":
-            self.requires("abseil/20240116.1", transitive_headers=True)
+            self.requires("abseil/[>=20240116.1]", transitive_headers=True)
 
     def validate(self):
         if Version(self.version) >= "20250805":
