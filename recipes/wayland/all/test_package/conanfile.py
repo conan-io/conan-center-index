8e15d0c3fab407eda07d98dc0cf6d97edab256d7
Andrew Marshall <planetmarshalluk@gmail.com>
2025-11-09 11:58:38 +0000

    dont test build requirement as fails CCI due to missing dep

--- recipes/wayland/all/test_package/conanfile.py
+++ recipes/wayland/all/test_package/conanfile.py
@@ -15,9 +15,6 @@ class TestPackageConan(ConanFile):
         self.requires(self.tested_reference_str)
 
     def build_requirements(self):
-        # test we can consume wayland scanner as a build requirement
-        self.tool_requires(self.tested_reference_str)
-
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/[2.2 <3]")
 
@@ -35,21 +32,11 @@ class TestPackageConan(ConanFile):
 
         assert tested_reference_version() == actual_version
 
-    def _test_wayland_scanner(self, env):
-        buffer = StringIO()
-        self.run(f"wayland-scanner --version", env=env, stderr=buffer)
-        output = buffer.getvalue().strip()
-        self.output.info(f"Wayland scanner output: {output}")
-        actual_version = output.split()[-1]
-        self._assert_expected_version(actual_version)
-
     def build(self):
         if self._has_libraries:
             cmake = CMake(self)
             cmake.configure()
             cmake.build()
-
-        self._test_wayland_scanner(env="conanbuild")
 
     def test(self):
         if can_run(self):
@@ -57,4 +44,9 @@ class TestPackageConan(ConanFile):
                 bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
                 self.run(bin_path, env="conanrun")
 
-            self._test_wayland_scanner(env="conanrun")
+            buffer = StringIO()
+            self.run(f"wayland-scanner --version", env="conanrun", stderr=buffer)
+            output = buffer.getvalue().strip()
+            self.output.info(f"Wayland scanner output: {output}")
+            actual_version = output.split()[-1]
+            self._assert_expected_version(actual_version)
