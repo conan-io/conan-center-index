5436eee096fcb65e4e2c8d29faa383500b6afcbf
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-11-14 06:20:12 +0900

    neko-schema: add new recipe

---
+++ recipes/neko-schema/all/conanfile.py
@@ -0,0 +1,76 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.files import copy
+import os
+
+
+class NekoSchemaConan(ConanFile):
+    name = "neko-schema"
+    version = "1.0.2"
+    license = "MIT OR Apache-2.0"
+    author = "moehoshio"
+    url = "https://github.com/moehoshio/NekoSchema"
+    description = "A lightweight C++20 header-only library providing fundamental type definitions and utilities for the Neko ecosystem"
+    topics = ("c++20", "header-only", "schema", "types", "auto-srcLoc" , "utilities")
+    
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "enable_module": [True, False],
+    }
+    default_options = {
+        "enable_module": False,
+    }
+    
+    # Header-only library
+    package_type = "header-library"
+    no_copy_source = True
+    
+    exports_sources = "CMakeLists.txt", "include/*", "tests/*", "LICENSE", "README.md"
+    
+    def layout(self):
+        cmake_layout(self)
+    
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["NEKO_SCHEMA_BUILD_TESTS"] = False
+        tc.variables["NEKO_SCHEMA_ENABLE_MODULE"] = self.options.enable_module
+        tc.variables["NEKO_SCHEMA_AUTO_FETCH_DEPS"] = False
+        tc.generate()
+        
+        deps = CMakeDeps(self)
+        deps.generate()
+    
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        if self.options.build_tests:
+            cmake.build()
+            cmake.test()
+    
+    def package(self):
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.hpp", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+        copy(self, "*.cppm", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+        
+        cmake = CMake(self)
+        cmake.install()
+    
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        
+        # Main header-only target
+        self.cpp_info.components["neko-schema"].set_property("cmake_target_name", "Neko::Schema")
+        self.cpp_info.components["neko-schema"].includedirs = ["include"]
+        self.cpp_info.components["neko-schema"].requires = []
+        
+        # C++20 requirements
+        self.cpp_info.components["neko-schema"].cxxflags = []
+        
+        # Module support (if enabled)
+        if self.options.build_module:
+            self.cpp_info.components["neko-schema-module"].set_property("cmake_target_name", "Neko::Schema::Module")
+            self.cpp_info.components["neko-schema-module"].includedirs = ["include"]
+    
+    def package_id(self):
+        self.info.clear()
