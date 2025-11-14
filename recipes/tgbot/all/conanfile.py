462aebdd1361e2cfa628168a41e88a52ff6570ae
Uilian Ries <uilianr@jfrog.com>
2025-11-14 13:51:29 +0100

    Use version range for Boost
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/tgbot/all/conanfile.py
+++ recipes/tgbot/all/conanfile.py
@@ -69,7 +69,7 @@ class TgbotConan(ConanFile):
 
     def requirements(self):
         # tgbot/Api.h:#include <boost/property_tree/ptree.hpp>
-        self.requires("boost/1.87.0", transitive_headers=True)
+        self.requires("boost/[>=1.83.0 <1.90.0]", transitive_headers=True)
         # tgbot/net/CurlHttpClient.h:#include <curl/curl.h>
         self.requires("libcurl/[>=7.78 <9]", transitive_headers=True, transitive_libs=True)
         self.requires("openssl/[>=1.1 <4]")
