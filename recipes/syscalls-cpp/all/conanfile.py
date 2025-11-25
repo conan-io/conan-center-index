6d3f39e301f11aca7e91dfd5544bad25fd4da5ba
Uilian Ries <uilianr@jfrog.com>
2025-11-25 13:55:13 +0100

    Require Conan 2.x
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/syscalls-cpp/all/conanfile.py
+++ recipes/syscalls-cpp/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.layout import basic_layout
 from conan.tools.build import check_min_cppstd
 import os
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.1.0"
 
 class SyscallsCppConan(ConanFile):
     name = "syscalls-cpp"
