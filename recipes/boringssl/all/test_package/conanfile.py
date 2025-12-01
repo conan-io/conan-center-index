7d10f447878bc0bb6d10fffdd99a225e328ab4cc
Uilian Ries <uilianr@jfrog.com>
2025-12-01 12:06:42 +0100

    Use test_package/conanfile.py template
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/boringssl/all/test_package/conanfile.py
+++ recipes/boringssl/all/test_package/conanfile.py
@@ -1,18 +1,18 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 import os
 
 
-class TestPackage(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = ("CMakeToolchain", "CMakeDeps")
-    test_type = "explicit"
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def layout(self):
         cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -20,6 +20,6 @@ class TestPackage(ConanFile):
         cmake.build()
 
     def test(self):
-        if not self.conf.get("tools.build:skip_test", default=False):
-            exe = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(exe, env="conanrun")
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(bin_path, env="conanrun")
