7e76eab06c4a8b2921118db2b86522ce39957775
Francisco Ramirez de Anton <franchuti688@gmail.com>
2025-12-02 15:31:32 +0100

    Linter and comment

--- recipes/onnx/all/conanfile.py
+++ recipes/onnx/all/conanfile.py
@@ -5,7 +5,7 @@ from conan import ConanFile
 from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rmdir, replace_in_file, export_conandata_patches, apply_conandata_patches
+from conan.tools.files import copy, get, rmdir, export_conandata_patches, apply_conandata_patches
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 
 required_conan_version = ">=2.0.5"
