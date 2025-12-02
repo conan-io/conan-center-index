c41f20fb76c3d5eeebd020847bb94a77b1ef78fe
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-12-02 21:24:18 +0100

    Add version range for libtiff, necessary to remove conflicts with openscenegrapph

--- recipes/libgeotiff/all/conanfile.py
+++ recipes/libgeotiff/all/conanfile.py
@@ -44,7 +44,7 @@ class LibgeotiffConan(ConanFile):
 
     def requirements(self):
         # libgeotiff/include/xtiffio.h includes libtiff/include/tiffio.h
-        self.requires("libtiff/4.6.0", transitive_headers=True, transitive_libs=True)
+        self.requires("libtiff/[>=4.6.0 <5]", transitive_headers=True, transitive_libs=True)
         self.requires("proj/9.3.1")
 
     def source(self):
