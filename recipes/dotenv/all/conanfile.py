3f92b0ccd23e1a90c502458ac711bd5f14c6ed7c
Ayush Acharjya <ayushacharjya@gmail.com>
2025-12-03 20:12:40 +0000

    Add C++23 validation and complete test_package

--- recipes/dotenv/all/conanfile.py
+++ recipes/dotenv/all/conanfile.py
@@ -1,6 +1,9 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2.0"
@@ -16,11 +19,34 @@ class DotenvConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
 
+    @property
+    def _min_cppstd(self):
+        return 23
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "12",
+            "clang": "15",
+            "apple-clang": "16",
+            "msvc": "193",
+            "Visual Studio": "17",
+        }
+
     def layout(self):
         basic_layout(self, src_folder="src")
 
     def package_id(self):
         self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
---
+++ recipes/dotenv/all/test_package/test_package.cpp
@@ -0,0 +1,9 @@
+#include <dotenv/dotenv.hpp>
+#include <iostream>
+
+int main()
+{
+    // Basic test to verify the library can be included and linked
+    std::cout << "dotenv library successfully included\n";
+    return 0;
+}
