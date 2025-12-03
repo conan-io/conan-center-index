d4901c5c63d93dd766614aba24095f067d551827
Francisco Ramírez <franchuti688@gmail.com>
2025-12-03 10:52:31 +0100

    Update recipes/activemq-cpp/all/conanfile.py

--- recipes/activemq-cpp/all/conanfile.py
+++ recipes/activemq-cpp/all/conanfile.py
@@ -1,12 +1,13 @@
+import os
+
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.build import check_max_cppstd, check_min_cppstd, cross_building
 from conan.tools.env import VirtualRunEnv
 from conan.tools.files import copy, get, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain, PkgConfigDeps
 from conan.tools.layout import basic_layout
-import os
+
 required_conan_version = ">=2.1"
 
 class PackageConan(ConanFile):
