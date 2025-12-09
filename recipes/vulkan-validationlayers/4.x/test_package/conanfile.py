9b13eb15b59c3e6e820cc8c3cade435911e52dfd
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-12-09 12:57:40 +0100

    No need to keep comented code around

--- recipes/vulkan-validationlayers/4.x/test_package/conanfile.py
+++ recipes/vulkan-validationlayers/4.x/test_package/conanfile.py
@@ -14,7 +14,6 @@ class TestPackageConan(ConanFile):
 
     def requirements(self):
         self.requires(self.tested_reference_str)
-        #self.requires("vulkan-loader/1.4.328.1")
 
     def build(self):
         cmake = CMake(self)
