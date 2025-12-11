1f59c97fa0cbae95f84366285599581f37d2a048
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-12-11 12:34:59 +0000

    canary and mfast: use newer boost

--- recipes/mfast/all/conanfile.py
+++ recipes/mfast/all/conanfile.py
@@ -38,23 +38,6 @@ class mFASTConan(ConanFile):
     }
     short_paths = True
 
-    @property
-    def _min_cppstd(self):
-        return "14" if Version(self.version) >= "1.2.2" else "98"
-
-    @property
-    def _compilers_minimum_version(self):
-        if Version(self.version) >= "1.2.2":
-            return {
-                "gcc": "6",
-                "Visual Studio": "14",
-                "msvc": "190",
-                "clang": "3.4",
-                "apple-clang": "5.1",
-            }
-        else:
-            return {}
-
     def export_sources(self):
         export_conandata_patches(self)
 
@@ -71,26 +54,13 @@ class mFASTConan(ConanFile):
 
     def requirements(self):
         # transitive_headers=True because mfast/mfast_export.h includes boost/config.hpp
-        self.requires("boost/1.75.0", transitive_headers=True)
+        self.requires("boost/[>=1.75.0 <=1.89.0]", transitive_headers=True)
         self.requires("tinyxml2/9.0.0")
         if self.options.with_sqlite3:
             self.requires("sqlite3/[>=3.43 <4]")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
-
-        def loose_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
-
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 14)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -101,12 +71,6 @@ class mFASTConan(ConanFile):
         tc.variables["BUILD_EXAMPLES"] = False
         tc.variables["BUILD_PACKAGES"] = False
         tc.variables["BUILD_SQLITE3"] = self.options.with_sqlite3
-        if not valid_min_cppstd(self, self._min_cppstd):
-            tc.variables["CMAKE_CXX_STANDARD"] = self._min_cppstd
-        if Version(self.version) <= "1.2.2": # pylint: disable=conan-condition-evals-to-constant
-            # Relocatable shared libs on macOS
-            tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
-            tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5" # CMake 4 support
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
