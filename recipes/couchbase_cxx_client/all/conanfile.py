b055ef4c964dc0e10a5e00711b33fee7ee57d44b
Sergio Haro <sharo@quickbase.com>
2025-12-12 10:10:48 -0800

    need to use underscore for name to work

--- recipes/couchbase_cxx_client/all/conanfile.py
+++ recipes/couchbase_cxx_client/all/conanfile.py
@@ -8,7 +8,7 @@ import re
 required_conan_version = ">=2.0.9"
 
 class CouchbaseCxxClientConan(ConanFile):
-    name = "couchbase-cxx-client"
+    name = "couchbase_cxx_client"
     description = "Couchbase C++ SDK"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
