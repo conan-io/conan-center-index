ad0e3dce2d915279f46fd193c7554a6261b1e3aa
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2025-12-12 14:58:05 +0100

    wip

--- recipes/pistache/all/conanfile.py
+++ recipes/pistache/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, replace_in_file, collect_libs
+from conan.tools.files import get, copy, rm, rmdir, replace_in_file, collect_libs
 from conan.tools.build import check_min_cppstd
 from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.meson import Meson, MesonToolchain
