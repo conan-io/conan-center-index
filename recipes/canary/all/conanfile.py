c36c96456621c661f7907836b9183a1d5334cc47
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-12-12 10:29:56 +0000

    canary and mfast: use newer boost (#29096)
    * canary and mfast: use newer boost
    
    * mfast: backport fix for clang build
    
    * mfast: fix_apple_shared_install_name
    
    * mfast: cleanup
    
    * libraw: versions

--- recipes/canary/all/conanfile.py
+++ recipes/canary/all/conanfile.py
@@ -5,6 +5,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rmdir
+from conan.tools.scm import Version
 
 required_conan_version = ">=1.53.0"
 
@@ -20,15 +21,11 @@ class SocketcanCanaryConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     no_copy_source = True
 
-    @property
-    def _min_cppstd(self):
-        return 11
-
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.74.0")
+        self.requires("boost/[>=1.74.0 <=1.89.0]")
 
     def package_id(self):
         self.info.clear()
@@ -36,8 +33,8 @@ class SocketcanCanaryConan(ConanFile):
     def validate(self):
         if self.settings.os != "Linux":
             raise ConanInvalidConfiguration(f"{self.ref} only supports Linux.")
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
+        
+        check_min_cppstd(self, 11)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -45,8 +42,10 @@ class SocketcanCanaryConan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.generate()
-        tc = CMakeDeps(self)
-        tc.generate()
+        deps = CMakeDeps(self)
+        if Version(self.dependencies["boost"].ref.version) >= "1.89.0":
+            deps.set_property("boost::headers", "cmake_target_aliases", ["Boost::system"])
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -60,7 +59,7 @@ class SocketcanCanaryConan(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "lib"))
 
     def package_info(self):
-        self.cpp_info.requires = ["boost::headers", "boost::system"]
+        self.cpp_info.requires = ["boost::headers"]
         self.cpp_info.set_property("cmake_file_name", "canary")
         self.cpp_info.set_property("cmake_target_name", "canary::canary")
         self.cpp_info.bindirs = []
