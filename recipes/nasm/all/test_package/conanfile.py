859c177f060a9b6d7a85f8f61a08938565a99344
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2025-12-17 09:54:30 +0000

    fixup

--- recipes/nasm/all/test_package/conanfile.py
+++ recipes/nasm/all/test_package/conanfile.py
@@ -4,7 +4,7 @@ from conan import ConanFile
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
-    def build_requirements(self):
+    def requirements(self):
         self.requires(self.tested_reference_str)
 
     def test(self):
