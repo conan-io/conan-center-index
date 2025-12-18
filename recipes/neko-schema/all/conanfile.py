4e624fadc1602ada9f585148333b1fcee85bff5f
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-12-19 06:26:12 +0900

    neko-schema: update to version 1.1.5 and remove previous versions

--- recipes/neko-schema/all/conanfile.py
+++ recipes/neko-schema/all/conanfile.py
@@ -1,77 +1,66 @@
 ﻿from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
-from conan.tools.files import copy
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, rmdir
+from conan.tools.build import check_min_cppstd
 import os
+
+
+required_conan_version = ">=2.1"
 
 
 class NekoSchemaConan(ConanFile):
     name = "neko-schema"
-    version = "1.0.4"
     license = "MIT OR Apache-2.0"
-    author = "moehoshio"
-    url = "https://github.com/moehoshio/NekoSchema"
+    homepage = "https://github.com/moehoshio/NekoSchema"
+    url = "https://github.com/conan-io/conan-center-index"
     description = "A lightweight C++20 header-only library providing fundamental type definitions and utilities for the Neko ecosystem"
-    topics = ("c++20", "header-only", "schema", "types", "auto-srcLoc" , "utilities")
-    
+    topics = ("c++20", "header-only", "schema", "types", "auto-srcLoc", "utilities")
     settings = "os", "compiler", "build_type", "arch"
-    options = {
-        "enable_module": [True, False],
-    }
-    default_options = {
-        "enable_module": False,
-    }
-    
-    # Header-only library
+    exports_sources = "CMakeLists.txt", "include/*", "cmake/*", "LICENSE", "README.md"
+
     package_type = "header-library"
-    no_copy_source = True
-    
-    exports_sources = "CMakeLists.txt", "include/*", "tests/*", "LICENSE", "README.md"
-    
+
     def layout(self):
-        cmake_layout(self)
-    
+        cmake_layout(self, src_folder="src")
+
+    def source(self):
+        data = self.conan_data or {}
+        sources = data.get("sources", {})
+
+        # Use remote source when available; otherwise fall back to exported sources (local workflow)
+        if self.version in sources:
+            get(self, **sources[self.version], strip_root=True)
+        else:
+            copy(self, pattern="*", src=self.export_sources_folder, dst=self.source_folder)
+
+    def validate(self):
+        check_min_cppstd(self, 20)
+
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["NEKO_SCHEMA_BUILD_TESTS"] = False
-        tc.variables["NEKO_SCHEMA_ENABLE_MODULE"] = self.options.enable_module
+        tc.variables["NEKO_SCHEMA_ENABLE_MODULE"] = False
         tc.variables["NEKO_SCHEMA_AUTO_FETCH_DEPS"] = False
         tc.generate()
-        
-        deps = CMakeDeps(self)
-        deps.generate()
-    
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
 
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
-        copy(self, "*.hpp", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
-        copy(self, "*.cppm", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
-        
         cmake = CMake(self)
         cmake.install()
-    
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+
     def package_info(self):
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
-        
+
         # Set the CMake package file name to match the official CMake config
         self.cpp_info.set_property("cmake_file_name", "NekoSchema")
-        
-        # Main header-only target
-        self.cpp_info.components["NekoSchema"].set_property("cmake_target_name", "Neko::Schema")
-        self.cpp_info.components["NekoSchema"].includedirs = ["include"]
-        self.cpp_info.components["NekoSchema"].requires = []
-        
-        # C++20 requirements
-        self.cpp_info.components["NekoSchema"].cxxflags = []
-        
-        # Module support (if enabled)
-        if self.options.enable_module:
-            self.cpp_info.components["NekoSchemaModule"].set_property("cmake_target_name", "Neko::Schema::Module")
-            self.cpp_info.components["NekoSchemaModule"].includedirs = ["include"]
-    
+        self.cpp_info.set_property("cmake_target_name", "NekoSchema")
+        self.cpp_info.set_property("cmake_target_aliases", ["Neko::Schema"])
+
     def package_id(self):
         self.info.clear()
-
