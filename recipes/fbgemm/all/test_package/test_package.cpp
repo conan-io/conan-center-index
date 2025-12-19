7cc1ebecb65971e4c92837cde97605f7d3adf5c4
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-12-19 10:23:19 +0100

    Simplify test package for all archs

--- recipes/fbgemm/all/test_package/test_package.cpp
+++ recipes/fbgemm/all/test_package/test_package.cpp
@@ -1,9 +1,7 @@
-#include <vector>
+#include <iostream>
 #include <fbgemm/Fbgemm.h>
 
 int main() {
-    auto data = std::vector<std::int8_t>{1, 2, 3, 4, 5, 6};
-    fbgemm::PackBMatrix<std::int8_t> matrix(fbgemm::matrix_op_t::Transpose, 3, 2, data.data(), 3);
-    matrix.printPackedMatrix("Packed Matrix");
+    std::cout << "Supported CPU?: " << fbgemm::fbgemmSupportedCPU() << std::endl;
     return 0;
 }
