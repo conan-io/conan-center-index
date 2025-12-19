657fb244a8dd636bf4934f5aeba0653f44fa597c
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-12-19 09:57:43 +0100

    Test that runs a function

--- recipes/d3d12-memory-allocator/all/test_package/test_package.cpp
+++ recipes/d3d12-memory-allocator/all/test_package/test_package.cpp
@@ -1,10 +1,10 @@
 #include <D3D12MemAlloc.h>
-
+#include <iostream>
 int main() {
   // A simple test to check the ability to link with the library
-  D3D12MA::ALLOCATION_DESC allocDesc = {};
-  (void)allocDesc;
-  D3D12MA::Allocator *allocator = nullptr;
-  (void)allocator;
+  D3D12MA::ALLOCATOR_DESC allocatorDesc = {};
+  D3D12MA::Allocator* allocator;
+  HRESULT hr = D3D12MA::CreateAllocator(&allocatorDesc, &allocator);
+  std::cout << "Alloactor result: " << hr << std::endl;
   return 0;
 }
