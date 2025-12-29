19159b89574f29582c7e47424d9349e3024e457c
Carlos Zoido <mrgalleta@gmail.com>
2025-12-29 18:26:08 +0100

    minor fixes

--- recipes/alpaka/all/conanfile.py
+++ recipes/alpaka/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.files import copy, get, rmdir
 import os
 
