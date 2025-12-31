bae028c52b470d57ceab1352b43956519432c460
Fernando Vilas <fvilas@iname.com>
2025-12-31 16:42:32 -0600

    pylint the import order

--- recipes/coin-cbc/all/conanfile.py
+++ recipes/coin-cbc/all/conanfile.py
@@ -1,10 +1,10 @@
+import os
 from conan import ConanFile
 from conan.tools.files import copy, get, mkdir, rename, rmdir
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.gnu import Autotools, AutotoolsToolchain, PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import is_msvc, msvc_runtime_flag, unix_path
-import os
 
 required_conan_version = ">=2.1.0"
 
