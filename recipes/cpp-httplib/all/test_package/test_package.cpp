265df27df41198bbf1747e3ab32c1cf7723d3573
PerseoGI <perseog@jfrog.com>
2026-01-02 10:18:57 +0100

    Restore test_package

--- recipes/cpp-httplib/all/test_package/test_package.cpp
+++ recipes/cpp-httplib/all/test_package/test_package.cpp
@@ -1,12 +1,6 @@
-// #include <httplib.h>
-//
-// int main() {
-//   httplib::Request request;
-//   return 0;
-// }
 #include <httplib.h>
+
 int main() {
-  httplib::Client cli("http://example.com");
-  auto res = cli.Get("/");
+  httplib::Request request;
   return 0;
 }
