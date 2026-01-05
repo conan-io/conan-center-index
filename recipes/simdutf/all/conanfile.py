6fc37a67f83d3cc2ee9d6ec9bac916594663b860
PerseoGI <perseog@jfrog.com>
2026-01-05 17:51:12 +0100

    scnlib, simdutf: stop publishing new revisions for older versions (#29287)
    * Stop publishing new recipe revisions for old scnlib and simdutf versions
    
    * Restore latest 3.x major for scnlib

--- recipes/simdutf/all/conanfile.py
+++ recipes/simdutf/all/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.scm import Version
 
 import os
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.1"
 
 class SimdutfConan(ConanFile):
     name = "simdutf"
@@ -27,10 +27,6 @@ class SimdutfConan(ConanFile):
         "fPIC": True,
     }
 
-    @property
-    def _min_cppstd(self):
-        return 11
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -43,8 +39,7 @@ class SimdutfConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        if self.info.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
+        check_min_cppstd(self, 11)
         if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "9.0":
             raise ConanInvalidConfiguration(f"{self.ref} doesn't support gcc < 9.")
         if self.settings.compiler == "gcc" and self.settings.build_type == "Debug" and \
