38ab7e71faafc67aee2c1bdf22d309fd2a52fa8a
Uilian Ries <uilianr@jfrog.com>
2026-01-05 10:09:36 +0100

    Remove unused imports
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/spdlog/all/conanfile.py
+++ recipes/spdlog/all/conanfile.py
@@ -3,7 +3,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.files import get, copy, rmdir, replace_in_file, apply_conandata_patches, export_conandata_patches
-from conan.tools.microsoft import check_min_vs, is_msvc, is_msvc_static_runtime
+from conan.tools.microsoft import is_msvc_static_runtime
 from conan.tools.scm import Version
 import os
 
