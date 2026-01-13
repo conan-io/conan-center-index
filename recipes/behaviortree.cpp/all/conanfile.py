4ba6b4c72b329edcb14f97c2caa93b4f848883aa
Riff, Eric <ericriff@gmail.com>
2026-01-13 11:15:04 -0300

    Fix cmake_file_name

--- recipes/behaviortree.cpp/all/conanfile.py
+++ recipes/behaviortree.cpp/all/conanfile.py
@@ -126,7 +126,7 @@ class BehaviorTreeCPPConan(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.set_property("cmake_file_name", "BehaviorTree")
+        self.cpp_info.set_property("cmake_file_name", "behaviortree_cpp")
         self.cpp_info.set_property("cmake_target_name", "BT::behaviortree_cpp")
 
         postfix = "d" if self.settings.os == "Windows" and self.settings.build_type == "Debug" else ""
