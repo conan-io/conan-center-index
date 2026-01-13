bec43eb35b85d03fd97c75258bd67533301afd5b
Fernando Vilas <fvilas@iname.com>
2026-01-13 17:18:15 -0600

    Cleanup dep tree more

--- recipes/ortools/all/conanfile.py
+++ recipes/ortools/all/conanfile.py
@@ -34,17 +34,17 @@ class PackageConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("abseil/20250814.0", transitive_headers=True, transitive_libs=True) # source is 20250814.1 which conflicts with protobuf 32.1, and that needs an upgrade anyway
-        #self.requires("boost/1.87.0", override=True) # build this because conan version lacks Boost::multiprecision or Boost::serialization
+        self.requires("abseil/20250814.0", transitive_headers=True, transitive_libs=True) # source is 20250814.1 which conflicts with protobuf 32.1 at 20250814.0
+        #self.requires("boost/1.87.0") # build this because conan version lacks Boost::multiprecision or Boost::serialization
         self.requires("bzip2/1.0.8")
-        #self.requires("coin-clp/[>=1.17.0]")
-        #self.requires("coin-osi/[>=0.108.7]") # source is 0.108.11
-        #self.requires("coin-utils/[>=2.11.09]") # source is 2.11.12
-        self.requires("coin-cbc/[>=2.10.5]") # source is 2.10.12
+        self.requires("coin-clp/[>=1.17.7 <=1.17.10]") # source is 1.17.10
+        self.requires("coin-osi/[>=0.108.7 <=0.108.11]") # source is 0.108.11
+        self.requires("coin-utils/[>=2.11.09 <=2.11.12]") # source is 2.11.12
+        self.requires("coin-cbc/[>=2.10.5 <=2.10.12]") # source is 2.10.12
         self.requires("eigen/3.4.0")
         self.requires("highs/1.12.0")
         self.requires("protobuf/6.32.1", transitive_headers=True, transitive_libs=True) # source is 33.1
-        self.requires("re2/[>=20250812]") # source is 20250812
+        self.requires("re2/[>=20250812]") # source is 20250812, but only 20250722 or 20251105 are in conan center
         #self.requires("scip/10.0.0") # build this due to not having SCIP::libscip target
         #self.requires("soplex/[>=7.1.3]") # build this due to conflicts in lib name for targets
         self.requires("zlib/1.3.1")
