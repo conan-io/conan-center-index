77528823a5d43d16e420761f219fe77022421b21
Fernando Vilas <fvilas@iname.com>
2026-01-13 20:21:34 -0600

    Remove transitive_libs calls

--- recipes/ortools/all/conanfile.py
+++ recipes/ortools/all/conanfile.py
@@ -34,7 +34,7 @@ class PackageConan(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("abseil/20250814.0", transitive_headers=True, transitive_libs=True) # source is 20250814.1 which conflicts with protobuf 32.1 at 20250814.0
+        self.requires("abseil/20250814.0", transitive_headers=True) # source is 20250814.1 which conflicts with protobuf 32.1 at 20250814.0
         #self.requires("boost/1.87.0") # build this because conan version lacks Boost::multiprecision or Boost::serialization
         self.requires("bzip2/1.0.8")
         self.requires("coin-clp/[>=1.17.7 <=1.17.10]") # source is 1.17.10
@@ -43,13 +43,13 @@ class PackageConan(ConanFile):
         self.requires("coin-cbc/[>=2.10.5 <=2.10.12]") # source is 2.10.12
         self.requires("eigen/3.4.0")
         self.requires("highs/1.12.0")
-        self.requires("protobuf/6.32.1", transitive_headers=True, transitive_libs=True) # source is 33.1
+        self.requires("protobuf/6.32.1", transitive_headers=True) # source is 33.1
         self.requires("re2/[>=20250812]") # source is 20250812, but only 20250722 or 20251105 are in conan center
         #self.requires("scip/10.0.0") # build this due to not having SCIP::libscip target
         #self.requires("soplex/[>=7.1.3]") # build this due to conflicts in lib name for targets
         self.requires("zlib/1.3.1")
         if self.options.with_glpk:
-            self.requires("glpk/5.0", transitive_headers=True, transitive_libs=True)
+            self.requires("glpk/5.0")
 
     def validate(self):
         if is_msvc(self):
