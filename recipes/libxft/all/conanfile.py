7dccac1f4276b993337b268f4ddfb9e1a35add6a
Uilian Ries <uilianr@jfrog.com>
2026-01-15 17:18:32 +0100

    Add version 2.3.9 for libxft
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/libxft/all/conanfile.py
+++ recipes/libxft/all/conanfile.py
@@ -1,11 +1,13 @@
 import os
 
 from conan import ConanFile
-from conan.tools.files import apply_conandata_patches, chdir, copy, export_conandata_patches, get, rm, rmdir
-from conan.tools.gnu import Autotools, AutotoolsToolchain, PkgConfigDeps
+from conan.tools.files import chdir, copy, get, rm, rmdir
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.meson import Meson, MesonToolchain
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.1"
 
 
 class libxftConan(ConanFile):
@@ -27,9 +29,6 @@ class libxftConan(ConanFile):
         "fPIC": True,
     }
 
-    def export_sources(self):
-        export_conandata_patches(self)
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -44,39 +43,37 @@ class libxftConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("xorg/system")
-        self.requires("freetype/2.13.2", transitive_headers=True)
-        self.requires("fontconfig/2.15.0", transitive_headers=True)
+        self.requires("xorg/system") # Xorg is for libX11 and xrender
+        self.requires("freetype/[>=2.13.2]", transitive_headers=True)
+        self.requires("fontconfig/[>=2.15.0]", transitive_headers=True)
+        self.requires("xorg-proto/2024.1", transitive_headers=True)
 
     def build_requirements(self):
+        self.tool_requires("meson/[>=1.3.1 <2]")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
-            self.tool_requires("pkgconf/2.1.0")
-        self.tool_requires("xorg-macros/1.20.0")
-        self.tool_requires("libtool/2.4.7")
+            self.tool_requires("pkgconf/[>=2.1.0 <3]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def generate(self):
-        tc = AutotoolsToolchain(self)
-        tc.configure_args.append("--disable-dependency-tracking")
-        tc.generate()
+    def generate(self):        
         tc = PkgConfigDeps(self)
+        tc.generate()
+        tc = MesonToolchain(self)
         tc.generate()
 
     def build(self):
-        apply_conandata_patches(self)
-        with chdir(self, self.source_folder):
-            autotools = Autotools(self)
-            autotools.configure()
-            autotools.make()
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
 
     def package(self):
         copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
         copy(self, pattern="COPYING", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
-        with chdir(self, self.source_folder):
-            autotools = Autotools(self)
-            autotools.install(args=["-j1"])
+
+        meson = Meson(self)
+        meson.install()
+
         rm(self, "*.la", f"{self.package_folder}/lib", recursive=True)
         rmdir(self, f"{self.package_folder}/lib/pkgconfig")
         rmdir(self, f"{self.package_folder}/share")
