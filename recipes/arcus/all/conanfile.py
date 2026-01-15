d394524a8c78150aae1d3ac2967dcf54d19d3795
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2026-01-15 11:09:42 +0000

    arcus: remove unused imports

--- recipes/arcus/all/conanfile.py
+++ recipes/arcus/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir, replace_in_file
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 from conan.tools.scm import Version
 import os
