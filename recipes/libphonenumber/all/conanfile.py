4c069da9e0b9740ba5fe994e2b1d4e2a5210569b
Ernesto de Gracia Herranz <ernestodegraciah@gmail.com>
2026-01-15 17:30:08 +0100

    raise range

--- recipes/libphonenumber/all/conanfile.py
+++ recipes/libphonenumber/all/conanfile.py
@@ -76,7 +76,7 @@ class LibphonenumberConan(ConanFile):
 
     def requirements(self):
         # https://github.com/google/libphonenumber/blob/v8.13.35/cpp/src/phonenumbers/phonenumberutil.h#L33-L34
-        self.requires("abseil/[>=20240116.2  <=20250127.0]", transitive_headers=True)
+        self.requires("abseil/[>=20240116.2  <=20250814.0]", transitive_headers=True)
         self.requires("protobuf/[>=5.27.0 <7]", transitive_headers=True, transitive_libs=True)
         if self.options.use_boost:
             # https://github.com/google/libphonenumber/blob/v8.13.35/cpp/src/phonenumbers/base/synchronization/lock_boost.h
