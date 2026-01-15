0b50b99f75e1fea67b1da9f769f49e638571e549
Ingmar Rieger <git@irieger.net>
2026-01-15 20:09:25 +0100

    libraw: Add optional file size limit setting for libraw 0.22+
    Implementation of https://github.com/LibRaw/LibRaw/issues/693

--- recipes/libraw/all/test_package/test_package.cpp
+++ recipes/libraw/all/test_package/test_package.cpp
@@ -1,8 +1,12 @@
 #include <libraw/libraw.h>
+#include <libraw/libraw_version.h>
 #include <iostream>
 
 int main()
 {
     std::cout << libraw_version() << "\n";
+    #if LIBRAW_MAJOR_VERSION >= 0 && LIBRAW_MINOR_VERSION >= 22
+        std::cout << "Maximum Canon CR3/CRM file size: " << LIBRAW_MAX_CR3_RAW_FILE_SIZE << "\n";
+    #endif
     return 0;
 }
