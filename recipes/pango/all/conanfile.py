4395498346012ce568ccd30d1e131fa519f7493a
Uilian Ries <uilianr@jfrog.com>
2026-01-21 07:53:32 +0100

    Use version ranges for Cairo
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/pango/all/conanfile.py
+++ recipes/pango/all/conanfile.py
@@ -78,7 +78,7 @@ class PangoConan(ConanFile):
             self.requires("xorg/system")  # for xorg::xrender
         if self.options.with_cairo:
             # "pango/pangocairo.h" includes "cairo.h"
-            self.requires("cairo/1.18.0", transitive_headers=True)
+            self.requires("cairo/[>=1.18.0 <2]", transitive_headers=True)
         self.requires("glib/[>=2.82 <3]", transitive_headers=True, transitive_libs=True)
         self.requires("fribidi/1.0.13")
         # "pango/pango-coverage.h" includes "hb.h"
