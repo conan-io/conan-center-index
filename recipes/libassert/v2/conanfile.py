8e6a98646a8c3455b42f39811a5bf913a9391472
Uilian Ries <uilianr@jfrog.com>
2026-01-22 16:07:31 +0100

    Require Conan 2.x
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/libassert/v2/conanfile.py
+++ recipes/libassert/v2/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 import os
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.1"
 
 class LibassertConan(ConanFile):
     name = "libassert"
