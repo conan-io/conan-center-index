756a32e5e46e632b4d5e16de6f08089df14c8760
Uilian Ries <uilianr@jfrog.com>
2026-01-22 13:29:20 +0100

    Require Conan 2.x by default
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/cpptrace/all/conanfile.py
+++ recipes/cpptrace/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 import os
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.1"
 
 class CpptraceConan(ConanFile):
     name = "cpptrace"
