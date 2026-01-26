dc867bc25c7378bc23d4ab3bd8fcfb86a6568a31
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-01-26 12:57:24 +0100

    Apply suggestion from @AbrilRBS

--- recipes/seqan/2.x.x/conanfile.py
+++ recipes/seqan/2.x.x/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=2"
 
 
 class SeqanConan(ConanFile):
