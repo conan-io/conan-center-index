397eae19131ab6e1f84a1a1a0580cd4823941a75
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-01-29 21:25:15 +0100

    Make linter happy

--- recipes/rapidcheck/all/conanfile.py
+++ recipes/rapidcheck/all/conanfile.py
@@ -4,7 +4,6 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rm, rmdir, save
 from conan.tools.microsoft import is_msvc
-from conan.tools.scm import Version
 from os.path import join
 import textwrap
 
