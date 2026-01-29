2b95e878b221e4ca3c1bbec334d6daa2db5ccc34
Gang Wu <ustcwg@gmail.com>
2026-01-29 22:23:11 +0800

    simplify test

--- recipes/casbin/all/test_package/test_package.cpp
+++ recipes/casbin/all/test_package/test_package.cpp
@@ -1,17 +1,9 @@
 #include <cstdlib>
 #include <iostream>
-#include <string>
-#include <vector>
 
 #include <casbin/casbin.h>
 
 int main() {
-    std::vector<std::string> a = {"alice", "bob"};
-    std::vector<std::string> b = {"alice", "bob"};
-    
-    if (casbin::ArrayEquals(a, b) && casbin::Join(a, ",") == "alice,bob") {
-        std::cout << "casbin test ok" << std::endl;
-        return EXIT_SUCCESS;
-    }
-    return EXIT_FAILURE;
+  std::cout << casbin::Join({"Test", "OK"}, " ") << std::endl;
+  return EXIT_SUCCESS;
 }
