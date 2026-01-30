44e4858849fcf7b4889bd1f426e3d55fe9f341dc
Fernando Vilas <fvilas@iname.com>
2026-01-30 08:12:22 -0600

    add missing import replace_in_file

--- recipes/or-tools/all/conanfile.py
+++ recipes/or-tools/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rm, rmdir
+from conan.tools.files import copy, get, replace_in_file, rm, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 import os
