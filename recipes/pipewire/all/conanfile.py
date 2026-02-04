fc23add8e4a2303e4b66e3b22539e4a0b8a0bad6
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2026-02-04 12:09:33 +0100

    fix import linter

--- recipes/pipewire/all/conanfile.py
+++ recipes/pipewire/all/conanfile.py
@@ -1,12 +1,9 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.apple import fix_apple_shared_install_name, is_apple_os
-from conan.tools.build import check_min_cppstd
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
+from conan.tools.files import copy, get, rm, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.layout import basic_layout
 from conan.tools.meson import Meson, MesonToolchain
-from conan.tools.microsoft import is_msvc
 import os
 
 
