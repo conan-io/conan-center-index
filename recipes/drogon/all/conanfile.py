d3d6e1f1af88c42f99db4da7cac7cb3543667a0c
Uilian Ries <uilianr@jfrog.com>
2026-02-06 08:41:03 +0100

    Keep logic for newer trantor versions
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/drogon/all/conanfile.py
+++ recipes/drogon/all/conanfile.py
@@ -72,10 +72,7 @@ class DrogonConan(ConanFile):
         check_min_cppstd(self, 17)
 
     def requirements(self):
-        if Version(self.version) <= "1.9.11":
-            self.requires("trantor/[>=1.5.21 <1.5.25]", transitive_headers=True, transitive_libs=True)
-        else:
-            self.requires("trantor/[>=1.5.25 <2]", transitive_headers=True, transitive_libs=True)
+        self.requires("trantor/[>=1.5.25 <2]", transitive_headers=True, transitive_libs=True)
         self.requires("jsoncpp/1.9.5", transitive_headers=True, transitive_libs=True)
         self.requires("openssl/[>=1.1 <4]")
         self.requires("zlib/[>=1.2.11 <2]")
