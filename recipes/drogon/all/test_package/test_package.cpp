d0ba76cd816c409540ecd84dfb8a69f7e9525559
Uilian Ries <uilianr@jfrog.com>
2026-02-06 10:57:39 +0100

    Simplify test package
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/drogon/all/test_package/test_package.cpp
+++ recipes/drogon/all/test_package/test_package.cpp
@@ -1,14 +1,7 @@
 #include "drogon/drogon.h"
 
 int main() {
-    trantor::Logger::setLogLevel(trantor::Logger::kTrace);
-
-    auto client = drogon::HttpClient::newHttpClient("http://www.example.com");
-    auto req    = drogon::HttpRequest::newHttpRequest();
-    req->setMethod(drogon::Get);
-    req->setPath("/s");
-    req->setParameter("wd", "wx");
-    req->setParameter("oq", "wx");
+    std::cout << "Drogon random string: " << drogon::utils::genRandomString(8) << std::endl;
 
     return 0;
 }
