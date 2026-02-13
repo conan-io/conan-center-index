ebe9538183609837654e824709077d8b07cc48cb
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-02-13 09:44:37 +0100

    Last cleanups

--- recipes/usrsctp/all/conanfile.py
+++ recipes/usrsctp/all/conanfile.py
@@ -4,7 +4,7 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.4"
 
 
 class UsrsctpConan(ConanFile):
@@ -25,25 +25,18 @@ class UsrsctpConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
+    implements = ["auto_shared_fpic"]
+    languages = "C"
 
     def export_sources(self):
         export_conandata_patches(self)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        self.settings.rm_safe("compiler.libcxx")
-        self.settings.rm_safe("compiler.cppstd")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -56,11 +49,7 @@ class UsrsctpConan(ConanFile):
         tc.variables["USE_NET_ROUTE_H"] = self.settings.os != "iOS"
         tc.generate()
 
-    def _patch_sources(self):
-        apply_conandata_patches(self)
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
