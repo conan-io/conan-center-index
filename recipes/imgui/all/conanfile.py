a599d2f5d4e3e245615396da4bc0f77f91c804e2
benji <benjamin.wolff@digitecgalaxus.ch>
2026-02-15 16:09:07 +0100

    broaden to all sdl3 versions

--- recipes/imgui/all/conanfile.py
+++ recipes/imgui/all/conanfile.py
@@ -32,7 +32,7 @@ class IMGUIConan(ConanFile):
 
     def requirements(self):
         if self.options.with_sdl3_bindings:
-            self.requires("sdl/3.4.0")
+            self.requires("sdl/[>3]")
 
     def export_sources(self):
         copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
