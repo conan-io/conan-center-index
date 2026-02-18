372aa2feedb08e0bf6074ff98e02f6c63c6951ee
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-02-18 10:17:28 +0100

    Minimize diff

--- recipes/usrsctp/all/conanfile.py
+++ recipes/usrsctp/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.scm import Version
 
-required_conan_version = ">=2.4"
+required_conan_version = ">=2.0"
 
 
 class UsrsctpConan(ConanFile):
@@ -26,11 +26,19 @@ class UsrsctpConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    implements = ["auto_shared_fpic"]
-    languages = "C"
 
     def export_sources(self):
         export_conandata_patches(self)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
