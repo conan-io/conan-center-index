ca1b336d60c548c5f39690dcc8c4e315f5c7110d
benji <benjamin.wolff@digitecgalaxus.ch>
2026-02-21 03:10:55 +0100

    generate cmake toolchain

--- recipes/sdl3webgpu/all/conanfile.py
+++ recipes/sdl3webgpu/all/conanfile.py
@@ -24,6 +24,10 @@ class SDL3WebGPU(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
