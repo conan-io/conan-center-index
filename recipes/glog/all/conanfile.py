5a7c3035b1714a340f7d2afe1cc4dcd1c27baaa1
Jens Windmüller <jens.windmueller@swplusplus.de>
2026-02-24 07:42:22 +0100

    glog: use version range for libunwind dependency

--- recipes/glog/all/conanfile.py
+++ recipes/glog/all/conanfile.py
@@ -54,7 +54,7 @@ class GlogConan(ConanFile):
             self.requires("gflags/2.2.2", transitive_headers=True, transitive_libs=True)
         # 0.4.0 requires libunwind unconditionally
         if self.options.get_safe("with_unwind"):
-            self.requires("libunwind/1.8.0", transitive_headers=True, transitive_libs=True)
+            self.requires("libunwind/[>=1.8.0 <2]", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
         if Version(self.version) < "0.7.0":
