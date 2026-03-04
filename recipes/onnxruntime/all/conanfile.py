09c2b7f8477b6ead85d601edfe8a720aa15b4047
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2026-03-04 14:22:29 +0000

    protobuf: expand version range to use version 7

--- recipes/onnxruntime/all/conanfile.py
+++ recipes/onnxruntime/all/conanfile.py
@@ -54,7 +54,7 @@ class OnnxRuntimeConan(ConanFile):
         required_onnx_version = self.conan_data["onnx_version_map"][self.version]
         self.requires(f"onnx/{required_onnx_version}")
         self.requires("abseil/[>=20240116.1 <=20260107.1]")
-        self.requires("protobuf/[>=3.21.12 <7]")
+        self.requires("protobuf/[>=3.21.12 <8]")
         self.requires("date/[>=3.0.1 <3.1]")
         self.requires("re2/[>=20231101]")
         self.requires("flatbuffers/23.5.26")
