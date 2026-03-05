ede0f551b361fc82776a73ae96e9dad1d434b2fd
Uilian Ries <uilianr@jfrog.com>
2026-03-05 10:52:02 +0100

    Simplify recipe for libpqxx
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/libpqxx/all/test_package/conanfile.py
+++ recipes/libpqxx/all/test_package/conanfile.py
@@ -1,29 +1,18 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake, CMakeDeps, CMakeToolchain
-from conan.tools.scm import Version
+from conan.tools.cmake import cmake_layout, CMake
 import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "VirtualRunEnv"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
-
-    @property
-    def _mac_os_minimum_required_version(self):
-        version = Version(self.dependencies[self.tested_reference_str].ref.version)
-        return "13.3" if version >= "8.0" else "10.15"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
     def layout(self):
         cmake_layout(self)
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.variables["CMAKE_OSX_DEPLOYMENT_TARGET"] = self._mac_os_minimum_required_version
-        tc.generate()
 
     def build(self):
         cmake = CMake(self)
