ca4fd7a160bca37242633cb3b0301646cb2081b5
Benjamin Wolff <103897890+benjiwolff@users.noreply.github.com>
2026-03-06 17:30:29 +0100

    Update recipes/imgui/all/conanfile.py
    Co-authored-by: Francisco Ramírez <franchuti688@gmail.com>

--- recipes/imgui/all/conanfile.py
+++ recipes/imgui/all/conanfile.py
@@ -66,8 +66,7 @@ class IMGUIConan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["IMGUI_SRC_DIR"] = self.source_folder.replace("\\", "/")
-        if self.options.with_sdl3_binding:
-            tc.variables["IMGUI_WITH_SDL3_BINDING"] = "ON"
+        tc.variables["IMGUI_WITH_SDL3_BINDING"] = self.options.get_safe("with_sdl3_binding", False)
         # test engine is not available for all versions
         if self.options.get_safe("enable_test_engine"):
             tc.preprocessor_definitions["IMGUI_ENABLE_TEST_ENGINE"] = "1"
