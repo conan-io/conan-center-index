853d4a1018a2eac94cb49be7eb170c83523294fd
Ernesto de Gracia Herranz <ernestodegraciah@gmail.com>
2026-03-09 13:13:56 +0100

    fix test windows error | remove assert

--- recipes/tz/all/test_package/conanfile.py
+++ recipes/tz/all/test_package/conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.errors import ConanException
 import io
 
 
@@ -12,8 +11,13 @@ class TzTestConan(ConanFile):
     def requirements(self):
         self.requires(self.tested_reference_str)
 
+    def build_requirements(self):
+        if self.settings_build.os == "Windows":
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                self.tool_requires("msys2/cci.latest")
+
     def test(self):
         if can_run(self):
             stdout = io.StringIO()
             self.run("zdump --help", env="conanrun", stdout=stdout)
-            assert "zdump: usage: zdump OPTIONS TIMEZONE ..." in stdout.getvalue()
