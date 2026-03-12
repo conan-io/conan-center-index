49883913281d292cb77d0dfec9b0801714b8af81
Uilian Ries <uilianr@jfrog.com>
2026-03-12 16:14:30 +0100

    Simplify recipe and fixed options
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/ompl/all/conanfile.py
+++ recipes/ompl/all/conanfile.py
@@ -1,10 +1,12 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
-from conan.tools.files import get, copy, rmdir, replace_in_file
+from conan.tools.files import get, copy, rmdir
 from conan.tools.build import check_min_cppstd
+from conan.tools.microsoft import is_msvc
+from conan.errors import ConanInvalidConfiguration
 import os
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2.1"
 
 
 class OmplConan(ConanFile):
@@ -14,151 +16,61 @@ class OmplConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://ompl.kavrakilab.org/"
     topics = ("motion-planning", "robotics", "path-planning")
-
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
-
-    # Add options for optional dependencies
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
-        "with_flann": [True, False],
-    }
-    default_options = {
-        "shared": False,
-        "fPIC": True,
-        "with_flann": True,  # Enabled by default, can be disabled by user
-    }
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    implements = ["auto_shared_fpic"]
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("eigen/3.4.0", transitive_headers=True)
-
-        # Configure Boost: Disable Python and other unused components to prevent build errors
-        self.requires(
-            "boost/1.83.0",
-            transitive_headers=True,
-            options={
-                # Explicitly disabled components (Not needed by OMPL)
-                "without_python": True,
-                "without_mpi": True,
-                "without_test": True,
-                "without_contract": True,
-                "without_stacktrace": True,
-                "without_fiber": True,
-                "without_log": True,
-                "without_wave": True,
-                "without_nowide": True,
-                "without_json": True,
-                "without_url": True,
-                "without_locale": True,
-                "without_context": True,
-                "without_coroutine": True,
-                "without_timer": True,
-                "without_type_erasure": True,
-                # NOTE: We KEEP these implicitly enabled by NOT listing them above:
-                # - filesystem
-                # - graph
-                # - math
-                # - program_options
-                # - serialization
-                # - system
-                # - iostreams (often a transitive dependency for serialization/fs)
-                # - chrono (required by thread)
-                # - container (required by thread)
-                # - thread (needed by system/graph)
-            },
-        )
-
-        if self.options.with_flann:
-            self.requires("flann/1.9.2")
+        self.requires("eigen/[>=3.4.0 <4]", transitive_headers=True)
+        self.requires("boost/1.88.0", transitive_headers=True)
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, 17)
+        check_min_cppstd(self, 17)
+        if is_msvc(self) and self.options.shared:
+            # INFO https://github.com/ompl/ompl/blob/1.7.0/src/ompl/CMakeLists.txt#L36
+            raise ConanInvalidConfiguration('Only static library is provided for MSVC compiler. Use -o "ompl/*:shared=False"')
+        elif not is_msvc(self) and not self.options.shared:
+            raise ConanInvalidConfiguration('Only shared library is provided for non-MSVC compilers. Use -o "ompl/*:shared=True"')
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-
-        # Enforce CMake Policy CMP0077 to NEW.
-        # This prevents internal 'option()' command overwrite back to default."
-        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
-
-        # Respect the shared option defined in this recipe.
-        tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.shared
-
         tc.cache_variables["OMPL_BUILD_PYBINDINGS"] = False
         tc.cache_variables["OMPL_BUILD_DEMOS"] = False
         tc.cache_variables["OMPL_BUILD_TESTS"] = False
         tc.cache_variables["OMPL_BUILD_PYTESTS"] = False
         tc.cache_variables["OMPL_REGISTRATION"] = False
         tc.cache_variables["OMPL_VERSIONED_INSTALL"] = False
-
-        # Handle Optional Dependencies in CMake
-        # If with_flann is False, explicitly disable finding the package
-        if not self.options.with_flann:
-            tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_flann"] = True
-
-        # Disable others not currently supported in this recipe
         tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_castxml"] = True
         tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_Doxygen"] = True
         tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_spot"] = True
         tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_Triangle"] = True
-
+        tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_flann"] = True
         tc.generate()
 
         cd = CMakeDeps(self)
         cd.generate()
 
     def build(self):
-        # PATCH: The OMPL CMakeLists.txt hardcodes "SHARED" for non-MSVC builds
-        # Remove this keyword so CMake respects Conan's "shared=False" setting
-        ompl_cmake_path = os.path.join(
-            self.source_folder, "src", "ompl", "CMakeLists.txt"
-        )
-        replace_in_file(
-            self,
-            ompl_cmake_path,
-            "add_library(ompl SHARED ${OMPL_SOURCE_CODE})",
-            "add_library(ompl ${OMPL_SOURCE_CODE})",
-        )
-
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def package(self):
-        copy(
-            self,
-            "LICENSE",
-            src=self.source_folder,
-            dst=os.path.join(self.package_folder, "licenses"),
-        )
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         rmdir(self, os.path.join(self.package_folder, "share"))
-        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.set_property("cmake_file_name", "ompl")
-        self.cpp_info.set_property("cmake_target_name", "ompl::ompl")
-        self.cpp_info.set_property("pkg_config_name", "ompl")
-
         self.cpp_info.libs = ["ompl"]
-
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs = ["m", "pthread"]
