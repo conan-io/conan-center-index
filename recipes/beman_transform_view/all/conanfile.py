b8579ca087f6a74fdc720a69daba17ac02d74adc
Elvis Dukaj <elvis.dukaj@gmail.com>
2026-03-15 09:51:05 +0100

    Adding options for header-only.
    Fix license copy

--- recipes/beman_transform_view/all/conanfile.py
+++ recipes/beman_transform_view/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import get, copy
 import os
 
@@ -10,16 +10,23 @@ required_conan_version = ">=2"
 
 class BemanTransformViewConan(ConanFile):
     name = "beman-transform_view"
-    package_type = "static-library"
+    package_type = "header-library"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/elvisdukaj/transform_view"
     license = "Apache-2.0"
-    description = ("A conditionally borrowed std::ranges::transform_view",)
+    description = "A conditionally borrowed std::ranges::transform_view"
     topics = ("algorithm", "ranges")
     settings = "os", "arch", "compiler", "build_type"
 
+    options = {
+        "header_only": [True, False],
+    }
+    default_options = {
+        "header_only": True,
+    }
+
     def validate(self):
-        if not self.settings.compiler.get_safe("cppstd"):
+        if not self.options.header_only and not self.settings.compiler.get_safe("cppstd"):
             raise ConanInvalidConfiguration(
                 f"{self.ref} requires C++23. Please set compiler.cppstd=23 in your profile."
             )
@@ -28,34 +35,49 @@ class BemanTransformViewConan(ConanFile):
     def layout(self):
         cmake_layout(self)
 
+    def configure(self):
+        if self.options.header_only:
+            self.package_type = "header-library"
+        else:
+            self.package_type = "static-library"
+
     def package_id(self):
-        self.info.clear()
+        if self.info.options.header_only:
+            self.info.clear()
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
-
-        tc = CMakeToolchain(self)
-        tc.variables["BEMAN_TRANSFORM_VIEW_BUILD_TESTS"] = False
-        tc.variables["BEMAN_TRANSFORM_VIEW_BUILD_EXAMPLES"] = False
-        tc.generate()
+        if not self.options.header_only:
+            tc = CMakeToolchain(self)
+            tc.variables["BEMAN_TRANSFORM_VIEW_BUILD_TESTS"] = False
+            tc.variables["BEMAN_TRANSFORM_VIEW_BUILD_EXAMPLES"] = False
+            tc.generate()
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        if not self.options.header_only:
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
 
     def package(self):
-        copy(self, "LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
-        cmake = CMake(self)
-        cmake.install()
+        copy(self, "LICENSE", dst=self.package_folder, src=self.source_folder)
+
+        if self.options.header_only:
+            copy(self, "*", dst=os.path.join(self.package_folder, "include"),
+                 src=os.path.join(self.source_folder, "include"))
+        else:
+            cmake = CMake(self)
+            cmake.install()
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "beman.transform_view")
         self.cpp_info.set_property("pkg_config_name", "beman.transform_view")
         self.cpp_info.set_property("cmake_target_name", "beman::transform_view")
 
-        self.cpp_info.libs = ["beman.transform_view"]
+        if self.options.header_only:
+            self.cpp_info.bindirs = []
+            self.cpp_info.libdirs = []
+        else:
+            self.cpp_info.libs = ["beman.transform_view"]
