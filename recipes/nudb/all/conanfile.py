21aac5a60dfddf03f92e5d44e793259e42be4111
Ayaz Salikhov <asalikhov@ripple.com>
2026-03-24 18:18:01 +0000

    nudb: Update boost to 1.90.0

--- recipes/nudb/all/conanfile.py
+++ recipes/nudb/all/conanfile.py
@@ -28,7 +28,7 @@ class NudbConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.83.0")
+        self.requires("boost/1.90.0")
 
     def package_id(self):
         self.info.clear()
@@ -61,7 +61,7 @@ class NudbConan(ConanFile):
         self.cpp_info.components["core"].set_property("cmake_target_name", "nudb")
         self.cpp_info.components["core"].names["cmake_find_package"] = "nudb"
         self.cpp_info.components["core"].names["cmake_find_package_multi"] = "nudb"
-        self.cpp_info.components["core"].requires = ["boost::thread", "boost::system"]
+        self.cpp_info.components["core"].requires = ["boost::thread"]
 
         # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.names["cmake_find_package"] = "NuDB"
