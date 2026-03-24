f7c56a4432f832b29f15f70ce8b7640370eccb33
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-03-24 13:03:49 +0100

    Cleanup, dont generate revisions for older versions

--- recipes/taskflow/all/conanfile.py
+++ recipes/taskflow/all/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.microsoft import is_msvc
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=2"
 
 
 class TaskflowConan(ConanFile):
@@ -22,7 +22,6 @@ class TaskflowConan(ConanFile):
     topics = ("tasking", "parallelism", "header-only")
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
-    short_paths = True
 
     @property
     def _min_cppstd(self):
@@ -56,25 +55,16 @@ class TaskflowConan(ConanFile):
         self.info.clear()
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
-
-        def loose_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
+        check_min_cppstd(self, self._min_cppstd)
 
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and loose_lt_semver(str(self.settings.compiler.version), minimum_version):
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
             raise ConanInvalidConfiguration(
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.",
             )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-
-    def build(self):
         apply_conandata_patches(self)
 
     def package(self):
@@ -93,7 +83,3 @@ class TaskflowConan(ConanFile):
             self.cpp_info.system_libs.append("pthread")
         if is_msvc(self):
             self.cpp_info.defines.append("_ENABLE_EXTENDED_ALIGNED_STORAGE")
-
-        # TODO: to remove in conan v2 once cmake_find_package* generators removed
-        self.cpp_info.names["cmake_find_package"] = "Taskflow"
-        self.cpp_info.names["cmake_find_package_multi"] = "Taskflow"
