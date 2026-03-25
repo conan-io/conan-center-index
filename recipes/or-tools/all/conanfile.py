8b0d5fefde6c1d2f3abf9631e729d34ea81d83f9
Fernando Vilas <fvilas@iname.com>
2026-03-25 08:14:27 -0500

    Remove static library builds from Linux and Macos

--- recipes/or-tools/all/conanfile.py
+++ recipes/or-tools/all/conanfile.py
@@ -30,6 +30,11 @@ class OrToolsConan(ConanFile):
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    def config_options(self):
+        if self.settings.os == "Macos" or self.settings.os == "Linux":
+            del self.options.shared
+            self.package_type = "shared-library"
+
     def requirements(self):
         self.requires("abseil/20250814.0", transitive_headers=True, transitive_libs=True)
         self.requires("bzip2/1.0.8")
