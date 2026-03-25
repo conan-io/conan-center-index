1dd3aaea290b60b5e3a4bceb1c5a1cabbca03b58
Raziel <raziel@digdyn.com>
2026-03-25 23:36:58 +0200

    Simplify test_package

--- recipes/ccronexpr/all/test_package/test_package.c
+++ recipes/ccronexpr/all/test_package/test_package.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include <time.h>
 #include <ccronexpr.h>
 
 int main(void) {
     cron_expr expr;
-    const char* err = NULL;
-    memset(&expr, 0, sizeof(expr));
-    cron_parse_expr("0 */2 1-4 * * *", &expr, &err);
-    if (err) {
-        fprintf(stderr, "Error parsing cron expression: %s\n", err);
-        return EXIT_FAILURE;
-    }
-    time_t cur = time(NULL);
-    time_t next = cron_next(&expr, cur);
+    const char* err;
 
-    struct tm* next_tm = localtime(&next);
-    printf("Next occurrence: %02d:%02d:%02d\n", next_tm->tm_hour, next_tm->tm_min, next_tm->tm_sec);
-    
-    return EXIT_SUCCESS;
+    cron_parse_expr("* * * * * *", &expr, &err);
+    cron_next(&expr, time(NULL));
+
+    printf("Succeeded\n");
+    return 0;
 }
