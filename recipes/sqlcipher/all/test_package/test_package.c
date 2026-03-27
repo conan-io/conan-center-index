62d3c75327cf68993e3dabc06cc59ab60fce9e76
Uilian Ries <uilianr@jfrog.com>
2026-03-27 10:08:07 +0100

    Simplify test package - do not open DB
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/sqlcipher/all/test_package/test_package.c
+++ recipes/sqlcipher/all/test_package/test_package.c
@@ -1,9 +1,8 @@
-#include <sqlcipher/sqlite3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "sqlcipher/sqlite3.h"
 
-int main() {
-  sqlite3* db;
-  sqlite3_open(":memory:", &db);
-  int ret = sqlite3_key(db, "toto", 5);
-  sqlite3_close(db);
-  return ret;
+int main(void) {
+    printf("SQLite version (%d): %s\n", sqlite3_libversion_number(), sqlite3_libversion());
+    return EXIT_SUCCESS;
 }
