b90ae2e62d068330ec7e14e87e9a2e8484fa8ec3
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2026-04-01 11:36:09 +0200

    simplify test, just check xml files

--- recipes/wayland-protocols/all/test_package/conanfile.py
+++ recipes/wayland-protocols/all/test_package/conanfile.py
@@ -1,39 +1,20 @@
 from conan import ConanFile
-from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.layout import basic_layout
 import os
+import glob
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    test_type = "explicit"
-
-    @property
-    def _has_build_profile(self):
-        return hasattr(self, "settings_build")
 
     def requirements(self):
         self.requires(self.tested_reference_str)
-        self.requires("wayland/[>=1.22.0]")
-
-    def build_requirements(self):
-        self.tool_requires("wayland/<host_version>")
 
     def layout(self):
-        cmake_layout(self)
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
-        deps = CMakeDeps(self)
-        deps.generate()
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        basic_layout(self)
 
     def test(self):
-        if can_run(self):
-            cmd = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(cmd, env="conanrun")
+        wayland_folder = os.path.join(self.dependencies[self.tested_reference_str].package_folder, "res", "wayland-protocols")
+        assert os.path.isdir(wayland_folder)
+        xml_files = glob.glob(os.path.join(wayland_folder, "**", "*.xml"), recursive=True)
+        assert len(xml_files) > 0
