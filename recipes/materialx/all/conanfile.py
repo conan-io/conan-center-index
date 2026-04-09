bce85e444860861d243f3891dc91a088168908ab
Ingmar Rieger <git@irieger.net>
2026-04-09 19:52:43 +0200

    Implement based on comment

--- recipes/materialx/all/conanfile.py
+++ recipes/materialx/all/conanfile.py
@@ -85,7 +85,7 @@ class MaterialXConan(ConanFile):
             )
 
     def build_requirements(self):
-        self.tool_requires("cmake/[>=3.24 <4]")
+        self.tool_requires("cmake/[>=3.26 <5]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
