180bb9f8165d6fc483ab91d9afa9f3e45abd54fe
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2026-04-10 12:16:52 +0200

    add libfabric 2.2.0 simplify recipe

--- recipes/libfabric/all/conanfile.py
+++ recipes/libfabric/all/conanfile.py
@@ -22,64 +22,14 @@ class LibfabricConan(ConanFile):
 
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
-    _providers = [
-        "dmabuf_peer_mem",
-        "efa",
-        "hook_debug",
-        "hook_hmem",
-        "mrail",
-        "perf",
-        "profile",
-        "rxd",
-        "rxm",
-        "shm",
-        "sm2",
-        "sockets",
-        "tcp",
-        "trace",
-        "ucx",
-        "udp",
-        "usnic",
-        "verbs",
-    ]
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        **{ p: [None, "ANY"] for p in _providers },
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "dmabuf_peer_mem": "yes",
-        "efa": "yes",
-        "hook_debug": "yes",
-        "hook_hmem": "yes",
-        "mrail": "yes",
-        "perf": "no",
-        "profile": "yes",
-        "rxd": "yes",
-        "rxm": "yes",
-        "shm": "yes",
-        "sm2": "yes",
-        "sockets": "yes",
-        "tcp": "yes",
-        "trace": "yes",
-        "ucx": "no",
-        "udp": "yes",
-        "usnic": "no",
-        "verbs": "yes"
     }
-
-    def config_options(self):
-        if is_apple_os(self):
-            # Requires libnl, which is not available on macOS
-            del self.options.usnic
-            # Require Linux-specific process_vm_readv syscall
-            del self.options.shm
-            del self.options.sm2
-            # rdma-core is not available on macOS
-            del self.options.efa
-            del self.options.verbs
 
     def configure(self):
         if self.options.shared:
@@ -87,13 +37,8 @@ class LibfabricConan(ConanFile):
         self.settings.rm_safe("compiler.libcxx")
         self.settings.rm_safe("compiler.cppstd")
 
-    def _is_enabled(self, opt):
-        return str(self.options.get_safe(opt)) == "yes" or str(self.options.get_safe(opt)).startswith("dl")
-
     def requirements(self):
-        if self._is_enabled("usnic"):
-            self.requires("libnl/3.8.0")
-        if self._is_enabled("efa") or self._is_enabled("usnic") or self._is_enabled("verbs"):
+        if self.settings.os == "Linux":
             self.requires("rdma-core/52.0")
 
     def layout(self):
@@ -104,17 +49,6 @@ class LibfabricConan(ConanFile):
             # FIXME: libfabric provides msbuild project files.
             raise ConanInvalidConfiguration(f"{self.ref} Conan recipes is not supported on Windows. Contributions are welcome.")
 
-        for provider in self._providers:
-            provider = str(self.options.get_safe(provider))
-            if provider.lower() not in ["yes", "no", "dl", "none"] and \
-                    not os.path.isdir(provider) and \
-                    (not provider.startswith("dl:") and not os.path.isdir(provider[3:])):
-                raise ConanInvalidConfiguration(f"{self.ref} provider option '{provider}' is not valid. It must be 'yes', 'no', 'dl', 'dl:<dir_path>' or a directory path.")
-
-        if self._is_enabled("verbs"):
-            if not self.dependencies["rdma-core"].options.build_librdmacm:
-                raise ConanInvalidConfiguration(f"{self.ref} '-o rdma-core/*:build_librdmacm=True' is required when '-o &:verbs=True'")
-
     def build_requirements(self):
         # Used in ./configure tests and build
         self.tool_requires("libtool/2.4.7")
@@ -124,46 +58,12 @@ class LibfabricConan(ConanFile):
 
     def generate(self):
         tc = AutotoolsToolchain(self)
-        for p in self._providers:
-            if p == "verbs" and self.options.get_safe(p, "no") != "no":
-                path = self.dependencies["rdma-core"].package_folder
-                if self.options.get_safe("verbs") == "dl":
-                    tc.configure_args.append(f"--enable-verbs=dl:{path}")
-                else:
-                    tc.configure_args.append(f"--enable-verbs={path}")
-            else:
-                tc.configure_args.append(f"--enable-{p}={self.options.get_safe(p, 'no')}")
         if self.settings.build_type == "Debug":
             tc.configure_args.append("--enable-debug")
-        tc.configure_args.append(f"--with-bgq-progress=no")
-        tc.configure_args.append(f"--with-bgq-mr=no")
-        tc.configure_args.append("--with-cassin-headers=no")
-        tc.configure_args.append("--with-cuda=no")  # TODO
-        tc.configure_args.append("--with-curl=no")  # TODO
-        tc.configure_args.append("--with-cxi-uapi-headers=no")
-        tc.configure_args.append("--with-dsa=no")
-        tc.configure_args.append("--with-gdrcopy=no")
-        tc.configure_args.append("--with-json-c=no")  # TODO
-        if self._is_enabled("usnic"):
-            tc.configure_args.append(f"--with-libnl={self.dependencies['libnl'].package_folder}")
-        else:
-            tc.configure_args.append("--with-libnl=no")
-        tc.configure_args.append("--with-lttng=no")
-        tc.configure_args.append("--with-neuron=no")
-        tc.configure_args.append(f"--with-numa=no")
-        tc.configure_args.append("--with-psm2-src=no")
-        tc.configure_args.append("--with-psm3-rv=no")
-        tc.configure_args.append("--with-rocr=no")
-        tc.configure_args.append("--with-synapseai=no")
-        tc.configure_args.append("--with-uring=no")  # TODO
-        tc.configure_args.append("--with-ze=no")
-        tc.configure_args.append("-enable-psm=no")
-        tc.configure_args.append("--enable-psm2=no")
-        tc.configure_args.append("--enable-psm3=no")
-        tc.configure_args.append("--enable-xpmem=no")
-        tc.configure_args.append("--enable-cxi=no")
-        tc.configure_args.append("--enable-opx=no")
-        tc.configure_args.append("--enable-bgq=no")
+        tc.configure_args.append("--with-libnl=no")
+        tc.configure_args.append("--enable-lpp=no")
+        if is_apple_os(self):
+            tc.extra_ldflags.append("-headerpad_max_install_names")
         tc.generate()
 
         deps = AutotoolsDeps(self)
