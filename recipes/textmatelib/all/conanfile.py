a24f045183b561b8b3c2d49f24b616b8ccde2ad8
Mickael Bonfill <mickael.bonfill@unity3d.com>
2026-04-13 17:52:39 -0400

    textmatelib: version 0.1.0

--- recipes/textmatelib/all/conanfile.py
+++ recipes/textmatelib/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
+from conan.tools.microsoft import is_msvc
 import os
 
 
@@ -36,7 +37,7 @@ class TextmatelibConan(ConanFile):
 
     def requirements(self):
         self.requires("oniguruma/6.9.9", transitive_headers=True, transitive_libs=True)
-        self.requires("rapidjson/cci.20230820", transitive_headers=True)
+        self.requires("rapidjson/cci.20250205", transitive_headers=True)
 
     def validate(self):
         check_min_cppstd(self, 17)
