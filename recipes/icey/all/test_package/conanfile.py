bc8fb55edfa51fbc62d21f7c017277bce590ea51
Uilian Ries <uilianr@jfrog.com>
2026-04-14 17:42:13 +0200

    Simplify recipe for ConanCenterIndex
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/icey/all/test_package/conanfile.py
+++ recipes/icey/all/test_package/conanfile.py
@@ -7,7 +7,6 @@ import os
 class IceyTestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
-    test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
@@ -22,5 +21,5 @@ class IceyTestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            exe = os.path.join(self.cpp.build.bindirs[0], "icey_conan_test")
+            exe = os.path.join(self.cpp.build.bindirs[0], "test_package")
             self.run(exe, env="conanrun")
