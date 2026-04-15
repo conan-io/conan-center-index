e7cab2d4d59763a3851c10e0a55854e81393d614
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2026-04-15 15:06:58 +0100

    unilink: use latest boost, account for boost::system deprecation, minor changes

--- recipes/unilink/all/conanfile.py
+++ recipes/unilink/all/conanfile.py
@@ -2,9 +2,10 @@ from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
+from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=2.0"
+required_conan_version = ">=2.4"
 
 
 class UnilinkConan(ConanFile):
@@ -47,25 +48,23 @@ class UnilinkConan(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def requirements(self):
-        self.requires("boost/1.88.0", transitive_headers=True)
+        self.requires("boost/[>=1.88.0]", transitive_headers=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["UNILINK_BUILD_SHARED"] = bool(self.options.shared)
-        tc.variables["UNILINK_ENABLE_INSTALL"] = (
-            True  # ensure unilinkConfig.cmake/targets are generated & installed
-        )
-        tc.variables["UNILINK_BUILD_EXAMPLES"] = False
-        tc.variables["UNILINK_BUILD_TESTS"] = False
-        tc.variables["UNILINK_BUILD_DOCS"] = False
-        tc.variables["UNILINK_ENABLE_PKGCONFIG"] = False
-        tc.variables["UNILINK_ENABLE_CONFIG"] = bool(self.options.enable_config)
-        tc.variables["UNILINK_ENABLE_MEMORY_TRACKING"] = bool(
-            self.options.enable_memory_tracking
-        )
+        tc.cache_variables["UNILINK_BUILD_SHARED"] = self.options.shared
+        tc.cache_variables["UNILINK_ENABLE_INSTALL"] = True
+        tc.cache_variables["UNILINK_BUILD_EXAMPLES"] = False
+        tc.cache_variables["UNILINK_BUILD_TESTS"] = False
+        tc.cache_variables["UNILINK_BUILD_DOCS"] = False
+        tc.cache_variables["UNILINK_ENABLE_PKGCONFIG"] = False
+        tc.cache_variables["UNILINK_ENABLE_CONFIG"] = self.options.enable_config
+        tc.cache_variables["UNILINK_ENABLE_MEMORY_TRACKING"] = self.options.enable_memory_tracking
         tc.generate()
 
         deps = CMakeDeps(self)
+        if Version(self.dependencies["boost"].ref.version) >= "1.89.0":
+            deps.set_property("boost::headers", "cmake_target_aliases", ["Boost::system"])
         deps.generate()
 
     def build(self):
