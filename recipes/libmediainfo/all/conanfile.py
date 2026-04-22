5ca657a5974653ed5bde200ea8942426b5feaf56
PerseoGI <perseog@jfrog.com>
2026-04-22 17:23:00 +0200

    Remove unneded exception

--- recipes/libmediainfo/all/conanfile.py
+++ recipes/libmediainfo/all/conanfile.py
@@ -67,9 +67,8 @@ class LibmediainfoConan(ConanFile):
         if Version(self.version) < "22.03":
             # Generate a relocatable shared lib on Macos
             tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
-        tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5" # CMake 4 support
-        if Version(self.version) > "26.01": # pylint: disable=conan-unreachable-upper-version
-            raise ConanException("CMAKE_POLICY_VERSION_MINIMUM hardcoded to 3.5, check if new version supports CMake 4")
+        if Version(self.version) <= "22.03":
+            tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
