f23f3da0076772b13db4455ae0d91a8612845a7d
Nerixyz <nerixdev@outlook.de>
2026-04-23 13:03:43 +0200

    chore: add message if test fails

--- recipes/libavif/all/test_package/test_package.c
+++ recipes/libavif/all/test_package/test_package.c
@@ -1,13 +1,17 @@
 #include <avif/avif.h>
 #include <stddef.h>
+#include <stdio.h>
 
 int main(int argc, char const* argv[])
 {
   (void)argc;
   (void)argv;
 
-  if (!avifCodecName(AVIF_CODEC_CHOICE_AUTO, AVIF_CODEC_FLAG_CAN_DECODE))
+  if (!avifCodecName(AVIF_CODEC_CHOICE_AUTO, AVIF_CODEC_FLAG_CAN_DECODE)) {
+    printf("No decoder available - there should be at least one. Was libavif "
+           "compiled without any codec?\n");
     return 1;
+  }
 
   return 0;
 }
