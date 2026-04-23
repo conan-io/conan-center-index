3cdfade3949d05e63ecc7a6cb29dbe48f0780255
Francisco Ramirez de Anton <franchuti688@gmail.com>
2026-04-23 17:18:35 +0200

    Using gflags as a version range in folly

--- recipes/folly/all/conanfile.py
+++ recipes/folly/all/conanfile.py
@@ -69,7 +69,7 @@ class FollyConan(ConanFile):
         self.requires("boost/1.85.0", transitive_headers=True, transitive_libs=True)
         self.requires("bzip2/1.0.8")
         self.requires("double-conversion/3.3.0", transitive_headers=True, transitive_libs=True)
-        self.requires("gflags/2.2.2")
+        self.requires("gflags/[>=2.2.2 <3]")
         self.requires("glog/0.7.1", transitive_headers=True, transitive_libs=True)
         self.requires("libevent/2.1.12", transitive_headers=True, transitive_libs=True)
         self.requires("openssl/[>=1.1 <4]")
