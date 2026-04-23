6369133c66174195c2795a096a7c744c8964b278
Andrew Turchyn <ultimate.question.of.life@gmail.com>
2026-04-24 02:21:24 +0300

    speexdsp: add new recipe for 1.2.1 (#30058)

---
+++ recipes/speexdsp/all/conanfile.py
@@ -0,0 +1,64 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, rm, rmdir
+import os
+
+required_conan_version = ">=2.4"
+
+
+class SpeexDspConan(ConanFile):
+    name = "speexdsp"
+    description = "SpeexDSP is a patent-free, Open Source/Free Software DSP library derived from Speex."
+    topics = ("speex", "dsp", "audio", "resampler", "echo-cancellation", "preprocessor")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://gitlab.xiph.org/xiph/speexdsp"
+    license = "BSD-3-Clause"
+
+    package_type = "library"
+    languages = "C"
+    implements = ["auto_shared_fpic"]
+    settings = "os", "arch", "compiler", "build_type"
+
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "fixed_point": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "fixed_point": False,
+    }
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["USE_FIXED_POINT"] = self.options.fixed_point
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rm(self, "Makefile.am", os.path.join(self.package_folder, "include"), recursive=True)
+        rm(self, "*.h.in", os.path.join(self.package_folder, "include"), recursive=True)
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "speexdsp")
+        self.cpp_info.set_property("cmake_target_name", "speexdsp::speexdsp")
+        self.cpp_info.set_property("pkg_config_name", "speexdsp")
+        self.cpp_info.libs = ["speexdsp"]
+        if self.settings.os in ["Linux", "FreeBSD", "Android"]:
+            self.cpp_info.system_libs.append("m")
