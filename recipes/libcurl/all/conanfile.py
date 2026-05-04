a03749c06f806661db20b85425209af2139f6a21
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-05-04 19:29:39 +0200

    Add missing cmake min version required

--- recipes/libcurl/all/conanfile.py
+++ recipes/libcurl/all/conanfile.py
@@ -202,6 +202,7 @@ class LibcurlConan(ConanFile):
 
     def build_requirements(self):
         if self._is_using_cmake_build:
+            self.tool_requires("cmake/[>=3.18]")
             if self._is_win_x_android:
                 self.tool_requires("ninja/[>=1.10.2 <2]")
         else:
