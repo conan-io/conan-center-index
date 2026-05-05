72c758694596b4a5bd3742b51abf8cc0ba15b64a
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-05-06 00:06:17 +0200

    Update recipes/sbepp/all/conanfile.py

--- recipes/sbepp/all/conanfile.py
+++ recipes/sbepp/all/conanfile.py
@@ -63,7 +63,7 @@ class PackageConan(ConanFile):
     def requirements(self):
         if self.options.with_sbeppc:
             # sbepp/<1.1.0 requires fmt and pugixml with hardcoded versions
-            self.requires("fmt/[>=9.1.0]")
+            self.requires("fmt/[>=9.1 <13]")
             self.requires("pugixml/[>=1.14 <2]")
 
     def validate(self):
