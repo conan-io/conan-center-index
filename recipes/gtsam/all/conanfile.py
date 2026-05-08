5e1f727af37c58e9f702a7641f4c42e298173ebd
Ernesto de Gracia Herranz <ernestodegraciah@gmail.com>
2026-05-08 17:26:23 +0200

    remove unused import

--- recipes/gtsam/all/conanfile.py
+++ recipes/gtsam/all/conanfile.py
@@ -3,7 +3,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rmdir, save
 from conan.tools.microsoft import check_min_vs, is_msvc, msvc_runtime_flag
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2"
