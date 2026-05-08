559a3e7cca0775e410651d7835c297519f1eccbc
Aleksandr Kovalko <gistrec@gmail.com>
2026-05-08 23:33:52 +0200

    geo-utils-cpp: new recipe

---
+++ recipes/geo-utils-cpp/all/conanfile.py
@@ -0,0 +1,52 @@
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+import os
+
+required_conan_version = ">=1.52.0"
+
+
+class GeoUtilsCppConan(ConanFile):
+    name = "geo-utils-cpp"
+    description = "Header-only C++17 library for geographic (lat/lng) geometry: distance, bearing, area, point-in-polygon."
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/gistrec/geo-utils-cpp"
+    topics = ("geometry", "geographic", "lat-lng", "spherical", "gps", "header-only")
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
+
+    @property
+    def _min_cppstd(self):
+        return 17
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def build(self):
+        pass
+
+    def package(self):
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.hpp",
+             os.path.join(self.source_folder, "include"),
+             os.path.join(self.package_folder, "include"))
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "GeoUtilsCpp")
+        self.cpp_info.set_property("cmake_target_name", "geo::utils")
+        self.cpp_info.set_property("pkg_config_name", "geo-utils-cpp")
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
