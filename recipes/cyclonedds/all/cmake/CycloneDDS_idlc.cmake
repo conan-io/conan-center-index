d4eea09da5cc68f87624bc4129f4abf3f6213c39
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-05-14 09:52:36 +0200

    Try to patch generate

--- recipes/cyclonedds/all/cmake/CycloneDDS_idlc.cmake
+++ recipes/cyclonedds/all/cmake/CycloneDDS_idlc.cmake
@@ -19,28 +19,3 @@ if(NOT TARGET CycloneDDS::idlc)
         set_property(TARGET CycloneDDS::idlc PROPERTY IMPORTED_LOCATION ${_idlc_executable})
     endif()
 endif()
-
-# Define CycloneDDS::libidlc so IDLC_GENERATE uses $<TARGET_FILE:CycloneDDS::libidlc>
-# rather than falling back to find_library(), which on Windows returns the import .lib
-# instead of the .dll, causing LoadLibrary to fail at code-generation time.
-if(NOT TARGET CycloneDDS::libidlc AND NOT CMAKE_CROSSCOMPILING)
-    if(WIN32)
-        find_file(_libidlc_location
-            NAMES cycloneddsidlc.dll
-            PATHS "${CMAKE_CURRENT_LIST_DIR}/../../../bin/"
-            NO_DEFAULT_PATH
-        )
-    else()
-        find_library(_libidlc_location
-            NAMES cycloneddsidlc
-            PATHS "${CMAKE_CURRENT_LIST_DIR}/../../../lib/"
-            NO_DEFAULT_PATH
-        )
-    endif()
-
-    if(_libidlc_location)
-        get_filename_component(_libidlc_location "${_libidlc_location}" ABSOLUTE)
-        add_library(CycloneDDS::libidlc SHARED IMPORTED)
-        set_property(TARGET CycloneDDS::libidlc PROPERTY IMPORTED_LOCATION ${_libidlc_location})
-    endif()
-endif()
--- recipes/cyclonedds/all/conanfile.py
+++ recipes/cyclonedds/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rm, rmdir, replace_in_file
 from conan.tools.scm import Version
 import os
 
@@ -148,6 +148,9 @@ class CycloneDDSConan(ConanFile):
         if self.settings.os == "Windows":
             for p in ("*.pdb", "concrt*.dll", "msvcp*.dll", "vcruntime*.dll"):
                 rm(self, p, os.path.join(self.package_folder, "bin"))
+        replace_in_file(self, os.path.join(self.package_folder, "lib", "cmake", "CycloneDDS", "idlc", "Generate.cmake"),
+                        "if (TARGET CycloneDDS::libidlc)",
+                        "if (0)")
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "CycloneDDS")
