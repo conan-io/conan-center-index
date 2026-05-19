f28ae7ee579bea8da06d2863b7b46d4358b4e6e8
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2026-05-19 14:52:01 +0100

    bmx: add rpath link for transitive dependencies

--- recipes/bmx/all/conanfile.py
+++ recipes/bmx/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, get, rmdir
 import os
 
-required_conan_version = ">=2.0"
+required_conan_version = ">=2.26"
 
 
 class BmxConan(ConanFile):
@@ -75,6 +75,7 @@ class BmxConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
+        tc.add_rpath_link = True
         tc.variables["BMX_BUILD_WITH_LIBCURL"] = self.options.with_libcurl
         tc.cache_variables["BMX_BUILD_EXPAT_SOURCE"] = False
         tc.cache_variables["BUILD_TESTING"] = False
@@ -135,4 +136,4 @@ class BmxConan(ConanFile):
             libbmx.requires.append("libuuid::libuuid")
 
         if self.options.with_libcurl:
-            libbmx.requires.append("libcurl::libcurl")
+            libbmx.requires.append("libcurl::curl")
