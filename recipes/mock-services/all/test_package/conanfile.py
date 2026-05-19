e0731866c37d96bbcc8e1a312b9e28bff748f5d1
Maikel Enrique Pernía Matos <25876961+mpernia@users.noreply.github.com>
2026-05-19 02:19:24 +0200

    mock-services: add version 0.1.1
    This PR adds the mock-services library (v0.1.0, v0.1.1) to Conan Center Index.

---
+++ recipes/mock-services/all/test_package/conanfile.py
@@ -0,0 +1,31 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
+
+class MockServicesTestConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+        self.requires("cpp-httplib/0.44.0")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if can_run(self):
+            cmake = CMake(self)
+            cmake.test()
