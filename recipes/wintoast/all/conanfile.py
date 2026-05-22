c5c3711f3873e312ef42ee8314b4622fd4072504
KebsCS <git@kebs.dev>
2026-05-22 23:18:47 +0200

    wintoast: Add recipe 1.3.2

---
+++ recipes/wintoast/all/conanfile.py
@@ -0,0 +1,62 @@
+import os
+
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get
+
+required_conan_version = ">=1.52.0"
+
+
+class WinToastConan(ConanFile):
+    name = "wintoast"
+    description = "Lightweight library which brings a complete integration of the modern toast notifications of Windows"
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/mohabouje/WinToast"
+    topics = ("windows", "toast", "notification")
+
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+    }
+    default_options = {
+        "shared": False,
+    }
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def validate(self):
+        if self.settings.os != "Windows":
+            raise ConanInvalidConfiguration(f"WinToast is only supported on Windows")
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 11)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["WINTOASTLIB_BUILD_EXAMPLES"] = False
+        tc.variables["WINTOASTLIB_QT_ENABLED"] = False
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "LICENSE.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.h", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+        copy(self, "*.lib", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.dll", src=self.build_folder, dst=os.path.join(self.package_folder, "bin"), keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "WinToast")
+        self.cpp_info.set_property("cmake_target_name", "WinToast::WinToast")
+        self.cpp_info.libs = ["WinToast"]
+        self.cpp_info.system_libs = ["psapi"]
---
+++ recipes/wintoast/all/test_package/test_package.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <wintoastlib.h>
+
+int main()
+{
+	WinToastLib::WinToast::isCompatible();
+    return EXIT_SUCCESS;
+}
