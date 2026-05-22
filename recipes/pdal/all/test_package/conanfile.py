33224b7e1fc3595bc2c3e3dd369de05e4b0256eb
Sébastien Hensgen <24550538+sebhmg@users.noreply.github.com>
2026-05-22 13:05:39 -0400

    remove "explicit" (a Conan 1.x idiom)

--- recipes/pdal/all/test_package/conanfile.py
+++ recipes/pdal/all/test_package/conanfile.py
@@ -8,7 +8,6 @@ from conan.tools.cmake import cmake_layout, CMake
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps", "CMakeToolchain"
-    test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
