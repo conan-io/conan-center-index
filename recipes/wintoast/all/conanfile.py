dc6332ef67a87b18a85a5df354c4099e1feaf191
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-05-25 18:36:07 +0200

    Missing import

--- recipes/wintoast/all/conanfile.py
+++ recipes/wintoast/all/conanfile.py
@@ -4,7 +4,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get
+from conan.tools.files import copy, get, replace_in_file
 
 required_conan_version = ">=2.4"
 
