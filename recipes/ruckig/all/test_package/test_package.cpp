f51134757f43ea4ffa111dc49f4dbe8c1b476990
Carlos Zoido <mrgalleta@gmail.com>
2026-05-27 14:28:05 +0200

    simpler test_packge

--- recipes/ruckig/all/test_package/test_package.cpp
+++ recipes/ruckig/all/test_package/test_package.cpp
@@ -4,19 +4,14 @@
 int main() {
     using namespace ruckig;
 
-    Ruckig<3> ruckig(0.01);
-    InputParameter<3> input;
-    OutputParameter<3> output;
+    Ruckig<1> ruckig(0.01);
+    InputParameter<1> input;
+    OutputParameter<1> output;
 
-    input.current_position = {0.0, 0.0, 0.0};
-    input.current_velocity = {0.0, 0.0, 0.0};
-    input.current_acceleration = {0.0, 0.0, 0.0};
-    input.target_position = {1.0, 1.0, 1.0};
-    input.target_velocity = {0.0, 0.0, 0.0};
-    input.target_acceleration = {0.0, 0.0, 0.0};
-    input.max_velocity = {1.0, 1.0, 1.0};
-    input.max_acceleration = {1.0, 1.0, 1.0};
-    input.max_jerk = {1.0, 1.0, 1.0};
+    input.target_position = {1.0};
+    input.max_velocity = {1.0};
+    input.max_acceleration = {1.0};
+    input.max_jerk = {1.0};
 
     auto result = ruckig.update(input, output);
     printf("Ruckig update result: %d\n", static_cast<int>(result));
