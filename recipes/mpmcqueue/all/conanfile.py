084fbff2be3f35e4f32d6354e293784103ba3fbe
Ingmar Rieger <git@irieger.net>
2026-05-28 04:55:39 +0200

    Backport the missing functions as a patch from current master.
    Adds only two simple const getters.

--- recipes/mpmcqueue/all/conanfile.py
+++ recipes/mpmcqueue/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import copy, get
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
 from conan.tools.layout import basic_layout
 from conan.tools.scm import Version
 import os
@@ -32,11 +32,14 @@ class MpmcqueueConan(ConanFile):
         if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "5":
             raise ConanInvalidConfiguration("gcc < 5 not supported")
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def build(self):
-        pass
+        apply_conandata_patches(self)
 
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
