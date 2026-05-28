9387ce7dc5d80103941e71bd26a94345f91f52e0
Uilian Ries <uilianries@gmail.com>
2026-05-28 16:56:45 +0200

    Remove unused import

--- recipes/perf/all/conanfile.py
+++ recipes/perf/all/conanfile.py
@@ -5,7 +5,6 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.files import apply_conandata_patches, chdir, copy, export_conandata_patches, get
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
-from conan.errors import ConanException
 
 required_conan_version = ">=1.47.0"
 
