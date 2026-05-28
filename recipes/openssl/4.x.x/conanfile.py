354213e9d229e89c1a1a12232d6b64d65ebd93e7
Tyler Mayoff <tyler.mayoff@hingehealth.com>
2026-05-28 12:52:55 -0400

    remove Version >= 3.xx checks

--- recipes/openssl/4.x.x/conanfile.py
+++ recipes/openssl/4.x.x/conanfile.py
@@ -517,14 +517,10 @@ class OpenSSLConan(ConanFile):
                 # When `--prefix=/`, the scripts derive `\` without escaping, which
                 # causes issues on Windows
                 replace_in_file(self, "Makefile", "INSTALLTOP_dir=\\", "INSTALLTOP_dir=\\\\")
-                if Version(self.version) >= "3.3.0":
-                    # replace backslashes in paths with forward slashes
-                    mkinstallvars_pl = os.path.join(self.source_folder, "util", "mkinstallvars.pl")
-                    if Version(self.version) >= "3.3.2":
-                        replace_in_file(self, mkinstallvars_pl, "push @{$values{$k}}, $v;", """$v =~ s|\\\\|/|g; push @{$values{$k}}, $v;""")
-                        replace_in_file(self, mkinstallvars_pl, "$values{$k} = $v;", """$v->[0] =~ s|\\\\|/|g; $values{$k} = $v;""")
-                    else:
-                        replace_in_file(self, mkinstallvars_pl, "$ENV{$k} = $v;", """$v =~ s|\\\\|/|g; $ENV{$k} = $v;""")
+                # replace backslashes in paths with forward slashes
+                mkinstallvars_pl = os.path.join(self.source_folder, "util", "mkinstallvars.pl")
+                replace_in_file(self, mkinstallvars_pl, "push @{$values{$k}}, $v;", """$v =~ s|\\\\|/|g; push @{$values{$k}}, $v;""")
+                replace_in_file(self, mkinstallvars_pl, "$values{$k} = $v;", """$v->[0] =~ s|\\\\|/|g; $values{$k} = $v;""")
             self._run_make()
 
     def _make_install(self):
