9b4f998f43c0711c4f3c151653cecc517cfe3bb3
Julian Groß <julian.g@posteo.de>
2026-05-31 15:15:45 +0200

    Starting with Qt 6.11, Qt WebEngine requires a NodeJS version of 20.0 or higher. See: https://doc.qt.io/qt-6.11/qtwebengine-platform-notes.html#all-platforms ``` WARNING: QtWebEngine won't be built. The following configure errors were found:  * 64-bit Node.js 20.0 version or later is required. ```

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -428,7 +428,7 @@ class QtConan(ConanFile):
             self.tool_requires("pkgconf/[>=2.2 <3]")
 
         if self.options.get_safe("qtwebengine"):
-            self.tool_requires("nodejs/18.15.0")
+            self.tool_requires("nodejs/22.20.0")
             self.tool_requires("gperf/3.1")
             # gperf, bison, flex, python >= 2.7.5 & < 3
             if self.settings_build.os == "Windows":
