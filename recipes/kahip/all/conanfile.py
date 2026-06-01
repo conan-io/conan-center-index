f336888b1d613558c811debd22e40d182307463e
Ernesto de Gracia Herranz <ernestodegraciah@gmail.com>
2026-06-01 18:48:11 +0200

    fix format | simplify recipe | remove python test | fix recipe

--- recipes/kahip/all/conanfile.py
+++ recipes/kahip/all/conanfile.py
@@ -1,70 +1,46 @@
 from conan import ConanFile
 from conan.tools.files import copy, get
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 import os
 
-class KahipConan(ConanFile):
-    name            = "kahip"
-    version         = "3.19"
-    license         = "MIT"
-    url             = "https://github.com/KaHIP/KaHIP"
-    homepage        = url
-    description     = "Karlsruhe High Quality Partitioning"
-    topics          = ("graph", "partitioning", "algorithms")
-    package_type    = "library"
+required_conan_version = ">=2.1"
 
-    settings = "os", "compiler", "arch", "build_type"
-    options  = {
-        "shared"         : [True, False],
-        "fPIC"           : [True, False],
-        "with_mpi"       : [True, False],
-        "python_module"  : [True, False],
+class KahipConan(ConanFile):
+    name = "kahip"
+    description = "Karlsruhe High Quality Partitioning"
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/KaHIP/KaHIP"
+    topics = ("graph", "partitioning", "algorithms")
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
     }
     default_options = {
-        "shared"         : False,
-        "fPIC"           : True,
-        "with_mpi"       : False,
-        "python_module"  : False,
+        "shared": False,
+        "fPIC": True,
     }
     implements = ["auto_shared_fpic"]
 
-
-    @property
-    def _min_cppstd(self):
-        return "11"
-    
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
-
-    def requirements(self):
-        if self.options.python_module:
-            self.requires("pybind11/2.13.6")
+        check_min_cppstd(self, 11)
 
     def package_id(self):
         self.info.clear()
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.cache_variables["NOMPI"]             = not self.options.with_mpi
-        tc.cache_variables["USE_TCMALLOC"]      = False
-        tc.cache_variables["USE_ILP"]           = False # would require gurobi
-        
-        tc.cache_variables["BUILDPYTHONMODULE"] = self.options.python_module
-        if self.options.python_module:
-            tc.cache_variables["CMAKE_INSTALL_PYTHONDIR"] = os.path.join(self.package_folder, "python")
-        
+        tc.cache_variables["NOMPI"] = True
         tc.generate()
-        
-        deps = CMakeDeps(self)
-        deps.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -77,12 +53,6 @@ class KahipConan(ConanFile):
         copy(self, "LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
 
     def package_info(self):
-        self.cpp_info.libs = ["kahip"]
-        
-        # KaHIP exposes a CMake target "KaHIP::KaHIP" and installs a config file
-        self.cpp_info.set_property("cmake_file_name", "KaHIP")   # <pkg>Config.cmake
+        self.cpp_info.set_property("cmake_file_name", "KaHIP")
         self.cpp_info.set_property("cmake_target_name", "KaHIP::KaHIP")
-            
-        if self.options.python_module:
-            kahip_dist_package = os.path.join(self.package_folder, "python", "kahip")
-            self.runenv_info.prepend_path("PYTHONPATH", kahip_dist_package)
+        self.cpp_info.libs = ["kahip"] if self.options.shared else ["kahip_static"]
--- recipes/kahip/all/test_package/test_package.cpp
+++ recipes/kahip/all/test_package/test_package.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
-#include <sstream>
+#include <vector>
 
 #include "kaHIP_interface.h"
 
+int main() {
+    std::cout << "partitioning graph from the manual" << std::endl;
 
-int main(int argn, char **argv) {
+    int n = 5;
+    std::vector<int> xadj = {0, 2, 5, 7, 9, 12};
+    std::vector<int> adjncy = {1, 4, 0, 2, 4, 1, 3, 2, 4, 0, 1, 3};
 
-    std::cout <<  "partitioning graph from the manual"  << std::endl;
-
-    int n            = 5;
-    int* xadj        = new int[6];
-    xadj[0] = 0; xadj[1] = 2; xadj[2] = 5; xadj[3] = 7; xadj[4] = 9; xadj[5] = 12;
-
-    int* adjncy      = new int[12];
-    adjncy[0]  = 1; adjncy[1]  = 4; adjncy[2]  = 0; adjncy[3]  = 2; adjncy[4]  = 4; adjncy[5]  = 1; 
-    adjncy[6]  = 3; adjncy[7]  = 2; adjncy[8]  = 4; adjncy[9]  = 0; adjncy[10] = 1; adjncy[11] = 3; 
-    
     double imbalance = 0.03;
-    int* part        = new int[n];
-    int edge_cut     = 0;
-    int nparts       = 2;
-    int* vwgt        = NULL;
-    int* adjcwgt     = NULL;
+    std::vector<int> part(n);
+    int edge_cut = 0;
+    int nparts = 2;
 
-    kaffpa(&n, vwgt, xadj, adjcwgt, adjncy, &nparts, &imbalance, false, 0, ECO, & edge_cut, part);
+    kaffpa(&n, nullptr, xadj.data(), nullptr, adjncy.data(), &nparts, &imbalance,
+           false, 0, ECO, &edge_cut, part.data());
 
-    std::cout <<  "edge cut " <<  edge_cut  << std::endl;
+    std::cout << "edge cut " << edge_cut << std::endl;
 
     return 0;
 }
