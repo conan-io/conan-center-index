92899d896a26529b8062709b9f64051542f5b9c2
Uilian Ries <uilianr@jfrog.com>
2026-06-02 11:37:37 +0200

    Add missing test package
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/msgpack23/all/test_package/conanfile.py
+++ recipes/msgpack23/all/test_package/conanfile.py
@@ -1,22 +1,18 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+from conan.tools.cmake import cmake_layout, CMake
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps"
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def layout(self):
         cmake_layout(self)
 
     def requirements(self):
         self.requires(self.tested_reference_str)
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
 
     def build(self):
         cmake = CMake(self)
