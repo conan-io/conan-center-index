a68bdc8341c9d61fd824f92278619d8032322104
Aleksandr Kovalko <gistrec@gmail.com>
2026-06-03 01:00:43 +0200

    geo-utils-cpp: reject compilers without C++17 support

--- recipes/geo-utils-cpp/all/conanfile.py
+++ recipes/geo-utils-cpp/all/conanfile.py
@@ -1,7 +1,9 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy, get
 from conan.tools.layout import basic_layout
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.52.0"
@@ -22,6 +24,16 @@ class GeoUtilsCppConan(ConanFile):
     def _min_cppstd(self):
         return 17
 
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "7",
+            "clang": "5",
+            "apple-clang": "10",
+            "Visual Studio": "15",
+            "msvc": "191",
+        }
+
     def layout(self):
         basic_layout(self, src_folder="src")
 
@@ -31,6 +43,11 @@ class GeoUtilsCppConan(ConanFile):
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support.",
+            )
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
