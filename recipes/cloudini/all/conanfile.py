1cdf708f484fad100b182ba5d16c9c890cdc2758
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-06-05 13:09:09 +0200

    Apply suggestion from @AbrilRBS

--- recipes/cloudini/all/conanfile.py
+++ recipes/cloudini/all/conanfile.py
@@ -6,7 +6,6 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get
 from conan.tools.microsoft import is_msvc
-from conan.tools.scm import Version
 
 required_conan_version = ">=2.0.9"
 
