fcb1a6e4703d7407f4ba93c8ca99464e68c1fc8f
Uilian Ries <uilianr@jfrog.com>
2026-06-09 16:16:15 +0200

    Simplify test package
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/ripper37-libbase/all/test_package/test_package.cpp
+++ recipes/ripper37-libbase/all/test_package/test_package.cpp
@@ -1,68 +1,10 @@
+#include <cstdlib>
 #include <iostream>
-#include <string>
-#include <vector>
 
-#include "base/callback.h"
-#include "base/init.h"
+#include "base/time/time.h"
 
-#if defined(LIBBASE_MODULE_NET)
-#include "base/net/init.h"
-#include "base/net/simple_url_loader.h"
-#include "base/net/resource_request.h"
-#include "base/net/resource_response.h"
-#endif  // defined(LIBBASE_MODULE_NET)
-
-#if defined(LIBBASE_MODULE_WIN)
-#include "base/message_loop/win/win_message_loop_attachment.h"
-#endif  // defined(LIBBASE_MODULE_WIN)
-
-#if defined(LIBBASE_MODULE_WX)
-#include "base/message_loop/wx/wx_message_loop_attachment.h"
-#endif // defined(LIBBASE_MODULE_WX)
-
-int main(int argc, char* argv[]) {
-  base::Initialize(argc, argv, {});
-#if defined(LIBBASE_MODULE_NET)
-  base::net::Initialize({});
-#endif  // defined(LIBBASE_MODULE_NET)
-
-  base::BindOnce([]() { std::cout << "Hello World!" << std::endl; }).Run();
-
-  std::vector<std::string> enabled_components;
-  enabled_components.push_back("core");
-#if defined(LIBBASE_MODULE_NET)
-  // Dummy code to check linkage
-  if (argc < 0) {
-    base::net::SimpleUrlLoader::DownloadUnbounded(
-      base::net::ResourceRequest{"https://example.com"},
-      base::BindOnce([](base::net::ResourceResponse) {}));
-  }
-  enabled_components.push_back("net");
-#endif  // defined(LIBBASE_MODULE_NET)
-#if defined(LIBBASE_MODULE_WIN)
-  // Dummy code to check linkage
-  if (argc < 0) {
-    auto ml_attachment = base::win::WinMessageLoopAttachment::TryCreate();
-  }
-  enabled_components.push_back("win");
-#endif  // defined(LIBBASE_MODULE_WIN)
-#if defined(LIBBASE_MODULE_WX)
-  // Dummy code to check linkage
-  if (argc < 0) {
-    auto ml_attachment = base::wx::WxMessageLoopAttachment{nullptr};
-  }
-  enabled_components.push_back("wx");
-#endif  // defined(LIBBASE_MODULE_WX)
-
-  std::cout << "Enabled components:";
-  for (const auto& component : enabled_components) {
-    std::cout << " " << component;
-  }
-  std::cout << std::endl;
-
-#if defined(LIBBASE_MODULE_NET)
-  base::net::Deinitialize();
-#endif  // defined(LIBBASE_MODULE_NET)
-  base::Deinitialize();
-  return 0;
+int main() {
+    const auto now = base::Time::Now();
+    std::cout << "Libbase - Current time: " << now.ToTimeT() << " seconds since the Unix epoch." << std::endl;
+    return EXIT_SUCCESS;
 }
