1e8375be3003a85ec23072210b26485f66cec874
Arnaud Mathias <arndmathias@gmail.com>
2026-06-12 10:08:02 +0200

    gtsam: add version 4.2.1

--- recipes/gtsam/all/conanfile.py
+++ recipes/gtsam/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rmdir, save
 from conan.tools.microsoft import check_min_vs, is_msvc, msvc_runtime_flag
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2"
@@ -121,17 +122,21 @@ class GtsamConan(ConanFile):
     @property
     def _required_boost_components(self):
         # Based on https://github.com/borglab/gtsam/blob/4.2.0/cmake/HandleBoost.cmake#L26
-        return [
+        components = [
             "chrono",
-            "date_time",
             "filesystem",
             "program_options",
-            "regex",
             "serialization",
-            "system",
-            "thread",
             "timer",
         ]
+        if Version(self.version) <= "4.2":
+            components += [
+                "date_time",
+                "regex",
+                "system",
+                "thread",
+            ]
+        return components
 
     def validate(self):
         miss_boost_required_comp = any(
