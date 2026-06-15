e74ff296683d5843409bbfc54575cb21d34ef09d
johan-bytecat <johan@bytecat.co.za>
2026-06-15 11:15:28 +0200

    Update recipes/blake3/all/test_package/test_package.c
    Co-authored-by: Uilian Ries <uilianries@gmail.com>

--- recipes/blake3/all/test_package/test_package.c
+++ recipes/blake3/all/test_package/test_package.c
@@ -1,22 +1,8 @@
 #include <blake3.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int main(void) {
-    blake3_hasher hasher;
-    blake3_hasher_init(&hasher);
-
-    const char *input = "Hello, BLAKE3!";
-    blake3_hasher_update(&hasher, input, strlen(input));
-
-    uint8_t output[BLAKE3_OUT_LEN];
-    blake3_hasher_finalize(&hasher, output, BLAKE3_OUT_LEN);
-
-    printf("BLAKE3 hash: ");
-    for (size_t i = 0; i < BLAKE3_OUT_LEN; i++) {
-        printf("%02x", output[i]);
-    }
-    printf("\n");
-
-    return 0;
+    printf("BLAKE3 Version: %s\n", blake3_version());
+    return EXIT_SUCCESS;
 }
