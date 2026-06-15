a58ddfd83e44f6c05e9d4841460edc1b68f7aac8
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-06-15 14:07:29 +0200

    Guard redirect copyn on option

--- recipes/mimalloc/all/conanfile.py
+++ recipes/mimalloc/all/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.scm import Version
 import os
 import shutil
 
-required_conan_version = ">=2"
+required_conan_version = ">=2.28"
 
 
 class MimallocConan(ConanFile):
@@ -150,16 +150,18 @@ class MimallocConan(ConanFile):
 
         if self.settings.os == "Windows" and self.options.shared:
             if self.settings.arch == "x86_64":
-                copy(self, "mimalloc-redirect.dll",
-                    src=os.path.join(self.source_folder, "bin"),
-                    dst=os.path.join(self.package_folder, "bin"))
+                if self.options.get_safe("win_redirect"):
+                    copy(self, "mimalloc-redirect.dll",
+                        src=os.path.join(self.source_folder, "bin"),
+                        dst=os.path.join(self.package_folder, "bin"))
                 copy(self, "minject.exe",
                     src=os.path.join(self.source_folder, "bin"),
                     dst=os.path.join(self.package_folder, "bin"))
             elif self.settings.arch == "x86":
-                copy(self, "mimalloc-redirect32.dll",
-                    src=os.path.join(self.source_folder, "bin"),
-                    dst=os.path.join(self.package_folder, "bin"))
+                if self.options.get_safe("win_redirect"):
+                    copy(self, "mimalloc-redirect32.dll",
+                        src=os.path.join(self.source_folder, "bin"),
+                        dst=os.path.join(self.package_folder, "bin"))
                 copy(self, "minject32.exe",
                     src=os.path.join(self.source_folder, "bin"),
                     dst=os.path.join(self.package_folder, "bin"))
@@ -207,6 +209,12 @@ class MimallocConan(ConanFile):
             self.cpp_info.bindirs = []
         else:
             self.cpp_info.libs = [self._lib_name]
+            if self.settings.os == "Windows" and self.options.shared:
+                # When redirection is enabled, there will be a few dlls, we need to ensure
+                # CMake finds the proper mimalloc.dll one and not the redirection dll.
+                self.cpp_info.type = "shared-library"
+                self.cpp_info.link_location = os.path.join(self.package_folder, "lib", f"{self._lib_name}.dll.lib")
+                self.cpp_info.location = os.path.join(self.package_folder, "bin", f"{self._lib_name}.dll")
 
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("pthread")
