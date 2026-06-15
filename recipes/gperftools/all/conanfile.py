d1d9894c9c7022d66a8c8bd64829d216f43f0652
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2026-06-15 17:25:26 +0100

    gperftools, util-linux-libuuid: remove redundant access to private attribute (#30364)
    * gperftools, util-linux-libuuid: remove redundant access to undocumented private attribute
    
    * fix missing import
    
    * fix missing import

--- recipes/gperftools/all/conanfile.py
+++ recipes/gperftools/all/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.apple import fix_apple_shared_install_name, is_apple_os, XCRun
+from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.build import cross_building, check_min_cppstd, stdcpp_library
 from conan.tools.cmake import cmake_layout
 from conan.tools.env import VirtualRunEnv
@@ -138,21 +138,6 @@ class GperftoolsConan(ConanFile):
             args["enable-emergency-malloc"] = self.options.emergency_malloc
         args["with-tcmalloc-alignment"] = self.options.tcmalloc_alignment
         args["with-tcmalloc-pagesize"] = self.options.tcmalloc_pagesize
-
-        # Based on https://github.com/conan-io/conan-center-index/blob/c647b1/recipes/libx264/all/conanfile.py#L94
-        if is_apple_os(self) and self.settings.arch == "armv8":
-            args["host"] = "aarch64-apple-darwin"
-            tc.extra_asflags = ["-arch arm64"]
-            tc.extra_ldflags = ["-arch arm64"]
-            if self.settings.os != "Macos":
-                xcrun = XCRun(self)
-                platform_flags = ["-isysroot", xcrun.sdk_path]
-                apple_min_version_flag = AutotoolsToolchain(self).apple_min_version_flag
-                if apple_min_version_flag:
-                    platform_flags.append(apple_min_version_flag)
-                tc.extra_asflags.extend(platform_flags)
-                tc.extra_cflags.extend(platform_flags)
-                tc.extra_ldflags.extend(platform_flags)
 
         for k, v in args.items():
             if v in [True, False]:
