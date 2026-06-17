7731cb2f411223c77ff1b7bcaae4f2e659b5f91a
Uilian Ries <uilianr@jfrog.com>
2026-06-17 17:54:53 +0200

    Simplify test build and execution
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/poco/all/test_package/conanfile.py
+++ recipes/poco/all/test_package/conanfile.py
@@ -6,26 +6,23 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "VirtualRunEnv"
-    test_type = "explicit"
+    generators = "CMakeDeps"
 
     def layout(self):
         cmake_layout(self)
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+        
+    @property
+    def _tested_poco_modules(self):
+        return ["crypto", "util", "net", "netssl", "data_sqlite", "encodings", "jwt", "prometheus"]
 
     def generate(self):
         tc = CMakeToolchain(self)
         poco_options = self.dependencies["poco"].options
-        tc.variables["TEST_CRYPTO"] = poco_options.enable_crypto
-        tc.variables["TEST_UTIL"] = poco_options.enable_util
-        tc.variables["TEST_NET"] = False
-        tc.variables["TEST_NETSSL"] = False
-        tc.variables["TEST_SQLITE"] = poco_options.enable_data_sqlite
-        tc.variables["TEST_ENCODINGS"] = poco_options.get_safe("enable_encodings", False)
-        tc.variables["TEST_JWT"] = poco_options.get_safe("enable_jwt", False)
-        tc.variables["TEST_PROMETHEUS"] = poco_options.get_safe("enable_prometheus", False)
+        for option in self._tested_poco_modules:
+            tc.cache_variables["TEST_{}".format(option.upper())] = poco_options.get_safe("enable_{}".format(option), False)
         tc.generate()
 
     def build(self):
@@ -35,28 +32,10 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
+            self.run(os.path.join(self.cpp.build.bindir, "core"), env="conanrun")
             poco_options = self.dependencies["poco"].options
-            if poco_options.enable_util:
-                self.run(os.path.join(self.cpp.build.bindirs[0], "util"), env="conanrun")
-            if poco_options.enable_crypto:
-                self.run("{} {}".format(os.path.join(self.cpp.build.bindirs[0], "crypto"), os.path.join(self.source_folder, "conanfile.py")), env="conanrun")
-            if False:
-                if poco_options.enable_net:
-                    self.run(os.path.join(self.cpp.build.bindirs[0], "net"), env="conanrun")
-                    if poco_options.enable_util:
-                        self.run(os.path.join(self.cpp.build.bindirs[0], "net_2"), env="conanrun")
-                test_netssl = os.path.join(self.cpp.build.bindirs[0], "netssl")
-                if os.path.exists(test_netssl):
-                    self.run(test_netssl, env="conanrun")
-            test_sqlite = os.path.join(self.cpp.build.bindirs[0], "sqlite")
-            if os.path.exists(test_sqlite):
-                self.run(test_sqlite, env="conanrun")
-            test_encodings = os.path.join(self.cpp.build.bindirs[0], "encodings")
-            if os.path.exists(test_encodings):
-                self.run(test_encodings, env="conanrun")
-            test_jwt = os.path.join(self.cpp.build.bindirs[0], "jwt")
-            if os.path.exists(test_jwt):
-                self.run(test_jwt, env="conanrun")
-            test_prometheus = os.path.join(self.cpp.build.bindirs[0], "prometheus")
-            if os.path.exists(test_prometheus):
-                self.run(test_prometheus, env="conanrun")
+            for option in self._tested_poco_modules:
+                if poco_options.get_safe("enable_{}".format(option), False):
+                    test_executable = os.path.join(self.cpp.build.bindir, option)
+                    self.run(test_executable, env="conanrun")
+            
