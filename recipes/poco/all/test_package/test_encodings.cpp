d86d2ce60ee8d365e8818b4a6d21a07451e24600
Uilian Ries <uilianr@jfrog.com>
2026-06-17 17:55:27 +0200

    Simplify Poco tests
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/poco/all/test_package/test_encodings.cpp
+++ recipes/poco/all/test_package/test_encodings.cpp
@@ -1,21 +1,10 @@
+#include "Poco/Encodings.h"
 #include "Poco/TextEncoding.h"
-
 #include <iostream>
 
-using namespace Poco;
 
 int main() {
-    auto encoding = TextEncoding::find("utf8");
-    const unsigned char utf8_bytes[] = {0xf0, 0x9f, 0x92, 0x90, 0x00};
-    const int expected_unicode = 0x1f490;
-    int unicode = encoding->convert(utf8_bytes);
-
-    if (unicode != expected_unicode) {
-        std::cerr << "Wrong unicode point!\n"
-                  << "Expected " << expected_unicode << ", got " << unicode << "\n";
-        return 1;
-    }
-    std::cout << "decoded unicode point correct!\n";
-
+    Poco::registerExtraEncodings();
+    std::cout << "Poco Encodings: " << Poco::TextEncoding::find("UTF-8")->canonicalName() << std::endl;
     return 0;
 }
