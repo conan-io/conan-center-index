de09b3d490089d6056061c0f021704a873265be3
Julian Groß <julian.g@posteo.de>
2026-06-18 15:14:46 +0200

    glad/0.*: Don't create reproducible builds on Windows, since GLAD fails to deal with the difference in line-endings. On some Windows systems this might work, due to git's global setting being different when it comes to line endings, but on GitHub Actions this fails.

--- recipes/glad/0.x/conanfile.py
+++ recipes/glad/0.x/conanfile.py
@@ -108,7 +108,8 @@ class GladConan(ConanFile):
         tc.variables["GLAD_EXPORT"] = True
         tc.variables["GLAD_INSTALL"] = True
         tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"  # CMake 4 support
-        tc.cache_variables["GLAD_REPRODUCIBLE"] = True  # Use bundled specs instead of downloading new ones.
+        if self.settings.os != "Windows":
+            tc.cache_variables["GLAD_REPRODUCIBLE"] = True  # Use bundled specs instead of downloading new ones.
         tc.generate()
 
     def build(self):
