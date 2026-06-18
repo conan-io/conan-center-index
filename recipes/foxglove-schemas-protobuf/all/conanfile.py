297d5dfb080be9f0ef1382b8bc5a60cfd92d0526
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-06-18 10:56:31 +0200

    keep shared as a validation

--- recipes/foxglove-schemas-protobuf/all/conanfile.py
+++ recipes/foxglove-schemas-protobuf/all/conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.build import check_min_cppstd
 from conan.tools.files import get, copy
@@ -30,12 +31,6 @@ class FoxgloveSchemasProtobufConan(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain"
     package_type = "library"
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-            del self.options.shared
-            self.package_type = "static-library"
-
     def export_sources(self):
         copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=(self.export_sources_folder + "/src"))
 
@@ -44,6 +39,8 @@ class FoxgloveSchemasProtobufConan(ConanFile):
 
     def validate(self):
         check_min_cppstd(self, 17)
+        if self.settings.os == "Windows" and self.options.shared:
+            raise ConanInvalidConfiguration("Windows shared builds are not supported yet.")
 
     def requirements(self):
         self.requires("protobuf/[>=3.21.12 <7]", transitive_headers=True, transitive_libs=True)
