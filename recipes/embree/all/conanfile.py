e1ade40c62b47816418390aa8742feb879461b1e
Ernesto de Gracia Herranz <ernestodegraciah@gmail.com>
2026-06-18 12:57:10 +0200

    remove recipe changes

--- recipes/embree/all/conanfile.py
+++ recipes/embree/all/conanfile.py
@@ -23,6 +23,7 @@ class EmbreeConan(ConanFile):
         "shared": [True, False],
         "fPIC": [True, False]
     }
+
     default_options = {
         "shared": False,
         "fPIC": True
@@ -42,7 +43,7 @@ class EmbreeConan(ConanFile):
 
     def requirements(self):
         if self.settings.os != "Emscripten":
-            self.requires("onetbb/2022.3.0")
+            self.requires("onetbb/2021.12.0")
 
     def validate(self):
         check_min_cppstd(self, 14)
