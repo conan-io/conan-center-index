2c550ba70dcb5537e8618e1e5626c2e88631cdc9
ericLemanissier <ericLemanissier@users.noreply.github.com>
2026-07-03 11:20:16 +0200

    gdal: use more recent versions of arrow (#30521)
    * gdal: bump arrow
    
    cf https://github.com/OSGeo/gdal/issues/14320
    
    * Restrict arrow version range for compatibility
    
    * Apply suggestion from @AbrilRBS
    
    ---------
    
    Co-authored-by: Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>

--- recipes/gdal/post_3.5.0/conanfile.py
+++ recipes/gdal/post_3.5.0/conanfile.py
@@ -183,7 +183,10 @@ class GdalConan(ConanFile):
         if self.options.with_armadillo:
             self.requires("armadillo/12.6.4")
         if self.options.with_arrow:
-            self.requires("arrow/[>=14.0.2 <20]")
+            if Version(self.version) >= "3.13.0":
+                self.requires("arrow/[>=14.0.2 <25]")
+            else:
+                self.requires("arrow/[>=14.0.2 <20]")
         if self.options.with_basisu:
             self.requires("libbasisu/1.15.0")
         if self.options.with_blosc:
