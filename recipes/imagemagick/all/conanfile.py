b55ca77806f492e19944c12e3f77d5a1ca4ae28d
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2026-07-06 01:58:59 +0200

    Simplify option count, don't remove xml runtime config files

--- recipes/imagemagick/all/conanfile.py
+++ recipes/imagemagick/all/conanfile.py
@@ -28,22 +28,16 @@ class ImageMagicConan(ConanFile):
         "fPIC": [True, False],
         "hdri": [True, False],
         "quantum_depth": [8, 16, 32],
-        "with_zlib": [True, False],
-        "with_bzlib": [True, False],
-        "with_lzma": [True, False],
         "with_lcms": [True, False],
         "with_openexr": [True, False],
         "with_heic": [True, False],
-        "with_jbig": [True, False],
-        "with_jpeg": [None, "libjpeg", "libjpeg-turbo"],
+        "with_jpeg": ["libjpeg", "libjpeg-turbo"],
         "with_openjp2": [True, False],
         "with_pango": [True, False],
-        "with_png": [True, False],
         "with_tiff": [True, False],
         "with_webp": [True, False],
         "with_xml2": [True, False],
         "with_freetype": [True, False],
-        "with_djvu": [True, False],
         "utilities": [True, False],
     }
     default_options = {
@@ -51,22 +45,16 @@ class ImageMagicConan(ConanFile):
         "fPIC": True,
         "hdri": True,
         "quantum_depth": 16,
-        "with_zlib": True,
-        "with_bzlib": True,
-        "with_lzma": True,
         "with_lcms": True,
         "with_openexr": True,
         "with_heic": True,
-        "with_jbig": True,
         "with_jpeg": "libjpeg",
         "with_openjp2": True,
         "with_pango": True,
-        "with_png": True,
         "with_tiff": True,
         "with_webp": False,
         "with_xml2": True,
         "with_freetype": True,
-        "with_djvu": False,
         "utilities": True,
     }
 
@@ -86,20 +74,18 @@ class ImageMagicConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        if self.options.with_zlib:
-            self.requires("zlib/[>=1.2.11 <2]")
-        if self.options.with_bzlib:
-            self.requires("bzip2/1.0.8")
-        if self.options.with_lzma:
-            self.requires("xz_utils/[>=5.4.5 <6]")
+        # Always required: foundational compression/format libraries with no real
+        # reason for a consumer to want them disabled.
+        self.requires("zlib/[>=1.2.11 <2]")
+        self.requires("bzip2/1.0.8")
+        self.requires("xz_utils/[>=5.4.5 <6]")
+        self.requires("libpng/[>=1.6 <2]")
         if self.options.with_lcms:
             self.requires("lcms/[>=2.16 <3]")
         if self.options.with_openexr:
             self.requires("openexr/[>=3.2.3 <4]")
         if self.options.with_heic:
             self.requires("libheif/[>=1.16 <2]")
-        if self.options.with_jbig:
-            self.requires("jbig/20160605")
         if self.options.with_jpeg == "libjpeg":
             self.requires("libjpeg/[>=9e]")
         elif self.options.with_jpeg == "libjpeg-turbo":
@@ -108,8 +94,6 @@ class ImageMagicConan(ConanFile):
             self.requires("openjpeg/[>=2.5.2 <3]")
         if self.options.with_pango:
             self.requires("pango/[>=1.50.7 <2]")
-        if self.options.with_png:
-            self.requires("libpng/[>=1.6 <2]")
         if self.options.with_tiff:
             self.requires("libtiff/[>=4.6.0 <5]")
         if self.options.with_webp:
@@ -118,12 +102,10 @@ class ImageMagicConan(ConanFile):
             self.requires("libxml2/[>=2.12.5 <3]")
         if self.options.with_freetype:
             self.requires("freetype/[>=2.13 <3]")
-        if self.options.with_djvu:
-            # FIXME: missing djvu recipe
-            self.output.warning(
-                "There is no djvu package available on Conan (yet). "
-                "This recipe will use the one present on the system (if available)."
-            )
+            # fontconfig only feeds discovered font paths to freetype for rasterization,
+            # so it is pointless without it. Already pulled in transitively by pango on
+            # most platforms, so this rarely adds a new dependency in practice.
+            self.requires("fontconfig/[>=2.13.91 <3]")
 
     def build_requirements(self):
         self.tool_requires("pkgconf/[>=2.2 <3]")
@@ -147,26 +129,37 @@ class ImageMagicConan(ConanFile):
             "--disable-docs",
             "--with-perl=no",
             "--with-x=no",
-            "--with-fontconfig=no",
             "--enable-hdri={}".format(yes_no(self.options.hdri)),
             "--with-quantum-depth={}".format(self.options.quantum_depth),
-            "--with-zlib={}".format(yes_no(self.options.with_zlib)),
-            "--with-bzlib={}".format(yes_no(self.options.with_bzlib)),
-            "--with-lzma={}".format(yes_no(self.options.with_lzma)),
+            "--with-zlib=yes",
+            "--with-bzlib=yes",
+            "--with-lzma=yes",
+            "--with-png=yes",
+            "--with-jpeg=yes",
             "--with-lcms={}".format(yes_no(self.options.with_lcms)),
             "--with-openexr={}".format(yes_no(self.options.with_openexr)),
             "--with-heic={}".format(yes_no(self.options.with_heic)),
-            "--with-jbig={}".format(yes_no(self.options.with_jbig)),
-            "--with-jpeg={}".format(yes_no(self.options.with_jpeg)),
             "--with-openjp2={}".format(yes_no(self.options.with_openjp2)),
             "--with-pango={}".format(yes_no(self.options.with_pango)),
-            "--with-png={}".format(yes_no(self.options.with_png)),
             "--with-tiff={}".format(yes_no(self.options.with_tiff)),
             "--with-webp={}".format(yes_no(self.options.with_webp)),
             "--with-xml={}".format(yes_no(self.options.with_xml2)),
             "--with-freetype={}".format(yes_no(self.options.with_freetype)),
-            "--with-djvu={}".format(yes_no(self.options.with_djvu)),
+            "--with-fontconfig={}".format(yes_no(self.options.with_freetype)),
             "--with-utilities={}".format(yes_no(self.options.utilities)),
+            # Not packaged by this recipe and auto-detected by default (--with-X=yes)
+            # otherwise. Disable them explicitly so the build does not silently link
+            # against whatever happens to be installed on the build machine.
+            "--without-jbig",
+            "--without-djvu",
+            "--without-zip",
+            "--without-zstd",
+            "--without-raqm",
+            "--without-gvc",
+            "--without-dmr",
+            "--without-jxl",
+            "--without-lqr",
+            "--without-raw",
         ]
         tc.generate()
 
@@ -187,8 +180,6 @@ class ImageMagicConan(ConanFile):
         autotools.install()
 
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
-        rmdir(self, os.path.join(self.package_folder, "etc"))
-        rmdir(self, os.path.join(self.package_folder, "share"))
         rm(self, "*.la", os.path.join(self.package_folder, "lib"))
         fix_apple_shared_install_name(self)
 
@@ -198,29 +189,18 @@ class ImageMagicConan(ConanFile):
 
     def package_info(self):
         # FIXME model official FindImageMagick https://cmake.org/cmake/help/latest/module/FindImageMagick.html
-        core_requires = []
-        if self.options.with_zlib:
-            core_requires.append("zlib::zlib")
-        if self.options.with_bzlib:
-            core_requires.append("bzip2::bzip2")
-        if self.options.with_lzma:
-            core_requires.append("xz_utils::xz_utils")
+        core_requires = ["zlib::zlib", "bzip2::bzip2", "xz_utils::xz_utils", "libpng::libpng"]
+        core_requires.append("{0}::{0}".format(self.options.with_jpeg))
         if self.options.with_lcms:
             core_requires.append("lcms::lcms")
         if self.options.with_openexr:
             core_requires.append("openexr::openexr")
         if self.options.with_heic:
             core_requires.append("libheif::libheif")
-        if self.options.with_jbig:
-            core_requires.append("jbig::jbig")
-        if self.options.with_jpeg:
-            core_requires.append("{0}::{0}".format(self.options.with_jpeg))
         if self.options.with_openjp2:
             core_requires.append("openjpeg::openjpeg")
         if self.options.with_pango:
             core_requires.append("pango::pango")
-        if self.options.with_png:
-            core_requires.append("libpng::libpng")
         if self.options.with_tiff:
             core_requires.append("libtiff::libtiff")
         if self.options.with_webp:
@@ -229,6 +209,7 @@ class ImageMagicConan(ConanFile):
             core_requires.append("libxml2::libxml2")
         if self.options.with_freetype:
             core_requires.append("freetype::freetype")
+            core_requires.append("fontconfig::fontconfig")
 
         if self.settings.os in ("Linux", "FreeBSD"):
             self.cpp_info.components["MagickCore"].system_libs.append("pthread")
