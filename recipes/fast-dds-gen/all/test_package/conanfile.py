d60d53b54ced264d150b6f3f4b043b2efa07bc41
Francisco Ramirez de Anton <franchuti688@gmail.com>
2026-07-08 18:35:51 +0200

    Ensuring we require the correct pkg_id

--- recipes/fast-dds-gen/all/test_package/conanfile.py
+++ recipes/fast-dds-gen/all/test_package/conanfile.py
@@ -5,8 +5,8 @@ from conan.tools.build import can_run
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+    def requirements(self):
+        self.requires(self.tested_reference_str, run=True)
 
     def test(self):
         if can_run(self):
