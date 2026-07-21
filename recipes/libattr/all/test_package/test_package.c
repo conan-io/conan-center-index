36b05a5fdf67ac72b68d8ebb8db6be14e7636da0
Roman Ondráček <mail@romanondracek.cz>
2026-07-21 02:55:51 +0200

    libattr: publish version 2.6.0
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

--- recipes/libattr/all/test_package/test_package.c
+++ recipes/libattr/all/test_package/test_package.c
@@ -1,12 +1,11 @@
 #include <sys/types.h>
-#include <attr/attributes.h>
+#include <sys/xattr.h>
 
 int main(int argc, char* argv[])
 {
     char value[255];
-    int len = sizeof(value);
     const char *path = "file_not_exist.txt";
     const char *attr = "attr_not_exist";
-    attr_get(path, attr, value, &len, 0);
+    getxattr(path, attr, value, sizeof(value));
     return 0;
 }
