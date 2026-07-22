42eea919a339aea7085b51575660ee789a5baa8e
Carlos Zoido <mrgalleta@gmail.com>
2026-07-22 15:02:33 +0200

    prepend

--- recipes/cpython-interpreter/all/test_package/conanfile.py
+++ recipes/cpython-interpreter/all/test_package/conanfile.py
@@ -2,7 +2,7 @@ from io import StringIO
 
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.env import Environment
+from conan.tools.env import Environment, VirtualRunEnv
 
 
 class TestPackageConan(ConanFile):
@@ -12,11 +12,13 @@ class TestPackageConan(ConanFile):
         self.requires(self.tested_reference_str)
 
     def generate(self):
-        # Disable Conan's automatic run env so a profile's [runenv] PATH entry
-        # can't outrank this package's bindir below.
-        self.virtualrunenv = False
+        # Prepend this package's bindir with precedence so it can't be outranked
+        # by a profile's [runenv] PATH entry, while keeping the rest of the
+        # auto-generated run env (other deps' PATH/LD_LIBRARY_PATH/etc.).
+        run_env = VirtualRunEnv(self).environment()
         env = Environment()
         env.prepend_path("PATH", self.dependencies[self.tested_reference_str].cpp_info.bindir)
+        env.compose_env(run_env)
         env.vars(self, scope="run").save_script("conanrunenv")
 
     def test(self):
