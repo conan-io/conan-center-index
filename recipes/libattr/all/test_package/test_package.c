fd2cd4e759426c758980e140bce6de8d69ab14ed
Uilian Ries <uilianr@jfrog.com>
2026-07-22 11:55:53 +0200

    Update test package to make sure will consume libattr
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/libattr/all/test_package/test_package.c
+++ recipes/libattr/all/test_package/test_package.c
@@ -1,11 +1,11 @@
-#include <sys/types.h>
-#include <sys/xattr.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <attr/libattr.h>
 
-int main(int argc, char* argv[])
-{
-    char value[255];
-    const char *path = "file_not_exist.txt";
-    const char *attr = "attr_not_exist";
-    getxattr(path, attr, value, sizeof(value));
-    return 0;
+
+int main(void) {
+    printf("libattr test package: attr_copy_file\n");
+    attr_copy_file("file_not_exist.txt", "attr_not_exist", NULL, NULL);
+    return EXIT_SUCCESS;
 }
