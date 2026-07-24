644035fa6df2e5b94d2c1549bb4a19056ed943f3
Uilian Ries <uilianr@jfrog.com>
2026-07-24 13:59:24 +0200

    Remove code related to old versions
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/laszip/all/conanfile.py
+++ recipes/laszip/all/conanfile.py
@@ -5,13 +5,13 @@ from conan.tools.files import apply_conandata_patches, copy, export_conandata_pa
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.54.0"
+required_conan_version = ">=2.1"
 
 
 class LaszipConan(ConanFile):
     name = "laszip"
     description = "C++ library for lossless LiDAR compression."
-    license = "LGPL-2.1"
+    license = "Apache-2.0"
     topics = ("las", "laz", "lidar", "compression", "decompression")
     homepage = "https://github.com/LASzip/LASzip"
     url = "https://github.com/conan-io/conan-center-index"
@@ -27,10 +27,6 @@ class LaszipConan(ConanFile):
         "fPIC": True,
     }
 
-    @property
-    def _min_cppstd(self):
-        return 14
-
     def export_sources(self):
         export_conandata_patches(self)
 
@@ -39,14 +35,11 @@ class LaszipConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
-        if Version(self.version) >= "3.4.4":
-            self.license = "Apache-2.0"
         if self.options.shared:
             self.options.rm_safe("fPIC")
-    
+
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
+        check_min_cppstd(self, 17)
 
     def layout(self):
         cmake_layout(self, src_folder="src")
@@ -66,20 +59,14 @@ class LaszipConan(ConanFile):
         cmake.build()
 
     def package(self):
-        # The license file was renamed from "COPYING" to "COPYING.txt" in 3.5.0
-        license_file = "COPYING.txt" if Version(self.version) >= "3.5.0" else "COPYING"
-        copy(self, license_file, src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "COPYING.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
         suffix = Version(self.version).major if self.settings.os == "Windows" else ""
         self.cpp_info.libs = [f"laszip{suffix}"]
-        if Version(self.version) >= "3.5.0":
-            # Since 3.5.0, laszip_api.h only uses the installed include layout
-            # (<laszip/laszip_common.h>) when LASZIP_API_VERSION is defined; otherwise
-            # it falls back to the in-tree <laszip_common.h> which we do not ship.
-            self.cpp_info.defines.append("LASZIP_API_VERSION")
+        self.cpp_info.includedirs.append(os.path.join("include", "laszip"))
         if self.options.shared:
             self.cpp_info.defines.append("LASZIP_DYN_LINK")
         else:
