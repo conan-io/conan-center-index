2a85a3014e81436a07a2e01d196c7c98229b800a
ericLemanissier <ericLemanissier@users.noreply.github.com>
2026-08-03 07:10:16 +0000

    ftxui 7.0.2

--- recipes/ftxui/all/conanfile.py
+++ recipes/ftxui/all/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.scm import Version
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 import os
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2"
 
 class FTXUIConan(ConanFile):
     name = "ftxui"
@@ -26,43 +26,17 @@ class FTXUIConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
+    implements = ["auto_shared_fpic"]
 
-    @property
-    def _min_cppstd(self):
-        return 17
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-			"gcc": "8",
-			"clang": "7",
-			"apple-clang": "12",
-			"Visual Studio": "16",
-			"msvc": "192",
-		}
 
     def export_sources(self):
         export_conandata_patches(self)
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def validate(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
+        check_min_cppstd(self, 17)
 
         if is_msvc(self) and self.options.shared and is_msvc_static_runtime(self):
             raise ConanInvalidConfiguration("shared with static runtime not supported")
@@ -92,15 +66,14 @@ class FTXUIConan(ConanFile):
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
-        if Version(self.version) >= "4.1.0":
-            rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
-        if Version(self.version) >= "4.1.1":
-            rm(self, "ftxui.pc", os.path.join(self.package_folder, "lib"), )
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rm(self, "ftxui.pc", os.path.join(self.package_folder, "lib"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "ftxui")
-        if Version(self.version) >= "4.1.0":
-            self.cpp_info.set_property("pkg_config_name", "ftxui")
+        self.cpp_info.set_property("pkg_config_name", "ftxui")
+        if Version(self.version) >= "7.0.0":
+            self.cpp_info.set_property("cmake_target_name", "ftxui::ftxui")
 
         self.cpp_info.components["ftxui-dom"].set_property("cmake_target_name", "ftxui::dom")
         self.cpp_info.components["ftxui-dom"].libs = ["ftxui-dom"]
@@ -116,13 +89,5 @@ class FTXUIConan(ConanFile):
         self.cpp_info.components["ftxui-component"].set_property("cmake_target_name", "ftxui::component")
         self.cpp_info.components["ftxui-component"].libs = ["ftxui-component"]
         self.cpp_info.components["ftxui-component"].requires = ["ftxui-dom"]
-        if self.settings.os in ["Linux", "FreeBSD"]:
+        if self.settings.os in ["Linux", "FreeBSD"] and Version(self.version) < "7.0.0":
             self.cpp_info.components["ftxui-component"].system_libs.append("pthread")
-
-        # TODO: to remove in conan v2 once cmake_find_package* generators removed
-        self.cpp_info.components["ftxui-dom"].names["cmake_find_package"] = "dom"
-        self.cpp_info.components["ftxui-dom"].names["cmake_find_package_multi"] = "dom"
-        self.cpp_info.components["ftxui-screen"].names["cmake_find_package"] = "screen"
-        self.cpp_info.components["ftxui-screen"].names["cmake_find_package_multi"] = "screen"
-        self.cpp_info.components["ftxui-component"].names["cmake_find_package"] = "component"
-        self.cpp_info.components["ftxui-component"].names["cmake_find_package_multi"] = "component"
