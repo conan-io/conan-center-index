82bae3c403aa65a3334ff833ed13654f390218c2
Glenn Fiedler <glenn@mas-bandwidth.com>
2026-08-06 06:05:12 -0700

    netcode/1.4.3: new recipe (#30730)
    * netcode/1.4.3: new recipe
    
    C library for secure client/server UDP connections from mas-bandwidth.
    Uses Conan libsodium (NETCODE_SYSTEM_SODIUM=ON) so the vendored crypto is
    never compiled. 1.4.0 fixed AEAD nonce reuse across in-process restart
    (GHSA-3x95-24j9-7448); packaging current 1.4.3. Dependency of yojimbo.
    Submitted by the upstream author.
    
    * Recipe cleanup
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    
    ---------
    
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>
    Co-authored-by: Uilian Ries <uilianr@jfrog.com>

---
+++ recipes/netcode/all/conanfile.py
@@ -0,0 +1,63 @@
+import os
+
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get
+from conan.tools.microsoft import is_msvc
+
+required_conan_version = ">=2.4.0"
+
+
+class NetcodeConan(ConanFile):
+    name = "netcode"
+    description = "A protocol for secure client/server connections over UDP"
+    license = "BSD-3-Clause"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/mas-bandwidth/netcode"
+    topics = ("udp", "networking", "games", "security", "client-server")
+    package_type = "library"
+    languages = "C"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    implements = ["auto_shared_fpic"]
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.16.0]")
+
+    def requirements(self):
+        self.requires("libsodium/[~1.0.20]")
+
+    def validate(self):
+        if self.options.shared and is_msvc(self):
+            raise ConanInvalidConfiguration("does not export symbols when built as shared library with Visual Studio")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["NETCODE_SYSTEM_SODIUM"] = True
+        tc.cache_variables["NETCODE_BUILD_TESTS"] = False
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "LICENCE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["netcode"]
+        if self.settings.os == "Windows":
+            self.cpp_info.system_libs = ["ws2_32", "iphlpapi"]
