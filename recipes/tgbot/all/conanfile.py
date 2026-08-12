db8cb65e616a1c24678fcae0307ff835973f5cfb
Oleg Morozenkov <o-morozenkov@yandex-team.ru>
2026-08-12 17:01:46 +0300

    Fix tgbot 1.12.2 recipe

--- recipes/tgbot/all/conanfile.py
+++ recipes/tgbot/all/conanfile.py
@@ -80,3 +80,6 @@ class TgbotConan(ConanFile):
         self.cpp_info.set_property("cmake_target_name", "TgBot::TgBot")
         if self.options.shared:
             self.cpp_info.defines.append("TGBOT_DLL")
+        if self.settings.os == "Windows":
+            self.cpp_info.system_libs.append("ws2_32")
+            self.cpp_info.defines.extend(["_WIN32_WINNT=0x0601", "WIN32_LEAN_AND_MEAN", "NOMINMAX"])
