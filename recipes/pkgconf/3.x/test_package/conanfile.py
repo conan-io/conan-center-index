3c309cfb1432da91b86169defab32928310a8e8b
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-08-18 13:31:19 +0200

    Simplify test package

--- recipes/pkgconf/3.x/test_package/conanfile.py
+++ recipes/pkgconf/3.x/test_package/conanfile.py
@@ -2,7 +2,7 @@ from io import StringIO
 
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.env import Environment, VirtualBuildEnv
+from conan.tools.env import VirtualBuildEnv
 from conan.tools.layout import basic_layout
 
 
@@ -16,10 +16,6 @@ class TestPackageConan(ConanFile):
         basic_layout(self)
 
     def generate(self):
-        # Expose `PKG_CONFIG_PATH` to be able to find libexample1.pc
-        env = Environment()
-        env.prepend_path("PKG_CONFIG_PATH", self.recipe_folder)
-        env.vars(self, scope="run").save_script("pkgconf-config-path")
         # Check build environment postconditions
         buildenv = VirtualBuildEnv(self)
         env = buildenv.vars(scope='build')
@@ -28,12 +24,10 @@ class TestPackageConan(ConanFile):
         buildenv.generate()
 
     def test(self):
-        # Check that we can find pkgconf in build environment
+        # Check that we can find pkgconf
         # and that it is the expected version
         if can_run(self):
             output = StringIO()
             self.run("pkgconf --about", output, env="conanrun")
             pkgconf_expected_version = self.dependencies[self.tested_reference_str].ref.version
             assert f"pkgconf {pkgconf_expected_version}" in output.getvalue()
-
-            self.run("pkgconf libexample1 -cflags", env="conanrun")
