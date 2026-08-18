a6e6360eff26a54b07348ceb91411f7bb3bb5445
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2026-08-18 11:44:16 +0100

    libgsf: review cleanup

--- recipes/libgsf/all/conanfile.py
+++ recipes/libgsf/all/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.apple import fix_apple_shared_install_name
 from conan.tools.files import copy, get, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain, PkgConfigDeps
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft import is_msvc, unix_path
+from conan.tools.microsoft import is_msvc
 
 required_conan_version = ">=2.1"
 
@@ -28,7 +28,6 @@ class LibgsfConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    implements = ["auto_shared_fpic"]
 
     def configure(self):
         if self.options.shared:
@@ -41,7 +40,7 @@ class LibgsfConan(ConanFile):
 
     def requirements(self):
         # INFO: gsf/gsf-fwd.h includes glib-object.h
-        self.requires("glib/2.81.0", transitive_headers=True, transitive_libs=True)
+        self.requires("glib/[>=2.81.0 <3]", transitive_headers=True, transitive_libs=True)
         # INFO: gsf/gsf-libxml.h includes libxml/parser.h
         self.requires("libxml2/[>=2.12.5 <3]", transitive_headers=True, transitive_libs=True)
         self.requires("zlib/[>=1.2.11 <2]")
@@ -51,16 +50,12 @@ class LibgsfConan(ConanFile):
             # INFO: upstream configure.ac requires POSIX headers not shipped by MSVC
             # (<sys/time.h> for struct timeval) and looks up zlib as -lz, which does
             # not match the zlib.lib/zdll.lib produced for MSVC.
-            raise ConanInvalidConfiguration(f"{self.ref} does not support MSVC. Use MinGW instead.")
+            raise ConanInvalidConfiguration("msvc is not supported by libgsf")
 
     def build_requirements(self):
         # upstream configure.ac relies on PKG_CHECK_MODULES
         if not self.conf.get("tools.gnu:pkg_config", check_type=str):
             self.tool_requires("pkgconf/[>=2.2 <3]")
-        if self.settings_build.os == "Windows":
-            self.win_bash = True
-            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
-                self.tool_requires("msys2/cci.latest")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -68,7 +63,7 @@ class LibgsfConan(ConanFile):
     def generate(self):
         tc = AutotoolsToolchain(self)
         # zlib is looked up by hand instead of pkg-config
-        zlib_root = unix_path(self, self.dependencies["zlib"].package_folder)
+        zlib_root = self.dependencies["zlib"].package_folder
         tc.configure_args.extend([
             f"--with-zlib={zlib_root}",
             "--without-bz2",
@@ -79,7 +74,8 @@ class LibgsfConan(ConanFile):
         ])
         tc.generate()
 
-        PkgConfigDeps(self).generate()
+        deps = PkgConfigDeps(self)
+        deps.generate()
 
     def build(self):
         autotools = Autotools(self)
