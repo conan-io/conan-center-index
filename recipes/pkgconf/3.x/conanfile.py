2b597aad1ab0734d619d6be0b6fef7fa95490044
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-08-18 13:54:33 +0200

    Fix licenes

--- recipes/pkgconf/3.x/conanfile.py
+++ recipes/pkgconf/3.x/conanfile.py
@@ -15,7 +15,7 @@ class PkgConfConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     topics = ("build", "configuration")
     homepage = "https://git.sr.ht/~kaniini/pkgconf"
-    license = "ISC"
+    license = ("pkgconf", "BSD-2-Clause")
     description = "package compiler and linker metadata toolkit"
     settings = "os", "arch", "compiler", "build_type"
     languages = "C"
