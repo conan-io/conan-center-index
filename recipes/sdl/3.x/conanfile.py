conan-io/conan-center-index

commit e2f2d21495c89de60d5319874ff9e3a63f21e9a3

sdl3: only require libusb if explicitly requested (#27779)
* sdl3: refactor logic around libusb

* Update recipes/sdl/3.x/conanfile.py

* remove unused imports

recipes/sdl/3.x/conanfile.py
@@ -1,11 +1,10 @@
-from conan import ConanFile, conan_version
+from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
-from conan.tools.files import get, replace_in_file, copy, rmdir
+from conan.tools.files import get, copy, replace_in_file, rmdir
 from conan.tools.gnu import PkgConfigDeps
 from conan.tools.microsoft import is_msvc
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2"
@@ -95,7 +94,7 @@ class SDLConan(ConanFile):
             "metal": True,
             "directx": True,
             ## Hidapi
-            "libusb": True,
+            "libusb": False,
             ## Other
             "libudev": True,
             "dbus": True,
@@ -161,7 +160,9 @@ class SDLConan(ConanFile):
             self.options.rm_safe("xshape")
             self.options.rm_safe("xsync")
 
-        if not self.options.get_safe("hidapi"):
+        if not self.options.get_safe("hidapi") or self.settings.os in ["Android", "iOS", "tvOS", "visionOS", "watchOS"]:
+            # libusb is only an option if hidapi is enabled: https://github.com/libsdl-org/SDL/blob/db3a35e9bc3aa245dfbe67585b3f67d7b4b62845/cmake/sdlchecks.cmake#L1111
+            # libusb is not available on Android/iOS/tvOS/visionOS/watchOS https://github.com/libsdl-org/SDL/blob/db3a35e9bc3aa245dfbe67585b3f67d7b4b62845/CMakeLists.txt#L159-L160
             self.options.rm_safe("libusb")
 
     def validate(self):
@@ -173,11 +174,6 @@ class SDLConan(ConanFile):
                         raise ConanInvalidConfiguration(f'-o="&:{subsystem}=True" subsystem requires -o="&:{dependency}=True"')
 
     def validate_build(self):
-        # TODO: Remove this one new CMakeDeps is default
-        if conan_version >= "2.12" and self._needs_libusb and self.dependencies["libusb"].options.get_safe("shared", True)\
-            and not self.conf.get("tools.cmake.cmakedeps:new"):
-            raise ConanInvalidConfiguration("SDL with shared libusb requires new CMakeDeps generator")
-
         if self.settings.os == "Android" and not self.conf.get("user.sdl:android", False):
             raise ConanInvalidConfiguration("SDL builds on android require extra configuration on the user's side. "
                                             "Set -c user.sdl:android=True if you understand it and want to build it")
@@ -188,16 +184,16 @@ class SDLConan(ConanFile):
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
+        # Prevent inspecting target properties for libusb to derive the name of the .so/.dll
+        # instead, just link the library normally, see
+        # https://github.com/libsdl-org/SDL/blob/96292a5b464258a2b926e0a3d72f8b98c2a81aa6/cmake/sdlchecks.cmake#L1107-L1113
+        replace_in_file(self, os.path.join(self.source_folder, "cmake", "sdlchecks.cmake"),
+                        "target_get_dynamic_library(dynamic_libusb", "#target_get_dynamic_library(dynamic_libusb")
+
     @property
     def _is_unix_sys(self):
         """ True for UNIX but not Macos/Android"""
         return self.settings.os in ("Linux", "FreeBSD")
-
-    @property
-    def _needs_libusb(self):
-        return (self.options.get_safe("libusb") and
-                (not is_apple_os(self) or self.settings.os == "Macos") and
-                self.settings.os != "Android")
 
     @property
     def _supports_opengl(self):
@@ -216,7 +212,7 @@ class SDLConan(ConanFile):
     def requirements(self):
         if self.options.get_safe("libiconv"):
             self.requires("libiconv/1.17")
-        if self._needs_libusb:
+        if self.options.get_safe("libusb"):
             self.requires("libusb/1.0.26")
         if self._supports_opengl:
             self.requires("opengl/system")
@@ -266,10 +262,12 @@ class SDLConan(ConanFile):
         if self._supports_opengles:
             tc.cache_variables["SDL_OPENGLES"] = True
 
-        tc.cache_variables["SDL_HIDAPI_LIBUSB"] = self._needs_libusb
-        if self._needs_libusb:
-            # TODO: This is a supported configuration in upstream
-            tc.cache_variables["SDL_HIDAPI_LIBUSB_SHARED"] = self.dependencies["libusb"].options.get_safe("shared", True)
+        if self.options.hidapi:
+            tc.cache_variables["SDL_HIDAPI_LIBUSB"] = self.options.get_safe("libusb")
+            # Prevent loading shared libusb during runtime
+            # This just means it will be linked traditionally, even when libusb is shared
+            # See https://github.com/libsdl-org/SDL/blob/96292a5b464258a2b926e0a3d72f8b98c2a81aa6/cmake/sdlchecks.cmake#L1107-L1113
+            tc.cache_variables["SDL_HIDAPI_LIBUSB_SHARED"] = False
 
         tc.variables["SDL_VULKAN"] = self.options.get_safe("vulkan")
         tc.variables["SDL_METAL"] = self.options.get_safe("metal")
@@ -319,18 +317,7 @@ class SDLConan(ConanFile):
         pcdeps = PkgConfigDeps(self)
         pcdeps.generate()
 
-    def _patch_sources(self):
-        # TODO: Once new CMakeDeps is default, remove this
-        # Right now this would fail at runtime for shared libusb, but we're
-        # validating it out for now. The new incubating cmakedeps in 2.12 is required
-        # to build that configuration, else remove the failing line in the old one
-        if conan_version < "2.12" or not self.conf.get("tools.cmake.cmakedeps:new"):
-            replace_in_file(self, os.path.join(self.source_folder, "cmake", "sdlchecks.cmake"),
-                        "target_get_dynamic_library(dynamic_libusb LibUSB::LibUSB)",
-                        "")
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -342,7 +329,6 @@ class SDLConan(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "cmake"))
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
-
 
     @property
     def _is_clang_cl(self):
@@ -376,7 +362,7 @@ class SDLConan(ConanFile):
         if self.options.get_safe("libudev"):
             self.cpp_info.components["sdl3"].requires.append("libudev::libudev")
 
-        if self._needs_libusb:
+        if self.options.get_safe("libusb"):
             self.cpp_info.components["sdl3"].requires.append("libusb::libusb")
 
         if self.options.get_safe("dbus"):

commit cf7791b7e2df7e6f09b550bfe674236c13704cb3

sdl: fix Android build linking host libusb
On Android/iOS/tvOS/visionOS/watchOS the libusb option is removed in
configure(), so self.options.get_safe("libusb") returns None. Assigning
None to SDL_HIDAPI_LIBUSB did not force the CMake variable off, so SDL
auto-detected and linked the host system libusb, breaking cross-compilation
(ld.lld: ...libusb-1.0.so is incompatible with aarch64linux).

Force the variable to a real bool so it is False where the option is
unavailable. Regression from #27779.

recipes/sdl/3.x/conanfile.py
@@ -267,7 +267,10 @@ class SDLConan(ConanFile):
         tc.cache_variables["SDL_OPENGLES"] = bool(self._supports_opengles)
 
         if self.options.hidapi:
-            tc.cache_variables["SDL_HIDAPI_LIBUSB"] = self.options.get_safe("libusb")
+            # get_safe returns None where the libusb option is removed (Android/iOS/tvOS/etc).
+            # Force the CMake variable to False there, otherwise SDL auto-detects and links the
+            # host system libusb, breaking cross-compilation.
+            tc.cache_variables["SDL_HIDAPI_LIBUSB"] = bool(self.options.get_safe("libusb", False))
             # Prevent loading shared libusb during runtime
             # This just means it will be linked traditionally, even when libusb is shared
             # See https://github.com/libsdl-org/SDL/blob/96292a5b464258a2b926e0a3d72f8b98c2a81aa6/cmake/sdlchecks.cmake#L1107-L1113