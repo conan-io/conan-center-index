86c4ad0e8f2fdbd19e9c8f3a84a4957d443472c3
Joseph Schmitz <jalsatcu@gmail.com>
2026-08-20 20:49:59 -0600

    one last comment to fix ci linting

--- recipes/cyclonedds-cxx/all/conanfile.py
+++ recipes/cyclonedds-cxx/all/conanfile.py
@@ -164,6 +164,5 @@ class CycloneDDSCXXConan(ConanFile):
         self.cpp_info.components["idlcxx"].type = 'shared-library'
         self.cpp_info.components["idlcxx"].set_property("cmake_target_name", "CycloneDDS-CXX::idlcxx")
         self.cpp_info.components["idlcxx"].requires = ["cyclonedds::idl"]
-        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
         self.buildenv_info.append_path("PATH", os.path.join(self.package_folder, "bin"))
         self.runenv_info.append_path("PATH", os.path.join(self.package_folder, "bin"))
