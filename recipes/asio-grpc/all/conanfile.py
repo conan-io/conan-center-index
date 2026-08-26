cf7f11e627c6c483e35ec59658ad962db86fe11f
Artem <36299791+chghehe@users.noreply.github.com>
2026-08-26 11:50:39 +0300

    Update recipes/asio-grpc/all/conanfile.py
    Co-authored-by: Francisco Ramírez <franchuti688@gmail.com>

--- recipes/asio-grpc/all/conanfile.py
+++ recipes/asio-grpc/all/conanfile.py
@@ -2,7 +2,6 @@ from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, get, rm
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=2.1"
