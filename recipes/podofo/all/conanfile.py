53a681c7f61f647cfc7bf0805c092b9991742023
Uilian Ries <uilianr@jfrog.com>
2026-08-27 13:22:34 +0200

    Fix license copying
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/podofo/all/conanfile.py
+++ recipes/podofo/all/conanfile.py
@@ -130,7 +130,7 @@ class PodofoConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "COPYING*", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
