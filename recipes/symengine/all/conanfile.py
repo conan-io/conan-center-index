6ccc55a053e43f6e6b8fe62b54eccab3526839a2
Uilian Ries <uilianr@jfrog.com>
2026-09-07 12:41:37 +0200

    Kepp only version 0.15.0
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/symengine/all/conanfile.py
+++ recipes/symengine/all/conanfile.py
@@ -1,18 +1,8 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
-from conan.tools.files import (
-    apply_conandata_patches,
-    collect_libs,
-    copy,
-    export_conandata_patches,
-    get,
-    rm,
-    rmdir,
-    replace_in_file,
-)
-from conan.tools.layout import basic_layout
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.files import collect_libs, copy, get, rm, rmdir, replace_in_file
 from conan.tools.microsoft import is_msvc_static_runtime
 from conan.tools.scm import Version
 
@@ -39,13 +29,9 @@ class SymengineConan(ConanFile):
         "fPIC": True,
         "integer_class": "gmp",
     }
-    short_paths = True
-
-    def export_sources(self):
-        export_conandata_patches(self)
 
     def layout(self):
-        basic_layout(self, src_folder="src")
+        cmake_layout(self, src_folder="src")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -62,16 +48,8 @@ class SymengineConan(ConanFile):
         if self.settings.compiler == "gcc" and Version(self.settings.compiler.version) < "7":
             raise ConanInvalidConfiguration(f"{self.ref} requires GCC >= 7")
 
-    @property
-    def _needs_fast_float(self):
-        return Version(self.version) >= "0.13.0"
-
-    @property
-    def _hardcodes_cxx_flags(self):
-        # Until 0.15.0 upstream forced -std=c++11 and -fPIC into CMAKE_CXX_FLAGS.
-        # Newer versions use target_compile_features(symengine PUBLIC cxx_std_11)
-        # and leave the PIC setting to the consumer.
-        return Version(self.version) < "0.15.0"
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.18.0]")
 
     def requirements(self):
         if self.options.integer_class == "boostmp":
@@ -79,8 +57,7 @@ class SymengineConan(ConanFile):
             self.requires("boost/1.86.0", transitive_headers=True)
         else:
             self.requires("gmp/6.3.0", transitive_headers=True, transitive_libs=True)
-        if self._needs_fast_float:
-            self.requires("fast_float/6.1.5")
+        self.requires("fast_float/6.1.5")
 
     def source(self):
         get(
@@ -89,7 +66,9 @@ class SymengineConan(ConanFile):
             strip_root=True,
             destination=self.source_folder,
         )
-        apply_conandata_patches(self)
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                                "set(LIBS ${LIBS} ${GMP_TARGETS})",
+                                "set(LIBS ${LIBS} gmp::gmp)")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -97,8 +76,7 @@ class SymengineConan(ConanFile):
         tc.variables["BUILD_BENCHMARKS"] = False
         tc.variables["INTEGER_CLASS"] = self.options.integer_class
         tc.variables["MSVC_USE_MT"] = is_msvc_static_runtime(self)
-        if self._needs_fast_float:
-            tc.variables["WITH_SYSTEM_FASTFLOAT"] = True
+        tc.variables["WITH_SYSTEM_FASTFLOAT"] = True
 
         tc.generate()
         deps = CMakeDeps(self)
@@ -107,27 +85,10 @@ class SymengineConan(ConanFile):
             # If we ever add support for gmpxx, we should set this property
             # if self.dependencies["gmp"].options.enable_cxx:
             #     deps.set_property("gmp::gmpxx", "cmake_target_name", "gmpxx")
-        if self._needs_fast_float:
-            deps.set_property("fast_float", "cmake_file_name", "FASTFLOAT")
+        deps.set_property("fast_float", "cmake_file_name", "FASTFLOAT")
         deps.generate()
 
-    def _patch_sources(self):
-        if self._hardcodes_cxx_flags:
-            # Disable hardcoded C++11
-            replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-                            'set(CMAKE_CXX_FLAGS "${CXX11_OPTIONS} ${CMAKE_CXX_FLAGS}")',
-                            '')
-            # Let Conan choose fPIC
-            replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-                            'set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${common}")',
-                            '')
-        # cmake_target_name not working?
-        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
-                        "set(LIBS ${LIBS} ${GMP_TARGETS})",
-                        "set(LIBS ${LIBS} gmp::gmp)")
-
     def build(self):
-        self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -154,3 +115,4 @@ class SymengineConan(ConanFile):
             self.cpp_info.system_libs.append("m")
 
         self.cpp_info.set_property("cmake_target_name", "symengine")
+        self.cpp_info.set_property("cmake_file_name", "SymEngine")
