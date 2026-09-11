bd78a392eb026780d2311a598cb2c3d839d1b83b
Francisco Ramirez de Anton <franchuti688@gmail.com>
2026-09-11 12:30:30 +0200

    Removed unused import and patches

--- recipes/shaderc/all/conanfile.py
+++ recipes/shaderc/all/conanfile.py
@@ -6,7 +6,6 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.scm import Version
 
 required_conan_version = ">=2"
 
