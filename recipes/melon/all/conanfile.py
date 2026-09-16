2a796f8a21939e6fc5d17ae9d54aaf658a352ae4
Uilian Ries <uilianries@gmail.com>
2026-09-16 17:27:01 +0200

    Require clang-16
    Co-authored-by: Francisco Ramírez <franchuti688@gmail.com>

--- recipes/melon/all/conanfile.py
+++ recipes/melon/all/conanfile.py
@@ -29,7 +29,7 @@ class MelonConan(ConanFile):
 
     def validate(self):
         check_min_cppstd(self, 23)
-        compilers = {"gcc": "14", "clang": "16", "apple-clang": "21", "msvc": "194"}
+        compilers = {"gcc": "14", "clang": "18", "apple-clang": "21", "msvc": "194"}
         if str(self.settings.compiler) in compilers and Version(self.settings.compiler.version) < compilers[str(self.settings.compiler)]:
             raise ConanInvalidConfiguration(f"{self.settings.compiler} version must be at least {compilers[str(self.settings.compiler)]}. See https://github.com/fhamonic/melon#installation")
 
