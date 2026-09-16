5f3495108aa6fcd8cbe379b3e9d0ec06c75d2a39
Carlos Zoido <mrgalleta@gmail.com>
2026-09-16 08:45:49 +0200

    reduce test_package

--- recipes/ouster_sdk/all/test_package/test_package.cpp
+++ recipes/ouster_sdk/all/test_package/test_package.cpp
@@ -23,17 +23,13 @@
 #endif
 
 int main() {
-    const ouster::sdk::core::LidarFrameFieldTypes field_types;
-    ouster::sdk::core::LidarFrame frame(64, 1024, field_types, 16);
-    std::cout << "Successfully created a core::LidarFrame object with " << frame.fields().size()
-              << " fields" << std::endl;
+    ouster::sdk::core::LidarFrame frame(64, 1024, ouster::sdk::core::LidarFrameFieldTypes{}, 16);
+    std::cout << "Successfully created a core::LidarFrame object" << std::endl;
 
-    ouster::sdk::core::ArrayX3dR points(1, 3);
-    points << 1.0, 2.0, 3.0;
-    ouster::sdk::core::ArrayX3dR normals(1, 3);
-    normals << 0.0, 0.0, 1.0;
-    auto downsampled = ouster::sdk::algorithm::voxel_downsample_with_normals(points, normals, 0.5);
-    std::cout << "Successfully downsampled " << downsampled.first.rows() << " point(s)" << std::endl;
+    ouster::sdk::core::ArrayX3dR points(0, 3);
+    ouster::sdk::core::ArrayX3dR normals(0, 3);
+    ouster::sdk::algorithm::voxel_downsample_with_normals(points, normals, 0.5);
+    std::cout << "Successfully called algorithm::voxel_downsample_with_normals" << std::endl;
 
 #ifdef WITH_OSF
     ouster::sdk::osf::Writer writer("tmp.osf");
@@ -55,9 +51,8 @@ int main() {
 
 #ifdef WITH_SENSOR
     try {
-        std::vector<std::string> sensors;
         ouster::sdk::sensor::SensorFrameSetSource sensor_source(
-            sensors, ouster::sdk::sensor::SensorFrameSetSourceOptions{});
+            std::vector<std::string>{}, ouster::sdk::sensor::SensorFrameSetSourceOptions{});
     } catch (const std::exception&) {
     }
     std::cout << "Successfully created a sensor::SensorFrameSetSource object" << std::endl;
@@ -65,10 +60,7 @@ int main() {
 
 #ifdef WITH_MAPPING
     ouster::sdk::mapping::SolverConfig config;
-    config.key_frame_distance = 1.5;
-    config.loss_function = ouster::sdk::mapping::LossFunction::HUBER_LOSS;
-    const std::string serialized = ouster::sdk::mapping::serialize_constraints_to_json(config);
-    std::cout << "Successfully serialized a mapping::SolverConfig of " << serialized.size()
-              << " bytes" << std::endl;
+    ouster::sdk::mapping::serialize_constraints_to_json(config);
+    std::cout << "Successfully serialized a mapping::SolverConfig object" << std::endl;
 #endif
 }
