02087ce2f5be584d1b6c4d3102c1f0d6d23ebf60
Francisco Ramirez de Anton <franchuti688@gmail.com>
2026-09-23 10:43:54 +0200

    Unused import

--- recipes/moltenvk/all/conanfile.py
+++ recipes/moltenvk/all/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.errors import ConanException, ConanInvalidConfiguration
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
