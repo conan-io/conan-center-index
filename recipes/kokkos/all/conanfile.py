45260fdc2807133113970953246f6154fcfcb2e0
Uilian Ries <uilianr@jfrog.com>
2026-09-23 10:09:12 +0200

    Remove unused imports
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/kokkos/all/conanfile.py
+++ recipes/kokkos/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy, get, rm, rmdir
+from conan.tools.files import copy, get, rmdir
 import os
 
 
