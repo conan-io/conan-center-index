f223c16efd4f2276d97bda10669c9842cc4eeffb
Marian Klymov <nekto1989@gmail.com>
2026-09-24 15:17:24 +0300

    Undo removal of Find*.cmake files

--- recipes/pcre2/all/conanfile.py
+++ recipes/pcre2/all/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rmdir
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 from conan.tools.scm import Version
 import os
@@ -123,8 +123,6 @@ class PCRE2Conan(ConanFile):
             # Do not add ${PROJECT_SOURCE_DIR}/cmake because versions older than 10.47 contain
             #  a custom FindPackageHandleStandardArgs.cmake which can break conan generators
             replace_in_file(self, cmakelists, "LIST(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake)", "")
-        rm(self, "FindEditline.cmake", os.path.join(self.source_folder, "cmake"))
-        rm(self, "FindReadline.cmake", os.path.join(self.source_folder, "cmake"))
 
         # pcre2-config does not correctly include '-static' in static library names
         if is_msvc(self):
