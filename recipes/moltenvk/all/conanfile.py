bf420fea6fdafee69c35cca578435f8a6aec5282
Francisco Ramírez <franchuti688@gmail.com>
2026-09-25 09:29:02 +0200

    moltenvk: publish version 1.4.2 (#30980)
    * Publish latest 1.4.2 version. Stop publishing revs for older versions
    
    * change cmake_target_name. Fix requires
    
    * Patch
    
    * Using vulkan SDK 1.4.357.0 version
    
    * Remove patch fields
    
    * Remove large comments
    
    * No public headers
    
    * Add patch instead of replacement line. Fix wrong cmake min version
    
    * Patch fpic. Require cmake>=3.21
    
    * change package_type to shared-library
    
    * Unused import
    
    * Simplify CMake patch
    
    * Applied suggestions. Improved patch to avoid printing CPM warning

--- recipes/moltenvk/all/conanfile.py
+++ recipes/moltenvk/all/conanfile.py
@@ -1,14 +1,12 @@
 from conan import ConanFile
-from conan.errors import ConanException, ConanInvalidConfiguration
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.apple import is_apple_os
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get
-from conan.tools.scm import Version
-import functools
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 import os
-import yaml
 
-required_conan_version = ">=1.53.0"
+required_conan_version = ">=2"
 
 
 class MoltenVKConan(ConanFile):
@@ -22,118 +20,71 @@ class MoltenVKConan(ConanFile):
     topics = ("moltenvk", "khronos", "vulkan", "metal")
     homepage = "https://github.com/KhronosGroup/MoltenVK"
     url = "https://github.com/conan-io/conan-center-index"
-
-    package_type = "library"
+    package_type = "shared-library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
-        "hide_vulkan_symbols": [True, False],
-        "with_spirv_tools": [True, False],
         "tools": [True, False],
     }
     default_options = {
-        "shared": True,
-        "fPIC": True,
-        "hide_vulkan_symbols": False,
-        "with_spirv_tools": True,
         "tools": True,
     }
 
-    @property
-    def _dependencies_filename(self):
-        return f"dependencies-{self.version}.yml"
-
-    @property
-    @functools.lru_cache(1)
-    def _dependencies_versions(self):
-        dependencies_filepath = os.path.join(self.recipe_folder, "dependencies", self._dependencies_filename)
-        if not os.path.isfile(dependencies_filepath):
-            raise ConanException(f"Cannot find {dependencies_filepath}")
-        cached_dependencies = yaml.safe_load(open(dependencies_filepath))
-        return cached_dependencies
-
-    @property
-    def _min_cppstd(self):
-        return 11 if Version(self.version) < "1.1.9" else 17
-
-    @property
-    def _has_hide_vulkan_symbols_option(self):
-        return Version(self.version) >= "1.1.7"
-
-    def export(self):
-        copy(self, f"dependencies/{self._dependencies_filename}", self.recipe_folder, self.export_folder)
-
     def export_sources(self):
-        copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
         export_conandata_patches(self)
-
-    def config_options(self):
-        if not self._has_hide_vulkan_symbols_option:
-            del self.options.hide_vulkan_symbols
-
-    def configure(self):
-        if self.options.shared:
-            del self.options.fPIC
-        elif self._has_hide_vulkan_symbols_option:
-            self.options.rm_safe("hide_vulkan_symbols")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
         self.requires("cereal/1.3.2")
-        self.requires(self._require("glslang"))
-        self.requires(self._require("spirv-cross"))
-        self.requires(self._require("vulkan-headers"), transitive_headers=True)
-        if self.options.with_spirv_tools:
-            self.requires(self._require("spirv-tools"))
-
-    def _require(self, recipe_name):
-        if recipe_name not in self._dependencies_versions:
-            raise ConanException(f"{recipe_name} is missing in {self._dependencies_filename}")
-        return f"{recipe_name}/{self._dependencies_versions[recipe_name]}"
+        version = "1.4.357.0"
+        self.requires(f"spirv-cross/{version}")
+        self.requires(f"vulkan-headers/{version}", transitive_headers=True)
+        self.requires(f"spirv-tools/{version}")
 
     def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
-        if self.settings.os not in ["Macos", "iOS", "tvOS"]:
-            raise ConanInvalidConfiguration(f"{self.ref} only supported on MacOS, iOS and tvOS")
-        if self.settings.compiler != "apple-clang":
-            raise ConanInvalidConfiguration(f"{self.ref} requires apple-clang")
-        if Version(self.settings.compiler.version) < "12.0":
-            raise ConanInvalidConfiguration(f"{self.ref} requires XCode 12.0 or higher")
+        check_min_cppstd(self, 17)
+        if not is_apple_os(self):
+            raise ConanInvalidConfiguration("Only supported on MacOS, iOS and tvOS")
         spirv_cross = self.dependencies["spirv-cross"]
         if spirv_cross.options.shared or not (spirv_cross.options.msl and spirv_cross.options.reflect):
-            raise ConanInvalidConfiguration(f"{self.ref} requires spirv-cross static with msl & reflect enabled")
+            raise ConanInvalidConfiguration("Requires spirv-cross static with msl & reflect enabled")
+
+    def build_requirements(self):
+        # cmake/MoltenVK/MoltenVK_CPM_Cache.cmake uses file(REAL_PATH ... EXPAND_TILDE),
+        # which requires CMake >= 3.21, even though upstream only checks for >= 3.18
+        self.tool_requires("cmake/[>=3.21]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        self._patch_sources()
+
+    def _patch_sources(self):
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["MVK_SRC_DIR"] = self.source_folder.replace("\\", "/")
-        tc.variables["MVK_VERSION"] = self.version
-        tc.variables["MVK_WITH_SPIRV_TOOLS"] = self.options.with_spirv_tools
-        tc.variables["MVK_BUILD_SHADERCONVERTER_TOOL"] = self.options.tools
-        if self._has_hide_vulkan_symbols_option and self.options.shared:
-            tc.variables["MVK_HIDE_VULKAN_SYMBOLS"] = self.options.hide_vulkan_symbols
+        tc.variables["MVK_BUILD_SHADER_CONVERTER_TOOL"] = self.options.tools
         tc.generate()
         deps = CMakeDeps(self)
+        deps.set_property("spirv-cross", "cmake_target_name", "SPRIV-Cross::SPRIV-Cross")
+        deps.set_property("spirv-tools", "cmake_target_name", "SPIRV-Tools::SPIRV-Tools")
         deps.generate()
 
     def build(self):
-        apply_conandata_patches(self)
         cmake = CMake(self)
-        cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
+        cmake.configure()
         cmake.build()
 
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
+        self.cpp_info.set_property("pkg_config_name", "MoltenVK")
+        self.cpp_info.includedirs = []
         self.cpp_info.libs = ["MoltenVK"]
         self.cpp_info.frameworks = ["Metal", "Foundation", "CoreFoundation", "QuartzCore", "IOSurface", "CoreGraphics"]
         if self.settings.os == "Macos":
@@ -142,19 +93,11 @@ class MoltenVKConan(ConanFile):
             self.cpp_info.frameworks.append("UIKit")
 
         self.cpp_info.requires = [
-            "cereal::cereal", "glslang::glslang-core", "glslang::spirv", "spirv-cross::spirv-cross-core",
+            "cereal::cereal", "spirv-cross::spirv-cross-core",
             "spirv-cross::spirv-cross-msl", "spirv-cross::spirv-cross-reflect", "vulkan-headers::vulkan-headers",
         ]
-        if self.options.with_spirv_tools:
-            self.cpp_info.requires.append("spirv-tools::spirv-tools-core")
+        self.cpp_info.requires.append("spirv-tools::spirv-tools-core")
 
-        if self.options.shared:
-            moltenvk_icd_path = os.path.join(self.package_folder, "lib", "MoltenVK_icd.json")
-            self.runenv_info.prepend_path("VK_DRIVER_FILES", moltenvk_icd_path)
-            self.runenv_info.prepend_path("VK_ICD_FILENAMES", moltenvk_icd_path)
-            # TODO: to remove after conan v2, it allows to not break consumers still relying on virtualenv generator
-            self.env_info.VK_DRIVER_FILES.append(moltenvk_icd_path)
-            self.env_info.VK_ICD_FILENAMES.append(moltenvk_icd_path)
-
-        if self.options.tools:
-            self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+        moltenvk_icd_path = os.path.join(self.package_folder, "etc", "vulkan", "icd.d", "MoltenVK_icd.json")
+        self.runenv_info.prepend_path("VK_DRIVER_FILES", moltenvk_icd_path)
+        self.runenv_info.prepend_path("VK_ICD_FILENAMES", moltenvk_icd_path)
